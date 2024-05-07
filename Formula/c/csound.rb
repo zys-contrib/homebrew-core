d16aeb844e12c9e5cf26eaac9d5a296a309ad4dd
Rui Chen <rui@chenrui.dev>
2024-05-07 09:18:09 -0400

    csound: revision bump (hdf5 1.14.4)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/csound.rb
+++ Formula/c/csound.rb
@@ -2,7 +2,7 @@ class Csound < Formula
   desc "Sound and music computing system"
   homepage "https://csound.com"
   license "LGPL-2.1-or-later"
-  revision 9
+  revision 10
   head "https://github.com/csound/csound.git", branch: "master"
 
   # Remove `stable` block when patches are no longer needed
