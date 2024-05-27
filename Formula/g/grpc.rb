8b004916e2430200ddf50611b6dfde93290104db
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-05-27 23:21:02 +0800

    grpc: revision bump (protobuf 27.0)

--- Formula/g/grpc.rb
+++ Formula/g/grpc.rb
@@ -2,6 +2,7 @@ class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://grpc.io/"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/grpc/grpc.git", branch: "master"
 
   stable do
