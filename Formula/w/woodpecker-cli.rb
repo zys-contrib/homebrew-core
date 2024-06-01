2d3f05f6c0303541d12a76b653e0dda2bd2044c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-01 12:39:48 +0000

    woodpecker-cli: update 2.5.0 bottle.

--- Formula/w/woodpecker-cli.rb
+++ Formula/w/woodpecker-cli.rb
@@ -7,13 +7,13 @@ class WoodpeckerCli < Formula
   head "https://github.com/woodpecker-ci/woodpecker.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b8916f1b08a5c3b40e049c88c3b5a57bac8d217a204b86f9437951a77053a2be"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f3b0dc1504bf0b553e57ba9d2592cabeea908ba1f92260179e85ad110834c99c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b321f4402f4a8ee6c378140cd8f35cd9599a25e49091c61cab041d838893a69a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "7a8349e92fe1dcc88fcdc931a6739b6781c6994ffaf84a981f893c9ee94ebd4d"
-    sha256 cellar: :any_skip_relocation, ventura:        "ada36bf0f39acd9f675cd6308dda4d373ac8b75570681940fad39652a1b33d7e"
-    sha256 cellar: :any_skip_relocation, monterey:       "4eba21254de390359a9dc6895efcafe9df3b5c3d8474040eb907a0a4ed8b16ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f786e1fc6f2c8395bbf3945c8935f98144a44cbb8b87180cd25c05c5379b088"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4cdd5322acffbb7ee18313f339ed8b63723499888d1906f840cce59e85e7fd5b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3e9c74f5ba947cc8236e6d14c3993438776ca109d816f13846396b80a1360229"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "36e1eb9f3ed54ea67fdf9696c544329a4877ed443acf664e8aa6c8b52351a7db"
+    sha256 cellar: :any_skip_relocation, sonoma:         "fafc1b9ecce4df8188fcccacbacf33a8b548980c396344e216b66ba95e4e1e20"
+    sha256 cellar: :any_skip_relocation, ventura:        "daa61ea57e113cecdd8057407b381da6135880c9d6f2547f7f7c464fd91b5120"
+    sha256 cellar: :any_skip_relocation, monterey:       "0f1510db1c38ba804bc6d6ec855bc7c877ebdd2799577342f2c4ef6072465199"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d99d430a95631c65267f324cd899577457111aa0dd40c991c8e89355f687fb5"
   end
 
   depends_on "go" => :build
