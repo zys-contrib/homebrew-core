1ef3ce373bbaf2ac8eae6a2d61e86fca2935b232
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-01 07:45:00 +0000

    sonar-scanner: update 6.0.0.4432 bottle.

--- Formula/s/sonar-scanner.rb
+++ Formula/s/sonar-scanner.rb
@@ -7,7 +7,13 @@ class SonarScanner < Formula
   head "https://github.com/SonarSource/sonar-scanner-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "13fadfccf23d8bffe46a1dfac508917900a967c558ba1e5970109967f404b7ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
+    sha256 cellar: :any_skip_relocation, ventura:        "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
+    sha256 cellar: :any_skip_relocation, monterey:       "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0dc9079487bbc35160e68c4c9fab52d391e3381eb91d8ce781538b2ec7763c49"
   end
 
   depends_on "openjdk"
