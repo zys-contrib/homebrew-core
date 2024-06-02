75325226663b7acd997fe46e9a53b8e0f1f482e1
Branch Vincent <branchevincent@gmail.com>
2024-06-02 16:37:46 -0700

    reorder-python-imports 3.13.0

--- Formula/r/reorder-python-imports.rb
+++ Formula/r/reorder-python-imports.rb
@@ -3,8 +3,8 @@ class ReorderPythonImports < Formula
 
   desc "Rewrites source to reorder python imports"
   homepage "https://github.com/asottile/reorder_python_imports"
-  url "https://files.pythonhosted.org/packages/f2/a4/b9299bb0ce17c405fda20efa39f6993b418b10c765e978961f3c97254f4e/reorder_python_imports-3.12.0.tar.gz"
-  sha256 "f93106a662b0c034ca81c91fd1c2f21a1e94ece47c9f192672e2a13c8ec1856c"
+  url "https://files.pythonhosted.org/packages/ae/f8/63ecf759c9149d7d7a8b612ebfe74901164dde9adcb1c40975ddc713db1c/reorder_python_imports-3.13.0.tar.gz"
+  sha256 "994235fe9273373af6df7290de6a362a2426eb9bb800f5197367fe54b081f4d9"
   license "MIT"
   head "https://github.com/asottile/reorder_python_imports.git", branch: "main"
 
