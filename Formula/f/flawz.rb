d4b66d2ea05d0048c69b0fe1aca03d6f6ee9b5c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-02 21:29:20 +0000

    flawz 0.2.1

--- Formula/f/flawz.rb
+++ Formula/f/flawz.rb
@@ -1,8 +1,8 @@
 class Flawz < Formula
   desc "Terminal UI for browsing security vulnerabilities (CVEs)"
   homepage "https://github.com/orhun/flawz"
-  url "https://github.com/orhun/flawz/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "dea2e679665ab8b9c2d39cca9911470b2ed0d57e386bb53d22de0774bac86ac6"
+  url "https://github.com/orhun/flawz/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "a20fd49a2a69fce1bc248c40dc159569d8f5db9117e468af3c589b7dde3b16f3"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/orhun/flawz.git", branch: "main"
 
