5b669e8910d7f3ade7292372b427d2ecd41696de
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-06-03 10:35:16 +0000

    oils-for-unix: add 0.22.0 bottle.

--- Formula/o/oils-for-unix.rb
+++ Formula/o/oils-for-unix.rb
@@ -10,6 +10,16 @@ class OilsForUnix < Formula
     regex(/href=.*?oils-for-unix[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "52ae2b5e7abe69ca0f9960b0a77e079729161578ce874b2cf327a09d8058c7e9"
+    sha256 cellar: :any,                 arm64_ventura:  "52f5d4e0ed390c42557419ffa92b7cb30bb4b78d87808a499f44d1993905caa4"
+    sha256 cellar: :any,                 arm64_monterey: "f79f2c3b7a1173f135f72d6d2b43cc1b20f79543e7f460bfb69c140022d298f3"
+    sha256 cellar: :any,                 sonoma:         "b545329dfb7aca7b9aa7546d64ec3705aa710e87bfcbd3bbe03201933ab7a091"
+    sha256 cellar: :any,                 ventura:        "c6bc66b8d7dad9dc715c2d829926a010d6146a1b47fe7dc4665c136f7457c719"
+    sha256 cellar: :any,                 monterey:       "ebfcaca05a8f8b90eb4c3f51f2fdac082dc8adcd475dc46846e1a55b1566bcb2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67a8f588f5a36d18210b4a5b12c3343ca234c96242076e588f071536f77306f0"
+  end
+
   depends_on "readline"
 
   def install
