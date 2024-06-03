c277b7322773c8686eed72eeeed3b1af327eb2aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-03 21:41:51 +0000

    nebula 1.9.2

--- Formula/n/nebula.rb
+++ Formula/n/nebula.rb
@@ -1,8 +1,8 @@
 class Nebula < Formula
   desc "Scalable overlay networking tool for connecting computers anywhere"
   homepage "https://github.com/slackhq/nebula"
-  url "https://github.com/slackhq/nebula/archive/refs/tags/v1.9.1.tar.gz"
-  sha256 "c4dc2138d67cd25925ebb788e6013cbb0cd6c94e84e76eeddea4dea1ddd22e86"
+  url "https://github.com/slackhq/nebula/archive/refs/tags/v1.9.2.tar.gz"
+  sha256 "8eae7e8158657d2ccabef24e4ddaf04e1903b5435c699b49f2cc63a21239a777"
   license "MIT"
   head "https://github.com/slackhq/nebula.git", branch: "master"
 
