a2922beb6f4c8e6d59c6d04dc27f69a603e88753
Rui Chen <rui@chenrui.dev>
2024-06-04 09:40:35 -0400

    spades 4.0.0
    autobump: add spades
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    spades: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/spades.rb
+++ Formula/s/spades.rb
@@ -3,22 +3,10 @@ class Spades < Formula
 
   desc "De novo genome sequence assembly"
   homepage "https://github.com/ablab/spades"
+  url "https://github.com/ablab/spades/releases/download/v4.0.0/SPAdes-4.0.0.tar.gz"
+  sha256 "07c02eb1d9d90f611ac73bdd30ddc242ed51b00c8a3757189e8a8137ad8cfb8b"
   license "GPL-2.0-only"
-
-  # TODO: Remove stable dependencies and fails_with in the next release.
-  # Instead, the head dependencies should be used everywhere.
-  stable do
-    url "https://github.com/ablab/spades/releases/download/v3.15.5/SPAdes-3.15.5.tar.gz"
-    sha256 "155c3640d571f2e7b19a05031d1fd0d19bd82df785d38870fb93bd241b12bbfa"
-
-    on_macos do
-      depends_on "gcc"
-    end
-
-    fails_with :clang do
-      cause "fails to link with recent `libomp`"
-    end
-  end
+  head "https://github.com/ablab/spades.git", branch: "next"
 
   livecheck do
     url :stable
@@ -33,14 +21,6 @@ class Spades < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "c284a1b00ebca8794f3f4396f481c09228aedc0ea4b6d2ad364a4efb29115a7e"
   end
 
-  head do
-    url "https://github.com/ablab/spades.git", branch: "next"
-
-    on_macos do
-      depends_on "libomp"
-    end
-  end
-
   depends_on "cmake" => :build
   depends_on "python@3.12"
 
@@ -48,14 +28,14 @@ class Spades < Formula
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
+  on_macos do
+    depends_on "libomp"
+  end
+
   on_linux do
     depends_on "jemalloc"
     depends_on "readline"
   end
-
-  # Drop distutils, upstream commit doesn't apply cleanly
-  # https://github.com/ablab/spades/commit/3ba9e5254b7d1ccb0c55d42b7d38b8be6f7d0648
-  patch :DATA
 
   def install
     system "cmake", "-S", "src", "-B", "build", *std_cmake_args
@@ -68,58 +48,3 @@ class Spades < Formula
     assert_match "TEST PASSED CORRECTLY", shell_output("#{bin}/spades.py --test")
   end
 end
-
-__END__
-diff --git a/src/spades_pipeline/support.py b/src/spades_pipeline/support.py
-index c66faf0..ac1af7d 100644
---- a/src/spades_pipeline/support.py
-+++ b/src/spades_pipeline/support.py
-@@ -20,7 +20,6 @@ import sys
- import tempfile
- import traceback
- from platform import uname
--from distutils.version import LooseVersion
- from os.path import abspath, expanduser, join
-
- import options_storage
-@@ -95,30 +94,16 @@ def sys_error(cmd, log, exit_code):
-
-
- def check_python_version():
--    def __next_version(version):
--        components = version.split('.')
--        for i in reversed(range(len(components))):
--            if components[i].isdigit():
--                components[i] = str(int(components[i]) + 1)
--                break
--        return '.'.join(components)
--
--    current_version = sys.version.split()[0]
--    supported_versions_msg = []
--    for supported_versions in options_storage.SUPPORTED_PYTHON_VERSIONS:
--        major = supported_versions[0]
--        if '-' in supported_versions:  # range
--            min_inc, max_inc = supported_versions.split('-')
--        elif supported_versions.endswith('+'):  # half open range
--            min_inc, max_inc = supported_versions[:-1], major
--        else:  # exact version
--            min_inc = max_inc = supported_versions
--        max_exc = __next_version(max_inc)
--        supported_versions_msg.append("Python%s: %s" % (major, supported_versions.replace('+', " and higher")))
--        if LooseVersion(min_inc) <= LooseVersion(current_version) < LooseVersion(max_exc):
--            return True
--    error("python version %s is not supported!\n"
--          "Supported versions are %s" % (current_version, ", ".join(supported_versions_msg)))
-+    MINIMAL_PYTHON_VERSION = (3, 2)
-+
-+    if sys.version_info < MINIMAL_PYTHON_VERSION:
-+        error(
-+            "\nPython version %s is not supported!\n"
-+            "Minimal supported version is %s"
-+            % (sys.version.split()[0], ".".join(list(map(str, MINIMAL_PYTHON_VERSION))))
-+        )
-+        return False
-+    return True
-
-
- def get_spades_binaries_info_message():
