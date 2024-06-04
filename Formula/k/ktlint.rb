ec540df08b067a9a2c8f9d0c286c4c60d3085a1a
Sha Sha Chu <shasha@pinterest.com>
2024-06-04 11:19:11 -0700

    ktlint 1.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/k/ktlint.rb
+++ Formula/k/ktlint.rb
@@ -1,8 +1,8 @@
 class Ktlint < Formula
   desc "Anti-bikeshedding Kotlin linter with built-in formatter"
   homepage "https://ktlint.github.io/"
-  url "https://github.com/pinterest/ktlint/releases/download/1.2.1/ktlint-1.2.1.zip"
-  sha256 "a6d84ae60fa5fa3b0a40af8d9f88ec7a0de789a3bbd5515c11e629b1820c32b6"
+  url "https://github.com/pinterest/ktlint/releases/download/1.3.0/ktlint-1.3.0.zip"
+  sha256 "4c2cf93a1ba306edb3505b3333960752fe84896be4bd04134340c64fd6836f43"
   license "MIT"
 
   bottle do
