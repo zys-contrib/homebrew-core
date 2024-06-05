c94a86ea2f0e962a5fa2118fe05856d528170306
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-05 09:32:34 +0000

    fleet-cli 0.9.5

--- Formula/f/fleet-cli.rb
+++ Formula/f/fleet-cli.rb
@@ -1,8 +1,8 @@
 class FleetCli < Formula
   desc "Manage large fleets of Kubernetes clusters"
   homepage "https://github.com/rancher/fleet"
-  url "https://github.com/rancher/fleet/archive/refs/tags/v0.9.4.tar.gz"
-  sha256 "e78ec7c02ec9437c079bbe95e4355ca7c9704ee043f2310cce2b512a195e58f4"
+  url "https://github.com/rancher/fleet/archive/refs/tags/v0.9.5.tar.gz"
+  sha256 "6aaebcf7d52594c527083f4bdc74fadfc2fb881c0afa6b334b238712ed6b71c4"
   license "Apache-2.0"
   head "https://github.com/rancher/fleet.git", branch: "master"
 
