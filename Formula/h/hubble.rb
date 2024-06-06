83b8296e7534c0c86fcb69b3d70b11518c91d2d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-06 00:39:28 +0000

    hubble 0.13.5

--- Formula/h/hubble.rb
+++ Formula/h/hubble.rb
@@ -1,8 +1,8 @@
 class Hubble < Formula
   desc "Network, Service & Security Observability for Kubernetes using eBPF"
   homepage "https://github.com/cilium/hubble"
-  url "https://github.com/cilium/hubble/archive/refs/tags/v0.13.4.tar.gz"
-  sha256 "5a1103bae2242df080436ee3ac3f0904b0208b5a00bf77c0a016f466a76203fe"
+  url "https://github.com/cilium/hubble/archive/refs/tags/v0.13.5.tar.gz"
+  sha256 "67cfcebbeade12d1b0ed7ab8b623186cf0cd0ab9fd8a82677ab7c06c9bb6da20"
   license "Apache-2.0"
 
   # There can be a notable gap between when a version is tagged and a
