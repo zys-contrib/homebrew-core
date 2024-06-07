16f7440757fe309d49474f228607f4c48c6605d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-07 15:37:38 +0000

    liboqs 0.10.1

--- Formula/lib/liboqs.rb
+++ Formula/lib/liboqs.rb
@@ -1,8 +1,8 @@
 class Liboqs < Formula
   desc "Library for quantum-safe cryptography"
   homepage "https://openquantumsafe.org/"
-  url "https://github.com/open-quantum-safe/liboqs/archive/refs/tags/0.10.0.tar.gz"
-  sha256 "2b7b4436be9825dd0adaf000ee4f322f06551e638a8a9c8d54eda48ed40e40a9"
+  url "https://github.com/open-quantum-safe/liboqs/archive/refs/tags/0.10.1.tar.gz"
+  sha256 "00ca8aba65cd8c8eac00ddf978f4cac9dd23bb039f357448b60b7e3eed8f02da"
   license "MIT"
 
   livecheck do
