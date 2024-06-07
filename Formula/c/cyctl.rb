75ae620201c63d1e84cf26a86bf7bbaedfd08358
Rui Chen <rui@chenrui.dev>
2024-06-07 15:06:17 +0000

    cyctl: add 0.6.2 bottle.

--- Formula/c/cyctl.rb
+++ Formula/c/cyctl.rb
@@ -6,6 +6,16 @@ class Cyctl < Formula
   license "Apache-2.0"
   head "https://github.com/cyclops-ui/cyclops.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0c5474e77d0e272daed4e3e2dd84d0bc5e3e8e61637b5de354c297e2bde22c82"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9e8263547ae547ac607efd8464f50560b8120780025023f0f68d6753b1e67aad"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c09fcd6c55798a3b3d365e4e45b232abdfe1b397f3ea028b7492952ea760bf9a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a60885c8ba77a7ed901f3a82653f522702e9e5565c3107ff3c32c3cc8354ae92"
+    sha256 cellar: :any_skip_relocation, ventura:        "236cbf3b52be6ee20b8bc75421047c6c0e085598a8234a6a44b282084fc014af"
+    sha256 cellar: :any_skip_relocation, monterey:       "18836736cefdbb1cffb78fd02dddec4fab2c3118d8b13d6742d2a3c1080d1acd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4671995661a9d02e5eefddd142e23bf31d88c279400bffcfe96a20fac39c33b8"
+  end
+
   depends_on "go" => :build
 
   def install
