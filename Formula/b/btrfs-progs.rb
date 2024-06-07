af9d5a7f2bde1ec896d05fc3f39d68ec8f79e763
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-07 18:25:56 +0000

    btrfs-progs 6.9

--- Formula/b/btrfs-progs.rb
+++ Formula/b/btrfs-progs.rb
@@ -1,8 +1,8 @@
 class BtrfsProgs < Formula
   desc "Userspace utilities to manage btrfs filesystems"
   homepage "https://btrfs.readthedocs.io/en/latest/"
-  url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.8.1.tar.xz"
-  sha256 "0e40a068a26c2969cb02a95ba9fef888d7a6356e3f457ff92ad2477d08735678"
+  url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.9.tar.xz"
+  sha256 "7e14a5d597f323dd7d1b453e3a4e661a7e9f07ea060efbff4f76ff8315917de8"
   license all_of: [
     "GPL-2.0-only",
     "LGPL-2.1-or-later", # libbtrfsutil
