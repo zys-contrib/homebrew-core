9557e7e1d97502001981b5b04905bae821170d17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-08 15:51:40 +0000

    typstfmt: update 0.2.10 bottle.

--- Formula/t/typstfmt.rb
+++ Formula/t/typstfmt.rb
@@ -7,13 +7,13 @@ class Typstfmt < Formula
   head "https://github.com/astrale-sharp/typstfmt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "90a1443ca38d6ed8b1a6cc21102622ad5b830d82948ec590d0435b010c147b80"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f2385acb81ddf04da547278e4beb2bcd6c8b75d6798e29cded3d1ed7300d2940"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b5b960ec044c1a222f614e9fbfce9b5b844e7f5a040d1c93c583b0b74ebe2c4f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "13b69d0ba9cf2046a896e2a2aa49aafbe640ca594d05ff2ab221288eb7c76e7a"
-    sha256 cellar: :any_skip_relocation, ventura:        "52c9b488a0f712675ba7a36b68f383e991fda985d6bed8e2062de3e46369f8cc"
-    sha256 cellar: :any_skip_relocation, monterey:       "f1b2b049e67eb424c44c33ae4e2321d7ed018eb5c8b373640224df3ba8e1719a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3f2282db238b16329daff514c79902b36c8d0c0035f1bc45a0749641568889de"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "844ad26eecdbd64cc9e98bea828c52772bb45a0f6fdc0e79a8ac8a8092679cc2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "582d6c33840b510206e0e80d85406a2f16de67fdcb93102f480330cb226b81d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bb0272d469193fa0dadcca3ee4d379204a58774ad69b84ec3330064793fecaa8"
+    sha256 cellar: :any_skip_relocation, sonoma:         "f676e54a7b0e56cce946b8da875e5c991490b6d9e25dbd573ce58dc788a0139f"
+    sha256 cellar: :any_skip_relocation, ventura:        "73843c71221c4b6ac172f2a3f62f0549841074a7b09ef5ef4794b9c88fd0be3e"
+    sha256 cellar: :any_skip_relocation, monterey:       "8bd83b378a237b4be80690c7ad71e7c5609e3b85c6179f0d5a5806179ee1321a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "560dc8c7913216f620d70b2322c5c384a756eee645d4db0369d798660fd0feef"
   end
 
   deprecate! date: "2024-06-08", because: :unmaintained
