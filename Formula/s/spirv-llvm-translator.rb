c099ef1ea457afc3d42ff4c1c10cc7113b97f98a
Rui Chen <rui@chenrui.dev>
2024-06-08 19:29:55 -0400

    spirv-llvm-translator 18.1.1
    autobump: add spirv-llvm-translator
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/spirv-llvm-translator.rb
+++ Formula/s/spirv-llvm-translator.rb
@@ -1,8 +1,8 @@
 class SpirvLlvmTranslator < Formula
   desc "Tool and a library for bi-directional translation between SPIR-V and LLVM IR"
   homepage "https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
-  url "https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v18.1.0.tar.gz"
-  sha256 "78a770eff24d5ffe2798479845adec4b909cbf058ddc55830ea00fa7d2c1698a"
+  url "https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v18.1.1.tar.gz"
+  sha256 "c1c7aee4ea23a6a1089bb7f7bad198c28ada65c5b7671434562fe0241d8674d6"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
