018fdcc7eab6703d70a208940ffa0bd68667a91c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-09 12:50:45 +0000

    typstyle 0.11.25

--- Formula/t/typstyle.rb
+++ Formula/t/typstyle.rb
@@ -1,8 +1,8 @@
 class Typstyle < Formula
   desc "Beautiful and reliable typst code formatter"
   homepage "https://enter-tainer.github.io/typstyle/"
-  url "https://github.com/Enter-tainer/typstyle/archive/refs/tags/v0.11.24.tar.gz"
-  sha256 "880e696d886acbdccee2200d75e600e621e4000be2f74725108627fe2e226530"
+  url "https://github.com/Enter-tainer/typstyle/archive/refs/tags/v0.11.25.tar.gz"
+  sha256 "078c5aa2db919ea8e14a675c9f32a7339ac28010c97aefdd952509980dbcb39c"
   license "Apache-2.0"
   head "https://github.com/Enter-tainer/typstyle.git", branch: "master"
 
