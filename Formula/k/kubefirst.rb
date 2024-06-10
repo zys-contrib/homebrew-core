3b394cf541c6ca0d2885a92e5c09343901414c0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-10 06:35:00 +0000

    kubefirst 2.4.9

--- Formula/k/kubefirst.rb
+++ Formula/k/kubefirst.rb
@@ -1,8 +1,8 @@
 class Kubefirst < Formula
   desc "GitOps Infrastructure & Application Delivery Platform for kubernetes"
   homepage "https://kubefirst.io/"
-  url "https://github.com/kubefirst/kubefirst/archive/refs/tags/v2.4.8.tar.gz"
-  sha256 "cc27847641a60892c933e139780f107e68e29f279d745e77f9dbca4229df00ee"
+  url "https://github.com/kubefirst/kubefirst/archive/refs/tags/v2.4.9.tar.gz"
+  sha256 "edcdac000cd662960164a12746165a4f6c1c0a2bde8ddcb534363929384317d8"
   license "MIT"
   head "https://github.com/kubefirst/kubefirst.git", branch: "main"
 
