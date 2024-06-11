4b8d83e6a328f1f61866aa8b7cd87b432ff13464
Bevan Kay <email@bevankay.me>
2024-06-12 09:37:38 +1000

    etcd-cpp-apiv3: revision bump (for protobuf 27.1)

--- Formula/e/etcd-cpp-apiv3.rb
+++ Formula/e/etcd-cpp-apiv3.rb
@@ -4,7 +4,7 @@ class EtcdCppApiv3 < Formula
   url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.15.4.tar.gz"
   sha256 "4516ecfa420826088c187efd42dad249367ca94ea6cdfc24e3030c3cf47af7b4"
   license "BSD-3-Clause"
-  revision 7
+  revision 8
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "616db5dee3027eee5c6420f314f8a5e1b55e65c1b95f093fed42e5dcaacfaca1"
