d604c0f5fe59a4b6a0bb3de92b4f88313832b7da
François Proulx <francois@boostsecurity.io>
2024-06-11 13:56:18 -0400

    poutine 0.10.1 (new formula)

---
+++ Formula/p/poutine.rb
@@ -0,0 +1,49 @@
+class Poutine < Formula
+  desc "Security scanner that detects vulnerabilities in build pipelines"
+  homepage "https://boostsecurityio.github.io/poutine/"
+  url "https://github.com/boostsecurityio/poutine/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "ad9fb2dc3ca35e0606aea6059d38a36552ab68643c79955deb1985f302d5aa91"
+  license "Apache-2.0"
+  head "https://github.com/boostsecurityio/poutine.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{tap.user}
+      -X main.date=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"poutine", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/poutine version")
+
+    # Creating local Git repo with vulnerable test file that the scanner can detect
+    # This makes no outbound network call and does not read or write outside the of the temp directory
+    (testpath/"repo/.github/workflows/").mkpath
+    system "git", "-C", testpath/"repo", "init"
+    system "git", "-C", testpath/"repo", "remote", "add", "origin", "git@github.com:actions/whatever.git"
+    vulnerable_workflow = <<-HEREDOC
+    on:
+      pull_request_target:
+    jobs:
+      test:
+        runs-on: ubuntu-latest
+        steps:
+        - uses: actions/checkout@v3
+          with:
+            ref: ${{ github.event.pull_request.head.sha }}
+        - run: make test
+    HEREDOC
+    (testpath/"repo/.github/workflows/build.yml").write(vulnerable_workflow)
+    system "git", "-C", testpath/"repo", "add", ".github/workflows/build.yml"
+    system "git", "-C", testpath/"repo", "commit", "-m", "message"
+    assert_match "Detected usage of `make`", shell_output("#{bin}/poutine analyze_local #{testpath}/repo")
+  end
+end
