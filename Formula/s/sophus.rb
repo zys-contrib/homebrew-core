dcc826c4dbdc308a2eec09941f6b95413e260262
Rui Chen <rui@chenrui.dev>
2024-06-12 15:18:47 -0400

    sophus 1.24.6
    autobump: add sophus
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    sophus: HelloSO3.cpp -> hello_so3.cpp
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sophus.rb
+++ Formula/s/sophus.rb
@@ -1,8 +1,8 @@
 class Sophus < Formula
   desc "C++ implementation of Lie Groups using Eigen"
-  homepage "https://strasdat.github.io/Sophus/latest/"
-  url "https://github.com/strasdat/Sophus/archive/refs/tags/1.22.10.tar.gz"
-  sha256 "eb1da440e6250c5efc7637a0611a5b8888875ce6ac22bf7ff6b6769bbc958082"
+  homepage "https://github.com/strasdat/Sophus"
+  url "https://github.com/strasdat/Sophus/archive/refs/tags/1.24.6.tar.gz"
+  sha256 "3f3098bdac2c74d42a921dbfb0e5e4b23601739e35a1c1236c2807c399da960c"
   license "MIT"
   version_scheme 1
   head "https://github.com/strasdat/Sophus.git", branch: "master"
@@ -23,21 +23,25 @@ class Sophus < Formula
                     "-DBUILD_SOPHUS_EXAMPLES=OFF"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    (pkgshare/"examples").install "examples/HelloSO3.cpp"
+    (pkgshare/"examples").install "examples/hello_so3.cpp"
   end
 
   test do
-    cp pkgshare/"examples/HelloSO3.cpp", testpath
+    cp pkgshare/"examples/hello_so3.cpp", testpath
     (testpath/"CMakeLists.txt").write <<~EOS
-      cmake_minimum_required(VERSION 3.5)
+      cmake_minimum_required(VERSION #{Formula["cmake"].version})
       project(HelloSO3)
+
+      set(CMAKE_CXX_STANDARD 17)
+      set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
       find_package(Sophus REQUIRED)
-      add_executable(HelloSO3 HelloSO3.cpp)
+      add_executable(HelloSO3 hello_so3.cpp)
       target_link_libraries(HelloSO3 Sophus::Sophus)
     EOS
 
-    system "cmake", "-DSophus_DIR=#{share}/Sophus", "."
-    system "make"
-    assert_match "The rotation matrices are", shell_output("./HelloSO3")
+    system "cmake", "-S", ".", "-B", "build", "-DSophus_DIR=#{share}/Sophus"
+    system "cmake", "--build", "build"
+    assert_match "The rotation matrices are", shell_output("./build/HelloSO3")
   end
 end
