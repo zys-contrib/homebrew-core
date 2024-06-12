b58892c074218790b51d532f74055e3e10e4e9f6
Rui Chen <rui@chenrui.dev>
2024-06-12 12:12:03 -0400

    ptex 2.4.3
    autobump: add ptex
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ptex: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/ptex.rb
+++ Formula/p/ptex.rb
@@ -1,8 +1,8 @@
 class Ptex < Formula
   desc "Texture mapping system"
   homepage "https://ptex.us/"
-  url "https://github.com/wdas/ptex/archive/refs/tags/v2.4.2.tar.gz"
-  sha256 "c8235fb30c921cfb10848f4ea04d5b662ba46886c5e32ad5137c5086f3979ee1"
+  url "https://github.com/wdas/ptex/archive/refs/tags/v2.4.3.tar.gz"
+  sha256 "435aa2ee1781ff24859bd282b7616bfaeb86ca10604b13d085ada8aa7602ad27"
   license "BSD-3-Clause"
 
   livecheck do
@@ -28,11 +28,6 @@ class Ptex < Formula
 
   uses_from_macos "zlib"
 
-  resource "wtest" do
-    url "https://raw.githubusercontent.com/wdas/ptex/v2.4.2/src/tests/wtest.cpp"
-    sha256 "95c78f97421eac034401b579037b7ba4536a96f4b356f8f1bb1e87b9db752444"
-  end
-
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
@@ -40,7 +35,12 @@ class Ptex < Formula
   end
 
   test do
-    resource("wtest").stage testpath
+    resource "homebrew-wtest" do
+      url "https://raw.githubusercontent.com/wdas/ptex/v2.4.2/src/tests/wtest.cpp"
+      sha256 "95c78f97421eac034401b579037b7ba4536a96f4b356f8f1bb1e87b9db752444"
+    end
+
+    testpath.install resource("homebrew-wtest")
     system ENV.cxx, "wtest.cpp", "-o", "wtest", "-I#{opt_include}", "-L#{opt_lib}", "-lPtex"
     system "./wtest"
     system bin/"ptxinfo", "-c", "test.ptx"
