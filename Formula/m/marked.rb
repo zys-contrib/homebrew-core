c2556af4ec929f74f7cbc48afde6ceebdcefe72b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-12 08:04:29 +0000

    marked: update 13.0.0 bottle.

--- Formula/m/marked.rb
+++ Formula/m/marked.rb
@@ -8,7 +8,13 @@ class Marked < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "717d409f830e2f01f5f209cd4273064d102e3e9ae919edd098c9bd82f81c1b49"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3fa4be245a5330c28d986fcb198c35e25e122991882bdbe8601ad76f165dbf33"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3fa4be245a5330c28d986fcb198c35e25e122991882bdbe8601ad76f165dbf33"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3fa4be245a5330c28d986fcb198c35e25e122991882bdbe8601ad76f165dbf33"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ae4b3bd58d952e0f700219ac5a67b1112c5472832542300810b6b3452bf02531"
+    sha256 cellar: :any_skip_relocation, ventura:        "ae4b3bd58d952e0f700219ac5a67b1112c5472832542300810b6b3452bf02531"
+    sha256 cellar: :any_skip_relocation, monterey:       "3fa4be245a5330c28d986fcb198c35e25e122991882bdbe8601ad76f165dbf33"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a00da522de59a1b2e5a124ef33c7e31e9ea046ecd279408ddc4650f502f3509c"
   end
 
   depends_on "node"
