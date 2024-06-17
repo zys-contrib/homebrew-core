17376710f657557e1709a1059b8fbbc1d268500a
Rui Chen <rui@chenrui.dev>
2024-06-16 23:23:04 -0400

    sslscan 2.1.4
    autobump: add sslscan
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sslscan.rb
+++ Formula/s/sslscan.rb
@@ -1,8 +1,8 @@
 class Sslscan < Formula
   desc "Test SSL/TLS enabled services to discover supported cipher suites"
   homepage "https://github.com/rbsec/sslscan"
-  url "https://github.com/rbsec/sslscan/archive/refs/tags/2.1.3.tar.gz"
-  sha256 "6beec9345635b41fa2c1bbc5f0854f10014e4b2b4179e9e9a3bda6bdb9e1aa41"
+  url "https://github.com/rbsec/sslscan/archive/refs/tags/2.1.4.tar.gz"
+  sha256 "3e2a5b1f53d1f132b4d999ff450d2cc40e9efb648cea89b74f5944b768a10e63"
   license "GPL-3.0-or-later" => { with: "openvpn-openssl-exception" }
   head "https://github.com/rbsec/sslscan.git", branch: "master"
 
