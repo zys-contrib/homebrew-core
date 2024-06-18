bce64edc13af15c11e90b27909e377fdf7bfb23a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-18 18:18:05 +0000

    touca: update 1.8.7_3 bottle.

--- Formula/t/touca.rb
+++ Formula/t/touca.rb
@@ -8,14 +8,13 @@ class Touca < Formula
   revision 3
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "343fc4996c6ce18a5d9491e17eff7d3f56bcbaa69530f1b357f65934804ee74b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "37cc781c1683fbaf74ad88c07e0e705be46abfcad90ae251a9898704d13a6907"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f4a9875943860bdc2220eb547cb93ae6507a7c29d81063ce867ddc59e15d555c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "fa149859df07f6dfdc1300e49655b6d7f7114dfdf750b2e09ebfaf6667b276f3"
-    sha256 cellar: :any_skip_relocation, ventura:        "ff80cdadebea93093541b65bf1948be128b348387ed102471816058eeb55326b"
-    sha256 cellar: :any_skip_relocation, monterey:       "0fa4544cc2f19b900ca5498a6bb621134c6dcae795beb38b98f681a0c18628c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "328bc9c8333c9678c53aa3f7992f7bc3addf66fc63369b9562c471be55cc04ea"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "db63d5f14e5b12b46f6bf83f299fbdaa6c635785979ded9aad35861543f4f4b9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "db63d5f14e5b12b46f6bf83f299fbdaa6c635785979ded9aad35861543f4f4b9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "db63d5f14e5b12b46f6bf83f299fbdaa6c635785979ded9aad35861543f4f4b9"
+    sha256 cellar: :any_skip_relocation, sonoma:         "6b45015272a5d8987cd49b5e672f6a4fe382d4dda1d8b893b474952d542e4fe9"
+    sha256 cellar: :any_skip_relocation, ventura:        "6b45015272a5d8987cd49b5e672f6a4fe382d4dda1d8b893b474952d542e4fe9"
+    sha256 cellar: :any_skip_relocation, monterey:       "8572f59ccea2d9d4f3aca0de8e6b103d5d2e407ca5c83cb080e09ba81cdcdb6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5eac5cb706608b3c375dd16029960ac67701c55ea254cd5bf8fdeeb19cc8b134"
   end
 
   depends_on "certifi"
