2f05eecce765e00e04a26aac3aa30eb22f658920
Patrick Linnane <patrick@linnane.io>
2024-06-18 17:34:37 +0000

    sherlock: update 0.14.4_1 bottle.

--- Formula/s/sherlock.rb
+++ Formula/s/sherlock.rb
@@ -10,13 +10,13 @@ class Sherlock < Formula
   head "https://github.com/sherlock-project/sherlock.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2c9aa8b48768ebdb3a1f75d57dbb1b5494e328cb960442d151dfe61d19af290c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f2313da93e039f071360a265ec9405e0c7ff99a24e3623908cfa1807e5b6d073"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "55b06aab82e51d750745088c8fc21c63c2c0544a7b7fe27198db418ece12bee7"
-    sha256 cellar: :any_skip_relocation, sonoma:         "962fb08052f1cef920bbfe7a7c7f3642f7b9c84a7037818f0f6f538b3dd42fee"
-    sha256 cellar: :any_skip_relocation, ventura:        "90db2f5ecac6ed021485664b2a477dea8e9c4e53c98063c83516521d1ca40386"
-    sha256 cellar: :any_skip_relocation, monterey:       "01e31530d3c0e54a1e6a255fb4d2640821e1b3a2a064371ae7bf9d8a64a86a92"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1aab9b488bfd11364f1b6e3a94f2ae2acfbb293e605bce48fb2b87815ccb0cbc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a15b64e7f91ffc6c277a3aab4d09e97afc3f934595120b15cf28413eee01fb60"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c1f8e81fbcdf27790c14444098a751f277235959d961d6b050b1fe368d6e60cd"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "09dcef1f9f7bfb150925610ae7f3daec2725d887b1791cb5fc636934bd60edef"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1ceb188b20a514ad30b250af49a03e1d1567557cc247b82454ba047f146c1850"
+    sha256 cellar: :any_skip_relocation, ventura:        "b2958f4a42bf5b78f74f64411bb42a6570f0ecbe035e854a31a67e8139979e07"
+    sha256 cellar: :any_skip_relocation, monterey:       "c03c267e690aaa91234a40ea5f002fe24e51966457e182ace22881ce5b91c61b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "80522cbd8ceb87f589daa8a6fa50ea6d587e0ffd180f75c996000283e7d1c303"
   end
 
   depends_on "cmake" => :build
