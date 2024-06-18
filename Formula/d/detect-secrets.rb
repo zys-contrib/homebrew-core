d9237d34a54391305cc41373ce0ea7655d551ef4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-18 05:07:59 +0000

    detect-secrets: update 1.5.0_2 bottle.

--- Formula/d/detect-secrets.rb
+++ Formula/d/detect-secrets.rb
@@ -10,13 +10,13 @@ class DetectSecrets < Formula
   head "https://github.com/Yelp/detect-secrets.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "32016e9cd914c77dca20151657b49a99b659039dd994adde46099addbe507a24"
-    sha256 cellar: :any,                 arm64_ventura:  "01aa11cd4ea2c96fdf106cb0c1077814049e594b52977b70d1d11f91fa22c56b"
-    sha256 cellar: :any,                 arm64_monterey: "44ec738cdc3f03bf96042df56161e92754b88b8f0f931f45ad33c75d40df07aa"
-    sha256 cellar: :any,                 sonoma:         "d70372d7051ce239332c75d78566901b1d7d12dafafff0da9c408fa01a2c5b67"
-    sha256 cellar: :any,                 ventura:        "18e0a50762143176fd5cfb720376f053e42f476783d9b580da4f180c8d09cee3"
-    sha256 cellar: :any,                 monterey:       "5f3e380bf40ba837d812eec0be395c1a3aa1530f9717ca38dc794fe7cf82d462"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c7d983dd78ee130861ae918b2c30b8bd50ef99f999448ee7274a9478f6935339"
+    sha256 cellar: :any,                 arm64_sonoma:   "9e5624cbe73f49b1ebbffcfda700f1f623bb6582258572bde646c42a00fe39af"
+    sha256 cellar: :any,                 arm64_ventura:  "e26c739780cc319a3b8cef1d1e1cb905eb0de581eab3b7276e902d07b8aa1af2"
+    sha256 cellar: :any,                 arm64_monterey: "367476416d869fe677128fcbfca1ee187e03cb812761f4a21e1ef8000d2fc231"
+    sha256 cellar: :any,                 sonoma:         "f76bcb80d787214a14cf6bf48fe99406b498dcecc80ec2ac14bc3854f52c935a"
+    sha256 cellar: :any,                 ventura:        "be69b69450ff8ad0c1db35952d9d90e9c64506fda2aeb74bbcff372381d53e81"
+    sha256 cellar: :any,                 monterey:       "2352b1aa00aea7861a7ca3b65ecc839db820a1800156c8ffe402fd9a37f57335"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "728c3b633a97b44e60129ae6a792ade97cad0ac8bc500bb043370be32be81d98"
   end
 
   depends_on "certifi"
