4cf3eb02e161060a457968b35b12633418fba482
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-18 02:25:20 +0000

    neosync: update 0.4.36 bottle.

--- Formula/n/neosync.rb
+++ Formula/n/neosync.rb
@@ -7,13 +7,13 @@ class Neosync < Formula
   head "https://github.com/nucleuscloud/neosync.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "513df7548e30f1fd0edefb1eab07320e65ebe7816968fb3b6c4c8f8e0603f4cf"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0f88fa53bb7e23f3f7b9d658349aef988853106d1f18750c84a07bef4d42ff24"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f4598dc524909bdc615693fb742afafe17840aee9d0ea1beef287cd16c3af301"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b1bfa7928f51b1b3dcee654b7fb1e886bdcc68735c6ba0d1fff3147e660f1618"
-    sha256 cellar: :any_skip_relocation, ventura:        "26038fc27baedbc11c8cc3e44abdf561649d89583d79785f4006838bef1143c3"
-    sha256 cellar: :any_skip_relocation, monterey:       "203492027a81838b34790e4aca53ee5ef79d45295d007c22f36598eaf671a471"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2811ef6673af6d42eb1c4dd8550e9798355edd45e6c8f8c75eca470df6ccf595"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "dbeaf50aa1af09344503d2ac87020db2249ee628042b8bdadfd7184ea61b3afd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8ff2da82b836c52aff2e04d358827b3f8b031b7c889c24c3f58beb1c23a06d35"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6fa7f73247d5699252c141313c38501fae63dc6885d8d61a616fee2ee78c0099"
+    sha256 cellar: :any_skip_relocation, sonoma:         "03f95b028af4b62da488e5a325961af7a0d23cd05c4a0770025d8081febe0fab"
+    sha256 cellar: :any_skip_relocation, ventura:        "c19e0a208e7c9a3d5ca7646e7c78bf58bf1dcedd8df14346a85d6d556ccd02ff"
+    sha256 cellar: :any_skip_relocation, monterey:       "a8db57d1700262501345c94483e27e2fe519c15c6b476c46590318fc01704f30"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b9576f47aea82bd639163685d1ef70cf8a5d3e0a21c675ce2c10fb3b558cf79f"
   end
 
   depends_on "go" => :build
