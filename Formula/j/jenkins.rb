fe1b0b5e03cf8e38ddd6b3124d9bc0e5a885382f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-18 21:29:04 +0000

    jenkins: update 2.463 bottle.

--- Formula/j/jenkins.rb
+++ Formula/j/jenkins.rb
@@ -11,13 +11,13 @@ class Jenkins < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "585b5b7557970dc9f3cde326bc06907a924e30976bc35bbed37270a4fbce063a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "585b5b7557970dc9f3cde326bc06907a924e30976bc35bbed37270a4fbce063a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "585b5b7557970dc9f3cde326bc06907a924e30976bc35bbed37270a4fbce063a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "585b5b7557970dc9f3cde326bc06907a924e30976bc35bbed37270a4fbce063a"
-    sha256 cellar: :any_skip_relocation, ventura:        "585b5b7557970dc9f3cde326bc06907a924e30976bc35bbed37270a4fbce063a"
-    sha256 cellar: :any_skip_relocation, monterey:       "585b5b7557970dc9f3cde326bc06907a924e30976bc35bbed37270a4fbce063a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cd2fb8fd7c6254976c7999bbc0c0bb729dfff00d7e98564f92a5bf28ae72b3f2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1a738674b29c2f69418f8d2ab327329714a8d24173544406439e7852e9589e32"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1a738674b29c2f69418f8d2ab327329714a8d24173544406439e7852e9589e32"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1a738674b29c2f69418f8d2ab327329714a8d24173544406439e7852e9589e32"
+    sha256 cellar: :any_skip_relocation, sonoma:         "fa48091c8b258a48f0a3f72d280fe2518140483c7ca55a09163a96c1712a8ae9"
+    sha256 cellar: :any_skip_relocation, ventura:        "fa48091c8b258a48f0a3f72d280fe2518140483c7ca55a09163a96c1712a8ae9"
+    sha256 cellar: :any_skip_relocation, monterey:       "1a738674b29c2f69418f8d2ab327329714a8d24173544406439e7852e9589e32"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6fe27eada9b7a66c82ca1ffe5888bdf4f2f4c9e0e19b3c26c3e59d0608f08a9b"
   end
 
   head do
