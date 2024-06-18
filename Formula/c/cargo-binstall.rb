d2f4393fa2173fb0c382c8503ac495cdefef8a59
Timothy Sutton <tim@synthist.net>
2024-06-18 06:52:59 +0000

    cargo-binstall 1.7.1

--- Formula/c/cargo-binstall.rb
+++ Formula/c/cargo-binstall.rb
@@ -1,8 +1,8 @@
 class CargoBinstall < Formula
   desc "Binary installation for rust projects"
   homepage "https://github.com/cargo-bins/cargo-binstall"
-  url "https://github.com/cargo-bins/cargo-binstall/archive/refs/tags/v1.6.9.tar.gz"
-  sha256 "0a41e12770fd2ed00704d7d9d01e001c11e5f3c9151803730d96a54eb2805d6d"
+  url "https://github.com/cargo-bins/cargo-binstall/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "594b1f0abdb02c588bbc9245ef7a922befb21228254f0c61742c0003abe9e843"
   license "GPL-3.0-only"
   head "https://github.com/cargo-bins/cargo-binstall.git", branch: "main"
 
@@ -23,6 +23,10 @@ class CargoBinstall < Formula
 
   depends_on "rust" => :build
 
+  # Remove after this is resolved:
+  # https://github.com/cargo-bins/cargo-binstall/pull/1781
+  patch :DATA
+
   def install
     system "cargo", "install", *std_cargo_args(path: "crates/bin")
   end
@@ -34,3 +38,20 @@ class CargoBinstall < Formula
     assert_equal version.to_s, shell_output("#{bin}/cargo-binstall -V").chomp
   end
 end
+__END__
+diff --git a/.cargo/config.toml b/.cargo/config.toml
+index 65dd57802..0a23a07bc 100644
+--- a/.cargo/config.toml
++++ b/.cargo/config.toml
+@@ -9,8 +9,8 @@ rustflags = ["-C", "link-arg=-fuse-ld=lld"]
+ rustflags = ["-C", "link-arg=-fuse-ld=lld"]
+ 
+ [target.x86_64-apple-darwin]
+-rustflags = ["-C", "link-arg=-fuse-ld=lld"]
++rustflags = ["-C", "link-arg=-fuse-ld=ld"]
+ [target.x86_64h-apple-darwin]
+-rustflags = ["-C", "link-arg=-fuse-ld=lld"]
++rustflags = ["-C", "link-arg=-fuse-ld=ld"]
+ [target.aarch64-apple-darwin]
+-rustflags = ["-C", "link-arg=-fuse-ld=lld"]
++rustflags = ["-C", "link-arg=-fuse-ld=ld"]
