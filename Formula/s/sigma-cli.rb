37c3ed92e3619dc2f1dd1fe2d4cff6cd9b7d3aa6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-18 11:53:01 +0000

    sigma-cli: bump python resources

--- Formula/s/sigma-cli.rb
+++ Formula/s/sigma-cli.rb
@@ -6,7 +6,7 @@ class SigmaCli < Formula
   url "https://files.pythonhosted.org/packages/cf/45/eef555a909ff716108ded653decf0a7642bc80169fff9590b665638164a0/sigma_cli-1.0.2.tar.gz"
   sha256 "8cb46dca0c5787969f33152c3cd085989fbc07a121d1c9a795664f7264c7ce9f"
   license "LGPL-2.1-or-later"
-  revision 1
+  revision 2
   head "https://github.com/SigmaHQ/sigma-cli.git", branch: "main"
 
   bottle do
@@ -69,8 +69,8 @@ class SigmaCli < Formula
   end
 
   resource "pysigma" do
-    url "https://files.pythonhosted.org/packages/8e/67/c02ba2ea1a93c9b614a7f694d62421dd8416da33b9ce8db9c39a55592885/pysigma-0.11.5.tar.gz"
-    sha256 "de27ef86db1b9341a8b540ca4691f489a9f5d63bc2de40bb3257221404d4f2d1"
+    url "https://files.pythonhosted.org/packages/dd/6b/964e015e1ef78647696eddf6ca58807f1c3ef409f26d9c0601db7b3b86dc/pysigma-0.11.7.tar.gz"
+    sha256 "0414db911bdb8aea11967e64a53da4288b869374135ee81a0e5e3658c7a79eb6"
   end
 
   resource "pysigma-backend-sqlite" do
@@ -84,13 +84,13 @@ class SigmaCli < Formula
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/86/ec/535bf6f9bd280de6a4637526602a146a68fde757100ecf8c9333173392db/requests-2.32.2.tar.gz"
-    sha256 "dd951ff5ecf3e3b3aa26b40703ba77495dab41da839ae72ef3c8e5d8e2433289"
+    url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
+    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/7a/50/7fd50a27caa0652cd4caf224aa87741ea41d3265ad13f010886167cfcc79/urllib3-2.2.1.tar.gz"
-    sha256 "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
+    url "https://files.pythonhosted.org/packages/43/6d/fa469ae21497ddc8bc93e5877702dca7cb8f911e337aca7452b5724f1bb6/urllib3-2.2.2.tar.gz"
+    sha256 "dd505485549a7a552833da5e6063639d0d177c04f23bc3864e41e5dc5f612168"
   end
 
   resource "wcwidth" do
