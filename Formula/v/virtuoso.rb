0494f3b00d973d0185033f788a937ddd55333235
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-19 20:24:04 +0000

    virtuoso: update 7.2.13 bottle.

--- Formula/v/virtuoso.rb
+++ Formula/v/virtuoso.rb
@@ -6,13 +6,13 @@ class Virtuoso < Formula
   license "GPL-2.0-only" => { with: "openvpn-openssl-exception" }
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "18b1170c79ecd56db751ada64e0d9f35936a37a3f4e0a85a6d3b0635bb1034fd"
-    sha256 cellar: :any,                 arm64_ventura:  "48493a2e269896d9feb3a4e2aa612121d0719ccc2911813fdd5e0ae7c30ef4fb"
-    sha256 cellar: :any,                 arm64_monterey: "d0d5e7606b243bbc3da373aedeceb854924f439f91ec6d162d2b35993a0f66e9"
-    sha256 cellar: :any,                 sonoma:         "68016908ca8ff05d59f08c7697d38f2945cade8f4eb868ea3cf0108d979d0c0e"
-    sha256 cellar: :any,                 ventura:        "79fb51b61682d4c922853b38692ae388420d8f2abb4f4598cf278eab677ea9fe"
-    sha256 cellar: :any,                 monterey:       "e83f497a56d041a4c759af571310236f7e7b51123c9618ddcea36bdde8e9c3f3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28bf8569380e06f49990915265b17a941cc6494442b006fe8f3035bcb0dab7cf"
+    sha256 cellar: :any,                 arm64_sonoma:   "73ea2ae83de5dede38609a3bb93fbd170bfbc84ab81a46d3653704f546b84270"
+    sha256 cellar: :any,                 arm64_ventura:  "aee80e84acb4348d360de4ec9d3fa868c5785983bc278d06cf25ac108493e564"
+    sha256 cellar: :any,                 arm64_monterey: "67399c6e0e2f05a0474ba1687ad45d4a4a306c70cbe2a568f47ae33eece75b50"
+    sha256 cellar: :any,                 sonoma:         "a73f74103ffb52c691aaf413358b04b389d325987e48fddfe13608c22d70f89d"
+    sha256 cellar: :any,                 ventura:        "10938d0db150f8c612e6126f22b6b25bbaaf424c23d17cbf36dc4df6420c1ecc"
+    sha256 cellar: :any,                 monterey:       "cb01a85ca94e9a63608653fa758453593081714f4a385f79d2e53c0bf4e889eb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24fc37ec8db10030a0acdbaea2185c30431936c16510ea6ba8efe0adc6c9a5ed"
   end
 
   head do
