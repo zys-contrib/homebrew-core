0adde694c5c2723a3fa2ce3d9429c75669428584
Rui Chen <rui@chenrui.dev>
2024-06-19 15:49:58 -0400

    sevenzip 24.07

--- Formula/s/sevenzip.rb
+++ Formula/s/sevenzip.rb
@@ -1,9 +1,9 @@
 class Sevenzip < Formula
   desc "7-Zip is a file archiver with a high compression ratio"
   homepage "https://7-zip.org"
-  url "https://7-zip.org/a/7z2406-src.tar.xz"
-  version "24.06"
-  sha256 "2aa1660c773525b2ed84d6cd7ff0680c786ec0893b87e4db44654dcb7f5ac8b5"
+  url "https://7-zip.org/a/7z2407-src.tar.xz"
+  version "24.07"
+  sha256 "d1b0874a3f1c26df21c761a4a30691dc1213e8577f18ee78326c14ca4d683e2b"
   license all_of: ["LGPL-2.1-or-later", "BSD-3-Clause"]
   head "https://github.com/ip7z/7zip.git", branch: "main"
 
