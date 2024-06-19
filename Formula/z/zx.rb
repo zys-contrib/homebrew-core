59f51b6f99ef6a8e188369bb607b549e6e491ed7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-19 22:24:19 +0000

    zx: update 8.1.3 bottle.

--- Formula/z/zx.rb
+++ Formula/z/zx.rb
@@ -8,13 +8,13 @@ class Zx < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8ed47a242327eaae55b85a1606fa40adf3de989bfd8f79908091f5cad5117e83"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8ed47a242327eaae55b85a1606fa40adf3de989bfd8f79908091f5cad5117e83"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8ed47a242327eaae55b85a1606fa40adf3de989bfd8f79908091f5cad5117e83"
-    sha256 cellar: :any_skip_relocation, sonoma:         "6673e326e7f262b58f9bf8e984393c94127e1d157d394c076096dd28470d48ec"
-    sha256 cellar: :any_skip_relocation, ventura:        "6673e326e7f262b58f9bf8e984393c94127e1d157d394c076096dd28470d48ec"
-    sha256 cellar: :any_skip_relocation, monterey:       "6673e326e7f262b58f9bf8e984393c94127e1d157d394c076096dd28470d48ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e59666cdb351dc5c2c8c34484134db620519b198933dd6b4120d7d544c4f4211"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9a6520a4148dce68a5398e12ec8f2848d1f818fb0b1e796fb40707569844262f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9a6520a4148dce68a5398e12ec8f2848d1f818fb0b1e796fb40707569844262f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9a6520a4148dce68a5398e12ec8f2848d1f818fb0b1e796fb40707569844262f"
+    sha256 cellar: :any_skip_relocation, sonoma:         "61f2978658133749c3fb8f2d450f79a9d9359089bf4bc10aad3f836582a4d239"
+    sha256 cellar: :any_skip_relocation, ventura:        "61f2978658133749c3fb8f2d450f79a9d9359089bf4bc10aad3f836582a4d239"
+    sha256 cellar: :any_skip_relocation, monterey:       "61f2978658133749c3fb8f2d450f79a9d9359089bf4bc10aad3f836582a4d239"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b4a2f020526e224fc01e55a83d7e2ed917f48ad2d6e5310f921137e03d58f60e"
   end
 
   depends_on "node"
