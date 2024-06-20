bd241889de34e4510ac7928fcb3cd07d1e92e2c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-20 23:09:47 +0000

    ocrmypdf: update 16.4.0 bottle.

--- Formula/o/ocrmypdf.rb
+++ Formula/o/ocrmypdf.rb
@@ -8,13 +8,13 @@ class Ocrmypdf < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "37d159793926529c1b0852fb5592ee091551d85d8da2dfa877d53cc77c0c8a7d"
-    sha256 cellar: :any,                 arm64_ventura:  "9044ea195e581aa99d0d7ac28f60b7ec4ef8030f47596d8607089af9c61efda8"
-    sha256 cellar: :any,                 arm64_monterey: "5f8cedf8cc960e4d5ebb3ba26826c06cd6faa34aea30ec936789c3f09b80617d"
-    sha256 cellar: :any,                 sonoma:         "204f71118f0b061b92db004014bf2ffb917ce72c1a8e4560eaad43fc5a1f24db"
-    sha256 cellar: :any,                 ventura:        "b821e24cc38b5488aeba7ec5ca683d5f9cec037900e1cdd7ed5432f33b6346be"
-    sha256 cellar: :any,                 monterey:       "01d784f058ee4b6e05a99ac1fdf68fb1c76a10a2e3a43034b8fb9e5c892c5374"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b3e91d6e19369ef133d73a1193fa3e50bf08095bbe9c0286505c556c894a00a3"
+    sha256 cellar: :any,                 arm64_sonoma:   "374f9fcfb1871c70c1f18a23323fc9818ac125f7c4240508b7cc36fa3609c447"
+    sha256 cellar: :any,                 arm64_ventura:  "b511e7a0561ca06a5b12b163e2271eaf566072e8dae07d88acd417dfcf2df0a2"
+    sha256 cellar: :any,                 arm64_monterey: "b11f098622caa2c7177883fe31947f3277e9743b74c161e6b3eb5f71ec179861"
+    sha256 cellar: :any,                 sonoma:         "72b4306d1a560f413f367b2ac9558b08820268c3f864cecd6226106c018fd7da"
+    sha256 cellar: :any,                 ventura:        "3f92cfb93418800adcac649fc49e10b0ae5749de5146b9a3b3b5f79d6aa7d11e"
+    sha256 cellar: :any,                 monterey:       "f3ffc590cc661d6dfc79095d2d9fabce03b8353bbe259b67ae2c2c44aa37db8b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec31739da8aa95cf7441b95f2c157191625d33a03bc0b4f50f94a9aa0fd48d93"
   end
 
   depends_on "pkg-config" => :build
