5165d2237fb3b811a2afcc821047234be999b58b
David Hummel <6109326+hummeltech@users.noreply.github.com>
2024-06-21 08:46:24 -0700

    mapnik 4.0.0
    New version of Mapnik v4.0.0 has been released.
    CMake is now used as the build system rather than SCons.
    All of the patches are no longer needed.
    Requires submodules in order to build.
    Now runs built-in tests which also requires submodules.

--- Formula/m/mapnik.rb
+++ Formula/m/mapnik.rb
@@ -1,37 +1,12 @@
 class Mapnik < Formula
   desc "Toolkit for developing mapping applications"
   homepage "https://mapnik.org/"
+  # needs submodules
+  url "https://github.com/mapnik/mapnik.git",
+      tag:      "v4.0.0",
+      revision: "85801bd4028fa1cbffd9f7de4e2458bfc55e44bd"
   license "LGPL-2.1-or-later"
-  revision 29
   head "https://github.com/mapnik/mapnik.git", branch: "master"
-
-  # TODO: Try switching to CMake build on next release as it works better with
-  # Homebrew's build environment and avoids `--env=std` when using `scons`.
-  stable do
-    url "https://github.com/mapnik/mapnik/releases/download/v3.1.0/mapnik-v3.1.0.tar.bz2"
-    sha256 "43d76182d2a975212b4ad11524c74e577576c11039fdab5286b828397d8e6261"
-
-    # Fix build with Scons 4 using Arch Linux patch. Remove in the next release.
-    # Ref: https://github.com/mapnik/mapnik/commit/84a05a6597a941acfad220dae3fbfe5d20bfeb26
-    patch do
-      url "https://raw.githubusercontent.com/archlinux/svntogit-community/239768d7cd1217d5910d3f7d8ace86a7f85ad23c/trunk/scons4.patch"
-      sha256 "79a85ddba3ec17b86cb216e21442611498a9f2612f03e98708057b3c3a6e8b06"
-    end
-
-    # Fix build with Boost v1.83 using Arch Linux patch. Remove in the next release.
-    # Ref: https://github.com/mapnik/mapnik/pull/4413
-    patch do
-      url "https://github.com/mapnik/mapnik/commit/26eb76cc07210d564d80d98948770c94d27c5243.patch?full_index=1"
-      sha256 "085408d6a38c77df7f333047bb1568bf0dfdf9c3b87fd9001997ba9b22be6d4c"
-    end
-
-    # Fix build with `boost` 1.85.0 using open PR.
-    # PR ref: https://github.com/mapnik/mapnik/pull/4448
-    patch do
-      url "https://github.com/mapnik/mapnik/commit/8088aa4319fd54d41e9b8534b964d113f687fc7f.patch?full_index=1"
-      sha256 "3d37cfb1b52a93e3deec09f290070d9ef8fdb85a4c5c393dd8ca924d8921d5a3"
-    end
-  end
 
   livecheck do
     url :stable
@@ -48,8 +23,8 @@ class Mapnik < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "689925341a4ef8a29cf1eb7a6c18ec1a177da427c831da77fd0646330de244a7"
   end
 
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "scons" => :build
   depends_on "boost"
   depends_on "cairo"
   depends_on "freetype"
@@ -60,83 +35,39 @@ class Mapnik < Formula
   depends_on "libpng"
   depends_on "libpq"
   depends_on "libtiff"
+  depends_on "libxml2"
   depends_on "proj"
   depends_on "sqlite"
   depends_on "webp"
 
   uses_from_macos "zlib"
 
+  fails_with :gcc do
+    version "14"
+    cause "Fails to build with GCC 14 (https://github.com/mapnik/mapnik/pull/4456)"
+  end
+
   def install
-    # Work around an Xcode 15 linker issue which causes linkage against LLVM's
-    # libunwind due to it being present in a library search path.
-    if DevelopmentTools.clang_build_version >= 1500
-      recursive_dependencies
-        .select { |d| d.name.match?(/^llvm(@\d+)?$/) }
-        .map { |llvm_dep| llvm_dep.to_formula.opt_lib }
-        .each { |llvm_lib| ENV.remove "HOMEBREW_LIBRARY_PATHS", llvm_lib }
-    end
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_BENCHMARK:BOOL=OFF"
+    cmake_args << "-DBUILD_DEMO_CPP:BOOL=OFF"
+    cmake_args << "-DBUILD_DEMO_VIEWER:BOOL=OFF"
+    cmake_args << "-DCMAKE_INSTALL_RPATH:PATH=#{rpath}"
 
-    boost = Formula["boost"]
-    harfbuzz = Formula["harfbuzz"]
-    icu = Formula["icu4c"]
-    jpeg = Formula["jpeg-turbo"]
-    libpng = Formula["libpng"]
-    libpq = Formula["libpq"]
-    libtiff = Formula["libtiff"]
-    proj = Formula["proj"]
-    sqlite = Formula["sqlite"]
-    webp = Formula["webp"]
-
-    # upstream issue, see https://github.com/boostorg/phoenix/issues/115
-    # workaround to avoid the inclusion of `boost/phoenix/stl/tuple.hpp`
-    ENV.append "CXXFLAGS", "-DBOOST_PHOENIX_STL_TUPLE_H_"
-
-    if OS.linux?
-      deps.map(&:to_formula).select(&:keg_only?).map(&:opt_lib).each do |libdir|
-        ENV.append "LDFLAGS", "-Wl,-rpath,#{libdir}"
-      end
-      ENV.append "LDFLAGS", "-Wl,-rpath,#{HOMEBREW_PREFIX}/lib"
-    end
-
-    args = %W[
-      CC=#{ENV.cc}
-      CXX=#{ENV.cxx}
-      CUSTOM_CFLAGS=#{ENV.cflags}
-      CUSTOM_CXXFLAGS=#{ENV.cxxflags}
-      CUSTOM_LDFLAGS=#{ENV.ldflags}
-      PREFIX=#{prefix}
-      FAST=True
-      CPP_TESTS=False
-      INPUT_PLUGINS=all
-      BOOST_INCLUDES=#{boost.opt_include}
-      BOOST_LIBS=#{boost.opt_lib}
-      FREETYPE_CONFIG=#{Formula["freetype"].opt_bin}/freetype-config
-      GDAL_CONFIG=#{Formula["gdal"].opt_bin}/gdal-config
-      HB_INCLUDES=#{harfbuzz.opt_include}
-      HB_LIBS=#{harfbuzz.opt_lib}
-      ICU_INCLUDES=#{icu.opt_include}
-      ICU_LIBS=#{icu.opt_lib}
-      JPEG_INCLUDES=#{jpeg.opt_include}
-      JPEG_LIBS=#{jpeg.opt_lib}
-      PG_CONFIG=#{libpq.opt_bin}/pg_config
-      PNG_INCLUDES=#{libpng.opt_include}
-      PNG_LIBS=#{libpng.opt_lib}
-      PROJ_INCLUDES=#{proj.opt_include}
-      PROJ_LIBS=#{proj.opt_lib}
-      SQLITE_INCLUDES=#{sqlite.opt_include}
-      SQLITE_LIBS=#{sqlite.opt_lib}
-      TIFF_INCLUDES=#{libtiff.opt_include}
-      TIFF_LIBS=#{libtiff.opt_lib}
-      WEBP_INCLUDES=#{webp.opt_include}
-      WEBP_LIBS=#{webp.opt_lib}
-    ]
-
-    system "scons", "configure", *args
-    system "scons", "install", "--jobs=#{ENV.make_jobs}"
+    system "cmake", "-S", ".", "-B", "build", *cmake_args
+    system "cmake", "--build", "build"
+    system "ctest", "--verbose", "--parallel", ENV.make_jobs, "--test-dir", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    output = shell_output("#{bin}/mapnik-config --prefix").chomp
+    output = shell_output("#{Formula["pkg-config"].bin}/pkg-config libmapnik --variable prefix").chomp
     assert_equal prefix.to_s, output
+
+    output = shell_output("#{bin}/mapnik-index --version 2>&1", 1).chomp
+    assert_equal "version #{stable.version}", output
+
+    output = shell_output("#{bin}/mapnik-render --version 2>&1", 1).chomp
+    assert_equal "version #{stable.version}", output
   end
 end
