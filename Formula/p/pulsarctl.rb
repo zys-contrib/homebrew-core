5c9daef4e3861e12e9e3a7d3189dbf5f15440ed4
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2024-06-22 23:25:03 +0000

    pulsarctl: add 3.3.0.3 bottle.

--- Formula/p/pulsarctl.rb
+++ Formula/p/pulsarctl.rb
@@ -11,6 +11,16 @@ class Pulsarctl < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3877f915e14d667f5a843246dc2dc0a883ee275bb59bff665096ec579628991b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4e6cd405b1dd04699a32b518452fc042e8223d2f218742ecc7cf529daba0a3d0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee8530293b9fd5f12df0fa7d420af64276491f89eb60eaffe50aedf6e911f517"
+    sha256 cellar: :any_skip_relocation, sonoma:         "f9b378a94ca49e1de77c8df47650ac8f83334d82f71e58d1ca881dbe50fa386f"
+    sha256 cellar: :any_skip_relocation, ventura:        "e23fe7c645052757252585b9ce5c7521aaaf34ff4a1462da5736b23cdbb29182"
+    sha256 cellar: :any_skip_relocation, monterey:       "98e94cb0c080cf66a0d5e5c0adb0841568ee1a7bb8fb7381d0a2b9e5c7c44ab9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "362884eccc65416cfe1cda7f12dceee9ce490d4293150ea3c238c08940c333c1"
+  end
+
   depends_on "go" => :build
 
   def install
