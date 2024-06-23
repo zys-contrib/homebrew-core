754a8a9ea8e3727c05d144a9d8ddc38f2746dcf5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-23 03:31:57 +0000

    enzyme 0.0.125

--- Formula/e/enzyme.rb
+++ Formula/e/enzyme.rb
@@ -1,8 +1,8 @@
 class Enzyme < Formula
   desc "High-performance automatic differentiation of LLVM"
   homepage "https://enzyme.mit.edu"
-  url "https://github.com/EnzymeAD/Enzyme/archive/refs/tags/v0.0.124.tar.gz"
-  sha256 "2a119c33c714cbb5d2a86384823721c9b35b8b240bb29141e84d7a53f926b93f"
+  url "https://github.com/EnzymeAD/Enzyme/archive/refs/tags/v0.0.125.tar.gz"
+  sha256 "4b32b3a37e2aa681075e73c1e8cffd2a211907cf635a22e7486b8e0da7d9e5d9"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/EnzymeAD/Enzyme.git", branch: "main"
 
