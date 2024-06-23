737dba9e0a83e57ed62d4ab786ca608296da44a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-23 13:51:44 +0000

    bashunit: update 0.13.0 bottle.

--- Formula/b/bashunit.rb
+++ Formula/b/bashunit.rb
@@ -6,13 +6,7 @@ class Bashunit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0786a8fffa284461deb7fc21fbaf599469b58167ace1cb2898a5202bb4e81a71"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0786a8fffa284461deb7fc21fbaf599469b58167ace1cb2898a5202bb4e81a71"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0786a8fffa284461deb7fc21fbaf599469b58167ace1cb2898a5202bb4e81a71"
-    sha256 cellar: :any_skip_relocation, sonoma:         "ee88008c50f8f4e795fb48c2b03bbcb3ad86a7dfd39c0a43e2f0824d705d7f16"
-    sha256 cellar: :any_skip_relocation, ventura:        "ee88008c50f8f4e795fb48c2b03bbcb3ad86a7dfd39c0a43e2f0824d705d7f16"
-    sha256 cellar: :any_skip_relocation, monterey:       "ee88008c50f8f4e795fb48c2b03bbcb3ad86a7dfd39c0a43e2f0824d705d7f16"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0786a8fffa284461deb7fc21fbaf599469b58167ace1cb2898a5202bb4e81a71"
+    sha256 cellar: :any_skip_relocation, all: "2bc31b114027161d266ce5de753b57692412fd684e7aba61010d416a1ca0cc61"
   end
 
   def install
