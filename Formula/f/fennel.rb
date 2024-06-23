90bef554b649419564e332659aea63ca982aaeb9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-23 18:30:10 +0000

    fennel 1.5.0

--- Formula/f/fennel.rb
+++ Formula/f/fennel.rb
@@ -1,8 +1,8 @@
 class Fennel < Formula
   desc "Lua Lisp Language"
   homepage "https://fennel-lang.org"
-  url "https://github.com/bakpakin/Fennel/archive/refs/tags/1.4.2.tar.gz"
-  sha256 "b44a205ee7ebdee22f83d2a7a87742172295b8086b5361850dfab4f49699e44f"
+  url "https://github.com/bakpakin/Fennel/archive/refs/tags/1.5.0.tar.gz"
+  sha256 "f5ca4a688e9b97d0783b10a834e3563e3d881669c29f89135b5abffce631527a"
   license "MIT"
 
   bottle do
