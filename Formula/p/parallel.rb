36d68ab74e3d12bb55724453930a90db3a01c514
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-24 22:52:57 +0000

    parallel: update 20240622 bottle.

--- Formula/p/parallel.rb
+++ Formula/p/parallel.rb
@@ -14,13 +14,13 @@ class Parallel < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "191329637b017a09911260be1032aeef8516eeea938453c029bce0a1d1742025"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "191329637b017a09911260be1032aeef8516eeea938453c029bce0a1d1742025"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "191329637b017a09911260be1032aeef8516eeea938453c029bce0a1d1742025"
-    sha256 cellar: :any_skip_relocation, sonoma:         "eb8ba088077adb6aaa17f7913b529c31321966d5a98261b64abaacea3996b315"
-    sha256 cellar: :any_skip_relocation, ventura:        "eb8ba088077adb6aaa17f7913b529c31321966d5a98261b64abaacea3996b315"
-    sha256 cellar: :any_skip_relocation, monterey:       "191329637b017a09911260be1032aeef8516eeea938453c029bce0a1d1742025"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "191329637b017a09911260be1032aeef8516eeea938453c029bce0a1d1742025"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e4cba90fdb5ba15d44ae8ce5db63f33060a0dc604b050661c0e1db5346f04276"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e4cba90fdb5ba15d44ae8ce5db63f33060a0dc604b050661c0e1db5346f04276"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e4cba90fdb5ba15d44ae8ce5db63f33060a0dc604b050661c0e1db5346f04276"
+    sha256 cellar: :any_skip_relocation, sonoma:         "e4cba90fdb5ba15d44ae8ce5db63f33060a0dc604b050661c0e1db5346f04276"
+    sha256 cellar: :any_skip_relocation, ventura:        "e4cba90fdb5ba15d44ae8ce5db63f33060a0dc604b050661c0e1db5346f04276"
+    sha256 cellar: :any_skip_relocation, monterey:       "e4cba90fdb5ba15d44ae8ce5db63f33060a0dc604b050661c0e1db5346f04276"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e552c25a556a149172ff09f9733a5ccba5cc27c2074bcbae2128a615a0cbbb79"
   end
 
   conflicts_with "moreutils", because: "both install a `parallel` executable"
