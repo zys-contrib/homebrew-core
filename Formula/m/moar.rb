42db4a6a8fde4ca56fd01d0b67f04abbf26721c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-24 18:25:18 +0000

    moar: update 1.24.3 bottle.

--- Formula/m/moar.rb
+++ Formula/m/moar.rb
@@ -6,13 +6,13 @@ class Moar < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c67b9bd10e90c58b03b4593399076d9e3d9e9dd7438b55855722e264995d354e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c67b9bd10e90c58b03b4593399076d9e3d9e9dd7438b55855722e264995d354e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c67b9bd10e90c58b03b4593399076d9e3d9e9dd7438b55855722e264995d354e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "e50396e6542dbe099ad107c4775f56cda3ff52be72546a8ac9b9b6eee2b4ad57"
-    sha256 cellar: :any_skip_relocation, ventura:        "e50396e6542dbe099ad107c4775f56cda3ff52be72546a8ac9b9b6eee2b4ad57"
-    sha256 cellar: :any_skip_relocation, monterey:       "e50396e6542dbe099ad107c4775f56cda3ff52be72546a8ac9b9b6eee2b4ad57"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d754de910909aefd9fa2e777208eb519fd2ae6266e108390fa3cbc70dca3a4fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "117b2641d677a3d122527a81f50e328e49b5336b516a753fba327748fbb7e3d6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "117b2641d677a3d122527a81f50e328e49b5336b516a753fba327748fbb7e3d6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "117b2641d677a3d122527a81f50e328e49b5336b516a753fba327748fbb7e3d6"
+    sha256 cellar: :any_skip_relocation, sonoma:         "67b651d81a437975fdf9f1b4eb8653355a1fc18fe478c1a7bc4dd73fc4cd5fb0"
+    sha256 cellar: :any_skip_relocation, ventura:        "67b651d81a437975fdf9f1b4eb8653355a1fc18fe478c1a7bc4dd73fc4cd5fb0"
+    sha256 cellar: :any_skip_relocation, monterey:       "67b651d81a437975fdf9f1b4eb8653355a1fc18fe478c1a7bc4dd73fc4cd5fb0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea4a72911e6c3ce9fc735663aefe20c93f4b385134ffa129b75724ef7cd8a410"
   end
 
   depends_on "go" => :build
