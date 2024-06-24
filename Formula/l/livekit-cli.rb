198102953abc57064ed184b475c907c85cf1cd16
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-24 13:45:45 +0000

    livekit-cli: update 1.5.1 bottle.

--- Formula/l/livekit-cli.rb
+++ Formula/l/livekit-cli.rb
@@ -7,13 +7,13 @@ class LivekitCli < Formula
   head "https://github.com/livekit/livekit-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "723f9db0e4dd92676bef2c53a40d3bbf69955dfaedad5c40ec1ee9df84e27313"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8157d3ba39c00de1cad5354a0946aeb7c9e5e48136eb84f7728b5facc91daf63"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e16946c0d79ae6459cb3dda4f6a81b1797eddcc5aca71023431f572b29ff57e0"
-    sha256 cellar: :any_skip_relocation, sonoma:         "25166f561d661bc9d9ea48c269061491e9cc227c630854e4d0b419ca47c31e99"
-    sha256 cellar: :any_skip_relocation, ventura:        "a983b708fe5a5f802625d33098b5c8a366fc4d697acd56fe620d5a107b861e5f"
-    sha256 cellar: :any_skip_relocation, monterey:       "56db10fb5eaaada6cff27b9dc9325ce3c4b38c10d662c7bf9ff21bfc22eb7112"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5ab186e12edd2ecd7727e0c435f1628adcb59144ccfb86bed17b9a25edcc2b13"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "1716b8893a32aa981a5db2bf21e3a8d75dd73931ef2fd7809bc91e1b45d58d80"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b1cdf1a22d80a5f40b860642205426cbf75517937e7c21105e831a28afa8df1c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a69a81faf71aec28a2179c4b67901e2663f6200390a82ae193e921f60c06816d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3a0f5249a07b0d0fc45486f54d5b27f8ce10647dd74647f62931772a77990fdb"
+    sha256 cellar: :any_skip_relocation, ventura:        "d647cc1f0ae3edb8c22113db063327a8bd962e7d33248835c4b2576de7636723"
+    sha256 cellar: :any_skip_relocation, monterey:       "5ed47607d646b1910adc30b2827b5667ac1ef5837c8b8e7722f4dd45be89b6f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1d7c21883a337d7c38c9f3d58cb3637698851304559f2a8b32eb75fb06ac14cd"
   end
 
   depends_on "go" => :build
