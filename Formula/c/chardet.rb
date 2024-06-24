bc6bf2165d53a1a37ddbb05aa3512ac4972dfc70
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-06-24 07:58:52 +0000

    chardet: add 5.2.0 bottle.

--- Formula/c/chardet.rb
+++ Formula/c/chardet.rb
@@ -7,6 +7,16 @@ class Chardet < Formula
   sha256 "1b3b6ff479a8c414bc3fa2c0852995695c4a026dcd6d0633b2dd092ca39c1cf7"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "db732b50f0ce62bfc6f1fe0472ad5e406617d5586299629c2d17e98233916fc3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "db732b50f0ce62bfc6f1fe0472ad5e406617d5586299629c2d17e98233916fc3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "db732b50f0ce62bfc6f1fe0472ad5e406617d5586299629c2d17e98233916fc3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "db732b50f0ce62bfc6f1fe0472ad5e406617d5586299629c2d17e98233916fc3"
+    sha256 cellar: :any_skip_relocation, ventura:        "db732b50f0ce62bfc6f1fe0472ad5e406617d5586299629c2d17e98233916fc3"
+    sha256 cellar: :any_skip_relocation, monterey:       "db732b50f0ce62bfc6f1fe0472ad5e406617d5586299629c2d17e98233916fc3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32a2dd8cdfbe1e564ea6b433c966262ed3d470420c5f34951361aef20bbd0215"
+  end
+
   depends_on "python@3.12"
 
   def install
