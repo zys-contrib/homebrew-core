7ec487265080edb32bd5138047806dc4cebed5db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-25 14:38:52 +0000

    ggshield: update 1.29.0 bottle.

--- Formula/g/ggshield.rb
+++ Formula/g/ggshield.rb
@@ -9,13 +9,13 @@ class Ggshield < Formula
   head "https://github.com/GitGuardian/ggshield.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "cfbdaf84eae9a219666dbdd0b0ede6b116182accc2f73027d0586df543b1d1a9"
-    sha256 cellar: :any,                 arm64_ventura:  "69758965dd794c7172a3b792ecb3594588b90047eb014844c3ff8c9ebe7de2e5"
-    sha256 cellar: :any,                 arm64_monterey: "56f957c2f0988f05ef3fb156099916aa4a3689ab4200c8ff055e046870b7a9fc"
-    sha256 cellar: :any,                 sonoma:         "30f05a7607fde532dc771ae795a85f1436e9afb8835870b50cd85241aa1f28c8"
-    sha256 cellar: :any,                 ventura:        "05dbb17e231e988568c776fc26d57e438f5b8ab0fe995f898bc3616ed7f0a7c2"
-    sha256 cellar: :any,                 monterey:       "55768676a8469102ceaa2cf3a345c9153e91ba131a9bbb95183881094862dc72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd4003678138ba04a7d81f3f86f35620480299cb9e2c17b0779bd12cdcdce53b"
+    sha256 cellar: :any,                 arm64_sonoma:   "c72fa3e2d412f8831dc49ef5fc079ce1ceda2cd233e7bb251a8cb7adecc23b19"
+    sha256 cellar: :any,                 arm64_ventura:  "6a503ac202cbed7501e0074d8ae746bf166b435d917e9609072a0919b95c70ee"
+    sha256 cellar: :any,                 arm64_monterey: "0f0fa4b503a8b4e70e554fd19012d8e8ba1fc10bde5c4ba23a17318b3339a23d"
+    sha256 cellar: :any,                 sonoma:         "617ee747bf669aece7802803c3a2a0341e56cb7ab58c5a7794e2030526cc2beb"
+    sha256 cellar: :any,                 ventura:        "f8b8f0ff8d765e9ed4f9ec21633fd6a4f575f1b7bd31352d8c1fe3855f2aec22"
+    sha256 cellar: :any,                 monterey:       "3ade16f84f0b693ad94e540778b85b37aea3cacedc7a495c14a2b54c51fba920"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8438a752b6fc269451df0480038a52185007d4d7a1fbbad4b09b530fbb301b75"
   end
 
   depends_on "certifi"
