fa3434c5b94efd6f0d2678474537ef685985d89b
Rui Chen <rui@chenrui.dev>
2024-06-26 21:22:35 +0000

    libdrm: update 2.4.122 bottle.

--- Formula/lib/libdrm.rb
+++ Formula/lib/libdrm.rb
@@ -11,7 +11,7 @@ class Libdrm < Formula
   end
 
   bottle do
-    sha256 x86_64_linux: "eb415f4e18a2dfd2cd5b054d89b88f05ed26685e1ad996b0f825009b54a94c4c"
+    sha256 x86_64_linux: "cfa757b286e6645a19b0d9b89b06358a6c3b0e9b928fa6ec9c495c963dea0b4c"
   end
 
   depends_on "docutils" => :build
