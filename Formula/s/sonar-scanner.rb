1e87c5dd35083960b29a7d0f74f8cec615d763cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-26 20:26:20 +0000

    sonar-scanner: update 6.1.0.4477 bottle.

--- Formula/s/sonar-scanner.rb
+++ Formula/s/sonar-scanner.rb
@@ -7,13 +7,13 @@ class SonarScanner < Formula
   head "https://github.com/SonarSource/sonar-scanner-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
-    sha256 cellar: :any_skip_relocation, ventura:        "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
-    sha256 cellar: :any_skip_relocation, monterey:       "c0cf45694c4683b4ffd2735c79433b954d7c531496255f3396a8ef6678ba301e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0dc9079487bbc35160e68c4c9fab52d391e3381eb91d8ce781538b2ec7763c49"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3a4a993d143f125bf902bfaea4e8a69c064f944eb682c4f2a311d765a8fa0b8c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3a4a993d143f125bf902bfaea4e8a69c064f944eb682c4f2a311d765a8fa0b8c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a4a993d143f125bf902bfaea4e8a69c064f944eb682c4f2a311d765a8fa0b8c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3a4a993d143f125bf902bfaea4e8a69c064f944eb682c4f2a311d765a8fa0b8c"
+    sha256 cellar: :any_skip_relocation, ventura:        "3a4a993d143f125bf902bfaea4e8a69c064f944eb682c4f2a311d765a8fa0b8c"
+    sha256 cellar: :any_skip_relocation, monterey:       "3a4a993d143f125bf902bfaea4e8a69c064f944eb682c4f2a311d765a8fa0b8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50f629e9d25548f2d0eb31cf42a16edcc5f4f94a4acfa649834949ebbc8fcaa2"
   end
 
   depends_on "openjdk"
