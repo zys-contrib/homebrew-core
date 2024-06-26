33fcddc62644fa904da1f44108ded08ecece042b
Michael Bryzek <mbryzek@alum.mit.edu>
2024-06-26 15:47:55 -0400

    apibuilder-cli 0.1.46

--- Formula/a/apibuilder-cli.rb
+++ Formula/a/apibuilder-cli.rb
@@ -1,8 +1,8 @@
 class ApibuilderCli < Formula
   desc "Command-line interface to generate clients for api builder"
   homepage "https://www.apibuilder.io"
-  url "https://github.com/apicollective/apibuilder-cli/archive/refs/tags/0.1.45.tar.gz"
-  sha256 "4048f5a0de69ef02e83245baf17b7bf5ee7f969c6d7e400194042f8e519f7fb9"
+  url "https://github.com/apicollective/apibuilder-cli/archive/refs/tags/0.1.46.tar.gz"
+  sha256 "8189b26f158d30ac09a8855fffa72fbaadbcb777eb3c1fca23e2532f333ba228"
   license "MIT"
 
   bottle do
