330f38337fa3e4e1619f9bb48313458c78b4b700
Peter Osinski <peter@fonoa.com>
2024-06-27 23:49:21 +0200

    logdy 0.12.0
    autobump: add logdy
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/l/logdy.rb
+++ Formula/l/logdy.rb
@@ -1,8 +1,8 @@
 class Logdy < Formula
   desc "Web based real-time log viewer"
   homepage "https://logdy.dev"
-  url "https://github.com/logdyhq/logdy-core/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "906fd9d019e6955ecad93e55c9497c39506f11d3bf4f2bbabe394a562caa91a0"
+  url "https://github.com/logdyhq/logdy-core/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "e188b1d1d6cc6678cabf663a4f4446201f6d1c9d65d8a6c0769848fd939e3deb"
   license "Apache-2.0"
 
   bottle do
