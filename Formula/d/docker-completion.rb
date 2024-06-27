8997ac304f074bff669fc2cb97ba478ff7f613e6
calvinit <541298948@qq.com>
2024-06-27 11:00:06 +0800

    docker-completion 27.0.2

--- Formula/d/docker-completion.rb
+++ Formula/d/docker-completion.rb
@@ -1,8 +1,8 @@
 class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
-  url "https://github.com/docker/cli/archive/refs/tags/v27.0.1.tar.gz"
-  sha256 "f73d47bd7dc176097c0e20797df1ec54bfaab3422aafd8810fd6830b626fe306"
+  url "https://github.com/docker/cli/archive/refs/tags/v27.0.2.tar.gz"
+  sha256 "a2b31d4c8143c8b126e98a359639f51727fc83fc1e61670efbdeaa7dea92fd5a"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
