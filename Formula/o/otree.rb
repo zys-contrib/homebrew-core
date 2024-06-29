a9f1529ea67cc7b3bde4aba1bd5c301e59ea0906
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2024-06-29 09:15:09 -0700

    otree 0.2.0 (new formula)
    autobump: add otree
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/o/otree.rb
@@ -0,0 +1,39 @@
+class Otree < Formula
+  desc "Command-line tool to view objects (JSON/YAML/TOML) in TUI tree widget"
+  homepage "https://github.com/fioncat/otree"
+  url "https://github.com/fioncat/otree/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "58ff9da6ed8653787082771377d6a7e099b187651f3e288856a7eb1f58355c81"
+  license "MIT"
+  head "https://github.com/fioncat/otree.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"example.json").write <<~JSON
+      {
+        "string": "Hello, World!",
+        "number": 12345,
+        "float": 123.45
+      }
+    JSON
+    require "pty"
+    r, w, pid = PTY.spawn("#{bin}/otree example.json")
+    r.winsize = [36, 120]
+    sleep 1
+    w.write "q"
+    begin
+      output = r.read
+      assert_match "Hello, World!", output
+      assert_match "12345", output
+      assert_match "123.45", output
+    rescue Errno::EIO
+      # GNU/Linux raises EIO when read is done on closed pty
+    end
+  ensure
+    Process.kill("TERM", pid)
+  end
+end
