ea465ebe246770e5517fb3e8208f52ad3ae2acde
Rui Chen <rui@chenrui.dev>
2024-06-30 03:50:45 +0000

    chkbit: add 4.2.1 bottle.

--- Formula/c/chkbit.rb
+++ Formula/c/chkbit.rb
@@ -8,6 +8,16 @@ class Chkbit < Formula
   license "MIT"
   head "https://github.com/laktak/chkbit-py.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "eca43da47371875365f790d768a4c659af462c1aa0b12fb5ad462dd45a20e38a"
+    sha256 cellar: :any,                 arm64_ventura:  "6266986c2a346550d5112318e3d3d174a531943e87640923f7a86286ad310dc8"
+    sha256 cellar: :any,                 arm64_monterey: "ea5749e4aa1f96805a69a0f0b0ceef26c49ba5894fce7aff8af1665997b85720"
+    sha256 cellar: :any,                 sonoma:         "9fbcdc42034ae0d906ca14794a07f0c2e37278046c5fdcfcc349043340a6e550"
+    sha256 cellar: :any,                 ventura:        "81ce5dc4ffcc1812df1101ab0c697c7eb60548ae9d4e42844267edabd2af25cc"
+    sha256 cellar: :any,                 monterey:       "73327c0a66310ba120b57391fc5e93601dea45fcd601712d0c3f2ca1f9f77e80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c5eb0a6a4d5101e55ce44c31bb6369b34c937723e844befd18c6271459d4f00"
+  end
+
   depends_on "rust" => :build
   depends_on "python@3.12"
 
