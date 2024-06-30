eb18cbf751d92534fd4001892f9569a7caa3121c
Rui Chen <rui@chenrui.dev>
2024-06-30 12:20:43 +0000

    porter: add 1.1.0 bottle.

--- Formula/p/porter.rb
+++ Formula/p/porter.rb
@@ -6,6 +6,16 @@ class Porter < Formula
   license "Apache-2.0"
   head "https://github.com/getporter/porter.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5132b84b398bfa7cc1fcc82143d0dca926be611cd13180c33cd3708e7c73e5df"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a77917b1ac5dca92cd823dc31a63763bfbf03640adb813642aa1eaebacff193f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2add51a382760271a7cec976bf34b3f9e596a70b98dfb9bb7b2b089898db2e93"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8eb3135d2dd15c03051302285658430822eb16157d833257b48a27fc5ac84fdb"
+    sha256 cellar: :any_skip_relocation, ventura:        "f5515c1f5876d71b62b3675313ad0db8f15aa01bba867f78d93b288708b9f10f"
+    sha256 cellar: :any_skip_relocation, monterey:       "290d638c58363ed55db7839cde4b611ac4ec4e7ca1a3ad66ba5dedbbaf70f763"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "00ae7c88b62c0519bd245b0374555acd50542e884995915ba39919384629f536"
+  end
+
   depends_on "go" => :build
 
   def install
