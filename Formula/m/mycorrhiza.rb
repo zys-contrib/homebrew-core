6a0f93636725ba01ffdff7831d4b5ba418bd22b5
Rui Chen <rui@chenrui.dev>
2024-06-30 12:14:07 -0400

    mycorrhiza 1.15.0
    autobump: add mycorrhiza
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mycorrhiza.rb
+++ Formula/m/mycorrhiza.rb
@@ -1,8 +1,8 @@
 class Mycorrhiza < Formula
   desc "Lightweight wiki engine with hierarchy support"
   homepage "https://mycorrhiza.wiki"
-  url "https://github.com/bouncepaw/mycorrhiza/archive/refs/tags/v1.14.0.tar.gz"
-  sha256 "6448fe7fa6198cb3346f63d0857d5f364d0aa8fafbdc95f56ef39fbe774bbabc"
+  url "https://github.com/bouncepaw/mycorrhiza/archive/refs/tags/v1.15.0.tar.gz"
+  sha256 "d3f462e022be6a68f8e40352db916d20adc0cb7ad63b11be53aebea369e22641"
   license "AGPL-3.0-only"
   head "https://github.com/bouncepaw/mycorrhiza.git", branch: "master"
 
