476b7c49582790780912fe3481a8d7d637f24958
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-01 00:54:10 +0000

    spirv-llvm-translator 18.1.2

--- Formula/s/spirv-llvm-translator.rb
+++ Formula/s/spirv-llvm-translator.rb
@@ -1,8 +1,8 @@
 class SpirvLlvmTranslator < Formula
   desc "Tool and a library for bi-directional translation between SPIR-V and LLVM IR"
   homepage "https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
-  url "https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v18.1.1.tar.gz"
-  sha256 "c1c7aee4ea23a6a1089bb7f7bad198c28ada65c5b7671434562fe0241d8674d6"
+  url "https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v18.1.2.tar.gz"
+  sha256 "4724372934041c8feb8bcafea1c9d086ab2de9f323599068943ef61ddb0bca51"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
