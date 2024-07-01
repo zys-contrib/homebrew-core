f552ac3ff119f2640ad9eea67cecd8417d451ff5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-01 18:52:42 +0000

    zpaqfranz 60.1

--- Formula/z/zpaqfranz.rb
+++ Formula/z/zpaqfranz.rb
@@ -1,8 +1,8 @@
 class Zpaqfranz < Formula
   desc "Deduplicating command-line archiver and backup tool"
   homepage "https://github.com/fcorbelli/zpaqfranz"
-  url "https://github.com/fcorbelli/zpaqfranz/archive/refs/tags/59.9.tar.gz"
-  sha256 "d29b124c93d1ba93831a0fc95b6ce16dfc02d667f86515720733cd9aa6caf1ef"
+  url "https://github.com/fcorbelli/zpaqfranz/archive/refs/tags/60.1.tar.gz"
+  sha256 "359d89f6fbbea6d730ffd592894a42e5769a10719fc9a5fdc0af2f96ad7e350e"
   license all_of: [:public_domain, "MIT", "Zlib", "Unlicense", "BSD-2-Clause", "Apache-2.0"]
   head "https://github.com/fcorbelli/zpaqfranz.git", branch: "main"
 
