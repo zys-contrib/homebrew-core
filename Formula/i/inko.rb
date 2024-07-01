f674f63dff0d912bcf34e7a89fc1eb5200ce4050
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-01 14:19:58 +0000

    inko: update 0.15.0 bottle.

--- Formula/i/inko.rb
+++ Formula/i/inko.rb
@@ -7,13 +7,13 @@ class Inko < Formula
   head "https://github.com/inko-lang/inko.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "f6ff66fdfb3aac69263c32a8a29d13e9d28a80ae33807f34460e55d8c1b228c6"
-    sha256 cellar: :any,                 arm64_ventura:  "be59d916d29d85bb8bc4474eb1c7d42a56236835c3c21b0e36fb9e9df0a25e6e"
-    sha256 cellar: :any,                 arm64_monterey: "9522c1f89b997dedaa3167ce4dbfa4a2d8c660acddecd32a99a515922e851b52"
-    sha256 cellar: :any,                 sonoma:         "8e32d823ce9712ae2d5a2b9cbe0c9b727223098b3e66b003da087032be9f6818"
-    sha256 cellar: :any,                 ventura:        "178865db1e2b60b4085a2465e8a3879794030a6d22c99b58c95e4bdf5418ef1b"
-    sha256 cellar: :any,                 monterey:       "6ef924939c42d7bb2ec4e0d65cf293147a593f829619928d2580b419ec19b26c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "14a02c119990d6a17062290439ac74e6667b41dcb90b18cd90b36d2a09715e10"
+    sha256 cellar: :any,                 arm64_sonoma:   "51a6d9635099eb9183bca841bc8e8eeb1b58bc22095354f4c510913ddc9e0690"
+    sha256 cellar: :any,                 arm64_ventura:  "9e48dbcd041065ab6e05cd77bf70b98005f73e8b85eb66dbddd2b92a3f3bebd6"
+    sha256 cellar: :any,                 arm64_monterey: "8a1302a1751c5e74a3791aaad462a9c7b976df193964e9a0b257a3a393af994c"
+    sha256 cellar: :any,                 sonoma:         "cc72afee9d9e29467f218873235f037e77313179911c73f0c1069bb0ab048a18"
+    sha256 cellar: :any,                 ventura:        "67708a7b08d56dfd788604698b060482b937902821484b5dc44b0f6e52a8ccff"
+    sha256 cellar: :any,                 monterey:       "c10652b60c03eb4ffb4f62d187a2a3b601449df0ff9ffbf9dc71d9bff85c7d85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b152aca6d20dd0e8b33867db08d2129ab72d1785b365d930baad9097f95b5ad"
   end
 
   depends_on "coreutils" => :build
