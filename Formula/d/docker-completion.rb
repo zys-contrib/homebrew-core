cf74efaf9ff9a7ccef58db81cd3f20367fefc7f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-01 21:03:30 +0000

    docker-completion: update 27.0.3 bottle.

--- Formula/d/docker-completion.rb
+++ Formula/d/docker-completion.rb
@@ -11,7 +11,7 @@ class DockerCompletion < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f536c8dffefc8894e4ae393997f7f02bdb9a06b9dc2bb3a51df193489428faa3"
+    sha256 cellar: :any_skip_relocation, all: "8ae0dd49970e812a8f3e1188e232c5002854c5ba8bb3a2e8d86183166cfcfca9"
   end
 
   # These used to also be provided by the `docker` formula.
