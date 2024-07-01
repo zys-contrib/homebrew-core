3646a53fcb27617475ac1d0eb841accd5581eea8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-01 19:22:24 +0000

    uvg266: update 0.8.1 bottle.

--- Formula/u/uvg266.rb
+++ Formula/u/uvg266.rb
@@ -7,13 +7,13 @@ class Uvg266 < Formula
   head "https://github.com/ultravideo/uvg266.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "b3f0dcbb7047b2982860c57b4b9d94da3b69ee513c6587be4dd95fbc223ab94f"
-    sha256 cellar: :any,                 arm64_ventura:  "fe2dc71d9b62684ff83a8de3fbd7ba21e491670d24a825a4eb19f05ac4df3d21"
-    sha256 cellar: :any,                 arm64_monterey: "96832bedf204b7315199fe2c73d915d8899b118c0fdeb7e7366444238dcad9c5"
-    sha256 cellar: :any,                 sonoma:         "84b745699bddeeda3528c792573cbad0bee61197b6d54520bf64eaecc53fc1da"
-    sha256 cellar: :any,                 ventura:        "a4903dd76bb6286cc20becf914f043649e8b7c3b3fd2029c966b1a7f4553a3e0"
-    sha256 cellar: :any,                 monterey:       "84e5397eca32b986e135eebf2bd67bf6b80cda9388d6396437d8d12b575aad21"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e64fb73e4bdaece14458cdfd4c8c123c298a8ce3fcfb483f2c3dc2ba22902bb8"
+    sha256 cellar: :any,                 arm64_sonoma:   "2e1d62a8487ecb14e1b585c0a7e4b6d3a59ee1a9c2a1ead291697968c1b6eb55"
+    sha256 cellar: :any,                 arm64_ventura:  "aeee7925bcfdc18d227a1d911489a03abd2f5ca434336c9da4b308516641d1dc"
+    sha256 cellar: :any,                 arm64_monterey: "b958ad1e7e6fa2607fc592db623418c2a7125d6bd7d75b247fe8c6d1fbb5b4ab"
+    sha256 cellar: :any,                 sonoma:         "1ff7b317c2ef872de0f794eb448e26cf4b1569352b2dac1e041357756d94b3da"
+    sha256 cellar: :any,                 ventura:        "9219b51849b23154d86aec45505cc450e83833a14b8c5ecf32cec6fc889a4096"
+    sha256 cellar: :any,                 monterey:       "5b0ac26a3a6a6162769cd3d08d7eeb2fc43ad4bfdab0905e6e0044ef8fa73c95"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b2b2fc85f72c8d371bd32609a2723478c4c5f7575e3337827b5991c5b91f498d"
   end
 
   depends_on "cmake" => :build
