2a5f762aa53e557cb9a372b9fe25c28874f3a53d
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-07-02 23:57:35 +0800

    cmake-docs 3.30.0

--- Formula/c/cmake-docs.rb
+++ Formula/c/cmake-docs.rb
@@ -1,10 +1,10 @@
 class CmakeDocs < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.29.6/cmake-3.29.6.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.29.6.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.29.6.tar.gz"
-  sha256 "1391313003b83d48e2ab115a8b525a557f78d8c1544618b48d1d90184a10f0af"
+  url "https://github.com/Kitware/CMake/releases/download/v3.30.0/cmake-3.30.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.30.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.30.0.tar.gz"
+  sha256 "157e5be6055c154c34f580795fe5832f260246506d32954a971300ed7899f579"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
