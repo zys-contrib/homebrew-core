f7406e3756637f92ec85392bf7e0f41fc1880764
Rui Chen <rui@chenrui.dev>
2024-07-03 14:21:55 +0000

    mako: add 0.7.4 bottle.

--- Formula/m/mako.rb
+++ Formula/m/mako.rb
@@ -7,6 +7,16 @@ class Mako < Formula
   sha256 "766b99d3116941f67f19cb055c8817c37f52dd1cbeb3b8953e64a3b085a1c38a"
   license "MIT"
 
+  bottle do
+    sha256                               arm64_sonoma:   "8c345f403df060973f1e2674bf73b889c348394fecd1417642f99f1999709cca"
+    sha256                               arm64_ventura:  "35a0b885b2db33012e6a3192f87739386eb02bf8d48c0c32db69061c9b867f00"
+    sha256                               arm64_monterey: "8f682a3ad47087344a75cae0aaf4e34e618c8b7205a369b30453ed9f6ffbe726"
+    sha256                               sonoma:         "8322466e39c872f1cdaf8e249aeada0afdfe7cd76e5b96db784ed1fecce70218"
+    sha256                               ventura:        "23ed55ec94f08d472852469fb4059faaad20f1798256b46d3d83c03cf8efea95"
+    sha256                               monterey:       "8d1bae99b177bbe20e5656c0096edba964e51c2276a4c142921d6c79e758d28d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d87a2db330372c4269876f4cccd4914bc0dd798db69f7ecd388aa170b6d30efd"
+  end
+
   depends_on "node"
 
   def install
