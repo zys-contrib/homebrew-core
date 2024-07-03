6a09a85e7d822de912db68955f5f0585d437c574
Rui Chen <rui@chenrui.dev>
2024-07-03 09:30:50 -0400

    beakerlib 1.31
    autobump: add beakerlib
    
    beakerlib: remove patch

--- Formula/b/beakerlib.rb
+++ Formula/b/beakerlib.rb
@@ -1,8 +1,8 @@
 class Beakerlib < Formula
   desc "Shell-level integration testing library"
   homepage "https://github.com/beakerlib/beakerlib"
-  url "https://github.com/beakerlib/beakerlib/archive/refs/tags/1.30.tar.gz"
-  sha256 "9161dd08ca7a9066d2d85ff6911b7c8271fbd6ba76d5fe168f2ad3e705bd2615"
+  url "https://github.com/beakerlib/beakerlib/archive/refs/tags/1.31.tar.gz"
+  sha256 "8aeba4c8174c31b86241d88138e61d541adaf07128d8233be2dd4e199c61f7d2"
   license "GPL-2.0-only"
 
   bottle do
@@ -19,13 +19,6 @@ class Beakerlib < Formula
     # Fix `readlink`
     depends_on "coreutils"
     depends_on "gnu-getopt"
-  end
-
-  # Add BSD compatibility. Squash commit of:
-  # https://github.com/beakerlib/beakerlib/pull/172
-  patch do
-    url "https://github.com/LecrisUT/beakerlib/commit/367ccaeb9983752b5c6e93277fd333c29a58e8c2.patch?full_index=1"
-    sha256 "e50e098bd1668feb22d27aa604750f222a0df8566ae4887075e2861b760de1b9"
   end
 
   def install
