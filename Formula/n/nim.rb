3d14ca75de74cbeb7b1f295a8c0ff6ccaa6aa11f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-03 15:50:59 +0000

    nim 2.0.8

--- Formula/n/nim.rb
+++ Formula/n/nim.rb
@@ -1,8 +1,8 @@
 class Nim < Formula
   desc "Statically typed compiled systems programming language"
   homepage "https://nim-lang.org/"
-  url "https://nim-lang.org/download/nim-2.0.6.tar.xz"
-  sha256 "fbcd5d731743adec2b3bb9bcf6f5fd993ed11b250f957166bebf328f307cba6a"
+  url "https://nim-lang.org/download/nim-2.0.8.tar.xz"
+  sha256 "5702da844700d3129db73170b5c606adbdfb87e82b816c0d91107ea20a65df16"
   license "MIT"
   head "https://github.com/nim-lang/Nim.git", branch: "devel"
 
