87bdc60eb4c7c0dadc4c6410e7ff31af77e2e157
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-04 23:08:14 +0000

    retire: update 5.1.1 bottle.

--- Formula/r/retire.rb
+++ Formula/r/retire.rb
@@ -9,13 +9,13 @@ class Retire < Formula
   head "https://github.com/RetireJS/retire.js.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "34f925e170fc98e2747c9c041c4d6046337ada00b677cea59cf36fa9c8d79cd2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "34f925e170fc98e2747c9c041c4d6046337ada00b677cea59cf36fa9c8d79cd2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "34f925e170fc98e2747c9c041c4d6046337ada00b677cea59cf36fa9c8d79cd2"
-    sha256 cellar: :any_skip_relocation, sonoma:         "34f925e170fc98e2747c9c041c4d6046337ada00b677cea59cf36fa9c8d79cd2"
-    sha256 cellar: :any_skip_relocation, ventura:        "34f925e170fc98e2747c9c041c4d6046337ada00b677cea59cf36fa9c8d79cd2"
-    sha256 cellar: :any_skip_relocation, monterey:       "34f925e170fc98e2747c9c041c4d6046337ada00b677cea59cf36fa9c8d79cd2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1eb9df55ab18cf43dd141a6e7a0e14dbb03156e2b79a5a81c2a9f9f35af53807"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4b9729783656d9a973f99f37fdd259f5679d5f758b9082614145384d67aed759"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4b9729783656d9a973f99f37fdd259f5679d5f758b9082614145384d67aed759"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4b9729783656d9a973f99f37fdd259f5679d5f758b9082614145384d67aed759"
+    sha256 cellar: :any_skip_relocation, sonoma:         "4b9729783656d9a973f99f37fdd259f5679d5f758b9082614145384d67aed759"
+    sha256 cellar: :any_skip_relocation, ventura:        "4b9729783656d9a973f99f37fdd259f5679d5f758b9082614145384d67aed759"
+    sha256 cellar: :any_skip_relocation, monterey:       "4b9729783656d9a973f99f37fdd259f5679d5f758b9082614145384d67aed759"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b0496957c34f96fc119904291904607c3fba31e3d11ddd2b398201b24f20772"
   end
 
   depends_on "node"
