2b370a5f29b30cab80c375d98bc32eda70acb285
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-04 19:02:40 +0000

    kew: update 2.6.0 bottle.

--- Formula/k/kew.rb
+++ Formula/k/kew.rb
@@ -7,7 +7,7 @@ class Kew < Formula
   head "https://github.com/ravachol/kew.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1ee5523696924ffcf200a3088f3bc15f42983e9c017d31c7ef9711cec1aa573d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "50b30ebd55c02917bde3d93e28b2c672e61f62821a5b5290a52eea467ef287dd"
   end
 
   depends_on "pkg-config" => :build
