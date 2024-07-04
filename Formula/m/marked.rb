c106a721105eb6cac8ba7ec380540c2fb7eb091e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-04 00:42:41 +0000

    marked 13.0.2

--- Formula/m/marked.rb
+++ Formula/m/marked.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Marked < Formula
   desc "Markdown parser and compiler built for speed"
   homepage "https://marked.js.org/"
-  url "https://registry.npmjs.org/marked/-/marked-13.0.1.tgz"
-  sha256 "fe242fab365db4d7dc5989d7c7e517a270a6f66ac52ba154790eb5739a5c9392"
+  url "https://registry.npmjs.org/marked/-/marked-13.0.2.tgz"
+  sha256 "52a0f001a07f21735259e7bdc9967bc2b2aa4b710bc0e2f198031cd0f2cd7e52"
   license "MIT"
 
   bottle do
