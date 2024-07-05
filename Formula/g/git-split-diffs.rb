27eee4c8ca6d7c8e6628ad60148228b757805768
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-05 06:32:15 +0000

    git-split-diffs 2.1.0

--- Formula/g/git-split-diffs.rb
+++ Formula/g/git-split-diffs.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GitSplitDiffs < Formula
   desc "Syntax highlighted side-by-side diffs in your terminal"
   homepage "https://github.com/banga/git-split-diffs"
-  url "https://registry.npmjs.org/git-split-diffs/-/git-split-diffs-2.0.1.tgz"
-  sha256 "02ed2229e56802e628182f0e86d2a77da741f899e07fdb5dcbafd74e3e4d1f74"
+  url "https://registry.npmjs.org/git-split-diffs/-/git-split-diffs-2.1.0.tgz"
+  sha256 "4d49a8d4fd4e674ecd639cd9057cd3e04a503af5322b61c62b82a8221fc60729"
   license "MIT"
 
   bottle do
