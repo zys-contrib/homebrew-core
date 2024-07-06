2d7a94064046c7d4cc96daaf4fcfd656ddc75241
Adrian Ho <the.gromgit@gmail.com>
2024-07-06 19:05:12 +0800

    gnu-sed: move `man` symlink inside `gnubin`

--- Formula/g/gnu-sed.rb
+++ Formula/g/gnu-sed.rb
@@ -40,7 +40,7 @@ class GnuSed < Formula
       (libexec/"gnuman/man1").install_symlink man1/"gsed.1" => "sed.1"
     end
 
-    libexec.install_symlink "gnuman" => "man"
+    (libexec/"gnubin").install_symlink "../gnuman" => "man"
   end
 
   def caveats
