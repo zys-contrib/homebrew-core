4ca7041ae89092f61c44a5108a87d94c254baadb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-06 10:25:40 +0000

    plantuml: update 1.2024.6 bottle.

--- Formula/p/plantuml.rb
+++ Formula/p/plantuml.rb
@@ -12,13 +12,13 @@ class Plantuml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c1419a93cd413b18e405195481fbb52a2c9502fa55d86341ea89d13a2278f7b0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "23e3b4e71bc8321cba4d8f57607c43011e222539bfc8e1857613c6e3a7faf290"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cd1e3963619bf85e8d57a4ab92cad35185141d2b62dde7473a57333b7382dba"
-    sha256 cellar: :any_skip_relocation, sonoma:         "83d578ffb2823c40bdd2afa956aed301bcda14272a7ad908f4342d2cf98694c2"
-    sha256 cellar: :any_skip_relocation, ventura:        "068d40e1e79bf45e2614bd66dcdb0e671be18e9e3dff20e9bb86523d07f1ba00"
-    sha256 cellar: :any_skip_relocation, monterey:       "d7173cd22f8d879dbf054ce3c63f58a9b7e38bc4814c6ec45b52c749985cc0f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d3b563ab40e785a91dc12f35c605f53afd0d18cb82e09d457290c5533f7c8e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0492d42c32b924bc419258fcab1fffa0f53bca721198f279f380a1c731375e40"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0492d42c32b924bc419258fcab1fffa0f53bca721198f279f380a1c731375e40"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0492d42c32b924bc419258fcab1fffa0f53bca721198f279f380a1c731375e40"
+    sha256 cellar: :any_skip_relocation, sonoma:         "0492d42c32b924bc419258fcab1fffa0f53bca721198f279f380a1c731375e40"
+    sha256 cellar: :any_skip_relocation, ventura:        "0492d42c32b924bc419258fcab1fffa0f53bca721198f279f380a1c731375e40"
+    sha256 cellar: :any_skip_relocation, monterey:       "0492d42c32b924bc419258fcab1fffa0f53bca721198f279f380a1c731375e40"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41fddbfe48d1c936259b4f57f677f8503bd2b7b5e49564455cf0dc1da22921ef"
   end
 
   depends_on "graphviz"
