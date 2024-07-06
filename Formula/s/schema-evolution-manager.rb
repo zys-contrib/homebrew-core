c631a7639500b576a6aca2b32fe91c5699af53a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-06 16:58:23 +0000

    schema-evolution-manager: update 0.9.48 bottle.

--- Formula/s/schema-evolution-manager.rb
+++ Formula/s/schema-evolution-manager.rb
@@ -6,7 +6,13 @@ class SchemaEvolutionManager < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "528017b98ceaa5b6847d04504e1f0abe47b4c67730456968f6d42b768379bab4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2ac1ec508afeeeab2339bb37433d0e23d840bc8217f2cd358eb2d6000cdbfccc"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2ac1ec508afeeeab2339bb37433d0e23d840bc8217f2cd358eb2d6000cdbfccc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ac1ec508afeeeab2339bb37433d0e23d840bc8217f2cd358eb2d6000cdbfccc"
+    sha256 cellar: :any_skip_relocation, sonoma:         "2ac1ec508afeeeab2339bb37433d0e23d840bc8217f2cd358eb2d6000cdbfccc"
+    sha256 cellar: :any_skip_relocation, ventura:        "2ac1ec508afeeeab2339bb37433d0e23d840bc8217f2cd358eb2d6000cdbfccc"
+    sha256 cellar: :any_skip_relocation, monterey:       "2ac1ec508afeeeab2339bb37433d0e23d840bc8217f2cd358eb2d6000cdbfccc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1f4da8c106298975f85975d237bc7bf2c3ecca621f1c25be376166c9e6036419"
   end
 
   uses_from_macos "ruby"
