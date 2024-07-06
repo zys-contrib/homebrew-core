72f526df601635dcb3b04c35dd97ecadbd0c03bc
Rui Chen <rui@chenrui.dev>
2024-07-06 12:38:11 -0400

    helm-docs 1.14.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    helm-docs: patch go version
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/helm-docs.rb
+++ Formula/h/helm-docs.rb
@@ -1,8 +1,8 @@
 class HelmDocs < Formula
   desc "Tool for automatically generating markdown documentation for helm charts"
   homepage "https://github.com/norwoodj/helm-docs"
-  url "https://github.com/norwoodj/helm-docs/archive/refs/tags/v1.13.1.tar.gz"
-  sha256 "b1a0eba4120614f3ddc4d5cbe1133dcfc44ef746543c49a3e07168d2db55827d"
+  url "https://github.com/norwoodj/helm-docs/archive/refs/tags/1.14.0.tar.gz"
+  sha256 "65386a0c6401486d135a7c436b28fd1e9b8d0232bc2c702f8c7406a1139aa54a"
   license "GPL-3.0-or-later"
 
   # This repository originally used a date-based version format like `19.0110`
@@ -26,6 +26,12 @@ class HelmDocs < Formula
   end
 
   depends_on "go" => :build
+
+  # patch go version, upstream pr ref, https://github.com/norwoodj/helm-docs/pull/247
+  patch do
+    url "https://github.com/norwoodj/helm-docs/commit/0b634309511c595c50b32d3a3090c45fa5a4cd2e.patch?full_index=1"
+    sha256 "6607867eb63efb3e67a42a704f37615277f4638f167cbfdd697d35a335710b0a"
+  end
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/helm-docs"
