4e9e6ff432289de59ca4433ceb42046bf09f04ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-06 08:09:15 +0000

    codecov-cli: bump python resources

--- Formula/c/codecov-cli.rb
+++ Formula/c/codecov-cli.rb
@@ -6,6 +6,7 @@ class CodecovCli < Formula
   url "https://files.pythonhosted.org/packages/fa/f1/daea230aceead1973be73df60173ba3f416d1931f7f5cb34b94ea8f4f83e/codecov-cli-0.7.2.tar.gz"
   sha256 "e755bca56404472c078255258854b5feca0b322f1ce613b140d193995e0a1bdc"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/codecov/codecov-cli.git", branch: "main"
 
   bottle do
@@ -28,8 +29,8 @@ class CodecovCli < Formula
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/07/b3/e02f4f397c81077ffc52a538e0aec464016f1860c472ed33bd2a1d220cc5/certifi-2024.6.2.tar.gz"
-    sha256 "3cd43f1c6fa7dedc5899d69d3ad0398fd018ad1a17fba83ddaf78aa46c747516"
+    url "https://files.pythonhosted.org/packages/c2/02/a95f2b11e207f68bc64d7aae9666fed2e2b3f307748d5123dffb72a1bbea/certifi-2024.7.4.tar.gz"
+    sha256 "5a1e7645bc0ec61a09e26c36f6106dd4cf40c6db3a1fb6352b0244e7fb057c7b"
   end
 
   resource "charset-normalizer" do
@@ -93,8 +94,8 @@ class CodecovCli < Formula
   end
 
   resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/0d/9d/c587bea18a7e40099857015baee4cece7aca32cd404af953bdeb95ac8e47/setuptools-70.1.1.tar.gz"
-    sha256 "937a48c7cdb7a21eb53cd7f9b59e525503aa8abaf3584c730dc5f7a5bec3a650"
+    url "https://files.pythonhosted.org/packages/8d/e6/2fc95aec377988ff3ca882aa58d4f6ab35ff59a12b1611a9fe3075eb3019/setuptools-70.2.0.tar.gz"
+    sha256 "bd63e505105011b25c3c11f753f7e3b8465ea739efddaccef8f0efac2137bac1"
   end
 
   resource "sniffio" do
