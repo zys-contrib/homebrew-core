88f53489cdf186e062c1f6ae626584efb68908a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-07 22:12:49 +0000

    jbang: update 0.117.1 bottle.

--- Formula/j/jbang.rb
+++ Formula/j/jbang.rb
@@ -6,7 +6,13 @@ class Jbang < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "79d6a7da49c643e131e430f3e538fa083c4bd1b33b287945e4851dcadbbc8e41"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8ab784d8fb46f220d5b9d2efc2f808d5ee3fe7157e00e4aa1b4bdbacaf9c1ae4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8ab784d8fb46f220d5b9d2efc2f808d5ee3fe7157e00e4aa1b4bdbacaf9c1ae4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8ab784d8fb46f220d5b9d2efc2f808d5ee3fe7157e00e4aa1b4bdbacaf9c1ae4"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8ab784d8fb46f220d5b9d2efc2f808d5ee3fe7157e00e4aa1b4bdbacaf9c1ae4"
+    sha256 cellar: :any_skip_relocation, ventura:        "8ab784d8fb46f220d5b9d2efc2f808d5ee3fe7157e00e4aa1b4bdbacaf9c1ae4"
+    sha256 cellar: :any_skip_relocation, monterey:       "8ab784d8fb46f220d5b9d2efc2f808d5ee3fe7157e00e4aa1b4bdbacaf9c1ae4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "48b6d07e4a603a02d26232d82e5ea9d28c74d418b4594aaa83ca2306b19d4a3e"
   end
 
   depends_on "openjdk"
