bb63c9c94a97bd064e1043e646cb068597c446f4
Rui Chen <rui@chenrui.dev>
2024-07-06 22:02:20 -0400

    minio-mc 20240703201725

--- Formula/m/minio-mc.rb
+++ Formula/m/minio-mc.rb
@@ -2,9 +2,9 @@ class MinioMc < Formula
   desc "Replacement for ls, cp and other commands for object storage"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc.git",
-      tag:      "RELEASE.2024-06-29T19-08-46Z",
-      revision: "f5b325d9d399e09b4d21384081e397a52f6ca89d"
-  version "20240629190846"
+      tag:      "RELEASE.2024-07-03T20-17-25Z",
+      revision: "d6ff97012549bfd91396447249297411ccfc79a7"
+  version "20240703201725"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/mc.git", branch: "master"
 
