a61fcb4d60aced80c046c89ba309071a3fb016c6
Sean Molenaar <SMillerDev@users.noreply.github.com>
2024-07-08 15:13:54 +0200

    stow: fix style

--- Formula/s/stow.rb
+++ Formula/s/stow.rb
@@ -6,8 +6,6 @@ class Stow < Formula
   sha256 "6fed67cf64deab6d3d9151a43e2c06c95cdfca6a88fab7d416f46a648b1d761d"
   license "GPL-3.0-or-later"
 
-  uses_from_macos "perl"
-
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "45497b1333daabd22772491fad60a232d13c06f9f2c4f492aa622ff739ed906a"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "29df1fe50041ef256b0a88499c935b560755a242690802fdd238d22e936e1c69"
@@ -17,6 +15,8 @@ class Stow < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "dba69c69de3ca06518f14f45c39affee246a630ecf53c6d6498e58afdbdc5f9f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c0c8ec472ea192228536e3bd3826b0156295bebfb6492cdff7b36c50a7c14a7a"
   end
+
+  uses_from_macos "perl"
 
   def install
     system "./configure", *std_configure_args
