b586b16e5b3be4af0e0ea62ad09083c303219deb
Alexander Bayandin <alexander@bayandin.dev>
2024-07-08 23:36:47 +0100

    x86_64-elf-gdb 15.1

--- Formula/x/x86_64-elf-gdb.rb
+++ Formula/x/x86_64-elf-gdb.rb
@@ -1,9 +1,9 @@
 class X8664ElfGdb < Formula
   desc "GNU debugger for x86_64-elf cross development"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-14.2.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gdb/gdb-14.2.tar.xz"
-  sha256 "2d4dd8061d8ded12b6c63f55e45344881e8226105f4d2a9b234040efa5ce7772"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-15.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-15.1.tar.xz"
+  sha256 "38254eacd4572134bca9c5a5aa4d4ca564cbbd30c369d881f733fb6b903354f2"
   license "GPL-3.0-or-later"
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
 
