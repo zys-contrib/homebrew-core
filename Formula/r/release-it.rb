67f8659a6962e7652b169f9a6cf51f013a447046
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-09 11:27:48 +0000

    release-it: update 17.5.0 bottle.

--- Formula/r/release-it.rb
+++ Formula/r/release-it.rb
@@ -8,13 +8,13 @@ class ReleaseIt < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5209d1094b0dc46e241bc3c06c25748a181bb000a9716d3db9c6dfdfb81e1f10"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5209d1094b0dc46e241bc3c06c25748a181bb000a9716d3db9c6dfdfb81e1f10"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5209d1094b0dc46e241bc3c06c25748a181bb000a9716d3db9c6dfdfb81e1f10"
-    sha256 cellar: :any_skip_relocation, sonoma:         "652ff57f9f4129d5c6d88eeceb21eeb891c7603eb5301a0901ab28cc836a88e1"
-    sha256 cellar: :any_skip_relocation, ventura:        "652ff57f9f4129d5c6d88eeceb21eeb891c7603eb5301a0901ab28cc836a88e1"
-    sha256 cellar: :any_skip_relocation, monterey:       "652ff57f9f4129d5c6d88eeceb21eeb891c7603eb5301a0901ab28cc836a88e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a83d8d60fbe16975193eed8e4c1576c0d7eb95778929185a458979e59e4568b2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "041711cdf9911a4e02edbd9067084a5e7787f416d48ce8b5af4af97129912395"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "041711cdf9911a4e02edbd9067084a5e7787f416d48ce8b5af4af97129912395"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "041711cdf9911a4e02edbd9067084a5e7787f416d48ce8b5af4af97129912395"
+    sha256 cellar: :any_skip_relocation, sonoma:         "d79a699bda619b1a8aab370adbd1f9abf96200e291b0104aa80509e6737f7651"
+    sha256 cellar: :any_skip_relocation, ventura:        "d79a699bda619b1a8aab370adbd1f9abf96200e291b0104aa80509e6737f7651"
+    sha256 cellar: :any_skip_relocation, monterey:       "d79a699bda619b1a8aab370adbd1f9abf96200e291b0104aa80509e6737f7651"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c7abe24d504a3b93cce391b3229d01d2cb88dfd8e646b10ce21a64fb703f0fe"
   end
 
   depends_on "node"
