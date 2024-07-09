77d9232e4275fb799b923ec5482a16a2c9fa028a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-09 01:27:11 +0000

    openrct2: update 0.4.12 bottle.

--- Formula/o/openrct2.rb
+++ Formula/o/openrct2.rb
@@ -8,13 +8,13 @@ class Openrct2 < Formula
   head "https://github.com/OpenRCT2/OpenRCT2.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any, arm64_sonoma:   "5ce75153a7f7612d0f58e3f6ba4f158f3c5eb531a5c269dd95f180940f340643"
-    sha256 cellar: :any, arm64_ventura:  "a362d1e4ddede47c09dcc95543bdc62a83caf7a8ad42780438658045ed5c808c"
-    sha256 cellar: :any, arm64_monterey: "a331b06f8d53d62d26088bda7554f78b87426ee301c305708cf7730e712427ef"
-    sha256 cellar: :any, sonoma:         "fe3058924ce5fbee3cef48d25abc8ddfb2f321e099e23bda2a2e046c7ba2d927"
-    sha256 cellar: :any, ventura:        "e68dce1d40ff3d3e57091b5bf7f17f8f12627d739e62f66e6200fddecd790094"
-    sha256 cellar: :any, monterey:       "03b739bf5751c063d8ff473be79d6c3b0f96ed2bebe9d511b6a05d85f111c0ce"
-    sha256               x86_64_linux:   "9fcc0f6f48c887dc2afe205844520158101f9b39ade7f00a4aff79aae743d306"
+    sha256 cellar: :any, arm64_sonoma:   "007465140d76d532c4794aecafb8c6906412949fccf9b8aa5829e91b716e1359"
+    sha256 cellar: :any, arm64_ventura:  "a063398bbf2dcd8699e7c4e08ad741b20aac0f022c3239f715e4e03f526a2968"
+    sha256 cellar: :any, arm64_monterey: "0d192f9b6702d6a0f7359320fe0a749a8060438eca36e1f7937c55caa0397499"
+    sha256 cellar: :any, sonoma:         "8238bf13a0f9f634a6de7be21c10a4de757ac15bf1ea33f882e1f1ce9865f101"
+    sha256 cellar: :any, ventura:        "c6a4af216409042272cc8c8a946918c5f9d2bda9d0791ebcf61fc6126ab90fb3"
+    sha256 cellar: :any, monterey:       "5e8d6f77d98ae90704ab470f7b80eb541642144b02831eaa93ff3047c1a7c175"
+    sha256               x86_64_linux:   "0ebcfea5973c2fa2bfa9a2b948ae5e79642c653d00479cb1422efc593ca0531a"
   end
 
   depends_on "cmake" => :build
