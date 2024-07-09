ca4373e83b05b6e764679a44ab9b6306b44c943a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-09 20:06:41 +0000

    python-setuptools: update 70.3.0 bottle.

--- Formula/p/python-setuptools.rb
+++ Formula/p/python-setuptools.rb
@@ -6,13 +6,13 @@ class PythonSetuptools < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a853a3f47b0c684845bcdd6fbb403fde1ef649351fd7eb5f0e903db4b5705c00"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a853a3f47b0c684845bcdd6fbb403fde1ef649351fd7eb5f0e903db4b5705c00"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a853a3f47b0c684845bcdd6fbb403fde1ef649351fd7eb5f0e903db4b5705c00"
-    sha256 cellar: :any_skip_relocation, sonoma:         "8f68199f96fb597ddd7bda594338d184f96425220066938ccc5397d667d5bb52"
-    sha256 cellar: :any_skip_relocation, ventura:        "8f68199f96fb597ddd7bda594338d184f96425220066938ccc5397d667d5bb52"
-    sha256 cellar: :any_skip_relocation, monterey:       "8f68199f96fb597ddd7bda594338d184f96425220066938ccc5397d667d5bb52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a853a3f47b0c684845bcdd6fbb403fde1ef649351fd7eb5f0e903db4b5705c00"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4891411677a80b8fa10903357526de1b19b7b70d373dfd16b95162b09e3ae878"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4891411677a80b8fa10903357526de1b19b7b70d373dfd16b95162b09e3ae878"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4891411677a80b8fa10903357526de1b19b7b70d373dfd16b95162b09e3ae878"
+    sha256 cellar: :any_skip_relocation, sonoma:         "3d26ae734c39e859e1366b14bb15391740f7db02fa1d383a7f67d03b1159653f"
+    sha256 cellar: :any_skip_relocation, ventura:        "3d26ae734c39e859e1366b14bb15391740f7db02fa1d383a7f67d03b1159653f"
+    sha256 cellar: :any_skip_relocation, monterey:       "582d5089333a9dc3a33b159d6cc261e8f620cd60f657dce5e34d12365eb0535f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4891411677a80b8fa10903357526de1b19b7b70d373dfd16b95162b09e3ae878"
   end
 
   depends_on "python@3.12" => [:build, :test]
