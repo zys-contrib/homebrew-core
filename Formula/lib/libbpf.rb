6a79988080b3a4e7299def16e64a96760839febc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-11 21:43:34 +0000

    libbpf 1.4.5

--- Formula/lib/libbpf.rb
+++ Formula/lib/libbpf.rb
@@ -1,8 +1,8 @@
 class Libbpf < Formula
   desc "Berkeley Packet Filter library"
   homepage "https://github.com/libbpf/libbpf"
-  url "https://github.com/libbpf/libbpf/archive/refs/tags/v1.4.4.tar.gz"
-  sha256 "b4fe864ed96cf276e3370a4706bafbafde1eccd5894418c4d8eeb941f371eb8a"
+  url "https://github.com/libbpf/libbpf/archive/refs/tags/v1.4.5.tar.gz"
+  sha256 "e225c1fe694b9540271b1f2f15eb882c21c34511ba7b8835b0a13003b3ebde8c"
   license "BSD-2-Clause"
 
   bottle do
