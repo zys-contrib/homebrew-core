1532384b874dd0edaed5d38a2ed976d00ab9fbe1
Patrick Linnane <patrick@linnane.io>
2024-07-13 01:14:39 +0000

    openbao: add 2.0.0-beta20 bottle.

--- Formula/o/openbao.rb
+++ Formula/o/openbao.rb
@@ -7,6 +7,16 @@ class Openbao < Formula
   license "MPL-2.0"
   head "https://github.com/openbao/openbao.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "05569884f8d47f8e4163768f5193c3140e7425d64a3a42900369b8bdf334f223"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e93f0aed96a95f00dfd195627d9a14b8e59a9eec50bf0d3f7e34769cc430721e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "73aec864eb75c6330ae59ce2fc6e8482a947b6f7a912b37957e74ea2d00fac21"
+    sha256 cellar: :any_skip_relocation, sonoma:         "34ecfcdd597db274b27f7b73346b9d67f99ac712ced3b9c84083373cb639da39"
+    sha256 cellar: :any_skip_relocation, ventura:        "3ddf7ed5f5d7727231a669b9649a904f8e2a14b4cd46a2e0180d658341bab30d"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b72e7ab6cee1e74ec06094b5dd063f28f7fc7754534518d0286d013eaa15aec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a45b87593643db336ee386d84bf1969eabfe6214fc80f56d96edc325a60d8d7e"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
   depends_on "yarn" => :build
