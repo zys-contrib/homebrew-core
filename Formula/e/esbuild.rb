d0d549533b9ab6c2540203c154d9b94ab4a9b0c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-13 19:43:26 +0000

    esbuild: update 0.23.0 bottle.

--- Formula/e/esbuild.rb
+++ Formula/e/esbuild.rb
@@ -6,13 +6,14 @@ class Esbuild < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "12311234421be478115e187c9013f91ccc8bb12145c3f7dc92c46d89e2bad6d5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "12311234421be478115e187c9013f91ccc8bb12145c3f7dc92c46d89e2bad6d5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "12311234421be478115e187c9013f91ccc8bb12145c3f7dc92c46d89e2bad6d5"
-    sha256 cellar: :any_skip_relocation, sonoma:         "46fd5d8cdf17a57939e8b72cfbdc2df4c73d21cca0666b7ded387d7a5b9429be"
-    sha256 cellar: :any_skip_relocation, ventura:        "46fd5d8cdf17a57939e8b72cfbdc2df4c73d21cca0666b7ded387d7a5b9429be"
-    sha256 cellar: :any_skip_relocation, monterey:       "46fd5d8cdf17a57939e8b72cfbdc2df4c73d21cca0666b7ded387d7a5b9429be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cf5ea30dc768145e289400d5e95ef9cfd467641b9ce06781334cda6360d59969"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "98279f532b870d25017a1fb370b308c179e25076a8a225ea5ec959428f111ce2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0bbe6514b794d5f0e9aafdb4b7f08f418133273f4f1525f58b48d7e8604318b5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ada0cf2a33099a5fc4a010e7dfca8309454d8915a70d37b07d7f82982776013c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1f0df1ef1dfd0fb24e8d274704535c5c43c72b6b3b6c49a6f10fbcb13200e922"
+    sha256 cellar: :any_skip_relocation, ventura:        "7f6f1549786b26bdc2137bb6cabfb0b9e7ff8301abfc111a5bf256b33938548d"
+    sha256 cellar: :any_skip_relocation, monterey:       "3cb9a7d924a11e2e3590a634aff74badc259f010dc9ea43bb3b6b6b7d9d39bd0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85aff4baa388f5404943c781633f79efbd2d28f37f928350beb356f2dc0599d6"
   end
 
   depends_on "go" => :build
