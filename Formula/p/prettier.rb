dd813ece8b99c9e57ea0a3ba4d070fc3cf94ac4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-13 12:57:44 +0000

    prettier: update 3.3.3 bottle.

--- Formula/p/prettier.rb
+++ Formula/p/prettier.rb
@@ -9,13 +9,13 @@ class Prettier < Formula
   head "https://github.com/prettier/prettier.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bf27af9f228a7857a92e337e48b8c452b8706dc85f0720b4ff01597672fb83c9"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bf27af9f228a7857a92e337e48b8c452b8706dc85f0720b4ff01597672fb83c9"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf27af9f228a7857a92e337e48b8c452b8706dc85f0720b4ff01597672fb83c9"
-    sha256 cellar: :any_skip_relocation, sonoma:         "bf27af9f228a7857a92e337e48b8c452b8706dc85f0720b4ff01597672fb83c9"
-    sha256 cellar: :any_skip_relocation, ventura:        "bf27af9f228a7857a92e337e48b8c452b8706dc85f0720b4ff01597672fb83c9"
-    sha256 cellar: :any_skip_relocation, monterey:       "bf27af9f228a7857a92e337e48b8c452b8706dc85f0720b4ff01597672fb83c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e1c6f6d9aa72c4ca1d1d2b824494ce415a086497821537eb0aed76824de5a68f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c0744f89a63b2e4a723402ec8f6e57fea7e71854ba3c2bc0049fee0eaddc97a5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c0744f89a63b2e4a723402ec8f6e57fea7e71854ba3c2bc0049fee0eaddc97a5"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c0744f89a63b2e4a723402ec8f6e57fea7e71854ba3c2bc0049fee0eaddc97a5"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c0744f89a63b2e4a723402ec8f6e57fea7e71854ba3c2bc0049fee0eaddc97a5"
+    sha256 cellar: :any_skip_relocation, ventura:        "c0744f89a63b2e4a723402ec8f6e57fea7e71854ba3c2bc0049fee0eaddc97a5"
+    sha256 cellar: :any_skip_relocation, monterey:       "c0744f89a63b2e4a723402ec8f6e57fea7e71854ba3c2bc0049fee0eaddc97a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "965733cf8e1e8e6bc22367d92eb1e7e93c1c0c373e0d9044d82249f62c28499b"
   end
 
   depends_on "node"
