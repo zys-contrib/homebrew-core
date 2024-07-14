11a427c0ebf88585b1d4c2bd1870b9d7b9cfbbe8
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-07-14 12:22:08 +0000

    node_exporter 1.8.2

--- Formula/n/node_exporter.rb
+++ Formula/n/node_exporter.rb
@@ -1,8 +1,8 @@
 class NodeExporter < Formula
   desc "Prometheus exporter for machine metrics"
   homepage "https://prometheus.io/"
-  url "https://github.com/prometheus/node_exporter/archive/refs/tags/v1.8.1.tar.gz"
-  sha256 "6a2dc6b0be27fa089574f2c32cee3673bbf4c6749c84f1d08f8c374e0908c0ad"
+  url "https://github.com/prometheus/node_exporter/archive/refs/tags/v1.8.2.tar.gz"
+  sha256 "f615c70be816550498dd6a505391dbed1a896705eff842628de13a1fa7654e8f"
   license "Apache-2.0"
   head "https://github.com/prometheus/node_exporter.git", branch: "master"
 
