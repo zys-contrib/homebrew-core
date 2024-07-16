b6bdc54f8e68577594111dc9743963503a17d6bf
Bevan Kay <email@bevankay.me>
2024-07-16 15:58:28 +1000

    gnupg: revision bump (for libassuan 3.0.1)

--- Formula/g/gnupg.rb
+++ Formula/g/gnupg.rb
@@ -4,6 +4,7 @@ class Gnupg < Formula
   url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.4.5.tar.bz2"
   sha256 "f68f7d75d06cb1635c336d34d844af97436c3f64ea14bcb7c869782f96f44277"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url "https://gnupg.org/ftp/gcrypt/gnupg/"
