4a5816da03e6b58fd66ae0c9a0843d138e50e9e3
Bevan Kay <email@bevankay.me>
2024-07-16 22:33:06 +1000

    libassuan@2 2.5.7 (new formula)

---
+++ Formula/lib/libassuan@2.rb
@@ -0,0 +1,32 @@
+class LibassuanAT2 < Formula
+  desc "Assuan IPC Library"
+  homepage "https://www.gnupg.org/related_software/libassuan/"
+  url "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.7.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-2.5.7.tar.bz2"
+  sha256 "0103081ffc27838a2e50479153ca105e873d3d65d8a9593282e9c94c7e6afb76"
+  license "GPL-3.0-only"
+
+  livecheck do
+    url "https://gnupg.org/ftp/gcrypt/libassuan/"
+    regex(/href=.*?libassuan[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "libgpg-error"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--enable-static"
+    system "make", "install"
+
+    # avoid triggering mandatory rebuilds of software that hard-codes this path
+    inreplace bin/"libassuan-config", prefix, opt_prefix
+  end
+
+  test do
+    system bin/"libassuan-config", "--version"
+  end
+end
