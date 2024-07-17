44bf1da68f9e592f94734f65f29c7c34c8c2864c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-17 18:26:34 +0000

    beakerlib 1.31.2

--- Formula/b/beakerlib.rb
+++ Formula/b/beakerlib.rb
@@ -1,8 +1,8 @@
 class Beakerlib < Formula
   desc "Shell-level integration testing library"
   homepage "https://github.com/beakerlib/beakerlib"
-  url "https://github.com/beakerlib/beakerlib/archive/refs/tags/1.31.1.tar.gz"
-  sha256 "5557f0226311d2cf234a4f5cd3edbf99f1e88c7f10210c5c0c17b43c220b6c81"
+  url "https://github.com/beakerlib/beakerlib/archive/refs/tags/1.31.2.tar.gz"
+  sha256 "2a171c5bf640758eb2c0f177f4f96bdd5badbb05e24b48ed8684dc88f80b6da5"
   license "GPL-2.0-only"
 
   bottle do
