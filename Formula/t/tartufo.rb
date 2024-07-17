9fffdd6c4dd2d6e6816b40561615457fe0e94e0f
Rui Chen <rui@chenrui.dev>
2024-07-17 12:38:32 -0400

    tartufo: depends_on pygit2
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    tartufo: add pygit 1.15.1 build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tartufo.rb
+++ Formula/t/tartufo.rb
@@ -6,7 +6,7 @@ class Tartufo < Formula
   url "https://files.pythonhosted.org/packages/d5/ea/6248387282150270f1886d75111f776f43e694f488a3a1ea3b5b0d1195f1/tartufo-5.0.0.tar.gz"
   sha256 "99ab6652cae6de295aeb31089e9ba27d66d0ad695af493d2d5cbc795397d1c84"
   license "GPL-2.0-only"
-  revision 2
+  revision 3
   head "https://github.com/godaddy/tartufo.git", branch: "main"
 
   bottle do
@@ -19,7 +19,7 @@ class Tartufo < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f44c5168bfefe9795d915ea7b527d1b67f39500e1186cbb6a4bcaa0b329ee7a"
   end
 
-  depends_on "libgit2@1.7"
+  depends_on "pygit2"
   depends_on "python@3.12"
 
   uses_from_macos "libffi"
@@ -27,11 +27,6 @@ class Tartufo < Formula
   resource "cached-property" do
     url "https://files.pythonhosted.org/packages/61/2c/d21c1c23c2895c091fa7a91a54b6872098fea913526932d21902088a7c41/cached-property-1.5.2.tar.gz"
     sha256 "9fa5755838eecbb2d234c3aa390bd80fbd3ac6b6869109bfc1b499f7bd89a130"
-  end
-
-  resource "cffi" do
-    url "https://files.pythonhosted.org/packages/68/ce/95b0bae7968c65473e1298efb042e10cafc7bafc14d9e4f154008241c91d/cffi-1.16.0.tar.gz"
-    sha256 "bcb3ef43e58665bbda2fb198698fcae6776483e0c4a631aa5647806c25e02cc0"
   end
 
   resource "click" do
@@ -49,21 +44,6 @@ class Tartufo < Formula
     sha256 "35f314a9f878467f5453cc1fee295c3e18e52f1b99f10f6cf5b1682e968a9e7c"
   end
 
-  resource "pycparser" do
-    url "https://files.pythonhosted.org/packages/1d/b2/31537cf4b1ca988837256c910a668b553fceb8f069bedc4b1c826024b52c/pycparser-2.22.tar.gz"
-    sha256 "491c8be9c040f5390f5bf44a5b07752bd07f56edf992381b05c701439eec10f6"
-  end
-
-  resource "pygit2" do
-    url "https://files.pythonhosted.org/packages/f0/5e/6e05213a9163bad15489beda5f958500881d45889b0df01d7b8964f031bf/pygit2-1.14.1.tar.gz"
-    sha256 "ec5958571b82a6351785ca645e5394c31ae45eec5384b2fa9c4e05dde3597ad6"
-  end
-
-  resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/65/d8/10a70e86f6c28ae59f101a9de6d77bf70f147180fbf40c3af0f64080adc3/setuptools-70.3.0.tar.gz"
-    sha256 "f171bab1dfbc86b132997f26a119f6056a57950d058587841a0082e8830f9dc5"
-  end
-
   resource "smmap" do
     url "https://files.pythonhosted.org/packages/88/04/b5bf6d21dc4041000ccba7eb17dd3055feb237e7ffc2c20d3fae3af62baa/smmap-5.0.1.tar.gz"
     sha256 "dceeb6c0028fdb6734471eb07c0cd2aae706ccaecab45965ee83f11c8d3b1f62"
@@ -72,6 +52,12 @@ class Tartufo < Formula
   resource "tomlkit" do
     url "https://files.pythonhosted.org/packages/10/37/dd53019ccb72ef7d73fff0bee9e20b16faff9658b47913a35d79e89978af/tomlkit-0.11.8.tar.gz"
     sha256 "9330fc7faa1db67b541b28e62018c17d20be733177d290a13b24c62d1614e0c3"
+  end
+
+  # pygit 1.15.1 build patch, upstream pr ref, https://github.com/godaddy/tartufo/pull/532
+  patch do
+    url "https://github.com/godaddy/tartufo/commit/c4fe2cb4011c3f830945f20593df81adfd4bcf17.patch?full_index=1"
+    sha256 "45d63c2c3bef7c11ffd37020f1b4e0276a930a212ca596de44b86807ff8eb062"
   end
 
   def install
