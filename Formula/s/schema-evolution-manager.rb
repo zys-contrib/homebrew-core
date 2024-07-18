4f1f2ea894f430d069c9cf612b13618b6f72068f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-18 13:57:49 +0000

    schema-evolution-manager: update 0.9.54 bottle.

--- Formula/s/schema-evolution-manager.rb
+++ Formula/s/schema-evolution-manager.rb
@@ -6,13 +6,13 @@ class SchemaEvolutionManager < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "68e92c7419b1938535058bdacb7317801df1869745f72f055bb419d166414ff1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "68e92c7419b1938535058bdacb7317801df1869745f72f055bb419d166414ff1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "68e92c7419b1938535058bdacb7317801df1869745f72f055bb419d166414ff1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "68e92c7419b1938535058bdacb7317801df1869745f72f055bb419d166414ff1"
-    sha256 cellar: :any_skip_relocation, ventura:        "68e92c7419b1938535058bdacb7317801df1869745f72f055bb419d166414ff1"
-    sha256 cellar: :any_skip_relocation, monterey:       "68e92c7419b1938535058bdacb7317801df1869745f72f055bb419d166414ff1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "568707ac58d57d539ef383eae2fba642adb3edd7f5f9b9317ebb245baaed674b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "da0a8735eecd1efe0602386929a515ae4bf9df6e49014963f2a74e5693ac2505"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "da0a8735eecd1efe0602386929a515ae4bf9df6e49014963f2a74e5693ac2505"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "da0a8735eecd1efe0602386929a515ae4bf9df6e49014963f2a74e5693ac2505"
+    sha256 cellar: :any_skip_relocation, sonoma:         "da0a8735eecd1efe0602386929a515ae4bf9df6e49014963f2a74e5693ac2505"
+    sha256 cellar: :any_skip_relocation, ventura:        "da0a8735eecd1efe0602386929a515ae4bf9df6e49014963f2a74e5693ac2505"
+    sha256 cellar: :any_skip_relocation, monterey:       "da0a8735eecd1efe0602386929a515ae4bf9df6e49014963f2a74e5693ac2505"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20071cf2c6a18e15cee9ca71adcd1bc77b5d73e899263bb9a8bd918cf3cbaefd"
   end
 
   uses_from_macos "ruby"
