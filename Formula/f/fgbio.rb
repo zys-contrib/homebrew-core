bf158f011f1a97bfe4a88bae9c36095c0558eb37
Rui Chen <rui@chenrui.dev>
2024-07-18 09:31:13 -0400

    fgbio 2.3.0
    autobump: add fgbio
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fgbio.rb
+++ Formula/f/fgbio.rb
@@ -1,8 +1,8 @@
 class Fgbio < Formula
   desc "Tools for working with genomic and high throughput sequencing data"
   homepage "https://fulcrumgenomics.github.io/fgbio/"
-  url "https://github.com/fulcrumgenomics/fgbio/releases/download/2.2.1/fgbio-2.2.1.jar"
-  sha256 "bb875e9a9218b841f39bdd007492b56be25b4d112ef7c22311c554b7b60fcd2c"
+  url "https://github.com/fulcrumgenomics/fgbio/releases/download/2.3.0/fgbio-2.3.0.jar"
+  sha256 "a0748b52a92403d88e7cf799368c313a05f89c5e3da04f7f8829593a603b7c69"
   license "MIT"
 
   bottle do
