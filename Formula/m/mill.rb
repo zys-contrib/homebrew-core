9b11b4f899e45e81f437cd1e683f2e543de24cd0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-19 12:55:15 +0000

    mill: update 0.11.9 bottle.

--- Formula/m/mill.rb
+++ Formula/m/mill.rb
@@ -11,13 +11,13 @@ class Mill < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0074a7a8c087e399795bc061b75e8658f1bf5f0174b857766e8cf8cac51df2e6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0074a7a8c087e399795bc061b75e8658f1bf5f0174b857766e8cf8cac51df2e6"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0074a7a8c087e399795bc061b75e8658f1bf5f0174b857766e8cf8cac51df2e6"
-    sha256 cellar: :any_skip_relocation, sonoma:         "0074a7a8c087e399795bc061b75e8658f1bf5f0174b857766e8cf8cac51df2e6"
-    sha256 cellar: :any_skip_relocation, ventura:        "0074a7a8c087e399795bc061b75e8658f1bf5f0174b857766e8cf8cac51df2e6"
-    sha256 cellar: :any_skip_relocation, monterey:       "0074a7a8c087e399795bc061b75e8658f1bf5f0174b857766e8cf8cac51df2e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76b9fa828dbc215635a723db587f676c6af2f626227dc5608d0f4f62af6ed62e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
+    sha256 cellar: :any_skip_relocation, ventura:        "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
+    sha256 cellar: :any_skip_relocation, monterey:       "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e2648488674a64bd96771e25d9dcb8130052716522ee46511b73fc2c59854971"
   end
 
   depends_on "openjdk"
