71c8229a565c65398ae388d916e2b322916930fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-19 21:31:37 +0000

    httm 0.39.2

--- Formula/h/httm.rb
+++ Formula/h/httm.rb
@@ -1,8 +1,8 @@
 class Httm < Formula
   desc "Interactive, file-level Time Machine-like tool for ZFS/btrfs"
   homepage "https://github.com/kimono-koans/httm"
-  url "https://github.com/kimono-koans/httm/archive/refs/tags/0.39.1.tar.gz"
-  sha256 "fa8151768ebb7e2667ce454fc74cce80cbc85d168cd0c05ffe6349c791e75fb2"
+  url "https://github.com/kimono-koans/httm/archive/refs/tags/0.39.2.tar.gz"
+  sha256 "1db23cf839df0a69d9801679b43eafd98481bcd28fe5f367731e6da966f7141f"
   license "MPL-2.0"
   head "https://github.com/kimono-koans/httm.git", branch: "master"
 
