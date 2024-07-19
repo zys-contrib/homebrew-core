fd3b0d569975bbb54b838eaf9e6905059b295456
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-19 21:21:12 +0000

    apache-opennlp 2.4.0
    apache-opennlp: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/apache-opennlp.rb
+++ Formula/a/apache-opennlp.rb
@@ -1,9 +1,9 @@
 class ApacheOpennlp < Formula
   desc "Machine learning toolkit for processing natural language text"
   homepage "https://opennlp.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=opennlp/opennlp-2.3.3/apache-opennlp-2.3.3-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/opennlp/opennlp-2.3.3/apache-opennlp-2.3.3-bin.tar.gz"
-  sha256 "63dacde27ee052aa6866d01fbe88adce8c13c0c207975d98569bdbc80b33697d"
+  url "https://www.apache.org/dyn/closer.lua?path=opennlp/opennlp-2.4.0/apache-opennlp-2.4.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/opennlp/opennlp-2.4.0/apache-opennlp-2.4.0-bin.tar.gz"
+  sha256 "dd0cee5542e128130333e99bbea649f5fd5eb6f264fd4e1c6403f0427077c2ef"
   license "Apache-2.0"
 
   bottle do
@@ -16,8 +16,6 @@ class ApacheOpennlp < Formula
     libexec.install Dir["*"]
     (bin/"opennlp").write_env_script libexec/"bin/opennlp", JAVA_HOME:    Formula["openjdk"].opt_prefix,
                                                             OPENNLP_HOME: libexec
-    # script uses a relative path to the conf folder
-    inreplace libexec/"bin/opennlp", "../conf", "$OPENNLP_HOME/conf"
   end
 
   test do
