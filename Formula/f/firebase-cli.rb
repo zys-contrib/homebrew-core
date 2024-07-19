a7d012f40889bd1634fce7eee9e8ed6ad37b62eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-19 00:37:18 +0000

    firebase-cli 13.13.3

--- Formula/f/firebase-cli.rb
+++ Formula/f/firebase-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-13.13.0.tgz"
-  sha256 "7501b91bf04fdd7b5e942121ac90c183bd1a3c5f2ae614be862e6b73da54f2bb"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-13.13.3.tgz"
+  sha256 "bd3c75699f4897da4b7337fbe45eef1d0aed7253314603f9d709bedea4cee93c"
   license "MIT"
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
