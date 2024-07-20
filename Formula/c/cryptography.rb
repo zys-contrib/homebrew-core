df80a362e2aa3a286c15a0831372e3b12b0c1646
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-20 18:31:24 +0000

    cryptography 43.0.0
    cryptography: update deps
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>

--- Formula/c/cryptography.rb
+++ Formula/c/cryptography.rb
@@ -1,8 +1,8 @@
 class Cryptography < Formula
   desc "Cryptographic recipes and primitives for Python"
   homepage "https://cryptography.io/en/latest/"
-  url "https://files.pythonhosted.org/packages/93/a7/1498799a2ea06148463a9a2c10ab2f6a921a74fb19e231b27dc412a748e2/cryptography-42.0.8.tar.gz"
-  sha256 "8d09d05439ce7baa8e9e95b07ec5b6c886f548deb7e0f69ef25f64b3bce842f2"
+  url "https://files.pythonhosted.org/packages/69/ec/9fb9dcf4f91f0e5e76de597256c43eedefd8423aa59be95c70c4c3db426a/cryptography-43.0.0.tar.gz"
+  sha256 "b88075ada2d51aa9f18283532c9f60e72170041bba88d7f37e49cbb10275299e"
   license any_of: ["Apache-2.0", "BSD-3-Clause"]
   head "https://github.com/pyca/cryptography.git", branch: "main"
 
@@ -23,14 +23,19 @@ class Cryptography < Formula
   depends_on "cffi"
   depends_on "openssl@3"
 
+  resource "maturin" do
+    url "https://files.pythonhosted.org/packages/80/da/a4bbd6e97f3645f4ebd725321aa235e22e31037dfd92caf4539f721c0a5a/maturin-1.7.0.tar.gz"
+    sha256 "1ba5277dd7832dc6181d69a005182b97b3520945825058484ffd9296f2efb59c"
+  end
+
   resource "semantic-version" do
     url "https://files.pythonhosted.org/packages/7d/31/f2289ce78b9b473d582568c234e104d2a342fd658cc288a7553d83bb8595/semantic_version-2.10.0.tar.gz"
     sha256 "bdabb6d336998cbb378d4b9db3a4b56a1e3235701dc05ea2690d9a997ed5041c"
   end
 
   resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/aa/60/5db2249526c9b453c5bb8b9f6965fcab0ddb7f40ad734420b3b421f7da44/setuptools-70.0.0.tar.gz"
-    sha256 "f211a66637b8fa059bb28183da127d4e86396c991a942b028c6650d4319c3fd0"
+    url "https://files.pythonhosted.org/packages/32/c0/5b8013b5a812701c72e3b1e2b378edaa6514d06bee6704a5ab0d7fa52931/setuptools-71.1.0.tar.gz"
+    sha256 "032d42ee9fb536e33087fb66cac5f840eb9391ed05637b3f2a76a7c8fb477936"
   end
 
   resource "setuptools-rust" do
@@ -45,11 +50,13 @@ class Cryptography < Formula
   end
 
   def install
+    ENV.append_path "PATH", buildpath/"bin"
     pythons.each do |python3|
       ENV.append_path "PYTHONPATH", buildpath/Language::Python.site_packages(python3)
 
-      resources.each do |r|
-        r.stage do
+      deps = %w[setuptools setuptools-rust semantic-version maturin]
+      deps.each do |r|
+        resource(r).stage do
           system python3, "-m", "pip", "install", *std_pip_args(prefix: buildpath), "."
         end
       end
