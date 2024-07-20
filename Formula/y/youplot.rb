d800a0914b008bc351bec7d2124de5c91482b516
Rui Chen <rui@chenrui.dev>
2024-07-20 13:40:19 -0400

    youplot 0.4.6
    youplot: update deps
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    autobump: add youplot
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/y/youplot.rb
+++ Formula/y/youplot.rb
@@ -1,10 +1,9 @@
 class Youplot < Formula
   desc "Command-line tool that draw plots on the terminal"
   homepage "https://github.com/red-data-tools/YouPlot/"
-  url "https://github.com/red-data-tools/YouPlot/archive/refs/tags/v0.4.5.tar.gz"
-  sha256 "aa7339139bc4ea9aa0b2279e4e8052fde673a60ad47e87d50fde06626dc2b3c3"
+  url "https://github.com/red-data-tools/YouPlot/archive/refs/tags/v0.4.6.tar.gz"
+  sha256 "126278103f6dbc4e28983b9e90a4e593f17e78b38d925a7df16965b5d3c145a4"
   license "MIT"
-  revision 1
 
   bottle do
     sha256                               arm64_sonoma:   "5790be8d29022d7574e830c5d2e1e994dc99d7c5185ac8cd0d60a49ee740e05e"
@@ -18,14 +17,19 @@ class Youplot < Formula
 
   uses_from_macos "ruby"
 
+  resource "enumerable-statistics" do
+    url "https://rubygems.org/downloads/enumerable-statistics-2.0.8.gem"
+    sha256 "1e0d69fcdec1d188dd529e6e5b2c27e8f88029c862f6094663c93806f6d313b3"
+  end
+
   resource "unicode_plot" do
     url "https://rubygems.org/downloads/unicode_plot-0.0.5.gem"
     sha256 "91ce6237bca67a3b969655accef91024c78ec6aad470fcddeb29b81f7f78f73b"
   end
 
-  resource "enumerable-statistics" do
-    url "https://rubygems.org/downloads/enumerable-statistics-2.0.7.gem"
-    sha256 "eeb84581376305327b31465e7b088146ea7909d19eb637d5677e51f099759636"
+  resource "csv" do
+    url "https://rubygems.org/downloads/csv-3.3.0.gem"
+    sha256 "0bbd1defdc31134abefed027a639b3723c2753862150f4c3ee61cab71b20d67d"
   end
 
   def install
