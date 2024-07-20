61aaff460da2118845ac0c0a5af08ca380438d6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-20 04:06:08 +0000

    cfn-lint: update 1.8.2 bottle.

--- Formula/c/cfn-lint.rb
+++ Formula/c/cfn-lint.rb
@@ -13,13 +13,13 @@ class CfnLint < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "d2d87edd99a6554af097b080797e9ed3541732f0ed4cb5f4b3165e5995062912"
-    sha256 cellar: :any,                 arm64_ventura:  "68c8f8ea14169eb5c51ed8379fd30319ef5745a6f25972f92e55a7ddb5dd25aa"
-    sha256 cellar: :any,                 arm64_monterey: "038d957349a81dedec78e7a030474d5bfc7ea1cdc28b13fb484f82a234e8b37a"
-    sha256 cellar: :any,                 sonoma:         "5be6fa015acc876d7a509ec71c115b9dd63202f0fc3d34b468559f1ed0cb0376"
-    sha256 cellar: :any,                 ventura:        "9e2609f61a4cab02ace472262f2c42bb484435b1315b8079fade3a56eac1cf4e"
-    sha256 cellar: :any,                 monterey:       "b89af94f8b18d8a93247200d68240aa788ec570a1959b49827340ad6c2771fb9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c3a16ff9bf3fed67ca8c92bd7db472a274fcee5b13abbaa38597984512cfb15e"
+    sha256 cellar: :any,                 arm64_sonoma:   "bc6b0124e4f4cce9071b32a70e7c21184c4ba0f8c5a35086c43388a0e24703a4"
+    sha256 cellar: :any,                 arm64_ventura:  "d3dceb97d2243a7fc042a54c0abd48a374ed914a22987aef6e34e1756ec05dee"
+    sha256 cellar: :any,                 arm64_monterey: "9442abd284a097d7e4493b33892f9bad4d87b0f87f4972f2fced1636e0f204c1"
+    sha256 cellar: :any,                 sonoma:         "f3c9abbc31dd08eb9a140e9ef00dddd064fc31abd928e75952c9ba636ce8280a"
+    sha256 cellar: :any,                 ventura:        "66bf5ee280f7fd7f756f2e6a32001371a2b51293ad1e29ee3546edcd63c697cf"
+    sha256 cellar: :any,                 monterey:       "819e58168b92d9819ab83b4709a8d990d11fddbc022a5132480f98bb566fa596"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "62f1e8835c2278accf955357fbe03043d05bc86bf0cbd561f86367a62e05060b"
   end
 
   depends_on "rust" => :build
