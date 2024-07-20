9c1423b5c9f82cf6d0304f1f84836ea1c3147c9b
Patrick Linnane <patrick@linnane.io>
2024-07-20 02:28:16 +0000

    terramaid: add 1.6.3 bottle.

--- Formula/t/terramaid.rb
+++ Formula/t/terramaid.rb
@@ -6,6 +6,16 @@ class Terramaid < Formula
   license "Apache-2.0"
   head "https://github.com/RoseSecurity/Terramaid.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6f1354304e8080600354dcda1c9242e7485e8e3bdcb6ee31e209abee357a292b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "21fc40af7dc399c781ee41d20d6e1f159eadb868df4701428fca4df3cbd19674"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "86f9b37ba899db6518519b4c4bed800823b8974b6ef854a9f444120ca07a32d2"
+    sha256 cellar: :any_skip_relocation, sonoma:         "47f9869b712707749fd97411f6f64ef6afb09409c38590912717746befb32489"
+    sha256 cellar: :any_skip_relocation, ventura:        "3ddd767871c0fac40b4e2e8302766e421a12a072b6bf9b883b3118603ced8be9"
+    sha256 cellar: :any_skip_relocation, monterey:       "12eba948707333fa7cf3b8e60cebd79fabc0ffcc079e5ea3b3a0d77e56f3590d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7bf0037ec68955aa258eddf78b5c697f257a088362c5806a3f7b33229196a990"
+  end
+
   depends_on "go" => [:build, :test]
 
   def install
