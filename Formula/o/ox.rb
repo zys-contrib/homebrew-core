81c14b4fb06cc4f7be48bf47cd47497cb6b06c72
Rui Chen <rui@chenrui.dev>
2024-07-21 13:48:29 -0400

    ox 0.4.0
    autobump: add ox
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/ox.rb
+++ Formula/o/ox.rb
@@ -1,8 +1,8 @@
 class Ox < Formula
   desc "Independent Rust text editor that runs in your terminal"
   homepage "https://github.com/curlpipe/ox"
-  url "https://github.com/curlpipe/ox/archive/refs/tags/0.2.7.tar.gz"
-  sha256 "ae08c18243b66d8c24f9e0844e499890be2864584a8d3e38f186fdf3aa5c09fc"
+  url "https://github.com/curlpipe/ox/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "504089d50ad549b013ca9108aaeb800928b35e4f36766ded4245c4026504073d"
   license "GPL-2.0-only"
   head "https://github.com/curlpipe/ox.git", branch: "master"
 
