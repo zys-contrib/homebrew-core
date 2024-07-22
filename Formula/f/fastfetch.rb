52e73383557f7ab2da57fd6133faaf7184ed0368
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-22 06:34:36 +0000

    fastfetch 2.20.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fastfetch.rb
+++ Formula/f/fastfetch.rb
@@ -1,8 +1,8 @@
 class Fastfetch < Formula
   desc "Like neofetch, but much faster because written mostly in C"
   homepage "https://github.com/fastfetch-cli/fastfetch"
-  url "https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/2.18.1.tar.gz"
-  sha256 "53421de317bfc918e168751cf910e7faa51ebbd71f6f4de3b56d2cc74794365f"
+  url "https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/2.20.0.tar.gz"
+  sha256 "461f64bda6ab4a33085b1b75afd01e72a7d0556b678852b95fbea4916ceec2a8"
   license "MIT"
   head "https://github.com/fastfetch-cli/fastfetch.git", branch: "dev"
 
