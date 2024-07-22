c3009c1507f38fae277faa04500e64b357c72c84
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-22 06:30:26 +0000

    pnpm: update 9.6.0 bottle.

--- Formula/p/pnpm.rb
+++ Formula/p/pnpm.rb
@@ -13,13 +13,13 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "dcc2c6845cdb6109aaa6a28e81c1f4915b419ad4eb04971bd472aeefe64496c1"
-    sha256 cellar: :any,                 arm64_ventura:  "dcc2c6845cdb6109aaa6a28e81c1f4915b419ad4eb04971bd472aeefe64496c1"
-    sha256 cellar: :any,                 arm64_monterey: "dcc2c6845cdb6109aaa6a28e81c1f4915b419ad4eb04971bd472aeefe64496c1"
-    sha256 cellar: :any,                 sonoma:         "d92232e35e03b1605789413f7ded81276a8934be14c9ac3219a6ba9a40781024"
-    sha256 cellar: :any,                 ventura:        "d92232e35e03b1605789413f7ded81276a8934be14c9ac3219a6ba9a40781024"
-    sha256 cellar: :any,                 monterey:       "d92232e35e03b1605789413f7ded81276a8934be14c9ac3219a6ba9a40781024"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c8606afd2cbcb42e7313a3711bf367396e347a8e7d49798ee0380f3cafb24c61"
+    sha256 cellar: :any,                 arm64_sonoma:   "8630463e5b5a84aeec092cee88c594aab35a599a388626c1fb1a5e6391545fa3"
+    sha256 cellar: :any,                 arm64_ventura:  "8630463e5b5a84aeec092cee88c594aab35a599a388626c1fb1a5e6391545fa3"
+    sha256 cellar: :any,                 arm64_monterey: "8630463e5b5a84aeec092cee88c594aab35a599a388626c1fb1a5e6391545fa3"
+    sha256 cellar: :any,                 sonoma:         "c4894a1b8fe3e1738d8b6622ba9ccc237b4db0f25a1fb0696454d12845d93186"
+    sha256 cellar: :any,                 ventura:        "c4894a1b8fe3e1738d8b6622ba9ccc237b4db0f25a1fb0696454d12845d93186"
+    sha256 cellar: :any,                 monterey:       "c4894a1b8fe3e1738d8b6622ba9ccc237b4db0f25a1fb0696454d12845d93186"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "27dcfff31395db4ea188f31ea3d16a379b4a86a5b30b3b7d476886d1c01de8c7"
   end
 
   depends_on "node" => [:build, :test]
