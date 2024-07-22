a6f51849c7bccbf835b968e2aeb351060d2d1976
Rui Chen <rui@chenrui.dev>
2024-07-22 15:13:58 -0400

    docker-completion 27.1.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/docker-completion.rb
+++ Formula/d/docker-completion.rb
@@ -1,8 +1,8 @@
 class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
-  url "https://github.com/docker/cli/archive/refs/tags/v27.0.3.tar.gz"
-  sha256 "f992e895c949852686abef9a6fa9efd622826c4f4d70b83876569a4641c4c8fc"
+  url "https://github.com/docker/cli/archive/refs/tags/v27.1.0.tar.gz"
+  sha256 "8e58a2e419ba91ce1a27831c394bf214f7076758f55eccc6762b5265bec58b9f"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
