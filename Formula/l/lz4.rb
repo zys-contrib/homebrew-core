cff3d2d19ced5db96720a1314f54ca02ed45a59e
MoonFruit <dkmoonfruit@gmail.com>
2024-07-22 16:57:26 +0800

    lz4 1.10.0
    autobump: add lz4
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/l/lz4.rb
+++ Formula/l/lz4.rb
@@ -1,10 +1,10 @@
 class Lz4 < Formula
   desc "Extremely Fast Compression algorithm"
   homepage "https://lz4.github.io/lz4/"
-  url "https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/lz4-1.9.4.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/lz4-1.9.4.tar.gz"
-  sha256 "0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b"
+  url "https://github.com/lz4/lz4/archive/refs/tags/v1.10.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/lz4-1.10.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/lz4-1.10.0.tar.gz"
+  sha256 "537512904744b35e232912055ccf8ec66d768639ff3abe5788d90d792ec5f48b"
   license "BSD-2-Clause"
   head "https://github.com/lz4/lz4.git", branch: "dev"
 
