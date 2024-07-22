7238680719980ea036a6010895f4a9c61f308d3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-22 00:52:32 +0000

    php-code-sniffer 3.10.2
    php-code-sniffer: bump resource

--- Formula/p/php-code-sniffer.rb
+++ Formula/p/php-code-sniffer.rb
@@ -1,8 +1,8 @@
 class PhpCodeSniffer < Formula
   desc "Check coding standards in PHP, JavaScript and CSS"
   homepage "https://github.com/PHPCSStandards/PHP_CodeSniffer"
-  url "https://github.com/PHPCSStandards/PHP_CodeSniffer/releases/download/3.10.1/phpcs.phar"
-  sha256 "0b9fe8296ba5ab5bd63a548caaed8c917d10dc48d87495fb340385c5c9a0a4df"
+  url "https://github.com/PHPCSStandards/PHP_CodeSniffer/releases/download/3.10.2/phpcs.phar"
+  sha256 "5f580b08328af20a4138a6dcefdbb4c3307e133d9dfbabdf925c08c7d87f18de"
   license "BSD-3-Clause"
 
   bottle do
@@ -18,8 +18,8 @@ class PhpCodeSniffer < Formula
   depends_on "php"
 
   resource "phpcbf.phar" do
-    url "https://github.com/PHPCSStandards/PHP_CodeSniffer/releases/download/3.10.1/phpcbf.phar"
-    sha256 "d0634acaafa1da30f6f9949b6330eeaf6eacee8be527c634d775c4da46ddc09c"
+    url "https://github.com/PHPCSStandards/PHP_CodeSniffer/releases/download/3.10.2/phpcbf.phar"
+    sha256 "7b4b1316ce388600c2a052b41b9badd2d906827f0a08a08873ca5d1063dd1038"
   end
 
   def install
