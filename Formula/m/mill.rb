94b0b2369c349ccb71157ddcec3ca913e0686161
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-23 15:28:26 +0000

    mill: update 0.11.10 bottle.

--- Formula/m/mill.rb
+++ Formula/m/mill.rb
@@ -11,13 +11,13 @@ class Mill < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
-    sha256 cellar: :any_skip_relocation, ventura:        "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
-    sha256 cellar: :any_skip_relocation, monterey:       "0b49362932711a6d1fd33734e1a453730d7d62bc66cd9905e0d29d2005d9bbc1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e2648488674a64bd96771e25d9dcb8130052716522ee46511b73fc2c59854971"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9955755ece4918b589a3dc034e5a907d94b58c03517d7583358bc6324e2a0128"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9955755ece4918b589a3dc034e5a907d94b58c03517d7583358bc6324e2a0128"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9955755ece4918b589a3dc034e5a907d94b58c03517d7583358bc6324e2a0128"
+    sha256 cellar: :any_skip_relocation, sonoma:         "9955755ece4918b589a3dc034e5a907d94b58c03517d7583358bc6324e2a0128"
+    sha256 cellar: :any_skip_relocation, ventura:        "d921df589be6909fe8d14110c2c3ba495fb54d03d4dbeb6002f5d67a7853cccd"
+    sha256 cellar: :any_skip_relocation, monterey:       "9955755ece4918b589a3dc034e5a907d94b58c03517d7583358bc6324e2a0128"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb19f51e773ee887ce884d9281902c2d32506198d7eba10ea33584c78ce16b5e"
   end
 
   depends_on "openjdk"
