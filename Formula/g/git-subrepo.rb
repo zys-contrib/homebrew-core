6d61641cb4c428024e4554497227874f2791c467
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-25 20:51:46 +0000

    git-subrepo: update 0.4.9 bottle.

--- Formula/g/git-subrepo.rb
+++ Formula/g/git-subrepo.rb
@@ -12,13 +12,13 @@ class GitSubrepo < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "55b279ba8e49f82b9f602f48972654977b5cd67f0fbceba443af3e730294d605"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "55b279ba8e49f82b9f602f48972654977b5cd67f0fbceba443af3e730294d605"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "55b279ba8e49f82b9f602f48972654977b5cd67f0fbceba443af3e730294d605"
-    sha256 cellar: :any_skip_relocation, sonoma:         "1f67c7fdad3d4d8abcd9ecca6500c4bcb730e686813f00df0bb736a5a535bbd4"
-    sha256 cellar: :any_skip_relocation, ventura:        "1f67c7fdad3d4d8abcd9ecca6500c4bcb730e686813f00df0bb736a5a535bbd4"
-    sha256 cellar: :any_skip_relocation, monterey:       "1f67c7fdad3d4d8abcd9ecca6500c4bcb730e686813f00df0bb736a5a535bbd4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55b279ba8e49f82b9f602f48972654977b5cd67f0fbceba443af3e730294d605"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8122f64f75d8a8560dcee7f34d83d9c994c10a51fcd93a4f543d52d002a9933c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8122f64f75d8a8560dcee7f34d83d9c994c10a51fcd93a4f543d52d002a9933c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8122f64f75d8a8560dcee7f34d83d9c994c10a51fcd93a4f543d52d002a9933c"
+    sha256 cellar: :any_skip_relocation, sonoma:         "d112ed09a9a2de61146ec0e937f95024948b3059bb9abe974da0d7596ddb86fd"
+    sha256 cellar: :any_skip_relocation, ventura:        "d112ed09a9a2de61146ec0e937f95024948b3059bb9abe974da0d7596ddb86fd"
+    sha256 cellar: :any_skip_relocation, monterey:       "1fdf81e178b50812b6aa615a8621e307a3df93f79cbbfbcb3cd2584a51a32084"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8122f64f75d8a8560dcee7f34d83d9c994c10a51fcd93a4f543d52d002a9933c"
   end
 
   depends_on "bash"
