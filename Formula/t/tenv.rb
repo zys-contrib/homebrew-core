194b6a90d19ef877b5233c07a6e05bfc2baf7745
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-25 18:49:53 +0000

    tenv 2.7.8

--- Formula/t/tenv.rb
+++ Formula/t/tenv.rb
@@ -1,8 +1,8 @@
 class Tenv < Formula
   desc "OpenTofu / Terraform / Terragrunt / Atmos version manager"
   homepage "https://tofuutils.github.io/tenv/"
-  url "https://github.com/tofuutils/tenv/archive/refs/tags/v2.7.2.tar.gz"
-  sha256 "36f8644c2cf0ac7cd35299ed67026fae0f83ba43dff940aa942ac20548f047f1"
+  url "https://github.com/tofuutils/tenv/archive/refs/tags/v2.7.8.tar.gz"
+  sha256 "0e8b8b165a7a3679010b7676a26fa6c902016acb3105ca13945c9c7aec4faef6"
   license "Apache-2.0"
   head "https://github.com/tofuutils/tenv.git", branch: "main"
 
