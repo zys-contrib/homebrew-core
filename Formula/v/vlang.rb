fb87c40348d42346624a20df9e9556681d69b596
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-26 17:31:31 +0000

    vlang: update 0.4.7 bottle.

--- Formula/v/vlang.rb
+++ Formula/v/vlang.rb
@@ -12,13 +12,13 @@ class Vlang < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "ca7a4d83c4335b14e3bd1195b783c03309fc02afa3822a69ea84e56479a5136f"
-    sha256 cellar: :any,                 arm64_ventura:  "52571db88727a04abfcb412833dc82d4c65edb07af24c80fc74a5fcb6fcf477a"
-    sha256 cellar: :any,                 arm64_monterey: "0506f70a1f64c3d7b7b48bb7ef197ca126fbddef3fb34de5e877a07303749db9"
-    sha256 cellar: :any,                 sonoma:         "686eb6f4b3abb7531f21bc3e302c759e0eeee79145e53127ba297daa233bd1ec"
-    sha256 cellar: :any,                 ventura:        "e8a6dab15b079c64c369f008aee9583f643685a5aa0c83f773cf4261115b24f0"
-    sha256 cellar: :any,                 monterey:       "32b5d206cad0c4b1995e595b57fbbeb55e404b2a37ef33c2a163179d6050b7be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "246e800f156122823dbd8e8756a46ecf2afd9ef46b24b9969ae7abe89319c4a4"
+    sha256 cellar: :any,                 arm64_sonoma:   "934a8183a614873e32f9b63ef08cd3e3b2cf2faf93b90c63d5f68567ea8ca2ae"
+    sha256 cellar: :any,                 arm64_ventura:  "3683411188d32596277900af6e1c7703176e1c2d39a0c87e631615cc86458b18"
+    sha256 cellar: :any,                 arm64_monterey: "7699eb19c77a46d2ae5ce338af93df4f2d106ca90774d1d7f024dc7c0e7a708b"
+    sha256 cellar: :any,                 sonoma:         "23e9d0f26124a5fe595acb13d4a3ff9312b65c13995f0e8bac0d234d7a4aeb7f"
+    sha256 cellar: :any,                 ventura:        "f000363ed502ea144cdfef7efee1fed5bf42457df047fb70572cadd9e13e6d66"
+    sha256 cellar: :any,                 monterey:       "19595ad544f767743dd3ac519a69c0578b72da179d5c0791eb2b844e49a56673"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "76612a548f534cff38b55517376fc9674caa7277243df4f671f89bccea0af87b"
   end
 
   depends_on "bdw-gc"
