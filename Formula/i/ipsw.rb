7dfd77d10996b1c6c897a810cd867e4a57ac2bee
Patrick Linnane <patrick@linnane.io>
2024-07-26 09:30:00 -0700

    ipsw 3.1.527 (new formula)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/i/ipsw.rb
@@ -0,0 +1,25 @@
+class Ipsw < Formula
+  desc "Research tool for iOS & macOS devices"
+  homepage "https://blacktop.github.io/ipsw"
+  url "https://github.com/blacktop/ipsw/archive/refs/tags/v3.1.527.tar.gz"
+  sha256 "de451c36286d0fdeb21b0dd048f0bb6d362667f4ad7f8bc05ce25e34ee49d4dc"
+  license "MIT"
+  head "https://github.com/blacktop/ipsw.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/blacktop/ipsw/cmd/ipsw/cmd.AppVersion=#{version}
+      -X github.com/blacktop/ipsw/cmd/ipsw/cmd.AppBuildCommit=Homebrew
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/ipsw"
+  end
+
+  test do
+    assert_match version.to_s, shell_output(bin/"ipsw version")
+
+    assert_match "MacFamily20,1", shell_output(bin/"ipsw device-list")
+  end
+end
