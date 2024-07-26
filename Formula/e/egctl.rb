318affb026376fc04d403807625fcf0a386a939c
Branch Vincent <branchevincent@gmail.com>
2024-07-25 21:36:14 -0700

    egctl 1.1.0

--- Formula/e/egctl.rb
+++ Formula/e/egctl.rb
@@ -1,8 +1,8 @@
 class Egctl < Formula
   desc "Command-line utility for operating Envoy Gateway"
   homepage "https://gateway.envoyproxy.io/"
-  url "https://github.com/envoyproxy/gateway/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "05406182dc165513925cf60722582613d4de9ea789d60e014e6da456bb229f65"
+  url "https://github.com/envoyproxy/gateway/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "f841cfa30416063b40062976d92afcd573a2c324bb30e4311e362ed9d5b7a2f5"
   license "Apache-2.0"
   head "https://github.com/envoyproxy/gateway.git", branch: "main"
 
@@ -17,6 +17,10 @@ class Egctl < Formula
   end
 
   depends_on "go" => :build
+
+  on_linux do
+    depends_on "btrfs-progs"
+  end
 
   def install
     ldflags = %W[
@@ -111,10 +115,25 @@ class Egctl < Formula
               virtualHosts:
               - domains:
                 - www.example.com
+                metadata:
+                  filterMetadata:
+                    envoy-gateway:
+                      resources:
+                      - kind: ""
+                        name: eg
+                        namespace: default
+                        sectionName: http
                 name: default/eg/http/www_example_com
                 routes:
                 - match:
                     prefix: /
+                  metadata:
+                    filterMetadata:
+                      envoy-gateway:
+                        resources:
+                        - kind: HTTPRoute
+                          name: backend
+                          namespace: default
                   name: httproute/default/backend/rule/0/match/0/www_example_com
                   route:
                     cluster: httproute/default/backend/rule/0
