efb835d7b967d4fbcdbd082d43ba872f5b1f7ccc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-26 09:31:09 +0000

    faas-cli 0.16.31

--- Formula/f/faas-cli.rb
+++ Formula/f/faas-cli.rb
@@ -1,8 +1,8 @@
 class FaasCli < Formula
   desc "CLI for templating and/or deploying FaaS functions"
   homepage "https://www.openfaas.com/"
-  url "https://github.com/openfaas/faas-cli/archive/refs/tags/0.16.30.tar.gz"
-  sha256 "7cf8aba4b73f3b76fe345203d1f5c2042b40ec8a28cb34c28a2edc5c5105e426"
+  url "https://github.com/openfaas/faas-cli/archive/refs/tags/0.16.31.tar.gz"
+  sha256 "322520eb0c94cbe513a140ca362550ccdef42639ace4bf8e4d2a505b1fb4d905"
   license "MIT"
   head "https://github.com/openfaas/faas-cli.git", branch: "master"
 
