26671446418296d5e4ba210e41ea90519cc2c1dd
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-07-27 13:02:29 +0700

    cfn-format 1.13.1

--- Formula/c/cfn-format.rb
+++ Formula/c/cfn-format.rb
@@ -1,8 +1,8 @@
 class CfnFormat < Formula
   desc "Command-line tool for formatting AWS CloudFormation templates"
   homepage "https://github.com/aws-cloudformation/rain"
-  url "https://github.com/aws-cloudformation/rain/archive/refs/tags/v1.12.1.tar.gz"
-  sha256 "53f3f412cca548c10b85e49958fb1bd43c065b1d3658500b2fadaf1dadd2edb9"
+  url "https://github.com/aws-cloudformation/rain/archive/refs/tags/v1.13.1.tar.gz"
+  sha256 "97cabab71ed9aa1eb77203c8419856d52a0443b317014a698b152ee3b0385b88"
   license "Apache-2.0"
 
   livecheck do
