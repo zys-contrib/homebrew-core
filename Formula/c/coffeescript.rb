b59f3e5e3d0f1d71e424699863ad763729abec10
Branch Vincent <branchevincent@gmail.com>
2024-07-30 06:44:21 -0700

    coffeescript: use `std_npm_args`

--- Formula/c/coffeescript.rb
+++ Formula/c/coffeescript.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Coffeescript < Formula
   desc "Unfancy JavaScript"
   homepage "https://coffeescript.org/"
@@ -17,7 +15,7 @@ class Coffeescript < Formula
   conflicts_with "cake", because: "both install `cake` binaries"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
