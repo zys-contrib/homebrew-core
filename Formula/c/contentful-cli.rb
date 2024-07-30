00e8cc8e10ee7e7c25ccc6b4defda4afdccdc225
Branch Vincent <branchevincent@gmail.com>
2024-07-30 06:44:21 -0700

    contentful-cli: use `std_npm_args`

--- Formula/c/contentful-cli.rb
+++ Formula/c/contentful-cli.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class ContentfulCli < Formula
   desc "Contentful command-line tools"
   homepage "https://github.com/contentful/contentful-cli"
@@ -21,7 +19,7 @@ class ContentfulCli < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
