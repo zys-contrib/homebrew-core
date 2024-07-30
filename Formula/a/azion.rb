904d320fca1982026b52e4aa0b0f6613ffc2d0f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-30 00:35:36 +0000

    azion: update 1.34.1 bottle.

--- Formula/a/azion.rb
+++ Formula/a/azion.rb
@@ -6,13 +6,13 @@ class Azion < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "894d68927cf3a27f31163ac99204b31c2e9731c0191662c78aa409bb91518e2d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b1380f1d31a40443abd8d2675d3275a97af8ffbf27e6772f7a022955a330d066"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a86d7df451c644c28573affdd381b7f6ce251bd900755ace7ad6f42e04a9351d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "98ed7543742720203f0073e8ab47009054e25833b5a8187251fbe7799a5d83e2"
-    sha256 cellar: :any_skip_relocation, ventura:        "5d38bfdde85928779998457c0028ad9598d582442a9457de1f6540ddb42c7d81"
-    sha256 cellar: :any_skip_relocation, monterey:       "c2e44b689d7c803ea5392579a22d83dbe68088ee08d11b4b18c094e2e17b4c0d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b93da6d51525d3b27b6525bea3fd51287242ef5a937b575a1c07dd43b83e6f6c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6432e4aff28986c18a5a669eb0cf68c06febef3b05182804d7f66c9d9cb06eb9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8e0f888fec8440860d8fb01e96372e7bf3713ef7171336ff1442c708e0e7a619"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5e44840a0e475f2996a8d50f6a9e1bfc03f4d84c1ba845cf269e41ed66141cf3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "ce7d4904e1d0807ec0c0adcf6439a2f493bcbfc07742630306adec14400e7439"
+    sha256 cellar: :any_skip_relocation, ventura:        "1b4f664fc19f46db95e75f668e40109bf996e2fc0f10f631cbb1842174fbd159"
+    sha256 cellar: :any_skip_relocation, monterey:       "1da9bb57d9b9c1c229eff6befdd319adad53774eb8ad0b601d94a7fd3f2a2730"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5cc2b62ddd1d254d4091a296f2bc45b4aad7df1bdc13bad7319721f018fee415"
   end
 
   depends_on "go" => :build
