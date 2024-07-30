2ec7310e7bcb9e0e9643c6e06a06f807ae1039f3
Branch Vincent <branchevincent@gmail.com>
2024-07-30 06:44:23 -0700

    czg: use `std_npm_args`

--- Formula/c/czg.rb
+++ Formula/c/czg.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Czg < Formula
   desc "Interactive Commitizen CLI that generate standardized commit messages"
   homepage "https://github.com/Zhengqbbb/cz-git"
@@ -20,7 +18,7 @@ class Czg < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
