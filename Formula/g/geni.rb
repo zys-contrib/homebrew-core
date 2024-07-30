f089cee7b75a8bd09c2d1d51c26dd950f8a89155
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-30 14:00:33 +0000

    geni: update 1.0.13 bottle.

--- Formula/g/geni.rb
+++ Formula/g/geni.rb
@@ -6,13 +6,13 @@ class Geni < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "288791c3f091a23c545498bac799e12c012c9bbd120072d0c534c896a21ba572"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8c4f6d2535cd19da121b077c38ca9d3fdd32c395f9029d18f853ccf71f4a0b7c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2995f45b8c7b1557f1c2ae87424c0507570bf0342ec375fb78d062789935d638"
-    sha256 cellar: :any_skip_relocation, sonoma:         "49af7c0704657ab750e25602289deec167742c1e724298ce0c0a6e95f33cf10b"
-    sha256 cellar: :any_skip_relocation, ventura:        "6fcdadcfecc05707d569635fc968c40b70a88536cd9a6b21aef02ec6f8b411fa"
-    sha256 cellar: :any_skip_relocation, monterey:       "00866a8f4f3ef1db70d38ce9edcd58d376590159656084e4fe4f502dad8a85e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7846ad610ba00259775fcc4a2f23bf37a2f7a626dc2b9eab81b3e2965f903ec9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7978c2a09a02fa3014376e699f112e570ac47776b2d6aec7914df969e163be8b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8118eafe9e8962701aff9d16317fcb2b2d47b3631d342c9524d7f37701a2b6b9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "19c9ce8b4bc27a62fd23c89a434d59fc57e5568e59be7606b0884d90d97abcae"
+    sha256 cellar: :any_skip_relocation, sonoma:         "38196584de6f10ebc656c0d29964eb241392072c20ce1321ef165c11e9f981aa"
+    sha256 cellar: :any_skip_relocation, ventura:        "474b45c0ee476c23e3cfebc96ae66b009a4adffb657bb32a4f43831b928ae367"
+    sha256 cellar: :any_skip_relocation, monterey:       "739bc8423bb18b2d59230def2315f48ef803adc8ff0628a339d3fc1636b32398"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "37e62346a16b3e96e3a3f8a80d4db6f528662a27fad83238a070613bc2f45465"
   end
 
   depends_on "rust" => :build
