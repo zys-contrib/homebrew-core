6629c62debea0cebed568af43b400b2f83889aaa
Branch Vincent <branchevincent@gmail.com>
2024-07-30 18:02:04 -0700

    devcontainer: use `std_npm_args`

--- Formula/d/devcontainer.rb
+++ Formula/d/devcontainer.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Devcontainer < Formula
   desc "Reference implementation for the Development Containers specification"
   homepage "https://containers.dev"
@@ -20,7 +18,7 @@ class Devcontainer < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
