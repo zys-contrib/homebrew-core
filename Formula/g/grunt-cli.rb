3e8acb044c182f74543bc27c0042bf926d50fea6
Branch Vincent <branchevincent@gmail.com>
2024-07-31 06:14:14 -0700

    grunt-cli: use `std_npm_args`

--- Formula/g/grunt-cli.rb
+++ Formula/g/grunt-cli.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class GruntCli < Formula
   desc "JavaScript Task Runner"
   homepage "https://gruntjs.com/"
@@ -20,7 +18,7 @@ class GruntCli < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
@@ -43,7 +41,7 @@ class GruntCli < Formula
       };
     EOS
 
-    system "npm", "install", *Language::Node.local_npm_install_args
+    system "npm", "install", *std_npm_args(prefix: false)
     system bin/"grunt"
     assert_predicate testpath/"output.txt", :exist?, "output.txt was not generated"
   end
