7300520656bc2cc18fb17514c3d72fe722b678e0
Branch Vincent <branchevincent@gmail.com>
2024-07-31 06:38:12 -0700

    ios-sim: use `std_npm_args`

--- Formula/i/ios-sim.rb
+++ Formula/i/ios-sim.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class IosSim < Formula
   desc "Command-line application launcher for the iOS Simulator"
   homepage "https://github.com/ios-control/ios-sim"
@@ -26,7 +24,7 @@ class IosSim < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
