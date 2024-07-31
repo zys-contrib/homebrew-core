1cbbd89279639baa66d1245e323a77b930a5e0bc
Branch Vincent <branchevincent@gmail.com>
2024-07-31 08:48:47 -0700

    kubevious: use `std_npm_args`

--- Formula/k/kubevious.rb
+++ Formula/k/kubevious.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Kubevious < Formula
   desc "Detects and prevents Kubernetes misconfigurations and violations"
   homepage "https://github.com/kubevious/cli"
@@ -20,7 +18,7 @@ class Kubevious < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
