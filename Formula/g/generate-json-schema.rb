b16833aa1a0003c0aeb6c57c2c3199f5db2aaad9
Branch Vincent <branchevincent@gmail.com>
2024-07-31 06:14:11 -0700

    generate-json-schema: use `std_npm_args`

--- Formula/g/generate-json-schema.rb
+++ Formula/g/generate-json-schema.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class GenerateJsonSchema < Formula
   desc "Generate a JSON Schema from Sample JSON"
   homepage "https://github.com/Nijikokun/generate-schema"
@@ -16,7 +14,7 @@ class GenerateJsonSchema < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
