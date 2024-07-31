ae00d27da5cab62bec56ad8286542a6c3e098c04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-31 03:43:32 +0000

    igv: update 2.18.0 bottle.

--- Formula/i/igv.rb
+++ Formula/i/igv.rb
@@ -11,7 +11,13 @@ class Igv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a0c84d6e6c34e3d257fa3365b56c33fb25c20d1f379c2e99e380c3c9fec1b969"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "030f6d4754c2eb1adc896ec8f0642e2e83f3d1f8f1e777e9f70e523cdd0fd237"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "030f6d4754c2eb1adc896ec8f0642e2e83f3d1f8f1e777e9f70e523cdd0fd237"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "030f6d4754c2eb1adc896ec8f0642e2e83f3d1f8f1e777e9f70e523cdd0fd237"
+    sha256 cellar: :any_skip_relocation, sonoma:         "030f6d4754c2eb1adc896ec8f0642e2e83f3d1f8f1e777e9f70e523cdd0fd237"
+    sha256 cellar: :any_skip_relocation, ventura:        "030f6d4754c2eb1adc896ec8f0642e2e83f3d1f8f1e777e9f70e523cdd0fd237"
+    sha256 cellar: :any_skip_relocation, monterey:       "030f6d4754c2eb1adc896ec8f0642e2e83f3d1f8f1e777e9f70e523cdd0fd237"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d701f4d22af3800ef187341b9b40dbc18baca38acf65cc624151917d3806927d"
   end
 
   depends_on "openjdk"
