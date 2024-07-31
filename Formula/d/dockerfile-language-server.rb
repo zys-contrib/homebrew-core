0c894c27bb1134e5966b4d1f71c769ec64a338f9
Branch Vincent <branchevincent@gmail.com>
2024-07-30 18:02:05 -0700

    dockerfile-language-server: use `std_npm_args`

--- Formula/d/dockerfile-language-server.rb
+++ Formula/d/dockerfile-language-server.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class DockerfileLanguageServer < Formula
   desc "Language server for Dockerfiles powered by Node, TypeScript, and VSCode"
   homepage "https://github.com/rcjsuen/dockerfile-language-server"
@@ -21,7 +19,7 @@ class DockerfileLanguageServer < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
