506caade7b6da9f54880e2d1519c557fc6abbc3a
Branch Vincent <branchevincent@gmail.com>
2024-07-30 18:05:41 -0700

    fauna-shell: use `std_npm_args`

--- Formula/f/fauna-shell.rb
+++ Formula/f/fauna-shell.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class FaunaShell < Formula
   desc "Interactive shell for FaunaDB"
   homepage "https://fauna.com/"
@@ -20,7 +18,7 @@ class FaunaShell < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
