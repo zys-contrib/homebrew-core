8ed667b8adee040126a6522feebe15aced598093
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-07-31 20:30:54 +0000

    gulp-cli: update 3.0.0 bottle.

--- Formula/g/gulp-cli.rb
+++ Formula/g/gulp-cli.rb
@@ -6,13 +6,14 @@ class GulpCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f07bd7a6d69f5b7e2476bcd3ea6ad2346d131c435267d551a6c2dd66ecf4369f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f07bd7a6d69f5b7e2476bcd3ea6ad2346d131c435267d551a6c2dd66ecf4369f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f07bd7a6d69f5b7e2476bcd3ea6ad2346d131c435267d551a6c2dd66ecf4369f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "3e874ad640e88888810e0b31f362efca2a993c5312f292cb82b172e0633bc662"
-    sha256 cellar: :any_skip_relocation, ventura:        "3e874ad640e88888810e0b31f362efca2a993c5312f292cb82b172e0633bc662"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e874ad640e88888810e0b31f362efca2a993c5312f292cb82b172e0633bc662"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f07bd7a6d69f5b7e2476bcd3ea6ad2346d131c435267d551a6c2dd66ecf4369f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b6b78b8a649dc8b662a2fa39814d039991950450e52b20692ff9756955f642d6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b6b78b8a649dc8b662a2fa39814d039991950450e52b20692ff9756955f642d6"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b6b78b8a649dc8b662a2fa39814d039991950450e52b20692ff9756955f642d6"
+    sha256 cellar: :any_skip_relocation, sonoma:         "97e82d635666e3f778dd12285f8881701110370eaefe6c22fb902b493ae2ba58"
+    sha256 cellar: :any_skip_relocation, ventura:        "97e82d635666e3f778dd12285f8881701110370eaefe6c22fb902b493ae2ba58"
+    sha256 cellar: :any_skip_relocation, monterey:       "97e82d635666e3f778dd12285f8881701110370eaefe6c22fb902b493ae2ba58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ca11835e27a0ab600dc0ce1c92b06791fb15b3f7c75d71d97ca4984983deff8"
   end
 
   depends_on "node"
