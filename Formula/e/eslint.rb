8b772f18dff21ce3548df1ec08837c5fdd8f5a3f
Branch Vincent <branchevincent@gmail.com>
2024-07-30 18:05:41 -0700

    eslint: use `std_npm_args`

--- Formula/e/eslint.rb
+++ Formula/e/eslint.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Eslint < Formula
   desc "AST-based pattern checker for JavaScript"
   homepage "https://eslint.org"
@@ -20,7 +18,7 @@ class Eslint < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
