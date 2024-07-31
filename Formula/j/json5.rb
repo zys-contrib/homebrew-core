0f395a517f2e9a2fe1d09db2c3f14b76693e3982
Branch Vincent <branchevincent@gmail.com>
2024-07-31 06:38:13 -0700

    json5: use `std_npm_args`

--- Formula/j/json5.rb
+++ Formula/j/json5.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Json5 < Formula
   desc "JSON enhanced with usability features"
   homepage "https://json5.org/"
@@ -14,7 +12,7 @@ class Json5 < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
