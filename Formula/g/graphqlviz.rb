6aff0273bcdcdd5847d8a2b848a8083b04a1f538
Branch Vincent <branchevincent@gmail.com>
2024-07-31 06:14:13 -0700

    graphqlviz: use `std_npm_args`

--- Formula/g/graphqlviz.rb
+++ Formula/g/graphqlviz.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Graphqlviz < Formula
   desc "GraphQL Server schema visualizer"
   homepage "https://github.com/sheerun/graphqlviz"
@@ -14,7 +12,7 @@ class Graphqlviz < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
