bc32e5b431af42fd7c1f31753a1cdc7f2d205964
Branch Vincent <branchevincent@gmail.com>
2024-07-31 06:38:13 -0700

    jscpd: use `std_npm_args`

--- Formula/j/jscpd.rb
+++ Formula/j/jscpd.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Jscpd < Formula
   desc "Copy/paste detector for programming source code"
   homepage "https://github.com/kucherenko/jscpd"
@@ -20,7 +18,7 @@ class Jscpd < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
