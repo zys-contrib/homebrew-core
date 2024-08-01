0a1107b1649dd0ca2dcd39fd648015361968482d
Branch Vincent <branchevincent@gmail.com>
2024-08-01 05:53:18 -0700

    prettierd: use `std_npm_args`

--- Formula/p/prettierd.rb
+++ Formula/p/prettierd.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Prettierd < Formula
   desc "Prettier daemon"
   homepage "https://github.com/fsouza/prettierd"
@@ -14,7 +12,7 @@ class Prettierd < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
