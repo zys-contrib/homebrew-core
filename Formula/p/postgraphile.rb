d9534da09feef3627378959b715ff88945cd4cab
Branch Vincent <branchevincent@gmail.com>
2024-08-01 05:53:18 -0700

    postgraphile: use `std_npm_args`

--- Formula/p/postgraphile.rb
+++ Formula/p/postgraphile.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Postgraphile < Formula
   desc "GraphQL schema created by reflection over a PostgreSQL schema"
   homepage "https://www.graphile.org/postgraphile/"
@@ -22,7 +20,7 @@ class Postgraphile < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
