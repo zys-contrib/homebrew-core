dd421d5e15b8b3a5d9ea8eb134431e43a82dccaa
Branch Vincent <branchevincent@gmail.com>
2024-08-01 08:03:15 -0700

    tailwindcss-language-server: use `std_npm_args`

--- Formula/t/tailwindcss-language-server.rb
+++ Formula/t/tailwindcss-language-server.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class TailwindcssLanguageServer < Formula
   desc "LSP for TailwindCSS"
   homepage "https://github.com/tailwindlabs/tailwindcss-intellisense/tree/HEAD/packages/tailwindcss-language-server"
@@ -20,7 +18,7 @@ class TailwindcssLanguageServer < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
 
     # Replace universal binaries with their native slices
