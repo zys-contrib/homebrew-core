f71dc47c6425efaaa445ea5875f32b3cae7bfc32
Branch Vincent <branchevincent@gmail.com>
2024-08-01 05:53:18 -0700

    patch-package: use `std_npm_args`

--- Formula/p/patch-package.rb
+++ Formula/p/patch-package.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class PatchPackage < Formula
   desc "Fix broken node modules instantly"
   homepage "https://github.com/ds300/patch-package"
@@ -20,7 +18,7 @@ class PatchPackage < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
