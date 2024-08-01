7967a7eef629faf804c6d121cd252e178fd0e814
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-01 12:51:58 +0000

    terragrunt 0.64.5

--- Formula/t/terragrunt.rb
+++ Formula/t/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v0.64.4.tar.gz"
-  sha256 "5e7c83c766660d11d6d7a384c6f1a1334b39759b744f9b419dfe28a0547f3a5b"
+  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v0.64.5.tar.gz"
+  sha256 "34ca9cd2b8b548b43997bad0bb3da318446c138c1b53ed74209efa3b232c5d38"
   license "MIT"
 
   bottle do
