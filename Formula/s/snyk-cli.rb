c5522d8a7136436ab7ba05c86bb5d04c88f966b2
Patrick Linnane <patrick@linnane.io>
2024-08-01 14:28:36 -0700

    snyk-cli: use `std_npm_args`
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/s/snyk-cli.rb
+++ Formula/s/snyk-cli.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class SnykCli < Formula
   desc "Scans and monitors projects for security vulnerabilities"
   homepage "https://snyk.io"
@@ -20,7 +18,7 @@ class SnykCli < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
