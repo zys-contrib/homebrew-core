b9cd99754921dc495f6a2706d0d4d194ff54a6c0
Branch Vincent <branchevincent@gmail.com>
2024-08-01 08:03:15 -0700

    terrahub: use `std_npm_args`

--- Formula/t/terrahub.rb
+++ Formula/t/terrahub.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Terrahub < Formula
   desc "Terraform automation and orchestration tool"
   homepage "https://docs.terrahub.io"
@@ -28,7 +26,7 @@ class Terrahub < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
