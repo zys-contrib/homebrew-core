fac48d4e6778b6f7236498fb8c6d681f7ca98d42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-01 06:40:00 +0000

    kubekey 3.1.3

--- Formula/k/kubekey.rb
+++ Formula/k/kubekey.rb
@@ -2,8 +2,8 @@ class Kubekey < Formula
   desc "Installer for Kubernetes and / or KubeSphere, and related cloud-native add-ons"
   homepage "https://kubesphere.io"
   url "https://github.com/kubesphere/kubekey.git",
-      tag:      "v3.1.1",
-      revision: "7a184f786b02bec20d5534af137896f50e510396"
+      tag:      "v3.1.3",
+      revision: "b3bb8538ee7518282b733040b068639070c19709"
   license "Apache-2.0"
   head "https://github.com/kubesphere/kubekey.git", branch: "master"
 
