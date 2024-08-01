56967b47cc70db7a13d3a4006512a60bb608091b
Branch Vincent <branchevincent@gmail.com>
2024-08-01 07:45:25 -0700

    solhint: use `std_npm_args`

--- Formula/s/solhint.rb
+++ Formula/s/solhint.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Solhint < Formula
   desc "Linter for Solidity code"
   homepage "https://protofire.github.io/solhint/"
@@ -20,7 +18,7 @@ class Solhint < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
