aa1059b29913a4b284aa8a8ff446b07bf03f99ea
Branch Vincent <branchevincent@gmail.com>
2024-07-31 18:16:29 -0700

    mjml: use `std_npm_args`

--- Formula/m/mjml.rb
+++ Formula/m/mjml.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Mjml < Formula
   desc "JavaScript framework that makes responsive-email easy"
   homepage "https://mjml.io"
@@ -20,7 +18,7 @@ class Mjml < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
