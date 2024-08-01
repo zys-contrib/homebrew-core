c8f3f3a2ca2b467998ec6fb7b9955b1400fc1004
Michael Cho <michael@michaelcho.dev>
2024-08-01 09:16:36 -0400

    root: revision bump (mysql-client 9.0.1)

--- Formula/r/root.rb
+++ Formula/r/root.rb
@@ -4,6 +4,7 @@ class Root < Formula
   url "https://root.cern.ch/download/root_v6.32.02.source.tar.gz"
   sha256 "3d0f76bf05857e1807ccfb2c9e014f525bcb625f94a2370b455f4b164961602d"
   license "LGPL-2.1-or-later"
+  revision 1
   head "https://github.com/root-project/root.git", branch: "master"
 
   livecheck do
