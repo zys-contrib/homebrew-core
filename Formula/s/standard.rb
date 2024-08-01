e36aea073b3266b1395b061df8a3ab4f4c5c90c8
Branch Vincent <branchevincent@gmail.com>
2024-08-01 07:45:26 -0700

    standard: use `std_npm_args`

--- Formula/s/standard.rb
+++ Formula/s/standard.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Standard < Formula
   desc "JavaScript Style Guide, with linter & automatic code fixer"
   homepage "https://standardjs.com/"
@@ -22,7 +20,7 @@ class Standard < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
