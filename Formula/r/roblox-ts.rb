f0dd917c4e630d67f7bec903ba4ea278de0d5abd
Branch Vincent <branchevincent@gmail.com>
2024-08-01 07:41:37 -0700

    roblox-ts: use `std_npm_args`

--- Formula/r/roblox-ts.rb
+++ Formula/r/roblox-ts.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class RobloxTs < Formula
   desc "TypeScript-to-Luau Compiler for Roblox"
   homepage "https://roblox-ts.com/"
@@ -20,7 +18,7 @@ class RobloxTs < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
