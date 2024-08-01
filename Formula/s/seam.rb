6e367d6e5247389e7c0ccb1265fa4efd7f77e780
Branch Vincent <branchevincent@gmail.com>
2024-08-01 07:45:25 -0700

    seam: use `std_npm_args`

--- Formula/s/seam.rb
+++ Formula/s/seam.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Seam < Formula
   desc "This utility lets you control Seam resources"
   homepage "https://github.com/seamapi/seam-cli"
@@ -20,7 +18,7 @@ class Seam < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
