97ddd6e075412c9a47d9f89fa7de04b1ab25006c
Branch Vincent <branchevincent@gmail.com>
2024-08-01 08:03:16 -0700

    typescript: use `std_npm_args`

--- Formula/t/typescript.rb
+++ Formula/t/typescript.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Typescript < Formula
   desc "Language for application scale JavaScript development"
   homepage "https://www.typescriptlang.org/"
@@ -21,7 +19,7 @@ class Typescript < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
