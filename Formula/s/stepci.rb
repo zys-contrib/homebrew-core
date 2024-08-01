845a037f74aa93037c27897d1adffd23be132bee
Branch Vincent <branchevincent@gmail.com>
2024-08-01 07:45:26 -0700

    stepci: use `std_npm_args`

--- Formula/s/stepci.rb
+++ Formula/s/stepci.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Stepci < Formula
   desc "API Testing and Monitoring made simple"
   homepage "https://stepci.com"
@@ -20,7 +18,7 @@ class Stepci < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
