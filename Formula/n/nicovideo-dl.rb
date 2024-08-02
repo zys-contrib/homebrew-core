ff2c4ac6f885e02816d612d651c077bace6c3d50
Rui Chen <rui@chenrui.dev>
2024-08-02 16:29:50 -0400

    nicovideo-dl: add license
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/nicovideo-dl.rb
+++ Formula/n/nicovideo-dl.rb
@@ -6,6 +6,7 @@ class NicovideoDl < Formula
   # Canonical: https://osdn.net/dl/nicovideo-dl/nicovideo-dl-0.0.20190126.tar.gz
   url "https://dotsrc.dl.osdn.net/osdn/nicovideo-dl/70568/nicovideo-dl-0.0.20190126.tar.gz"
   sha256 "886980d154953bc5ff5d44758f352ce34d814566a83ceb0b412b8d2d51f52197"
+  license "MIT"
   revision 3
 
   livecheck do
