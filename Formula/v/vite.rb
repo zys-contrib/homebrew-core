142d0032005ca2722801a4a90d18f0e04797df71
Branch Vincent <branchevincent@gmail.com>
2024-08-01 17:49:58 -0700

    vite: use `std_npm_args`

--- Formula/v/vite.rb
+++ Formula/v/vite.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Vite < Formula
   desc "Next generation frontend tooling. It's fast!"
   homepage "https://vitejs.dev/"
@@ -20,7 +18,7 @@ class Vite < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
