737b55861048bdea7ecfcb0d12056307c6533e8c
Michael Cho <michael@michaelcho.dev>
2024-08-01 22:05:43 -0400

    libgedit-gfls 0.1.0 (new formula)

---
+++ Formula/lib/libgedit-gfls.rb
@@ -0,0 +1,43 @@
+class LibgeditGfls < Formula
+  desc "Gedit Technology - File loading and saving"
+  homepage "https://gitlab.gnome.org/World/gedit/libgedit-gfls"
+  url "https://gitlab.gnome.org/World/gedit/libgedit-gfls/-/archive/0.1.0/libgedit-gfls-0.1.0.tar.bz2"
+  sha256 "306ba5b25c73a6901069f0d535b845eb13167a7be2d15f86451ae5d9016229ca"
+  license "LGPL-3.0-or-later"
+  head "https://gitlab.gnome.org/World/gedit/libgedit-gfls.git", branch: "main"
+
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "glib"
+  depends_on "gtk+3"
+
+  def install
+    system "meson", "setup", "build", "-Dgtk_doc=false", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <gfls/gfls.h>
+
+      int main(int argc, char *argv[]) {
+        gfls_init();
+        GflsUnsavedDocumentTitles *titles = gfls_unsaved_document_titles_new();
+        gchar *title = gfls_unsaved_document_titles_get_title(titles, 1);
+        printf("%s", title);
+        g_free(title);
+        g_object_unref(titles);
+        gfls_finalize();
+        return 0;
+      }
+    EOS
+
+    flags = shell_output("pkg-config --cflags --libs libgedit-gfls-1").strip.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    assert_equal "Unsaved Document 1", shell_output("./test")
+  end
+end
