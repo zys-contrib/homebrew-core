f1a90ed79a0ec8f7c4571be29f232ed2848cad1e
Branch Vincent <branchevincent@gmail.com>
2024-08-01 17:49:59 -0700

    zx: use `std_npm_args`

--- Formula/z/zx.rb
+++ Formula/z/zx.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Zx < Formula
   desc "Tool for writing better scripts"
   homepage "https://github.com/google/zx"
@@ -20,7 +18,7 @@ class Zx < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
