1fd26e0f345fb800ebc752473615ef454ddcb7ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-02 01:06:09 +0000

    fern-api: update 0.37.5 bottle.

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -6,13 +6,13 @@ class FernApi < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bbc58bef4447d767fd0ce1742b9cd178eec9c4b514def10973e7ddc1e36624d1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bbc58bef4447d767fd0ce1742b9cd178eec9c4b514def10973e7ddc1e36624d1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bbc58bef4447d767fd0ce1742b9cd178eec9c4b514def10973e7ddc1e36624d1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "bbc58bef4447d767fd0ce1742b9cd178eec9c4b514def10973e7ddc1e36624d1"
-    sha256 cellar: :any_skip_relocation, ventura:        "bbc58bef4447d767fd0ce1742b9cd178eec9c4b514def10973e7ddc1e36624d1"
-    sha256 cellar: :any_skip_relocation, monterey:       "bbc58bef4447d767fd0ce1742b9cd178eec9c4b514def10973e7ddc1e36624d1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c92b058939613495d2d11902f491d3125c6540232f7e08173cde3a0fd63b6f9a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "14dc46a137496c6702106670383b148e534f7d8e2a0d78c901ecc88c0a249753"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "14dc46a137496c6702106670383b148e534f7d8e2a0d78c901ecc88c0a249753"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "14dc46a137496c6702106670383b148e534f7d8e2a0d78c901ecc88c0a249753"
+    sha256 cellar: :any_skip_relocation, sonoma:         "14dc46a137496c6702106670383b148e534f7d8e2a0d78c901ecc88c0a249753"
+    sha256 cellar: :any_skip_relocation, ventura:        "14dc46a137496c6702106670383b148e534f7d8e2a0d78c901ecc88c0a249753"
+    sha256 cellar: :any_skip_relocation, monterey:       "14dc46a137496c6702106670383b148e534f7d8e2a0d78c901ecc88c0a249753"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd1bba75a88b9cd0e4e73e01bdb075b9796a12d382e057e0b0af5ae86e508359"
   end
 
   depends_on "node"
