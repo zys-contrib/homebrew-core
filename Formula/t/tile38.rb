ecaeeaae9a686ea154e9d3dd75f47f33a4ca49ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-02 19:09:06 +0000

    tile38 1.33.2

--- Formula/t/tile38.rb
+++ Formula/t/tile38.rb
@@ -2,8 +2,8 @@ class Tile38 < Formula
   desc "In-memory geolocation data store, spatial index, and realtime geofence"
   homepage "https://tile38.com/"
   url "https://github.com/tidwall/tile38.git",
-      tag:      "1.33.1",
-      revision: "4a1c800b0ec76f924d4b9c21b1ed08948e26e851"
+      tag:      "1.33.2",
+      revision: "a953466318ce1d65a16259099a5b023650bfdf11"
   license "MIT"
   head "https://github.com/tidwall/tile38.git", branch: "master"
 
