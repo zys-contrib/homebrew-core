f02f108ea90abeb029f59cddb9de569f84d2191a
Michael Cho <michael@michaelcho.dev>
2024-08-02 13:12:24 -0400

    ncnn: enable Vulkan on Linux

--- Formula/n/ncnn.rb
+++ Formula/n/ncnn.rb
@@ -18,15 +18,18 @@ class Ncnn < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "abseil"
+  depends_on "glslang"
   depends_on "protobuf"
 
   on_macos do
-    depends_on "vulkan-headers" => [:build, :test]
-    depends_on "abseil"
-    depends_on "glslang"
     depends_on "libomp"
     depends_on "molten-vk"
     depends_on "spirv-tools"
+  end
+
+  on_linux do
+    depends_on "vulkan-tools" => :test
   end
 
   def install
@@ -37,30 +40,41 @@ class Ncnn < Formula
     args = %W[
       -DCMAKE_CXX_STANDARD=17
       -DCMAKE_CXX_STANDARD_REQUIRED=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
       -DNCNN_SHARED_LIB=ON
       -DNCNN_BUILD_BENCHMARK=OFF
       -DNCNN_BUILD_EXAMPLES=OFF
-      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DNCNN_SYSTEM_GLSLANG=ON
+      -DGLSLANG_TARGET_DIR=#{Formula["glslang"].opt_lib}/cmake
+      -DNCNN_VULKAN=ON
     ]
 
     if OS.mac?
       args += %W[
-        -DNCNN_SYSTEM_GLSLANG=ON
-        -DGLSLANG_TARGET_DIR=#{Formula["glslang"].opt_lib/"cmake"}
-        -DNCNN_VULKAN=ON
         -DVulkan_INCLUDE_DIR=#{Formula["molten-vk"].opt_include}
         -DVulkan_LIBRARY=#{Formula["molten-vk"].opt_lib/shared_library("libMoltenVK")}
       ]
     end
 
-    inreplace "src/gpu.cpp", "glslang/glslang", "glslang"
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
   test do
+    vulkan = 1
+    if OS.linux?
+      # Use a fake Vulkan ICD on Linux as it is lighter-weight than testing
+      # with `vulkan-loader` and `mesa` (CPU/LLVMpipe) dependencies.
+      ENV["VK_ICD_FILENAMES"] = Formula["vulkan-tools"].lib/"mock_icd/VkICD_mock_icd.json"
+    elsif ENV["HOMEBREW_GITHUB_ACTIONS"] && Hardware::CPU.intel?
+      # Don't test Vulkan on GitHub Intel macOS runners as they fail with: "vkCreateInstance failed -9"
+      vulkan = 0
+    end
+
     (testpath/"test.cpp").write <<~EOS
+      #include <cassert>
+      #include <ncnn/gpu.h>
       #include <ncnn/mat.h>
 
       int main(void) {
@@ -69,12 +83,19 @@ class Ncnn < Formula
           ncnn::Mat myMatClone = myMat.clone();
           myMat.release();
           myMatClone.release();
+
+      #if #{vulkan}
+          ncnn::create_gpu_instance();
+          assert(ncnn::get_gpu_count() > 0);
+          ncnn::destroy_gpu_instance();
+      #endif
+
           return 0;
       }
     EOS
 
     system ENV.cxx, "test.cpp", "-std=c++11",
-                    "-I#{Formula["vulkan-headers"].opt_include}", "-I#{include}", "-L#{lib}", "-lncnn",
+                    "-I#{include}", "-L#{lib}", "-lncnn",
                     "-o", "test"
     system "./test"
   end
