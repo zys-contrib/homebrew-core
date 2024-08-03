e6a6e7b0bbcfe968b6029652db121e327d71fe97
Nathaniel Varona <194283+nathanielvarona@users.noreply.github.com>
2024-08-03 09:05:06 +0800

    whalebrew 0.5.0
    Co-authored-by: Branch Vincent <branchevincent@gmail.com>

--- Formula/w/whalebrew.rb
+++ Formula/w/whalebrew.rb
@@ -1,9 +1,8 @@
 class Whalebrew < Formula
   desc "Homebrew, but with Docker images"
   homepage "https://github.com/whalebrew/whalebrew"
-  url "https://github.com/whalebrew/whalebrew.git",
-      tag:      "0.4.1",
-      revision: "8137e562f38ce32d425df4a3676143c2d631d0f1"
+  url "https://github.com/whalebrew/whalebrew/archive/refs/tags/0.5.0.tar.gz"
+  sha256 "2abea4171dbdca429b6476cffdfe7c94ce27028dc5d96e0f3a9a4fdeab77c4fb"
   license "Apache-2.0"
   head "https://github.com/whalebrew/whalebrew.git", branch: "master"
 
@@ -21,6 +20,13 @@ class Whalebrew < Formula
   end
 
   depends_on "go" => :build
+  depends_on "docker" => :test
+
+  # Run "go mod tidy": https://github.com/whalebrew/whalebrew/pull/299
+  patch do
+    url "https://github.com/whalebrew/whalebrew/commit/f64b9db9a5f1c91571a622a58eb93233f8c59642.patch?full_index=1"
+    sha256 "62d5cb208d60ed123a5be234a1c3d48aeead703a83eb262408a101aec66bd0d6"
+  end
 
   def install
     ldflags = %W[
@@ -33,8 +39,9 @@ class Whalebrew < Formula
 
   test do
     assert_match "Whalebrew #{version}+homebrew", shell_output("#{bin}/whalebrew version")
+    assert_match "whalebrew/whalesay", shell_output("#{bin}/whalebrew search whalesay")
 
-    output = shell_output("#{bin}/whalebrew install whalebrew/whalesay -y", 255)
+    output = shell_output("#{bin}/whalebrew install whalebrew/whalesay -y 2>&1", 255)
     assert_match(/connect to the Docker daemon|operation not permitted/, output)
   end
 end
