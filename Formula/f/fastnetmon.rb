47bc6f891c77122df28c1e77bf98c5c11f0e2b2a
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-08-03 12:13:31 +0800

    fastnetmon: revision bump (abseil 20240722.0)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.6.tar.gz"
   sha256 "b6a7d1e9ba98c1c042d774bff82ea3e8bbf03085e0be43a2676e41d590f668cf"
   license "GPL-2.0-only"
-  revision 14
+  revision 15
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "779080428f45d396f2dddfde202e9a301d84099b99a85be3b16fa549294c7e20"
