e958561eacb4e2d2c1f0dccc1e4f142cd911b706
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-03 22:52:44 +0000

    solhint: update 5.0.3 bottle.

--- Formula/s/solhint.rb
+++ Formula/s/solhint.rb
@@ -6,14 +6,13 @@ class Solhint < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "882122bc044a0bf518f9d059ac50307bbc6ad8afc0392bb8086375f8d01b1575"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "882122bc044a0bf518f9d059ac50307bbc6ad8afc0392bb8086375f8d01b1575"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "882122bc044a0bf518f9d059ac50307bbc6ad8afc0392bb8086375f8d01b1575"
-    sha256 cellar: :any_skip_relocation, sonoma:         "bb2b6f082e264891758f7cbe3ae8b38ac721bc76de86ff2ee4ceec0578f61eb7"
-    sha256 cellar: :any_skip_relocation, ventura:        "bb2b6f082e264891758f7cbe3ae8b38ac721bc76de86ff2ee4ceec0578f61eb7"
-    sha256 cellar: :any_skip_relocation, monterey:       "bb2b6f082e264891758f7cbe3ae8b38ac721bc76de86ff2ee4ceec0578f61eb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b56dff559bba772052a1d8928b9834944c6b1e7f1bf72a194420b193ec3f5c6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5bcf8c9119a06cc86fa904103605f918a4f5c53a3ad14452c6d8eae2710223f8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5bcf8c9119a06cc86fa904103605f918a4f5c53a3ad14452c6d8eae2710223f8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5bcf8c9119a06cc86fa904103605f918a4f5c53a3ad14452c6d8eae2710223f8"
+    sha256 cellar: :any_skip_relocation, sonoma:         "c61c21de99312d1f161f29e0d5fe723ac206cfe0850d0a75b71a04b3d50e67ce"
+    sha256 cellar: :any_skip_relocation, ventura:        "c61c21de99312d1f161f29e0d5fe723ac206cfe0850d0a75b71a04b3d50e67ce"
+    sha256 cellar: :any_skip_relocation, monterey:       "c61c21de99312d1f161f29e0d5fe723ac206cfe0850d0a75b71a04b3d50e67ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5bcf8c9119a06cc86fa904103605f918a4f5c53a3ad14452c6d8eae2710223f8"
   end
 
   depends_on "node"
