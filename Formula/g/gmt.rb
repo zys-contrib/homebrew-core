zys-contrib/homebrew-core

commit 5c4ae1794d3b00bbdfcfdbe9a124661f9c0d4a48

gmt: Stop interpolating `bin`
Signed-off-by: Rui Chen <rui@chenrui.dev>

Formula/g/gmt.rb
@@ -85,7 +85,7 @@ class Gmt < Formula
   end
 
   test do
-    system "#{bin}/gmt pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -G240 -W1/0 -P > test.ps"
+    system bin/" pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -G240 -W1/0 -P > test.ps"
     assert_predicate testpath/"test.ps", :exist?
   end
 end

commit 4a5284124ae70f34f03d679cf61c77e64d74c103

gmt: fix test
This was accidentally broken in 5c4ae1794d3b00bbdfcfdbe9a124661f9c0d4a48
during a bulk fix for `bin` interpolation.

Formula/g/gmt.rb
@@ -85,7 +85,7 @@ class Gmt < Formula
   end
 
   test do
-    system bin/" pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -G240 -W1/0 -P > test.ps"
-    assert_predicate testpath/"test.ps", :exist?
+    cmd = "#{bin}/gmt pscoast -R0/360/-70/70 -Jm1.2e-2i -Ba60f30/a30f15 -Dc -G240 -W1/0 -P"
+    refute_predicate shell_output(cmd), :empty?
   end
 end