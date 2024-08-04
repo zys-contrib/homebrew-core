4da52d306134e42afc16fc6a9c954dc0ce973e72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-04 06:23:21 +0000

    age-plugin-yubikey 0.5.0

--- Formula/a/age-plugin-yubikey.rb
+++ Formula/a/age-plugin-yubikey.rb
@@ -1,8 +1,8 @@
 class AgePluginYubikey < Formula
   desc "Plugin for encrypting files with age and PIV tokens such as YubiKeys"
   homepage "https://github.com/str4d/age-plugin-yubikey"
-  url "https://github.com/str4d/age-plugin-yubikey/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "721c2fd08fe8b7228ea43398475b954a8f0bc259b3a152f6f3b0dc66022df55e"
+  url "https://github.com/str4d/age-plugin-yubikey/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "65807403f0098569a473ffa76302b205da148a7f46b61fd331b8e323959978ba"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/str4d/age-plugin-yubikey.git", branch: "main"
 
