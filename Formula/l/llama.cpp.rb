d033ee0a6e9ef983f2f1da851ae876f5f9955b65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-05 15:22:05 +0000

    llama.cpp: update 3520 bottle.

--- Formula/l/llama.cpp.rb
+++ Formula/l/llama.cpp.rb
@@ -14,13 +14,13 @@ class LlamaCpp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "28d9c43a88b298f3606b71bb78bfae5c2e4e2eb8c4fcb352f19da9697db64c28"
-    sha256 cellar: :any,                 arm64_ventura:  "15f5ca3e2230d984ab8942c748749ede97fd16997bdce9b634c8e538400b4b43"
-    sha256 cellar: :any,                 arm64_monterey: "3250af5c90e6f46218b2de54268f94b46242e52359a70ecf4a9bcb3ce760997c"
-    sha256 cellar: :any,                 sonoma:         "51c1dbce40914be85e47a76916a9c22d7901a2f6069d9a3880532b6958777d22"
-    sha256 cellar: :any,                 ventura:        "3f3a4971f92625682828bb2a2403a126895c22e337baa651c178369fd3db7d55"
-    sha256 cellar: :any,                 monterey:       "5277589b86d7a9df09b3e117b32197c3cf8c50006fa5d3a8ca2d6581f2b4b263"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "33d51f88afb6859f74506990fe856a6f4f8c72c00fce8fa65220a3a272682312"
+    sha256 cellar: :any,                 arm64_sonoma:   "d738a8504090e456626dd513f4615a03aa3b884970a63477355928dac3628e4e"
+    sha256 cellar: :any,                 arm64_ventura:  "5a55ffdf7aeb95e1b39a58fd10ae2673fdf7596cb8b2288eddb740ef58967d42"
+    sha256 cellar: :any,                 arm64_monterey: "3a6181bd086d3f198bb15808bfe0687169372563f99f5cd6ba3eb9e2c8fa7f14"
+    sha256 cellar: :any,                 sonoma:         "ae3cea99908ffb44a94b8dc08c6603025f27c86941704f485d06c3fa37f5a906"
+    sha256 cellar: :any,                 ventura:        "b5c20722e21f66dc720b61db1f3586f265234de2bcf962cdd5274b6c30603028"
+    sha256 cellar: :any,                 monterey:       "ef7a4a3a493389d4c2c44993f884d3c2b6f982179ae41e12416ccf0488e040c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "41437ad26b2508be095f702622c597047a8c0c20a763a5a4fe4a16be5323fff0"
   end
 
   depends_on "cmake" => :build
