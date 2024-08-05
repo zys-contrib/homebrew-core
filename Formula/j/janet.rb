ffbf0178c3b77d68c7e7b57f8517a64a67f9dac9
Michael Camilleri <mike@inqk.net>
2024-08-05 13:56:06 +0900

    janet: improve stability of JPM and modules
    The current formula for Janet installs the Janet Project Manager
    ("JPM"). JPM is Janet's official mechanism for installing Janet modules.
    Janet modules are bundles of code that can be either libaries or
    executables. Libraries can be source code in Janet that is loaded by a
    Janet project directly or source code in other languages that is
    compiled to use Janet's C interface. Similarly, executables can be
    source code in Janet that is run as executable scripts or source code
    in other languages that is compiled to an executable binary on
    installation.
    
    Homebrew's formula installs JPM in the keg where Janet itself is
    installed. This means that JPM uses the `bin`, `lib` and `share/man`
    directories within the keg for modules that _it_ downloads. This is
    problematic for three reasons. First, it means that when Janet is
    updated, libraries installed previously seem to the user to have have
    disappeared. Second, executables are not on the user's PATH after
    installation unless the user add's the keg's `bin` directory to their
    path (which will in turn break if the respective keg is uninstalled).
    Third, man pages installed by JPM do not appear unless the user add's
    the keg's `share/man` directory to their manpath.
    
    As with other formulae that install package managers (e.g. Node,
    Python), a solution that is more stable across runtime updates is for
    the package manager to use one or more directories outside the keg.
    This commit does that by configuring JPM during the bootstrap phase to
    use the respective directories within `{HOMEBREW_PREFIX}`. The downside
    of this approach is that when Janet is uninstalled completely, the files
    and directories created by JPM remain. Nevertheless, this approach is
    preferred to the status quo.
    
    In addition to improving JPM stability, this commit also removes the
    build-time dependency on Meson and Ninja. Janet comes with a
    well-designed Makefile that works consistently across macOS and Linux
    without the need for other build tools.

--- Formula/j/janet.rb
+++ Formula/j/janet.rb
@@ -4,6 +4,7 @@ class Janet < Formula
   url "https://github.com/janet-lang/janet/archive/refs/tags/v1.35.2.tar.gz"
   sha256 "947dfdab6c1417c7c43efef2ecb7a92a3c339ce2135233fe88323740e6e7fab1"
   license "MIT"
+  revision 1
   head "https://github.com/janet-lang/janet.git", branch: "master"
 
   bottle do
@@ -17,30 +18,57 @@ class Janet < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "31581c3ce8ea833fe2a60d42ce0879093be588c37d4336910dc59f891aa40eba"
   end
 
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
-
   resource "jpm" do
     url "https://github.com/janet-lang/jpm/archive/refs/tags/v1.1.0.tar.gz"
     sha256 "337c40d9b8c087b920202287b375c2962447218e8e127ce3a5a12e6e47ac6f16"
   end
 
+  def syspath
+    HOMEBREW_PREFIX/"lib/janet"
+  end
+
   def install
-    system "meson", "setup", "build", *std_meson_args
-    cd "build" do
-      system "ninja"
-      system "ninja", "install"
-    end
+    # Replace lines in the Makefile that attempt to create the `syspath`
+    # directory (which is a directory outside the sandbox).
+    inreplace "Makefile", /^.*?\bmkdir\b.*?\$\(JANET_PATH\).*?$/, "# Line removed by Homebrew formula"
+
     ENV["PREFIX"] = prefix
+    ENV["JANET_BUILD"] = "\\\"homebrew\\\""
+    ENV["JANET_PATH"] = syspath
+
+    system "make"
+    system "make", "install"
+  end
+
+  def post_install
+    mkdir_p syspath unless syspath.exist?
+
     resource("jpm").stage do
+      ENV["PREFIX"] = prefix
+      ENV["JANET_BINPATH"] = HOMEBREW_PREFIX/"bin"
+      ENV["JANET_HEADERPATH"] = HOMEBREW_PREFIX/"include/janet"
+      ENV["JANET_LIBPATH"] = HOMEBREW_PREFIX/"lib"
+      ENV["JANET_MANPATH"] = HOMEBREW_PREFIX/"share/man/man1"
+      ENV["JANET_MODPATH"] = syspath
       system bin/"janet", "bootstrap.janet"
     end
   end
 
+  def caveats
+    <<~EOS
+      When uninstalling Janet, please delete the following manually:
+      - #{HOMEBREW_PREFIX}/lib/janet
+      - #{HOMEBREW_PREFIX}/bin/jpm
+      - #{HOMEBREW_PREFIX}/share/man/man1/jpm.1
+    EOS
+  end
+
   test do
-    assert_equal "12", shell_output("#{bin}/janet -e '(print (+ 5 7))'").strip
-    assert_predicate HOMEBREW_PREFIX/"bin/jpm", :exist?, "jpm must exist"
-    assert_predicate HOMEBREW_PREFIX/"bin/jpm", :executable?, "jpm must be executable"
-    assert_match prefix.to_s, shell_output("#{bin}/jpm show-paths")
+    janet = bin/"janet"
+    jpm = HOMEBREW_PREFIX/"bin/jpm"
+    assert_equal "12", shell_output("#{janet} -e '(print (+ 5 7))'").strip
+    assert_predicate jpm, :exist?, "jpm must exist"
+    assert_predicate jpm, :executable?, "jpm must be executable"
+    assert_match Regexp.new(HOMEBREW_PREFIX/"lib/janet".to_s), shell_output("#{jpm} show-paths")
   end
 end
