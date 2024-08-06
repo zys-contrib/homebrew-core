bf54d2a9c2ad754ff25716cfaade97006ae8d0aa
Michael Cho <michael@michaelcho.dev>
2024-08-05 23:52:12 -0400

    fceux: use latest `ffmpeg`

--- Formula/f/fceux.rb
+++ Formula/f/fceux.rb
@@ -2,7 +2,7 @@ class Fceux < Formula
   desc "All-in-one NES/Famicom Emulator"
   homepage "https://fceux.com/"
   license "GPL-2.0-only"
-  revision 2
+  revision 3
   head "https://github.com/TASEmulators/fceux.git", branch: "master"
 
   stable do
@@ -30,7 +30,7 @@ class Fceux < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
-  depends_on "ffmpeg@6"
+  depends_on "ffmpeg"
   depends_on "libarchive"
   depends_on "minizip"
   depends_on "qt"
