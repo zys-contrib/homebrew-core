66f43eff8f9a8a4780e9b05e1536564ff035fa67
Rui Chen <rui@chenrui.dev>
2024-08-06 17:18:48 -0400

    io: update cmake build
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    io: add glibc patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    io: fix rpath
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/io.rb
+++ Formula/i/io.rb
@@ -1,11 +1,17 @@
 class Io < Formula
   desc "Small prototype-based programming language"
   homepage "http://iolanguage.com/"
-  url "https://github.com/IoLanguage/io/archive/refs/tags/2017.09.06.tar.gz"
-  sha256 "9ac5cd94bbca65c989cd254be58a3a716f4e4f16480f0dc81070457aa353c217"
   license "BSD-3-Clause"
   revision 1
   head "https://github.com/IoLanguage/io.git", branch: "master"
+
+  stable do
+    url "https://github.com/IoLanguage/io/archive/refs/tags/2017.09.06.tar.gz"
+    sha256 "9ac5cd94bbca65c989cd254be58a3a716f4e4f16480f0dc81070457aa353c217"
+
+    # build patch for sysctl.h as glibc 2.32 removed <sys/sysctl.h>
+    patch :DATA
+  end
 
   bottle do
     sha256 sonoma:       "18dfd552cc463cf9be8dab9be3c3c4704f3996ec9a9e66d45cfcde7c531d88a5"
@@ -36,13 +42,15 @@ class Io < Formula
                                   "#add_subdirectory(addons)"
     end
 
-    mkdir "buildroot" do
-      system "cmake", "..", "-DCMAKE_DISABLE_FIND_PACKAGE_ODE=ON",
-                            "-DCMAKE_DISABLE_FIND_PACKAGE_Theora=ON",
-                            *std_cmake_args
-      system "make"
-      system "make", "install"
-    end
+    args = %w[
+      -DCMAKE_DISABLE_FIND_PACKAGE_ODE=ON
+      -DCMAKE_DISABLE_FIND_PACKAGE_Theora=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
@@ -53,3 +61,18 @@ class Io < Formula
     assert_equal "it works!\n", shell_output("#{bin}/io test.io")
   end
 end
+
+__END__
+diff --git a/libs/iovm/source/IoSystem.c b/libs/iovm/source/IoSystem.c
+index a6234f7..af3a975 100755
+--- a/libs/iovm/source/IoSystem.c
++++ b/libs/iovm/source/IoSystem.c
+@@ -22,7 +22,7 @@ Contains methods related to the IoVM.
+ #if defined(__NetBSD__) || defined(__OpenBSD__)
+ # include <sys/param.h>
+ #endif
+-#ifndef __CYGWIN__
++#if defined(HAVE_SYS_SYSCTL_H) && !defined(__GLIBC__)
+ # include <sys/sysctl.h>
+ #endif
+ #endif
