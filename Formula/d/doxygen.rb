6d7f9227b7fbecdc63a7be3eae95bd9a8df72acb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-07 09:30:33 +0000

    doxygen 1.12.0

--- Formula/d/doxygen.rb
+++ Formula/d/doxygen.rb
@@ -1,9 +1,9 @@
 class Doxygen < Formula
   desc "Generate documentation for several programming languages"
   homepage "https://www.doxygen.nl/"
-  url "https://doxygen.nl/files/doxygen-1.11.0.src.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.11.0/doxygen-1.11.0.src.tar.gz"
-  sha256 "c9edfdf8c5f3e8bee0c4c967850caead27099883ee7ff8b11044e6d63faf3607"
+  url "https://doxygen.nl/files/doxygen-1.12.0.src.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.12.0/doxygen-1.12.0.src.tar.gz"
+  sha256 "a3a3dba2018ef409d83d81a2fc42a0d19bdbe087252ef342bf214b51b8b01634"
   license "GPL-2.0-only"
   head "https://github.com/doxygen/doxygen.git", branch: "master"
 
