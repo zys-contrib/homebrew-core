447288b5d72eae11f5e051ab3acdde18ea067dee
Patrick Linnane <patrick@linnane.io>
2024-08-08 22:31:15 +0000

    wcurl: update 2024.07.10 bottle.

--- Formula/w/wcurl.rb
+++ Formula/w/wcurl.rb
@@ -7,13 +7,8 @@ class Wcurl < Formula
   head "https://github.com/curl/wcurl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d7c86c3062340c46b0f34fe4c17635d8a454524bf2f8fd86afc7efeb066686aa"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d7c86c3062340c46b0f34fe4c17635d8a454524bf2f8fd86afc7efeb066686aa"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d7c86c3062340c46b0f34fe4c17635d8a454524bf2f8fd86afc7efeb066686aa"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d7c86c3062340c46b0f34fe4c17635d8a454524bf2f8fd86afc7efeb066686aa"
-    sha256 cellar: :any_skip_relocation, ventura:        "d7c86c3062340c46b0f34fe4c17635d8a454524bf2f8fd86afc7efeb066686aa"
-    sha256 cellar: :any_skip_relocation, monterey:       "d7c86c3062340c46b0f34fe4c17635d8a454524bf2f8fd86afc7efeb066686aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "32103a14a42d6532c6a4b3d4558c3ebd0b07cffbfc761ef83e5300b38f18c23c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "af8adb8368a8844d3552b6b6efed89c10d4bb6b03220dab7bdc73b658422e04b"
   end
 
   depends_on "curl"
