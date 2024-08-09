f336956e84f013c781ca1fae614f05809543c97b
Rui Chen <rui@chenrui.dev>
2024-08-09 16:08:44 -0400

    serveit: fix linux build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/serveit.rb
+++ Formula/s/serveit.rb
@@ -11,8 +11,30 @@ class Serveit < Formula
     sha256 cellar: :any_skip_relocation, all: "e17a21fd5705de1b2e96656ad703f51c8f1781601f295ff7028cfc859bd0ca44"
   end
 
+  uses_from_macos "ruby"
+
+  # webrick is needed for ruby 3.0+ (as it is not part of the default gems)
+  # upstream report, https://github.com/garybernhardt/serveit/issues/13
+  resource "webrick" do
+    on_linux do
+      url "https://rubygems.org/downloads/webrick-1.8.1.gem"
+      sha256 "19411ec6912911fd3df13559110127ea2badd0c035f7762873f58afc803e158f"
+    end
+  end
+
   def install
     bin.install "serveit"
+
+    if OS.linux?
+      ENV["GEM_HOME"] = libexec
+      resources.each do |r|
+        r.fetch
+        system "gem", "install", r.cached_download, "--no-document",
+                      "--install-dir", libexec
+      end
+
+      bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    end
   end
 
   test do
