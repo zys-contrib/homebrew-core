7fd40e216bf6099bbb778fe00fc325b55f3b4783
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-10 15:26:16 +0000

    easy-rsa: update 3.2.0 bottle.

--- Formula/e/easy-rsa.rb
+++ Formula/e/easy-rsa.rb
@@ -7,13 +7,14 @@ class EasyRsa < Formula
   head "https://github.com/OpenVPN/easy-rsa.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d5efd48b9124a25e409090f477abd19764aa5fa9c88c4e267e2e6466fdb5aaaf"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "376a1e6c33f1c9220982516223a430725527066c1aa96498bbc20bef53e8063e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a97a074340e6adb1af4c8d31136a8d666f1c70a1a1c350f4c6722370ad86568"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a813c5e2dd29c13362011203e37417dae5d977f89bb82d2dbf541a55fd9aea90"
-    sha256 cellar: :any_skip_relocation, ventura:        "a99d0ee6a849da660390dd73893d81c26a83d8e7e734072c463b3c315ce5afaa"
-    sha256 cellar: :any_skip_relocation, monterey:       "8d1b5c8dc4bffbe8c60d5b871d941e411b618de54bb9c30e925867aa4beea900"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f1b45cfc4e2dc2ef886d86e1ba8b2eeae91ef2239f60cdb3af4caa2acd35015"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0c548341d3cf5900a1565f5c0464a082ed5f9f3c062025b97731804df0ef7a31"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0c548341d3cf5900a1565f5c0464a082ed5f9f3c062025b97731804df0ef7a31"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0c548341d3cf5900a1565f5c0464a082ed5f9f3c062025b97731804df0ef7a31"
+    sha256 cellar: :any_skip_relocation, sonoma:         "05a1c069fee5a14aef5bd45fd6a64f265197dc12671e3f7739b4403d26a36a10"
+    sha256 cellar: :any_skip_relocation, ventura:        "05a1c069fee5a14aef5bd45fd6a64f265197dc12671e3f7739b4403d26a36a10"
+    sha256 cellar: :any_skip_relocation, monterey:       "05a1c069fee5a14aef5bd45fd6a64f265197dc12671e3f7739b4403d26a36a10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0c548341d3cf5900a1565f5c0464a082ed5f9f3c062025b97731804df0ef7a31"
   end
 
   depends_on "openssl@3"
