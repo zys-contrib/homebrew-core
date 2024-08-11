69f136770e159e315ff9980b0cf5b1c5de0b71fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-11 17:37:39 +0000

    gfxutil: add 1.84b bottle.

--- Formula/g/gfxutil.rb
+++ Formula/g/gfxutil.rb
@@ -7,6 +7,15 @@ class Gfxutil < Formula
   license :public_domain
   head "https://github.com/acidanthera/gfxutil.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c0bcf7f6998efc819378c5b42de7ef889877a8dfee552c50b19b2e936ba90730"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0346234f7ca528ca436186c2b4aa58a398756e6ac6ebaaa3329ba95b915835b2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "884219668e6a3456f9060d164c912368804a2a3f07787276933ea231bbc04a27"
+    sha256 cellar: :any_skip_relocation, sonoma:         "a5ab4c2ec89a9792213e18bfcd7527ebb520be4cf9d857f5cdddf0c28c26c298"
+    sha256 cellar: :any_skip_relocation, ventura:        "c4e9bae4270a0855f7c5ca16fb69afc795c967ac09eea19a4114695275c93334"
+    sha256 cellar: :any_skip_relocation, monterey:       "2fbaff1968522b5bbfd4c0e040f20da9c366a98a3e760403067edbad6a4d5ad1"
+  end
+
   depends_on xcode: :build
   depends_on :macos
 
