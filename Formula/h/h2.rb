ae7527090c8d2e078045459c6ce484e89a1725e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-11 20:21:42 +0000

    h2: update 2.3.232 bottle.

--- Formula/h/h2.rb
+++ Formula/h/h2.rb
@@ -7,13 +7,7 @@ class H2 < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "89ab4c6adafc53246cb073e24daba248bcf4bb5ef6cee3cf703a1021b26a485c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "89ab4c6adafc53246cb073e24daba248bcf4bb5ef6cee3cf703a1021b26a485c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "89ab4c6adafc53246cb073e24daba248bcf4bb5ef6cee3cf703a1021b26a485c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "7348c41c4b93988cb9dfbe4b2458ed928d9308b49a53883127f2b24f106fdd97"
-    sha256 cellar: :any_skip_relocation, ventura:        "7348c41c4b93988cb9dfbe4b2458ed928d9308b49a53883127f2b24f106fdd97"
-    sha256 cellar: :any_skip_relocation, monterey:       "89ab4c6adafc53246cb073e24daba248bcf4bb5ef6cee3cf703a1021b26a485c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05377d69788c04d8c4ad7e329f1594f53c061d5690c522d1e272d7d573bea389"
+    sha256 cellar: :any_skip_relocation, all: "5fd92e04e213f01b78acb580651cf0ef22ccc29ea925e903eed2ddf4f907254b"
   end
 
   depends_on "openjdk"
