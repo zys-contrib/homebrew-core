196b8e964aab50593fe994ab6b741f83e375d29b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-12 15:26:15 +0000

    cargo-binstall: update livecheck
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cargo-binstall.rb
+++ Formula/c/cargo-binstall.rb
@@ -6,9 +6,12 @@ class CargoBinstall < Formula
   license "GPL-3.0-only"
   head "https://github.com/cargo-bins/cargo-binstall.git", branch: "main"
 
+  # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
+  # labeled as "pre-release" on GitHub before the version is released, so it's
+  # necessary to use the `GithubLatest` strategy.
   livecheck do
     url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_latest
   end
 
   bottle do
