0f667421370c2bd03c038190301e8fd2b84ad98e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-13 03:54:14 +0000

    devcontainer: update 0.68.0 bottle.

--- Formula/d/devcontainer.rb
+++ Formula/d/devcontainer.rb
@@ -6,14 +6,13 @@ class Devcontainer < Formula
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0aa24a84e36726b9f575dcd248e8fb4e5f22130a0fdb2b996c856efb9da96b77"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0aa24a84e36726b9f575dcd248e8fb4e5f22130a0fdb2b996c856efb9da96b77"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0aa24a84e36726b9f575dcd248e8fb4e5f22130a0fdb2b996c856efb9da96b77"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5c119586215d89a3a1f09b4f98345e53716d254f74c89c4e56971f6b26f701f5"
-    sha256 cellar: :any_skip_relocation, ventura:        "5c119586215d89a3a1f09b4f98345e53716d254f74c89c4e56971f6b26f701f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "5c119586215d89a3a1f09b4f98345e53716d254f74c89c4e56971f6b26f701f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9136d4f9c0c69288c6a39abc420336b09a45e91cc4dd4ad1cc2159b77bf8978b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c316392bdb7ddd56e0e86a339823f02c7950f60f7c504de56fbcd82d8b4f0ac4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c316392bdb7ddd56e0e86a339823f02c7950f60f7c504de56fbcd82d8b4f0ac4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c316392bdb7ddd56e0e86a339823f02c7950f60f7c504de56fbcd82d8b4f0ac4"
+    sha256 cellar: :any_skip_relocation, sonoma:         "422f465804f4bdc367b8852aa8fc3807c20ba7cbba5b4280a7b7c552a1c4aefb"
+    sha256 cellar: :any_skip_relocation, ventura:        "422f465804f4bdc367b8852aa8fc3807c20ba7cbba5b4280a7b7c552a1c4aefb"
+    sha256 cellar: :any_skip_relocation, monterey:       "422f465804f4bdc367b8852aa8fc3807c20ba7cbba5b4280a7b7c552a1c4aefb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c316392bdb7ddd56e0e86a339823f02c7950f60f7c504de56fbcd82d8b4f0ac4"
   end
 
   depends_on "node"
