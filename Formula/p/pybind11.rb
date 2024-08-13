9323dac99786b9f62ff140d6819ff7aac4028144
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-13 18:56:54 +0000

    pybind11 2.13.3

--- Formula/p/pybind11.rb
+++ Formula/p/pybind11.rb
@@ -1,8 +1,8 @@
 class Pybind11 < Formula
   desc "Seamless operability between C++11 and Python"
   homepage "https://github.com/pybind/pybind11"
-  url "https://github.com/pybind/pybind11/archive/refs/tags/v2.13.2.tar.gz"
-  sha256 "50eebef369d28f07ce1fe1797f38149e5928817be8e539239f2aadfd95b227f3"
+  url "https://github.com/pybind/pybind11/archive/refs/tags/v2.13.3.tar.gz"
+  sha256 "6e7a84ec241544f2f5e30c7a82c09c81f0541dd14e9d9ef61051e07105f9c445"
   license "BSD-3-Clause"
 
   livecheck do
