510c7a39aaa73a9d998b4d7768e0971d2f3e3e8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-13 11:15:17 +0000

    dstack: update 0.18.10 bottle.

--- Formula/d/dstack.rb
+++ Formula/d/dstack.rb
@@ -8,14 +8,13 @@ class Dstack < Formula
   license "MPL-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sonoma:   "cf62fc577ee65c8a7d345b3b9a5b038ba3cbc89652496d61fb407a252c66d54e"
-    sha256 cellar: :any,                 arm64_ventura:  "0a191d09a15b56f8a817aac3c0a893541d8c8ef3d072fe2d713fbcc66cc4c780"
-    sha256 cellar: :any,                 arm64_monterey: "bbadb5cc4be2bdb68dab46c7d402e2c3ddf70d39dd7ae8a8fa2fb5538880c7ac"
-    sha256 cellar: :any,                 sonoma:         "a6c14a4ccfc3b87fe31c0aae78134488aa00140d7d2c9a7401b3de968b614900"
-    sha256 cellar: :any,                 ventura:        "6244e030d7d929f433ca14dba954ec5ead901c60856d64d2c781a3749b9abdaf"
-    sha256 cellar: :any,                 monterey:       "e692770bef324b8a851787723d8b41bb6dd459d88c7def848a0e1dd5ec443ac5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b9aeb4354f80a4229202052e9ac296969bbfe4f2b4305e020d4cdf78646705d"
+    sha256 cellar: :any,                 arm64_sonoma:   "3a91b17452ecf353297a65bf607920e7455bf496bab8a707a67a615789c66fe6"
+    sha256 cellar: :any,                 arm64_ventura:  "66be5b52c4b3cfb0377cf14c26befa526a54495e2186968fcdabfb65f13bc07d"
+    sha256 cellar: :any,                 arm64_monterey: "ffba1729045839536728692d93d032fd499815227346cd379fa556c3f1d1cc09"
+    sha256 cellar: :any,                 sonoma:         "00000586cc5b818b813d58dea31572cc27cb24e65a46c25d0972fd8e34dde25a"
+    sha256 cellar: :any,                 ventura:        "0f752f6ad33e2a9beab8fae41df50cbbe14783850eb33559fc50c9a0a8074bad"
+    sha256 cellar: :any,                 monterey:       "78e55e01c08658125877153a09d47a8df0b3e6643dfd2a4d7fc16afa56f62d42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "776af4627c1d35abaa6106064279ee95b000f67f72cf1ce21da3a6ad4d58fb81"
   end
 
   # `pkg-config` and `rust` are for bcrypt.
