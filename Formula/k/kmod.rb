935c7e4bbc85f5dd0f534aae2a657892ce7f8fa8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-13 18:43:31 +0000

    kmod 33
    kmod: needs scdoc for manpage build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/kmod.rb
+++ Formula/k/kmod.rb
@@ -1,8 +1,8 @@
 class Kmod < Formula
   desc "Linux kernel module handling"
   homepage "https://git.kernel.org/pub/scm/utils/kernel/kmod/kmod.git"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/kernel/kmod/kmod-32.tar.xz"
-  sha256 "630ed0d92275a88cb9a7bf68f5700e911fdadaf02e051cf2e4680ff8480bd492"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/kernel/kmod/kmod-33.tar.xz"
+  sha256 "dc768b3155172091f56dc69430b5481f2d76ecd9ccb54ead8c2540dbcf5ea9bc"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
 
   livecheck do
@@ -14,12 +14,11 @@ class Kmod < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "92f97941033e512c0489249f4cb7573c45b9424286daedf688f6634892f01834"
   end
 
+  depends_on "scdoc" => :build
   depends_on :linux
 
   def install
-    system "./configure", "--with-bashcompletiondir=#{bash_completion}",
-      *std_configure_args, "--disable-silent-rules"
-    system "make"
+    system "./configure", "--with-bashcompletiondir=#{bash_completion}", "--disable-silent-rules", *std_configure_args
     system "make", "install"
 
     bin.install_symlink "kmod" => "depmod"
