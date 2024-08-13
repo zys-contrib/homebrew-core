ece8b2deadaba8a6c9c07ee8b4ce093175a98bfa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-13 18:54:04 +0000

    openshift-cli 4.16.6

--- Formula/o/openshift-cli.rb
+++ Formula/o/openshift-cli.rb
@@ -1,7 +1,7 @@
 class OpenshiftCli < Formula
   desc "OpenShift command-line interface tools"
   homepage "https://www.openshift.com/"
-  url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.16.5/openshift-client-src.tar.gz"
+  url "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.16.6/openshift-client-src.tar.gz"
   sha256 "c0a3da890c60359c9666505cca29c17470800bb0fc6faf02de106de7dc4ca657"
   license "Apache-2.0"
   head "https://github.com/openshift/oc.git", shallow: false, branch: "master"
