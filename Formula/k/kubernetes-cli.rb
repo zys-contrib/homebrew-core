177a0a917d912c7e05059c0a725b1461159e85e6
Branch Vincent <branchevincent@gmail.com>
2024-08-13 19:43:08 -0700

    kubernetes-cli 1.31.0

--- Formula/k/kubernetes-cli.rb
+++ Formula/k/kubernetes-cli.rb
@@ -2,8 +2,8 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/docs/reference/kubectl/"
   url "https://github.com/kubernetes/kubernetes.git",
-      tag:      "v1.30.3",
-      revision: "6fc0a69044f1ac4c13841ec4391224a2df241460"
+      tag:      "v1.31.0",
+      revision: "9edcffcde5595e8a5b1a35f88c421764e575afce"
   license "Apache-2.0"
   head "https://github.com/kubernetes/kubernetes.git", branch: "master"
 
