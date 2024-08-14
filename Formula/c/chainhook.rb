742e4a0c984bba416fc053afd496ffee245f46e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-14 03:26:38 +0000

    chainhook: update 1.8.0 bottle.

--- Formula/c/chainhook.rb
+++ Formula/c/chainhook.rb
@@ -7,13 +7,13 @@ class Chainhook < Formula
   head "https://github.com/hirosystems/chainhook.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "db1c0014de5d08c85bc9f780772ff6b489eddf91be6dd87c74d00a798afc9677"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "464a2c4e1b2758039c5b0ffc10ace9cee8c60322c29a94e0ef3b3542ecdb78a4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a5c18ed13814ca6ea90de8fa107b4b0fd45466d0384416c85d38c0f572a030cc"
-    sha256 cellar: :any_skip_relocation, sonoma:         "9ae8f567262e46b68cb509eb47e9bc9fca23860e8ef6e3c819eb76eb8096a0b2"
-    sha256 cellar: :any_skip_relocation, ventura:        "c6b317d43cfa5b797868fc122485e2722d0f31ef8ee34a0a9c8b55dbd89f2a8e"
-    sha256 cellar: :any_skip_relocation, monterey:       "e91426547d1206929b68461712958dc340fb1bff182de299f873c65e9e90ebb9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3697182a2f3d066cdc0ac0b70e318337d2462cc6126524d4f74a4f973e15e8c4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d84022e09a8266a52686ed6c8ecda900051f13a4cc848daa0f6c518337cbb3ed"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0a30eb3a6b72c2c0cdd41d65f55f8b4317d1261b07fbcceca6363b39bef1ed7f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a0e6da7d2520a8954d52270dda75248be81de3ff667ce6489b76a75d94d20ee7"
+    sha256 cellar: :any_skip_relocation, sonoma:         "2e99210c74407049e8ea75816c0fb3b1ca111ca056c2f95ee923c8202dedb7cc"
+    sha256 cellar: :any_skip_relocation, ventura:        "2d5f0d818bd4abb96efa88b7200ea9f3532169eca622e07f61d50a9fc7e4f92a"
+    sha256 cellar: :any_skip_relocation, monterey:       "213dea4d00349141619d2682bee6191d18aaeeb5aa99340599f54dab75abb521"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1baa3948cbd9735f9249af29ff40e21d005185c70dd0f2a023cc733e298505f3"
   end
 
   depends_on "rust" => :build
