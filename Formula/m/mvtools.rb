2b42dafe3cc89e308130a3b3a43b6c9093084f53
Rui Chen <rui@chenrui.dev>
2024-08-15 10:20:26 -0400

    mvtools 24
    autobump: add mvtools
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    mvtools: remove build patch and update license
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mvtools.rb
+++ Formula/m/mvtools.rb
@@ -1,10 +1,9 @@
 class Mvtools < Formula
   desc "Filters for motion estimation and compensation"
   homepage "https://github.com/dubhater/vapoursynth-mvtools"
-  url "https://github.com/dubhater/vapoursynth-mvtools/archive/refs/tags/v23.tar.gz"
-  sha256 "3b5fdad2b52a2525764510a04af01eab3bc5e8fe6a02aba44b78955887a47d44"
-  license "GPL-2.0"
-  revision 1
+  url "https://github.com/dubhater/vapoursynth-mvtools/archive/refs/tags/v24.tar.gz"
+  sha256 "ccff47f4ea25aa13b13fabd5cf38dd0be1ceda10d9ad6b52bd42ecf9d6eb24ad"
+  license "GPL-2.0-or-later"
   head "https://github.com/dubhater/vapoursynth-mvtools.git", branch: "master"
 
   bottle do
@@ -25,16 +24,13 @@ class Mvtools < Formula
   depends_on "libtool" => :build
   depends_on "nasm" => :build
   depends_on "pkg-config" => :build
+
   depends_on "fftw"
   depends_on "vapoursynth"
 
-  # Fixes build issues on arm
-  # https://github.com/dubhater/vapoursynth-mvtools/pull/55
-  patch :DATA
-
   def install
     system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args.reject { |s| s["--disable-debug"] }
     system "make", "install"
   end
 
@@ -59,51 +55,3 @@ class Mvtools < Formula
     system python, "-c", script
   end
 end
-
-__END__
---- a/configure.ac
-+++ b/configure.ac
-@@ -54,7 +54,7 @@ AS_CASE(
-   [i?86],         [BITS="32" NASMFLAGS="$NASMFLAGS -DARCH_X86_64=0" X86="true"],
-   [x86_64|amd64], [BITS="64" NASMFLAGS="$NASMFLAGS -DARCH_X86_64=1 -DPIC" X86="true"],
-   [powerpc*],     [PPC="true"],
--  [arm*],         [ARM="true"],
-+  [arm*|aarch*],  [ARM="true"],
-   [AC_MSG_ERROR([Unknown host CPU: $host_cpu.])]
- )
- 
---- a/src/SADFunctions.cpp
-+++ b/src/SADFunctions.cpp
-@@ -646,7 +646,7 @@ static unsigned int Satd_C(const uint8_t *pSrc, intptr_t nSrcPitch, const uint8_
-     }
- }
- 
--
-+#if defined(MVTOOLS_X86)
- template <unsigned nBlkWidth, unsigned nBlkHeight, InstructionSets opt>
- static unsigned int Satd_SIMD(const uint8_t *pSrc, intptr_t nSrcPitch, const uint8_t *pRef, intptr_t nRefPitch) {
-     const unsigned partition_width = 16;
-@@ -676,7 +676,7 @@ static unsigned int Satd_SIMD(const uint8_t *pSrc, intptr_t nSrcPitch, const uin
- 
-     return sum;
- }
--
-+#endif
- 
- #if defined(MVTOOLS_X86)
- #define SATD_X264_U8_MMX(width, height) \
-@@ -753,12 +753,14 @@ static const std::unordered_map<uint32_t, SADFunction> satd_functions = {
-     SATD_X264_U8_AVX2(8, 8)
-     SATD_X264_U8_AVX2(16, 8)
-     SATD_X264_U8_AVX2(16, 16)
-+    #if defined(MVTOOLS_X86)
-     SATD_U8_SIMD(32, 16)
-     SATD_U8_SIMD(32, 32)
-     SATD_U8_SIMD(64, 32)
-     SATD_U8_SIMD(64, 64)
-     SATD_U8_SIMD(128, 64)
-     SATD_U8_SIMD(128, 128)
-+    #endif
- };
- 
- SADFunction selectSATDFunction(unsigned width, unsigned height, unsigned bits, int opt, unsigned cpu) {
