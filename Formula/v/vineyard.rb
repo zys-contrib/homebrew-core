9aee40ad58764c1a7bcc62921663173ce95b8b45
Michael Cho <michael@michaelcho.dev>
2024-08-15 18:33:06 -0400

    vineyard: revision bump (boost 1.86.0)

--- Formula/v/vineyard.rb
+++ Formula/v/vineyard.rb
@@ -6,7 +6,7 @@ class Vineyard < Formula
   url "https://github.com/v6d-io/v6d/releases/download/v0.23.2/v6d-0.23.2.tar.gz"
   sha256 "2a2788ed77b9459477b3e90767a910e77e2035a34f33c29c25b9876568683fd4"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256                               arm64_sonoma:   "505a9a4e63d54c9b9bb323193d98f6ce81944a545a18615bc7031b600a6e605f"
