bf494ab49b91747581b893a400e52ec53ca5a36e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-15 11:13:27 +0000

    aliyun-cli: update 3.0.219 bottle.

--- Formula/a/aliyun-cli.rb
+++ Formula/a/aliyun-cli.rb
@@ -7,13 +7,13 @@ class AliyunCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b7f02de748c5acbed9b561067b356147e37c995ca34e875a314e39b250196e4d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "11e25a46acd7b4530b6e08571af5c9b5cd9635a22a3bc28afd5623a8baa0b47d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "900d4224627cc2cc732934efc26162066fcfd8f5479217d26825d2720626237f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "32164c5ac27f007aa3f2acf046cf3fffa9dbd597111344744f108f41406ee964"
-    sha256 cellar: :any_skip_relocation, ventura:        "2225849e8f274bd6cc67a6b76d9b83d6df4801f0ead1c018db9f3c1e49939b51"
-    sha256 cellar: :any_skip_relocation, monterey:       "c91f4e0946f282962043b112a1e6427bda0ecad566159d5f72efc8f46962ab41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1efa6ed832c1ac329957557f22f68a868a7d280824f66d2d1c63259b3fe517c0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "7dbb6db84238cc5c6063f85aae224da866ae62176aa23fe4086cf7e83c864379"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9293eac12024d5ecd3780596cd7f0cd0b3edb5e848e6ed7e45657a9f06d5bebb"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5ba589e769fe20030eef5139855e3d5e4d784682a873044d686bd3b4940e60f1"
+    sha256 cellar: :any_skip_relocation, sonoma:         "8d28f94f65c9f9f90fdc35217786f04251aba6e0d1d0037f8c180e8d20aca389"
+    sha256 cellar: :any_skip_relocation, ventura:        "1e0849797b6019a88780657a86d5dac0b1ef9bd1dbfb669e76b2f5c3a2077826"
+    sha256 cellar: :any_skip_relocation, monterey:       "564d66e7c729ab5611cb6a8ce7460ee2aaf5035fa9603488f376a1ace2cecc6e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a6c17ba79cc71f7e9cde1dd70e162df5a784e16d88d21d664d1ead418a74cbc1"
   end
 
   depends_on "go" => :build
