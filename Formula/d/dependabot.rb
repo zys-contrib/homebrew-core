6d8b8475f537e0a89e8a77ebe464033f2d277f69
Luke Carrier <luke@carrier.family>
2024-08-15 10:08:04 +0100

    dependabot 1.54.0 (new formula)

---
+++ Formula/d/dependabot.rb
@@ -0,0 +1,27 @@
+class Dependabot < Formula
+  desc "Tool for testing and debugging Dependabot update jobs"
+  homepage "https://github.com/dependabot/cli"
+  url "https://github.com/dependabot/cli/archive/refs/tags/v1.54.0.tar.gz"
+  sha256 "ae496c65e4f947cca1251c9f786fbcc1e5e1a079af7fee08b893265e69d39295"
+  license "MIT"
+  head "https://github.com/dependabot/cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/dependabot/cli/cmd/dependabot/internal/cmd.version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/dependabot"
+
+    generate_completions_from_executable(bin/"dependabot", "completion")
+  end
+
+  test do
+    ENV["DOCKER_HOST"] = "unix://#{testpath}/invalid.sock"
+    assert_match("dependabot version #{version}", shell_output("#{bin}/dependabot --version"))
+    output = shell_output("#{bin}/dependabot update bundler Homebrew/homebrew 2>&1", 1)
+    assert_match("Cannot connect to the Docker daemon", output)
+  end
+end
