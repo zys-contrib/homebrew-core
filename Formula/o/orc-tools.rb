f7a085bbfa72ea844f2163b64e66e6c143d04c58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-15 19:42:18 +0000

    orc-tools: update 2.0.2 bottle.

--- Formula/o/orc-tools.rb
+++ Formula/o/orc-tools.rb
@@ -11,13 +11,7 @@ class OrcTools < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e9b07c22548e70fa2f9d41569909ad0616117efe6e2f9345843e97ab52be3de6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a35bdecc2b37467a1dcd06920730cd4f51b1cfd0a771db1386a899e183ab8d04"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e0e91b1e8e34d21643bd341f65cd5118400a8d5bc59687711f4968c5f83650ef"
-    sha256 cellar: :any_skip_relocation, sonoma:         "1edc465ca38c8f9613c8b43db03a741de8ba4973c1d5d3efb1202441215ae848"
-    sha256 cellar: :any_skip_relocation, ventura:        "8ac41d97b85a5bbbbf90c794cf8b0368b7618ffc7c9218309c81e552e9f0d04e"
-    sha256 cellar: :any_skip_relocation, monterey:       "0c2c3778d4d6ad01863c8b957425b722f0ac4f33fa9403c45b371f28e8eea6eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae202cc972ab42ec26b9d83174aaf643198f06c727914e9b8ae6c55d78ec7afc"
+    sha256 cellar: :any_skip_relocation, all: "4a902d30ecf17f68855819623ed89a239b4b59cb1bb3d0fcd9ca452569396730"
   end
 
   depends_on "openjdk"
