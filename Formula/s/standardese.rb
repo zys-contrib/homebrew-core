eb46a347f2dcdfaecffad4209c98df8cbeb2637f
Michael Cho <michael@michaelcho.dev>
2024-08-15 18:33:02 -0400

    standardese: revision bump (boost 1.86.0)

--- Formula/s/standardese.rb
+++ Formula/s/standardese.rb
@@ -3,7 +3,7 @@ class Standardese < Formula
   homepage "https://standardese.github.io"
   # TODO: use resource blocks for vendored deps
   license "MIT"
-  revision 18
+  revision 19
   head "https://github.com/standardese/standardese.git", branch: "master"
 
   # Remove stable block when patch is no longer needed.
