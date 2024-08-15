75193c1d7e8345c8a45a78de631fac7c69324d3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-15 18:26:41 +0000

    buildkit 0.15.2

--- Formula/b/buildkit.rb
+++ Formula/b/buildkit.rb
@@ -2,8 +2,8 @@ class Buildkit < Formula
   desc "Concurrent, cache-efficient, and Dockerfile-agnostic builder toolkit"
   homepage "https://github.com/moby/buildkit"
   url "https://github.com/moby/buildkit.git",
-      tag:      "v0.15.1",
-      revision: "979542e90f2cb38077c808e0867d8d2c16ed10b8"
+      tag:      "v0.15.2",
+      revision: "9e14164a1099d3e41b58fc879cbdd6f2b2edb04e"
   license "Apache-2.0"
   head "https://github.com/moby/buildkit.git", branch: "master"
 
