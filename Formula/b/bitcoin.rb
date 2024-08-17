d19141e29b3d23f35981597b4def4a7e1a1fef80
Michael Cho <michael@michaelcho.dev>
2024-08-16 21:23:06 -0400

    bitcoin: resource bdb4 so that the formula can be retired
    All other usage was moved to `berkeley-db@5`, which is the default used
    by major Linux distros.

--- Formula/b/bitcoin.rb
+++ Formula/b/bitcoin.rb
@@ -3,7 +3,12 @@ class Bitcoin < Formula
   homepage "https://bitcoincore.org/"
   url "https://bitcoincore.org/bin/bitcoin-core-27.1/bitcoin-27.1.tar.gz"
   sha256 "0c1051fd921b8fae912f5c2dfd86b085ab45baa05cd7be4585b10b4d1818f3da"
-  license "MIT"
+  license all_of: [
+    "MIT",
+    "BSD-3-Clause", # src/crc32c, src/leveldb
+    "BSL-1.0", # src/tinyformat.h
+    "Sleepycat", # resource("bdb")
+  ]
   head "https://github.com/bitcoin/bitcoin.git", branch: "master"
 
   livecheck do
@@ -23,13 +28,9 @@ class Bitcoin < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "boost" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  # berkeley db should be kept at version 4
-  # https://github.com/bitcoin/bitcoin/blob/master/doc/build-osx.md
-  # https://github.com/bitcoin/bitcoin/blob/master/doc/build-unix.md
-  depends_on "berkeley-db@4"
-  depends_on "boost"
   depends_on "libevent"
   depends_on macos: :big_sur
   depends_on "miniupnpc"
@@ -46,6 +47,26 @@ class Bitcoin < Formula
     cause "Requires C++ 20"
   end
 
+  # berkeley db should be kept at version 4
+  # https://github.com/bitcoin/bitcoin/blob/master/doc/build-osx.md
+  # https://github.com/bitcoin/bitcoin/blob/master/doc/build-unix.md
+  resource "bdb" do
+    url "https://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz"
+    sha256 "12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef"
+
+    # Fix build with recent clang
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/4c55b1/berkeley-db%404/clang.diff"
+      sha256 "86111b0965762f2c2611b302e4a95ac8df46ad24925bbb95a1961542a1542e40"
+    end
+    # Fix -flat_namespace being used on Big Sur and later.
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+      sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+      directory "dist"
+    end
+  end
+
   # Skip two tests that currently fail in the brew CI
   patch do
     url "https://github.com/fanquake/bitcoin/commit/9b03fb7603709395faaf0fac409465660bbd7d81.patch?full_index=1"
@@ -53,10 +74,30 @@ class Bitcoin < Formula
   end
 
   def install
+    # https://github.com/bitcoin/bitcoin/blob/master/doc/build-unix.md#berkeley-db
+    # https://github.com/bitcoin/bitcoin/blob/master/depends/packages/bdb.mk
+    resource("bdb").stage do
+      with_env(CFLAGS: ENV.cflags) do
+        # Fix compile with newer Clang
+        ENV.append "CFLAGS", "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1200
+        # BerkeleyDB requires you to build everything from the build_unix subdirectory
+        cd "build_unix" do
+          system "../dist/configure", "--disable-replication",
+                                      "--disable-shared",
+                                      "--enable-cxx",
+                                      *std_configure_args(prefix: buildpath/"bdb")
+          system "make", "libdb_cxx-4.8.a", "libdb-4.8.a"
+          system "make", "install_lib", "install_include"
+        end
+      end
+    end
+
     system "./autogen.sh"
-    system "./configure", *std_configure_args,
-                          "--disable-silent-rules",
-                          "--with-boost-libdir=#{Formula["boost"].opt_lib}"
+    system "./configure", "--disable-silent-rules",
+                          "--with-boost-libdir=#{Formula["boost"].opt_lib}",
+                          "BDB_LIBS=-L#{buildpath}/bdb/lib -ldb_cxx-4.8",
+                          "BDB_CFLAGS=-I#{buildpath}/bdb/include",
+                          *std_configure_args
     system "make", "install"
     pkgshare.install "share/rpcauth"
   end
