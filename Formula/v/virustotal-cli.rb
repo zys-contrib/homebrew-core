5a841a3f796538f19fc8865410181ffae38e70ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-18 16:36:10 +0000

    virustotal-cli: update 1.0.1 bottle.

--- Formula/v/virustotal-cli.rb
+++ Formula/v/virustotal-cli.rb
@@ -14,13 +14,13 @@ class VirustotalCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "51c8c0dd2a1a71900e5daac273038cd32baa8a95b05d0606d5f7c58533344fef"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a17e44fe55495d9f0ca0ec43c6aa41d9be56d69db62d674d655ee8acc5f511d1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "94cab43683d247b0e69b8e4f0d55d6171746d6a3f13629d2a58a14828dcd810d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "82518e0802aa20ab83531a3d947d96b4ce61c1ff9bf173d224609b75dddaf7f3"
-    sha256 cellar: :any_skip_relocation, ventura:        "80bda0a84dfb631649cdd4c5d292066778d2f1fc5c7d2d76b13323bd485d7c4f"
-    sha256 cellar: :any_skip_relocation, monterey:       "90fe0c52129b503c43222e844a491bebb9dd70631e520b8d0c970b4c73a92e5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "089aa3d9f6c9d7deb1f49c5d622575b55b48b75a899a36ef56bf1347f3211dff"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f309321a7f51f6803a98b9814f59862fc5646c0a4b0e1096b3da002ab8e3eb8b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f309321a7f51f6803a98b9814f59862fc5646c0a4b0e1096b3da002ab8e3eb8b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f309321a7f51f6803a98b9814f59862fc5646c0a4b0e1096b3da002ab8e3eb8b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5b5ce2e8074d942a6c7e04863c60b3eebac2bf85616d2b04219adc1424644509"
+    sha256 cellar: :any_skip_relocation, ventura:        "5b5ce2e8074d942a6c7e04863c60b3eebac2bf85616d2b04219adc1424644509"
+    sha256 cellar: :any_skip_relocation, monterey:       "5b5ce2e8074d942a6c7e04863c60b3eebac2bf85616d2b04219adc1424644509"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "09709ec89f8c6ee8016c507ac911a7d0d20dd9ea93ca5009538ec6047cd2fb72"
   end
 
   depends_on "go" => :build
