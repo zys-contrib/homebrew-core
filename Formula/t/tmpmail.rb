9e62d6fb04b348e67bc4ad7dda4a3c0e4ca9c9a2
Rui Chen <rui@chenrui.dev>
2024-08-18 13:02:36 +0000

    tmpmail: add 1.2.3 bottle.

--- Formula/t/tmpmail.rb
+++ Formula/t/tmpmail.rb
@@ -6,6 +6,10 @@ class Tmpmail < Formula
   license "MIT"
   head "https://github.com/sdushantha/tmpmail.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "81e6aa8ea9a04b09f48bde663f58eb155061f1f83f8647727371a72cafaf9c0d"
+  end
+
   depends_on "jq"
   depends_on "w3m"
   depends_on "xclip"
