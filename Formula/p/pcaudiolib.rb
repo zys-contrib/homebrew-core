ac00128722612465765c1769ac7d5f54e3df95f5
Rui Chen <rui@chenrui.dev>
2024-08-19 20:23:15 +0000

    pcaudiolib: add 1.2 bottle.

--- Formula/p/pcaudiolib.rb
+++ Formula/p/pcaudiolib.rb
@@ -14,6 +14,16 @@ class Pcaudiolib < Formula
     end
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "93e464880ad92fe6d01a9d7ae289de1d651e754bd7dc73ff36f997e440690ef4"
+    sha256 cellar: :any,                 arm64_ventura:  "d4d3d51973306d23db005641d4ff0bf7fa253f45e40a6eb257d3444822aa5f4f"
+    sha256 cellar: :any,                 arm64_monterey: "e869b2820a0891d695690497ed88f6f82e59d2c42abb9d384367fef1815cf111"
+    sha256 cellar: :any,                 sonoma:         "d7397d64207e96b166f20e90ed93be29644946a6b35437af9b002e60e94d93f6"
+    sha256 cellar: :any,                 ventura:        "f905d01515df1bb7babc8eae222534fbde844979d6ffd596a5ed71c89db93b31"
+    sha256 cellar: :any,                 monterey:       "1b2d24e3561a8008d5b5c13aa903759db76c6dbe96953a5865a651cbee4a5d0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1c3de7e6ac9b806793767db2b2d9ded8152a1909ba4254215b3508251a346d55"
+  end
+
   head do
     url "https://github.com/espeak-ng/pcaudiolib.git", branch: "master"
 
