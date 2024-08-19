24b08bd842e318658764d945760b0479a029c624
Brona <brona@robenek.me>
2024-08-19 18:43:06 +0200

    iproute2mac 1.5.3

--- Formula/i/iproute2mac.rb
+++ Formula/i/iproute2mac.rb
@@ -3,8 +3,8 @@ class Iproute2mac < Formula
 
   desc "CLI wrapper for basic network utilities on macOS - ip command"
   homepage "https://github.com/brona/iproute2mac"
-  url "https://github.com/brona/iproute2mac/releases/download/v1.5.2/iproute2mac-1.5.2.tar.gz"
-  sha256 "1e9874b0cb3a18c98dc80f234322f5924e361d80bfd4b6581fca7f9874bcad4f"
+  url "https://github.com/brona/iproute2mac/releases/download/v1.5.3/iproute2mac-1.5.3.tar.gz"
+  sha256 "a5d790863b2ad92ff2e918908a82563898ee8165278ffa0b5e453437bd5ef9fb"
   license "MIT"
 
   bottle do
