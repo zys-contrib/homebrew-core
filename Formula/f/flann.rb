3dd77e1857ea0ee8f6c99b550c9168d93bbe4acd
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-08-19 15:12:41 +0800

    flann: declare indirect dependencies with linkage
    Spotted at #181636.

--- Formula/f/flann.rb
+++ Formula/f/flann.rb
@@ -21,6 +21,7 @@ class Flann < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "hdf5"
+  depends_on "lz4"
 
   def install
     system "cmake", "-S", ".", "-B", "build",
