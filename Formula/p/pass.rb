66599a71009dfb4eeb02eec357e654250c5ea292
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-20 17:02:19 +0000

    pass: update 1.7.4 bottle.

--- Formula/p/pass.rb
+++ Formula/p/pass.rb
@@ -12,17 +12,14 @@ class Pass < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b440bc2538908aeab525c211228b75c82c6548d1f1c0949053cd59814f4061a3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7ee1c46e172270c3161ef1d843c37f0bc21fb874af0b55dcd16ad32e795d17e5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7ee1c46e172270c3161ef1d843c37f0bc21fb874af0b55dcd16ad32e795d17e5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dbf5e1b314720d846525cc81a51f30e7cf7319a943f2fb395fb62202eff2c95c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "c168f0693175727cfcf58a796200f6e002c497cab23da16ca37e9dfabfa02c5d"
-    sha256 cellar: :any_skip_relocation, ventura:        "d667e58dae216055777c9780d522b68c6868d7b23f9f712c09c29b1daf215b35"
-    sha256 cellar: :any_skip_relocation, monterey:       "d667e58dae216055777c9780d522b68c6868d7b23f9f712c09c29b1daf215b35"
-    sha256 cellar: :any_skip_relocation, big_sur:        "80812f17b470ea37c9027851ed71a6a09a8d0be359e6770c9e836646c68ade9e"
-    sha256 cellar: :any_skip_relocation, catalina:       "80812f17b470ea37c9027851ed71a6a09a8d0be359e6770c9e836646c68ade9e"
-    sha256 cellar: :any_skip_relocation, mojave:         "80812f17b470ea37c9027851ed71a6a09a8d0be359e6770c9e836646c68ade9e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "59753aa6bb5027b238e43bcdf87f603c9850166b485b85b6e457ecc8f4aff25c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "14e3206a94f04e911f0168b7e458f0149b8c42cd34014a113610431d2a142e1b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "14e3206a94f04e911f0168b7e458f0149b8c42cd34014a113610431d2a142e1b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "14e3206a94f04e911f0168b7e458f0149b8c42cd34014a113610431d2a142e1b"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1132f363a63efb874ebf98f406dcc6f9346496b10b0a3b3c2063b447c8035180"
+    sha256 cellar: :any_skip_relocation, ventura:        "1132f363a63efb874ebf98f406dcc6f9346496b10b0a3b3c2063b447c8035180"
+    sha256 cellar: :any_skip_relocation, monterey:       "1132f363a63efb874ebf98f406dcc6f9346496b10b0a3b3c2063b447c8035180"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee3104f2584abf3c35e811152282541832cc3e672d8b28024ee8d77d49cca172"
   end
 
   depends_on "gnu-getopt"
