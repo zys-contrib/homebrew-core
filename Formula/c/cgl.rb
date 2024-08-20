c620c5915bbc77c5e13b3db4f63f28a05412f4fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-20 09:26:55 +0000

    cgl 0.60.9

--- Formula/c/cgl.rb
+++ Formula/c/cgl.rb
@@ -1,8 +1,8 @@
 class Cgl < Formula
   desc "Cut Generation Library"
   homepage "https://github.com/coin-or/Cgl"
-  url "https://github.com/coin-or/Cgl/archive/refs/tags/releases/0.60.8.tar.gz"
-  sha256 "1482ba38afb783d124df8d5392337f79fdd507716e9f1fb6b98fc090acd1ad96"
+  url "https://github.com/coin-or/Cgl/archive/refs/tags/releases/0.60.9.tar.gz"
+  sha256 "558421ccd6aa91d6922dd1baa04e37aa4c75ba0472118dc11779e5d6a19bfb38"
   license "EPL-2.0"
 
   livecheck do
