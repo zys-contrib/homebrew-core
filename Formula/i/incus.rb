5c42a4c38d4eb0cf287e8142985014fa0864d549
Thomas Danielsson <thomas@elajt.se>
2024-08-21 22:04:35 +0200

    incus: generate completions from executable

--- Formula/i/incus.rb
+++ Formula/i/incus.rb
@@ -20,6 +20,8 @@ class Incus < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/incus"
+
+    generate_completions_from_executable(bin/"incus", "completion")
   end
 
   test do
