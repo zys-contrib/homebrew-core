9ed7a4c29231cef1def26d7f46e8eff0748873c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-21 05:17:55 +0000

    gabo: add 1.1.2 bottle.

--- Formula/g/gabo.rb
+++ Formula/g/gabo.rb
@@ -5,6 +5,16 @@ class Gabo < Formula
   sha256 "2aba8f85f9d241217a56847c372f7d7a001218073f76f8e791bc161edd525611"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8759efedfe0dbf8ce146a936e7cfd9cdd55d7f97ff55b39fb133eb73e9d9d850"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8759efedfe0dbf8ce146a936e7cfd9cdd55d7f97ff55b39fb133eb73e9d9d850"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "8759efedfe0dbf8ce146a936e7cfd9cdd55d7f97ff55b39fb133eb73e9d9d850"
+    sha256 cellar: :any_skip_relocation, sonoma:         "1c281c3251a06765ee26bad7de6452720dfc64443e66f635843575a2cac0bad0"
+    sha256 cellar: :any_skip_relocation, ventura:        "1c281c3251a06765ee26bad7de6452720dfc64443e66f635843575a2cac0bad0"
+    sha256 cellar: :any_skip_relocation, monterey:       "1c281c3251a06765ee26bad7de6452720dfc64443e66f635843575a2cac0bad0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4e8eb90c42fa05a03d5a69091f31b658083a1c139386d3050b2d5d1fb1d2e54b"
+  end
+
   depends_on "go" => :build
 
   def install
