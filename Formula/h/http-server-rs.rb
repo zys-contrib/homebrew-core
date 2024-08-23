cfbfbfe0177d161747a044e29ba9bd125f95b662
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-23 21:32:23 +0000

    http-server-rs: add 0.8.9 bottle.

--- Formula/h/http-server-rs.rb
+++ Formula/h/http-server-rs.rb
@@ -6,6 +6,16 @@ class HttpServerRs < Formula
   license "Apache-2.0"
   head "https://github.com/http-server-rs/http-server.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d6f29de99576b74d2398a7fc093bc700a3de1d7d5c1db98224c3750c1ae79226"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "07f0f7eaf185964fa80e46e9b0b797c8f454345fb3eb72d21ca618379c59602d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "02223f122e30366797da3e9016ce454653dc8db7a07f79fbd6392aa42dee20a3"
+    sha256 cellar: :any_skip_relocation, sonoma:         "9fd1a8e7f2e528913edb6e48b07d03d61b7b39e63173b77e64696394a5f33318"
+    sha256 cellar: :any_skip_relocation, ventura:        "2b555ba245fc88ce42aacc38c80c1409176d0f7900d85945609111bc3bdf6573"
+    sha256 cellar: :any_skip_relocation, monterey:       "6611eb126def91614acb470f6393229f827905ee621fedf4f3791f8c94cff625"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "580cdfea1cd7aacdc67e69c5db6e16f057c8a459c8567b0eea601628c30352b7"
+  end
+
   depends_on "rust" => :build
 
   def install
