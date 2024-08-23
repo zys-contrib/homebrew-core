795ef2847e5a488577a31aaeb55f2acc88c28d69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-23 16:34:40 +0000

    ucommon: update 7.0.1 bottle.

--- Formula/u/ucommon.rb
+++ Formula/u/ucommon.rb
@@ -14,20 +14,13 @@ class Ucommon < Formula
   end
 
   bottle do
-    sha256 arm64_sonoma:   "bdb6d02a1245b3839f3d9daacc5cf458daa04c25e152ac5e42e528ca07edee2d"
-    sha256 arm64_ventura:  "1f95d42a1b7169c3aab6490ecba6ca2dbff77446f65ff197fe2cd002ab6138a5"
-    sha256 arm64_monterey: "52b2e7720afe7f2a3a9de958bcfd949215a74e565b7520aa99fa94025b861b09"
-    sha256 arm64_big_sur:  "1270ebc3579e74f3f044e88d7bca663efac0aa581ab214514f6345ade2a7ba16"
-    sha256 sonoma:         "a6ff1edc79dc57453f394d81e74426e909ad4313230bb4a5df16776369530ffb"
-    sha256 ventura:        "7501d398470b723c5cae9d4a47804069c56d620228ef547e04cb9a537a29cf47"
-    sha256 monterey:       "7f4755beded307911032d7952a51e9e5e710cbf246bfcce8f67e079978f28b82"
-    sha256 big_sur:        "ca1bc13b9def95eb4839a628d6936ea799a284ac4d61dd53a77e77a046d3ffe1"
-    sha256 catalina:       "3040baab77d1ff69f36ff21ec9259c8512170f361119e66b446a48b86f157320"
-    sha256 mojave:         "34ef3423a4f8f0de02e05e8a00a5f1cb12bd0b9790103354792c24b7613ccb80"
-    sha256 high_sierra:    "650bda43b289012df676190269cde7bb3be3e1337f4f2eddc6f472ae38bbda1c"
-    sha256 sierra:         "0546fbc44ac1e17d8757b41a67b2d68b15bc872b4b19fea649e5d7fe54a4d2d4"
-    sha256 el_capitan:     "57756d7809936ed885ef8fc7a284498ab12a5be6cc1ad41ad148dd45074fc322"
-    sha256 x86_64_linux:   "f3624d1bf63f84175560167e9887d9fa88533aad863e697894683214091acb5b"
+    sha256 arm64_sonoma:   "aef98eb17b5948c37b54052b5695cbc7de0e3ebfed947973ccf5df6bc99280db"
+    sha256 arm64_ventura:  "2f5710346714a1abd916b61ac2f8cdb3935ffb8f25d975e9188b102aa0d2cfc9"
+    sha256 arm64_monterey: "38d325d005640c936e08eee001469b019b63be1a604a4cb7ef2def41f2dda2eb"
+    sha256 sonoma:         "d9e52557eb8da88b69857f8ada8e310ece430553c7ac5c1b1769fa4b354cc497"
+    sha256 ventura:        "7fa74730296e4837ff21ff4a40320d8a5fa0f0f1217bac95875bd59a001fdd44"
+    sha256 monterey:       "593ba25333186ce44b5f5c1fadcdc4d1afed3429a7961758128036c5a7f3a115"
+    sha256 x86_64_linux:   "3cb9863a696fdced7013b30774bea48fced75770d6e2c7a85d4dda337f1ee271"
   end
 
   depends_on "autoconf" => :build
