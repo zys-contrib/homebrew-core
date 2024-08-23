848ea53f60fe3cc5ea2797d8d6fab87800968edd
Michael Cho <michael@michaelcho.dev>
2024-08-23 09:04:18 -0400

    libspelling: revision bump to migrate to `icu4c@75`

--- Formula/lib/libspelling.rb
+++ Formula/lib/libspelling.rb
@@ -4,6 +4,7 @@ class Libspelling < Formula
   url "https://gitlab.gnome.org/GNOME/libspelling/-/archive/0.2.1/libspelling-0.2.1.tar.bz2"
   sha256 "5393a9b93fda445598348a47c42d1ad13586c0bcf35dfd257afd613fd31812c1"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any, arm64_sequoia:  "2c4271f43d13aea74b7c4d582489b7c442efd19cc71bcc7d27c741e5069b4794"
@@ -26,7 +27,7 @@ class Libspelling < Formula
   depends_on "glib"
   depends_on "gtk4"
   depends_on "gtksourceview5"
-  depends_on "icu4c"
+  depends_on "icu4c@75"
   depends_on "pango"
 
   on_macos do
