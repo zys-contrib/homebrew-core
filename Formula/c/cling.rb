24b342c5cb23dc72681870cee25088c910bfb4b0
Michael Cho <michael@michaelcho.dev>
2024-08-24 16:46:15 -0400

    cling 1.0

--- Formula/c/cling.rb
+++ Formula/c/cling.rb
@@ -1,10 +1,12 @@
 class Cling < Formula
   desc "C++ interpreter"
-  homepage "https://root.cern.ch/cling"
-  url "https://github.com/root-project/cling.git",
-      tag:      "v0.9",
-      revision: "f3768a4c43b0f3b23eccc6075fa178861a002a10"
-  license any_of: ["LGPL-2.1-only", "NCSA"]
+  homepage "https://root.cern/cling/"
+  url "https://github.com/root-project/cling/archive/refs/tags/v1.0.tar.gz"
+  sha256 "93252c72bae2a660d9e2c718af18b767a09928078a854b2fcd77f28cc9fa71ae"
+  license all_of: [
+    { any_of: ["LGPL-2.1-only", "NCSA"] },
+    { "Apache-2.0" => { with: "LLVM-exception" } }, # llvm
+  ]
 
   bottle do
     sha256               arm64_monterey: "ae9ec74f889a58e57f00394e3b46dd1793d975ce7dc5907c71e2e15853610a62"
@@ -16,41 +18,40 @@ class Cling < Formula
     sha256               x86_64_linux:   "315073c45b0684a970493476b9c8476ddf90eb7d69bd5326efdf97b79ec55e25"
   end
 
-  # Does not build on Ventura
-  # https://github.com/Homebrew/homebrew-core/pull/131473
-  # https://github.com/root-project/cling/issues/492#issuecomment-1555938334
-  deprecate! date: "2023-08-24", because: :does_not_build
-
   depends_on "cmake" => :build
 
+  uses_from_macos "python" => :build, since: :catalina
+  uses_from_macos "libedit"
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
-  resource "clang" do
-    url "http://root.cern.ch/git/clang.git",
-        tag:      "cling-v0.9",
-        revision: "b7fa7dcfd21cac3d67688be9bdc83a35778e53e1"
-  end
-
   resource "llvm" do
-    url "http://root.cern.ch/git/llvm.git",
-        tag:      "cling-v0.9",
-        revision: "85e42859fb6de405e303fc8d92e37ff2b652b4b5"
+    url "https://github.com/root-project/llvm-project/archive/refs/tags/cling-llvm13-20240614-01.tar.gz"
+    sha256 "c8afe609e9319009adf4e0fcb5021b663250ffbac1212c15058effb9ae6739d8"
   end
 
   def install
-    (buildpath/"src").install resource("llvm")
-    (buildpath/"src/tools/cling").install buildpath.children - [buildpath/"src"]
-    (buildpath/"src/tools/clang").install resource("clang")
-    mkdir "build" do
-      system "cmake", *std_cmake_args, "../src",
-                      "-DCMAKE_INSTALL_PREFIX=#{libexec}",
-                      "-DCLING_CXX_PATH=clang++"
-      system "make", "install"
-    end
-    bin.install_symlink libexec/"bin/cling"
-    prefix.install_metafiles buildpath/"src/tools/cling"
+    # Skip modification of CLING_OSX_SYSROOT to the unversioned SDK path
+    # Related: https://github.com/Homebrew/homebrew-core/issues/135714
+    # Related: https://github.com/root-project/cling/issues/457
+    inreplace "lib/Interpreter/CMakeLists.txt", '"MacOSX[.0-9]+\.sdk"', '"SKIP"'
+
+    (buildpath/"llvm").install resource("llvm")
+
+    system "cmake", "-S", "llvm/llvm", "-B", "build",
+                    "-DCLING_CXX_PATH=clang++",
+                    "-DLLVM_BUILD_TOOLS=OFF",
+                    "-DLLVM_ENABLE_PROJECTS=clang",
+                    "-DLLVM_EXTERNAL_CLING_SOURCE_DIR=#{buildpath}",
+                    "-DLLVM_EXTERNAL_PROJECTS=cling",
+                    "-DLLVM_TARGETS_TO_BUILD=host;NVPTX",
+                    *std_cmake_args(install_prefix: libexec)
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    # We use an exec script as a symlink causes issues finding headers
+    bin.write_exec_script libexec/"bin/cling"
   end
 
   test do
