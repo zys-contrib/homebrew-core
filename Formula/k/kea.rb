75e739d891985ba6dd81cfb31e373f806c6f6433
Rui Chen <rui@chenrui.dev>
2024-08-25 01:18:53 +0000

    kea: add 2.6.1 bottle.

--- Formula/k/kea.rb
+++ Formula/k/kea.rb
@@ -7,6 +7,16 @@ class Kea < Formula
   sha256 "d2ce14a91c2e248ad2876e29152d647bcc5e433bc68dafad0ee96ec166fcfad1"
   license "MPL-2.0"
 
+  bottle do
+    sha256 arm64_sonoma:   "198b4e13a2d22180c619d3cdd825e06c2ea6deacf1e1e0f1edb4bf8a4f811a56"
+    sha256 arm64_ventura:  "dd9a0ee71e6edb0445804e2acdac49e4ac9f14e954c8716f2bccf217b3c42960"
+    sha256 arm64_monterey: "4d6ef86f0bc8fd533f67de887f6aa0244fa0199c82fd3f596e8ca7af6616b9fc"
+    sha256 sonoma:         "b12c07c7ea8512625d415ac5f90b2acf153f49bd57764a3c173448235906b0ee"
+    sha256 ventura:        "d2443974aad0fdc56d03a21795d83e8101928ff0605467ee6055b9939d34ef9d"
+    sha256 monterey:       "29c57e487849a00ae7bcf4ee78afade4e64503cb197b63684dbe60777fae4d22"
+    sha256 x86_64_linux:   "b45ed2602b200d7f2ea8ccad1a72b9c8362e63766b606edf9ae3ddb5a780abea"
+  end
+
   head do
     url "https://gitlab.isc.org/isc-projects/kea.git", branch: "master"
 
