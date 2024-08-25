e8065ad13f20bb947b345f97f7164fd5b2942b47
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-08-25 08:46:06 +0000

    packcc: add 2.0.2 bottle.

--- Formula/p/packcc.rb
+++ Formula/p/packcc.rb
@@ -6,6 +6,16 @@ class Packcc < Formula
   license "MIT"
   head "https://github.com/arithy/packcc.git", branch: "master"
 
+  bottle do
+    sha256 arm64_sonoma:   "34ee3deeba08a37415ff82cd923ce9238e369126ef3197eeca741b4be69ccc10"
+    sha256 arm64_ventura:  "e45d252d935dfc169a8d31fd840cde9353b95c5a17f3a9dff48de10961110b7d"
+    sha256 arm64_monterey: "8126fe567e6bac3caf32352caefab42b66f46ad53d7cd51a24ef5642d92601a7"
+    sha256 sonoma:         "c341e89c607e4e418c4553245ea00881c9b7df52c3c86d66e6e26bbf970433a2"
+    sha256 ventura:        "8308b2948c1f811a6a44b507f8c569bebbf1ce4250a5c9558facec5e59296f63"
+    sha256 monterey:       "3653656d276ca1dc93c1ea029a43b3b47ac7c5c4e774bf0ce624ae0d5465c071"
+    sha256 x86_64_linux:   "b85293be77d9ccaa373653335ec4e2de764f708ffa2910bb0847060186fa3e6e"
+  end
+
   def install
     inreplace "src/packcc.c", "/usr/share/packcc/", "#{pkgshare}/"
     build_dir = buildpath/"build"/ENV.compiler.to_s.sub(/-\d+$/, "")
