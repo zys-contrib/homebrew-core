978aeae9f892b0324a26ab119bcfb745f5edf380
Michael Cho <michael@michaelcho.dev>
2024-08-24 23:41:47 -0400

    xshogi: workaround to build on Sonoma

--- Formula/x/xshogi.rb
+++ Formula/x/xshogi.rb
@@ -30,8 +30,9 @@ class Xshogi < Formula
   depends_on "libxt"
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-dependency-tracking"
+    ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1403
+
+    system "./configure", "--disable-silent-rules", *std_configure_args
     system "make", "install"
   end
 end
