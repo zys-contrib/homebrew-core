1ed83a84f31785f59af26863599e7bd826c1af12
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-08-27 17:20:45 +0800

    cmrc 2.0.1 (new formula)
    Needed for new versions of `vcpkg`.

---
+++ Formula/c/cmrc.rb
@@ -0,0 +1,30 @@
+class Cmrc < Formula
+  desc "CMake Resource Compiler"
+  homepage "https://github.com/vector-of-bool/cmrc"
+  url "https://github.com/vector-of-bool/cmrc/archive/refs/tags/2.0.1.tar.gz"
+  sha256 "edad5faaa0bea1df124b5e8cb00bf0adbd2faeccecd3b5c146796cbcb8b5b71b"
+  license "MIT"
+  head "https://github.com/vector-of-bool/cmrc.git", branch: "master"
+
+  depends_on "cmake" => [:build, :test]
+
+  def install
+    (share/"cmake").install "CMakeRC.cmake"
+  end
+
+  def caveats
+    <<~EOS
+      To use CMakeRC, add
+        #{opt_share}/cmake
+      to your `CMAKE_MODULE_PATH`.
+    EOS
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 3.30)
+      include(CMakeRC)
+    CMAKE
+    system "cmake", ".", "-DCMAKE_MODULE_PATH=#{share}/cmake", *std_cmake_args
+  end
+end
