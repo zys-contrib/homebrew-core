c90438c631d4d16a7e3eae6d1e17e370c0d106ea
Rui Chen <rui@chenrui.dev>
2024-08-27 12:57:21 +0000

    slackdump: add 2.5.10 bottle.

--- Formula/s/slackdump.rb
+++ Formula/s/slackdump.rb
@@ -6,6 +6,16 @@ class Slackdump < Formula
   license "GPL-3.0-only"
   head "https://github.com/rusq/slackdump.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "60f59c95b43afa78b11b248e5a966c1e396e371250966418454ad90956963645"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "60f59c95b43afa78b11b248e5a966c1e396e371250966418454ad90956963645"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "60f59c95b43afa78b11b248e5a966c1e396e371250966418454ad90956963645"
+    sha256 cellar: :any_skip_relocation, sonoma:         "cc7bb533148c2ada79d1935bc7b1ce85113a73254e69dcb88c5463dd006c0130"
+    sha256 cellar: :any_skip_relocation, ventura:        "cc7bb533148c2ada79d1935bc7b1ce85113a73254e69dcb88c5463dd006c0130"
+    sha256 cellar: :any_skip_relocation, monterey:       "cc7bb533148c2ada79d1935bc7b1ce85113a73254e69dcb88c5463dd006c0130"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f72a051d2309351af0cc9f9c86a8907b586d48f1c8cb532efe78887f3965e1d7"
+  end
+
   depends_on "go" => :build
 
   def install
