14f10d38e93482c6614fafe237e57c54b9a0b712
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-28 21:57:37 +0000

    zsh-you-should-use 1.9.0

--- Formula/z/zsh-you-should-use.rb
+++ Formula/z/zsh-you-should-use.rb
@@ -1,8 +1,8 @@
 class ZshYouShouldUse < Formula
   desc "ZSH plugin that reminds you to use existing aliases for commands you just typed"
   homepage "https://github.com/MichaelAquilina/zsh-you-should-use"
-  url "https://github.com/MichaelAquilina/zsh-you-should-use/archive/refs/tags/1.8.0.tar.gz"
-  sha256 "a32337515d3c47b4d8a65e7302433d570de296741c48e3137b1d2c788e4e7d28"
+  url "https://github.com/MichaelAquilina/zsh-you-should-use/archive/refs/tags/1.9.0.tar.gz"
+  sha256 "6ca0128883ab73d3782f70a6b16c95ed033c2497c3e27d5db0a68c90d8fde4a2"
   license "GPL-3.0-only"
 
   bottle do
