1d575ceb8605a85db8ea95ded9ec5aff889cc9d0
Rui Chen <rui@chenrui.dev>
2024-08-28 15:48:36 +0000

    dart-sdk: update 3.5.2 bottle.

--- Formula/d/dart-sdk.rb
+++ Formula/d/dart-sdk.rb
@@ -6,13 +6,13 @@ class DartSdk < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c07e41301a72ff5c7283805eb4a3d7aef57fbae3853bc80132381149ab86fc57"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "845793c4b8c4117dfef7d3897bd51cf6f22187bf6bda8360c281f0f3c87ceaea"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5bf26c106d6b905edcb05b757696bd264fc8405cd7f315ad20c5183b8ea5530a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "b884d3d6a53f8562c17a5ecb89f93a950e13d3b7ac96549475dc6cd68c6f9a81"
-    sha256 cellar: :any_skip_relocation, ventura:        "237a1c26fb1f6bdc83db73ec2c9f80e768c9e852f63ad7f9ab9a7e088a112742"
-    sha256 cellar: :any_skip_relocation, monterey:       "43c408a80ccbb0f21a972afe748d59fe572017fa193e51ea8884ac8f001cecd6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d69159b7e98dfddd4c7073538f8c9f9cc47920aef6aea8b485ba76e83839cada"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f339a1f580efd55e84e1d996b148b1499f38a3d49b30cc3461a6570bf691ebd3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4c3a11f5f8eaea9773aea5a4daa70d4934d72c485929430ba74929af3bb6b8f9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "792dc2c00092a1562d75fbd76e57688b490c52781f07fe6bac8502f8555be31e"
+    sha256 cellar: :any_skip_relocation, sonoma:         "50487539baf01a9b4faadde935b5a4606abae761cd5aedf3761200d5cc336851"
+    sha256 cellar: :any_skip_relocation, ventura:        "68b6a85719c2f5b60d90a21b0f324b6a554c9122eb5ad27d2df7457d085f0c9c"
+    sha256 cellar: :any_skip_relocation, monterey:       "5aaa37b01b07e28e5aee4704bf666b5fcd27eb03974045e40590135859641efd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "417d28d6b166b96bacf48ec8803741abddf88cf72f0b6f1135c914e3e8f22a75"
   end
 
   depends_on "ninja" => :build
