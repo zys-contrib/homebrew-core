c6b48363c2887f696d20c985b43aa999713d9a5f
Rui Chen <rui@chenrui.dev>
2024-08-29 15:28:40 -0400

    screen 5.0.0
    autobump: add screen
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    screen: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    screen: needs linux-pam for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/screen.rb
+++ Formula/s/screen.rb
@@ -1,21 +1,11 @@
 class Screen < Formula
   desc "Terminal multiplexer with VT100/ANSI terminal emulation"
   homepage "https://www.gnu.org/software/screen/"
+  url "https://ftp.gnu.org/gnu/screen/screen-5.0.0.tar.gz"
+  mirror "https://ftpmirror.gnu.org/screen/screen-5.0.0.tar.gz"
+  sha256 "f04a39d00a0e5c7c86a55338808903082ad5df4d73df1a2fd3425976aed94971"
   license "GPL-3.0-or-later"
   head "https://git.savannah.gnu.org/git/screen.git", branch: "master"
-
-  stable do
-    url "https://ftp.gnu.org/gnu/screen/screen-4.9.1.tar.gz"
-    mirror "https://ftpmirror.gnu.org/screen/screen-4.9.1.tar.gz"
-    sha256 "26cef3e3c42571c0d484ad6faf110c5c15091fbf872b06fa7aa4766c7405ac69"
-
-    # This patch is to disable the error message
-    # "/var/run/utmp: No such file or directory" on launch
-    patch :p2 do
-      url "https://gist.githubusercontent.com/yujinakayama/4608863/raw/75669072f227b82777df25f99ffd9657bd113847/gistfile1.diff"
-      sha256 "9c53320cbe3a24c8fb5d77cf701c47918b3fabe8d6f339a00cfdb59e11af0ad5"
-    end
-  end
 
   bottle do
     sha256 arm64_sonoma:   "7eb9de9e4d0e2afb4c4ae8f243a680195a407ff90b52a621bc6381b949175890"
@@ -35,31 +25,22 @@ class Screen < Formula
   uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
 
+  on_linux do
+    depends_on "linux-pam"
+  end
+
   def install
-    cd "src" if build.head?
-
-    # Fix error: dereferencing pointer to incomplete type 'struct utmp'
-    ENV.append_to_cflags "-include utmp.h"
-
-    # Fix compile with newer Clang
-    # https://savannah.gnu.org/bugs/index.php?59465
-    ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1200
-
-    # master branch configure script has no
-    # --enable-colors256, so don't use it
-    # when `brew install screen --HEAD`
-    args = [
-      "--prefix=#{prefix}",
-      "--mandir=#{man}",
-      "--infodir=#{info}",
-      "--enable-pam",
+    args = %W[
+      --mandir=#{man}
+      --infodir=#{info}
+      --enable-pam
     ]
-    args << "--enable-colors256" unless build.head?
 
     system "./autogen.sh"
-    system "./configure", *args
 
-    system "make"
+    # Exclude unrecognized options
+    std_args = std_configure_args.reject { |s| s["--disable-debug"] || s["--disable-dependency-tracking"] }
+    system "./configure", *args, *std_args
     system "make", "install"
   end
 
