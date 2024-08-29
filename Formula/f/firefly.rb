3f1cda5b7b2bc592e8db158fde09322bd92c9c56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-29 09:09:51 +0000

    firefly: add 1.3.1 bottle.

--- Formula/f/firefly.rb
+++ Formula/f/firefly.rb
@@ -11,6 +11,16 @@ class Firefly < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4aa882c225163110ccf449a1a35b324724cc6cfd11a1f671cd437329688a18f0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3e777e7aac58c177aad15d9e3f174c5aaced3a6f1ea31099991eca326b47f2cc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2024d3903e2a956ece3d20aef8c6b806829ffac3c02f261d1d0ed1ceb67cdcdd"
+    sha256 cellar: :any_skip_relocation, sonoma:         "18d5aee81d0e4941a66805680fc0d397a958029d9979586fa73428202a76c281"
+    sha256 cellar: :any_skip_relocation, ventura:        "c393d7ffe35d00d3c267908e3a4511c7189d3ffe2578cafe816db42a9c7f32e1"
+    sha256 cellar: :any_skip_relocation, monterey:       "91cc49ed362308ae1f6a691588f174aabcbeac6a92c9f9690d0428ed65ebf55d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19c7752cc7b75887c29cd5e153340c0ee8bf984ba6c04ca6ec3a12b583f62f34"
+  end
+
   depends_on "go" => :build
 
   def install
