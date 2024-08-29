27116da731544839816ab0dab72265e52c4593da
Michael Cho <michael@michaelcho.dev>
2024-08-29 13:41:57 -0400

    js-beautify 1.15.1 (new formula)

---
+++ Formula/j/js-beautify.rb
@@ -0,0 +1,27 @@
+class JsBeautify < Formula
+  desc "JavaScript, CSS and HTML unobfuscator and beautifier"
+  homepage "https://beautifier.io"
+  url "https://registry.npmjs.org/js-beautify/-/js-beautify-1.15.1.tgz"
+  sha256 "b840f39d9e8c41a54909f299e1850c254ed33cdefc2d983726ee1f607506a36b"
+  license "MIT"
+
+  depends_on "node"
+
+  conflicts_with "jsbeautifier", because: "both install `js-beautify` binaries"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    javascript = "if ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}"
+    assert_equal <<~EOS.chomp, pipe_output(bin/"js-beautify", javascript)
+      if ('this_is' == /an_example/) {
+          of_beautifier();
+      } else {
+          var a = b ? (c % d) : e[f];
+      }
+    EOS
+  end
+end
