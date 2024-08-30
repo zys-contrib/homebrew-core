5551151579de4ed8acc3374b8732c2d1f6d384dc
Klaus Hipp <khipp@users.noreply.github.com>
2024-08-30 17:05:37 +0200

    nss 3.104

--- Formula/n/nss.rb
+++ Formula/n/nss.rb
@@ -1,8 +1,8 @@
 class Nss < Formula
   desc "Libraries for security-enabled client and server applications"
   homepage "https://firefox-source-docs.mozilla.org/security/nss/index.html"
-  url "https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_103_RTM/src/nss-3.103.tar.gz"
-  sha256 "7b4ab657f772dc7520c46e8d481940b292dcfc6a4c90150a7c26672384cee962"
+  url "https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_104_RTM/src/nss-3.104.tar.gz"
+  sha256 "e2763223622d1e76b98a43030873856f248af0a41b03b2fa2ca06a91bc50ac8e"
   license "MPL-2.0"
 
   livecheck do
