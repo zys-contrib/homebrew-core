122e2917ceaa31da65b8e05b14db2bb4997a05dc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-08-31 23:00:59 +0800

    poac: move patch into `stable` block
    poac-dev/poac#975 has been merged.
    
    Also, add patch for the latest `libgit2`, and use Homebrew `toml11`
    instead of cloning it at build-time.

--- Formula/p/poac.rb
+++ Formula/p/poac.rb
@@ -1,11 +1,28 @@
 class Poac < Formula
   desc "Package manager and build system for C++"
   homepage "https://github.com/poac-dev/poac"
-  url "https://github.com/poac-dev/poac/archive/refs/tags/0.10.0.tar.gz"
-  sha256 "4bdede67b28f9622c071bef8c7eae76062c9ef2ad122deee49d994668e846288"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/poac-dev/poac.git", branch: "main"
+
+  # Remove `stable` block when patches are no longer needed.
+  stable do
+    url "https://github.com/poac-dev/poac/archive/refs/tags/0.10.0.tar.gz"
+    sha256 "4bdede67b28f9622c071bef8c7eae76062c9ef2ad122deee49d994668e846288"
+
+    # Allow usage of fmt 11
+    # https://github.com/poac-dev/poac/pull/975
+    patch do
+      url "https://github.com/poac-dev/poac/commit/e38d0c542538204b7e0522d07c65d0c787cb4eb9.patch?full_index=1"
+      sha256 "b1456f819f8079d6e051c95ec7b43dfc42d8f5998e7521e6534047cd2348638e"
+    end
+
+    # Fix for libgit2 1.8
+    patch do
+      url "https://github.com/poac-dev/poac/commit/97b43cb52fda635c75746df27664187fb2f00d7a.patch?full_index=1"
+      sha256 "bbe146c76ea9728b46608d0de7c8a5ded732b4418597c9b20a6025a02193bb34"
+    end
+  end
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "f09825ec68f5e41ba291fedca8088eaa70c1f8f54148dc1a8ff13c8039a70c13"
@@ -17,10 +34,11 @@ class Poac < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b0ce0818ff8f0c64e459b859bc139c481e2bb796db206f02bf94dcaf2a77d4b"
   end
 
+  depends_on "toml11" => :build
   depends_on "curl"
   depends_on "fmt"
-  depends_on "gcc" # C++20
-  depends_on "libgit2@1.7"
+  depends_on "gcc" # C++20, FIXME: This should be Linux-only.
+  depends_on "libgit2"
   depends_on "nlohmann-json"
   depends_on "pkg-config"
   depends_on "tbb"
@@ -36,15 +54,10 @@ class Poac < Formula
 
   fails_with gcc: "11" # C++20
 
-  # Allow usage of fmt 11
-  # https://github.com/poac-dev/poac/pull/975
-  patch do
-    url "https://github.com/poac-dev/poac/commit/e38d0c542538204b7e0522d07c65d0c787cb4eb9.patch?full_index=1"
-    sha256 "b1456f819f8079d6e051c95ec7b43dfc42d8f5998e7521e6534047cd2348638e"
-  end
-
   def install
     ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1200)
+    # Avoid cloning `toml11` at build-time.
+    (buildpath/"build-out/DEPS/toml11").install_symlink Formula["toml11"].opt_include
     system "make", "RELEASE=1", "PREFIX=#{prefix}", "install"
   end
 
