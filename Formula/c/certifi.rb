6a958eba16326111d8f1171e55aad8992f6ef409
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2024-09-01 22:35:53 +0000

    certifi: update 2024.8.30 bottle.

--- Formula/c/certifi.rb
+++ Formula/c/certifi.rb
@@ -6,13 +6,13 @@ class Certifi < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4106d660c4417641c4384ee68ab6c4146da998c22da4798ff94b03acd9ea31b4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4106d660c4417641c4384ee68ab6c4146da998c22da4798ff94b03acd9ea31b4"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4106d660c4417641c4384ee68ab6c4146da998c22da4798ff94b03acd9ea31b4"
-    sha256 cellar: :any_skip_relocation, sonoma:         "eecaf796430a3551f53fbe900e1455236af45d3996940288788293cda464dd92"
-    sha256 cellar: :any_skip_relocation, ventura:        "eecaf796430a3551f53fbe900e1455236af45d3996940288788293cda464dd92"
-    sha256 cellar: :any_skip_relocation, monterey:       "a33c294eb85e0ec0aadffe848e9b2be405c676a8435c50c7f9ddb5891ab633b7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4106d660c4417641c4384ee68ab6c4146da998c22da4798ff94b03acd9ea31b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "c109e2f1cbdb2c868d964f8eb4816e865ffacbf9c7d187865ac9e4b729557448"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c109e2f1cbdb2c868d964f8eb4816e865ffacbf9c7d187865ac9e4b729557448"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c109e2f1cbdb2c868d964f8eb4816e865ffacbf9c7d187865ac9e4b729557448"
+    sha256 cellar: :any_skip_relocation, sonoma:         "b72c41cf69a1e7317f9994e6f0dbdf1abb3ada5d767db3c10d2b4ac63d1245d4"
+    sha256 cellar: :any_skip_relocation, ventura:        "b72c41cf69a1e7317f9994e6f0dbdf1abb3ada5d767db3c10d2b4ac63d1245d4"
+    sha256 cellar: :any_skip_relocation, monterey:       "b72c41cf69a1e7317f9994e6f0dbdf1abb3ada5d767db3c10d2b4ac63d1245d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c109e2f1cbdb2c868d964f8eb4816e865ffacbf9c7d187865ac9e4b729557448"
   end
 
   depends_on "python@3.11" => [:build, :test]
