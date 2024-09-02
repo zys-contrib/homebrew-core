072e6f5a23ba57e91aeae9f16efec33af95cf1d4
Rui Chen <rui@chenrui.dev>
2024-09-02 14:57:24 -0400

    cminpack 1.3.9
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cminpack.rb
+++ Formula/c/cminpack.rb
@@ -1,8 +1,8 @@
 class Cminpack < Formula
   desc "Solves nonlinear equations and nonlinear least squares problems"
   homepage "http://devernay.free.fr/hacks/cminpack/cminpack.html"
-  url "https://github.com/devernay/cminpack/archive/refs/tags/v1.3.8.tar.gz"
-  sha256 "3ea7257914ad55eabc43a997b323ba0dfee0a9b010d648b6d5b0c96425102d0e"
+  url "https://github.com/devernay/cminpack/archive/refs/tags/v1.3.9.tar.gz"
+  sha256 "aa37bac5b5caaa4f5805ea5c4240e3834c993672f6dab0b17190ee645e251c9f"
   license "Minpack"
   head "https://github.com/devernay/cminpack.git", branch: "master"
 
@@ -21,22 +21,60 @@ class Cminpack < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "openblas"
 
   def install
-    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON",
-                         "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON",
-                         "-DCMINPACK_LIB_INSTALL_DIR=lib",
-                         *std_cmake_args
-    system "make", "install"
+    args = %w[
+      -DUSE_BLAS=ON
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+      -DCMINPACK_LIB_INSTALL_DIR=lib
+    ]
 
-    man3.install Dir["doc/*.3"]
-    doc.install Dir["doc/*"]
-    pkgshare.install "examples/thybrdc.c"
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    man3.install Dir["docs/*.3"]
+    doc.install Dir["docs/*"]
   end
 
   test do
-    system ENV.cc, "-I#{include}/cminpack-1", pkgshare/"thybrdc.c",
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <cminpack.h>
+
+      int main() {
+          int m = 2;
+          int n = 2;
+          double x[2] = {-1.2, 1.0};
+          double fvec[2] = {0};
+          double fjac[4] = {0};
+          double tol = 1e-8;
+          int info = -1;
+          int ipvt[2] = {0};
+          int ldfjac = 2;
+          int lwa = m * n + 5 * n + m;
+          double wa[lwa];
+
+          for (int i = 0; i < lwa; i++) {
+              wa[i] = 0;
+          }
+
+          info = lmder1(NULL, NULL, 0, n, x, fvec, fjac, ldfjac, tol, ipvt, wa, lwa);
+
+          if (info >= 0) {
+              printf("Success: lmder1 returned %d\\n", info);
+          } else {
+              printf("Error: lmder1 returned %d\\n", info);
+          }
+
+          return info;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}/cminpack-1",
                    "-L#{lib}", "-lcminpack", "-lm", "-o", "test"
-    assert_match "number of function evaluations", shell_output("./test")
+    system "./test"
   end
 end
