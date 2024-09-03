547427d2ef646d4416de20f2769a0385ecdbd738
Rui Chen <rui@chenrui.dev>
2024-09-03 15:41:23 -0400

    mubeng 0.15.3
    autobump: add mubeng
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    mubeng: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mubeng.rb
+++ Formula/m/mubeng.rb
@@ -1,8 +1,8 @@
 class Mubeng < Formula
   desc "Incredibly fast proxy checker & IP rotator with ease"
   homepage "https://github.com/kitabisa/mubeng"
-  url "https://github.com/kitabisa/mubeng/archive/refs/tags/v0.14.2.tar.gz"
-  sha256 "27842f0d587ced3b79b3c5e68be7e59272b0f2e89f754e3322d17bf7eda6802c"
+  url "https://github.com/kitabisa/mubeng/archive/refs/tags/v0.15.3.tar.gz"
+  sha256 "3970d542404ff25df673012280d90fef67e0be1489fd2ad429df4a9c47e5ce5e"
   license "Apache-2.0"
   head "https://github.com/kitabisa/mubeng.git", branch: "master"
 
@@ -19,13 +19,13 @@ class Mubeng < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X ktbs.dev/mubeng/common.Version=#{version}"
+    ldflags = "-s -w -X github.com/kitabisa/mubeng/common.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/mubeng"
   end
 
   test do
-    output = shell_output("#{bin}/mubeng 2>&1", 1)
-    assert_match "no proxy file provided", output
+    expected = OS.mac? ? "no proxy file provided" : "has no valid proxy URLs"
+    assert_match expected, shell_output("#{bin}/mubeng 2>&1", 1)
 
     assert_match version.to_s, shell_output("#{bin}/mubeng --version", 1)
   end
