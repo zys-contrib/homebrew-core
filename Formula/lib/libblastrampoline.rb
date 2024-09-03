7eda2578fd9d5aea802815fee4ebc5fd98286628
Rui Chen <rui@chenrui.dev>
2024-09-03 17:24:21 +0000

    libblastrampoline: add 5.11.0 bottle.

--- Formula/lib/libblastrampoline.rb
+++ Formula/lib/libblastrampoline.rb
@@ -9,6 +9,16 @@ class Libblastrampoline < Formula
     "BSD-3-Clause",       # include/common/lapacke*
   ]
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "70c25f76601949269e21048be82f671ca0d97bf5b71c11eddae338b3916b198f"
+    sha256 cellar: :any,                 arm64_ventura:  "51cee0c324df3077f746332e9a428d0ca1b7d81faab257e371b2d92147b02973"
+    sha256 cellar: :any,                 arm64_monterey: "254d9acb9cfb8be58703c06e621cd4df120bac6c1464eb6bdc73325b0e3002d8"
+    sha256 cellar: :any,                 sonoma:         "48f2f7813d5b7c04267e924fed7ba578acb8e4e139d70df80a2dc8cb4ebbfdd7"
+    sha256 cellar: :any,                 ventura:        "84e50ef11eb134a5822d1f765f91ce8235c6ff675020b3bc33a8b6951b0f64c9"
+    sha256 cellar: :any,                 monterey:       "f3fb195e78ad38bdcdf6d5e68f3290e72ff435d85a9bc9052f5db1bd4bcccfa5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5b45c0f742c4f3699842d75b19438efc8a49816ab82b46352a4a197ce63fed42"
+  end
+
   depends_on "openblas" => :test
 
   def install
