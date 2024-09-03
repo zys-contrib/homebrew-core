f4c7dfb5acd932c4ff3878591ce9843f8624965f
Michael Cho <michael@michaelcho.dev>
2024-09-03 00:03:18 -0400

    grpc: revision bump (protobuf 28.0)

--- Formula/g/grpc.rb
+++ Formula/g/grpc.rb
@@ -2,7 +2,7 @@ class Grpc < Formula
   desc "Next generation open source RPC library and framework"
   homepage "https://grpc.io/"
   license "Apache-2.0"
-  revision 4
+  revision 5
   head "https://github.com/grpc/grpc.git", branch: "master"
 
   stable do
