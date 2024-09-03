8dc4b73e9608d0b340c7e70a55c922890f845fd8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-03 15:57:37 +0000

    qsv 0.133.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/q/qsv.rb
+++ Formula/q/qsv.rb
@@ -1,8 +1,8 @@
 class Qsv < Formula
   desc "Ultra-fast CSV data-wrangling toolkit"
   homepage "https://github.com/jqnatividad/qsv"
-  url "https://github.com/jqnatividad/qsv/archive/refs/tags/0.132.0.tar.gz"
-  sha256 "bc7a1405a1543a6930d3478d80288d213acc3104a0510a7bceffc2bb32b972e0"
+  url "https://github.com/jqnatividad/qsv/archive/refs/tags/0.133.1.tar.gz"
+  sha256 "f2f59c07ea8d84d527641b6f6c2e1eeff114d04860d2904a2b85361ea62db204"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/jqnatividad/qsv.git", branch: "master"
 
@@ -32,9 +32,9 @@ class Qsv < Formula
   test do
     (testpath/"test.csv").write("first header,second header")
     assert_equal <<~EOS, shell_output("#{bin}/qsv stats test.csv")
-      field,type,is_ascii,sum,min,max,range,min_length,max_length,mean,sem,stddev,variance,cv,nullcount,max_precision,sparsity
-      first header,NULL,,,,,,,,,,,,,0,,
-      second header,NULL,,,,,,,,,,,,,0,,
+      field,type,is_ascii,sum,min,max,range,sort_order,min_length,max_length,mean,sem,stddev,variance,cv,nullcount,max_precision,sparsity
+      first header,NULL,,,,,,,,,,,,,,0,,
+      second header,NULL,,,,,,,,,,,,,,0,,
     EOS
   end
 end
