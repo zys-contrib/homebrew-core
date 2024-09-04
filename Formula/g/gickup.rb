857206e6bf7c650b7d66afb9f3391ef509db8307
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-04 18:35:38 +0000

    gickup 0.10.36

--- Formula/g/gickup.rb
+++ Formula/g/gickup.rb
@@ -1,8 +1,8 @@
 class Gickup < Formula
   desc "Backup all your repositories with Ease"
   homepage "https://cooperspencer.github.io/gickup-documentation/"
-  url "https://github.com/cooperspencer/gickup/archive/refs/tags/v0.10.35.tar.gz"
-  sha256 "dcf397c4c253858cb279acd991e53093e28e30ab1557b770bc2102599312c502"
+  url "https://github.com/cooperspencer/gickup/archive/refs/tags/v0.10.36.tar.gz"
+  sha256 "208de2a724fbcdbc7d2b8ec38d8d61451fe8967bab5329ca4400c323378e53da"
   license "Apache-2.0"
   head "https://github.com/cooperspencer/gickup.git", branch: "main"
 
