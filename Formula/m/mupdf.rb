d9a96603ad52d04146797dbafa6f99483148b5b4
Michael Cho <michael@michaelcho.dev>
2024-09-04 22:31:22 -0400

    mupdf 1.24.9

--- Formula/m/mupdf.rb
+++ Formula/m/mupdf.rb
@@ -1,10 +1,9 @@
 class Mupdf < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  url "https://mupdf.com/downloads/archive/mupdf-1.23.11-source.tar.gz"
-  sha256 "478f2a167feae2a291c8b8bc5205f2ce2f09f09b574a6eb0525bfad95a3cfe66"
+  url "https://mupdf.com/downloads/archive/mupdf-1.24.9-source.tar.gz"
+  sha256 "0b446aa0eecc114e9969dccd70c9789358fccb6589a81d470dc941a0874da98a"
   license "AGPL-3.0-or-later"
-  revision 1
   head "https://git.ghostscript.com/mupdf.git", branch: "master"
 
   livecheck do
@@ -22,14 +21,20 @@ class Mupdf < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "aafc7ee71749329c186c19c1d2aaa8cca880aa81e56f788a150de6070e8e0cb1"
   end
 
+  depends_on "llvm" => :build
   depends_on "pkg-config" => :build
+  depends_on "swig" => :build
   depends_on "freetype"
+  depends_on "gumbo-parser"
   depends_on "harfbuzz"
   depends_on "jbig2dec"
   depends_on "jpeg-turbo"
+  depends_on "leptonica"
   depends_on "mujs"
   depends_on "openjpeg"
   depends_on "openssl@3"
+  depends_on "python@3.12"
+  depends_on "tesseract"
 
   uses_from_macos "zlib"
 
@@ -43,34 +48,62 @@ class Mupdf < Formula
   conflicts_with "mupdf-tools", because: "both install the same binaries"
 
   def install
-    # Remove bundled libraries excluding `extract`, `gumbo-parser` (deprecated), and
-    # "strongly preferred" `lcms2mt` (lcms2 fork)
-    keep = %w[extract gumbo-parser lcms2]
+    # Remove bundled libraries excluding `extract` and "strongly preferred" `lcms2mt` (lcms2 fork)
+    keep = %w[extract lcms2]
     (buildpath/"thirdparty").each_child { |path| rm_r(path) if keep.exclude? path.basename.to_s }
+
+    # For python bindings needed by `pymupdf`: https://pymupdf.readthedocs.io/en/latest/packaging.html
+    site_packages = Language::Python.site_packages("python3.12")
+    ENV.prepend_path "PYTHONPATH", Formula["llvm"].opt_prefix/site_packages
 
     args = %W[
       build=release
       shared=yes
+      tesseract=yes
       verbose=yes
       prefix=#{prefix}
+      pydir=#{prefix/site_packages}
       CC=#{ENV.cc}
-      USE_SYSTEM_GUMBO=no
       USE_SYSTEM_LIBS=yes
       USE_SYSTEM_MUJS=yes
+      VENV_FLAG=
     ]
+
     # Build only runs pkg-config for libcrypto on macOS, so help find other libs
     if OS.mac?
       [
         ["FREETYPE", "freetype2"],
+        ["GUMBO", "gumbo"],
         ["HARFBUZZ", "harfbuzz"],
+        ["LEPTONICA", "lept"],
         ["LIBJPEG", "libjpeg"],
         ["OPENJPEG", "libopenjp2"],
       ].each do |argname, libname|
         args << "SYS_#{argname}_CFLAGS=#{Utils.safe_popen_read("pkg-config", "--cflags", libname).strip}"
         args << "SYS_#{argname}_LIBS=#{Utils.safe_popen_read("pkg-config", "--libs", libname).strip}"
+        args << "HAVE_SYS_#{argname}=yes"
       end
+
+      # Workarounds since build scripts for Python bindings don't support macOS
+      # Issue ref: https://bugs.ghostscript.com/show_bug.cgi?id=705376
+      inreplace "Makefile" do |s|
+        # Avoid creating a symlink that overwrites installed file
+        s.gsub!(/^\s*ln -sf libmupdf/, "#\\0")
+
+        # FIXME: libmupdfcpp should be a shared lib (.dylib) while _mupdf should be a bundle
+        # (.so) as the former is a C++ library installed into `lib` while latter is loaded by
+        # Python bindings. However, the python build scripts hardcode `.so` and uses `-shared`
+        # which result in neither being correct. Also, the Makefile installs with $(SO) which
+        # fails to find `.so`. For now we do the easier workaround of installing as `.so`.
+        s.gsub! "libmupdfcpp.$(SO)", "libmupdfcpp.so"
+        s.gsub! "_mupdf.$(SO)", "_mupdf.so"
+      end
+
+      ENV.cxx11
     end
+
     system "make", "install", *args
+    system "make", "install-shared-python", *args
 
     # Symlink `mutool` as `mudraw` (a popular shortcut for `mutool draw`).
     bin.install_symlink bin/"mutool" => "mudraw"
