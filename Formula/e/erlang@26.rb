9c633822e21aca9643bf51e0b0fc64d9adf79218
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-09-05 22:32:51 +0100

    erlang@26
    Allow for dependants to depend on this new formula
    (as copied from erlang.rb)

---
+++ Formula/e/erlang@26.rb
@@ -0,0 +1,106 @@
+class ErlangAT26 < Formula
+  desc "Programming language for highly scalable real-time systems"
+  homepage "https://www.erlang.org/"
+  # Download tarball from GitHub; it is served faster than the official tarball.
+  # Don't forget to update the documentation resource along with the url!
+  url "https://github.com/erlang/otp/releases/download/OTP-26.2.5/otp_src_26.2.5.tar.gz"
+  sha256 "de155c4ad9baab2b9e6c96dbd03bf955575a04dd6feee9c08758beb28484c9f6"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^OTP[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "openssl@3"
+  depends_on "unixodbc"
+  depends_on "wxwidgets" # for GUI apps like observer
+
+  uses_from_macos "libxslt" => :build
+
+  resource "html" do
+    url "https://github.com/erlang/otp/releases/download/OTP-26.2.5/otp_doc_html_26.2.5.tar.gz"
+    sha256 "7c9c99695f8b0218f655ec154954bac1d70e25fe3d54810cf81590e1eb711e47"
+  end
+
+  def install
+    odie "html resource needs to be updated" if version != resource("html").version
+
+    # Unset these so that building wx, kernel, compiler and
+    # other modules doesn't fail with an unintelligible error.
+    %w[LIBS FLAGS AFLAGS ZFLAGS].each { |k| ENV.delete("ERL_#{k}") }
+
+    # Do this if building from a checkout to generate configure
+    system "./otp_build", "autoconf" unless File.exist? "configure"
+
+    args = %W[
+      --enable-dynamic-ssl-lib
+      --enable-hipe
+      --enable-shared-zlib
+      --enable-smp-support
+      --enable-threads
+      --enable-wx
+      --with-odbc=#{Formula["unixodbc"].opt_prefix}
+      --with-ssl=#{Formula["openssl@3"].opt_prefix}
+      --without-javac
+    ]
+
+    if OS.mac?
+      args << "--enable-darwin-64bit"
+      args << "--enable-kernel-poll" if MacOS.version > :el_capitan
+      args << "--with-dynamic-trace=dtrace" if MacOS::CLT.installed?
+    end
+
+    system "./configure", *std_configure_args, *args
+    system "make"
+    system "make", "install"
+
+    # Build the doc chunks (manpages are also built by default)
+    ENV.deparallelize { system "make", "docs", "DOC_TARGETS=chunks" }
+    ENV.deparallelize { system "make", "install-docs" }
+
+    doc.install resource("html")
+  end
+
+  def caveats
+    <<~EOS
+      Man pages can be found in:
+        #{opt_lib}/erlang/man
+
+      Access them with `erl -man`, or add this directory to MANPATH.
+    EOS
+  end
+
+  test do
+    system bin/"erl", "-noshell", "-eval", "crypto:start().", "-s", "init", "stop"
+
+    (testpath/"factorial").write <<~EOS
+      #!#{bin}/escript
+      %% -*- erlang -*-
+      %%! -smp enable -sname factorial -mnesia debug verbose
+      main([String]) ->
+          try
+              N = list_to_integer(String),
+              F = fac(N),
+              io:format("factorial ~w = ~w\n", [N,F])
+          catch
+              _:_ ->
+                  usage()
+          end;
+      main(_) ->
+          usage().
+
+      usage() ->
+          io:format("usage: factorial integer\n").
+
+      fac(0) -> 1;
+      fac(N) -> N * fac(N-1).
+    EOS
+
+    chmod 0755, "factorial"
+    assert_match "usage: factorial integer", shell_output("./factorial")
+    assert_match "factorial 42 = 1405006117752879898543142606244511569936384000000000", shell_output("./factorial 42")
+  end
+end
