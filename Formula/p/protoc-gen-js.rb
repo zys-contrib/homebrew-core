bd71e2a039ef2ea8121a8a48a582d3a3da59deea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-06 18:37:36 +0000

    protoc-gen-js: update 3.21.4 bottle.

--- Formula/p/protoc-gen-js.rb
+++ Formula/p/protoc-gen-js.rb
@@ -7,13 +7,13 @@ class ProtocGenJs < Formula
   head "https://github.com/protocolbuffers/protobuf-javascript.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "cc2ba4ee9b1a566ee47dafd96f73dce89b383291c1504950aad5334c7945d661"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "596914c981706cef366b362d4f514a35b6697baeb7646f8d323779ac0a1167a7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "37e7fbdf46b8b8a5df2d7f21458bf8bb613ed96d5ce0599b7ecd7038a691938e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a444f2d40d5032e35985f1d9f10994fb69171bd318fe4ad43c01292cc66bfb94"
-    sha256 cellar: :any_skip_relocation, ventura:        "7d18411c33bca80e42e2ab2ee14003d5bda1fc185192f656716bb2491ce58528"
-    sha256 cellar: :any_skip_relocation, monterey:       "82495e5cd063520c537b811bb6a14c11371c7d540b4f31412074a55ff4883a5c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb375b65b3a2fbfa936054f5899c0a9ac5324c027475133d3b5ed36d5da2f055"
+    sha256 cellar: :any,                 arm64_sonoma:   "e88d8c9da16d90a087cc06fd673be15d0e0fbb994db142aace7f7d60b47ab98f"
+    sha256 cellar: :any,                 arm64_ventura:  "e9eb75b369a9299861a58c79ab2be463859c715492d09ae74d762e7bd255c8c1"
+    sha256 cellar: :any,                 arm64_monterey: "fd3b137f6fb2a3ef81352949a399c8a3674f04331903f054732fa13740e3a2d3"
+    sha256 cellar: :any,                 sonoma:         "8d3e2bc657967f30ea03409f6942fc9f6e1c4297a36266dc82872bc9b20d1299"
+    sha256 cellar: :any,                 ventura:        "76a813a7c9228db8c43d7860ffe1ba0c421aaf0c5e5932a65579e78a57e61c14"
+    sha256 cellar: :any,                 monterey:       "c381ff81bee4bc885a5f94d3369cb2ae94ed2edba0309ffbd34f0725cf2c94e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4cf5bc2842890a2fa165808f0ccd944f952f42a1d18573def7c21d7516dadf71"
   end
 
   depends_on "pkg-config" => :build
