cd5d37bb579dd1fb7b53ac8d508446151fe016a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-06 22:45:24 +0000

    python-build: update 1.2.2 bottle.

--- Formula/p/python-build.rb
+++ Formula/p/python-build.rb
@@ -9,13 +9,13 @@ class PythonBuild < Formula
   head "https://github.com/pypa/build.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8a02b06273633d165aeb7f3a6f9c799c688e95a3e0e6a99dff4447c0d174b49c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8a02b06273633d165aeb7f3a6f9c799c688e95a3e0e6a99dff4447c0d174b49c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8a02b06273633d165aeb7f3a6f9c799c688e95a3e0e6a99dff4447c0d174b49c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "40ca53f82b413997243774f2adf79eb0baa6900d9ed6db87c4269fa8d43f1908"
-    sha256 cellar: :any_skip_relocation, ventura:        "40ca53f82b413997243774f2adf79eb0baa6900d9ed6db87c4269fa8d43f1908"
-    sha256 cellar: :any_skip_relocation, monterey:       "40ca53f82b413997243774f2adf79eb0baa6900d9ed6db87c4269fa8d43f1908"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7f685d6de8ad4b0d9779ea800b6c3b4a5f7af3caa5d4b8ea8064dff279fb6906"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "058b5a61e8df225c99753f870b5f23801dccd34d79bc2193f7e8c2b0f8cc3895"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "058b5a61e8df225c99753f870b5f23801dccd34d79bc2193f7e8c2b0f8cc3895"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "058b5a61e8df225c99753f870b5f23801dccd34d79bc2193f7e8c2b0f8cc3895"
+    sha256 cellar: :any_skip_relocation, sonoma:         "d4ea94bfdc112f2ae7026b4eef2353969fca988ea4d80f57a76ef28b0796cf1c"
+    sha256 cellar: :any_skip_relocation, ventura:        "d4ea94bfdc112f2ae7026b4eef2353969fca988ea4d80f57a76ef28b0796cf1c"
+    sha256 cellar: :any_skip_relocation, monterey:       "d4ea94bfdc112f2ae7026b4eef2353969fca988ea4d80f57a76ef28b0796cf1c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "058b5a61e8df225c99753f870b5f23801dccd34d79bc2193f7e8c2b0f8cc3895"
   end
 
   depends_on "python@3.12"
