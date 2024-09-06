016c97d2ba8b1b094d945a4517afd7da92b4cd7a
Rui Chen <rui@chenrui.dev>
2024-09-06 14:06:44 -0400

    gimme 1.5.6
    autobump: ådd gimme
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gimme.rb
+++ Formula/g/gimme.rb
@@ -1,8 +1,8 @@
 class Gimme < Formula
   desc "Shell script to install any Go version"
   homepage "https://github.com/travis-ci/gimme"
-  url "https://github.com/travis-ci/gimme/archive/refs/tags/v1.5.5.tar.gz"
-  sha256 "7854c3f90c2274e14041138f53c9c0bf671be86e49dfb4a61b024270a514fb40"
+  url "https://github.com/travis-ci/gimme/archive/refs/tags/v1.5.6.tar.gz"
+  sha256 "50c42ec01505bee0e5b60165a0f577fe1e08fe9278fe3fe4b073c521f781c61e"
   license "MIT"
 
   bottle do
