b6e3eaff48f4401a1aff2ba73287e465f582d577
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-09-07 08:27:29 +0000

    bed: add 0.2.5 bottle.

--- Formula/b/bed.rb
+++ Formula/b/bed.rb
@@ -6,6 +6,16 @@ class Bed < Formula
   license "MIT"
   head "https://github.com/itchyny/bed.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "eb29ee4f9d369143a3177e52c4e6e412e76baec765ecf20f16334f117d5287fe"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "eb29ee4f9d369143a3177e52c4e6e412e76baec765ecf20f16334f117d5287fe"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "eb29ee4f9d369143a3177e52c4e6e412e76baec765ecf20f16334f117d5287fe"
+    sha256 cellar: :any_skip_relocation, sonoma:         "9c23fbc4983730757f05f06f6e69250062e291f945efc4433b051a285d2ec342"
+    sha256 cellar: :any_skip_relocation, ventura:        "9c23fbc4983730757f05f06f6e69250062e291f945efc4433b051a285d2ec342"
+    sha256 cellar: :any_skip_relocation, monterey:       "9c23fbc4983730757f05f06f6e69250062e291f945efc4433b051a285d2ec342"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d12893a697b18fcc08cff0db017ddffeb5754f7e514a6d8cdfc3b81665936a22"
+  end
+
   depends_on "go" => :build
 
   def install
