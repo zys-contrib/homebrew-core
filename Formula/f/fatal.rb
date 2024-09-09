59b3a8aec4904e0e5c083f7bcafeb65075cea28b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-10 02:33:24 +0800

    fatal 2024.09.09.00 (new formula)

---
+++ Formula/f/fatal.rb
@@ -0,0 +1,25 @@
+class Fatal < Formula
+  desc "Facebook Template Library"
+  homepage "https://www.facebook.com/groups/libfatal/"
+  url "https://github.com/facebook/fatal/releases/download/v2024.09.09.00/fatal-v2024.09.09.00.tar.gz"
+  sha256 "b5d1c450b04ba56eebd39863a3962a0bdbdf0e6f75bc8f8440bdd426f01c81c3"
+  license "BSD-3-Clause"
+  head "https://github.com/facebook/fatal.git", branch: "main"
+
+  def install
+    rm "fatal/.clang-tidy"
+    include.install "fatal"
+    pkgshare.install "demo", "lesson", *buildpath.glob("*.sh")
+    inreplace "README.md" do |s|
+      s.gsub!("(lesson/)", "(share/fatal/lesson/)")
+      s.gsub!("(demo/)", "(share/fatal/demo/)")
+    end
+  end
+
+  test do
+    system ENV.cxx, "-std=c++14", "-I#{include}",
+                    include/"fatal/benchmark/test/benchmark_test.cpp",
+                    "-o", "benchmark_test"
+    system "./benchmark_test"
+  end
+end
