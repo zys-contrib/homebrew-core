a529aef3a09fd9a6d220f7bf8ad115b0978e8909
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-09 20:49:21 +0000

    kubekey 3.1.6

--- Formula/k/kubekey.rb
+++ Formula/k/kubekey.rb
@@ -2,8 +2,8 @@ class Kubekey < Formula
   desc "Installer for Kubernetes and / or KubeSphere, and related cloud-native add-ons"
   homepage "https://kubesphere.io"
   url "https://github.com/kubesphere/kubekey.git",
-      tag:      "v3.1.5",
-      revision: "8347277057bf9f84e89fec174019a675d582b23b"
+      tag:      "v3.1.6",
+      revision: "5cad5b5357e80fee211faed743c8f9d452c13b5b"
   license "Apache-2.0"
   head "https://github.com/kubesphere/kubekey.git", branch: "master"
 
