02aee30c5fb16cfd2e6ee4c9954e5f900a83b05f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-10 20:09:30 +0000

    istioctl: update 1.23.1 bottle.

--- Formula/i/istioctl.rb
+++ Formula/i/istioctl.rb
@@ -12,14 +12,13 @@ class Istioctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "0ce004768c3c13144fce13118ac2a76a0210f9bbc75f92a48ce4e976d193f865"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "597550de23075b6c6d5bd761463901fb2a0082cbad036aa6d4bda1b9438a4bb3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f1482b0bbedef2b5ab3b2b7e7dc489de56933ff0d02da57ecd2b4ee019c13b6c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f2b20320f2c5de3aa1573fab236a79aaa451314b92e7d67659fe361f24401db1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "90afdcfcbdb14799e60058649ebaebb94fea8cf9caf09aa198570e0b7cf873ad"
-    sha256 cellar: :any_skip_relocation, ventura:        "07e4438c6a7fe04349eb90f3f3c9411f473f2d643bf8f52c1991dfe6f6448f5a"
-    sha256 cellar: :any_skip_relocation, monterey:       "a908c065ea37a577690c6c91f57456b9a82764b0f22b890acfa28796aa398849"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cab719dda903558daeb5d48eb18a3b25069532f53144edf4a37b3853aadbe0ca"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "fe7d63528ef12bd50518341b9bdb261fb144cdddb84ba3e02140bbb6ccac6843"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f3bd242dfba512cfd62f8eb325bd293dea988a2034336d2fd6a72ff51ceee86e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4122198279283f72bd6b0d0221ca0a27b756413126591f8a843815507f88e49d"
+    sha256 cellar: :any_skip_relocation, sonoma:         "5b8c6bae59c43018bb0bd44f01c27b0171f8c688d12285fbf8fa70cd01847d41"
+    sha256 cellar: :any_skip_relocation, ventura:        "e288183b9f28721d6d59b557140cc44d2d3720827a834928a5d1c73ecfd07fcc"
+    sha256 cellar: :any_skip_relocation, monterey:       "0fa92f209e68a170b17fb97543678fd4ebefe276bf149ee23067a1fdcdab8ce1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5ede8b7ea425dd6abc05d2a6ca4b288af4798ee08683daba1a1faa8cd5fac93c"
   end
 
   depends_on "go" => :build
