080748424f95b1424d4a28c4e8ade0d18208a9c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-10 16:37:39 +0000

    couchbase-shell: update 1.0.0 bottle.

--- Formula/c/couchbase-shell.rb
+++ Formula/c/couchbase-shell.rb
@@ -7,15 +7,13 @@ class CouchbaseShell < Formula
   head "https://github.com/couchbaselabs/couchbase-shell.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "25b370cd8dbc1c5df81496309a679c713820cbbf34806b7ea56ac17174edb52b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "311bd08f0e1124ec4bcb6b10c4b0837cc153678ed436e7d1c876ea447c5ddd0f"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "332d09a84990ce0176557353b842d7ecdcfb3279b90eee9f6742be3c87eaa910"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "de39e28bff9b2e2d1cb5c2b54e10ee4bb1646fa19ea0ab3feef5729ba1ea7bfd"
-    sha256 cellar: :any_skip_relocation, sonoma:         "c4a61feed429597d57de9fb262c077cd133293c3fffe9deb44f66c2a2ef45168"
-    sha256 cellar: :any_skip_relocation, ventura:        "65515ea14fee716e8b7b6bd3c1afae1cefdff348b493a985ebdcc81c86c81f0d"
-    sha256 cellar: :any_skip_relocation, monterey:       "1c0ef9f1d4803f103ad256a3b8248867a9d3b25294bda98434b84229a1f78419"
-    sha256 cellar: :any_skip_relocation, big_sur:        "81798858544b739e736244e94098131f6e7d2c5ad3dedcff91aa641e0bd875f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ea805526c8d9cf548f9771302c502c5e3c809a2f09063d80e278e091170ac7a7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6b7fbbd9244f86b8a2b62e9b66031f6b626458cdd02bf98aa1678da2f6e1f057"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6c2a1deb9682f3abf26523e1ee3ee06373cda0070b4d1a40112705fd1a054341"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "95b26b103f2e99c9a19dfe8b3362fc387ce561fa56e802fb764eb11882d9c509"
+    sha256 cellar: :any_skip_relocation, sonoma:         "eb0a6ecb43eb94f41945d1b51b18c5c0af5d4dbce058ce67f3ee118235884065"
+    sha256 cellar: :any_skip_relocation, ventura:        "97c85165badffe574ea33ccc90a1ef50663a43a3113e0dda0430906009df2752"
+    sha256 cellar: :any_skip_relocation, monterey:       "b5324717a89c5d0f84710c0c349e2fb0c7c9cfa8ba582c2c7163f9910c0f64de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "85e214221dd2de75cbc472660b3ca76f9909be8dc3da77de19d2934030caace9"
   end
 
   depends_on "rust" => :build
