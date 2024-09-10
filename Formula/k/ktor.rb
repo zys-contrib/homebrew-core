dc3461924cd9dfc3d5ffa2efed8312edab110b0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-10 09:53:20 +0000

    ktor: add 0.2.0 bottle.

--- Formula/k/ktor.rb
+++ Formula/k/ktor.rb
@@ -6,6 +6,16 @@ class Ktor < Formula
   license "Apache-2.0"
   head "https://github.com/ktorio/ktor-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e2477ed41bf2cfe39653ddc1e61cd370eb8a79574798621aef5c3adfed537661"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e2477ed41bf2cfe39653ddc1e61cd370eb8a79574798621aef5c3adfed537661"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2477ed41bf2cfe39653ddc1e61cd370eb8a79574798621aef5c3adfed537661"
+    sha256 cellar: :any_skip_relocation, sonoma:         "0eb5d96c0bafe248c56d9434420403cf7e3d106aaf4da2397d2a090ce84cf787"
+    sha256 cellar: :any_skip_relocation, ventura:        "0eb5d96c0bafe248c56d9434420403cf7e3d106aaf4da2397d2a090ce84cf787"
+    sha256 cellar: :any_skip_relocation, monterey:       "0eb5d96c0bafe248c56d9434420403cf7e3d106aaf4da2397d2a090ce84cf787"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ed8ea3a6a761a8f040b936e3cb4ee5a6a95e02989cc94dd1837fcbdbd3a3172"
+  end
+
   depends_on "go" => :build
 
   def install
