c5a38026b0fd444e082389301112077013aed409
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-10 02:41:45 +0000

    storj-uplink: update 1.111.4 bottle.

--- Formula/s/storj-uplink.rb
+++ Formula/s/storj-uplink.rb
@@ -18,13 +18,13 @@ class StorjUplink < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bb8d08506c7b3366bea6b1d56a57aefb82cfdce1a270f632cb353fbc960833e0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bb8d08506c7b3366bea6b1d56a57aefb82cfdce1a270f632cb353fbc960833e0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bb8d08506c7b3366bea6b1d56a57aefb82cfdce1a270f632cb353fbc960833e0"
-    sha256 cellar: :any_skip_relocation, sonoma:         "aaac9a78dfdc5d7a3a95e4ab39445eb9badb6ec4ff8ec7fe3e689cb86a18b794"
-    sha256 cellar: :any_skip_relocation, ventura:        "aaac9a78dfdc5d7a3a95e4ab39445eb9badb6ec4ff8ec7fe3e689cb86a18b794"
-    sha256 cellar: :any_skip_relocation, monterey:       "aaac9a78dfdc5d7a3a95e4ab39445eb9badb6ec4ff8ec7fe3e689cb86a18b794"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3179a6c407b4e81c891757af050f2cfcb1ff6719e45f80ee3027cf1449574c77"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "568bf2156e57d1306a46461d3dc656331b8db5ec8f0dc476387e62959c2cc958"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "568bf2156e57d1306a46461d3dc656331b8db5ec8f0dc476387e62959c2cc958"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "568bf2156e57d1306a46461d3dc656331b8db5ec8f0dc476387e62959c2cc958"
+    sha256 cellar: :any_skip_relocation, sonoma:         "24623db05452961cff77763ba93021e08160bd944bbe90ba331caeec933453a9"
+    sha256 cellar: :any_skip_relocation, ventura:        "24623db05452961cff77763ba93021e08160bd944bbe90ba331caeec933453a9"
+    sha256 cellar: :any_skip_relocation, monterey:       "24623db05452961cff77763ba93021e08160bd944bbe90ba331caeec933453a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7641d688af8b8c00ac182d2080ae13b173d53e0eb06ce4e2e744ca64cc249207"
   end
 
   depends_on "go" => :build
