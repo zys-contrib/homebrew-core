fd9ff12c3120dc501b15a162bf3a3b43580822dc
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-09-11 12:10:20 +0000

    cdk8s: update 2.198.214 bottle.

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -6,6 +6,7 @@ class Cdk8s < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "6da2a21226923a5a2a588487947f70eb1c50e7665cba9ff6dae1d129cb48f34b"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6da2a21226923a5a2a588487947f70eb1c50e7665cba9ff6dae1d129cb48f34b"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6da2a21226923a5a2a588487947f70eb1c50e7665cba9ff6dae1d129cb48f34b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6da2a21226923a5a2a588487947f70eb1c50e7665cba9ff6dae1d129cb48f34b"
