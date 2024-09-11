ec876df8da6d6007c9fe7c8a4ac07a6f3941a2d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-11 07:30:37 +0000

    jackett: update 0.22.584 bottle.

--- Formula/j/jackett.rb
+++ Formula/j/jackett.rb
@@ -7,13 +7,13 @@ class Jackett < Formula
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "917a7fd278672c155d550b3422d55d258a4749fb229dbdf8f9a485fb40f582c4"
-    sha256 cellar: :any,                 arm64_ventura:  "7b71c2d5cb2a62fe1216394af3dfcb9a8498aa3ee06666aaa99f0328a7b2751f"
-    sha256 cellar: :any,                 arm64_monterey: "24adec95f320a493652ccafb5623e623cfe0d8c369a83e5ca361d7787eb84b0e"
-    sha256 cellar: :any,                 sonoma:         "625d9b47c0d0d53a700c6abc1d7de4d3278263ba0850a2e5d25b8cc8fd7ca95d"
-    sha256 cellar: :any,                 ventura:        "afa02d96955c063be63e92944a45ef850a234a4f19308efbe1f5ee5a68c535f3"
-    sha256 cellar: :any,                 monterey:       "02180a3eb753603bcffa7a03e2d3fb150ebc5ed6ab9fe0931300028c54cc65ae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1190ba7c3b1a7c33d383e05ada76d3bb749cb1a7d2722dcbebf002b47f7e7cd1"
+    sha256 cellar: :any,                 arm64_sonoma:   "0a282cb761056468c02881fafbfb00f0afbee17da2432ccdd4e2ac03637a3beb"
+    sha256 cellar: :any,                 arm64_ventura:  "a12f4812d041e0effb5c4d8da992c2a4a3069d7a92adf949237621b95a1dbbbe"
+    sha256 cellar: :any,                 arm64_monterey: "f97d9eedfd782d72f43f1c6123db3f06e457e7b502755d0e830456e0a84ad436"
+    sha256 cellar: :any,                 sonoma:         "dfc8fe3924542224b91418f30be608fa25129faae69bbf609e5a15e64c9a569b"
+    sha256 cellar: :any,                 ventura:        "f31629db033827ddbafdfac0453bf6288d15f8ceb1c33952bd76fae13f38d5fc"
+    sha256 cellar: :any,                 monterey:       "5c87fb42a9f6533559d7050aec0d40d5593056cc35efeb0a0c7bfe5a7f447a96"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "965c4faf54cb1f07bc4af22b4f8b02dcb2ebb32330a1967eb1370830ed2a053d"
   end
 
   depends_on "dotnet"
