1683210f0fd9fe7df6881d23823d7297d8e24e15
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-12 16:33:57 +0800

    fastnetmon: revision bump (protobuf 28.1)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.6.tar.gz"
   sha256 "b6a7d1e9ba98c1c042d774bff82ea3e8bbf03085e0be43a2676e41d590f668cf"
   license "GPL-2.0-only"
-  revision 17
+  revision 18
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia:  "aaa6e4d54a1c50c139ae8297347a56869d53dd1f5ee486a0d8f9b940e88901cf"
