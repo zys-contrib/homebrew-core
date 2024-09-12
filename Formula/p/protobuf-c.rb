0351073f983ce5e808df70a62287a04412a527d6
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-12 16:34:07 +0800

    protobuf-c: revision bump (protobuf 28.1)

--- Formula/p/protobuf-c.rb
+++ Formula/p/protobuf-c.rb
@@ -4,7 +4,7 @@ class ProtobufC < Formula
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.5.0/protobuf-c-1.5.0.tar.gz"
   sha256 "7b404c63361ed35b3667aec75cc37b54298d56dd2bcf369de3373212cc06fd98"
   license "BSD-2-Clause"
-  revision 9
+  revision 10
 
   bottle do
     rebuild 1
