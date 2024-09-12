07b776c630d74fd8fbd786034bbe4fc0b13b66fc
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-12 16:33:57 +0800

    etcd-cpp-apiv3: revision bump (protobuf 28.1)

--- Formula/e/etcd-cpp-apiv3.rb
+++ Formula/e/etcd-cpp-apiv3.rb
@@ -4,7 +4,7 @@ class EtcdCppApiv3 < Formula
   url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.15.4.tar.gz"
   sha256 "4516ecfa420826088c187efd42dad249367ca94ea6cdfc24e3030c3cf47af7b4"
   license "BSD-3-Clause"
-  revision 11
+  revision 12
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia:  "a3e301db665d32c103a3f81bb8a8c66e9d34103c6fd9dda8d14f992ef6e8c001"
