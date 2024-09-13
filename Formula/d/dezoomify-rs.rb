c5a64d0d2f4784459304b2dcf9b6fd431b62ada6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-13 15:32:10 +0000

    dezoomify-rs 2.12.5

--- Formula/d/dezoomify-rs.rb
+++ Formula/d/dezoomify-rs.rb
@@ -1,8 +1,8 @@
 class DezoomifyRs < Formula
   desc "Tiled image downloader"
   homepage "https://github.com/lovasoa/dezoomify-rs"
-  url "https://github.com/lovasoa/dezoomify-rs/archive/refs/tags/v2.12.4.tar.gz"
-  sha256 "e566a0d3b5ca0d43d854fe61d4179c5a0e00df7ee30db8e433794c587a3007f7"
+  url "https://github.com/lovasoa/dezoomify-rs/archive/refs/tags/v2.12.5.tar.gz"
+  sha256 "aa3e34c257c6f4bda6afb485a9f787e8d99664fce0f8a87303d77f4cc7dd3c72"
   license "GPL-3.0-only"
   head "https://github.com/lovasoa/dezoomify-rs.git", branch: "master"
 
