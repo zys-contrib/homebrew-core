a8d07a4c214b11aac4545dda0d2337e0c172c9c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-14 23:15:49 +0000

    fanyi: update 9.0.4 bottle.

--- Formula/f/fanyi.rb
+++ Formula/f/fanyi.rb
@@ -6,14 +6,12 @@ class Fanyi < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "79e56b0b5ab12581cb88e33f0a1413b9dfaacaeb876c538b80fa25c18d8bad71"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "79e56b0b5ab12581cb88e33f0a1413b9dfaacaeb876c538b80fa25c18d8bad71"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "79e56b0b5ab12581cb88e33f0a1413b9dfaacaeb876c538b80fa25c18d8bad71"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "79e56b0b5ab12581cb88e33f0a1413b9dfaacaeb876c538b80fa25c18d8bad71"
-    sha256 cellar: :any_skip_relocation, sonoma:         "14b94f46a7c03117bc78e7a012325796e005d0a6ddc4d7eb6c595a692f3f3cb9"
-    sha256 cellar: :any_skip_relocation, ventura:        "14b94f46a7c03117bc78e7a012325796e005d0a6ddc4d7eb6c595a692f3f3cb9"
-    sha256 cellar: :any_skip_relocation, monterey:       "14b94f46a7c03117bc78e7a012325796e005d0a6ddc4d7eb6c595a692f3f3cb9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae5ef133343ea7ab08e3d3e643ccf365aac5cad11fc1709c89c1550bca46d552"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3fe5844f5e6970789a887cb7319ac1693b5e9cddfc0a813e016f0e094c1157c6"
+    sha256 cellar: :any_skip_relocation, ventura:       "3fe5844f5e6970789a887cb7319ac1693b5e9cddfc0a813e016f0e094c1157c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
   end
 
   depends_on "node"
