8e7c0cd5cc2af2364418fec187267ef79f4690cc
Patrick Linnane <patrick@linnane.io>
2024-09-13 22:46:14 -0700

    gollama 1.27.6 (new formula)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/g/gollama.rb
@@ -0,0 +1,35 @@
+class Gollama < Formula
+  desc "Go manage your Ollama models"
+  homepage "https://smcleod.net"
+  url "https://github.com/sammcj/gollama/archive/refs/tags/v1.27.6.tar.gz"
+  sha256 "88c027874b85f6e00b8b421bfe5789c21be6ee1b8ec529f0393a9f682bd9fd65"
+  license "MIT"
+  head "https://github.com/sammcj/gollama.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "ollama" => :test
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output(bin/"gollama -v")
+
+    port = free_port
+    ENV["OLLAMA_HOST"] = "localhost:#{port}"
+
+    pid = fork { exec "#{Formula["ollama"].opt_bin}/ollama", "serve" }
+    sleep 3
+    begin
+      assert_match "No matching models found.",
+        shell_output(bin/"gollama -h http://localhost:#{port} -s chatgpt")
+    ensure
+      Process.kill "SIGTERM", pid
+    end
+  end
+end
