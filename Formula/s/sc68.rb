c4d404dff1f8a1dda633d8bd9a65d010729ad12f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2024-09-14 22:30:21 +0000

    sc68: update 2.2.1 bottle.

--- Formula/s/sc68.rb
+++ Formula/s/sc68.rb
@@ -11,6 +11,7 @@ class Sc68 < Formula
   end
 
   bottle do
+    sha256 arm64_sequoia:  "530da22ced84ec1181ddf6887028d6a4e9fa955446a492c039e5c807d6f2ec82"
     sha256 arm64_sonoma:   "f6c0c19ce245cb76ed53a1e9d017eb6b598ac1834f5c7ff215978147498be18e"
     sha256 arm64_ventura:  "f30cf9999c9d98d9e0c2ecee6b0af0f5f550391ecfbfa51d8f5139dce0aaa0c6"
     sha256 arm64_monterey: "f0f1ad019d6ae62b500fe9395c6e2cc9fe902532c13a2ffef0001763da251433"
