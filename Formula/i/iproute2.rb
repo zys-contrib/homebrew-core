7c8e3ec7ed8ddfed8d2fefcec8dbf745c1239521
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-15 21:37:35 +0000

    iproute2 6.11.0

--- Formula/i/iproute2.rb
+++ Formula/i/iproute2.rb
@@ -1,8 +1,8 @@
 class Iproute2 < Formula
   desc "Linux routing utilities"
   homepage "https://wiki.linuxfoundation.org/networking/iproute2"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-6.10.0.tar.xz"
-  sha256 "91a62f82737b44905a00fa803369c447d549e914e9a2a4018fdd75b1d54e8dce"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-6.11.0.tar.xz"
+  sha256 "1f795398a04aeaacd06a8f6ace2cfd913c33fa5953ca99daae83bb5c534611c3"
   license "GPL-2.0-only"
   head "https://git.kernel.org/pub/scm/network/iproute2/iproute2.git", branch: "main"
 
