1f371ab5884ccc81913fb9da6c615fba58359a5d
Rui Chen <rui@chenrui.dev>
2024-09-15 10:40:50 -0400

    pegtl 3.2.8
    autobump: add pegtl
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pegtl.rb
+++ Formula/p/pegtl.rb
@@ -1,8 +1,8 @@
 class Pegtl < Formula
   desc "Parsing Expression Grammar Template Library"
   homepage "https://github.com/taocpp/PEGTL"
-  url "https://github.com/taocpp/PEGTL/archive/refs/tags/3.2.7.tar.gz"
-  sha256 "d6cd113d8bd14e98bcbe7b7f8fc1e1e33448dc359e8cd4cca30e034ec2f0642d"
+  url "https://github.com/taocpp/PEGTL/archive/refs/tags/3.2.8.tar.gz"
+  sha256 "319e8238daebc3a163f60c88c78922a8012772076fdd64a8dafaf5619cd64773"
   # license got changed to BSL-1.0 in main per https://github.com/taocpp/PEGTL/commit/c7630f1649906daf08b8ddca1420e66b542bae2b
   license "MIT"
 
