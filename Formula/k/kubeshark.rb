fdb95078bbe9d7b0021d4140c6b28d27333d0dbc
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2024-09-14 17:25:23 -0700

    kubeshark 52.3.82
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/k/kubeshark.rb
+++ Formula/k/kubeshark.rb
@@ -1,8 +1,8 @@
 class Kubeshark < Formula
   desc "API Traffic Analyzer providing real-time visibility into Kubernetes network"
   homepage "https://www.kubeshark.co/"
-  url "https://github.com/kubeshark/kubeshark/archive/refs/tags/v52.3.79.tar.gz"
-  sha256 "682f3c8622c9a75905de1528f49d96a77294bfe5dc697314b0b0f3a6e34fb78b"
+  url "https://github.com/kubeshark/kubeshark/archive/refs/tags/v52.3.82.tar.gz"
+  sha256 "6a1b0938ba6a5521d0d8a21cadb821a14cb622eb3200b62f12c4cb050cacc263"
   license "Apache-2.0"
 
   # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
