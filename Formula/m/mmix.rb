ce6bb12e2e6a098590f622b0741ccd957d905ef8
Rui Chen <rui@chenrui.dev>
2024-09-15 11:16:07 -0400

    mmix: add upstream build patch for newer clang and gcc 10+
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mmix.rb
+++ Formula/m/mmix.rb
@@ -29,6 +29,16 @@ class Mmix < Formula
 
   depends_on "cweb" => :build
 
+  # fix implicit int build error
+  # upstream patch ref, https://gitlab.lrz.de/mmix/mmixware/-/commit/c02e7081d033895dfaeb8154ad9bd6f5893487ea
+  patch :DATA
+
+  # fix duplicate declaration of buffer
+  patch do
+    url "https://gitlab.lrz.de/mmix/mmixware/-/commit/2eddd633bc98fd320e317bbcd6c98399250e68ec.diff"
+    sha256 "512fc7d27b974bf5a58781464d4dba1c2147142ba749a2eb17c1a7b358ef8db9"
+  end
+
   def install
     ENV.deparallelize
     system "make", "all"
@@ -52,3 +62,18 @@ class Mmix < Formula
     assert_equal "Hello world!", shell_output("#{bin}/mmix hello.mmo")
   end
 end
+
+__END__
+diff --git a/abstime.w b/abstime.w
+index 50d6aa9f7585afae69ff22ee9b58a919c2c1db97..6605ba1071995e70b3e435009b52f5f3c2f7ea72 100644
+--- a/abstime.w
++++ b/abstime.w
+@@ -18,7 +18,7 @@ hold more than 32 bits.
+ #include <stdio.h>
+ #include <time.h>
+ @#
+-main()
++int main()
+ {
+   printf("#define ABSTIME %ld\n",time(NULL));
+   return 0;
