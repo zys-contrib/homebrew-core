821417d5e78170f9686ed2fcf6b182658f77667c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-16 09:23:40 +0000

    adwaita-icon-theme 47.0

--- Formula/a/adwaita-icon-theme.rb
+++ Formula/a/adwaita-icon-theme.rb
@@ -1,8 +1,8 @@
 class AdwaitaIconTheme < Formula
   desc "Icons for the GNOME project"
   homepage "https://developer.gnome.org"
-  url "https://download.gnome.org/sources/adwaita-icon-theme/46/adwaita-icon-theme-46.2.tar.xz"
-  sha256 "beb126b9429339ba762e0818d5e73b2c46f444975bf80076366eae2d0f96b5cb"
+  url "https://download.gnome.org/sources/adwaita-icon-theme/47/adwaita-icon-theme-47.0.tar.xz"
+  sha256 "ad088a22958cb8469e41d9f1bba0efb27e586a2102213cd89cc26db2e002bdfe"
   license any_of: ["LGPL-3.0-or-later", "CC-BY-SA-3.0"]
 
   bottle do
