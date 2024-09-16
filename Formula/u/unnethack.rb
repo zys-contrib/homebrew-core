f935f4d01616b6d9cea78d4ccc8efea9660c972a
Michael Cho <michael@michaelcho.dev>
2024-09-15 22:52:15 -0400

    unnethack: workaround for Sequoia, fix HEAD

--- Formula/u/unnethack.rb
+++ Formula/u/unnethack.rb
@@ -1,10 +1,24 @@
 class Unnethack < Formula
   desc "Fork of Nethack"
   homepage "https://unnethack.wordpress.com/"
-  url "https://github.com/UnNetHack/UnNetHack/archive/refs/tags/5.3.2.tar.gz"
-  sha256 "a32a2c0e758eb91842033d53d43f718f3bc719a346e993d9b23bac06f0ac9004"
   license "NGPL"
-  head "https://github.com/UnNetHack/UnNetHack.git", branch: "master"
+
+  stable do
+    url "https://github.com/UnNetHack/UnNetHack/archive/refs/tags/5.3.2.tar.gz"
+    sha256 "a32a2c0e758eb91842033d53d43f718f3bc719a346e993d9b23bac06f0ac9004"
+
+    # Apply upstream commit to fix build with newer bison. Remove with next release.
+    patch do
+      url "https://github.com/UnNetHack/UnNetHack/commit/04f0a3a850a94eb8837ddcef31303968240d1c31.patch?full_index=1"
+      sha256 "5285dc2e57b378bc77c01879399e2af248ef967977ed50e0c13a80b1993a7081"
+    end
+
+    # Fix implicit `ioctl` function declaration. Remove with the next release.
+    patch do
+      url "https://github.com/UnNetHack/UnNetHack/commit/33a3bb6539452875a88efbf6da0148a1cccc00c1.patch?full_index=1"
+      sha256 "07e1bb472c4f20957dafc6cfc49fcfd3178a5e04fcebf93a4fc7922ec8c0a963"
+    end
+  end
 
   livecheck do
     url :stable
@@ -26,6 +40,12 @@ class Unnethack < Formula
     sha256 x86_64_linux:   "31307b80abcdcf33c36d3716969e3a2b8202d80e6ea79574f3689d21eb3faac5"
   end
 
+  head do
+    url "https://github.com/UnNetHack/UnNetHack.git", branch: "master"
+
+    depends_on "lua"
+  end
+
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
@@ -36,19 +56,13 @@ class Unnethack < Formula
   # directory for temporary level data of running games
   skip_clean "var/unnethack/level"
 
-  # Apply upstream commit to fix build with newer bison. Remove with next release.
-  patch do
-    url "https://github.com/UnNetHack/UnNetHack/commit/04f0a3a850a94eb8837ddcef31303968240d1c31.patch?full_index=1"
-    sha256 "5285dc2e57b378bc77c01879399e2af248ef967977ed50e0c13a80b1993a7081"
-  end
-
-  # Fix implicit `ioctl` function declaration. Remove with the next release.
-  patch do
-    url "https://github.com/UnNetHack/UnNetHack/commit/33a3bb6539452875a88efbf6da0148a1cccc00c1.patch?full_index=1"
-    sha256 "07e1bb472c4f20957dafc6cfc49fcfd3178a5e04fcebf93a4fc7922ec8c0a963"
-  end
-
   def install
+    # Workaround for newer Clang. Fixed in HEAD but requires large patch
+    # Ref: https://github.com/UnNetHack/UnNetHack/commit/00dd95ccad390e72d6a4fb2e058df48ed509b564
+    ENV.append_to_cflags "-Wno-implicit-int" if DevelopmentTools.clang_build_version >= 1403
+
+    ENV["LUA_INCLUDE"] = "-I#{Formula["lua"].opt_include}/lua" if build.head?
+
     # directory for version specific files that shouldn't be deleted when
     # upgrading/uninstalling
     version_specific_directory = "#{var}/unnethack/#{version}"
