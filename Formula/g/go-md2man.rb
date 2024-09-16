a4607ba522c8340f368b1b84a3c7d93a88e8b15c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-16 18:41:06 +0000

    go-md2man 2.0.5
    go-md2man: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/go-md2man.rb
+++ Formula/g/go-md2man.rb
@@ -2,8 +2,8 @@ class GoMd2man < Formula
   desc "Converts markdown into roff (man pages)"
   homepage "https://github.com/cpuguy83/go-md2man"
   url "https://github.com/cpuguy83/go-md2man.git",
-      tag:      "v2.0.4",
-      revision: "d6816bfbea7506064a28119f805fb79f9bc5aeec"
+      tag:      "v2.0.5",
+      revision: "b14773d4db11046c50d0d1c05955839604aae991"
   license "MIT"
 
   bottle do
@@ -27,6 +27,6 @@ class GoMd2man < Formula
 
   test do
     assert_includes pipe_output(bin/"go-md2man", "# manpage\nand a half\n"),
-                    ".TH manpage\n.PP\nand a half\n"
+                    ".TH manpage\nand a half\n"
   end
 end
