edec4b547e8e90e704a65e21c5080a63bf9f978f
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-17 17:41:24 +0000

    libdex: add 0.8.0 bottle.

--- Formula/lib/libdex.rb
+++ Formula/lib/libdex.rb
@@ -6,6 +6,15 @@ class Libdex < Formula
   license "LGPL-2.1-or-later"
   head "https://gitlab.gnome.org/GNOME/libdex.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "90f1a3c13f23736f73ce0789786831d7c9601d8b356f28a8a9e5854585abd51d"
+    sha256 cellar: :any,                 arm64_sonoma:  "eed9f949f89c1320de99270b6e068e0e961cb220b65f772bc1d8d46727a5ae85"
+    sha256 cellar: :any,                 arm64_ventura: "83129eb405194ffdd455e350b5ea3a264e95f67e80cad93383647388e5e2555b"
+    sha256 cellar: :any,                 sonoma:        "87dc37d4cd9c899ecea51cae9ad51308d3fb532c6ec4f2b5b6fbc2f4358c9c88"
+    sha256 cellar: :any,                 ventura:       "468fbeb9589c1341484cf4ea12a5924b396356b2fbb2acfde35fa8a2eede65da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b3ea4e1a78f97c13eca1e1fa59aecfc668876730a8d01c96af363892cd64c2e4"
+  end
+
   depends_on "gobject-introspection" => [:build, :test]
   depends_on "meson" => :build
   depends_on "ninja" => :build
