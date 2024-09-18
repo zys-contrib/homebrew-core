b26647ed01114bbc27a4f954cab04ff86e1d6ea9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-18 00:52:51 +0000

    opensearch 2.17.0

--- Formula/o/opensearch.rb
+++ Formula/o/opensearch.rb
@@ -1,8 +1,8 @@
 class Opensearch < Formula
   desc "Open source distributed and RESTful search engine"
   homepage "https://github.com/opensearch-project/OpenSearch"
-  url "https://github.com/opensearch-project/OpenSearch/archive/refs/tags/2.16.0.tar.gz"
-  sha256 "a97926572ab790dc9e3af4075c957bd2225fd9dfb22fcdbc66073bd7c024c1c2"
+  url "https://github.com/opensearch-project/OpenSearch/archive/refs/tags/2.17.0.tar.gz"
+  sha256 "6af45ea416cf72e08680726ef59f0379fcbfa889845fd2626c86d9ae8ebbd9c4"
   license "Apache-2.0"
 
   bottle do
