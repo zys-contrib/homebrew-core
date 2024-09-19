595ad11c33944ecd3508f613f05c79219720a528
Rui Chen <rui@chenrui.dev>
2024-09-19 16:26:23 -0400

    fbthrift 2024.09.16.00
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fbthrift.rb
+++ Formula/f/fbthrift.rb
@@ -1,8 +1,8 @@
 class Fbthrift < Formula
   desc "Facebook's branch of Apache Thrift, including a new C++ server"
   homepage "https://github.com/facebook/fbthrift"
-  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2024.09.09.00.tar.gz"
-  sha256 "b1428c43c46ace466783f83eae5cba94d658783d38794db17f586fbb94a508a5"
+  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2024.09.16.00.tar.gz"
+  sha256 "ece90d7b558734317fb85142727641c59394bc345c6191a41d0774e383904cab"
   license "Apache-2.0"
   head "https://github.com/facebook/fbthrift.git", branch: "main"
 
