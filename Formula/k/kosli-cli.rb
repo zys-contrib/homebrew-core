7538d971b18b4199128cc3bd25b9bd320a4db5f8
Sami Alajrami <s.alajrami@gmail.com>
2024-09-19 13:46:02 +0200

    kosli-cli 2.10.16
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/k/kosli-cli.rb
+++ Formula/k/kosli-cli.rb
@@ -1,8 +1,8 @@
 class KosliCli < Formula
   desc "CLI for managing Kosli"
   homepage "https://docs.kosli.com/client_reference/"
-  url "https://github.com/kosli-dev/cli/archive/refs/tags/v2.10.15.tar.gz"
-  sha256 "a03dd42c6bd5ce0ab73e7c1431e3c2f1253119a7dfba4d0118acb055eb951c63"
+  url "https://github.com/kosli-dev/cli/archive/refs/tags/v2.10.16.tar.gz"
+  sha256 "8a12c70cc024c62560a651a7ea8b98aa4df2cb07cfa77af6823aacddfb4d5b2e"
   license "MIT"
   head "https://github.com/kosli-dev/cli.git", branch: "main"
 
