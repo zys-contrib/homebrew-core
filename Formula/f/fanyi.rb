712b181683d4aec4419ad5d06fea36bf086b40a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-20 22:50:30 +0000

    fanyi: update 9.0.5 bottle.

--- Formula/f/fanyi.rb
+++ Formula/f/fanyi.rb
@@ -6,12 +6,12 @@ class Fanyi < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3fe5844f5e6970789a887cb7319ac1693b5e9cddfc0a813e016f0e094c1157c6"
-    sha256 cellar: :any_skip_relocation, ventura:       "3fe5844f5e6970789a887cb7319ac1693b5e9cddfc0a813e016f0e094c1157c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "adc3260b9356abf89eb2355ed8a1ab05330bff3a2ba3ee0195e62fab07cb09bf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dcb306e52d34c98e9183e2bc2260bc5e210a1473afe4da9f4dd54f63858bbecc"
+    sha256 cellar: :any_skip_relocation, ventura:       "dcb306e52d34c98e9183e2bc2260bc5e210a1473afe4da9f4dd54f63858bbecc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
   end
 
   depends_on "node"
