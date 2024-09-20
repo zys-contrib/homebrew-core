2f9986b30aff664015a2e231bb2bc32b7c4760c2
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-20 15:01:30 +0000

    veryfasttree: add 4.0.4 bottle.

--- Formula/v/veryfasttree.rb
+++ Formula/v/veryfasttree.rb
@@ -6,6 +6,15 @@ class Veryfasttree < Formula
   license "GPL-3.0-only"
   head "https://github.com/citiususc/veryfasttree.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "be966294bd7e7793920e064538c8f301d30cf36e836149151a7dc9749751699e"
+    sha256 cellar: :any,                 arm64_sonoma:  "ad4a872d4517fad3f3a1ee924c42cd529cc50583d1cd01267808a64f30122c33"
+    sha256 cellar: :any,                 arm64_ventura: "5feeb83511115643cddc8bd05cea9b8215606d1e39edb9eba23028c5d646a02e"
+    sha256 cellar: :any,                 sonoma:        "84f8d609b53f4eccf0471604f90098e8a2e9525cc1eb8a942a0a9896f405bcf5"
+    sha256 cellar: :any,                 ventura:       "b675c4443158d46942e000086a79e1b3d8262ffb9703d9dd23ae256fea2895d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "93bc3678c5bc47c3f3a82b01e6a81ce7c247119fa1086cd1c5c1bafa4f979720"
+  end
+
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "robin-map" => :build
