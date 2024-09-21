9b3dcdb6c83c2c37f3413f832d01df3d14944040
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-21 14:48:17 +0000

    kamal-proxy: add 0.4.0 bottle.

--- Formula/k/kamal-proxy.rb
+++ Formula/k/kamal-proxy.rb
@@ -5,6 +5,15 @@ class KamalProxy < Formula
   sha256 "c622e6dcfa2c0728407d4ff9ce663484da3e025a2089e498a61950163fac656d"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83fd5398d267e295d9d042b360f366b0328fa18d9942e8ac4cdf9182536b73ad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "83fd5398d267e295d9d042b360f366b0328fa18d9942e8ac4cdf9182536b73ad"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "83fd5398d267e295d9d042b360f366b0328fa18d9942e8ac4cdf9182536b73ad"
+    sha256 cellar: :any_skip_relocation, sonoma:        "de9de9c2ca1eff2e4377a472e168af94ee96a59970377378e5891db8a5694eb7"
+    sha256 cellar: :any_skip_relocation, ventura:       "de9de9c2ca1eff2e4377a472e168af94ee96a59970377378e5891db8a5694eb7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21b6d6b281f7863ea592e5df30fe66acbe8bb075f1b890783836133a3563f422"
+  end
+
   depends_on "go" => :build
 
   def install
