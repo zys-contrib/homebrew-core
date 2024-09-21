ae9ee66898c20aea02c060e68895e37a3b0a724c
Michael Cho <michael@michaelcho.dev>
2024-09-21 13:12:43 -0400

    pinentry-mac: backport support for newer `libassuan`

--- Formula/p/pinentry-mac.rb
+++ Formula/p/pinentry-mac.rb
@@ -1,11 +1,24 @@
 class PinentryMac < Formula
   desc "Pinentry for GPG on Mac"
   homepage "https://github.com/GPGTools/pinentry"
-  url "https://github.com/GPGTools/pinentry/archive/refs/tags/v1.1.1.1.tar.gz"
-  sha256 "1a414f2e172cf8c18a121e60813413f27aedde891c5955151fbf8d50c46a9098"
   license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later"]
   revision 1
   head "https://github.com/GPGTools/pinentry.git", branch: "master"
+
+  stable do
+    url "https://github.com/GPGTools/pinentry/archive/refs/tags/v1.1.1.1.tar.gz"
+    sha256 "1a414f2e172cf8c18a121e60813413f27aedde891c5955151fbf8d50c46a9098"
+
+    # Backport support for newer `libassuan`
+    patch do
+      url "https://github.com/GPGTools/pinentry/commit/d8ca98aec634256cf4f6801874b6730eda12c5c5.patch?full_index=1"
+      sha256 "1490963f2a0ce75879123e56f94064e1b3263ef8aad222c3ca1966807c67ff7e"
+    end
+    patch do
+      url "https://github.com/GPGTools/pinentry/commit/a39ba412ab24721d4edb6476156371f8bf1d3ff9.patch?full_index=1"
+      sha256 "277d20f59bd37b3d41e547561b048f6a2af97f96157f1d4adc785bb57f387b5d"
+    end
+  end
 
   bottle do
     sha256 cellar: :any, arm64_sequoia:  "1759d4b87b044210921e272a7b17088619a0926cc0cf2e6575ce41a67e23dd76"
@@ -20,22 +33,21 @@ class PinentryMac < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on xcode: :build
-  depends_on "gettext"
-  depends_on "libassuan@2"
+  depends_on xcode: :build # for ibtool
+  depends_on "libassuan"
+  depends_on "libgpg-error"
   depends_on :macos
 
-  on_ventura :or_newer do
-    depends_on "texinfo" => :build
-  end
-
   def install
-    system "autoreconf", "--force", "--install", "--verbose"
-    system "autoconf"
-    system "./configure", "--disable-ncurses", "--enable-maintainer-mode"
+    system "./autogen.sh"
+    system "./configure", "--disable-doc",
+                          "--disable-ncurses",
+                          "--disable-silent-rules",
+                          "--enable-maintainer-mode",
+                          *std_configure_args
     system "make"
     prefix.install "macosx/pinentry-mac.app"
-    bin.write_exec_script "#{prefix}/pinentry-mac.app/Contents/MacOS/pinentry-mac"
+    bin.write_exec_script prefix/"pinentry-mac.app/Contents/MacOS/pinentry-mac"
   end
 
   def caveats
