7069eb0f51b5c77c17f8896eb0c82b8f5ce91860
Rui Chen <rui@chenrui.dev>
2024-09-22 20:02:00 +0000

    goyacc: add 1.0.3 bottle.

--- Formula/g/goyacc.rb
+++ Formula/g/goyacc.rb
@@ -5,6 +5,15 @@ class Goyacc < Formula
   sha256 "a999dd35759dfa35cc2cff70b840db0cd41443ef7aff7b090ac170731e6a6c6c"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b225608148f3a4a021cf2c97288a15a0896f98772c89f2908c8abb359ffb0138"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b225608148f3a4a021cf2c97288a15a0896f98772c89f2908c8abb359ffb0138"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b225608148f3a4a021cf2c97288a15a0896f98772c89f2908c8abb359ffb0138"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a9b364cae415d90c5c3a38cbe6e214ad339933905cc27e330cc01c70bd136c70"
+    sha256 cellar: :any_skip_relocation, ventura:       "a9b364cae415d90c5c3a38cbe6e214ad339933905cc27e330cc01c70bd136c70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "16bab84f275141c4258582ae879d9ca737c2ad94d0ca2d1997672b4523778be7"
+  end
+
   depends_on "go" => :build
 
   def install
