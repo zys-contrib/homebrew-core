a8699daa1fe39a9699504d66b59002e0bca154d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-23 18:45:14 +0000

    bomber: update 0.5.1 bottle.

--- Formula/b/bomber.rb
+++ Formula/b/bomber.rb
@@ -6,14 +6,12 @@ class Bomber < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "9dc1109affbf410a0d1f3bc3678a08345f0af2a2a38e83d7f31d151163a5bb13"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f2d9ee19477631f20213b148d7949b4cc579ed166ff85c4bb25b0b98c4ac0a0b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c6476378d4fdf202c5dacf811ea22b43d73ac1a2ab334e0356c71dd431fb0d8a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "496ad89cd3de482d2c0595a43cc0407f122c41658faf22a87dfeb7c493e2a3cd"
-    sha256 cellar: :any_skip_relocation, sonoma:         "c24d982ea826137d3c860f4f48a17f2d45405513d0f6604b6ee3f686d55fe1a8"
-    sha256 cellar: :any_skip_relocation, ventura:        "ce0bcd10fa126b773f14c0dab28e2d1fda20801002bb09c4c11b8250eef04559"
-    sha256 cellar: :any_skip_relocation, monterey:       "5e852c782540052df6a605a1d3f3dc06c03fbbe6b53ee02d980a6d08671d102f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "16c0deffdaa4a0ffa0e7d5014c6aa625cad9d8183b8ac9fee4b3d061ccd61bab"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "989ad14e281150f655296b8aa8ca9a3d24965bbd690d9ce8db45163e92429004"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "989ad14e281150f655296b8aa8ca9a3d24965bbd690d9ce8db45163e92429004"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "989ad14e281150f655296b8aa8ca9a3d24965bbd690d9ce8db45163e92429004"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ebe873ca7a04da1270598e2067be206460afa6d3779582773fc60e1529a3386c"
+    sha256 cellar: :any_skip_relocation, ventura:       "ebe873ca7a04da1270598e2067be206460afa6d3779582773fc60e1529a3386c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "59deb5d9be3344d651aeaa337219f5183857dcb09e1b460788f3549e57bc6d0d"
   end
 
   depends_on "go" => :build
