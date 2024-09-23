cb39c4256b1bdc30ffd395fd949427404ebafc74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-23 21:39:35 +0000

    kerl 4.3.0

--- Formula/k/kerl.rb
+++ Formula/k/kerl.rb
@@ -1,8 +1,8 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/refs/tags/4.2.0.tar.gz"
-  sha256 "a9306ad72d1d94d5e2592689cf54997e2481bd16813ed6eb4f8ed4f83f595ad4"
+  url "https://github.com/kerl/kerl/archive/refs/tags/4.3.0.tar.gz"
+  sha256 "3270070f4a61a080508810a9fea2a3173439cc5dcaf12ea69ca8baf0d68aadd9"
   license "MIT"
   head "https://github.com/kerl/kerl.git", branch: "master"
 
