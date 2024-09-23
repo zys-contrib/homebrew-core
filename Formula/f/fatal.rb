e7060dcffccc4c4eca11b020e6273ffdaf9594e0
Rui Chen <rui@chenrui.dev>
2024-09-23 19:03:00 -0400

    fatal 2024.09.23.00
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fatal.rb
+++ Formula/f/fatal.rb
@@ -1,8 +1,8 @@
 class Fatal < Formula
   desc "Facebook Template Library"
   homepage "https://www.facebook.com/groups/libfatal/"
-  url "https://github.com/facebook/fatal/releases/download/v2024.09.09.00/fatal-v2024.09.09.00.tar.gz"
-  sha256 "b5d1c450b04ba56eebd39863a3962a0bdbdf0e6f75bc8f8440bdd426f01c81c3"
+  url "https://github.com/facebook/fatal/releases/download/v2024.09.23.00/fatal-v2024.09.23.00.tar.gz"
+  sha256 "c4c473051d1953a40a0c8172e6dea5a71d6522aa03c61aa52ec64a1636df8190"
   license "BSD-3-Clause"
   head "https://github.com/facebook/fatal.git", branch: "main"
 
