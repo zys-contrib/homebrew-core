34dfa446fa291e4104f0ea5b6cb324cebd815bc8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-24 21:59:27 +0000

    rswift 7.6.1

--- Formula/r/rswift.rb
+++ Formula/r/rswift.rb
@@ -1,8 +1,8 @@
 class Rswift < Formula
   desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
-  url "https://github.com/mac-cain13/R.swift/releases/download/7.6.0/rswift-7.6.0-source.tar.gz"
-  sha256 "7bb66abd5ce75b134af273233b9349602014a7cd28013fd6b45583a26b05ab9b"
+  url "https://github.com/mac-cain13/R.swift/releases/download/7.6.1/rswift-7.6.1-source.tar.gz"
+  sha256 "163f8b2cbd79a97a623efed2390f963aaa1896b78cc6a31751735548a2d26f19"
   license "MIT"
   head "https://github.com/mac-cain13/R.swift.git", branch: "master"
 
