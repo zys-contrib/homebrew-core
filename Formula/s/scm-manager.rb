b9dfad3d6284383522a62ec6bc2b62e8100b1549
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-24 12:56:38 +0000

    scm-manager 3.4.2

--- Formula/s/scm-manager.rb
+++ Formula/s/scm-manager.rb
@@ -1,8 +1,8 @@
 class ScmManager < Formula
   desc "Manage Git, Mercurial, and Subversion repos over HTTP"
   homepage "https://www.scm-manager.org"
-  url "https://packages.scm-manager.org/repository/releases/sonia/scm/packaging/unix/3.4.1/unix-3.4.1.tar.gz"
-  sha256 "0951d1492f37acd9636e00d873a19814774ea13b09209b5bc2a972415b6df177"
+  url "https://packages.scm-manager.org/repository/releases/sonia/scm/packaging/unix/3.4.2/unix-3.4.2.tar.gz"
+  sha256 "4e9de9146b13938e65fa53154e7e86aac3763ba5cbf7e5277407ca83195ec94d"
   license all_of: ["Apache-2.0", "MIT"]
 
   livecheck do
