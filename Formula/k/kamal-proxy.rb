8d439accd1ac909c1f2dd7b93afa18da230d41fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-25 14:00:33 +0000

    kamal-proxy: update 0.5.0 bottle.

--- Formula/k/kamal-proxy.rb
+++ Formula/k/kamal-proxy.rb
@@ -6,12 +6,12 @@ class KamalProxy < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83fd5398d267e295d9d042b360f366b0328fa18d9942e8ac4cdf9182536b73ad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "83fd5398d267e295d9d042b360f366b0328fa18d9942e8ac4cdf9182536b73ad"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "83fd5398d267e295d9d042b360f366b0328fa18d9942e8ac4cdf9182536b73ad"
-    sha256 cellar: :any_skip_relocation, sonoma:        "de9de9c2ca1eff2e4377a472e168af94ee96a59970377378e5891db8a5694eb7"
-    sha256 cellar: :any_skip_relocation, ventura:       "de9de9c2ca1eff2e4377a472e168af94ee96a59970377378e5891db8a5694eb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21b6d6b281f7863ea592e5df30fe66acbe8bb075f1b890783836133a3563f422"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5f59ccfd0e8fdd4a502f22e4db0b3b25550f1188cabea1b0225ed53eccc46a09"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5f59ccfd0e8fdd4a502f22e4db0b3b25550f1188cabea1b0225ed53eccc46a09"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5f59ccfd0e8fdd4a502f22e4db0b3b25550f1188cabea1b0225ed53eccc46a09"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6628b1db22b4a8004eee23e64bbcc2b94ca2f6e5c8b7cde4e8baf5a8e044b723"
+    sha256 cellar: :any_skip_relocation, ventura:       "6628b1db22b4a8004eee23e64bbcc2b94ca2f6e5c8b7cde4e8baf5a8e044b723"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0eb772cb2fc5f7ba4a3c014ed6d39a6c73a2d7dddd3367e9b58d6db4697ec98"
   end
 
   depends_on "go" => :build
