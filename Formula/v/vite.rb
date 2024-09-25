4405237497a3ee74f4882347db09b01a3ce8876d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-25 11:35:57 +0000

    vite: update 5.4.8 bottle.

--- Formula/v/vite.rb
+++ Formula/v/vite.rb
@@ -6,12 +6,12 @@ class Vite < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "17274d90a9c59f389b78772dca522e784b04e267d3cc7568e87d582670d30d44"
-    sha256 cellar: :any,                 arm64_sonoma:  "17274d90a9c59f389b78772dca522e784b04e267d3cc7568e87d582670d30d44"
-    sha256 cellar: :any,                 arm64_ventura: "17274d90a9c59f389b78772dca522e784b04e267d3cc7568e87d582670d30d44"
-    sha256 cellar: :any,                 sonoma:        "c9e734f0c4dbff8a34bb57a476eae3697dc2d00217e403ae41cb6339362b9ca3"
-    sha256 cellar: :any,                 ventura:       "c9e734f0c4dbff8a34bb57a476eae3697dc2d00217e403ae41cb6339362b9ca3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c0a1a2d02743a9eb5e24876e8f6979cc7231816e7eaaef02fdb60bc4f1486e9"
+    sha256 cellar: :any,                 arm64_sequoia: "ced252645cb1c94a24f4d9a182e8f52b0f1aa09542ce72ec725eec877dd7839d"
+    sha256 cellar: :any,                 arm64_sonoma:  "ced252645cb1c94a24f4d9a182e8f52b0f1aa09542ce72ec725eec877dd7839d"
+    sha256 cellar: :any,                 arm64_ventura: "ced252645cb1c94a24f4d9a182e8f52b0f1aa09542ce72ec725eec877dd7839d"
+    sha256 cellar: :any,                 sonoma:        "9603740ed9386b4f68eba628718d740feac4a2731be70787b851264baf165f1d"
+    sha256 cellar: :any,                 ventura:       "9603740ed9386b4f68eba628718d740feac4a2731be70787b851264baf165f1d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc66ddadb56c8941b25c92303b5e330c5cfd59ba021c68ed59ff9d39e209e468"
   end
 
   depends_on "node"
