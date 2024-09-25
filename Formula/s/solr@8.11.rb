95540f031237e1ef6e46eb45bd0452ea5906d381
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-25 03:18:28 +0000

    solr@8.11: update 8.11.4 bottle.

--- Formula/s/solr@8.11.rb
+++ Formula/s/solr@8.11.rb
@@ -12,7 +12,7 @@ class SolrAT811 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9215d75465b9410dc267924991044a2b4e0e781e654583db2d01a7767d631bfa"
+    sha256 cellar: :any_skip_relocation, all: "65d79494d324de0e00931020fc1e1624c7929566a4d66cee0b33052111f6e523"
   end
 
   keg_only :versioned_formula
