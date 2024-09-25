68a3e1d1eef9ab0f34e6c540bd9d679402731f3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-25 01:06:01 +0000

    openrtsp 2024.09.25

--- Formula/o/openrtsp.rb
+++ Formula/o/openrtsp.rb
@@ -1,10 +1,10 @@
 class Openrtsp < Formula
   desc "Command-line RTSP client"
   homepage "http://www.live555.com/openRTSP"
-  url "http://www.live555.com/liveMedia/public/live.2024.09.24.tar.gz"
-  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2024.09.24.tar.gz"
+  url "http://www.live555.com/liveMedia/public/live.2024.09.25.tar.gz"
+  mirror "https://download.videolan.org/pub/videolan/testing/contrib/live555/live.2024.09.25.tar.gz"
   # Keep a mirror as upstream tarballs are removed after each version
-  sha256 "15249fe0ae4827c74d76460e66b357c8009541a1331ad858bfe94f592d6f7167"
+  sha256 "0b7e54a9982291d6ad7935276e86a0778d6cc0b5c61d8344de90db55d48e9495"
   license "LGPL-3.0-or-later"
 
   livecheck do
