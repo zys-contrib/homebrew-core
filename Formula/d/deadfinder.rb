13cce23dd93eda9aa0de2a33d8dbec2b326acad5
Bevan Kay <email@bevankay.me>
2024-09-26 16:47:01 +1000

    deadfinder 1.3.5

--- Formula/d/deadfinder.rb
+++ Formula/d/deadfinder.rb
@@ -1,8 +1,8 @@
 class Deadfinder < Formula
   desc "Finds broken links"
   homepage "https://rubygems.org/gems/deadfinder"
-  url "https://github.com/hahwul/deadfinder/archive/refs/tags/1.3.4.tar.gz"
-  sha256 "ed99ee05c308095763b01adcbc4560c99576c7ed1af59b38a7786bb1469b3a90"
+  url "https://github.com/hahwul/deadfinder/archive/refs/tags/1.3.5.tar.gz"
+  sha256 "d87efcfabbbb85307c8b90dd39e3da086d402d2dde11b1ad0837a3713deec435"
   license "MIT"
   head "https://github.com/hahwul/deadfinder.git", branch: "main"
 
@@ -20,26 +20,24 @@ class Deadfinder < Formula
   depends_on "ruby"
 
   uses_from_macos "libffi"
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
   uses_from_macos "xz"
   uses_from_macos "zlib"
 
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
   def install
     ENV["GEM_HOME"] = libexec
+    ENV["NOKOGIRI_USE_SYSTEM_LIBRARIES"] = "1"
     system "bundle", "config", "set", "without", "development", "test"
     system "bundle", "install"
     system "gem", "build", "deadfinder.gemspec"
     system "gem", "install", "deadfinder-#{version}.gem"
     bin.install libexec/"bin/deadfinder"
     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
-
-    # Avoid references to the Homebrew shims directory
-    if OS.mac?
-      shims_references = Dir[libexec/"extensions/**/ffi-*/mkmf.log"].select { |f| File.file? f }
-      inreplace shims_references,
-                Superenv.shims_path.to_s,
-                "<**Reference to the Homebrew shims directory**>",
-                audit_result: false
-    end
   end
 
   test do
