ca30be739a12e4aeaedbf00517e21c7a64ff2e56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-27 13:06:08 +0000

    pmd 7.6.0

--- Formula/p/pmd.rb
+++ Formula/p/pmd.rb
@@ -1,8 +1,8 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.5.0/pmd-dist-7.5.0-bin.zip"
-  sha256 "d188d592409033712bf7b99dc739ca6c238f296c6dbe6bc5f95e9403684c8ee1"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.6.0/pmd-dist-7.6.0-bin.zip"
+  sha256 "e07f7a9c3607d643509a96d7f5f891961e98ea88b6eba85d120d08f0c08c985e"
   license "BSD-4-Clause"
 
   bottle do
