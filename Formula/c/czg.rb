0cf74918685e7848033e4ffa73a0cdd48fd5c23d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-28 09:29:05 +0000

    czg 1.10.0

--- Formula/c/czg.rb
+++ Formula/c/czg.rb
@@ -1,8 +1,8 @@
 class Czg < Formula
   desc "Interactive Commitizen CLI that generate standardized commit messages"
   homepage "https://github.com/Zhengqbbb/cz-git"
-  url "https://registry.npmjs.org/czg/-/czg-1.9.4.tgz"
-  sha256 "d005cdff971561e70fe64d615ab67e60d5eba0e8f9dd8994fa22b54f861f9b47"
+  url "https://registry.npmjs.org/czg/-/czg-1.10.0.tgz"
+  sha256 "8409c3e6656d5b860951615871fb546e809d0ecd12d67da0c2857b9720de75db"
   license "MIT"
 
   bottle do
