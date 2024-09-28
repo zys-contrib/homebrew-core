5db1c9c128eb12e0acc3b6d80ee7865a9b1c55a2
Casey Deccio <casey@deccio.net>
2024-09-27 21:19:22 -0600

    update tests

--- Formula/d/dnsviz.rb
+++ Formula/d/dnsviz.rb
@@ -38,96 +38,22 @@ class Dnsviz < Formula
   end
 
   def install
-    ENV["SWIG_FEATURES"] = "-I#{Formula["openssl@3"].opt_include}"
     virtualenv_install_with_resources(link_manpages: true)
   end
 
   test do
-    (testpath/"example.com.zone.signed").write <<~EOS
-      ; File written on Thu Jan 10 21:14:03 2019
-      ; dnssec_signzone version 9.11.4-P2-3~bpo9+1-Debian
-      example.com.		3600	IN SOA	example.com. root.example.com. (
-      				1          ; serial
-      				3600       ; refresh (1 hour)
-      				3600       ; retry (1 hour)
-      				14400      ; expire (4 hours)
-      				3600       ; minimum (1 hour)
-      				)
-      		3600	RRSIG	SOA 10 2 3600 (
-      				20230110031403 20190111031403 39026 example.com.
-      				D2WDMpH4Ip+yi2wQFmCq8iPWWdHo/vGig/rG
-      				+509RbOLHbeFaO84PrPvw/dS6kjDupQbyG1t
-      				8Hx0XzlvitBZjpYFq3bd/k0zU/S39IroeDfU
-      				xR/BlI2bEaIPxgG2AulJjS6lnYigfko4AKfe
-      				AqssO7P1jpiUUYtFpivK3ybl03o= )
-      		3600	NS	example.com.
-      		3600	RRSIG	NS 10 2 3600 (
-      				20230110031403 20190111031403 39026 example.com.
-      				bssTLRwAeyn0UtOjWKVbaJdq+lNbeOKBE2a4
-      				QdR2lrgNDVenY8GciWarYcd5ldPfrfX5t5I9
-      				QwiIsv/xAPgksVlmWcZGVDAAzzlglVhCg2Ys
-      				J7YEcV2DDIMZLx2hm6gu9fKaMcqp8lhUSCBD
-      				h4VTswLV1HoUDGYwEsjLEtiRin8= )
-      		3600	A	127.0.0.1
-      		3600	RRSIG	A 10 2 3600 (
-      				20230110031403 20190111031403 39026 example.com.
-      				TH+PWGhFd3XL09IkCeAd0TNrWVsj+bAcQESx
-      				F27lCgMnYYebiy86QmhEGzM+lu7KX1Vn15qn
-      				2KnyEKofW+kFlCaOMZDmwBcU0PznBuGJ/oQ9
-      				2OWe3X2bw5kMEQdxo7tjMlDo+v975VaZgbCz
-      				od9pETQxdNBHkEfKmxWpenMi9PI= )
-      		3600	AAAA	::1
-      		3600	RRSIG	AAAA 10 2 3600 (
-      				20230110031403 20190111031403 39026 example.com.
-      				qZM60MUJp95oVqQwdW03eoCe5yYu8hdpnf2y
-      				Z7eyxTDg1qEgF+NUF6Spe8OKsu2SdTolT0CF
-      				8X068IGTEr2rbFK/Ut1owQEyYuAnbNGBmg99
-      				+yo1miPgxpHL/GbkMiSK7q6phMdF+LOmGXkQ
-      				G3wbQ5LUn2R7uSPehDwXiRbD0V8= )
-      		3600	NSEC	example.com. A NS SOA AAAA RRSIG NSEC DNSKEY
-      		3600	RRSIG	NSEC 10 2 3600 (
-      				20230110031403 20190111031403 39026 example.com.
-      				Rdx/TmynYt0plItVI10plFis6PbsH29qyXBw
-      				NLOEAMNLvU6IhCOlv7T8YxZWsamg3NyM0det
-      				NgQqIFfJCfLEn2mzHdqfPeVqxyKgXF1mEwua
-      				TZpE8nFw95buxV0cg67N8VF7PZX6zr1aZvEn
-      				b022mYFpqaGMhaA6f++lGChDw80= )
-      		3600	DNSKEY	256 3 10 (
-      				AwEAAaqQ5dsqndLRH+9j/GbtUObxgAEvM7VH
-      				/y12xjouBFnqTkAL9VvonNwYkFjnCZnIriyl
-      				jOkNDgE4G8pYzYlK13EtxBDJrUoHU11ZdL95
-      				ZQEpd8hWGqSG2KQiCYwAAhmG1qu+I+LtexBe
-      				kNwT3jJ1BMgGB3xsCluUYHBeSlq9caU/
-      				) ; ZSK; alg = RSASHA512 ; key id = 39026
-      		3600	DNSKEY	257 3 10 (
-      				AwEAAaLSZl7J7bJnFAcRrqWE7snJvJ1uzkS8
-      				p1iq3ciHnt6rZJq47HYoP5TCnKgCpje/HtZt
-      				L/7n8ixPjhgj8/GkfOwoWq5kU3JUN2uX6pBb
-      				FhSsVeNe2JgEFtloZSMHhSU52yS009WcjZJV
-      				O2QX2JXcLy0EMI2S4JIFLa5xtatXQ2/F
-      				) ; KSK; alg = RSASHA512 ; key id = 34983
-      		3600	RRSIG	DNSKEY 10 2 3600 (
-      				20230110031403 20190111031403 34983 example.com.
-      				g1JfHNrvVch3pAX3/qHuiivUeSawpmO7h2Pp
-      				Hqt9hPbR7jpzOxbOzLAxHopMR/xxXN1avyI5
-      				dh23ySy1rbRMJprz2n09nYbK7m695u7P18+F
-      				sCmI8pjqtpJ0wg/ltEQBCRNaYOrHvK+8NLvt
-      				PGJqJru7+7aaRr1PP+ne7Wer+gE= )
-    EOS
-    (testpath/"example.com.zone-delegation").write <<~EOS
-      example.com.	IN	NS	ns1.example.com.
-      ns1.example.com.	IN	A	127.0.0.1
-      example.com.		IN DS 34983 10 1 EC358CFAAEC12266EF5ACFC1FEAF2CAFF083C418
-      example.com.		IN DS 34983 10 2 608D3B089D79D554A1947BD10BEC0A5B1BDBE67B4E60E34B1432ED00 33F24B49
-    EOS
-    system bin/"dnsviz", "probe", "-d", "0", "-A",
-      "-x", "example.com:example.com.zone.signed",
-      "-N", "example.com:example.com.zone-delegation",
-      "-D", "example.com:example.com.zone-delegation",
-      "-o", "example.com.json",
-      "example.com"
-    system bin/"dnsviz", "graph", "-r", "example.com.json", "-Thtml", "-o", "/dev/null"
-    system bin/"dnsviz", "grok", "-r", "example.com.json", "-o", "/dev/null"
-    system bin/"dnsviz", "print", "-r", "example.com.json", "-o", "/dev/null"
+    resource "example-com-probe-auth" do
+      url "https://raw.githubusercontent.com/dnsviz/dnsviz/refs/heads/master/tests/zones/unsigned/example.com-probe-auth.json"
+      sha256 "6d75bf4e6289db41f8da6263aed2e0e8c910b8f303e4f065ec7d359997248997"
+    end
+
+    resource("example-com-probe-auth").stage do
+      system bin/"dnsviz", "probe", "-d", "0",
+        "-r", "example.com-probe-auth.json",
+        "-o", "example.com.json"
+      system bin/"dnsviz", "graph", "-r", "example.com.json", "-Thtml", "-o", "/dev/null"
+      system bin/"dnsviz", "grok", "-r", "example.com.json", "-o", "/dev/null"
+      system bin/"dnsviz", "print", "-r", "example.com.json", "-o", "/dev/null"
+    end
   end
 end
