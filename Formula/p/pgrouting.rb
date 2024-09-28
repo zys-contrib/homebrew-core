f46270be72a3660b5e2cb876328f6e0d62c87104
Michael Cho <michael@michaelcho.dev>
2024-09-28 10:40:32 -0400

    pgrouting: add support for `postgresql@17`

--- Formula/p/pgrouting.rb
+++ Formula/p/pgrouting.rb
@@ -23,15 +23,14 @@ class Pgrouting < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c9d83c063ea735b32fb4e1abdab9d567003c42c3fe8144bbf648e3147167d6f7"
   end
 
+  depends_on "boost" => :build
   depends_on "cmake" => :build
-  depends_on "boost"
-  depends_on "cgal"
-  depends_on "gmp"
+  depends_on "postgresql@14" => [:build, :test]
+  depends_on "postgresql@17" => [:build, :test]
   depends_on "postgis"
-  depends_on "postgresql@14"
 
-  def postgresql
-    Formula["postgresql@14"]
+  def postgresqls
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("postgresql@") }
   end
 
   def install
@@ -44,11 +43,18 @@ class Pgrouting < Formula
         .each { |llvm_lib| ENV.remove "HOMEBREW_LIBRARY_PATHS", llvm_lib }
     end
 
-    mkdir "stage"
-    mkdir "build" do
-      system "cmake", "-DPOSTGRESQL_PG_CONFIG=#{postgresql.opt_bin}/pg_config", "..", *std_cmake_args
-      system "make"
-      system "make", "install", "DESTDIR=#{buildpath}/stage"
+    ENV["DESTDIR"] = buildpath/"stage"
+
+    postgresqls.each do |postgresql|
+      builddir = "build-pg#{postgresql.version.major}"
+      args = ["-DPOSTGRESQL_PG_CONFIG=#{postgresql.opt_bin}/pg_config"]
+      # CMake MODULE libraries use .so on macOS but PostgreSQL 16+ looks for .dylib
+      # Ref: https://github.com/postgres/postgres/commit/b55f62abb2c2e07dfae99e19a2b3d7ca9e58dc1a
+      args << "-DCMAKE_SHARED_MODULE_SUFFIX_CXX=.dylib" if OS.mac? && postgresql.version >= 16
+
+      system "cmake", "-S", ".", "-B", builddir, *args, *std_cmake_args
+      system "cmake", "--build", builddir
+      system "cmake", "--install", builddir
     end
 
     stage_path = File.join("stage", HOMEBREW_PREFIX)
@@ -57,21 +63,25 @@ class Pgrouting < Formula
   end
 
   test do
-    pg_ctl = postgresql.opt_bin/"pg_ctl"
-    psql = postgresql.opt_bin/"psql"
-    port = free_port
+    ENV["LC_ALL"] = "C"
+    postgresqls.each do |postgresql|
+      pg_ctl = postgresql.opt_bin/"pg_ctl"
+      psql = postgresql.opt_bin/"psql"
+      port = free_port
 
-    system pg_ctl, "initdb", "-D", testpath/"test"
-    (testpath/"test/postgresql.conf").write <<~EOS, mode: "a+"
+      datadir = testpath/postgresql.name
+      system pg_ctl, "initdb", "-D", datadir
+      (datadir/"postgresql.conf").write <<~EOS, mode: "a+"
 
-      shared_preload_libraries = 'libpgrouting-#{version.major_minor}'
-      port = #{port}
-    EOS
-    system pg_ctl, "start", "-D", testpath/"test", "-l", testpath/"log"
-    begin
-      system psql, "-p", port.to_s, "-c", "CREATE EXTENSION \"pgrouting\" CASCADE;", "postgres"
-    ensure
-      system pg_ctl, "stop", "-D", testpath/"test"
+        shared_preload_libraries = 'libpgrouting-#{version.major_minor}'
+        port = #{port}
+      EOS
+      system pg_ctl, "start", "-D", datadir, "-l", testpath/"log-#{postgresql.name}"
+      begin
+        system psql, "-p", port.to_s, "-c", "CREATE EXTENSION \"pgrouting\" CASCADE;", "postgres"
+      ensure
+        system pg_ctl, "stop", "-D", datadir
+      end
     end
   end
 end
