f338f69783886d0a6a7b4a46033b0d3caf59c823
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-09-29 04:36:34 +0800

    vineyard: revision bump (grpc 1.66.2)

--- Formula/v/vineyard.rb
+++ Formula/v/vineyard.rb
@@ -6,7 +6,7 @@ class Vineyard < Formula
   url "https://github.com/v6d-io/v6d/releases/download/v0.23.2/v6d-0.23.2.tar.gz"
   sha256 "2a2788ed77b9459477b3e90767a910e77e2035a34f33c29c25b9876568683fd4"
   license "Apache-2.0"
-  revision 3
+  revision 4
 
   bottle do
     rebuild 1
