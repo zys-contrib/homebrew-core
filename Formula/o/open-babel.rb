f7cdd2f4b5d403125631dad47a53c3d49b9d8041
Michael Cho <michael@michaelcho.dev>
2024-09-29 15:16:48 -0400

    open-babel: use `inchi`

--- Formula/o/open-babel.rb
+++ Formula/o/open-babel.rb
@@ -1,12 +1,20 @@
 class OpenBabel < Formula
   desc "Chemical toolbox"
   homepage "https://github.com/openbabel/openbabel"
-  url "https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz"
-  version "3.1.1"
-  sha256 "c97023ac6300d26176c97d4ef39957f06e68848d64f1a04b0b284ccff2744f02"
   license "GPL-2.0-only"
   revision 2
   head "https://github.com/openbabel/openbabel.git", branch: "master"
+
+  stable do
+    url "https://github.com/openbabel/openbabel/releases/download/openbabel-3-1-1/openbabel-3.1.1-source.tar.bz2"
+    sha256 "a6ec8381d59ea32a4b241c8b1fbd799acb52be94ab64cdbd72506fb4e2270e68"
+
+    # Backport support for configuring PYTHON_INSTDIR to avoid Setuptools
+    patch do
+      url "https://github.com/openbabel/openbabel/commit/f7910915c904a18ac1bdc209b2dc9deeb92f7db3.patch?full_index=1"
+      sha256 "f100bb9bffb82b318624933ddc0027eeee8546bf4d6deda5067ecbd1ebd138ea"
+    end
+  end
 
   bottle do
     rebuild 2
@@ -22,12 +30,12 @@ class OpenBabel < Formula
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "python-setuptools" => :build
   depends_on "rapidjson" => :build
   depends_on "swig" => :build
 
   depends_on "cairo"
   depends_on "eigen"
+  depends_on "inchi"
   depends_on "python@3.12"
 
   uses_from_macos "libxml2"
@@ -41,16 +49,38 @@ class OpenBabel < Formula
 
   def install
     system "cmake", "-S", ".", "-B", "build",
+                    "-DINCHI_INCLUDE_DIR=#{Formula["inchi"].opt_include}/inchi",
+                    "-DOPENBABEL_USE_SYSTEM_INCHI=ON",
                     "-DRUN_SWIG=ON",
                     "-DPYTHON_BINDINGS=ON",
                     "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                    "-DPYTHON_INSTDIR=#{prefix/Language::Python.site_packages(python3)}",
                     *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
   test do
-    system bin/"obabel", "-:'C1=CC=CC=C1Br'", "-omol"
+    assert_match <<~EOS, shell_output("#{bin}/obabel -:'C1=CC=CC=C1Br' -omol")
+
+        7  7  0  0  0  0  0  0  0  0999 V2000
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 Br  0  0  0  0  0  0  0  0  0  0  0  0
+        1  6  1  0  0  0  0
+        1  2  2  0  0  0  0
+        2  3  1  0  0  0  0
+        3  4  2  0  0  0  0
+        4  5  1  0  0  0  0
+        5  6  2  0  0  0  0
+        6  7  1  0  0  0  0
+      M  END
+    EOS
+
     system python3, "-c", "from openbabel import openbabel"
   end
 end
