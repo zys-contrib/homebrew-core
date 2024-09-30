ca88d8a100447434fa54c03891ab2e6cc37a2abf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-09-30 21:32:38 +0000

    dyld-headers 1231.3

--- Formula/d/dyld-headers.rb
+++ Formula/d/dyld-headers.rb
@@ -1,8 +1,8 @@
 class DyldHeaders < Formula
   desc "Header files for the dynamic linker"
   homepage "https://opensource.apple.com/"
-  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-1165.3.tar.gz"
-  sha256 "f2cd78cdcf9d63011d0cee0047033b0815355a9f5d25df2a0690b47a05602e5f"
+  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-1231.3.tar.gz"
+  sha256 "ec5459db9cba71e14431ec8f8c5b5d30db956ea16a0cad839886480fa2350225"
   license "APSL-2.0"
 
   bottle do
