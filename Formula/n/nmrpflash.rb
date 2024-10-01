381f1e8d6454b7c24f625bae221fe77a534dd207
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2024-10-01 16:00:52 +0200

    nmrpflash 0.9.24

--- Formula/n/nmrpflash.rb
+++ Formula/n/nmrpflash.rb
@@ -1,8 +1,8 @@
 class Nmrpflash < Formula
   desc "Netgear Unbrick Utility"
   homepage "https://github.com/jclehner/nmrpflash"
-  url "https://github.com/jclehner/nmrpflash/archive/refs/tags/v0.9.22.tar.gz"
-  sha256 "cef3b54c798a4a049a2d9b959e1d6a0ac2f4f31b802d6be4f79351b9a96c3f39"
+  url "https://github.com/jclehner/nmrpflash/archive/refs/tags/v0.9.24.tar.gz"
+  sha256 "e902b8098a41c4c949fccd661dedcc8ca3791c83919a2b233286eae4752f25ea"
   license "GPL-3.0-or-later"
 
   bottle do
