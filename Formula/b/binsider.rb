e23293c6cf4c21897b9abe3c7f199f8d9537d7ff
Rui Chen <rui@chenrui.dev>
2024-10-01 18:54:19 +0000

    binsider: add 0.2.0 bottle.

--- Formula/b/binsider.rb
+++ Formula/b/binsider.rb
@@ -6,6 +6,15 @@ class Binsider < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/orhun/binsider.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "64396fbb1c6e0860d5d79aa86820ae8b2bb5f35195a1b440af10a59e2f89f4b9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8e8a90f19ab27480bbf7fbf38db0eaa1253c7b026d607792ef8af29a7aefde94"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "79279a89007e958b047a8067dd640931b303ac1f4c73556bdf2fbc26b8258719"
+    sha256 cellar: :any_skip_relocation, sonoma:        "37802739bda335014bf87fd3550e7d78b7d1a6fc642f34771c650988fd1934ae"
+    sha256 cellar: :any_skip_relocation, ventura:       "e5b66954f76d99ac630bfcce0af22b15a21879a1b4edbe65a0288eb51294bffe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06379f6228b7fcd71be830e3f876fee8fd08c3806c17f51d6cbd160cc1fabda6"
+  end
+
   depends_on "rust" => :build
 
   def install
