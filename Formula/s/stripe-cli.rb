7b900bf279a4a9fe7b8fc7609149fef26a14edfe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-01 22:04:52 +0000

    stripe-cli 1.21.8

--- Formula/s/stripe-cli.rb
+++ Formula/s/stripe-cli.rb
@@ -1,8 +1,8 @@
 class StripeCli < Formula
   desc "Command-line tool for Stripe"
   homepage "https://stripe.com/docs/stripe-cli"
-  url "https://github.com/stripe/stripe-cli/archive/refs/tags/v1.21.7.tar.gz"
-  sha256 "d7e1acaf1720a2e039695f56d800e1d31f74ef165159c1745222c02a64294153"
+  url "https://github.com/stripe/stripe-cli/archive/refs/tags/v1.21.8.tar.gz"
+  sha256 "7b19bf53df2668c86ffbe18e2d72c9f80a51f88d63eaf419a37e1a7f7fd5071e"
   license "Apache-2.0"
 
   bottle do
