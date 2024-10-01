d434c25f21d3f6a1e724815b6885dc89d6881f3a
Patrick Linnane <patrick@linnane.io>
2024-10-01 19:24:57 +0000

    rsgain: add 3.5.2 bottle.

--- Formula/r/rsgain.rb
+++ Formula/r/rsgain.rb
@@ -6,6 +6,15 @@ class Rsgain < Formula
   license "BSD-2-Clause"
   head "https://github.com/complexlogic/rsgain.git", branch: "master"
 
+  bottle do
+    sha256 arm64_sequoia: "e4c203a6c7ab07d27b3dcdf8466d296bb155be0b38c12eb429d0e1a79e0097e9"
+    sha256 arm64_sonoma:  "66ee5294b464d968abc2847838f7da21175aa2fc3bd785fcf4f647457af14ebb"
+    sha256 arm64_ventura: "85fc8c0aeb7b824e7c7b85b0b956a549b5d5498d247377d2d8e98f55842a41e5"
+    sha256 sonoma:        "4db25f5d6f21d0ba16d4ba9efe9bac22f648959b7ee3ba8b7a515f1a392c512b"
+    sha256 ventura:       "5a233790765bd76e9f5d9f2029c43045ee81a9921d3d67fa4f6c57f747943424"
+    sha256 x86_64_linux:  "373aa6793709cb1d1ea760e08e56f80a8d2ced8d952d0e2aefedf53c40519c41"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "ffmpeg"
