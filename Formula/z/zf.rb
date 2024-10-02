2e2f86cfc6755100841dd5db45cf82f791e322f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-02 01:06:39 +0000

    zf 0.10.0

--- Formula/z/zf.rb
+++ Formula/z/zf.rb
@@ -1,8 +1,8 @@
 class Zf < Formula
   desc "Command-line fuzzy finder that prioritizes matches on filenames"
   homepage "https://github.com/natecraddock/zf"
-  url "https://github.com/natecraddock/zf/archive/refs/tags/0.9.2.tar.gz"
-  sha256 "61a4db245213f0319dfb34758f5c9c00d4945d7ae6187a6220e88ebef95e4494"
+  url "https://github.com/natecraddock/zf/archive/refs/tags/0.10.0.tar.gz"
+  sha256 "a90b2fff1d552910897487d177eaf92176e421f097ad61c70b8f8b127deef160"
   license "MIT"
   head "https://github.com/natecraddock/zf.git", branch: "master"
 
