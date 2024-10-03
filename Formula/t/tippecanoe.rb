21eb7a6f22290f1c8fe5fd2c1d41441e50d8ff90
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-03 01:46:13 +0000

    tippecanoe: update 2.63.0 bottle.

--- Formula/t/tippecanoe.rb
+++ Formula/t/tippecanoe.rb
@@ -11,12 +11,12 @@ class Tippecanoe < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9666f5aa4071e48a3b2e4837ed8e4f2d7bc32b1b957356b96a1dbc6bcb104d36"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ac5f2fc85a1400d69d51cf6415da4038991a7f72eacfdad76416dc332be969e0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e29ecf757144b87d7d3f40674234cdd729618d2c4fabb214bff038f277d5316e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "de54931b484478e1a0e254984e6c258b381a61ae28c4f04238edf92c6ffea421"
-    sha256 cellar: :any_skip_relocation, ventura:       "2426444eb0483e222ecc1498c621fb163b031328854064835b0025be5c4cbbfc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a81dab1609155d3a72963c79d8e94693a105d276d21f0b89bf01170aed4c2d9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "67f21238fc3a8d988e641d72247040b2c9e8528a45b16059b35d0501e6778919"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ac7fea14ca847cfab73d0fec0751a19b13d665f30ce3fc057a1c1ed1e507cbbb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "75a8118f6941579ac13aa90dc1694012643011d6479d33c21dc803fe2b9f35a3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aab506afdfac678669b4afed23f847754b9e1732c21a5fc1e2b75d4c88898b0d"
+    sha256 cellar: :any_skip_relocation, ventura:       "db875104c477fbb377e9ed088e574f5ed01f7fdc46eb6ce3fd0de73a2b55a084"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e642bab20f9a68b18b35acb7436b3d2e64a3091d71a48f7998b40af06c1a81ad"
   end
 
   uses_from_macos "sqlite"
