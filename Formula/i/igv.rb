77870cc1bc6784597bda7c91e74c09adcb5ca250
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-03 22:54:40 +0000

    igv: update 2.18.4 bottle.

--- Formula/i/igv.rb
+++ Formula/i/igv.rb
@@ -11,7 +11,7 @@ class Igv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "721e0a52db6733ce82a3a1beec845e9243260a36aadcfbae7022c27b5328d988"
+    sha256 cellar: :any_skip_relocation, all: "bb216357c28ddab5510cd2316993a61b4d36f2ca509eb1b4ee6db960113cce9b"
   end
 
   depends_on "openjdk"
