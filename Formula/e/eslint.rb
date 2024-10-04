6e3de2a386750a15097045a9524035d97fea3e38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-04 22:14:03 +0000

    eslint: update 9.12.0 bottle.

--- Formula/e/eslint.rb
+++ Formula/e/eslint.rb
@@ -6,12 +6,12 @@ class Eslint < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6ad5da2b53bea1a9d0523ea7a3011562e7fd4abf4bd5b64fa8820fa17d27e8cd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ad5da2b53bea1a9d0523ea7a3011562e7fd4abf4bd5b64fa8820fa17d27e8cd"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6ad5da2b53bea1a9d0523ea7a3011562e7fd4abf4bd5b64fa8820fa17d27e8cd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e53b15886e751715e952e46425438f7aec7bbfc9699c924ab391acc776c8df36"
-    sha256 cellar: :any_skip_relocation, ventura:       "e53b15886e751715e952e46425438f7aec7bbfc9699c924ab391acc776c8df36"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ad5da2b53bea1a9d0523ea7a3011562e7fd4abf4bd5b64fa8820fa17d27e8cd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e816261b95765e65ebb8473595be5588969fb04a95af26a14634b678eeaacb8b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e816261b95765e65ebb8473595be5588969fb04a95af26a14634b678eeaacb8b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e816261b95765e65ebb8473595be5588969fb04a95af26a14634b678eeaacb8b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "012ff1069a1cd3872e3500ffa3eb484c86ec6dc4ad88e3276ce6160a51b37259"
+    sha256 cellar: :any_skip_relocation, ventura:       "012ff1069a1cd3872e3500ffa3eb484c86ec6dc4ad88e3276ce6160a51b37259"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e816261b95765e65ebb8473595be5588969fb04a95af26a14634b678eeaacb8b"
   end
 
   depends_on "node"
