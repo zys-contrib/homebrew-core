2bab2d4ca766744635ffa33b4af8f7058a2bb97f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-04 04:44:58 +0000

    opentofu: update 1.8.3 bottle.

--- Formula/o/opentofu.rb
+++ Formula/o/opentofu.rb
@@ -7,14 +7,12 @@ class Opentofu < Formula
   head "https://github.com/opentofu/opentofu.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "e037af85fb9b97931d4ce00e6e78900f845f82c85ff9247751969394b855a807"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "f21bd3d331f36ee695233d8cbcf7f6892aa66f084dd1366534b98080375fcaca"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f21bd3d331f36ee695233d8cbcf7f6892aa66f084dd1366534b98080375fcaca"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f21bd3d331f36ee695233d8cbcf7f6892aa66f084dd1366534b98080375fcaca"
-    sha256 cellar: :any_skip_relocation, sonoma:         "85ef5702b8f8fec1c73fd48083b7c72b14a02b86a69c3edb80e1af46dda11aae"
-    sha256 cellar: :any_skip_relocation, ventura:        "85ef5702b8f8fec1c73fd48083b7c72b14a02b86a69c3edb80e1af46dda11aae"
-    sha256 cellar: :any_skip_relocation, monterey:       "85ef5702b8f8fec1c73fd48083b7c72b14a02b86a69c3edb80e1af46dda11aae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "539c2f522ee963d56e355b13630dae0daca8ffe79edd1af1c68fe8048c3552a5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "89a64c811f9ee32d0495132643ede9b31123378571ca3ddd90ba497d0adc25cc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89a64c811f9ee32d0495132643ede9b31123378571ca3ddd90ba497d0adc25cc"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "89a64c811f9ee32d0495132643ede9b31123378571ca3ddd90ba497d0adc25cc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "edce89dfdd0aece61c8b791e6df15f36048a4bbbc63844ce5e18401d4f06fc77"
+    sha256 cellar: :any_skip_relocation, ventura:       "edce89dfdd0aece61c8b791e6df15f36048a4bbbc63844ce5e18401d4f06fc77"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05c3ff5180dac3e5dd29732baa35eec909245b7d2fe45fbcd67be2a5411d8086"
   end
 
   depends_on "go" => :build
