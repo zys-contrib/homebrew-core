1f7077e29a1e944589b5c36bb59069b06702abc4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-05 15:48:38 +0000

    miller 6.13.0

--- Formula/m/miller.rb
+++ Formula/m/miller.rb
@@ -1,8 +1,8 @@
 class Miller < Formula
   desc "Like sed, awk, cut, join & sort for name-indexed data such as CSV"
   homepage "https://github.com/johnkerl/miller"
-  url "https://github.com/johnkerl/miller/archive/refs/tags/v6.12.0.tar.gz"
-  sha256 "e97dab1a514ecd88da9374b2a5759cebc607476596f6b1d6d8fbe444b9e0eab2"
+  url "https://github.com/johnkerl/miller/archive/refs/tags/v6.13.0.tar.gz"
+  sha256 "6beca48af3066fc8d87c3ce17c5dd3debac61ff8f296c0e95c0634fd3ab05599"
   license "BSD-2-Clause"
   head "https://github.com/johnkerl/miller.git", branch: "main"
 
