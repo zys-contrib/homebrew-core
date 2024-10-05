788561949ea1c94b44a0c18de1213ad3d249e181
Rui Chen <rui@chenrui.dev>
2024-10-04 23:56:35 -0400

    wthrr 1.2.1
    autobump: add wthrr
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/wthrr.rb
+++ Formula/w/wthrr.rb
@@ -1,8 +1,8 @@
 class Wthrr < Formula
   desc "Weather Companion for the Terminal"
   homepage "https://github.com/ttytm/wthrr-the-weathercrab"
-  url "https://github.com/ttytm/wthrr-the-weathercrab/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "6dbad5cf37fc364ec383f3c04cb4c676b0be8b760c06c0b689d1a04fc082c66c"
+  url "https://github.com/ttytm/wthrr-the-weathercrab/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "ff5b47f2046ebefa9ff28cb52ece49a06f7b89230578801c338c77802aa721e0"
   license "MIT"
   head "https://github.com/ttytm/wthrr-the-weathercrab.git", branch: "main"
 
