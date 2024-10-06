1541f009303bfd8af319cfc8aa34820555b416ae
Sam Ford <1584702+samford@users.noreply.github.com>
2024-10-05 22:33:12 -0400

    goredo: update urls
    The existing www.goredo.cypherpunks.ru URLs in the `goredo` formula
    redirect to the .su domain, so this updates the URLs to avoid the
    redirection.

--- Formula/g/goredo.rb
+++ Formula/g/goredo.rb
@@ -1,12 +1,12 @@
 class Goredo < Formula
   desc "Go implementation of djb's redo, a Makefile replacement that sucks less"
-  homepage "http://www.goredo.cypherpunks.ru/"
-  url "http://www.goredo.cypherpunks.ru/download/goredo-2.6.2.tar.zst"
+  homepage "http://www.goredo.cypherpunks.su/"
+  url "http://www.goredo.cypherpunks.su/download/goredo-2.6.2.tar.zst"
   sha256 "5663ed0da911f1b2c0d13e92cc06ede3738639edb48499eb9e53d38e8e435d75"
   license "GPL-3.0-only"
 
   livecheck do
-    url "http://www.goredo.cypherpunks.ru/Install.html"
+    url "http://www.goredo.cypherpunks.su/Install.html"
     regex(/href=.*?goredo[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
