344b80c1d6360627c98efa7d3e7aad4c3dbb41f9
Michael Cho <michael@michaelcho.dev>
2024-10-06 16:17:06 -0400

    pypy3.10: use `tcl-tk@8`

--- Formula/p/pypy3.10.rb
+++ Formula/p/pypy3.10.rb
@@ -4,6 +4,7 @@ class Pypy310 < Formula
   url "https://downloads.python.org/pypy/pypy3.10-v7.3.17-src.tar.bz2"
   sha256 "6ad74bc578e9c6d3a8a1c51503313058e3c58c35df86f7485453c4be6ab24bf7"
   license "MIT"
+  revision 1
   head "https://github.com/pypy/pypy.git", branch: "main"
 
   livecheck do
@@ -27,7 +28,7 @@ class Pypy310 < Formula
   depends_on "gdbm"
   depends_on "openssl@3"
   depends_on "sqlite"
-  depends_on "tcl-tk"
+  depends_on "tcl-tk@8"
   depends_on "xz"
 
   uses_from_macos "bzip2"
@@ -66,11 +67,13 @@ class Pypy310 < Formula
 
   def install
     # The `tcl-tk` library paths are hardcoded and need to be modified for non-/usr/local prefix
+    tcltk = Formula["tcl-tk@8"]
     inreplace "lib_pypy/_tkinter/tklib_build.py" do |s|
-      s.gsub! "/usr/local/opt/tcl-tk/", Formula["tcl-tk"].opt_prefix/""
-      # We moved `tcl-tk` headers to `include/tcl-tk`.
+      s.gsub! "['/usr/local/opt/tcl-tk/include']", "[]"
+      # We moved `tcl-tk` headers to `include/tcl-tk` and versioned TCL 8
       # TODO: upstream this.
-      s.gsub! "/include'", "/include/tcl-tk'"
+      s.gsub! "(homebrew + '/include')", "('#{tcltk.opt_include}/tcl-tk')"
+      s.gsub! "(homebrew + '/opt/tcl-tk/lib')", "('#{tcltk.opt_lib}')"
     end
 
     if OS.mac?
