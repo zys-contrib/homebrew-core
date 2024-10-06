7db6d71bf3a1b2339e206da2f73f1dad5029318e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-06 22:47:50 +0000

    cpplint: update 2.0.0 bottle.

--- Formula/c/cpplint.rb
+++ Formula/c/cpplint.rb
@@ -8,15 +8,7 @@ class Cpplint < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 5
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "a520db08cb75abe19ed23000c038712f423b26212d425b0a4495d18c36fd4dd1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "b1ebcdaadbf95e043eee26a5e61ef099d163f02b1024bfd6a48e25b43bb1e40a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "160d2679081ce1941f4c026ae9808ad2b7611afb937b6a3536b00be8709796f5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5ae38619920a9e68a04561d58ddfdbdca7a214beaa4f2d7a0a53a37c1da3e4db"
-    sha256 cellar: :any_skip_relocation, sonoma:         "491893d9094e7f1565ae342c8aceecde9f7ccbc011fe10546b2690507bd8fee1"
-    sha256 cellar: :any_skip_relocation, ventura:        "4e6456089c2d047c90d1533bbe3ea2be9fe341ed595c990eeff4a4f44b31be6a"
-    sha256 cellar: :any_skip_relocation, monterey:       "5f433ee51e12a95f15dae3023877f3e84a6cb87ae27cc20f7052a5de39e9d7df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f7f80a3e8068bebc91afb5c7cb391aaddc15bc098ae7f5698a4b5bfbd09439dc"
+    sha256 cellar: :any_skip_relocation, all: "3ded04a7bbc125e6c0117de4b193c16f2d26468159d0c895ce0555d723c78a10"
   end
 
   depends_on "python@3.12"
