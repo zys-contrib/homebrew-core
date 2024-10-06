89fe365cd968523b5e3e819988d3c7bb20e45bc1
Michael Cho <michael@michaelcho.dev>
2024-10-06 13:14:02 -0400

    dwdiff: revision bump to migrate to `icu4c@75`

--- Formula/d/dwdiff.rb
+++ Formula/d/dwdiff.rb
@@ -4,7 +4,7 @@ class Dwdiff < Formula
   url "https://os.ghalkes.nl/dist/dwdiff-2.1.4.tar.bz2"
   sha256 "df16fec44dcb467d65a4246a43628f93741996c1773e930b90c6dde22dd58e0a"
   license "GPL-3.0-only"
-  revision 7
+  revision 8
 
   livecheck do
     url "https://os.ghalkes.nl/dist/"
@@ -24,7 +24,7 @@ class Dwdiff < Formula
 
   depends_on "gettext" => :build
   depends_on "pkg-config" => :build
-  depends_on "icu4c"
+  depends_on "icu4c@75"
 
   on_macos do
     depends_on "gettext"
