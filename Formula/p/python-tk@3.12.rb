536ed404f0f0ace728b292ce57750e2d45cb9c98
Michael Cho <michael@michaelcho.dev>
2024-10-06 16:03:41 -0400

    python-tk@3.12: revision bump (tcl-tk 9.0.0)

--- Formula/p/python-tk@3.12.rb
+++ Formula/p/python-tk@3.12.rb
@@ -4,6 +4,7 @@ class PythonTkAT312 < Formula
   url "https://www.python.org/ftp/python/3.12.7/Python-3.12.7.tgz"
   sha256 "73ac8fe780227bf371add8373c3079f42a0dc62deff8d612cd15a618082ab623"
   license "Python-2.0"
+  revision 1
 
   livecheck do
     formula "python@3.12"
@@ -35,7 +36,7 @@ class PythonTkAT312 < Formula
 
     cd "Modules" do
       tcltk_version = Formula["tcl-tk"].any_installed_version.major_minor
-      (Pathname.pwd/"setup.py").write <<~PYTHON
+      Pathname("setup.py").write <<~PYTHON
         from setuptools import setup, Extension
 
         setup(name="tkinter",
@@ -45,7 +46,7 @@ class PythonTkAT312 < Formula
                 Extension("_tkinter", ["_tkinter.c", "tkappinit.c"],
                           define_macros=[("WITH_APPINIT", 1)],
                           include_dirs=["#{python_include}/internal", "#{Formula["tcl-tk"].opt_include/"tcl-tk"}"],
-                          libraries=["tcl#{tcltk_version}", "tk#{tcltk_version}"],
+                          libraries=["tcl#{tcltk_version}", "tcl#{tcltk_version.major}tk#{tcltk_version}"],
                           library_dirs=["#{Formula["tcl-tk"].opt_lib}"])
               ]
         )
