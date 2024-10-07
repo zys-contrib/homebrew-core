aa254d87099c35bc00a29d2c5291b33df9c7ae7a
Rui Chen <rui@chenrui.dev>
2024-10-07 12:38:20 +0000

    dug: add 0.0.94 bottle.

--- Formula/d/dug.rb
+++ Formula/d/dug.rb
@@ -10,6 +10,15 @@ class Dug < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5fc22af9ae66c539b84fd9202cc15442c5cb9ccc2da736c25073c01cd7cb7d1c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b218fc33ff689d05ad7637d3a90516eff22c12721dce9c45a6bc0b9d7d8005b8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a2b486b453398832444fe293c2114ae52415e32450a307efd4f8d794e612978b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bafbcd64d21759be827798be5e716d58f4c5b25272884d6efc517d0c56a88885"
+    sha256 cellar: :any_skip_relocation, ventura:       "c5fca1866650201da0bf750a980bb99c02635b45c10d12f07de0dd004ea65b5d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "373a0464e15b4ceb0af958ceaf67c701946f521bec7654477f0036b585aff48c"
+  end
+
   depends_on "dotnet"
   uses_from_macos "zlib"
 
