c2397d0d3d0f68f728ca35b807b19c5d96d7f2e3
Rui Chen <rui@chenrui.dev>
2024-10-07 02:48:43 +0000

    libcss: add 0.9.2 bottle.

--- Formula/lib/libcss.rb
+++ Formula/lib/libcss.rb
@@ -6,6 +6,15 @@ class Libcss < Formula
   license "MIT"
   head "https://git.netsurf-browser.org/libcss.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "f065539111cfaeb57ffaa1f1918d0a5eb40a27193a0de07a976d8822941bb5a0"
+    sha256 cellar: :any,                 arm64_sonoma:  "c7083640028fdeeed54ea337f87d86686a92659392ed2939ed60947f06c95e43"
+    sha256 cellar: :any,                 arm64_ventura: "50b100529a9304753171f567a8099c347d5f538d89b5fc0a673fa22c7a2ca347"
+    sha256 cellar: :any,                 sonoma:        "69f0f19a1a2b2cd7e9493620542a5a2479035e2c2366889993091240366f21c5"
+    sha256 cellar: :any,                 ventura:       "3b327a9d304d9964c5b98157661b194ba5156f4a32ca01af4e0ae835655aebbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "459691f366d4877a30ac535cf1767ed6f681a922435c3b7893d52eb6871c93a8"
+  end
+
   depends_on "netsurf-buildsystem" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on "libparserutils"
