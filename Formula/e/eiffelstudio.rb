d37b7f3271ff88ac06ced0566e492fc4feb57b05
Michael Cho <michael@michaelcho.dev>
2024-10-06 21:43:03 -0400

    eiffelstudio 23.09.107341

--- Formula/e/eiffelstudio.rb
+++ Formula/e/eiffelstudio.rb
@@ -1,11 +1,10 @@
 class Eiffelstudio < Formula
   desc "Development environment for the Eiffel language"
   homepage "https://www.eiffel.com"
-  url "https://ftp.eiffel.com/pub/download/22.05/pp/PorterPackage_std_106302.tar"
-  version "22.05.10.6302"
-  sha256 "c2ede38b19cedead58a9e075cf79d6a4b113e049c0723fe9556c4f36ee68b80d"
+  url "https://ftp.eiffel.com/pub/download/23.09/pp/PorterPackage_std_23.09_107341.tar"
+  version "23.09.107341"
+  sha256 "f92dad3226b81e695ba6deb752d7b8e84351f1dcab20e18492cc56a2b7d8d4b1"
   license "GPL-2.0-only"
-  revision 1
 
   livecheck do
     url "https://ftp.eiffel.com/pub/download/latest/pp/"
@@ -26,40 +25,52 @@ class Eiffelstudio < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "gdk-pixbuf"
+  depends_on "glib"
   depends_on "gtk+3"
+  depends_on "libx11"
+  depends_on "pango"
 
   uses_from_macos "pax" => :build
 
+  on_macos do
+    depends_on "at-spi2-core"
+    depends_on "gettext"
+    depends_on "harfbuzz"
+  end
+
   def install
-    # Fix flat namespace usage in C shared library.
-    if OS.mac?
-      system "tar", "xf", "c.tar.bz2"
-      inreplace "C/CONFIGS/macosx-x86-64", "-flat_namespace -undefined suppress", "-undefined dynamic_lookup"
-      system "tar", "cjf", "c.tar.bz2", "C"
-    end
+    platform = "#{OS.mac? ? "macosx" : OS.kernel_name.downcase}-x86-64"
 
-    # Use ENV.cc to link shared objects instead of directly invoking ld.
-    # Reported upstream: https://support.eiffel.com/report_detail/19873.
-    if OS.linux?
-      system "tar", "xf", "c.tar.bz2"
-      inreplace "C/CONFIGS/linux-x86-64", "sharedlink='ld'", "sharedlink='#{ENV.cc}'"
-      inreplace "C/CONFIGS/linux-x86-64", "ldflags=\"-m elf_x86_64\"", "ldflags=''"
-      system "tar", "cjf", "c.tar.bz2", "C"
+    # Apply workarounds
+    ENV.append_to_cflags "-Wno-incompatible-function-pointer-types" if DevelopmentTools.clang_build_version >= 1500
+    system "tar", "xf", "c.tar.bz2"
+    inreplace "C/CONFIGS/#{platform}" do |s|
+      if OS.mac?
+        # Fix flat namespace usage in C shared library.
+        s.gsub! "-flat_namespace -undefined suppress", "-undefined dynamic_lookup"
+      else
+        # Use ENV.cc to link shared objects instead of directly invoking ld.
+        # Reported upstream: https://support.eiffel.com/report_detail/19873.
+        s.gsub! "sharedlink='ld'", "sharedlink='#{ENV.cc}'"
+        s.gsub! "ldflags=\"-m elf_x86_64\"", "ldflags=''"
+      end
     end
+    system "tar", "cjf", "c.tar.bz2", "C"
 
-    os = OS.mac? ? "macosx" : OS.kernel_name.downcase
-    os_tag = "#{os}-x86-64"
-    system "./compile_exes", os_tag
-    system "./make_images", os_tag
-    prefix.install Dir["Eiffel_#{version.major}.#{version.minor.to_s.rjust(2, "0")}/*"]
-    eiffel_env = { ISE_EIFFEL: prefix, ISE_PLATFORM: os_tag }
+    system "./compile_exes", platform
+    system "./make_images", platform
+    prefix.install (buildpath/"Eiffel_#{version.to_s[/^(\d+\.\d+)/, 1]}").children
+
+    eiffel_env = { ISE_EIFFEL: prefix, ISE_PLATFORM: platform }
     {
       studio:       %w[ec ecb estudio finish_freezing],
       tools:        %w[compile_all iron syntax_updater],
       vision2_demo: %w[vision2_demo],
     }.each do |subdir, targets|
       targets.each do |target|
-        (bin/target).write_env_script prefix/subdir.to_s/"spec"/os_tag/"bin"/target, eiffel_env
+        (bin/target).write_env_script prefix/subdir.to_s/"spec"/platform/"bin"/target, eiffel_env
       end
     end
   end
