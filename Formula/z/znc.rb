2554e04007bb3a09a3015ea15680a31074fb1233
Michael Cho <michael@michaelcho.dev>
2024-10-07 15:09:26 -0400

    znc: revision bump to migrate to `icu4c@76`

--- Formula/z/znc.rb
+++ Formula/z/znc.rb
@@ -4,7 +4,7 @@ class Znc < Formula
   url "https://znc.in/releases/znc-1.9.1.tar.gz"
   sha256 "e8a7cf80e19aad510b4e282eaf61b56bc30df88ea2e0f64fadcdd303c4894f3c"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://znc.in/releases/"
@@ -24,7 +24,7 @@ class Znc < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
-  depends_on "icu4c@75"
+  depends_on "icu4c@76"
   depends_on "openssl@3"
   depends_on "python@3.13"
 
