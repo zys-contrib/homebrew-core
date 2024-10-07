691c4a35bc69aa27738dcdaed70f18ddbb5d5be1
Patrick Linnane <patrick@linnane.io>
2024-10-07 20:24:35 +0000

    libhubbub: add 0.3.8 bottle.

--- Formula/lib/libhubbub.rb
+++ Formula/lib/libhubbub.rb
@@ -6,6 +6,15 @@ class Libhubbub < Formula
   license "MIT"
   head "https://git.netsurf-browser.org/libhubbub.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "508f0ebd9f00da4d05a4d477c55c9cae35ecd8b2b80ae83871623ae11c67ef9b"
+    sha256 cellar: :any,                 arm64_sonoma:  "e024f9c815850423b7eed9a0cb5c9d3f0a83e980f6a5aced31b006c9f43fd6f8"
+    sha256 cellar: :any,                 arm64_ventura: "81e8aa3aae89f339efbf14d216270d1c57729e525b20c7d977dfa539f71012b0"
+    sha256 cellar: :any,                 sonoma:        "015c9012d635556347bf3a0e1f5d003b81a5834f2a91c717ebe9929e5fd8a232"
+    sha256 cellar: :any,                 ventura:       "0544ada9a05f3db96da5338b41f0251e265fb784b1109ec0e688ffeb942f568b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1156a785370dd503db4817f034028986f1c6eea21d79c0c59a2a66f6ca07391c"
+  end
+
   depends_on "netsurf-buildsystem" => :build
   depends_on "pkg-config" => :build
   depends_on "libparserutils"
