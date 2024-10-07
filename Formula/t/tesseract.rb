be8dafdb4338e702d045e46af5d598279880666e
Michael Cho <michael@michaelcho.dev>
2024-10-07 14:01:52 -0400

    tesseract: revision bump to migrate to `icu4c@76`

--- Formula/t/tesseract.rb
+++ Formula/t/tesseract.rb
@@ -4,7 +4,7 @@ class Tesseract < Formula
   url "https://github.com/tesseract-ocr/tesseract/archive/refs/tags/5.4.1.tar.gz"
   sha256 "c4bc2a81c12a472f445b7c2fb4705a08bd643ef467f51ec84f0e148bd368051b"
   license "Apache-2.0"
-  revision 1
+  revision 2
   head "https://github.com/tesseract-ocr/tesseract.git", branch: "main"
 
   livecheck do
@@ -29,7 +29,7 @@ class Tesseract < Formula
   depends_on "fontconfig"
   depends_on "glib"
   depends_on "harfbuzz"
-  depends_on "icu4c@75"
+  depends_on "icu4c@76"
   depends_on "leptonica"
   depends_on "libarchive"
   depends_on "pango"
