b755f08088000fe1197d15a2c0f88284537c9cee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-07 11:47:23 +0000

    afl++: update 4.21_1 bottle.

--- Formula/a/afl++.rb
+++ Formula/a/afl++.rb
@@ -7,12 +7,13 @@ class Aflxx < Formula
   revision 1
 
   bottle do
-    sha256 arm64_sequoia: "2d620933992908d3493f17b4e44c9f388e560c61ea07f5eeeaba6f06958c2981"
-    sha256 arm64_sonoma:  "a91b21cfbebbbf85e072a2b564391e9abe6dacd23ae846d779a274f54c5d56a0"
-    sha256 arm64_ventura: "af51fb3aacb8fc34e83c0d5473bcce88bcfcd5bee97a7d200df6b330573b1e74"
-    sha256 sonoma:        "9972f30928d25849cf62eec6af83b4598e4cd38e34a465af4b871ee8cfac443f"
-    sha256 ventura:       "3ecd8dae6fd187a902ac7e448e5f180440fc0a392ceb84569bc67a4dc3c498db"
-    sha256 x86_64_linux:  "d405240caa4d8bd5c3f2d912da1aa755134d450f87c05929560356692354cedb"
+    rebuild 1
+    sha256 arm64_sequoia: "bc36a276193f8bc347b58ede62c96289e5f4760c49df5b34880af7e1b096adef"
+    sha256 arm64_sonoma:  "007e43ccfcd0ababf4201c61ca8bd5af261aa2d8ccd92554dbe07f90466adf08"
+    sha256 arm64_ventura: "19152794969e4f6a35db1cfce3bba890a624ce00d60f0eb562fa9ded84026af7"
+    sha256 sonoma:        "0c1fb8e9a68cb26013482eb476e1b137193aba3c4b99e45ea53d16adc602a2a3"
+    sha256 ventura:       "e1339535ffd0683a2f9159054ed40d603a2071398a90acc0b76c470810a2e0b5"
+    sha256 x86_64_linux:  "e303493ba40b6ba430be07a4eaabe3ed65c86308dbdff712be5c16a527f808bf"
   end
 
   depends_on "coreutils" => :build
