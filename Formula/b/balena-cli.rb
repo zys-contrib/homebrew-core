5c26109f996f122005079a633c9eb55d3ecc3b26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-08 19:04:23 +0000

    balena-cli: update 19.0.18 bottle.

--- Formula/b/balena-cli.rb
+++ Formula/b/balena-cli.rb
@@ -11,12 +11,12 @@ class BalenaCli < Formula
   end
 
   bottle do
-    sha256                               arm64_sequoia: "ccfdaab6b15cd9976da61a688db0c4d1e55a15081906ae3621822c23cba40f21"
-    sha256                               arm64_sonoma:  "dfca4aad6c612e6dae6a34478d175018056386dd8fe4851e08e525f72a5eb206"
-    sha256                               arm64_ventura: "05d4792b6d847be16ad1b7de0fa2fe344b2566bb68af0f277c36b3cbe24b0520"
-    sha256                               sonoma:        "22e8ba93cc4464cc510f672f581b82a343830d2de792fa43d8e01be19257efae"
-    sha256                               ventura:       "778a508c58a7bfde28d3dccf2ff57a3b507fbb6c8c530968d77c7da614332b79"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e220e48dbbbcc0ac587f668582498186d616026b619f217b9b1248154350029d"
+    sha256                               arm64_sequoia: "67f612248e59c144679203b02d210425a85fe09aa05ab8c73f41dc9c972a380e"
+    sha256                               arm64_sonoma:  "94b6a412bb1382dd2ea9a33dbac2ae6f3879b022d7078d9ec141c692c45e5746"
+    sha256                               arm64_ventura: "4a9558ea160d29aed3085414d6ecd3507995a7e5c937e4cd0cb6e4abeb4b3b64"
+    sha256                               sonoma:        "3f9b983bb1f798b70d054fbaae636fa9e925b878f4ecbb8779362e56fc8c3c62"
+    sha256                               ventura:       "11535f15b14e3312ecdb9109d07b11ae65697c9a1a6025a603c81ab807619cb7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55c3584f0fed3d3a6f49918a9d5e12814a0a3d667345478fd1e4fe391fd6077d"
   end
 
   # need node@20, and also align with upstream, https://github.com/balena-io/balena-cli/blob/master/.github/actions/publish/action.yml#L21
