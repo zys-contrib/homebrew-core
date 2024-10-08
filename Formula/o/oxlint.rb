b0a0038cff95832abac358f10f78611ef8fffb4b
Rui Chen <rui@chenrui.dev>
2024-10-07 23:39:56 -0400

    oxlint 0.9.9 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/o/oxlint.rb
@@ -0,0 +1,28 @@
+class Oxlint < Formula
+  desc "Suite of high-performance tools for JavaScript and TypeScript written in Rust"
+  homepage "https://oxc.rs/"
+  url "https://github.com/oxc-project/oxc/archive/refs/tags/oxlint_v0.9.9.tar.gz"
+  sha256 "75f8c87eadae6260469dd4bcc8ce5c7ae9cffcff77c2c477d2fc09ac0a351666"
+  license "MIT"
+  head "https://github.com/oxc-project/oxc.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^oxlint[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    ENV["OXC_VERSION"] = version.to_s
+    system "cargo", "install", *std_cargo_args(path: "apps/oxlint")
+  end
+
+  test do
+    (testpath/"test.js").write "const x = 1;"
+    output = shell_output("#{bin}/oxlint test.js 2>&1")
+    assert_match "eslint(no-unused-vars): Variable 'x' is declared but never used", output
+
+    assert_match version.to_s, shell_output("#{bin}/oxlint --version")
+  end
+end
