b99585a937fb1fc8bb509e6e7a865eb7e8cf6166
Michael Cho <michael@michaelcho.dev>
2024-10-07 21:43:59 -0400

    brag: use `tcl-tk@8` on Linux

--- Formula/b/brag.rb
+++ Formula/b/brag.rb
@@ -4,6 +4,7 @@ class Brag < Formula
   url "https://downloads.sourceforge.net/project/brag/brag/1.4.3/brag-1.4.3.tar.gz"
   sha256 "f2c8110c38805c31ad181f4737c26e766dc8ecfa2bce158197b985be892cece6"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     rebuild 1
@@ -13,11 +14,14 @@ class Brag < Formula
   depends_on "uudeview"
 
   on_linux do
-    depends_on "tcl-tk"
+    depends_on "tcl-tk@8"
   end
 
   def install
     bin.install "brag"
+    # macOS needs /usr/bin before #{HOMEBREW_PREFIX}/bin to avoid incompatible TCL 9.
+    # We prepend both PATH on all OS to retain `all` bottle.
+    bin.env_script_all_files libexec, PATH: "#{Formula["tcl-tk@8"].opt_bin}:/usr/bin:${PATH}"
     man1.install "brag.1"
   end
 
