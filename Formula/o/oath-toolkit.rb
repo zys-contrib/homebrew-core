61fb3a2bbfec377c9f0b0c65b4afd806698e45e7
Rui Chen <rui@chenrui.dev>
2024-10-08 18:38:50 -0400

    oath-toolkit 2.6.12
    autobump: add oath-toolkit
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/oath-toolkit.rb
+++ Formula/o/oath-toolkit.rb
@@ -1,9 +1,9 @@
 class OathToolkit < Formula
   desc "Tools for one-time password authentication systems"
   homepage "https://www.nongnu.org/oath-toolkit/"
-  url "https://download-mirror.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.6.11.tar.gz"
-  mirror "https://fossies.org/linux/privat/oath-toolkit-2.6.11.tar.gz"
-  sha256 "fc512a4a5b46f4c43ab0586c3189fece4d54f7e649397d6fa1e23428431e2cb4"
+  url "https://download-mirror.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.6.12.tar.gz"
+  mirror "https://fossies.org/linux/privat/oath-toolkit-2.6.12.tar.gz"
+  sha256 "cafdf739b1ec4b276441c6aedae6411434bbd870071f66154b909cc6e2d9e8ba"
   license all_of: ["GPL-3.0-or-later", "LGPL-2.1-or-later"]
 
   livecheck do
