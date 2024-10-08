a697e57134181ef042fb0fdd56de905438574fd0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-08 03:26:08 +0000

    bazel-diff 7.2.16

--- Formula/b/bazel-diff.rb
+++ Formula/b/bazel-diff.rb
@@ -1,8 +1,8 @@
 class BazelDiff < Formula
   desc "Performs Bazel Target Diffing between two revisions in Git"
   homepage "https://github.com/Tinder/bazel-diff/"
-  url "https://github.com/Tinder/bazel-diff/releases/download/7.2.15/bazel-diff_deploy.jar"
-  sha256 "d90278cbccb87c57fc8359ffb4c1c74b9c7b1069e777df44b6e02ef65cc95971"
+  url "https://github.com/Tinder/bazel-diff/releases/download/7.2.16/bazel-diff_deploy.jar"
+  sha256 "5d447e3f71d1d1e253bd78233c9989e1078b0fd6c453a6d594a4fda7f6fd483f"
   license "BSD-3-Clause"
 
   bottle do
