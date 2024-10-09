72d8be7e53b91ee7ce941855604f632c66bb2c9e
Branch Vincent <branchevincent@gmail.com>
2024-10-09 08:02:10 -0700

    six: add `python@3.13`

--- Formula/s/six.rb
+++ Formula/s/six.rb
@@ -4,7 +4,7 @@ class Six < Formula
   url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
   sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   license "MIT"
-  revision 4
+  revision 5
 
   bottle do
     rebuild 1
@@ -13,6 +13,7 @@ class Six < Formula
 
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
+  depends_on "python@3.13" => [:build, :test]
 
   def pythons
     deps.map(&:to_formula).sort_by(&:version).select { |f| f.name.start_with?("python@") }
