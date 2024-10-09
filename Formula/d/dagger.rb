8fdf1ef96ff0ffc09abac8359daf7086810a25d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-09 18:30:49 +0000

    dagger 0.13.4

--- Formula/d/dagger.rb
+++ Formula/d/dagger.rb
@@ -2,8 +2,8 @@ class Dagger < Formula
   desc "Portable devkit for CI/CD pipelines"
   homepage "https://dagger.io"
   url "https://github.com/dagger/dagger.git",
-      tag:      "v0.13.3",
-      revision: "d3a02170c96d4d81343dbbcd5c555ba6b8d38b45"
+      tag:      "v0.13.4",
+      revision: "b35757b05ff35243483dbc6ab0a3cf9a95b41b59"
   license "Apache-2.0"
   head "https://github.com/dagger/dagger.git", branch: "main"
 
