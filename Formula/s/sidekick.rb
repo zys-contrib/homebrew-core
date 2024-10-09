7238f8947307132594441e82f0f241fb82a2518b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-09 12:20:38 +0000

    sidekick: update 0.6.3 bottle.

--- Formula/s/sidekick.rb
+++ Formula/s/sidekick.rb
@@ -6,12 +6,12 @@ class Sidekick < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d2b3ec898a4dc3d31021292935b605c5b9416157527e9f2891a495215e7f2767"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d2b3ec898a4dc3d31021292935b605c5b9416157527e9f2891a495215e7f2767"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d2b3ec898a4dc3d31021292935b605c5b9416157527e9f2891a495215e7f2767"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5071238ce5b3835245d352ef87b62845d792872e0062cd604a13236b12fe5ca7"
-    sha256 cellar: :any_skip_relocation, ventura:       "5071238ce5b3835245d352ef87b62845d792872e0062cd604a13236b12fe5ca7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1fe09931ae7cd50e23f5f40a97273a085a842c90b3c7333d115090317fa84fcf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45adfa0f7640f06983f46e8a56599f58d347ade6c65e273bc1bb748d5c18c467"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45adfa0f7640f06983f46e8a56599f58d347ade6c65e273bc1bb748d5c18c467"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "45adfa0f7640f06983f46e8a56599f58d347ade6c65e273bc1bb748d5c18c467"
+    sha256 cellar: :any_skip_relocation, sonoma:        "283ed8b1d05a515100fb1a2654698ed49bd860fb43ec0606ed0dd279fa1d0781"
+    sha256 cellar: :any_skip_relocation, ventura:       "283ed8b1d05a515100fb1a2654698ed49bd860fb43ec0606ed0dd279fa1d0781"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "366a5fd8e25c7fe85cfece626d62ccbb273254f1d9ee3c259796bd0980c42790"
   end
 
   depends_on "go" => :build
