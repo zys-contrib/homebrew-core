3938b8f055f40cd7fbe3fca29661a638b4bdee19
Rui Chen <rui@chenrui.dev>
2024-10-09 18:25:18 -0400

    fonttools: migrate to `python@3.13`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fonttools.rb
+++ Formula/f/fonttools.rb
@@ -17,7 +17,7 @@ class Fonttools < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d6473b1ce2ae00ef7427679b108fb339ddf90e4795910a75fbaf7e60cd461f1"
   end
 
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "brotli" do
     url "https://files.pythonhosted.org/packages/2f/c2/f9e977608bdf958650638c3f1e28f85a1b075f075ebbe77db8555463787b/Brotli-1.1.0.tar.gz"
