dba2cd89ff6a7f38eddbcb820fe4692295a22a88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-09 19:52:45 +0000

    promptfoo: update 0.92.2 bottle.

--- Formula/p/promptfoo.rb
+++ Formula/p/promptfoo.rb
@@ -6,12 +6,12 @@ class Promptfoo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e407f206d29bb54638c402471f8bacb7e271352c83526dd7a5677df261abc1e0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f8ced4887ad3d3511aab9e4d37968c66eb99670532a1eaa59c37ddacc77d7f39"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3a9ed39710973396ff25ff721f62ab4e7c6c1f596d7ae968caaa9db3f2ffeee2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "58d953ab80201bc85b937c45d01cfe61f812e513d39f3c27c07a39a9501ac254"
-    sha256 cellar: :any_skip_relocation, ventura:       "ad55108579962e03a387ffc9dc918866d54d1954e6a7dfe4d38da8ceaf5722e9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10d22b27f6efd9394de55907f1c9ec898d3550243efb783e017568da214ea666"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "415bca172ea0534d6163c3c83e547b49777f7d414780b54bd7a049c338131f16"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3f8d3384db6647115bbffaf687ed396c4bae7a9224b0d127117bcdd98d6a0a99"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7a0f70dd1b5684125b6137161c4b7f118693daaa2f5b1fa72f2ed784881ae888"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c359efff8c1b134c367fe2d94f1e3c6cc82b0dd5ca9e04a2f0876e09df9a96e7"
+    sha256 cellar: :any_skip_relocation, ventura:       "131c0b05f9ea6fcf78eb8efd5d28f65c66d5853adc4eb9e9bc0a077160360630"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d1823cdb93e98c8e800ffc14a1b2dadec2b206820731709a0d9ca6d083bb07c"
   end
 
   depends_on "node"
