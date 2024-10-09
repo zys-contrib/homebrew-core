474b682a5407fcfa4cf8d2b73c5301470146003a
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-10-09 12:41:41 +0800

    etcd-cpp-apiv3: revision bump (grpc 1.67.0)

--- Formula/e/etcd-cpp-apiv3.rb
+++ Formula/e/etcd-cpp-apiv3.rb
@@ -4,7 +4,7 @@ class EtcdCppApiv3 < Formula
   url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.15.4.tar.gz"
   sha256 "4516ecfa420826088c187efd42dad249367ca94ea6cdfc24e3030c3cf47af7b4"
   license "BSD-3-Clause"
-  revision 14
+  revision 15
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "20f82f903c044f30052624d69290f367cb7248ae3ff900cd73c97cdde5335980"
