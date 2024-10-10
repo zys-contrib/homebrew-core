0876b4b71c7f0ceb7626b7db9dbc2b79bc8ce419
Alex Gaynor <alex.gaynor@gmail.com>
2024-10-10 16:43:06 -0400

    virtualfish: migrate to `python@3.13`
    virtualfish: migrate to `python@3.13`

--- Formula/v/virtualfish.rb
+++ Formula/v/virtualfish.rb
@@ -20,7 +20,7 @@ class Virtualfish < Formula
   end
 
   depends_on "fish"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "distlib" do
     url "https://files.pythonhosted.org/packages/c4/91/e2df406fb4efacdf46871c25cde65d3c6ee5e173b7e5a4547a47bae91920/distlib-0.3.8.tar.gz"
