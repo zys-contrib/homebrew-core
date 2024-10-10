2d24f6f216abbc7e493ce5754fd27663144d1e7c
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-10-10 15:01:30 +0800

    watchman: migrate to `python@3.13`
    I've skipped the `revision` bump since this will probably get a version
    bump in a few days.

--- Formula/w/watchman.rb
+++ Formula/w/watchman.rb
@@ -31,7 +31,7 @@ class Watchman < Formula
   depends_on "libevent"
   depends_on "openssl@3"
   depends_on "pcre2"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   on_linux do
     depends_on "boost"
@@ -50,7 +50,7 @@ class Watchman < Formula
     # Use the upstream default for WATCHMAN_STATE_DIR by unsetting it.
     args = %W[
       -DENABLE_EDEN_SUPPORT=ON
-      -DPython3_EXECUTABLE=#{which("python3.12")}
+      -DPython3_EXECUTABLE=#{which("python3.13")}
       -DWATCHMAN_VERSION_OVERRIDE=#{version}
       -DWATCHMAN_BUILDINFO_OVERRIDE=#{tap&.user || "Homebrew"}
       -DWATCHMAN_STATE_DIR=
