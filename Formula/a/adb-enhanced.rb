dd9f8dd055fd15d1f3af5268b68651674ba389bb
Branch Vincent <branchevincent@gmail.com>
2024-10-10 07:50:16 -0700

    adb-enhanced: migrate to `python@3.13`

--- Formula/a/adb-enhanced.rb
+++ Formula/a/adb-enhanced.rb
@@ -18,7 +18,7 @@ class AdbEnhanced < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ce41b0c1a4fbcea81a08237bdf8630bcd93e8af60420bd346c482b2401ad2267"
   end
 
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "docopt" do
     url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
@@ -26,8 +26,8 @@ class AdbEnhanced < Formula
   end
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/90/c7/6dc0a455d111f68ee43f27793971cf03fe29b6ef972042549db29eec39a2/psutil-5.9.8.tar.gz"
-    sha256 "6be126e3225486dff286a8fb9a06246a5253f4c7c53b475ea5f5ac934e64194c"
+    url "https://files.pythonhosted.org/packages/18/c7/8c6872f7372eb6a6b2e4708b88419fb46b857f7a2e1892966b851cc79fc9/psutil-6.0.0.tar.gz"
+    sha256 "8faae4f310b6d969fa26ca0545338b21f73c6b15db7c4a8d934a5482faa818f2"
   end
 
   def install
