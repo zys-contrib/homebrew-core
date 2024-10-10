c427a8866bb6e29b552b5d606a3690b9d7e18126
Rui Chen <rui@chenrui.dev>
2024-10-10 00:39:39 -0400

    woof: migrate to `python@3.13`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/woof.rb
+++ Formula/w/woof.rb
@@ -12,9 +12,15 @@ class Woof < Formula
     sha256 cellar: :any_skip_relocation, all: "85edeaa4bfcf9350a5d1822bb96d1381174a359a4f036f11c71c16829177bd87"
   end
 
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   conflicts_with "woof-doom", because: "both install `woof` binaries"
+
+  # patch to not use cgi module (removed in python 3.13)
+  patch do
+    url "https://github.com/simon-budig/woof/commit/f501798350f98338678832010a26d53f9c33e9d6.patch?full_index=1"
+    sha256 "8f4b895081cb177ae9aa9b0acee0c42775d2072495eb31c1a2ae9bccd97cce47"
+  end
 
   def install
     rewrite_shebang detected_python_shebang, "woof"
