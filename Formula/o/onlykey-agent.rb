1ba4887bbc633dd1c774906c7f28a6e78f6c990a
Branch Vincent <branchevincent@gmail.com>
2024-10-09 20:20:56 -0700

    onlykey-agent: migrate to `python@3.13`

--- Formula/o/onlykey-agent.rb
+++ Formula/o/onlykey-agent.rb
@@ -26,7 +26,7 @@ class OnlykeyAgent < Formula
   depends_on "gnupg"
   depends_on "hidapi"
   depends_on "libusb"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "aenum" do
     url "https://files.pythonhosted.org/packages/d0/f8/33e75863394f42e429bb553e05fda7c59763f0fd6848de847a25b3fbccf6/aenum-3.1.15.tar.gz"
@@ -44,8 +44,8 @@ class OnlykeyAgent < Formula
   end
 
   resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
-    sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
+    url "https://files.pythonhosted.org/packages/f2/4f/e1808dc01273379acc506d18f1504eb2d299bd4131743b9fc54d7be4df1e/charset_normalizer-3.4.0.tar.gz"
+    sha256 "223217c3d4f82c3ac5e29032b3f1c2eb0fb591b72161f86d93f5719079dae93e"
   end
 
   resource "click" do
