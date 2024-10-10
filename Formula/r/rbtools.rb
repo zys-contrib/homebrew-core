f0666d69c25296208af5347db2db97448be45ef5
Branch Vincent <branchevincent@gmail.com>
2024-10-09 21:37:24 -0700

    rbtools: migrate to `python@3.13`

--- Formula/r/rbtools.rb
+++ Formula/r/rbtools.rb
@@ -21,7 +21,7 @@ class Rbtools < Formula
   end
 
   depends_on "certifi"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "colorama" do
     url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
