77f424288941fa235140216e5913a0dd1922541a
Alex Gaynor <alex.gaynor@gmail.com>
2024-10-10 16:43:38 -0400

    yt-dlp: migrate to `python@3.13`
    yt-dlp: migrate to `python@3.13`

--- Formula/y/yt-dlp.rb
+++ Formula/y/yt-dlp.rb
@@ -27,7 +27,7 @@ class YtDlp < Formula
   end
 
   depends_on "certifi"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "brotli" do
     url "https://files.pythonhosted.org/packages/2f/c2/f9e977608bdf958650638c3f1e28f85a1b075f075ebbe77db8555463787b/Brotli-1.1.0.tar.gz"
