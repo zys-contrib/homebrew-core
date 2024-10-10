a7cca64eed51d64b26ff15bfff2d1398f5052e5b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-10 20:46:52 +0000

    wakeonlan: update 0.42 bottle.

--- Formula/w/wakeonlan.rb
+++ Formula/w/wakeonlan.rb
@@ -6,13 +6,8 @@ class Wakeonlan < Formula
   license "Artistic-1.0-Perl"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65c38012ee2876a996b157899c39255db1b15ff3775f1ce6e8af4726533cd2d1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "65c38012ee2876a996b157899c39255db1b15ff3775f1ce6e8af4726533cd2d1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "65a0401b3ead62ae9434db4dce4244928aae7970d030f1a18fe713345d696b5a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "65c38012ee2876a996b157899c39255db1b15ff3775f1ce6e8af4726533cd2d1"
-    sha256 cellar: :any_skip_relocation, ventura:       "65a0401b3ead62ae9434db4dce4244928aae7970d030f1a18fe713345d696b5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e36175eff9d610ef9dea871b747a6e1b0414d3578b9ebc7ee9cecf0f9326d46"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, all: "0859c811ae72fce06de1a607d36b0955517c80f5ea73431ee6c1dd38c749a0c6"
   end
 
   # Build with Homebrew `perl` to build an `:all` bottle.
