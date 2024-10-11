dd5f345a61a7b9218c37296e2bc52e76156efa04
Branch Vincent <branchevincent@gmail.com>
2024-10-11 09:40:00 -0700

    netaddr: migrate to `python@3.13`

--- Formula/n/netaddr.rb
+++ Formula/n/netaddr.rb
@@ -13,7 +13,7 @@ class Netaddr < Formula
     sha256 cellar: :any_skip_relocation, all: "a7284179680caf2362b7b59aece0fd13ce71d9ee590609b68c8706b0baf8671c"
   end
 
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   def install
     virtualenv_install_with_resources
