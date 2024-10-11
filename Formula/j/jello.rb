89faa9de1c984bc51e31b70c320ba2fd1e5db527
Branch Vincent <branchevincent@gmail.com>
2024-10-11 11:29:18 -0700

    jello: migrate to `python@3.13`

--- Formula/j/jello.rb
+++ Formula/j/jello.rb
@@ -19,11 +19,11 @@ class Jello < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec5972dc34b0f729637a56f358c9a3e1f56906a54264c64cc625fdae5352935e"
   end
 
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "pygments" do
-    url "https://files.pythonhosted.org/packages/55/59/8bccf4157baf25e4aa5a0bb7fa3ba8600907de105ebc22b0c78cfbf6f565/pygments-2.17.2.tar.gz"
-    sha256 "da46cec9fd2de5be3a8a784f434e4c4ab670b4ff54d605c4c2717e9d49c4c367"
+    url "https://files.pythonhosted.org/packages/8e/62/8336eff65bcbc8e4cb5d05b55faf041285951b6e80f33e2bff2024788f31/pygments-2.18.0.tar.gz"
+    sha256 "786ff802f32e91311bff3889f6e9a86e81505fe99f2735bb6d60ae0c5004f199"
   end
 
   def install
