953958dc9c32eb01dfaae8c35de0c073075b4949
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-11 22:44:23 +0000

    livekit-cli: update 2.2.1 bottle.

--- Formula/l/livekit-cli.rb
+++ Formula/l/livekit-cli.rb
@@ -7,12 +7,12 @@ class LivekitCli < Formula
   head "https://github.com/livekit/livekit-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "be624403e0097a5812adde65ca86ba0d2603be14bee3bb549b4f4392d08f1e95"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "be624403e0097a5812adde65ca86ba0d2603be14bee3bb549b4f4392d08f1e95"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "be624403e0097a5812adde65ca86ba0d2603be14bee3bb549b4f4392d08f1e95"
-    sha256 cellar: :any_skip_relocation, sonoma:        "54f1a75ddb37a1c773b42ef2581e95c46b007d1216d24dbb700c45afa9d669ef"
-    sha256 cellar: :any_skip_relocation, ventura:       "54f1a75ddb37a1c773b42ef2581e95c46b007d1216d24dbb700c45afa9d669ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7195dc8e1401cfef3b767ad32ee3713baa2e4069b29c47d0f69ad36a88e32212"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ef43d828cfc44303ce80d6075fa13a8170a13d31b9efbd8eb81e5553222eb53"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4ef43d828cfc44303ce80d6075fa13a8170a13d31b9efbd8eb81e5553222eb53"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4ef43d828cfc44303ce80d6075fa13a8170a13d31b9efbd8eb81e5553222eb53"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dcceac9d9ccec2f0b82895bf25ac4f96be2b311209594694d2d16a2e806afecd"
+    sha256 cellar: :any_skip_relocation, ventura:       "dcceac9d9ccec2f0b82895bf25ac4f96be2b311209594694d2d16a2e806afecd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c4d030d2a870a1b6d77674b23bec5b2d94f92813c9dbce6d8fd7f824d01e9da2"
   end
 
   depends_on "go" => :build
