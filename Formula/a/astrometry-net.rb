4c16c92bd6c16f07efc0bd868f2642f501524311
Rui Chen <rui@chenrui.dev>
2024-10-12 13:13:45 -0400

    astrometry-net: migrate to `python@3.13`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/astrometry-net.rb
+++ Formula/a/astrometry-net.rb
@@ -31,7 +31,7 @@ class AstrometryNet < Formula
   depends_on "libpng"
   depends_on "netpbm"
   depends_on "numpy"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
   depends_on "wcslib"
 
   resource "fitsio" do
@@ -48,7 +48,7 @@ class AstrometryNet < Formula
     ENV["NETPBM_INC"] = "-I#{Formula["netpbm"].opt_include}/netpbm"
     ENV["NETPBM_LIB"] = "-L#{Formula["netpbm"].opt_lib} -lnetpbm"
     ENV["SYSTEM_GSL"] = "yes"
-    ENV["PYTHON"] = python3 = which("python3.12")
+    ENV["PYTHON"] = python3 = which("python3.13")
 
     venv = virtualenv_create(libexec, python3)
     venv.pip_install(resources, build_isolation: false)
