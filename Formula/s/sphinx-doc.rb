6c105e741a1ae67f2c689bdbfe6322fbd439079b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-12 19:05:00 +0000

    sphinx-doc 8.1.2

--- Formula/s/sphinx-doc.rb
+++ Formula/s/sphinx-doc.rb
@@ -3,8 +3,8 @@ class SphinxDoc < Formula
 
   desc "Tool to create intelligent and beautiful documentation"
   homepage "https://www.sphinx-doc.org/"
-  url "https://files.pythonhosted.org/packages/ca/ff/60f166ff66cce657ef6275f013c9555a9a814a80e0a8e86707ac5fdbf247/sphinx-8.1.1.tar.gz"
-  sha256 "65e0ee8f76c9cbfd53ec8466ac8c87a73f9ed911767a8ef36c3bf3c522242bcd"
+  url "https://files.pythonhosted.org/packages/4c/a4/fca423c01a257a56cbd99b8cf2a0e11e065c2e8988b7da603f79b60ca454/sphinx-8.1.2.tar.gz"
+  sha256 "b19e24b51d1926567e0bb8f4fbd372e40cb19f1f62bcba91b45c9ee0cdd8874e"
   license "BSD-2-Clause"
 
   bottle do
