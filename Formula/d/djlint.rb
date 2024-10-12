9265c8d942d3daa5d01414c17de942cdc70c6385
Rui Chen <rui@chenrui.dev>
2024-10-12 15:16:01 -0400

    djlint: migrate to `python@3.13`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/djlint.rb
+++ Formula/d/djlint.rb
@@ -18,7 +18,7 @@ class Djlint < Formula
   end
 
   depends_on "libyaml"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
