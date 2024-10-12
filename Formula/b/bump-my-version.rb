51eb32cacc21aa48efc6279fdb955c79648cb550
Rui Chen <rui@chenrui.dev>
2024-10-12 14:34:29 -0400

    bump-my-version: migrate to `python@3.13`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/bump-my-version.rb
+++ Formula/b/bump-my-version.rb
@@ -18,7 +18,7 @@ class BumpMyVersion < Formula
   end
 
   depends_on "rust" => :build # for pydantic_core
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "annotated-types" do
     url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
