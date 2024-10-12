4b871334b293ab0dc0e00028779e7a3faf419666
Rui Chen <rui@chenrui.dev>
2024-10-12 17:04:00 +0000

    fanyi: update 9.0.7 bottle.

--- Formula/f/fanyi.rb
+++ Formula/f/fanyi.rb
@@ -6,12 +6,12 @@ class Fanyi < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "dcb306e52d34c98e9183e2bc2260bc5e210a1473afe4da9f4dd54f63858bbecc"
-    sha256 cellar: :any_skip_relocation, ventura:       "dcb306e52d34c98e9183e2bc2260bc5e210a1473afe4da9f4dd54f63858bbecc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "647b163411b47fcecd6c1342116888679b0a75a78ed2af252bc11efa507f39c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c57d1e565dc5dec910e10f45f362c19e56ae87b904c34b43e9963990ba384ce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2c57d1e565dc5dec910e10f45f362c19e56ae87b904c34b43e9963990ba384ce"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2c57d1e565dc5dec910e10f45f362c19e56ae87b904c34b43e9963990ba384ce"
+    sha256 cellar: :any_skip_relocation, sonoma:        "35da6341e33ac6ebf7de22e8b43b57e7bc74890e77660f499f927e6cfa13ea84"
+    sha256 cellar: :any_skip_relocation, ventura:       "35da6341e33ac6ebf7de22e8b43b57e7bc74890e77660f499f927e6cfa13ea84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c57d1e565dc5dec910e10f45f362c19e56ae87b904c34b43e9963990ba384ce"
   end
 
   depends_on "node"
