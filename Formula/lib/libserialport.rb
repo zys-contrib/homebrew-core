cb922a16cf0a3fa19e273509c02e9ec6437c7eb5
Rui Chen <rui@chenrui.dev>
2024-10-13 23:25:28 -0400

    libserialport 0.1.2
    autobump: add libserialport
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libserialport.rb
+++ Formula/lib/libserialport.rb
@@ -1,8 +1,8 @@
 class Libserialport < Formula
   desc "Cross-platform serial port C library"
   homepage "https://sigrok.org/wiki/Libserialport"
-  url "https://sigrok.org/download/source/libserialport/libserialport-0.1.1.tar.gz"
-  sha256 "4a2af9d9c3ff488e92fb75b4ba38b35bcf9b8a66df04773eba2a7bbf1fa7529d"
+  url "https://sigrok.org/download/source/libserialport/libserialport-0.1.2.tar.gz"
+  sha256 "5deb92b5ca72c0347b07b786848350deca2dcfd975ce613b8e0e1d947a4b4ca9"
   license "LGPL-3.0-or-later"
 
   livecheck do
