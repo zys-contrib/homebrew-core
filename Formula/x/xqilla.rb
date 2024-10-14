7d71b2f064d70678b2195e0f36603814dad8b7d6
Rui Chen <rui@chenrui.dev>
2024-10-14 19:56:43 -0400

    xqilla: revision bump (xerces-c 3.3.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/x/xqilla.rb
+++ Formula/x/xqilla.rb
@@ -4,6 +4,7 @@ class Xqilla < Formula
   url "https://downloads.sourceforge.net/project/xqilla/XQilla-2.3.4.tar.gz"
   sha256 "292631791631fe2e7eb9727377335063a48f12611d641d0296697e0c075902eb"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
