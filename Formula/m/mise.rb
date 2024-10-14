a068d05143991809c15fe6a4bb3928253338bb93
mise-en-dev <123107610+mise-en-dev@users.noreply.github.com>
2024-10-14 08:22:06 +0000

    mise 2024.10.5

--- Formula/m/mise.rb
+++ Formula/m/mise.rb
@@ -1,8 +1,8 @@
 class Mise < Formula
   desc "Polyglot runtime manager (asdf rust clone)"
   homepage "https://mise.jdx.dev/"
-  url "https://github.com/jdx/mise/archive/refs/tags/v2024.10.4.tar.gz"
-  sha256 "ee5f7a4e5fcfa369f4fdf00f3756d57905c37b03ac7762b46b6ce760decf8f42"
+  url "https://github.com/jdx/mise/archive/refs/tags/v2024.10.5.tar.gz"
+  sha256 "6a2edff4dd0cc91c2d81741b29b6fa2eebfeae3b6d76eec677d55f439104631d"
   license "MIT"
   head "https://github.com/jdx/mise.git", branch: "main"
 
