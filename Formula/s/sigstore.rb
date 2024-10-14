08a7f8c2955530a1f3608f8713d52ba5b2e8e518
Rui Chen <rui@chenrui.dev>
2024-10-13 20:27:57 -0400

    sigstore: migrate to `python@3.13`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sigstore.rb
+++ Formula/s/sigstore.rb
@@ -19,7 +19,7 @@ class Sigstore < Formula
   depends_on "rust" => :build
   depends_on "certifi"
   depends_on "cryptography"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   resource "annotated-types" do
     url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
