ab257615a820a8b3f681828588c4e9dac0470b53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-14 16:10:53 +0000

    testkube 2.1.39

--- Formula/t/testkube.rb
+++ Formula/t/testkube.rb
@@ -1,8 +1,8 @@
 class Testkube < Formula
   desc "Kubernetes-native framework for test definition and execution"
   homepage "https://testkube.io"
-  url "https://github.com/kubeshop/testkube/archive/refs/tags/v2.1.38.tar.gz"
-  sha256 "02808c0352337e0757ba161d73b1f0c44e2d7ac9622ce0cc60bfaf5432bbbb49"
+  url "https://github.com/kubeshop/testkube/archive/refs/tags/v2.1.39.tar.gz"
+  sha256 "d1dd72de54b272282fe2e9c0deeccd055148085e1724767be351c5baf05fc477"
   license "MIT"
   head "https://github.com/kubeshop/testkube.git", branch: "main"
 
