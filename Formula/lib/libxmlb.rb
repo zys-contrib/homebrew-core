487211bcae0161fc73c1af1afa8f676d1cf583ce
Rui Chen <rui@chenrui.dev>
2024-10-15 16:04:51 -0400

    libxmlb: migrate to `python@3.13`
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libxmlb.rb
+++ Formula/lib/libxmlb.rb
@@ -22,7 +22,7 @@ class Libxmlb < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.12" => :build
+  depends_on "python@3.13" => :build
   depends_on "vala" => :build
   depends_on "glib"
   depends_on "xz"
