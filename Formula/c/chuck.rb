bfa01e584640d333f14cd1fc63b38a3a9137b75c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-15 06:30:18 +0000

    chuck 1.5.3.2

--- Formula/c/chuck.rb
+++ Formula/c/chuck.rb
@@ -1,9 +1,9 @@
 class Chuck < Formula
   desc "Concurrent, on-the-fly audio programming language"
   homepage "https://chuck.cs.princeton.edu/"
-  url "https://chuck.cs.princeton.edu/release/files/chuck-1.5.3.1.tgz"
-  mirror "https://chuck.stanford.edu/release/files/chuck-1.5.3.1.tgz"
-  sha256 "551130d682c1ed29db4a589fd7302697403a9aa2534c6da982cf8a3dd0265407"
+  url "https://chuck.cs.princeton.edu/release/files/chuck-1.5.3.2.tgz"
+  mirror "https://chuck.stanford.edu/release/files/chuck-1.5.3.2.tgz"
+  sha256 "7926211292af59f81ce823db9deda981f98c0cf784ab62d3b66c57dfc92f9561"
   license "GPL-2.0-or-later"
   head "https://github.com/ccrma/chuck.git", branch: "main"
 
