a8fd7205b914f75417bc572ffb6c76cb8be19093
Rui Chen <rui@chenrui.dev>
2024-10-15 04:00:46 +0000

    minijinja-cli: add 2.3.1 bottle.

--- Formula/m/minijinja-cli.rb
+++ Formula/m/minijinja-cli.rb
@@ -6,6 +6,15 @@ class MinijinjaCli < Formula
   license "Apache-2.0"
   head "https://github.com/mitsuhiko/minijinja.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1421a45038fdb9afd07caa3246725eae31a457dccfc539aa45ca269305c7f006"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f39c4a55ea47f3f81a7668bbc0f05da02366b9ffca972b80c3b6597f75e2b80b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "bb8d85e5a0583951dc7458723aae61199b6a9ff44d8add9c8b225e094109d8e0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4a25f09846e9049b7434b21311ac2429155f34780f0957575ebb9767aac745e1"
+    sha256 cellar: :any_skip_relocation, ventura:       "9a98186630876749742d45ea66651b271a5c2f23d507c73f10c288ffe3195b42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1dd5a7fda724a3138d24196f077f009884796d2d8f5cf080e94b3df87bce9021"
+  end
+
   depends_on "rust" => :build
 
   def install
