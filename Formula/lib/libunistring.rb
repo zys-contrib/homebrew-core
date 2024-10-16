2d8d98e7eb3aa8d3850125ecfb40f201abaa1207
Rui Chen <rui@chenrui.dev>
2024-10-16 19:22:40 -0400

    libunistring 1.3
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libunistring.rb
+++ Formula/lib/libunistring.rb
@@ -1,10 +1,10 @@
 class Libunistring < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-1.2.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz"
-  sha256 "fd6d5662fa706487c48349a758b57bc149ce94ec6c30624ec9fdc473ceabbc8e"
+  url "https://ftp.gnu.org/gnu/libunistring/libunistring-1.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-1.3.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libunistring/libunistring-1.3.tar.gz"
+  sha256 "8ea8ccf86c09dd801c8cac19878e804e54f707cf69884371130d20bde68386b7"
   license any_of: ["GPL-2.0-only", "LGPL-3.0-or-later"]
 
   bottle do
@@ -19,11 +19,14 @@ class Libunistring < Formula
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    # macOS iconv implementation is slightly broken since Sonoma.
+    # This is also why we skip `make check`.
+    # https://github.com/coreutils/gnulib/commit/bab130878fe57086921fa7024d328341758ed453
+    # https://savannah.gnu.org/bugs/?65686
+    ENV["am_cv_func_iconv_works"] = "yes" if OS.mac? && MacOS.version == :sequoia
+    system "./configure", "--disable-silent-rules", *std_configure_args
     system "make"
-    system "make", "check" if !OS.mac? || MacOS.version < :sonoma
+    system "make", "check" if !OS.mac? || MacOS.version < :sonoma || MacOS.version > :sequoia
     system "make", "install"
   end
 
