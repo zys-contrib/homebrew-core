1bd552f4702b5e589a490f40f6ee626567eba9d4
Rui Chen <rui@chenrui.dev>
2024-10-16 19:10:46 -0400

    autoconf-archive 2024.10.16
    autobump: add autoconf-archive
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    autoconf-archive: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/autoconf-archive.rb
+++ Formula/a/autoconf-archive.rb
@@ -1,9 +1,9 @@
 class AutoconfArchive < Formula
   desc "Collection of over 500 reusable autoconf macros"
   homepage "https://savannah.gnu.org/projects/autoconf-archive/"
-  url "https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2023.02.20.tar.xz"
-  mirror "https://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2023.02.20.tar.xz"
-  sha256 "71d4048479ae28f1f5794619c3d72df9c01df49b1c628ef85fde37596dc31a33"
+  url "https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2024.10.16.tar.xz"
+  mirror "https://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2024.10.16.tar.xz"
+  sha256 "7bcd5d001916f3a50ed7436f4f700e3d2b1bade3ed803219c592d62502a57363"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -25,26 +25,18 @@ class AutoconfArchive < Formula
   conflicts_with "gnome-common", because: "both install ax_check_enable_debug.m4 and ax_code_coverage.m4"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
   test do
-    (testpath/"test.m4").write <<~EOS
-      AC_INIT(myconfig, version-0.1)
-      AC_MSG_NOTICE([Hello, world.])
-
-      m4_include([#{share}/aclocal/ax_have_select.m4])
-
-      # from https://www.gnu.org/software/autoconf-archive/ax_have_select.html
-      AX_HAVE_SELECT(
-        [AX_CONFIG_FEATURE_ENABLE(select)],
-        [AX_CONFIG_FEATURE_DISABLE(select)])
-      AX_CONFIG_FEATURE(
-        [select], [This platform supports select(7)],
-        [HAVE_SELECT], [This platform supports select(7).])
+    (testpath/"configure.ac").write <<~EOS
+      AC_INIT([test], [0.1])
+      AX_CHECK_ENABLE_DEBUG
+      AC_OUTPUT
     EOS
 
-    system "#{Formula["autoconf"].bin}/autoconf", "test.m4"
+    system Formula["autoconf"].bin/"autoconf", "configure.ac"
+    assert_path_exists testpath/"autom4te.cache"
   end
 end
