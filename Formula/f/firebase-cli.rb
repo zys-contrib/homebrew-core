e1e381bfceafffa40a2d7e037472047f10c27684
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-16 23:23:49 +0000

    firebase-cli: update 13.22.1 bottle.

--- Formula/f/firebase-cli.rb
+++ Formula/f/firebase-cli.rb
@@ -7,12 +7,12 @@ class FirebaseCli < Formula
   head "https://github.com/firebase/firebase-tools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e68291a93816867eb2125b3aa29edcb24fe04530a3d71bf5ac1deaf650a8a905"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e68291a93816867eb2125b3aa29edcb24fe04530a3d71bf5ac1deaf650a8a905"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e68291a93816867eb2125b3aa29edcb24fe04530a3d71bf5ac1deaf650a8a905"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c398f1c901d6d2a0ef342f6105ba1203814a449bb0d5d8502ca32053dd07b58d"
-    sha256 cellar: :any_skip_relocation, ventura:       "c398f1c901d6d2a0ef342f6105ba1203814a449bb0d5d8502ca32053dd07b58d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a38c761ce517a732eb38d618223a5521ee79f8f29b3a83fc03c829960c4c385"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "27871d2f7bc6ff5d7b2ae0ee76ca99d81c10f8170fab92e939e0e4aebd37a96d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "27871d2f7bc6ff5d7b2ae0ee76ca99d81c10f8170fab92e939e0e4aebd37a96d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "27871d2f7bc6ff5d7b2ae0ee76ca99d81c10f8170fab92e939e0e4aebd37a96d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "427cb68226c763e2f8de38dd1adf29f340d4b4fff1452b8376a4b61d79b47026"
+    sha256 cellar: :any_skip_relocation, ventura:       "427cb68226c763e2f8de38dd1adf29f340d4b4fff1452b8376a4b61d79b47026"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf320d687a4414d2cd85f2fb33f9717758403706f75cd3f55d0e07a234b05cc8"
   end
 
   depends_on "node"
