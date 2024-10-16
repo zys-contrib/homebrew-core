5056c92ef0ea6424d5edbc787ec982a529a83bb7
Rui Chen <rui@chenrui.dev>
2024-10-15 22:16:46 -0400

    spirv-headers 1.3.296.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/spirv-headers.rb
+++ Formula/s/spirv-headers.rb
@@ -1,8 +1,8 @@
 class SpirvHeaders < Formula
   desc "Headers for SPIR-V"
   homepage "https://github.com/KhronosGroup/SPIRV-Headers"
-  url "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/vulkan-sdk-1.3.290.0.tar.gz"
-  sha256 "1b9ff8a33e07814671dee61fe246c67ccbcfc9be6581f229e251784499700e24"
+  url "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/vulkan-sdk-1.3.296.0.tar.gz"
+  sha256 "1423d58a1171611d5aba2bf6f8c69c72ef9c38a0aca12c3493e4fda64c9b2dc6"
   license "MIT"
   head "https://github.com/KhronosGroup/SPIRV-Headers.git", branch: "main"
 
