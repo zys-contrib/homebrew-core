66fd571085899139679eead2f98941bff265ddc9
Andrés Catalán <hola@andres.cat>
2024-10-16 12:34:41 -0400

    sf 2.61.8 (new formula)

---
+++ Formula/s/sf.rb
@@ -0,0 +1,23 @@
+class Sf < Formula
+  desc "Command-line toolkit for Salesforce development"
+  homepage "https://developer.salesforce.com/tools/salesforcecli"
+  url "https://registry.npmjs.org/@salesforce/cli/-/cli-2.61.8.tgz"
+  sha256 "6d7388565f83c15edd2ad05d1f42737de64680b8ea18a7f569435e93e00d6620"
+  license "BSD-3-Clause"
+
+  depends_on "node@20"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"sf", "project", "generate", "-n", "projectname", "-t", "empty"
+    assert_predicate testpath/"projectname", :exist?
+    assert_predicate testpath/"projectname/config/project-scratch-def.json", :exist?
+    assert_predicate testpath/"projectname/README.md", :exist?
+    assert_predicate testpath/"projectname/sfdx-project.json", :exist?
+    assert_predicate testpath/"projectname/.forceignore", :exist?
+  end
+end
