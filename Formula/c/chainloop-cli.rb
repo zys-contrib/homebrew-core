2a60315b5ad2a627390fa338aead3dd361ca579e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-16 21:27:15 +0000

    chainloop-cli 0.96.22

--- Formula/c/chainloop-cli.rb
+++ Formula/c/chainloop-cli.rb
@@ -1,8 +1,8 @@
 class ChainloopCli < Formula
   desc "CLI for interacting with Chainloop"
   homepage "https://docs.chainloop.dev"
-  url "https://github.com/chainloop-dev/chainloop/archive/refs/tags/v0.96.21.tar.gz"
-  sha256 "8915acf082c74fc9c6db80b1e92468a8eb20dbbfc86c6945d25f01b1be33d886"
+  url "https://github.com/chainloop-dev/chainloop/archive/refs/tags/v0.96.22.tar.gz"
+  sha256 "d7f7d10af9b0f3b7226006b89add11e48bd2bb9d6aa579f784b213924c791d00"
   license "Apache-2.0"
   head "https://github.com/chainloop-dev/chainloop.git", branch: "main"
 
