b60f889e715dd439cc2018f002908ec00ea48fc9
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-10-18 04:31:19 +0800

    fennel: use `make install`
    This makes the install method a bit simpler.
    
    Also, support usage with luajit instead of lua.

--- Formula/f/fennel.rb
+++ Formula/f/fennel.rb
@@ -9,17 +9,18 @@ class Fennel < Formula
     sha256 cellar: :any_skip_relocation, all: "c76f783953158324a8398d7a2cb95bebfdfdeb3991f134fd013ef47febc27a26"
   end
 
+  depends_on "luajit" => [:build, :test]
   depends_on "lua"
 
   def install
-    system "make"
-    bin.install "fennel"
-
-    lua = Formula["lua"]
-    (share/"lua"/lua.version.major_minor).install "fennel.lua"
+    system "make", "PREFIX=#{prefix}", "install"
+    system "make", "LUA=luajit", "PREFIX=#{prefix}", "install"
+    share.install "man" if OS.mac? # macOS `make` doesn't install the manpages correctly.
   end
 
   test do
     assert_match "hello, world!", shell_output("#{bin}/fennel -e '(print \"hello, world!\")'")
+    system "lua", "-e", "require 'fennel'"
+    system "luajit", "-e", "require 'fennel'"
   end
 end
