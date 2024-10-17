a79e9f4ae3651215d898c65ce20f2a7df897b1e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-17 09:28:23 +0000

    cloudfox 1.14.3

--- Formula/c/cloudfox.rb
+++ Formula/c/cloudfox.rb
@@ -1,8 +1,8 @@
 class Cloudfox < Formula
   desc "Automating situational awareness for cloud penetration tests"
   homepage "https://github.com/BishopFox/cloudfox"
-  url "https://github.com/BishopFox/cloudfox/archive/refs/tags/v1.14.2.tar.gz"
-  sha256 "fd0873c5fddd9a8d80a786c002721d0dd320c745edaf742b094d720299e32cda"
+  url "https://github.com/BishopFox/cloudfox/archive/refs/tags/v1.14.3.tar.gz"
+  sha256 "eebb1b802d08dda7d75e11c4bd49fa219fa9309b0adf7faf61be2a07bcd42123"
   license "MIT"
   head "https://github.com/BishopFox/cloudfox.git", branch: "main"
 
