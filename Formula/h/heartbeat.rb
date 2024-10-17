ce1d003f0b097db96f49d1acd3b0b5dd7fe2592d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-17 15:37:57 +0000

    heartbeat 8.15.3
    heartbeat: needs py3.12
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/heartbeat.rb
+++ Formula/h/heartbeat.rb
@@ -2,8 +2,8 @@ class Heartbeat < Formula
   desc "Lightweight Shipper for Uptime Monitoring"
   homepage "https://www.elastic.co/beats/heartbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.15.2",
-      revision: "26daf71e4ec87172523af7f0e916cba9f79dc0d0"
+      tag:      "v8.15.3",
+      revision: "bbed3ae55602e83f57c62de85b57a3593aa49efa"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "master"
 
@@ -18,8 +18,8 @@ class Heartbeat < Formula
 
   depends_on "go" => :build
   depends_on "mage" => :build
+  depends_on "python@3.12" => :build
 
-  uses_from_macos "python" => :build
   uses_from_macos "netcat" => :test
 
   def install
