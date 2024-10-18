37fcb6ad68ddb4b9e1466e3d58684455b52657e9
Rui Chen <rui@chenrui.dev>
2024-10-18 21:51:15 +0000

    sf: add 2.61.8 bottle.

--- Formula/s/sf.rb
+++ Formula/s/sf.rb
@@ -5,6 +5,15 @@ class Sf < Formula
   sha256 "6d7388565f83c15edd2ad05d1f42737de64680b8ea18a7f569435e93e00d6620"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "14ff2a892709ac82d7a4014554625c5932709a99b4f648895b4d5e6191d38afa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14ff2a892709ac82d7a4014554625c5932709a99b4f648895b4d5e6191d38afa"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "14ff2a892709ac82d7a4014554625c5932709a99b4f648895b4d5e6191d38afa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "76274fdf218d9258908732ae44fea274f9a7d41328575eb82bb766e14823a77a"
+    sha256 cellar: :any_skip_relocation, ventura:       "76274fdf218d9258908732ae44fea274f9a7d41328575eb82bb766e14823a77a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14ff2a892709ac82d7a4014554625c5932709a99b4f648895b4d5e6191d38afa"
+  end
+
   depends_on "node@20"
 
   def install
