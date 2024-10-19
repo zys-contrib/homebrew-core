9ded9e850f9e68bf229888087e9549601d756cd8
Rui Chen <rui@chenrui.dev>
2024-10-18 20:44:33 -0400

    vulkan-utility-libraries 1.3.299
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vulkan-utility-libraries.rb
+++ Formula/v/vulkan-utility-libraries.rb
@@ -1,8 +1,8 @@
 class VulkanUtilityLibraries < Formula
   desc "Utility Libraries for Vulkan"
   homepage "https://github.com/KhronosGroup/Vulkan-Utility-Libraries"
-  url "https://github.com/KhronosGroup/Vulkan-Utility-Libraries/archive/refs/tags/v1.3.298.tar.gz"
-  sha256 "c0c49289bf8265beb5ded06826be742f3af2726d394595cbc56044149bcf5798"
+  url "https://github.com/KhronosGroup/Vulkan-Utility-Libraries/archive/refs/tags/v1.3.299.tar.gz"
+  sha256 "300c40cf23960672b9de4740fc454eed66c30b7a6314d59c02bdb29116728f83"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-Utility-Libraries.git", branch: "main"
 
