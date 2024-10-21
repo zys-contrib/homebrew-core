1d7c98e55a716391263d666eb3d847997524b41d
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2024-10-21 12:29:51 +0000

    node-sass: add 1.80.3 bottle.

--- Formula/n/node-sass.rb
+++ Formula/n/node-sass.rb
@@ -5,6 +5,15 @@ class NodeSass < Formula
   sha256 "34603949cfae96a8cc846c4cd910871d430e3dee08ceebfb0c36888637d81c89"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "438078471dc191fedd23f444d07b539a033a0e7b11a5700f248407b256a9f0fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "438078471dc191fedd23f444d07b539a033a0e7b11a5700f248407b256a9f0fa"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "438078471dc191fedd23f444d07b539a033a0e7b11a5700f248407b256a9f0fa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3ce2c3d62fe181f90286b0047c482431f1014e6efc3e0c16e497023c4b7964a4"
+    sha256 cellar: :any_skip_relocation, ventura:       "3ce2c3d62fe181f90286b0047c482431f1014e6efc3e0c16e497023c4b7964a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "90b14243cbdf8246c5d4a977eb39835bc589a821a2cb538cb2e73d949b74dc2e"
+  end
+
   depends_on "node"
 
   def install
