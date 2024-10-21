6bdf6c2cf6ef42a0592c29e530f6d429a6bd837c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-21 09:31:22 +0000

    faircamp 0.18.0

--- Formula/f/faircamp.rb
+++ Formula/f/faircamp.rb
@@ -1,8 +1,8 @@
 class Faircamp < Formula
   desc "Static site generator for audio producers"
   homepage "https://codeberg.org/simonrepp/faircamp"
-  url "https://codeberg.org/simonrepp/faircamp/archive/0.17.0.tar.gz"
-  sha256 "026d4a0dcb16f0db145a100b97bfc52f1544ce632c42a198517ab3fbdb89d029"
+  url "https://codeberg.org/simonrepp/faircamp/archive/0.18.0.tar.gz"
+  sha256 "211cd18deb15901b44c4cb11a506394a21ad3288dbf8b007158d55f619e9f5f1"
   license "AGPL-3.0-or-later"
 
   bottle do
