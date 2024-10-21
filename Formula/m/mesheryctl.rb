578cc29116e914fcd36c62c633c192d92f609c5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-21 20:11:25 +0000

    mesheryctl: update 0.7.125 bottle.

--- Formula/m/mesheryctl.rb
+++ Formula/m/mesheryctl.rb
@@ -8,12 +8,12 @@ class Mesheryctl < Formula
   head "https://github.com/meshery/meshery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f0c4533f6f98742823339e480d39f28cc29b210a4052431796b16983787b9893"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f0c4533f6f98742823339e480d39f28cc29b210a4052431796b16983787b9893"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f0c4533f6f98742823339e480d39f28cc29b210a4052431796b16983787b9893"
-    sha256 cellar: :any_skip_relocation, sonoma:        "53c8ec9e4e8131bf87429f41966fe7513f1af89a301cc956c3d4695a5fff473c"
-    sha256 cellar: :any_skip_relocation, ventura:       "53c8ec9e4e8131bf87429f41966fe7513f1af89a301cc956c3d4695a5fff473c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dbf9b4c594b7b814aad5d1647e86bcab641aa20419dfb1b672b250c3f7130ace"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "febb5bc7a3ade6c9e25d544c28139ab809565d7eebb6b213d3ad636714bb60f0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "febb5bc7a3ade6c9e25d544c28139ab809565d7eebb6b213d3ad636714bb60f0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "febb5bc7a3ade6c9e25d544c28139ab809565d7eebb6b213d3ad636714bb60f0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "96a732b1f49c4d3c7d31d13a594343689688227c1dcfaba70935fe704ceb0b7d"
+    sha256 cellar: :any_skip_relocation, ventura:       "96a732b1f49c4d3c7d31d13a594343689688227c1dcfaba70935fe704ceb0b7d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99fecbb8085562f280463cf1e3595b790be6b77b4e05deb2793dd3d29921ed1d"
   end
 
   depends_on "go" => :build
