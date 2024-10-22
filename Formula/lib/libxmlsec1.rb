d2cfe4b3504b9d6106ed05292bbc7a7f2ae604c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-22 15:45:04 +0000

    libxmlsec1 1.3.6

--- Formula/lib/libxmlsec1.rb
+++ Formula/lib/libxmlsec1.rb
@@ -1,8 +1,8 @@
 class Libxmlsec1 < Formula
   desc "XML security library"
   homepage "https://www.aleksey.com/xmlsec/"
-  url "https://www.aleksey.com/xmlsec/download/xmlsec1-1.3.5.tar.gz"
-  sha256 "2ffd4ad1f860ec93e47a680310ab2bc94968bd07566e71976bd96133d9504917"
+  url "https://www.aleksey.com/xmlsec/download/xmlsec1-1.3.6.tar.gz"
+  sha256 "952b626ad3f3be1a4598622dab52fdab2a8604d0837c1b00589f3637535af92f"
   license "MIT"
 
   livecheck do
