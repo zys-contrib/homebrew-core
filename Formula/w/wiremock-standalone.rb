e35a5413f4dfcd4a88984feba2192d1034717810
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-22 16:02:46 +0000

    wiremock-standalone 3.9.2

--- Formula/w/wiremock-standalone.rb
+++ Formula/w/wiremock-standalone.rb
@@ -1,8 +1,8 @@
 class WiremockStandalone < Formula
   desc "Simulator for HTTP-based APIs"
   homepage "https://wiremock.org/docs/running-standalone/"
-  url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/3.9.1/wiremock-standalone-3.9.1.jar"
-  sha256 "723a880d50d3b0a145af0df07e578c2cb85e77feb2231e6991c9a1366926912c"
+  url "https://search.maven.org/remotecontent?filepath=org/wiremock/wiremock-standalone/3.9.2/wiremock-standalone-3.9.2.jar"
+  sha256 "77b88dc41c3268e3ce391f3fef222b0cd0696f286d3fc35a84ba22ffcb877012"
   license "Apache-2.0"
 
   livecheck do
