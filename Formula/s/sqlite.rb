045fdde0b5514c6234dec6148aff3534773b1c10
Rui Chen <rui@chenrui.dev>
2024-10-21 23:09:48 -0400

    sqlite 3.47.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sqlite.rb
+++ Formula/s/sqlite.rb
@@ -1,9 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/index.html"
-  url "https://www.sqlite.org/2024/sqlite-autoconf-3460100.tar.gz"
-  version "3.46.1"
-  sha256 "67d3fe6d268e6eaddcae3727fce58fcc8e9c53869bdd07a0c61e38ddf2965071"
+  url "https://www.sqlite.org/2024/sqlite-autoconf-3470000.tar.gz"
+  version "3.47.0"
+  sha256 "83eb21a6f6a649f506df8bd3aab85a08f7556ceed5dbd8dea743ea003fc3a957"
   license "blessing"
 
   livecheck do
