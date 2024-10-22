a32473fdc2d17e732c77dab82fd1f01cea5fda25
Rui Chen <rui@chenrui.dev>
2024-10-21 23:09:47 -0400

    dbhash 3.47.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/dbhash.rb
+++ Formula/d/dbhash.rb
@@ -1,9 +1,9 @@
 class Dbhash < Formula
   desc "Computes the SHA1 hash of schema and content of a SQLite database"
   homepage "https://www.sqlite.org/dbhash.html"
-  url "https://www.sqlite.org/2024/sqlite-src-3460100.zip"
-  version "3.46.1"
-  sha256 "def3fc292eb9ecc444f6c1950e5c79d8462ed5e7b3d605fd6152d145e1d5abb4"
+  url "https://www.sqlite.org/2024/sqlite-src-3470000.zip"
+  version "3.47.0"
+  sha256 "f59c349bedb470203586a6b6d10adb35f2afefa49f91e55a672a36a09a8fedf7"
   license "blessing"
 
   livecheck do
