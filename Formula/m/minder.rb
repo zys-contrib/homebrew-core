89973975a812f667dd1a508272847a14f7147c4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-22 09:51:00 +0000

    minder 0.0.69

--- Formula/m/minder.rb
+++ Formula/m/minder.rb
@@ -1,8 +1,8 @@
 class Minder < Formula
   desc "CLI for interacting with Stacklok's Minder platform"
   homepage "https://mindersec.github.io/"
-  url "https://github.com/mindersec/minder/archive/refs/tags/v0.0.68.tar.gz"
-  sha256 "75f7bd3a9be5c404a53d6df33cc747a1c4158e1a0183fc8cbe4deeb3281c3835"
+  url "https://github.com/mindersec/minder/archive/refs/tags/v0.0.69.tar.gz"
+  sha256 "fe0dfe45a5346e39a38e0ef0a1dc369cb2cf6226d07c5d601f6d970857c687a9"
   license "Apache-2.0"
   head "https://github.com/mindersec/minder.git", branch: "main"
 
