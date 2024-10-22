f4b824e8fcee3d56dc90a6816246a7dd8bd9307a
Rui Chen <rui@chenrui.dev>
2024-10-21 23:04:11 -0400

    nspr 4.36
    autobump: add nspr
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/nspr.rb
+++ Formula/n/nspr.rb
@@ -1,8 +1,8 @@
 class Nspr < Formula
   desc "Platform-neutral API for system-level and libc-like functions"
   homepage "https://hg.mozilla.org/projects/nspr"
-  url "https://archive.mozilla.org/pub/nspr/releases/v4.35/src/nspr-4.35.tar.gz"
-  sha256 "7ea3297ea5969b5d25a5dd8d47f2443cda88e9ee746301f6e1e1426f8a6abc8f"
+  url "https://archive.mozilla.org/pub/nspr/releases/v4.36/src/nspr-4.36.tar.gz"
+  sha256 "55dec317f1401cd2e5dba844d340b930ab7547f818179a4002bce62e6f1c6895"
   license "MPL-2.0"
 
   livecheck do
