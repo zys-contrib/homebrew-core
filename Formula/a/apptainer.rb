253e77b0009e73775928018ef9148f192f670ff2
Michael Cho <michael@michaelcho.dev>
2024-10-22 21:28:36 -0400

    apptainer 1.3.4 (new formula)

---
+++ Formula/a/apptainer.rb
@@ -0,0 +1,39 @@
+class Apptainer < Formula
+  desc "Application container and unprivileged sandbox platform for Linux"
+  homepage "https://apptainer.org/"
+  url "https://github.com/apptainer/apptainer/releases/download/v1.3.4/apptainer-1.3.4.tar.gz"
+  sha256 "c6ccfdd7c967e5c36dde8711f369c4ac669a16632b79fa0dcaf7e772b7a47397"
+  license "BSD-3-Clause"
+
+  # No relocation, the localstatedir to find configs etc is compiled into the program
+  pour_bottle? only_if: :default_prefix
+
+  depends_on "go" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libseccomp"
+  depends_on :linux
+  depends_on "squashfs"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --without-suid
+      -P release-stripped
+      -v
+    ]
+    ENV.O0
+    system "./mconfig", *args
+    cd "./builddir" do
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match(/There are [0-9]+ container file/, shell_output("#{bin}/apptainer cache list"))
+    # This does not work inside older github runners, but for a simple quick check, run:
+    # singularity exec library://alpine cat /etc/alpine-release
+  end
+end
