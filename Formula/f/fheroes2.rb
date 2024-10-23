56047faa0f6f6c489efc10f7183300b5cc79bb35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-23 03:36:29 +0000

    fheroes2 1.1.3

--- Formula/f/fheroes2.rb
+++ Formula/f/fheroes2.rb
@@ -1,8 +1,8 @@
 class Fheroes2 < Formula
   desc "Recreation of the Heroes of Might and Magic II game engine"
   homepage "https://ihhub.github.io/fheroes2/"
-  url "https://github.com/ihhub/fheroes2/archive/refs/tags/1.1.2.tar.gz"
-  sha256 "2c1ecc167de9b4a5f0eb6b6c88392ba021b4e031844760611067971e71e58b6d"
+  url "https://github.com/ihhub/fheroes2/archive/refs/tags/1.1.3.tar.gz"
+  sha256 "f91760f7e8a512fa4b2b5eb02d852d358106fca50faa13db942d8314926ca6d8"
   license "GPL-2.0-or-later"
   head "https://github.com/ihhub/fheroes2.git", branch: "master"
 
