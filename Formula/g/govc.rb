4918be541e64cbd8f98084193df366f4162149fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-23 18:36:45 +0000

    govc 0.45.1

--- Formula/g/govc.rb
+++ Formula/g/govc.rb
@@ -1,8 +1,8 @@
 class Govc < Formula
   desc "Command-line tool for VMware vSphere"
   homepage "https://github.com/vmware/govmomi/tree/master/govc"
-  url "https://github.com/vmware/govmomi/archive/refs/tags/v0.45.0.tar.gz"
-  sha256 "02de33215086100002b204688685ab81961bdd0f583404c49c8c759fffd7eef5"
+  url "https://github.com/vmware/govmomi/archive/refs/tags/v0.45.1.tar.gz"
+  sha256 "37077b5534f3ade75bc766a492d2c24901a0fd65cde6208d1a5e7b3cc6c02088"
   license "Apache-2.0"
 
   # Upstream appears to use GitHub releases to indicate that a version is
