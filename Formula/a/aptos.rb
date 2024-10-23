8b98bdebee11f1dd2a86467f168458fdf5953e59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-23 14:44:23 +0000

    aptos: update 4.2.5 bottle.

--- Formula/a/aptos.rb
+++ Formula/a/aptos.rb
@@ -12,12 +12,12 @@ class Aptos < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c2c15211d174cf950dd7c9071064a28174902602610936ed60534a75cb71fa5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fbf3f16a8c84aa051a6388fef1c81c911f8f856e15ce40b7258a06ce6b2e8019"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "06ab595b5dffa2a9d6dacbc6d74968002bd4664927de573aa0073257dd8ca438"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bdec168ab14c7e68ae06a1dd90854a187d8c4e3a58e23c159c1b8508025cb043"
-    sha256 cellar: :any_skip_relocation, ventura:       "1b1efc1c28e50325c2c6bcc43e90b482e7ddc0872b06cd0f8ff4945e6b1d4274"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04b2d3a9a8b047d487ae4618ebeda6f8d3efc72eb949f84238e973f787b6e174"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b29b28ff1ba304255ed6d9fbabe9013324423d0214ca745ea09fde8ed743bcb6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "16f40fb63b2f8bb5d66409ca41f199ddd5901bd2ca41b7deb8e39d7381b557ef"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "935fb5cd9669431a5113b923dad9f8f5d8ba4383985bb6228bc68abf16ece2da"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c70d0effb30fa9e254d15aa60ca89b1b663136f687754d3cc5ac2318c03554dc"
+    sha256 cellar: :any_skip_relocation, ventura:       "6a260e59ba53a75cc9b691c685e0c875bb145cb78cae88611a066108c8d25451"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5a39e179494cd656e2babba01d6f43c7fee9db85e226698aeb1aa22f5fc7fe7e"
   end
 
   depends_on "cmake" => :build
