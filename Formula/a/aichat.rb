dc3b443b0101dc1aed5280c056774fa3ea3d02a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-24 09:23:04 +0000

    aichat 0.23.0

--- Formula/a/aichat.rb
+++ Formula/a/aichat.rb
@@ -1,8 +1,8 @@
 class Aichat < Formula
   desc "All-in-one AI-Powered CLI Chat & Copilot"
   homepage "https://github.com/sigoden/aichat"
-  url "https://github.com/sigoden/aichat/archive/refs/tags/v0.22.0.tar.gz"
-  sha256 "a0f108c7da9c361fd30c1c8ba386cb33b6f9861c90bab663bcd3bdc89c9f29c7"
+  url "https://github.com/sigoden/aichat/archive/refs/tags/v0.23.0.tar.gz"
+  sha256 "c96341fe4c2223da2e069f16561166e5f699085597c17cdb128e86c0947ac7f6"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sigoden/aichat.git", branch: "main"
 
