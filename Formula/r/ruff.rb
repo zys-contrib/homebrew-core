73f9a478d644b8f6176205b454ba9c5b74247224
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-24 16:51:19 +0000

    ruff: update 0.7.1 bottle.

--- Formula/r/ruff.rb
+++ Formula/r/ruff.rb
@@ -7,12 +7,12 @@ class Ruff < Formula
   head "https://github.com/astral-sh/ruff.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2293a7a91801873802a1a41c6c67b9032e4268384e92c8d1dfb2de258bc05728"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "235204564ca38b3ee3bc8b5a366abc91a20dcbeb42a6262529148d3aa312a6e1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0d25e9ecd575e6c4b86a99d3cd32ea3b80e1208bb796a9a290e176ac772f9841"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d27637b87fdafe9238b3f47dc0e2f3b2184f38d66f53fbb3ac7f6f12d2959ff7"
-    sha256 cellar: :any_skip_relocation, ventura:       "8ae5b2301b935302dc48f71d985ab7642d0c47af07e8cc5c6abb599a3ce88dcd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec4f1f71046f8e49489ab23b28975c4a2474c0ccb5abe3fb63563a6e7fc1f355"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9f1546f66cb73351332e4aa3406e4e51f195446797b1876392f2c48e0aac4297"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2f4eb67da6271a61da2aa809c183239ee68d6db78f495cbb33823e7765eb7c1b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fce3e68e1b3c8b20a6f964f726713caa0ed58300a0b28246fb9a94a3f9723f87"
+    sha256 cellar: :any_skip_relocation, sonoma:        "62bff9637cd6671e37e29e2fef0e8b58311b53bc63231675060b07ea50e2b17e"
+    sha256 cellar: :any_skip_relocation, ventura:       "8ccfa9b34118734c23e5226f8b88ca9ac9ea6a37dffa64354bd3e9cfb48bd04a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e4a19cfa7533da7d9cc16d9aaefcb060d3411b2ea0d483b68446bc78b8f550c"
   end
 
   depends_on "rust" => :build
