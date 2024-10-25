d20528f171f37c6b8be3c2cee83ac5b0e1b0835c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-25 18:36:55 +0000

    gallery-dl 1.27.7

--- Formula/g/gallery-dl.rb
+++ Formula/g/gallery-dl.rb
@@ -3,8 +3,8 @@ class GalleryDl < Formula
 
   desc "Command-line downloader for image-hosting site galleries and collections"
   homepage "https://github.com/mikf/gallery-dl"
-  url "https://files.pythonhosted.org/packages/f0/9f/e67b9635bf24b287f2f985a50ae6ca9e278edabe34adb94a8d0918c61e94/gallery_dl-1.27.6.tar.gz"
-  sha256 "6f1875e5a648ba94da1e760ceb930062e62d2b1d33d8e21097758a114d603b84"
+  url "https://files.pythonhosted.org/packages/4a/32/359add1bb070d5cbce70fcf348f1b61312db833b8a4b10187d3aaa35fa2d/gallery_dl-1.27.7.tar.gz"
+  sha256 "f587310fb007e14d9d3a1022527927c30185cbda69945150ed19f870ebc37447"
   license "GPL-2.0-only"
   head "https://github.com/mikf/gallery-dl.git", branch: "master"
 
