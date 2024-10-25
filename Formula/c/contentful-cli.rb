a8be4a561bff9814451eb647ebd09d973aa2b1f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-25 19:48:54 +0000

    contentful-cli: update 3.4.2 bottle.

--- Formula/c/contentful-cli.rb
+++ Formula/c/contentful-cli.rb
@@ -7,12 +7,12 @@ class ContentfulCli < Formula
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d389b1d906a6c1ab9e39ee047b3f40c222d645b560be4cf7c51c0095ede2bc1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d389b1d906a6c1ab9e39ee047b3f40c222d645b560be4cf7c51c0095ede2bc1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3d389b1d906a6c1ab9e39ee047b3f40c222d645b560be4cf7c51c0095ede2bc1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4a08575eec2d5f0e1e6e781d827ed39113f258ee6b14781dd39962725a4582b3"
-    sha256 cellar: :any_skip_relocation, ventura:       "4a08575eec2d5f0e1e6e781d827ed39113f258ee6b14781dd39962725a4582b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d389b1d906a6c1ab9e39ee047b3f40c222d645b560be4cf7c51c0095ede2bc1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9254c1b58b4f75a2b1c0e3c3e3589986581f86becf2f95d7019c3eee118aa4b5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9254c1b58b4f75a2b1c0e3c3e3589986581f86becf2f95d7019c3eee118aa4b5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9254c1b58b4f75a2b1c0e3c3e3589986581f86becf2f95d7019c3eee118aa4b5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7b60c0033673047c54c2e522c46c01d8746ae8c398a87c33b2fde6a596e72b03"
+    sha256 cellar: :any_skip_relocation, ventura:       "7b60c0033673047c54c2e522c46c01d8746ae8c398a87c33b2fde6a596e72b03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9254c1b58b4f75a2b1c0e3c3e3589986581f86becf2f95d7019c3eee118aa4b5"
   end
 
   depends_on "node"
