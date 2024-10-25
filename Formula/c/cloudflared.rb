9bd7dfb3c004abe4f6ff8693168b6e0d5c7eede6
Rui Chen <rui@chenrui.dev>
2024-10-24 20:43:22 -0400

    cloudflared 2024.10.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cloudflared.rb
+++ Formula/c/cloudflared.rb
@@ -1,8 +1,8 @@
 class Cloudflared < Formula
   desc "Cloudflare Tunnel client (formerly Argo Tunnel)"
   homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
-  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2024.10.0.tar.gz"
-  sha256 "4e9a86e1ead708cd48a00d3739483b60cb77879c3e0b6cc6d89ee6ea9be4d7a0"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2024.10.1.tar.gz"
+  sha256 "7e35e3e57a65f8914c5b53896cfa3711153af78b95d971791602c6624d53a1e1"
   license "Apache-2.0"
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
