9dd808207db6aefef0c0a77125850d4c0bd98367
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-25 18:55:23 +0000

    s3fs 1.95

--- Formula/s/s3fs.rb
+++ Formula/s/s3fs.rb
@@ -1,8 +1,8 @@
 class S3fs < Formula
   desc "FUSE-based file system backed by Amazon S3"
   homepage "https://github.com/s3fs-fuse/s3fs-fuse/wiki"
-  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v1.94.tar.gz"
-  sha256 "46eb3bcc16eff63008ae2c3177765264b88627482bdb978fc3d10e34e9d52284"
+  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v1.95.tar.gz"
+  sha256 "0c97b8922f005500d36f72aee29a1345c94191f61d795e2a7b79fb7e3e6f5517"
   license "GPL-2.0-or-later"
   head "https://github.com/s3fs-fuse/s3fs-fuse.git", branch: "master"
 
