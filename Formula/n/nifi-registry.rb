a445359e92c0834ce2f2d0bff86c25c1a8c4ede5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-26 21:53:01 +0000

    nifi-registry 1.28.0

--- Formula/n/nifi-registry.rb
+++ Formula/n/nifi-registry.rb
@@ -1,9 +1,9 @@
 class NifiRegistry < Formula
   desc "Centralized storage & management of NiFi/MiNiFi shared resources"
   homepage "https://nifi.apache.org/projects/registry"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.27.0/nifi-registry-1.27.0-bin.zip"
-  mirror "https://archive.apache.org/dist/nifi/1.27.0/nifi-registry-1.27.0-bin.zip"
-  sha256 "e2f9e0e952462b466a4a4d217560a3d60646ec2f194761a1a708440d0a94d541"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.28.0/nifi-registry-1.28.0-bin.zip"
+  mirror "https://archive.apache.org/dist/nifi/1.28.0/nifi-registry-1.28.0-bin.zip"
+  sha256 "6422273f1a19f15a1e8a3c4ec33f0384c8456acb5ae2726465eb37cca90e5b5b"
   license "Apache-2.0"
 
   bottle do
