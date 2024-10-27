650d7b6883d53d259c84d27234598e7808299711
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-27 21:31:49 +0000

    gnupg-pkcs11-scd 0.11.0
    gnupg-pkcs11-scd: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gnupg-pkcs11-scd.rb
+++ Formula/g/gnupg-pkcs11-scd.rb
@@ -1,10 +1,9 @@
 class GnupgPkcs11Scd < Formula
   desc "Enable the use of PKCS#11 tokens with GnuPG"
   homepage "https://gnupg-pkcs11.sourceforge.net/"
-  url "https://github.com/alonbl/gnupg-pkcs11-scd/releases/download/gnupg-pkcs11-scd-0.10.0/gnupg-pkcs11-scd-0.10.0.tar.bz2"
-  sha256 "29bf29e7780f921c6d3a11f608e2b0483c1bb510c5afa8473090249dd57c5249"
+  url "https://github.com/alonbl/gnupg-pkcs11-scd/releases/download/gnupg-pkcs11-scd-0.11.0/gnupg-pkcs11-scd-0.11.0.tar.bz2"
+  sha256 "954787e562f2b3d9294212c32dd0d81a2cd37aca250e6685002d2893bb959087"
   license "BSD-3-Clause"
-  revision 1
 
   livecheck do
     url :stable
@@ -32,13 +31,6 @@ class GnupgPkcs11Scd < Formula
   depends_on "openssl@3"
   depends_on "pkcs11-helper"
 
-  # Backport pkg-config usage to support newer `libassuan`
-  patch :DATA
-  patch do
-    url "https://github.com/alonbl/gnupg-pkcs11-scd/commit/de08969ae92d31b585d7055eb0734962f55a7282.patch?full_index=1"
-    sha256 "591833296a6e7401732f2ea104004d1dc57567ea2b661a2a1688bcd8e1f7fed8"
-  end
-
   def install
     system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", "--disable-silent-rules", *std_configure_args
@@ -51,34 +43,3 @@ class GnupgPkcs11Scd < Formula
     system bin/"gnupg-pkcs11-scd", "--help"
   end
 end
-
-__END__
-diff --git a/configure.ac b/configure.ac
-index 816ab5b..5ec9323 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -168,21 +168,21 @@ AC_ARG_WITH(
-
- AC_ARG_WITH(
- 	[libgpg-error-prefix],
--	[AC_HELP_STRING([--with-libgpg-error-prefix=DIR], [define libgpgp-error prefix])],
-+	[AS_HELP_STRING([--with-libgpg-error-prefix=DIR], [define libgpgp-error prefix])],
- 	,
- 	[with_libgpg_error_prefix="/usr" ]
- )
-
- AC_ARG_WITH(
- 	[libassuan-prefix],
--	[AC_HELP_STRING([--with-libassuan-prefix=DIR], [define libassuan prefix])],
-+	[AS_HELP_STRING([--with-libassuan-prefix=DIR], [define libassuan prefix])],
- 	,
- 	[with_libassuan_prefix="/usr" ]
- )
-
- AC_ARG_WITH(
- 	[libgcrypt-prefix],
--	[AC_HELP_STRING([--with-libgcrypt-prefix=DIR], [define libgcrypt prefix])],
-+	[AS_HELP_STRING([--with-libgcrypt-prefix=DIR], [define libgcrypt prefix])],
- 	,
- 	[with_libgcrypt_prefix="/usr" ]
- )
