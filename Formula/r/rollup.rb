85d0064f1c3144bc4c037b9149ab05b3a7d9d907
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-27 07:21:53 +0000

    rollup: update 4.24.1 bottle.

--- Formula/r/rollup.rb
+++ Formula/r/rollup.rb
@@ -6,12 +6,12 @@ class Rollup < Formula
   license all_of: ["ISC", "MIT"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "fa3ce4739918b37e36d909d724f1f9e45f64361e392d2e0631f79cae821a01ae"
-    sha256 cellar: :any,                 arm64_sonoma:  "fa3ce4739918b37e36d909d724f1f9e45f64361e392d2e0631f79cae821a01ae"
-    sha256 cellar: :any,                 arm64_ventura: "fa3ce4739918b37e36d909d724f1f9e45f64361e392d2e0631f79cae821a01ae"
-    sha256 cellar: :any,                 sonoma:        "5825a23ab69809a4b8e1518243f107f6ea0b2339f2cf4503532eb4d4550447a0"
-    sha256 cellar: :any,                 ventura:       "5825a23ab69809a4b8e1518243f107f6ea0b2339f2cf4503532eb4d4550447a0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3571ab20728d25a4650821168091cc63e566cda2348d3f810df13c5cf81f336e"
+    sha256 cellar: :any,                 arm64_sequoia: "ca6ae1bce426a8c258db0ac40328b924bac41f51c0fb772fba92e7d645ea03a3"
+    sha256 cellar: :any,                 arm64_sonoma:  "ca6ae1bce426a8c258db0ac40328b924bac41f51c0fb772fba92e7d645ea03a3"
+    sha256 cellar: :any,                 arm64_ventura: "ca6ae1bce426a8c258db0ac40328b924bac41f51c0fb772fba92e7d645ea03a3"
+    sha256 cellar: :any,                 sonoma:        "0da3da9482e1381c6ebd7c0554057c7be11c09c75d9e0f6a4e92af25435cba91"
+    sha256 cellar: :any,                 ventura:       "0da3da9482e1381c6ebd7c0554057c7be11c09c75d9e0f6a4e92af25435cba91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34110756394f5f6bbbbebdbaf5f21d422beb5b89b2e80ae7bd62e01d9b21e3b2"
   end
 
   depends_on "node"
