1c12ef2c4e50fcfcb438a0a61f669febfa301b84
eleanordoesntcode <eleanor.publicmail@gmail.com>
2024-10-27 18:14:40 +0100

    openliberty-webprofile8 24.0.0.10

--- Formula/o/openliberty-webprofile8.rb
+++ Formula/o/openliberty-webprofile8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyWebprofile8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/24.0.0.8/openliberty-webProfile8-24.0.0.8.zip"
-  sha256 "7e8341734702d9f36387fd3c77c0c70f0cff85677b8e2a09c45f87f5b97fae86"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/24.0.0.10/openliberty-webProfile8-24.0.0.10.zip"
+  sha256 "c3ea55a3a2d375d4b2b5311f3ba20db29ab2e2afcdd4085e12c60c56310e5590"
   license "EPL-1.0"
 
   livecheck do
