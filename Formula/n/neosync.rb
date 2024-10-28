47b3bfaf3338777ae769851537f19053cb29f22f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-28 19:18:29 +0000

    neosync: update 0.4.83 bottle.

--- Formula/n/neosync.rb
+++ Formula/n/neosync.rb
@@ -7,12 +7,12 @@ class Neosync < Formula
   head "https://github.com/nucleuscloud/neosync.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d38b9dc27bb8439fe65f950fcafec34e331a7fd3ff31fc6dbbe841daddbe8021"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d38b9dc27bb8439fe65f950fcafec34e331a7fd3ff31fc6dbbe841daddbe8021"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d38b9dc27bb8439fe65f950fcafec34e331a7fd3ff31fc6dbbe841daddbe8021"
-    sha256 cellar: :any_skip_relocation, sonoma:        "439618cb8b814443079d6f12a1aac7da18cf7aca83b75763180af21b437a1bca"
-    sha256 cellar: :any_skip_relocation, ventura:       "439618cb8b814443079d6f12a1aac7da18cf7aca83b75763180af21b437a1bca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13c5331dd56744eb0e7b7dc70c37d63e93373b052a5d207d5b51fced225c8e31"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1f6678f3abe66efcc424be812359796c45e243d7bd8c0017781bf042e47db1ab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1f6678f3abe66efcc424be812359796c45e243d7bd8c0017781bf042e47db1ab"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1f6678f3abe66efcc424be812359796c45e243d7bd8c0017781bf042e47db1ab"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3758d77834ee737ad1c246f0a4e958407cbedfca71aab277097a7a0d30f449f2"
+    sha256 cellar: :any_skip_relocation, ventura:       "3758d77834ee737ad1c246f0a4e958407cbedfca71aab277097a7a0d30f449f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ad43b2a51bb24427c2aa00b8d76c1cc1ab28c75633e32c2148b3c8af165a71d"
   end
 
   depends_on "go" => :build
