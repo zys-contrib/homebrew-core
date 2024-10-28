2cda39efd528014778fe3dd6bf1865e1c89a301a
Rui Chen <rui@chenrui.dev>
2024-10-28 15:00:08 -0400

    megatools 1.11.1.20241028
    autobump: add megatools
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/megatools.rb
+++ Formula/m/megatools.rb
@@ -1,10 +1,9 @@
 class Megatools < Formula
   desc "Command-line client for Mega.co.nz"
   homepage "https://megatools.megous.com/"
-  url "https://megatools.megous.com/builds/megatools-1.11.1.20230212.tar.gz"
-  sha256 "ecfa2ee4b277c601ebae648287311030aa4ca73ea61ee730bc66bef24ef19a34"
+  url "https://megatools.megous.com/builds/megatools-1.11.1.20241028.tar.gz"
+  sha256 "3632135a23aa737950e74183199eb23d4e44461ca4221842717225fb31527a4d"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
-  revision 1
 
   livecheck do
     url "https://megatools.megous.com/builds/"
