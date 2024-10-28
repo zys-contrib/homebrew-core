a500c508b3e909268e9756a999b1afff844d1c00
Sutou Kouhei <kou@clear-code.com>
2024-10-29 06:12:37 +0900

    vineyard: revision bump (apache-arrow 18.0.0)

--- Formula/v/vineyard.rb
+++ Formula/v/vineyard.rb
@@ -6,7 +6,7 @@ class Vineyard < Formula
   url "https://github.com/v6d-io/v6d/releases/download/v0.23.2/v6d-0.23.2.tar.gz"
   sha256 "2a2788ed77b9459477b3e90767a910e77e2035a34f33c29c25b9876568683fd4"
   license "Apache-2.0"
-  revision 6
+  revision 7
 
   bottle do
     sha256                               arm64_sequoia: "f207ec66068890b869177a0eadad0c42e5347f4da45b0536746d792b4559e76f"
