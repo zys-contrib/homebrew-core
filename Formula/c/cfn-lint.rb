46c025965d9dd5f1f72fccf4010a015fd4932dc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-28 21:27:29 +0000

    cfn-lint 1.18.2

--- Formula/c/cfn-lint.rb
+++ Formula/c/cfn-lint.rb
@@ -3,8 +3,8 @@ class CfnLint < Formula
 
   desc "Validate CloudFormation templates against the CloudFormation spec"
   homepage "https://github.com/aws-cloudformation/cfn-lint/"
-  url "https://files.pythonhosted.org/packages/80/82/142f1f3333e02cad6f545d7f5ad3eae6303fe98b80e4f23830f1aa4ce1c7/cfn_lint-1.18.1.tar.gz"
-  sha256 "09139c4260897ada66034b57c9d9979cc2ec25856660d8f114f373dc4f70a34a"
+  url "https://files.pythonhosted.org/packages/1a/7d/c63c5c6c5723b2c07a30647087ea79f06e3c6b794e9a9fa3e35e131538dd/cfn_lint-1.18.2.tar.gz"
+  sha256 "9ee576db7804839a7f11a7509205bf39ec2663cea2d0c2c09f35c41e2bc03673"
   license "MIT-0"
 
   livecheck do
@@ -41,13 +41,13 @@ class CfnLint < Formula
   end
 
   resource "boto3" do
-    url "https://files.pythonhosted.org/packages/39/61/a545e0d5c331205bcf9e6d24b0bfc95dce91d285787f7d1f967b8228bfce/boto3-1.35.45.tar.gz"
-    sha256 "9f4a081e1940846171b51d903000a04322f1356d53225ce1028fc1760a155a70"
+    url "https://files.pythonhosted.org/packages/5f/2e/553086c009b4b9975f4af9f0efaac5855f2fb69dae4b94638df477a3ff37/boto3-1.35.50.tar.gz"
+    sha256 "4f15d1ccb481d66f6925b8c91c970ce41b956b6ecf7c479f23e2159531b37eec"
   end
 
   resource "botocore" do
-    url "https://files.pythonhosted.org/packages/6f/c8/7cd28382cd4d45210cbd81ad452add0b8e198c9fc5dc2b1bd27df53e1b4b/botocore-1.35.45.tar.gz"
-    sha256 "9a898bfdd6b0027fee2018711192c15c2716bf6a7096b1168bd8a896df3664a1"
+    url "https://files.pythonhosted.org/packages/ef/16/04bc1b17f085a435e28b101e8a62cfe9588d6eeeab5ffa39c07d134da512/botocore-1.35.50.tar.gz"
+    sha256 "136ecef8d5a1088f1ba485c0bbfca40abd42b9f9fe9e11d8cde4e53b4c05b188"
   end
 
   resource "jmespath" do
