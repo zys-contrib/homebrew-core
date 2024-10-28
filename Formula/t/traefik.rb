9f754e830c9aff01e808828440fc6cf0e97d9b1a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-28 14:02:58 +0000

    traefik: update 3.1.7 bottle.

--- Formula/t/traefik.rb
+++ Formula/t/traefik.rb
@@ -7,12 +7,12 @@ class Traefik < Formula
   head "https://github.com/traefik/traefik.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2904f9094a89e3ac1e205e68f7afddfb4dd07ae16c7a2d8826bcca46b3ab2b24"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2904f9094a89e3ac1e205e68f7afddfb4dd07ae16c7a2d8826bcca46b3ab2b24"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "2904f9094a89e3ac1e205e68f7afddfb4dd07ae16c7a2d8826bcca46b3ab2b24"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f8e8b2eac048d445df763db8616ce66397935e26c15521212825a047d2d4240a"
-    sha256 cellar: :any_skip_relocation, ventura:       "f8e8b2eac048d445df763db8616ce66397935e26c15521212825a047d2d4240a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c9763687e8f9fc56c215e6daca1f7997ec09ed8682e2b6dd5b32b4855189156c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d7158155f7336491b25879759d994f0f12ed134b296a2d291a4001986eb030ce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7158155f7336491b25879759d994f0f12ed134b296a2d291a4001986eb030ce"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d7158155f7336491b25879759d994f0f12ed134b296a2d291a4001986eb030ce"
+    sha256 cellar: :any_skip_relocation, sonoma:        "defc5a0d1115defa27c04576ba9cb7e5fd8098f0b1900db9019098f9c9da1541"
+    sha256 cellar: :any_skip_relocation, ventura:       "defc5a0d1115defa27c04576ba9cb7e5fd8098f0b1900db9019098f9c9da1541"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5eab2903f4c4ad76b790e704acf21794aca2b1aba6089459339d457441d8d829"
   end
 
   depends_on "go" => :build
