98c15f9610a09b40ddad513b1569b3278a572ef2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-29 21:26:50 +0000

    buf 1.46.0

--- Formula/b/buf.rb
+++ Formula/b/buf.rb
@@ -1,8 +1,8 @@
 class Buf < Formula
   desc "New way of working with Protocol Buffers"
   homepage "https://github.com/bufbuild/buf"
-  url "https://github.com/bufbuild/buf/archive/refs/tags/v1.45.0.tar.gz"
-  sha256 "18c083b02faeb59bf504e6e462e4b6aaf1e5db5ec4dd739322fc804cda6a069b"
+  url "https://github.com/bufbuild/buf/archive/refs/tags/v1.46.0.tar.gz"
+  sha256 "0a45975205fe8ac0eea6673dff6b86603eaadbc8ce6dd62bc95dd925ab6d2419"
   license "Apache-2.0"
   head "https://github.com/bufbuild/buf.git", branch: "main"
 
