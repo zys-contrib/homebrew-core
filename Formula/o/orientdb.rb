fa34a231e18f648fdc747bf326d2a9a6b8a3415f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-29 15:51:04 +0000

    orientdb 3.2.35

--- Formula/o/orientdb.rb
+++ Formula/o/orientdb.rb
@@ -1,8 +1,8 @@
 class Orientdb < Formula
   desc "Graph database"
   homepage "https://orientdb.org/"
-  url "https://search.maven.org/remotecontent?filepath=com/orientechnologies/orientdb-community/3.2.34/orientdb-community-3.2.34.zip"
-  sha256 "a53493414daac3008ddd15c27a4d1ab7c4dafc5474ef10445ed5561707bcd39a"
+  url "https://search.maven.org/remotecontent?filepath=com/orientechnologies/orientdb-community/3.2.35/orientdb-community-3.2.35.zip"
+  sha256 "b5bbc84023740da6fd0f78d5819cb3413db492dd582aa6402fbf4c1e1f0261ba"
   license "Apache-2.0"
 
   livecheck do
