a301b54106179e0eaeda3b8bd521ff0138c1c518
Rui Chen <rui@chenrui.dev>
2024-10-30 13:39:07 +0000

    boring: add 0.6.0 bottle.

--- Formula/b/boring.rb
+++ Formula/b/boring.rb
@@ -5,6 +5,15 @@ class Boring < Formula
   sha256 "460b007759ab2956da2b0f56478cff1574dc668791842c55aa9c2e2c25b03a3d"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3542db45abbb02972370f48346ad52941fdb0b7983f6a55cf90785b1cc1a6c8c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3542db45abbb02972370f48346ad52941fdb0b7983f6a55cf90785b1cc1a6c8c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3542db45abbb02972370f48346ad52941fdb0b7983f6a55cf90785b1cc1a6c8c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a2abab54176d5aa084f54d4165dce4d613e6219e412a387f45a873c3cd1d2d2e"
+    sha256 cellar: :any_skip_relocation, ventura:       "a2abab54176d5aa084f54d4165dce4d613e6219e412a387f45a873c3cd1d2d2e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78b1476f4eeb5b3f49e8a4efdeb9642b468da9f36674202d7b4b79d098337c6d"
+  end
+
   depends_on "go" => :build
 
   def install
