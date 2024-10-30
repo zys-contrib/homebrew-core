6924f96fda7f8eaa3099bc90f833f8a36f0eb52e
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-10-30 08:21:29 +0700

    rain 1.18.0

--- Formula/r/rain.rb
+++ Formula/r/rain.rb
@@ -1,8 +1,8 @@
 class Rain < Formula
   desc "Command-line tool for working with AWS CloudFormation"
   homepage "https://github.com/aws-cloudformation/rain"
-  url "https://github.com/aws-cloudformation/rain/archive/refs/tags/v1.17.0.tar.gz"
-  sha256 "8c43c6079081184d6526bb1901d9731ecbb1db20089dd689aa816bffba931d7d"
+  url "https://github.com/aws-cloudformation/rain/archive/refs/tags/v1.18.0.tar.gz"
+  sha256 "b742cfbbb89740f4633fc9811dce9bac2d91612b9c9384d07439152f5af29daa"
   license "Apache-2.0"
 
   bottle do
