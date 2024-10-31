60469c1da90ac38094fdaec692d54c303ba4e44d
Michael Cho <20700669+cho-m@users.noreply.github.com>
2024-10-31 03:09:28 +0000

    node: update 23.1.0_1 bottle.

--- Formula/n/node.rb
+++ Formula/n/node.rb
@@ -13,12 +13,12 @@ class Node < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "5824a64a342860ad4ac5b18176aaa2814895cfbe4e8cd80542153c2ad9eb16b0"
-    sha256 arm64_sonoma:  "ca3540c4cb81d2f0fef4780561947dad31659e5dc4fafbbce369727d6a69e98c"
-    sha256 arm64_ventura: "662948dd51379a9d7c41a3e2899f00269a14b80f4183d821b208f4b203b6b09a"
-    sha256 sonoma:        "30b416b36b2a01b1678ce99fb76616433a0b7fdbd5654222cff058c55018c7e7"
-    sha256 ventura:       "741dc0fa1a33104d0fb69a28cf10b25c9672edb78bc2d68f81001c41a799dd03"
-    sha256 x86_64_linux:  "8eea14e17b67e32f59325d0c61d904fc0f69d255854edb7d8ae7117cc8dc1126"
+    sha256 arm64_sequoia: "23880f5ec9a76db3849a3f37b65536f94c67244f91096aef2e0a7c20c5a53f9c"
+    sha256 arm64_sonoma:  "4933b550c70a22bdf1f3f22eb344db6da5c6881b0a0df8efd87135032700f347"
+    sha256 arm64_ventura: "ea816bfb85d12377f6e140eb307ef093c4b64bc0d3f2db63304838ddfbeb727a"
+    sha256 sonoma:        "6a3605890e590286f117dc94779ea1df756546c92c9f3766eecfe5b95716f05f"
+    sha256 ventura:       "45a8ebd5667cc203abbc45f4cc8e7b651aff64612cc316371cdc36361ed1f359"
+    sha256 x86_64_linux:  "e44cf954f3834ae57ade5d162c9bae349da4be5364a857b4a7d7a4cae3113df4"
   end
 
   depends_on "pkg-config" => :build
