d792816e12221a0d0caef708558f206b21d73108
Michael Cho <20700669+cho-m@users.noreply.github.com>
2024-10-31 03:09:22 +0000

    icu4c@76: add 76.1 bottle.

--- Formula/i/icu4c@76.rb
+++ Formula/i/icu4c@76.rb
@@ -17,6 +17,15 @@ class Icu4cAT76 < Formula
     end
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "864ab79f49fd097e8537c281af64e3987d085c753086c60cde74fc84f55ee771"
+    sha256 cellar: :any,                 arm64_sonoma:  "2b28efee579ee1a87cb4264e4ea714dd4af6edf59fa2e29955ffe4408428d726"
+    sha256 cellar: :any,                 arm64_ventura: "7ca03c808b01c40b270146e476bfcb18367f830e9f1722c9effc4f1c5954b20f"
+    sha256 cellar: :any,                 sonoma:        "30d9e64dbac8658ab81012ccfe1e52f87cd1ec8cb247b562d4484665ef6b5247"
+    sha256 cellar: :any,                 ventura:       "6d57d5ff7ed6d83916f9c47aa82eb84d1555fc23f8c779491e42e71817d8b2ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0e8e698c09aee143fce7529fcdf776be98d15f3a000ed8c3e74dc387ce364ac"
+  end
+
   # TODO: Switch keg_only reason after renaming `icu4c` formula to `icu4c@75` and updating alias to `icu4c@76`
   # keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
   keg_only :versioned_formula
