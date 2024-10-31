7d12739a3e5eaa489865f2bf2d5f19391fd7cd78
Adam Jensen <adam@acj.sh>
2024-10-31 02:44:45 +0000

    rbspy 0.26.0

--- Formula/r/rbspy.rb
+++ Formula/r/rbspy.rb
@@ -1,8 +1,8 @@
 class Rbspy < Formula
   desc "Sampling profiler for Ruby"
   homepage "https://rbspy.github.io/"
-  url "https://github.com/rbspy/rbspy/archive/refs/tags/v0.25.0.tar.gz"
-  sha256 "7d1541ae9ce3ed9085ca4c0e4e77ef68528b9ad60b9a3ae5b98f1fd6834546d3"
+  url "https://github.com/rbspy/rbspy/archive/refs/tags/v0.26.0.tar.gz"
+  sha256 "30f34212f34037a8652c0aea1d26c3bd9ac65a8bb75f2960868a2b2117a4784d"
   license "MIT"
 
   bottle do
