7ea8e4eb3ade265e29d255a328d2e87b75185472
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-31 09:44:22 +0000

    jreleaser 1.15.0

--- Formula/j/jreleaser.rb
+++ Formula/j/jreleaser.rb
@@ -1,8 +1,8 @@
 class Jreleaser < Formula
   desc "Release projects quickly and easily with JReleaser"
   homepage "https://jreleaser.org/"
-  url "https://github.com/jreleaser/jreleaser/releases/download/v1.14.0/jreleaser-1.14.0.zip"
-  sha256 "9ffaae7cef052bc01415f47865f6fe3cccef7e470527eaaf5d3c0245f36acba9"
+  url "https://github.com/jreleaser/jreleaser/releases/download/v1.15.0/jreleaser-1.15.0.zip"
+  sha256 "fd3d5b424190956313da111c144bb4788fc647907722e4a662c1e6654c2ac384"
   license "Apache-2.0"
 
   bottle do
