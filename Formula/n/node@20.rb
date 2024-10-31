873f4079762e23a41b335105fbe5d2bf77c952ad
Michael Cho <michael@michaelcho.dev>
2024-10-31 08:58:27 -0400

    node@20: revision bump to migrate to `icu4c@76`

--- Formula/n/node@20.rb
+++ Formula/n/node@20.rb
@@ -4,7 +4,7 @@ class NodeAT20 < Formula
   url "https://nodejs.org/dist/v20.18.0/node-v20.18.0.tar.xz"
   sha256 "7d9433e91fd88d82ba8de86e711ec41907638e227993d22e95126b02f6cd714a"
   license "MIT"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://nodejs.org/dist/"
@@ -30,7 +30,7 @@ class NodeAT20 < Formula
   depends_on "python@3.13" => :build
   depends_on "brotli"
   depends_on "c-ares"
-  depends_on "icu4c@75"
+  depends_on "icu4c@76"
   depends_on "libnghttp2"
   depends_on "libuv"
   depends_on "openssl@3"
@@ -50,6 +50,12 @@ class NodeAT20 < Formula
   end
 
   fails_with gcc: "5"
+
+  # Backport support for ICU 76+
+  patch do
+    url "https://github.com/nodejs/node/commit/81517faceac86497b3c8717837f491aa29a5e0f9.patch?full_index=1"
+    sha256 "79a5489617665c5c88651a7dc364b8967bebdea5bdf361b85572d041a4768662"
+  end
 
   def install
     ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1100)
