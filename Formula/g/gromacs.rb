f512d6a794364b466cc3347bd56ff917c6029c82
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-31 15:37:38 +0000

    gromacs 2024.4

--- Formula/g/gromacs.rb
+++ Formula/g/gromacs.rb
@@ -1,8 +1,8 @@
 class Gromacs < Formula
   desc "Versatile package for molecular dynamics calculations"
   homepage "https://www.gromacs.org/"
-  url "https://ftp.gromacs.org/pub/gromacs/gromacs-2024.3.tar.gz"
-  sha256 "bbda056ee59390be7d58d84c13a9ec0d4e3635617adf2eb747034922cba1f029"
+  url "https://ftp.gromacs.org/pub/gromacs/gromacs-2024.4.tar.gz"
+  sha256 "ac618ece2e58afa86b536c5a2c4fcb937f0760318f12d18f10346b6bdebd86a8"
   license "LGPL-2.1-or-later"
 
   livecheck do
