4d17ec9e922d1920a6650c3f6fe09786593bab9f
Johan Walles <johan.walles@gmail.com>
2024-11-01 21:41:34 +0000

    moar 1.28.0

--- Formula/m/moar.rb
+++ Formula/m/moar.rb
@@ -1,8 +1,8 @@
 class Moar < Formula
   desc "Nice to use pager for humans"
   homepage "https://github.com/walles/moar"
-  url "https://github.com/walles/moar/archive/refs/tags/v1.27.3.tar.gz"
-  sha256 "323e6f645dad30bc1399f9fe9537f70288f705549ba6e7ceceb62d11020f858c"
+  url "https://github.com/walles/moar/archive/refs/tags/v1.28.0.tar.gz"
+  sha256 "6997b8c675950481793fbdcd6d0bb85b887901865031136a8135ca3cb28b1dd3"
   license "BSD-2-Clause"
 
   bottle do
