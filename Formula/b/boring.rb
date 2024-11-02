5b22bd642cb29528937238c154ec4aa2d8e20099
Alexander Becker <alexander.becker@tum.de>
2024-11-02 17:55:13 +0100

    boring 0.7.0
    This commit updates the boring formula to version 0.7.0. It additionally introduces
    a post_install hook which kills an existing boring daemon instance (if running). This
    ensures that CLI and daemon have the same version. Upon first invocation of the updated
    binary, it automatically spawns a new daemon (as in previous versions).

--- Formula/b/boring.rb
+++ Formula/b/boring.rb
@@ -1,8 +1,8 @@
 class Boring < Formula
   desc "`boring` SSH tunnel manager"
   homepage "https://github.com/alebeck/boring"
-  url "https://github.com/alebeck/boring/archive/refs/tags/0.6.0.tar.gz"
-  sha256 "460b007759ab2956da2b0f56478cff1574dc668791842c55aa9c2e2c25b03a3d"
+  url "https://github.com/alebeck/boring/archive/refs/tags/0.7.0.tar.gz"
+  sha256 "ea04fc196d8c29fa4f533a6ecb2d3feeecbefea3f6b6bb614416612f05a3b1af"
   license "MIT"
 
   bottle do
@@ -21,17 +21,22 @@ class Boring < Formula
     system "go", "build", *std_go_args(ldflags:), "./cmd/boring"
   end
 
+  def post_install
+    quiet_system "killall", "boring"
+  end
+
   test do
     assert_match version.to_s, shell_output(bin/"boring", 1)
 
     # first interaction with boring should create the user config file
     # and correctly output that no tunnels are currently configured
+    test_config = testpath/".boring.toml"
+    ENV["BORING_CONFIG"] = test_config
     output = shell_output("#{bin}/boring list")
     assert output.end_with?("No tunnels configured.\n")
-    assert_predicate testpath/".boring.toml", :exist?
+    assert_predicate test_config, :exist?
 
     # now add an example tunnel and check that it is parsed correctly
-    test_config = testpath/".boring.toml"
     test_config.write <<~EOF, mode: "a+"
       [[tunnels]]
       name = "dev"
