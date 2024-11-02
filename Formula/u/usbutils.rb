035f31ddaebf322ba6b746b801b71008ec621c32
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-02 01:17:28 +0000

    usbutils: update 018 bottle.

--- Formula/u/usbutils.rb
+++ Formula/u/usbutils.rb
@@ -16,14 +16,12 @@ class Usbutils < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia:  "29214690f12e4b8adb6616dd47c41b43cec827637d09c1f4dab15a255050ca68"
-    sha256 cellar: :any,                 arm64_sonoma:   "374ce8a0c6782808d4fd194dbfd177c779ca568e7d5b0a5687412add96cce4ed"
-    sha256 cellar: :any,                 arm64_ventura:  "735a4d7ec2d7393bb3421c27583f1df30455c5a4c3b092786dfe3ee92144806e"
-    sha256 cellar: :any,                 arm64_monterey: "eabe8a0f9cee89292386b2b0900677127c8827882bf43323dfb732013d6a2f1a"
-    sha256 cellar: :any,                 sonoma:         "3c3468d2b41346db49f356b1f0ab04e86bfbcc93954bbcfc2c343c6fbd720eaa"
-    sha256 cellar: :any,                 ventura:        "1c2b43d31059a4b67815be116ab308817487a9f06d670244a2a234a023851d93"
-    sha256 cellar: :any,                 monterey:       "6a760ee183985a6d80d58efe5e78c945511c3b89cce5e01bfb48858637fdb322"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "17f5961f3ebd783d33e4851654070498c44c65c2bf235754ce8dcd704063dcaa"
+    sha256 cellar: :any, arm64_sequoia: "19bc5627cb39ad4877754708f98e513f74d74d51a66a2c40e5fb904b44c97851"
+    sha256 cellar: :any, arm64_sonoma:  "04127d6f7b7f69f9cf13d59e64c41f75628b9ac4cd4c72da70d9ee892eec1be5"
+    sha256 cellar: :any, arm64_ventura: "620a18e00aedf3f6216972e411044b3b212186daf06de35aaba4c01b0da88b75"
+    sha256 cellar: :any, sonoma:        "5a46602f76dd8cc39b901f1ec384593ca95183ecb8c013dd5db6be6692129ef9"
+    sha256 cellar: :any, ventura:       "4e352bf259ad82c3c2f794c009cb7adcc3fe29829373720529180c28a03902b5"
+    sha256               x86_64_linux:  "b6f4d16b0e4a42673c5725cbc31c25a8bfbe702ca82ecc1b58493248285f57c4"
   end
 
   depends_on "meson" => :build
