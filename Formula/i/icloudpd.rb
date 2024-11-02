1472ae4474d70833e4625d2eec384d2c589a01e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-02 18:42:07 +0000

    icloudpd 1.24.2
    icloudpd: update deps
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/icloudpd.rb
+++ Formula/i/icloudpd.rb
@@ -5,8 +5,8 @@ class Icloudpd < Formula
   homepage "https://github.com/icloud-photos-downloader/icloud_photos_downloader"
   # We use a git checkout as scripts/patch_version runs git commands to update SHA
   url "https://github.com/icloud-photos-downloader/icloud_photos_downloader.git",
-      tag:      "v1.24.1",
-      revision: "e8121e6dd7db697b136ebe98490a9219058bfa6d"
+      tag:      "v1.24.2",
+      revision: "2967f82b5a5794bb551565e55312ffe17e97b955"
   license "MIT"
   head "https://github.com/icloud-photos-downloader/icloud_photos_downloader.git", branch: "master"
 
@@ -131,8 +131,8 @@ class Icloudpd < Formula
   end
 
   resource "srp" do
-    url "https://files.pythonhosted.org/packages/f4/a3/f74ae47c52ef866e3e454c3f863fb2a73eb41803d12d20349b62a3924f36/srp-1.0.21.tar.gz"
-    sha256 "866813bcf521189a1563e6ca3112b6f54fdf725a410a2dbebb6f0d84b82a1f1d"
+    url "https://files.pythonhosted.org/packages/8d/fb/9210875dd162d3977580407b1c5ce6e779e770b8197a0de76819144a9755/srp-1.0.22.tar.gz"
+    sha256 "f330d0ec7387e2ac8577487b164963155d4a031bca6e2024f1b0930eb92baa5d"
   end
 
   resource "tqdm" do
@@ -161,8 +161,8 @@ class Icloudpd < Formula
   end
 
   resource "Werkzeug" do
-    url "https://files.pythonhosted.org/packages/d4/f9/0ba83eaa0df9b9e9d1efeb2ea351d0677c37d41ee5d0f91e98423c7281c9/werkzeug-3.0.6.tar.gz"
-    sha256 "a8dd59d4de28ca70471a34cba79bed5f7ef2e036a76b3ab0835474246eb41f8d"
+    url "https://files.pythonhosted.org/packages/32/af/d4502dc713b4ccea7175d764718d5183caf8d0867a4f0190d5d4a45cea49/werkzeug-3.1.1.tar.gz"
+    sha256 "8cd39dfbdfc1e051965f156163e2974e52c210f130810e9ad36858f0fd3edad4"
   end
 
   # support python 3.13
