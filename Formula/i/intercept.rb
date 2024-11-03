d1489903a4a8874ecf2c3d6b6f4a3885cfc74bd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-03 00:53:58 +0000

    intercept: update 1.0.12 bottle.

--- Formula/i/intercept.rb
+++ Formula/i/intercept.rb
@@ -16,13 +16,12 @@ class Intercept < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "462d8ea33b9bc231246f675b27aae0410bc3533f7936ddb325825676b7dc6e4b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7b9f713421eb60a9f282382c984910102b4ee038a9696646692a58f95b60deb0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4ffd7963af43fd4e77eac4bedc85b02a8464eb2ba5d68794e9fe2efea1639d2d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "0a11dd7aa846ced12af4e9f7e86ff34a64af742687df91e37b6147d310db1b95"
-    sha256 cellar: :any_skip_relocation, ventura:        "8a75a820f8305407af68d2fd1af865e785281de68a88a36e9a19e76638206844"
-    sha256 cellar: :any_skip_relocation, monterey:       "2c9cb3a1eb2649a7d6a94cadc651f260b0778f784d9f25117cf2c9f4ee42f4ed"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5f0ae352d17745513393daf0a38051c8b7aa753fc9154af446470d474585731"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b2ca377ce1685e85a012867dbddc3a499ff32c063c29b361f653a7a4f073aa4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b2ca377ce1685e85a012867dbddc3a499ff32c063c29b361f653a7a4f073aa4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7b2ca377ce1685e85a012867dbddc3a499ff32c063c29b361f653a7a4f073aa4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4e28e659023400422b0e0b74efe5bfc8c1e6ba00f7a22882bc38b708997bc8a8"
+    sha256 cellar: :any_skip_relocation, ventura:       "4e28e659023400422b0e0b74efe5bfc8c1e6ba00f7a22882bc38b708997bc8a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38d2e66df02d2438485a11a941f5c53287609a58a39aae3570e10294332688e4"
   end
 
   depends_on "go" => :build
