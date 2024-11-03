f8dd690c249f1a11b142b8e0abbf3d5ba78c4475
Michael Cho <michael@michaelcho.dev>
2024-11-03 15:53:34 -0500

    onnxruntime 1.20.0

--- Formula/o/onnxruntime.rb
+++ Formula/o/onnxruntime.rb
@@ -2,8 +2,8 @@ class Onnxruntime < Formula
   desc "Cross-platform, high performance scoring engine for ML models"
   homepage "https://github.com/microsoft/onnxruntime"
   url "https://github.com/microsoft/onnxruntime.git",
-      tag:      "v1.17.1",
-      revision: "8f5c79cb63f09ef1302e85081093a3fe4da1bc7d"
+      tag:      "v1.20.0",
+      revision: "c4fb724e810bb496165b9015c77f402727392933"
   license "MIT"
 
   livecheck do
@@ -21,21 +21,78 @@ class Onnxruntime < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "dde778dd1564940af7b2116cba7d4c8b45b4d6e335c78620cf97b1e1e6a688b1"
   end
 
+  depends_on "boost" => :build
   depends_on "cmake" => :build
-  depends_on "python@3.12" => :build
+  depends_on "cpp-gsl" => :build
+  depends_on "flatbuffers" => :build # NOTE: links to static library
+  depends_on "howard-hinnant-date" => :build
+  depends_on "nlohmann-json" => :build
+  depends_on "python@3.13" => :build
+  depends_on "safeint" => :build
+  depends_on "abseil"
+  depends_on "nsync"
+  depends_on "protobuf@21" # https://github.com/microsoft/onnxruntime/issues/21308
+  depends_on "re2"
 
-  fails_with gcc: "5" # GCC version < 7 is no longer supported
+  # Need newer than stable `eigen` after https://github.com/microsoft/onnxruntime/pull/21492
+  # element_wise_ops.cc:708:32: error: no matching member function for call to 'min'
+  #
+  # https://github.com/microsoft/onnxruntime/blob/v#{version}/cmake/deps.txt#L25
+  resource "eigen" do
+    url "https://gitlab.com/libeigen/eigen/-/archive/e7248b26a1ed53fa030c5c459f7ea095dfd276ac/eigen-e7248b26a1ed53fa030c5c459f7ea095dfd276ac.tar.bz2"
+    sha256 "a3f1724de1dc7e7f74fbcc206ffcaeba27fd89b37dc71f9c31e505634d0c1634"
+  end
+
+  # https://github.com/microsoft/onnxruntime/blob/v#{version}/cmake/deps.txt#L52
+  resource "pytorch_cpuinfo" do
+    url "https://github.com/pytorch/cpuinfo/archive/ca678952a9a8eaa6de112d154e8e104b22f9ab3f.tar.gz"
+    sha256 "c8f43b307fa7d911d88fec05448161eb1949c3fc0cb62f3a7a2c61928cdf2e9b"
+  end
+
+  # TODO: Consider making separate formula
+  resource "onnx" do
+    url "https://github.com/onnx/onnx/archive/refs/tags/v1.17.0.tar.gz"
+    sha256 "8d5e983c36037003615e5a02d36b18fc286541bf52de1a78f6cf9f32005a820e"
+  end
+
+  # Fix build on Linux
+  # TODO: Upstream if it works
+  patch :DATA
 
   def install
-    cmake_args = %W[
-      -Donnxruntime_RUN_ONNX_TESTS=OFF
-      -Donnxruntime_GENERATE_TEST_REPORTS=OFF
-      -DPYTHON_EXECUTABLE=#{which("python3.12")}
+    python3 = which("python3.13")
+
+    # Workaround to use brew `nsync`. Remove in future release with
+    # https://github.com/microsoft/onnxruntime/commit/88676e62b966add2cc144a4e7d8ae1dbda1148e8
+    inreplace "cmake/external/onnxruntime_external_deps.cmake" do |s|
+      s.gsub!(/ NAMES nsync unofficial-nsync$/, " NAMES nsync_cpp")
+      s.gsub!(/\bunofficial::nsync::nsync_cpp\b/, "nsync_cpp")
+    end
+
+    resources.each do |r|
+      (buildpath/"build/_deps/#{r.name}-src").install r
+    end
+
+    args = %W[
+      -DHOMEBREW_ALLOW_FETCHCONTENT=ON
+      -DFETCHCONTENT_FULLY_DISCONNECTED=ON
+      -DFETCHCONTENT_SOURCE_DIR_PYTORCH_CLOG=#{buildpath}/build/_deps/pytorch_cpuinfo-src
+      -DFETCHCONTENT_TRY_FIND_PACKAGE_MODE=ALWAYS
+      -DPYTHON_EXECUTABLE=#{python3}
+      -DONNX_CUSTOM_PROTOC_EXECUTABLE=#{Formula["protobuf@21"].opt_bin}/protoc
       -Donnxruntime_BUILD_SHARED_LIB=ON
       -Donnxruntime_BUILD_UNIT_TESTS=OFF
+      -Donnxruntime_GENERATE_TEST_REPORTS=OFF
+      -Donnxruntime_RUN_ONNX_TESTS=OFF
+      -Donnxruntime_USE_FULL_PROTOBUF=ON
     ]
 
-    system "cmake", "-S", "cmake", "-B", "build", *cmake_args, *std_cmake_args
+    # Regenerate C++ bindings to use newer `flatbuffers`
+    flatc = Formula["flatbuffers"].opt_bin/"flatc"
+    system python3, "onnxruntime/core/flatbuffers/schema/compile_schema.py", "--flatc", flatc
+    system python3, "onnxruntime/lora/adapter_format/compile_schema.py", "--flatc", flatc
+
+    system "cmake", "-S", "cmake", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -50,8 +107,21 @@ class Onnxruntime < Formula
         return 0;
       }
     C
-    system ENV.cc, "-I#{include}", testpath/"test.c",
-           "-L#{lib}", "-lonnxruntime", "-o", testpath/"test"
+    system ENV.cc, "-I#{include}", "test.c", "-L#{lib}", "-lonnxruntime", "-o", "test"
     assert_equal version, shell_output("./test").strip
   end
 end
+
+__END__
+diff --git a/onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc b/onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
+index 470838d36e..81a842eb87 100644
+--- a/onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
++++ b/onnxruntime/core/optimizer/transpose_optimization/onnx_transpose_optimization.cc
+@@ -5,6 +5,7 @@
+ 
+ #include <algorithm>
+ #include <cassert>
++#include <cstring>
+ #include <iostream>
+ #include <memory>
+ #include <unordered_map>
