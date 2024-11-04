13ddf81a1f230dde1ea678ca2bf78640d5f60ad0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-04 18:37:20 +0000

    go-size-analyzer 1.7.5

--- Formula/g/go-size-analyzer.rb
+++ Formula/g/go-size-analyzer.rb
@@ -1,8 +1,8 @@
 class GoSizeAnalyzer < Formula
   desc "Analyzing the dependencies in compiled Golang binaries"
   homepage "https://github.com/Zxilly/go-size-analyzer"
-  url "https://github.com/Zxilly/go-size-analyzer/archive/refs/tags/v1.7.4.tar.gz"
-  sha256 "92c9fe6300bb9b5aae3f26e1524da3410a490a133ce9b28f34c6dd6ca80fc13e"
+  url "https://github.com/Zxilly/go-size-analyzer/archive/refs/tags/v1.7.5.tar.gz"
+  sha256 "55dfd6e1d80300028e0f47ba5cceaa1de74b0a8a110f1ebfbf1c8fadf6bcea7f"
   license "AGPL-3.0-only"
   head "https://github.com/Zxilly/go-size-analyzer.git", branch: "master"
 
