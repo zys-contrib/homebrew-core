266da5ac67dec1c9347e28ea162192101c87dee2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-05 03:35:14 +0000

    ssldump: update 1.9 bottle.

--- Formula/s/ssldump.rb
+++ Formula/s/ssldump.rb
@@ -6,14 +6,12 @@ class Ssldump < Formula
   license "BSD-4-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia:  "e03414b1accfe6900e7ca54b236c583dceced96e8d93ab5d06733240cd8b5961"
-    sha256 cellar: :any,                 arm64_sonoma:   "5a04f758e392c85c23e64281e62e56355494e8c17cc2916d4b5b3496e13337c6"
-    sha256 cellar: :any,                 arm64_ventura:  "48bec7a09eb15f3b58e960495016638e6a8b8c2e041f00f20d2ef7d7b7197917"
-    sha256 cellar: :any,                 arm64_monterey: "bf61f7bbad2f9c944136b9338cc2ad2ae7a7d501f72762ee03969bb8f95e7290"
-    sha256 cellar: :any,                 sonoma:         "ec039ce32c6879ccc9febdfd3cc80aefbf434fdd53191046ceecacf74625470d"
-    sha256 cellar: :any,                 ventura:        "af9a81b16fc912ffd160db362c9e78512e9d06452d028045dea42491a23afc5d"
-    sha256 cellar: :any,                 monterey:       "97a5488ee177f7e6fa0dd65ec4f06dfb61a94719577c44ee44cf231111be2b91"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2431676c289f4b0000dbff8a7c131803dc06385d18f9445affbbe1f83b1c1810"
+    sha256 cellar: :any,                 arm64_sequoia: "4108e474738b3c274d3124a2325e910109556d45568f6f3e299882d9a7b3697c"
+    sha256 cellar: :any,                 arm64_sonoma:  "bef8ddb0f055225bd2f77fc6ad5270c52b7db46f9797a3d5e23b9c9872bd0791"
+    sha256 cellar: :any,                 arm64_ventura: "b0c5a32df2ef8e072f50f5302ef2b1813de8d0b4fcd13660847b193ba1449260"
+    sha256 cellar: :any,                 sonoma:        "d0e771ebfc5cac4998d58d7b84a188fb94e26ccdae77ecb8625fc0a9b5bfdf09"
+    sha256 cellar: :any,                 ventura:       "9b11e1e942fddc5ff4d2189a69e9acc4bef77fa5ea0a7e6bce789804f9512c6a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a258e929e9b40617be052eb2026e693d39ec47ad006c231e2e542668ffe322d"
   end
 
   depends_on "cmake" => :build
