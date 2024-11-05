3501b7281a4476191728dcebf2bd2724442b6953
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-05 12:50:10 +0000

    nerdfetch 8.3.1

--- Formula/n/nerdfetch.rb
+++ Formula/n/nerdfetch.rb
@@ -1,8 +1,8 @@
 class Nerdfetch < Formula
   desc "POSIX *nix fetch script using Nerdfonts"
   homepage "https://github.com/ThatOneCalculator/NerdFetch"
-  url "https://github.com/ThatOneCalculator/NerdFetch/archive/refs/tags/v8.3.0.tar.gz"
-  sha256 "7450609658f603f38a3d6fc1b8f2e1f95724144cbcf042d2062e55dd57b202af"
+  url "https://github.com/ThatOneCalculator/NerdFetch/archive/refs/tags/v8.3.1.tar.gz"
+  sha256 "07722131df254910b0ff336350b87341bc0413b18a9a197543bf2c97ee634c88"
   license "MIT"
   head "https://github.com/ThatOneCalculator/NerdFetch.git", branch: "main"
 
