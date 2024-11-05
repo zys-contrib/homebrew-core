2a05f8005cac8aeb3df09e9c2aeacd7e9516e0c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-05 00:53:23 +0000

    nanoflann 1.6.2

--- Formula/n/nanoflann.rb
+++ Formula/n/nanoflann.rb
@@ -1,8 +1,8 @@
 class Nanoflann < Formula
   desc "Header-only library for Nearest Neighbor search with KD-trees"
   homepage "https://github.com/jlblancoc/nanoflann"
-  url "https://github.com/jlblancoc/nanoflann/archive/refs/tags/v1.6.1.tar.gz"
-  sha256 "e258d6fd6ad18e1809fa9c081553e78036fd6e7b418d3762875c2c5a015dd431"
+  url "https://github.com/jlblancoc/nanoflann/archive/refs/tags/v1.6.2.tar.gz"
+  sha256 "c1b8f2e4d32c040249dad14a89dd03c5106a8c56f3e9ca4e60a0836a59259c0c"
   license "BSD-3-Clause"
   head "https://github.com/jlblancoc/nanoflann.git", branch: "master"
 
