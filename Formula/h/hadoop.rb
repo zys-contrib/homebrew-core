e626b2f16d7c62434b6db9e49b33b2b4b9af4eea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-05 19:42:56 +0000

    hadoop: update 3.4.1 bottle.

--- Formula/h/hadoop.rb
+++ Formula/h/hadoop.rb
@@ -12,14 +12,12 @@ class Hadoop < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "947ff1f70270d1f4028678211dfdf773be119bafcbbf15e4a7f24fe90996fb0f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
-    sha256 cellar: :any_skip_relocation, sonoma:         "090c88b44a952701c87d753f376e9465571bfdd4fcb4de38c245e217964574da"
-    sha256 cellar: :any_skip_relocation, ventura:        "090c88b44a952701c87d753f376e9465571bfdd4fcb4de38c245e217964574da"
-    sha256 cellar: :any_skip_relocation, monterey:       "090c88b44a952701c87d753f376e9465571bfdd4fcb4de38c245e217964574da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3bc4d1af76f0d40746abffa84b42508f1ef53eda3d612b03e5c1f4144bb4826a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f85402488931c97be6c4c8eb13b1c802a417aa79cef03b59a6b294a51c34f261"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f85402488931c97be6c4c8eb13b1c802a417aa79cef03b59a6b294a51c34f261"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f85402488931c97be6c4c8eb13b1c802a417aa79cef03b59a6b294a51c34f261"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a778cfa6e5a611c84fc5ff42e00ad5d568ef7328c41c74a69455b008efcd5221"
+    sha256 cellar: :any_skip_relocation, ventura:       "a778cfa6e5a611c84fc5ff42e00ad5d568ef7328c41c74a69455b008efcd5221"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f85402488931c97be6c4c8eb13b1c802a417aa79cef03b59a6b294a51c34f261"
   end
 
   # WARNING: Check https://cwiki.apache.org/confluence/display/HADOOP/Hadoop+Java+Versions before updating JDK version
