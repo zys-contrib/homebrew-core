d9e7d5602e27d44462d92a9f88bb17a22da4959f
Michael Cho <michael@michaelcho.dev>
2024-11-05 16:14:46 -0500

    libzdb: switch to `mariadb-connector-c`

--- Formula/lib/libzdb.rb
+++ Formula/lib/libzdb.rb
@@ -4,7 +4,7 @@ class Libzdb < Formula
   url "https://tildeslash.com/libzdb/dist/libzdb-3.2.3.tar.gz"
   sha256 "a1957826fab7725484fc5b74780a6a7d0d8b7f5e2e54d26e106b399e0a86beb0"
   license "GPL-3.0-only"
-  revision 5
+  revision 6
 
   livecheck do
     url :homepage
@@ -23,15 +23,10 @@ class Libzdb < Formula
 
   depends_on "libpq"
   depends_on macos: :high_sierra # C++ 17 is required
-  depends_on "mysql-client"
+  depends_on "mariadb-connector-c"
   depends_on "sqlite"
 
-  patch :DATA # Fix build error my mysql-client 8.3.0 https://bitbucket.org/tildeslash/libzdb/issues/67/build-error-with-mysql-83
-
   def install
-    # Reduce linkage on macOS from `mysql-client`
-    ENV.append "LDFLAGS", "-Wl,-dead_strip_dylibs" if OS.mac?
-
     system "./configure", "--disable-silent-rules", "--enable-sqliteunlock", *std_configure_args
     system "make", "install"
     (pkgshare/"test").install Dir["test/*.{c,cpp}"]
@@ -45,22 +40,3 @@ class Libzdb < Formula
     end
   end
 end
-
-__END__
-diff --git a/src/db/mysql/MysqlConnection.c b/src/db/mysql/MysqlConnection.c
-index 45ae896..7b6c1e3 100644
---- a/src/db/mysql/MysqlConnection.c
-+++ b/src/db/mysql/MysqlConnection.c
-@@ -96,8 +96,10 @@ static MYSQL *_doConnect(Connection_T delegator, char **error) {
-         // Options
-         if (IS(URL_getParameter(url, "compress"), "true"))
-                 clientFlags |= CLIENT_COMPRESS;
--        if (IS(URL_getParameter(url, "use-ssl"), "true"))
--                mysql_ssl_set(db, 0,0,0,0,0);
-+        if (IS(URL_getParameter(url, "use-ssl"), "true")) {
-+                enum mysql_ssl_mode ssl_mode = SSL_MODE_REQUIRED;
-+                mysql_options(db, MYSQL_OPT_SSL_MODE, &ssl_mode);
-+        }
- #if MYSQL_VERSION_ID < 80000
-         if (IS(URL_getParameter(url, "secure-auth"), "true"))
-                 mysql_options(db, MYSQL_SECURE_AUTH, (const char*)&yes);
