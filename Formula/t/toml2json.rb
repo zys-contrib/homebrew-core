f84a669b7a7e7ebfebbe26c8eae8952654840d05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-06 09:56:56 +0000

    toml2json: add 1.3.1 bottle.

--- Formula/t/toml2json.rb
+++ Formula/t/toml2json.rb
@@ -6,6 +6,15 @@ class Toml2json < Formula
   license "MIT"
   head "https://github.com/woodruffw/toml2json.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c236bae5e89882b86de4a4432cd45c265e52b120cfb92f5df4668a7d5d5ae28e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e916b159e93d256b232b7e05b9c70fd8efed7fcf90ba0e54f8fa1d47660fdc96"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c41859623992135a393b49977c66b3defdcaf597c025ebcbb2e5ce2a1a59b220"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0b63f1c3d4f53869cd7178d1150d47e24af6c4f2075e0a128a2ee180e2696d38"
+    sha256 cellar: :any_skip_relocation, ventura:       "263ada74ecbaadf923978f9018950a6d361fe16d16f08033dcdea6b86bd6718c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ef85b1667b92098289aa2bc8a6c2747188350d052a699b780909aeeae6455db"
+  end
+
   depends_on "rust" => :build
 
   def install
