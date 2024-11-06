bb5f526c4ccab11aa5c764594abe00f8fdf9fb5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-06 18:38:14 +0000

    firefoxpwa 2.13.1

--- Formula/f/firefoxpwa.rb
+++ Formula/f/firefoxpwa.rb
@@ -1,8 +1,8 @@
 class Firefoxpwa < Formula
   desc "Tool to install, manage and use Progressive Web Apps in Mozilla Firefox"
   homepage "https://pwasforfirefox.filips.si/"
-  url "https://github.com/filips123/PWAsForFirefox/archive/refs/tags/v2.12.5.tar.gz"
-  sha256 "97f6efb5a43a3fe32f09b0ae7b8669f755d6df7f7bff531f3cd64b392358a4d8"
+  url "https://github.com/filips123/PWAsForFirefox/archive/refs/tags/v2.13.1.tar.gz"
+  sha256 "d0af7a8b97555ed46cc6eb8f971e520149c9a4b520f0c0364b67530d0cccd8a1"
   license "MPL-2.0"
   head "https://github.com/filips123/PWAsForFirefox.git", branch: "main"
 
