2b78cbbaddcb6a0b93b6a4b660a037da02f95f31
Štefan Baebler <stefan.baebler@gmail.com>
2024-11-06 19:00:15 +0100

    osmcoastline: revision bump (gdal 3.10.0)

--- Formula/o/osmcoastline.rb
+++ Formula/o/osmcoastline.rb
@@ -4,7 +4,7 @@ class Osmcoastline < Formula
   url "https://github.com/osmcode/osmcoastline/archive/refs/tags/v2.4.0.tar.gz"
   sha256 "2c1a28313ed19d6e2fb1cb01cde8f4f44ece378393993b0059f447c5fce11f50"
   license "GPL-3.0-or-later"
-  revision 6
+  revision 7
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia:  "217a446421753f6779411129b48f94d6f7ac323a9485428adc89ca27636b0e52"
