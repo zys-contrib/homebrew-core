c72f265e1c52a5322d1efdc0571b25d492501bf5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-07 15:09:54 +0000

    libtatsu: add 1.0.4 bottle.

--- Formula/lib/libtatsu.rb
+++ Formula/lib/libtatsu.rb
@@ -6,6 +6,15 @@ class Libtatsu < Formula
   license "LGPL-2.1-or-later"
   head "https://github.com/libimobiledevice/libtatsu.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "3490caa14c81cdffce28d50ce34880ab9b95a193a26906bf035492e3c37a3b2d"
+    sha256 cellar: :any,                 arm64_sonoma:  "870c73a45691c13d672235e9a3ed53d0a9d76a7f1b9aafda775b541c1735c3d1"
+    sha256 cellar: :any,                 arm64_ventura: "4a33d3d2692d953e3bd1c0a8561011152ab9dcb5d029f0bbc33315233337fe52"
+    sha256 cellar: :any,                 sonoma:        "fafd0d67b5ccd44ee5c33fd4ba38f063029a89582a21e5a88104f00638bc620f"
+    sha256 cellar: :any,                 ventura:       "70223395b5fb5525f701e78c40b9b5b4c9f897fec98bee2dff4af63612440da4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dd96d83e2a85b4c479893f9493d0ffb9d7f66d357216d08adb6a4a158f72a582"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
