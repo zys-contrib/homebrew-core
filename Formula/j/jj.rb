5d53d135da0eb069b5e2b7a2600f781656a4538d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-07 06:38:12 +0000

    jj 0.23.0

--- Formula/j/jj.rb
+++ Formula/j/jj.rb
@@ -1,8 +1,8 @@
 class Jj < Formula
   desc "Git-compatible distributed version control system"
   homepage "https://github.com/martinvonz/jj"
-  url "https://github.com/martinvonz/jj/archive/refs/tags/v0.22.0.tar.gz"
-  sha256 "ed49b1c01ee6086bb782a465a4437e2f1b66f43bcf39c231df2b261091ab114b"
+  url "https://github.com/martinvonz/jj/archive/refs/tags/v0.23.0.tar.gz"
+  sha256 "18e0cc5600c06e940defce0a23ab4b29885bac265f94176603e5f8f926ed000e"
   license "Apache-2.0"
   head "https://github.com/martinvonz/jj.git", branch: "main"
 
