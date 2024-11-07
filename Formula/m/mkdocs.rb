050e2b728dfc8d673ac30402ff4e1204fa652674
CJ Steiner <clintonsteiner@gmail.com>
2024-11-07 16:06:49 -0600

    mkdocs: add conflict with mkdocs-material

--- Formula/m/mkdocs.rb
+++ Formula/m/mkdocs.rb
@@ -20,6 +20,8 @@ class Mkdocs < Formula
   depends_on "libyaml"
   depends_on "python@3.13"
 
+  conflicts_with "mkdocs-material", because: "both install `mkdocs` binaries"
+
   resource "click" do
     url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
     sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
