30894b2945f397368a1e6d1eb9c77a3352cf7a97
Rui Chen <rui@chenrui.dev>
2024-11-08 18:04:24 -0500

    ampl-mp 4.0.0
    ampl-mp: remove build patches
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    autobump: add ampl-mp
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ampl-mp: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/ampl-mp.rb
+++ Formula/a/ampl-mp.rb
@@ -1,10 +1,9 @@
 class AmplMp < Formula
   desc "Open-source library for mathematical programming"
   homepage "https://www.ampl.com/"
-  url "https://github.com/ampl/mp/archive/refs/tags/3.1.0.tar.gz"
-  sha256 "587c1a88f4c8f57bef95b58a8586956145417c8039f59b1758365ccc5a309ae9"
+  url "https://github.com/ampl/mp/archive/refs/tags/v4.0.0.tar.gz"
+  sha256 "9ac4b03dd03285cfcf998d81b53410611dd3ba0515463b70980965ec51e29f0f"
   license "MIT"
-  revision 3
 
   livecheck do
     url :stable
@@ -23,49 +22,33 @@ class AmplMp < Formula
 
   depends_on "cmake" => :build
 
-  resource "miniampl" do
-    url "https://github.com/dpo/miniampl/archive/refs/tags/v1.0.tar.gz"
-    sha256 "b836dbf1208426f4bd93d6d79d632c6f5619054279ac33453825e036a915c675"
-  end
-
-  # Removes Google Benchmark - as already done so upstream
-  # All it did was conflict with the google-benchmark formula
-  patch do
-    url "https://github.com/ampl/mp/commit/96e332bb8cb7ba925e3ac947d6df515496027eed.patch?full_index=1"
-    sha256 "1a4ef4cd1f4e8b959c20518f8f00994ef577e74e05824b2d1b241b1c3c1f84eb"
-  end
-
-  # Install missing header files, remove in > 3.1.0
-  # https://github.com/ampl/mp/issues/110
-  patch do
-    url "https://github.com/ampl/mp/commit/8183be3e486d38d281e0c5a02a1ea4239695035e.patch?full_index=1"
-    sha256 "6b37201f1d0d6dba591e7e1b81fb16d2694d118605c92c422dcdaaedb463c367"
-  end
-
-  # Backport fmt header update. Remove in the next release
-  # https://github.com/ampl/mp/issues/108
-  patch do
-    url "https://github.com/ampl/mp/commit/1f39801af085656e4bf72250356a3a70d5d98e73.patch?full_index=1"
-    sha256 "b0e0185f24caba54cb38b65a638ebda6eb4db3e8c74d71ca79f072b8337e8e2c"
-  end
-
   def install
-    system "cmake", "-S", ".", "-B", "build",
-                    "-DAMPL_LIBRARY_DIR=#{libexec}/bin",
-                    "-DBUILD_SHARED_LIBS=ON",
-                    "-DCMAKE_INSTALL_RPATH=#{rpath};#{rpath(source: libexec/"bin")}",
-                    *std_cmake_args
+    args = %W[
+      -DAMPL_LIBRARY_DIR=#{libexec}/bin
+      -DBUILD_SHARED_LIBS=ON
+      -DBUILD_TESTS=OFF
+      -DCMAKE_INSTALL_RPATH=#{rpath};#{rpath(source: libexec/"bin")}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
   test do
-    resource("miniampl").stage do
-      testpath.install "src/miniampl.c", Dir["examples/wb.*"]
-    end
+    (testpath/"test.c").write <<~C
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include "mp/ampls-c-api.h"
 
-    system ENV.cc, "miniampl.c", "-std=c99", "-I#{include}/asl", "-L#{lib}", "-lasl", "-lmp"
-    output = shell_output("./a.out wb showname=1 showgrad=1")
-    assert_match "Objective name: objective", output
+      int main() {
+          AMPLS_MP_Solver* solver = (AMPLS_MP_Solver*)malloc(sizeof(AMPLS_MP_Solver));
+          free(solver);
+          return 0;
+      }
+    C
+
+    system ENV.cc, "test.c", "-I#{include}/mp", "-L#{lib}", "-lmp", "-o", "test"
+    system "./test"
   end
 end
