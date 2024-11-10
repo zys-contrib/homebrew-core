fe59eae168b5db2a4a2c70242c3c3b35806af4da
Michael Cho <20700669+cho-m@users.noreply.github.com>
2024-11-10 13:46:17 +0000

    python-tk@3.11: update 3.11.10_1 bottle.

--- Formula/p/python-tk@3.11.rb
+++ Formula/p/python-tk@3.11.rb
@@ -11,14 +11,12 @@ class PythonTkAT311 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia:  "bd4c45eefc752d5eaf5c4cdf7743f01e1246fee55877e799ec5084e19da0358e"
-    sha256 cellar: :any,                 arm64_sonoma:   "9287934719a90c587a8f9f5f1ce573b981c04ff4157b34659bd31d9e2ef20a54"
-    sha256 cellar: :any,                 arm64_ventura:  "2c19ff182ef75d024aded65fd95c0d9f670d849577297cb882d98b0b2851fa6a"
-    sha256 cellar: :any,                 arm64_monterey: "4bb989fa2b025dbf2d8a3a582ffca9ee8fc5b18dff05a73af5d26832f2f54b62"
-    sha256 cellar: :any,                 sonoma:         "168e04abd04bee52b6466e5f56eeaf4bd7565b5a20008d2d698f46b4340d0b10"
-    sha256 cellar: :any,                 ventura:        "2fba5988208e5ac77365e46ce4be73e8ffe43fdf1279c221a6688fd8d4f036e4"
-    sha256 cellar: :any,                 monterey:       "6ccfb528437d9888e01be38a7108c93ed35e6b08a5fd88d313a5a23857abde65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b20e3064ca7d36a64c665962d4e866d9f8bdb3e611617fcd6bdd7d5043c43b81"
+    sha256 cellar: :any,                 arm64_sequoia: "92778211497ed2841346c7e7f1916c6096e29e7c648073a1abe70dde5a36621a"
+    sha256 cellar: :any,                 arm64_sonoma:  "e37be25a37436d98d32d2900dee5904df56e383d0110ea7292b6c3beaa7def3b"
+    sha256 cellar: :any,                 arm64_ventura: "ba50ca97d3d6e3c64636d048283c391a8348924dc27156ad4da53c1d98a0ccd6"
+    sha256 cellar: :any,                 sonoma:        "f4d2305f37b5f17b165dd3ad63ee695e692505cfc367c64623715365fba1c387"
+    sha256 cellar: :any,                 ventura:       "d807dc0c3a10262b9ad4a74798640efbfda9876396b2f1ce8da2ad2917b714bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8595687a22fd4fafdf76edca3ecc710dad371864a0ec4f1cc303d5e49a2a870"
   end
 
   depends_on "python@3.11"
