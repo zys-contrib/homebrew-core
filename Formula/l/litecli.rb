093d8039bdc25f38be7f906c885b9e426f269eed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-11 18:43:08 +0000

    litecli 1.12.4

--- Formula/l/litecli.rb
+++ Formula/l/litecli.rb
@@ -3,10 +3,9 @@ class Litecli < Formula
 
   desc "CLI for SQLite Databases with auto-completion and syntax highlighting"
   homepage "https://github.com/dbcli/litecli"
-  url "https://files.pythonhosted.org/packages/4d/1d/4e033f1f9777c734d1fa6503b73c486f4bd8b574dbec47ba03ababb62fab/litecli-1.12.3.tar.gz"
-  sha256 "dd707c1bc04047aa6dba1916d204be47003aa383cf638bbe0f1d70bc6571d681"
+  url "https://files.pythonhosted.org/packages/97/9c/3a2ab329a5349aa41c01ff01f944309d3d43b9d77c422fee26466ffe2800/litecli-1.12.4.tar.gz"
+  sha256 "53f7fa067acdb2714dfaa2145ed27937b14d0751c781c2d09e80e5d848e05602"
   license "BSD-3-Clause"
-  revision 1
 
   bottle do
     rebuild 1
