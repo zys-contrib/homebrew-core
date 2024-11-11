5f69e35b3f16732e27779f1d25e8858608e8e1e2
Rui Chen <rui@chenrui.dev>
2024-11-11 16:14:36 +0000

    typos-lsp: add 0.1.30 bottle.

--- Formula/t/typos-lsp.rb
+++ Formula/t/typos-lsp.rb
@@ -5,6 +5,15 @@ class TyposLsp < Formula
   sha256 "176db7e46c135158d385092547d1cb999de7420fb8b2e0b7a491ca67af6faa56"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9732e4320c0cfee13fbc845ddfe382efd60c06eba5645fb74f49c6d35badec83"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5c84aaa3ce4889d53a139a093aac178deb71a4816d983c9a38c0d5a63c65f793"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "bae43f7eaee667284571dcb599202f3682d9625fb23f1c75177939a0b634a805"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7a858477adfabd87f9414ac76b5905e1703689a218930f8d9fd64d4f13625834"
+    sha256 cellar: :any_skip_relocation, ventura:       "a9cd5d73df7dfbd832f99ae6da110ee3749d777a755fc56a0b83cdb92078624f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f00f6af3c783f468abf28a640678f17da966c815595de1d4225dc9db62857f36"
+  end
+
   depends_on "rust" => :build
 
   def install
