ae73c31b1da647659c8075330af955afc92ea39b
Carlo Cabrera <github@carlo.cab>
2024-11-12 17:07:18 +0800

    tree-sitter: backport patch to fix Neovim freezing in some configurations
    Neovim can freeze if you have some plugins enabled. This is a regression
    in v0.24.4.
    
    We can fix that by backporting tree-sitter/tree-sitter#3898.
    
    See neovim/neovim#31163 and tree-sitter/tree-sitter#3930.

--- Formula/t/tree-sitter.rb
+++ Formula/t/tree-sitter.rb
@@ -23,6 +23,24 @@ class TreeSitter < Formula
   depends_on "rust" => :build
   depends_on "node" => :test
 
+  # The next three patches fix Neovim freezing in some configurations.
+  # See: https://github.com/neovim/neovim/issues/31163
+  #      https://github.com/tree-sitter/tree-sitter/issues/3930
+  #      https://github.com/tree-sitter/tree-sitter/pull/3898
+  # Remove in next release.
+  patch do
+    url "https://github.com/tree-sitter/tree-sitter/commit/ced69d59da9537abef13dbe027ee3a4a20e0ab77.patch?full_index=1"
+    sha256 "f8440176a9354066bbe5e6eca535f7645ac9110d68f71cb903eb1b1d9f561773"
+  end
+  patch do
+    url "https://github.com/tree-sitter/tree-sitter/commit/e6cb7f3f61cc99b78892a23ceb2cb2e8dde40a49.patch?full_index=1"
+    sha256 "c84bda1fa49a88d41306d4fce6f128329c942403d16f1b75d7d4c2305c097eaa"
+  end
+  patch do
+    url "https://github.com/tree-sitter/tree-sitter/commit/455aa0d9b2d97514079783c7b7de718c0a148c5c.patch?full_index=1"
+    sha256 "ce1cc2ebbad714c76fc12e74bb72a04f95dcccd474f4af1ef5ef23976624110c"
+  end
+
   def install
     system "make", "install", "AMALGAMATED=1", "PREFIX=#{prefix}"
     system "cargo", "install", *std_cargo_args(path: "cli")
