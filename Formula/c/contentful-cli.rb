d0f777bb9b789eefd5e06103abf94990df103100
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-13 00:40:25 +0000

    contentful-cli 3.5.4

--- Formula/c/contentful-cli.rb
+++ Formula/c/contentful-cli.rb
@@ -1,8 +1,8 @@
 class ContentfulCli < Formula
   desc "Contentful command-line tools"
   homepage "https://github.com/contentful/contentful-cli"
-  url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-3.5.3.tgz"
-  sha256 "893aa859ade8be01881b881fdd0058e802b6221805f465dafbdad1d1f4364361"
+  url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-3.5.4.tgz"
+  sha256 "5626345775f1a9d48304931f958b8419ad855de4ffeb2fc76dfa8ad2759b5772"
   license "MIT"
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
