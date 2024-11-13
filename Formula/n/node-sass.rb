b80f1f44e953038ae4aff29713e0aa62941bec38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-13 01:22:46 +0000

    node-sass: update 1.80.7 bottle.

--- Formula/n/node-sass.rb
+++ Formula/n/node-sass.rb
@@ -6,12 +6,12 @@ class NodeSass < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3fadfc4b4332daa007b24acc7ddc8cc0b21847dd5a5fa810032ce9fc5c8354c5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3fadfc4b4332daa007b24acc7ddc8cc0b21847dd5a5fa810032ce9fc5c8354c5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3fadfc4b4332daa007b24acc7ddc8cc0b21847dd5a5fa810032ce9fc5c8354c5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7b28a760140153e0b57a222ad4b7a313bfb37afab9ebd6ea9abca41cb7e0da19"
-    sha256 cellar: :any_skip_relocation, ventura:       "7b28a760140153e0b57a222ad4b7a313bfb37afab9ebd6ea9abca41cb7e0da19"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a1f1ce67ee5f67dfe39a363ce73680bb165a094af0546d288879cff1a75d460"
+    sha256                               arm64_sequoia: "051750b69307b154e8effd99a2554a7edbabd487b4f9e3b7dc6f2f143c628c47"
+    sha256                               arm64_sonoma:  "70c869d58403fabcf7d16d611b447ac7616f0537df911e94dd05b19725a66559"
+    sha256                               arm64_ventura: "f3fb691ca9f7b5a1d5f2d991b6e89d84b5fbbca36d2d7b07697d65d3d6050b85"
+    sha256                               sonoma:        "f6f9ef82ebf16c4ed29fd98a693b39abf0cd6b1be6eeba9196d9b5a8f5d8c9c7"
+    sha256                               ventura:       "607d9ba35709b45f950f9fa795e310e247789a10e603f1c4fa9362f575687893"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89bb530dceccd5e48957fbbcfb1cb2a6c44003bf2ef231cdad2f887e03edb191"
   end
 
   depends_on "node"
