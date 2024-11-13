c2055f94939f76257171a62ab76dcdaf1afffbcd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-13 22:09:07 +0000

    lando-cli: update 3.23.7 bottle.

--- Formula/l/lando-cli.rb
+++ Formula/l/lando-cli.rb
@@ -12,12 +12,12 @@ class LandoCli < Formula
   end
 
   bottle do
-    sha256                               arm64_sequoia: "7dc9a19e5deef5bcf4a8b6d4f6c4d629de3dc129409bb5bebdfd024bdc11a6e6"
-    sha256                               arm64_sonoma:  "c8d639964dfb5460bc464fcfebb809b6edaebb1e695156d4d2e11879c5ef9a05"
-    sha256                               arm64_ventura: "0356a8fe9e4c3a8c62ffca21093bd855c0ce1669837850c7e18e6932f313ae24"
-    sha256                               sonoma:        "1d7030c5ccb046aca8f946ed5da210d9886bdae891b92e783c1287f5c2c2e076"
-    sha256                               ventura:       "5794a6389ca8083b3c0c39567afefa1bd4684ec80730c5bb8e5e5b99e473d1f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3fa9dc14244d7b257665d1d66407576385fa0cc1aa8b74a8302a95ef82efc296"
+    sha256                               arm64_sequoia: "1886da7f0dfbba801f2d1c44dc50e100cf5907ac5d184d0fda4a90f381f05b62"
+    sha256                               arm64_sonoma:  "57f955383a45af74d96bb7f2d60a25df4f0e5ea3b182ce14f05e07f116a93f44"
+    sha256                               arm64_ventura: "b1b35a50cd87075e1d040792f12e011ad2f41bb3a2df35293e7614f230b5dd87"
+    sha256                               sonoma:        "cd0f227c21dd214eb55f40f725ca2db73762ef2ef4dd489687387e87fba4d349"
+    sha256                               ventura:       "684af73247b50e9b4280fafc551007badad5173333c9a02edd48e97387ae769a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97c1a18e40beac6462b86349785045f99ddbe47a27fff639923f937968684c49"
   end
 
   depends_on "node"
