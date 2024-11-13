b48037d81db4e1ab85b28a6443d565ac2ffdfe32
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-11-14 02:05:38 +0800

    libxml2: fix build when `pkg-config` is present
    When `pkg-config` is present during build, checks for ICU libraries do
    not work correctly, causing the build to fail. See [1], [2] for more
    details. Let's apply the patch that fixes this issue.
    
    Also, use `pkg-config` for stable builds too. When `pkg-config` is
    missing, build falls back to using `icu-config` for ICU checks, but
    that is deprecated upstream [3].
    
    In addition, avoid hard-coding cellar paths that could invalidate
    dependents' pkg-config files after version bumps.
    
    [1]: https://github.com/orgs/Homebrew/discussions/5699#discussioncomment-11243333
    [2]: https://gitlab.gnome.org/GNOME/libxml2/-/merge_requests/289
    [3]: https://unicode-org.github.io/icu/userguide/icu/howtouseicu.html#notes-on-icu-config

--- Formula/lib/libxml2.rb
+++ Formula/lib/libxml2.rb
@@ -1,9 +1,23 @@
 class Libxml2 < Formula
   desc "GNOME XML library"
   homepage "http://xmlsoft.org/"
-  url "https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.5.tar.xz"
-  sha256 "74fc163217a3964257d3be39af943e08861263c4231f9ef5b496b6f6d4c7b2b6"
   license "MIT"
+
+  stable do
+    url "https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.5.tar.xz"
+    sha256 "74fc163217a3964257d3be39af943e08861263c4231f9ef5b496b6f6d4c7b2b6"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+
+    # Fix pkg-config checks for libicuuc. Patch taken from:
+    # https://gitlab.gnome.org/GNOME/libxml2/-/commit/b57e022d75425ef8b617a1c3153198ee0a941da8
+    # When the patch is no longer needed, remove along with the `stable` block
+    # and the autotools dependencies above. Also uncomment `if build.head?`
+    # condition in the `install` block.
+    patch :DATA
+  end
 
   # We use a common regex because libxml2 doesn't use GNOME's "even-numbered
   # minor is stable" version scheme.
@@ -27,19 +41,23 @@ class Libxml2 < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on "pkg-config" => :build
   end
 
   keg_only :provided_by_macos
 
+  depends_on "pkg-config" => [:build, :test]
   depends_on "python-setuptools" => :build
   depends_on "python@3.12" => [:build, :test]
   depends_on "python@3.13" => [:build, :test]
-  depends_on "pkg-config" => :test
   depends_on "icu4c@76"
   depends_on "readline"
 
   uses_from_macos "zlib"
+
+  def icu4c
+    deps.find { |dep| dep.name.match?(/^icu4c(@\d+)?$/) }
+        .to_formula
+  end
 
   def pythons
     deps.map(&:to_formula)
@@ -54,7 +72,7 @@ class Libxml2 < Formula
     # nanohttp.c:1019:42: error: invalid use of undefined type 'struct addrinfo'
     ENV.append "CFLAGS", "-std=gnu11" if OS.linux?
 
-    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "autoreconf", "--force", "--install", "--verbose" # if build.head?
     system "./configure", "--disable-silent-rules",
                           "--sysconfdir=#{etc}",
                           "--with-history",
@@ -66,9 +84,19 @@ class Libxml2 < Formula
                           *std_configure_args
     system "make", "install"
 
-    icu4c = deps.find { |dep| dep.name.match?(/^icu4c(@\d+)?$/) }
-                .to_formula
-    inreplace [bin/"xml2-config", lib/"pkgconfig/libxml-2.0.pc"], icu4c.prefix.realpath, icu4c.opt_prefix
+    inreplace [bin/"xml2-config", lib/"pkgconfig/libxml-2.0.pc"] do |s|
+      s.gsub! prefix, opt_prefix
+      s.gsub! icu4c.prefix.realpath, icu4c.opt_prefix, audit_result: false
+    end
+
+    # `icu4c` is keg-only, so we need to tell `pkg-config` where to find its
+    # modules.
+    if OS.mac?
+      icu_uc_pc = icu4c.opt_lib/"pkgconfig/icu-uc.pc"
+      inreplace lib/"pkgconfig/libxml-2.0.pc",
+                /^Requires\.private:(.*)\bicu-uc\b(.*)$/,
+                "Requires.private:\\1#{icu_uc_pc}\\2"
+    end
 
     sdk_include = if OS.mac?
       sdk = MacOS.sdk_path_if_needed
@@ -124,5 +152,30 @@ class Libxml2 < Formula
         system python, "-c", "import libxml2"
       end
     end
+
+    # Make sure cellar paths are not baked into these files.
+    [bin/"xml2-config", lib/"pkgconfig/libxml-2.0.pc"].each do |file|
+      refute_match HOMEBREW_CELLAR.to_s, file.read
+    end
   end
 end
+
+__END__
+diff --git a/configure.ac b/configure.ac
+index c6dc93d58f84f21c4528753d2ee1bc1d50e67ced..e7bad24d8f1aa7659e1aa4e2ad1986cc2167483b 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -984,10 +984,10 @@ if test "$with_icu" != "no" && test "$with_icu" != "" ; then
+
+     # Try pkg-config first so that static linking works.
+     # If this succeeeds, we ignore the WITH_ICU directory.
+-    PKG_CHECK_MODULES([ICU], [icu-i18n], [
+-        WITH_ICU=1; XML_PC_REQUIRES="${XML_PC_REQUIRES} icu-i18n"
++    PKG_CHECK_MODULES([ICU], [icu-uc], [
++        WITH_ICU=1; XML_PC_REQUIRES="${XML_PC_REQUIRES} icu-uc"
+         m4_ifdef([PKG_CHECK_VAR],
+-            [PKG_CHECK_VAR([ICU_DEFS], [icu-i18n], [DEFS])])
++            [PKG_CHECK_VAR([ICU_DEFS], [icu-uc], [DEFS])])
+         if test "x$ICU_DEFS" != "x"; then
+             ICU_CFLAGS="$ICU_CFLAGS $ICU_DEFS"
+         fi],[:])
