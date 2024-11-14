d24ee38a63288acdf7420ddc018e1d6183b5992e
Rui Chen <rui@chenrui.dev>
2024-11-14 13:28:09 +0000

    bibtex-tidy: add 1.5.0 bottle.

--- Formula/b/bibtex-tidy.rb
+++ Formula/b/bibtex-tidy.rb
@@ -5,6 +5,10 @@ class BibtexTidy < Formula
   sha256 "8d8d40351193f2b447a22fa04d8490da32bcc285694b3a169a8a8af57e231823"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "3750c55681a8fd87a5c4dcd876ac66c9b88b7b80ca9bc8d2f0deb1c2bed03f8e"
+  end
+
   depends_on "node"
 
   def install
