c0c88b0de19953c8042a571cd48581bb5fd9867e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-14 01:22:35 +0000

    util-macros 1.20.2

--- Formula/u/util-macros.rb
+++ Formula/u/util-macros.rb
@@ -1,8 +1,8 @@
 class UtilMacros < Formula
   desc "X.Org: Set of autoconf macros used to build other xorg packages"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/util/util-macros-1.20.1.tar.xz"
-  sha256 "0b308f62dce78ac0f4d9de6888234bf170f276b64ac7c96e99779bb4319bcef5"
+  url "https://www.x.org/archive/individual/util/util-macros-1.20.2.tar.xz"
+  sha256 "9ac269eba24f672d7d7b3574e4be5f333d13f04a7712303b1821b2a51ac82e8e"
   license "MIT"
 
   bottle do
