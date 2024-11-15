047a5dea6c5d165c43e6f48a47a0f46d00a04066
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-15 13:04:54 +0000

    ktor: update 0.3.0 bottle.

--- Formula/k/ktor.rb
+++ Formula/k/ktor.rb
@@ -7,12 +7,12 @@ class Ktor < Formula
   head "https://github.com/ktorio/ktor-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da21184d7653719644bd43e485f9a9fe5209bbd5760ee408123339fcb7d31235"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "da21184d7653719644bd43e485f9a9fe5209bbd5760ee408123339fcb7d31235"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "da21184d7653719644bd43e485f9a9fe5209bbd5760ee408123339fcb7d31235"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f2db82b992dcc990a985a350ec48691e463d6aceec3f92da717e8b93a1829290"
-    sha256 cellar: :any_skip_relocation, ventura:       "f2db82b992dcc990a985a350ec48691e463d6aceec3f92da717e8b93a1829290"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4dbabc68d684985930c294bdcc47ab59007dd4f7570191eaf17f2d879699ca7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc9d7a77698749a4f3794d4dc6554bfe340124fce946052c3a07590e37fd4428"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fc9d7a77698749a4f3794d4dc6554bfe340124fce946052c3a07590e37fd4428"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fc9d7a77698749a4f3794d4dc6554bfe340124fce946052c3a07590e37fd4428"
+    sha256 cellar: :any_skip_relocation, sonoma:        "89c03c35c64647ed36aac771b4f09eacbdee8566a2ea459aa8c6473515759b73"
+    sha256 cellar: :any_skip_relocation, ventura:       "89c03c35c64647ed36aac771b4f09eacbdee8566a2ea459aa8c6473515759b73"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f3a77be3b68e5095fd14eb239d7c439ee46f76ee341096f6136fb452dad4a69"
   end
 
   depends_on "go" => :build
