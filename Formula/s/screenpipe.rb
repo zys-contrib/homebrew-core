410eda467f47041f671576efa220b384e38e4d5b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-16 14:24:34 +0000

    screenpipe: add 0.1.98 bottle.

--- Formula/s/screenpipe.rb
+++ Formula/s/screenpipe.rb
@@ -5,6 +5,15 @@ class Screenpipe < Formula
   sha256 "cb3c8039ecb60d35bacd2b9673db112f907b4a1d3d7c32f49a5e77c0274268ad"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "0ab3cdb0a6676c440a02eebc342341017f95c0295073bf507cef513a61a704af"
+    sha256 cellar: :any,                 arm64_sonoma:  "441c211f4af82c9a34053905d7f1055e24e417d715b85aba03b4b82aa8a38262"
+    sha256 cellar: :any,                 arm64_ventura: "de4e7286e9a19ad32fc47aeefb60b424870171898bc9cd39bf70ef383ca0eb11"
+    sha256 cellar: :any,                 sonoma:        "474e316a3d35adb731bd5d053583ad74f11735e27b11cdc8d1d69ace46631c5d"
+    sha256 cellar: :any,                 ventura:       "0798f9f36e491aed26d598d744a06487d039eae71f91cb5113d6bdbe41272823"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b45dcfd02154031f502f7824477a6127e6e5730bb9db7cc918bbf75539e0dd2"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
   depends_on "ffmpeg"
