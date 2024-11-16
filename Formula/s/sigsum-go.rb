82c41866175f75654d324b1e795992dc3e76ad5f
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-11-16 11:05:51 +0000

    sigsum-go: add 0.9.1 bottle.

--- Formula/s/sigsum-go.rb
+++ Formula/s/sigsum-go.rb
@@ -5,6 +5,15 @@ class SigsumGo < Formula
   sha256 "68f3a66af6727fa1b44d7dd2429748b23b8767547750113318494c58d7b04ae8"
   license "BSD-2-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5a977129deb4d7146c9a4a5e8cc40cc414b1a05fa755f0c2c83f4e4028275f29"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5a977129deb4d7146c9a4a5e8cc40cc414b1a05fa755f0c2c83f4e4028275f29"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5a977129deb4d7146c9a4a5e8cc40cc414b1a05fa755f0c2c83f4e4028275f29"
+    sha256 cellar: :any_skip_relocation, sonoma:        "273c62d9c41f9cc2a0398928e08fcde450e00dbe88c319c5389831b9d231f167"
+    sha256 cellar: :any_skip_relocation, ventura:       "273c62d9c41f9cc2a0398928e08fcde450e00dbe88c319c5389831b9d231f167"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49ca2235c08e62ddc302f3d03a7aae581d400c6694f723265c173333fed62d33"
+  end
+
   depends_on "go" => :build
 
   def install
