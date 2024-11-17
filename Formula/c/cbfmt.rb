48a6d2ee2b6893631cf6fcd58726922836f81b6b
Rui Chen <rui@chenrui.dev>
2024-11-17 15:37:36 +0000

    cbfmt: add 0.2.0 bottle.

--- Formula/c/cbfmt.rb
+++ Formula/c/cbfmt.rb
@@ -5,6 +5,15 @@ class Cbfmt < Formula
   sha256 "42973e9b1e95f4f3d7e72ef17a41333dab1e04d3d91c7930aabfc08f72c14126"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "19d661d5df158a765e9d9103ffa76a63d069106a79da5e96425cb3f7860cc054"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9450f078799be6460d360bddede919f33f4afcf8480f951edadc5d93ad2b9887"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "cd536fee66c066b0027da4659d5b7e6ec316a98545fbc84b00a6833d919d2e78"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f40cb83e59d37c6826a8f99871de12db752fcc8f9830bc4857347ed733cd0b02"
+    sha256 cellar: :any_skip_relocation, ventura:       "d08bb1b815be4f880c84e38c49cf1bf7c14ba83670350fc7c77305bebc97faa5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bef8994fb32668715f46c8efcd584938cb5cdce740e65f98f9047698ee4e284f"
+  end
+
   depends_on "rust" => [:build, :test]
 
   def install
