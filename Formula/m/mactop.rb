a80e1aefb76167aca99cb20023552004ca40eb56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-17 00:59:24 +0000

    mactop 0.2.2

--- Formula/m/mactop.rb
+++ Formula/m/mactop.rb
@@ -1,8 +1,8 @@
 class Mactop < Formula
   desc "Apple Silicon Monitor Top written in Golang"
   homepage "https://github.com/context-labs/mactop"
-  url "https://github.com/context-labs/mactop/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "740b78a672ab3b38be9d581cb0f95af7b669e46da77a66085b4e107cc0c8bbe4"
+  url "https://github.com/context-labs/mactop/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "bbba285a00f7d2f92615bcb18adfc5d8ca0c94b801bfcf67e0084ad18882f4f9"
   license "MIT"
 
   bottle do
