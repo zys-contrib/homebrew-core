bab4e07bbb43323ded30d8917bfd470678e62cf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-18 21:28:51 +0000

    bee 1.104

--- Formula/b/bee.rb
+++ Formula/b/bee.rb
@@ -1,8 +1,8 @@
 class Bee < Formula
   desc "Tool for managing database changes"
   homepage "https://github.com/bluesoft/bee"
-  url "https://github.com/bluesoft/bee/releases/download/1.103/bee-1.103.zip"
-  sha256 "7b44f6994b4e658420044891922486d1ffcd96d7af27cf3a3b6cd2ca0ec8a599"
+  url "https://github.com/bluesoft/bee/releases/download/1.104/bee-1.104.zip"
+  sha256 "b0f1ab8daf944fcb80c85ec9939bf830dbb0aab9231c217e360a41ea012d12f2"
   license "MPL-1.1"
 
   bottle do
