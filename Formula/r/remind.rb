87af76b92561fc31f04149630ef4dad22c7b776f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-18 15:53:19 +0000

    remind 05.01.00

--- Formula/r/remind.rb
+++ Formula/r/remind.rb
@@ -1,8 +1,8 @@
 class Remind < Formula
   desc "Sophisticated calendar and alarm"
   homepage "https://dianne.skoll.ca/projects/remind/"
-  url "https://dianne.skoll.ca/projects/remind/download/remind-05.00.07.tar.gz"
-  sha256 "89ddf2572287452256866f2be1999cfe4eb501952dd706b7940ad092d0dbb7dc"
+  url "https://dianne.skoll.ca/projects/remind/download/remind-05.01.00.tar.gz"
+  sha256 "722a6fd4427c1bb1ae0af5ad66678afa4e6df45b03068ae4323e07144e1682e7"
   license "GPL-2.0-only"
   head "https://git.skoll.ca/Skollsoft-Public/Remind.git", branch: "master"
 
