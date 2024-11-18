4add1642cba366b9850cda6694245fc3f034e35e
Michael Cho <michael@michaelcho.dev>
2024-11-18 13:01:50 -0500

    source-highlight: revision bump (boost 1.87.0)

--- Formula/s/source-highlight.rb
+++ Formula/s/source-highlight.rb
@@ -5,7 +5,7 @@ class SourceHighlight < Formula
   mirror "https://ftpmirror.gnu.org/src-highlite/source-highlight-3.1.9.tar.gz"
   sha256 "3a7fd28378cb5416f8de2c9e77196ec915145d44e30ff4e0ee8beb3fe6211c91"
   license "GPL-3.0-or-later"
-  revision 5
+  revision 6
 
   livecheck do
     url :stable
