69017bc83dd6ea8447ac424932c6a40c63189882
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-18 02:23:50 +0000

    percona-server: update 8.0.36-28_2 bottle.

--- Formula/p/percona-server.rb
+++ Formula/p/percona-server.rb
@@ -19,12 +19,13 @@ class PerconaServer < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "f33ab3853ad5d77d832172a84bc26d7caff044417c9f4eb6aace70a4ad962a41"
-    sha256 arm64_sonoma:  "d3a25c59d673eb187a0de78311a22327632966e4b55ea7a929288d28e5c1a6a0"
-    sha256 arm64_ventura: "326320bf4026b8a1e01a615e71521b336b6ce7467e9c6efcea6961f2504d0815"
-    sha256 sonoma:        "44a4869d90907cf599832f67d59ab2b2719fbdd3a2efc4a8e4c38c8a573b35b4"
-    sha256 ventura:       "5d482f723ab038acaf8a355db7d464bbe74d1a50d84795627091ae758213f216"
-    sha256 x86_64_linux:  "28712e5fe869747e2a045981475ddb7c35ca138f8588510d68c241e946fb5805"
+    rebuild 1
+    sha256 arm64_sequoia: "29e0b962cb2f38106efe55c33b8110c5d531c88157ff52d8ce5657667f147e36"
+    sha256 arm64_sonoma:  "534428d3ac87a01e2ed1eda3ee30790c9f065dc89f24d1df9afbcc32c3fd3a23"
+    sha256 arm64_ventura: "8ed29556bfdd2c5f81d8b7414b6baba9d04ea19822b816de39f7c22fb37b9313"
+    sha256 sonoma:        "20abc25692b98473b4963e242c8cc38bcafd8c5aea4f0251dc6b783f8c756800"
+    sha256 ventura:       "abadd8d5152e2e72c8d8a5ad030bed067d3dcc6acad2919abe95fcbe1be8a728"
+    sha256 x86_64_linux:  "621e57fa5a0c7cc891a531caf7ec36ad1403dffc269ad6a3a33210419666ed83"
   end
 
   depends_on "bison" => :build
