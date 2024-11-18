4eb5b89118ca3b516091680157dc8b53ac83a81d
Michael Cho <michael@michaelcho.dev>
2024-11-17 22:16:44 -0500

    igraph: revision bump for `arpack` changes

--- Formula/i/igraph.rb
+++ Formula/i/igraph.rb
@@ -4,6 +4,7 @@ class Igraph < Formula
   url "https://github.com/igraph/igraph/releases/download/0.10.15/igraph-0.10.15.tar.gz"
   sha256 "03ba01db0544c4e32e51ab66f2356a034394533f61b4e14d769b9bbf5ad5e52c"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "bd56ca8e66713f2f94eb60d15ee8da571f3d1ce271f769fd9918245e1fc69548"
@@ -23,33 +24,31 @@ class Igraph < Formula
   uses_from_macos "libxml2"
 
   def install
-    mkdir "build" do
-      # explanation of extra options:
-      # * we want a shared library, not a static one
-      # * link-time optimization should be enabled if the compiler supports it
-      # * thread-local storage of global variables is enabled
-      # * force the usage of external dependencies from Homebrew where possible
-      # * GraphML support should be compiled in (needs libxml2)
-      # * BLAS and LAPACK should come from OpenBLAS
-      # * prevent the usage of ccache even if it is installed to ensure that we
-      #    have a clean build
-      system "cmake", "-G", "Unix Makefiles",
-                      "-DBUILD_SHARED_LIBS=ON",
-                      "-DIGRAPH_ENABLE_LTO=AUTO",
-                      "-DIGRAPH_ENABLE_TLS=ON",
-                      "-DIGRAPH_GLPK_SUPPORT=ON",
-                      "-DIGRAPH_GRAPHML_SUPPORT=ON",
-                      "-DIGRAPH_USE_INTERNAL_ARPACK=OFF",
-                      "-DIGRAPH_USE_INTERNAL_BLAS=OFF",
-                      "-DIGRAPH_USE_INTERNAL_GLPK=OFF",
-                      "-DIGRAPH_USE_INTERNAL_GMP=OFF",
-                      "-DIGRAPH_USE_INTERNAL_LAPACK=OFF",
-                      "-DBLA_VENDOR=OpenBLAS",
-                      "-DUSE_CCACHE=OFF",
-                      "..", *std_cmake_args
-      system "make"
-      system "make", "install"
-    end
+    # explanation of extra options:
+    # * we want a shared library, not a static one
+    # * link-time optimization should be enabled if the compiler supports it
+    # * thread-local storage of global variables is enabled
+    # * force the usage of external dependencies from Homebrew where possible
+    # * GraphML support should be compiled in (needs libxml2)
+    # * BLAS and LAPACK should come from OpenBLAS
+    # * prevent the usage of ccache even if it is installed to ensure that we
+    #    have a clean build
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DIGRAPH_ENABLE_LTO=AUTO",
+                    "-DIGRAPH_ENABLE_TLS=ON",
+                    "-DIGRAPH_GLPK_SUPPORT=ON",
+                    "-DIGRAPH_GRAPHML_SUPPORT=ON",
+                    "-DIGRAPH_USE_INTERNAL_ARPACK=OFF",
+                    "-DIGRAPH_USE_INTERNAL_BLAS=OFF",
+                    "-DIGRAPH_USE_INTERNAL_GLPK=OFF",
+                    "-DIGRAPH_USE_INTERNAL_GMP=OFF",
+                    "-DIGRAPH_USE_INTERNAL_LAPACK=OFF",
+                    "-DBLA_VENDOR=OpenBLAS",
+                    "-DUSE_CCACHE=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
