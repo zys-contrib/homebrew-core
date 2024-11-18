5708b33ee40d8d3f683135be0d41569c1fb142ec
Michael Cho <michael@michaelcho.dev>
2024-11-18 13:00:42 -0500

    eigenpy: revision bump (boost-python3 1.87.0)

--- Formula/e/eigenpy.rb
+++ Formula/e/eigenpy.rb
@@ -4,6 +4,7 @@ class Eigenpy < Formula
   url "https://github.com/stack-of-tasks/eigenpy/releases/download/v3.10.1/eigenpy-3.10.1.tar.gz"
   sha256 "7b4ea31c8eda2eeba6b1ebb22ebfe72b650e04da20e6ef48d48008c2afb1bbc4"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/stack-of-tasks/eigenpy.git", branch: "master"
 
   bottle do
