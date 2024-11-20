40f1471c8fbe3e4b898ef929889d6dc9a215ae0a
Rui Chen <rui@chenrui.dev>
2024-11-20 15:58:12 +0000

    eslint_d: add 14.2.2 bottle.

--- Formula/e/eslint_d.rb
+++ Formula/e/eslint_d.rb
@@ -5,6 +5,15 @@ class EslintD < Formula
   sha256 "2146352364383bbf0f8935fd2460aea04a1fff96af4e26a7974fbee91ccfce51"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9d5b4d6bdbc13ac53b1667b7b247116830d9ebbe1c8454027a9ec38a43dbdbd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9d5b4d6bdbc13ac53b1667b7b247116830d9ebbe1c8454027a9ec38a43dbdbd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d9d5b4d6bdbc13ac53b1667b7b247116830d9ebbe1c8454027a9ec38a43dbdbd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1351de51120b458392426e395363ef55ed05caaa0a76a91d53ef0eebe6df851a"
+    sha256 cellar: :any_skip_relocation, ventura:       "1351de51120b458392426e395363ef55ed05caaa0a76a91d53ef0eebe6df851a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d9d5b4d6bdbc13ac53b1667b7b247116830d9ebbe1c8454027a9ec38a43dbdbd"
+  end
+
   depends_on "node"
 
   def install
