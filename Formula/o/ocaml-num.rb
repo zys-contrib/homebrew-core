58fabd60c40dfa3e3d3fe04df850536d7b60eb39
Daeho Ro <lamanus@outlook.kr>
2024-11-21 20:35:48 +0900

    ocaml-num: revision bump (ocaml 5.2.1)

--- Formula/o/ocaml-num.rb
+++ Formula/o/ocaml-num.rb
@@ -4,7 +4,7 @@ class OcamlNum < Formula
   url "https://github.com/ocaml/num/archive/refs/tags/v1.5.tar.gz"
   sha256 "7ae07c8f5601e2dfc5008a62dcaf2719912ae596a19365c5d7bdf2230515959a"
   license "LGPL-2.1-only" => { with: "OCaml-LGPL-linking-exception" }
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia:  "a50336b3ee1e973d360acb08d7b62f6945d73242a312ea047a1ad51912d96261"
