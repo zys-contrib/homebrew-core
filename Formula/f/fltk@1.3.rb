afbd76e058a88e3151cb0e76d5b564038bf85e6b
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-11-21 16:29:22 +0000

    fltk@1.3: add 1.3.10 bottle.

--- Formula/f/fltk@1.3.rb
+++ Formula/f/fltk@1.3.rb
@@ -10,6 +10,15 @@ class FltkAT13 < Formula
     regex(/href=.*?fltk[._-]v?(1\.3(?:\.\d+)*(?:-\d+)?)-source\.t/i)
   end
 
+  bottle do
+    sha256 arm64_sequoia: "42137ae37ed975c86cf8dfae5fa052ecf9cd9e07a0f4719da35e0bffb95f555e"
+    sha256 arm64_sonoma:  "96d8e718dbef7557ff0fd302420ce4ffd1fd3fe0ed3178c6eca3bbdaaeb5b776"
+    sha256 arm64_ventura: "4e025d394e75457fa5aea55ea1e0ac47df89c6c1fe1cc9b6fd5e4ed5de99eea1"
+    sha256 sonoma:        "d44bc567d2045088a4f9ebde2fdcc606fb05d21726de5abe836eee74ce3cef4e"
+    sha256 ventura:       "f31586117b37b2bb707bbea3037270805dc5982e4fed54d7b6b59c51c144a612"
+    sha256 x86_64_linux:  "695b8822888b50a7989144daca06fa371d3aa9bd47026009d35fb40885cf44d1"
+  end
+
   keg_only :versioned_formula
 
   depends_on "jpeg-turbo"
