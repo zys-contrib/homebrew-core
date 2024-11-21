e5e8ff1f3429341f991257192a4ffde492e10244
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-21 00:49:18 +0000

    gptline: update 1.0.8_5 bottle.

--- Formula/g/gptline.rb
+++ Formula/g/gptline.rb
@@ -10,13 +10,12 @@ class Gptline < Formula
   head "https://github.com/gnachman/gptline.git", branch: "main"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any,                 arm64_sequoia: "650ef331308a98d2c20fa626723be3ad1c115361e94c814a1212f44a8b59af50"
-    sha256 cellar: :any,                 arm64_sonoma:  "8eb95de668be8418c48593cbba35c0cfe810cae8d0e4d791503f1f1a858791dd"
-    sha256 cellar: :any,                 arm64_ventura: "04a17734e9ba104cf24cb31d06b65b6105ef9a633fd5651b9ae5e332daeeae5d"
-    sha256 cellar: :any,                 sonoma:        "7dd08c503affce20282059634445fcfbb6851590b0ab9171aec5f5cf0f04a5c4"
-    sha256 cellar: :any,                 ventura:       "b42e55837cf3f51e695fda77e69590d00a8f331a3b4f7a6943904e599d9b1ab2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "547646d936886c3f12ec8c53d2d0b76189763ae23d515ad0674717faafe54143"
+    sha256 cellar: :any,                 arm64_sequoia: "4074b9e4ac69f7e4e52e1e21bdb3cb5a718102fb51705942b07f7fcd85b8e741"
+    sha256 cellar: :any,                 arm64_sonoma:  "03318e9ded3fdb634878e71e33584e3e8f7aa1a69c427a51a9b2e66bffea25ab"
+    sha256 cellar: :any,                 arm64_ventura: "18703eff958131ad4f93590c9d92b41849ac40d4352535e1093711827f9f08ea"
+    sha256 cellar: :any,                 sonoma:        "72993ffdc79a9d62a3e3f1f3b597eabfa6d8fd6621ae89d659f724586a00760a"
+    sha256 cellar: :any,                 ventura:       "7d5b94c7a3d101980a1176975e7d1e907755aadeacb6004cd0f0f02cf1ad3762"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51b645176411e2a998ee08e303c00524bceae0688402752e920ec08a55c5b78b"
   end
 
   depends_on "pkg-config" => :build
