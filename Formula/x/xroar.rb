821d2bcb0b622dbb95fdbde0df0ea0a8249faa2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-22 10:01:35 +0000

    xroar 1.7.1

--- Formula/x/xroar.rb
+++ Formula/x/xroar.rb
@@ -1,8 +1,8 @@
 class Xroar < Formula
   desc "Dragon and Tandy 8-bit computer emulator"
   homepage "https://www.6809.org.uk/xroar/"
-  url "https://www.6809.org.uk/xroar/dl/xroar-1.6.6.tar.gz"
-  sha256 "2f5f95f655beb41fecb65019f0a24a9c026527edf6d83bd1df55decdf1466494"
+  url "https://www.6809.org.uk/xroar/dl/xroar-1.7.1.tar.gz"
+  sha256 "39a9182ac5a07993b42d70c218b29b13f0466711e2a31c223621db00e3b813c2"
   license "GPL-3.0-or-later"
 
   bottle do
