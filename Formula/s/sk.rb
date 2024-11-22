c40428b2b67070204e253a74a3b966e19ea58efa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-22 16:15:48 +0000

    sk: update 0.11.11 bottle.

--- Formula/s/sk.rb
+++ Formula/s/sk.rb
@@ -7,12 +7,12 @@ class Sk < Formula
   head "https://github.com/lotabout/skim.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f6f82c8d2fcedf1b4a664163476438284f853bbd9575a631a9729edd43b27473"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bf329cf5b1925e41fa60792885aff50150edf7ca2e98264171c2dd90128870c9"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "1df66c117b2af855465200073cf725e2b35af76dc800be4e0ee30373f94f08c0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c04ac28cb36a595fd78cede0c2ab94094f871a1565d0bbcf84f01ec4bc2cdaab"
-    sha256 cellar: :any_skip_relocation, ventura:       "e1e0142c4b8e32ac2006880e2f5c4b3234b3ff35476131833425040a45f9ca44"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "24b5fdee768072598953540010ff8f98d66d6a8443be89d9fd7c906e5812d254"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3c23255422ed5e5166b56bbbad681f6fd0ff2d005f090f8860e78677a1b9360d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "769e0a5438a6efe824a049f1993a70b71f9dbb7c12fccc5ccb985345f94d08db"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2e5b4485f237137a66d84da8aa92d7f01cabb680310fdfc50e5514ec0c51ec2e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "359c7245277871761f4c467888838e64f506ed0ba0374b43fa7b7aac78f651f0"
+    sha256 cellar: :any_skip_relocation, ventura:       "e7287288f99dac97e317a425ae84d8bfeb308793cb436a471e5c0c7d60cc36aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "72101d040476e4a97afe0172c9d3007ba1a44cc68c4ad85eb25e2c0cf580211b"
   end
 
   depends_on "rust" => :build
