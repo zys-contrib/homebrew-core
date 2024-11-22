870d0021dcaea38843fcb1130b5bc555dc475c57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-22 14:22:39 +0000

    sk: update 0.11.10 bottle.

--- Formula/s/sk.rb
+++ Formula/s/sk.rb
@@ -7,16 +7,12 @@ class Sk < Formula
   head "https://github.com/lotabout/skim.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "e8607c7f9e3c72059b5168aded78582bfe459e754381f669cce9a6169523832f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0aa120c7df0fb4a61903c952d5a64dad4030a0b1a48df8c2d0bac01dac458ec8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e28dbbdb5930443d04b934d8966af2dec58f037f859432f7a412c52568990e1d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a96933375963dac24744a541d7835a9694bf9050481e8d302b9f22187a0e8184"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e7b9c3bc71263bd5f16a6cdf08cb36e1076ea698f5b26fcac5fa8fe1c82032c1"
-    sha256 cellar: :any_skip_relocation, sonoma:         "e6e9e40fdafaf8d5807947b960698535edf426923fbcef6ebc5ff70dc114aa89"
-    sha256 cellar: :any_skip_relocation, ventura:        "afaa23049ae7c7268e5bc86bdba95abe0d99dfe83057c4614ce95ae7ac580830"
-    sha256 cellar: :any_skip_relocation, monterey:       "de7b821fe89afa96598770fdd98d7b55a78b57be6867284f6e4aa41db9469331"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ab8e698b22382f4faed083f426fb8aa1fa0e0393c7e43169deba03021fa502ea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9dee90b1f10ffc8ab60ed7549c0c5fb16afda00b94de1a34eee59ffd21205412"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f6f82c8d2fcedf1b4a664163476438284f853bbd9575a631a9729edd43b27473"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bf329cf5b1925e41fa60792885aff50150edf7ca2e98264171c2dd90128870c9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1df66c117b2af855465200073cf725e2b35af76dc800be4e0ee30373f94f08c0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c04ac28cb36a595fd78cede0c2ab94094f871a1565d0bbcf84f01ec4bc2cdaab"
+    sha256 cellar: :any_skip_relocation, ventura:       "e1e0142c4b8e32ac2006880e2f5c4b3234b3ff35476131833425040a45f9ca44"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "24b5fdee768072598953540010ff8f98d66d6a8443be89d9fd7c906e5812d254"
   end
 
   depends_on "rust" => :build
