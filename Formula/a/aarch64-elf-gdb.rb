2028fab1ba584e3138bf9dbea467aff208123d81
Michael Cho <michael@michaelcho.dev>
2024-11-22 19:09:05 -0500

    aarch64-elf-gdb: migrate to `python@3.13`

--- Formula/a/aarch64-elf-gdb.rb
+++ Formula/a/aarch64-elf-gdb.rb
@@ -24,7 +24,7 @@ class Aarch64ElfGdb < Formula
   depends_on "aarch64-elf-gcc" => :test
   depends_on "gmp"
   depends_on "mpfr"
-  depends_on "python@3.12"
+  depends_on "python@3.13"
   depends_on "readline"
   depends_on "xz" # required for lzma support
   depends_on "zstd"
@@ -49,7 +49,7 @@ class Aarch64ElfGdb < Formula
       --with-curses
       --with-expat
       --with-lzma
-      --with-python=#{which("python3.12")}
+      --with-python=#{which("python3.13")}
       --with-system-readline
       --with-system-zlib
       --with-zstd
