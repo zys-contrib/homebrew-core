ae46ec10a00a27dd2efea2bf2b1256ab7c020d45
Rui Chen <rui@chenrui.dev>
2024-11-23 23:21:14 +0000

    coal: add 3.0.0 bottle.

--- Formula/c/coal.rb
+++ Formula/c/coal.rb
@@ -11,6 +11,15 @@ class Coal < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "f153b19d41512585b847003ef4f1586ffb75ed3517da98396e84393aaa27e44e"
+    sha256 cellar: :any,                 arm64_sonoma:  "1c506cc8bb8da18b2a51c6fb6e12752168a330b499ee2a0d2935f01743fc11e9"
+    sha256 cellar: :any,                 arm64_ventura: "271b8781233c2a06d72a9fee9cb2dca85366f0ae69b4f566c3b770ccfffe74e6"
+    sha256 cellar: :any,                 sonoma:        "84f719d46ed7c2c09ca17eac6d7e05c0258faf74f23d95a6a779c06cae35b7bc"
+    sha256 cellar: :any,                 ventura:       "31b33f22e75a9afd53c5fe8c4fb2959b62af1e61c9233e11887eb4186222c131"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e9bf476dc22586356da2407ee3b2e0efb1985605eaa6cffb18890c11a3e3361"
+  end
+
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "pkgconf" => :build
