629182e46125748c21b4dd61ad9ca5019858ab3c
Rui Chen <rui@chenrui.dev>
2024-11-23 19:04:59 +0000

    ampl-mp: update 4.0.0 bottle.

--- Formula/a/ampl-mp.rb
+++ Formula/a/ampl-mp.rb
@@ -11,13 +11,12 @@ class AmplMp < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "d02c9a51954b17ab0a6caa31093daa7be212239b957d2224b773aaaa19b89c59"
-    sha256 cellar: :any,                 arm64_sonoma:  "02f9efaecf470dffc6bff5b71530d0245b342f0e3805f73a5741b15a175e348a"
-    sha256 cellar: :any,                 arm64_ventura: "cf8589ed55bdfa5612fe9f4efc698eed1000e332446900798b1d4ebcd9f4e77e"
-    sha256 cellar: :any,                 sonoma:        "fdf52601bd4c9990a80d1a14600d7c8649f2597bf4af38c966f9f806835eff44"
-    sha256 cellar: :any,                 ventura:       "48756d110b0bac36eec0f33e181ad12f68632cb3087ccebb8451ef1d31798294"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "14117080e712ec6bcfd79084440b9264783a4567b9f31368bf8fa7109d718f58"
+    sha256 cellar: :any,                 arm64_sequoia: "07c2f88aaeea323a876870c8c4fb107193cee09fc8e591cf39c9710812be85e7"
+    sha256 cellar: :any,                 arm64_sonoma:  "3daf4ac5b5d9765794ed359c5838c3e07183f7e209e962a4d30a0f2a8bf06062"
+    sha256 cellar: :any,                 arm64_ventura: "3984cfa43e3ec758a3d4cda736af4f70d51a55e4c8fbc271b691a3fc6a42e33d"
+    sha256 cellar: :any,                 sonoma:        "81efa3c47b7241ad4adf50e1a50653472de841337c8a72620b8b8015eda4ba02"
+    sha256 cellar: :any,                 ventura:       "d102b69020fdb1d2d0c49b2e4bb70498b154a1950533640b0dcfec0ab0069f8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1548c6099ab8d8e657a0ab983deb4c7b4f1c32d0658f40c90336d7571e0b19ed"
   end
 
   depends_on "cmake" => :build
