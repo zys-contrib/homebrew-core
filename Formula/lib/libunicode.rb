ff6ea1bf2d910ede7a162210e0f2a96107c71e0e
Rui Chen <rui@chenrui.dev>
2024-11-23 19:50:11 -0500

    libunicode 0.6.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/lib/libunicode.rb
@@ -0,0 +1,63 @@
+class Libunicode < Formula
+  desc "Modern C++20 Unicode library"
+  homepage "https://github.com/contour-terminal/libunicode"
+  url "https://github.com/contour-terminal/libunicode/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "0c217f8264000f1b8c36e78969cb9cf91ac97de937cc141ab78e6b1ad7f404ef"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "python" => :build
+
+  on_macos do
+    depends_on "llvm" if DevelopmentTools.clang_build_version <= 1500
+  end
+
+  fails_with :clang do
+    build 1500
+    cause "Requires C++20"
+  end
+
+  fails_with :gcc do
+    version "11"
+    cause "Requires C++20"
+  end
+
+  def install
+    ENV.llvm_clang if OS.mac? && DevelopmentTools.clang_build_version <= 1500
+
+    args = %W[
+      -DLIBUNICODE_EXAMPLES=OFF
+      -DLIBUNICODE_TESTING=OFF
+      -DLIBUNICODE_BENCHMARK=OFF
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # ENV.llvm_clang doesn't work in the test block
+    ENV["CXX"] = Formula["llvm"].opt_bin/"clang++" if OS.mac? && DevelopmentTools.clang_build_version <= 1500
+
+    (testpath/"test.cpp").write <<~CPP
+      #include <iostream>
+      #include <libunicode/capi.h>
+
+      int main() {
+          u32_char_t u32_codepoints[] = { 'h', 'e', 'l', 'l', 'o', ' ', 0x1F30D };
+          int gc_count = u32_gc_count(u32_codepoints, 7);
+          std::cout << "Grapheme cluster count: " << gc_count << "\\n";
+
+          return 0;
+      }
+    CPP
+
+    system ENV.cxx, "-std=c++17", "-o", "test", "test.cpp", "-I#{include}", "-L#{lib}", "-lunicode"
+    assert_match "Grapheme cluster count: 7", shell_output("./test")
+
+    assert_match "HYPHEN", shell_output("#{bin}/unicode-query U+2D")
+  end
+end
