dddf77baf1d6489b1bffd3f9f798cae41e6e38ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-24 20:07:29 +0000

    cdk8s: update 2.198.268 bottle.

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -7,12 +7,12 @@ class Cdk8s < Formula
   head "https://github.com/cdk8s-team/cdk8s-cli.git", branch: "2.x"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "03a8583ba48a204826987f9b2e698ce1058ecb12ddb26f79b923e0f0d9143d37"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "03a8583ba48a204826987f9b2e698ce1058ecb12ddb26f79b923e0f0d9143d37"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "03a8583ba48a204826987f9b2e698ce1058ecb12ddb26f79b923e0f0d9143d37"
-    sha256 cellar: :any_skip_relocation, sonoma:        "32094b1420173d679e1f1d153f16d2959cbb314967409e4709e9ab6767d96407"
-    sha256 cellar: :any_skip_relocation, ventura:       "32094b1420173d679e1f1d153f16d2959cbb314967409e4709e9ab6767d96407"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "03a8583ba48a204826987f9b2e698ce1058ecb12ddb26f79b923e0f0d9143d37"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5de9856ed23e14f4827451a28c12efbe553abbdbc389890d7f88db6778986fbd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5de9856ed23e14f4827451a28c12efbe553abbdbc389890d7f88db6778986fbd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5de9856ed23e14f4827451a28c12efbe553abbdbc389890d7f88db6778986fbd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a1accf9fd7e3cae31a54d0cdff66688b77694c610b4ad733146c6df06ae396ee"
+    sha256 cellar: :any_skip_relocation, ventura:       "a1accf9fd7e3cae31a54d0cdff66688b77694c610b4ad733146c6df06ae396ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5de9856ed23e14f4827451a28c12efbe553abbdbc389890d7f88db6778986fbd"
   end
 
   depends_on "node"
