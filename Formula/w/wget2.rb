7e265c0a434d6ff65c2f171e9fe6feed91571745
Rui Chen <rui@chenrui.dev>
2024-11-24 17:23:10 -0500

    wget2 2.2.0
    autobump: add wget2
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/wget2.rb
+++ Formula/w/wget2.rb
@@ -1,8 +1,8 @@
 class Wget2 < Formula
   desc "Successor of GNU Wget, a file and recursive website downloader"
   homepage "https://gitlab.com/gnuwget/wget2"
-  url "https://ftp.gnu.org/gnu/wget/wget2-2.1.0.tar.gz"
-  sha256 "a05dc5191c6bad9313fd6db2777a78f5527ba4774f665d5d69f5a7461b49e2e7"
+  url "https://ftp.gnu.org/gnu/wget/wget2-2.2.0.tar.gz"
+  sha256 "2b3b9c85b7fb26d33ca5f41f1f8daca71838d869a19b406063aa5c655294d357"
   license "GPL-3.0-or-later"
 
   livecheck do
