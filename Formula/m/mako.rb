4e27fbfc4a33fbc431e800c853c0a0f71f4516bd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-25 05:12:39 +0000

    mako 0.9.7

--- Formula/m/mako.rb
+++ Formula/m/mako.rb
@@ -1,8 +1,8 @@
 class Mako < Formula
   desc "Production-grade web bundler based on Rust"
   homepage "https://makojs.dev"
-  url "https://registry.npmjs.org/@umijs/mako/-/mako-0.9.6.tgz"
-  sha256 "7274a7af2fcfd251f7bb5dfd9605ca831418153112dd9976d2f8d7a75799add1"
+  url "https://registry.npmjs.org/@umijs/mako/-/mako-0.9.7.tgz"
+  sha256 "71d7fc962dfcec1b61ae4caec71fe1b64477bed395817140ddbfeb6cb3cb8e93"
   license "MIT"
 
   bottle do
