5e06771d30debcf7b515d76fbf4232cd4b115fba
Patrick Linnane <patrick@linnane.io>
2024-11-25 07:12:41 +0000

    sesh: add 2.6.0 bottle.

--- Formula/s/sesh.rb
+++ Formula/s/sesh.rb
@@ -6,6 +6,15 @@ class Sesh < Formula
   license "MIT"
   head "https://github.com/joshmedeski/sesh.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a82813dca10e229c3d4bfeeca54e2c8c11916a4f24f37bbba6a532bf1324a2e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a82813dca10e229c3d4bfeeca54e2c8c11916a4f24f37bbba6a532bf1324a2e4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a82813dca10e229c3d4bfeeca54e2c8c11916a4f24f37bbba6a532bf1324a2e4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "afa6ce77d55abf50d90046fdd299b1941fa0892eb1bb2f14c23f2c4ab05cba51"
+    sha256 cellar: :any_skip_relocation, ventura:       "afa6ce77d55abf50d90046fdd299b1941fa0892eb1bb2f14c23f2c4ab05cba51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a25b4abfbb3eeacf4fd0104738e41dc7b0208ca254989b9799e507738913b84f"
+  end
+
   depends_on "go" => :build
 
   def install
