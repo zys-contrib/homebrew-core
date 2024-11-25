d1936bc1fd30a6f3e52c0a6a184a3c7deb8ea4eb
Rui Chen <rui@chenrui.dev>
2024-11-25 13:31:42 -0500

    folly 2024.11.25.00
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/folly.rb
+++ Formula/f/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/refs/tags/v2024.11.18.00.tar.gz"
-  sha256 "b2c6879ba8ba625218d1ab9eefcc1611a9003d05bc2cb1a38f3bae21892e5167"
+  url "https://github.com/facebook/folly/archive/refs/tags/v2024.11.25.00.tar.gz"
+  sha256 "c41bcd5c3a4c08efe7166605bc29ffdf278bd0a7ca23d8d2b6e89b9651971e53"
   license "Apache-2.0"
   head "https://github.com/facebook/folly.git", branch: "main"
 
