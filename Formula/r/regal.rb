d333010ed1f6626523cbd87b2cc6ac2bfb362268
Rui Chen <rui@chenrui.dev>
2024-11-25 06:08:46 +0000

    regal: add 0.29.2 bottle.

--- Formula/r/regal.rb
+++ Formula/r/regal.rb
@@ -6,6 +6,15 @@ class Regal < Formula
   license "Apache-2.0"
   head "https://github.com/StyraInc/regal.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a680e72469c2fb37d63092e46e02ef8fde288839d367c6677173c4ec4ac0c077"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a680e72469c2fb37d63092e46e02ef8fde288839d367c6677173c4ec4ac0c077"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a680e72469c2fb37d63092e46e02ef8fde288839d367c6677173c4ec4ac0c077"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3062812d9e9103c84d661cf90fdd55d2c5c0782613b79c2848c1ee7aead6632f"
+    sha256 cellar: :any_skip_relocation, ventura:       "3062812d9e9103c84d661cf90fdd55d2c5c0782613b79c2848c1ee7aead6632f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa8e9476623d7b3585717d9fb0fe8716ffa6c1880c0f70aa875b21957d65467a"
+  end
+
   depends_on "go" => :build
 
   def install
