137a5b85c4b2226397992383b2672456aefe462d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-26 05:08:06 +0000

    llama.cpp 4174

--- Formula/l/llama.cpp.rb
+++ Formula/l/llama.cpp.rb
@@ -3,8 +3,8 @@ class LlamaCpp < Formula
   homepage "https://github.com/ggerganov/llama.cpp"
   # CMake uses Git to generate version information.
   url "https://github.com/ggerganov/llama.cpp.git",
-      tag:      "b4164",
-      revision: "9ca2e677626fce759d5d95c407c03677b9c87a26"
+      tag:      "b4174",
+      revision: "0eb4e12beebabae46d37b78742f4c5d4dbe52dc1"
   license "MIT"
   head "https://github.com/ggerganov/llama.cpp.git", branch: "master"
 
