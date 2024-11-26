dd4dc0ef62bf9a7343102704f787fcea501cf7cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-26 16:57:31 +0000

    deployer 7.5.6

--- Formula/d/deployer.rb
+++ Formula/d/deployer.rb
@@ -2,10 +2,9 @@ class Deployer < Formula
   desc "Deployment tool written in PHP with support for popular frameworks"
   homepage "https://deployer.org/"
   # Bump to php 8.4 on the next release, if possible.
-  url "https://github.com/deployphp/deployer/releases/download/v7.5.5/deployer.phar"
-  sha256 "30c1c09038b0c390f40d21b3cb0a9e1ff46cfeefe4da1c834aeca6adfb5952d4"
+  url "https://github.com/deployphp/deployer/releases/download/v7.5.6/deployer.phar"
+  sha256 "ee741006edc46a97d522c41b84cdcdcec0da5807afcd0e7d6e737d0970b723aa"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "7e54fce47176409d67d6508aa9b661e62600d211fe8ad81948029823f7a51b32"
