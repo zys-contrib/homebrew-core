e4cd0e79127cdf533e2728252d810fe294fb6348
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-27 20:34:55 +0000

    lla: update 0.2.7 bottle.

--- Formula/l/lla.rb
+++ Formula/l/lla.rb
@@ -6,12 +6,12 @@ class Lla < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "707b5dafc64e1b2609ea977b3c3257aebc83e25c0bd84a2e36b7a2aef5c4d2ba"
-    sha256 cellar: :any,                 arm64_sonoma:  "a380aea3b72d793c764ff101693528a9369bc831778fce1e873be91dc53887c4"
-    sha256 cellar: :any,                 arm64_ventura: "75cd897665d5681623403ae103d654815b1c9e0acd7ff88d35bb843bdd2da5ff"
-    sha256 cellar: :any,                 sonoma:        "bc60d26fb6254b101d19d0475f99e51880fd754dc835275c4ec9fc3c602023ba"
-    sha256 cellar: :any,                 ventura:       "d2674437c54bf7135791f528b5c81faae11440fa8dc5615c4dc96e999e2d603f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ecefe7a035fd7efa52309ec690fe14fd4d12e67ec17dddd36b20bdb6f9fe5fdc"
+    sha256 cellar: :any,                 arm64_sequoia: "c55294aa140b5e5643251a774189dcfb15549d903c8d22f87f98d721972e8862"
+    sha256 cellar: :any,                 arm64_sonoma:  "de3015b2a489de168bfb217f66c16475e9e6d92e093cfce85647402735074412"
+    sha256 cellar: :any,                 arm64_ventura: "cd36dbbe69774f47e159abd6968d38e801f3831f433735761115e502f8966a5f"
+    sha256 cellar: :any,                 sonoma:        "a9e89bfb960e8cfc1f393f1c3614f28ced5ef58db5a249f129c9f7aed28a0733"
+    sha256 cellar: :any,                 ventura:       "38d95db1e5156465318dcabac345bf3f956f61d5fb9554a9e5b405f1a3d4c9fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab53dced7964bcf2f33d9e8202c8926f827a9fa24216365b922b798c9e09014a"
   end
 
   depends_on "rust" => :build
