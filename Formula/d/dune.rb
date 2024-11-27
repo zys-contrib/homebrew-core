9ee4cd09124c7080d7e515f2834628fec316254f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-27 19:56:59 +0000

    dune 3.17.0

--- Formula/d/dune.rb
+++ Formula/d/dune.rb
@@ -1,8 +1,8 @@
 class Dune < Formula
   desc "Composable build system for OCaml"
   homepage "https://dune.build/"
-  url "https://github.com/ocaml/dune/releases/download/3.16.1/dune-3.16.1.tbz"
-  sha256 "b781ae20f87613c2a11bd0717809e00470c82d615e15264f9a64e033051ac3de"
+  url "https://github.com/ocaml/dune/releases/download/3.17.0/dune-3.17.0.tbz"
+  sha256 "2c3aa6c41ed39e3d6c1a292d75f4806bd80310841afc51673aa59ce9c816507c"
   license "MIT"
   head "https://github.com/ocaml/dune.git", branch: "main"
 
