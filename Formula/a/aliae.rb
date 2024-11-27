46243962c5383ccd381f68da48d3bc7978b96efa
Rui Chen <rui@chenrui.dev>
2024-11-26 23:31:10 -0500

    aliae 0.23.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/a/aliae.rb
@@ -0,0 +1,40 @@
+class Aliae < Formula
+  desc "Cross shell and platform alias management"
+  homepage "https://aliae.dev"
+  url "https://github.com/jandedobbeleer/aliae/archive/refs/tags/v0.23.0.tar.gz"
+  sha256 "080d243fbd333f2684989ccdd71bd3069ff36db6ac968acc656d4e1bbd924b34"
+  license "MIT"
+  head "https://github.com/jandedobbeleer/aliae.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    cd "src" do
+      ldflags = "-s -w -X main.Version=#{version}"
+      system "go", "build", *std_go_args(ldflags:)
+    end
+
+    generate_completions_from_executable(bin/"aliae", "completion")
+  end
+
+  test do
+    (testpath/".aliae.yaml").write <<~YAML
+      alias:
+        - name: a
+          value: aliae
+        - name: hello-world
+          value: echo "hello world"
+          type: function
+    YAML
+
+    output = shell_output("#{bin}/aliae init bash")
+    assert_equal <<~SHELL.chomp, output
+      alias a="aliae"
+      hello-world() {
+          echo "hello world"
+      }
+    SHELL
+
+    assert_match version.to_s, shell_output("#{bin}/aliae --version")
+  end
+end
