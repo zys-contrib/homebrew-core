746d9f5ffa38f91a645595d8b4fb375c6b93b23c
Michael Cho <michael@michaelcho.dev>
2024-11-27 16:06:12 -0500

    mosh: revision bump (protobuf 29.0)

--- Formula/m/mosh.rb
+++ Formula/m/mosh.rb
@@ -4,7 +4,7 @@ class Mosh < Formula
   url "https://github.com/mobile-shell/mosh/releases/download/mosh-1.4.0/mosh-1.4.0.tar.gz"
   sha256 "872e4b134e5df29c8933dff12350785054d2fd2839b5ae6b5587b14db1465ddd"
   license "GPL-3.0-or-later"
-  revision 22
+  revision 23
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "33dc51bf37be8f249ba0004d5e647d689de56639bc8664a785390807c7be5164"
