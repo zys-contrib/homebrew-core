f6a2d0c71faf24c01b5d09dde1dad9e2547d834f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-28 13:41:01 +0000

    lxc: update 6.2 bottle.

--- Formula/l/lxc.rb
+++ Formula/l/lxc.rb
@@ -7,14 +7,12 @@ class Lxc < Formula
   head "https://github.com/canonical/lxd.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "4819357eca98fa9b3275b36c9aa346a9933c7cd3562fc84cc3551d9ce05836c2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2a80b2547e2b6db519b582309a06f4f7905820aa626555cf4ec259ee6db58c92"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d0a71f095561e5c2ed19054c16455079bae4d6fdbe367f45364fcc2535c395a2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5fd47741213d1e89b424321a723f6c5a3157cebd794d8a49899f7002e7b49f59"
-    sha256 cellar: :any_skip_relocation, sonoma:         "e21a2bfe721dc23fce826f3f9dcf63060d24eadee5b2322bd23ba72d09755df3"
-    sha256 cellar: :any_skip_relocation, ventura:        "5c8951a235c73e4ed0205ea9ec398b23a048bb0470b76c25892774e1e94085a6"
-    sha256 cellar: :any_skip_relocation, monterey:       "c91c3b6382c0f3c1d07cc2cf96609baffce5667bc5fbd4c7fc07f218d89d9336"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7a78e129573f90fa081adf1a31f7824ea4690dc93e88c21b3da97ca08aeab163"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "557ad99b5bcc304ebe9fa87cfecc9df2e5df9f260f98add9ad9904831c2109f4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "557ad99b5bcc304ebe9fa87cfecc9df2e5df9f260f98add9ad9904831c2109f4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "557ad99b5bcc304ebe9fa87cfecc9df2e5df9f260f98add9ad9904831c2109f4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "38d351b3937d975859d7298d54b14fb06a8293fd999674d45f07c04f6223cd7a"
+    sha256 cellar: :any_skip_relocation, ventura:       "38d351b3937d975859d7298d54b14fb06a8293fd999674d45f07c04f6223cd7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "debfd85a1ae466f804ddcccf425d317d4bbfce55db8e42ea7ba3d87a32f9b460"
   end
 
   depends_on "go" => :build
