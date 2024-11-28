0d41ff7ee6f9aec4e5ffae228217590d3ef0d7c1
Rui Chen <rui@chenrui.dev>
2024-11-28 15:18:45 -0500

    cloudquery 6.12.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cloudquery.rb
@@ -0,0 +1,35 @@
+class Cloudquery < Formula
+  desc "Data movement tool to sync data from any source to any destination"
+  homepage "https://www.cloudquery.io"
+  url "https://github.com/cloudquery/cloudquery/archive/refs/tags/cli-v6.12.1.tar.gz"
+  sha256 "3fc269fda9b26208cd862e5ec532e2a95862d93fb343953b7845724ba9425333"
+  license "MPL-2.0"
+  head "https://github.com/cloudquery/cloudquery.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^cli-v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/cloudquery/cloudquery/cli/cmd.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    system bin/"cloudquery", "init", "--source", "aws", "--destination", "bigquery"
+
+    assert_path_exists testpath/"cloudquery.log"
+    assert_match <<~YAML, (testpath/"aws_to_bigquery.yaml").read
+      kind: source
+      spec:
+        # Source spec section
+        name: aws
+        path: cloudquery/aws
+    YAML
+
+    assert_match version.to_s, shell_output("#{bin}/cloudquery --version")
+  end
+end
