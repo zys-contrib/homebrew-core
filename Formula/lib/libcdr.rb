820a1dccbaa2daf49746f5953922fa4cc49638c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-28 04:31:15 +0000

    libcdr: update 0.1.8 bottle.

--- Formula/lib/libcdr.rb
+++ Formula/lib/libcdr.rb
@@ -11,12 +11,12 @@ class Libcdr < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "bbc2a54bb8f6d6a4e466865b14d7b70a6fbc858d388548cf9b953f201de3c990"
-    sha256 cellar: :any,                 arm64_sonoma:  "cc66b1548f086a48f271bf053c229ee390cd2bd1156903d29ff0021f0df9b5a5"
-    sha256 cellar: :any,                 arm64_ventura: "4490629bd88271316e181a59054bdbca640a68fc742ad2e072b7c00d529787ad"
-    sha256 cellar: :any,                 sonoma:        "1229fd37c6e1c8e952b63e78bd58778a92f64da3fda9f7f6ac0f07c6b8b61150"
-    sha256 cellar: :any,                 ventura:       "d36c29ef45a56def001e4401ef3a2efce00352621ff1dd2f23260d6d6dadff5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "11f1b27844634dbdba55e73959c47a4cf20189936b203790f5f98ac48e7174c7"
+    sha256 cellar: :any,                 arm64_sequoia: "cb6eff3f4627fcc102cb45b8da2f35f82e5049d6a0875c84334484388ede2025"
+    sha256 cellar: :any,                 arm64_sonoma:  "1798c673e5c3c0c793cf9e3d5454bda00724244e189535c68ef476303d2a0405"
+    sha256 cellar: :any,                 arm64_ventura: "2ef42ae303f70cc8294e5082588f5b3c63dcffa70acba167d63b65e3b1d92798"
+    sha256 cellar: :any,                 sonoma:        "889c0540fcae08302afdd508ecf8287c41b7b8cac892cd5b834383dce1310a4c"
+    sha256 cellar: :any,                 ventura:       "9bb8d4bc98a361e1eee954a04f8ad930dcfc3958cc80ecf5c493cfdf83d74448"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a4dd30a7edf347fdb569e2d1e9a4a0f053dbce86d23f2096b8f26f542768517d"
   end
 
   depends_on "boost" => :build
