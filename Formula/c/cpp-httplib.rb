2a5e49e502bdd4155fac121e79f914694be4e9bc
Michael Cho <michael@michaelcho.dev>
2024-11-29 15:06:18 -0500

    cpp-httplib: build with CMake for config files

--- Formula/c/cpp-httplib.rb
+++ Formula/c/cpp-httplib.rb
@@ -9,8 +9,9 @@ class CppHttplib < Formula
     sha256 cellar: :any_skip_relocation, all: "51cf6494b3fec136d413705e4b6ac0394b45b565a0b62b6c8d68c3afbeb371b3"
   end
 
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
+  depends_on "cmake" => :build
+  depends_on "openssl@3" => :build
+  uses_from_macos "zlib" => :build
 
   fails_with :clang do
     build 1300
@@ -22,9 +23,16 @@ class CppHttplib < Formula
   end
 
   def install
-    system "meson", "setup", "build", *std_meson_args
-    system "meson", "compile", "-C", "build", "--verbose"
-    system "meson", "install", "-C", "build"
+    # Set args for consistent dependencies used in generated CMake config
+    args = %w[
+      -DHTTPLIB_REQUIRE_OPENSSL=ON
+      -DHTTPLIB_REQUIRE_ZLIB=ON
+      -DHTTPLIB_USE_BROTLI_IF_AVAILABLE=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
