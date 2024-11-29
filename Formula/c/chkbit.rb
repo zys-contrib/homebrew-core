c57939e445d4fb75f0150a9e1f04c9000af4bd65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-11-29 20:18:10 +0000

    chkbit: update 5.4.0 bottle.

--- Formula/c/chkbit.rb
+++ Formula/c/chkbit.rb
@@ -7,12 +7,12 @@ class Chkbit < Formula
   head "https://github.com/laktak/chkbit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7beb4e1287bd9e353d88ea6382bd1da7626789e3cab128dcadc95bb5ca599fa1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7beb4e1287bd9e353d88ea6382bd1da7626789e3cab128dcadc95bb5ca599fa1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7beb4e1287bd9e353d88ea6382bd1da7626789e3cab128dcadc95bb5ca599fa1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "66e10e990ce4c166674ae01ac20dcbf4e023efd52cd640ac9bcd7a8d7c7ff3ca"
-    sha256 cellar: :any_skip_relocation, ventura:       "66e10e990ce4c166674ae01ac20dcbf4e023efd52cd640ac9bcd7a8d7c7ff3ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3002b57bbd6b83bb1dc5e06a2eb0585ec87aefd3496fec983775a1c3cfa7b178"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5f82a3b2a523e105f931fc045d5af9de18115117fdb2b96aaaabc50523fc4fc6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5f82a3b2a523e105f931fc045d5af9de18115117fdb2b96aaaabc50523fc4fc6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5f82a3b2a523e105f931fc045d5af9de18115117fdb2b96aaaabc50523fc4fc6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6cd781742c5fd2b4834c832225e35a13e0bd8d834d974da9c1e4f90c807f2cb6"
+    sha256 cellar: :any_skip_relocation, ventura:       "6cd781742c5fd2b4834c832225e35a13e0bd8d834d974da9c1e4f90c807f2cb6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "72c157b96149cfc77d83a067d51ceed5b912e84e7bbf97521b0600eb56b03bde"
   end
 
   depends_on "go" => :build
