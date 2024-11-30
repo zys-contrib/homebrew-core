b71ef032bfe4e8658ef6a497c8d6a8d6861c8c38
Michael Cho <michael@michaelcho.dev>
2024-11-30 02:08:36 -0500

    aws-c-s3 0.7.2 (new formula)

---
+++ Formula/a/aws-c-s3.rb
@@ -0,0 +1,63 @@
+class AwsCS3 < Formula
+  desc "C99 library implementation for communicating with the S3 service"
+  homepage "https://github.com/awslabs/aws-c-s3"
+  url "https://github.com/awslabs/aws-c-s3/archive/refs/tags/v0.7.2.tar.gz"
+  sha256 "4af522456b6e48741aea190ad9a32cc0fcb81b01c5e9ec5d0a782f855329c8b1"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "aws-c-auth"
+  depends_on "aws-c-cal"
+  depends_on "aws-c-common"
+  depends_on "aws-c-http"
+  depends_on "aws-c-io"
+  depends_on "aws-checksums"
+
+  def install
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_MODULE_PATH=#{Formula["aws-c-common"].opt_lib}/cmake
+    ]
+    # Avoid linkage to `aws-c-compression` and `aws-c-sdkutils`
+    args << "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-dead_strip_dylibs" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <aws/common/allocator.h>
+      #include <aws/common/error.h>
+      #include <aws/s3/s3.h>
+      #include <aws/s3/s3_client.h>
+      #include <assert.h>
+      #include <string.h>
+
+      int main(void) {
+        struct aws_allocator *allocator = aws_default_allocator();
+        aws_s3_library_init(allocator);
+
+        assert(0 == strcmp("HeadObject", aws_s3_request_type_operation_name(AWS_S3_REQUEST_TYPE_HEAD_OBJECT)));
+
+        for (enum aws_s3_request_type type = AWS_S3_REQUEST_TYPE_UNKNOWN + 1; type < AWS_S3_REQUEST_TYPE_MAX; ++type) {
+          const char *operation_name = aws_s3_request_type_operation_name(type);
+          assert(NULL != operation_name);
+          assert(strlen(operation_name) > 1);
+        }
+
+        assert(NULL != aws_s3_request_type_operation_name(AWS_S3_REQUEST_TYPE_UNKNOWN));
+        assert(0 == strcmp("", aws_s3_request_type_operation_name(AWS_S3_REQUEST_TYPE_UNKNOWN)));
+        assert(0 == strcmp("", aws_s3_request_type_operation_name(AWS_S3_REQUEST_TYPE_MAX)));
+        assert(0 == strcmp("", aws_s3_request_type_operation_name(-1)));
+
+        aws_s3_library_clean_up();
+        return 0;
+      }
+    C
+    system ENV.cc, "test.c", "-o", "test", "-L#{lib}", "-laws-c-s3",
+                   "-L#{Formula["aws-c-common"].opt_lib}", "-laws-c-common"
+    system "./test"
+  end
+end
