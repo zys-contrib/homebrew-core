8cae928b7930a5de2330e7b7707fc6abcd35b3ef
Rui Chen <rui@chenrui.dev>
2024-11-29 23:13:40 -0500

    asm6809 2.14
    autobump: add asm6809
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/asm6809.rb
+++ Formula/a/asm6809.rb
@@ -1,8 +1,8 @@
 class Asm6809 < Formula
   desc "Cross assembler targeting the Motorola 6809 and Hitachi 6309"
   homepage "https://www.6809.org.uk/asm6809/"
-  url "https://www.6809.org.uk/asm6809/dl/asm6809-2.13.tar.gz"
-  sha256 "1a5caa2ab411d6f0bdcb97004f7aa2b7f3ade0b7a281a2322380ff9fe116c6cb"
+  url "https://www.6809.org.uk/asm6809/dl/asm6809-2.14.tar.gz"
+  sha256 "c7b5c8a17f329a88c8ec466ebf000047879bab3716f7df2ed2579e2623f22c0c"
   license "GPL-3.0-or-later"
 
   bottle do
