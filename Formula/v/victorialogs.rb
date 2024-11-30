c3e0fb19b24c99856508c2caca549fbf6735d261
Daeho Ro <lamanus@outlook.kr>
2024-11-30 15:12:37 +0000

    victorialogs: add 1.106.1 bottle.

--- Formula/v/victorialogs.rb
+++ Formula/v/victorialogs.rb
@@ -13,6 +13,15 @@ class Victorialogs < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "59308bae9011fc0aa0a76ca1570f6d7eb609bf4b5fe95f2955cc713e1ccd3869"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e8dc1da2d9173d4529ebd351f6be9777c3311776bab470ea6ad1956585746276"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d1c5780f5b89a4e1187ccb737028be61b5a9bcca785f6dc49961640be0925f10"
+    sha256 cellar: :any_skip_relocation, sonoma:        "549f0bb91f874755f57e9427d0f370e7befdb8c70d7b1b13865798ea7c66b456"
+    sha256 cellar: :any_skip_relocation, ventura:       "d3bc33967fe3aebc6750c73a0b2344da3594d499754c8e1b36b1a77839f8681f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d62464311a9f38f47916987366a55250f5a57f11c6aabfbae74b5d1288c4f14b"
+  end
+
   depends_on "go" => :build
 
   def install
