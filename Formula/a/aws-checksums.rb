7421f5f70e18ee5dac1fdb4a7927433408935c32
Michael Cho <20700669+cho-m@users.noreply.github.com>
2024-11-30 16:35:28 +0000

    aws-checksums: add 0.2.2 bottle.

--- Formula/a/aws-checksums.rb
+++ Formula/a/aws-checksums.rb
@@ -5,6 +5,15 @@ class AwsChecksums < Formula
   sha256 "96acfea882c06acd5571c845e4968892d6ffc0fae81b31a0b1565100049743b2"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "4e802dc8eac8372e653773a877ff36975a07798f2ae51d0a3b2a899e349b633d"
+    sha256 cellar: :any,                 arm64_sonoma:  "067ba18a6a9c5653525345a2cc1c1bf05beefdb677255850fb4fe29f5467cae7"
+    sha256 cellar: :any,                 arm64_ventura: "5f0aabfecca0f8c781d8ef1f13ac16ff833d9faf3cab49ff8bbe3b8cd937de3b"
+    sha256 cellar: :any,                 sonoma:        "d1d9d6c6b6e04ec433b22103bcab8170ba4f4129692c1d19aa7d99824e79d051"
+    sha256 cellar: :any,                 ventura:       "22d8baee39eb4a1ce7094d73ee636c7662aa6f0a9d814e5c11282a286f708a48"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "613c246f48282a253b59f8fbccf60497d656e794e98ab9e882f783e872b877f7"
+  end
+
   depends_on "cmake" => :build
   depends_on "aws-c-common"
 
