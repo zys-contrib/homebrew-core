18670a23df81531c1413d54e39fae984c15bb6cb
Michael Cho <michael@michaelcho.dev>
2024-11-30 13:33:21 -0500

    aws-c-mqtt 0.11.0 (new formula)

---
+++ Formula/a/aws-c-mqtt.rb
@@ -0,0 +1,42 @@
+class AwsCMqtt < Formula
+  desc "C99 implementation of the MQTT 3.1.1 specification"
+  homepage "https://github.com/awslabs/aws-c-mqtt"
+  url "https://github.com/awslabs/aws-c-mqtt/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "3854664c13896b6de3d56412f928435a4933259cb7fe62b10c1f497e6999333c"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "aws-c-common"
+  depends_on "aws-c-http"
+  depends_on "aws-c-io"
+
+  def install
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_MODULE_PATH=#{Formula["aws-c-common"].opt_lib}/cmake
+    ]
+    # Avoid linkage to `aws-c-cal` and `aws-c-compression`
+    args << "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-dead_strip_dylibs" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <aws/common/allocator.h>
+      #include <aws/mqtt/mqtt.h>
+
+      int main(void) {
+        struct aws_allocator *allocator = aws_default_allocator();
+        aws_mqtt_library_init(allocator);
+        aws_mqtt_library_clean_up();
+        return 0;
+      }
+    C
+    system ENV.cc, "test.c", "-o", "test", "-L#{lib}", "-laws-c-mqtt",
+                   "-L#{Formula["aws-c-common"].opt_lib}", "-laws-c-common"
+    system "./test"
+  end
+end
