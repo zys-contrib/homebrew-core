bc55f8a047aad00934f4fd2da5a06539652a050f
Michael Cho <michael@michaelcho.dev>
2024-11-30 13:50:37 -0500

    aws-crt-cpp 0.29.6 (new formula)

---
+++ Formula/a/aws-crt-cpp.rb
@@ -0,0 +1,52 @@
+class AwsCrtCpp < Formula
+  desc "C++ wrapper around the aws-c-* libraries"
+  homepage "https://github.com/awslabs/aws-crt-cpp"
+  url "https://github.com/awslabs/aws-crt-cpp/archive/refs/tags/v0.29.6.tar.gz"
+  sha256 "d8ac5455517487f35137ff5b35d5130238781febb08715d1e0754b90c642f7b7"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "aws-c-auth"
+  depends_on "aws-c-cal"
+  depends_on "aws-c-common"
+  depends_on "aws-c-event-stream"
+  depends_on "aws-c-http"
+  depends_on "aws-c-io"
+  depends_on "aws-c-mqtt"
+  depends_on "aws-c-s3"
+  depends_on "aws-c-sdkutils"
+  depends_on "aws-checksums"
+
+  def install
+    args = %W[
+      -DBUILD_DEPS=OFF
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_MODULE_PATH=#{Formula["aws-c-common"].opt_lib}/cmake
+    ]
+    # Avoid linkage to `aws-c-compression`
+    args << "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-dead_strip_dylibs" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <aws/crt/Allocator.h>
+      #include <aws/crt/Api.h>
+      #include <aws/crt/Types.h>
+      #include <aws/crt/checksum/CRC.h>
+
+      int main() {
+        Aws::Crt::ApiHandle apiHandle(Aws::Crt::DefaultAllocatorImplementation());
+        uint8_t data[32] = {0};
+        Aws::Crt::ByteCursor dataCur = Aws::Crt::ByteCursorFromArray(data, sizeof(data));
+        assert(0x190A55AD == Aws::Crt::Checksum::ComputeCRC32(dataCur));
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-L#{lib}", "-laws-crt-cpp"
+    system "./test"
+  end
+end
