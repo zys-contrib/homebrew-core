0fe6e8428175ec397a8a0782c4c9d2a87d3461a1
Michael Cho <michael@michaelcho.dev>
2024-11-30 00:12:46 -0500

    aws-c-io 0.15.3 (new formula)

---
+++ Formula/a/aws-c-io.rb
@@ -0,0 +1,55 @@
+class AwsCIo < Formula
+  desc "Event driven framework for implementing application protocols"
+  homepage "https://github.com/awslabs/aws-c-io"
+  url "https://github.com/awslabs/aws-c-io/archive/refs/tags/v0.15.3.tar.gz"
+  sha256 "d8cb4d7d3ec4fb27cbce158d6823a1f2f5d868e116f1d6703db2ab8159343c3f"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "aws-c-cal"
+  depends_on "aws-c-common"
+
+  on_linux do
+    depends_on "s2n"
+  end
+
+  def install
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_MODULE_PATH=#{Formula["aws-c-common"].opt_lib}/cmake
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <aws/io/io.h>
+      #include <aws/io/retry_strategy.h>
+      #include <aws/common/allocator.h>
+      #include <aws/common/error.h>
+      #include <assert.h>
+
+      int main(void) {
+        struct aws_allocator *allocator = aws_default_allocator();
+        aws_io_library_init(allocator);
+
+        struct aws_retry_strategy *retry_strategy = aws_retry_strategy_new_no_retry(allocator, NULL);
+        assert(NULL != retry_strategy);
+
+        int rv = aws_retry_strategy_acquire_retry_token(retry_strategy, NULL, NULL, NULL, 0);
+        assert(rv == AWS_OP_ERR);
+        assert(aws_last_error() == AWS_IO_RETRY_PERMISSION_DENIED);
+
+        aws_retry_strategy_release(retry_strategy);
+        aws_io_library_clean_up();
+        return 0;
+      }
+    C
+    system ENV.cc, "test.c", "-o", "test", "-L#{lib}", "-laws-c-io",
+                   "-L#{Formula["aws-c-common"].opt_lib}", "-laws-c-common"
+    system "./test"
+  end
+end
