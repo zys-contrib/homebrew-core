a2e5513045b51cba0b3864b56b2cfbe3893f249a
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-12-01 01:11:17 +0000

    aws-c-mqtt: add 0.11.0 bottle.

--- Formula/a/aws-c-mqtt.rb
+++ Formula/a/aws-c-mqtt.rb
@@ -5,6 +5,15 @@ class AwsCMqtt < Formula
   sha256 "3854664c13896b6de3d56412f928435a4933259cb7fe62b10c1f497e6999333c"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "f71ac6a29fa188ddd70bd7870f7665b4757e71fc82737cfe912c728939763168"
+    sha256 cellar: :any,                 arm64_sonoma:  "d8e0c8bc123dbd4cf4af878c3c89814800ae1d96c21407efc73a81e84e0d22a5"
+    sha256 cellar: :any,                 arm64_ventura: "419d0e2564f24fa36aa02b40bb3181bdc9021d18f039113890ccc6a182ae4f97"
+    sha256 cellar: :any,                 sonoma:        "5aeca1f69688ac8c0b8216d03758d202f5707672aa832748771708db4d3a3109"
+    sha256 cellar: :any,                 ventura:       "73e51202918b3786afd558b258e7df44f5af15427ce7df72e10ed0e8e7729858"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b29582b715e70f1563489af2613c25683a7c6e3188dd9ef53ac27d7d6484f21"
+  end
+
   depends_on "cmake" => :build
   depends_on "aws-c-common"
   depends_on "aws-c-http"
