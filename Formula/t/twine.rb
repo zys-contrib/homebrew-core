b76dbbc96090b1cb183588722e2ed9b5332e6830
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-01 03:23:49 +0000

    twine: update 6.0.1 bottle.

--- Formula/t/twine.rb
+++ Formula/t/twine.rb
@@ -9,12 +9,12 @@ class Twine < Formula
   head "https://github.com/pypa/twine.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "463a2ef6685385cfd0ac25e3d883ba9722d9fe1dfd4f05144ca13cc88f512b96"
-    sha256 cellar: :any,                 arm64_sonoma:  "b811639acab20cc9e33e1bbdb82c781d4d16b34d0687fe91dedb3e87684192ec"
-    sha256 cellar: :any,                 arm64_ventura: "0e38c2991a89795a6f139dafd1ce4fb9a2f135096dff52c874ebdcc0034b6810"
-    sha256 cellar: :any,                 sonoma:        "09553607fc1922923950de009e19d26ea3c78bc3539893c26c37fc014e64a10f"
-    sha256 cellar: :any,                 ventura:       "e5c62b12b1a138cee90f4b909f9708575a99a7451b53bfec08e3c9fb5caccb76"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a03701421c865e460daaadff8ef958d4df56c4d5778a6c8282ed9eff6baec09d"
+    sha256 cellar: :any,                 arm64_sequoia: "6dff759325c7f1603c79ca46882f6e0cb9bfee6366ebae605da842e900a59693"
+    sha256 cellar: :any,                 arm64_sonoma:  "f3dbf5104b23ef0c7a05c7167e7c1333dcc18dc84fbec2bb1330033237016efc"
+    sha256 cellar: :any,                 arm64_ventura: "227ed1cf4d96f4219797912f80a4b29c1ef117de6b07885041b73589ed1b4db3"
+    sha256 cellar: :any,                 sonoma:        "41f03b8ab1da44839e1bc19fefb72d02611c588a50f6db7a0e823d746a12d26a"
+    sha256 cellar: :any,                 ventura:       "abd7516363a24c1bd228c0adcf91c5aacbb39c42c8ed6ec713012f30db45a4d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "005112eb84f72ca2e0fc714a3f139da8036985658ca3809c3e13709c70f97ce7"
   end
 
   depends_on "rust" => :build
