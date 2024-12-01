42567c7a1c7beeb0c8f85d9247ff8a0678b5b956
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-12-01 01:11:16 +0000

    aws-c-auth: add 0.8.0 bottle.

--- Formula/a/aws-c-auth.rb
+++ Formula/a/aws-c-auth.rb
@@ -5,6 +5,15 @@ class AwsCAuth < Formula
   sha256 "217a0ebf8d7c5ad7e5f5ae814c2a371042164b64b4b9330c1c4bb2c6db1dbd33"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "f0e670a32f6332167df1da92300073e615ada5d97c3eb521749a90a8ad43f455"
+    sha256 cellar: :any,                 arm64_sonoma:  "b0f3c85c9c911a7ce9d6900a15906e6db65759d4dbc07f4080ea77c8adf70657"
+    sha256 cellar: :any,                 arm64_ventura: "c9fefe246aea02390515230b9db840099bebaf70de69ed01a75a31429f57b36a"
+    sha256 cellar: :any,                 sonoma:        "d24ada67482431127dfbf8073d2e32b5eba77b1a9ba02f0331604cbbd27f7c1d"
+    sha256 cellar: :any,                 ventura:       "d3466a15deda7825361c58485c83a02ee0edfa8880ca27aede5a388386b164c6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "71003d289a501c8e20696aac4cef02b4edca469f38eff38fafe5a0c84f75c296"
+  end
+
   depends_on "cmake" => :build
   depends_on "aws-c-cal"
   depends_on "aws-c-common"
