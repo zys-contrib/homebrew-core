a8977b2fbaaf32192a93af9b469f253add4d48b2
Patrick Linnane <patrick@linnane.io>
2024-12-02 20:38:31 +0000

    azure-core-cpp: add 1.14.1 bottle.

--- Formula/a/azure-core-cpp.rb
+++ Formula/a/azure-core-cpp.rb
@@ -10,6 +10,15 @@ class AzureCoreCpp < Formula
     regex(/^azure-core[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "35dd75437ffee884b48f62fcbf9b78d0166d08943990af00d3b73ec8847ee28b"
+    sha256 cellar: :any,                 arm64_sonoma:  "7cb7895078ff88f4bd2b1495bc567b06d9c2d7beb1ddbe97a245ae525a6aff2a"
+    sha256 cellar: :any,                 arm64_ventura: "01a5bb7fcd40b9e7203db81a3505dc44fc295fb48cabea1a2009b8bb63688197"
+    sha256 cellar: :any,                 sonoma:        "9808cb4439c393a94a69033c5190e999648b3824d0b38122609d727a0defffed"
+    sha256 cellar: :any,                 ventura:       "c641fe6c2e5a32df63d61ddc4a21ef04d04e25c92d42da3c434a02201ef67b72"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1901b330cd4461a948c90d730cb3dd3b9afda6495151712ab168bb1bcf5c72e"
+  end
+
   depends_on "cmake" => :build
   depends_on "openssl@3"
 
