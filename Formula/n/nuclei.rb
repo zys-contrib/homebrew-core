17e53bef36606db1664659ddf40e437a0383313b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-02 14:05:47 +0000

    nuclei 3.3.7

--- Formula/n/nuclei.rb
+++ Formula/n/nuclei.rb
@@ -1,8 +1,8 @@
 class Nuclei < Formula
   desc "HTTP/DNS scanner configurable via YAML templates"
   homepage "https://nuclei.projectdiscovery.io/"
-  url "https://github.com/projectdiscovery/nuclei/archive/refs/tags/v3.3.6.tar.gz"
-  sha256 "7fb42cf5c9c4fa8800d40a997466dbfeac9954e1cae8d98a7af25c19801eb113"
+  url "https://github.com/projectdiscovery/nuclei/archive/refs/tags/v3.3.7.tar.gz"
+  sha256 "3d12498f90b4babead541578f095c8f8aac7d08038073f0f239142356d3a4c79"
   license "MIT"
   head "https://github.com/projectdiscovery/nuclei.git", branch: "master"
 
