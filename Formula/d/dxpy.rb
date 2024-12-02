9d0f4784570e1c2d2f49e892dd20c7e76e8389aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-02 13:55:48 +0000

    dxpy 0.386.0
    dxpy: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/dxpy.rb
+++ Formula/d/dxpy.rb
@@ -3,8 +3,8 @@ class Dxpy < Formula
 
   desc "DNAnexus toolkit utilities and platform API bindings for Python"
   homepage "https://github.com/dnanexus/dx-toolkit"
-  url "https://files.pythonhosted.org/packages/18/8a/6266c7959fad535dc6168b59a62a4b7a6d0b2a217707ada2c2c4fb7d5e44/dxpy-0.385.0.tar.gz"
-  sha256 "6e0d06e9704e9845967caaf301e918496056ad58e15bd610fb02c3c4d775adcf"
+  url "https://files.pythonhosted.org/packages/90/34/f6361dc1c72a5a6ba83f3c65ddceed00a986bd92bf7add1603c3d76de3b9/dxpy-0.386.0.tar.gz"
+  sha256 "9568441861351bd590a2612c34137b266f63df2fab7ee0a88d566a4ee85c6128"
   license "Apache-2.0"
 
   bottle do
@@ -56,9 +56,6 @@ class Dxpy < Formula
     sha256 "10e511ea3a8c744631d3bd77e61eb17ed09304c413ad42cf6ddfa4c7787e8fe6"
   end
 
-  # Replace `pipes` usage for python 3.13: https://github.com/dnanexus/dx-toolkit/pull/1410
-  patch :DATA
-
   def install
     virtualenv_install_with_resources
   end
@@ -75,108 +72,3 @@ class Dxpy < Formula
     assert_match dxenv, shell_output("#{bin}/dx env")
   end
 end
-
-__END__
-diff --git a/dxpy/cli/exec_io.py b/dxpy/cli/exec_io.py
-index 982d60450..e1d927f01 100644
---- a/dxpy/cli/exec_io.py
-+++ b/dxpy/cli/exec_io.py
-@@ -22,7 +22,7 @@ from __future__ import print_function, unicode_literals, division, absolute_impo
- 
- # TODO: refactor all dx run helper functions here
- 
--import os, sys, json, collections, pipes
-+import os, sys, json, collections, shlex
- from ..bindings.dxworkflow import DXWorkflow
- 
- import dxpy
-@@ -327,7 +327,7 @@ def format_choices_or_suggestions(header, items, obj_class, initial_indent=' ' *
-         # TODO: in interactive prompts the quotes here may be a bit
-         # misleading. Perhaps it should be a separate mode to print
-         # "interactive-ready" suggestions.
--        return fill(header + ' ' + ', '.join([pipes.quote(str(item)) for item in items]),
-+        return fill(header + ' ' + ', '.join([shlex.quote(str(item)) for item in items]),
-                     initial_indent=initial_indent,
-                     subsequent_indent=subsequent_indent)
- 
-diff --git a/dxpy/utils/exec_utils.py b/dxpy/utils/exec_utils.py
-index ce0b9f5b7..8d02293b5 100644
---- a/dxpy/utils/exec_utils.py
-+++ b/dxpy/utils/exec_utils.py
-@@ -23,7 +23,7 @@ from __future__ import print_function, unicode_literals, division, absolute_impo
- import os, sys, json, re, collections, logging, argparse, string, itertools, subprocess, tempfile
- from functools import wraps
- from collections import namedtuple
--import pipes
-+import shlex
- 
- import dxpy
- from ..compat import USING_PYTHON2, open, Mapping
-@@ -435,7 +435,7 @@ class DXExecDependencyInstaller(object):
-                 dxpy.download_dxfile(bundle["id"], bundle["name"], project=dxpy.WORKSPACE_ID)
-             except dxpy.exceptions.ResourceNotFound:
-                 dxpy.download_dxfile(bundle["id"], bundle["name"])
--            self.run("dx-unpack {}".format(pipes.quote(bundle["name"])))
-+            self.run("dx-unpack {}".format(shlex.quote(bundle["name"])))
-         else:
-             self.log('Skipping bundled dependency "{name}" because it does not refer to a file'.format(**bundle))
- 
-diff --git a/dxpy/utils/file_load_utils.py b/dxpy/utils/file_load_utils.py
-index 89aed97cf..6f1566401 100644
---- a/dxpy/utils/file_load_utils.py
-+++ b/dxpy/utils/file_load_utils.py
-@@ -83,7 +83,7 @@ will download into the execution environment:
- from __future__ import print_function, unicode_literals, division, absolute_import
- 
- import json
--import pipes
-+import shlex
- import os
- import fnmatch
- import sys
-@@ -401,10 +401,6 @@ def analyze_bash_vars(job_input_file, job_homedir):
-     return file_key_descs, rest_hash
- 
- 
--#
--# Note: pipes.quote() to be replaced with shlex.quote() in Python 3
--# (see http://docs.python.org/2/library/pipes.html#pipes.quote)
--#
- def gen_bash_vars(job_input_file, job_homedir=None, check_name_collision=True):
-     """
-     :param job_input_file: path to a JSON file describing the job inputs
-@@ -427,7 +423,7 @@ def gen_bash_vars(job_input_file, job_homedir=None, check_name_collision=True):
-             result = json.dumps(dxpy.dxlink(elem))
-         else:
-             result = json.dumps(elem)
--        return pipes.quote(result)
-+        return shlex.quote(result)
- 
-     def string_of_value(val):
-         if isinstance(val, list):
-diff --git a/dxpy/utils/local_exec_utils.py b/dxpy/utils/local_exec_utils.py
-index 72d798136..6d1e6b0d9 100755
---- a/dxpy/utils/local_exec_utils.py
-+++ b/dxpy/utils/local_exec_utils.py
-@@ -16,7 +16,7 @@
- 
- from __future__ import print_function, unicode_literals, division, absolute_import
- 
--import os, sys, json, subprocess, pipes
-+import os, sys, json, subprocess, shlex
- import collections, datetime
- 
- import dxpy
-@@ -351,9 +351,9 @@ def run_one_entry_point(job_id, function, input_hash, run_spec, depends_on, name
-           if [[ $(type -t {function}) == "function" ]];
-           then {function};
-           else echo "$0: Global scope execution complete. Not invoking entry point function {function} because it was not found" 1>&2;
--          fi'''.format(homedir=pipes.quote(job_homedir),
--                       env_path=pipes.quote(os.path.join(job_env['HOME'], 'environment')),
--                       code_path=pipes.quote(environ['DX_TEST_CODE_PATH']),
-+          fi'''.format(homedir=shlex.quote(job_homedir),
-+                       env_path=shlex.quote(os.path.join(job_env['HOME'], 'environment')),
-+                       code_path=shlex.quote(environ['DX_TEST_CODE_PATH']),
-                        function=function)
-         invocation_args = ['bash', '-c', '-e'] + (['-x'] if environ.get('DX_TEST_X_FLAG') else []) + [script]
-     elif run_spec['interpreter'] == 'python2.7':
