c4d07a82338b05dc4ab596836bff23a274b246fb
John K. Luebs <john@luebs.org>
2024-12-03 21:37:35 -0600

    libsidplayfp 2.12.0 (new formula)
    Adds formula for libsidplayfp, an active fork of sidplay2. It is
    an emulator for the Commodore 64 chipset to play C64 SID music.
    
    libsidplayfp is packaged by all major Linux distribution (Debian,
    Fedora, Arch, and derivatives). It is an optional dependency
    of the audacious music player available in homebrew.
    
    libsidplayfp: update test
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/lib/libsidplayfp.rb
@@ -0,0 +1,43 @@
+class Libsidplayfp < Formula
+  desc "Library to play Commodore 64 music"
+  homepage "https://github.com/libsidplayfp/libsidplayfp"
+  url "https://github.com/libsidplayfp/libsidplayfp/releases/download/v2.12.0/libsidplayfp-2.12.0.tar.gz"
+  sha256 "bc4f4fa203dcf0736fe48c23dce9aa0db825370e5941e7595e4851efe6937cdc"
+  license "GPL-2.0-or-later"
+
+  head do
+    url "https://github.com/libsidplayfp/libsidplayfp.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "coreutils" => :build
+    depends_on "libtool" => :build
+    depends_on "xa" => :build
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "libgcrypt"
+
+  def install
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <iostream>
+      #include <sidplayfp/sidplayfp.h>
+
+      int main() {
+          sidplayfp play;
+          std::cout << LIBSIDPLAYFP_VERSION_MAJ << "."
+                    << LIBSIDPLAYFP_VERSION_MIN << "."
+                    << LIBSIDPLAYFP_VERSION_LEV;
+          return 0;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-L#{lib}", "-I#{include}", "-lsidplayfp", "-o", "test"
+    assert_equal version.to_s, shell_output("./test")
+  end
+end
