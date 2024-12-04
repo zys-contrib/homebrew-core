16f97f83d8b66bf8f686c35ea0c1fe4ca5570fb9
Rui Chen <rui@chenrui.dev>
2024-12-04 00:46:22 -0500

    seqkit: add completion support and version check
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/seqkit.rb
+++ Formula/s/seqkit.rb
@@ -19,9 +19,21 @@ class Seqkit < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./seqkit"
+
+    # generate_completions_from_executable(bin/"seqkit", "genautocomplete", "--shell")
+    # We do this because the command to generate completions doesn't print them
+    # to stdout and only writes them to a file
+    system bin/"seqkit", "genautocomplete", "--shell", "bash", "--file", "seqkit.bash"
+    system bin/"seqkit", "genautocomplete", "--shell", "zsh", "--file", "_seqkit"
+    system bin/"seqkit", "genautocomplete", "--shell", "fish", "--file", "seqkit.fish"
+    bash_completion.install "seqkit.bash" => "seqkit"
+    zsh_completion.install "_seqkit"
+    fish_completion.install "seqkit.fish"
   end
 
   test do
+    assert_match version.to_s, shell_output("#{bin}/seqkit version")
+
     resource "homebrew-testdata" do
       url "https://raw.githubusercontent.com/shenwei356/seqkit/e37d70a7e0ca0e53d6dbd576bd70decac32aba64/tests/seqs4amplicon.fa"
       sha256 "b0f09da63e3c677cc698d5cdff60e2d246368263c22385937169a9a4c321178a"
