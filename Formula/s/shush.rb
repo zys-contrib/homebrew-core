9f35d488edb84a4cd88eed899b44cf28118ef552
Rui Chen <rui@chenrui.dev>
2024-12-04 00:56:16 -0500

    shush: update go build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/shush.rb
+++ Formula/s/shush.rb
@@ -20,7 +20,7 @@ class Shush < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
