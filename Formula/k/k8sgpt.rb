f7bba5cc8a1ee6eb90066d449e6c8db01cbf366c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-04 04:08:47 +0000

    k8sgpt: update 0.3.47 bottle.

--- Formula/k/k8sgpt.rb
+++ Formula/k/k8sgpt.rb
@@ -7,12 +7,13 @@ class K8sgpt < Formula
   head "https://github.com/k8sgpt-ai/k8sgpt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "63b791e4f3661cfb751d998dee5d0745dfa90aea656c33e4f9a716d6342f7649"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7f90f6436b2856038a0753731033b0999b18f891279a683d11dc0e8793373d77"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "db013d60e5965e432c4327d1a30bae3d25fd14ac128916d99d1b07c34a449da9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f66fa475d61567f6cf39db74a5c6605ebd36a7d3cd373cafd1cbc8c5e41231ce"
-    sha256 cellar: :any_skip_relocation, ventura:       "e555ed5bd0ed6f172a1ac73d8d0f927f0785609ba85081305b11266a103379cd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e133f8e36e0ca4210d5ee4b67fee3fd393d72ce03bb8070d77638bc82d270fc1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ec9ccdbff82bf2761f13756c0daafe0e12bd4cda5750624451ec413404ff232"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "588c268d57554313fe57ca026da02df45799fcf3b187aeb46adbff24692275e2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "30c9a50027b0f1293a6f10100aea3a796876606db09add08a1a45806d2966fd8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "defece94ff1e0596d1c1253ca5f35295c5517a0d48a683caef8ab32e1b0592d1"
+    sha256 cellar: :any_skip_relocation, ventura:       "1294218977e4c850a1ec300cb3a402c6c0f64c5b3e1d2ce43952e719ebec33c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89d7692c563a34b3c60a3e0e809b62da76d5a5e42f51f36db858988d281defb3"
   end
 
   depends_on "go" => :build
