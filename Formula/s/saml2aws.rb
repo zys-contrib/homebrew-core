db74393928219709c806406b48aaa423138716ed
Rui Chen <rui@chenrui.dev>
2024-12-04 00:37:51 -0500

    saml2aws: update completion (add bash completion)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/saml2aws.rb
+++ Formula/s/saml2aws.rb
@@ -19,19 +19,9 @@ class Saml2aws < Formula
   def install
     ldflags = "-s -w -X main.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/saml2aws"
-    (zsh_completion/"_saml2aws").write <<~EOS
-      #compdef saml2aws
 
-      _saml2aws_bash_autocomplete() {
-          local cur prev opts base
-          COMPREPLY=()
-          cur="${COMP_WORDS[COMP_CWORD]}"
-          opts=$( ${COMP_WORDS[0]} --completion-bash ${COMP_WORDS[@]:1:$COMP_CWORD} )
-          COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-          return 0
-      }
-      complete -F _saml2aws_bash_autocomplete saml2aws
-    EOS
+    generate_completions_from_executable(bin/"saml2aws", shell_parameter_format: "--completion-script-",
+                                                         shells:                 [:bash, :zsh])
   end
 
   test do
