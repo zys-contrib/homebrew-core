512974d4db6dc52ba83f84fba2693e7ce99e03dd
Rui Chen <rui@chenrui.dev>
2024-12-05 14:43:32 +0000

    cobra-cli: add 1.3.0 bottle.

--- Formula/c/cobra-cli.rb
+++ Formula/c/cobra-cli.rb
@@ -6,6 +6,15 @@ class CobraCli < Formula
   license "Apache-2.0"
   head "https://github.com/spf13/cobra-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d05baad8dd6bcebac5c184b14d30a29ac84e15c8f5cb74a19f604c87d4a42869"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d05baad8dd6bcebac5c184b14d30a29ac84e15c8f5cb74a19f604c87d4a42869"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d05baad8dd6bcebac5c184b14d30a29ac84e15c8f5cb74a19f604c87d4a42869"
+    sha256 cellar: :any_skip_relocation, sonoma:        "66330dc06dd3a086ca90c0c5fa45e3b69295fbe30a029a70f4f7dc70c7b6daef"
+    sha256 cellar: :any_skip_relocation, ventura:       "66330dc06dd3a086ca90c0c5fa45e3b69295fbe30a029a70f4f7dc70c7b6daef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "116ebe675a0b8b1f1c398867a95cf3fa0c27b097e64ae5772e836c83113f4ddb"
+  end
+
   depends_on "go"
 
   def install
