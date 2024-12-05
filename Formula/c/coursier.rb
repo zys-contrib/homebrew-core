da8ad7d82ffaf2270bdddefcb8ac18ff3d8d7be7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-05 16:55:34 +0000

    coursier 2.1.20

--- Formula/c/coursier.rb
+++ Formula/c/coursier.rb
@@ -1,8 +1,8 @@
 class Coursier < Formula
   desc "Pure Scala Artifact Fetching"
   homepage "https://get-coursier.io/"
-  url "https://github.com/coursier/coursier/releases/download/v2.1.19/coursier.jar"
-  sha256 "5ac0b48426b04df1b7f033ee223cbfd0e622dac97ee86a51130b751b68fd9061"
+  url "https://github.com/coursier/coursier/releases/download/v2.1.20/coursier.jar"
+  sha256 "b025a180b5918e27fbc286f4a9ef7622ecd2bd0f7cf482c3522a71db7162e0f8"
   license "Apache-2.0"
 
   # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
