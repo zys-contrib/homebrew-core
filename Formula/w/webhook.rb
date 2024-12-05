7be18bd1df627f3d659f0cd6c1438ef41d5fa518
Rui Chen <rui@chenrui.dev>
2024-12-05 00:35:09 -0500

    webhook: update go build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/webhook.rb
+++ Formula/w/webhook.rb
@@ -18,7 +18,7 @@ class Webhook < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
