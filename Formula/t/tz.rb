baf9be7a3f6165918ba32daf4d713bebd1dc71c6
Rui Chen <rui@chenrui.dev>
2024-12-05 00:17:52 -0500

    tz: update go build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tz.rb
+++ Formula/t/tz.rb
@@ -20,7 +20,7 @@ class Tz < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
