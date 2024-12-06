206f7d6c22e8dd99626f47f34f049a901854eb72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-06 04:55:48 +0000

    render: add 1.0.0 bottle.

--- Formula/r/render.rb
+++ Formula/r/render.rb
@@ -6,6 +6,15 @@ class Render < Formula
   license "Apache-2.0"
   head "https://github.com/render-oss/cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "989266d826f6ec5c24297cc06d72d2ea9df4a302b2ba2e3e9f4b8d87ec20da82"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "989266d826f6ec5c24297cc06d72d2ea9df4a302b2ba2e3e9f4b8d87ec20da82"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "989266d826f6ec5c24297cc06d72d2ea9df4a302b2ba2e3e9f4b8d87ec20da82"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6678f2653f8a3704084dae3909135b55ca0e74071bdcc3b22528a1388ea160d6"
+    sha256 cellar: :any_skip_relocation, ventura:       "6678f2653f8a3704084dae3909135b55ca0e74071bdcc3b22528a1388ea160d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48f13c0765d3b920586ca77f1ae82e1d9084653bdd4039209c20935168f920c7"
+  end
+
   depends_on "go" => :build
 
   def install
