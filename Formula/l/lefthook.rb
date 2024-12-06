6faa5a54b10c2a198eccace8b4f0060ee6538e43
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-06 09:16:35 +0000

    lefthook: update 1.9.0 bottle.

--- Formula/l/lefthook.rb
+++ Formula/l/lefthook.rb
@@ -7,12 +7,12 @@ class Lefthook < Formula
   head "https://github.com/evilmartians/lefthook.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ecf99f9d5a58d03cb9612f765228d36b05eb1f99bc4ac9fa4c153ac0bd439042"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ecf99f9d5a58d03cb9612f765228d36b05eb1f99bc4ac9fa4c153ac0bd439042"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ecf99f9d5a58d03cb9612f765228d36b05eb1f99bc4ac9fa4c153ac0bd439042"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8e7d91e58bab87d6225899d04744f84c9f0cbd064b3dade52668d6375df29d14"
-    sha256 cellar: :any_skip_relocation, ventura:       "8e7d91e58bab87d6225899d04744f84c9f0cbd064b3dade52668d6375df29d14"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "27d02ad7c5696414c80d147a574ed53a7f69b81edc05cbcd03457d8bc4b6ec84"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ee53f8026e060991e3cdd7b87f79ab2a16043e21aac41b8ea38c04b7e7481d7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1ee53f8026e060991e3cdd7b87f79ab2a16043e21aac41b8ea38c04b7e7481d7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1ee53f8026e060991e3cdd7b87f79ab2a16043e21aac41b8ea38c04b7e7481d7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d728ef1bc57e5d19068849e409a8c248c82c7d0d12690e890ec4cdb679ef2ff1"
+    sha256 cellar: :any_skip_relocation, ventura:       "d728ef1bc57e5d19068849e409a8c248c82c7d0d12690e890ec4cdb679ef2ff1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1107de58c2daaf9ac251d9efe9462df51779b8dd5f8d34da3bb8fbd1365d75f6"
   end
 
   depends_on "go" => :build
