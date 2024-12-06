390afc50389f1b24043f81bc1aac3dfafb8bd966
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-06 18:01:34 +0000

    nebula: update 1.9.5 bottle.

--- Formula/n/nebula.rb
+++ Formula/n/nebula.rb
@@ -7,14 +7,12 @@ class Nebula < Formula
   head "https://github.com/slackhq/nebula.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "02628b6ec323eeaa31833f3561af8eba7679d57c082b8ab644f48e2937d2c28e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "02628b6ec323eeaa31833f3561af8eba7679d57c082b8ab644f48e2937d2c28e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "02628b6ec323eeaa31833f3561af8eba7679d57c082b8ab644f48e2937d2c28e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "02628b6ec323eeaa31833f3561af8eba7679d57c082b8ab644f48e2937d2c28e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "768c505ba1596f1973144647d81b7a4185315ec26fd1b00da632944d81580ced"
-    sha256 cellar: :any_skip_relocation, ventura:        "768c505ba1596f1973144647d81b7a4185315ec26fd1b00da632944d81580ced"
-    sha256 cellar: :any_skip_relocation, monterey:       "768c505ba1596f1973144647d81b7a4185315ec26fd1b00da632944d81580ced"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "901dc2cb116c094a96a28a2711da1791f9b2367365cfd7358464a79f2ae1ba24"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9be5d6d3107b432e480fee5d8de763b7a635f8405e89dc18d03e2eb87280c519"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9be5d6d3107b432e480fee5d8de763b7a635f8405e89dc18d03e2eb87280c519"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9be5d6d3107b432e480fee5d8de763b7a635f8405e89dc18d03e2eb87280c519"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f99c5d8ee04af63d8b66d8621a7fdade99320a79d40c858e41ae9d63b3afbb07"
+    sha256 cellar: :any_skip_relocation, ventura:       "f99c5d8ee04af63d8b66d8621a7fdade99320a79d40c858e41ae9d63b3afbb07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "261454c498815640ce24b1e470de31ec0edc17a37818b07f08c7c3037cc8d716"
   end
 
   depends_on "go" => :build
