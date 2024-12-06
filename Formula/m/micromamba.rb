433eb51e499942b529e00f89be3845eb6fe0a7c4
Rui Chen <rui@chenrui.dev>
2024-12-06 01:05:26 -0500

    micromamba: revision bump (simdjson 3.11.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/micromamba.rb
+++ Formula/m/micromamba.rb
@@ -4,6 +4,7 @@ class Micromamba < Formula
   url "https://github.com/mamba-org/mamba/archive/refs/tags/micromamba-2.0.4.tar.gz"
   sha256 "29281fe9b8fa99ecaa01d791b00889fb953fdafa154bbdf877a0858044334439"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/mamba-org/mamba.git", branch: "main"
 
   livecheck do
