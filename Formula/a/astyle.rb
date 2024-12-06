04d5cc386f0579068c5be405267c3de98296270f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-06 16:52:04 +0000

    astyle 3.6.5

--- Formula/a/astyle.rb
+++ Formula/a/astyle.rb
@@ -1,8 +1,8 @@
 class Astyle < Formula
   desc "Source code beautifier for C, C++, C#, and Java"
   homepage "https://astyle.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.6/astyle-3.6.4.tar.bz2"
-  sha256 "1e94b64f4f06461f9039d094aefe9d4b28c66d34916b27a456055e7d62d73702"
+  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.6/astyle-3.6.5.tar.bz2"
+  sha256 "f231cd641047adb6c01adb2511573326a8bb7a293d17b54c770550993deade8b"
   license "MIT"
   head "https://svn.code.sf.net/p/astyle/code/trunk/AStyle"
 
