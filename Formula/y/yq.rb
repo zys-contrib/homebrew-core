f3636704697800d5bb7c9d7ea3ae0704cb75732b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-07 08:11:58 +0000

    yq 4.44.6

--- Formula/y/yq.rb
+++ Formula/y/yq.rb
@@ -1,8 +1,8 @@
 class Yq < Formula
   desc "Process YAML, JSON, XML, CSV and properties documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  url "https://github.com/mikefarah/yq/archive/refs/tags/v4.44.5.tar.gz"
-  sha256 "1505367f4a6c0c4f3b91c6197ffed4112d29ef97c48d0b5e66530cfa851d3f0e"
+  url "https://github.com/mikefarah/yq/archive/refs/tags/v4.44.6.tar.gz"
+  sha256 "c0acef928168e5fdb26cd7e8320eddde822f30cf1942817f3f6b854dd721653f"
   license "MIT"
   head "https://github.com/mikefarah/yq.git", branch: "master"
 
