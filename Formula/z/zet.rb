93f6643d66265d641f1558411e759812e8d2040a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-07 07:43:05 +0000

    zet: update 2.0.1 bottle.

--- Formula/z/zet.rb
+++ Formula/z/zet.rb
@@ -11,15 +11,12 @@ class Zet < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "53c70e56c15f910f0da22bb3bc5203c5574ba2847bd79a940e6c0bec1ed98302"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5ad3c94f7c59db72d11e2443f81f65c81fe07985040da61df8266fbf0f02b187"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d5c53469bc92408c6554f1d2fe342752c54b038ce953a5b36b641973c1337284"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f9e32e3a4567f0e85ca9cdb39089afd19fc27eb14e12c36bc4a398494cfe292d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "680526ad804b26e3028dbef1d6ccc7bf97dba1cbb5d184d57d3b9cb27ff71aca"
-    sha256 cellar: :any_skip_relocation, ventura:        "0055f89e1ea739132aabddab22e2dd39318686938349b2f8682ccb3ac4c18463"
-    sha256 cellar: :any_skip_relocation, monterey:       "5fd368452ffd4879a2eafa2d1c068551685ca79c89d7e82b82cee222d20c49fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "77d31e1f9c755fed55bed70f31de9c61dced7aa03f178dd1d6f06bfb3d9a4b23"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "43ee895f1a866c5279689ea67039f268f036126cdaa95fce89ef694c67c9d469"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eaf4041c27353f5278d0b7f232040a91cd8d87e9a6eb0d6dd6417186f3ab121d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1b42203cb1f07a7f436b7c6ba9a98667ca1e55dd65157fe8fb445a3de4b3c9e0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4f7359f71bb633a4dca674758356c976b74cb8c9b0a78d705c07d33cbd157414"
+    sha256 cellar: :any_skip_relocation, ventura:       "7155960676e9ea9a1e323e80cf7dd1191a63951c097d1339d179873218519677"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "85c0892b0c2723950c55e1df613854ef10c21b708392084176ab0917c8a0ba7c"
   end
 
   depends_on "rust" => :build
