f0315da68304661c3e2fc164b8627698d84ab8ba
Rui Chen <rui@chenrui.dev>
2024-12-08 14:18:17 -0500

    ignite: update go build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/ignite.rb
+++ Formula/i/ignite.rb
@@ -18,7 +18,7 @@ class Ignite < Formula
   depends_on "node"
 
   def install
-    system "go", "build", "-mod=readonly", *std_go_args(output: bin/"ignite"), "./ignite/cmd/ignite"
+    system "go", "build", "-mod=readonly", *std_go_args(ldflags: "-s -w", output: bin/"ignite"), "./ignite/cmd/ignite"
   end
 
   test do
