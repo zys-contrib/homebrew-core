14097576b11d72f63b611343a5be7f6be11807ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-08 14:21:12 +0000

    tex-fmt: update 0.5.0 bottle.

--- Formula/t/tex-fmt.rb
+++ Formula/t/tex-fmt.rb
@@ -7,12 +7,12 @@ class TexFmt < Formula
   head "https://github.com/WGUNDERWOOD/tex-fmt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b0636bf40ec68b03b914d0dd4115a8230f6ffd1d3b6495cb19ce3dc304ca9ecc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5afc92da72d274f39bd1684150acea5a6000ad632b1234605cda190b5cff7a43"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5cf0767c4734a75ce5372f7cda16960d9ed983d9a34c33d1812fc41ada68424c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "236d4385a0656b16fe0fcb2e0ba8f6dec2833c89554c2e00a692a76d93b03965"
-    sha256 cellar: :any_skip_relocation, ventura:       "c8e1949a1da55a773b52cb1bd0d7454a9a09147c668438f12ff1e70c485a72b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9064b7e146abadfcff4001160ec5b4fb2c97a83d18007fa2afc0b07bc341afa7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3775b42e11ecb559f9bc5567f66de0f4038d3ffdbbd097bce1bbaf00894ff18"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de44711eff1377cee5c6d67c7e790895a461b3322ce72d04016c407efb59974e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c77ac12a9512163f1e465e30be889b9dc1b7de8e888a0db6bfcd30f42e93f948"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a0af7273123a4e0f87537581af71d8e4931136183cf52de960fc5e37a807f484"
+    sha256 cellar: :any_skip_relocation, ventura:       "7fc7b730ca6eb9acd969394fd5ea5d7d960f089c4c5abc480134b867dae537b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f815ad483f8c7dc60e51581f01945adb01f4a92c732882961e3960e62e0ade26"
   end
 
   depends_on "rust" => :build
