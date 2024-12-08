677fd1c768382cb91f27028cfe23321ec969f465
Patrick Linnane <patrick@linnane.io>
2024-12-08 07:51:53 +0000

    sequin: add 0.2.0 bottle.

--- Formula/s/sequin.rb
+++ Formula/s/sequin.rb
@@ -6,6 +6,15 @@ class Sequin < Formula
   license "MIT"
   head "https://github.com/charmbracelet/sequin.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "809be5b62e9e1c4afa7387c7eef4ecc59076a7638a7e5025fcf2debdcb2242d6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "809be5b62e9e1c4afa7387c7eef4ecc59076a7638a7e5025fcf2debdcb2242d6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "809be5b62e9e1c4afa7387c7eef4ecc59076a7638a7e5025fcf2debdcb2242d6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "60a7d172d0a73886457f1de58e443f15beb3e291eec974caef85d241d1cf70cf"
+    sha256 cellar: :any_skip_relocation, ventura:       "60a7d172d0a73886457f1de58e443f15beb3e291eec974caef85d241d1cf70cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4f9789f45c52ffc2b2c4890cc244d2da0c997aa71bce8f6c7632f621aab77e2"
+  end
+
   depends_on "go" => :build
 
   def install
