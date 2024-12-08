8c1d5abecbe66250d8c1afb2f10eb695776c1b03
Rui Chen <rui@chenrui.dev>
2024-12-08 14:35:43 -0500

    jvgrep: update go build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/j/jvgrep.rb
+++ Formula/j/jvgrep.rb
@@ -20,7 +20,7 @@ class Jvgrep < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
