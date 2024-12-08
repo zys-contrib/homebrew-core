95f7c742628a3ca073f616c5f12731ab6252eddb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-08 05:22:06 +0000

    codelimit: update 0.12.1 bottle.

--- Formula/c/codelimit.rb
+++ Formula/c/codelimit.rb
@@ -8,12 +8,12 @@ class Codelimit < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "17b55349fd9440e81999dfe2e8b3dc9ad3c6e2afbfd2add89e26cf9b6e7cfd21"
-    sha256 cellar: :any,                 arm64_sonoma:  "1c7f116fd67c3ea81f90bed277e9ce62cf1d9bd0c8ced2b9965d382ec9572c08"
-    sha256 cellar: :any,                 arm64_ventura: "f10a7d25c51f39d8786e453263c334edfdf52659fd2d367a19854beb0e184f53"
-    sha256 cellar: :any,                 sonoma:        "2af813192d5997ba110ca9fada93a696228c2e4deafe40f27e4726d2fd328d92"
-    sha256 cellar: :any,                 ventura:       "51ef2e6d62da2add164c247fdb3bd5651260714352496279c9dcfd7d869bd91e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ffb7c1b9122307bc892e97d6f19f790589bf55e679b7b9222d1d3befcd1b4c4a"
+    sha256 cellar: :any,                 arm64_sequoia: "82ffc46773c06b74d0221b3cba5867ff2c7ff85c5c38b0e027cbc42e108b56ba"
+    sha256 cellar: :any,                 arm64_sonoma:  "c113e7434bc139ada958de868769f6f9cddaabd23d97a9c90e5111c6d212fbc8"
+    sha256 cellar: :any,                 arm64_ventura: "2f81481ba455f927e9bbd2c0556dc640413090b96b9a3d0421824bbefbcf3a07"
+    sha256 cellar: :any,                 sonoma:        "6b215e1045fcb8f5c9e006c86f15feff6a1ddfdde6c8cff5b86a2f5cb9205dc1"
+    sha256 cellar: :any,                 ventura:       "8b8bde9ed26015d41aa0194c401bb1c88d3159de772be9050e9f4e0d6cd1c7e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cce2d9cbf013636fa5a26e5eb867d82a8b580513e3f09aca171feccd8db0cf2d"
   end
 
   depends_on "certifi"
