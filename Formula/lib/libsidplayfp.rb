719ecc09dc524fb2c4cd4eabde73ea101cafb7c2
Rui Chen <rui@chenrui.dev>
2024-12-09 03:29:59 +0000

    libsidplayfp: add 2.12.0 bottle.

--- Formula/lib/libsidplayfp.rb
+++ Formula/lib/libsidplayfp.rb
@@ -5,6 +5,15 @@ class Libsidplayfp < Formula
   sha256 "bc4f4fa203dcf0736fe48c23dce9aa0db825370e5941e7595e4851efe6937cdc"
   license "GPL-2.0-or-later"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "9ec485272ea954ac65d65dfa486b8c8bb8a750c8d9e512fbc8932534c19e9c5e"
+    sha256 cellar: :any,                 arm64_sonoma:  "66d7a7105e07d9edc9fbf861f2e4a6a2910bb45fc2953c70f7f6011770b80051"
+    sha256 cellar: :any,                 arm64_ventura: "6045375b29f669fddb8c36f5ab296a30b3aec238f3d45314f512d304564371f0"
+    sha256 cellar: :any,                 sonoma:        "85e9084be1da3e80a63220bb6333fb067c910816a19db40ccf85a88642a5ee24"
+    sha256 cellar: :any,                 ventura:       "52c66fe4af852e57a6144207bbe5929f79837114b86e4506bc0a19f5e3f4bd30"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34d0b7ca32d92c58eca1bf863b232b0937691481b178b4d6e62c11dabe634b17"
+  end
+
   head do
     url "https://github.com/libsidplayfp/libsidplayfp.git", branch: "master"
 
