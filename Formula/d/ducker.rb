b824ddf3932b4e7b6326fdc8c8eec1481e3fa27f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-09 22:55:36 +0000

    ducker 0.2.2

--- Formula/d/ducker.rb
+++ Formula/d/ducker.rb
@@ -1,8 +1,8 @@
 class Ducker < Formula
   desc "Slightly quackers Docker TUI based on k9s"
   homepage "https://github.com/robertpsoane/ducker"
-  url "https://github.com/robertpsoane/ducker/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "47c8ec1ad253e530f3cc858dba94b52607beae547a6892dacd43b0bdd4a615d2"
+  url "https://github.com/robertpsoane/ducker/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "23ab9c9588c65a7d650cdfcb73b3e3659d5943352599150445b3270b8a3b3241"
   license "MIT"
 
   bottle do
