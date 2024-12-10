f4bd95d6949fdd143006857be3cdad74e5f9b4d7
Ryan Rotter <rrotter@users.noreply.github.com>
2024-12-10 15:37:45 -0500

    kubernetes-cli@1.31 1.31.4 (new formula)

---
+++ Formula/k/kubernetes-cli@1.31.rb
@@ -0,0 +1,50 @@
+class KubernetesCliAT131 < Formula
+  desc "Kubernetes command-line interface"
+  homepage "https://kubernetes.io/docs/reference/kubectl/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      tag:      "v1.31.4",
+      revision: "a78aa47129b8539636eb86a9d00e31b2720fe06b"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(1\.31(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  # https://kubernetes.io/releases/patch-releases/#1-31
+  disable! date: "2025-10-28", because: :deprecated_upstream
+
+  depends_on "bash" => :build
+  depends_on "go" => :build
+
+  uses_from_macos "rsync" => :build
+
+  on_macos do
+    depends_on "coreutils" => :build
+  end
+
+  def install
+    ENV.prepend_path "PATH", Formula["coreutils"].libexec/"gnubin" if OS.mac? # needs GNU date
+    ENV["FORCE_HOST_GO"] = "1"
+    system "make", "WHAT=cmd/kubectl"
+    bin.install "_output/bin/kubectl"
+
+    generate_completions_from_executable(bin/"kubectl", "completion", base_name: "kubectl")
+
+    # Install man pages
+    # Leave this step for the end as this dirties the git tree
+    system "hack/update-generated-docs.sh"
+    man1.install Dir["docs/man/man1/*.1"]
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client --output=yaml 2>&1")
+    assert_match "gitTreeState: clean", version_output
+    assert_match stable.specs[:revision].to_s, version_output
+  end
+end
