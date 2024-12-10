df934827d5e7b3899b28922c84a4b612a1012d30
Bevan Kay <email@bevankay.me>
2024-12-10 23:29:46 +1100

    glib 2.82.3
    glib: add explicit zlib dependency
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: Sean Molenaar <sean@seanmolenaar.eu>
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Formula/g/glib.rb
+++ Formula/g/glib.rb
@@ -3,8 +3,8 @@ class Glib < Formula
 
   desc "Core application library for C"
   homepage "https://docs.gtk.org/glib/"
-  url "https://download.gnome.org/sources/glib/2.82/glib-2.82.2.tar.xz"
-  sha256 "ab45f5a323048b1659ee0fbda5cecd94b099ab3e4b9abf26ae06aeb3e781fd63"
+  url "https://download.gnome.org/sources/glib/2.82/glib-2.82.3.tar.xz"
+  sha256 "5d669bd01ad23b8ef8082c58a0d205f4007a9b9c42b0a69fe7aeb17bbee58ce4"
   license "LGPL-2.1-or-later"
 
   bottle do
@@ -28,6 +28,7 @@ class Glib < Formula
 
   uses_from_macos "flex" => :build # for gobject-introspection
   uses_from_macos "libffi", since: :catalina
+  uses_from_macos "zlib"
 
   on_macos do
     depends_on "gettext"
