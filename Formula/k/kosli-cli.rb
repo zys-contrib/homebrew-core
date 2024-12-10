beab3ddfa9a6f432b842f16142e814def5d31f9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-10 11:34:52 +0000

    kosli-cli: update 2.11.1 bottle.

--- Formula/k/kosli-cli.rb
+++ Formula/k/kosli-cli.rb
@@ -12,12 +12,12 @@ class KosliCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "95c8b25de1f2e383cf9e63a7fedf22b9872ee040344e6a11385a1ec2dea8d991"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "95c8b25de1f2e383cf9e63a7fedf22b9872ee040344e6a11385a1ec2dea8d991"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "95c8b25de1f2e383cf9e63a7fedf22b9872ee040344e6a11385a1ec2dea8d991"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c01271439bd4a5ef9a8d2c4dca87e614125c732a4f9a9895eaf7b68c44cb7399"
-    sha256 cellar: :any_skip_relocation, ventura:       "c01271439bd4a5ef9a8d2c4dca87e614125c732a4f9a9895eaf7b68c44cb7399"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b917954d0d43e69ac6e49fbec75408d622b1c42c039f11c75dceb54b3d38d699"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6461ceb7783eff4a0a02c18f658b10d8f879cc2caf1c43819fd434a6fc956ef7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f1fdef434e79c6a52689a68b3637a57944ce957502f4d73c90f435a63d721b8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "20b70988c29c160ac6493554c90a9cc1ec6b6b384aac02d46e5cf959e17d383c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ac9ce3bf4fce8989515957b800bd72220d79c579f0518c90068da52c1423ed4b"
+    sha256 cellar: :any_skip_relocation, ventura:       "5e0df82641074afc4e8838725d4d25134da71e36c95ea86aeadf632f88082c8c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "710549e983b3dda5d3eb380be29b63cc411acd9d2488eba1e58da8e4b99fa650"
   end
 
   depends_on "go" => :build
