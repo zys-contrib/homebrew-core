2879593a05b45702dcf19b90c67c75f9791f3945
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-10 05:07:09 +0000

    sponge: update 0.70 bottle.

--- Formula/s/sponge.rb
+++ Formula/s/sponge.rb
@@ -11,14 +11,12 @@ class Sponge < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "c2222a13d620121800f207f8af1b345e1cbf9e771c322e910e4f65b100c5d160"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "9c72b740a203d02fd12bd27949f90e1662fdbbf1c9e19abcde0b66b9d032a9b2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "82d216473d739f924c7b973b4aae9f311c481c3412ebe6f0c1d58f46186327e8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "35c308698a3368f0e2182e5a0a0b167222ed57f6bac9a0c28f1e13fc45e254b0"
-    sha256 cellar: :any_skip_relocation, sonoma:         "09d8cf8703e517cc9fb003c4ef0b7200a1542a064be631d6da30156cc7f1078f"
-    sha256 cellar: :any_skip_relocation, ventura:        "983d686c95825a8fe56952dadf415a6a0dbacacf281d0d0d51669539c5585f41"
-    sha256 cellar: :any_skip_relocation, monterey:       "7655e10c757e087426d388b1068a1b6dad16bf36c655bbd9436cff37dddaf816"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ebabd0385ae159408efc8187938abdba896fa7fbac002d1e2efcdb555f15b87"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "695617b8c9e56e08a5bda5a83859bd8b15db0075eaad8f9f94f980d9cbdfddbf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "32624015555fbcfe609e8bf7d203c43413dcf0d8568c27d3cbbc8612c40df98d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4f4e10bb9eea15e17aa64b895b04fa9422091065ba67588d5f915a718d59f8e6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a3725ce0dca9b5bc8666199d9a44b142be0eda23b9f9f66086f06298e24a36da"
+    sha256 cellar: :any_skip_relocation, ventura:       "49f2c6da0632f13e6dc655fe1ec9f1c261f2fafb5f0f692bc27e79c43658b9ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "12987019e4ea708b67334ec946a8cc5e7e052e87db10107cd7bbea862268dcbf"
   end
 
   conflicts_with "moreutils", because: "both install a `sponge` executable"
