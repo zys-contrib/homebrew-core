66c542b85ebd01ffc379597074be039a26f1f937
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-11 02:26:03 +0000

    hoverfly 1.10.6

--- Formula/h/hoverfly.rb
+++ Formula/h/hoverfly.rb
@@ -1,8 +1,8 @@
 class Hoverfly < Formula
   desc "API simulations for development and testing"
   homepage "https://hoverfly.io/"
-  url "https://github.com/SpectoLabs/hoverfly/archive/refs/tags/v1.10.5.tar.gz"
-  sha256 "38639779eda33b8924e23a8640c9b07821795bf7d0c5defd5a02614e60ace550"
+  url "https://github.com/SpectoLabs/hoverfly/archive/refs/tags/v1.10.6.tar.gz"
+  sha256 "116755ae5ac7af9c2a63ee2ab9b8fb26fe451af4722f26f95121de240167acd6"
   license "Apache-2.0"
   head "https://github.com/SpectoLabs/hoverfly.git", branch: "master"
 
