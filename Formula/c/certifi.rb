5109ac7c41e75de9c3eb2a2b6a9158a6964f3065
Michael Cho <michael@michaelcho.dev>
2024-12-10 20:06:34 -0500

    certifi: drop support for `python@3.11`

--- Formula/c/certifi.rb
+++ Formula/c/certifi.rb
@@ -15,7 +15,6 @@ class Certifi < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f1fc985a1c89bd40c73b17e3dfbf5483cb0417c8d4d12e2be66158a503ab169"
   end
 
-  depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
   depends_on "python@3.13" => [:build, :test]
   depends_on "ca-certificates"
@@ -25,6 +24,9 @@ class Certifi < Formula
   end
 
   def install
+    # Avoid difference in generated METADATA files across bottles
+    inreplace "README.rst", "/usr/local", HOMEBREW_PREFIX
+
     pythons.each do |python|
       python_exe = python.opt_libexec/"bin/python"
       system python_exe, "-m", "pip", "install", *std_pip_args(build_isolation: true), "."
@@ -34,6 +36,9 @@ class Certifi < Formula
       rm prefix/site_packages/"certifi/cacert.pem"
       (prefix/site_packages/"certifi").install_symlink Formula["ca-certificates"].pkgetc/"cert.pem" => "cacert.pem"
     end
+
+    # Revert first inreplace to avoid difference in README.rst across bottles
+    inreplace "README.rst", HOMEBREW_PREFIX, "/usr/local"
   end
 
   test do
