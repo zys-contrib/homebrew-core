ca644784362838e71ad42b59b9cf5bf4bdd532b6
Rui Chen <rui@chenrui.dev>
2024-12-11 16:09:19 +0000

    macmon: add 0.4.0 bottle.

--- Formula/m/macmon.rb
+++ Formula/m/macmon.rb
@@ -6,6 +6,12 @@ class Macmon < Formula
   license "MIT"
   head "https://github.com/vladkens/macmon.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f77e3d5f6eb464102626d590520d175fc6fc29ec416abfbf58cd13c46e7e01c2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a147275fad2dc8144ae79ff6ba2a9b475054df1268a6ec7d016dc91b8b374b9a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e5c39d15ed9bafd987ddcfaa895ac9402a35758a97b0dd0767acc5cfff8ff39d"
+  end
+
   depends_on "rust" => :build
   depends_on arch: :arm64
   depends_on :macos
