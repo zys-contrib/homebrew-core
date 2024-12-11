075fbae4f300746545c836cea37cf42788767363
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-11 18:17:51 +0000

    weaviate: update 1.28.0 bottle.

--- Formula/w/weaviate.rb
+++ Formula/w/weaviate.rb
@@ -6,12 +6,12 @@ class Weaviate < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f8ab5aa4c27a35e7a82aebb938b171b6229441037ec6e8e5aaa9e03eb6083498"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f8ab5aa4c27a35e7a82aebb938b171b6229441037ec6e8e5aaa9e03eb6083498"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f8ab5aa4c27a35e7a82aebb938b171b6229441037ec6e8e5aaa9e03eb6083498"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9dd0477b886b90520daea7dbd039198440e58291fd3fb10c6af762f554bc57dd"
-    sha256 cellar: :any_skip_relocation, ventura:       "9dd0477b886b90520daea7dbd039198440e58291fd3fb10c6af762f554bc57dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "766306e236ea56bdbc5672ca0d93004bc1f6e8081520d68c22022154067f8ad4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5802601806067d7a69307ce08eb5622de43bf3fb37e84f71eb97a6b31eba9fd0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5802601806067d7a69307ce08eb5622de43bf3fb37e84f71eb97a6b31eba9fd0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5802601806067d7a69307ce08eb5622de43bf3fb37e84f71eb97a6b31eba9fd0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "55529cf681230869df668a064b0122519d8abcf24d7a478f5a1457ab786e2540"
+    sha256 cellar: :any_skip_relocation, ventura:       "55529cf681230869df668a064b0122519d8abcf24d7a478f5a1457ab786e2540"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "15f283bf0ed26ab489bcfe78fec218cc69b7193af95868f849411f03a20d96c3"
   end
 
   depends_on "go" => :build
