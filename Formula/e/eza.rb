f7fb10aabe251c99f5d2cca495fef023fcdfc719
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-11 17:08:14 +0000

    eza 0.20.12

--- Formula/e/eza.rb
+++ Formula/e/eza.rb
@@ -1,8 +1,8 @@
 class Eza < Formula
   desc "Modern, maintained replacement for ls"
   homepage "https://github.com/eza-community/eza"
-  url "https://github.com/eza-community/eza/archive/refs/tags/v0.20.11.tar.gz"
-  sha256 "1a3ec9fb87c61e2369b46945ff1fafb7cbb5e6cb4693bb59003f4b628a93a04c"
+  url "https://github.com/eza-community/eza/archive/refs/tags/v0.20.12.tar.gz"
+  sha256 "c40edf02e662f6bba4997a3d497a30561c3d2de9a329f9869761acf8b7f69562"
   license "EUPL-1.2"
 
   bottle do
