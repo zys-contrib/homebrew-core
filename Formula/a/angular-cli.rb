99d29b8bc8cde2a3014595854d0f60b389e38554
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-12 20:22:53 +0000

    angular-cli: update 19.0.5 bottle.

--- Formula/a/angular-cli.rb
+++ Formula/a/angular-cli.rb
@@ -6,12 +6,12 @@ class AngularCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "411326632b56deb643f89a2b0f627937ada676497b1612c7305ef2b3b3b4cf36"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "411326632b56deb643f89a2b0f627937ada676497b1612c7305ef2b3b3b4cf36"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "411326632b56deb643f89a2b0f627937ada676497b1612c7305ef2b3b3b4cf36"
-    sha256 cellar: :any_skip_relocation, sonoma:        "518a556032bb8e5991a401a1e67ed55d6e2217fc99d3f32c95ccd100b00eac5c"
-    sha256 cellar: :any_skip_relocation, ventura:       "518a556032bb8e5991a401a1e67ed55d6e2217fc99d3f32c95ccd100b00eac5c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "411326632b56deb643f89a2b0f627937ada676497b1612c7305ef2b3b3b4cf36"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c070f3935f6f3af877beb3106ff70ba894157d007a68ff586145ddf6cedd2e85"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c070f3935f6f3af877beb3106ff70ba894157d007a68ff586145ddf6cedd2e85"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c070f3935f6f3af877beb3106ff70ba894157d007a68ff586145ddf6cedd2e85"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f70ad98861920523e06bb2f9d16d2e1f40bf7c9cd8e45390efa8e31d652743aa"
+    sha256 cellar: :any_skip_relocation, ventura:       "f70ad98861920523e06bb2f9d16d2e1f40bf7c9cd8e45390efa8e31d652743aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c070f3935f6f3af877beb3106ff70ba894157d007a68ff586145ddf6cedd2e85"
   end
 
   depends_on "node"
