d60af08e843ed1f06066ec9c2e99b6ed00607e52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-12 19:11:39 +0000

    usage: update 1.5.1 bottle.

--- Formula/u/usage.rb
+++ Formula/u/usage.rb
@@ -7,12 +7,12 @@ class Usage < Formula
   head "https://github.com/jdx/usage.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ed314bcd97767478fafeac88dc301d47585b24547396c6089175aa01e9fe0850"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0b83e3b8cee08465c539f1224bc981e82282754add0a3783754f718fd7fe684b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ec0841914ec2f689c036d22467167a617d3d0ca396092c41caed0c8f361d757a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "74fef6507270d96e4572804421c0f75ed9dd36fe87b1b5ded77d0e94e11bcbc6"
-    sha256 cellar: :any_skip_relocation, ventura:       "65e9ac1027e27fc0a8c3817566d337f7dc27606f610339e127711f5389508f5e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f4ed4ac6cd8d79b53c613c3866a007af6a328a0233f19a91117165308c33adc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "324183582f36e1835b2d26d4291902d48499005bf757d07e3a03445f4575e978"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ab4bd2f296549dacdc16b0e5fe20032c6a1b01fd9e8b911947c9ab98dfa1bed"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "09f6ccfca746951f62bc18c017566bb837f36319293040a2ab1840acc634049d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c987d6ad164cf1500d9d411d6bb7c2a54089c8132e803cd224659c0afbd5a341"
+    sha256 cellar: :any_skip_relocation, ventura:       "6ce8bea7a5faf8a6e60c6e71795d3a96891df0d16c16f4c7d7535927681f3109"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "df5520d359ef96b2f79f44a80883abc7b7d9eb7c47e3feeb8e5afe66f4c07008"
   end
 
   depends_on "rust" => :build
