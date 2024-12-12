a26276e2aaa9baab3dc36beaf3c3d424e231e93a
Rui Chen <rui@chenrui.dev>
2024-12-11 23:35:32 -0500

    bruno-cli 1.36.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/b/bruno-cli.rb
@@ -0,0 +1,20 @@
+class BrunoCli < Formula
+  desc "CLI of the open-source IDE For exploring and testing APIs"
+  homepage "https://www.usebruno.com/"
+  url "https://registry.npmjs.org/@usebruno/cli/-/cli-1.36.3.tgz"
+  sha256 "306960cae30c183fa219527c4898dad71ee6236c8651fa028b4db711d38e5d67"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    # supress `punycode` module deprecation warning, upstream issue: https://github.com/usebruno/bruno/issues/2229
+    (bin/"bru").write_env_script libexec/"bin/bru", NODE_OPTIONS: "--no-deprecation"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bru --version")
+    assert_match "You can run only at the root of a collection", shell_output("#{bin}/bru run 2>&1", 4)
+  end
+end
