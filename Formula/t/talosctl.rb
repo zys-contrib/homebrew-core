4c9a994ca4b7d00f24a2517d255a07153a0533cf
Rui Chen <rui@chenrui.dev>
2024-12-13 12:05:46 -0500

    talosctl 1.8.4 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/talosctl.rb
@@ -0,0 +1,30 @@
+class Talosctl < Formula
+  desc "CLI for out-of-band management of Kubernetes nodes created by Talos"
+  homepage "https://www.talos.dev/"
+  url "https://github.com/siderolabs/talos/archive/refs/tags/v1.8.4.tar.gz"
+  sha256 "f611046299fda6e9838b70f6a05abf7de9f3d5dcade2ec1b1bd7618b27c33910"
+  license "MPL-2.0"
+  head "https://github.com/siderolabs/talos.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/siderolabs/talos/pkg/machinery/version.Tag=#{version}
+      -X github.com/siderolabs/talos/pkg/machinery/version.Built=#{time.iso8601}
+
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/talosctl"
+
+    generate_completions_from_executable(bin/"talosctl", "completion")
+  end
+
+  test do
+    # version check also failed with `failed to determine endpoints` for server config
+    assert_match version.to_s, shell_output("#{bin}/talosctl version 2>&1", 1)
+
+    output = shell_output("#{bin}/talosctl list 2>&1", 1)
+    assert_match "error constructing client: failed to determine endpoints", output
+  end
+end
