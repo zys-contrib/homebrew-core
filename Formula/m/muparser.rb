3955e36202bc06858b13c548b5138d631bc2c408
Rui Chen <rui@chenrui.dev>
2024-12-13 01:51:49 -0500

    muparser 2.3.5
    autobump: add muparser
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/muparser.rb
+++ Formula/m/muparser.rb
@@ -1,8 +1,8 @@
 class Muparser < Formula
   desc "C++ math expression parser library"
   homepage "https://github.com/beltoforion/muparser"
-  url "https://github.com/beltoforion/muparser/archive/refs/tags/v2.3.4.tar.gz"
-  sha256 "0c3fa54a3ebf36dda0ed3e7cd5451c964afbb15102bdbcba08aafb359a290121"
+  url "https://github.com/beltoforion/muparser/archive/refs/tags/v2.3.5.tar.gz"
+  sha256 "20b43cc68c655665db83711906f01b20c51909368973116dfc8d7b3c4ddb5dd4"
   license "BSD-2-Clause"
   head "https://github.com/beltoforion/muparser.git", branch: "master"
 
