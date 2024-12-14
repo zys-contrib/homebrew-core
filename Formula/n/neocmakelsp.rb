3e7af25c5de1cab69d62f674cd366eae30dda1cc
Rui Chen <rui@chenrui.dev>
2024-12-14 15:09:26 -0500

    neocmakelsp 0.8.13
    autobump: add neocmakelsp
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/neocmakelsp.rb
+++ Formula/n/neocmakelsp.rb
@@ -1,8 +1,8 @@
 class Neocmakelsp < Formula
   desc "Another cmake lsp"
   homepage "https://neocmakelsp.github.io/"
-  url "https://github.com/neocmakelsp/neocmakelsp/archive/refs/tags/v0.8.12.2.tar.gz"
-  sha256 "74f6979fc6e3dd2e5d58cce314eb0e71280b7da2a4874795634a01c01e0fbab2"
+  url "https://github.com/neocmakelsp/neocmakelsp/archive/refs/tags/v0.8.13.tar.gz"
+  sha256 "57c68173ddd2362d14a272a57eba25892ce0be02ac864b2d7f572e06f77ab55a"
   license "MIT"
   head "https://github.com/neocmakelsp/neocmakelsp.git", branch: "master"
 
