53442b5b93fa3962c79b0128ba21aad8b64ffa46
Rui Chen <rui@chenrui.dev>
2024-12-14 18:05:58 -0500

    nqp 2024.12
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/nqp.rb
+++ Formula/n/nqp.rb
@@ -1,8 +1,8 @@
 class Nqp < Formula
   desc "Lightweight Raku-like environment for virtual machines"
   homepage "https://github.com/Raku/nqp"
-  url "https://github.com/Raku/nqp/releases/download/2024.10/nqp-2024.10.tar.gz"
-  sha256 "1fd1ea24af91fa64f72880af8351de5970c3499dc89699a435572eee0cf5f482"
+  url "https://github.com/Raku/nqp/releases/download/2024.12/nqp-2024.12.tar.gz"
+  sha256 "026ff25d7eaae299b2d644e46b389642774cdf51fd803047f4291731dc4b2477"
   license "Artistic-2.0"
 
   livecheck do
