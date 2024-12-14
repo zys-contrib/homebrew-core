761da3b952e12a7054f1df64b67993c0970f75d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-14 20:26:53 +0000

    porter: update 1.2.0 bottle.

--- Formula/p/porter.rb
+++ Formula/p/porter.rb
@@ -7,12 +7,13 @@ class Porter < Formula
   head "https://github.com/getporter/porter.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "11cf0d1bbe48519d60db8ea4ac1ae1828484b2648a02669351b39aebd28d6b7d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "11cf0d1bbe48519d60db8ea4ac1ae1828484b2648a02669351b39aebd28d6b7d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "11cf0d1bbe48519d60db8ea4ac1ae1828484b2648a02669351b39aebd28d6b7d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6f7f79ba1e2fdb1ec4ffa0578b640d96ca6a97fe987c12da6baeeabae2b5c6bf"
-    sha256 cellar: :any_skip_relocation, ventura:       "6f7f79ba1e2fdb1ec4ffa0578b640d96ca6a97fe987c12da6baeeabae2b5c6bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19fdcacb29459c665c770de54c49cd113149d5c98045d21cb857dccdebbab56b"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "31afcec22d4dd9f5fd1e659aa434e114cd7faad54eb78706d77437d8bf678497"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "31afcec22d4dd9f5fd1e659aa434e114cd7faad54eb78706d77437d8bf678497"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "31afcec22d4dd9f5fd1e659aa434e114cd7faad54eb78706d77437d8bf678497"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1ad4eeec835a2fd33908de7b1d103f8a85651b53acb890d70795cac58a16ffec"
+    sha256 cellar: :any_skip_relocation, ventura:       "1ad4eeec835a2fd33908de7b1d103f8a85651b53acb890d70795cac58a16ffec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "524afbfbbaaf9936335ccf8ccfb290eaa181f415eacf550c23f4d6195b345da2"
   end
 
   depends_on "go" => :build
