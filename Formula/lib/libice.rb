1edb83b108fc8bb76144fc7a1798abd6f5e0e8d6
Rui Chen <rui@chenrui.dev>
2024-12-14 14:59:22 -0500

    libice 1.1.2
    autobump: add libice
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libice.rb
+++ Formula/lib/libice.rb
@@ -1,8 +1,8 @@
 class Libice < Formula
   desc "X.Org: Inter-Client Exchange Library"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libICE-1.1.1.tar.xz"
-  sha256 "03e77afaf72942c7ac02ccebb19034e6e20f456dcf8dddadfeb572aa5ad3e451"
+  url "https://www.x.org/archive/individual/lib/libICE-1.1.2.tar.xz"
+  sha256 "974e4ed414225eb3c716985df9709f4da8d22a67a2890066bc6dfc89ad298625"
   license "MIT"
 
   bottle do
