a2d8b757c040bfbfa63b3825f6fbaa184f02b157
Rui Chen <rui@chenrui.dev>
2024-12-14 01:33:06 +0000

    gopeed: add 1.6.4 bottle.

--- Formula/g/gopeed.rb
+++ Formula/g/gopeed.rb
@@ -5,6 +5,15 @@ class Gopeed < Formula
   sha256 "73e9fdc4100d1a02d8b0ab442fddaf33e287b58dbdf916c033a312feddc1461f"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea70bdb86f8c81f89cf034115f9ecd39d8862eae0cd9524eddcfaccb74915981"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ec735127fa80d2d7a9e157ff9221751e49d9c5f682b90c15c3fab60f547e36c4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "42c5e12669da3487d7b87fbeb527d87eec81d0222311ec40886917452fa8051b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "46c572fea117f23df571cc32810f7d599a79f49ea4317d971b5a06d72a136933"
+    sha256 cellar: :any_skip_relocation, ventura:       "bdd7e0e6733ce902d3c27416187f717d102791484e4a0c26ecdfe36148969d68"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb0f579b6c15577d4c631e2037f9da903eaadee612237eff79a2690257211ec5"
+  end
+
   depends_on "go"
 
   def install
