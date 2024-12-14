52c7d36fb5b758febdfbdead09722e06e9c7dbf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-14 22:50:59 +0000

    chezmoi 2.56.0

--- Formula/c/chezmoi.rb
+++ Formula/c/chezmoi.rb
@@ -1,8 +1,8 @@
 class Chezmoi < Formula
   desc "Manage your dotfiles across multiple diverse machines, securely"
   homepage "https://chezmoi.io/"
-  url "https://github.com/twpayne/chezmoi/archive/refs/tags/v2.55.0.tar.gz"
-  sha256 "1fa36ce5ffff5a49e5a69c0910b866086749f359ae7afa06cbcc17b3bc9d8ca0"
+  url "https://github.com/twpayne/chezmoi/archive/refs/tags/v2.56.0.tar.gz"
+  sha256 "bc56294a3c47c0dfa5e22f05b1e8f6f656b650fd212d83975039a521b74e3d3c"
   license "MIT"
   head "https://github.com/twpayne/chezmoi.git", branch: "master"
 
