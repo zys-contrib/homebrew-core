7c3ebe8622a7533e8a4e923b4825ca757e302f5c
Rui Chen <rui@chenrui.dev>
2024-12-14 01:06:31 +0000

    parca: add 0.22.0 bottle.

--- Formula/p/parca.rb
+++ Formula/p/parca.rb
@@ -6,6 +6,15 @@ class Parca < Formula
   license "Apache-2.0"
   head "https://github.com/parca-dev/parca.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a731dff07d7576ff50e07c99a870a59293974a6c0b43bc0837f2e1df7833e9f7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a731dff07d7576ff50e07c99a870a59293974a6c0b43bc0837f2e1df7833e9f7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a731dff07d7576ff50e07c99a870a59293974a6c0b43bc0837f2e1df7833e9f7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "729e2c16c850b5afa8edb50f35133e53c865f514dc24def2757a6f181417b19c"
+    sha256 cellar: :any_skip_relocation, ventura:       "729e2c16c850b5afa8edb50f35133e53c865f514dc24def2757a6f181417b19c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9204200ba7fa0ab6dab75950c5da72037e4c9bbead8cbab429657825849c189"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
   depends_on "pnpm" => :build
