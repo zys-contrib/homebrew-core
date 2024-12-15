6fd062f0291588ff6743620acf7032aed5c29c73
Rui Chen <rui@chenrui.dev>
2024-12-15 04:39:52 +0000

    kool: add 3.3.0 bottle.

--- Formula/k/kool.rb
+++ Formula/k/kool.rb
@@ -6,6 +6,15 @@ class Kool < Formula
   license "MIT"
   head "https://github.com/kool-dev/kool.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c5e462831f84464e6dd2ccd9b412851430d07aefbbbebafa02b6ca924debdef6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c5e462831f84464e6dd2ccd9b412851430d07aefbbbebafa02b6ca924debdef6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c5e462831f84464e6dd2ccd9b412851430d07aefbbbebafa02b6ca924debdef6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b615c087c3c76e1824e4cc5e4589f013c7fa751b00615f6027fc5f7638ea9c8d"
+    sha256 cellar: :any_skip_relocation, ventura:       "b615c087c3c76e1824e4cc5e4589f013c7fa751b00615f6027fc5f7638ea9c8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d8bbefb970f59bb5d970eba3f2ecb5b82b607b06eeeaf4e1b93c8fe8497e598"
+  end
+
   depends_on "go" => :build
 
   def install
