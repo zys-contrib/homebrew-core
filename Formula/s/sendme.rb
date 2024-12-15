a16292916d44514b50b6e5932089f2fc6e7a64e2
Rui Chen <rui@chenrui.dev>
2024-12-15 04:18:59 +0000

    sendme: add 0.20.0 bottle.

--- Formula/s/sendme.rb
+++ Formula/s/sendme.rb
@@ -6,6 +6,15 @@ class Sendme < Formula
   license "MIT"
   head "https://github.com/n0-computer/sendme.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "009742856c7989e0c079dc07f8dd8e29cde7d090758b79439b77df5e5348c12d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "35e189943605eee575b0ddb7eea8a2b786ff73702b48f859751cc8bd28e5f6f6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "20920b258f088a7e190b751d0de305c0b52a5a60abc348d088202fabaf11c55c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "500c4f0987b1dd5fa6269f2a6b525f10f1a0ef09ce47316d7b4940c7069a60f5"
+    sha256 cellar: :any_skip_relocation, ventura:       "b6b8cb416cb23b3ab863d81ab788e582e76ddfbf0510f9db7d069d8d33c551ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5a1f013adf0db044df5de27999423c80d5edbb6926c8d7401f33d5ff7f21e10b"
+  end
+
   depends_on "rust" => :build
 
   def install
