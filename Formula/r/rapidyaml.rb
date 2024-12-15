121f5276d25a68db8bc3784c565c0b276dec52ce
Rui Chen <rui@chenrui.dev>
2024-12-15 22:51:47 +0000

    rapidyaml: add 0.7.2 bottle.

--- Formula/r/rapidyaml.rb
+++ Formula/r/rapidyaml.rb
@@ -5,6 +5,15 @@ class Rapidyaml < Formula
   sha256 "175b71074005a7d48ae03e973f1a7a5c104c588f193ba85044c5b4a97341aae0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "bed20b676b6e5e438347f920c4ee52f9b8690e33ecafdcdbc7cf31b147db691e"
+    sha256 cellar: :any,                 arm64_sonoma:  "8b9c7096e0cd01edd2b36cdbe6889062137862941df260698b320cdb54f57286"
+    sha256 cellar: :any,                 arm64_ventura: "5b026d039db10c29eb9658b555904ff8df6f2065d5824273626e23306402dc7f"
+    sha256 cellar: :any,                 sonoma:        "b442c77ff0044171081f73c28a7964ec2773ac913f8e49c6bb4c9d3e70ac2a9b"
+    sha256 cellar: :any,                 ventura:       "b2c36f5eaf706fd6195cbb2a33ad1fad2b93cb1877449e2ecf10fcb627b4239e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e25635ec621c1d6913c10b1303e0fa46d25a34a79d216eaab89a145748fc9f8"
+  end
+
   depends_on "cmake" => :build
 
   def install
