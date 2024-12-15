b5529b5f8076e4046a49141153c91fad54dde7cd
Kévin Dunglas <kevin@dunglas.fr>
2024-12-15 23:01:11 +0100

    watcher: add rpath
    Set CMAKE_INSTALL_RPATH to fix linking to the library.

--- Formula/w/watcher.rb
+++ Formula/w/watcher.rb
@@ -18,7 +18,7 @@ class Watcher < Formula
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
