14cfa20654892970e9aef3f22d0e3e10bef95a39
Rui Chen <rui@chenrui.dev>
2024-12-16 02:44:55 +0000

    azurehound: add 2.2.1 bottle.

--- Formula/a/azurehound.rb
+++ Formula/a/azurehound.rb
@@ -6,6 +6,15 @@ class Azurehound < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/SpecterOps/AzureHound.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "408035d9189ac52a7fe735555433d50a63e889fd0956043aead2613cc77062d6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "408035d9189ac52a7fe735555433d50a63e889fd0956043aead2613cc77062d6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "408035d9189ac52a7fe735555433d50a63e889fd0956043aead2613cc77062d6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8e4244e6edfc896cf06d696a6ba393c62a3fa0637147990aff78549c024d8c0b"
+    sha256 cellar: :any_skip_relocation, ventura:       "8e4244e6edfc896cf06d696a6ba393c62a3fa0637147990aff78549c024d8c0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57b3a7f62a9fc4f7564eb8241848a6b91821f73811e51576261c3ec5d8ba552a"
+  end
+
   depends_on "go" => :build
 
   def install
