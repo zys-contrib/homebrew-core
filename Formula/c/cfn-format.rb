1599f32a21a4c166b9444ff4adae920a615e5b25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-16 23:03:51 +0000

    cfn-format: update 1.20.0 bottle.

--- Formula/c/cfn-format.rb
+++ Formula/c/cfn-format.rb
@@ -10,13 +10,12 @@ class CfnFormat < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4a94e9c2ca4f6fdcca464759cbfa7efda78b54ffe3924186908ed2075b676315"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a94e9c2ca4f6fdcca464759cbfa7efda78b54ffe3924186908ed2075b676315"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "4a94e9c2ca4f6fdcca464759cbfa7efda78b54ffe3924186908ed2075b676315"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0c66ed9512ba2c3e3c4c61500f1d690fef539565f66f0937f0c7e15943ceaaf1"
-    sha256 cellar: :any_skip_relocation, ventura:       "0c66ed9512ba2c3e3c4c61500f1d690fef539565f66f0937f0c7e15943ceaaf1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "433f31f7b7a1fa8e3d2f965afacf2375b47ef13d7b8232068a3112013128cedf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d3dd33f784718ce6b03f989eac8ccbe124414c23f53f8f28dacc669e24f71277"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d3dd33f784718ce6b03f989eac8ccbe124414c23f53f8f28dacc669e24f71277"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d3dd33f784718ce6b03f989eac8ccbe124414c23f53f8f28dacc669e24f71277"
+    sha256 cellar: :any_skip_relocation, sonoma:        "11a3b72af2e83c3d6271fd5042e2bd40536450994ac0330dbd73172d78b031ea"
+    sha256 cellar: :any_skip_relocation, ventura:       "11a3b72af2e83c3d6271fd5042e2bd40536450994ac0330dbd73172d78b031ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8096b0dd8dea4de5ce74290768f27ce14b44d158fac69d240efd798da6937f0"
   end
 
   depends_on "go" => :build
