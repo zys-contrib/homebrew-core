208da51b15b44cbd20b8afc70d678fb492450010
Rui Chen <rui@chenrui.dev>
2024-12-15 21:41:19 -0500

    tun2proxy 0.7.10 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/tun2proxy.rb
@@ -0,0 +1,31 @@
+class Tun2proxy < Formula
+  desc "Tunnel (TUN) interface for SOCKS and HTTP proxies"
+  homepage "https://github.com/tun2proxy/tun2proxy"
+  url "https://github.com/tun2proxy/tun2proxy/archive/refs/tags/v0.7.10.tar.gz"
+  sha256 "792b1267d47380289745dca1300d2e42a1c6f3f33af475a54031b0b56c4ff61c"
+  license "MIT"
+  head "https://github.com/tun2proxy/tun2proxy.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tun2proxy-bin --version")
+
+    expected = if OS.mac?
+      "Operation not permitted (os error 1)"
+    else
+      "No such file or directory (os error 2)"
+    end
+
+    assert_match expected, shell_output("#{bin}/tun2proxy-bin --proxy socks5://127.0.0.1:1080 --tun utun4 2>&1", 1)
+  end
+end
