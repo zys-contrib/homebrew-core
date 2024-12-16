e2459a79cec23ebd2f0149449d482d18a56208c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-16 20:18:47 +0000

    cloudflare-wrangler2: update 3.96.0 bottle.

--- Formula/c/cloudflare-wrangler2.rb
+++ Formula/c/cloudflare-wrangler2.rb
@@ -6,12 +6,12 @@ class CloudflareWrangler2 < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24394980ccf292523f9d723f7495e366ad9d1def66341bd7f2c5346653442a8c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "24394980ccf292523f9d723f7495e366ad9d1def66341bd7f2c5346653442a8c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "24394980ccf292523f9d723f7495e366ad9d1def66341bd7f2c5346653442a8c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3072a6a911aae7aca42b8c5165dd1bed964edc6f29e77439c050a4b0a078aab2"
-    sha256 cellar: :any_skip_relocation, ventura:       "3072a6a911aae7aca42b8c5165dd1bed964edc6f29e77439c050a4b0a078aab2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "689bec0ae9dc84553cda5b02d45aa5bc3423a58e22b427d72fc288d91553e770"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "47c42b7a324508e081434e9aa29919d23410b605b31b8758e4f045988fcb6f26"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "47c42b7a324508e081434e9aa29919d23410b605b31b8758e4f045988fcb6f26"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "47c42b7a324508e081434e9aa29919d23410b605b31b8758e4f045988fcb6f26"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0caad71fd8105211571afebac69fb97b18182947e090bfe26252c2882387b15c"
+    sha256 cellar: :any_skip_relocation, ventura:       "0caad71fd8105211571afebac69fb97b18182947e090bfe26252c2882387b15c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "097565c4716ee9868a712eb2b1c777a6208ea28ccea9d98e4cd60a6f470a3a0a"
   end
 
   depends_on "node"
