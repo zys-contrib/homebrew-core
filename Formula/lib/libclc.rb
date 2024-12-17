47aa6bcc497256130d983686cfa454dac469389a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-17 17:55:27 +0000

    libclc: update 19.1.6 bottle.

--- Formula/lib/libclc.rb
+++ Formula/lib/libclc.rb
@@ -11,12 +11,12 @@ class Libclc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b4c3e941d8bcef5c9f84161437e7c0d35f63c98e20f88dd125db91ed8b8d0b2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b4c3e941d8bcef5c9f84161437e7c0d35f63c98e20f88dd125db91ed8b8d0b2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5b4c3e941d8bcef5c9f84161437e7c0d35f63c98e20f88dd125db91ed8b8d0b2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5b4c3e941d8bcef5c9f84161437e7c0d35f63c98e20f88dd125db91ed8b8d0b2"
-    sha256 cellar: :any_skip_relocation, ventura:       "5b4c3e941d8bcef5c9f84161437e7c0d35f63c98e20f88dd125db91ed8b8d0b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2deadf35baf81547213c75458f7f776d55d2c3797a63e6ef67030dcf10283e95"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d4e650a4a48a9b086daa704ac5fea47f5ab552cb9aee02d9ba47324a463db19f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d4e650a4a48a9b086daa704ac5fea47f5ab552cb9aee02d9ba47324a463db19f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d4e650a4a48a9b086daa704ac5fea47f5ab552cb9aee02d9ba47324a463db19f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d4e650a4a48a9b086daa704ac5fea47f5ab552cb9aee02d9ba47324a463db19f"
+    sha256 cellar: :any_skip_relocation, ventura:       "d4e650a4a48a9b086daa704ac5fea47f5ab552cb9aee02d9ba47324a463db19f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48bcec01c7418f3c60ec9d4d8a4a523b57674cc2a69ca30a330d3b1290e283dd"
   end
 
   depends_on "cmake" => :build
