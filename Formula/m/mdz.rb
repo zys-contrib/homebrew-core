77a48598be54dfe591b717c67c148d744a18bcb7
maxwelbm <maxwelbm@hotmail.com>
2024-12-17 16:48:19 -0300

    mdz 1.31.0 (new formula)
    mdz: fix style issues and SPDX license
    
    - Align concatenated strings properly to comply with style guidelines.
    - Update the license field to follow SPDX standards ('Apache-2.0').
    
    These changes resolve issues reported by 'brew audit' and 'brew style'.
    
    mdz: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/m/mdz.rb
@@ -0,0 +1,32 @@
+class Mdz < Formula
+  desc "CLI for the mdz ledger Open Source"
+  homepage "https://github.com/LerianStudio/midaz"
+  url "https://github.com/LerianStudio/midaz/archive/refs/tags/v1.31.0.tar.gz"
+  sha256 "16b0e877091c1ef6fc466e52c4281a9407081459264f3c8bb7917699687224fa"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/LerianStudio/midaz/components/mdz/pkg/environment.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "./components/mdz"
+  end
+
+  test do
+    assert_match "Mdz CLI #{version}", shell_output("#{bin}/mdz --version")
+
+    client_id = "9670e0ca55a29a466d31"
+    client_secret = "dd03f916cacf4a98c6a413d9c38ba102dce436a9"
+    url_api_auth = "http://127.0.0.1:8080"
+    url_api_ledger = "http://127.0.0.1:3000"
+
+    output = shell_output("#{bin}/mdz configure --client-id #{client_id} " \
+                          "--client-secret #{client_secret} --url-api-auth #{url_api_auth} " \
+                          "--url-api-ledger #{url_api_ledger}")
+
+    assert_match "client-id:       9670e0ca55a29a466d31", output
+    assert_match "client-secret:   dd03f916cacf4a98c6a413d9c38ba102dce436a9", output
+    assert_match "url-api-auth:    http://127.0.0.1:8080", output
+    assert_match "url-api-ledger:  http://127.0.0.1:3000", output
+  end
+end
