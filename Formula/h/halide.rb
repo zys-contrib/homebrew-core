9738f45a72116bc015a800f3406aa8bd573e876b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-17 02:27:02 +0000

    halide 19.0.0
    Co-authored-by: Alex Reinking <alex.reinking@gmail.com>

--- Formula/h/halide.rb
+++ Formula/h/halide.rb
@@ -1,10 +1,9 @@
 class Halide < Formula
   desc "Language for fast, portable data-parallel computation"
   homepage "https://halide-lang.org"
-  url "https://github.com/halide/Halide/archive/refs/tags/v18.0.0.tar.gz"
-  sha256 "1176b42a3e2374ab38555d9316c78e39b157044b5a8e765c748bf3afd2edb351"
+  url "https://github.com/halide/Halide/archive/refs/tags/v19.0.0.tar.gz"
+  sha256 "83bae1f0e24dc44d9d85014d5cd0474df2dd03975680894ce3fafd6e97dffee2"
   license "MIT"
-  revision 1
   head "https://github.com/halide/Halide.git", branch: "main"
 
   livecheck do
@@ -30,14 +29,10 @@ class Halide < Formula
   depends_on "lld"
   depends_on "llvm"
   depends_on "python@3.13"
+  depends_on "wabt"
 
-  # Check wabt version in `dependencies/wasm/CMakeLists.txt`.
-  # TODO: Ask upstream to support usage of a system-provided wabt.
-  # TODO: Do we really need a git checkout here?
-  resource "wabt" do
-    url "https://github.com/WebAssembly/wabt.git",
-        tag:      "1.0.33",
-        revision: "963f973469b45969ce198e0c86d3af316790a780"
+  on_macos do
+    depends_on "openssl@3"
   end
 
   def python3
@@ -45,24 +40,18 @@ class Halide < Formula
   end
 
   def install
-    builddir = buildpath/"build"
-    (builddir/"_deps/wabt-src").install resource("wabt")
-
     site_packages = prefix/Language::Python.site_packages(python3)
     rpaths = [rpath, rpath(source: site_packages/"halide")]
     args = [
       "-DCMAKE_INSTALL_RPATH=#{rpaths.join(";")}",
-      "-DHalide_INSTALL_PYTHONDIR=#{site_packages}",
-      "-DHalide_SHARED_LLVM=ON",
-      "-DPYBIND11_USE_FETCHCONTENT=OFF",
-      "-DFLATBUFFERS_USE_FETCHCONTENT=OFF",
-      "-DFETCHCONTENT_SOURCE_DIR_WABT=#{builddir}/_deps/wabt-src",
-      "-DCMAKE_SHARED_LINKER_FLAGS=-llldCommon",
+      "-DHalide_INSTALL_PYTHONDIR=#{site_packages}/halide",
+      "-DHalide_LLVM_SHARED_LIBS=ON",
+      "-DHalide_USE_FETCHCONTENT=OFF",
+      "-DWITH_TESTS=NO",
     ]
-    odie "CMAKE_SHARED_LINKER_FLAGS can be removed from `args`" if build.bottle? && version > "18.0.0"
-    system "cmake", "-S", ".", "-B", builddir, *args, *std_cmake_args
-    system "cmake", "--build", builddir
-    system "cmake", "--install", builddir
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
