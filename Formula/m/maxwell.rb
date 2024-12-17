a226c5670779ef411b2ad82cc5692e168e14a1cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-17 20:29:29 +0000

    maxwell: update 1.42.0 bottle.

--- Formula/m/maxwell.rb
+++ Formula/m/maxwell.rb
@@ -11,14 +11,7 @@ class Maxwell < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "af6bcc4cc48efddde658922e204638e6bd691768a8c52561ab70c2e541c8ade6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "03871ea82d9bb31d8b94e330ca4cd56fb418c97a2798a50e3363b8e2f88b5db2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "03871ea82d9bb31d8b94e330ca4cd56fb418c97a2798a50e3363b8e2f88b5db2"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "03871ea82d9bb31d8b94e330ca4cd56fb418c97a2798a50e3363b8e2f88b5db2"
-    sha256 cellar: :any_skip_relocation, sonoma:         "03871ea82d9bb31d8b94e330ca4cd56fb418c97a2798a50e3363b8e2f88b5db2"
-    sha256 cellar: :any_skip_relocation, ventura:        "03871ea82d9bb31d8b94e330ca4cd56fb418c97a2798a50e3363b8e2f88b5db2"
-    sha256 cellar: :any_skip_relocation, monterey:       "03871ea82d9bb31d8b94e330ca4cd56fb418c97a2798a50e3363b8e2f88b5db2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9bdc4d7e80f49c298f7314682b8b2fada302b5237a6f3fb7d40abf72513444d2"
+    sha256 cellar: :any_skip_relocation, all: "990392eb0fc2999456ca7586d3e0c6ccb777e1e490bfd6b0e306f1b68c114ac9"
   end
 
   depends_on "openjdk@11"
