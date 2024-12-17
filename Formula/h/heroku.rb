9fd163d8d07a8b88416fb2dc64861c600059229b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-17 21:30:51 +0000

    heroku: add 9.5.1 bottle.

--- Formula/h/heroku.rb
+++ Formula/h/heroku.rb
@@ -5,6 +5,14 @@ class Heroku < Formula
   sha256 "1dd66255bdc57f742c091671b2f84f6ee4faca8b9bd967d8528678182cf2a6b5"
   license "ISC"
 
+  bottle do
+    sha256 arm64_sequoia: "c6118421b58f3f14972958fe4fff97d486e633899aede9cd4abfd7a35d7a9a5f"
+    sha256 arm64_sonoma:  "e5e891e569dbb21b3e28b2aefc24700eed95ac97dfb4d49db162467c42ff2308"
+    sha256 arm64_ventura: "c0322f0b6c79f0c72941d7e1a59ab716136298abdd5f003e0cdf17fff6350d4a"
+    sha256 sonoma:        "bc923961ae796a43b7f2adf8885c946f8419a0628c286c137dc39cb62ebac1c0"
+    sha256 ventura:       "0edccb3453122d52dbd33a30bf052d3d5936e7db01dda63de13fea5a5f7abd36"
+  end
+
   depends_on "node"
   depends_on "terminal-notifier"
 
