3a420ccce0eb54ee144498b815a540def2cbe382
egibs <20933572+egibs@users.noreply.github.com>
2024-12-18 12:07:29 -0600

    malcontent 1.7.1
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- Formula/m/malcontent.rb
+++ Formula/m/malcontent.rb
@@ -1,8 +1,8 @@
 class Malcontent < Formula
   desc "Supply Chain Attack Detection, via context differential analysis and YARA"
   homepage "https://github.com/chainguard-dev/malcontent"
-  url "https://github.com/chainguard-dev/malcontent/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "a0d53b624fb6f786939e10740d7633d88da7443044a21ea706b8eb2ca15e59fc"
+  url "https://github.com/chainguard-dev/malcontent/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "e2f6b43715cefc00f1a07c84bb899fddc5defa3f69a1a68f5a4051680b0ca4b5"
   license "Apache-2.0"
 
   bottle do
