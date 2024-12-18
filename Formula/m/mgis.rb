be93af7fb8fc6cd14332cac1d6286f23f61b6ec9
Rui Chen <rui@chenrui.dev>
2024-12-18 17:23:53 -0500

    mgis 3.0
    autobump: add mgis
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mgis.rb
+++ Formula/m/mgis.rb
@@ -1,10 +1,9 @@
 class Mgis < Formula
   desc "Provide tools to handle MFront generic interface behaviours"
   homepage "https://thelfer.github.io/mgis/web/index.html"
-  url "https://github.com/thelfer/MFrontGenericInterfaceSupport/archive/refs/tags/MFrontGenericInterfaceSupport-2.2.tar.gz"
-  sha256 "b3776d7b3a534ca626525a42b97665f7660ae2b28ea57b3f53fd7e8538da1ceb"
+  url "https://github.com/thelfer/MFrontGenericInterfaceSupport/archive/refs/tags/MFrontGenericInterfaceSupport-3.0.tar.gz"
+  sha256 "dae915201fd20848b69745dabda1a334eb242d823af600825b8b010ddc597640"
   license any_of: ["LGPL-3.0-only", "CECILL-1.0"]
-  revision 5
   head "https://github.com/thelfer/MFrontGenericInterfaceSupport.git", branch: "master"
 
   bottle do
