8e9cf49104e42dea766e672eb01088d7c65ec093
Michael Cho <michael@michaelcho.dev>
2024-12-18 15:15:02 -0500

    fastnetmon: revision bump (protobuf 29.2)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.7.tar.gz"
   sha256 "c21fcbf970214dd48ee8aa11e6294e16bea86495085315e7b370a84b316d0af9"
   license "GPL-2.0-only"
-  revision 8
+  revision 9
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "bcfeae61b3264b242131db1504ec2b4c5a7890f7b33642f79af08bf8f9ff5f60"
