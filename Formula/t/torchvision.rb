9f78c2f3480786ee4f30e23c1ff9d8f36666f31d
Michael Cho <michael@michaelcho.dev>
2024-12-18 15:15:05 -0500

    torchvision: revision bump (protobuf 29.2)

--- Formula/t/torchvision.rb
+++ Formula/t/torchvision.rb
@@ -6,7 +6,7 @@ class Torchvision < Formula
   url "https://github.com/pytorch/vision/archive/refs/tags/v0.20.1.tar.gz"
   sha256 "7e08c7f56e2c89859310e53d898f72bccc4987cd83e08cfd6303513da15a9e71"
   license "BSD-3-Clause"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable
