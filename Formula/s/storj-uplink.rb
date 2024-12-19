df7b49add5cf31ca98b505181f6288544dd24a95
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-19 14:09:30 +0000

    storj-uplink 1.119.8

--- Formula/s/storj-uplink.rb
+++ Formula/s/storj-uplink.rb
@@ -1,8 +1,8 @@
 class StorjUplink < Formula
   desc "Uplink CLI for the Storj network"
   homepage "https://storj.io"
-  url "https://github.com/storj/storj/archive/refs/tags/v1.119.3.tar.gz"
-  sha256 "0db646795786a5162bed03832024a05fb063c6983af5743be6867f767d7f19f0"
+  url "https://github.com/storj/storj/archive/refs/tags/v1.119.8.tar.gz"
+  sha256 "18216b1e59ec670e8230bef8ecad8bca496a997644ecbebb0c7e8d10e82da8cf"
   license "AGPL-3.0-only"
 
   # Upstream creates stable releases and marks them as "pre-release" before
