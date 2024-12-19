fb0c29b54ef97abb89ef06bcb7b2e06e848a919f
Rui Chen <rui@chenrui.dev>
2024-12-19 13:37:53 +0000

    pscale: add 0.218.0 bottle.

--- Formula/p/pscale.rb
+++ Formula/p/pscale.rb
@@ -6,6 +6,15 @@ class Pscale < Formula
   license "Apache-2.0"
   head "https://github.com/planetscale/cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e5589751ad82bf89c09bfa2face8ef12026bc34e8a6f38ee9158f084b9eccc4a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2018d4c58acb3fea7e3d700e74a3904906074696362eca8a28c8214cbf6f7ccc"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2d3116807a07a1b7074cb0fe97ac34affeec77ebdc099db5929ee6eb2b1b1cae"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7e7306f487b6361a8667d2096454a2d961dc44661b6c6221a88337ca034e70d6"
+    sha256 cellar: :any_skip_relocation, ventura:       "8be107f8ce574bd1f5ed1aa253fc3921a64cd8d5dc84fa53414d44725a723c7d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a2e3bf08a91d8704e479736d2ab2f0d6faabc244f6908718fa3106b378096bed"
+  end
+
   depends_on "go" => :build
 
   def install
