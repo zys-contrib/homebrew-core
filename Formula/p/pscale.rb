1da7d99f7c7bf5a3cb0cb6bdb616e54521827d6e
Rui Chen <rui@chenrui.dev>
2024-12-18 22:48:14 -0500

    pscale 0.218.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/pscale.rb
@@ -0,0 +1,23 @@
+class Pscale < Formula
+  desc "CLI for PlanetScale Database"
+  homepage "https://www.planetscale.com/"
+  url "https://github.com/planetscale/cli/archive/refs/tags/v0.218.0.tar.gz"
+  sha256 "dca5d5cce7e001c46ea8e39dc8717185d4818861c093d1d4a9c83f1efc41b362"
+  license "Apache-2.0"
+  head "https://github.com/planetscale/cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/pscale"
+
+    generate_completions_from_executable(bin/"pscale", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pscale version")
+
+    assert_match "Error: not authenticated yet", shell_output("#{bin}/pscale org list 2>&1", 2)
+  end
+end
