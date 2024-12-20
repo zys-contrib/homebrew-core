25af88cf91caf5e42f30eb5a9ec7609dbe2cdae6
Patrick Linnane <patrick@linnane.io>
2024-12-20 11:49:54 -0800

    gh: revision bump
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/g/gh.rb
+++ Formula/g/gh.rb
@@ -1,9 +1,10 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://cli.github.com/"
-  url "https://github.com/cli/cli/archive/refs/tags/2.64.0.tar.gz"
+  url "https://github.com/cli/cli/archive/refs/tags/v2.64.0.tar.gz"
   sha256 "229fd8fc51325ebb5a357af6af116094d6be6a5f1e0f0923b7892ed01b208abb"
   license "MIT"
+  revision 1
   head "https://github.com/cli/cli.git", branch: "trunk"
 
   livecheck do
