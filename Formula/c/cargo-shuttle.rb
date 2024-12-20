046cebf167982f017ebe5000f9c8ad0a55512251
Rui Chen <rui@chenrui.dev>
2024-12-20 23:44:43 +0000

    cargo-shuttle: add 0.49.0 bottle.

--- Formula/c/cargo-shuttle.rb
+++ Formula/c/cargo-shuttle.rb
@@ -6,6 +6,15 @@ class CargoShuttle < Formula
   license "Apache-2.0"
   head "https://github.com/shuttle-hq/shuttle.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "7d7df9f8bd0165fa7dc319a5c292a1963dbbbd02fb9acc9a82be84a324069497"
+    sha256 cellar: :any,                 arm64_sonoma:  "e8ea01a46ba6b94af54656b0a97ac89258031a9a364aeb6a92c9cf995132a2aa"
+    sha256 cellar: :any,                 arm64_ventura: "d67182c4ef74a39a5dcdf99b2b6cd1405211f098b2a41d2ba7482cbcd954abb3"
+    sha256 cellar: :any,                 sonoma:        "562c9bdcb385fb3d89567b28e90bd5e6eb6a7695b88c3ee2b1a5c6ae6bb07220"
+    sha256 cellar: :any,                 ventura:       "97723f1be157be4af14ed4d76c82b4fa210a8b1252b57362fce0273654576339"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92e199d3de11901d1ebb0d2bb4b475ad9a6d9d1157d858c159773c0bc6b7c992"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "libgit2"
