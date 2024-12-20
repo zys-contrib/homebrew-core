bfc383a9f797e37c0ad984a57904e7661c325432
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-20 17:17:52 +0000

    wayland-protocols 1.39

--- Formula/w/wayland-protocols.rb
+++ Formula/w/wayland-protocols.rb
@@ -1,8 +1,8 @@
 class WaylandProtocols < Formula
   desc "Additional Wayland protocols"
   homepage "https://wayland.freedesktop.org"
-  url "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.38/downloads/wayland-protocols-1.38.tar.xz"
-  sha256 "ff17292c05159d2b20ce6cacfe42d7e31a28198fa1429a769b03af7c38581dbe"
+  url "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.39/downloads/wayland-protocols-1.39.tar.xz"
+  sha256 "e1dcdcbbf08e2e0a8a02ee5d9a0be3a6aafc39a4b51fa7e0d2f1a16411cb72fa"
   license "MIT"
 
   livecheck do
