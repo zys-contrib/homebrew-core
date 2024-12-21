fed731d52ec0b6739f99db394f43e0cfa43066b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-21 06:34:34 +0000

    navidrome: update 0.54.1 bottle.

--- Formula/n/navidrome.rb
+++ Formula/n/navidrome.rb
@@ -7,13 +7,12 @@ class Navidrome < Formula
   head "https://github.com/navidrome/navidrome.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "257ab85c8b8644bcd0c3efacf5f9baaab6b03bd340ac2fb054b617d493e7af10"
-    sha256 cellar: :any,                 arm64_sonoma:  "bd075df7bf8f6646a0845c593d3c848880b010c326ac471a511b3a71e622db37"
-    sha256 cellar: :any,                 arm64_ventura: "5502c591958cd682ed5641a77ff12c2f705551b09e02eafa4ccee21fe291f17b"
-    sha256 cellar: :any,                 sonoma:        "b04fb011646ab0dc5e9f830e2f1cae9bfff0e17e8d55783ac3f89bcda586c0d9"
-    sha256 cellar: :any,                 ventura:       "528486a0908d40a33c92c42882b8008301c647ff20df809ddc7f353f24d907c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0b621aae535c95904be115de7e3b8c640c9b1b01906295b54ab917bf5a5b3f5"
+    sha256 cellar: :any,                 arm64_sequoia: "8300ee77926f4019cc102efaef2211202cdb13730ec4b7f466155819326b6ffc"
+    sha256 cellar: :any,                 arm64_sonoma:  "5aafeddb57e26fd7e6a38f05086c03666deeba1ce5355c4e2f779d2475cbb5e0"
+    sha256 cellar: :any,                 arm64_ventura: "abc7b433c2955b63d6e5462432eea0e00d02b2631909bee6e696b1de8e7d2533"
+    sha256 cellar: :any,                 sonoma:        "2f7265f468f42b0696ab28827b25bdbb9cb8135fcd2e1f85017ea0c3eb54f592"
+    sha256 cellar: :any,                 ventura:       "680367d67e967db95b2c542c12d1484404913a9766e08cba133edbec590390d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af571fcca73be08d597684cd276a01558fc92d9450e75a81fe53620dc409cbd7"
   end
 
   depends_on "go" => :build
