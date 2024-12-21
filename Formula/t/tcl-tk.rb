dee08d0166c0fa4ae5965b44f89da204b6b08a21
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-21 17:10:49 +0000

    tcl-tk 9.0.1

--- Formula/t/tcl-tk.rb
+++ Formula/t/tcl-tk.rb
@@ -1,11 +1,10 @@
 class TclTk < Formula
   desc "Tool Command Language"
   homepage "https://www.tcl-lang.org"
-  url "https://downloads.sourceforge.net/project/tcl/Tcl/9.0.0/tcl9.0.0-src.tar.gz"
-  mirror "https://fossies.org/linux/misc/tcl9.0.0-src.tar.gz"
-  sha256 "3bfda6dbaee8e9b1eeacc1511b4e18a07a91dff82d9954cdb9c729d8bca4bbb7"
+  url "https://downloads.sourceforge.net/project/tcl/Tcl/9.0.1/tcl9.0.1-src.tar.gz"
+  mirror "https://fossies.org/linux/misc/tcl9.0.1-src.tar.gz"
+  sha256 "a72b1607d7a399c75148c80fcdead88ed3371a29884181f200f2200cdee33bbc"
   license "TCL"
-  revision 1
 
   livecheck do
     url :stable
@@ -39,6 +38,10 @@ class TclTk < Formula
   resource "critcl" do
     url "https://github.com/andreas-kupries/critcl/archive/refs/tags/3.3.1.tar.gz"
     sha256 "d970a06ae1cdee7854ca1bc571e8b5fe7189788dc5a806bce67e24bbadbe7ae2"
+
+    livecheck do
+      regex(/^v?(\d+(?:\.\d+)+)$/i)
+    end
   end
 
   resource "tcllib" do
@@ -48,16 +51,17 @@ class TclTk < Formula
 
   # There is no tcltls release compatible with TCL 9 so using latest
   # check-in at https://core.tcl-lang.org/tcltls/timeline
+  # Ref: https://core.tcl-lang.org/tcltls/tktview/f5a0fe8ddf
   # Ref: https://sourceforge.net/p/tcl/mailman/tcl-core/thread/eab3a8bf-b846-45ef-a80c-6bc94d6dfe91@elmicron.de/
   resource "tcltls" do
-    url "https://core.tcl-lang.org/tcltls/tarball/6d3664930c/tcltls-6d3664930c.tar.gz"
-    sha256 "dfd1401f8b0bfa0f2992d480b4e8ba085a4a17dc54e6b0db080e8bd6bdda725f"
+    url "https://core.tcl-lang.org/tcltls/tarball/e03e54ee87/tcltls-e03e54ee87.tar.gz"
+    sha256 "db473afa98924c0a2b44ecacea35bb2609e6810de1df389ad55bb3688023f8d1"
   end
 
   resource "tk" do
-    url "https://downloads.sourceforge.net/project/tcl/Tcl/9.0.0/tk9.0.0-src.tar.gz"
-    mirror "https://fossies.org/linux/misc/tk9.0.0-src.tar.gz"
-    sha256 "f166e3c20773c82243f753cef4b091d05267cb7f87da64be88cb2ca5a2ba027e"
+    url "https://downloads.sourceforge.net/project/tcl/Tcl/9.0.1/tk9.0.1-src.tar.gz"
+    mirror "https://fossies.org/linux/misc/tk9.0.1-src.tar.gz"
+    sha256 "d6f01a4d598bfc6398be9584e1bab828c907b0758db4bbb351a1429106aec527"
   end
 
   # "https://downloads.sourceforge.net/project/incrtcl/%5Bincr%20Tcl_Tk%5D-4-source/itk%204.1.0/itk4.1.0.tar.gz"
@@ -66,6 +70,7 @@ class TclTk < Formula
   # so we use the GitHub mirror which is easier to access than Fossil
   resource "itk4" do
     url "https://github.com/tcltk/itk/archive/refs/tags/itk-4-2-3.tar.gz"
+    version "4.2.3"
     sha256 "3eea66dfc57259d85ad741a01786b70ae4b3ae9774d27e24f7d0917c7451e94b"
   end
 
