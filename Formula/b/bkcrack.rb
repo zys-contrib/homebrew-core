c2d25209d0e3c2b82cb3cb9ce7c99b35e0ee0a8a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-21 19:51:02 +0000

    bkcrack 1.7.1

--- Formula/b/bkcrack.rb
+++ Formula/b/bkcrack.rb
@@ -1,8 +1,8 @@
 class Bkcrack < Formula
   desc "Crack legacy zip encryption with Biham and Kocher's known plaintext attack"
   homepage "https://github.com/kimci86/bkcrack"
-  url "https://github.com/kimci86/bkcrack/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "6d5f6d4aca155d232d0dd9c33618797c973883a95e078beb92287470101d452f"
+  url "https://github.com/kimci86/bkcrack/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "1ed073f9640baac4ba0156a00359e5a08e784cacd9f7de0bd07e2f49616c7a6a"
   license "Zlib"
   head "https://github.com/kimci86/bkcrack.git", branch: "master"
 
