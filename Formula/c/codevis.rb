59932c290d318314d8788460fa585924ec38ad0a
Rui Chen <rui@chenrui.dev>
2024-12-22 03:06:11 +0000

    codevis: add 0.8.4 bottle.

--- Formula/c/codevis.rb
+++ Formula/c/codevis.rb
@@ -6,6 +6,15 @@ class Codevis < Formula
   license "MIT"
   head "https://github.com/sloganking/codevis.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "809aab83526ed594b872942f4edb8cdae2530d7f9abb37fcf148690bf37adbe0"
+    sha256 cellar: :any,                 arm64_sonoma:  "51ea40fc3d8a3ec373285257f809f4b3eb034b1ca70fd8929600c71df31ae06e"
+    sha256 cellar: :any,                 arm64_ventura: "ea1a0d87b40ef80b9703fd13aea536c990a22486e29ffd3da58fe69d358d67ff"
+    sha256 cellar: :any,                 sonoma:        "fb7c8b97b77b706314852bd71838810c67e783903409ce94edf69b00ad1a36a4"
+    sha256 cellar: :any,                 ventura:       "acc82b560737c762384696f71560573a66d2327f827265819ebf99c52851bdb2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de3939e0b77b99e6d62c786955215be6273d5cedae6ccc7e63c3b9a775f7c2cd"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "oniguruma"
