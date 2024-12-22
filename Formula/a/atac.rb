9a8d464581cffac6502f2b17da86babc423e2718
Rui Chen <rui@chenrui.dev>
2024-12-22 02:46:55 +0000

    atac: add 0.18.1 bottle.

--- Formula/a/atac.rb
+++ Formula/a/atac.rb
@@ -6,6 +6,15 @@ class Atac < Formula
   license "MIT"
   head "https://github.com/Julien-cpsn/ATAC.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "ce5f3928fbc429786e273084c33babc88f907d0175f747d625a852197db074af"
+    sha256 cellar: :any,                 arm64_sonoma:  "956cf68c882c25a7212e4c9d6d2312005e9f5e271565c77a9bb7f7f8e24627a5"
+    sha256 cellar: :any,                 arm64_ventura: "6b80f5ab9dcadfab3e7670a00446bc466bdedb77bb95c068fa13d883fd208b58"
+    sha256 cellar: :any,                 sonoma:        "d1bc5efb7dfc5b69ee1b482943c58c38df7e3ae49364edde5ea162b173978341"
+    sha256 cellar: :any,                 ventura:       "3ada5c6a72f1c794d9ca65fd5c5f53d49371bd05ea4d3cfd5598fc16106689fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d028a40d0235c7325ef751b42ef33f2f9ad66c357990bbc29ab39361d23738c1"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "oniguruma"
