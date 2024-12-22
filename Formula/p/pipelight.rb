f611e54a6f48d8012b21e90c6b53e1db808cef04
Rui Chen <rui@chenrui.dev>
2024-12-22 03:05:49 +0000

    pipelight: add 0.10.0 bottle.

--- Formula/p/pipelight.rb
+++ Formula/p/pipelight.rb
@@ -6,6 +6,15 @@ class Pipelight < Formula
   license "GPL-2.0-only"
   head "https://github.com/pipelight/pipelight.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96adf14db27651fe99ec9078aa498d9ab44bbee167181e28ef3f2e618936983f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21227916e98d2e5abc4efc7ae84c4eef1e19ee0cdc5fb80e9e72bbed7fd14253"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "41fa397d45714a97f19f9b0993a12b064cd66052751fa8a9c1d9bd78b9f1cdfd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9538539a6b67e33d5db97fc883d0833661b6995b6174e380759beb75b732fe2d"
+    sha256 cellar: :any_skip_relocation, ventura:       "49ac565299b8f5d08ead5b65eb27548b4c2fe993bf2121115e3c13efb5267fcc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46123e6bfd89ff6ad47c942c4c8ced3b26ba3dabe51969e42419c99c16804b40"
+  end
+
   depends_on "rust" => :build
 
   def install
