a4f296aff6f5e36a04dc166933446710759e6046
Rui Chen <rui@chenrui.dev>
2024-12-22 11:42:16 -0500

    procps 4.0.5
    autobump: add procps
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/procps.rb
+++ Formula/p/procps.rb
@@ -1,8 +1,8 @@
 class Procps < Formula
   desc "Utilities for browsing procfs"
   homepage "https://gitlab.com/procps-ng/procps"
-  url "https://gitlab.com/procps-ng/procps/-/archive/v4.0.4/procps-v4.0.4.tar.gz"
-  sha256 "3214fab0f817d169f2c117842ba635bafb1cd6090273e311a8b5c6fc393ddb9d"
+  url "https://gitlab.com/procps-ng/procps/-/archive/v4.0.5/procps-v4.0.5.tar.gz"
+  sha256 "2c6d7ed9f2acde1d4dd4602c6172fe56eff86953fe8639bd633dbd22cc18f5db"
   license "GPL-2.0-or-later"
   head "https://gitlab.com/procps-ng/procps.git", branch: "master"
 
