cf0e866a37a7d95eb1d41412ac8cbce8a134cf25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-22 03:12:07 +0000

    git-delta: update 0.18.2_1 bottle.

--- Formula/g/git-delta.rb
+++ Formula/g/git-delta.rb
@@ -8,13 +8,12 @@ class GitDelta < Formula
   head "https://github.com/dandavison/delta.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72f71a3c65ca42df002ecb19c2c2753ad3f7775627a695b883b46397f6388d32"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0adce7cf374bb8b2e8f60814122c53127d655e4b8417b4e99b1ff6ba01fa9dcd"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e362ccd581b14fb81081d88acf80430420181dc75b76f0a14a490bb08e286f6d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "96faa7e5ee63146bebf5d19c81866b7a2c75d86251b6f510b651831a74df8d17"
-    sha256 cellar: :any_skip_relocation, ventura:       "f8347ff888ee3413ae229016599be1282ae64513fb59f6cd109117ad323fccdb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "648cb09c1f3e235689a01517c4953948f22e49be3a36c2038917744ce094bcdf"
+    sha256 cellar: :any,                 arm64_sequoia: "7c6b8a8d5802e42770a3f5da3e276a3dcc2e8b7a6945c3f5e7e240e95274afe1"
+    sha256 cellar: :any,                 arm64_sonoma:  "aaaa87419f0ac6b1ca16345e9299b93a4c26646bdf7b27048b4789a68c38e785"
+    sha256 cellar: :any,                 arm64_ventura: "e1dd88715549906c8d75fda37aba45bebccc2feaa5152e8444739f80bf349fe6"
+    sha256 cellar: :any,                 sonoma:        "4800301c726f6b22a99a9f6f9294799e9ae2b7b4625c87fbb63890358c984aaa"
+    sha256 cellar: :any,                 ventura:       "ed18eb50a566ddbae43d7cfad0e090e8774a213ed3a6a7a7d3832c7e23a59469"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ea94075ccecbdb82e38f3b57a2e08ec6c46958fd6f723c11c6515e359985b07"
   end
 
   depends_on "pkgconf" => :build
