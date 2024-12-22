d134cb450e0530c61ee191e3eb781f7141a66898
Rui Chen <rui@chenrui.dev>
2024-12-22 02:07:54 -0500

    cargo-dist 0.27.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cargo-dist.rb
@@ -0,0 +1,34 @@
+class CargoDist < Formula
+  desc "Tool for building final distributable artifacts and uploading them to an archive"
+  homepage "https://opensource.axo.dev/cargo-dist/"
+  url "https://github.com/axodotdev/cargo-dist/archive/refs/tags/v0.27.0.tar.gz"
+  sha256 "d673c270c9da1e57a294c45ff18a1adee1b7739e05450e0062a243de8283d041"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/axodotdev/cargo-dist.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "rustup" => :test
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "cargo-dist")
+  end
+
+  test do
+    # Show that we can use a different toolchain than the one provided by the `rust` formula.
+    # https://github.com/Homebrew/homebrew-core/pull/134074#pullrequestreview-1484979359
+    ENV.prepend_path "PATH", Formula["rustup"].bin
+    system "rustup", "default", "beta"
+    system "rustup", "set", "profile", "minimal"
+
+    assert_match version.to_s, shell_output("#{bin}/dist --version")
+
+    system "cargo", "new", "--bin", "test_project"
+    cd "test_project" do
+      output = shell_output("#{bin}/dist init 2>&1", 255)
+      assert_match "added [profile.dist] to your workspace Cargo.toml", output
+
+      output = shell_output("#{bin}/dist plan 2>&1", 255)
+      assert_match "You specified --artifacts, disabling host mode, but specified no targets to build", output
+    end
+  end
+end
