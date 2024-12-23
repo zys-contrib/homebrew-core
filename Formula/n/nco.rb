0519083aa41accc4e26da5de5f98fede0383680f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-23 20:03:42 +0000

    nco 5.3.0

--- Formula/n/nco.rb
+++ Formula/n/nco.rb
@@ -1,8 +1,8 @@
 class Nco < Formula
   desc "Command-line operators for netCDF and HDF files"
   homepage "https://nco.sourceforge.net/"
-  url "https://github.com/nco/nco/archive/refs/tags/5.2.9.tar.gz"
-  sha256 "6245886e2a18a4821b0fb768cf9906de09aeb47c303462c8e85f0d1a4f34956d"
+  url "https://github.com/nco/nco/archive/refs/tags/5.3.0.tar.gz"
+  sha256 "661d12f4eb678ca301bf6000f1c1d0fb0e32a69def237dc14f3253e9fc1aaf6a"
   license "BSD-3-Clause"
 
   bottle do
