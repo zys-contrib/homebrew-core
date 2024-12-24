9bb65bb3637fcc967deb17db08d6e91f22f4b657
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-24 06:20:22 +0000

    autoconf-archive: update 2024.10.16 bottle.

--- Formula/a/autoconf-archive.rb
+++ Formula/a/autoconf-archive.rb
@@ -7,12 +7,13 @@ class AutoconfArchive < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7f12265a7c9b810622a120fd397fed95c5b8d4f690df105f842a227f0e6be8f1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7f12265a7c9b810622a120fd397fed95c5b8d4f690df105f842a227f0e6be8f1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7f12265a7c9b810622a120fd397fed95c5b8d4f690df105f842a227f0e6be8f1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a20b687cb4ce1878faa361fa1fc4c466b9450a027a6875cfa982ab754d4206c1"
-    sha256 cellar: :any_skip_relocation, ventura:       "a20b687cb4ce1878faa361fa1fc4c466b9450a027a6875cfa982ab754d4206c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "146d9b404292ff71f731bac5517d404fd541a6483c0ecbf334eacf35d3cb0d18"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7652c1e3d7ef6dc9cc8d6ef298f1bfe80d9888876052bedba5f5638b5e280945"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7652c1e3d7ef6dc9cc8d6ef298f1bfe80d9888876052bedba5f5638b5e280945"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7652c1e3d7ef6dc9cc8d6ef298f1bfe80d9888876052bedba5f5638b5e280945"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ffb73dbda72f41e7b21402df83c9b72f2570e37a8cdad47d0a90768aa5b5d2a3"
+    sha256 cellar: :any_skip_relocation, ventura:       "ffb73dbda72f41e7b21402df83c9b72f2570e37a8cdad47d0a90768aa5b5d2a3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ce036c58366f08d63911e57d2bf5db88d008ac6fce194f8e8a41a073e92a1a8"
   end
 
   # autoconf-archive is useless without autoconf
