39d3dea4a330706c709c65a9678f2ff31fb35af6
Rui Chen <rui@chenrui.dev>
2024-12-23 23:06:18 -0500

    fastnetmon: revision bump (grpc 1.69.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.7.tar.gz"
   sha256 "c21fcbf970214dd48ee8aa11e6294e16bea86495085315e7b370a84b316d0af9"
   license "GPL-2.0-only"
-  revision 10
+  revision 11
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "56970d538952e97b29ea0fda16f5789e796163cd3c91d09ece5b10a4c90a36ed"
