a3e615ce133d3d9697a7cac923c7a51c5fc5dac5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-24 20:19:16 +0000

    tmuxp 1.50.1

--- Formula/t/tmuxp.rb
+++ Formula/t/tmuxp.rb
@@ -3,8 +3,8 @@ class Tmuxp < Formula
 
   desc "Tmux session manager. Built on libtmux"
   homepage "https://tmuxp.git-pull.com/"
-  url "https://files.pythonhosted.org/packages/59/08/6236b39dfa1fce0efae2fdec2666603a212f8ee6c6b2e34b6a155768adf9/tmuxp-1.50.0.tar.gz"
-  sha256 "59e51e7bffbfefff448625207ded685f0ff10eedb9ac6e32d3ac8ecba6791107"
+  url "https://files.pythonhosted.org/packages/96/73/5f60553ac43c881d789c0eaf3532e3916412c9836d3bcbacbfd05f1c8b13/tmuxp-1.50.1.tar.gz"
+  sha256 "bfb9344b474c9a99b066408984f2a213eb04b5590e2feb44e08a3ae842045cfd"
   license "MIT"
 
   bottle do
@@ -26,8 +26,8 @@ class Tmuxp < Formula
   end
 
   resource "libtmux" do
-    url "https://files.pythonhosted.org/packages/b7/15/769e4985afd47d2706f4286dcef69cea77974728b65cec84f945209e8a5e/libtmux-0.40.0.tar.gz"
-    sha256 "047e19bd6c2e58311734207c78741530e7085c769bf62b09b2b0365e9b577155"
+    url "https://files.pythonhosted.org/packages/20/53/d30bf462b0a005e4541b1c217e15107970063aad99145a259d75b23d35bd/libtmux-0.40.1.tar.gz"
+    sha256 "98ffe2b4922449e89dcd0072e1852bcd5757944fd34159d70ed3e57df398796d"
   end
 
   resource "pyyaml" do
