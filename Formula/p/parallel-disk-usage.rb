348ae936414507d91eb7867f1bfc47a88de855df
Rui Chen <rui@chenrui.dev>
2024-12-24 03:58:01 +0000

    parallel-disk-usage: add 0.11.0 bottle.

--- Formula/p/parallel-disk-usage.rb
+++ Formula/p/parallel-disk-usage.rb
@@ -6,6 +6,15 @@ class ParallelDiskUsage < Formula
   license "Apache-2.0"
   head "https://github.com/KSXGitHub/parallel-disk-usage.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54a80c6cab3c8a5865b1b518f193f63d49dd6d7990d45d725407faa3afb55927"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "30b5f5b49a939dd902b34b6d13be2aa49d632ca8fce5b5754ba397d358699ad1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "813b20404ed758b4e2fceecc75ef706871696c98930ad185f182fd14c96bc20b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ab569a47c73fcfa96edf0bcb3015caee836d7630ed0b064533c9e258382fc0ec"
+    sha256 cellar: :any_skip_relocation, ventura:       "27d6b6a2502e8c45bb315b12864ce6fe7a8d9c07ef2bccffc7dd1f4547fc72a3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f233a9ade03a7211653eca2b211b91538a9c54a5f4ebfab3b0865c9b4eb168d4"
+  end
+
   depends_on "rust" => :build
 
   def install
