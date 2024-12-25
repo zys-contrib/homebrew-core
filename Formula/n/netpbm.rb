ce56446b1ef8953e59936e6a757cbc94d97bbc2b
Rui Chen <rui@chenrui.dev>
2024-12-24 22:13:03 -0500

    netpbm 11.02.12

--- Formula/n/netpbm.rb
+++ Formula/n/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.net/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4950"
-  version "11.02.11"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4982"
+  version "11.02.12"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
