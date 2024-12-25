0ce3505c658821160df92a63d6000f5d81de0521
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-25 11:21:48 +0000

    nickle: update 2.102 bottle.

--- Formula/n/nickle.rb
+++ Formula/n/nickle.rb
@@ -12,12 +12,12 @@ class Nickle < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "086d96c6d7654c8dff548cea841b36708e5ec48652691360a822c45586b7191f"
-    sha256 arm64_sonoma:  "b92df9ca14b05dc3ab95bfe8fcdb1e490d88f2d62d4da930b13a6fbf5e36e993"
-    sha256 arm64_ventura: "ac06c1f80b2239e1e83c8d30be1a90bd4190c4859dae00569e34d22e6beff1fd"
-    sha256 sonoma:        "126b3d10b45bbaf8cd57064291be29870ca99faf070ef773e9210654c211d4e1"
-    sha256 ventura:       "c346eb183a5491ce280c5a16fe4e1b3bce57e6650cb6aea471bc066adfedc33b"
-    sha256 x86_64_linux:  "21aee2fc692a614e3e5db78e083e802b3132e36b07f2e7f0b07adef96f9c8386"
+    sha256 arm64_sequoia: "2fdc47d6d5048414dfb052a1435db3384faf66f24aa5676bc44d6103b6566b5e"
+    sha256 arm64_sonoma:  "44a28de3bfd6cbcbd1c3241cc6c159eb6c1d872b66bcda0b9753a9cff6f73829"
+    sha256 arm64_ventura: "43b3243b4a9484fda1eea6f44ce4e0f2e205d4f1a1a089f4d8bfce6c0f8cea52"
+    sha256 sonoma:        "a545c7cf04ec60df69b247257d58305914b4e799604afa959cae74e616debf89"
+    sha256 ventura:       "0c7c8b844b443aed8417bc9e0061cd6d242d6f8fb89d7f7e2efdb6efe70c7080"
+    sha256 x86_64_linux:  "714be532767cf1fadc8094350bec6e70459a8dd8e891a8eeba3de01099638dde"
   end
 
   depends_on "autoconf" => :build
