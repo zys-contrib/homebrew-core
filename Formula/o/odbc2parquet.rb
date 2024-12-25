e6bda62bfb5f752ca9ca4f09bd1868ec8a08591e
Rui Chen <rui@chenrui.dev>
2024-12-25 04:53:12 +0000

    odbc2parquet: add 6.3.1 bottle.

--- Formula/o/odbc2parquet.rb
+++ Formula/o/odbc2parquet.rb
@@ -6,6 +6,15 @@ class Odbc2parquet < Formula
   license "MIT"
   head "https://github.com/pacman82/odbc2parquet.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "c127ed1d31a4716cdfa3d39019769e2826589e7071e39a085c52c7cfefed43dd"
+    sha256 cellar: :any,                 arm64_sonoma:  "436af15821db55593d14af9866169eb2aff5ed14142bcf227c3788dc8f944c8a"
+    sha256 cellar: :any,                 arm64_ventura: "9030aad4adb8a44069aca011d6b10e679959b22f34236507973d2ddc5d1665e8"
+    sha256 cellar: :any,                 sonoma:        "f986e20a827ba9f3f56d5500146ef4f51e3e0330ce14c2964f49782555640eb4"
+    sha256 cellar: :any,                 ventura:       "79845a24e840157eed1f55181cbc1455c53b13e17f8be5cf0b5875f7f5e1d7a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "957fb36c957dde2d24d3d835c9612a74a870ac159f3844ade44c752c3f1247eb"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "unixodbc"
