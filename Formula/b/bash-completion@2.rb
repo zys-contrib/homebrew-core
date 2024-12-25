45811a198a69e1fb54848a17b7bcc10efa821e89
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-25 10:50:11 +0000

    bash-completion@2 2.16.0

--- Formula/b/bash-completion@2.rb
+++ Formula/b/bash-completion@2.rb
@@ -1,8 +1,8 @@
 class BashCompletionAT2 < Formula
   desc "Programmable completion for Bash 4.2+"
   homepage "https://github.com/scop/bash-completion"
-  url "https://github.com/scop/bash-completion/releases/download/2.15.0/bash-completion-2.15.0.tar.xz"
-  sha256 "976a62ee6226970283cda85ecb9c7a4a88f62574c0a6f9e856126976decf1a06"
+  url "https://github.com/scop/bash-completion/releases/download/2.16.0/bash-completion-2.16.0.tar.xz"
+  sha256 "3369bd5e418a75fb990863925aed5b420398acebb320ec4c0306b3eae23f107a"
   license "GPL-2.0-or-later"
 
   livecheck do
