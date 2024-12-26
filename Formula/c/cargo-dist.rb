7aef7807b996b36b3ffad7ad6f013a314e327441
Patrick Linnane <patrick@linnane.io>
2024-12-26 18:43:16 +0000

    cargo-dist: add 0.27.0 bottle.

--- Formula/c/cargo-dist.rb
+++ Formula/c/cargo-dist.rb
@@ -6,6 +6,15 @@ class CargoDist < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/axodotdev/cargo-dist.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1c2e6416e92c1cfdcb1f730fd323e60e41e94456a0e2c400a44fc01bf6c13bb2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6b7773c14c3a6af2e56413f5dab8b2ac97457169ec35daf90cc8599bc6318dab"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f0183e2adfee6d2677a331e885d1c0c9b7e1bb189ed718bd218fcae5127ad664"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ddd21a44e0226327993c43cf09ba3e58181a0dd55db60b2289fc47643c1edf5e"
+    sha256 cellar: :any_skip_relocation, ventura:       "d2e5dbed32a18337964e9afbda507c7aebf99b43a369c0f5ce0da77c2509bdaa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "221f019eb73ea38d5d5b5f4860e3f5d729dc89fd4f0068d48ceb00f4850684c5"
+  end
+
   depends_on "rust" => :build
   depends_on "rustup" => :test
 
