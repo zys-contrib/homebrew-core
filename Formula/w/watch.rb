4ef95c9bf2629d952763dd0ce50c7f38220b12a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-26 18:42:56 +0000

    watch: update 4.0.5 bottle.

--- Formula/w/watch.rb
+++ Formula/w/watch.rb
@@ -8,16 +8,12 @@ class Watch < Formula
   head "https://gitlab.com/procps-ng/procps.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia:  "6a87db4955bd26f571c90378186d245a6dcc50a2c8b6c8026f69f81328679ec6"
-    sha256 cellar: :any,                 arm64_sonoma:   "9d8b55c73a9b913186f6ef8dc7642e8a718b5edea93fc3301fff5f44ad42fe90"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f2e3977aacd949425257bb08b9ed66125e4cdff76a6e5b2464718139bc966d8c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5f7ea5b77d12731688f4e2e72e8190f70c62763d4bdb94e8c30ea1c0625db9d6"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3aac6404005a0953a1126687829863e19fa4d0f02acc4e58d8d099615bd9d014"
-    sha256 cellar: :any,                 sonoma:         "58baecc442fe806ece26dcd2c055532f226b8a06a732d32392c0858c56a6ac67"
-    sha256 cellar: :any_skip_relocation, ventura:        "80193cc3557144f620767de324af7f45bd0717496b81d8d09f811cf0e9e7397c"
-    sha256 cellar: :any_skip_relocation, monterey:       "f52987abe01c3e3a09c5608d02fd8a4714632f4256ae58c79d4a32f41e42669b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d61077f4bffe12e0132a86c138630d2c422932272a61959ab1a01e8b7c244edb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "03aa0061c8707c4d31402f1697429c7619e08e29221de08eed00ec9a26d3bc1e"
+    sha256 cellar: :any,                 arm64_sequoia: "a2c77480716cdd27593a8a81fcbb7b1f8c19b9f1d82847e4328efb98703e2045"
+    sha256 cellar: :any,                 arm64_sonoma:  "6bb0691e3d38e625b5842646d8a0c2439d5889a63dbf6a45a1455fd6c900dae2"
+    sha256 cellar: :any,                 arm64_ventura: "e5b2fe0f325b928c9a448b350e626fe254e0c313e2dbcc69a582f231cf5bb0f4"
+    sha256 cellar: :any,                 sonoma:        "bed3496e896be3b00ef559bf3bafbc0fc0bc94c2fc0383e72bb45dc9a714200d"
+    sha256 cellar: :any,                 ventura:       "658ca1a1a072b329b96e394ff3b7640883dc5e0ad1e02a3b3f5fa3afc88f8a09"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6bb244d3c65a2710ac9922b4cd7d2bf25c22ebd7a865f26b7e32d512291efff"
   end
 
   depends_on "autoconf" => :build
