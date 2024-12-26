f6ef93fb1228c373f3a64582952a2b9e3ddbec8a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-26 10:44:28 +0000

    mlx: update 0.21.1 bottle.

--- Formula/m/mlx.rb
+++ Formula/m/mlx.rb
@@ -21,11 +21,11 @@ class Mlx < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_sequoia: "fd18516eef6904ee30fff4c8a064bb5a26e2f760b160452881b737058440e991"
-    sha256 cellar: :any, arm64_sonoma:  "23800da529205b61f80056c4b23f4977f09312cce9f078f49ed369954711599e"
-    sha256 cellar: :any, arm64_ventura: "e85d2d9d9c10d5b8a52033c38da8e8a3ae6c65712610cc2e6424fb87adff3a1b"
-    sha256 cellar: :any, sonoma:        "969615ae82059b014730553d207d8f0f6bf83e2d2f8d60b966b1582be2a2d0fa"
-    sha256 cellar: :any, ventura:       "ff0a2819c11d989de908e39319dddd20c3f8a828ae8df287074ffc9a1abe8260"
+    sha256 cellar: :any, arm64_sequoia: "575a6fdd16b8e35a74c193a8e34caace3c057e1dae8829bf8b3e5320f5c41e5a"
+    sha256 cellar: :any, arm64_sonoma:  "889879a0ab49703e93eb5ae95d973ae957b72ce7cbb4ec3ea924bd6e14dc2790"
+    sha256 cellar: :any, arm64_ventura: "a5732b975b0fa32bd286ddc3889d0a70c8cb2203ca9ed35f6822f4f106f06761"
+    sha256 cellar: :any, sonoma:        "a94154645f164f56c18c97a2e11056bdfefbc505e7f982b2562c3a52bda285b1"
+    sha256 cellar: :any, ventura:       "95d49fdef66a38e083d0380d72b6feabd6e386c01f0556a1e7b711ef30cf99e7"
   end
 
   depends_on "cmake" => :build
