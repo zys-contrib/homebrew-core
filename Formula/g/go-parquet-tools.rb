bc965ab6e23d1f11f162de93376e1f7abd0f1306
Hang Xie <7977860+hangxie@users.noreply.github.com>
2024-12-27 18:39:39 -0800

    go-parquet-tools v1.25.9 (new formula)
    Add formula for paquet-tools, a CLI tool to manipulate files in Apache
    Parquet format.

---
+++ Formula/g/go-parquet-tools.rb
@@ -0,0 +1,32 @@
+class GoParquetTools < Formula
+  desc "Utility to deal with Parquet data"
+  homepage "https://github.com/hangxie/parquet-tools"
+  url "https://github.com/hangxie/parquet-tools/archive/refs/tags/v1.25.9.tar.gz"
+  sha256 "61df81b68b58cfccd6b819bc3edf50e6ecf58e294195167517d71076fb047fd3"
+  license "BSD-3-Clause"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/hangxie/parquet-tools/cmd.version=v#{version}
+      -X github.com/hangxie/parquet-tools/cmd.build=#{time.iso8601}
+      -X github.com/hangxie/parquet-tools/cmd.gitHash=#{tap.user}
+      -X github.com/hangxie/parquet-tools/cmd.source=source
+    ]
+    system "go", "build", *std_go_args(ldflags:, output: bin/"parquet-tools")
+  end
+
+  test do
+    resource("test-parquet") do
+      url "https://github.com/hangxie/parquet-tools/raw/v1.25.9/testdata/good.parquet"
+      sha256 "daf5090fbc5523cf06df8896cf298dd5e53c058457e34766407cb6bff7522ba5"
+    end
+
+    resource("test-parquet").stage testpath
+
+    output = shell_output("#{bin}/parquet-tools schema #{testpath}/good.parquet")
+    assert_match "name=Parquet_go_root", output
+  end
+end
