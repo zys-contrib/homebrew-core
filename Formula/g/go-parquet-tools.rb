923cf821e3641eca6fde5064ed89da4710337db0
Rui Chen <rui@chenrui.dev>
2024-12-28 15:12:12 +0000

    go-parquet-tools: add 1.25.9 bottle.

--- Formula/g/go-parquet-tools.rb
+++ Formula/g/go-parquet-tools.rb
@@ -5,6 +5,15 @@ class GoParquetTools < Formula
   sha256 "61df81b68b58cfccd6b819bc3edf50e6ecf58e294195167517d71076fb047fd3"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cc38ca450eb9d0f1843e6e10e93cc107ecb804f0cf1d91f9fb77032b387175ba"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc38ca450eb9d0f1843e6e10e93cc107ecb804f0cf1d91f9fb77032b387175ba"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "cc38ca450eb9d0f1843e6e10e93cc107ecb804f0cf1d91f9fb77032b387175ba"
+    sha256 cellar: :any_skip_relocation, sonoma:        "74cb4880214635871a752c27a47c76400c52860400e2bede6c69ea7d9a1f0104"
+    sha256 cellar: :any_skip_relocation, ventura:       "74cb4880214635871a752c27a47c76400c52860400e2bede6c69ea7d9a1f0104"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66fc030869fd9fe6e0781601ce1b2da8797bcdc68857d563468c4d756c7310d2"
+  end
+
   depends_on "go" => :build
 
   def install
