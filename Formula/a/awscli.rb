d8f8b13c62d88dc814c01d2e8fb02e80280bd735
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-28 07:50:43 +0000

    awscli 2.22.26

--- Formula/a/awscli.rb
+++ Formula/a/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/refs/tags/2.22.25.tar.gz"
-  sha256 "45e3d224d278e71046f9dbfeee170d2a74f3280056cea8dfbd86698f29a6b279"
+  url "https://github.com/aws/aws-cli/archive/refs/tags/2.22.26.tar.gz"
+  sha256 "f47e44655ba66fb817e073ce717b6307d8d070bb28fb3a76f071217bd5c438cd"
   license "Apache-2.0"
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
