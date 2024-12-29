1d7890578142d9c3a00420f4799dd86570047ad9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-29 11:18:15 +0000

    sourcery 2.2.6

--- Formula/s/sourcery.rb
+++ Formula/s/sourcery.rb
@@ -1,10 +1,9 @@
 class Sourcery < Formula
   desc "Meta-programming for Swift, stop writing boilerplate code"
   homepage "https://github.com/krzysztofzablocki/Sourcery"
-  url "https://github.com/krzysztofzablocki/Sourcery/archive/refs/tags/2.2.5.tar.gz"
-  sha256 "6f4d4d2859e57039f9d49f737a696d0f22aecaffd553a7d5039fa2007103994f"
+  url "https://github.com/krzysztofzablocki/Sourcery/archive/refs/tags/2.2.6.tar.gz"
+  sha256 "5e63ed976ca74df7483a6508796ea9d570766e387d3631b474c81b67d1c771f9"
   license "MIT"
-  revision 1
   version_scheme 1
   head "https://github.com/krzysztofzablocki/Sourcery.git", branch: "master"
 
@@ -20,6 +19,7 @@ class Sourcery < Formula
   depends_on xcode: "14.3"
 
   uses_from_macos "ruby" => :build
+  uses_from_macos "swift" => :build, since: :sonoma # swift 5.10+
   uses_from_macos "curl"
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
