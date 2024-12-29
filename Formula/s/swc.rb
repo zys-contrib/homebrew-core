6ca4d9e8621718dc2ec9270574d87ac0a15e91a4
Rui Chen <rui@chenrui.dev>
2024-12-29 00:36:33 -0500

    swc 1.10.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/swc.rb
@@ -0,0 +1,29 @@
+class Swc < Formula
+  desc "Super-fast Rust-based JavaScript/TypeScript compiler"
+  homepage "https://swc.rs"
+  url "https://github.com/swc-project/swc/archive/refs/tags/v1.10.3.tar.gz"
+  sha256 "04c4cd3cbe0dd1bebf0d9b1a00b5cf4671045fd1b9cc8ebb6746f99529435687"
+  license "Apache-2.0"
+  head "https://github.com/swc-project/swc.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    # `-Zshare-generics=y` flag is only supported on nightly Rust
+    rm ".cargo/config.toml"
+
+    system "cargo", "install", *std_cargo_args(path: "crates/swc_cli_impl")
+  end
+
+  test do
+    (testpath/"test.js").write <<~JS
+      const x = () => 42;
+    JS
+
+    system bin/"swc", "compile", "test.js", "--out-file", "test.out.js"
+    assert_path_exists testpath/"test.out.js"
+
+    output = shell_output("#{bin}/swc lint 2>&1", 101)
+    assert_match "Lint command is not yet implemented", output
+  end
+end
