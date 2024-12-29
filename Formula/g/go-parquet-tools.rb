7332c533625b8fc63f7699efa45b07cb86faa5fd
Hang Xie <7977860+hangxie@users.noreply.github.com>
2024-12-28 17:02:26 -0800

    go-parquet-tools 1.25.10
    Bump version to 1.25.10, also change build source to Homebrew and
    deprecate githash.

--- Formula/g/go-parquet-tools.rb
+++ Formula/g/go-parquet-tools.rb
@@ -1,8 +1,8 @@
 class GoParquetTools < Formula
   desc "Utility to deal with Parquet data"
   homepage "https://github.com/hangxie/parquet-tools"
-  url "https://github.com/hangxie/parquet-tools/archive/refs/tags/v1.25.9.tar.gz"
-  sha256 "61df81b68b58cfccd6b819bc3edf50e6ecf58e294195167517d71076fb047fd3"
+  url "https://github.com/hangxie/parquet-tools/archive/refs/tags/v1.25.10.tar.gz"
+  sha256 "4255a765c295603fdc51b3479ff8d0defbb864971b2e1f732d1278231603b18b"
   license "BSD-3-Clause"
 
   bottle do
@@ -21,15 +21,15 @@ class GoParquetTools < Formula
       -s -w
       -X github.com/hangxie/parquet-tools/cmd.version=v#{version}
       -X github.com/hangxie/parquet-tools/cmd.build=#{time.iso8601}
-      -X github.com/hangxie/parquet-tools/cmd.gitHash=#{tap.user}
-      -X github.com/hangxie/parquet-tools/cmd.source=source
+      -X github.com/hangxie/parquet-tools/cmd.gitHash=Deprecated
+      -X github.com/hangxie/parquet-tools/cmd.source=Homebrew
     ]
     system "go", "build", *std_go_args(ldflags:, output: bin/"parquet-tools")
   end
 
   test do
     resource("test-parquet") do
-      url "https://github.com/hangxie/parquet-tools/raw/v1.25.9/testdata/good.parquet"
+      url "https://github.com/hangxie/parquet-tools/raw/v1.25.10/testdata/good.parquet"
       sha256 "daf5090fbc5523cf06df8896cf298dd5e53c058457e34766407cb6bff7522ba5"
     end
 
