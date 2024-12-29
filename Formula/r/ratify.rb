d2080bf14b50bbadd36fee64f3ed849de4041fa2
Patrick Linnane <patrick@linnane.io>
2024-12-29 03:51:08 +0000

    ratify: add 1.3.1 bottle.

--- Formula/r/ratify.rb
+++ Formula/r/ratify.rb
@@ -6,6 +6,15 @@ class Ratify < Formula
   license "Apache-2.0"
   head "https://github.com/ratify-project/ratify.git", branch: "dev"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0e288bb20adfce4de29b56e39519eb0a46aef9c63f8ac9a99495d30c775fcba3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0e288bb20adfce4de29b56e39519eb0a46aef9c63f8ac9a99495d30c775fcba3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0e288bb20adfce4de29b56e39519eb0a46aef9c63f8ac9a99495d30c775fcba3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fdc911061d14b31045fcb8a24564bbb131ba1faaa9ad7de05059e682c1e54395"
+    sha256 cellar: :any_skip_relocation, ventura:       "0818e5ce845caafbdf43d5559e20a723d13b015bcc54ba2945dd1e4ba92be846"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "71644e4fbc5f83bf238d1ea71cdd2a2e607089e614b7e109febce75c2adcdead"
+  end
+
   depends_on "go" => :build
 
   def install
