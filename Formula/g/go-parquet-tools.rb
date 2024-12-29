1bd27f4c5862e0b77387d41f6a89bf6fd73d6969
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-29 02:39:01 +0000

    go-parquet-tools: update 1.25.10 bottle.

--- Formula/g/go-parquet-tools.rb
+++ Formula/g/go-parquet-tools.rb
@@ -6,12 +6,12 @@ class GoParquetTools < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cc38ca450eb9d0f1843e6e10e93cc107ecb804f0cf1d91f9fb77032b387175ba"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc38ca450eb9d0f1843e6e10e93cc107ecb804f0cf1d91f9fb77032b387175ba"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "cc38ca450eb9d0f1843e6e10e93cc107ecb804f0cf1d91f9fb77032b387175ba"
-    sha256 cellar: :any_skip_relocation, sonoma:        "74cb4880214635871a752c27a47c76400c52860400e2bede6c69ea7d9a1f0104"
-    sha256 cellar: :any_skip_relocation, ventura:       "74cb4880214635871a752c27a47c76400c52860400e2bede6c69ea7d9a1f0104"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66fc030869fd9fe6e0781601ce1b2da8797bcdc68857d563468c4d756c7310d2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "62082925811d7afb270bb749409a2cf3521d87f6ad38223355d366fd69b38baf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62082925811d7afb270bb749409a2cf3521d87f6ad38223355d366fd69b38baf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "62082925811d7afb270bb749409a2cf3521d87f6ad38223355d366fd69b38baf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aea7e86a5e01af18e7462964fafc4a0d635f59da2d0d49a46a6dced3609013bd"
+    sha256 cellar: :any_skip_relocation, ventura:       "aea7e86a5e01af18e7462964fafc4a0d635f59da2d0d49a46a6dced3609013bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8659d6a678a44c0dfabdfdb12d4d4c399004abd2ebeb7ef3d745d31d7a47a104"
   end
 
   depends_on "go" => :build
