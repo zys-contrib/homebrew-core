8cf42a786f4fc4acd2927006715c28681ca44c69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-29 08:05:15 +0000

    pylyzer 0.0.76

--- Formula/p/pylyzer.rb
+++ Formula/p/pylyzer.rb
@@ -1,8 +1,8 @@
 class Pylyzer < Formula
   desc "Fast static code analyzer & language server for Python"
   homepage "https://github.com/mtshiba/pylyzer"
-  url "https://github.com/mtshiba/pylyzer/archive/refs/tags/v0.0.75.tar.gz"
-  sha256 "fbf4fed387cbc37f0fda57960fe120c718f35a4616a725a26c754dcb45a57a0f"
+  url "https://github.com/mtshiba/pylyzer/archive/refs/tags/v0.0.76.tar.gz"
+  sha256 "e6e60ddcff14ec57e13fd810f57fa105d5ce5a1795fbe58c37684d69dcf4c52d"
   license "MIT"
 
   bottle do
