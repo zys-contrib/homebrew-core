00e5d2cc399af9c5d4947d0f4d4c982b81b076cc
Patrick Linnane <patrick@linnane.io>
2024-12-29 10:19:19 -0800

    pinact 1.1.2 (new formula)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/p/pinact.rb
@@ -0,0 +1,41 @@
+class Pinact < Formula
+  desc "Pins GitHub Actions to full hashes and versions"
+  homepage "https://github.com/suzuki-shunsuke/pinact"
+  url "https://github.com/suzuki-shunsuke/pinact/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "f41078d480ca37a945b90d8aad7cdd3e2d0aaf8dace497d592702b0a8e0de170"
+  license "MIT"
+  head "https://github.com/suzuki-shunsuke/pinact.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{tap.user}
+      -X main.date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/pinact"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pinact --version")
+
+    (testpath/"action.yml").write <<~YAML
+      name: CI
+
+      on: push
+
+      jobs:
+        build:
+          runs-on: ubuntu-latest
+          steps:
+            - uses: actions/checkout@v3
+            - run: npm install && npm test
+    YAML
+
+    system bin/"pinact", "run", "action.yml"
+
+    assert_match(%r{.*?actions/checkout@[a-f0-9]{40}}, (testpath/"action.yml").read)
+  end
+end
