374a3f10d6857b3dba79dd33feca1164b0b837b1
Bevan Kay <email@bevankay.me>
2024-12-30 12:38:43 +0000

    cabin: add 0.11.0 bottle.

--- Formula/c/cabin.rb
+++ Formula/c/cabin.rb
@@ -6,6 +6,15 @@ class Cabin < Formula
   license "Apache-2.0"
   head "https://github.com/cabinpkg/cabin.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "718a61b02f0570116f728f6781b324960c1befdcebb323b11e9a5579df070949"
+    sha256 cellar: :any,                 arm64_sonoma:  "428beabf2dbf2d516fc142fed8d159876f10f4561cb830e48f85e1ea6092a788"
+    sha256 cellar: :any,                 arm64_ventura: "93f89a86a5cbf309887eadb863631829e0c5fc899dd11c1c23f64d55323d0e47"
+    sha256 cellar: :any,                 sonoma:        "05bbcea8035588f3d0fecf766109e7e69baee7c573b1e326126d8812f642cc0e"
+    sha256 cellar: :any,                 ventura:       "f28c62b0d3c9ea57cdcd40b84beb17e57d9bb56f10043632f87a71f0e9da8100"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e30409295c48daf0a199dfdaee56914cd22cfc2601923c72be7e16031a1234f3"
+  end
+
   depends_on "nlohmann-json" => :build
   depends_on "toml11" => :build
   depends_on "curl"
