f19b3139b17666ca1aac945f84fdf49ad02e956b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-30 13:54:43 +0000

    elixir-ls 0.26.0

--- Formula/e/elixir-ls.rb
+++ Formula/e/elixir-ls.rb
@@ -1,8 +1,8 @@
 class ElixirLs < Formula
   desc "Language Server and Debugger for Elixir"
   homepage "https://elixir-lsp.github.io/elixir-ls"
-  url "https://github.com/elixir-lsp/elixir-ls/archive/refs/tags/v0.25.0.tar.gz"
-  sha256 "43cc1ee3b99adff6af93d3ad49959041875b8d1eee8cc517b4b1a65d583f857c"
+  url "https://github.com/elixir-lsp/elixir-ls/archive/refs/tags/v0.26.0.tar.gz"
+  sha256 "4e4507d95e216550f7913f7e2023fd24c6c86103c74cb9ed8b94e86fafa4eef0"
   license "Apache-2.0"
 
   bottle do
