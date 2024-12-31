153503bfc2ae4dffa704d66298b353a5d57b2ee8
Rui Chen <rui@chenrui.dev>
2024-12-30 21:30:51 -0500

    libxcrypt 4.4.37
    autobump: add libxcrypt
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    libxcrypt: add pkgconf build dep
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libxcrypt.rb
+++ Formula/lib/libxcrypt.rb
@@ -1,8 +1,8 @@
 class Libxcrypt < Formula
   desc "Extended crypt library for descrypt, md5crypt, bcrypt, and others"
   homepage "https://github.com/besser82/libxcrypt"
-  url "https://github.com/besser82/libxcrypt/releases/download/v4.4.36/libxcrypt-4.4.36.tar.xz"
-  sha256 "e5e1f4caee0a01de2aee26e3138807d6d3ca2b8e67287966d1fefd65e1fd8943"
+  url "https://github.com/besser82/libxcrypt/releases/download/v4.4.37/libxcrypt-4.4.37.tar.xz"
+  sha256 "902aa2976f959b5ebe55679b1722b8479f8f13cd4ce2ef432b0a84ae298fffd0"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -25,16 +25,18 @@ class Libxcrypt < Formula
 
   keg_only :provided_by_macos
 
+  depends_on "pkgconf" => :build
+
   link_overwrite "include/crypt.h"
   link_overwrite "lib/libcrypt.so"
 
   def install
-    system "./configure", *std_configure_args,
-                          "--disable-static",
+    system "./configure", "--disable-static",
                           "--disable-obsolete-api",
                           "--disable-xcrypt-compat-files",
                           "--disable-failure-tokens",
-                          "--disable-valgrind"
+                          "--disable-valgrind",
+                          *std_configure_args
     system "make", "install"
   end
 
