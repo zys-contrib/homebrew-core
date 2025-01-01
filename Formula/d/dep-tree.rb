c6e03864d2482214717ebb29838da427f0eccffe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-01 07:53:36 +0000

    dep-tree 0.23.2

--- Formula/d/dep-tree.rb
+++ Formula/d/dep-tree.rb
@@ -1,8 +1,8 @@
 class DepTree < Formula
   desc "Tool for visualizing dependencies between files and enforcing dependency rules"
   homepage "https://github.com/gabotechs/dep-tree"
-  url "https://github.com/gabotechs/dep-tree/archive/refs/tags/v0.23.1.tar.gz"
-  sha256 "cfc88b80c2fdd72f878dac5f364023c9ad7c7a892b0619701b076caf2257b3eb"
+  url "https://github.com/gabotechs/dep-tree/archive/refs/tags/v0.23.2.tar.gz"
+  sha256 "a1af6a57c5ccb84bbae737712c3bb393dd62c76b8d09e2aac648a4f4df34d742"
   license "MIT"
   head "https://github.com/gabotechs/dep-tree.git", branch: "main"
 
