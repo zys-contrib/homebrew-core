8799532ec99fe4b6dc77ef3829ec24e25ca50996
Rui Chen <rui@chenrui.dev>
2024-12-31 19:47:40 -0500

    pygit2: update to use libgit2 1.8
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pygit2.rb
+++ Formula/p/pygit2.rb
@@ -4,6 +4,7 @@ class Pygit2 < Formula
   url "https://files.pythonhosted.org/packages/a4/85/c848cdf44214bf541c4a725a0a6e271f8db9f18cfccef702d53f83f1e19a/pygit2-1.16.0.tar.gz"
   sha256 "7b29a6796baa15fc89d443ac8d51775411d9b1e5b06dc40d458c56c8576b48a2"
   license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
+  revision 1
   head "https://github.com/libgit2/pygit2.git", branch: "master"
 
   bottle do
@@ -18,7 +19,7 @@ class Pygit2 < Formula
   depends_on "python@3.12" => [:build, :test]
   depends_on "python@3.13" => [:build, :test]
   depends_on "cffi"
-  depends_on "libgit2"
+  depends_on "libgit2@1.8"
 
   def pythons
     deps.map(&:to_formula)
