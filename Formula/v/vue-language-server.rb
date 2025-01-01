aa7f10f61eef6bf2da57039255b577ee83493790
Rui Chen <rui@chenrui.dev>
2025-01-01 14:45:49 +0000

    vue-language-server: add 2.2.0 bottle.

--- Formula/v/vue-language-server.rb
+++ Formula/v/vue-language-server.rb
@@ -5,6 +5,10 @@ class VueLanguageServer < Formula
   sha256 "7e858a1212bc604e19b1b7a325428a3cd47e1bb66b25620f124ff53d57ba611a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e27b0b1ad330d4f57e3e86caf4480c17595d85323d866cf512e33d88e318fa4d"
+  end
+
   depends_on "node"
 
   def install
