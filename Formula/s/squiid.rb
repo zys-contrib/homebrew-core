f58e0798c3697f761db7ff7d785e4ef353789738
TabulateJarl8 <tabulatejarl8@gmail.com>
2025-01-01 17:23:23 -0500

    squiid 1.2.0

--- Formula/s/squiid.rb
+++ Formula/s/squiid.rb
@@ -1,8 +1,8 @@
 class Squiid < Formula
   desc "Do advanced algebraic and RPN calculations"
   homepage "https://imaginaryinfinity.net/projects/squiid/"
-  url "https://gitlab.com/ImaginaryInfinity/squiid-calculator/squiid/-/archive/1.1.3/squiid-1.1.3.tar.gz"
-  sha256 "3944faeff6787e52bb045a0353c72a29acff919abfa41b275add7189355c01c9"
+  url "https://gitlab.com/ImaginaryInfinity/squiid-calculator/squiid/-/archive/1.2.0/squiid-1.2.0.tar.gz"
+  sha256 "01f6142c986ce744ea7df86da17d43b593d56c8b843e6f502b0327ef43e3fcb8"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -20,35 +20,13 @@ class Squiid < Formula
   end
 
   depends_on "rust" => :build
-  depends_on "nng"
 
   def install
-    # Avoid vendoring `nng`.
-    # "build-nng" is the `nng` crate's only default feature. To check:
-    # https://gitlab.com/neachdainn/nng-rs/-/blob/v#{nng_crate_version}/Cargo.toml
-    inreplace "Cargo.toml",
-              /^nng = "(.+)"$/,
-              'nng = { version = "\\1", default-features = false }'
-    inreplace "squiid-engine/Cargo.toml",
-              /^nng = { version = "(.+)", optional = true }$/,
-              'nng = { version = "\\1", optional = true, default-features = false }'
-
     system "cargo", "install", *std_cargo_args
-  end
-
-  def check_binary_linkage(binary, library)
-    binary.dynamically_linked_libraries.any? do |dll|
-      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
-
-      File.realpath(dll) == File.realpath(library)
-    end
   end
 
   test do
     # squiid is a TUI app
     assert_match version.to_s, shell_output("#{bin}/squiid --version")
-
-    assert check_binary_linkage(bin/"squiid", Formula["nng"].opt_lib/shared_library("libnng")),
-      "No linkage with libnng! Cargo is likely using a vendored version."
   end
 end
