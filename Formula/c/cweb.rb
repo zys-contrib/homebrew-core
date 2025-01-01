7f382b2eb1ba30ec7645569d5011227c97ce09f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-01 13:53:27 +0000

    cweb 4.12.1

--- Formula/c/cweb.rb
+++ Formula/c/cweb.rb
@@ -1,8 +1,8 @@
 class Cweb < Formula
   desc "Literate documentation system for C, C++, and Java"
   homepage "https://cs.stanford.edu/~knuth/cweb.html"
-  url "https://github.com/ascherer/cweb/archive/refs/tags/cweb-4.12.tar.gz"
-  sha256 "d2fb0f8bc315fb36dfc300a1593fee43c58df45120371dc30b5a0762b26fda72"
+  url "https://github.com/ascherer/cweb/archive/refs/tags/cweb-4.12.1.tar.gz"
+  sha256 "c6e08d726b1b587187a70d45cf49dd990dfe70053059064fea0999e3f4fa976f"
   # See discussions in this thread, https://github.com/ascherer/cweb/issues/29
   license :cannot_represent
 
