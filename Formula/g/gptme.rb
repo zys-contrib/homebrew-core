989e97b0a2dc5d343b126e16b8ad2cdc7f210e5e
Michael Cho <michael@michaelcho.dev>
2025-01-02 01:27:32 -0500

    gptme: add click shell completions

--- Formula/g/gptme.rb
+++ Formula/g/gptme.rb
@@ -277,6 +277,8 @@ class Gptme < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"gptme", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
