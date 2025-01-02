e271f8b966401d4052f55f6ca612c01122c796df
Michael Cho <michael@michaelcho.dev>
2025-01-02 12:47:02 -0500

    snakefmt: add click shell completions

--- Formula/s/snakefmt.rb
+++ Formula/s/snakefmt.rb
@@ -57,6 +57,8 @@ class Snakefmt < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"snakefmt", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
