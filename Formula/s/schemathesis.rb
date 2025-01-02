f63bd44f66376a2120d978b9d83e0603b627836f
Michael Cho <michael@michaelcho.dev>
2025-01-02 12:43:15 -0500

    schemathesis: add click shell completions

--- Formula/s/schemathesis.rb
+++ Formula/s/schemathesis.rb
@@ -280,6 +280,8 @@ class Schemathesis < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"st", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
