7a45964c4f0f773d7d6b78fbf67deedd6e2ebd8f
Rui Chen <rui@chenrui.dev>
2025-01-02 18:59:14 +0000

    cocogitto: update 6.2.0_2 bottle.

--- Formula/c/cocogitto.rb
+++ Formula/c/cocogitto.rb
@@ -7,12 +7,12 @@ class Cocogitto < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "815c4318d69c91b36986c47f897fba9e5823d9b239998383bb41ec56f37daf2d"
-    sha256 cellar: :any,                 arm64_sonoma:  "20ee790e3d75ec38bcb3e75bb972691ddf65a137ebd904e6d48d50278e104f78"
-    sha256 cellar: :any,                 arm64_ventura: "afb783b372f3f929fffc61d1961cf5d97253df44b1ff6579f3980f69df3a5e70"
-    sha256 cellar: :any,                 sonoma:        "0f2c1fb174b85d710dd1291285ab159f8e567c59ed63c510d54744f1ea4fbdb6"
-    sha256 cellar: :any,                 ventura:       "66d397eea2b0cd6c707931f01062aec463d68e2ae48e009ed9ccb6134be7f2ed"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25667769aa036ee9cfffc83ebae377ab6c80d45d0d9cad82b1cece4102a2b64c"
+    sha256 cellar: :any,                 arm64_sequoia: "3475b0711ea44c686df09a0291f4460d7b7ebcc5c875dee87843ad92fb78e37a"
+    sha256 cellar: :any,                 arm64_sonoma:  "d42d4265db5f0b59377c90438b2e0fae62b4bd3df51b87c6b8eea41a65e53283"
+    sha256 cellar: :any,                 arm64_ventura: "1c620a2e86a848bc471f09030b07fd78fa273ef496abd6b4c36bf4a27e135a32"
+    sha256 cellar: :any,                 sonoma:        "5849ca6b15217adc79b5d586a76279c106ca5b55b376b16d637ac491c5a0e2a5"
+    sha256 cellar: :any,                 ventura:       "92a509b0b0abbe45719479c63ed0d487eb47431b85e3c1ef49355a8b9f88e309"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8338df38de50ce8a00b8dcb98805b4ef5cef67ca19963bfea9e844bc8083132c"
   end
 
   depends_on "pkgconf" => :build
