44dc49e88bb0f06a541a7d5e42405a48ef5c42b8
Michael Cho <michael@michaelcho.dev>
2025-01-02 16:39:47 -0500

    langgraph-cli: add click shell completions

--- Formula/l/langgraph-cli.rb
+++ Formula/l/langgraph-cli.rb
@@ -20,6 +20,8 @@ class LanggraphCli < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"langgraph", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
