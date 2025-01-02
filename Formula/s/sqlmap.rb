a935057887410d7ecf5eb22c25709b863aa00654
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-02 02:52:17 +0000

    sqlmap: update 1.9 bottle.

--- Formula/s/sqlmap.rb
+++ Formula/s/sqlmap.rb
@@ -9,12 +9,12 @@ class Sqlmap < Formula
   head "https://github.com/sqlmapproject/sqlmap.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "68dd79394ca2e3a447beb619bfe8ed77f374a59cfa57f41fb399028b9aa45270"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "68dd79394ca2e3a447beb619bfe8ed77f374a59cfa57f41fb399028b9aa45270"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "68dd79394ca2e3a447beb619bfe8ed77f374a59cfa57f41fb399028b9aa45270"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0aa1d780269e0c5b29ab952c0211caa5ed80792e665f0e532071238f77b258ab"
-    sha256 cellar: :any_skip_relocation, ventura:       "0aa1d780269e0c5b29ab952c0211caa5ed80792e665f0e532071238f77b258ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "87914cae7214d403699724f8943e05176ff14b564ad984115cc2a555974ea183"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "70ffb9e084517b8170bf1b8f3a8c846f7b30f7b33968d238c0c9d2432da51c89"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "70ffb9e084517b8170bf1b8f3a8c846f7b30f7b33968d238c0c9d2432da51c89"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "70ffb9e084517b8170bf1b8f3a8c846f7b30f7b33968d238c0c9d2432da51c89"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ed4c1840ec94c4887fb1f75c742769dd4df3d34944c68117601c123809ea6ffd"
+    sha256 cellar: :any_skip_relocation, ventura:       "ed4c1840ec94c4887fb1f75c742769dd4df3d34944c68117601c123809ea6ffd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dfed37e19af330c4fab2b5feb4f8b31183bee140c457963974314f2547a922e1"
   end
 
   depends_on "python@3.13"
