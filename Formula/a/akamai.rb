b1412ba9cb333cafca00fe70d41b3982dec61394
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-02 11:31:24 +0000

    akamai: update 1.6.1 bottle.

--- Formula/a/akamai.rb
+++ Formula/a/akamai.rb
@@ -6,13 +6,12 @@ class Akamai < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bb62f539efcfbdf1d97670325d7de4ddd943299a057fbe1f25ade6faac568565"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bb62f539efcfbdf1d97670325d7de4ddd943299a057fbe1f25ade6faac568565"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "bb62f539efcfbdf1d97670325d7de4ddd943299a057fbe1f25ade6faac568565"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8bdf2ab67fd7687bbda07711da9fd36afd50f23783e46e5d8386252d5fe90c26"
-    sha256 cellar: :any_skip_relocation, ventura:       "8bdf2ab67fd7687bbda07711da9fd36afd50f23783e46e5d8386252d5fe90c26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0de49b9102884029167dc753ecdc0894c81a7067a283893cb07a1971ce164a23"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dc904bcc85efab494238756c0cc09bc9afcb011006c77c185db0851db18302e0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dc904bcc85efab494238756c0cc09bc9afcb011006c77c185db0851db18302e0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "dc904bcc85efab494238756c0cc09bc9afcb011006c77c185db0851db18302e0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "55212608af5d376981fd87232f8558d9640636ba83bc39ca05ff9ff24d0f53cd"
+    sha256 cellar: :any_skip_relocation, ventura:       "55212608af5d376981fd87232f8558d9640636ba83bc39ca05ff9ff24d0f53cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9fe29135622fbeb235f21de31bf5c7879ea3441eee7a7e8df9c5dcaf5100daa3"
   end
 
   depends_on "go" => [:build, :test]
