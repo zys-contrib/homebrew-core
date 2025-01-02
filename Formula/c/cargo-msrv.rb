976dcb4651916659cc2905f6222595788294e266
Rui Chen <rui@chenrui.dev>
2025-01-02 01:47:39 +0000

    cargo-msrv: add 0.17.1 bottle.

--- Formula/c/cargo-msrv.rb
+++ Formula/c/cargo-msrv.rb
@@ -6,6 +6,15 @@ class CargoMsrv < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/foresterre/cargo-msrv.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e8083a067bcac304b1f84110555863bb37cacc040d08812f1521998d36d3600e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ec7c44782080181ec1cfe6c80dc4b623ce910856a1a47ae4c342de2c58f9afe"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "37d436e976955a03374cdcd1562d79b9c93f0373a416943c4939f45f63e6d0a7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "69f34989b88de9b80b0294b0db5e477740b6e5e46974a1c906998ae380cf6f14"
+    sha256 cellar: :any_skip_relocation, ventura:       "9be524682f16b37d3b403bc94042fbee0c59225890bbcca9b7a0b5fb5a057e65"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fb3027915726759404e1ff5e842e4c0950edb8be93b229057df42bf261a5e911"
+  end
+
   depends_on "rust" => :build
   depends_on "rustup" => :test
 
