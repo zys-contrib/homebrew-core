42566647f5d8ee171158c57eacf99053681d7b57
Michael Cho <michael@michaelcho.dev>
2025-01-02 11:55:43 -0500

    mkdocs: add click shell completions

--- Formula/m/mkdocs.rb
+++ Formula/m/mkdocs.rb
@@ -100,6 +100,8 @@ class Mkdocs < Formula
   def install
     ENV["PIP_USE_PEP517"] = "1"
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"mkdocs", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
