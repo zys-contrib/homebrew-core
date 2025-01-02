ce2a1ce99cf61641fdda81810ee272ea76eecfe4
Michael Cho <michael@michaelcho.dev>
2025-01-02 16:23:55 -0500

    compiledb: add click shell completions

--- Formula/c/compiledb.rb
+++ Formula/c/compiledb.rb
@@ -31,6 +31,8 @@ class Compiledb < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"compiledb", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
