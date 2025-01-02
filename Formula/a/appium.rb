671abd67ffab5d1a0f482237d06ea3e6dcc984c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-02 19:49:35 +0000

    appium 2.14.0

--- Formula/a/appium.rb
+++ Formula/a/appium.rb
@@ -1,8 +1,8 @@
 class Appium < Formula
   desc "Automation for Apps"
   homepage "https://appium.io/"
-  url "https://registry.npmjs.org/appium/-/appium-2.13.1.tgz"
-  sha256 "96a0771bff8182cf90978dcfbf796c82521f999b53570e68aecaba1a33a60ed9"
+  url "https://registry.npmjs.org/appium/-/appium-2.14.0.tgz"
+  sha256 "2a017b6c869ee4d8245cb2986c1fd879c179aa3558b1d9710e7376c30489ff6f"
   license "Apache-2.0"
   head "https://github.com/appium/appium.git", branch: "master"
 
