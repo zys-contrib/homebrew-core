eade4f254ae2a89b9c80748c71b5fbce6b171449
Michael Cho <michael@michaelcho.dev>
2025-01-02 13:14:42 -0500

    weaviate-cli: add click shell completions

--- Formula/w/weaviate-cli.rb
+++ Formula/w/weaviate-cli.rb
@@ -142,6 +142,8 @@ class WeaviateCli < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"weaviate-cli", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
