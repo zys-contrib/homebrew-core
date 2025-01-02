3175258fcaf08962533dedc3d7328d157895219b
Michael Cho <michael@michaelcho.dev>
2025-01-02 12:03:25 -0500

    notifiers: add click shell completions

--- Formula/n/notifiers.rb
+++ Formula/n/notifiers.rb
@@ -77,6 +77,8 @@ class Notifiers < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"notifiers", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
