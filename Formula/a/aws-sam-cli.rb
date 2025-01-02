e5575d56d17f8e4e7600c5e62d009140f41931e5
Michael Cho <michael@michaelcho.dev>
2025-01-01 20:00:28 -0500

    aws-sam-cli: add shell completions

--- Formula/a/aws-sam-cli.rb
+++ Formula/a/aws-sam-cli.rb
@@ -408,6 +408,8 @@ class AwsSamCli < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"sam", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
