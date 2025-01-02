db7cdf9703438a36825a2ef2b72de7776932b588
Michael Cho <michael@michaelcho.dev>
2025-01-02 16:35:16 -0500

    goolabs: add click shell completions

--- Formula/g/goolabs.rb
+++ Formula/g/goolabs.rb
@@ -48,6 +48,8 @@ class Goolabs < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"goolabs", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
