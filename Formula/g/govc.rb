6a82662630c570abb02b923ad032e21da9bf3d64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-03 07:09:43 +0000

    govc: update 0.47.0 bottle.

--- Formula/g/govc.rb
+++ Formula/g/govc.rb
@@ -14,12 +14,12 @@ class Govc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "798fc456960d71bed7d401a63620ece2c925b741bcac8b600b9aed459f55180a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "edfed6ec44d9d5f7586e7ae130ecb2c6bddcbe5231fd8d7a09cd2fa84928ce84"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0ef3b8be85964085c5916e4dbe6ee6f16459d2fe5f73e2d9e8ccd6e4e4653bb7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ed5dd3d0bfe2f94444d88f755bf83094a1851ad5f310a64ec1c9c2a4f5674ea3"
-    sha256 cellar: :any_skip_relocation, ventura:       "7a663e53146e4bc2e88d8e9f547f66530ae4c0d1072d0d080f411615a902f04a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21cf444ab571bc4f0f2d8039465bc638682ff2fc32a6f26c3d96be8a0e7d0c9e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "00f1326daf0d4764961c14831cc9fec94cdd9b81cc383355689a183e5da3adc9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00f1326daf0d4764961c14831cc9fec94cdd9b81cc383355689a183e5da3adc9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "00f1326daf0d4764961c14831cc9fec94cdd9b81cc383355689a183e5da3adc9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3feb1a70cba6d3b52c54aff0a5cc3f78b4ac035c439b59d72f0a0c379f990cd5"
+    sha256 cellar: :any_skip_relocation, ventura:       "3feb1a70cba6d3b52c54aff0a5cc3f78b4ac035c439b59d72f0a0c379f990cd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "093ada8e378646f6bbc2d2f21b96b9986a418c923f21104e0ea1371eb88ec06c"
   end
 
   depends_on "go" => :build
