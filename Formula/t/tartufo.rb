438dba4335ac2db26ca7a79599a3b1b607fb8a0e
Michael Cho <michael@michaelcho.dev>
2025-01-02 20:01:03 -0500

    tartufo: add click shell completions

--- Formula/t/tartufo.rb
+++ Formula/t/tartufo.rb
@@ -49,6 +49,8 @@ class Tartufo < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"tartufo", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
