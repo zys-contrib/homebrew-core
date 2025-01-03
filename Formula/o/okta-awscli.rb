65c140dcbe9b2366470fa83c073ba13f5e965c5c
Michael Cho <michael@michaelcho.dev>
2025-01-02 19:54:12 -0500

    okta-awscli: add click shell completions

--- Formula/o/okta-awscli.rb
+++ Formula/o/okta-awscli.rb
@@ -93,6 +93,8 @@ class OktaAwscli < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"okta-awscli", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
