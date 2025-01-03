f5b263209613d5552dcd3be0a720de455f24559d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-03 01:16:57 +0000

    name-that-hash: update 1.11.0_1 bottle.

--- Formula/n/name-that-hash.rb
+++ Formula/n/name-that-hash.rb
@@ -10,8 +10,8 @@ class NameThatHash < Formula
   head "https://github.com/HashPals/Name-That-Hash.git", branch: "main"
 
   bottle do
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, all: "f5f2e0cbb22fb4686764e9d6eed612bd179fa3bee5b9fd8e2040541a291f87aa"
+    rebuild 4
+    sha256 cellar: :any_skip_relocation, all: "ebc63e1cd2a29eded2a1d8bd312f4df14882bd40e4e98f713e69aad521b47e0e"
   end
 
   depends_on "python@3.13"
