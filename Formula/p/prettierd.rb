9abf1da37949a5e8ecdd789da969a81ee6ace1d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-04 05:06:25 +0000

    prettierd 0.26.0

--- Formula/p/prettierd.rb
+++ Formula/p/prettierd.rb
@@ -1,8 +1,8 @@
 class Prettierd < Formula
   desc "Prettier daemon"
   homepage "https://github.com/fsouza/prettierd"
-  url "https://registry.npmjs.org/@fsouza/prettierd/-/prettierd-0.25.4.tgz"
-  sha256 "3150e6d3502855632dc259a717e3833d321db6915d57c958cab6463e292cc033"
+  url "https://registry.npmjs.org/@fsouza/prettierd/-/prettierd-0.26.0.tgz"
+  sha256 "4e99452dd2fb62971f8bf035d76af38d0af9a10ce9166f82b9a42d86b9ce0d7c"
   license "ISC"
 
   bottle do
