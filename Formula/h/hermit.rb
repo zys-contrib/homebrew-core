016335cb5651e9f472724fa0d6ddfd9720adb469
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-06 16:59:54 +0000

    hermit 0.42.0

--- Formula/h/hermit.rb
+++ Formula/h/hermit.rb
@@ -1,8 +1,8 @@
 class Hermit < Formula
   desc "Manages isolated, self-bootstrapping sets of tools in software projects"
   homepage "https://cashapp.github.io/hermit"
-  url "https://github.com/cashapp/hermit/archive/refs/tags/v0.41.0.tar.gz"
-  sha256 "330ee3826bc686135b8c6e3ae764e27fe2e446df484f04d08fc90bb86beea9db"
+  url "https://github.com/cashapp/hermit/archive/refs/tags/v0.42.0.tar.gz"
+  sha256 "fb03ba33da2c38bde1cff17dab66b84cdc49bec6b38860f7b662ad782ea64d98"
   license "Apache-2.0"
 
   bottle do
