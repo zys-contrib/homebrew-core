e77a0bebed4ccacf28af5ce8b757b5d1eaaaf26b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-06 19:44:22 +0000

    hermit: update 0.42.0 bottle.

--- Formula/h/hermit.rb
+++ Formula/h/hermit.rb
@@ -6,12 +6,12 @@ class Hermit < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07dc2c7c6a3327033f61755f0e4860ee1e7972ff0643b576fcb03b14bffbd629"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e8da34bff41a7f5c0c4e738e5ecbcb1184334d307cf0046eb9cdb1c1976b1d02"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b680e7d511afdebf205f0b51be756a7f92079a48ec5a8cfc9e511ce38fbd94be"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5268f3ae0ed4ce9dc0d54ede0bf21a88924cab70be595e02ae8c476ff35faed0"
-    sha256 cellar: :any_skip_relocation, ventura:       "092637f88e7638750b7118cf4f0b3688fb8b314116267b16d8e01f659fbf15d3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61ae0e9f614015f43fea330c98565e51e636147ae12e2ba61afd042f26a645d5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d6b58196bdb896a83f7b2ee9ecc9fa3a6595d2e44add7d51961b6b802049d829"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e2678e186fc45eac1df9ae62106d8b7531ca56fa9486c654b3d73bd81a3493d9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "cce18ba51647e26e039192d296599a4fc1d832c407723a0053eba9d93c63e1f6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b4ad09a922c071184f7f989268087f997e80c969f43f0d86985c748992bb1bf7"
+    sha256 cellar: :any_skip_relocation, ventura:       "4980ff0900535c34c42a4be5336af8531d922e570687808f98f7fe76eeafc38a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d183ca6ddc7cb88bd9f2a2b6e9a4abd6d3a5aafb56b28c495c0484c77621fd79"
   end
 
   depends_on "go" => :build
