46b21aa6a15f20520204f40c8d67c7ee8b3a5029
Jared Allard <jaredallard@users.noreply.github.com>
2025-01-06 09:38:13 -0800

    stencil: switch to rgst-io/stencil and upgrade to v2
    Following https://github.com/orgs/Homebrew/discussions/5587, I'm
    updating the `stencil` formula to point to
    https://github.com/rgst-io/stencil.
    
    Upstream now points to this fork as its successor:
    https://github.com/getoutreach/stencil#stencil.

--- Formula/s/stencil.rb
+++ Formula/s/stencil.rb
@@ -1,10 +1,10 @@
 class Stencil < Formula
-  desc "Smart templating engine for service development"
-  homepage "https://engineering.outreach.io/stencil/"
-  url "https://github.com/getoutreach/stencil/archive/refs/tags/v1.40.0.tar.gz"
-  sha256 "4d7ae67613a5dc6e710f551a528fa136ecdcad7bb758d47d542f0e8b5692b7cb"
+  desc "Modern living-template engine for evolving repositories"
+  homepage "https://stencil.rgst.io"
+  url "https://github.com/rgst-io/stencil/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "3a281bca9d895e8b1945e441f3671dacdbc8651bb02685e52019ac0333b0f374"
   license "Apache-2.0"
-  head "https://github.com/getoutreach/stencil.git", branch: "main"
+  head "https://github.com/rgst-io/stencil.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "f8dc046cc95bb71c79805b47df3a912787bcbfe344e9b843120a709ecf55aafb"
@@ -18,8 +18,13 @@ class Stencil < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/getoutreach/gobox/pkg/app.Version=v#{version} -X github.com/getoutreach/gobox/pkg/updater/Disabled=true"),
-      "./cmd/stencil"
+    ldflags = %W[
+      -s -w
+      -X go.rgst.io/stencil/v2/internal/version.version=#{version}
+      -X go.rgst.io/stencil/v2/internal/version.builtBy=homebrew
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "./cmd/stencil"
   end
 
   test do
