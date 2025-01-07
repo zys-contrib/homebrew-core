4baa52eee8054ffd6916c61f5e773d41970f7d68
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-07 08:09:17 +0000

    sesh 2.8.0

--- Formula/s/sesh.rb
+++ Formula/s/sesh.rb
@@ -1,8 +1,8 @@
 class Sesh < Formula
   desc "Smart session manager for the terminal"
   homepage "https://github.com/joshmedeski/sesh"
-  url "https://github.com/joshmedeski/sesh/archive/refs/tags/v2.7.0.tar.gz"
-  sha256 "59d69e130dccad1784357ac31e929178e49396af1478c8964acef9e3de9710d3"
+  url "https://github.com/joshmedeski/sesh/archive/refs/tags/v2.8.0.tar.gz"
+  sha256 "a0e0cb66932265aec2f03fd07507c1b4a94748b21fafcc29d1e3b57b19016e79"
   license "MIT"
   head "https://github.com/joshmedeski/sesh.git", branch: "main"
 
