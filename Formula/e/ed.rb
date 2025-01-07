531801322483366f135038af929d028539745eb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-07 19:57:49 +0000

    ed 1.21

--- Formula/e/ed.rb
+++ Formula/e/ed.rb
@@ -1,9 +1,9 @@
 class Ed < Formula
   desc "Classic UNIX line editor"
   homepage "https://www.gnu.org/software/ed/ed.html"
-  url "https://ftp.gnu.org/gnu/ed/ed-1.20.2.tar.lz"
-  mirror "https://ftpmirror.gnu.org/ed/ed-1.20.2.tar.lz"
-  sha256 "65fec7318f48c2ca17f334ac0f4703defe62037bb13cc23920de077b5fa24523"
+  url "https://ftp.gnu.org/gnu/ed/ed-1.21.tar.lz"
+  mirror "https://ftpmirror.gnu.org/ed/ed-1.21.tar.lz"
+  sha256 "60e24998727d453a5cf02c54664b97536de46a2b34cd1f4f67c1c1a61bbbad75"
   license "GPL-3.0-or-later"
 
   bottle do
