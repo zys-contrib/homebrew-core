106e997826ddd656ced4e696790ee8b440a48a18
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-07 14:55:07 +0000

    cargo-shuttle 0.50.0
    cargo-shuttle: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cargo-shuttle.rb
+++ Formula/c/cargo-shuttle.rb
@@ -1,10 +1,9 @@
 class CargoShuttle < Formula
   desc "Build & ship backends without writing any infrastructure files"
   homepage "https://shuttle.dev"
-  url "https://github.com/shuttle-hq/shuttle/archive/refs/tags/v0.49.0.tar.gz"
-  sha256 "fe13c6a0717edd1d6ec838c6abf02d3230b379083d4daf8f63621d47d1ceded6"
+  url "https://github.com/shuttle-hq/shuttle/archive/refs/tags/v0.50.0.tar.gz"
+  sha256 "25dd018a61debf6cb84de0085646367185912f1db8441a3b6a177a8365d8de42"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/shuttle-hq/shuttle.git", branch: "main"
 
   bottle do
@@ -36,7 +35,7 @@ class CargoShuttle < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/shuttle --version")
-    assert_match "Error: \e[1m401 Unauthorized", shell_output("#{bin}/shuttle account 2>&1", 1)
+    assert_match "Forbidden", shell_output("#{bin}/shuttle account 2>&1", 1)
     assert_match "Error: failed to get cargo metadata", shell_output("#{bin}/shuttle deployment status 2>&1", 1)
   end
 end
