7a9937424117f97955fc0e29085f1455d0bf1a0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-08 21:51:38 +0000

    semgrep: update 1.102.0 bottle.

--- Formula/s/semgrep.rb
+++ Formula/s/semgrep.rb
@@ -15,12 +15,12 @@ class Semgrep < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "f2e8ecf5f1bf339eca3118ce1f5e4c83c77c2f415f14fcfa494efc890cfc5825"
-    sha256 cellar: :any,                 arm64_sonoma:  "35dcbcabf8153bad7fd770d3df74d6a65f7bcf1db3bddb8bab9d15d878f57120"
-    sha256 cellar: :any,                 arm64_ventura: "51dcd5cbfc45d5861fe06997048fca72d51a5363ccfe84e05ea996e755838eeb"
-    sha256 cellar: :any,                 sonoma:        "d988e73ec368ca5cef2a2eb110001e1f4a4ec432a95306cd43fff581565a8496"
-    sha256 cellar: :any,                 ventura:       "bca0bf6d8fac572da83784b031c21fe108025b83f54620cc049b306834d090ae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec01020e2b5334a91d30723ee87b5fef11e57bce577ba153a14a7fb2eb5b47ce"
+    sha256 cellar: :any,                 arm64_sequoia: "dcf489e6e4fd68ba8ca3c20081d252ff2508ab50b4040ce5cca117650491235a"
+    sha256 cellar: :any,                 arm64_sonoma:  "41178995bd032293f766ca564948de6de4d000707e49dd1d232815b295b3c577"
+    sha256 cellar: :any,                 arm64_ventura: "fdd2335db39a74e92947aa09a183805e03fd4800ee910bf7e2ea17b69dbf855d"
+    sha256 cellar: :any,                 sonoma:        "8eea5a07f525d5947ae101d3fcfee7126fce450ab0180e7055941fa4fd5fbc80"
+    sha256 cellar: :any,                 ventura:       "bfecb19c1d355d270e99e6e26902ddc9759ef6ee634dc155e91ea364c35f487d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "091c7592a7772b0f150c0d7fd9cf18152de140dbe604aacebc8ea2dea659f225"
   end
 
   depends_on "autoconf" => :build
