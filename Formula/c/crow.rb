bfa5cf0c8efde4b292fb824622bd0a0aaa5895ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-08 02:18:37 +0000

    crow 1.2.1

--- Formula/c/crow.rb
+++ Formula/c/crow.rb
@@ -1,8 +1,8 @@
 class Crow < Formula
   desc "Fast and Easy to use microframework for the web"
   homepage "https://crowcpp.org"
-  url "https://github.com/CrowCpp/Crow/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "c80d0b23c6a20f8aa6fe776669dc8a9fb984046891d2f70bfc0539d16998164b"
+  url "https://github.com/CrowCpp/Crow/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "552f2e447adf70ed4c667d6f82db53dfc70710b50431004ab1405f5b53f04c30"
   license "BSD-3-Clause"
   head "https://github.com/CrowCpp/Crow.git", branch: "master"
 
