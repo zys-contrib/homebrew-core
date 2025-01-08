f18d04d591e0c5c53a9fdc55ff73566390999187
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-08 10:50:22 +0000

    bash_unit 2.3.2

--- Formula/b/bash_unit.rb
+++ Formula/b/bash_unit.rb
@@ -1,8 +1,8 @@
 class BashUnit < Formula
   desc "Bash unit testing enterprise edition framework for professionals"
   homepage "https://github.com/pgrange/bash_unit"
-  url "https://github.com/pgrange/bash_unit/archive/refs/tags/v2.3.1.tar.gz"
-  sha256 "30aefc0a75196000680f0668b495f3f98c568eb06d9187eab1e9b2e07237802c"
+  url "https://github.com/pgrange/bash_unit/archive/refs/tags/v2.3.2.tar.gz"
+  sha256 "368d1712d4c265909a5039ea91180dba1db5b15b5a02cf24cfb3b7547c0e9150"
   license "GPL-3.0-only"
 
   bottle do
