23c6ea3dc0539c74f2b8edef06fd974d0b31cd30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-08 08:05:08 +0000

    kubie 0.24.1

--- Formula/k/kubie.rb
+++ Formula/k/kubie.rb
@@ -1,8 +1,8 @@
 class Kubie < Formula
   desc "Much more powerful alternative to kubectx and kubens"
   homepage "https://blog.sbstp.ca/introducing-kubie/"
-  url "https://github.com/sbstp/kubie/archive/refs/tags/v0.24.0.tar.gz"
-  sha256 "a1ed2272808eeb444adcb405ef385e1f03cd0d4f4dc12f4418ebde5f0b1789ac"
+  url "https://github.com/sbstp/kubie/archive/refs/tags/v0.24.1.tar.gz"
+  sha256 "ccf6bbe16e80c73cf9d17b50342cfc31886e5a8cc6b35bc794dd12ac4a781257"
   license "Zlib"
   head "https://github.com/sbstp/kubie.git", branch: "master"
 
