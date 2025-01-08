f1d0820f91d45a52b002d61cfaec68697afc1d74
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-08 18:19:27 +0000

    metabase: update 0.52.5 bottle.

--- Formula/m/metabase.rb
+++ Formula/m/metabase.rb
@@ -11,7 +11,7 @@ class Metabase < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9c1b59132401472ad8f657a8917e4ff704b29e4f3f11b131f39e240c7a86feb5"
+    sha256 cellar: :any_skip_relocation, all: "6a03950f6548f9e8b7d2ea14b48153da630a08a9430c29f154c79f294398afb0"
   end
 
   head do
