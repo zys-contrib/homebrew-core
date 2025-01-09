455ca090af90ddfd0fc0078be9e187bf59001ea1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-09 13:59:06 +0000

    gnu-getopt 2.40.3

--- Formula/g/gnu-getopt.rb
+++ Formula/g/gnu-getopt.rb
@@ -1,8 +1,8 @@
 class GnuGetopt < Formula
   desc "Command-line option parsing utility"
   homepage "https://github.com/util-linux/util-linux"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.40/util-linux-2.40.2.tar.xz"
-  sha256 "d78b37a66f5922d70edf3bdfb01a6b33d34ed3c3cafd6628203b2a2b67c8e8b3"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.40/util-linux-2.40.3.tar.xz"
+  sha256 "0d6429d0cc474eafb972b0f4fee6b9c3d3f31c7bbada012bb3a1e255f00612c5"
   license "GPL-2.0-or-later"
 
   livecheck do
