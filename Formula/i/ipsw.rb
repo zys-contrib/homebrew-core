7a3d51f2b6cdf2c8a4ba44c2f1295a32dd2f6fab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-09 20:44:48 +0000

    ipsw: update 3.1.562 bottle.

--- Formula/i/ipsw.rb
+++ Formula/i/ipsw.rb
@@ -12,12 +12,12 @@ class Ipsw < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35a1fc12e91cad407b2f6dc885348435a3e54bd39bcbe7fb30262277c29a362b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "702fbf68680926ca427b87234a9a5cf7d5f0f44b4933b79ab2d1d9e26c486f01"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8d2a65cac0b47801477eb72fd92a9f30c35dc977911fa3587e349ba13a2b2583"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9f69b13ecec444fe3d58a56e151f0de403ce89280c3c40c870997353458c2c97"
-    sha256 cellar: :any_skip_relocation, ventura:       "e82974a7e39756aed32a76db44841b472fc073bc4cef5e1902973f4df8a50f62"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5744cc09a5467c618b91a78a34b79274ee7c77f5cf672e92151a9a340a49a169"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3937624091bcd8ffb2b95fff5fd64dd21069dad0bd83ee5a5a3c9f51ab9dc2b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f89ca357f44e0851b5cfbddb78665ca08058c01f395ccdcd4ab13971ea1b241"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f690fc508fd3d77e3045e217123192c8023f8ec1104b2553a9344178093f6934"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6a1a190b3e0bfe29c71950bd617e4c6847245e3b561e4cc7e44cf867f4ab8589"
+    sha256 cellar: :any_skip_relocation, ventura:       "a6e142a93b564d60ddedcdaf388a04c257461e4acdd6ef3343131dc9421df586"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "22a904c004392135c6120a02ce45405f5c2e37a6bf23f9a68943e2c8adbf1231"
   end
 
   depends_on "go" => :build
