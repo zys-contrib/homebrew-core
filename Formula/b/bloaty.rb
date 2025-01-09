3265b7e4ec741bc915715c58c5ce1b5e3ac212b0
Rui Chen <rui@chenrui.dev>
2025-01-08 20:30:59 -0500

    bloaty: revision bump (protobuf 29.3)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/bloaty.rb
+++ Formula/b/bloaty.rb
@@ -4,7 +4,7 @@ class Bloaty < Formula
   url "https://github.com/google/bloaty/releases/download/v1.1/bloaty-1.1.tar.bz2"
   sha256 "a308d8369d5812aba45982e55e7c3db2ea4780b7496a5455792fb3dcba9abd6f"
   license "Apache-2.0"
-  revision 35
+  revision 36
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "3903a8c98c15249ab765ea00d0def107762b404593bf37cc48366fbd02eb3e4d"
