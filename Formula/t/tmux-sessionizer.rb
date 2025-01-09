c91dc2b76fc018fcd1b8e647993c5e29e236dcb8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-09 13:50:18 +0000

    tmux-sessionizer: update 0.4.4_2 bottle.

--- Formula/t/tmux-sessionizer.rb
+++ Formula/t/tmux-sessionizer.rb
@@ -7,12 +7,12 @@ class TmuxSessionizer < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "4042bf4b217a95b253ea859c8c4426950ec8b0653fe873f8b5b2624399e36186"
-    sha256 cellar: :any,                 arm64_sonoma:  "6d78bca70b4b810883e57ced9e19de3970f44e4817f7fc344f64f15b9c2598fd"
-    sha256 cellar: :any,                 arm64_ventura: "768a4b4e6037be396301f0dc9f121134034bd6388e800df6b3f68d1acc3a397a"
-    sha256 cellar: :any,                 sonoma:        "a50d45fc6db7c4ce2f5190900adb7f278635d58dbdbb844e39c3d99f1dcea374"
-    sha256 cellar: :any,                 ventura:       "b209c7ff2dd2e36422eda390fd21cab761b44b0378727de497068f9b8327c533"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "67cde82247503f1f870cfe96d88e70a55c91edbb14fb25cdc2098a90723f8ae1"
+    sha256 cellar: :any,                 arm64_sequoia: "36296ed7973d20aa0bb90e6b500a0136ed4f367553608f2c4dd1ef658345523f"
+    sha256 cellar: :any,                 arm64_sonoma:  "53b15bddb62d79bde6f63a5cfc18910fee1e0c3f7201ee7c9dd1e0aab367ace9"
+    sha256 cellar: :any,                 arm64_ventura: "f0eadc021f990f7d2db80142244a4eb679aa1125984589e50b77b6d8a6d9c2a3"
+    sha256 cellar: :any,                 sonoma:        "10ee6f3d53591391cb711b5ed9876bc152a4bdaddb9488ab0bd27315f6c98fc5"
+    sha256 cellar: :any,                 ventura:       "9369b6cea10b3b085a0d92fd59dba3e9951ab319e378f1b4a47e02623b67a038"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e65235329ebce9f8e96e603c471bcab6b377234c6a90171b837fce670f617ecf"
   end
 
   depends_on "pkgconf" => :build
