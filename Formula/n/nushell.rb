a8250470789fb330bb0c11621668e16ece196c94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-10 06:10:31 +0000

    nushell: update 0.101.0_2 bottle.

--- Formula/n/nushell.rb
+++ Formula/n/nushell.rb
@@ -20,12 +20,12 @@ class Nushell < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "62e10339cf5c4ff5c7843d847e4273196db19ae731acdf13dc69e812ea5720d0"
-    sha256 cellar: :any,                 arm64_sonoma:  "0ddf79eae5d8ebd70131436862ea88fd0f8edb1882057061bf9bb82636b78818"
-    sha256 cellar: :any,                 arm64_ventura: "ca2991396f58784cf3cb26924b8d940610f787f195d64c32514b167c34342a16"
-    sha256 cellar: :any,                 sonoma:        "6fb7e6320828abfaca040d0afc32da95df84fef057220df6c78881bca559a83d"
-    sha256 cellar: :any,                 ventura:       "d892426eb6a147c2abf2b94808caabe64ffd4f25a56b60eceb510dccb78bba4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e77c7dd2a77f4cbfce2deb012c6481a8c7898615216f320e8f887dc688f693be"
+    sha256 cellar: :any,                 arm64_sequoia: "a25a1d727d750e850bc21be58a35ed017d1295dbbde66d1bf571c7e6b94b216c"
+    sha256 cellar: :any,                 arm64_sonoma:  "93bb51e8020d2117712274f8970e5aed7c9091d17e87678ef9511f5cf3f8ca44"
+    sha256 cellar: :any,                 arm64_ventura: "80612bc90412f28b96056e3081ea1f2bf0dd2ef515572729ca05834561287cdd"
+    sha256 cellar: :any,                 sonoma:        "0aaf21248af61048b45a7d5b7d97e10f4989e7a13b12e9cf451007fc34ed4bb8"
+    sha256 cellar: :any,                 ventura:       "7af7cce0e2dbba3a6c41592a0cd2f2f8f83fa7040ac8fbb2454e5609c3aabfe1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad018dbb49364c9fbe462c267b7c626a47a8307068da49a3d34e36b1fc85cc48"
   end
 
   depends_on "rust" => :build
