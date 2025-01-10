a5fdfb985d96a8728285eb7500aa626bd4c531f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-10 02:39:38 +0000

    re-flex 5.1.1

--- Formula/r/re-flex.rb
+++ Formula/r/re-flex.rb
@@ -1,8 +1,8 @@
 class ReFlex < Formula
   desc "Regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/refs/tags/v5.1.0.tar.gz"
-  sha256 "42bb511b6d7e12faf2972c4311534d3cca71cc84d2e9f3dd433e0eefb3b46fd1"
+  url "https://github.com/Genivia/RE-flex/archive/refs/tags/v5.1.1.tar.gz"
+  sha256 "550e371b3b52aad8836b0679ce0f6898f39cf4a6865651aa3f59b2201dd0740c"
   license "BSD-3-Clause"
 
   bottle do
