1434f7d45eb10094256fa89b3846565db2d5c5d2
Rui Chen <rui@chenrui.dev>
2025-01-10 17:18:37 -0500

    kube-bench 0.9.4 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/k/kube-bench.rb
@@ -0,0 +1,23 @@
+class KubeBench < Formula
+  desc "Checks Kubernetes deployment against security best practices (CIS Benchmark)"
+  homepage "https://github.com/aquasecurity/kube-bench"
+  url "https://github.com/aquasecurity/kube-bench/archive/refs/tags/v0.9.4.tar.gz"
+  sha256 "d1f7f4837485cb34cb6e1b8141d20ee8eb0e3db30c52dec6a329f73721251422"
+  license "Apache-2.0"
+  head "https://github.com/aquasecurity/kube-bench.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/aquasecurity/kube-bench/cmd.KubeBenchVersion=#{version}")
+
+    generate_completions_from_executable(bin/"kube-bench", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kube-bench version")
+
+    output = shell_output("#{bin}/kube-bench run 2>&1", 1)
+    assert_match "error: config file is missing 'version_mapping' section", output
+  end
+end
