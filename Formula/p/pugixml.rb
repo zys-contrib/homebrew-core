fa106f5e1119d4a55592b77198d7058182d06750
Rui Chen <rui@chenrui.dev>
2025-01-11 14:54:17 -0500

    pugixml 1.15
    autobump: add pugixml
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pugixml.rb
+++ Formula/p/pugixml.rb
@@ -1,8 +1,8 @@
 class Pugixml < Formula
   desc "Light-weight C++ XML processing library"
   homepage "https://pugixml.org/"
-  url "https://github.com/zeux/pugixml/releases/download/v1.14/pugixml-1.14.tar.gz"
-  sha256 "2f10e276870c64b1db6809050a75e11a897a8d7456c4be5c6b2e35a11168a015"
+  url "https://github.com/zeux/pugixml/releases/download/v1.15/pugixml-1.15.tar.gz"
+  sha256 "655ade57fa703fb421c2eb9a0113b5064bddb145d415dd1f88c79353d90d511a"
   license "MIT"
 
   livecheck do
@@ -24,9 +24,12 @@ class Pugixml < Formula
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-                    "-DBUILD_SHARED_LIBS=ON",
-                    "-DPUGIXML_BUILD_SHARED_AND_STATIC_LIBS=ON"
+    args = %w[
+      -DBUILD_SHARED_LIBS=ON
+      -DPUGIXML_BUILD_SHARED_AND_STATIC_LIBS=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -50,8 +53,7 @@ class Pugixml < Formula
       <root>Hello world!</root>
     XML
 
-    system ENV.cxx, "test.cpp", "-o", "test", "-I#{include}",
-                    "-L#{lib}", "-lpugixml"
+    system ENV.cxx, "test.cpp", "-o", "test", "-I#{include}", "-L#{lib}", "-lpugixml"
     system "./test"
   end
 end
