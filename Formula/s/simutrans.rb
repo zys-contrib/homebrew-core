87a0b2b81e328c8961241d2e0bdccf743be26a17
Rui Chen <rui@chenrui.dev>
2025-01-10 21:59:57 -0500

    simutrans: revision bump (miniupnpc 2.3.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/simutrans.rb
+++ Formula/s/simutrans.rb
@@ -4,7 +4,7 @@ class Simutrans < Formula
   url "svn://servers.simutrans.org/simutrans/trunk/", revision: "11395"
   version "124.2.2"
   license "Artistic-1.0"
-  revision 1
+  revision 2
   head "https://github.com/simutrans/simutrans.git", branch: "master"
 
   livecheck do
