e035f7e89cc2240ba6fdef12119c4e4973f0fe0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-11 13:55:01 +0000

    cppinsights 19.1
    cppinsights: remove build patches
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cppinsights.rb
+++ Formula/c/cppinsights.rb
@@ -1,10 +1,9 @@
 class Cppinsights < Formula
   desc "See your source code with the eyes of a compiler"
   homepage "https://cppinsights.io/"
-  url "https://github.com/andreasfertig/cppinsights/archive/refs/tags/v_17.0.tar.gz"
-  sha256 "2dd6bcfcdba65c0ed2e1f04ef79d57285186871ad8bd481d63269f3115276216"
+  url "https://github.com/andreasfertig/cppinsights/archive/refs/tags/v_19.1.tar.gz"
+  sha256 "88853a67b9eaf6917c531071436a275c62f1dcfe6f2e02e521c39ce81b05e6a7"
   license "MIT"
-  revision 2
 
   bottle do
     rebuild 1
@@ -24,23 +23,15 @@ class Cppinsights < Formula
     cause "Requires Clang > 15.0"
   end
 
-  # Patch from https://github.com/andreasfertig/cppinsights/pull/622
-  # Support for LLVM 18, remove in next version
-  patch :DATA
-
-  # Support for LLVM 19, remove in next version
-  patch do
-    url "https://github.com/andreasfertig/cppinsights/commit/a84a979abdd0cd57790d0795c3642198188215e9.patch?full_index=1"
-    sha256 "fcfccbddc4e1c4b0fbb359fcd1c9dca58c4a5f15a175c53c449586b17217e079"
-  end
-
   def install
     ENV.llvm_clang if OS.mac? && DevelopmentTools.clang_build_version <= 1500
 
-    system "cmake", "-S", ".", "-B", "build",
-                    "-DINSIGHTS_LLVM_CONFIG=#{Formula["llvm"].opt_bin}/llvm-config",
-                    "-DINSIGHTS_USE_SYSTEM_INCLUDES=Off",
-                    *std_cmake_args
+    args = %W[
+      -DINSIGHTS_LLVM_CONFIG=#{Formula["llvm"].opt_bin}/llvm-config
+      -DINSIGHTS_USE_SYSTEM_INCLUDES=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -54,51 +45,3 @@ class Cppinsights < Formula
     assert_match "{2, 3, 4, 0, 0}", shell_output("#{bin}/insights ./test.cpp")
   end
 end
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 31341709..8b7430db 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -1,5 +1,4 @@
--cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
--# 3.8* is required because of C++17 support
-+cmake_minimum_required(VERSION 3.20 FATAL_ERROR)
- 
- # For better control enable MSVC_RUNTIME_LIBRARY target property
- # see https://cmake.org/cmake/help/latest/policy/CMP0091.html
-@@ -33,7 +32,7 @@ option(INSIGHTS_STATIC              "Use static linking"         Off)
- 
- set(INSIGHTS_LLVM_CONFIG "llvm-config" CACHE STRING "LLVM config executable to use")
- 
--set(INSIGHTS_MIN_LLVM_MAJOR_VERSION 17)
-+set(INSIGHTS_MIN_LLVM_MAJOR_VERSION 18)
- set(INSIGHTS_MIN_LLVM_VERSION ${INSIGHTS_MIN_LLVM_MAJOR_VERSION}.0)
- 
- if(NOT DEFINED LLVM_VERSION_MAJOR)  # used when build inside the clang tool/extra folder
-@@ -372,6 +371,17 @@ if (BUILD_INSIGHTS_OUTSIDE_LLVM)
-     # additional libs required when building insights outside llvm
-     set(ADDITIONAL_LIBS
-         ${LLVM_LDFLAGS}
-+    )
-+
-+    if(${LLVM_PACKAGE_VERSION_PLAIN} VERSION_GREATER_EQUAL "18.0.0")
-+        set(ADDITIONAL_LIBS
-+            ${ADDITIONAL_LIBS}
-+            clangAPINotes
-+        )
-+    endif()
-+
-+    set(ADDITIONAL_LIBS
-+        ${ADDITIONAL_LIBS}
-         clangFrontend
-         clangDriver
-         clangSerialization
-@@ -768,6 +778,7 @@ message(STATUS "[ Build summary ]")
- message(STATUS "CMAKE_GENERATOR       : ${CMAKE_GENERATOR}")
- message(STATUS "CMAKE_EXE_LINKER_FLAGS: ${CMAKE_EXE_LINKER_FLAGS}")
- message(STATUS "CMAKE_LINKER          : ${CMAKE_LINKER}")
-+message(STATUS "CMAKE_OSX_ARCHITECTURES : ${CMAKE_OSX_ARCHITECTURES}")
- message(STATUS "Compiler ID           : ${CMAKE_CXX_COMPILER_ID}")
- message(STATUS "Compiler version      : ${CMAKE_CXX_COMPILER_VERSION}")
- message(STATUS "Compiler path         : ${CMAKE_CXX_COMPILER}")
