39a5817ee7eb4886dae54ee2cde04c6b03609830
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-11 21:43:15 +0000

    oha: update 1.6.0 bottle.

--- Formula/o/oha.rb
+++ Formula/o/oha.rb
@@ -7,12 +7,12 @@ class Oha < Formula
   head "https://github.com/hatoo/oha.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "984dc4607535bb3a869d60fd1c34804ebfdd882b24dfd990eecafcd8679f4bf5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b23f4409e37e53965b0a82bfde684184f3080353c2e3aeac601e317069d953f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "4132c0d27dc38f1745b0179b77498c1edaca6fb7d540346a23d0a08e1387a6c4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "923cfd10cb1a3a398758aa6d2903df5ac91b473e0d49883db429eb759e38676c"
-    sha256 cellar: :any_skip_relocation, ventura:       "eaed6763c85875e5e8b71a6f707364a5404aed32bec63b30a7914ec5d2c89719"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9c077fcfef79627928278c2ac332303b34ffcdf13aa13e22d2d68ca992fe793c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4eac11cb3fb9c60fbffe35ae659200e4491eca44ba1d6dd9c790a1a9c20c328f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92d34d54400e6fd3b89941b53d9123ad3ccf48729f5d1be339d0bc82a866f723"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e1023f87ef5f7e7d0a8822b28e0b7686ffadb4a79ffcd1530c30ed0126926c1c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e0c6a3ab1ef063700120ae41e4d0effb55c4c7f77874a8c3c130e6529542c702"
+    sha256 cellar: :any_skip_relocation, ventura:       "33932e34c92a5cf9da0c0750b367db40fc63892c56cc045995038c7acfe427a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa9ae4a5a98546c0aea7407e2626a616c9e38b84db2649a947ce882812edcea7"
   end
 
   depends_on "cmake" => :build # for aws-lc-sys
