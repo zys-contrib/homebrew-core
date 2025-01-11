96659d4dd48d39189b29498f8723e188a7062d96
Rui Chen <rui@chenrui.dev>
2025-01-11 02:56:13 +0000

    kube-bench: add 0.9.4 bottle.

--- Formula/k/kube-bench.rb
+++ Formula/k/kube-bench.rb
@@ -6,6 +6,15 @@ class KubeBench < Formula
   license "Apache-2.0"
   head "https://github.com/aquasecurity/kube-bench.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da842a717f44a34d430bce35c7ca9d44ba6cdfff9b5656372741617abd535557"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "da842a717f44a34d430bce35c7ca9d44ba6cdfff9b5656372741617abd535557"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "da842a717f44a34d430bce35c7ca9d44ba6cdfff9b5656372741617abd535557"
+    sha256 cellar: :any_skip_relocation, sonoma:        "93fe13161db7fc7e9a5bd5ac80122238bf6e1fdded8918e4785138f0bd88de7b"
+    sha256 cellar: :any_skip_relocation, ventura:       "93fe13161db7fc7e9a5bd5ac80122238bf6e1fdded8918e4785138f0bd88de7b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac91acc5024d45376c9b9f97a8a69d6853b69559bb8ff55952f72e5a99185dfc"
+  end
+
   depends_on "go" => :build
 
   def install
