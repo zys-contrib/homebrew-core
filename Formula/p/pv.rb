3390495a20ecb01d19acb1448759522627ad4612
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-12 17:05:41 +0000

    pv 1.9.27

--- Formula/p/pv.rb
+++ Formula/p/pv.rb
@@ -1,8 +1,8 @@
 class Pv < Formula
   desc "Monitor data's progress through a pipe"
   homepage "https://www.ivarch.com/programs/pv.shtml"
-  url "https://www.ivarch.com/programs/sources/pv-1.9.25.tar.gz"
-  sha256 "162495aabb1cb842186cb224995e3d5f60a9f527a49ccbd8212383cc72b7c36c"
+  url "https://www.ivarch.com/programs/sources/pv-1.9.27.tar.gz"
+  sha256 "253659dc86569363f065f5e881e135a0c9594b987f34a19b104c7414a2d2c479"
   license "Artistic-2.0"
 
   livecheck do
