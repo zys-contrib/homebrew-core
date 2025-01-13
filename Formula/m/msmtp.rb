910cdfa078cec31f679b999eb5faaf369039b21e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-13 20:10:10 +0000

    msmtp 1.8.28

--- Formula/m/msmtp.rb
+++ Formula/m/msmtp.rb
@@ -1,8 +1,8 @@
 class Msmtp < Formula
   desc "SMTP client that can be used as an SMTP plugin for Mutt"
   homepage "https://marlam.de/msmtp/"
-  url "https://marlam.de/msmtp/releases/msmtp-1.8.27.tar.xz"
-  sha256 "94030580a63a747faa0a3b9b1b264ae355aad33a4d94b832bfeb5b21633c965e"
+  url "https://marlam.de/msmtp/releases/msmtp-1.8.28.tar.xz"
+  sha256 "3a57f155f54e4860f7dd42138d9bea1af615b99dfab5ab4cd728fc8c09a647a4"
   license "GPL-3.0-or-later"
 
   livecheck do
