71897b3d63a8f40cdb683a8e0aa133315005e4ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-13 10:53:35 +0000

    cypher-shell 5.26.1

--- Formula/c/cypher-shell.rb
+++ Formula/c/cypher-shell.rb
@@ -1,8 +1,8 @@
 class CypherShell < Formula
   desc "Command-line shell where you can execute Cypher against Neo4j"
   homepage "https://neo4j.com"
-  url "https://dist.neo4j.org/cypher-shell/cypher-shell-5.26.0.zip"
-  sha256 "a4bc4cd3ef6479fddb408f25affd98fdbca41562e24b576f1baa6bc943751e70"
+  url "https://dist.neo4j.org/cypher-shell/cypher-shell-5.26.1.zip"
+  sha256 "719c8e9a1e0cf34987e7f6dfa1d30c6a36093e661f306c8bcb1319bb8deb236b"
   license "GPL-3.0-only"
   version_scheme 1
 
