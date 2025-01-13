e97e9d1d595f651ddc2b6633062a195ba2721289
Branch Vincent <branchevincent@gmail.com>
2025-01-12 21:48:19 -0800

    scalapack 2.2.2
    scalapack: update test
    
    scalapack: remove livecheck
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/scalapack.rb
+++ Formula/s/scalapack.rb
@@ -1,15 +1,10 @@
 class Scalapack < Formula
   desc "High-performance linear algebra for distributed memory machines"
   homepage "https://netlib.org/scalapack/"
-  url "https://netlib.org/scalapack/scalapack-2.2.0.tgz"
-  sha256 "40b9406c20735a9a3009d863318cb8d3e496fb073d201c5463df810e01ab2a57"
+  url "https://github.com/Reference-ScaLAPACK/scalapack/archive/refs/tags/v2.2.2.tar.gz"
+  sha256 "a2f0c9180a210bf7ffe126c9cb81099cf337da1a7120ddb4cbe4894eb7b7d022"
   license "BSD-3-Clause"
-  revision 1
-
-  livecheck do
-    url :homepage
-    regex(/href=.*?scalapack[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
+  head "https://github.com/Reference-ScaLAPACK/scalapack.git", branch: "master"
 
   bottle do
     rebuild 1
@@ -28,55 +23,28 @@ class Scalapack < Formula
   depends_on "open-mpi"
   depends_on "openblas"
 
-  # Apply upstream commit to fix build with gfortran-12.  Remove in next release.
-  patch do
-    url "https://github.com/Reference-ScaLAPACK/scalapack/commit/a0f76fc0c1c16646875b454b7d6f8d9d17726b5a.patch?full_index=1"
-    sha256 "2b42d282a02b3e56bb9b3178e6279dc29fc8a17b9c42c0f54857109286a9461e"
-  end
-
-  patch :DATA
-
   def install
     # Fix compile with newer Clang
     ENV.append_to_cflags "-Wno-implicit-function-declaration" if DevelopmentTools.clang_build_version >= 1403
 
-    mkdir "build" do
-      blas = "-L#{Formula["openblas"].opt_lib} -lopenblas"
-      system "cmake", "..", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON",
-                      "-DBLAS_LIBRARIES=#{blas}", "-DLAPACK_LIBRARIES=#{blas}"
-      system "make", "all"
-      system "make", "install"
-    end
+    blas = "-L#{Formula["openblas"].opt_lib} -lopenblas"
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DBLAS_LIBRARIES=#{blas}",
+                    "-DLAPACK_LIBRARIES=#{blas}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     pkgshare.install "EXAMPLE"
   end
 
   test do
-    cp_r pkgshare/"EXAMPLE", testpath
-    cd "EXAMPLE" do
-      system "mpif90", "-o", "xsscaex", "psscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
-      assert `mpirun -np 4 ./xsscaex | grep 'INFO code' | awk '{print $NF}'`.to_i.zero?
-      system "mpif90", "-o", "xdscaex", "pdscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
-      assert `mpirun -np 4 ./xdscaex | grep 'INFO code' | awk '{print $NF}'`.to_i.zero?
-      system "mpif90", "-o", "xcscaex", "pcscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
-      assert `mpirun -np 4 ./xcscaex | grep 'INFO code' | awk '{print $NF}'`.to_i.zero?
-      system "mpif90", "-o", "xzscaex", "pzscaex.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack"
-      assert `mpirun -np 4 ./xzscaex | grep 'INFO code' | awk '{print $NF}'`.to_i.zero?
+    cp_r (pkgshare/"EXAMPLE").children, testpath
+
+    %w[psscaex pdscaex pcscaex pzscaex].each do |name|
+      system "mpif90", "#{name}.f", "pdscaexinfo.f", "-L#{opt_lib}", "-lscalapack", "-o", name
+      assert_match(/INFO code returned by .* 0/, shell_output("mpirun --map-by :OVERSUBSCRIBE -np 4 ./#{name}"))
     end
   end
 end
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 85ea82a..86222e0 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -232,7 +232,7 @@ append_subdir_files(src-C "SRC")
-
- if (UNIX)
-    add_library(scalapack ${blacs} ${tools} ${tools-C} ${extra_lapack} ${pblas} ${pblas-F} ${ptzblas} ${ptools} ${pbblas} ${redist} ${src} ${src-C})
--   target_link_libraries( scalapack ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
-+   target_link_libraries( scalapack ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES} ${MPI_Fortran_LIBRARIES})
-    scalapack_install_library(scalapack)
- else (UNIX) # Need to separate Fortran and C Code
-    OPTION(BUILD_SHARED_LIBS "Build shared libraries" ON )
