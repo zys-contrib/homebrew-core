0a2e447abcad6e09871b19ea5fea12371d99604d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-14 21:35:39 +0000

    git-xargs: update 0.1.12 bottle.

--- Formula/g/git-xargs.rb
+++ Formula/g/git-xargs.rb
@@ -11,14 +11,12 @@ class GitXargs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "bd5bd2a2ccae4ba3ea9a11351f784a716f6c04c5d19ff3550ead3ad65a3fc374"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "a5744688092b131063e03cff3e5b4296454034a676969ce876f2a60c20098ffc"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5c5f8b0819584d6ab7fc754c00fba6378e2c516b6a16892e2b61b6c0d6b94cbd"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ce1925921de92cfe8c090ed7ef3feadb73d4e35096206522c35f072f73db0dd2"
-    sha256 cellar: :any_skip_relocation, sonoma:         "04a14bf2456b204fb19af32f48b82fc516beb70c4b363f6d529f135463cf2910"
-    sha256 cellar: :any_skip_relocation, ventura:        "8b666cf6d66317160e4c2d17b11305fe42bbd1d5ad906153f276ec74eb4d0a35"
-    sha256 cellar: :any_skip_relocation, monterey:       "cf5f928741816c3d2a24216a30b3cc9dccc3c21a3beddd2887e5e63e69af1877"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05a2bcdc53dc8f8a42ff0b5d9bcab9041c736a8b9638c3da96fbbf55d354e2e1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "95c54cf7ddac0552103dfb59c8370857e55becd16c2e4e755db15b684e747daf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "95c54cf7ddac0552103dfb59c8370857e55becd16c2e4e755db15b684e747daf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "95c54cf7ddac0552103dfb59c8370857e55becd16c2e4e755db15b684e747daf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "774200b89aa3097caa2029f97ca0846e0dd1e13fc856d71cbb03d79c23105719"
+    sha256 cellar: :any_skip_relocation, ventura:       "774200b89aa3097caa2029f97ca0846e0dd1e13fc856d71cbb03d79c23105719"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e2ae97c43721e908e2656ff0293113953a71fd69be37a50049953f450a5dbfb"
   end
 
   depends_on "go" => :build
