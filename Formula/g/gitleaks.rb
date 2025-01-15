2a1e9881b4b7f1fbd196fe05a869b1b049a66ef9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-15 18:57:59 +0000

    gitleaks: update 8.23.1 bottle.

--- Formula/g/gitleaks.rb
+++ Formula/g/gitleaks.rb
@@ -14,12 +14,12 @@ class Gitleaks < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54ce122d426c542381aa7f3dc25915353c48ff3d26e1797461ca817a36d2780e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54ce122d426c542381aa7f3dc25915353c48ff3d26e1797461ca817a36d2780e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "54ce122d426c542381aa7f3dc25915353c48ff3d26e1797461ca817a36d2780e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d6e8dc3596269357baab3e3f37fee8ef7ed6180188eaded82de150be732d71ff"
-    sha256 cellar: :any_skip_relocation, ventura:       "d6e8dc3596269357baab3e3f37fee8ef7ed6180188eaded82de150be732d71ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d3a3a10473bd39ed1d1c8a3c5337485a92457c1584b028a5785ad26a23dcf7c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9d79c8f7a919b28755c181340f2c4c99cd0f6f949c1435952d07675f23c782ad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d79c8f7a919b28755c181340f2c4c99cd0f6f949c1435952d07675f23c782ad"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9d79c8f7a919b28755c181340f2c4c99cd0f6f949c1435952d07675f23c782ad"
+    sha256 cellar: :any_skip_relocation, sonoma:        "30bc8d2d9968cccd1fe9354d9b5995e27b19c8e19a1b74048709922ef1f6f67a"
+    sha256 cellar: :any_skip_relocation, ventura:       "30bc8d2d9968cccd1fe9354d9b5995e27b19c8e19a1b74048709922ef1f6f67a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "be1b69ab462a08b21ce360e887f7276416b222674e05e6de2cb735ce3f6ae957"
   end
 
   depends_on "go" => :build
