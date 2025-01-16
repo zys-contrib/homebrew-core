3db4a6467b928977befbd2df1241afd335cf84be
Rui Chen <rui@chenrui.dev>
2025-01-16 18:01:40 +0000

    mago: update 0.1.0 bottle.

--- Formula/m/mago.rb
+++ Formula/m/mago.rb
@@ -6,12 +6,12 @@ class Mago < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9f41b1e2da0163632b915325143f551f60b02fa05252b6c64492cf149f5e59c1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ab224a5ab535945cfb58bb00323bac9c195d0ade3721ab63b6a1b1a0b722b4d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6c9251da9a830249977a323e4b855d33492eb3418050a97d170c339ce32ab150"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e63c1a0bcddf5e339009bb9b966005ef09a805ffc211a05435bcb234d5c8ad2a"
-    sha256 cellar: :any_skip_relocation, ventura:       "56e27f96ac77e4575a9b528dfbe033a67767fc05343892550a3c456f34906c17"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8f28507a58c9b1989163daae2950fe3d29c68e9b5cf12544289fdbfaea3cf86"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a2c7a4ca1fab7df4c9cf5f198c8d1c81d7dc0a3d3a776b3760d2ef819808fb16"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fc794027df4bed8cf620362377b6f8640f1b766e3eaf54c3d9ac547b4c55d24e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "60998aabb9edcb5cbc786263b17063a01f4b294f5271e4768577bfddf17e738c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "21f1063d636fb85dc6f022b4efb3baeaddaf1bf44aa007165ba36241a04f65c5"
+    sha256 cellar: :any_skip_relocation, ventura:       "ddb2b2462f0a05f44f097fd6f08baba83bc922e7a25fe8b7e0be9f0bb1c88099"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0528a08376114b4d1a090524fad4608a1ef7beccbe8c293fb91732437e0eaee8"
   end
 
   depends_on "pkgconf" => :build
