f3b2d28f8652c2034ca43caeef9f2c2eadb80bb7
Patrick Linnane <patrick@linnane.io>
2025-01-16 04:46:53 +0000

    prism-cli: add 5.12.0 bottle.

--- Formula/p/prism-cli.rb
+++ Formula/p/prism-cli.rb
@@ -5,6 +5,15 @@ class PrismCli < Formula
   sha256 "43eef5f8b30f0da3d27f4d7dcd539fe91d0f0adfa2c8c5cef7d7e95601113cf4"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13c54a7c8d5e0ada56a59a40b42c65915f9b5df39f46e03a093a491272b06d07"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "13c54a7c8d5e0ada56a59a40b42c65915f9b5df39f46e03a093a491272b06d07"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "13c54a7c8d5e0ada56a59a40b42c65915f9b5df39f46e03a093a491272b06d07"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ec079ffe4838d0eef1b31982ed56f4f63884d7ade66e7ab75d3109c6ad17e8f4"
+    sha256 cellar: :any_skip_relocation, ventura:       "ec079ffe4838d0eef1b31982ed56f4f63884d7ade66e7ab75d3109c6ad17e8f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13c54a7c8d5e0ada56a59a40b42c65915f9b5df39f46e03a093a491272b06d07"
+  end
+
   depends_on "node"
 
   def install
