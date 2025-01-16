288cd7e0cfcc15c34631a50ecdd11539d23f0cea
Rui Chen <rui@chenrui.dev>
2025-01-16 22:02:03 +0000

    html2markdown: add 2.2.2 bottle.

--- Formula/h/html2markdown.rb
+++ Formula/h/html2markdown.rb
@@ -6,6 +6,15 @@ class Html2markdown < Formula
   license "MIT"
   head "https://github.com/JohannesKaufmann/html-to-markdown.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4c49578941f00d7f66318cdef1ece3d09b9cced00e27a76ac7bde1b9db6b115c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4c49578941f00d7f66318cdef1ece3d09b9cced00e27a76ac7bde1b9db6b115c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4c49578941f00d7f66318cdef1ece3d09b9cced00e27a76ac7bde1b9db6b115c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "662ab7768938a28f8135a1f4f3a6d6f2f218b1c2217ba73b78df0a6b9d51d0a0"
+    sha256 cellar: :any_skip_relocation, ventura:       "662ab7768938a28f8135a1f4f3a6d6f2f218b1c2217ba73b78df0a6b9d51d0a0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ca32e272b8621aad6ecd4572399e61d614d2af2dec2157741f31203a3fa672b4"
+  end
+
   depends_on "go" => :build
 
   def install
