4e53072cfaf9fdfd4319be6aa4fe2893f0c84c1b
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2025-01-17 23:18:30 +0000

    pkgx: add 2.1.0 bottle.

--- Formula/p/pkgx.rb
+++ Formula/p/pkgx.rb
@@ -6,6 +6,15 @@ class Pkgx < Formula
   license "Apache-2.0"
   head "https://github.com/pkgxdev/pkgx.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "acc706537e61dd2af1820c050d597391aa2ab77f564a8cde4dfb5a9518e219c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "05431a3bfe321785575475f0a965482452b1178f136081d2ff74d33c13128d55"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "224a1ffe0c053f6cb98d113d8d16fe94edd397600da7d07625e132bc6ce3e72b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "914f6a489d66b9821b1b22a78848692a1ba7cf93c7f070f42a1d7f2547b69a89"
+    sha256 cellar: :any_skip_relocation, ventura:       "b777cd91d17e9a7f0a3ea9415c44db1e1c5c9692c1c66520d0f5cd6b74fb2f9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7158dc75ccab6c6fafcafeff6b8d976afbc1ee478bdab4e6981fde060b402b83"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
