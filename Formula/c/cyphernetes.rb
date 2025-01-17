78c549a292fac552cab8296bef6ffb9d21ad904d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-17 23:59:29 +0000

    cyphernetes: update 0.15.3 bottle.

--- Formula/c/cyphernetes.rb
+++ Formula/c/cyphernetes.rb
@@ -6,12 +6,12 @@ class Cyphernetes < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "220b46e99f9831415a9ebb574164bd7ac6f156aa8d8700be6faa2d0c29ae99cb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "220b46e99f9831415a9ebb574164bd7ac6f156aa8d8700be6faa2d0c29ae99cb"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "220b46e99f9831415a9ebb574164bd7ac6f156aa8d8700be6faa2d0c29ae99cb"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2cbd9d7fbbc1dcbd5075b55cac66baa10b5635951d5717bcb8b4683ab759ccd1"
-    sha256 cellar: :any_skip_relocation, ventura:       "2cbd9d7fbbc1dcbd5075b55cac66baa10b5635951d5717bcb8b4683ab759ccd1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d49d216a246d947c431d624b23c92394b6801ca14a24670c1cb97c90cf92ea7e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f462f836991c6e166732b8e85b25f30c6d31d9a8262be472fdbb89818beec67b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f462f836991c6e166732b8e85b25f30c6d31d9a8262be472fdbb89818beec67b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f462f836991c6e166732b8e85b25f30c6d31d9a8262be472fdbb89818beec67b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "890404e2e099a3a26f72aea1fe414a4776c6c3095f431437a0cd938502545f7c"
+    sha256 cellar: :any_skip_relocation, ventura:       "890404e2e099a3a26f72aea1fe414a4776c6c3095f431437a0cd938502545f7c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34cfacc5355e0b595f57de98b151d7ecebca58c0fe330442d8d8b2b7a356fae1"
   end
 
   depends_on "go" => :build
