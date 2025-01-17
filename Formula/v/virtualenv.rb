5e00a998ed0cbd44edcec369740b280593331299
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-17 20:15:21 +0000

    virtualenv 20.29.1

--- Formula/v/virtualenv.rb
+++ Formula/v/virtualenv.rb
@@ -3,8 +3,8 @@ class Virtualenv < Formula
 
   desc "Tool for creating isolated virtual python environments"
   homepage "https://virtualenv.pypa.io/"
-  url "https://files.pythonhosted.org/packages/5a/5d/8d625ebddf9d31c301f85125b78002d4e4401fe1c15c04dca58a54a3056a/virtualenv-20.29.0.tar.gz"
-  sha256 "6345e1ff19d4b1296954cee076baaf58ff2a12a84a338c62b02eda39f20aa982"
+  url "https://files.pythonhosted.org/packages/a7/ca/f23dcb02e161a9bba141b1c08aa50e8da6ea25e6d780528f1d385a3efe25/virtualenv-20.29.1.tar.gz"
+  sha256 "b8b8970138d32fb606192cb97f6cd4bb644fa486be9308fb9b63f81091b5dc35"
   license "MIT"
   head "https://github.com/pypa/virtualenv.git", branch: "main"
 
