a233df583610cbae86a1daabf40fe26769059e60
Rui Chen <rui@chenrui.dev>
2025-01-17 14:39:00 +0000

    cargo-update: add 16.0.0 bottle.

--- Formula/c/cargo-update.rb
+++ Formula/c/cargo-update.rb
@@ -6,6 +6,15 @@ class CargoUpdate < Formula
   license "MIT"
   head "https://github.com/nabijaczleweli/cargo-update.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "2ff92e4df4e653b3321277e42b7141a1719c67a051e0c45e45d89911d5c04192"
+    sha256 cellar: :any,                 arm64_sonoma:  "eec24dd052eedc88109119bf77a9e54420767e488ee6dbd6762e0e0897ededce"
+    sha256 cellar: :any,                 arm64_ventura: "cb1fc2fa67c9b1dda6e801c50c6e68cb1e9bcb7c8b6886979562aeb4e4f4edca"
+    sha256 cellar: :any,                 sonoma:        "89264ed913452f134aeba5d1c58e8a29bd4e4f75808c5439395c8ddaad3cdd02"
+    sha256 cellar: :any,                 ventura:       "5fa331e905ebaff4f74780ab20fe70d1a8ab6a5da3fde7abb7b5abedeb2f524a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d7a296c94773835194c18b14d75b0935c6d4d9292774107c5078c831fe8144b"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "rustup" => :test
