515564ffad389c8918cbcbb228d653f1627781d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-17 00:27:04 +0000

    device-mapper: update 2.03.30 bottle.

--- Formula/d/device-mapper.rb
+++ Formula/d/device-mapper.rb
@@ -13,7 +13,7 @@ class DeviceMapper < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "0e9e3a6ed8b745338383e56a2cde2fd67190ff2006a666f8774df3941628000c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8c321e894295316cd5f53609d6a95f372f37dfdc427cfe44f43423a1a2c9836c"
   end
 
   depends_on "pkgconf" => :build
