2c3eab143bec62bb31fc735a2d016189c9c451d4
Rui Chen <rui@chenrui.dev>
2025-01-18 14:12:19 +0000

    fastbuild: add 1.14 bottle.

--- Formula/f/fastbuild.rb
+++ Formula/f/fastbuild.rb
@@ -5,6 +5,15 @@ class Fastbuild < Formula
   sha256 "07ebc683634aef24868f3247eedaf6e895869fa4f5b70e26a1d0c7ec892dfbc3"
   license "Zlib"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa94377400382948908b80e25b38e8bf672ed78ebfb31c657baffa8ab2c9d1c4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aa94377400382948908b80e25b38e8bf672ed78ebfb31c657baffa8ab2c9d1c4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "887598b185d7d0bf0d4282e84099ec251382e776de6181012c8e6082b2f20a66"
+    sha256 cellar: :any_skip_relocation, sonoma:        "afa21b687f9ac9f3cdea6e2daa7195454e0ff0e65997a55b7eb737f854ca1cde"
+    sha256 cellar: :any_skip_relocation, ventura:       "fbbd437a0c4a44c0972a1073c62999233efb27d7845c48502d85f33b6abefe48"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b9b481314df6fd3ea74a31f910d386ac6f569562b9996ea860e0d8d0f05a69b"
+  end
+
   resource "bootstrap-fastbuild" do
     on_macos do
       url "https://fastbuild.org/downloads/v1.13/FASTBuild-OSX-x64%2BARM-v1.13.zip"
