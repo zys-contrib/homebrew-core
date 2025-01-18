db2be550dc25b189e0ff3e8107c3501fdd1420ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-18 14:38:25 +0000

    spot: update 2.12.2 bottle.

--- Formula/s/spot.rb
+++ Formula/s/spot.rb
@@ -11,13 +11,12 @@ class Spot < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "13273a424546330dcb88457667df63bcc4441ba52748f1a2f998847e21579fe1"
-    sha256 cellar: :any,                 arm64_sonoma:  "3f3e6b30ada8fca2095c2f9e48543a7efb06a283c0c0be2fe58f16d9d77d058f"
-    sha256 cellar: :any,                 arm64_ventura: "8d0eeb9eb003a39959a556cc47c3c5680ec077a8cab4527fc2fb5126d96892eb"
-    sha256 cellar: :any,                 sonoma:        "14f6bc12870fb15b58cfee446186261c7acbfb785ff14fca2cd01377b71d1036"
-    sha256 cellar: :any,                 ventura:       "072a39a85251c944035b33d5463b7af0ffdc9b081014825a78a99c4d5f88c686"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e91723e2bf847484701af072c3bc4cd85aa22bfcde4b6b590a849e62c15ff7a4"
+    sha256 cellar: :any,                 arm64_sequoia: "82c09d875f4e37b130c36270f35f83fb4ec296a5f62620adcee6cf7ad9b71f4b"
+    sha256 cellar: :any,                 arm64_sonoma:  "7a6a26e2561b4420d81d9dc14e183ddeb3abc291cddae35f45dd124c17ad8c2a"
+    sha256 cellar: :any,                 arm64_ventura: "7f798e72df761b27eb9546475ceb39dfcb8c164f9e07dfad789cfc5ca804b71d"
+    sha256 cellar: :any,                 sonoma:        "48fc323e63d1e0d7efc50a44ece6301fa03983a4b6df4596e43be9d7f01b0b34"
+    sha256 cellar: :any,                 ventura:       "2957d881e871e1168b47668b2c5d066f0bf92ab762a028ce9048d0b6c9b6c1e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f43e975ba5ef966d887256c7674e2b7b5755e54dbf5311f2b506fb80dc00fe56"
   end
 
   depends_on "python@3.13" => :build
