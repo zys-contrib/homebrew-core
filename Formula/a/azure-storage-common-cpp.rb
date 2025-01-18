6bd59d6fc14c02663fdbdd214c8e982a40335107
Michael Cho <michael@michaelcho.dev>
2025-01-18 13:53:25 -0500

    azure-storage-common-cpp 12.9.0 (new formula)

---
+++ Formula/a/azure-storage-common-cpp.rb
@@ -0,0 +1,50 @@
+class AzureStorageCommonCpp < Formula
+  desc "Provides common Azure Storage-related abstractions for Azure SDK"
+  homepage "https://github.com/Azure/azure-sdk-for-cpp/tree/main/sdk/storage/azure-storage-common"
+  url "https://github.com/Azure/azure-sdk-for-cpp/archive/refs/tags/azure-storage-common_12.9.0.tar.gz"
+  sha256 "bb465f2e05bb4356dcf3bfdfd149a8727e222452f76a63d51c853513c0e3de1f"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^azure-storage-common[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => :build
+  depends_on "azure-core-cpp"
+  depends_on "openssl@3"
+
+  uses_from_macos "libxml2"
+
+  def install
+    ENV["AZURE_SDK_DISABLE_AUTO_VCPKG"] = "1"
+    system "cmake", "-S", "sdk/storage/azure-storage-common", "-B", "build", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # From https://github.com/Azure/azure-sdk-for-cpp/blob/main/sdk/storage/azure-storage-common/test/ut/crypt_functions_test.cpp
+    (testpath/"test.cpp").write <<~CPP
+      #include <cassert>
+      #include <string>
+      #include <vector>
+      #include <azure/storage/common/crypt.hpp>
+
+      static std::vector<uint8_t> ComputeHash(const std::string& data) {
+        const uint8_t* ptr = reinterpret_cast<const uint8_t*>(data.data());
+        Azure::Storage::Crc64Hash instance;
+        return instance.Final(ptr, data.length());
+      }
+
+      int main() {
+        assert(Azure::Core::Convert::Base64Encode(ComputeHash("Hello Azure!")) == "DtjZpL9/o8c=");
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "-std=c++14", "test.cpp", "-o", "test",
+                    "-L#{lib}", "-lazure-storage-common",
+                    "-L#{Formula["azure-core-cpp"].opt_lib}", "-lazure-core"
+    system "./test"
+  end
+end
