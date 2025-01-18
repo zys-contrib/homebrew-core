e610435c56e71ca389035566fadf0fc8e0bde70d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-18 22:50:26 +0000

    bfs 4.0.5

--- Formula/b/bfs.rb
+++ Formula/b/bfs.rb
@@ -1,8 +1,8 @@
 class Bfs < Formula
   desc "Breadth-first version of find"
   homepage "https://tavianator.com/projects/bfs.html"
-  url "https://github.com/tavianator/bfs/archive/refs/tags/4.0.4.tar.gz"
-  sha256 "209da9e9f43d8fe30fd689c189ea529e9d6b5358ce84a63a44721003aea3e1ca"
+  url "https://github.com/tavianator/bfs/archive/refs/tags/4.0.5.tar.gz"
+  sha256 "f7d9ebff00d9a010a5d6cc9b7bf1933095d7e5c0b11a8ec48c96c7ed8f993e5f"
   license "0BSD"
 
   bottle do
