6d3884f7eb038d33de85329fe9acf0b74a188a14
Daeho Ro <lamanus@outlook.kr>
2025-01-18 02:42:13 +0000

    libcotp: add 3.1.0 bottle.

--- Formula/lib/libcotp.rb
+++ Formula/lib/libcotp.rb
@@ -6,6 +6,15 @@ class Libcotp < Formula
   license "Apache-2.0"
   head "https://github.com/paolostivanin/libcotp.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "a55767669aadfbc33ca5047a9bf5a1371ebaf4ccb3c9e3e14a3296516f0702e9"
+    sha256 cellar: :any,                 arm64_sonoma:  "3c1f7d889c8212c2a7e8763c9bc712ce98ce0e8cd2520a27e6ed13468bdfa922"
+    sha256 cellar: :any,                 arm64_ventura: "626c19565338f9e6ebf991ffdb7c7acb0beea6fb5ce65317ea12a08cdaea63d8"
+    sha256 cellar: :any,                 sonoma:        "6a56e1465c12f7f4a88da73e4feb96255e03b2739a4a9e2d7fe176c9a9290960"
+    sha256 cellar: :any,                 ventura:       "540f3a6ecfea711699139ec94e16bf0e194094a0508e7e111777632c637388b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc3ff644b09e647890b68df9d726edcc2a8d18e293cc4773d921141147a6ffc4"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "libgcrypt"
