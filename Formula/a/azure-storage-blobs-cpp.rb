47d40b2423790cad8295d1519c26ec2859cfe203
Daeho Ro <lamanus@outlook.kr>
2025-01-19 07:17:44 +0000

    azure-storage-blobs-cpp: add 12.13.0 bottle.

--- Formula/a/azure-storage-blobs-cpp.rb
+++ Formula/a/azure-storage-blobs-cpp.rb
@@ -10,6 +10,15 @@ class AzureStorageBlobsCpp < Formula
     regex(/^azure-storage-blobs[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "7cb6b42324abc38c5297be494fef26300d16d68a88fdbe1df4645f1f1b0ebe01"
+    sha256 cellar: :any,                 arm64_sonoma:  "f2011c1a37ef61aff9cd77e9d218eee42754a1eb80a8972182c2e70b23131a2c"
+    sha256 cellar: :any,                 arm64_ventura: "8a2daa496708ac76de0b7834c1b1cc572e5a17a96d14b1347d76e1fd04be6ac2"
+    sha256 cellar: :any,                 sonoma:        "1b0611992e23c76d3ed8a663934ea94b5234d71fa68936ad7e9491912492a7dd"
+    sha256 cellar: :any,                 ventura:       "83d7b90ab7086f61b121ba374eb9bd1ac05f4c3f33964d1ab8823889fd5ce70a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4451a392473b4d4aafb8ac8f631d9eededb7851751dcb1064b5681d426d9a9f5"
+  end
+
   depends_on "cmake" => :build
   depends_on "azure-core-cpp"
   depends_on "azure-storage-common-cpp"
