86896cfcdb81db092eaf940a40f986da9b061cdc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-19 03:07:55 +0000

    oxlint: update 0.15.7 bottle.

--- Formula/o/oxlint.rb
+++ Formula/o/oxlint.rb
@@ -12,12 +12,12 @@ class Oxlint < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9a85babd915f1def10d8cc5b43141fc7e8d8ae26a57e5911e799246ab5f5a2a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3cedac7d183ca2d0dbee75d969fb630eec763765d70dafc5ba824d4ba1bebd18"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b5d6bab0b5efdbe4914ddc4a2561d7e7c4227fd5e58c45996336a007166c552b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bdef45867056ca79fa38666e66fbda3e1f59aeb8c00f064c469d3a53053ce970"
-    sha256 cellar: :any_skip_relocation, ventura:       "eb4bcfe03ba676598495a7ce3e04b2f91c65423dabcdee8f3705250f0b20bb0d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7345f1efde0be44a38867657bd1ca31392968f8ec8926e8a30a3cfe623a2cba1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1359b5a592d5c619f569a806c58203ae7bf3c2be81b2cb5815f6a841547a2323"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "473fbe04d5bda9d6854c85c699409242ccaead52dd4166ec5bc9decc8cd26bf2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1ea7785c44cb5257e367e5bcabaee1d7f228087d7db624a8a2b14b1429d2ec1b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4339a6d70dc2ed09dbbce4a5fc63a23aa429211ac1176619a499fc06a21833fe"
+    sha256 cellar: :any_skip_relocation, ventura:       "09dcf6df2ea667ad189e0ae6b0c09aae4b90171893f09547d21869b8e9eb2e9c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "efb5e306c8eeda8bffe5715feb9ea28c033f654707793f9f8aba6351e8ac5fd8"
   end
 
   depends_on "rust" => :build
