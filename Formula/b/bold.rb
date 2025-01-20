26411cf01060af934faa4768352c079c1ed2b4a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-20 07:11:10 +0000

    bold: add 0.1.0 bottle.

--- Formula/b/bold.rb
+++ Formula/b/bold.rb
@@ -5,6 +5,14 @@ class Bold < Formula
   sha256 "2496f04e47c9d5e17ef273b26519abf429b5c3e3df6d264f2941735088253ec0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a1deba05b1a4ab3d20a443e7b5a11f63b6cfc44f2996c41dce352fbdc48e78db"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75029020d0fa32ee7573f9c730b9bbb09a6c98fb310dee951b284df022dfd3b6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5e4087ae6dc9a698588838f127ed739eaa55d899b143efd5079b40a613db6d1b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2539d42202287085cf3e89c7e06fce587166465fbb8df8ba4fa23cf8bde298d1"
+    sha256 cellar: :any_skip_relocation, ventura:       "84a0a90973ee9a14a971d3c700f64bd164167518bcb978830080d5361c13ba5c"
+  end
+
   depends_on "zig" => :build
   depends_on :macos # does not build on linux
 
