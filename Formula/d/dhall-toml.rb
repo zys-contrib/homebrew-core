a8d5edd61bea6c43aa2302dcaaacd78c8ed9e7af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-20 04:49:33 +0000

    dhall-toml: update 1.0.4 bottle.

--- Formula/d/dhall-toml.rb
+++ Formula/d/dhall-toml.rb
@@ -7,14 +7,12 @@ class DhallToml < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "780e0f69804e6ec42753ddab457ba65206b731e68ee6e1b655f3dba777507978"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "77850e914f5774c848519d088375e566ac96946ded3ca7428da4fac4394464ec"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7d7f38cc68cd120e3869d3a535cd7c1c4b81d094f1754f395cb7864b66fdac94"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a8a362df2578a98f41e80638d7adefa2863c96ba616ba47d4d97c4e8b71c9d54"
-    sha256 cellar: :any_skip_relocation, sonoma:         "269389acf7ed1db191734e00d35889ce824d8ca709c7753f4282c3b4019f5e4b"
-    sha256 cellar: :any_skip_relocation, ventura:        "4053831141433159830a642a31991506255863cd34f88cf59ee2ffa2b49954b1"
-    sha256 cellar: :any_skip_relocation, monterey:       "251b76a91597eb42b33b19c96650e0959fcf5ea5b444071d14dcaf8496fe30d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39d2f70692a5706db31dd4d82561f774cd476053fe678b6656309cd7ad074099"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa04be536e7446641318c9d7ee241d0280b24061fda69b889a2988f48898e8b9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0b91f73c50c4c44ac7c5edef748f1c4f880a3ff0690ef64463f5dc04dfff85d5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b221d5914aad68c046ae8f9f0fee29cd0e74ad55f4db5fd027f334daf71282e2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "230e845fc7963e4f421247c6f7733af8a588782ad39232a446f6e1a65fccbb80"
+    sha256 cellar: :any_skip_relocation, ventura:       "a118e324119fb58470cf49f4f898f0c3a603fc3975a11b2b3f60d66eeefdabe0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e7a0839d98769280508ea3d559828b88c1a78838f21bbceb64e179f6a7078f07"
   end
 
   depends_on "cabal-install" => :build
