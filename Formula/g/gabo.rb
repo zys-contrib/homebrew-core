21397c54b8b42a33e9eea0e7025597e9ebbf88fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-20 08:34:38 +0000

    gabo: update 1.4.0 bottle.

--- Formula/g/gabo.rb
+++ Formula/g/gabo.rb
@@ -6,12 +6,12 @@ class Gabo < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "927e72f8dfa3a82635e53f2f4de8c83a7d7145250a735faee21e1c884e61dee0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "927e72f8dfa3a82635e53f2f4de8c83a7d7145250a735faee21e1c884e61dee0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "927e72f8dfa3a82635e53f2f4de8c83a7d7145250a735faee21e1c884e61dee0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8d88f0baa044e096d7da59e1772ad906fbfd9e6496a7e44375dc52a4573396e8"
-    sha256 cellar: :any_skip_relocation, ventura:       "8d88f0baa044e096d7da59e1772ad906fbfd9e6496a7e44375dc52a4573396e8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c99ae0e97b2b7bbef5ad821f09962361ea0176b7c6b467e178fe965bb3cc8286"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f55431c042ddb4204746d8b42fbf6c3f2b3cf4e8370f95ad5a36a205e57cbbd6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f55431c042ddb4204746d8b42fbf6c3f2b3cf4e8370f95ad5a36a205e57cbbd6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f55431c042ddb4204746d8b42fbf6c3f2b3cf4e8370f95ad5a36a205e57cbbd6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b95d6c2a8e2326651e71168129b741d95d3842291e4d02233ef9d68ebc1e73f6"
+    sha256 cellar: :any_skip_relocation, ventura:       "b95d6c2a8e2326651e71168129b741d95d3842291e4d02233ef9d68ebc1e73f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7bc811507c1f8b75f900f67e06c4577ba69ddd776b2a15f79c26965baabada5b"
   end
 
   depends_on "go" => :build
