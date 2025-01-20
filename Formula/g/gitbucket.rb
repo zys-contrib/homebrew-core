189898377e6f6add6de08c62a6f5d16c8b91b810
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-20 16:57:58 +0000

    gitbucket 4.42.1

--- Formula/g/gitbucket.rb
+++ Formula/g/gitbucket.rb
@@ -1,8 +1,8 @@
 class Gitbucket < Formula
   desc "Git platform powered by Scala offering"
   homepage "https://github.com/gitbucket/gitbucket"
-  url "https://github.com/gitbucket/gitbucket/releases/download/4.42.0/gitbucket.war"
-  sha256 "f521aba7f220a45c48704d9a653d567f3bf0c4b733e6be8d4eb92c2222cad57b"
+  url "https://github.com/gitbucket/gitbucket/releases/download/4.42.1/gitbucket.war"
+  sha256 "0091ab52edac75279d2cf35b207aadfc38a87e10218f57930ecae54886c403d3"
   license "Apache-2.0"
 
   bottle do
