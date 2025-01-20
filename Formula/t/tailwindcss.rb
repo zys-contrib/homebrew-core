6c16811f4ebe7e40339f341c23d73c79262d0269
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-20 06:49:57 +0000

    tailwindcss: update 3.4.17 bottle.

--- Formula/t/tailwindcss.rb
+++ Formula/t/tailwindcss.rb
@@ -6,12 +6,13 @@ class Tailwindcss < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ef1e89aab19737e3c66462f57f0f61267e4ed4fdd9967df8f41b37ef66db56a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1ef1e89aab19737e3c66462f57f0f61267e4ed4fdd9967df8f41b37ef66db56a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8a0a4bcb9fc57504bc217357eb758034d900b5b2040198f1d65153ccbeade325"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9c15b605c267dd61402165f81113bcf07296d6941ef8b574c9b039154fff9651"
-    sha256 cellar: :any_skip_relocation, ventura:       "879e7436c37cb8f65c418009e85216707354285730be1078795405d8ce9dac56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "646671381a5cbb0a2231f3a40021fbc2790311303f5c131d0d844ef892104ca2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6642f35504e8ab8ce029025e265972bb9a74d9030bd879973388ba245f5707ce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6642f35504e8ab8ce029025e265972bb9a74d9030bd879973388ba245f5707ce"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6642f35504e8ab8ce029025e265972bb9a74d9030bd879973388ba245f5707ce"
+    sha256 cellar: :any_skip_relocation, sonoma:        "470d9bbfd904d02d77d565faa66afddcb033b7030ff41baf28ad47294c65c4b5"
+    sha256 cellar: :any_skip_relocation, ventura:       "470d9bbfd904d02d77d565faa66afddcb033b7030ff41baf28ad47294c65c4b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6642f35504e8ab8ce029025e265972bb9a74d9030bd879973388ba245f5707ce"
   end
 
   depends_on "node"
