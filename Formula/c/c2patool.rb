6193194baf9dd80c9874ab6e9e1647b3c532f2d1
botantony <antonsm21@gmail.com>
2025-01-21 03:06:10 +0100

    c2patool 0.11.1 (new formula)
    CLI tool for working with C2PA
    
    c2patool: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/c2patool.rb
@@ -0,0 +1,39 @@
+class C2patool < Formula
+  desc "CLI for working with C2PA manifests and media assets"
+  homepage "https://contentauthenticity.org"
+  url "https://github.com/contentauth/c2pa-rs/archive/refs/tags/c2patool-v0.11.1.tar.gz"
+  sha256 "f4ba182f88242ac69bf73ff33d2fd11ba460ae49b28b333aaffd573ca69bb2bf"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/contentauth/c2pa-rs.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+
+  depends_on "openssl@3"
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "cli")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/c2patool -V").strip
+
+    (testpath/"test.json").write <<~JSON
+      {
+        "assertions": [
+          {
+            "label": "com.example.test",
+            "data": {
+              "my_key": "my_value"
+            }
+          }
+        ]
+      }
+    JSON
+
+    system bin/"c2patool", test_fixtures("test.png"), "-m", "test.json", "-o", "signed.png", "--force"
+
+    output = shell_output("#{bin}/c2patool signed.png")
+    assert_match "\"issuer\": \"C2PA Test Signing Cert\"", output
+  end
+end
