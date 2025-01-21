e7893655601697366392a52dce99588c18b28d11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-21 23:15:48 +0000

    wasmtime 29.0.1

--- Formula/w/wasmtime.rb
+++ Formula/w/wasmtime.rb
@@ -2,8 +2,8 @@ class Wasmtime < Formula
   desc "Standalone JIT-style runtime for WebAssembly, using Cranelift"
   homepage "https://wasmtime.dev/"
   url "https://github.com/bytecodealliance/wasmtime.git",
-      tag:      "v29.0.0",
-      revision: "545407736534daa9cbe3bdb8829ef5744429d5c8"
+      tag:      "v29.0.1",
+      revision: "58282df898d79a787a726d829b166272dde155b9"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/bytecodealliance/wasmtime.git", branch: "main"
 
