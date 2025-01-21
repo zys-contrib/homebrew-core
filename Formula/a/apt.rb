f9ea8f40faed62c1c48996a896d9efe8ed8f3c44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-21 16:50:02 +0000

    apt 2.9.24

--- Formula/a/apt.rb
+++ Formula/a/apt.rb
@@ -1,8 +1,8 @@
 class Apt < Formula
   desc "Advanced Package Tool"
   homepage "https://wiki.debian.org/Apt"
-  url "https://deb.debian.org/debian/pool/main/a/apt/apt_2.9.23.tar.xz"
-  sha256 "b77fc5ac92a88645b2a3bda43c2c9b69d29ccbc5564f8fa25bc8f46ae09aa0dd"
+  url "https://deb.debian.org/debian/pool/main/a/apt/apt_2.9.24.tar.xz"
+  sha256 "d9aa7a1877b41ac5eb8412abb63c6e6ff4f6e0b97a8fa6ec29ba086b2bd288b7"
   license "GPL-2.0-or-later"
 
   livecheck do
