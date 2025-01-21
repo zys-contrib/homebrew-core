3398fed349199b41efc51ae038e954d69f9b1c72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-21 22:55:52 +0000

    dynaconf 3.2.7

--- Formula/d/dynaconf.rb
+++ Formula/d/dynaconf.rb
@@ -3,8 +3,8 @@ class Dynaconf < Formula
 
   desc "Configuration Management for Python"
   homepage "https://www.dynaconf.com/"
-  url "https://files.pythonhosted.org/packages/56/1a/324f1bf234cc4f98445305fd8719245318466e310e05caea7ef052748ecd/dynaconf-3.2.6.tar.gz"
-  sha256 "74cc1897396380bb957730eb341cc0976ee9c38bbcb53d3307c50caed0aedfb8"
+  url "https://files.pythonhosted.org/packages/35/3c/cbb09611ee5e182083484a18418e43176cf91b25144215703fc54dbb19d4/dynaconf-3.2.7.tar.gz"
+  sha256 "1246ef16b0577aa25876f1606e8eb2ea2d9acc61a39087e01c90c5a9da787b46"
   license "MIT"
 
   bottle do
