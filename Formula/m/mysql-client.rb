6d92d3f6ceb6938287299db7acb63f858d866754
Bevan Kay <email@bevankay.me>
2025-01-21 21:24:53 +1100

    mysql-client 9.2.0
    mysql-client: add dependency

--- Formula/m/mysql-client.rb
+++ Formula/m/mysql-client.rb
@@ -1,8 +1,8 @@
 class MysqlClient < Formula
   desc "Open source relational database management system"
-  homepage "https://dev.mysql.com/doc/refman/9.1/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-9.1/mysql-9.1.0.tar.gz"
-  sha256 "52c3675239bfd9d3c83224ff2002aa6e286fab97bf5b2b5ca1a85c9c347766fc"
+  homepage "https://dev.mysql.com/doc/refman/9.2/en/"
+  url "https://cdn.mysql.com/Downloads/MySQL-9.2/mysql-9.2.0.tar.gz"
+  sha256 "a39d11fdf6cf8d1b03b708d537a9132de4b99a9eb4d610293937f0687cd37a12"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
 
   livecheck do
@@ -38,6 +38,11 @@ class MysqlClient < Formula
   on_ventura :or_older do
     depends_on "llvm@18"
     fails_with :clang
+  end
+
+  on_linux do
+    depends_on "libtirpc" => :build
+    depends_on "krb5"
   end
 
   fails_with :gcc do
