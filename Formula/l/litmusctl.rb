8c5ae18cedc556c4e0706d002b175541366bcf52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-21 11:03:06 +0000

    litmusctl 1.14.0

--- Formula/l/litmusctl.rb
+++ Formula/l/litmusctl.rb
@@ -1,8 +1,8 @@
 class Litmusctl < Formula
   desc "Command-line interface for interacting with LitmusChaos"
   homepage "https://litmuschaos.io"
-  url "https://github.com/litmuschaos/litmusctl/archive/refs/tags/1.13.0.tar.gz"
-  sha256 "b23e39085a0c9ea20c1fc1aaed1bcbe83c7971780411f098f2fa08061c060e41"
+  url "https://github.com/litmuschaos/litmusctl/archive/refs/tags/1.14.0.tar.gz"
+  sha256 "a1cf50378e83cd256e96157fe86dbe3cd6ba8a5084606b90b765d17c74545ca3"
   license "Apache-2.0"
   head "https://github.com/litmuschaos/litmusctl.git", branch: "master"
 
