b4465abf476e183f4a0eab9891b46b69aef2194a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-21 05:03:17 +0000

    otree 0.3.1

--- Formula/o/otree.rb
+++ Formula/o/otree.rb
@@ -1,8 +1,8 @@
 class Otree < Formula
   desc "Command-line tool to view objects (JSON/YAML/TOML) in TUI tree widget"
   homepage "https://github.com/fioncat/otree"
-  url "https://github.com/fioncat/otree/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "63a63b09af2186d2bc3aa8631989f1fbdeb7659d7f1857be7fae1e0af4d42fde"
+  url "https://github.com/fioncat/otree/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "f71064cac1c7fc1047a119bdd2b630eac67008546d13b51b1ace1ab9b0314e02"
   license "MIT"
   head "https://github.com/fioncat/otree.git", branch: "main"
 
