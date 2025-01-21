385db0d5d5c757f43bc28e829a7803effa99e9e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-21 19:52:40 +0000

    clusterctl 1.9.4

--- Formula/c/clusterctl.rb
+++ Formula/c/clusterctl.rb
@@ -1,8 +1,8 @@
 class Clusterctl < Formula
   desc "Home for the Cluster Management API work, a subproject of sig-cluster-lifecycle"
   homepage "https://cluster-api.sigs.k8s.io"
-  url "https://github.com/kubernetes-sigs/cluster-api/archive/refs/tags/v1.9.3.tar.gz"
-  sha256 "44f413ed7a65509575d757922e84479ed76c757b8fa798d71d90699c87671c60"
+  url "https://github.com/kubernetes-sigs/cluster-api/archive/refs/tags/v1.9.4.tar.gz"
+  sha256 "d604c1c767e6520aa5b1ebd035f09a1abb7a130a163a04ba4025242e17e436f5"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cluster-api.git", branch: "main"
 
