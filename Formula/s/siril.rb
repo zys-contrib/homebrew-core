167f5697f9cd17e1116ab99a882864bf3acf3f66
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-22 11:18:29 +0000

    siril 1.2.6

--- Formula/s/siril.rb
+++ Formula/s/siril.rb
@@ -1,10 +1,9 @@
 class Siril < Formula
   desc "Astronomical image processing tool"
   homepage "https://www.siril.org"
-  url "https://free-astro.org/download/siril-1.2.5.tar.bz2"
-  sha256 "698be7f689cffa0d657261f67990e7de7d02d527f999cedfa48af523dcd74270"
+  url "https://free-astro.org/download/siril-1.2.6.tar.bz2"
+  sha256 "312f82e78599f796d163a6d1c90589df1ed920b9ff2bb7ab5b808e43872817fa"
   license "GPL-3.0-or-later"
-  revision 1
   head "https://gitlab.com/free-astro/siril.git", branch: "master"
 
   livecheck do
