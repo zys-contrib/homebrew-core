848e2eb1e5752c8a02824af793266a08aff1b09a
Nanda H Krishna <me@nandahkrishna.com>
2025-01-22 09:59:07 +0000

    diary: add 0.15 bottle.

--- Formula/d/diary.rb
+++ Formula/d/diary.rb
@@ -10,6 +10,15 @@ class Diary < Formula
     regex(/href=.*?diary[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a366f0be6bb1d23a3c0365b4c9e3c1308a9f25fe98949faa0344bfbde84e4393"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "596cf38b2aa8ef25fef5533c74673718d05150b9605f8fdd703a064eb40604a3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "07272e5e6d3df2aefe2401bd9adf0c2bac89011c7dcf7829a8e63eaf4f1482c5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e2be54322972d89253aa4036eb889863f9b76b02d4182a73ebd18e7f27f905a7"
+    sha256 cellar: :any_skip_relocation, ventura:       "db5ed2d29ada410541262e0104a6cbbcef5a7a76e027e61463af6f28efe8f2a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45417001ea62ec5dd28ef4620cc0f78da5d100b02e922c5948e961cb24328671"
+  end
+
   depends_on "pkgconf" => :build
 
   uses_from_macos "curl"
