31a7b265af5302a8359b841c5af5d7bc8a8b812c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-22 07:12:30 +0000

    teku: update 25.1.0 bottle.

--- Formula/t/teku.rb
+++ Formula/t/teku.rb
@@ -8,12 +8,12 @@ class Teku < Formula
   head "https://github.com/ConsenSys/teku.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "467d5d5e298111902d69c503521058e30ba6fa1ff37e9da32158a0b2107c4e94"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "467d5d5e298111902d69c503521058e30ba6fa1ff37e9da32158a0b2107c4e94"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "467d5d5e298111902d69c503521058e30ba6fa1ff37e9da32158a0b2107c4e94"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c9adc8d3d88295278463190607280f60260b1d1848beae1f3f0fc0631ea56754"
-    sha256 cellar: :any_skip_relocation, ventura:       "c9adc8d3d88295278463190607280f60260b1d1848beae1f3f0fc0631ea56754"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "467d5d5e298111902d69c503521058e30ba6fa1ff37e9da32158a0b2107c4e94"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c586891b13d7ae78aecdedb0b16f91aa546f4ee6944429d6a36ea429f7384503"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c586891b13d7ae78aecdedb0b16f91aa546f4ee6944429d6a36ea429f7384503"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c586891b13d7ae78aecdedb0b16f91aa546f4ee6944429d6a36ea429f7384503"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f4af267b49ca77b05f6202d5433d6c3e69d64ea027eb1da39d7362713a9ddb35"
+    sha256 cellar: :any_skip_relocation, ventura:       "f4af267b49ca77b05f6202d5433d6c3e69d64ea027eb1da39d7362713a9ddb35"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c586891b13d7ae78aecdedb0b16f91aa546f4ee6944429d6a36ea429f7384503"
   end
 
   depends_on "gradle" => :build
