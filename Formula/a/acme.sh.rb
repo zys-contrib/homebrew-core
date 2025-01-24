d1994f8414837fd0b9ec06a85502b4c942d75b98
Rui Chen <rui@chenrui.dev>
2025-01-24 19:25:24 +0000

    acme.sh: add 3.1.0 bottle.

--- Formula/a/acme.sh.rb
+++ Formula/a/acme.sh.rb
@@ -5,6 +5,15 @@ class AcmeSh < Formula
   sha256 "5bc8a72095e16a1a177d1a516728bbd3436abf8060232d5d36b462fce74447aa"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ed468ee7ca382405545a7c022fcf28f45e8cdbc0feb87e0e4c5e64018600f74"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ed468ee7ca382405545a7c022fcf28f45e8cdbc0feb87e0e4c5e64018600f74"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3ed468ee7ca382405545a7c022fcf28f45e8cdbc0feb87e0e4c5e64018600f74"
+    sha256 cellar: :any_skip_relocation, sonoma:        "45d864aacf2a2e4d2eb4ec6b2eea0f8d1f65fc2f533d549d780f7e03f54afc4c"
+    sha256 cellar: :any_skip_relocation, ventura:       "45d864aacf2a2e4d2eb4ec6b2eea0f8d1f65fc2f533d549d780f7e03f54afc4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ed468ee7ca382405545a7c022fcf28f45e8cdbc0feb87e0e4c5e64018600f74"
+  end
+
   def install
     libexec.install [
       "acme.sh",
