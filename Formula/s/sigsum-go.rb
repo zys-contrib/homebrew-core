dc8fe17f7b1e01edd3db4e0044bf83ee8c22cdbe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-24 08:39:35 +0000

    sigsum-go: update 0.10.1 bottle.

--- Formula/s/sigsum-go.rb
+++ Formula/s/sigsum-go.rb
@@ -6,12 +6,12 @@ class SigsumGo < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f7014991ad0f6a7f496dac69aea18c4490d4a4fd88f4dd36bb5956ceaf577885"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f7014991ad0f6a7f496dac69aea18c4490d4a4fd88f4dd36bb5956ceaf577885"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f7014991ad0f6a7f496dac69aea18c4490d4a4fd88f4dd36bb5956ceaf577885"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e5fbfa9b8578f857989d80a8ccc9e16f41b25989f575aa2293d565efe9734563"
-    sha256 cellar: :any_skip_relocation, ventura:       "e5fbfa9b8578f857989d80a8ccc9e16f41b25989f575aa2293d565efe9734563"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05b65af19ba9f4c2974aa45f38e698c75ed2dc93811f80491eabd39ea7c02d15"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96537b5947091ef902a6064914e41baab59b1437ec9a568fe50793dfcd329a35"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "96537b5947091ef902a6064914e41baab59b1437ec9a568fe50793dfcd329a35"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "96537b5947091ef902a6064914e41baab59b1437ec9a568fe50793dfcd329a35"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9fc8d859e0b1bc751cbb93293736dbe3f8faa03559738224a676c2269f27cadf"
+    sha256 cellar: :any_skip_relocation, ventura:       "9fc8d859e0b1bc751cbb93293736dbe3f8faa03559738224a676c2269f27cadf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "363dbc9e425648b90b1c35e17ec477a82542627e4b7341e15ebff37fc50b863b"
   end
 
   depends_on "go" => :build
