d23c2c63e913439b484424c95b3fae21d501765d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-24 20:13:06 +0000

    snyk-cli 1.1295.2

--- Formula/s/snyk-cli.rb
+++ Formula/s/snyk-cli.rb
@@ -1,8 +1,8 @@
 class SnykCli < Formula
   desc "Scans and monitors projects for security vulnerabilities"
   homepage "https://snyk.io"
-  url "https://registry.npmjs.org/snyk/-/snyk-1.1295.1.tgz"
-  sha256 "93c59814e837a56bde96c2f66f2765891180a2947606fbd783ff32fd5aa46366"
+  url "https://registry.npmjs.org/snyk/-/snyk-1.1295.2.tgz"
+  sha256 "97d1af89b953958d842aa157b4796ccd566e6665de6fb5921c8e13a300e2e7bb"
   license "Apache-2.0"
 
   bottle do
