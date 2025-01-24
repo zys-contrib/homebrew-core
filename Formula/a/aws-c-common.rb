36e6ee4a7b7ec9c36a151dbdf80189a51b03180c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-24 19:50:01 +0000

    aws-c-common 0.10.8

--- Formula/a/aws-c-common.rb
+++ Formula/a/aws-c-common.rb
@@ -1,8 +1,8 @@
 class AwsCCommon < Formula
   desc "Core c99 package for AWS SDK for C"
   homepage "https://github.com/awslabs/aws-c-common"
-  url "https://github.com/awslabs/aws-c-common/archive/refs/tags/v0.10.7.tar.gz"
-  sha256 "29440e1db8fe9c7a217a2948d7755ceb91b6b2617f73c99592a55aa32a21fb19"
+  url "https://github.com/awslabs/aws-c-common/archive/refs/tags/v0.10.8.tar.gz"
+  sha256 "fb7ce3cf22aac2c70a7676cd8ceeea785bb6ee2e4fea7d6cfb225a12fdc62775"
   license "Apache-2.0"
 
   bottle do
