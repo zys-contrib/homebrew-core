3d3e09164dddb1e0c47417ca685b47e19d2d4ec4
Rui Chen <rui@chenrui.dev>
2025-01-24 22:16:00 -0500

    gnu-which 2.22
    autobump: add gnu-which
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gnu-which.rb
+++ Formula/g/gnu-which.rb
@@ -2,9 +2,9 @@ class GnuWhich < Formula
   desc "GNU implementation of which utility"
   # Previous homepage is dead. Have linked to the GNU Projects page for now.
   homepage "https://savannah.gnu.org/projects/which/"
-  url "https://ftp.gnu.org/gnu/which/which-2.21.tar.gz"
-  mirror "https://ftpmirror.gnu.org/which/which-2.21.tar.gz"
-  sha256 "f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad"
+  url "https://ftp.gnu.org/gnu/which/which-2.22.tar.gz"
+  mirror "https://ftpmirror.gnu.org/which/which-2.22.tar.gz"
+  sha256 "9f85cde3f2c257021b8508f756704bee6002fa8f5680297d30f186cc1f342af5"
   license "GPL-3.0-or-later"
 
   bottle do
