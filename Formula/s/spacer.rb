38c5f2fd9395d66c44e88a4ed9981e7764641bc4
Rui Chen <rui@chenrui.dev>
2025-01-24 22:14:45 -0500

    spacer 0.3.1
    autobump: add spacer
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/spacer.rb
+++ Formula/s/spacer.rb
@@ -1,8 +1,8 @@
 class Spacer < Formula
   desc "Small command-line utility for adding spacers to command output"
   homepage "https://github.com/samwho/spacer"
-  url "https://github.com/samwho/spacer/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "b8cebdebd3845843f15f0040f36b7e06398de45c721679672e7107f8b735ab54"
+  url "https://github.com/samwho/spacer/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "65c14dde2e90827c8b91353824cece614e1e645657ee3200eafa13165f27a3da"
   license "MIT"
 
   bottle do
