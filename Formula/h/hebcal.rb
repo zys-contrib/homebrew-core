c342dbd92bad390576de6379a7953b3850550202
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-27 02:20:00 +0000

    hebcal 5.9.0

--- Formula/h/hebcal.rb
+++ Formula/h/hebcal.rb
@@ -1,8 +1,8 @@
 class Hebcal < Formula
   desc "Perpetual Jewish calendar for the command-line"
   homepage "https://github.com/hebcal/hebcal"
-  url "https://github.com/hebcal/hebcal/archive/refs/tags/v5.8.7.tar.gz"
-  sha256 "5b8536b3738fb9a8dba9da9c27d9375761a98978dab9759eda0bf6b38b701121"
+  url "https://github.com/hebcal/hebcal/archive/refs/tags/v5.9.0.tar.gz"
+  sha256 "236617ad37c7621e61eb4aa10e407cf95950563bf5e0c663e4720e5a9fb5e3dd"
   license "GPL-2.0-or-later"
   head "https://github.com/hebcal/hebcal.git", branch: "main"
 
