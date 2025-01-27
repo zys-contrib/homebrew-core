ba1a684eebd0af49b32e02b82a057ce03df9701c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-27 15:04:28 +0000

    qsv: update 2.2.1 bottle.

--- Formula/q/qsv.rb
+++ Formula/q/qsv.rb
@@ -15,12 +15,12 @@ class Qsv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "874ff0a240e86352ac849710ec20cf524df1e7a72743603620d38e4331c583fb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4c5854edcc5107cd8de2a55aba850a98f085463799d932b20d13a9ba2764ab55"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6eb8b9461f24af3f1f71ee4358be99f29a7ecf22abdf7449936ebec993edda74"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fc147f08473543adfe5226fc044ef831ccfe60219af750397628974170554f57"
-    sha256 cellar: :any_skip_relocation, ventura:       "f2f614472c6464b5f6e7e2a2610368ab91b4939661a5b3ad261c5afa1e54a722"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "540626ab4353fbf87ebb19171096db003b25a1749b0cf3a6e480ef076bb3c23b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c2a58f287033c1a73c0ae90a72d564109a2e59d848a17fc48348e12eae36f847"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2cad0df4512177bd581822b3bbf7438517fb085d8471795ae2138d8873737662"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ee5501186db0ca2a558a406c653821b86e9bcc93911f68f2032aefbb29225004"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aeaac5da5b9a491e5773a075dcff134c429a67579936b5d5d35d64323ed219b6"
+    sha256 cellar: :any_skip_relocation, ventura:       "ef852130a655f3598489fc9f81224d626c971483d73254019ff25395579bb7e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae46300ed7267bfb3fa6621212e93806ea8a0f5f484e1d273be5caaae0abe1f2"
   end
 
   depends_on "rust" => :build
