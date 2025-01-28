8cb9099d93fc7349c9f9ca46457a90096125bb3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-28 18:17:43 +0000

    gallery-dl: update 1.28.5 bottle.

--- Formula/g/gallery-dl.rb
+++ Formula/g/gallery-dl.rb
@@ -9,7 +9,7 @@ class GalleryDl < Formula
   head "https://github.com/mikf/gallery-dl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "66df5086ab86628ded83bdbf84ada8eea87511719da957f6b2f703f1ddca6e13"
+    sha256 cellar: :any_skip_relocation, all: "dd9e051a421727a1be4365d5e0e4bd05fe815143b686a86be79a21926d1352d0"
   end
 
   depends_on "certifi"
