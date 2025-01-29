15b2b8ab660e8e80b247cb1938b727bea817e69b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-29 14:33:35 +0000

    phpunit: update 11.5.5 bottle.

--- Formula/p/phpunit.rb
+++ Formula/p/phpunit.rb
@@ -12,7 +12,7 @@ class Phpunit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e385728896450764c8da5a12fe318bb8694f2996574d9acab28afb4d6a31bd78"
+    sha256 cellar: :any_skip_relocation, all: "cf91e288242c6952cd871dde378b9b7b195f91fde7222b70a1e65435742eb159"
   end
 
   depends_on "php" => :test
