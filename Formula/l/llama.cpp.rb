8a6fcf746fe8a3fa65bc6de5fc5bfa3b263aee91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-29 23:47:16 +0000

    llama.cpp: update 4589 bottle.

--- Formula/l/llama.cpp.rb
+++ Formula/l/llama.cpp.rb
@@ -14,12 +14,12 @@ class LlamaCpp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "2c97a22410d4330caad105278bb8362f7a1e4fc66854adcacfe178ff630249d3"
-    sha256 cellar: :any,                 arm64_sonoma:  "975aa2f242de7f0ed911758781e07579a3045cca3275e0e8bbbc878e8a5cb502"
-    sha256 cellar: :any,                 arm64_ventura: "64343d0c15a8c1db5e45fd6dde81c71e3c081c47097ab3fba7eef26f8c34e763"
-    sha256 cellar: :any,                 sonoma:        "0d909b77e7c8a13606ffc392c6a9de988970b4f243941fa8eb6781ea4ec81161"
-    sha256 cellar: :any,                 ventura:       "69d0866acd875be37742e1160ce822f68a8d15439a1e5dc65fd375807a866747"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49f825062007bc8a721769fd8a39116ab130ba7b7e2c3e3f4f8ac78d180e9453"
+    sha256 cellar: :any,                 arm64_sequoia: "c6088efb4a8dd133941c2872897f6f19a085d3cb0bbc9d44116a703389ae70f6"
+    sha256 cellar: :any,                 arm64_sonoma:  "99f33166d579b96710612bcb7a577af4a2553e06f8c5168ff3805bc5904c895b"
+    sha256 cellar: :any,                 arm64_ventura: "32eb9963ac1348b544f2f014fca6e187989a974300e995d7eb7fe182ebc0ccaf"
+    sha256 cellar: :any,                 sonoma:        "883ca55be872f0ea816984062a8c4198bb6cbc386dffea126f1d614b14f459f2"
+    sha256 cellar: :any,                 ventura:       "76c384a98f324b97be509be1e949f8cc333a95c04ccaf8752cdd16c5faca48dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f65040d6ca798d28d4f26e1a17314d2008443e26297d844f44d3e03b770d1b4d"
   end
 
   depends_on "cmake" => :build
