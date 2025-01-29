f15a5ee9a6380060e6cbc53a4ae1a99cb4ba7020
Rui Chen <rui@chenrui.dev>
2025-01-28 23:45:52 -0500

    selenium-server 4.28.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/selenium-server.rb
+++ Formula/s/selenium-server.rb
@@ -1,8 +1,8 @@
 class SeleniumServer < Formula
   desc "Browser automation for testing purposes"
   homepage "https://www.selenium.dev/"
-  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.28.0/selenium-server-4.28.0.jar"
-  sha256 "258cb4014e2850ede1d7d97e8daed2385df8a84289456d7c1617c8071dd24ad9"
+  url "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.28.0/selenium-server-4.28.1.jar"
+  sha256 "72975a0c09135efb1f14262a05ab570dfd9c9c345840a37702c580941350ed0d"
   license "Apache-2.0"
 
   livecheck do
