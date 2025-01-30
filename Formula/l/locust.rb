4d25f8c2e63612b8fdbc8ea4847daebadffe3239
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 21:16:22 +0000

    locust: update 2.32.8 bottle.

--- Formula/l/locust.rb
+++ Formula/l/locust.rb
@@ -8,12 +8,12 @@ class Locust < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "9db6584a3ce1737a3094ab8165811ed2c4171421f84e86ab4b3e29eff3f54ade"
-    sha256 cellar: :any,                 arm64_sonoma:  "c5e2c2e12ef67e2a0bd1a99bb64003d34bb9a0d0bf1deca68af593fc31bdffee"
-    sha256 cellar: :any,                 arm64_ventura: "7b7f934ab6060648de4438658579a8427dd2c5ff0daed1f4142e9d8a29ccbaa6"
-    sha256 cellar: :any,                 sonoma:        "b5ad0b1b189b41f6b5c8706b365fe0102bb5ec9aa018e9f446787daf3ccb1592"
-    sha256 cellar: :any,                 ventura:       "84a5f6ae6aee565a3185f59c2dea83c399032b381ad0ba9ee8eb98d05b5e539c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e459a288852cfbdeedae3965dd0a35c41b4571fac09dbc863609ad743d08840d"
+    sha256 cellar: :any,                 arm64_sequoia: "1a1c4e184f877580cbac014a8d90e248e6405efe2210e8f18d2fdad152745257"
+    sha256 cellar: :any,                 arm64_sonoma:  "7cd959659ae83e524726ccad59e7d9c00b09a974892bf6f12f1afcfbc2ee9c7e"
+    sha256 cellar: :any,                 arm64_ventura: "a5a02c03a50f3e25ba284a2eabf92484a89c5e3f41f2569b8c4ba883c5afde1b"
+    sha256 cellar: :any,                 sonoma:        "d0edf267dc666539c3bda06a65262caf3191c5d2675db29685487d62989ad5b9"
+    sha256 cellar: :any,                 ventura:       "64b4703d6e2c1388d798d0e112f59f2c983c01eab779a8d828d2babd8127ee0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "536363e9d5ac6196fc55ac97d3b8208d903abca9f9e42a6fb5db2871a0e81e77"
   end
 
   depends_on "cmake" => :build # for pyzmq
