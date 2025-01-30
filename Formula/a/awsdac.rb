cb6a4965aaadc7753d1b19f35e39c782a2d16ad8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 12:50:39 +0000

    awsdac: update 0.21.8 bottle.

--- Formula/a/awsdac.rb
+++ Formula/a/awsdac.rb
@@ -7,11 +7,11 @@ class Awsdac < Formula
   head "https://github.com/awslabs/diagram-as-code.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "28f8cfe265c549e4df24ad5e9eb202c4e2211ea56bf7a3a52071013aaf4956ad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "28f8cfe265c549e4df24ad5e9eb202c4e2211ea56bf7a3a52071013aaf4956ad"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "28f8cfe265c549e4df24ad5e9eb202c4e2211ea56bf7a3a52071013aaf4956ad"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0c10a66632f66b680f258c05607916fd64ef3f98cd9f88440aaa430f73b2e2d3"
-    sha256 cellar: :any_skip_relocation, ventura:       "0c10a66632f66b680f258c05607916fd64ef3f98cd9f88440aaa430f73b2e2d3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2bb67b966b698a1a769477b2d31ad17d5ef339983c3522db9949f0cb7452a8b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2bb67b966b698a1a769477b2d31ad17d5ef339983c3522db9949f0cb7452a8b4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2bb67b966b698a1a769477b2d31ad17d5ef339983c3522db9949f0cb7452a8b4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "995edb7b24fc13b39c56d62af45779a6717df133e453722999b6c577da1d313c"
+    sha256 cellar: :any_skip_relocation, ventura:       "995edb7b24fc13b39c56d62af45779a6717df133e453722999b6c577da1d313c"
   end
 
   depends_on "go" => :build
