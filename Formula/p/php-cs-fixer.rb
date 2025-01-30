41c461c0d08fcba80a9f387384ee99d1a3a4a04f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 11:40:40 +0000

    php-cs-fixer: update 3.68.4 bottle.

--- Formula/p/php-cs-fixer.rb
+++ Formula/p/php-cs-fixer.rb
@@ -7,7 +7,7 @@ class PhpCsFixer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "134cae418233016c3a6f459a24cd059d0e1ea62a774db90555fc0577791c8812"
+    sha256 cellar: :any_skip_relocation, all: "e5e0e5084d197956289adbf6d52c72dfa85af254f7fcda00aabeb2aab19114ed"
   end
 
   depends_on "php@8.3" # php 8.4 support milestone, https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/milestone/173
