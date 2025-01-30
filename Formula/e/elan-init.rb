25ff60ffd0b6a29a30d8bb8709d5531181e2a4d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 13:56:26 +0000

    elan-init 4.0.0
    elan-init: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/e/elan-init.rb
+++ Formula/e/elan-init.rb
@@ -1,19 +1,10 @@
 class ElanInit < Formula
   desc "Lean Theorem Prover installer and version manager"
   homepage "https://github.com/leanprover/elan"
+  url "https://github.com/leanprover/elan/archive/refs/tags/v4.0.0.tar.gz"
+  sha256 "47db0835fb55ce394b4b10a41ad414831ea3a33f44c5b5c1f1feb28b44170b03"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/leanprover/elan.git", branch: "master"
-
-  stable do
-    url "https://github.com/leanprover/elan/archive/refs/tags/v3.1.1.tar.gz"
-    sha256 "b3e3c7dc6cc23dd319725fccf58c898bf4920ff3440c97cb8f3a35fc0d5bbf5c"
-
-    # rust 1.80 build patch, upstream pr ref, https://github.com/leanprover/elan/pull/134
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/815affb24bff77daad4e99e31afed8de42a31312/elan-init/rust-1.80.patch"
-      sha256 "244420efc73ba1fb4908023047fbb1650f783c30e73aa1facb82645d80c7136f"
-    end
-  end
 
   livecheck do
     url :stable
