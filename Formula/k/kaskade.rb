2f0ace9e697565732f90402ae68743544c28864f
Daeho Ro <lamanus@outlook.kr>
2025-01-30 16:41:22 +0900

    kaskade: update source date to the fixed one with comments

--- Formula/k/kaskade.rb
+++ Formula/k/kaskade.rb
@@ -187,9 +187,8 @@ class Kaskade < Formula
   end
 
   def install
-    # attrs > hatchling, fix to `ZIP does not support timestamps before 1980` error
-    ENV["SOURCE_DATE_EPOCH"] = Time.now.to_i.to_s
-
+    # The source doesn't have a valid SOURCE_DATE_EPOCH, so here we set default.
+    ENV["SOURCE_DATE_EPOCH"] = "1451574000"
     virtualenv_install_with_resources
   end
 
