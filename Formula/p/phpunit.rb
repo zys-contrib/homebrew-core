2f0d9c24a7252284262150c1229bef48149a4cd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-31 08:14:39 +0000

    phpunit 11.5.6

--- Formula/p/phpunit.rb
+++ Formula/p/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-11.5.5.phar"
-  sha256 "ce683f1aae066a5282a8acc4d723cfdae627dcd24d4f6314159a9c325326422c"
+  url "https://phar.phpunit.de/phpunit-11.5.6.phar"
+  sha256 "d4619ce9cb7b30442850a8ddcf64bc52e5bc8228f8959c415f4e8fded8656b23"
   license "BSD-3-Clause"
 
   livecheck do
