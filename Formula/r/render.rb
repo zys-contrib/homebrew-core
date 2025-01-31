aba481033b334713062e39661abd51661a9e2b7d
Rui Chen <rui@chenrui.dev>
2025-01-30 23:01:04 -0500

    render 1.1.1
    autobump: add render
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/render.rb
+++ Formula/r/render.rb
@@ -1,8 +1,8 @@
 class Render < Formula
   desc "Command-line interface for Render"
   homepage "https://github.com/render-oss/cli"
-  url "https://github.com/render-oss/cli/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "62d2b561c501646f89045a26d6a9a7d9444457bc725ac0cb1ca9ec204cf334c1"
+  url "https://github.com/render-oss/cli/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "c68166aab1f2160be2ad7662e9174498235d5a345d54c6a0588a3ac835dcc5ea"
   license "Apache-2.0"
   head "https://github.com/render-oss/cli.git", branch: "main"
 
