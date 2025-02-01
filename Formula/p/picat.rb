a48627665a141e4ed5a3047cb01aaea9126b2c83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-01 13:43:01 +0000

    picat: update 3.8 bottle.

--- Formula/p/picat.rb
+++ Formula/p/picat.rb
@@ -12,14 +12,12 @@ class Picat < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "40d2d3c516684f4f5d84b188df3f38fadaac62a9e7364dd59f3e32f25ca0e988"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "41a8357957061027fa117db6f182fbd2ed1963882a2c58fee34df499bffdd42b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ada862573506f31365c089f07ddcad78f9b9584a867439de6dfa32064d070833"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5bef2001e18fe6aeaff008f15a22409507985bf7c6f87f818e83a43c4047552d"
-    sha256 cellar: :any_skip_relocation, sonoma:         "67de472ddbf884bbd3a2d9025470bb0f6739bef146ccaefac16b52145437992c"
-    sha256 cellar: :any_skip_relocation, ventura:        "3737e45b985ab3d8c531a25cfbd6b34a2391070d9204f98acb452c1e41cf3f88"
-    sha256 cellar: :any_skip_relocation, monterey:       "f986ad40642357958593818df9ca9d5f4437213c9ff9e245e4ba0f226c139ac9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b1d2248d9720aefca12a6c9e8b86fb8999e644688f5564ceb6828b2f0d9e104f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8baa72e2d29b850efa2c9f048041e4efbf1264b303d2f87907f21ba702022255"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ec6711ce15529f9918b690ac2a897f375768f9b1188212449ef5a10b52305820"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "da90a0ec7a0b2d745e06880c27e9851004140559b83e2e62adccee92b5b1eb62"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4e7142a99b62517ab3ec4ef69bc87b0f2a2495679bb96071e8bde0f4032b26f7"
+    sha256 cellar: :any_skip_relocation, ventura:       "f32341afb685b5a5a1c34bea930101e949c03a414ffaa3fb8dfc197228c0b262"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a1a72ffb232b2ac33a9eb001751a63ddee735c248cdf55c43cb0482be26391d"
   end
 
   def install
