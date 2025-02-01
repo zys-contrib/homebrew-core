39b9c551df592862b36f88bb596268ed7e7873f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-01 20:47:59 +0000

    mago: update 0.8.1 bottle.

--- Formula/m/mago.rb
+++ Formula/m/mago.rb
@@ -6,12 +6,12 @@ class Mago < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c9c8ac77fe5e2333d27fef2a31ef81ebd3025698ca87d880032aa1cdfb38eb8b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "048b4d95edf79e82243a27c784c06deaf9a1244a3826df6677520e88c9ecbb9d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f6de591ce8c8dcecb628b4547eb10ecb603403615c6679a5bc7d858969f0086d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "abbe2c05f83320d9bfa1a6996ad409dd3fbbec99dde2434e8554e92208dec67c"
-    sha256 cellar: :any_skip_relocation, ventura:       "5058ee2857f05456b3be090349a57a3465022582a0e8b187720e34ef78262556"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "98b367bfe7df7b15cf4ff3f15e4efdbc924934d26467114b28c7fdf6d6fbb0e5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c55bbcd8c02fd66084a51b053dcb3cabaf48765989e0b5dac70af0d82429b76"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e0ea7d1d9802288996fc395fc5d688b13d77a9efbcb66b7ab900835e24f4cd28"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "18aca80201ab7f3e5857eac4f43d60662d7fd9c055f42f13ae9056d38b664837"
+    sha256 cellar: :any_skip_relocation, sonoma:        "64de20d694b9623c1161addb9775b773e7a3450c5d5f55f264d774f12214f3c4"
+    sha256 cellar: :any_skip_relocation, ventura:       "3457bad88cbff03276917464c1ab0af09fafd08301039449833e54f70f14ac56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b40a8292e1746bcf27f662229d266f60eb44e366107a02e56b6a43d7e363d0dd"
   end
 
   depends_on "pkgconf" => :build
