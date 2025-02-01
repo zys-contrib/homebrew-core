541d7a815476dd4a6f9b7acf5793ab0369d1d3e5
Pierre Wendling <pierre.wendling.4@gmail.com>
2025-02-01 22:47:22 +0100

    sdl3_image 3.2.0 (new formula)

---
+++ Formula/s/sdl3_image.rb
@@ -0,0 +1,49 @@
+class Sdl3Image < Formula
+  desc "Library for loading images as SDL surfaces and textures"
+  homepage "https://github.com/libsdl-org/SDL_image"
+  url "https://github.com/libsdl-org/SDL_image/releases/download/release-3.2.0/SDL3_image-3.2.0.tar.gz"
+  sha256 "1690baea71b2b4ded9895126cddbc03a1000b027d099a4fb4669c4d23d73b19f"
+  license "Zlib"
+  head "https://github.com/libsdl-org/SDL_image.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "jpeg-turbo"
+  depends_on "jpeg-xl"
+  depends_on "libavif"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "sdl3"
+  depends_on "webp"
+
+  uses_from_macos "perl" => :build
+
+  def install
+    system "cmake", "-S", ".",
+                    "-B", "build",
+                    "-DSDLIMAGE_BACKEND_IMAGEIO=OFF",
+                    "-DSDLIMAGE_BACKEND_STB=OFF",
+                    "-DSDLIMAGE_DEPS_SHARED=OFF",
+                    "-DSDLIMAGE_INSTALL_MAN=ON",
+                    "-DSDLIMAGE_JXL=ON",
+                    "-DSDLIMAGE_STRICT=ON",
+                    "-DSDLIMAGE_SAMPLES=OFF",
+                    "-DSDLIMAGE_TESTS=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <SDL3_image/SDL_image.h>
+      #include <stdlib.h>
+
+      int main() {
+        return IMG_Version() == SDL_IMAGE_VERSION ? EXIT_SUCCESS : EXIT_FAILURE;
+      }
+    C
+    system ENV.cc, "test.c", "-I#{Formula["sdl3"].opt_include}", "-L#{lib}", "-lSDL3_image", "-o", "test"
+    system "./test"
+  end
+end
