78e29c869a0e14c90f400f52d08e6d7f802f8e42
Daeho Ro <lamanus@outlook.kr>
2025-02-02 14:58:09 +0900

    overtls: fix checksum

--- Formula/o/overtls.rb
+++ Formula/o/overtls.rb
@@ -2,7 +2,7 @@ class Overtls < Formula
   desc "Simple proxy tunnel for bypassing the GFW"
   homepage "https://github.com/ShadowsocksR-Live/overtls"
   url "https://github.com/ShadowsocksR-Live/overtls/archive/refs/tags/v0.2.40.tar.gz"
-  sha256 "b88d27e6418170e9f6e4af0df0947c43b8a100299883f3e13b5bced47d3da341"
+  sha256 "321f2355703a794995ebae3495670b36534dfcd4b4bccfbe6396fae0f83b7edb"
   license "MIT"
   head "https://github.com/ShadowsocksR-Live/overtls.git", branch: "master"
 
