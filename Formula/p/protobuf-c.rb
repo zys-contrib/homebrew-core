7d5a3189d5ca42257632d3f3928202091d81a56e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-02 08:37:06 +0000

    protobuf-c: update 1.5.1 bottle.

--- Formula/p/protobuf-c.rb
+++ Formula/p/protobuf-c.rb
@@ -6,12 +6,12 @@ class ProtobufC < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "e126a9b3a7187dd6d62c635505b284c9bf15375f76f53e8fcb37b4864a9d4606"
-    sha256 cellar: :any,                 arm64_sonoma:  "ebfe8527faeef9424fc49ece715eb37648789eaf39bb6d46b9f57b17fdc88af7"
-    sha256 cellar: :any,                 arm64_ventura: "bb0ad5bef5a788dd56b3472bc3f8c62879c483c786b9a3a3c9ea61be5d5486a5"
-    sha256 cellar: :any,                 sonoma:        "a0db027a6bc9d54bd10f0bfa47ff2fb4ad0fcf4efd4ecf0f1243651dc8d84a49"
-    sha256 cellar: :any,                 ventura:       "ba6edb598c82c195302f320fc934ffc3e640b89744c93ec96fe8c8f64ea5aeca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d79036e60276199da3118381a3b616b125e1a17d3a8b8f43a9c12d0f905f861a"
+    sha256 cellar: :any,                 arm64_sequoia: "446715f1e5c9c96bcbac59e1e4f5744c6ac29b72e17f01c0440cac3badc1f4bd"
+    sha256 cellar: :any,                 arm64_sonoma:  "8ae30fa7cf6e99f3fbfd668e674d03a2c7e37a7cf1220f033df668c0b2c22573"
+    sha256 cellar: :any,                 arm64_ventura: "fabc965b14258983dfa5b10485661faed63954430a2264cbbba5ea7fa2726ff7"
+    sha256 cellar: :any,                 sonoma:        "f79f20f93a46402bbef5efabd202cbf8bb9056e8ca59a5fc7dcdfeeb6a4cc8c6"
+    sha256 cellar: :any,                 ventura:       "ee5ce24a79f4602cee1f23e4c60aff63f18023a02ee118d21bc1e4e3f23ff089"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ddcd7687f0010b40c304dde7d099395d509b9fac55638e41aaa159f0ef795893"
   end
 
   head do
