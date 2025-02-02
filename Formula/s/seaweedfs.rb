f1d654a412281de576e9e02abae91f957b0520ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-02 02:34:54 +0000

    seaweedfs 3.83

--- Formula/s/seaweedfs.rb
+++ Formula/s/seaweedfs.rb
@@ -2,8 +2,8 @@ class Seaweedfs < Formula
   desc "Fast distributed storage system"
   homepage "https://github.com/seaweedfs/seaweedfs"
   url "https://github.com/seaweedfs/seaweedfs.git",
-      tag:      "3.82",
-      revision: "ca4dca14d8b92bc193477f1b97d95fb440f21b43"
+      tag:      "3.83",
+      revision: "5c3d7e6ae63e0e07375d64a9926ed45b536f671c"
   license "Apache-2.0"
   head "https://github.com/seaweedfs/seaweedfs.git", branch: "master"
 
