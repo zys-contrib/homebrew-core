040df4f5a3351c06a919d4572cc1c8d1c47fce77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-04 23:14:38 +0000

    snort 3.6.3.0

--- Formula/s/snort.rb
+++ Formula/s/snort.rb
@@ -1,9 +1,9 @@
 class Snort < Formula
   desc "Flexible Network Intrusion Detection System"
   homepage "https://www.snort.org"
-  url "https://github.com/snort3/snort3/archive/refs/tags/3.6.2.0.tar.gz"
-  mirror "https://fossies.org/linux/misc/snort3-3.6.2.0.tar.gz"
-  sha256 "26cb2d06de9f9575e62087869fbb6fc52a409ce8cdf2da85c287e1f27e3da4d6"
+  url "https://github.com/snort3/snort3/archive/refs/tags/3.6.3.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/snort3-3.6.3.0.tar.gz"
+  sha256 "23476247a7e785008f91bebc84342d7c38bf608ce6ebefc352f3b7d008563b5f"
   license "GPL-2.0-only"
   head "https://github.com/snort3/snort3.git", branch: "master"
 
