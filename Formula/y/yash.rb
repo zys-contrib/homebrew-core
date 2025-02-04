95a2f5932cae80f880e12fb9e3413d98a48d66ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-04 14:15:37 +0000

    yash 2.58.1

--- Formula/y/yash.rb
+++ Formula/y/yash.rb
@@ -1,8 +1,8 @@
 class Yash < Formula
   desc "Yet another shell: a POSIX-compliant command-line shell"
   homepage "https://magicant.github.io/yash/"
-  url "https://github.com/magicant/yash/releases/download/2.58/yash-2.58.tar.xz"
-  sha256 "1a027496a6b8d2aa946d0b13407fdc3d5030f1d17f09b27768967c50f09e61f0"
+  url "https://github.com/magicant/yash/releases/download/2.58.1/yash-2.58.1.tar.xz"
+  sha256 "7674ece98dc77bcc753db49c4311c30532f981682205f9047f20213a3a6755bb"
   license "GPL-2.0-or-later"
 
   bottle do
