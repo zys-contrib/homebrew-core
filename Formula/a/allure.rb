d195623d71e56f77dac7b62e5484b5c2fc5faf2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-04 13:48:39 +0000

    allure 2.32.2

--- Formula/a/allure.rb
+++ Formula/a/allure.rb
@@ -1,8 +1,8 @@
 class Allure < Formula
   desc "Flexible lightweight test report tool"
   homepage "https://github.com/allure-framework/allure2"
-  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.32.0/allure-commandline-2.32.0.zip"
-  sha256 "d0670d85cda9677409b1e8615c2a26f5acfbf64658fbb0e05958d70626f99318"
+  url "https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.32.2/allure-commandline-2.32.2.zip"
+  sha256 "3f28885e2118f6317c92f667eaddcc6491400af1fb9773c1f3797a5fa5174953"
   license "Apache-2.0"
 
   livecheck do
