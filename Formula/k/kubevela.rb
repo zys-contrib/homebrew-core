8f6776a8c000fc90cac4ccd7df29dda605621523
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-05 23:00:14 +0000

    kubevela 1.10.0

--- Formula/k/kubevela.rb
+++ Formula/k/kubevela.rb
@@ -2,8 +2,8 @@ class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
   url "https://github.com/kubevela/kubevela.git",
-      tag:      "v1.9.13",
-      revision: "d56da069eb5ee06d9716810e828d6370b93b70ba"
+      tag:      "v1.10.0",
+      revision: "bc15e5b3592467db3d71f166835eb05540af2abe"
   license "Apache-2.0"
   head "https://github.com/kubevela/kubevela.git", branch: "master"
 
