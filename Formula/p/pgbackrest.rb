4a1c84a887fed5ab3144191dea2dabf079855d9d
Rui Chen <rui@chenrui.dev>
2025-02-05 13:46:05 +0000

    pgbackrest: add 2.54.2 bottle.

--- Formula/p/pgbackrest.rb
+++ Formula/p/pgbackrest.rb
@@ -5,6 +5,15 @@ class Pgbackrest < Formula
   sha256 "f4d21dd8079c0c5255122530b166bebbf48b3d4361b14d6094197809ffb48f98"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any, arm64_sequoia: "0079ce5ea11175cc717d7de39e95e40f6c27467a32592c25695067e2b62ce7db"
+    sha256 cellar: :any, arm64_sonoma:  "908047fb0cd421af0f9bca8ebef4855f8442e9a027b238a2f305232dfef58419"
+    sha256 cellar: :any, arm64_ventura: "06e35c096155ef777b63ab8546434553faae37ca8163fd6b4a656a25db9c1200"
+    sha256 cellar: :any, sonoma:        "0c97b5bf0b4a530fa7616f8bd720b46c321ff58ec90adaeb97af33fe6455fafa"
+    sha256 cellar: :any, ventura:       "38dc79cb0a755a5401dab40938e44753fd525283f1340248df51bc4aa3e653ce"
+    sha256               x86_64_linux:  "26b7b370dc188c4ef542705baf1774243a6bb9073f6f7a1155cdf3d403b10ba4"
+  end
+
   depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
