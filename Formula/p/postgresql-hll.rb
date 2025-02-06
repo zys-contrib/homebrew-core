3a2aff78b8feea6e8926fe96b7769a85b4c1c151
Andrew Kane <andrew@ankane.org>
2025-02-06 09:02:42 -0800

    postgresql-hll 2.18 (new formula)

---
+++ Formula/p/postgresql-hll.rb
@@ -0,0 +1,46 @@
+class PostgresqlHll < Formula
+  desc "PostgreSQL extension adding HyperLogLog data structures as a native data type"
+  homepage "https://github.com/citusdata/postgresql-hll"
+  url "https://github.com/citusdata/postgresql-hll/archive/refs/tags/v2.18.tar.gz"
+  sha256 "e2f55a6f4c4ab95ee4f1b4a2b73280258c5136b161fe9d059559556079694f0e"
+  license "Apache-2.0"
+
+  depends_on "postgresql@14" => [:build, :test]
+  depends_on "postgresql@17" => [:build, :test]
+
+  def postgresqls
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("postgresql@") }
+  end
+
+  def install
+    postgresqls.each do |postgresql|
+      ENV["PG_CONFIG"] = postgresql.opt_bin/"pg_config"
+      system "make"
+      system "make", "install", "pkglibdir=#{lib/postgresql.name}",
+                                "datadir=#{share/postgresql.name}",
+                                "pkgincludedir=#{include/postgresql.name}"
+      system "make", "clean"
+    end
+  end
+
+  test do
+    ENV["LC_ALL"] = "C"
+    postgresqls.each do |postgresql|
+      pg_ctl = postgresql.opt_bin/"pg_ctl"
+      psql = postgresql.opt_bin/"psql"
+      port = free_port
+
+      datadir = testpath/postgresql.name
+      system pg_ctl, "initdb", "-D", datadir
+      (datadir/"postgresql.conf").write <<~EOS, mode: "a+"
+        port = #{port}
+      EOS
+      system pg_ctl, "start", "-D", datadir, "-l", testpath/"log-#{postgresql.name}"
+      begin
+        system psql, "-p", port.to_s, "-c", "CREATE EXTENSION hll;", "postgres"
+      ensure
+        system pg_ctl, "stop", "-D", datadir
+      end
+    end
+  end
+end
