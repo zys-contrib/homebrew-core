208a2972cda027324132196fc0b20fe306d0e2db
Rui Chen <rui@chenrui.dev>
2025-02-07 13:13:35 -0500

    topgit 0.19.14
    autobump: add topgit
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/topgit.rb
+++ Formula/t/topgit.rb
@@ -1,8 +1,8 @@
 class Topgit < Formula
   desc "Git patch queue manager"
   homepage "https://github.com/mackyle/topgit"
-  url "https://github.com/mackyle/topgit/archive/refs/tags/topgit-0.19.13.tar.gz"
-  sha256 "eaab17c64c95e70acfcc9d4061e7cc4143eb5f6dbe7bc23a5091cb45885a682c"
+  url "https://github.com/mackyle/topgit/archive/refs/tags/topgit-0.19.14.tar.gz"
+  sha256 "0556485ca8ddf0cf863de4da36b11351545aca74fbf71581ffe9f5a5ce0718cb"
   license "GPL-2.0-only"
 
   bottle do
