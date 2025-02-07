ecf4558e4e6c276605081a851e6972f24d4e8a7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-07 11:07:55 +0000

    lima 1.0.5

--- Formula/l/lima.rb
+++ Formula/l/lima.rb
@@ -1,8 +1,8 @@
 class Lima < Formula
   desc "Linux virtual machines"
   homepage "https://lima-vm.io/"
-  url "https://github.com/lima-vm/lima/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "dd8bb1c6ee536666f093eb13d5831a906ca3eaa9aebc914bdfb006d4b114a949"
+  url "https://github.com/lima-vm/lima/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "8087267177549aaa695178431e5ef02eacced0802927004d6bb066afeff5b4e8"
   license "Apache-2.0"
   head "https://github.com/lima-vm/lima.git", branch: "master"
 
