2824c5f65515b821d2a6a8502286587af726efcc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-08 05:17:52 +0000

    envelope: update 0.3.12 bottle.

--- Formula/e/envelope.rb
+++ Formula/e/envelope.rb
@@ -7,14 +7,12 @@ class Envelope < Formula
   head "https://github.com/mattrighetti/envelope.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "3b39029d44b63c22689187ba607bfd908704b7a6f1079d8ce703b2e36babc380"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "5b1c6693872f280e3835ec2d2d165f7a1573aad8062c6d532e54b64d172bb6a4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e3722c57985931fefbab87d9626cc1fec6540648585b662a0d346401752902cb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4a28d3c6400f252fa81effabe3cf5c2b1deb17f8b165984cce4cd39e934f752f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "3840579bcf9c3a0de3826f3a58ee7dbdf345fb4e88d124ee86826e99cbbd43de"
-    sha256 cellar: :any_skip_relocation, ventura:        "0ac1e844f2cb4b61af8b3f14d38a8ca899827b4b5f6bace2cfd19f74ba393482"
-    sha256 cellar: :any_skip_relocation, monterey:       "e4e48ba6d4360ea32d899a859ed2056b539f04a6f11f7c128a573d4b023eaa36"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0084ff7d440399bb969e0b3e05bcb197afa8357c36e7832baf44963804784a7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c0cf2f0fdc14d5952058149e3e46e2676217cd8afc292acdac7a12156db38db"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8dac95f255b257fa8122c5c479422b3c368d67948eeb91bb7e3144ac72e5eb74"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5fb67ba1dd8b37503332ff4e3b3c126460945a94891e7014c473065a59ad4c46"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cd582e7e0b208fb0985c3e8ec7ed2058d7437c02b48fc590e83d7f51e138ddae"
+    sha256 cellar: :any_skip_relocation, ventura:       "0157f4505292701dc9b16fb42a7d46b28a82a8a7205ca3c6451a36efaf4b767c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "284137aa430e081be9a59a0c1616fbae45147d86ca8cc5642418959cf5f25f5b"
   end
 
   depends_on "pandoc" => :build
