5c3d1f3000c039fdd4633115720c6e02f0f3c7e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-08 19:54:53 +0000

    di 5.0.5

--- Formula/d/di.rb
+++ Formula/d/di.rb
@@ -1,8 +1,8 @@
 class Di < Formula
   desc "Advanced df-like disk information utility"
   homepage "https://diskinfo-di.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/diskinfo-di/di-5.0.4.tar.gz"
-  sha256 "2055abd8501cfc376b34f6fd69a32ed949ff26325ce57df5c82a3d8f71bab76b"
+  url "https://downloads.sourceforge.net/project/diskinfo-di/di-5.0.5.tar.gz"
+  sha256 "47522146deab224f94df02e67c5256295795690cf70f6a71d361f3efc497a175"
   license "Zlib"
 
   # This only matches tarballs in the root directory, as a way of avoiding
