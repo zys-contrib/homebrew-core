e685a8aa2e7562a20bb0059dfaf4db51761ed2e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-08 17:07:47 +0000

    basedpyright: update 1.27.0 bottle.

--- Formula/b/basedpyright.rb
+++ Formula/b/basedpyright.rb
@@ -7,12 +7,12 @@ class Basedpyright < Formula
   head "https://github.com/detachhead/basedpyright.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a033658be3a1a32fd3f767a1accc157a56359a9aa2711025314b9b24ab9b81a1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a033658be3a1a32fd3f767a1accc157a56359a9aa2711025314b9b24ab9b81a1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a033658be3a1a32fd3f767a1accc157a56359a9aa2711025314b9b24ab9b81a1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "031ee6fca017cd844847defd29792f2795b1c02b4102c3472b65ed8627a78b54"
-    sha256 cellar: :any_skip_relocation, ventura:       "031ee6fca017cd844847defd29792f2795b1c02b4102c3472b65ed8627a78b54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a033658be3a1a32fd3f767a1accc157a56359a9aa2711025314b9b24ab9b81a1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "00d36a279e0ae38832d72ecef02d79a0c95f4ad6661970a4b419242e778d9504"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00d36a279e0ae38832d72ecef02d79a0c95f4ad6661970a4b419242e778d9504"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "00d36a279e0ae38832d72ecef02d79a0c95f4ad6661970a4b419242e778d9504"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6e42a46b71d74134c4fc0c03739e57b4f7d49d2698047e57f5a31fea6b3d2270"
+    sha256 cellar: :any_skip_relocation, ventura:       "6e42a46b71d74134c4fc0c03739e57b4f7d49d2698047e57f5a31fea6b3d2270"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00d36a279e0ae38832d72ecef02d79a0c95f4ad6661970a4b419242e778d9504"
   end
 
   depends_on "node"
