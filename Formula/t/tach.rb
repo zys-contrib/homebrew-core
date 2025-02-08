a0313310d4c62d83780bb34f0398ed3d977fa209
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-08 20:12:57 +0000

    tach 0.25.1

--- Formula/t/tach.rb
+++ Formula/t/tach.rb
@@ -3,8 +3,8 @@ class Tach < Formula
 
   desc "Tool to enforce dependencies using modular architecture"
   homepage "https://docs.gauge.sh/getting-started/introduction"
-  url "https://files.pythonhosted.org/packages/05/2c/1afb1a3c16125b9cfc5a1da79ba2329dec11e16b9c9eea7ac411074a49cb/tach-0.24.1.tar.gz"
-  sha256 "63f7f3b3e3458a97ded020b524f32fc72bc731ff880d0709301b2802ff759721"
+  url "https://files.pythonhosted.org/packages/59/2d/1810262cf770eb33b715eac4f203c7d66a72ae96e094c29374753cd67547/tach-0.25.1.tar.gz"
+  sha256 "4e2df61e08039e2d51f4fa166ab450bf25f6452e70e684d33181a3dbb1848186"
   license "MIT"
   head "https://github.com/gauge-sh/tach.git", branch: "main"
 
