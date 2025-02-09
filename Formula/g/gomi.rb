e006b99136becff33cc98a853f2a68bc08ebde45
Daeho Ro <lamanus@outlook.kr>
2025-02-09 08:05:57 +0000

    gomi: add 1.2.2 bottle.

--- Formula/g/gomi.rb
+++ Formula/g/gomi.rb
@@ -6,6 +6,15 @@ class Gomi < Formula
   license "MIT"
   head "https://github.com/babarot/gomi.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9fe650231c6cdb0a468e2d8f7283270f12d8e429278157d66f32128222226c20"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9fe650231c6cdb0a468e2d8f7283270f12d8e429278157d66f32128222226c20"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9fe650231c6cdb0a468e2d8f7283270f12d8e429278157d66f32128222226c20"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a618ab04c5f5578912dadf86b10e84fe9feafd3e700a2a53660347008717f312"
+    sha256 cellar: :any_skip_relocation, ventura:       "a618ab04c5f5578912dadf86b10e84fe9feafd3e700a2a53660347008717f312"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "372badc575a4efd9c31cc21ef47c454ea53c3f69b0929ae0a2758dd0731ebdee"
+  end
+
   depends_on "go" => :build
 
   def install
