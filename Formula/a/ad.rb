ecda2356c86e86ef1c3afa559b51260d26db5f6b
Timothy Sutton <119358+timsutton@users.noreply.github.com>
2025-02-09 18:44:51 +0000

    ad: add 0.2.0 bottle.

--- Formula/a/ad.rb
+++ Formula/a/ad.rb
@@ -6,6 +6,15 @@ class Ad < Formula
   license "MIT"
   head "https://github.com/sminez/ad.git", branch: "develop"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0362655e5d1c192e376fd56ee567e9be2433ed3249cf63b5eaf259868869b3b7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "88017d42ccdeeaf111e0f739ccb8dedbb61c4279525c861bb55398aeb931541c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "178214b5293b70bacd98ba7d2bd5577cb951ceed7aadde102240c9032c1dbc8f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9a40d1565fa03746e07bb1ab5fd968fc3f350c711093460b8246f6d619402094"
+    sha256 cellar: :any_skip_relocation, ventura:       "164bd7e2ccd5e4fa23f170f9aef09e1b6fee344196e1503902b67e9142db31d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "95139e4a509983eaf89866618c291300b10eeb2ab196012c6ae758aba5feab16"
+  end
+
   depends_on "rust" => :build
 
   def install
