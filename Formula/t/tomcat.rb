6de101865d80c6a7b3cbb28cb8d376ad869e7e1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-10 14:13:57 +0000

    tomcat 11.0.3

--- Formula/t/tomcat.rb
+++ Formula/t/tomcat.rb
@@ -1,9 +1,9 @@
 class Tomcat < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-11/v11.0.2/bin/apache-tomcat-11.0.2.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-11/v11.0.2/bin/apache-tomcat-11.0.2.tar.gz"
-  sha256 "c1b31a6989d30a7344c1a3655da74679ce9f9b48a271e864647a141de3b78cb8"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-11/v11.0.3/bin/apache-tomcat-11.0.3.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-11/v11.0.3/bin/apache-tomcat-11.0.3.tar.gz"
+  sha256 "00a01f9886593859716bc40473db6db9d5c0c3b5569c810898e33d73157aba56"
   license "Apache-2.0"
 
   bottle do
