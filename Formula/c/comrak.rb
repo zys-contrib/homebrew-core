9fed0d0f8e333fa5a75cdd2ac455dc9d0fadc930
Rui Chen <rui@chenrui.dev>
2025-02-11 03:23:05 +0000

    comrak: add 0.35.0 bottle.

--- Formula/c/comrak.rb
+++ Formula/c/comrak.rb
@@ -6,6 +6,15 @@ class Comrak < Formula
   license "BSD-2-Clause"
   head "https://github.com/kivikakk/comrak.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3452b00cb548122e85e7a687ffde8de953486e12a143f752dd900bc4a0fbb2d9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a2469b2f806efb35c6c5424e9dda06f7e6f7f2b2924b3e3996503016b28abde8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "14a0fd6da68a832a0f5102dba679bb44e930c415c152d7a7efe0a868bf69a792"
+    sha256 cellar: :any_skip_relocation, sonoma:        "52b2f6f0364fd8542b5b0d429158e534c15a10d35523ca0cbe73c694681dca85"
+    sha256 cellar: :any_skip_relocation, ventura:       "085d626d2f40cbf51556c200669e75657a639855fcf2757d6f472c93dd74f891"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f22f5a32c1b8c159a56dc3faeefada0b3ea785408ed6ba642a0a611c5e86dbf"
+  end
+
   depends_on "rust" => :build
 
   def install
