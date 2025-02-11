a132472b1da310d33702c0a527428bafcf31ff15
Rui Chen <rui@chenrui.dev>
2025-02-11 03:23:03 +0000

    cfnctl: add 0.1.1 bottle.

--- Formula/c/cfnctl.rb
+++ Formula/c/cfnctl.rb
@@ -6,6 +6,15 @@ class Cfnctl < Formula
   license "Apache-2.0"
   head "https://github.com/rogerwelin/cfnctl.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d18150beed91b47a03146e5cceaa9d117dbde79fc14bdc923fd32b83443af4e5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d18150beed91b47a03146e5cceaa9d117dbde79fc14bdc923fd32b83443af4e5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d18150beed91b47a03146e5cceaa9d117dbde79fc14bdc923fd32b83443af4e5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "09f3b8deeefea7efd4bbdc4f7abc993cf90e9c8791fd32bc45685bd290bc0372"
+    sha256 cellar: :any_skip_relocation, ventura:       "09f3b8deeefea7efd4bbdc4f7abc993cf90e9c8791fd32bc45685bd290bc0372"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c656851efb6ce909bc879ba960c4d134c824d725909b01799dea6a29405cfe9"
+  end
+
   depends_on "go" => :build
 
   def install
