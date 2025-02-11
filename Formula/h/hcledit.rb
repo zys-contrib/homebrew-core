112a45ebc52f59663bab6900d56927c4423c9cf8
Rui Chen <rui@chenrui.dev>
2025-02-11 03:23:15 +0000

    hcledit: add 0.2.16 bottle.

--- Formula/h/hcledit.rb
+++ Formula/h/hcledit.rb
@@ -6,6 +6,15 @@ class Hcledit < Formula
   license "MIT"
   head "https://github.com/minamijoyo/hcledit.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f2d194861571963e5e60773623c3a6f0b20c39b62f7484ad5cad1d41ddfe4857"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f2d194861571963e5e60773623c3a6f0b20c39b62f7484ad5cad1d41ddfe4857"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f2d194861571963e5e60773623c3a6f0b20c39b62f7484ad5cad1d41ddfe4857"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2a201ba527e406a1b0d2005b736613294217ad2a6634a656a1dc99f192e7ebc4"
+    sha256 cellar: :any_skip_relocation, ventura:       "2a201ba527e406a1b0d2005b736613294217ad2a6634a656a1dc99f192e7ebc4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3d1694eb92ee46fcd13872c9a9124bfc33cb1f1b44bfd99b3f11c285152edf1"
+  end
+
   depends_on "go" => :build
 
   def install
