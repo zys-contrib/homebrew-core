22d4c3f55dcb2c5d7a4afd68d71397d9939c99dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 19:03:46 +0000

    i686-elf-binutils: update 2.44 bottle.

--- Formula/i/i686-elf-binutils.rb
+++ Formula/i/i686-elf-binutils.rb
@@ -11,14 +11,12 @@ class I686ElfBinutils < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia:  "a982ec150ffe8a236c134e11da09031fb988b635b3dcde8dab900be7e2307327"
-    sha256 arm64_sonoma:   "c666e0fa7eb192bd26a49bcb5e0f400b497e6a4afa6c972d90a53c1b7817407c"
-    sha256 arm64_ventura:  "dbbe5217dbadd954e0bc166586e2b02a5da58369e53b7e7d0fe8d151e72457e2"
-    sha256 arm64_monterey: "5c0a59163674c73d8ec51d7483d088b0d82b0226f7cdb50ae3f412c93430c25f"
-    sha256 sonoma:         "411bc87f8ccab10165f7e1001471f0241b07d67610267cb707be8974aa1af508"
-    sha256 ventura:        "3652d2d27a8efe6525e7b74b52c4c4c01bcf24e7f4a1d1d65f5bf77ae0bac8fc"
-    sha256 monterey:       "ebc991155f41f233df8eaab15da8ff2755f9cc0e8ff00a4fb39412613711a957"
-    sha256 x86_64_linux:   "261dda7591614a6c0ff80dff4f7b85b1b746c97dc5510a695fae6f894559e6f9"
+    sha256 arm64_sequoia: "b0a006a8cb8b4b032c38c07349f0287d0cfb719ca75845cccb13415908b4938c"
+    sha256 arm64_sonoma:  "9afba9508ee359df64f8f4aa6b8281a9b10955e3931466018f055191b3fdc955"
+    sha256 arm64_ventura: "23dc4080e2539e76c030d156545e70b8e4884d382c960edc8f2d5380d4a96a14"
+    sha256 sonoma:        "3396bc05cca54e0beed9815a264d62409606b9c769b735961d6b6cf8f09f73bc"
+    sha256 ventura:       "c8aa9c42376c639a42c169b739428ce2686bf580c387830b13972ee33ee4742c"
+    sha256 x86_64_linux:  "958ff43e0bc720c34882ec39f853810e651989dfed66fc2af63c88361a96ab74"
   end
 
   depends_on "pkgconf" => :build
