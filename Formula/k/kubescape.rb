56c8a72582436de5b00abe1bb22c6be747c14b59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 11:40:59 +0000

    kubescape: update 3.0.27 bottle.

--- Formula/k/kubescape.rb
+++ Formula/k/kubescape.rb
@@ -12,12 +12,12 @@ class Kubescape < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fcf13bd317f6c21989edcfadde199002ba30b9b0295006505522c87883927159"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0eec3be2b04ae3cd046793eba2b7de34003902dcdd202e8ae1d03a159fe2c06e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b206367e470887b7d77564b4d040c77e518f13943de0e780cb3057a80ba62f49"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0cd22703c10a03bd89c5fd880334bbd1703f66afee53a8d686712862556908f9"
-    sha256 cellar: :any_skip_relocation, ventura:       "7f1a5353bcd6b4abcc41fdd76647864d6aa8c77c6388723569837471a1e3817c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9bf511cf4c3b87b689edeaff521d02e0d542a37e945d8e8eeaec3817de416ad3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3a236f8a7df21dccce136e3432a5e456b3aba32c1cfda4ba5e589fd29d86ef5f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "449e52b1aea9a7f9dd0dab55183d8b0f9e540b27ade9903d0a0319c66502e5cb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2a34be25485a5b3578bcb520ee7a51c37d8bea3d6cfd5d26e9565275c8142c9d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "db6316fb902d4fe8ce008a7554f89ba919b3a25e82ca34b00650a41707f0a2f7"
+    sha256 cellar: :any_skip_relocation, ventura:       "5d71021798592d82559cb8a85d542f2df6d57c0f2b7f7041e346ba2a6a4fa994"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b4a1bca41616676f7507a941853273153ea102721e90011e2e2edc1a5af98f8"
   end
 
   depends_on "go" => :build
