5b2827717941f82f37ccdc148a23fc372d20367c
ellie is not coding in the slightest <166862894+eleanordoesntcode@users.noreply.github.com>
2025-02-12 19:59:41 +0100

    arm-none-eabi-gdb 16.2

--- Formula/a/arm-none-eabi-gdb.rb
+++ Formula/a/arm-none-eabi-gdb.rb
@@ -1,9 +1,9 @@
 class ArmNoneEabiGdb < Formula
   desc "GNU debugger for arm-none-eabi cross development"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-16.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gdb/gdb-16.1.tar.xz"
-  sha256 "c2cc5ccca029b7a7c3879ce8a96528fdfd056b4d884f2b0511e8f7bc723355c6"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-16.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-16.2.tar.xz"
+  sha256 "4002cb7f23f45c37c790536a13a720942ce4be0402d929c9085e92f10d480119"
   license "GPL-3.0-or-later"
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
 
@@ -33,10 +33,6 @@ class ArmNoneEabiGdb < Formula
   on_system :linux, macos: :ventura_or_newer do
     depends_on "texinfo" => :build
   end
-
-  # Fix build on Linux
-  # Ref: https://sourceware.org/bugzilla/show_bug.cgi?id=32578
-  patch :DATA
 
   def install
     target = "arm-none-eabi"
@@ -69,31 +65,3 @@ class ArmNoneEabiGdb < Formula
           shell_output("#{bin}/arm-none-eabi-gdb -batch -ex 'info address _start' a.out")
   end
 end
-
-__END__
-diff --git a/bfd/Makefile.in b/bfd/Makefile.in
-index aec3717485a..ee674a36c5b 100644
---- a/bfd/Makefile.in
-+++ b/bfd/Makefile.in
-@@ -1318,7 +1318,7 @@ REGEN_TEXI = \
- 	$(MKDOC) -f $(srcdir)/doc/doc.str < $< > $@.tmp; \
- 	texi=$@; \
- 	texi=$${texi%.stamp}.texi; \
--	test -e $$texi || test ! -f $(srcdir)/$$texi || $(LN_S) $(srcdir)/$$texi $$texi; \
-+	test -e $$texi || test ! -f $(srcdir)/$$texi || $(LN_S) $(abs_srcdir)/$$texi $$texi; \
- 	$(SHELL) $(srcdir)/../move-if-change $@.tmp $$texi; \
- 	touch $@; \
- 	)
-diff --git a/bfd/doc/local.mk b/bfd/doc/local.mk
-index 97d658b5a48..9b75402387c 100644
---- a/bfd/doc/local.mk
-+++ b/bfd/doc/local.mk
-@@ -101,7 +101,7 @@ REGEN_TEXI = \
- 	$(MKDOC) -f $(srcdir)/%D%/doc.str < $< > $@.tmp; \
- 	texi=$@; \
- 	texi=$${texi%.stamp}.texi; \
--	test -e $$texi || test ! -f $(srcdir)/$$texi || $(LN_S) $(srcdir)/$$texi $$texi; \
-+	test -e $$texi || test ! -f $(srcdir)/$$texi || $(LN_S) $(abs_srcdir)/$$texi $$texi; \
- 	$(SHELL) $(srcdir)/../move-if-change $@.tmp $$texi; \
- 	touch $@; \
- 	)
