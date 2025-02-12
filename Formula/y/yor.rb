c0d93dcee4190f8fb1a7dbd2ccaccc318da7a114
Rui Chen <rui@chenrui.dev>
2025-02-12 05:34:13 +0000

    yor: add 0.1.199 bottle.

--- Formula/y/yor.rb
+++ Formula/y/yor.rb
@@ -6,6 +6,15 @@ class Yor < Formula
   license "Apache-2.0"
   head "https://github.com/bridgecrewio/yor.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "add161546ea037ca791427da0e8970f296b353ded3647123f58be512171a9509"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e5ba4db629f59e900f0a7b752a37248bde740abf02f51253db305d9915b32280"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "aba57a6a7dd44edbf2e1c0809e9e240c11cb59e811753695e6f047fe1fb6428e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1a21fd35f91937dbd3d27bc4d22c58e23c4bfe9996fb378819d31746b8864ad9"
+    sha256 cellar: :any_skip_relocation, ventura:       "8ebec36b59a55e788f79adfebb6c1af2649fc5f8fb1aaf23988f69d86f6ad95c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46f04666cd4d097b5395e4516d2bb396f9c868dce659571054e503926eb13e33"
+  end
+
   depends_on "go" => :build
 
   def install
