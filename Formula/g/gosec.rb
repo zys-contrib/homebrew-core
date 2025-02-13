a06ed8036bebffac83e0c093d8339cd4d2a8a712
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 14:30:14 +0000

    gosec: update 2.22.1 bottle.

--- Formula/g/gosec.rb
+++ Formula/g/gosec.rb
@@ -7,12 +7,12 @@ class Gosec < Formula
   head "https://github.com/securego/gosec.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71ccd318efdec88f99b582e308d716420fe64f93c28954458da8fceab99c3870"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "71ccd318efdec88f99b582e308d716420fe64f93c28954458da8fceab99c3870"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "71ccd318efdec88f99b582e308d716420fe64f93c28954458da8fceab99c3870"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1a5498584c52af1c8bf5c075acf546ae32f0141a45d4049ff32b4a3f1c0fd3a4"
-    sha256 cellar: :any_skip_relocation, ventura:       "1a5498584c52af1c8bf5c075acf546ae32f0141a45d4049ff32b4a3f1c0fd3a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e3d6d5669d73ca4f32446fd63f4f5e36b0f4f42fe58f8d419065feeb435dda1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "39547df556beebd459dbb1515f9a485facdcf17cb51743e6264608ff45e74881"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "39547df556beebd459dbb1515f9a485facdcf17cb51743e6264608ff45e74881"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "39547df556beebd459dbb1515f9a485facdcf17cb51743e6264608ff45e74881"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ca281764cda7eeae23c8331e7cc869c3a1b070086e7ada1b5ef6554d6735b34b"
+    sha256 cellar: :any_skip_relocation, ventura:       "ca281764cda7eeae23c8331e7cc869c3a1b070086e7ada1b5ef6554d6735b34b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "16400b90bdea85343536474bcdc34515d8c2420d0e5ada72e9e4a8ba82bd1006"
   end
 
   depends_on "go"
