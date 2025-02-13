3429aad38b7ca525406e4aba987cf358d102c5a4
Caleb Xu <calebcenter@live.com>
2025-02-13 10:59:39 -0500

    mac 10.96 (new formula)

---
+++ Formula/m/mac.rb
@@ -0,0 +1,29 @@
+class Mac < Formula
+  desc "Monkey's Audio lossless codec"
+  homepage "https://www.monkeysaudio.com"
+  url "https://monkeysaudio.com/files/MAC_1096_SDK.zip"
+  version "10.96"
+  sha256 "73b25a517079bb015e2066b65854a7de0d8468f050b9b96025b7a3cb038f4dba"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://www.monkeysaudio.com/versionhistory.html"
+    regex(%r{<div\s+class="release">Version\s+(.*)\s+\(.*\)</div>}i)
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system bin/"mac", test_fixtures("test.wav"), "test.ape", "-c2000"
+    system bin/"mac", "test.ape", "-V"
+    system bin/"mac", "test.ape", "test.wav", "-d"
+    assert_equal Digest::SHA256.hexdigest(test_fixtures("test.wav").read),
+                 Digest::SHA256.hexdigest((testpath/"test.wav").read)
+  end
+end
