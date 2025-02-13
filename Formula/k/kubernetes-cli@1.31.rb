29828c494062bc71a412a00224fc57fe1aca6d63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 11:18:31 +0000

    kubernetes-cli@1.31 1.31.6

--- Formula/k/kubernetes-cli@1.31.rb
+++ Formula/k/kubernetes-cli@1.31.rb
@@ -2,8 +2,8 @@ class KubernetesCliAT131 < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/docs/reference/kubectl/"
   url "https://github.com/kubernetes/kubernetes.git",
-      tag:      "v1.31.5",
-      revision: "af64d838aacd9173317b39cf273741816bd82377"
+      tag:      "v1.31.6",
+      revision: "6b3560758b37680cb713dfc71da03c04cadd657c"
   license "Apache-2.0"
 
   livecheck do
