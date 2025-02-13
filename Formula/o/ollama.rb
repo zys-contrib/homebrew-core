f86ed25db1b16d9bfde771bb4ca6bace065a1fee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 20:05:49 +0000

    ollama 0.5.9
    ollama: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/ollama.rb
+++ Formula/o/ollama.rb
@@ -2,8 +2,8 @@ class Ollama < Formula
   desc "Create, run, and share large language models (LLMs)"
   homepage "https://ollama.com/"
   url "https://github.com/ollama/ollama.git",
-      tag:      "v0.5.7",
-      revision: "a420a453b4783841e3e79c248ef0fe9548df6914"
+      tag:      "v0.5.9",
+      revision: "a4f69a0191b304c204ef074ccd6523f121bfddfe"
   license "MIT"
   head "https://github.com/ollama/ollama.git", branch: "main"
 
@@ -28,10 +28,6 @@ class Ollama < Formula
   depends_on "go" => :build
 
   def install
-    # Fix to makefile path, should be checked in next release
-    # https://github.com/ollama/ollama/blob/89d5e2f2fd17e03fd7cd5cb2d8f7f27b82e453d7/llama/llama.go#L3
-    inreplace "llama/llama.go", "//go:generate make -j", "//go:generate make -C ../ -j"
-
     # Silence tens of thousands of SDK warnings
     ENV["SDKROOT"] = MacOS.sdk_path if OS.mac?
 
