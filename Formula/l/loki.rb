bdeba26ed4b565d3dd85434ac8b7e8a366500774
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 02:37:29 +0000

    loki: update 3.4.0 bottle.

--- Formula/l/loki.rb
+++ Formula/l/loki.rb
@@ -12,12 +12,12 @@ class Loki < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5fd0beded217871d642e1f09eca8198cfff0e480b1851f1139fb2349e377d70b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9536ea8a854152b333305728480ed1af59bf5a3ba9fa7fc76f1f27fb06fcd971"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7dfd2b5f0f0b314654dd9298f2403212ac4abdaaad87e1ec1858532a63cfd54e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9d1d8c5232fd60805acabf75955e860a781a40de47171f3cd439ef71af255617"
-    sha256 cellar: :any_skip_relocation, ventura:       "685c61f7b828b0e6e37326001c10522533cc9d37341b6592cc44f3d0d23afeab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04d00ca11fc0cf92bb0493bb4f965410ba887c18399d4f276bff76259764aa65"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c3f3061d8355561e68c46b68fdbbf66480100d9fc7d9c64d0c2619d71b2303f5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e7c36b1c4f110f00c692b2cbcac1fe46f76aeb009b75ee508cc4f45b9958a3fd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0c5a7dacb70b8c0c9dd549839d9da7580d81bb1a7e34d1c073a3621e0be100ed"
+    sha256 cellar: :any_skip_relocation, sonoma:        "001c39a2928d736ca66acedcfb50e735de1374d2aba6ff97d117a7d9abc83b5e"
+    sha256 cellar: :any_skip_relocation, ventura:       "eef670ba891f920be08beddbbd33e8a3c0dbaf7971d521a3a16c4af2d9afddc1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50ecc922c8f4bf9924c4e8e001b4a48b63be9c345a1be16336658ff4e41fbf94"
   end
 
   depends_on "go" => :build
