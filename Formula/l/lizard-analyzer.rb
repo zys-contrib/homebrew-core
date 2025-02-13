beff9fcccfd3b6a458a89e3957a53868b12d1884
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 11:22:12 +0000

    lizard-analyzer 1.17.19

--- Formula/l/lizard-analyzer.rb
+++ Formula/l/lizard-analyzer.rb
@@ -3,8 +3,8 @@ class LizardAnalyzer < Formula
 
   desc "Extensible Cyclomatic Complexity Analyzer"
   homepage "https://github.com/terryyin/lizard"
-  url "https://files.pythonhosted.org/packages/0a/6f/e657290c267d99a59e937a1509a29641482ee2035c46e1067ecb0d40330e/lizard-1.17.18.tar.gz"
-  sha256 "7a6bc473b8b85ec4676b24762ae7a0c522b07e0b45430c068a2cb8f5b7c5e9a4"
+  url "https://files.pythonhosted.org/packages/92/fc/b7d0e763bc17533b80c174d3da8d6321d32a1d844b6eed1344628961a42c/lizard-1.17.19.tar.gz"
+  sha256 "366b79106018b465fee88ac971beef47b5b8be1b0dd6e1d4e5823f48af34ae6e"
   license "MIT"
 
   bottle do
