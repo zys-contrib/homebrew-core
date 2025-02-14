84d371b49147e32026331f63b09edcb35da9982e
Daeho Ro <lamanus@outlook.kr>
2025-02-14 23:43:43 +0900

    libsndfile: revision bump (flac 1.5.0)

--- Formula/lib/libsndfile.rb
+++ Formula/lib/libsndfile.rb
@@ -4,7 +4,7 @@ class Libsndfile < Formula
   url "https://github.com/libsndfile/libsndfile/releases/download/1.2.2/libsndfile-1.2.2.tar.xz"
   sha256 "3799ca9924d3125038880367bf1468e53a1b7e3686a934f098b7e1d286cdb80e"
   license "LGPL-2.1-or-later"
-
+  revision 1
   livecheck do
     url :stable
     strategy :github_latest
