d79856d3420889cf141458622a2315ed1105223d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-14 10:55:06 +0000

    double-conversion 3.3.1

--- Formula/d/double-conversion.rb
+++ Formula/d/double-conversion.rb
@@ -1,8 +1,8 @@
 class DoubleConversion < Formula
   desc "Binary-decimal and decimal-binary routines for IEEE doubles"
   homepage "https://github.com/google/double-conversion"
-  url "https://github.com/google/double-conversion/archive/refs/tags/v3.3.0.tar.gz"
-  sha256 "04ec44461850abbf33824da84978043b22554896b552c5fd11a9c5ae4b4d296e"
+  url "https://github.com/google/double-conversion/archive/refs/tags/v3.3.1.tar.gz"
+  sha256 "fe54901055c71302dcdc5c3ccbe265a6c191978f3761ce1414d0895d6b0ea90e"
   license "BSD-3-Clause"
   head "https://github.com/google/double-conversion.git", branch: "master"
 
