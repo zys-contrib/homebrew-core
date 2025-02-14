8ab0b00ccc8b73e50cfe273c50267c0284188788
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-14 11:48:41 +0000

    asyncapi: update 2.16.2 bottle.

--- Formula/a/asyncapi.rb
+++ Formula/a/asyncapi.rb
@@ -6,12 +6,12 @@ class Asyncapi < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "497d70ae8b6f5141c3ded08dbcbdc8941c5699193f4ddea8c27becc77ed777f2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "497d70ae8b6f5141c3ded08dbcbdc8941c5699193f4ddea8c27becc77ed777f2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "497d70ae8b6f5141c3ded08dbcbdc8941c5699193f4ddea8c27becc77ed777f2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9fdf6689d6c5fa01e79c6315bd867d181fa95e15a1ae2292b56a7b525f83ed83"
-    sha256 cellar: :any_skip_relocation, ventura:       "9fdf6689d6c5fa01e79c6315bd867d181fa95e15a1ae2292b56a7b525f83ed83"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "497d70ae8b6f5141c3ded08dbcbdc8941c5699193f4ddea8c27becc77ed777f2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "82de7ea48d83dc2a7539875926a249d03b3c79b1cd1d896cf099eefee9a8ea4c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "82de7ea48d83dc2a7539875926a249d03b3c79b1cd1d896cf099eefee9a8ea4c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "82de7ea48d83dc2a7539875926a249d03b3c79b1cd1d896cf099eefee9a8ea4c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "38083d42b679cd765d4e9e11c349e0d0edc19bd6dbeaf53ebe2a958cf57ac7c3"
+    sha256 cellar: :any_skip_relocation, ventura:       "38083d42b679cd765d4e9e11c349e0d0edc19bd6dbeaf53ebe2a958cf57ac7c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "82de7ea48d83dc2a7539875926a249d03b3c79b1cd1d896cf099eefee9a8ea4c"
   end
 
   depends_on "node"
