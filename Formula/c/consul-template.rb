31e00d41c3e7de4c0ce7546f13dfe4a337bd448b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-14 14:15:35 +0000

    consul-template: update 0.40.0 bottle.

--- Formula/c/consul-template.rb
+++ Formula/c/consul-template.rb
@@ -7,14 +7,12 @@ class ConsulTemplate < Formula
   head "https://github.com/hashicorp/consul-template.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "241086dbcd92027f8c28c0c526afaecee7083faf6769b18da3df01864f41145f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e94ae8bac9e71da4f91b25bfb301728c6a0255a92cd43838e4179857027d22e5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "89ad41bf65fc9f430b17ed8a789a36a28c870977936cc0ba9d71c68069b27baa"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e167655f9b1be88a2c2a254855fe2888b538a3f048ea94280707e17709b6cb46"
-    sha256 cellar: :any_skip_relocation, sonoma:         "7ddaac2a8089187a7a3b968c4eeb6f40a2882ffdcf7a3d09d539432dfb9948cd"
-    sha256 cellar: :any_skip_relocation, ventura:        "e89f2997a3595d91896706d6e09202028f93ae8a126b6630943317dc5eabba7c"
-    sha256 cellar: :any_skip_relocation, monterey:       "a8a53aa07b37cbf6164d0e17c617b5ca0c60c3660c9c23b9eaad23a7c907c1bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43250e2791cddb58e5b9f8a7d640e2de2a393046a9453fb7968bb866703ad353"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "368a75569525e7512f65e51d89af81a299732099f24aff6d4af0c473caadf8ad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "368a75569525e7512f65e51d89af81a299732099f24aff6d4af0c473caadf8ad"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "368a75569525e7512f65e51d89af81a299732099f24aff6d4af0c473caadf8ad"
+    sha256 cellar: :any_skip_relocation, sonoma:        "335573d7ba780228b60e9e93f01e37600c3fb62988328be963a7e24c6ce137a1"
+    sha256 cellar: :any_skip_relocation, ventura:       "335573d7ba780228b60e9e93f01e37600c3fb62988328be963a7e24c6ce137a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cca6f0ad7417f567f58295292468355109990ab7d0ffb715b09deb7b599f6ea0"
   end
 
   depends_on "go" => :build
