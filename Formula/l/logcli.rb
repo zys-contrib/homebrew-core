591c4eb2db209714811e20f0e40de1151a89032a
Rui Chen <rui@chenrui.dev>
2025-02-15 01:04:08 -0500

    logcli 3.4.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/l/logcli.rb
+++ Formula/l/logcli.rb
@@ -1,8 +1,8 @@
 class Logcli < Formula
   desc "Run LogQL queries against a Loki server"
   homepage "https://grafana.com/loki"
-  url "https://github.com/grafana/loki/archive/refs/tags/v3.4.1.tar.gz"
-  sha256 "8e496f9abc85f7d4fa05efb70fbff419bc581f342574afdb13fd3c4ec33a77bf"
+  url "https://github.com/grafana/loki/archive/refs/tags/v3.4.2.tar.gz"
+  sha256 "37572bf4d444db657d397d205f5998c200233f9f568efc5d99a2b24c3589fbe5"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/loki.git", branch: "main"
 
