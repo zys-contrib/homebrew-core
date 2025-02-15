47300082e24e5c0198e3bf03812b395b965566c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-15 09:23:40 +0000

    jd: update 2.0.2 bottle.

--- Formula/j/jd.rb
+++ Formula/j/jd.rb
@@ -6,12 +6,12 @@ class Jd < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8fa4a95de36de37c007af7f9a0ef9071c59bb4b8cf380e8424e37c33c0015e17"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8fa4a95de36de37c007af7f9a0ef9071c59bb4b8cf380e8424e37c33c0015e17"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8fa4a95de36de37c007af7f9a0ef9071c59bb4b8cf380e8424e37c33c0015e17"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0bad7b517562b8ac64cf1be5adc4590df3471171abf226e7739c70760dddf008"
-    sha256 cellar: :any_skip_relocation, ventura:       "0bad7b517562b8ac64cf1be5adc4590df3471171abf226e7739c70760dddf008"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8afe08b6d15c4610251e9cce90b56988d2ac1a577a5d213c8f34e5a0bbf3a87"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0baf5de150f1da03dc98acffa7824c38caff444c0c554ff7fc2494d2ef0265a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c0baf5de150f1da03dc98acffa7824c38caff444c0c554ff7fc2494d2ef0265a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c0baf5de150f1da03dc98acffa7824c38caff444c0c554ff7fc2494d2ef0265a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8f4651c19240462e0c7db62791b348e0b1a951c5ac7b3b6a4a74634997df227c"
+    sha256 cellar: :any_skip_relocation, ventura:       "8f4651c19240462e0c7db62791b348e0b1a951c5ac7b3b6a4a74634997df227c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "196ec37dbd6b46e247d1476cf104e9568d7ac6e2ea254937e75e357c6ab9a8ff"
   end
 
   depends_on "go" => :build
