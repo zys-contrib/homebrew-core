b21c6771fc8719318cbab4e4c66bde11cdb2d74d
Michael Cho <michael@michaelcho.dev>
2025-02-15 16:34:18 -0500

    yelp-tools: bundle `mallard-rng` to run offline
    This helps with current situation where upstream site is down
    at http://projectmallard.org/1.1/mallard-1.1.rng

--- Formula/y/yelp-tools.rb
+++ Formula/y/yelp-tools.rb
@@ -29,19 +29,25 @@ class YelpTools < Formula
   uses_from_macos "libxslt"
 
   resource "lxml" do
-    url "https://files.pythonhosted.org/packages/2b/b4/bbccb250adbee490553b6a52712c46c20ea1ba533a643f1424b27ffc6845/lxml-5.1.0.tar.gz"
-    sha256 "3eea6ed6e6c918e468e693c41ef07f3c3acc310b70ddd9cc72d9ef84bc9564ca"
+    url "https://files.pythonhosted.org/packages/ef/f6/c15ca8e5646e937c148e147244817672cf920b56ac0bf2cc1512ae674be8/lxml-5.3.1.tar.gz"
+    sha256 "106b7b5d2977b339f1e97efe2778e2ab20e99994cbb0ec5e55771ed0795920c8"
   end
 
   resource "yelp-xsl" do
-    url "https://download.gnome.org/sources/yelp-xsl/42/yelp-xsl-42.0.tar.xz"
-    sha256 "29b273cc0bd16efb6e983443803f1e9fdc03511e5c4ff6348fd30a604d4dc846"
+    url "https://download.gnome.org/sources/yelp-xsl/42/yelp-xsl-42.1.tar.xz"
+    sha256 "238be150b1653080ce139971330fd36d3a26595e0d6a040a2c030bf3d2005bcd"
+  end
+
+  resource "mallard-rng" do
+    url "https://deb.debian.org/debian/pool/main/m/mallard-rng/mallard-rng_1.1.0.orig.tar.bz2"
+    sha256 "66bc8c38758801d5a1330588589b6e81f4d7272a6fbdad0cd4cfcd266848e160"
   end
 
   def install
-    venv = virtualenv_create(libexec, "python3.13")
-    venv.pip_install resource("lxml")
-    ENV.prepend_path "PATH", venv.root/"bin"
+    resource("mallard-rng").stage do
+      system "./configure", "--disable-silent-rules", *std_configure_args(prefix: libexec)
+      system "make", "install"
+    end
 
     resource("yelp-xsl").stage do
       system "./configure", "--disable-silent-rules", *std_configure_args
@@ -49,10 +55,17 @@ class YelpTools < Formula
       ENV.append_path "PKG_CONFIG_PATH", share/"pkgconfig"
     end
 
+    venv = virtualenv_create(libexec, "python3.13")
+    venv.pip_install resource("lxml")
+    ENV.prepend_path "PATH", venv.root/"bin"
+
     system "meson", "setup", "build", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
     rewrite_shebang python_shebang_rewrite_info(venv.root/"bin/python"), *bin.children
+
+    xml_catalog_files = libexec/"etc/xml/mallard/catalog"
+    bin.env_script_all_files(libexec/"bin", XML_CATALOG_FILES: "${XML_CATALOG_FILES:-#{xml_catalog_files}}")
   end
 
   test do
