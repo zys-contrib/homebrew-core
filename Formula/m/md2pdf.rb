d54ae767018824277aa3291c081475d2d73472df
Rui Chen <rui@chenrui.dev>
2025-02-16 16:09:44 +0000

    md2pdf: add 2.2.11 bottle.

--- Formula/m/md2pdf.rb
+++ Formula/m/md2pdf.rb
@@ -6,6 +6,15 @@ class Md2pdf < Formula
   license "MIT"
   head "https://github.com/solworktech/mdtopdf.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4791b2ba497858777f98bd53c5e4d3ca87f34774797c2f88926d49a1c9f8f0d2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4791b2ba497858777f98bd53c5e4d3ca87f34774797c2f88926d49a1c9f8f0d2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4791b2ba497858777f98bd53c5e4d3ca87f34774797c2f88926d49a1c9f8f0d2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0a788862de77d806bd6fb147360d3e9e6dd228805a65eb05b99e31a12002716e"
+    sha256 cellar: :any_skip_relocation, ventura:       "0a788862de77d806bd6fb147360d3e9e6dd228805a65eb05b99e31a12002716e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e543d38d3490456b19bbecca945c9afafe7b61f7421f8c599cee8c0c0ac97a4"
+  end
+
   depends_on "go" => :build
 
   def install
