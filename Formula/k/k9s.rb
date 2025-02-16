812f5af06f1ef39ac282a0a31cf45768f5fe0a99
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-16 19:58:48 +0000

    k9s 0.40.1

--- Formula/k/k9s.rb
+++ Formula/k/k9s.rb
@@ -2,8 +2,8 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.40.0",
-      revision: "35c2b6c509a3c4cb0d6aabc94e4c13eb53ef740d"
+      tag:      "v0.40.1",
+      revision: "61f0f80c4a5f31ba5b7ad3cd98ef622d5aba8edd"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git", branch: "master"
 
