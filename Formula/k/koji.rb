2ada12fdb8c21f3290059d1695341629a90bd5f1
Patrick Linnane <patrick@linnane.io>
2025-02-16 05:44:58 +0000

    koji: add 3.2.0 bottle.

--- Formula/k/koji.rb
+++ Formula/k/koji.rb
@@ -6,6 +6,15 @@ class Koji < Formula
   license "MIT"
   head "https://github.com/cococonscious/koji.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "6ad589d2ccaf89bbc8e56b42cc1ea81757a914c29447f77f6862ef84f3a5a064"
+    sha256 cellar: :any,                 arm64_sonoma:  "5655e3a07a057391e84ed1fbfabc7887d6d37d511581c59e5b9c49b6bff3ec93"
+    sha256 cellar: :any,                 arm64_ventura: "9fd26b1e044fb57e2d8ef0d7bbe1ea57aa7ab8b124277bd89adbfc67f7cdd467"
+    sha256 cellar: :any,                 sonoma:        "b4b2066c3785fb96b1fc5f280a586a553ba3af079b6536020b4b7ba61073adaa"
+    sha256 cellar: :any,                 ventura:       "cdbc54030a73304779b4f7456a7d51bbcfa257cf818043eb63c09171f98f238b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3439dfc748254b2ca5f525304e3d329899829bd11a1cfe7593d2a45e38ac4ccd"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
