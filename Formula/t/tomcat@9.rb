f7300a2b8d8bb8f96b53632f45b598d59513d151
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 14:11:43 +0000

    tomcat@9 9.0.100

--- Formula/t/tomcat@9.rb
+++ Formula/t/tomcat@9.rb
@@ -1,9 +1,9 @@
 class TomcatAT9 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.99/bin/apache-tomcat-9.0.99.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.99/bin/apache-tomcat-9.0.99.tar.gz"
-  sha256 "ee988aaa119e0eeef13ee0a46ff5aaa82536f6f5f76bea75157f4eac92a45d00"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.100/bin/apache-tomcat-9.0.100.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.100/bin/apache-tomcat-9.0.100.tar.gz"
+  sha256 "1253158c45a31a8168b41436512acc2292e94a3468b1bd8e4d0a7a79dcc1f5ff"
   license "Apache-2.0"
 
   livecheck do
