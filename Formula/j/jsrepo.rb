23b2b377640ac5f7aac61ef626589d47240c1cd6
Rui Chen <rui@chenrui.dev>
2025-02-17 02:27:04 +0000

    jsrepo: add 1.36.0 bottle.

--- Formula/j/jsrepo.rb
+++ Formula/j/jsrepo.rb
@@ -5,6 +5,15 @@ class Jsrepo < Formula
   sha256 "780aae81814e5247809f5df7746450bfea7c8e45c6f12cabf3f54f70feff033a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "98f22e15fb503b9935a7701b8f55deb8694d173a32d635f6c4dce25ffe044b73"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "98f22e15fb503b9935a7701b8f55deb8694d173a32d635f6c4dce25ffe044b73"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "98f22e15fb503b9935a7701b8f55deb8694d173a32d635f6c4dce25ffe044b73"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6cf39e78596b9612ccfeebc673fe8ff0a2bc24643379b84e7fd541e30f7cd5b3"
+    sha256 cellar: :any_skip_relocation, ventura:       "6cf39e78596b9612ccfeebc673fe8ff0a2bc24643379b84e7fd541e30f7cd5b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "98f22e15fb503b9935a7701b8f55deb8694d173a32d635f6c4dce25ffe044b73"
+  end
+
   depends_on "node"
 
   def install
