9ff2daa8d2a957757366125227b443764d51e840
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 02:12:38 +0000

    aliyun-cli 3.0.253

--- Formula/a/aliyun-cli.rb
+++ Formula/a/aliyun-cli.rb
@@ -2,8 +2,8 @@ class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
   url "https://github.com/aliyun/aliyun-cli.git",
-      tag:      "v3.0.252",
-      revision: "59581ab6074b35c5ce6b6febd755d31243b16649"
+      tag:      "v3.0.253",
+      revision: "95ab966bd1c8484f27167cb1012c5e4d5ad55c58"
   license "Apache-2.0"
 
   bottle do
