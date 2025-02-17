dbc2c477816b65869fe66a9c5d35b09bdf439d8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 20:09:40 +0000

    quilt-installer 0.11.3

--- Formula/q/quilt-installer.rb
+++ Formula/q/quilt-installer.rb
@@ -1,8 +1,8 @@
 class QuiltInstaller < Formula
   desc "Installer for Quilt for the vanilla launcher"
   homepage "https://quiltmc.org/"
-  url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.11.2/quilt-installer-0.11.2.jar"
-  sha256 "f6a596ce6857c04e950fb84508f3a8e56e546b6a4a5d8727d878f76af047a41f"
+  url "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.11.3/quilt-installer-0.11.3.jar"
+  sha256 "58e209b370a3889be3049895012abf69077ccd82064b187da2bc3026783510b3"
   license "Apache-2.0"
 
   livecheck do
