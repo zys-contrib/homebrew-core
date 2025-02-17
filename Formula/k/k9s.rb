c6ec7d7264dac314e7a66f5f5e5c6c33a1849c05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 02:41:40 +0000

    k9s: update 0.40.2 bottle.

--- Formula/k/k9s.rb
+++ Formula/k/k9s.rb
@@ -13,12 +13,12 @@ class K9s < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a26eb472637caaf727e3afe0de2ae3efc3199d129aeafb88de033883818c71af"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fc9093e163a61ff99b6752f2077e3cd176237bb98f2d49e8753427459bd03784"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5917f493473359ae4a7e90dc2909f99d80368062b387977e122828ba2401a1dc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "11426b4ad3ea2ccac5088611a1940143dcce6ad61de82a0fd02c6c2daf58b48d"
-    sha256 cellar: :any_skip_relocation, ventura:       "8bbe3da12dce831f4da1af863e88b119f58291bbfeac64d674d1ecf5b925af09"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "521703a767017be1d50fc6b2b1d58eaf87bf3281ddb478769c2eca2f851b2e24"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "82d2a29ec2b4cfd18489901696c4bbf56e817ed4a1a55afba7c2edad6429616f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b8cc70a8d37d92f29e864e9b9eaabfff38eb42367bfd2feae38bda5a50bc878"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1ebbb80d09d48d72ef087fca13df763ebae5272081aae0cbfb5f9d93b6f635fc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aa35fbb06c523b5535c0372e6deac51fada84284776c8aeb99f8bf94edfdfc01"
+    sha256 cellar: :any_skip_relocation, ventura:       "6c48e780bbe7652a832e2199defaa24dd3bfd9134592ceb9b1a22b27da5ccf22"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a80582edc00b08a01dce6805c7ab44997ef96e7fafcb2198947f7aa5f067be38"
   end
 
   depends_on "go" => :build
