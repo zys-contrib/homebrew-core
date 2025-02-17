2ef4437e62b8bbe55c0fc0c79c8da6d99220767f
Rui Chen <rui@chenrui.dev>
2025-02-17 01:43:10 +0000

    gowall: add 0.2.0 bottle.

--- Formula/g/gowall.rb
+++ Formula/g/gowall.rb
@@ -6,6 +6,15 @@ class Gowall < Formula
   license "MIT"
   head "https://github.com/Achno/gowall.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34dc908f5f3d79cbec77054867e82ef3df922edd34d5911f4682962b8554ac28"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "39ec1eb39a2b1c802bd77fe3cd726749e958387affdc9aafc1df9d3632a15c08"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "05ac7c037fe03766da5ddbd174b7269cd15a3776e8ea24a59a4867a136e79be0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "253286c45351373ed0553a5b7112a66d0357ba19ac7f10df563580dce782cfb1"
+    sha256 cellar: :any_skip_relocation, ventura:       "e1eaca5511b100652751f38a3aad0d706dc2b4c1c68bc43b6a14493fb34cb9ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d366964c9da1cde599496cf92ef070bdffb9cbf1e71c96c1dedffe02af05e916"
+  end
+
   depends_on "go" => :build
 
   def install
