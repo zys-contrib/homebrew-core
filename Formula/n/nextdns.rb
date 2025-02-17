105a9d4e7a728663277b82bee26efecd86010131
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 23:40:01 +0000

    nextdns: update 1.45.0 bottle.

--- Formula/n/nextdns.rb
+++ Formula/n/nextdns.rb
@@ -7,12 +7,12 @@ class Nextdns < Formula
   head "https://github.com/nextdns/nextdns.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "12c9e9a5456452f673919b66d6af187af345fe3e732e808dbe90cfb23e338660"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4bf866c3b162efb5dafdd123396da2c98bd4e11b800ec78a67af46765e1dfb02"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "723e151d10bad60c7eb5b0121cefd7fc8ab7d9b429cbe58ac015654a6b1ca958"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e56f0c75689b84b6deb79c210779ec544faf9476ccb20220baa29865adb5616d"
-    sha256 cellar: :any_skip_relocation, ventura:       "37f04be89c69bc3acc359b2353a2b4f2f0a5d07a4fdf39782bbb6a3b8381a5aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f5bff941057570f209d9f3ffff8aa44f777d4d0c6c0e86ba774a11b55f07bb80"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c22e8b27dddf03f2d284ad7e2c52253af0b2fcdf99e659b0412aca0abb507cf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6c8437e377626604b3c60ecf0a93942276d7981e18402ed0312d2da7ffde3b06"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4de219fb06a06fa4fb45d7623bda26345d7f1ca91d8a963b3747c805edd034f0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4de99ef7a9206a2adbdc0c1d91c0c1614338a3ace18700ac75c127725c836873"
+    sha256 cellar: :any_skip_relocation, ventura:       "78f243bdbcae80d65405ac24eb110f31b820401b141491ecf18c3a7c5e8dffb2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ca408d706026e9cb54aae6f6b0ade858d3f21c26f35da8522df5cc942c307975"
   end
 
   depends_on "go" => :build
