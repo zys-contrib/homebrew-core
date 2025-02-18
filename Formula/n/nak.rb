8d438f6cbdf5f0fd1976d29153bc84680da9cdad
Rui Chen <rui@chenrui.dev>
2025-02-18 00:09:45 -0500

    nak 0.11.4 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/n/nak.rb
@@ -0,0 +1,20 @@
+class Nak < Formula
+  desc "CLI for doing all things nostr"
+  homepage "https://github.com/fiatjaf/nak"
+  url "https://github.com/fiatjaf/nak/archive/refs/tags/v0.11.4.tar.gz"
+  sha256 "b2fa16c3582b3fdefc909adf46fd227f840c9430c606581087c6c8a0c877d2f2"
+  license "Unlicense"
+  head "https://github.com/fiatjaf/nak.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nak --version")
+    assert_match "hello from the nostr army knife", shell_output("#{bin}/nak event")
+    assert_match "\"method\":\"listblockedips\"", shell_output("#{bin}/nak relay listblockedips")
+  end
+end
