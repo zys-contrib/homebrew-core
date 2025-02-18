9dd4815d35b34f81733fffccb6e2ebd3ac97370d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-18 17:04:44 +0000

    libassuan 3.0.2

--- Formula/lib/libassuan.rb
+++ Formula/lib/libassuan.rb
@@ -3,9 +3,9 @@ class Libassuan < Formula
   homepage "https://www.gnupg.org/related_software/libassuan/"
   # TODO: On next release, check if `-std=gnu89` workaround can be removed.
   # Ref: https://dev.gnupg.org/T7246
-  url "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-3.0.1.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-3.0.1.tar.bz2"
-  sha256 "c8f0f42e6103dea4b1a6a483cb556654e97302c7465308f58363778f95f194b1"
+  url "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-3.0.2.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-3.0.2.tar.bz2"
+  sha256 "d2931cdad266e633510f9970e1a2f346055e351bb19f9b78912475b8074c36f6"
   license all_of: [
     "LGPL-2.1-or-later",
     "GPL-3.0-or-later", # assuan.info
