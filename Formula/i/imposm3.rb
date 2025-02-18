061eb7a1ff1372d840e6aa050ea827ae849932f3
Rui Chen <rui@chenrui.dev>
2025-02-18 15:47:51 -0500

    imposm3: update homepage
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/imposm3.rb
+++ Formula/i/imposm3.rb
@@ -1,6 +1,6 @@
 class Imposm3 < Formula
   desc "Imports OpenStreetMap data into PostgreSQL/PostGIS databases"
-  homepage "https://imposm.org"
+  homepage "https://imposm.org/docs/imposm3/latest/"
   url "https://github.com/omniscale/imposm3/archive/refs/tags/v0.14.0.tar.gz"
   sha256 "d6b012497eff1b8faa25d125ce0becb97f68c95a68dd2c35cf65a0bf3c34b833"
   license "Apache-2.0"
