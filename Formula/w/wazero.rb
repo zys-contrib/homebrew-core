95813ecb56db96d2788e6776fcab60f2c50fa986
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-18 17:18:11 +0000

    wazero 1.9.0

--- Formula/w/wazero.rb
+++ Formula/w/wazero.rb
@@ -1,8 +1,8 @@
 class Wazero < Formula
   desc "Zero dependency WebAssembly runtime"
   homepage "https://wazero.io"
-  url "https://github.com/tetratelabs/wazero/archive/refs/tags/v1.8.2.tar.gz"
-  sha256 "dd6dca5ab206a1b6031ed977922eed225814bc5452792799e106636b25444333"
+  url "https://github.com/tetratelabs/wazero/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "b294ef32baf69cb7ef9be85ecdfa13b0e59a4bac2f1ad58ec21231e7e0d592e0"
   license "Apache-2.0"
 
   livecheck do
