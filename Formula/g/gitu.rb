f9bd5e655baafe0ac9b608195b7cd23a5d4956f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-19 20:58:32 +0000

    gitu: update 0.28.2 bottle.

--- Formula/g/gitu.rb
+++ Formula/g/gitu.rb
@@ -7,12 +7,12 @@ class Gitu < Formula
   head "https://github.com/altsem/gitu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35442e59f8843d4c7a467e9167bcaafe66d0f8335087fb89e189e01ed7061ca8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0da0eba3134f9b7c5ee79296416938bb317245478d569515c062945e91b380c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "53f72762177b1f64fa1f99b1e7975916db94c3ac8859a374d5925d7df7382b19"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5ec03a97f248ed4e4ba38f9a2e194829989eae27b3e2215f21c4ab8bc1799369"
-    sha256 cellar: :any_skip_relocation, ventura:       "8975563169f41d8a4087371cd8a034ebafce0840b3b01b1ecad52be35c89dc93"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bfa9ad63bb4bc50c36ec712ffb61d591576b09a31d8f2f669b10ef236565c393"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d1a661359988c38166a22b5a1cd2f372e65acc6e2fd81fdd334d8338496847b1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "742d966fa2247083051fa8ef4df3d0ee91de80a38f4bf4b40fcc0f8bae76eed5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7ab2aab5e9ee419f64ee102236a0380a41678ee545bc765402f8996d54937f8b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e640a1bc3d4841ed5c3e083fb30ef01144b9cd8b0b9a1313c79ac45b02e5ad19"
+    sha256 cellar: :any_skip_relocation, ventura:       "d2c113b919890dcbadbb16515e31a7b7fccb1cfc5203e32caae7c8f1ed9749fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa5c0ef5e9ae82a2b3d8e2b8833183bc52a687aca8f4379c9dfe1c70b3aa01ac"
   end
 
   depends_on "rust" => :build
