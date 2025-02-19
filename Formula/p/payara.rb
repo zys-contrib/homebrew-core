00b15917f3ede71e0fc1c95e2927971c68eee674
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-19 11:10:00 +0000

    payara 6.2025.2

--- Formula/p/payara.rb
+++ Formula/p/payara.rb
@@ -1,8 +1,8 @@
 class Payara < Formula
   desc "Java EE application server forked from GlassFish"
   homepage "https://www.payara.fish"
-  url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/6.2025.1/payara-6.2025.1.zip"
-  sha256 "72f20bb8bb087f1d126577aa619f1ee15a156cc1eb287803941cb21d3cb41c23"
+  url "https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara/6.2025.2/payara-6.2025.2.zip"
+  sha256 "c06edc1f39903c874decf9615ef641ea18d3f7969d38927c282885960d5ee796"
   license any_of: [
     "CDDL-1.1",
     { "GPL-2.0-only" => { with: "Classpath-exception-2.0" } },
