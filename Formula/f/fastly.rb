affa9c789ccb51fbd872fa571eaeb004503698b5
Rui Chen <rui@chenrui.dev>
2025-02-18 19:09:39 -0500

    fastly 10.19.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/fastly.rb
@@ -0,0 +1,38 @@
+class Fastly < Formula
+  desc "Build, deploy and configure Fastly services"
+  homepage "https://www.fastly.com/documentation/reference/cli/"
+  url "https://github.com/fastly/cli/archive/refs/tags/v10.19.0.tar.gz"
+  sha256 "0f9f256c3c3636cd5f19ac79c6b140f4bdc26cf6320ba919479c4fa55335e8d6"
+  license "Apache-2.0"
+  head "https://github.com/fastly/cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    mv ".fastly/config.toml", "pkg/config/config.toml"
+
+    os = Utils.safe_popen_read("go", "env", "GOOS").strip
+    arch = Utils.safe_popen_read("go", "env", "GOARCH").strip
+
+    ldflags = %W[
+      -s -w
+      -X github.com/fastly/cli/pkg/revision.AppVersion=v#{version}
+      -X github.com/fastly/cli/pkg/revision.GitCommit=#{tap.user}
+      -X github.com/fastly/cli/pkg/revision.GoHostOS=#{os}
+      -X github.com/fastly/cli/pkg/revision.GoHostArch=#{arch}
+      -X github.com/fastly/cli/pkg/revision.Environment=release
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "./cmd/fastly"
+
+    generate_completions_from_executable(bin/"fastly", shell_parameter_format: "--completion-script-",
+                                                       shells:                 [:bash, :zsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fastly version")
+
+    output = shell_output("#{bin}/fastly service list 2>&1", 1)
+    assert_match "Fastly API returned 401 Unauthorized", output
+  end
+end
