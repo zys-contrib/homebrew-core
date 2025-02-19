7d86e1ae9d0ee1b88b4d9de8544b66c49f46a8c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-19 17:58:52 +0000

    openfga: update 1.8.5 bottle.

--- Formula/o/openfga.rb
+++ Formula/o/openfga.rb
@@ -7,12 +7,12 @@ class Openfga < Formula
   head "https://github.com/openfga/openfga.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0d3b6b143e83c324b654f969aac59b94df403c0f31dc7569fbdbd1960ab0c07"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d0d3b6b143e83c324b654f969aac59b94df403c0f31dc7569fbdbd1960ab0c07"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d0d3b6b143e83c324b654f969aac59b94df403c0f31dc7569fbdbd1960ab0c07"
-    sha256 cellar: :any_skip_relocation, sonoma:        "16255f253c9302b4dcbf5491351d2ffccbb013d3a5e74ec349398c2a575c10ea"
-    sha256 cellar: :any_skip_relocation, ventura:       "16255f253c9302b4dcbf5491351d2ffccbb013d3a5e74ec349398c2a575c10ea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2cbe45b512d0aa41f92b2ccbd0b63fb598ae5851aea142e19501f9e39383e54"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "43a22a7b3bbbec3b277c6edb919a6b95d49210b02769b312a589eb9671f1d838"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43a22a7b3bbbec3b277c6edb919a6b95d49210b02769b312a589eb9671f1d838"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "43a22a7b3bbbec3b277c6edb919a6b95d49210b02769b312a589eb9671f1d838"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f1eb41a60d437fdb5da7e9694a93e159b280d68eb9e021c377806ebd672c6af5"
+    sha256 cellar: :any_skip_relocation, ventura:       "f1eb41a60d437fdb5da7e9694a93e159b280d68eb9e021c377806ebd672c6af5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c4d85792a08331b2edd6e3a8096967e20a6756b1b5e2bdfb99f4db8a184dcce"
   end
 
   depends_on "go" => :build
