193c02c3e37cc968d96cded1810efe38d2f4ba2b
Branch Vincent <branchevincent@gmail.com>
2025-02-18 19:32:00 -0800

    vue-language-server: link all binaries

--- Formula/v/vue-language-server.rb
+++ Formula/v/vue-language-server.rb
@@ -13,7 +13,7 @@ class VueLanguageServer < Formula
 
   def install
     system "npm", "install", *std_npm_args
-    bin.install_symlink libexec/"bin/vue-language-server"
+    bin.install_symlink libexec.glob("bin/*")
   end
 
   test do
