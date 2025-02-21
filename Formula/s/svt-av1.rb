e121c6716d087a5b1acce9b63118ab0289216eb2
Caleb Xu <calebcenter@live.com>
2025-02-20 22:08:39 -0500

    svt-av1 3.0.0

--- Formula/s/svt-av1.rb
+++ Formula/s/svt-av1.rb
@@ -1,10 +1,9 @@
 class SvtAv1 < Formula
   desc "AV1 encoder"
   homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
-  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v2.3.0/SVT-AV1-v2.3.0.tar.bz2"
-  sha256 "f65358499f572a47d6b076dda73681a8162b02c0b619a551bc2d62ead8ee719a"
+  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v3.0.0/SVT-AV1-v3.0.0.tar.bz2"
+  sha256 "852d3be2cea244dc76747a948dfcffb82d42dc42e1bd86830e591ea29b91c4fd"
   license "BSD-3-Clause"
-  revision 1
   head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
 
   bottle do
@@ -19,13 +18,42 @@ class SvtAv1 < Formula
   depends_on "cmake" => :build
   depends_on "nasm" => :build
 
+  # Match the version of cpuinfo specified in https://gitlab.com/AOMediaCodec/SVT-AV1/-/blob/master/cmake/cpuinfo.cmake
+  resource "cpuinfo" do
+    url "https://github.com/1480c1/cpuinfo/archive/e649baaa95efeb61517c06cc783287d4942ffe0e.tar.gz"
+    sha256 "f89abf172b93d75a79a5456fa778a401ab2fc4ef84d538f5c4df7c6938591c6f"
+  end
+
   def install
     # Features are enabled based on compiler support, and then the appropriate
     # implementations are chosen at runtime.
     # See https://gitlab.com/AOMediaCodec/SVT-AV1/-/blob/master/Source/Lib/Codec/common_dsp_rtcd.c
     ENV.runtime_cpu_detection
 
-    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
+    (buildpath/"cpuinfo").install resource("cpuinfo")
+
+    cd "cpuinfo" do
+      args = %W[
+        -DCPUINFO_BUILD_TOOLS=OFF
+        -DCPUINFO_BUILD_UNIT_TESTS=OFF
+        -DCPUINFO_BUILD_MOCK_TESTS=OFF
+        -DCPUINFO_BUILD_BENCHMARKS=OFF
+        -DCMAKE_INSTALL_PREFIX=#{buildpath}/cpuinfo-install
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+      ] + std_cmake_args.reject { |arg| arg.start_with? "-DCMAKE_INSTALL_PREFIX=" }
+
+      system "cmake", "-S", ".", "-B", "cpuinfo-build", *args
+      system "cmake", "--build", "cpuinfo-build"
+      system "cmake", "--install", "cpuinfo-build"
+    end
+
+    args = %W[
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DUSE_CPUINFO=SYSTEM
+      -Dcpuinfo_DIR=#{buildpath/"cpuinfo-install/share/cpuinfo"}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
