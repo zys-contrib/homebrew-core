b28f21081bb6ef26c0554c425092e8238adc80a1
Daeho Ro <lamanus@outlook.kr>
2025-02-21 18:05:47 +0900

    cloudflare-wrangler 3.109.2 (new formula)

---
+++ Formula/c/cloudflare-wrangler.rb
@@ -0,0 +1,19 @@
+class CloudflareWrangler < Formula
+  desc "CLI tool for Cloudflare Workers"
+  homepage "https://github.com/cloudflare/workers-sdk"
+  url "https://registry.npmjs.org/wrangler/-/wrangler-3.109.2.tgz"
+  sha256 "46933854f9964f912e34b1023556ab8eebcc360cd178e9c92d57e0fb3627a4c1"
+  license any_of: ["Apache-2.0", "MIT"]
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/wrangler*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/wrangler -v")
+    assert_match "Required Worker name missing", shell_output("#{bin}/wrangler secret list 2>&1", 1)
+  end
+end
