243cbffc2a6acf6fd21fcc409305390e1b280f2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-21 11:10:47 +0000

    texttest 4.4.3

--- Formula/t/texttest.rb
+++ Formula/t/texttest.rb
@@ -3,8 +3,8 @@ class Texttest < Formula
 
   desc "Tool for text-based Approval Testing"
   homepage "https://www.texttest.org/"
-  url "https://files.pythonhosted.org/packages/0b/e0/4c6eb823436d221ccfeec099c228ee5c477483693fa167f0c70a3b55e5e4/texttest-4.4.2.tar.gz"
-  sha256 "144c9ac050e836ef1aa7a5668519640cd449b8b7031513348d5fcbb9f6623952"
+  url "https://files.pythonhosted.org/packages/83/34/d32d805a55b80cbc9fa1b4892b52160126b5106c4aab6cb8c6e2cd82481c/texttest-4.4.3.tar.gz"
+  sha256 "41b41f5f222764f2c915b388c43eac5aebcf45c6e6c4a59c83e6a4e5202b73bb"
   license "LGPL-2.1-or-later"
 
   bottle do
@@ -22,8 +22,8 @@ class Texttest < Formula
   depends_on "python@3.13"
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/1f/5a/07871137bb752428aa4b659f910b399ba6f291156bdea939be3e96cae7cb/psutil-6.1.1.tar.gz"
-    sha256 "cf8496728c18f2d0b45198f06895be52f36611711746b7f30c464b422b50e2f5"
+    url "https://files.pythonhosted.org/packages/2a/80/336820c1ad9286a4ded7e845b2eccfcb27851ab8ac6abece774a6ff4d3de/psutil-7.0.0.tar.gz"
+    sha256 "7be9c3eba38beccb6495ea33afd982a44074b78f28c434a1f51cc07fd315c456"
   end
 
   def install
