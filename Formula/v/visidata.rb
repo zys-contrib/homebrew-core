35a924a3313723d211eace58c2f98d7a918e2dda
Rui Chen <rui@chenrui.dev>
2025-02-21 14:24:36 +0000

    visidata: add 3.1.1 bottle.

--- Formula/v/visidata.rb
+++ Formula/v/visidata.rb
@@ -7,6 +7,15 @@ class Visidata < Formula
   sha256 "8c4484158f0851e4887e595542bd65bdd991de9c20652b442b3fd9742ce8b031"
   license "GPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c59340511e66537da6a9d30fc32913ab638cde115e0f9ebcf39c3a60e9251734"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c59340511e66537da6a9d30fc32913ab638cde115e0f9ebcf39c3a60e9251734"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c59340511e66537da6a9d30fc32913ab638cde115e0f9ebcf39c3a60e9251734"
+    sha256 cellar: :any_skip_relocation, sonoma:        "386ca2b4afbaf1f0e79ee848379fd2d11b023134dcb58e2dd95d259e62b02706"
+    sha256 cellar: :any_skip_relocation, ventura:       "386ca2b4afbaf1f0e79ee848379fd2d11b023134dcb58e2dd95d259e62b02706"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c59340511e66537da6a9d30fc32913ab638cde115e0f9ebcf39c3a60e9251734"
+  end
+
   depends_on "python@3.13"
 
   resource "importlib-metadata" do
