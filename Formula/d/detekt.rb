f0e7a2c7f65f89cd9da7ea15a83d7e8bbe5564fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-21 16:57:05 +0000

    detekt 1.23.8

--- Formula/d/detekt.rb
+++ Formula/d/detekt.rb
@@ -1,8 +1,8 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/detekt/detekt"
-  url "https://github.com/detekt/detekt/releases/download/v1.23.7/detekt-cli-1.23.7-all.jar"
-  sha256 "84beded283012cb2b38bcaef4996452fcd6069d2e9ca74b50eaa79e0ad21897e"
+  url "https://github.com/detekt/detekt/releases/download/v1.23.8/detekt-cli-1.23.8-all.jar"
+  sha256 "2ce2ff952e150baf28a29cda70a363b0340b3e81a55f43e51ec5edffc3d066c1"
   license "Apache-2.0"
 
   livecheck do
