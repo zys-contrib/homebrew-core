7c10e0ff6b255800235b7897cd5ff93f293b64dc
Rui Chen <rui@chenrui.dev>
2025-02-21 14:24:41 +0000

    dyff: add 1.9.4 bottle.

--- Formula/d/dyff.rb
+++ Formula/d/dyff.rb
@@ -6,6 +6,15 @@ class Dyff < Formula
   license "MIT"
   head "https://github.com/homeport/dyff.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0bd2bafc8986b7ec08c50a975015c8a158a32936492dc47d28fa5c5074cdd4eb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0bd2bafc8986b7ec08c50a975015c8a158a32936492dc47d28fa5c5074cdd4eb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0bd2bafc8986b7ec08c50a975015c8a158a32936492dc47d28fa5c5074cdd4eb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "033161a99e49c24b6a1bea9ef77b97a7e10216af324d125535720b2373b502a1"
+    sha256 cellar: :any_skip_relocation, ventura:       "033161a99e49c24b6a1bea9ef77b97a7e10216af324d125535720b2373b502a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "94019386c95755c6017eb2e968a7f0b2e9c9676a3431f34f295702e512b2606c"
+  end
+
   depends_on "go" => :build
 
   def install
