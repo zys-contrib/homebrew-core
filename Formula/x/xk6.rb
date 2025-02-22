51f9ca649531734de8ed3ad0e0f39c08f5e4f8d7
Rui Chen <rui@chenrui.dev>
2025-02-22 22:03:29 +0000

    xk6: add 0.14.3 bottle.

--- Formula/x/xk6.rb
+++ Formula/x/xk6.rb
@@ -6,6 +6,15 @@ class Xk6 < Formula
   license "Apache-2.0"
   head "https://github.com/grafana/xk6.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2560adfdcd7e7a89a49a29444c369a2d16f60057a8d6b224f19bf2e8686b1147"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2560adfdcd7e7a89a49a29444c369a2d16f60057a8d6b224f19bf2e8686b1147"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2560adfdcd7e7a89a49a29444c369a2d16f60057a8d6b224f19bf2e8686b1147"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bb7ff607339030f947cf90c8d58a2dc786831e7b94d22bab730698c2d5887f3e"
+    sha256 cellar: :any_skip_relocation, ventura:       "bb7ff607339030f947cf90c8d58a2dc786831e7b94d22bab730698c2d5887f3e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55ad8aa5decacad6423bd22b41e20c2a350307807db7ffa62b0d1fcdcd82359d"
+  end
+
   depends_on "go"
 
   def install
