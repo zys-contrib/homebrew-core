9a4e7c0b7bcf7c04ce416861f2a88563f3f7040f
printfn <printfn@users.noreply.github.com>
2025-02-23 05:14:51 +0000

    exiftool: add brotli support

--- Formula/e/exiftool.rb
+++ Formula/e/exiftool.rb
@@ -22,15 +22,90 @@ class Exiftool < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d55bfc592867a88920fdb6687a5d6b83c4b842a7a4f09d01490c7156641cb245"
   end
 
+  depends_on "cmake" => :build
+
   uses_from_macos "perl"
 
-  def install
-    # replace the hard-coded path to the lib directory
-    inreplace "exiftool", "unshift @INC, $incDir;", "unshift @INC, \"#{libexec}/lib\";"
+  resource "FFI::CheckLib" do
+    url "https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/FFI-CheckLib-0.31.tar.gz"
+    sha256 "04d885fc377d44896e5ea1c4ec310f979bb04f2f18658a7e7a4d509f7e80bb80"
+  end
 
-    system "perl", "Makefile.PL"
-    system "make", "all"
-    libexec.install "lib"
+  resource "File::Which" do
+    url "https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/File-Which-1.27.tar.gz"
+    sha256 "3201f1a60e3f16484082e6045c896842261fc345de9fb2e620fd2a2c7af3a93a"
+  end
+
+  resource "Capture::Tiny" do
+    url "https://cpan.metacpan.org/authors/id/D/DA/DAGOLDEN/Capture-Tiny-0.48.tar.gz"
+    sha256 "6c23113e87bad393308c90a207013e505f659274736638d8c79bac9c67cc3e19"
+  end
+
+  resource "File::chdir" do
+    url "https://cpan.metacpan.org/authors/id/D/DA/DAGOLDEN/File-chdir-0.1011.tar.gz"
+    sha256 "31ebf912df48d5d681def74b9880d78b1f3aca4351a0ed1fe3570b8e03af6c79"
+  end
+
+  resource "Path::Tiny" do
+    url "https://cpan.metacpan.org/authors/id/D/DA/DAGOLDEN/Path-Tiny-0.144.tar.gz"
+    sha256 "f6ea094ece845c952a02c2789332579354de8d410a707f9b7045bd241206487d"
+  end
+
+  resource "Alien::Build" do
+    url "https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/Alien-Build-2.84.tar.gz"
+    sha256 "8e891fd3acbac39dd8fdc01376b9abff931e625be41e0910ca30ad59363b4477"
+  end
+
+  resource "Mozilla::CA" do
+    url "https://cpan.metacpan.org/authors/id/L/LW/LWP/Mozilla-CA-20250202.tar.gz"
+    sha256 "32d43ce8cb3b201813898f0c4c593a08df350c1e47484e043fc8adebbda60dbf"
+  end
+
+  resource "Sort::Versions" do
+    url "https://cpan.metacpan.org/authors/id/N/NE/NEILB/Sort-Versions-1.62.tar.gz"
+    sha256 "bf5f3307406ebe2581237f025982e8c84f6f6625dd774e457c03f8994efd2eaa"
+  end
+
+  resource "Alien::cmake3" do
+    url "https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/Alien-cmake3-0.08.tar.gz"
+    sha256 "93dfb1146f0053ec1ed59558f5f6d8f85d87b822a8433c6485d419c4f0182f1f"
+  end
+
+  resource "File::Slurper" do
+    url "https://cpan.metacpan.org/authors/id/L/LE/LEONT/File-Slurper-0.014.tar.gz"
+    sha256 "d5a36487339888c3cd758e648160ee1d70eb4153cacbaff57846dbcefb344b0c"
+  end
+
+  resource "IO::Compress::Brotli" do
+    url "https://cpan.metacpan.org/authors/id/T/TI/TIMLEGGE/IO-Compress-Brotli-0.019.tar.gz"
+    sha256 "37f40dd7cee44acea26f2f763a773e61d4ec223305ddeeca4612443cbf288fbf"
+  end
+
+  def install
+    perl_lib = libexec/"lib/perl5"
+    ENV.prepend_create_path "PERL5LIB", perl_lib
+
+    resources.each do |r|
+      r.stage do
+        system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+        if r.name == "IO::Compress::Brotli"
+          ENV.deparallelize { system "make", "install" }
+        else
+          system "make", "install"
+        end
+      end
+    end
+
+    system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+    system "make", "install"
+
+    # replace the hard-coded path to the lib directory
+    inreplace "exiftool", "unshift @INC, $incDir;", <<~EOS
+      use Config;
+      unshift @INC, "#{perl_lib}";
+      unshift @INC, "#{perl_lib}/$Config{archname}";
+    EOS
+
     bin.install "exiftool"
     doc.install Dir["html/*"]
     man1.install "blib/man1/exiftool.1"
@@ -41,5 +116,15 @@ class Exiftool < Formula
     test_image = test_fixtures("test.jpg")
     assert_match %r{MIME Type\s+: image/jpeg},
                  shell_output("#{bin}/exiftool #{test_image}")
+
+    resource "sunset-logo-jxl" do
+      url "https://github.com/libjxl/conformance/blob/5399ecf01e50ec5230912aa2df82286dc1c379c9/testcases/sunset_logo/input.jxl?raw=true"
+      sha256 "6617480923e1fdef555e165a1e7df9ca648068dd0bdbc41a22c0e4213392d834"
+    end
+
+    resource("sunset-logo-jxl").stage do
+      system bin/"exiftool", "-api", "Compress=1", "-Artist=homebrew", "-m", "input.jxl"
+      assert_match "BrotliEXIF", shell_output("#{bin}/exiftool -verbose input.jxl")
+    end
   end
 end
