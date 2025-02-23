564dabda4fd778197bdd61529b47d0d2caff4c58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-23 23:24:12 +0000

    asymptote: update 3.01 bottle.

--- Formula/a/asymptote.rb
+++ Formula/a/asymptote.rb
@@ -12,12 +12,12 @@ class Asymptote < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "bfdecf42d170d12d5c1feb127c0b79b0abdbf0807edf978c516a5ab1b26a6e1a"
-    sha256 arm64_sonoma:  "eff92539937c51a8ed9eee32385942a64841c9abef29da5cf6b93a247e9f9317"
-    sha256 arm64_ventura: "44a698222dfc47ec8be0d35bf713759f01841454446710a49443c01d850cac9c"
-    sha256 sonoma:        "d9c366b77a99dfa20d24351a2497e7927ec867c38116fe5fd02ba039dc9762e5"
-    sha256 ventura:       "e50632551a67d5d66e40725157ab526ba986cfc5c947c39699bf663f0320be13"
-    sha256 x86_64_linux:  "7f3c207da64d586ea1a1f5f7bb9885fa3b8363c75702b07719a90cfde9df22e2"
+    sha256 arm64_sequoia: "9e31ee502188a1fd16053b869888bc385e20c1509a35c744c3ede06d82cc8e60"
+    sha256 arm64_sonoma:  "f2d69db601de32e45e848cb3683e5a3f74cb5bd4cf5b11087cab352308f41edd"
+    sha256 arm64_ventura: "679785fc2b2b9bcc8f88b954f2135b392191913cc454a8b29336bffac8f4e236"
+    sha256 sonoma:        "c66197c421a4c45d521a56afce6ac4c322f6256f232267816598ae6c5aecd9df"
+    sha256 ventura:       "ff73c501bc220577f13d4f4cf2c407fb0f3b3faac7b08a7a5117bf50c1090f9a"
+    sha256 x86_64_linux:  "2fc5cb3f09f0ed19aecbd59d308fb499bc25fff4c056783670acb184590bb3a1"
   end
 
   depends_on "glm" => :build
