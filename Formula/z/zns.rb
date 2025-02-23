401a98cc3058b6e6eca16ae0b37ae34b7786251d
Rui Chen <rui@chenrui.dev>
2025-02-23 15:23:26 +0000

    zns: add 0.2.0 bottle.

--- Formula/z/zns.rb
+++ Formula/z/zns.rb
@@ -6,6 +6,15 @@ class Zns < Formula
   license "MIT"
   head "https://github.com/znscli/zns.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c77a27a8771587f16fb718f16b4685037d8860f0ba6a2c2851f42d8a999131f8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c77a27a8771587f16fb718f16b4685037d8860f0ba6a2c2851f42d8a999131f8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c77a27a8771587f16fb718f16b4685037d8860f0ba6a2c2851f42d8a999131f8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f8a96e4b0379acb1d3e38a7345147b2738bac2a37a6ff10819a5d2335faaaba3"
+    sha256 cellar: :any_skip_relocation, ventura:       "f8a96e4b0379acb1d3e38a7345147b2738bac2a37a6ff10819a5d2335faaaba3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e33a19b79bf18eb61ddc8ae3d9baa512b3cf6f70d679fcdba27095f97baeeb8"
+  end
+
   depends_on "go" => :build
 
   def install
