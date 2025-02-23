d95dd7974dfa538cd5c6cd901590289c5b45cbea
Rui Chen <rui@chenrui.dev>
2025-02-23 14:57:29 +0000

    dtsroll: add 1.4.0 bottle.

--- Formula/d/dtsroll.rb
+++ Formula/d/dtsroll.rb
@@ -5,6 +5,15 @@ class Dtsroll < Formula
   sha256 "6a187a076a26ecd9cd9e43f95a1e7b4cdc9979b6bd2a7661b0c2c39d79015f39"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "4aaf6da89655bd3ef6efffaa43864993834a2c2906a8ce38d82b5520ccc7734a"
+    sha256 cellar: :any,                 arm64_sonoma:  "4aaf6da89655bd3ef6efffaa43864993834a2c2906a8ce38d82b5520ccc7734a"
+    sha256 cellar: :any,                 arm64_ventura: "4aaf6da89655bd3ef6efffaa43864993834a2c2906a8ce38d82b5520ccc7734a"
+    sha256 cellar: :any,                 sonoma:        "8ba13591c84098ce42062f50731285ab777027da8081117304284c2d359e75c3"
+    sha256 cellar: :any,                 ventura:       "8ba13591c84098ce42062f50731285ab777027da8081117304284c2d359e75c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aaa7c618b76c1a0781a581ac9cd7b929b64530eb32f6bcad45fb8f19a9f4139b"
+  end
+
   depends_on "node"
 
   def install
