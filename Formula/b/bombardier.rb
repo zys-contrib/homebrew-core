7508b1cb7816088682f41e1c46930366b8da235c
Rui Chen <rui@chenrui.dev>
2025-02-23 19:03:39 +0000

    bombardier: add 2.0 bottle.

--- Formula/b/bombardier.rb
+++ Formula/b/bombardier.rb
@@ -6,6 +6,15 @@ class Bombardier < Formula
   license "MIT"
   head "https://github.com/codesenberg/bombardier.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d1d958586d41468d4cbd0be867d113fd7a92da3acf20abee3610825f2dc04eb7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d1d958586d41468d4cbd0be867d113fd7a92da3acf20abee3610825f2dc04eb7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d1d958586d41468d4cbd0be867d113fd7a92da3acf20abee3610825f2dc04eb7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9af36eb04d67a945da3416439d7b73106904de2df9bbb3924b52392b4a3e4bff"
+    sha256 cellar: :any_skip_relocation, ventura:       "9af36eb04d67a945da3416439d7b73106904de2df9bbb3924b52392b4a3e4bff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "848602f067f4491a724dca02bd7972a38035aaa08ff811a92f0d08f115a6cfc1"
+  end
+
   depends_on "go" => :build
 
   def install
