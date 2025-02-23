3b0b613f992f461d4d2dcf68bf8ffe2b23a8daf7
Bevan Kay <email@bevankay.me>
2025-02-23 22:48:05 +1100

    dtsroll 1.4.0 (new formula)

---
+++ Formula/d/dtsroll.rb
@@ -0,0 +1,22 @@
+class Dtsroll < Formula
+  desc "CLI tool for bundling TypeScript declaration files"
+  homepage "https://github.com/privatenumber/dtsroll"
+  url "https://registry.npmjs.org/dtsroll/-/dtsroll-1.4.0.tgz"
+  sha256 "6a187a076a26ecd9cd9e43f95a1e7b4cdc9979b6bd2a7661b0c2c39d79015f39"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dtsroll --version")
+
+    (testpath/"dts.d.ts").write "export type Foo = string;"
+
+    assert_match "Entry points\n → dts.d.ts", shell_output("#{bin}/dtsroll dts.d.ts")
+  end
+end
