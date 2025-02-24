f45435ca270bd6d878305b6f2e789bb8de06ee49
Patrick Linnane <patrick@linnane.io>
2025-02-24 17:16:32 +0000

    nuitka: add 2.6.7 bottle.

--- Formula/n/nuitka.rb
+++ Formula/n/nuitka.rb
@@ -7,6 +7,15 @@ class Nuitka < Formula
   sha256 "ad8b0cf431650210efa2dc4dcd146d2c76d4a6a9b39edcc213a08ca312d1a8fa"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b900c0b8c540ee1238ec36eb8b964b8bd359e546f6dfa4aaf10ce3c19517196"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7145e3a37c507241e1e88d421ef18a5dadc71e8651be47fdb80857d9980c47f9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1549951388ab0eebf581ae3cf0357bf6d0018e03b466588087df0fb880130ff4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1abd763a58c9cdd85ad327c9682a660ba79c4f3fd6774a3540ce4192136a7e07"
+    sha256 cellar: :any_skip_relocation, ventura:       "9b7d8c97c3ad7a3bf08c96804d7f64cb1713efc98d29728ef8797f3c1448c104"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7fd22504ead4ad3b2e1f3806e1077ef29b0e946e83262515b9f54626382065fa"
+  end
+
   depends_on "ccache"
   depends_on "python@3.13"
 
