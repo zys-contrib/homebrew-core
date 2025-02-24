517a21f1f948bcd8a8aad5df940f853c5940aa24
Adrian Ho <the.gromgit@gmail.com>
2025-02-24 22:23:53 +0800

    nuitka 2.6.7 (new formula)

---
+++ Formula/n/nuitka.rb
@@ -0,0 +1,47 @@
+class Nuitka < Formula
+  include Language::Python::Virtualenv
+
+  desc "Python compiler written in Python"
+  homepage "https://nuitka.net"
+  url "https://github.com/Nuitka/Nuitka/archive/refs/tags/2.6.7.tar.gz"
+  sha256 "ad8b0cf431650210efa2dc4dcd146d2c76d4a6a9b39edcc213a08ca312d1a8fa"
+  license "Apache-2.0"
+
+  depends_on "ccache"
+  depends_on "python@3.13"
+
+  on_linux do
+    depends_on "patchelf"
+  end
+
+  resource "ordered-set" do
+    url "https://files.pythonhosted.org/packages/4c/ca/bfac8bc689799bcca4157e0e0ced07e70ce125193fc2e166d2e685b7e2fe/ordered-set-4.1.0.tar.gz"
+    sha256 "694a8e44c87657c59292ede72891eb91d34131f6531463aab3009191c77364a8"
+  end
+
+  resource "zstandard" do
+    url "https://files.pythonhosted.org/packages/ed/f6/2ac0287b442160a89d726b17a9184a4c615bb5237db763791a7fd16d9df1/zstandard-0.23.0.tar.gz"
+    sha256 "b2d8c62d08e7255f68f7a740bae85b3c9b8e5466baa9cbf7f57f1cde0ac6bc09"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    man1.install Dir["doc/*.1"]
+  end
+
+  test do
+    (testpath/"test.py").write <<~EOS
+      def talk(message):
+          return "Talk " + message
+
+      def main():
+          print(talk("Hello World"))
+
+      if __name__ == "__main__":
+          main()
+    EOS
+    assert_match "Talk Hello World", shell_output("python3 test.py")
+    system bin/"nuitka", "--onefile", "-o", "test", "test.py"
+    assert_match "Talk Hello World", shell_output("./test")
+  end
+end
