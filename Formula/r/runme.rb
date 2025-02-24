195e1ea4ea80c8a35c70107f24c841724e11bb11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-24 22:38:35 +0000

    runme: update 3.12.3 bottle.

--- Formula/r/runme.rb
+++ Formula/r/runme.rb
@@ -7,12 +7,12 @@ class Runme < Formula
   head "https://github.com/stateful/runme.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "608f70f1e5b5df294403e01eb555b7084bf7a51952b9e1dcf2417b65d055202b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "608f70f1e5b5df294403e01eb555b7084bf7a51952b9e1dcf2417b65d055202b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "608f70f1e5b5df294403e01eb555b7084bf7a51952b9e1dcf2417b65d055202b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6f476a2ba32f75df7369957fe920b76792d2ff7c1bbfc2d445ae51a5e09f07c2"
-    sha256 cellar: :any_skip_relocation, ventura:       "6f476a2ba32f75df7369957fe920b76792d2ff7c1bbfc2d445ae51a5e09f07c2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "94abb8ad9ac3d536533e595b8b7dfa86843c7bbfb0e0afca491f2aaf925d0825"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a94a5935d4e224a82629137a6e7f4733c5af6ce0704eb653873b5262c3e1b66f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a94a5935d4e224a82629137a6e7f4733c5af6ce0704eb653873b5262c3e1b66f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a94a5935d4e224a82629137a6e7f4733c5af6ce0704eb653873b5262c3e1b66f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f2eae784fe2b57329fc50444876c9c0625940b5abe0ae99d92b2dc4c5e695af7"
+    sha256 cellar: :any_skip_relocation, ventura:       "f2eae784fe2b57329fc50444876c9c0625940b5abe0ae99d92b2dc4c5e695af7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c7c8bd9c038a58611ddfbac521e27ff927bd2042dc8bd6fc5e53239a49854d3"
   end
 
   depends_on "go" => :build
