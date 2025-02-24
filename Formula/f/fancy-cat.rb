e90b005ff1f4262ee5d1cb0a9784214804f205dd
Patrick Linnane <patrick@linnane.io>
2025-02-24 22:38:55 +0000

    fancy-cat: add 0.2.0 bottle.

--- Formula/f/fancy-cat.rb
+++ Formula/f/fancy-cat.rb
@@ -5,6 +5,15 @@ class FancyCat < Formula
   sha256 "60f92cd08da6ed845b81edca75f376bf312b26e5420b7a244b275845b6f38af8"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "d12990678c4072a43aa05986d1513e50c7ea99b949c40739a967ccf71eff1977"
+    sha256 cellar: :any,                 arm64_sonoma:  "bd6b511521b63625553e008e395532c724d3e7e3c58e7e45250b6f3c77a00927"
+    sha256 cellar: :any,                 arm64_ventura: "76940b4ef61d513168042b3a7c685d0c849de25f23d4fdb0346693b91a16641f"
+    sha256 cellar: :any,                 sonoma:        "df6618f24ba5d6ddd25c72fc45febe04794489ecff753b813cd893b4fd979a9a"
+    sha256 cellar: :any,                 ventura:       "99cb60960cab7749739a1c9707dc2bdf60f1212d55dc642a5edab24ff8f11580"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "210539b80b7fdfedd1e27cfbf36ad1f516eed15620c7a70f7cead4ce29699e3e"
+  end
+
   depends_on "zig" => :build
   depends_on "mupdf"
 
