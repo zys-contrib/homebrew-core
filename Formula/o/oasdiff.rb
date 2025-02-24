58664330556b44c79c539af4668382ba73ae4d3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-24 17:25:05 +0000

    oasdiff: update 1.10.29 bottle.

--- Formula/o/oasdiff.rb
+++ Formula/o/oasdiff.rb
@@ -13,12 +13,12 @@ class Oasdiff < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "36e27df9700bf2e119eaf5b1b77ad5e40081aa1ad6b276d28fdb32d1d1d8d390"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "36e27df9700bf2e119eaf5b1b77ad5e40081aa1ad6b276d28fdb32d1d1d8d390"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "36e27df9700bf2e119eaf5b1b77ad5e40081aa1ad6b276d28fdb32d1d1d8d390"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a01dbfd8634244409d275a9efd4e042fc6aad7d208deb5863759140901cf2dd8"
-    sha256 cellar: :any_skip_relocation, ventura:       "a01dbfd8634244409d275a9efd4e042fc6aad7d208deb5863759140901cf2dd8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9caa39e9feacbf3e99305dfe4d8df43b716c5d947539375441b6382bd248ed7d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9554db562a9d9317df9c1767348a78f2926c5c7ce0cab43f303c8c64e3921849"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9554db562a9d9317df9c1767348a78f2926c5c7ce0cab43f303c8c64e3921849"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9554db562a9d9317df9c1767348a78f2926c5c7ce0cab43f303c8c64e3921849"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4d79e72048874757a763b80c3ab04fb7fdf905bb5027165b9e33c10cdf6c26ec"
+    sha256 cellar: :any_skip_relocation, ventura:       "4d79e72048874757a763b80c3ab04fb7fdf905bb5027165b9e33c10cdf6c26ec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "94b5aa2e0bc5435823589f43e9bac00cea3b145656d0fd2d9e3e61de4966206a"
   end
 
   depends_on "go" => :build
