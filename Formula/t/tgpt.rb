449f17b85b3f1c2455e32ff7e96e8629aaa0533e
Rui Chen <rui@chenrui.dev>
2025-02-24 13:34:29 +0000

    tgpt: add 2.9.1 bottle.

--- Formula/t/tgpt.rb
+++ Formula/t/tgpt.rb
@@ -6,6 +6,15 @@ class Tgpt < Formula
   license "GPL-3.0-only"
   head "https://github.com/aandrew-me/tgpt.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a1d9b62dbe8a16a44d4ed5dc39b05f1c5fa2e272ad019105ab0b6f2073d0fb06"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a1d9b62dbe8a16a44d4ed5dc39b05f1c5fa2e272ad019105ab0b6f2073d0fb06"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a1d9b62dbe8a16a44d4ed5dc39b05f1c5fa2e272ad019105ab0b6f2073d0fb06"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fc1e4fd268f8cc1956e6205ac0b7279277c4fae6c99e3e4d36b2af62067c0506"
+    sha256 cellar: :any_skip_relocation, ventura:       "fc1e4fd268f8cc1956e6205ac0b7279277c4fae6c99e3e4d36b2af62067c0506"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ab8339a7869e2770fde0ca817c85231dc0878fbef00968eb334222f934550be"
+  end
+
   depends_on "go" => :build
 
   def install
