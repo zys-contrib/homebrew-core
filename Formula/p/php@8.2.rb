925a03377941ae10028478128bc09463bb8406bd
Michael Cho <michael@michaelcho.dev>
2025-02-25 17:53:09 -0500

    php@8.2: revision bump to migrate to `icu4c@77`

--- Formula/p/php@8.2.rb
+++ Formula/p/php@8.2.rb
@@ -6,6 +6,7 @@ class PhpAT82 < Formula
   mirror "https://fossies.org/linux/www/php-8.2.28.tar.xz"
   sha256 "af8c9153153a7f489153b7a74f2f29a5ee36f5cb2c6c6929c98411a577e89c91"
   license "PHP-3.01"
+  revision 1
 
   livecheck do
     url "https://www.php.net/downloads"
@@ -39,7 +40,7 @@ class PhpAT82 < Formula
   depends_on "gd"
   depends_on "gettext"
   depends_on "gmp"
-  depends_on "icu4c@76"
+  depends_on "icu4c@77"
   depends_on "krb5"
   depends_on "libpq"
   depends_on "libsodium"
