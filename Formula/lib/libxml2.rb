ee2eb1e061d599cccd158991c61c5b97b446bfa2
Michael Cho <michael@michaelcho.dev>
2025-02-25 17:30:49 -0500

    libxml2: revision bump to migrate to `icu4c@77`

--- Formula/lib/libxml2.rb
+++ Formula/lib/libxml2.rb
@@ -2,6 +2,7 @@ class Libxml2 < Formula
   desc "GNOME XML library"
   homepage "http://xmlsoft.org/"
   license "MIT"
+  revision 1
 
   stable do
     url "https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.6.tar.xz"
@@ -49,7 +50,7 @@ class Libxml2 < Formula
   depends_on "python-setuptools" => :build
   depends_on "python@3.12" => [:build, :test]
   depends_on "python@3.13" => [:build, :test]
-  depends_on "icu4c@76"
+  depends_on "icu4c@77"
   depends_on "readline"
 
   uses_from_macos "zlib"
