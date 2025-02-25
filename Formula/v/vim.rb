4286d06a1085107e4eadd935eadc7cf091c772f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-25 20:17:47 +0000

    vim 9.1.1150

--- Formula/v/vim.rb
+++ Formula/v/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/refs/tags/v9.1.1100.tar.gz"
-  sha256 "68606662a74e8c18560a0752f634c68477b2d63d469b3af38fd69c5e6ed5dbd6"
+  url "https://github.com/vim/vim/archive/refs/tags/v9.1.1150.tar.gz"
+  sha256 "5a0f44fdf05198541e6cb76171a227a46b43c1b999fc1bfde85b3c3dc5b8943e"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
