d9a08ad693549a46c5d80099c07063bd77bb48dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-26 02:11:29 +0000

    fricas: add 1.3.11 bottle.

--- Formula/f/fricas.rb
+++ Formula/f/fricas.rb
@@ -6,6 +6,15 @@ class Fricas < Formula
   license "BSD-3-Clause"
   head "https://github.com/fricas/fricas.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "f979124c221aacc487700749338a4be2926cf8e5e41108e9f9c0a55a35d05e7e"
+    sha256 cellar: :any,                 arm64_sonoma:  "8989cfeb1e1d63f6489e402ad19fd8433d1b3d206535e9b08af6d16e2c26c6d7"
+    sha256 cellar: :any,                 arm64_ventura: "6c37fc5e9a30a29f94aba6999246cdc4428242afbb250836aeaf7e139d359f79"
+    sha256 cellar: :any,                 sonoma:        "8ead560ceff9155edf01af8e4d7bdebd803df863848d94aa423431ce6633f58e"
+    sha256 cellar: :any,                 ventura:       "eafaa5ea9b679d04b2d77f6c98c2348a1cbc43abd74a70b5bed283524881070a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3704109df28d50e400c8a0e58e23b6d3e7b9eff93adfc2c543bf8c089de170a1"
+  end
+
   depends_on "gmp"
   depends_on "libice"
   depends_on "libsm"
