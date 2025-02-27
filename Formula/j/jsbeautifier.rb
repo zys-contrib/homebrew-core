152765cabb36ccbc4bd2401250b0e6bea36d8376
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-27 20:01:56 +0000

    jsbeautifier 1.15.4

--- Formula/j/jsbeautifier.rb
+++ Formula/j/jsbeautifier.rb
@@ -3,8 +3,8 @@ class Jsbeautifier < Formula
 
   desc "JavaScript unobfuscator and beautifier"
   homepage "https://beautifier.io"
-  url "https://files.pythonhosted.org/packages/cf/74/a37cc6fe8ab3f217657d345dfba0612758807536dca5ca5d2f6a81e3623d/jsbeautifier-1.15.3.tar.gz"
-  sha256 "5f1baf3d4ca6a615bb5417ee861b34b77609eeb12875555f8bbfabd9bf2f3457"
+  url "https://files.pythonhosted.org/packages/ea/98/d6cadf4d5a1c03b2136837a435682418c29fdeb66be137128544cecc5b7a/jsbeautifier-1.15.4.tar.gz"
+  sha256 "5bb18d9efb9331d825735fbc5360ee8f1aac5e52780042803943aa7f854f7592"
   license "MIT"
 
   bottle do
