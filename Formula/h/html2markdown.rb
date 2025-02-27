bfaccb22052b62a64418066934108cc0f86060a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-27 13:59:08 +0000

    html2markdown 2.3.0

--- Formula/h/html2markdown.rb
+++ Formula/h/html2markdown.rb
@@ -1,8 +1,8 @@
 class Html2markdown < Formula
   desc "Convert HTML to Markdown"
   homepage "https://html-to-markdown.com"
-  url "https://github.com/JohannesKaufmann/html-to-markdown/archive/refs/tags/v2.2.2.tar.gz"
-  sha256 "6d45930df912143f7f8bc8097e28f9c93f39d733e7536a3801e294abbfe7eddb"
+  url "https://github.com/JohannesKaufmann/html-to-markdown/archive/refs/tags/v2.3.0.tar.gz"
+  sha256 "39b0078fc5deff3990a3fbaab43083ca083fc562ea73302a1d7583495c4699e2"
   license "MIT"
   head "https://github.com/JohannesKaufmann/html-to-markdown.git", branch: "main"
 
