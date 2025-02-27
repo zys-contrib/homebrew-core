24e3752813164caa5810e9ddcb3555646ccff967
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-02-27 05:20:17 +0000

    zlib-ng-compat: add 2.2.3 bottle.

--- Formula/z/zlib-ng-compat.rb
+++ Formula/z/zlib-ng-compat.rb
@@ -10,6 +10,15 @@ class ZlibNgCompat < Formula
     formula "zlib-ng"
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "76c20d6ed2c49ff701a6b6481a77f9552b483689c7a34ca80cf3ee81ea618983"
+    sha256 cellar: :any,                 arm64_sonoma:  "ffb9478de009ee51ef72b0457eef3210c505d7fe905428df405227afb53f3833"
+    sha256 cellar: :any,                 arm64_ventura: "47cee490e09b583e965247a94c0c7868e1a60fcb6c80096fd047b6be58c84ae3"
+    sha256 cellar: :any,                 sonoma:        "2b33cae6f27b6bd9f0ae79614b9cada2d89710916c4fb111ae6faac9c59c48a9"
+    sha256 cellar: :any,                 ventura:       "7e8ea237b3589fa3f07f71de4c11687d10efc1c7c8d0640ec5a1564e477bdd79"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3eefb9467069e984bfcc3f1c2a18262fa424e56664a9c97631ca0ce7c045b35"
+  end
+
   keg_only :shadowed_by_macos, "macOS provides zlib"
 
   on_linux do
