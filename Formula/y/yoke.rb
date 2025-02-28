8058ca2f29f999ddcc482e4d898865f8cd4284f1
Rui Chen <rui@chenrui.dev>
2025-02-28 22:22:50 +0000

    yoke: add 0.9.3 bottle.

--- Formula/y/yoke.rb
+++ Formula/y/yoke.rb
@@ -8,6 +8,15 @@ class Yoke < Formula
   license "MIT"
   head "https://github.com/yokecd/yoke.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1eb4d14303d0e106c110f56850ff59b051964562891c10b4592ef613110f4493"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1eb4d14303d0e106c110f56850ff59b051964562891c10b4592ef613110f4493"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1eb4d14303d0e106c110f56850ff59b051964562891c10b4592ef613110f4493"
+    sha256 cellar: :any_skip_relocation, sonoma:        "157ab955f0bd2fef270fc859a60b6abbfed8110d9d21a9eadfef12889c83f926"
+    sha256 cellar: :any_skip_relocation, ventura:       "157ab955f0bd2fef270fc859a60b6abbfed8110d9d21a9eadfef12889c83f926"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63f02fbd4897e5195b990f73b7d4fb943591bd5c3dbbd970922d41d7f63ec81b"
+  end
+
   depends_on "go" => :build
 
   def install
