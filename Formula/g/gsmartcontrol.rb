ba7ff2964678686b41e6c6aba501985a7ff040dd
Michael Cho <michael@michaelcho.dev>
2025-02-28 11:55:56 -0500

    gsmartcontrol 2.0.2

--- Formula/g/gsmartcontrol.rb
+++ Formula/g/gsmartcontrol.rb
@@ -1,8 +1,8 @@
 class Gsmartcontrol < Formula
   desc "Graphical user interface for smartctl"
   homepage "https://gsmartcontrol.shaduri.dev/"
-  url "https://downloads.sourceforge.net/project/gsmartcontrol/2.0.1/v2.0.1%20source%20code.tar.gz"
-  sha256 "9a7e4fefc34aef7358a5196633e89924ff1e94e721b53609a51af68b1902a947"
+  url "https://downloads.sourceforge.net/project/gsmartcontrol/2.0.2/gsmartcontrol-2.0.2.tar.gz"
+  sha256 "7cebd83fd34883d51e143389aa88f8173ea7b67c760b12b7de847f3c3c8cee34"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
 
   bottle do
@@ -16,30 +16,26 @@ class Gsmartcontrol < Formula
 
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
-  depends_on "at-spi2-core"
   depends_on "atkmm@2.28"
   depends_on "cairo"
   depends_on "cairomm@1.14"
-  depends_on "gdk-pixbuf"
   depends_on "glib"
   depends_on "glibmm@2.66"
   depends_on "gtk+3"
   depends_on "gtkmm3"
-  depends_on "harfbuzz"
   depends_on "libsigc++@2"
-  depends_on "pango"
   depends_on "pangomm@2.46"
-  depends_on "pcre2"
   depends_on "smartmontools"
 
   on_macos do
     depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1500
 
+    depends_on "at-spi2-core"
+    depends_on "gdk-pixbuf"
     depends_on "gettext"
-  end
-
-  on_linux do
-    depends_on "gcc"
+    depends_on "harfbuzz"
+    depends_on "pango"
+    depends_on "pcre2"
   end
 
   fails_with :clang do
@@ -48,7 +44,7 @@ class Gsmartcontrol < Formula
   end
 
   fails_with :gcc do
-    version "11"
+    version "10"
     cause "Requires C++20"
   end
 
