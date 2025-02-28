566a8bbb82c674815f91df6d1a7b8245c2a49dfd
Anka <runner@Mac-1740737835128.local>
2025-02-28 10:25:53 +0000

    vespa-cli 8.488.1

--- Formula/v/vespa-cli.rb
+++ Formula/v/vespa-cli.rb
@@ -1,8 +1,8 @@
 class VespaCli < Formula
   desc "Command-line tool for Vespa.ai"
   homepage "https://vespa.ai"
-  url "https://github.com/vespa-engine/vespa/archive/refs/tags/v8.485.42.tar.gz"
-  sha256 "ed4a37991a8aa2eaf288a51498c83d569575daa1ce5e9826d5ac668a494fa078"
+  url "https://github.com/vespa-engine/vespa/archive/refs/tags/v8.488.1.tar.gz"
+  sha256 "325594c09636280651e6c9173aa5cbdbf176d05bbd110dc12c7e882ef50c5c82"
   license "Apache-2.0"
 
   livecheck do
