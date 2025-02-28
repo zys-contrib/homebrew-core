06efd18e54357dede37a9fb2793ac813b64645e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-28 20:01:36 +0000

    lando-cli 3.24.0

--- Formula/l/lando-cli.rb
+++ Formula/l/lando-cli.rb
@@ -1,8 +1,8 @@
 class LandoCli < Formula
   desc "Cli part of Lando"
   homepage "https://docs.lando.dev/cli"
-  url "https://github.com/lando/core/archive/refs/tags/v3.23.26.tar.gz"
-  sha256 "27d8d8e1a0f8aea119148860416374939db9621fd096e8c930ce2d8dfd03f6ba"
+  url "https://github.com/lando/core/archive/refs/tags/v3.24.0.tar.gz"
+  sha256 "7c730924e3b41ca8dbc2e4350d3ee57511a1809f276d4ae84283fb59c58146c2"
   license "GPL-3.0-or-later"
   head "https://github.com/lando/core.git", branch: "main"
 
