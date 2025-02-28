9ab36bc513c2fd743cb834c7deacb47ce01aa7a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-28 02:29:16 +0000

    opensearch 2.19.1
    opensearch: skip test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/opensearch.rb
+++ Formula/o/opensearch.rb
@@ -1,8 +1,8 @@
 class Opensearch < Formula
   desc "Open source distributed and RESTful search engine"
   homepage "https://github.com/opensearch-project/OpenSearch"
-  url "https://github.com/opensearch-project/OpenSearch/archive/refs/tags/2.19.0.tar.gz"
-  sha256 "c94e9f793393aa6ff4eb00f0ed0dd18a9f3502d2e1c84f947dad6d5c64f1fe31"
+  url "https://github.com/opensearch-project/OpenSearch/archive/refs/tags/2.19.1.tar.gz"
+  sha256 "99999a392dcf90bafebfa143ed071b45662fb022dcbcfa77df802248338d3a63"
   license "Apache-2.0"
 
   bottle do
@@ -16,6 +16,11 @@ class Opensearch < Formula
 
   depends_on "gradle" => :build
   depends_on "openjdk"
+
+  # Fix for gradle 8.13, could be removed in the next release
+  # We modify the patch to remove binary changes for linux
+  # PR Ref: https://github.com/opensearch-project/OpenSearch/pull/17345
+  patch :DATA
 
   def install
     platform = OS.kernel_name.downcase
@@ -105,3 +110,81 @@ class Opensearch < Formula
     system bin/"opensearch-plugin", "list"
   end
 end
+
+__END__
+diff --git a/build.gradle b/build.gradle
+index 679f7b9299248fb0f5173db8fccdfb77965e394b..187574da9e62aec063548871f5dc1a7fbf62a082 100644
+--- a/build.gradle
++++ b/build.gradle
+@@ -721,7 +721,7 @@ subprojects {
+ reporting {
+   reports {
+     testAggregateTestReport(AggregateTestReport) {
+-      testType = TestSuiteType.UNIT_TEST
++      testSuiteName = "test"
+     }
+   }
+ }
+diff --git a/distribution/packages/build.gradle b/distribution/packages/build.gradle
+index ada19dfa38e785aed9ea01d613226d624856ebbd..d3cecde24a35dade98ed18c2d5f55dba0ee5b5b7 100644
+--- a/distribution/packages/build.gradle
++++ b/distribution/packages/build.gradle
+@@ -63,7 +63,7 @@ import java.util.regex.Pattern
+  */
+ 
+ plugins {
+-  id "com.netflix.nebula.ospackage-base" version "11.10.1"
++  id "com.netflix.nebula.ospackage-base" version "11.11.1"
+ }
+ 
+ void addProcessFilesTask(String type, boolean jdk) {
+diff --git a/gradle/code-coverage.gradle b/gradle/code-coverage.gradle
+index eb27dd1a76634251bceafd6fefbafd65eafd5c66..1e41f12e1cc48de3ec9bcd0078f348f3a30af8f3 100644
+--- a/gradle/code-coverage.gradle
++++ b/gradle/code-coverage.gradle
+@@ -38,7 +38,7 @@ if (System.getProperty("tests.coverage")) {
+   reporting {
+     reports {
+       testCodeCoverageReport(JacocoCoverageReport) {
+-        testType = TestSuiteType.UNIT_TEST
++        testSuiteName = "test"
+       }
+     }
+   }
+diff --git a/gradle/wrapper/gradle-wrapper.properties b/gradle/wrapper/gradle-wrapper.properties
+index c51246f2815f5294bd8a51b3ac25c19964577ac1..95e1a2f213a063c0f371f4eab8e67ba860be7baa 100644
+--- a/gradle/wrapper/gradle-wrapper.properties
++++ b/gradle/wrapper/gradle-wrapper.properties
+@@ -11,7 +11,7 @@
+ 
+ distributionBase=GRADLE_USER_HOME
+ distributionPath=wrapper/dists
+-distributionUrl=https\://services.gradle.org/distributions/gradle-8.12.1-all.zip
++distributionUrl=https\://services.gradle.org/distributions/gradle-8.13-all.zip
+ zipStoreBase=GRADLE_USER_HOME
+ zipStorePath=wrapper/dists
+-distributionSha256Sum=296742a352f0b20ec14b143fb684965ad66086c7810b7b255dee216670716175
++distributionSha256Sum=fba8464465835e74f7270bbf43d6d8a8d7709ab0a43ce1aa3323f73e9aa0c612
+diff --git a/gradlew b/gradlew
+index f5feea6d6b116baaca5a2642d4d9fa1f47d574a7..faf93008b77e7b52e18c44e4eef257fc2f8fd76d 100755
+--- a/gradlew
++++ b/gradlew
+@@ -86,8 +86,7 @@ done
+ # shellcheck disable=SC2034
+ APP_BASE_NAME=${0##*/}
+ # Discard cd standard output in case $CDPATH is set (https://github.com/gradle/gradle/issues/25036)
+-APP_HOME=$( cd -P "${APP_HOME:-./}" > /dev/null && printf '%s
+-' "$PWD" ) || exit
++APP_HOME=$( cd -P "${APP_HOME:-./}" > /dev/null && printf '%s\n' "$PWD" ) || exit
+ 
+ # Use the maximum available, or set MAX_FD != -1 to use that value.
+ MAX_FD=maximum
+@@ -206,7 +205,7 @@ fi
+ DEFAULT_JVM_OPTS='"-Xmx64m" "-Xms64m"'
+ 
+ # Collect all arguments for the java command:
+-#   * DEFAULT_JVM_OPTS, JAVA_OPTS, JAVA_OPTS, and optsEnvironmentVar are not allowed to contain shell fragments,
++#   * DEFAULT_JVM_OPTS, JAVA_OPTS, and optsEnvironmentVar are not allowed to contain shell fragments,
+ #     and any embedded shellness will be escaped.
+ #   * For example: A user cannot expect ${Hostname} to be expanded, as it is an environment variable and will be
+ #     treated as '${Hostname}' itself on the command line.
