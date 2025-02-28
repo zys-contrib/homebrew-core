42be1e133ee40030686fbcad22f2c79db4e821db
Patrick Linnane <patrick@linnane.io>
2025-02-28 11:55:46 -0800

    yoke 0.9.3 (new formula)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/y/yoke.rb
@@ -0,0 +1,22 @@
+class Yoke < Formula
+  desc "Helm-inspired infrastructure-as-code package deployer"
+  homepage "https://yokecd.github.io/docs/"
+  # We use a git checkout since the build relies on tags for the version
+  url "https://github.com/yokecd/yoke.git",
+      tag:      "v0.9.3",
+      revision: "3c0b7fa00c5b5355370c569e6b73acf35c78229d"
+  license "MIT"
+  head "https://github.com/yokecd/yoke.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/yoke"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/yoke version")
+
+    assert_match "failed to build k8 config", shell_output("#{bin}/yoke inspect 2>&1", 1)
+  end
+end
