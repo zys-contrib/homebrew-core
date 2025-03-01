2ec312771634bd5f5371fe107b9481bd5f936ba2
Rui Chen <rui@chenrui.dev>
2025-03-01 01:21:53 -0500

    kirimase 0.0.62 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/k/kirimase.rb
@@ -0,0 +1,21 @@
+class Kirimase < Formula
+  desc "CLI for building full-stack Next.js apps"
+  homepage "https://kirimase.dev/"
+  url "https://registry.npmjs.org/kirimase/-/kirimase-0.0.62.tgz"
+  sha256 "5d6d0e43b8bd07bcae71b279820491053b8a1445c5e6f8f66f5f0d158a67d16c"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kirimase --version")
+
+    output = shell_output("#{bin}/kirimase init test-app 2>&1")
+    assert_match "[fatal] No Next.js project detected", output
+  end
+end
