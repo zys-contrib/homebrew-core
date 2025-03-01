8cc7a4a27fc90fdaf92212d674993e52e23ff455
Patrick Linnane <patrick@linnane.io>
2025-03-01 04:14:45 +0000

    cf-terraforming: add 0.23.3 bottle.

--- Formula/c/cf-terraforming.rb
+++ Formula/c/cf-terraforming.rb
@@ -6,6 +6,15 @@ class CfTerraforming < Formula
   license "MPL-2.0"
   head "https://github.com/cloudflare/cf-terraforming.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8b864912ec138ee9ef2ae649ef429a9fa0130ecba6fa19916bf6354e1154c847"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8b864912ec138ee9ef2ae649ef429a9fa0130ecba6fa19916bf6354e1154c847"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "8b864912ec138ee9ef2ae649ef429a9fa0130ecba6fa19916bf6354e1154c847"
+    sha256 cellar: :any_skip_relocation, sonoma:        "98e33f9481a4d829903e8ca9599a386d156783e78b23d6a7389ca6f4a08e536f"
+    sha256 cellar: :any_skip_relocation, ventura:       "98e33f9481a4d829903e8ca9599a386d156783e78b23d6a7389ca6f4a08e536f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8d0585cbbfd30b0f028b16611fccb5d3fcc741fe88a37c6618dd2fa8db9fb7e"
+  end
+
   depends_on "go" => :build
 
   def install
