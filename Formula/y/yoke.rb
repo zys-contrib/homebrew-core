c62f17200710d539455379a33d4f26de634bda8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 05:13:59 +0000

    yoke 0.10.0

--- Formula/y/yoke.rb
+++ Formula/y/yoke.rb
@@ -3,8 +3,8 @@ class Yoke < Formula
   homepage "https://yokecd.github.io/docs/"
   # We use a git checkout since the build relies on tags for the version
   url "https://github.com/yokecd/yoke.git",
-      tag:      "v0.9.3",
-      revision: "3c0b7fa00c5b5355370c569e6b73acf35c78229d"
+      tag:      "v0.10.0",
+      revision: "c395f1004936a457b954017a2518070cb30e94a8"
   license "MIT"
   head "https://github.com/yokecd/yoke.git", branch: "main"
 
