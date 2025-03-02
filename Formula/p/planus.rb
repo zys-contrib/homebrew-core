77bd7deed6da17dd9e252ab0da2d62474768236e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 23:13:51 +0000

    planus 1.1.1

--- Formula/p/planus.rb
+++ Formula/p/planus.rb
@@ -1,8 +1,8 @@
 class Planus < Formula
   desc "Alternative compiler for flatbuffers,"
   homepage "https://github.com/planus-org/planus"
-  url "https://github.com/planus-org/planus/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "292423124df64a6b66a93daaa93c8f3ee19582968198bcf49b3f137f4e20e4ef"
+  url "https://github.com/planus-org/planus/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "d79f5d9a1acfcadc86376537c297853dcd6f326016f8049c28df57bb4f39c957"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/planus-org/planus.git", branch: "main"
 
