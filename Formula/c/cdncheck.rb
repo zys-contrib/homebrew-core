d92300c8d94e1f09cc67cbdb60e9869d399d983b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 04:49:03 +0000

    cdncheck: update 1.1.8 bottle.

--- Formula/c/cdncheck.rb
+++ Formula/c/cdncheck.rb
@@ -12,12 +12,12 @@ class Cdncheck < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "798c0d3a7ceb600de7c404c09b1fe456a1bb6a523095cf8941ca3d8ccefa4147"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f907adbe226a3ace4d4737a64cacbfa4d78aafbe6bc2ab31595ab080f20318df"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "1ccee596ed4ab144505f37ed526f053fc2758c65dec2ab2e6684f03e64513c25"
-    sha256 cellar: :any_skip_relocation, sonoma:        "add64a8426a5dba643a13d944133290ced888faa278d1f0693714b182076b394"
-    sha256 cellar: :any_skip_relocation, ventura:       "4c2ddd85bcd511e51d091d395dd699bbe6f1eac5c49d7be13cbf354bc29430be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2ec54f7fbcac282d39add2dda350d7bba4547bddf23035b5812614dc3f7a0d0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4a8b0068faa5672ab65a1c21aec90eb4c45b1676a59acfcdc62405cc17332443"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "266656802b450c810e68643abed5c7510a9d5cfceddea13cbfeba8462b85544e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "de2d3fc0b1e1459de756522f86a02282cbb4706289771a529fd263f088be5cdb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3dc740975e40cb8382261421807303e80fa252e5d531032efc8470c0d9191c01"
+    sha256 cellar: :any_skip_relocation, ventura:       "d303886921a37cb0d63e35919a7910e008f833c822ccff8eed0c6d8fce967d06"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "82c11a7cffe76914f8831a6346c1ac18b78280d2c75fa24b09660fe726c4ca32"
   end
 
   depends_on "go" => :build
