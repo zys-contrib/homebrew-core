f0f3cb4ec8796ba8da2081aa8947d8db5215f9d6
Rui Chen <rui@chenrui.dev>
2025-03-02 12:56:07 +0000

    sdl3_ttf: add 3.2.0 bottle.

--- Formula/s/sdl3_ttf.rb
+++ Formula/s/sdl3_ttf.rb
@@ -6,6 +6,15 @@ class Sdl3Ttf < Formula
   license "Zlib"
   head "https://github.com/libsdl-org/SDL_ttf.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "b21aac59ebbfa8d1d48a6b4af48d3d40bfca296969ac68c865a08b3658b7a55e"
+    sha256 cellar: :any,                 arm64_sonoma:  "7091e1cf1509f9dd45f83e7e35e1c56b26a2d32e94de49a6ed736368987a2b5a"
+    sha256 cellar: :any,                 arm64_ventura: "6b3fbbd588070465a0c7f7e4c6e9912a2fc205eae35a35110c33b95e9b4ff4fb"
+    sha256 cellar: :any,                 sonoma:        "84e823f88b1f5065b9257c65957cf09c12767ceb76069463b0290c2e754b4b82"
+    sha256 cellar: :any,                 ventura:       "aa23f7ed84eb8e6612b43e147eb3dd49ccaa7839637ace30b5ec587a5ee1a143"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88e14391b5465568ff8b81a6600ea53734bea2899f3922b626fe05704b4b8dcc"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "freetype"
