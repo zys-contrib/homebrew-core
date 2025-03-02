f0e8d5fe35f5a8db149ea628b2daf63385d0c230
Rui Chen <rui@chenrui.dev>
2025-03-02 13:24:50 -0500

    libnghttp2 1.65.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libnghttp2.rb
+++ Formula/lib/libnghttp2.rb
@@ -1,12 +1,12 @@
 class Libnghttp2 < Formula
   desc "HTTP/2 C Library"
   homepage "https://nghttp2.org/"
-  url "https://github.com/nghttp2/nghttp2/releases/download/v1.64.0/nghttp2-1.64.0.tar.gz"
-  mirror "http://fresh-center.net/linux/www/nghttp2-1.64.0.tar.gz"
-  mirror "http://fresh-center.net/linux/www/legacy/nghttp2-1.64.0.tar.gz"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.65.0/nghttp2-1.65.0.tar.gz"
+  mirror "http://fresh-center.net/linux/www/nghttp2-1.65.0.tar.gz"
+  mirror "http://fresh-center.net/linux/www/legacy/nghttp2-1.65.0.tar.gz"
   # this legacy mirror is for user to install from the source when https not working for them
   # see discussions in here, https://github.com/Homebrew/homebrew-core/pull/133078#discussion_r1221941917
-  sha256 "20e73f3cf9db3f05988996ac8b3a99ed529f4565ca91a49eb0550498e10621e8"
+  sha256 "8ca4f2a77ba7aac20aca3e3517a2c96cfcf7c6b064ab7d4a0809e7e4e9eb9914"
   license "MIT"
 
   livecheck do
