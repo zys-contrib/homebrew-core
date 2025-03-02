5b5748168cd37c8f25b973c82021cda9c4783bb8
Rui Chen <rui@chenrui.dev>
2025-03-02 21:30:54 +0000

    todoist: add 0.22.0 bottle.

--- Formula/t/todoist.rb
+++ Formula/t/todoist.rb
@@ -6,6 +6,15 @@ class Todoist < Formula
   license "MIT"
   head "https://github.com/sachaos/todoist.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4a545ff334ff243fa80136cd50d3dd67d4dec54469e02550f359d60686dcbb3e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a545ff334ff243fa80136cd50d3dd67d4dec54469e02550f359d60686dcbb3e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4a545ff334ff243fa80136cd50d3dd67d4dec54469e02550f359d60686dcbb3e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e226a4397f68b89ef40017e4c78cf9480eb4157c6525bbef69e66269bb3dacfc"
+    sha256 cellar: :any_skip_relocation, ventura:       "e226a4397f68b89ef40017e4c78cf9480eb4157c6525bbef69e66269bb3dacfc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e390f5c218ded4bb493af4beff7bc48203cc3cac2d541225b685fe3929b3ce5b"
+  end
+
   depends_on "go" => :build
 
   def install
