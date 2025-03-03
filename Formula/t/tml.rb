370217b745a507080620ea44c48e9da659397174
Bevan Kay <email@bevankay.me>
2025-03-03 11:46:55 +0000

    tml: add 0.7.0 bottle.

--- Formula/t/tml.rb
+++ Formula/t/tml.rb
@@ -6,6 +6,15 @@ class Tml < Formula
   license "Unlicense"
   head "https://github.com/liamg/tml.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b29c24330dd225c36eca02a8b4ebaf812acea9b2f6fb927e5130cf03261afa32"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b29c24330dd225c36eca02a8b4ebaf812acea9b2f6fb927e5130cf03261afa32"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b29c24330dd225c36eca02a8b4ebaf812acea9b2f6fb927e5130cf03261afa32"
+    sha256 cellar: :any_skip_relocation, sonoma:        "614aa201d2d108d63b5758ddd9ff35d591bd129f096fbc04c702868422576e6c"
+    sha256 cellar: :any_skip_relocation, ventura:       "614aa201d2d108d63b5758ddd9ff35d591bd129f096fbc04c702868422576e6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a69543363747967ff8fdc7ff2252bb11ceaafc97fd9374832dd5576485928cd6"
+  end
+
   depends_on "go" => :build
 
   def install
