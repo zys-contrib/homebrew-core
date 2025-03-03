a7d3159dcef7cf9ce85c987f5c475107e0ab8209
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-03 13:45:18 +0000

    ov: add 0.39.0 bottle.

--- Formula/o/ov.rb
+++ Formula/o/ov.rb
@@ -6,6 +6,15 @@ class Ov < Formula
   license "MIT"
   head "https://github.com/noborus/ov.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "784eff397e175380af35feaa109b1f1565eab5ab931b327007d17db672925b63"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "784eff397e175380af35feaa109b1f1565eab5ab931b327007d17db672925b63"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "784eff397e175380af35feaa109b1f1565eab5ab931b327007d17db672925b63"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0fff1f4c38cf8f81bb787632a5bb4b3844152411f7cef89ac20681c46b955ad6"
+    sha256 cellar: :any_skip_relocation, ventura:       "0fff1f4c38cf8f81bb787632a5bb4b3844152411f7cef89ac20681c46b955ad6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d1f4053711a121cd5e8225d725a1a0cbe48020323c21398503a819d60db6d1c"
+  end
+
   depends_on "go" => :build
 
   def install
