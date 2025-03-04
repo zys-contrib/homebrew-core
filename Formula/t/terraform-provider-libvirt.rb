81acb858b39648ec7d258f44fb2c53b5ee6f0571
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-04 11:22:39 +0000

    terraform-provider-libvirt 0.8.3

--- Formula/t/terraform-provider-libvirt.rb
+++ Formula/t/terraform-provider-libvirt.rb
@@ -1,8 +1,8 @@
 class TerraformProviderLibvirt < Formula
   desc "Terraform provisioning with Linux KVM using libvirt"
   homepage "https://github.com/dmacvicar/terraform-provider-libvirt"
-  url "https://github.com/dmacvicar/terraform-provider-libvirt/archive/refs/tags/v0.8.2.tar.gz"
-  sha256 "b1770b8980a093af43f5f449c3365c6b99ef184b1d6e531d03979959fff279e9"
+  url "https://github.com/dmacvicar/terraform-provider-libvirt/archive/refs/tags/v0.8.3.tar.gz"
+  sha256 "9d04ca75d7cb3db511ea251369890d1bdf2a6b7d77c4d4ce5d3cb0d2c0214252"
   license "Apache-2.0"
 
   bottle do
