9c745402f116b44b612190f67663592277160101
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-04 11:05:03 +0000

    chuck: update 1.5.5.0 bottle.

--- Formula/c/chuck.rb
+++ Formula/c/chuck.rb
@@ -13,12 +13,12 @@ class Chuck < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4c9a2adb9ee9ade9e1c73c3c5c9a598f5c952b20958c27e8133c6db403a392ed"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "204c18dc2e95d84c3e9c418b66c89a10ea2d399d4a123fa79de0223ca357e379"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "1d3ff5a5d188bd91ad4aa19ad123782858778e1ed93c4b7fea0f8cfbdf7943fe"
-    sha256 cellar: :any_skip_relocation, sonoma:        "434cdc4ed0d648e25520cf96199b6b4b5faf9a5aa4730606b7026f7e9e4250e6"
-    sha256 cellar: :any_skip_relocation, ventura:       "00c0c2b78cb9c2e56b870191b7a22b62796fc99aae1a482f43ef8b8d4540bb65"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7d8cf39a956e5031f5384f070e3656c2e320e59d28ffcbe26568066cbcdb9163"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "15f98a95a0bf7f5f26a8ec9e2931f75aab7fa17197dfed7841f904e6572700d1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1c90b4b2b59d928c661da269ecb1ec62545c1e2c71c9d1866506d8a6a60ef899"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b8e37a648a05e866e47cb5bbe4b1f1a4dfcd33e8ea10ee48d76418a8da479444"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2a90e85d99574ef5a786832aae043f15eb01246c5ce9bd3b4a4dd7718b394b9e"
+    sha256 cellar: :any_skip_relocation, ventura:       "d601b642482546d029f603fdf96732429130c1de8b8551db68fb2c2b69a9289f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7598ed9db4c8a52610575233f746dce77087ae8b32714cf7f6320ff3d4ddb686"
   end
 
   uses_from_macos "bison" => :build
