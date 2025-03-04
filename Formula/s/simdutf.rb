fd2553cee54838f2a3c9e2fd772e2f841a006aa9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-04 20:12:51 +0000

    simdutf 6.2.1

--- Formula/s/simdutf.rb
+++ Formula/s/simdutf.rb
@@ -1,8 +1,8 @@
 class Simdutf < Formula
   desc "Unicode conversion routines, fast"
   homepage "https://simdutf.github.io/simdutf/"
-  url "https://github.com/simdutf/simdutf/archive/refs/tags/v6.2.0.tar.gz"
-  sha256 "f3ef16cb86d866d2271a9a2a539b6ed9ef9083d524963919ce6792a0e3750fe3"
+  url "https://github.com/simdutf/simdutf/archive/refs/tags/v6.2.1.tar.gz"
+  sha256 "85a29bdcc2780ab473a0912d7a8259f85a98a2a661e7497d46b8e323840a6aac"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/simdutf/simdutf.git", branch: "master"
 
