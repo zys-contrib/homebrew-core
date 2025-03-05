8688dc02f016cadf10cfb75810e6c87502dd6d8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 20:39:02 +0000

    pscale: update 0.228.0 bottle.

--- Formula/p/pscale.rb
+++ Formula/p/pscale.rb
@@ -7,12 +7,12 @@ class Pscale < Formula
   head "https://github.com/planetscale/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7898bc04589af283bb0bd35e6668eea20194ae3f831b175295b4c5dd11b6054a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "35134205d9fa599386f3027abe272a4ba391db014f229dba3d4bc00c583a1d1d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6e33ea12c627fc2b605c79d44c0515fb1605e1c426bc97cd4fe6d0ac05533895"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d2dfc876b2cbf7dc225e2251b0b62ad333a37bf0d50a59c08b5b90c2c708936f"
-    sha256 cellar: :any_skip_relocation, ventura:       "1c36f43606f2198ebe3041a1176b3109edf9f988b7928140b59d8e76c070692a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e26b33ddeaa49533be513fbc1a0899f1d82cb3ffef9437c129029317b8207b5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c58d69fd45752e3b8af398c8c3fe122bc84bc76e8d52f7ff4987bb82bc87dc8d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4120ed6a67d42b9215e4095ced129c80957c8ed1c7261a23b17bb19f6ae9e78f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "529e4219b7467fa305bb8ffb8a99f42104d1f5ecc009f77c5d4f61a02792ac44"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0f9ed66c382c55aa1c8cf833569e52b40b6be05b5cc53ed048eeb7e3d224132a"
+    sha256 cellar: :any_skip_relocation, ventura:       "3658206ef5c4d26276123b3a195fdba4c35b994164de432f165bdec1b36294a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf8a0cb84d0a6bb677171e6ecb16265a86d89b660c57d43b6a8a959e80de8aa3"
   end
 
   depends_on "go" => :build
