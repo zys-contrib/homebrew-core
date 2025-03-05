a3806500104baeb139b80de998f08a6b9099b238
Rui Chen <rui@chenrui.dev>
2025-03-05 12:30:52 -0500

    gersemi 0.19.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/g/gersemi.rb
@@ -0,0 +1,57 @@
+class Gersemi < Formula
+  include Language::Python::Virtualenv
+
+  desc "Formatter to make your CMake code the real treasure"
+  homepage "https://github.com/BlankSpruce/gersemi"
+  url "https://files.pythonhosted.org/packages/28/fa/65880ccdaf9711da590b8f1906aaf279788d51ac2e117840cd4207fbd742/gersemi-0.19.2.tar.gz"
+  sha256 "bc31021b7f7a6d30360e6600ed1c6621a02c627a7b5d8f237cbcc8adcc7b1201"
+  license "MPL-2.0"
+
+  depends_on "libyaml"
+  depends_on "python@3.13"
+
+  resource "appdirs" do
+    url "https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/appdirs-1.4.4.tar.gz"
+    sha256 "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
+  end
+
+  resource "lark" do
+    url "https://files.pythonhosted.org/packages/af/60/bc7622aefb2aee1c0b4ba23c1446d3e30225c8770b38d7aedbfb65ca9d5a/lark-1.2.2.tar.gz"
+    sha256 "ca807d0162cd16cef15a8feecb862d7319e7a09bdb13aef927968e45040fed80"
+  end
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gersemi --version")
+
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 3.10)
+      project(TestProject)
+
+      add_executable(test main.cpp)
+    CMAKE
+
+    # Return 0 when there's nothing to reformat.
+    # Return 1 when some files would be reformatted.
+    system bin/"gersemi", "--check", testpath/"CMakeLists.txt"
+
+    system bin/"gersemi", testpath/"CMakeLists.txt"
+
+    expected_content = <<~CMAKE
+      cmake_minimum_required(VERSION 3.10)
+      project(TestProject)
+
+      add_executable(test main.cpp)
+    CMAKE
+
+    assert_equal expected_content, (testpath/"CMakeLists.txt").read
+  end
+end
