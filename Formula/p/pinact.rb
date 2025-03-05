c635fa603b45e8115ffe45fdf59f06bf4d76f10d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 14:44:32 +0000

    pinact: update 1.2.2 bottle.

--- Formula/p/pinact.rb
+++ Formula/p/pinact.rb
@@ -7,12 +7,12 @@ class Pinact < Formula
   head "https://github.com/suzuki-shunsuke/pinact.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "32c26c242d9811ef6638eee692db928e73e1414e144596bfb8eb4eff90c6a542"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "32c26c242d9811ef6638eee692db928e73e1414e144596bfb8eb4eff90c6a542"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "32c26c242d9811ef6638eee692db928e73e1414e144596bfb8eb4eff90c6a542"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7d1edcce660b0f49fba5c3423a4dd1636f2bc76b23627cf2367f041caa8c75a3"
-    sha256 cellar: :any_skip_relocation, ventura:       "7d1edcce660b0f49fba5c3423a4dd1636f2bc76b23627cf2367f041caa8c75a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b4f7835fa328625a885a971fef65a75e322f8293d43fd981b316110d9e422895"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13fa4890f41e71f5aea2b8280511a331985cf6ea5206a5e2cb28fbcdbef92086"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "13fa4890f41e71f5aea2b8280511a331985cf6ea5206a5e2cb28fbcdbef92086"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "13fa4890f41e71f5aea2b8280511a331985cf6ea5206a5e2cb28fbcdbef92086"
+    sha256 cellar: :any_skip_relocation, sonoma:        "642c7a86251030a4af2d69359c3173f6f3210e851168d565f8cb063ef3758021"
+    sha256 cellar: :any_skip_relocation, ventura:       "642c7a86251030a4af2d69359c3173f6f3210e851168d565f8cb063ef3758021"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "504a5b29485561ceee0562a0dc7e868d865a80e2b48c7e1c956ba0897bc35baf"
   end
 
   depends_on "go" => :build
