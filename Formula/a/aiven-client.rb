ed184bae16a6a3ae9b77f88cf5d669bce81dcc75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 11:14:26 +0000

    aiven-client: update 4.6.1 bottle.

--- Formula/a/aiven-client.rb
+++ Formula/a/aiven-client.rb
@@ -9,7 +9,7 @@ class AivenClient < Formula
   head "https://github.com/aiven/aiven-client.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "dad3ddcaff32cb602598c5569f1069b434cb95c820d7e73083867582e1688961"
+    sha256 cellar: :any_skip_relocation, all: "f63ee735597d6b2f03d718162aad0d01cd64c0db655912d2f133ca9267690099"
   end
 
   depends_on "certifi"
