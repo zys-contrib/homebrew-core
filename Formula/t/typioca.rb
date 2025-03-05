001d1297f315075360eeb29021d93d8c1e2f49b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 10:45:38 +0000

    typioca: add 3.1.0 bottle.

--- Formula/t/typioca.rb
+++ Formula/t/typioca.rb
@@ -6,6 +6,15 @@ class Typioca < Formula
   license "MIT"
   head "https://github.com/bloznelis/typioca.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d67b7e8e8f4bf06519b363da75af9c40805aec9a4a11373cd464d9d3acd59785"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d67b7e8e8f4bf06519b363da75af9c40805aec9a4a11373cd464d9d3acd59785"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d67b7e8e8f4bf06519b363da75af9c40805aec9a4a11373cd464d9d3acd59785"
+    sha256 cellar: :any_skip_relocation, sonoma:        "001224a612f62adb17c03bf47007fa8fabafa8b3005be2f48db19cd311a4bcdb"
+    sha256 cellar: :any_skip_relocation, ventura:       "001224a612f62adb17c03bf47007fa8fabafa8b3005be2f48db19cd311a4bcdb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23a8e545f920e97d3dd4cf38f12ddf5bc4f2d62af406c2e7d27daa7501b12e5c"
+  end
+
   depends_on "go" => :build
 
   def install
