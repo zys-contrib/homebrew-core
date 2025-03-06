c996096d6822730762369d3996ce68c3b9712c27
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-06 08:07:38 +0000

    redress 1.2.19

--- Formula/r/redress.rb
+++ Formula/r/redress.rb
@@ -1,8 +1,8 @@
 class Redress < Formula
   desc "Tool for analyzing stripped Go binaries compiled with the Go compiler"
   homepage "https://github.com/goretk/redress"
-  url "https://github.com/goretk/redress/archive/refs/tags/v1.2.18.tar.gz"
-  sha256 "f46b2a3721cb51b103db70bee98cebdfba38f1c645508cd4793b6763022ab73e"
+  url "https://github.com/goretk/redress/archive/refs/tags/v1.2.19.tar.gz"
+  sha256 "b7c4076b83f4c70cea241920b317069ae1b5cce6ce8169b6add2294f3efa6574"
   license "AGPL-3.0-only"
   head "https://github.com/goretk/redress.git", branch: "develop"
 
