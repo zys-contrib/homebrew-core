f30f106c5ededaa56f78cfd825475a5956caa1fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-06 09:08:58 +0000

    lazyjournal: add 0.7.4 bottle.

--- Formula/l/lazyjournal.rb
+++ Formula/l/lazyjournal.rb
@@ -6,6 +6,15 @@ class Lazyjournal < Formula
   license "MIT"
   head "https://github.com/Lifailon/lazyjournal.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "025c5da08d23eab8dc62b177c8c6b43bf379e80c0413df7861363eb309e7b1cb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "025c5da08d23eab8dc62b177c8c6b43bf379e80c0413df7861363eb309e7b1cb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "025c5da08d23eab8dc62b177c8c6b43bf379e80c0413df7861363eb309e7b1cb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5046800f8cef4d935c5e36cbd1a178865e147aaa87171cc8a860af697c67913e"
+    sha256 cellar: :any_skip_relocation, ventura:       "5046800f8cef4d935c5e36cbd1a178865e147aaa87171cc8a860af697c67913e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6de7c2e3c757c2e4e38e63ab33a91815d6bc34aac916c218ad03295cb2653d02"
+  end
+
   depends_on "go" => :build
 
   def install
