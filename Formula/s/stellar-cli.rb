794eef7e99920181402f23dca35e22bb360ea714
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-06 04:43:57 +0000

    stellar-cli: update 22.3.0 bottle.

--- Formula/s/stellar-cli.rb
+++ Formula/s/stellar-cli.rb
@@ -7,12 +7,12 @@ class StellarCli < Formula
   head "https://github.com/stellar/stellar-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa6989f6eabaeae249e88bbd25c0a8067e9af9a86cfc3fc49ec4b29f2d5b2a42"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "82e2d151869728d9e926a24b264e907f4b4b1d4809c940bdd220736ac7963a6a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "92dc936950cadcea49f27ce03cb4471eb0e3ce74be22a8c4c7acda1a8dd3a687"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e387c6ad7f716081c4f0e843c818ce7e216b3c8ef2550b825686762fd419146a"
-    sha256 cellar: :any_skip_relocation, ventura:       "5e0dec872a5489581b2cf5046683ef81a2b2717cca7c58d9c4daadc94a731570"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "da2cdff8788ce8cc5931015a74dc461878240ccf38e9d550369bc4a60f132098"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dc440feec95560befc6f897ad9d35c7c94c33951cad66c38bf66fff3d30c73be"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e2744c25f71ee2cd483954a89b3fa92867ac8cf2c2ce7b198d5737fbf810a7ea"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "084c413de377523beb81ec084760e45990d55ec8561738b84fb421eb1042cf73"
+    sha256 cellar: :any_skip_relocation, sonoma:        "80ea755df0a760f0f03658344ce88b47339950b0cc47ebbcbd0ac481a684112c"
+    sha256 cellar: :any_skip_relocation, ventura:       "78de1f3155f28a8f3e3a8261e8e1c7c04aeb6c0952f3c5e853083c7617242efc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f14e98a0fb5131b95e3c0b3214afefdeffd120ff35fe6dedc78de3bb014c5eda"
   end
 
   depends_on "pkgconf" => :build
