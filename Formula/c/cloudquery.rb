f4844436f41a72ecc0a178869b9361f1b087b687
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-06 10:54:55 +0000

    cloudquery 6.15.4

--- Formula/c/cloudquery.rb
+++ Formula/c/cloudquery.rb
@@ -1,8 +1,8 @@
 class Cloudquery < Formula
   desc "Data movement tool to sync data from any source to any destination"
   homepage "https://www.cloudquery.io"
-  url "https://github.com/cloudquery/cloudquery/archive/refs/tags/cli-v6.15.3.tar.gz"
-  sha256 "d47b445db1c7de96640a0be28a832c086f6a21b3e6949a7712bd9bda3a5d52ec"
+  url "https://github.com/cloudquery/cloudquery/archive/refs/tags/cli-v6.15.4.tar.gz"
+  sha256 "e0bea02824b0168a499a7c2c9fa0f24fdb84379d421beaab35b0388da97806a1"
   license "MPL-2.0"
   head "https://github.com/cloudquery/cloudquery.git", branch: "main"
 
