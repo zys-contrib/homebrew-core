53d5a1323334389fdc762db94aa57fca21775ce0
Michael Cho <michael@michaelcho.dev>
2025-03-05 21:50:23 -0500

    ifuse: backport support for libfuse 3

--- Formula/i/ifuse.rb
+++ Formula/i/ifuse.rb
@@ -1,11 +1,20 @@
 class Ifuse < Formula
   desc "FUSE module for iOS devices"
   homepage "https://libimobiledevice.org/"
-  url "https://github.com/libimobiledevice/ifuse/archive/refs/tags/1.1.4.tar.gz"
-  sha256 "2a00769e8f1d8bad50898b9d00baf12c8ae1cda2d19ff49eaa9bf580e5dbe78c"
   license "LGPL-2.1-or-later"
   revision 1
   head "https://github.com/libimobiledevice/ifuse.git", branch: "master"
+
+  stable do
+    url "https://github.com/libimobiledevice/ifuse/archive/refs/tags/1.1.4.tar.gz"
+    sha256 "2a00769e8f1d8bad50898b9d00baf12c8ae1cda2d19ff49eaa9bf580e5dbe78c"
+
+    # Backport support for libfuse 3
+    patch do
+      url "https://github.com/libimobiledevice/ifuse/commit/36956a5179e224f57ebb9d0f01314c09c8bf0f97.patch?full_index=1"
+      sha256 "47c87159f085977bc728f586c975b343f67bdf221b164bfef10a572546e80df3"
+    end
+  end
 
   bottle do
     rebuild 1
@@ -17,7 +26,7 @@ class Ifuse < Formula
   depends_on "libtool" => :build
   depends_on "pkgconf" => :build
   depends_on "glib"
-  depends_on "libfuse@2"
+  depends_on "libfuse"
   depends_on "libimobiledevice"
   depends_on "libplist"
   depends_on :linux # on macOS, requires closed-source macFUSE
