a2e2bd6278e86125f656fdbaef120026f853ea51
Michael Cho <michael@michaelcho.dev>
2025-03-05 19:11:40 -0500

    gnome-recipes: apply Debian patch for newer libsoup and librest

--- Formula/g/gnome-recipes.rb
+++ Formula/g/gnome-recipes.rb
@@ -1,12 +1,10 @@
 class GnomeRecipes < Formula
   desc "Formula for GNOME recipes"
   homepage "https://wiki.gnome.org/Apps/Recipes"
-  # needs submodules
-  url "https://gitlab.gnome.org/GNOME/recipes.git",
-      tag:      "2.0.4",
-      revision: "d5e9733c49ea4f99e72c065c05ee1a35ef65e67d"
+  url "https://download.gnome.org/sources/gnome-recipes/2.0/gnome-recipes-2.0.4.tar.xz"
+  sha256 "b30e96985f66fe138a17189c77af44d34d0b4c859b304ebdb52033bc2cd3ffed"
   license "GPL-3.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
     sha256 arm64_sequoia: "aeca025b7378f47ce41dc347deb424d0944de6e7da3408c6e775570ec25a321c"
@@ -31,36 +29,40 @@ class GnomeRecipes < Formula
   depends_on "gspell"
   depends_on "gtk+3"
   depends_on "json-glib" # for goa
-  depends_on "libarchive"
   depends_on "libcanberra"
   depends_on "librest" # for goa
-  depends_on "libsoup@2" # libsoup 3 issue: https://gitlab.gnome.org/GNOME/recipes/-/issues/155
-  depends_on "libxml2"
+  depends_on "libsoup"
   depends_on "pango"
 
   on_macos do
-    depends_on "at-spi2-core"
-    depends_on "enchant"
     depends_on "gettext"
-    depends_on "harfbuzz"
   end
 
   resource "goa" do
-    url "https://download.gnome.org/sources/gnome-online-accounts/3.43/gnome-online-accounts-3.43.1.tar.xz"
-    sha256 "3bcb3663a12efd4482d9fdda3e171676267fc739eb6440a2b7109a0e87afb7e8"
+    url "https://download.gnome.org/sources/gnome-online-accounts/3.52/gnome-online-accounts-3.52.3.1.tar.xz"
+    sha256 "49ed727d6fc49474996fa7edf0919b21e4fc856ea37e6e30f17b50b103af9701"
+  end
 
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    end
+  # Apply Debian patch to support newer libsoup and librest
+  # PR ref: https://gitlab.gnome.org/GNOME/recipes/-/merge_requests/47
+  patch do
+    url "https://sources.debian.org/data/main/g/gnome-recipes/2.0.4-3/debian/patches/Port-to-libsoup-3.0-and-librest-1.0.patch"
+    sha256 "15a06b277d3961d4a00e71eb37b12f4f63f42c99bc1c1a6c9d9f4ead879d4c32"
   end
 
   def install
+    # stop meson_post_install.py from doing what needs to be done in the post_install step
+    ENV["DESTDIR"] = "/"
+
     resource("goa").stage do
-      system "./configure", "--disable-silent-rules",
-                            "--disable-backend",
-                            *std_configure_args(prefix: libexec)
-      system "make", "install"
+      system "meson", "setup", "build", "-Dgoabackend=false",
+                                        "-Ddocumentation=false",
+                                        "-Dintrospection=false",
+                                        "-Dman=false",
+                                        "-Dvapi=false",
+                                        *std_meson_args.map { |s| s.sub prefix, libexec }
+      system "meson", "compile", "-C", "build", "--verbose"
+      system "meson", "install", "-C", "build"
     end
 
     ENV.prepend_path "PKG_CONFIG_PATH", libexec/"lib/pkgconfig"
@@ -72,10 +74,8 @@ class GnomeRecipes < Formula
     end
 
     # BSD tar does not support the required options
-    inreplace "src/gr-recipe-store.c", "argv[0] = \"tar\";", "argv[0] = \"gtar\";"
-    # stop meson_post_install.py from doing what needs to be done in the post_install step
-    ENV["DESTDIR"] = ""
-    ENV.delete "PYTHONPATH"
+    inreplace "src/gr-recipe-store.c", "argv[0] = \"tar\";", "argv[0] = \"gtar\";" if OS.mac?
+
     system "meson", "setup", "build", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
