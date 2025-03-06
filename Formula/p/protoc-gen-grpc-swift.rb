d74c6f3eae88434cd7fb2f5efa9377bbd21d21da
Michael Cho <michael@michaelcho.dev>
2025-03-05 19:14:48 -0500

    protoc-gen-grpc-swift 1.1.0

---
+++ Formula/p/protoc-gen-grpc-swift.rb
@@ -0,0 +1,52 @@
+class ProtocGenGrpcSwift < Formula
+  desc "Protoc plugin for generating gRPC Swift stubs"
+  homepage "https://github.com/grpc/grpc-swift-protobuf"
+  url "https://github.com/grpc/grpc-swift-protobuf/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "fe19d6931605e957031369578ab03faa6b3b3ad8fdee0214a08275ef209f36f9"
+  license "Apache-2.0"
+  version_scheme 1
+  head "https://github.com/grpc/grpc-swift-protobuf.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on xcode: ["15.0", :build]
+  # https://swiftpackageindex.com/grpc/grpc-swift/documentation/grpccore/compatibility#Platforms
+  depends_on macos: :sequoia
+  depends_on "protobuf"
+  depends_on "swift-protobuf"
+
+  uses_from_macos "swift" => :build
+
+  def install
+    args = if OS.mac?
+      ["--disable-sandbox"]
+    else
+      ["--static-swift-stdlib"]
+    end
+    system "swift", "build", *args, "-c", "release", "--product", "protoc-gen-grpc-swift"
+    bin.install ".build/release/protoc-gen-grpc-swift"
+  end
+
+  test do
+    (testpath/"echo.proto").write <<~PROTO
+      syntax = "proto3";
+      service Echo {
+        rpc Get(EchoRequest) returns (EchoResponse) {}
+        rpc Expand(EchoRequest) returns (stream EchoResponse) {}
+        rpc Collect(stream EchoRequest) returns (EchoResponse) {}
+        rpc Update(stream EchoRequest) returns (stream EchoResponse) {}
+      }
+      message EchoRequest {
+        string text = 1;
+      }
+      message EchoResponse {
+        string text = 1;
+      }
+    PROTO
+    system Formula["protobuf"].opt_bin/"protoc", "echo.proto", "--grpc-swift_out=."
+    assert_path_exists testpath/"echo.grpc.swift"
+  end
+end
