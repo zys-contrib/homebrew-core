32b6eb069b6c9549b0a07ba34939377e76a7a109
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-06 05:20:40 +0000

    infisical: update 0.36.11 bottle.

--- Formula/i/infisical.rb
+++ Formula/i/infisical.rb
@@ -12,12 +12,12 @@ class Infisical < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "119b2a3cdb85af858270cdaa88d96ef0624484055c1517febaed490211be09d5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "119b2a3cdb85af858270cdaa88d96ef0624484055c1517febaed490211be09d5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "119b2a3cdb85af858270cdaa88d96ef0624484055c1517febaed490211be09d5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6596b16691e4f9296a8eca5642af4a1e10b62735ac16d87c239e6b489092bd19"
-    sha256 cellar: :any_skip_relocation, ventura:       "6596b16691e4f9296a8eca5642af4a1e10b62735ac16d87c239e6b489092bd19"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "054db4879fe510f789911d5c8ea617e461818043101e8433200f4839093ec2cd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b86286ae87df86cfabb35acc51a0c790737a7f7c949da2120ca3da8342f1aae6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b86286ae87df86cfabb35acc51a0c790737a7f7c949da2120ca3da8342f1aae6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b86286ae87df86cfabb35acc51a0c790737a7f7c949da2120ca3da8342f1aae6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7dce21002eae451028eed396758bae61804d4e8e5e0c2fdca13015b7f0d49df9"
+    sha256 cellar: :any_skip_relocation, ventura:       "7dce21002eae451028eed396758bae61804d4e8e5e0c2fdca13015b7f0d49df9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "42b1babd596b4c089e292f1aa7d86652df0af4b8cc1042b41ca867c8ac3884bc"
   end
 
   depends_on "go"
