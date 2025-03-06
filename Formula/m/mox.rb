9ac6888e569aa79d87aad922fe89945d01f9cedc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-06 01:02:59 +0000

    mox: add 0.0.14 bottle.

--- Formula/m/mox.rb
+++ Formula/m/mox.rb
@@ -5,6 +5,15 @@ class Mox < Formula
   sha256 "77d6424e6b4cdafdb5a19ea79adeef982c426c58804671a6a0167edda883d50c"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b7f267981e17ec7e001f6c88b2c372e8e6da332a6efb28794ec589e7d480c7c0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b7f267981e17ec7e001f6c88b2c372e8e6da332a6efb28794ec589e7d480c7c0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b7f267981e17ec7e001f6c88b2c372e8e6da332a6efb28794ec589e7d480c7c0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b699c3df96a1dca8de7b8d09ca8088cd290af42bc9b2deb09a0d0651b18297e2"
+    sha256 cellar: :any_skip_relocation, ventura:       "b699c3df96a1dca8de7b8d09ca8088cd290af42bc9b2deb09a0d0651b18297e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0c2d9848f3d8ce296861407977a374744c8260c484b4498e4dd0ce9c08046c8b"
+  end
+
   depends_on "go" => :build
 
   # Allow setting the version during buildtime
