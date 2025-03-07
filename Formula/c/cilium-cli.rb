ba058f93b2df27ca6da3b303ebc13f3a2e6d3f3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 22:53:14 +0000

    cilium-cli 0.18.2

--- Formula/c/cilium-cli.rb
+++ Formula/c/cilium-cli.rb
@@ -1,8 +1,8 @@
 class CiliumCli < Formula
   desc "CLI to install, manage & troubleshoot Kubernetes clusters running Cilium"
   homepage "https://cilium.io"
-  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.18.1.tar.gz"
-  sha256 "d07a964bd097b49193f05795c431f2774e14cb658f68725d181ad3484ad9fcf3"
+  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.18.2.tar.gz"
+  sha256 "d5ad24a477354396c4765c9b76d3e85ee53a63ff9fecd58bf144f624d16e2a56"
   license "Apache-2.0"
 
   # Upstream uses GitHub releases to indicate that a version is released
