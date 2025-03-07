567ccd153bfebc9e109a9cfae89389535340906e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 17:08:03 +0000

    kubecfg 0.36.0

--- Formula/k/kubecfg.rb
+++ Formula/k/kubecfg.rb
@@ -1,8 +1,8 @@
 class Kubecfg < Formula
   desc "Manage complex enterprise Kubernetes environments as code"
   homepage "https://github.com/kubecfg/kubecfg"
-  url "https://github.com/kubecfg/kubecfg/archive/refs/tags/v0.35.2.tar.gz"
-  sha256 "a59fe54d60230eebed172a2e72459234e3dfe5da5182608b4e824b023fec878a"
+  url "https://github.com/kubecfg/kubecfg/archive/refs/tags/v0.36.0.tar.gz"
+  sha256 "0f135465c512f8d5017f30f595669bed6a1c65b39b10178ede6989e15cbc84a9"
   license "Apache-2.0"
 
   bottle do
