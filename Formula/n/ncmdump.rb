7e808b12401f0b9d6ad466609976ef9c0c02053f
botantony <antonsm21@gmail.com>
2025-03-07 23:44:23 +0100

    ncmdump 1.5.0

--- Formula/n/ncmdump.rb
+++ Formula/n/ncmdump.rb
@@ -1,8 +1,8 @@
 class Ncmdump < Formula
   desc "Convert Netease Cloud Music ncm files to mp3/flac files"
   homepage "https://github.com/taurusxin/ncmdump"
-  url "https://github.com/taurusxin/ncmdump/archive/refs/tags/1.2.1.tar.gz"
-  sha256 "a1bd97fd1b46f9ba4ffaac0cf6cf1e920b49bf6ec753870ad0e6e07a72c2de2d"
+  url "https://github.com/taurusxin/ncmdump/archive/refs/tags/1.5.0.tar.gz"
+  sha256 "f59e4e5296b939c88a45d37844545d2e9c4c2cd3bb4f1f1a53a8c4fb72d53a2d"
   license "MIT"
   head "https://github.com/taurusxin/ncmdump.git", branch: "main"
 
@@ -17,13 +17,22 @@ class Ncmdump < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "8ef82870ff5763efecb19096dd73cc4090c392d694cdce5e4e1d22cd169cb568"
   end
 
+  depends_on "cmake" => :build
   depends_on "taglib"
 
   def install
-    os = OS.mac? ? "macos-" : "linux"
-    arch = Hardware::CPU.intel? ? "intel" : Hardware::CPU.arch.to_s if OS.mac?
-    system "make", "#{os}#{arch}"
-    bin.install "ncmdump"
+    # Use Homebrew's taglib
+    # See discussion: https://github.com/taurusxin/ncmdump/discussions/49
+    inreplace "CMakeLists.txt", "add_subdirectory(taglib)\n", ""
+    inreplace buildpath/"src/ncmcrypt.cpp" do |s|
+      s.gsub! "#define TAGLIB_STATIC\n", ""
+      s.gsub! "#include \"taglib/tag.h\"", "#include <taglib/tag.h>"
+      s.gsub!(%r{#include "taglib/.*/(.*)\.h"}, '#include <taglib/\1.h>')
+    end
+
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_PREFIX_PATH=#{Formula["taglib"].opt_prefix}", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
@@ -32,14 +41,8 @@ class Ncmdump < Formula
       sha256 "a1586bbbbad95019eee566411de58a57c3a3bd7c86d97f2c3c82427efce8964b"
     end
 
-    resource "homebrew-expect" do
-      url "https://raw.githubusercontent.com/taurusxin/ncmdump/2e40815b5a83236f3feb44720954dd3a02eb00f1/test/expect.bin"
-      sha256 "6e0de7017c996718a8931bc3ec8061f27ed73bee10efe6b458c10191a1c2aac2"
-    end
-
-    resources.each { |r| r.stage(testpath) }
+    resource("homebrew-test").stage(testpath)
     system bin/"ncmdump", "#{testpath}/test.ncm"
     assert_path_exists testpath/"test.flac"
-    assert_equal File.read("test.flac"), File.read("expect.bin")
   end
 end
