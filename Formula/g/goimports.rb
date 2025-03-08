4b7020b2c0d9ca48bf80be22529736288d3620da
Nicolas Karolak <nicolas@karolak.fr>
2025-03-08 15:52:37 +0100

    goimports 0.31.0

---
+++ Formula/g/goimports.rb
@@ -0,0 +1,33 @@
+class Goimports < Formula
+  desc "Go formatter that additionally inserts import statements"
+  homepage "https://pkg.go.dev/golang.org/x/tools/cmd/goimports"
+  url "https://github.com/golang/tools/archive/refs/tags/v0.31.0.tar.gz"
+  sha256 "e5d74f1e63a1ee669e75e76668cea1b110e2b9d19c67710f60939ee38070a5a7"
+  license "BSD-3-Clause"
+  head "https://github.com/golang/tools.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go"
+
+  def install
+    chdir "cmd/goimports" do
+      system "go", "build", *std_go_args
+    end
+  end
+
+  test do
+    (testpath/"main.go").write <<~GO
+      package main
+
+      func main() {
+        fmt.Println("hello")
+      }
+    GO
+
+    assert_match(/\+import "fmt"/, shell_output("#{bin}/goimports -d #{testpath}/main.go"))
+  end
+end
