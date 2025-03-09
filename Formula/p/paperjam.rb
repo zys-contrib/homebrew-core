9615cc24bf1d6ec49d4f259d503562122b1dd17a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 22:12:10 +0000

    paperjam: update 1.2.1_1 bottle.

--- Formula/p/paperjam.rb
+++ Formula/p/paperjam.rb
@@ -12,12 +12,12 @@ class Paperjam < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "1173139f00d87d9853a76d10a2c05b0ab451e5d5388582cf3d10291d8b70a66e"
-    sha256 cellar: :any,                 arm64_sonoma:  "39c2d7a60e6ccc75a168b7e0be8696b91a033fd360d426403ab50e20df7043ee"
-    sha256 cellar: :any,                 arm64_ventura: "e879f41bf1c0ef0f824e302e2515b921fe9009de00de18c2ba2035c259b2f90c"
-    sha256 cellar: :any,                 sonoma:        "a6d35687e6f83ac011a32b169878d55656477a5db46531a5d9a80e46ffafa303"
-    sha256 cellar: :any,                 ventura:       "7823de97267e2efebc6efaba7ae5c4448269d79c98ab613c6115c1d033cfce5e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ecb416231708d36a49ec9dd59319a9cf9ae1443d3c962f16b8b4d514d3b12d7d"
+    sha256 cellar: :any,                 arm64_sequoia: "271b0d2b33c3f06f81fac58248a81a95eef0bcc36929230e2bad7b4d38ff34db"
+    sha256 cellar: :any,                 arm64_sonoma:  "4df949ea647a2056ea7d7ab0ea2d298f45da5643ae92c25aaac80aad7e1301c7"
+    sha256 cellar: :any,                 arm64_ventura: "1fd3b138f16296ab6107a43387eb5ea3b54e5dac2358b7b5b6e8bc6c7951f512"
+    sha256 cellar: :any,                 sonoma:        "0d6af7df1e10f96f42f56cf222c6bfb35d384ed547f1b8b0e11352906a5af41c"
+    sha256 cellar: :any,                 ventura:       "2fc406ca453bf24ade3b65111f80bb3fd0b1a761c316ee48540ed8341afc6d63"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05712c9aeabd27e0ca3dd38ce377228044e31bccb7ee9bfb0a218b239f4838be"
   end
 
   depends_on "asciidoc" => :build
