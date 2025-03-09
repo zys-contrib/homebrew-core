be56e4ebbdfd93164ec372b1c5ff8caf5e38b852
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 15:22:11 +0000

    chiko: update 0.1.0 bottle.

--- Formula/c/chiko.rb
+++ Formula/c/chiko.rb
@@ -7,12 +7,12 @@ class Chiko < Formula
   head "https://github.com/felangga/chiko.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3bf3bc9f7cf55b56f24b95bbc15efe3e72f2f7fb95306b7bcfd5a5ce888fc3f7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3bf3bc9f7cf55b56f24b95bbc15efe3e72f2f7fb95306b7bcfd5a5ce888fc3f7"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3bf3bc9f7cf55b56f24b95bbc15efe3e72f2f7fb95306b7bcfd5a5ce888fc3f7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0f5d3d68b54b6978276870173f9097ed716f6028c92c7fd8ca7b97ebcf7ea7ea"
-    sha256 cellar: :any_skip_relocation, ventura:       "0f5d3d68b54b6978276870173f9097ed716f6028c92c7fd8ca7b97ebcf7ea7ea"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "985fec7e611dd6be7e2925396906174a3f98fa543b044e311408d733948fe8a7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fd41383d3f47e13a627ff206c0d872c0f9f9c370d35b11c1df23d47ce0bc9a01"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fd41383d3f47e13a627ff206c0d872c0f9f9c370d35b11c1df23d47ce0bc9a01"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fd41383d3f47e13a627ff206c0d872c0f9f9c370d35b11c1df23d47ce0bc9a01"
+    sha256 cellar: :any_skip_relocation, sonoma:        "95f3adb6387216e35e0a08c60849da2cf505d272d722a945552e828938223a1f"
+    sha256 cellar: :any_skip_relocation, ventura:       "95f3adb6387216e35e0a08c60849da2cf505d272d722a945552e828938223a1f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e08c09d30f7c1d79a484bdfc1a4d97f36cb85ac0022a167af63055b49463bbcf"
   end
 
   depends_on "go" => :build
