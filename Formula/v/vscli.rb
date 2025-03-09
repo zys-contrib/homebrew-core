fce8e6a238eafec0f019b8481d267d08fca3bfa4
Rui Chen <rui@chenrui.dev>
2025-03-09 13:42:18 -0400

    vscli 1.3.0
    autobump: add vscli
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vscli.rb
+++ Formula/v/vscli.rb
@@ -1,9 +1,10 @@
 class Vscli < Formula
   desc "CLI/TUI that launches VSCode projects, with a focus on dev containers"
   homepage "https://github.com/michidk/vscli"
-  url "https://github.com/michidk/vscli/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "5d3eed6c34541fca9f98d766a94b287f648af43d219d68e8546f9862abc34259"
+  url "https://github.com/michidk/vscli/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "91384db69cf5b32af96178df79634d5707eaffaf3517a567e965d3c5a32f81fb"
   license "MIT"
+  head "https://github.com/michidk/vscli.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "a7c075db90e8c2d6a452882b22ee9bcd0cf5982aae60c86e5c26b0564b50d535"
@@ -23,7 +24,7 @@ class Vscli < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/vscli --version")
 
-    output = pipe_output("#{bin}/vscli open --dry-run 2>&1")
-    assert_match "No dev container found, opening on host system with code..", output
+    output = shell_output("#{bin}/vscli open --dry-run 2>&1", 1)
+    assert_match "No dev container found, opening on host system with Visual Studio Code...", output
   end
 end
