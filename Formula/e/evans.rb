7b1a57d6fdf8dedd25667413a425ade15eb3fb4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 04:01:01 +0000

    evans: add 0.10.11 bottle.

--- Formula/e/evans.rb
+++ Formula/e/evans.rb
@@ -6,6 +6,15 @@ class Evans < Formula
   license "MIT"
   head "https://github.com/ktr0731/evans.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4d1bb213d01116bdef091ca159ddeba2e97315958cfecc1526d65d67e5e29ae7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d1bb213d01116bdef091ca159ddeba2e97315958cfecc1526d65d67e5e29ae7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4d1bb213d01116bdef091ca159ddeba2e97315958cfecc1526d65d67e5e29ae7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "afb78ceadf7221eb37ffa03f0ad50c565085486ff3b51440bfeb9eee6e86b1d7"
+    sha256 cellar: :any_skip_relocation, ventura:       "afb78ceadf7221eb37ffa03f0ad50c565085486ff3b51440bfeb9eee6e86b1d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d1551ab2a16f1e6c5f692fb21f44821ed7ddbdec7fd138fb7af9928ad3d4ec62"
+  end
+
   depends_on "go" => :build
 
   def install
