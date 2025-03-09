a919bed7fca00a9cc5464d96a1ec71abe4f0e03a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 04:04:24 +0000

    restish: add 0.20.0 bottle.

--- Formula/r/restish.rb
+++ Formula/r/restish.rb
@@ -6,6 +6,15 @@ class Restish < Formula
   license "MIT"
   head "https://github.com/danielgtaylor/restish.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "af849ab00700354c245e51554623c3ebff73e73c96d4111b27d91fd63c3a46de"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "af849ab00700354c245e51554623c3ebff73e73c96d4111b27d91fd63c3a46de"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "af849ab00700354c245e51554623c3ebff73e73c96d4111b27d91fd63c3a46de"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e095d39764cc37c5a7d04ff8f7844fe85ef49872a48d4edb10e8456aa514b47a"
+    sha256 cellar: :any_skip_relocation, ventura:       "e095d39764cc37c5a7d04ff8f7844fe85ef49872a48d4edb10e8456aa514b47a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a5dc617d1e1861651fba58b970338a23cf9fabbba515d11b4e8a51e8898e9ec"
+  end
+
   depends_on "go" => :build
 
   def install
