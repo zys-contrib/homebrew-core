9b4cf54ae819580cc31e16f58740d65d92df3704
Michael Cho <michael@michaelcho.dev>
2025-03-10 19:56:13 -0400

    falcosecurity-libs 0.20.0 (new formula)

---
+++ Formula/f/falcosecurity-libs.rb
@@ -0,0 +1,65 @@
+class FalcosecurityLibs < Formula
+  desc "Core libraries for Falco and Sysdig"
+  homepage "https://falcosecurity.github.io/libs/"
+  url "https://github.com/falcosecurity/libs/archive/refs/tags/0.20.0.tar.gz"
+  sha256 "4ae6ddb42a1012bacd88c63abdaa7bd27ca0143c4721338a22c45597e63bc99d"
+  license all_of: [
+    "Apache-2.0",
+    { any_of: ["GPL-2.0-only", "MIT"] }, # driver/
+    { "GPL-2.0-only" => { with: "Linux-syscall-note" } }, # userspace/libscap/compat/
+  ]
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => :build
+  depends_on "valijson" => :build
+  depends_on "googletest" => :test
+  depends_on "jsoncpp"
+  depends_on "re2"
+  depends_on "tbb"
+  depends_on "uthash" # headers needed for libscap/uthash_ext.h
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "abseil"
+    depends_on "curl"
+    depends_on "elfutils"
+    depends_on "grpc"
+    depends_on "protobuf"
+  end
+
+  def install
+    args = %W[
+      -DBUILD_DRIVER=OFF
+      -DBUILD_LIBSCAP_GVISOR=OFF
+      -DBUILD_LIBSCAP_EXAMPLES=OFF
+      -DBUILD_LIBSINSP_EXAMPLES=OFF
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DCREATE_TEST_TARGETS=OFF
+      -DFALCOSECURITY_LIBS_VERSION=#{version}
+      -DUSE_BUNDLED_DEPS=OFF
+    ]
+    # TODO: remove on next release which has dropped option
+    # https://github.com/falcosecurity/libs/commit/d45d53a1e0e397658d23b216c3c1716a68481554
+    args << "-DMINIMAL_BUILD=ON" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "test/libscap/test_suites/userspace/scap_event.cpp"
+  end
+
+  test do
+    system ENV.cxx, "-std=c++17", pkgshare/"scap_event.cpp", "-o", "test",
+                    "-I#{include}/falcosecurity",
+                    "-L#{Formula["googletest"].lib}", "-L#{lib}",
+                    "-lgtest", "-lgtest_main", "-lsinsp"
+    system "./test"
+  end
+end
