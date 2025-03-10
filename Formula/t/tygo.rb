9505c8ab9e77e7a78450efc31c6386051a6b1c70
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 11:25:29 +0000

    tygo: update 0.2.18 bottle.

--- Formula/t/tygo.rb
+++ Formula/t/tygo.rb
@@ -8,12 +8,12 @@ class Tygo < Formula
   head "https://github.com/gzuidhof/tygo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec71365ca25be8b0695248d573b60ed77bc5165d902049bb9842098a9097d562"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ec71365ca25be8b0695248d573b60ed77bc5165d902049bb9842098a9097d562"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ec71365ca25be8b0695248d573b60ed77bc5165d902049bb9842098a9097d562"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d931db0194cded9eeb030acc08e037599e99931611082175012c611f70de21f8"
-    sha256 cellar: :any_skip_relocation, ventura:       "d931db0194cded9eeb030acc08e037599e99931611082175012c611f70de21f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a559c9e90fcf6faec5c8603121223975717d3912aa22acc252c9c326201aa89"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "08e314c0945c7d47759b02934f9a1a3d3321de212bd62d4fae71c48a1effbd75"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "08e314c0945c7d47759b02934f9a1a3d3321de212bd62d4fae71c48a1effbd75"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "08e314c0945c7d47759b02934f9a1a3d3321de212bd62d4fae71c48a1effbd75"
+    sha256 cellar: :any_skip_relocation, sonoma:        "58078f2a3fd657c1c18af1e15b6d8431de201df2cdffd66566f5d43e5897c7c0"
+    sha256 cellar: :any_skip_relocation, ventura:       "58078f2a3fd657c1c18af1e15b6d8431de201df2cdffd66566f5d43e5897c7c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2f75998b51e4061e7b4d2c366b58b77e6914105cd2d3afbbaabbb06147646e0b"
   end
 
   depends_on "go" => [:build, :test]
