d87d1084d1739cf81327f724e6a0abdae557deaa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 13:56:59 +0000

    sentry-native: add 0.8.1 bottle.

--- Formula/s/sentry-native.rb
+++ Formula/s/sentry-native.rb
@@ -5,6 +5,15 @@ class SentryNative < Formula
   sha256 "65e3de5708e57f30e821a474f7bdd02bb8ccafd97b5553ec8ecb1791a161ef3d"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "bb88bd85d644a3143498af7de7aa6bfa1829d7aa74597288e806af99a22ea42b"
+    sha256 cellar: :any,                 arm64_sonoma:  "9ec6af2ada5caf735c422a6989b879e0afa104c0d8fdb582055a52fd3db632b6"
+    sha256 cellar: :any,                 arm64_ventura: "53ee50fc8d60ae5599fab68b9d8ced1b1b6e31647f99321778747c8397a0d588"
+    sha256 cellar: :any,                 sonoma:        "355846b2dd1f3a74663c297956523988fa579d131c9dc469d629c2900ffa602a"
+    sha256 cellar: :any,                 ventura:       "14cecef7d13f4be1781f36e1bded6f436b5d77c0935abc6a537a384e078470d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b1b80b8f129431ba46f99e8497654019887bc9ef32f05fd92936060489372ee5"
+  end
+
   depends_on "cmake" => :build
   uses_from_macos "curl"
   uses_from_macos "zlib"
