600e70b94ba370ad3cbf7fd0051bb9975e2aabf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 14:05:52 +0000

    mago 0.19.3

--- Formula/m/mago.rb
+++ Formula/m/mago.rb
@@ -1,8 +1,8 @@
 class Mago < Formula
   desc "Toolchain for PHP to help developers write better code"
   homepage "https://github.com/carthage-software/mago"
-  url "https://github.com/carthage-software/mago/archive/refs/tags/0.19.1.tar.gz"
-  sha256 "6634f4252c17856ee12d2f15fc46c0ef24fc14d94511f6857071043a098ee6d1"
+  url "https://github.com/carthage-software/mago/archive/refs/tags/0.19.3.tar.gz"
+  sha256 "7905f406afafcbfd735f2c13043b8fb2326e983b176655886b4d159864d18f73"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
