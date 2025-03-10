bee34d6e94c242437b0e38f4dc1389353f9d4412
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 20:35:43 +0000

    borgmatic: update 1.9.14 bottle.

--- Formula/b/borgmatic.rb
+++ Formula/b/borgmatic.rb
@@ -8,12 +8,12 @@ class Borgmatic < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "812a85fb426806605681a340e8fb3531567f2a9618b313ccf05a9c465b842f83"
-    sha256 cellar: :any,                 arm64_sonoma:  "dc39d8998414c929101a6dd63973dea925919cdcad6d9491e05436b57adc5bee"
-    sha256 cellar: :any,                 arm64_ventura: "9b33c697c2e903b87fe79b19ed86a25b3b32eaf5aa4050eeb7a7c43daebb02d7"
-    sha256 cellar: :any,                 sonoma:        "910dfe435cc75473f8d468fd37dda20a2bafcce09d2a68e89907f1cbff6dca3b"
-    sha256 cellar: :any,                 ventura:       "e0862cf04f9549c2b0daeaa4e84029ab42f1d4a5a0da687efccc9a22a822e232"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ff2fc71a38f35319874bc7f442f2db7df50a10854043e2561c19b3c56d8b5bc1"
+    sha256 cellar: :any,                 arm64_sequoia: "4f3fb421bbb7785f78a14b9b8752aecb8fde909929fc5e2e22a70de814089ceb"
+    sha256 cellar: :any,                 arm64_sonoma:  "488b17b1034cf08e3fa5599765cdd1957046a6b63cb6998419454927e27ba522"
+    sha256 cellar: :any,                 arm64_ventura: "0e37b81433319885aeb4e3560141e750bb8c647f275718ac5b814ee310aa7701"
+    sha256 cellar: :any,                 sonoma:        "e9a6edd1a05a3fc02362b3b70d0c8429dc007864790eda113ede74e34baed66c"
+    sha256 cellar: :any,                 ventura:       "0867c1c3f2126a0b12dc480903d5ee714a04f42af042ca25e1567a9d9f7aa453"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b930466b239a80e674c6aced80ee16aed66dd22a94461f80515df5303c9a2961"
   end
 
   depends_on "rust" => :build # for rpds-py
