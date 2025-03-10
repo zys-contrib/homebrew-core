8b561182d49f0bae61527ce020278456eb171b14
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 14:08:50 +0000

    oasdiff 1.11.1
    oasdiff: update repo location
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/oasdiff.rb
+++ Formula/o/oasdiff.rb
@@ -1,10 +1,10 @@
 class Oasdiff < Formula
   desc "OpenAPI Diff and Breaking Changes"
   homepage "https://www.oasdiff.com/"
-  url "https://github.com/Tufin/oasdiff/archive/refs/tags/v1.10.29.tar.gz"
-  sha256 "aeb12389da462ef83e7348ac8b9e90d07af526ce3a86a20b92b401f2c109bb80"
+  url "https://github.com/oasdiff/oasdiff/archive/refs/tags/v1.11.1.tar.gz"
+  sha256 "d77b2604dcdf1656b92ea96c45604af4ad3541b1a5be03f3830b1fa2a273f728"
   license "Apache-2.0"
-  head "https://github.com/Tufin/oasdiff.git", branch: "main"
+  head "https://github.com/oasdiff/oasdiff.git", branch: "main"
 
   # Livecheck against GitHub latest releases is necessary because there was a v1.6.0 release after v2.1.2.
   livecheck do
@@ -24,7 +24,7 @@ class Oasdiff < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X github.com/tufin/oasdiff/build.Version=#{version}"
+    ldflags = "-s -w -X github.com/oasdiff/oasdiff/build.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
     generate_completions_from_executable(bin/"oasdiff", "completion")
@@ -32,12 +32,12 @@ class Oasdiff < Formula
 
   test do
     resource "homebrew-openapi-test1.yaml" do
-      url "https://raw.githubusercontent.com/Tufin/oasdiff/8fdb99634d0f7f827810ee1ba7b23aa4ada8b124/data/openapi-test1.yaml"
+      url "https://raw.githubusercontent.com/oasdiff/oasdiff/8fdb99634d0f7f827810ee1ba7b23aa4ada8b124/data/openapi-test1.yaml"
       sha256 "f98cd3dc42c7d7a61c1056fa5a1bd3419b776758546cf932b03324c6c1878818"
     end
 
     resource "homebrew-openapi-test5.yaml" do
-      url "https://raw.githubusercontent.com/Tufin/oasdiff/8fdb99634d0f7f827810ee1ba7b23aa4ada8b124/data/openapi-test5.yaml"
+      url "https://raw.githubusercontent.com/oasdiff/oasdiff/8fdb99634d0f7f827810ee1ba7b23aa4ada8b124/data/openapi-test5.yaml"
       sha256 "07e872b876df5afdc1933c2eca9ee18262aeab941dc5222c0ae58363d9eec567"
     end
 
