c3d13f499d3290e70a7137e8cba78d9a92beefe2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 11:14:48 +0000

    fpm: add 0.11.0 bottle.

--- Formula/f/fpm.rb
+++ Formula/f/fpm.rb
@@ -5,6 +5,15 @@ class Fpm < Formula
   sha256 "988a3317ee2448ee7207d0a29410f08a79c86bddac3314b2a175801a9cf58d27"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "21e756e56a9c35cbb2c0849bf7767ae194283d6c466de61e3ec9c5b922f33373"
+    sha256 cellar: :any,                 arm64_sonoma:  "2ed294de5db197bb728de646f43560bd0cac9702f848e97ce51b758697258b4c"
+    sha256 cellar: :any,                 arm64_ventura: "41324372541beac98661f13ac43ebe6b04e022d77b911896a4a968acd25f31b6"
+    sha256 cellar: :any,                 sonoma:        "4f07fdd691265418345caf32995c53ace46f85580433a3430b17415cd8a77bcf"
+    sha256 cellar: :any,                 ventura:       "13c413c368aa578e3f01029eb0e6f32145ae27dbf3039521fa3a4a28ccf26e92"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "484e5f0966895beaa67c50b37422f287e35787da9a4aa3e2940b07a479c236c0"
+  end
+
   depends_on "gcc" # for gfortran
 
   def install
