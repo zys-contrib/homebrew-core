b1519dac840afeb316dfaf148dda60da9c508e15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-11 10:55:57 +0000

    unciv: update 4.15.16 bottle.

--- Formula/u/unciv.rb
+++ Formula/u/unciv.rb
@@ -11,7 +11,7 @@ class Unciv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9a52da9bcf45a5442e83b18bf67a31f8bf855e78cddb92e11f0d92b0ea6fe040"
+    sha256 cellar: :any_skip_relocation, all: "45470bcf77ecc546d217e675303307d7be09bb8c0fce15b047a752081a510a8f"
   end
 
   depends_on "openjdk"
