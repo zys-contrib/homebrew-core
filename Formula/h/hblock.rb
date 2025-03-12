51a72edd19c2bf6bb8944bcb93779754ecbaf49c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-12 19:58:00 +0000

    hblock 3.5.1

--- Formula/h/hblock.rb
+++ Formula/h/hblock.rb
@@ -1,8 +1,8 @@
 class Hblock < Formula
   desc "Adblocker that creates a hosts file from multiple sources"
   homepage "https://hblock.molinero.dev/"
-  url "https://github.com/hectorm/hblock/archive/refs/tags/v3.5.0.tar.gz"
-  sha256 "99de5c6231f2495efaecf53db957272d216c652c2fb795c8d30a2f2e490e8098"
+  url "https://github.com/hectorm/hblock/archive/refs/tags/v3.5.1.tar.gz"
+  sha256 "af98a6753e5de1406b63cd1fabf4b3eae84816168c532dae40c83092acb69941"
   license "MIT"
 
   bottle do
