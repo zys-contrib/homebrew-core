b23882845182075f355fcd0bc293d56c067c8cd4
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2025-03-12 16:42:07 -0600

    chapel 2.4

--- Formula/c/chapel.rb
+++ Formula/c/chapel.rb
@@ -3,10 +3,9 @@ class Chapel < Formula
 
   desc "Programming language for productive parallel computing at scale"
   homepage "https://chapel-lang.org/"
-  url "https://github.com/chapel-lang/chapel/releases/download/2.3.0/chapel-2.3.0.tar.gz"
-  sha256 "0185970388aef1f1fae2a031edf060d5eac4eb6e6b1089e7e3b15a130edd8a31"
+  url "https://github.com/chapel-lang/chapel/releases/download/2.4.0/chapel-2.4.0.tar.gz"
+  sha256 "a51a472488290df12d1657db2e7118ab519743094f33650f910d92b54c56f315"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/chapel-lang/chapel.git", branch: "main"
 
   bottle do
@@ -36,7 +35,6 @@ class Chapel < Formula
     # It should be noted that this will expand to: 'for cmd in python3.13 python3 python python2; do'
     # in our find-python.sh script.
     inreplace "util/config/find-python.sh", /^(for cmd in )(python3 )/, "\\1#{python} \\2"
-    inreplace "third-party/chpl-venv/Makefile", "python3 -c ", "#{python} -c "
 
     # a lot of scripts have a python3 or python shebang, which does not point to python3.12 anymore
     Pathname.glob("**/*.py") do |pyfile|
@@ -56,7 +54,7 @@ class Chapel < Formula
     (libexec/"chplconfig").write <<~EOS
       CHPL_RE2=bundled
       CHPL_GMP=system
-      CHPL_MEM=jemalloc
+      CHPL_TARGET_MEM=jemalloc
       CHPL_TARGET_JEMALLOC=system
       CHPL_HWLOC=system
       CHPL_LLVM_CONFIG=#{llvm.opt_bin}/llvm-config
@@ -84,7 +82,6 @@ class Chapel < Formula
       rm_r("third-party/llvm/llvm-src/")
       rm_r("third-party/gasnet/gasnet-src/")
       rm_r("third-party/libfabric/libfabric-src/")
-      rm_r("third-party/fltk/fltk-1.3.8-source.tar.gz")
       rm_r("third-party/libunwind/libunwind-src/")
       rm_r("third-party/gmp/gmp-src/")
       rm_r("third-party/qthread/qthread-src/")
