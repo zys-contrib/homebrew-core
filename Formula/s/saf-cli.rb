3a39aecfa21abc0ad5bca924e3d490235bb6fba3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-12 04:44:33 +0000

    saf-cli: update 1.4.20 bottle.

--- Formula/s/saf-cli.rb
+++ Formula/s/saf-cli.rb
@@ -6,12 +6,12 @@ class SafCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6b9704227909d9c5312052fbf165c2cabef79450627d2cbc246c54fb5cea2906"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6b9704227909d9c5312052fbf165c2cabef79450627d2cbc246c54fb5cea2906"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6b9704227909d9c5312052fbf165c2cabef79450627d2cbc246c54fb5cea2906"
-    sha256 cellar: :any_skip_relocation, sonoma:        "29caaaeca6cc4cdc3f5dd55aa6b49ca2d076c7badd3d447104444a59431077e2"
-    sha256 cellar: :any_skip_relocation, ventura:       "29caaaeca6cc4cdc3f5dd55aa6b49ca2d076c7badd3d447104444a59431077e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b9704227909d9c5312052fbf165c2cabef79450627d2cbc246c54fb5cea2906"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6144d26c414053dc74c93974c499a709344899a4a95d5dbb95dff5d9847cf88a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6144d26c414053dc74c93974c499a709344899a4a95d5dbb95dff5d9847cf88a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6144d26c414053dc74c93974c499a709344899a4a95d5dbb95dff5d9847cf88a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8af07ac28bd4bed17d61fbc2196faec7c0839d702632252c735433cabcc3fdcf"
+    sha256 cellar: :any_skip_relocation, ventura:       "8af07ac28bd4bed17d61fbc2196faec7c0839d702632252c735433cabcc3fdcf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32526d3b5e442c083cb4c7eb756c95192b36f7b2aed51b680756a6cbaa04036f"
   end
 
   depends_on "node"
