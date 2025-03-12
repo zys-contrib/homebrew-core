a7573294e0b9c502db771a4088fbf7b86d21dc62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-12 18:22:02 +0000

    deadfinder: update 1.7.0 bottle.

--- Formula/d/deadfinder.rb
+++ Formula/d/deadfinder.rb
@@ -7,12 +7,12 @@ class Deadfinder < Formula
   head "https://github.com/hahwul/deadfinder.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "efc9928572c884c80a7e59c3c675ea46d781a106f4e1a236acec6ed7f0976905"
-    sha256 cellar: :any,                 arm64_sonoma:  "b731c16bf1072df8a1011135c1a14183307c2c0f9bf10c6d4e6c7dd2aa8cfb01"
-    sha256 cellar: :any,                 arm64_ventura: "f765a8266eed82684d1a041f3d3e113cba5f24e406f108b08b7225cbf72beef3"
-    sha256 cellar: :any,                 sonoma:        "1a73db10abec6cb50b32b4994be240a9ca70cfa7cac16a8e217455b00600bf7d"
-    sha256 cellar: :any,                 ventura:       "f89f8c61528f8e464eb8c37a4cf0d78e3885c3a4e2fada667bee2861a0c849a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b5fb674b529b8a3549a7af2244045f8d3a4982c45351829f1856998e0bccd649"
+    sha256 cellar: :any,                 arm64_sequoia: "3802f961fbb591240f1c49edb47c6eb7e64371681571fe83628f481a1a026270"
+    sha256 cellar: :any,                 arm64_sonoma:  "3974c5c083fc9a56f25802b003367ee4b63c95aa26431c1eb72d87083eb135c5"
+    sha256 cellar: :any,                 arm64_ventura: "fdd069f39c89ddedb5f4171852c3572e7afcd2df697d53406b79e915a47979a0"
+    sha256 cellar: :any,                 sonoma:        "2fa21d9440b959062d56c84129c2c79a1ae4480707c65ba021ec1574d7423547"
+    sha256 cellar: :any,                 ventura:       "f4b93d6e0266b8336349e9d98eb35fe6e57d9c9f83e542922fad7cc2d2b638ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "92657c8342bb2a52ee4a33e5d56a884d07fc4e9e8cfd7d34418f6110db977578"
   end
 
   depends_on "ruby"
