7762bf1c2a533f16e33d4d575f63cf7fbbc7e6a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-12 17:15:11 +0000

    tailwindcss-language-server 0.14.9

--- Formula/t/tailwindcss-language-server.rb
+++ Formula/t/tailwindcss-language-server.rb
@@ -1,8 +1,8 @@
 class TailwindcssLanguageServer < Formula
   desc "LSP for TailwindCSS"
   homepage "https://github.com/tailwindlabs/tailwindcss-intellisense/tree/HEAD/packages/tailwindcss-language-server"
-  url "https://github.com/tailwindlabs/tailwindcss-intellisense/archive/refs/tags/v0.14.8.tar.gz"
-  sha256 "c0446b820a73b2524de9242afb1369a58bbc8275a5798b94d29f3a5e0ad02dcf"
+  url "https://github.com/tailwindlabs/tailwindcss-intellisense/archive/refs/tags/v0.14.9.tar.gz"
+  sha256 "2616d7da876b9b81ded4db27f4e2e25831a042877c80c0f24c3d796ab71fac89"
   license "MIT"
 
   livecheck do
@@ -21,7 +21,7 @@ class TailwindcssLanguageServer < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "741a069ef27553a58ed7dc6d7833257950df65ac29fb9fe4854c4263f75a6dd1"
   end
 
-  depends_on "pnpm" => :build
+  depends_on "pnpm@9" => :build
   depends_on "node"
 
   def install
