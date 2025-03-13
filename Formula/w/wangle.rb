5dff168ea283dcb5fc0a865e53bb04e8b9fb2f83
Rui Chen <rui@chenrui.dev>
2025-03-12 21:29:32 -0400

    wangle 2025.03.10.00
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/wangle.rb
+++ Formula/w/wangle.rb
@@ -1,8 +1,8 @@
 class Wangle < Formula
   desc "Modular, composable client/server abstractions framework"
   homepage "https://github.com/facebook/wangle"
-  url "https://github.com/facebook/wangle/archive/refs/tags/v2025.03.03.00.tar.gz"
-  sha256 "5bdff857eea06e665c6fdb39af4c9c6e1f9b87efb1901bf72fadf7e695c92c32"
+  url "https://github.com/facebook/wangle/archive/refs/tags/v2025.03.10.00.tar.gz"
+  sha256 "5d26ffc54fb47ecb041dcaf9d31e5f14fd3426b427108e3a87fb115211a00d11"
   license "Apache-2.0"
   head "https://github.com/facebook/wangle.git", branch: "main"
 
