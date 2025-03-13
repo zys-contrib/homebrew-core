6e5fbd186e337a4cc3a3c132431e0136045a8646
Michael Cho <michael@michaelcho.dev>
2025-03-13 15:13:22 -0400

    grafana-alloy 1.7.3 (new formula)

---
+++ Formula/g/grafana-alloy.rb
@@ -0,0 +1,66 @@
+class GrafanaAlloy < Formula
+  desc "OpenTelemetry Collector distribution with programmable pipelines"
+  homepage "https://grafana.com/oss/alloy-opentelemetry-collector/"
+  url "https://github.com/grafana/alloy/archive/refs/tags/v1.7.3.tar.gz"
+  sha256 "f45657c08d2097cf5dc35199d4f878a55ad58f8767f27867c6993c9ed58b596f"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
+  on_linux do
+    depends_on "systemd" # for go-systemd (dlopen-ed)
+  end
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/grafana/alloy/internal/build.Branch=HEAD
+      -X github.com/grafana/alloy/internal/build.Version=v#{version}
+      -X github.com/grafana/alloy/internal/build.BuildUser=#{tap.user}
+      -X github.com/grafana/alloy/internal/build.BuildDate=#{time.iso8601}
+    ]
+
+    # https://github.com/grafana/alloy/blob/main/tools/make/packaging.mk
+    tags = %w[netgo builtinassets]
+    tags << "promtail_journal_enabled" if OS.linux?
+
+    # Build the UI, which is baked into the final binary when the builtinassets
+    # tag is set.
+    system "yarn", "--cwd", "internal/web/ui"
+    system "yarn", "--cwd", "internal/web/ui", "run", "build"
+
+    system "go", "build", "-tags=#{tags.join(",")}", *std_go_args(ldflags:, output: bin/"alloy")
+
+    generate_completions_from_executable(bin/"alloy", "completion")
+  end
+
+  def post_install
+    pkgetc.mkpath
+  end
+
+  def caveats
+    "Alloy configuration directory is #{pkgetc}"
+  end
+
+  service do
+    run [opt_bin/"alloy", "run", "--storage.path=#{var}/lib/grafana-alloy/data", etc/"grafana-alloy"]
+    keep_alive true
+    log_path var/"log/grafana-alloy.log"
+    error_log_path var/"log/grafana-alloy.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/alloy --version")
+
+    port = free_port
+    pid = spawn bin/"alloy", "run", "--server.http.listen-addr=127.0.0.1:#{port}", testpath
+    sleep 10
+    output = shell_output("curl -s 127.0.0.1:#{port}/metrics")
+    assert_match "alloy_build_info", output
+  ensure
+    Process.kill "TERM", pid
+    Process.wait pid
+  end
+end
