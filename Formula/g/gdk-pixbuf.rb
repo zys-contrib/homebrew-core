424da75a20596466a6f57afffa9bc6bee5411553
Carlo Cabrera <github@carlo.cab>
2025-03-14 16:01:35 +0800

    gdk-pixbuf: revision bump
    Fixes Homebrew/discussions#5998

--- Formula/g/gdk-pixbuf.rb
+++ Formula/g/gdk-pixbuf.rb
@@ -4,6 +4,7 @@ class GdkPixbuf < Formula
   url "https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-2.42.12.tar.xz"
   sha256 "b9505b3445b9a7e48ced34760c3bcb73e966df3ac94c95a148cb669ab748e3c7"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
     rebuild 1
