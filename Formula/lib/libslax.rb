1acf388712c79bb76b48a451c5a006efe1b479b5
Rui Chen <rui@chenrui.dev>
2025-03-13 20:41:42 -0400

    libslax 3.1.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/lib/libslax.rb
+++ Formula/lib/libslax.rb
@@ -1,11 +1,9 @@
 class Libslax < Formula
   desc "Implementation of the SLAX language (an XSLT alternative)"
   homepage "https://github.com/Juniper/libslax/wiki"
-  url "https://github.com/Juniper/libslax/releases/download/0.22.1/libslax-0.22.1.tar.gz"
-  sha256 "4da6fb9886e50d75478d5ecc6868c90dae9d30ba7fc6e6d154fc92e6a48d9a95"
+  url "https://github.com/Juniper/libslax/releases/download/3.1.2/libslax-3.1.2.tar.gz"
+  sha256 "ec1a08620201ac27800fc85f36602b5b9d46f8963647fffe8f9269083b677189"
   license "BSD-3-Clause"
-  revision 1
-  head "https://github.com/Juniper/libslax.git", branch: "master"
 
   livecheck do
     url :stable
@@ -25,12 +23,18 @@ class Libslax < Formula
     sha256 x86_64_linux:   "433ed00d6104bc49ff4dc3bac740c7854e12e54d5c9f52c4db160f1673398f48"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  head do
+    url "https://github.com/Juniper/libslax.git", branch: "main"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  depends_on "bison" => :build
   depends_on "libtool" => :build
+  depends_on :macos # needs libxslt built --with-debugger
   depends_on "openssl@3"
 
-  uses_from_macos "bison" => :build
   uses_from_macos "curl"
   uses_from_macos "libedit"
   uses_from_macos "libxml2"
@@ -40,36 +44,9 @@ class Libslax < Formula
   conflicts_with "genometools", because: "both install `bin/gt`"
   conflicts_with "libxi", because: "both install `libxi.a`"
 
-  # Fix compilation when using bison 3.7.6+. Patch accepted upstream, remove on next release
-  patch do
-    url "https://github.com/Juniper/libslax/commit/cc693df657bc078cd11abe910cbb94ce2acaed67.patch?full_index=1"
-    sha256 "68cdafb11450cd07bdfd15e5309979040e5956c3e36d9f8978890c29c8f20e87"
-  end
-
-  # Fix detection of libxml2 in configure. Two following patches accepted upstream, remove on next release
-  patch do
-    url "https://github.com/Juniper/libslax/commit/5fda392d357b753f7e163f94b8795c028300b024.patch?full_index=1"
-    sha256 "0a424f900e76faa8f1f1c7de282455d1b77c402329a6dc0be7e6370e9aa790de"
-  end
-
-  patch do
-    url "https://github.com/Juniper/libslax/commit/c1b0ba1a342bd4f1ee58f8a339cbd29938d58ba9.patch?full_index=1"
-    sha256 "fa5ecd56672843838cef62d7d44520613c5fa0e5904e3497266d0ee45b16df04"
-  end
-
   def install
-    # Upstream patches have already bumped package version in configure.ac ahead of new release being tagged,
-    # remove on next release
-    inreplace "configure.ac",
-              "AC_INIT([libslax],[0.22.2],[phil@juniper.net])",
-              "AC_INIT([libslax],[#{version}],[phil@juniper.net])"
-
-    system "autoreconf", "--force", "--install", "--verbose"
-
-    args = std_configure_args + %w[--enable-libedit]
-    args << "--with-sqlite3=#{Formula["sqlite"].opt_prefix}" if OS.linux?
-
-    system "./configure", *args
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "./configure", "--disable-silent-rules", "--enable-libedit", *std_configure_args
     system "make", "install"
   end
 
