7092efb4a38f73eb1e3e0e4648d1bd00744d9995
Michael Cho <michael@michaelcho.dev>
2025-03-13 22:43:23 -0400

    protobuf@29 29.3 (new formula)

---
+++ Formula/p/protobuf@29.rb
@@ -0,0 +1,72 @@
+class ProtobufAT29 < Formula
+  desc "Protocol buffers (Google's data interchange format)"
+  homepage "https://protobuf.dev/"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v29.3/protobuf-29.3.tar.gz"
+  sha256 "008a11cc56f9b96679b4c285fd05f46d317d685be3ab524b2a310be0fbad987e"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :stable
+    regex(/^v?(29(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  # Support for protoc 29.x (protobuf C++ 5.29.x) will end on 2026-03-31
+  # Ref: https://protobuf.dev/support/version-support/#cpp
+  deprecate! date: "2026-03-31", because: :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "abseil"
+  uses_from_macos "zlib"
+
+  on_macos do
+    # We currently only run tests on macOS.
+    # Running them on Linux requires rebuilding googletest with `-fPIC`.
+    depends_on "googletest" => :build
+  end
+
+  # Backport to expose java-related symbols
+  patch do
+    url "https://github.com/protocolbuffers/protobuf/commit/9dc5aaa1e99f16065e25be4b9aab0a19bfb65ea2.patch?full_index=1"
+    sha256 "edc1befbc3d7f7eded6b7516b3b21e1aa339aee70e17c96ab337f22e60e154d7"
+  end
+
+  def install
+    # Keep `CMAKE_CXX_STANDARD` in sync with the same variable in `abseil.rb`.
+    abseil_cxx_standard = 17
+    cmake_args = %W[
+      -DCMAKE_CXX_STANDARD=#{abseil_cxx_standard}
+      -DBUILD_SHARED_LIBS=ON
+      -Dprotobuf_BUILD_LIBPROTOC=ON
+      -Dprotobuf_BUILD_SHARED_LIBS=ON
+      -Dprotobuf_INSTALL_EXAMPLES=ON
+      -Dprotobuf_BUILD_TESTS=#{OS.mac? ? "ON" : "OFF"}
+      -Dprotobuf_USE_EXTERNAL_GTEST=ON
+      -Dprotobuf_ABSL_PROVIDER=package
+      -Dprotobuf_JSONCPP_PROVIDER=package
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *cmake_args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "ctest", "--test-dir", "build", "--verbose" if OS.mac?
+    system "cmake", "--install", "build"
+
+    (share/"vim/vimfiles/syntax").install "editors/proto.vim"
+    elisp.install "editors/protobuf-mode.el"
+  end
+
+  test do
+    (testpath/"test.proto").write <<~PROTO
+      syntax = "proto3";
+      package test;
+      message TestCase {
+        string name = 4;
+      }
+      message Test {
+        repeated TestCase case = 1;
+      }
+    PROTO
+    system bin/"protoc", "test.proto", "--cpp_out=."
+  end
+end
