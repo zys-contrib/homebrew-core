cd47c50b06278f7a1e26206291115b51ff675599
Rui Chen <rui@chenrui.dev>
2025-03-15 19:50:14 +0000

    feluda: update 1.5.2 bottle.

--- Formula/f/feluda.rb
+++ Formula/f/feluda.rb
@@ -15,12 +15,12 @@ class Feluda < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bde60255804528b16024a0a5cf6a2374c7b4c7c66704e81226d553170149a523"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7de367d5168b7fae342d04d23b36e64d926c428c40c379fd19b98ddd9465d154"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "2e866b0c1a6fed729999e9f589d47483cdc1da0d8b0bb3697e0a8d8a2295b336"
-    sha256 cellar: :any_skip_relocation, sonoma:        "458b17566634cc889dd0da29a915671d31695bdb64fcbdd62b5b37e4823a3118"
-    sha256 cellar: :any_skip_relocation, ventura:       "3bdf9918d0c175880b5fe45e5612e9434772e0da0367723a24d850362fa4aad7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3433062dd336c05b3dc2ba8f6e81e14bee38ea01014031a119329930df9f745d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c5df88d79697c254985465ec47f99625bc65c858f8976ec45b6c65c5e33c6e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "703ad52e2af1363805527b485e3573ae70bf55cc71cd6c8564713b32a51cabb2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "561f6a0aa0ab59e8a491bc3a164690bdb5b6dc83dc74c5dd4bfb5bc83a472712"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6a5a07d307f232d29941f8e15082759ca590dd138a1de3a5fb17fe94d1ddac86"
+    sha256 cellar: :any_skip_relocation, ventura:       "98388541b0dacd5a380dbd22a7988526de0ae5f4af5618d1d08980b595eae691"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f312b11585d3df8518fb4de513efcf3d8d49b1875bc9436b230f03653835bb28"
   end
 
   depends_on "pkgconf" => :build
