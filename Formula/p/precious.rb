31d155d986d92a4334cafa2c45dc9500b4623213
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-15 23:13:03 +0000

    precious 0.8.0

--- Formula/p/precious.rb
+++ Formula/p/precious.rb
@@ -1,8 +1,8 @@
 class Precious < Formula
   desc "One code quality tool to rule them all"
   homepage "https://github.com/houseabsolute/precious"
-  url "https://github.com/houseabsolute/precious/archive/refs/tags/v0.7.3.tar.gz"
-  sha256 "dedb229dec25ec2d8ce627c65aa4a7625af2373456d2f6e76489990b917a248c"
+  url "https://github.com/houseabsolute/precious/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "163b284fff7b723f9645312d1a45fe7149885fa40269c6ee92866d46a1da177b"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/houseabsolute/precious.git", branch: "master"
 
