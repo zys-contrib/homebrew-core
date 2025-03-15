54c5455d3a1c59481555342ce0b3c979314e142b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-15 04:31:39 +0000

    ra-multiplex: add 0.2.5 bottle.

--- Formula/r/ra-multiplex.rb
+++ Formula/r/ra-multiplex.rb
@@ -6,6 +6,15 @@ class RaMultiplex < Formula
   license "MIT"
   head "https://github.com/pr2502/ra-multiplex.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e50b9bbf819cb3d63e167b26f8eaf81b455383223a918c1de2d8f95847b34591"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3368801066ec9a6af29b02b91b4d94d5ab518bafef2e43b6b6bb124ddf0219be"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b4c694cbe6cbcd8eed58c68e0bacc0711b4ee407b9b27f188d555774997768b8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1d1b275cbd97cb722b65047ac00e0ef68272e6334127b3e76e8239416dc0cb5d"
+    sha256 cellar: :any_skip_relocation, ventura:       "6e7171999fb2b27b47ca7b1ff5672dc716f79b87a1d0e8c5fea79e627ffad620"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8fa3b594fcdab0a99ba159c74def4f4a14d2e7f79f4909a0e9c4f907da45d198"
+  end
+
   depends_on "rust" => :build
   depends_on "rust-analyzer"
 
