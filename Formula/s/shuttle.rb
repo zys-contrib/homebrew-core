df5d1e0d8a4420a9cd9e3eb9ea9ebdad79e66682
Rui Chen <rui@chenrui.dev>
2025-03-16 12:07:36 -0400

    shuttle 0.24.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/shuttle.rb
@@ -0,0 +1,40 @@
+class Shuttle < Formula
+  desc "CLI for handling shared build and deploy tools between many projects"
+  homepage "https://github.com/lunarway/shuttle"
+  url "https://github.com/lunarway/shuttle/archive/refs/tags/v0.24.3.tar.gz"
+  sha256 "4a8b93f01e9e21e543c393f59214145850895c89c2c6924a7faac6f8f12292cb"
+  license "Apache-2.0"
+  head "https://github.com/lunarway/shuttle.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/lunarway/shuttle/cmd.version=#{version}
+      -X github.com/lunarway/shuttle/cmd.commit=#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"shuttle", "completion", shells: [:bash, :zsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/shuttle version")
+
+    (testpath/"shuttle.yaml").write <<~YAML
+      plan: 'https://github.com/lunarway/shuttle-example-go-plan.git'
+      vars:
+        docker:
+          baseImage: golang
+          baseTag: stretch
+          destImage: repo-project
+          destTag: latest
+    YAML
+
+    assert_match "Plan:", shell_output("#{bin}/shuttle config")
+
+    output = shell_output("#{bin}/shuttle telemetry upload 2>&1", 1)
+    assert_match "SHUTTLE_REMOTE_TRACING_URL or upload-url is not set", output
+  end
+end
