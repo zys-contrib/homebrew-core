f717701f29f0bde9c6fbceb6a771cf9ddc99c1c9
Michael Cho <michael@michaelcho.dev>
2025-03-16 14:02:50 -0400

    gnustep-base: revision bump to migrate to `icu4c@77`

--- Formula/g/gnustep-base.rb
+++ Formula/g/gnustep-base.rb
@@ -4,6 +4,7 @@ class GnustepBase < Formula
   url "https://github.com/gnustep/libs-base/releases/download/base-1_31_1/gnustep-base-1.31.1.tar.gz"
   sha256 "e7546f1c978a7c75b676953a360194a61e921cb45a4804497b4f346a460545cd"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :stable
@@ -37,7 +38,7 @@ class GnustepBase < Formula
   uses_from_macos "zlib"
 
   on_system :linux, macos: :big_sur_or_older do
-    depends_on "icu4c@76"
+    depends_on "icu4c@77"
   end
 
   on_linux do
