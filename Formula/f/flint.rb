7c814dd9d5b3589d3b84792a76f1998c8568de7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 02:27:59 +0000

    flint 3.2.1

--- Formula/f/flint.rb
+++ Formula/f/flint.rb
@@ -1,8 +1,8 @@
 class Flint < Formula
   desc "C library for number theory"
   homepage "https://flintlib.org/"
-  url "https://github.com/flintlib/flint/releases/download/v3.2.0/flint-3.2.0.tar.gz"
-  sha256 "6d182c4a05d3d6bfc611565d6331d02f94066a3be32df36ed880264afa9c30f4"
+  url "https://github.com/flintlib/flint/releases/download/v3.2.1/flint-3.2.1.tar.gz"
+  sha256 "ca7be46d77972277eb6fe0c4f767548432f56bb534aa17d6dba2d7cce15cd23f"
   license "LGPL-3.0-or-later"
 
   livecheck do
