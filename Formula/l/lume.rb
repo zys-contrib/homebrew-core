f73245a74fe0581205d07706b3a1cf0f74c7377e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 14:35:27 +0000

    lume: add 0.1.9 bottle.

--- Formula/l/lume.rb
+++ Formula/l/lume.rb
@@ -6,6 +6,11 @@ class Lume < Formula
   license "MIT"
   head "https://github.com/trycua/lume.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e4df79bd82fda01c6eadfad7575496c8c888cab6a007e72ae22014547db7e225"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c655fdab0f83facb0d66f6280d739eb0de9648d6f54f4dbde541cf15ef7599c"
+  end
+
   depends_on xcode: ["16.0", :build]
   depends_on arch: :arm64 # For Swift 6.0
   depends_on :macos
