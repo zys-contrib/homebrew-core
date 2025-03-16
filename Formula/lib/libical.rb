eaababa1992a012e760d85a1090edb264a21cf6f
Michael Cho <michael@michaelcho.dev>
2025-03-16 13:56:46 -0400

    libical: revision bump to migrate to `icu4c@77`

--- Formula/lib/libical.rb
+++ Formula/lib/libical.rb
@@ -4,6 +4,7 @@ class Libical < Formula
   url "https://github.com/libical/libical/releases/download/v3.0.20/libical-3.0.20.tar.gz"
   sha256 "e73de92f5a6ce84c1b00306446b290a2b08cdf0a80988eca0a2c9d5c3510b4c2"
   license any_of: ["LGPL-2.1-or-later", "MPL-2.0"]
+  revision 1
 
   livecheck do
     url :stable
@@ -22,7 +23,7 @@ class Libical < Formula
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "glib"
-  depends_on "icu4c@76"
+  depends_on "icu4c@77"
 
   uses_from_macos "libxml2"
 
