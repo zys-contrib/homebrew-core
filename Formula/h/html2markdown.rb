8c196259e20fdf283f6c7a83498d25b7eab57a7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 17:12:05 +0000

    html2markdown: update 2.3.1 bottle.

--- Formula/h/html2markdown.rb
+++ Formula/h/html2markdown.rb
@@ -7,12 +7,12 @@ class Html2markdown < Formula
   head "https://github.com/JohannesKaufmann/html-to-markdown.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bb21d1313f47f5e2110e1145d7e0649b568f57226d1fd2605d3fd7b962e4c00f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bb21d1313f47f5e2110e1145d7e0649b568f57226d1fd2605d3fd7b962e4c00f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "bb21d1313f47f5e2110e1145d7e0649b568f57226d1fd2605d3fd7b962e4c00f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "805eb6b305354ca461fad370a9ac75d61e48cfb89392561be5c105e1eda4a4dc"
-    sha256 cellar: :any_skip_relocation, ventura:       "805eb6b305354ca461fad370a9ac75d61e48cfb89392561be5c105e1eda4a4dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e29074c57de20e57f8b2bd3335542ed1931c8517a09aa0e4e97c2f64f141531a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c219db5e39b7af648e6f9c12dd8021f370d589bd3d160d910ccb501b12ef0d1e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c219db5e39b7af648e6f9c12dd8021f370d589bd3d160d910ccb501b12ef0d1e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c219db5e39b7af648e6f9c12dd8021f370d589bd3d160d910ccb501b12ef0d1e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ddf7d58c05a7ad768de1fd74b8d61b57af4dbeae9bc1a20075beb29b2ddde73a"
+    sha256 cellar: :any_skip_relocation, ventura:       "ddf7d58c05a7ad768de1fd74b8d61b57af4dbeae9bc1a20075beb29b2ddde73a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bbdad706da16870661eff95f676d348122cdb03589efa9989bf7085764e633b3"
   end
 
   depends_on "go" => :build
