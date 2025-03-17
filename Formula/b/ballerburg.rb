3bb21c0d8cd7b16bb542a356a4b2385545bf9a84
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-17 19:55:02 +0000

    ballerburg: update 1.2.3 bottle.

--- Formula/b/ballerburg.rb
+++ Formula/b/ballerburg.rb
@@ -7,14 +7,12 @@ class Ballerburg < Formula
   head "https://framagit.org/baller/ballerburg.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia:  "18e27b920964203d30abe78eaf1ad8b3542b9ad1dca97303841f0cf2a83f0b8d"
-    sha256 cellar: :any,                 arm64_sonoma:   "6e60dfeb0525cbff53762cb9e529a15d87c4e48988e84c6884ed8b582944d5c6"
-    sha256 cellar: :any,                 arm64_ventura:  "01fbf1c2eddb7cd5b18b44dbfdd5faaf0f5a774dbb41600af52066decedccd99"
-    sha256 cellar: :any,                 arm64_monterey: "b752e6592084f8d57b5c23c616305ef53486884c35f2c18da3d81a18a1e3b6d6"
-    sha256 cellar: :any,                 sonoma:         "527b34086caa339bc5e3bfa3f911aacedf0cd420a64522c77d09b75cd5d19465"
-    sha256 cellar: :any,                 ventura:        "7e840cfda17d76b404136e80cda808e8049fb9d710c013d17290d85fe8270b48"
-    sha256 cellar: :any,                 monterey:       "0ab3dfc0ea9f8f0e116794c913b9dbd393d0018a598d0ea2ba180d18dd5e25e2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52a20fccf1c95f7d72148a613968943963d16e341e344431ff155a42bc65d05a"
+    sha256 cellar: :any,                 arm64_sequoia: "dd778359cebe62952c33d6a2e5c2274f7e4784ccc9f02dd86ca15bb783487608"
+    sha256 cellar: :any,                 arm64_sonoma:  "bf505c27f0af03d53a92ef5816d66ccaf2843ef65cfb59aec887a5ec0c1f7a57"
+    sha256 cellar: :any,                 arm64_ventura: "1c01a6a26ad1acfef28ad833391a9d004a2a70d8d97e6e5a475f26210c2bf43c"
+    sha256 cellar: :any,                 sonoma:        "29c01bc593a8279839cee765f58ec00e8e7e52486099f8309b97b5f493e45e11"
+    sha256 cellar: :any,                 ventura:       "ee583b89ffe454e9c550860d852333d5f91c23155c426c384727dfd1f689a0fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1aa85f3ee1fc21cda5916659e10333c563cd918356d3eca5016847a48ff03e57"
   end
 
   depends_on "cmake" => :build
