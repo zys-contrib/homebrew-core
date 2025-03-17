8b31518aeb67230bc80baa958b5f567e63744d26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-17 01:02:30 +0000

    decompose: add 1.11.3 bottle.

--- Formula/d/decompose.rb
+++ Formula/d/decompose.rb
@@ -6,6 +6,15 @@ class Decompose < Formula
   license "MIT"
   head "https://github.com/s0rg/decompose.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a6ac95d556532dcdb8ccb6bee22c93941e70d472d6fe2e2a04aee692e69fd12"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1a6ac95d556532dcdb8ccb6bee22c93941e70d472d6fe2e2a04aee692e69fd12"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1a6ac95d556532dcdb8ccb6bee22c93941e70d472d6fe2e2a04aee692e69fd12"
+    sha256 cellar: :any_skip_relocation, sonoma:        "04959a2108da5a70a89780f02cc5691c39aa163d69921507743c56407b0353ac"
+    sha256 cellar: :any_skip_relocation, ventura:       "04959a2108da5a70a89780f02cc5691c39aa163d69921507743c56407b0353ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f1de0150f4dcf6163ee980363062881828b21085f5a44f38a1a14fad124a986d"
+  end
+
   depends_on "go" => :build
 
   def install
