68bf9fcd5e1bff026dc829be15c9334af0c7c495
Carlo Cabrera <github@carlo.cab>
2025-03-17 15:04:08 +0800

    zlib-rs 0.4.2 (new formula)

---
+++ Formula/z/zlib-rs.rb
@@ -0,0 +1,39 @@
+class ZlibRs < Formula
+  desc "C API for zlib-rs"
+  homepage "https://github.com/trifectatechfoundation/zlib-rs/tree/main/libz-rs-sys-cdylib#libz-rs-sys-cdylib"
+  url "https://github.com/trifectatechfoundation/zlib-rs/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "c1ddc10c38e50e9a465e7d76d6a5a8182656ae3fd35094af2b2d5c9606e58092"
+  license "Zlib"
+  head "https://github.com/trifectatechfoundation/zlib-rs.git", branch: "main"
+
+  depends_on "cargo-c" => :build
+  depends_on "rust" => :build
+  uses_from_macos "zlib" => :test
+
+  def install
+    # https://github.com/trifectatechfoundation/zlib-rs/tree/main/libz-rs-sys-cdylib#-cllvm-args-enable-dfa-jump-thread
+    ENV.append "RUSTFLAGS", "-Cllvm-args=-enable-dfa-jump-thread"
+    cd "libz-rs-sys-cdylib" do
+      system "cargo", "cinstall", "--jobs", ENV.make_jobs.to_s, "--prefix", prefix, "--libdir", lib, "--release"
+    end
+  end
+
+  test do
+    # https://zlib.net/zlib_how.html
+    resource "test_artifact" do
+      url "https://zlib.net/zpipe.c"
+      version "20051211"
+      sha256 "68140a82582ede938159630bca0fb13a93b4bf1cb2e85b08943c26242cf8f3a6"
+    end
+
+    testpath.install resource("test_artifact")
+    ENV.append_to_cflags "-I#{Formula["zlib"].opt_include}" if OS.linux?
+    ENV.append "LDFLAGS", "-L#{lib}"
+    ENV.append "LDLIBS", "-lz_rs"
+    system "make", "zpipe"
+
+    text = "Hello, Homebrew!"
+    compressed = pipe_output("./zpipe", text)
+    assert_equal text, pipe_output("./zpipe -d", compressed)
+  end
+end
