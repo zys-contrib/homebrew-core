dd726cb8aeb6fe159a6f4fd1eac271e09f3654ac
Val V <scm@vaygr.net>
2025-03-17 14:42:16 +0000

    fabric-ai 1.4.161 (new formula)

---
+++ Formula/f/fabric-ai.rb
@@ -0,0 +1,22 @@
+class FabricAi < Formula
+  desc "Open-source framework for augmenting humans using AI"
+  homepage "https://danielmiessler.com/p/fabric-origin-story"
+  url "https://github.com/danielmiessler/fabric/archive/refs/tags/v1.4.161.tar.gz"
+  sha256 "b5b53903b36ddecf26dbd63418ad60e15e16a99d546fe39ed874ca26885af37a"
+  license "MIT"
+  head "https://github.com/danielmiessler/fabric.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fabric-ai --version")
+
+    (testpath/".config/fabric/.env").write("t\n")
+    output = shell_output("#{bin}/fabric-ai --dry-run < /dev/null 2>&1")
+    assert_match "error loading .env file: unexpected character", output
+  end
+end
