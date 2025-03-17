941f257a5900c25db7bdef65de19978a7095775b
Sam Ford <1584702+samford@users.noreply.github.com>
2025-03-16 20:33:07 -0400

    libadwaita 1.7.0
    `libadwaita` is in the autobump list but it can't be bumped to 1.7.0
    until the version in the audit allowlist is updated to 1.7. This
    updates the allowlist and manually updates the formula.
    
    Co-authored-by: Francois-Xavier Coudert <fxcoudert@gmail.com>

--- Formula/lib/libadwaita.rb
+++ Formula/lib/libadwaita.rb
@@ -1,8 +1,8 @@
 class Libadwaita < Formula
   desc "Building blocks for modern adaptive GNOME applications"
   homepage "https://gnome.pages.gitlab.gnome.org/libadwaita/"
-  url "https://download.gnome.org/sources/libadwaita/1.6/libadwaita-1.6.4.tar.xz"
-  sha256 "1011a5a93dc3b87e82da19910d7fab01a3b9d7fcb0b09751babb476eedb3d9a3"
+  url "https://download.gnome.org/sources/libadwaita/1.7/libadwaita-1.7.0.tar.xz"
+  sha256 "58bf99b8a9f8b0171964de0ae741d01d5a09db3662134fa67541c99a8ed7dec0"
   license "LGPL-2.1-or-later"
 
   # libadwaita doesn't use GNOME's "even-numbered minor is stable" version
