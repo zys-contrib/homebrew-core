a920a8fd0249bf6ebfd25b368c40460d3ae5920d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-18 08:10:32 +0000

    pinact 1.4.0

--- Formula/p/pinact.rb
+++ Formula/p/pinact.rb
@@ -1,8 +1,8 @@
 class Pinact < Formula
   desc "Pins GitHub Actions to full hashes and versions"
   homepage "https://github.com/suzuki-shunsuke/pinact"
-  url "https://github.com/suzuki-shunsuke/pinact/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "8625435f1a23a3cb392a3640eff4c44d24409242c0aeac541f00a3bd699594bd"
+  url "https://github.com/suzuki-shunsuke/pinact/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "34722b6e78cbc11c650f2ab48f0028dafb3a794ad0723bcc131431228bd02a2d"
   license "MIT"
   head "https://github.com/suzuki-shunsuke/pinact.git", branch: "main"
 
