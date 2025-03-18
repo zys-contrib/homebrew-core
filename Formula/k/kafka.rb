9fb881dcca2e7dac52c6e792bebcf86fa549f958
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-18 17:02:14 +0000

    kafka 4.0.0
    kafka: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/kafka.rb
+++ Formula/k/kafka.rb
@@ -1,9 +1,9 @@
 class Kafka < Formula
   desc "Open-source distributed event streaming platform"
   homepage "https://kafka.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=kafka/3.9.0/kafka_2.13-3.9.0.tgz"
-  mirror "https://archive.apache.org/dist/kafka/3.9.0/kafka_2.13-3.9.0.tgz"
-  sha256 "abc44402ddf103e38f19b0e4b44e65da9a831ba9e58fd7725041b1aa168ee8d1"
+  url "https://www.apache.org/dyn/closer.lua?path=kafka/4.0.0/kafka_2.13-4.0.0.tgz"
+  mirror "https://archive.apache.org/dist/kafka/4.0.0/kafka_2.13-4.0.0.tgz"
+  sha256 "7b852e938bc09de10cd96eca3755258c7d25fb89dbdd76305717607e1835e2aa"
   license "Apache-2.0"
 
   livecheck do
@@ -21,24 +21,18 @@ class Kafka < Formula
   end
 
   depends_on "openjdk"
-  depends_on "zookeeper"
 
   def install
     data = var/"lib"
+
     inreplace "config/server.properties",
-      "log.dirs=/tmp/kafka-logs", "log.dirs=#{data}/kafka-logs"
-
-    inreplace "config/kraft/server.properties",
       "log.dirs=/tmp/kraft-combined-logs", "log.dirs=#{data}/kraft-combined-logs"
-
-    inreplace "config/kraft/controller.properties",
+    inreplace "config/controller.properties",
       "log.dirs=/tmp/kraft-controller-logs", "log.dirs=#{data}/kraft-controller-logs"
-
-    inreplace "config/kraft/broker.properties",
+    inreplace "config/connect-standalone.properties",
+      "filename=/tmp/connect.offsets", "filename=#{data}/connect.offsets"
+    inreplace "config/broker.properties",
       "log.dirs=/tmp/kraft-broker-logs", "log.dirs=#{data}/kraft-broker-logs"
-
-    inreplace "config/zookeeper.properties",
-      "dataDir=/tmp/zookeeper", "dataDir=#{data}/zookeeper"
 
     # remove Windows scripts
     rm_r("bin/windows")
@@ -52,9 +46,19 @@ class Kafka < Formula
     mv "config", "kafka"
     etc.install "kafka"
     libexec.install_symlink etc/"kafka" => "config"
+  end
 
+  def post_install
     # create directory for kafka stdout+stderr output logs when run by launchd
-    (var+"log/kafka").mkpath
+    (var/"log/kafka").mkpath
+
+    generate_log_dir(etc/"kafka/server.properties") unless (var/"lib/kraft-combined-logs/meta.properties").exist?
+  end
+
+  def generate_log_dir(path)
+    uuid = Utils.safe_popen_read(bin/"kafka-storage", "random-uuid").strip
+    system bin/"kafka-storage", "format", "--standalone",
+           "-t", uuid, "-c", path
   end
 
   service do
@@ -73,27 +77,23 @@ class Kafka < Formula
     ENV.delete "COLUMNS"
 
     (testpath/"kafka").mkpath
-    cp "#{etc}/kafka/zookeeper.properties", testpath/"kafka"
     cp "#{etc}/kafka/server.properties", testpath/"kafka"
-    inreplace "#{testpath}/kafka/zookeeper.properties", "#{var}/lib", testpath
-    inreplace "#{testpath}/kafka/server.properties", "#{var}/lib", testpath
 
-    zk_port = free_port
     kafka_port = free_port
-    inreplace "#{testpath}/kafka/zookeeper.properties", "clientPort=2181", "clientPort=#{zk_port}"
+    controller_port = free_port
     inreplace "#{testpath}/kafka/server.properties" do |s|
-      s.gsub! "zookeeper.connect=localhost:2181", "zookeeper.connect=localhost:#{zk_port}"
-      s.gsub! "#listeners=PLAINTEXT://:9092", "listeners=PLAINTEXT://:#{kafka_port}"
+      s.gsub! "#{var}/lib", testpath
+      s.gsub! "controller.quorum.bootstrap.servers=localhost:9093",
+              "controller.quorum.bootstrap.servers=localhost:#{controller_port}"
+      s.gsub! "listeners=PLAINTEXT://:9092,CONTROLLER://:9093",
+              "listeners=PLAINTEXT://:#{kafka_port},CONTROLLER://:#{controller_port}"
+      s.gsub! "advertised.listeners=PLAINTEXT://localhost:9092,CONTROLLER://localhost:9093",
+              "advertised.listeners=PLAINTEXT://localhost:#{kafka_port},CONTROLLER://localhost:#{controller_port}"
     end
 
+    generate_log_dir(testpath/"kafka/server.properties")
+
     begin
-      fork do
-        exec "#{bin}/zookeeper-server-start #{testpath}/kafka/zookeeper.properties " \
-             "> #{testpath}/test.zookeeper-server-start.log 2>&1"
-      end
-
-      sleep 15
-
       fork do
         exec "#{bin}/kafka-server-start #{testpath}/kafka/server.properties " \
              "> #{testpath}/test.kafka-server-start.log 2>&1"
@@ -112,7 +112,6 @@ class Kafka < Formula
              ">> #{testpath}/kafka/demo.out 2>/dev/null"
     ensure
       system bin/"kafka-server-stop"
-      system bin/"zookeeper-server-stop"
       sleep 10
     end
 
