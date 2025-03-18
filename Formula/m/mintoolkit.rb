0a7e89cf3cb4c4f2e595ace52dd027da022f3551
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-18 10:29:36 +0000

    mintoolkit: add 1.41.7 bottle.

--- Formula/m/mintoolkit.rb
+++ Formula/m/mintoolkit.rb
@@ -11,6 +11,15 @@ class Mintoolkit < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3191425ab42a27ac181105eb522ba926cb52c2a6a7034b9cf697e34a41506627"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "33e215a31ad61ca3f81951ca6863d406fd06a376ec10fd2e81f3ae058d6449e5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "485651b0b4be33e48ef7907f6239b83585e3028902c266fbab797b4c39bdd46a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c9c396b50f98b248eb69cafd4c800f6ad8ae8b25ed136fbcbeff4ec0ec9bc93c"
+    sha256 cellar: :any_skip_relocation, ventura:       "fa2b6fcf6dc740377b6400639d8d1fb00af713fd86f1c7b4027bbe2285f9dcbc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc9d72e0904771231b5c75d6444d6cbdc8d7185abeaa1cafb18257a731e98f8d"
+  end
+
   depends_on "go" => :build
 
   on_linux do
