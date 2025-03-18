209f99104bd8b32032920bfbfe10119ceae96c01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-18 20:23:11 +0000

    trufflehog 3.88.18

--- Formula/t/trufflehog.rb
+++ Formula/t/trufflehog.rb
@@ -1,8 +1,8 @@
 class Trufflehog < Formula
   desc "Find and verify credentials"
   homepage "https://trufflesecurity.com/"
-  url "https://github.com/trufflesecurity/trufflehog/archive/refs/tags/v3.88.17.tar.gz"
-  sha256 "8ab59e272e30e69a7ba4f44cb9b3973b9ac6ce56ea42f2829d6c90e2175bd23f"
+  url "https://github.com/trufflesecurity/trufflehog/archive/refs/tags/v3.88.18.tar.gz"
+  sha256 "39e8431891187386bfc77d1e15feb912110c3dfe1106e66a6c7ad62774621d95"
   # upstream license ask, https://github.com/trufflesecurity/trufflehog/issues/1446
   license "AGPL-3.0-only"
   head "https://github.com/trufflesecurity/trufflehog.git", branch: "main"
