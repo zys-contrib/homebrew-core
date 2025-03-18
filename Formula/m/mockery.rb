720a1c261c516c5b700d5f1652129f8f2c129ef1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-18 23:11:04 +0000

    mockery 2.53.3

--- Formula/m/mockery.rb
+++ Formula/m/mockery.rb
@@ -1,8 +1,8 @@
 class Mockery < Formula
   desc "Mock code autogenerator for Golang"
   homepage "https://github.com/vektra/mockery"
-  url "https://github.com/vektra/mockery/archive/refs/tags/v2.53.2.tar.gz"
-  sha256 "dbc2acfa0bbd1a6cf7fce0d8b70b71b234950762377be644cd4614a2f34ce997"
+  url "https://github.com/vektra/mockery/archive/refs/tags/v2.53.3.tar.gz"
+  sha256 "c9495d19de1ca52a61a9ed299d9988a4f4a3cf1ad614954a15ca3920d8b852a2"
   license "BSD-3-Clause"
   head "https://github.com/vektra/mockery.git", branch: "master"
 
