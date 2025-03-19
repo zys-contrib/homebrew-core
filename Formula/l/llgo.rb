2c5435bc7b37a829df59293d7bc33bf8f717ed82
Li Jie <cpunion@gmail.com>
2025-03-19 13:58:44 +0800

    llgo 0.10.1

--- Formula/l/llgo.rb
+++ Formula/l/llgo.rb
@@ -1,10 +1,9 @@
 class Llgo < Formula
   desc "Go compiler based on LLVM integrate with the C ecosystem and Python"
   homepage "https://github.com/goplus/llgo"
-  url "https://github.com/goplus/llgo/archive/refs/tags/v0.9.9.tar.gz"
-  sha256 "705fed97ef8b337863fd9bbb40653c22fd93ba689f879db06801d37e5d8fe809"
+  url "https://github.com/goplus/llgo/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "4ef8a05ff2739617be48de1eb7cfa3e37bd402595fda6bb6df106aadb6c96965"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
@@ -22,15 +21,30 @@ class Llgo < Formula
 
   depends_on "bdw-gc"
   depends_on "go"
+  depends_on "libffi"
+  depends_on "libuv"
   depends_on "llvm@18"
   depends_on "openssl@3"
   depends_on "pkgconf"
+  uses_from_macos "zlib"
 
-  def llvm
-    deps.map(&:to_formula).find { |f| f.name.match?(/^llvm(@\d+)?$/) }
+  on_linux do
+    depends_on "libunwind"
+  end
+
+  def find_dep(name)
+    deps.map(&:to_formula).find { |f| f.name.match?(/^#{name}(@\d+)?$/) }
   end
 
   def install
+    llvm = find_dep("llvm")
+    ldflags = %W[
+      -s -w
+      -X github.com/goplus/llgo/compiler/internal/env.buildVersion=v#{version}
+      -X github.com/goplus/llgo/compiler/internal/env.buildTime=#{time.iso8601}
+      -X github.com/goplus/llgo/compiler/internal/env/llvm.ldLLVMConfigBin=#{llvm.opt_bin/"llvm-config"}
+    ]
+    tags = nil
     if OS.linux?
       ENV.prepend "CGO_CPPFLAGS",
         "-I#{llvm.opt_include} " \
@@ -39,54 +53,82 @@ class Llgo < Formula
         "-D__STDC_FORMAT_MACROS " \
         "-D__STDC_LIMIT_MACROS"
       ENV.prepend "CGO_LDFLAGS", "-L#{llvm.opt_lib} -lLLVM"
+      tags = "byollvm"
     end
 
-    ldflags = %W[
-      -s -w
-      -X github.com/goplus/llgo/x/env.buildVersion=v#{version}
-      -X github.com/goplus/llgo/x/env.buildTime=#{time.iso8601}
-      -X github.com/goplus/llgo/xtool/env/llvm.ldLLVMConfigBin=#{llvm.opt_bin/"llvm-config"}
-    ]
-    tags = "byollvm" if OS.linux?
-    system "go", "build", *std_go_args(ldflags:, tags:), "-o", libexec/"bin/", "./cmd/llgo"
+    cd "compiler" do
+      system "go", "build", *std_go_args(ldflags:, tags:), "-o", libexec/"bin/", "./cmd/llgo"
+    end
 
-    libexec.install "LICENSE", "README.md"
+    libexec.install "LICENSE", "README.md", "runtime"
 
-    path = llvm.opt_bin + ":" + %w[go pkgconf].map { |f| Formula[f].opt_bin }.join(":")
-    opt_lib = %w[bdw-gc openssl@3].map { |f| Formula[f].opt_lib }.join(":")
+    path_deps = %w[llvm go pkgconf].map { |name| find_dep(name).opt_bin }
+    script_env = { PATH: "#{path_deps.join(":")}:$PATH" }
+
+    if OS.linux?
+      libunwind = find_dep("libunwind")
+      script_env[:CFLAGS] = "-I#{libunwind.opt_include} $CFLAGS"
+      script_env[:LDFLAGS] = "-L#{libunwind.opt_lib} -rpath #{libunwind.opt_lib} $LDFLAGS"
+    end
 
     (libexec/"bin").children.each do |f|
       next if f.directory?
 
       cmd = File.basename(f)
-      (bin/cmd).write_env_script libexec/"bin"/cmd,
-        PATH:            "#{path}:$PATH",
-        LD_LIBRARY_PATH: "#{opt_lib}:$LD_LIBRARY_PATH"
+      (bin/cmd).write_env_script libexec/"bin"/cmd, script_env
     end
   end
 
   test do
-    opt_lib = %w[bdw-gc openssl@3].map { |f| Formula[f].opt_lib }.join(":")
-    ENV.prepend_path "LD_LIBRARY_PATH", opt_lib
-
-    goos = shell_output(Formula["go"].opt_bin/"go env GOOS").chomp
-    goarch = shell_output(Formula["go"].opt_bin/"go env GOARCH").chomp
+    goos = shell_output("go env GOOS").chomp
+    goarch = shell_output("go env GOARCH").chomp
     assert_equal "llgo v#{version} #{goos}/#{goarch}", shell_output("#{bin}/llgo version").chomp
 
     (testpath/"hello.go").write <<~GO
       package main
 
-      import "github.com/goplus/llgo/c"
+      import (
+          "fmt"
+
+          "github.com/goplus/llgo/c"
+      )
+
+      func Foo() string {
+        return "Hello LLGO by Foo"
+      }
 
       func main() {
-        c.Printf(c.Str("Hello LLGO\\n"))
+        fmt.Println("Hello LLGO by fmt.Println")
+        c.Printf(c.Str("Hello LLGO by c.Printf\\n"))
+      }
+    GO
+    (testpath/"hello_test.go").write <<~GO
+      package main
+
+      import "testing"
+
+      func Test_Foo(t *testing.T) {
+        got := Foo()
+        want := "Hello LLGO by Foo"
+        if got != want {
+          t.Errorf("foo() = %q, want %q", got, want)
+        }
       }
     GO
     (testpath/"go.mod").write <<~GOMOD
       module hello
     GOMOD
-    system Formula["go"].opt_bin/"go", "get", "github.com/goplus/llgo@v#{version}"
+    system "go", "get", "github.com/goplus/llgo"
+    # Test llgo run
+    assert_equal "Hello LLGO by fmt.Println\n" \
+                 "Hello LLGO by c.Printf\n",
+                 shell_output("#{bin}/llgo run .")
+    # Test llgo build
     system bin/"llgo", "build", "-o", "hello", "."
-    assert_equal "Hello LLGO\n", shell_output("./hello")
+    assert_equal "Hello LLGO by fmt.Println\n" \
+                 "Hello LLGO by c.Printf\n",
+                 shell_output("./hello")
+    # Test llgo test
+    assert_match "PASS", shell_output("#{bin}/llgo test .")
   end
 end
