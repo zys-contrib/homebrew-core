6df0d67fbc7117f933f6c08043aa7c87e99efe15
Michael Cho <michael@michaelcho.dev>
2025-03-18 20:55:35 -0400

    xkeyboard-config: rename from `xkeyboardconfig`

---
+++ Formula/x/xkeyboard-config.rb
@@ -0,0 +1,29 @@
+class XkeyboardConfig < Formula
+  desc "Keyboard configuration database for the X Window System"
+  homepage "https://www.freedesktop.org/wiki/Software/XKeyboardConfig/"
+  url "https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.44.tar.xz"
+  sha256 "54d2c33eeebb031d48fa590c543e54c9bcbd0f00386ebc6489b2f47a0da4342a"
+  license "MIT"
+  head "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git", branch: "master"
+
+  depends_on "gettext" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => [:build, :test]
+  depends_on "python@3.13" => :build
+
+  uses_from_macos "libxslt" => :build
+
+  def install
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    assert_path_exists man7/"xkeyboard-config.7"
+    assert_equal "#{share}/X11/xkb", shell_output("pkg-config --variable=xkb_base xkeyboard-config").chomp
+    assert_match "Language-Team: English",
+      shell_output("strings #{share}/locale/en_GB/LC_MESSAGES/xkeyboard-config.mo")
+  end
+end
