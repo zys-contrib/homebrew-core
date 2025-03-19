46daf2381cf039374c7e00a55b4868f33bb1247b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-19 14:15:40 +0000

    skaffold 2.14.2

--- Formula/s/skaffold.rb
+++ Formula/s/skaffold.rb
@@ -2,8 +2,8 @@ class Skaffold < Formula
   desc "Easy and Repeatable Kubernetes Development"
   homepage "https://skaffold.dev/"
   url "https://github.com/GoogleContainerTools/skaffold.git",
-      tag:      "v2.14.1",
-      revision: "62fba6a236833db0d0d689a33e0b1007518d3456"
+      tag:      "v2.14.2",
+      revision: "d76dbabc8f951df21560ccf2063c1d04cef3b6f8"
   license "Apache-2.0"
   head "https://github.com/GoogleContainerTools/skaffold.git", branch: "main"
 
