677914da4bd2b3fac740a929297ec28d2e2f8f49
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-19 05:13:02 +0000

    pmtiles 1.26.0

--- Formula/p/pmtiles.rb
+++ Formula/p/pmtiles.rb
@@ -1,8 +1,8 @@
 class Pmtiles < Formula
   desc "Single-file executable tool for creating, reading and uploading PMTiles archives"
   homepage "https://protomaps.com/docs/pmtiles"
-  url "https://github.com/protomaps/go-pmtiles/archive/refs/tags/v1.25.3.tar.gz"
-  sha256 "2e4aa3a475015d834dc18e1726054bf73a32adaeaa46ce250cedcd60ff1aed01"
+  url "https://github.com/protomaps/go-pmtiles/archive/refs/tags/v1.26.0.tar.gz"
+  sha256 "ea48f193c6da3bf809a549561ca95b73cc02c832ca450c352110d1ba0defe42b"
   license "BSD-3-Clause"
 
   livecheck do
