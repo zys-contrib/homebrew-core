7d08d833552ee17a10881aa4ca997210788299c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-19 02:23:30 +0000

    git-who: add 0.6 bottle.

--- Formula/g/git-who.rb
+++ Formula/g/git-who.rb
@@ -5,6 +5,15 @@ class GitWho < Formula
   sha256 "4d81d2bdbd3fb2a0f393feb76981e33b3d31aa49bf97b4c0a69bad148aa8fbc0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0a276471aae545231042e81995fe72045c42c24169e0497fb1cb9ae421dbdce5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a276471aae545231042e81995fe72045c42c24169e0497fb1cb9ae421dbdce5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0a276471aae545231042e81995fe72045c42c24169e0497fb1cb9ae421dbdce5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9f1affd17457651a4574c7d31ff6de062ab24ee4bb9c12f9787eca1d2bac4176"
+    sha256 cellar: :any_skip_relocation, ventura:       "9f1affd17457651a4574c7d31ff6de062ab24ee4bb9c12f9787eca1d2bac4176"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b16499bcb1b77e62b67272f51eca2f57cfe0df8e596a9dce83c0e662193a1f81"
+  end
+
   depends_on "go" => :build
 
   def install
