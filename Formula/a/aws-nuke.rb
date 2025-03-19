7482d42aae7228c70da069e4862f11406a4796bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-19 02:53:50 +0000

    aws-nuke: update 3.50.1 bottle.

--- Formula/a/aws-nuke.rb
+++ Formula/a/aws-nuke.rb
@@ -7,12 +7,12 @@ class AwsNuke < Formula
   head "https://github.com/ekristen/aws-nuke.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f975c41b6fb2d72c0e6df70a2c09c47494a1737f082adb9f708176512c30c24c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f975c41b6fb2d72c0e6df70a2c09c47494a1737f082adb9f708176512c30c24c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f975c41b6fb2d72c0e6df70a2c09c47494a1737f082adb9f708176512c30c24c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "31712507c80e442a657151aa6a5cd3e4dd43a70384a087c28c1108c94e7546e1"
-    sha256 cellar: :any_skip_relocation, ventura:       "31712507c80e442a657151aa6a5cd3e4dd43a70384a087c28c1108c94e7546e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2cc7502a3846c82de4726d4dd039e385731e090420ad01f33180bf1b5ae27a4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "803118cb594fca0d343c1b588b990977d062a9dfadf65a75d6ae5fd24cbe4a85"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "803118cb594fca0d343c1b588b990977d062a9dfadf65a75d6ae5fd24cbe4a85"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "803118cb594fca0d343c1b588b990977d062a9dfadf65a75d6ae5fd24cbe4a85"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e7de6d3d012b1bac47d3122d4ee600e7286026f4c3d2714ade9e1a3ee86e8c8d"
+    sha256 cellar: :any_skip_relocation, ventura:       "e7de6d3d012b1bac47d3122d4ee600e7286026f4c3d2714ade9e1a3ee86e8c8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "642230d7fbd464407c343edc8fb71f1a1c071af178fed2821c01e3492b5d7775"
   end
 
   depends_on "go" => :build
