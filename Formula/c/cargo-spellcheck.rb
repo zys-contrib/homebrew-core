443236aecd2b0182468acab05ea3ac645377c596
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-19 01:47:38 +0000

    cargo-spellcheck: add 0.15.5 bottle.

--- Formula/c/cargo-spellcheck.rb
+++ Formula/c/cargo-spellcheck.rb
@@ -6,6 +6,15 @@ class CargoSpellcheck < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/drahnr/cargo-spellcheck.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8e4c34eabd5c01a01e81429472363789f826a4107fb087feb88f6350cc459e7c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "429f9b37061d3e2f7d69e85f8e2457642d32b621966ebd25b20717af067be0ed"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7be9cb9bf49c88cd65a94b13c136ec844ab8f7d00b57b7404d944199f12cde65"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7476f53530cadc1d65a76aab700faa7c547fae7b4230c7df8e4d6b90e99183ad"
+    sha256 cellar: :any_skip_relocation, ventura:       "43e0cd41ac0873292789d23e3b913ff9b0dab96db3fd25d2df8c1ed8ea335735"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5bf9c80c76e0eadf342f2dd4dde6e6c6f07c1a713fe2feb7e9c096a3877310d7"
+  end
+
   depends_on "llvm" => :build
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
