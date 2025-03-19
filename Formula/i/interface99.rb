0a5b8204ccd9631643d11d2de018291f9226e9e2
Rui Chen <rui@chenrui.dev>
2025-03-18 23:49:01 -0400

    interface99 1.0.2
    autobump: add interface99
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/interface99.rb
+++ Formula/i/interface99.rb
@@ -1,8 +1,8 @@
 class Interface99 < Formula
   desc "Full-featured interfaces for C99"
   homepage "https://github.com/Hirrolot/interface99"
-  url "https://github.com/Hirrolot/interface99/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "ddc7cd979cf9c964a4313a5e6bdc87bd8df669142f29c8edb71d2f2f7822d9aa"
+  url "https://github.com/Hirrolot/interface99/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "8bd007c48cf05436ced60884e8e3a05ede46105f3efae9bf29e0f4d30f938f9e"
   license "MIT"
   head "https://github.com/Hirrolot/interface99.git", branch: "master"
 
