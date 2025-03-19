566534f9bdaac7b45a0b9e30e34c6b32b085d2ee
Rui Chen <rui@chenrui.dev>
2025-03-19 00:19:35 -0400

    cargo-sort 1.1.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cargo-sort.rb
@@ -0,0 +1,42 @@
+class CargoSort < Formula
+  desc "Tool to check that your Cargo.toml dependencies are sorted alphabetically"
+  homepage "https://github.com/devinr528/cargo-sort"
+  url "https://github.com/DevinR528/cargo-sort/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "2add0719d3309e868e8e305ce33bfbbd59554853e1cef2866af6745b750a689a"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/devinr528/cargo-sort.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on "rustup" => :test
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # Show that we can use a different toolchain than the one provided by the `rust` formula.
+    # https://github.com/Homebrew/homebrew-core/pull/134074#pullrequestreview-1484979359
+    ENV.prepend_path "PATH", Formula["rustup"].bin
+    system "rustup", "default", "beta"
+    system "rustup", "set", "profile", "minimal"
+
+    assert_match version.to_s, shell_output("#{bin}/cargo-sort --version")
+
+    mkdir "brewtest" do
+      (testpath/"brewtest/Cargo.toml").write <<~TOML
+        [package]
+        name = "test"
+        version = "0.1.0"
+        edition = "2018"
+
+        [dependencies]
+        c = "0.7.0"
+        a = "0.5.0"
+        b = "0.6.0"
+      TOML
+
+      output = shell_output("#{bin}/cargo-sort --check 2>&1", 1)
+      assert_match "Dependencies for brewtest are not sorted", output
+    end
+  end
+end
