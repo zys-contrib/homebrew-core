fd4b6f406b10b44c574956bc45756d811da64e56
Rui Chen <rui@chenrui.dev>
2025-03-20 22:49:11 +0000

    jackett: update 0.22.1669 bottle.

--- Formula/j/jackett.rb
+++ Formula/j/jackett.rb
@@ -11,6 +11,7 @@ class Jackett < Formula
     sha256 cellar: :any,                 arm64_sonoma:  "68c8a5208632ae2dcf4d7031ba2df33676cb716516aad8cc1428738e0c680fb7"
     sha256 cellar: :any,                 arm64_ventura: "ca527c1a5e3257d92a4b866738fba0bf00676d3f6172778fffaf5b43c1ab01d4"
     sha256 cellar: :any,                 ventura:       "732d1e5be266b5b0b7f396ea5faba7b0f3a4a53ce163271fb059ebd9b0ecf030"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e9dfe0f2b68f2d62c25b08359196877a579dace673f4e232b2ce9e57c36d0c36"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc2361a75f1a4ed5be7c912d3613c7adcd97bbd20b9c9e06b5a0659d4e7cf300"
   end
 
