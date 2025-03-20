4bc93fc02fa24121646f4d457bc640733cca50a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-20 23:01:01 +0000

    libdpp 10.1.2

--- Formula/lib/libdpp.rb
+++ Formula/lib/libdpp.rb
@@ -1,8 +1,8 @@
 class Libdpp < Formula
   desc "C++ Discord API Bot Library"
   homepage "https://github.com/brainboxdotcc/DPP"
-  url "https://github.com/brainboxdotcc/DPP/releases/download/v10.1.1/DPP-10.1.1.tar.gz"
-  sha256 "73b57a2b0927d4b5903b78a51cc4284ce0c95d036ee597aec186bf4a4cd78d59"
+  url "https://github.com/brainboxdotcc/DPP/releases/download/v10.1.2/DPP-10.1.2.tar.gz"
+  sha256 "587ef044775e6bdd560ec17afc302c1048ebb3454455116d7241431fbb16a823"
   license "Apache-2.0"
 
   bottle do
