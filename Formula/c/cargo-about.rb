5c3cdcd959a10a0669b9e396ba6b7254fa109dde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-20 10:52:27 +0000

    cargo-about 0.7.1

--- Formula/c/cargo-about.rb
+++ Formula/c/cargo-about.rb
@@ -1,8 +1,8 @@
 class CargoAbout < Formula
   desc "Cargo plugin to generate list of all licenses for a crate"
   homepage "https://github.com/EmbarkStudios/cargo-about"
-  url "https://github.com/EmbarkStudios/cargo-about/archive/refs/tags/0.7.0.tar.gz"
-  sha256 "baad943a78f25676e4ebfd089b49d3e275f3e015d5633f2c1c274c5ca607eeb9"
+  url "https://github.com/EmbarkStudios/cargo-about/archive/refs/tags/0.7.1.tar.gz"
+  sha256 "5b090871bda2c2cf645de826c219b41486c36ffe0b474ad9f6ecba7e21d279a0"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/EmbarkStudios/cargo-about.git", branch: "main"
 
