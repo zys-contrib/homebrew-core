6ce730168ebac97a69ab9aa8076547c296d8a646
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-20 11:56:59 +0000

    psalm: update 6.9.3 bottle.

--- Formula/p/psalm.rb
+++ Formula/p/psalm.rb
@@ -11,12 +11,12 @@ class Psalm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fd040d7d65ddc11d59ce635dad2389e5987111481ad48e61f6d6ef515a6355fd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fd040d7d65ddc11d59ce635dad2389e5987111481ad48e61f6d6ef515a6355fd"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "fd040d7d65ddc11d59ce635dad2389e5987111481ad48e61f6d6ef515a6355fd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9348376a6a30632f70c02cbeda788516dddfa106274d1bb085f3422b994b52a2"
-    sha256 cellar: :any_skip_relocation, ventura:       "9348376a6a30632f70c02cbeda788516dddfa106274d1bb085f3422b994b52a2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fd040d7d65ddc11d59ce635dad2389e5987111481ad48e61f6d6ef515a6355fd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2007f5f2ab3c1b545ba8beaa3f148e3e35de9ced42ab4a6eb025290d5b2f1d10"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2007f5f2ab3c1b545ba8beaa3f148e3e35de9ced42ab4a6eb025290d5b2f1d10"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2007f5f2ab3c1b545ba8beaa3f148e3e35de9ced42ab4a6eb025290d5b2f1d10"
+    sha256 cellar: :any_skip_relocation, sonoma:        "822c774b3552858e41f8df74e7e74bc52487b6f5c629dd2a3eb3158aacd214c0"
+    sha256 cellar: :any_skip_relocation, ventura:       "822c774b3552858e41f8df74e7e74bc52487b6f5c629dd2a3eb3158aacd214c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2007f5f2ab3c1b545ba8beaa3f148e3e35de9ced42ab4a6eb025290d5b2f1d10"
   end
 
   depends_on "composer" => :test
