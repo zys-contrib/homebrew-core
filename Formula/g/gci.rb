8824b4900087150fe57f5b0b3a1556c6615e2087
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-21 11:10:57 +0000

    gci: update 0.13.6 bottle.

--- Formula/g/gci.rb
+++ Formula/g/gci.rb
@@ -7,13 +7,12 @@ class Gci < Formula
   head "https://github.com/daixiang0/gci.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ca22bfa1698f88b7b13685952488a1676f72a0e1bf8f6aaa921af7f0b177cfe4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ca22bfa1698f88b7b13685952488a1676f72a0e1bf8f6aaa921af7f0b177cfe4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ca22bfa1698f88b7b13685952488a1676f72a0e1bf8f6aaa921af7f0b177cfe4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1de2b679e09da2cdd4b851e14d02b639e3eb9f8dbd9b3b44cfbf2b8bc5b9a48f"
-    sha256 cellar: :any_skip_relocation, ventura:       "1de2b679e09da2cdd4b851e14d02b639e3eb9f8dbd9b3b44cfbf2b8bc5b9a48f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "91bf88994d9d8986d778178b2bfeab695f73fc5923c9aee55d3283b64579c37d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3831ca85a8c9143b1eaa2cb0925078f2c7b0a6efc12f6e00a30419c9d88ae964"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3831ca85a8c9143b1eaa2cb0925078f2c7b0a6efc12f6e00a30419c9d88ae964"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3831ca85a8c9143b1eaa2cb0925078f2c7b0a6efc12f6e00a30419c9d88ae964"
+    sha256 cellar: :any_skip_relocation, sonoma:        "79bfc67c8457bd743262701889db578835c69709a757e340f22e6dbb89f5195f"
+    sha256 cellar: :any_skip_relocation, ventura:       "79bfc67c8457bd743262701889db578835c69709a757e340f22e6dbb89f5195f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9a1e1cd4652c79147e3940b6022b64d3e468baeaff0fa6d085341d455ec3335"
   end
 
   depends_on "go" => :build
