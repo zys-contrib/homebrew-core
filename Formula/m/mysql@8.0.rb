029b208b911c1a40353185badf4744c90725c84d
Rui Chen <rui@chenrui.dev>
2025-03-20 21:47:37 -0400

    mysql@8.0: revision bump (protobuf@29 29.4)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mysql@8.0.rb
+++ Formula/m/mysql@8.0.rb
@@ -4,7 +4,7 @@ class MysqlAT80 < Formula
   url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.41.tar.gz"
   sha256 "719589993b1a6769edb82b59f28e0dab8d47df94fa53ac4e9340b7c5eaba937c"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
-  revision 2
+  revision 3
 
   livecheck do
     url "https://dev.mysql.com/downloads/mysql/8.0.html?tpl=files&os=src&version=8.0"
