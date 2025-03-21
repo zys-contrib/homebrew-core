f342750d4906ce40c87b97efacd4df59b7e8a1c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-21 15:49:45 +0000

    xclogparser: update 0.2.41 bottle.

--- Formula/x/xclogparser.rb
+++ Formula/x/xclogparser.rb
@@ -6,12 +6,12 @@ class Xclogparser < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "edfc8bee21f0a9b8d5128b1c63cf6947f2b6dd94f5028d7055487685f616a130"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "05b37c69dd5b69fdc9e7af5257c3f75a633ef474c023f76452914b96544a858c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "4f17d868764764e163c07ff3603cc172aa805cfb2484da9313544ee46d050fed"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8583a877ef3ba6056c3d9010578c32a4c403887ee46f2dddc245d939b4d0e7ea"
-    sha256 cellar: :any_skip_relocation, ventura:       "619ab4c7ceeeb6260ffc8c89cb336f8e4f45066724506f76cd72a4ecba2eb4f5"
-    sha256                               x86_64_linux:  "c724f328b743f2d466efb32a3ceae6d5d99d497b86b218028b2f751d679e3ae1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0506dd7d0fd34cf648007ebaa19c22ee4a7950d621d0a81ecc0de1c4ab72e36f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f12a935ef504acb540d3e894cb29f4178b78e78cab47992801cc934edb22f82a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "325dca4b969addb194bc8be42bdfdda4b61cc6549d186f1372cd7e781e03db96"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3ecb9ea804d75559570439b988c2f75310052ac31362d5c39e0e58f66ed0b177"
+    sha256 cellar: :any_skip_relocation, ventura:       "ce099c27e0ee07214729ee9578472919bafc72056c80e5560293715fb45701ef"
+    sha256                               x86_64_linux:  "3a456db1fd20985ed03089f28e6256f7c0b4441cf70ad4d4c87f12cf021cc7db"
   end
 
   depends_on xcode: "13.0"
