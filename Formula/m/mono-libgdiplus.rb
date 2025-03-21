8c6190b2d1df7b9c9cd8e653400d29f4571c0e80
Michael Cho <michael@michaelcho.dev>
2025-03-21 19:54:33 -0400

    mono-libgdiplus 6.2

--- Formula/m/mono-libgdiplus.rb
+++ Formula/m/mono-libgdiplus.rb
@@ -1,13 +1,12 @@
 class MonoLibgdiplus < Formula
   desc "GDI+-compatible API on non-Windows operating systems"
   homepage "https://www.mono-project.com/docs/gui/libgdiplus/"
-  url "https://download.mono-project.com/sources/libgdiplus/libgdiplus-6.1.tar.gz"
-  sha256 "97d5a83d6d6d8f96c27fb7626f4ae11d3b38bc88a1726b4466aeb91451f3255b"
+  url "https://dl.winehq.org/mono/sources/libgdiplus/libgdiplus-6.2.tar.gz"
+  sha256 "683adb7d99d03f6ee7985173a206a2243f76632682334ced4cae2fcd20c83bc9"
   license "MIT"
-  revision 2
 
   livecheck do
-    url "https://download.mono-project.com/sources/libgdiplus/index.html"
+    url "https://dl.winehq.org/mono/sources/libgdiplus/"
     regex(/href=.*?libgdiplus[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
@@ -29,8 +28,6 @@ class MonoLibgdiplus < Formula
 
   depends_on "cairo"
   depends_on "fontconfig"
-  depends_on "freetype"
-  depends_on "gettext"
   depends_on "giflib"
   depends_on "glib"
   depends_on "harfbuzz"
@@ -40,22 +37,17 @@ class MonoLibgdiplus < Formula
   depends_on "libtiff"
   depends_on "pango"
 
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  on_macos do
+    depends_on "freetype"
+    depends_on "gettext"
   end
 
   def install
     system "./configure", "--disable-silent-rules",
-                          "--disable-tests",
                           "--without-x11",
                           *std_configure_args
     system "make"
-    cd "tests" do
-      system "make", "testbits"
-      system "./testbits"
-    end
+    system "./tests/testbits"
     system "make", "install"
   end
 
