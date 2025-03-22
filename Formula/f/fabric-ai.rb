064504a0fc546401c3df5017d83df5661befcf4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-22 10:56:58 +0000

    fabric-ai 1.4.164

--- Formula/f/fabric-ai.rb
+++ Formula/f/fabric-ai.rb
@@ -1,8 +1,8 @@
 class FabricAi < Formula
   desc "Open-source framework for augmenting humans using AI"
   homepage "https://danielmiessler.com/p/fabric-origin-story"
-  url "https://github.com/danielmiessler/fabric/archive/refs/tags/v1.4.163.tar.gz"
-  sha256 "afd64502c79a88b292428d0a8756b1f914b9d5472612382c95183ada0f799feb"
+  url "https://github.com/danielmiessler/fabric/archive/refs/tags/v1.4.164.tar.gz"
+  sha256 "481718a599663fe5d4ea00aea6e53c81f64183a1c551535573d8faf9b8f3bb67"
   license "MIT"
   head "https://github.com/danielmiessler/fabric.git", branch: "main"
 
