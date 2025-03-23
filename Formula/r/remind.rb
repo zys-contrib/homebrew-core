fd9c2f8c7dea063fb3bfc4a12f47e4c4ba251e62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-23 20:29:58 +0000

    remind: update 05.03.05 bottle.

--- Formula/r/remind.rb
+++ Formula/r/remind.rb
@@ -12,13 +12,13 @@ class Remind < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "be33e8e3942c1c2374658574d8638542c6a57ad76f16a516c35f489c4a46885b"
-    sha256 arm64_sonoma:  "2222f83345c80d6141bff980029f6143db3498f2dc4145be51145123cfb00638"
-    sha256 arm64_ventura: "ec6fe533bd3946608f26bc20deea6f94dbc19f64163c0ccd3b23e140290316cb"
-    sha256 sonoma:        "8befcf3aae83d65211cc5bba1878bfa110ae6bfbdc0fdab0eaf67d15d97075ce"
-    sha256 ventura:       "e09c71bd15a0add123fdd0471ea3ab2c938f4db98f354677ae16024aaa435b5c"
-    sha256 arm64_linux:   "a437d7244c3aa9e6cdefd444e66990b52da16d773841dca0865e3b0d35e40038"
-    sha256 x86_64_linux:  "07044ddeb712cab031f1d1c738139bfada0be99083294850df6a87f833042efa"
+    sha256 arm64_sequoia: "2a4f7f92cb06a3eb67bffd5c202532489543ce625b13fdf1f8ac63d5c827fd67"
+    sha256 arm64_sonoma:  "b735dac1e89c0b476c331e23483e78b06def50c75a7c670eccea6f36829b8321"
+    sha256 arm64_ventura: "3cca7b705afc133bec7fccfd98fac4cf72d623fabe50ca14900ede4fb3e78fbf"
+    sha256 sonoma:        "1485093c92771312c5871e46e426f79e5c976843e02d3a959e42b0dfb1bd3984"
+    sha256 ventura:       "8bd4246f518930674497a3cb428064964ea5b228aad68dcb330b43d2901f66cd"
+    sha256 arm64_linux:   "9c21778b244d799f910e995ec9548b6a542ccf7f27ae8026ab8e974f13bd970b"
+    sha256 x86_64_linux:  "784fcf32166ad34b0c9583336bcb06d936908e77233b80e5940abc031e7bc3a5"
   end
 
   conflicts_with "rem", because: "both install `rem` binaries"
