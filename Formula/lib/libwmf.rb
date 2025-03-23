9833977ac36ba3fcaee67dba0fc0c234990f46bb
Michael Cho <michael@michaelcho.dev>
2025-03-23 15:54:49 -0400

    libwmf 0.2.13

--- Formula/lib/libwmf.rb
+++ Formula/lib/libwmf.rb
@@ -1,15 +1,13 @@
 class Libwmf < Formula
   desc "Library for converting WMF (Window Metafile Format) files"
-  homepage "https://wvware.sourceforge.net/libwmf.html"
-  url "https://downloads.sourceforge.net/project/wvware/libwmf/0.2.8.4/libwmf-0.2.8.4.tar.gz"
-  sha256 "5b345c69220545d003ad52bfd035d5d6f4f075e65204114a9e875e84895a7cf8"
-  license "LGPL-2.1-only" # http://wvware.sourceforge.net/libwmf.html#download
-  revision 3
-
-  livecheck do
-    url :stable
-    regex(%r{url=.*?/libwmf[._-]v?(\d+(?:\.\d+)+)\.t}i)
-  end
+  homepage "https://github.com/caolanm/libwmf"
+  url "https://github.com/caolanm/libwmf/archive/refs/tags/v0.2.13.tar.gz"
+  sha256 "18ba69febd2f515d98a2352de284a8051896062ac9728d2ead07bc39ea75a068"
+  license all_of: [
+    "LGPL-2.0-or-later",
+    "GPL-2.0-or-later", # COPYING
+    "GD", # src/extra/gd
+  ]
 
   bottle do
     sha256 arm64_sequoia:  "e0cc5c1d1e18f0a0ebf5bd15ece8c8b542c0838756d51a1b6c4ff6374cb8bc32"
@@ -29,28 +27,37 @@ class Libwmf < Formula
   depends_on "pkgconf" => :build
 
   depends_on "freetype"
-  depends_on "gd"
   depends_on "jpeg-turbo"
   depends_on "libpng"
 
   uses_from_macos "expat"
   uses_from_macos "zlib"
 
-  def install
-    args = []
-    # Help old config scripts identify arm64 linux
-    args << "--build=aarch64-unknown-linux-gnu" if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+  # Backport fix for macOS
+  patch do
+    url "https://github.com/caolanm/libwmf/commit/5c0ffc6320c40a565ff8014d772670df3e0ad87d.patch?full_index=1"
+    sha256 "80ae84a904baa21e1566e3d2bca1c6aaa0a2a30f684fe50f25e7e5751ef3ec93"
+  end
 
-    system "./configure", "--with-png=#{Formula["libpng"].opt_prefix}",
-                          "--with-freetype=#{Formula["freetype"].opt_prefix}",
-                          "--with-jpeg=#{Formula["jpeg-turbo"].opt_prefix}",
-                          *args, *std_configure_args
-    system "make"
-    ENV.deparallelize # yet another rubbish Makefile
+  def install
+    system "./configure", "--disable-silent-rules",
+                          "--with-gsfontdir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts",
+                          "--with-gsfontmap=#{HOMEBREW_PREFIX}/share/ghostscript/Resource/Init/Fontmap.GS",
+                          "--without-x",
+                          *std_configure_args
     system "make", "install"
   end
 
   test do
+    resource "formula1.wmf" do
+      url "https://github.com/caolanm/libwmf/raw/3ea3a65ad1b4528ed1c5795071a0142a0e61ec7b/examples/formula1.wmf"
+      sha256 "a0d9829692eebfa3bdb23d62f474d58cc4ea2489c07c6fcb63338eb3fb2c14d2"
+    end
+    resource("formula1.wmf").stage(testpath)
+
+    output = shell_output("#{bin}/wmf2svg --maxwidth=100 --maxheight=100 formula1.wmf")
+    assert_match '<svg width="100" height="18"', output
+
     assert_match version.major_minor_patch.to_s, shell_output("#{bin}/wmf2svg --version 2>&1", 2)
   end
 end
