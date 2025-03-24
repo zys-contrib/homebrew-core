4eabddea5c5a85be323c6fca254ed91951af3d2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-24 13:25:52 +0000

    lavat: add 2.1.0 bottle.

--- Formula/l/lavat.rb
+++ Formula/l/lavat.rb
@@ -6,6 +6,15 @@ class Lavat < Formula
   license "MIT"
   head "https://github.com/AngelJumbo/lavat.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "38711addd8f972b79986ae0bbaa31468cf7d90ce859835bf7a48e25a97c5b93e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa92f862f00dff1a0d2e8d0c54ae9b3ca03e3318b84926fc191159a3705ea13a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "aa35b73d1492a19eafc5bbd3302a1077edaa9b278e693d9794bc277e3612ec48"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2e47c15c73dd362b47bf2cbf6a41a331f74d2e52d66a8ce400799f22cb832457"
+    sha256 cellar: :any_skip_relocation, ventura:       "374fe07d517384128561390cb971d836add8956932030f29062df39e97f3e23e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25c91db37d725ae7e134e65cc7151e104e57dc0fd0adf2b431feb1593bb00a9b"
+  end
+
   def install
     system "make" # `make install` doesn't work on macOS
     bin.install "lavat"
