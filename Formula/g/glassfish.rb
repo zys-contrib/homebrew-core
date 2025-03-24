aaa48be0fb028e165a27c546409f1345638bc0c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-24 23:01:37 +0000

    glassfish 7.0.23

--- Formula/g/glassfish.rb
+++ Formula/g/glassfish.rb
@@ -1,9 +1,9 @@
 class Glassfish < Formula
   desc "Java EE application server"
   homepage "https://glassfish.org/"
-  url "https://download.eclipse.org/ee4j/glassfish/glassfish-7.0.22.zip"
-  mirror "https://github.com/eclipse-ee4j/glassfish/releases/download/7.0.22/glassfish-7.0.22.zip"
-  sha256 "9bc7ece53c454b088c49997214d4a002d2256cbe039403951ebde4a257d14c8e"
+  url "https://download.eclipse.org/ee4j/glassfish/glassfish-7.0.23.zip"
+  mirror "https://github.com/eclipse-ee4j/glassfish/releases/download/7.0.23/glassfish-7.0.23.zip"
+  sha256 "e3fc0e6b3cddaaf9038fa01839687086c2e89f7df47c27645d28f264b967a1c9"
   license "EPL-2.0"
 
   livecheck do
