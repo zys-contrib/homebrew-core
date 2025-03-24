517d1bbb929c6239a609c8702765b79be60ca841
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-24 10:15:35 +0000

    statesmith: add 0.17.5 bottle.

--- Formula/s/statesmith.rb
+++ Formula/s/statesmith.rb
@@ -5,6 +5,14 @@ class Statesmith < Formula
   sha256 "185fc6c05c8c950153bb871ffdad6de47ebf2db18c4607cd4005662d5d9f79b6"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "06a75aa770bfbe5736191b850192f3018d60de182fb55dbb3a84dba2b4a7c90c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "926fafa1d15bcca00d6372824d26c6bbd55a59b0a6cf5cf34ec338c7462321e4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0a11d4baa36bad4b9781afeda8e6767a5e6883e800cfbda179755c41f0344f13"
+    sha256 cellar: :any_skip_relocation, ventura:       "0a52242c5fc55c753a125e12de944672d59ca660c5f7852b7e8809996380b0b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9cccfa195619b84f8094ad0a86f8b6ec9b6f11d6c137bbedca6e7f10d06c3c2c"
+  end
+
   depends_on "dotnet"
   depends_on "icu4c@77"
   uses_from_macos "zlib"
