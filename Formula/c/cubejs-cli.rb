ea5c28bc844fd707fd0de52ac57ee0ba6e546c7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-25 19:55:17 +0000

    cubejs-cli 1.2.27

--- Formula/c/cubejs-cli.rb
+++ Formula/c/cubejs-cli.rb
@@ -1,8 +1,8 @@
 class CubejsCli < Formula
   desc "Cube.js command-line interface"
   homepage "https://cube.dev/"
-  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-1.2.26.tgz"
-  sha256 "561c4cbd7aa9fdb4c245861041d73570815cc3a0f2600e50d76477bac60f49eb"
+  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-1.2.27.tgz"
+  sha256 "b86ed668cb416995bdc41ffb38f11d339ffd239d709b9a1fff6b1d448666f43a"
   license "Apache-2.0"
 
   bottle do
