4200873bd11b8e3625fbca43fabf3117d408fe3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-25 19:52:53 +0000

    calicoctl 3.29.3

--- Formula/c/calicoctl.rb
+++ Formula/c/calicoctl.rb
@@ -2,8 +2,8 @@ class Calicoctl < Formula
   desc "Calico CLI tool"
   homepage "https://www.projectcalico.org"
   url "https://github.com/projectcalico/calico.git",
-      tag:      "v3.29.2",
-      revision: "c29210835f7a2795d0791974602c8e1c625c8ca1"
+      tag:      "v3.29.3",
+      revision: "b5d7d74c111d943251b61827ea88d9e5518f823b"
   license "Apache-2.0"
   head "https://github.com/projectcalico/calico.git", branch: "master"
 
