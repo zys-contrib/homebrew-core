9621daf7b1b4659c62bc301e9bbd39076188ad26
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-25 02:34:03 +0000

    rakudo-star 2025.03

--- Formula/r/rakudo-star.rb
+++ Formula/r/rakudo-star.rb
@@ -1,8 +1,8 @@
 class RakudoStar < Formula
   desc "Rakudo compiler and commonly used packages"
   homepage "https://rakudo.org/"
-  url "https://github.com/rakudo/star/releases/download/2025.02/rakudo-star-2025.02.tar.gz"
-  sha256 "0f85bd4556d89941bead607e8316af5632fb256b0569d49273aa5e9a043f8b6f"
+  url "https://github.com/rakudo/star/releases/download/2025.03/rakudo-star-2025.03.tar.gz"
+  sha256 "fb5f2aade099dfa66b258eedc73d408f1d83d25fefe5432349a39d815f586126"
   license "Artistic-2.0"
 
   livecheck do
