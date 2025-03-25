707f937c68461672e7ad978970649f5949ba7587
Rui Chen <rui@chenrui.dev>
2025-03-24 23:25:32 -0400

    redstore: add explicit `--build` flag for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/redstore.rb
+++ Formula/r/redstore.rb
@@ -39,7 +39,11 @@ class Redstore < Formula
 
     ENV.append "CFLAGS", "-D_GNU_SOURCE" unless OS.mac?
 
-    system "./configure", *std_configure_args.reject { |s| s["--disable-debug"] }
+    args = []
+    # Help old config scripts identify arm64 linux
+    args << "--build=aarch64-unknown-linux-gnu" if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+
+    system "./configure", *args, *std_configure_args
     system "make", "install"
   end
 
