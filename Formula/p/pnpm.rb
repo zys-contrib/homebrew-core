2706961bda7bfe30ee2c6727725936f43f2dbb1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-26 15:32:08 +0000

    pnpm: update 10.7.0 bottle.

--- Formula/p/pnpm.rb
+++ Formula/p/pnpm.rb
@@ -11,13 +11,13 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "f3a079b4fbf63b9998b9ad1b9ebb523dfaed5448ac57c944c6976eeaf9af4d35"
-    sha256 cellar: :any,                 arm64_sonoma:  "f3a079b4fbf63b9998b9ad1b9ebb523dfaed5448ac57c944c6976eeaf9af4d35"
-    sha256 cellar: :any,                 arm64_ventura: "f3a079b4fbf63b9998b9ad1b9ebb523dfaed5448ac57c944c6976eeaf9af4d35"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8441b2a5c37257e6e4362a5fde05bf148a398a1fd79cff1b91ad14fd0d275161"
-    sha256 cellar: :any_skip_relocation, ventura:       "8441b2a5c37257e6e4362a5fde05bf148a398a1fd79cff1b91ad14fd0d275161"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a76a27c0cacb931bf31e0bf3ef4f3b588d61e657786ce506b0cb54cae2cb061d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a76a27c0cacb931bf31e0bf3ef4f3b588d61e657786ce506b0cb54cae2cb061d"
+    sha256 cellar: :any,                 arm64_sequoia: "a43562d7dfd8eb751a6913548e6adb85b5e2debf918e51e655e67ee6e5e12a43"
+    sha256 cellar: :any,                 arm64_sonoma:  "a43562d7dfd8eb751a6913548e6adb85b5e2debf918e51e655e67ee6e5e12a43"
+    sha256 cellar: :any,                 arm64_ventura: "a43562d7dfd8eb751a6913548e6adb85b5e2debf918e51e655e67ee6e5e12a43"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8544b60b94883dd73b8a2e166f057b84cf9fefe4a2ebfba9ff61df3d36f1186d"
+    sha256 cellar: :any_skip_relocation, ventura:       "8544b60b94883dd73b8a2e166f057b84cf9fefe4a2ebfba9ff61df3d36f1186d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f8cec625645b0c3510cb5a4189d645b59b9c087db5dcbaf82b936a7a4436d09e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f8cec625645b0c3510cb5a4189d645b59b9c087db5dcbaf82b936a7a4436d09e"
   end
 
   depends_on "node" => [:build, :test]
