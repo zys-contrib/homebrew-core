6ef7ade35d0bfeed90bb6eb39b14035c7bf69df8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-27 17:30:35 +0000

    kyma-cli: update 3.0.0 bottle.

--- Formula/k/kyma-cli.rb
+++ Formula/k/kyma-cli.rb
@@ -15,14 +15,12 @@ class KymaCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "8e2908d074bedcae0b73a8279a6c3856d0a6bc2234dd20adabc3abe2dd47d352"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bff4fcdeda28c0ff582c34aa30ee171319b4478b9d6af175d949d525c6cbf5d5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9f4d62bc61545db83c20e731e14ed0de8d35856f88bc9d0f3ae2008f6c72f8fb"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c5193a6b23e406ef87e9c6829e63de28888199ffdbe5bff65dec95bd9e87e07b"
-    sha256 cellar: :any_skip_relocation, sonoma:         "51ea7ae1f069f4ae9b80044ba461c314c5c8e68b1fbbcb1dfb4ab03c2aa160ed"
-    sha256 cellar: :any_skip_relocation, ventura:        "6ee34b3498729c5069494b95b08a589d4468834a2001c17e45131ed2ce53e647"
-    sha256 cellar: :any_skip_relocation, monterey:       "adafe8dcaf3bf488a2df29c7e92647fb45f097f3c532b78d7a60074a49bc60e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9cb2e46ef5d6cb0ca12ae214815c9c40b6e233f403a0574f4833df8d413ff37c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4014dd109281c286199191b2b2fc55413c4693db0580a192c02cfc4a94b96594"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4014dd109281c286199191b2b2fc55413c4693db0580a192c02cfc4a94b96594"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4014dd109281c286199191b2b2fc55413c4693db0580a192c02cfc4a94b96594"
+    sha256 cellar: :any_skip_relocation, sonoma:        "06ffb6fe90d8c3bc33e677490a6296f83aae63ac35a6656d5f2bd94ebb279a7a"
+    sha256 cellar: :any_skip_relocation, ventura:       "06ffb6fe90d8c3bc33e677490a6296f83aae63ac35a6656d5f2bd94ebb279a7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45737ddf5c24904e45ed8059c0fd608b43e8b58669c3a755053a2f4195c7f150"
   end
 
   depends_on "go" => :build
