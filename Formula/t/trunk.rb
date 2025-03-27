d2452123d5d3f9c652f6182f9ff5a08986a312a4
Anthony Dodd <2380740+thedodd@users.noreply.github.com>
2025-03-27 17:12:17 +0100

    trunk 0.21.12
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/t/trunk.rb
+++ Formula/t/trunk.rb
@@ -1,8 +1,8 @@
 class Trunk < Formula
   desc "Build, bundle & ship your Rust WASM application to the web"
   homepage "https://trunkrs.dev/"
-  url "https://github.com/trunk-rs/trunk/archive/refs/tags/v0.21.11.tar.gz"
-  sha256 "ce6a4baf16f2ce9f738312392ddd4f751e9d76ad646e92052720e807e4bd3970"
+  url "https://github.com/trunk-rs/trunk/archive/refs/tags/v0.21.12.tar.gz"
+  sha256 "8b4f6be421c9aec08b327e79e50fad8015dc6f56fc0c856d9320bdf97df0bc05"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/trunk-rs/trunk.git", branch: "main"
 
