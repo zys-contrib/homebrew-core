940cd3c8c212addebbb687d1e182d0a3171347f0
Michael Cho <michael@michaelcho.dev>
2025-03-28 17:48:45 -0400

    enzyme: revision bump (llvm 20.1.1)

--- Formula/e/enzyme.rb
+++ Formula/e/enzyme.rb
@@ -4,6 +4,7 @@ class Enzyme < Formula
   url "https://github.com/EnzymeAD/Enzyme/archive/refs/tags/v0.0.173.tar.gz"
   sha256 "b8477fb5bead9e9ece76d450ebd0afee99914235c6e1a6ef8c05bf288e3c0478"
   license "Apache-2.0" => { with: "LLVM-exception" }
+  revision 1
   head "https://github.com/EnzymeAD/Enzyme.git", branch: "main"
 
   bottle do
