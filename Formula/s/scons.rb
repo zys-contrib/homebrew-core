848012d025bf56739d02e39d8ec9f038e2a5ed7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-28 00:06:15 +0000

    scons: update 4.9.1 bottle.

--- Formula/s/scons.rb
+++ Formula/s/scons.rb
@@ -8,13 +8,13 @@ class Scons < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "151c811ed2fb8f49ded747f540963eeb0c9e0709791e4f19de0fe3fef97dc88a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "151c811ed2fb8f49ded747f540963eeb0c9e0709791e4f19de0fe3fef97dc88a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "151c811ed2fb8f49ded747f540963eeb0c9e0709791e4f19de0fe3fef97dc88a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c4e4aac6930d155026974edde3bf8a1d4784794d55afc7d6715efdad9fb8a407"
-    sha256 cellar: :any_skip_relocation, ventura:       "c4e4aac6930d155026974edde3bf8a1d4784794d55afc7d6715efdad9fb8a407"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "469d4d9ed388432e0fa083001925d30aa16975e9a1f138234deb556418cb7b57"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "151c811ed2fb8f49ded747f540963eeb0c9e0709791e4f19de0fe3fef97dc88a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e0a81c5d96f6ac781416672b44e49febc790de4cf60a239485efb95b3d6cadce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e0a81c5d96f6ac781416672b44e49febc790de4cf60a239485efb95b3d6cadce"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e0a81c5d96f6ac781416672b44e49febc790de4cf60a239485efb95b3d6cadce"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6da09c0f0c4cec9bad1951ce49c3b37c51b3596639fa4c90b60745bcc81b8c55"
+    sha256 cellar: :any_skip_relocation, ventura:       "6da09c0f0c4cec9bad1951ce49c3b37c51b3596639fa4c90b60745bcc81b8c55"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e0a81c5d96f6ac781416672b44e49febc790de4cf60a239485efb95b3d6cadce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0a81c5d96f6ac781416672b44e49febc790de4cf60a239485efb95b3d6cadce"
   end
 
   depends_on "python@3.13"
