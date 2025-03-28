ce56c757be97296ce8c8c0b6de5b807f6821f917
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-28 12:12:11 +0000

    nuitka: update 2.6.9 bottle.

--- Formula/n/nuitka.rb
+++ Formula/n/nuitka.rb
@@ -8,13 +8,13 @@ class Nuitka < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8be3c1282263241d5677ab40491d55c67657726031072f7581c7175a052623b8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5ffa16c87a1f0684c7248bd3a4d1d116db0d7aeb7abf58d16abbbfe3d8a17a32"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8f4189b7dc7bf64150cfeeede16272412ad197d4ad8690b81d2254609607475f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "918010f5e04e295cfbeb7c9a020bea832a883996d841e77af439f35ee1488508"
-    sha256 cellar: :any_skip_relocation, ventura:       "b82268482c0cce7b07ba5942b0c469d802a26cddb9f91ad2caf4e34dfccb309a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6505207697ee114bb97ebb59843b004e9c4fab75fff222fd6a4b4be6f01cd1f3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fd23a4f4dde84767f031fb10c5805a192596274988a6694ecdd90d97be5df10d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "23880f776dca541ee9fd6712b432f240c7e30a45948edd9672cfa358646bb18d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b6d6e0ae25f51fdbe55e9cbcea852802ea8cc0bcf0a990e2e0dce3255a73437e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a80b5946706c15115b4fc6735e2dc253f103d8f2a02be2a91182d2fbf16064b9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "45318fba7a8320fc0a4f1a83d6faf9a674dd8dc9adac1e9050332131694de01b"
+    sha256 cellar: :any_skip_relocation, ventura:       "aa4d62f97c733f3a0c1ef9ea8690844733e9017c5a00238c1b173bec0c08555d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eab8e120765d074929bdc249fd8d0c66bfed842a381f8387b6eb5b945ed72bd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "011682b6279cc07756cf03f49842e4ee4ae023eb227d8ead30568a996ab75649"
   end
 
   depends_on "ccache"
