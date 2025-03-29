d3218b31ce0597ebf3ad3191f3d265ac2fa444c0
Thierry Moisan <thierry.moisan@gmail.com>
2025-03-29 15:11:43 -0400

    ibex 2.9.0
    * Build shared libraries
    * Allow building on arm64
    * Add workarounds for Intel macOS
    * Use CMake to test
    
    Co-authored-by: Michael Cho <michael@michaelcho.dev>

--- Formula/i/ibex.rb
+++ Formula/i/ibex.rb
@@ -1,8 +1,8 @@
 class Ibex < Formula
   desc "C++ library for constraint processing over real numbers"
   homepage "https://ibex-team.github.io/ibex-lib/"
-  url "https://github.com/ibex-team/ibex-lib/archive/refs/tags/ibex-2.8.9.tar.gz"
-  sha256 "fee448b3fa3929a50d36231ff2f14e5480a0b82506594861536e3905801a6571"
+  url "https://github.com/ibex-team/ibex-lib/archive/refs/tags/ibex-2.9.0.tar.gz"
+  sha256 "8d16ac2dfbc6de0353a12b7008d1d566bda52178f247d8461be02063972311a6"
   license "LGPL-3.0-only"
   head "https://github.com/ibex-team/ibex-lib.git", branch: "master"
 
@@ -21,18 +21,33 @@ class Ibex < Formula
   end
 
   depends_on "bison" => :build
-  depends_on "cmake" => :build
+  depends_on "cmake" => [:build, :test]
   depends_on "flex" => :build
-  depends_on "pkgconf" => [:build, :test]
-  depends_on arch: :x86_64
+  depends_on "pkgconf" => :build
 
   uses_from_macos "zlib"
 
-  def install
-    ENV.cxx11
+  # Workaround for Intel macOS processor detection
+  # Issue ref: https://github.com/ibex-team/ibex-lib/issues/567
+  patch :DATA
 
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args.reject { |s| s["CMAKE_INSTALL_LIBDIR"] }
-    system "cmake", "--build", "build", "--", "SHARED=true"
+  def install
+    rpaths = [loader_path, rpath, rpath(target: lib/"ibex/3rd")]
+
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpaths.join(";")}",
+                    *std_cmake_args.reject { |s| s["CMAKE_INSTALL_LIBDIR"] }
+
+    # Workaround for Intel macOS build error: no member named '__fpcr' in 'fenv_t'
+    # Issue ref: https://github.com/ibex-team/ibex-lib/issues/567
+    if OS.mac? && Hardware::CPU.intel?
+      inreplace "build/interval_lib_wrapper/gaol/gaol-4.2.3alpha0/gaol/gaol_fpu_fenv.h",
+                "#if __APPLE__\n#   define CTRLWORD(v) (v).__fpcr",
+                "#if __APPLE__\n#   define CTRLWORD(v) (v).__control"
+    end
+
+    system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
     pkgshare.install %w[examples benchs/solver]
@@ -44,18 +59,24 @@ class Ibex < Formula
   end
 
   test do
-    ENV.cxx11
-
-    cp_r (pkgshare/"examples").children, testpath
-
-    (1..8).each do |n|
-      system "make", "lab#{n}"
-      system "./lab#{n}"
-    end
-
-    (1..3).each do |n|
-      system "make", "-C", "slam", "slam#{n}"
-      system "./slam/slam#{n}"
-    end
+    system "cmake", "-S", pkgshare/"examples", "-B", ".", "-DCMAKE_BUILD_RPATH=#{lib};#{lib}/ibex/3rd"
+    system "cmake", "--build", "."
+    (1..8).each { |n| system "./lab#{n}" }
+    (1..3).each { |n| system "./slam#{n}" }
   end
 end
+
+__END__
+diff --git a/interval_lib_wrapper/gaol/3rd/mathlib-2.1.1/CMakeLists.txt b/interval_lib_wrapper/gaol/3rd/mathlib-2.1.1/CMakeLists.txt
+index 65b5ea8b..24a2e5b9 100644
+--- a/interval_lib_wrapper/gaol/3rd/mathlib-2.1.1/CMakeLists.txt
++++ b/interval_lib_wrapper/gaol/3rd/mathlib-2.1.1/CMakeLists.txt
+@@ -43,7 +43,7 @@ elseif (CMAKE_SYSTEM MATCHES "CYGWIN" AND CMAKE_SYSTEM_PROCESSOR MATCHES "i.86")
+ elseif (CMAKE_SYSTEM MATCHES "Darwin")
+   if (CMAKE_SYSTEM_PROCESSOR MATCHES "arm64")
+     set (MATHLIB_AARCH64 ON)
+-  elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "i.86")
++  elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "i.86|86_64")
+     set (MATHLIB_I86_MACOSX ON)
+     set (IX86_CPU ON)
+   else ()
