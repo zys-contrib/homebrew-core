07fae6f98527c421013c2752251b5df2455a2117
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-29 14:17:22 +0000

    spdlog 1.15.2

--- Formula/s/spdlog.rb
+++ Formula/s/spdlog.rb
@@ -1,8 +1,8 @@
 class Spdlog < Formula
   desc "Super fast C++ logging library"
   homepage "https://github.com/gabime/spdlog"
-  url "https://github.com/gabime/spdlog/archive/refs/tags/v1.15.1.tar.gz"
-  sha256 "25c843860f039a1600f232c6eb9e01e6627f7d030a2ae5e232bdd3c9205d26cc"
+  url "https://github.com/gabime/spdlog/archive/refs/tags/v1.15.2.tar.gz"
+  sha256 "7a80896357f3e8e920e85e92633b14ba0f229c506e6f978578bdc35ba09e9a5d"
   license "MIT"
   head "https://github.com/gabime/spdlog.git", branch: "v1.x"
 
