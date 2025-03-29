767811fcb7d4c9f34a2c04dd4d5a108c393c97db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-29 11:11:28 +0000

    nvtop 3.2.0

--- Formula/n/nvtop.rb
+++ Formula/n/nvtop.rb
@@ -1,8 +1,8 @@
 class Nvtop < Formula
   desc "Interactive GPU process monitor"
   homepage "https://github.com/Syllo/nvtop"
-  url "https://github.com/Syllo/nvtop/archive/refs/tags/3.1.0.tar.gz"
-  sha256 "9481c45c136163574f1f16d87789859430bc90a1dc62f181b269b5edd92f01f3"
+  url "https://github.com/Syllo/nvtop/archive/refs/tags/3.2.0.tar.gz"
+  sha256 "d26df685455023cedc4dda033862dcddb67402fbdb685da70da78492f73c41d0"
   license "GPL-3.0-or-later"
 
   bottle do
