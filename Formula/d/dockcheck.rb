9df7093ce51c863aa131a42e727645e626c7a9d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-30 13:54:33 +0000

    dockcheck 0.6.0

--- Formula/d/dockcheck.rb
+++ Formula/d/dockcheck.rb
@@ -1,8 +1,8 @@
 class Dockcheck < Formula
   desc "CLI tool to automate docker image updates"
   homepage "https://github.com/mag37/dockcheck"
-  url "https://github.com/mag37/dockcheck/archive/refs/tags/v0.5.8.0.tar.gz"
-  sha256 "bb9c5b5868496188ba001c8a81acf34da1e774886571819ab636ef80afc6a56d"
+  url "https://github.com/mag37/dockcheck/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "1b00ab268918f2a5ee7f7836855e38bb13ed452c0fa3c5ade9ddd0a2c5589424"
   license "GPL-3.0-only"
   head "https://github.com/mag37/dockcheck.git", branch: "main"
 
