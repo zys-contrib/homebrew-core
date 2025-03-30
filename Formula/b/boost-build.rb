cddc1c9f27184ce52d766a0c28648bcac4c7fe15
Michael Cho <michael@michaelcho.dev>
2025-03-29 21:57:18 -0400

    boost-build 1.88.0

--- Formula/b/boost-build.rb
+++ Formula/b/boost-build.rb
@@ -1,8 +1,8 @@
 class BoostBuild < Formula
   desc "C++ build system"
   homepage "https://www.boost.org/build/"
-  url "https://github.com/boostorg/build/archive/refs/tags/boost-1.87.0.tar.gz"
-  sha256 "827cf29078d41d6906e07f32715fccf1b4635f8995170e500b18b89a55fec10b"
+  url "https://github.com/boostorg/build/archive/refs/tags/boost-1.88.0.tar.gz"
+  sha256 "a131c25bfe7c1b1e20da89a4c6e90a58a4bc55b361ae8c10199bb68b280aab96"
   license "BSL-1.0"
   version_scheme 1
   head "https://github.com/boostorg/build.git", branch: "develop"
