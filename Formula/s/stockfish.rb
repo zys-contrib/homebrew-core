0b934d3c4a003aa7d4d55c23104594427e52a066
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-30 14:13:41 +0000

    stockfish 17.1

--- Formula/s/stockfish.rb
+++ Formula/s/stockfish.rb
@@ -1,8 +1,8 @@
 class Stockfish < Formula
   desc "Strong open-source chess engine"
   homepage "https://stockfishchess.org/"
-  url "https://github.com/official-stockfish/Stockfish/archive/refs/tags/sf_17.tar.gz"
-  sha256 "8f9b52285c3348c065b7cb58410626df16d7416a2e60a3b04f3ec7c038e67ad1"
+  url "https://github.com/official-stockfish/Stockfish/archive/refs/tags/sf_17.1.tar.gz"
+  sha256 "0cfd9396438798cc68f5c0d5fa0bb458bb8ffff7de06add841aaeace86bec1f1"
   license "GPL-3.0-only"
   head "https://github.com/official-stockfish/Stockfish.git", branch: "master"
 
