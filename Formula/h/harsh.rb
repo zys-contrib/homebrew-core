199ccc32e7c2a6533970d90e3077cfeaa72bbd3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-30 02:56:54 +0000

    harsh: add 0.10.15 bottle.

--- Formula/h/harsh.rb
+++ Formula/h/harsh.rb
@@ -6,6 +6,15 @@ class Harsh < Formula
   license "MIT"
   head "https://github.com/wakatara/harsh.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e177987c659c5778c76ebc560b3c0be8c7c1b599b6ab0c4375ca469b31d4ef63"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e177987c659c5778c76ebc560b3c0be8c7c1b599b6ab0c4375ca469b31d4ef63"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e177987c659c5778c76ebc560b3c0be8c7c1b599b6ab0c4375ca469b31d4ef63"
+    sha256 cellar: :any_skip_relocation, sonoma:        "df08ab27386a55b3da263d2ebb0172e9499ffb0b5abc0347eb46e2ff67e38111"
+    sha256 cellar: :any_skip_relocation, ventura:       "df08ab27386a55b3da263d2ebb0172e9499ffb0b5abc0347eb46e2ff67e38111"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2f886079d7cfe3c054717edaa8bcc48bd92e788a249459a2b02bebc1c58a0438"
+  end
+
   depends_on "go" => :build
 
   def install
