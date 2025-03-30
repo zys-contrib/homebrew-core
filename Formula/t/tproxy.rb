2e7ec974c46244de96a39d71b8a738c25f6a5ead
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-30 15:02:52 +0000

    tproxy: update 0.9.0 bottle.

--- Formula/t/tproxy.rb
+++ Formula/t/tproxy.rb
@@ -7,14 +7,12 @@ class Tproxy < Formula
   head "https://github.com/kevwan/tproxy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "dbe9eb41efb8f2bd94a30e1675db6cc5778c0fd50cec078aede144a545699890"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "cb06f411cc40f326f48954e8d94460c263d1f3f7bd85a039ccd3ae561931fc3a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e68cf3e95c0cd12044d42c914b9abb0ddda44cb291f7619d773e151c1121d7e0"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "79bcfec2219b2dacb014ccb0e8630438399bc61d6a85bcf94ad0361259958f7f"
-    sha256 cellar: :any_skip_relocation, sonoma:         "cb066a9fd07ea71affb5763d25109842bff24c51b734e8a285b16f57b6d09117"
-    sha256 cellar: :any_skip_relocation, ventura:        "84338b9a2e65797eee898956f9534d1d3f7690a2f11dd57293a9b5b7b252f06a"
-    sha256 cellar: :any_skip_relocation, monterey:       "353712201999660dfb19485fc8694964b94a57041b9957c3b2f7c9aa25345b8c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f70748e6b276d4a23d074fa908f1a4914a314aff7cb6aee26fc2961f23d68813"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d8ec0fe223603e2b5f100a9aefd415c8e7c27c7db92aadade292812d0cd14d66"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d8ec0fe223603e2b5f100a9aefd415c8e7c27c7db92aadade292812d0cd14d66"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d8ec0fe223603e2b5f100a9aefd415c8e7c27c7db92aadade292812d0cd14d66"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6f8605f431b78c3cbf0792d2764aed057479af3c93428e872e7aa36249155331"
+    sha256 cellar: :any_skip_relocation, ventura:       "6f8605f431b78c3cbf0792d2764aed057479af3c93428e872e7aa36249155331"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "123df9d7f9259ef84639a7ab4dd1c75e9f1844d1c3044848aad74fec24be5d27"
   end
 
   depends_on "go" => :build
