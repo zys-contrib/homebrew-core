b03605f04db38df52b6006e233e99b1137a64c41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-31 16:53:02 +0000

    bender 0.28.2

--- Formula/b/bender.rb
+++ Formula/b/bender.rb
@@ -1,8 +1,8 @@
 class Bender < Formula
   desc "Dependency management tool for hardware projects"
   homepage "https://github.com/pulp-platform/bender"
-  url "https://github.com/pulp-platform/bender/archive/refs/tags/v0.28.1.tar.gz"
-  sha256 "939ab78fcc9b02947db0e65363b8ac8e9a2109f02b2abd646fc9591b036136a3"
+  url "https://github.com/pulp-platform/bender/archive/refs/tags/v0.28.2.tar.gz"
+  sha256 "1de599efd238f3238e9bed09005547e63b82c4d98affcd63c565ab650bebc9ad"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/pulp-platform/bender.git", branch: "master"
 
