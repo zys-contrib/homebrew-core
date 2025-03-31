ed1c15601b6b83406226a06cbb2fe74d6f93f401
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-31 14:47:24 +0000

    apify-cli: update 0.21.3 bottle.

--- Formula/a/apify-cli.rb
+++ Formula/a/apify-cli.rb
@@ -6,13 +6,13 @@ class ApifyCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "89b16e4443929bcafb2c9479d10a6de26d9c0e823b963f348fa336fd45e1d303"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89b16e4443929bcafb2c9479d10a6de26d9c0e823b963f348fa336fd45e1d303"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "89b16e4443929bcafb2c9479d10a6de26d9c0e823b963f348fa336fd45e1d303"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f1bdd007a01d2a2d2d19e58e287b2378bd855a76a5c17bf36374bf8b0c3329bf"
-    sha256 cellar: :any_skip_relocation, ventura:       "f1bdd007a01d2a2d2d19e58e287b2378bd855a76a5c17bf36374bf8b0c3329bf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0ecd1d22553f0921e7b270ef3e018a555163d4fba8937a9267199867668155fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21d2a680762fbb4c90fac1ee02e07fdccb09c046a3b141b81daec93bd0d8bf1a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ed5db17096f5e734a3bdd35fa4eeb00296fa75570aa39b2683d315a883837ca7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ed5db17096f5e734a3bdd35fa4eeb00296fa75570aa39b2683d315a883837ca7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ed5db17096f5e734a3bdd35fa4eeb00296fa75570aa39b2683d315a883837ca7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "035450807712a495cad287da51f226f67188fc6ea3427bc817f28598ee686e4c"
+    sha256 cellar: :any_skip_relocation, ventura:       "035450807712a495cad287da51f226f67188fc6ea3427bc817f28598ee686e4c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6320376afc731b3353acea94a163af53dda8f2ada758272f9138565ca3ad5a7b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6320376afc731b3353acea94a163af53dda8f2ada758272f9138565ca3ad5a7b"
   end
 
   depends_on "node"
