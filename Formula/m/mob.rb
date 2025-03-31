88a1d3836a4664c07e0e3cc2cd4e1fd8072d5853
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-31 21:11:53 +0000

    mob: add 5.4.0 bottle.

--- Formula/m/mob.rb
+++ Formula/m/mob.rb
@@ -6,6 +6,15 @@ class Mob < Formula
   license "MIT"
   head "https://github.com/remotemobprogramming/mob.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea259acacda5e76abcb29c2291753b5567acc90d86d7e698645b48cfe5eb4a21"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ea259acacda5e76abcb29c2291753b5567acc90d86d7e698645b48cfe5eb4a21"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ea259acacda5e76abcb29c2291753b5567acc90d86d7e698645b48cfe5eb4a21"
+    sha256 cellar: :any_skip_relocation, sonoma:        "edd877ddebf533a27a61a45d4ffe44b909678a3fac5d80883e6cd73c33a45284"
+    sha256 cellar: :any_skip_relocation, ventura:       "edd877ddebf533a27a61a45d4ffe44b909678a3fac5d80883e6cd73c33a45284"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a41769e9089f0fb440df59c22add6b1578b1b445c7cdae53496126d7283caa74"
+  end
+
   depends_on "go" => :build
 
   def install
