a894e04e58bd87b25ad37e746b8b1f4d924e0609
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-01 20:51:18 +0000

    renovate: update 39.230.0 bottle.

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -17,13 +17,13 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6a8a051f71b520970adeb3e8a91d6bf3cc259e80993b72377dcf9c472f32720a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7f073bbaa92b5ad9c7e1fc1b0b44f9ba1f93a24e06ed4a79fcecddc4e87644b8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "536aa8f42bc725bc2059e7a00a92e9ca7502d84521a965ef92b12cc9ad69eb41"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3f5ae520d456fdf9522f10d3bba25268ccf2a4f74fc1ae9fc2c714260fe14b15"
-    sha256 cellar: :any_skip_relocation, ventura:       "d5bbcba308fde426527df9bca953f2c486d1cfe7c0deba21d4abfa4fa3e619c7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "75d118aadb691c2b25cafc27c9410c2f65fe17789eec3dfcdf688b843c5f656d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a8d0b17df89608720423615a3bbe81d10f86c6de7126649582e35128a7c404b1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0cbe4743756542c154daa7c67ebcbe511f77359877b2e027644890e0a7a73f3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a2a98cc15459cfdd5a23e1a7e7fddf63a90a0d9aa35e0226391887491b9ddc54"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c2cdf1f6bbb157b4d97decea734ef897ef0e0e59e75b7d776f6133bbe2df8f7e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dd9b4dcb476831e37871f90f60cb82ef947367dc167d46ab93a58de682cc5d92"
+    sha256 cellar: :any_skip_relocation, ventura:       "281df7c621f40f6e157b58293dac5c55a0747abf9136a2a8b218864b63ed96b2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c821a3c00b2ad2571e684668d5f4459b36aae937438e2931c227121ac578673d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3de74a7aca5547d02da94071283c519ef708f453225cfbc637dabe295878c53"
   end
 
   depends_on "node@22"
