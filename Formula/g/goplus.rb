91a420c7d31f5d9d314e50ae6b3b2848c2ad56ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-01 19:56:45 +0000

    goplus 1.3.8

--- Formula/g/goplus.rb
+++ Formula/g/goplus.rb
@@ -1,8 +1,8 @@
 class Goplus < Formula
   desc "Programming language for engineering, STEM education, and data science"
   homepage "https://goplus.org"
-  url "https://github.com/goplus/gop/archive/refs/tags/v1.3.7.tar.gz"
-  sha256 "427db0b9ce97c4d0b287b03eb1e5ba44dc88f5d146894636f1c61acea63d0260"
+  url "https://github.com/goplus/gop/archive/refs/tags/v1.3.8.tar.gz"
+  sha256 "f81dea9c29f4781b6165e0b4820b15ebbb7e39939f9aa6c49cddac30dca4b2c8"
   license "Apache-2.0"
   head "https://github.com/goplus/gop.git", branch: "main"
 
