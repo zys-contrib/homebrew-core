505427473eb69d03509b0dc902132a199eb0a832
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-02 16:56:50 +0000

    cloudflared 2025.4.0

--- Formula/c/cloudflared.rb
+++ Formula/c/cloudflared.rb
@@ -1,8 +1,8 @@
 class Cloudflared < Formula
   desc "Cloudflare Tunnel client (formerly Argo Tunnel)"
   homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
-  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2025.2.1.tar.gz"
-  sha256 "c82ac315fa081cc18c98c9f63657375151a8c6141e7597d794232a1d35f93a4c"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2025.4.0.tar.gz"
+  sha256 "731694e178c7671ee9210cc7aca87aa35a5f0114e834c4e83f49dbb97b2b2b0f"
   license "Apache-2.0"
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
