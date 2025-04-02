0ef02596f60949d4ab8d7384e39c1bba266ce28b
Rui Chen <rui@chenrui.dev>
2025-04-02 02:41:28 +0000

    otf2 3.1.1
    otf2: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/otf2.rb
+++ Formula/o/otf2.rb
@@ -1,9 +1,8 @@
 class Otf2 < Formula
   desc "Open Trace Format 2 file handling library"
   homepage "https://www.vi-hps.org/projects/score-p/"
-  # TODO: check if we can remove `autoconf` + `automake` at version bump.
-  url "https://perftools.pages.jsc.fz-juelich.de/cicd/otf2/tags/otf2-3.0.3/otf2-3.0.3.tar.gz"
-  sha256 "18a3905f7917340387e3edc8e5766f31ab1af41f4ecc5665da6c769ca21c4ee8"
+  url "https://perftools.pages.jsc.fz-juelich.de/cicd/otf2/tags/otf2-3.1.1/otf2-3.1.1.tar.gz"
+  sha256 "5a4e013a51ac4ed794fe35c55b700cd720346fda7f33ec84c76b86a5fb880a6e"
   license "BSD-3-Clause"
 
   livecheck do
@@ -22,8 +21,6 @@ class Otf2 < Formula
     sha256 x86_64_linux:  "ea14ea82474ebc29426a280e43409ebce2a688ba2e4de3ef11aa82564841327a"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "python-setuptools" => :build
   depends_on "sphinx-doc" => :build
   depends_on "gcc" # for gfortran
@@ -31,8 +28,8 @@ class Otf2 < Formula
   depends_on "python@3.13"
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
-    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+    url "https://files.pythonhosted.org/packages/94/e7/b2c673351809dca68a0e064b6af791aa332cf192da575fd474ed7d6f16a2/six-1.17.0.tar.gz"
+    sha256 "ff70335d468e7eb6ec65b95b99d3a2836546063f63acc5171de367e834932a81"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
@@ -60,9 +57,6 @@ class Otf2 < Formula
     ENV["PYTHON"] = which(python3)
     ENV["SPHINX"] = Formula["sphinx-doc"].opt_bin/"sphinx-build"
 
-    # Bundled `build-config/py-compile` isn't compatible with python 3.12 due to `imp` usage
-    # TODO: check if we can remove this and `autoconf` + `automake` deps
-    system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", "--disable-silent-rules", *std_configure_args
     system "make"
     system "make", "install"
