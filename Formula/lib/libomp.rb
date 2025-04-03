9b24ed48beeb26196094a2a723d1ddd471cfa382
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-03 10:42:09 +0000

    libomp: update 20.1.2 bottle.

--- Formula/lib/libomp.rb
+++ Formula/lib/libomp.rb
@@ -11,13 +11,13 @@ class Libomp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "8168a1da1ba2fdfca159fc501e40bdccf2dd6645a491fe7f6094d93308720b51"
-    sha256 cellar: :any,                 arm64_sonoma:  "3951019b9f485c749e47b3aa428739a6108f8de336b6ce83ccdd28d3d0bf38e3"
-    sha256 cellar: :any,                 arm64_ventura: "873bdae8c0c7b52562614a936a7e92eec5d1ff211140e6ef4adf9a84e544cc4d"
-    sha256 cellar: :any,                 sonoma:        "edaa8b5568131f6f78c4994ce1d7fffdbf8b03f9f24eb83a7f3bbf7e79afefcd"
-    sha256 cellar: :any,                 ventura:       "62a0297459dfc6d2afed5c96262dda587aee4f5ac01732e7d4b2d640c3f94505"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e165d1c341672f66474f7561162210afea7f27902b0c3c2be53ce604737e5254"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "713a0c09054a13c1beb37e2efc84dc0dac048f01a5ffbab805b32a545f9b59c0"
+    sha256 cellar: :any,                 arm64_sequoia: "7f13318c3701440f35002eb43cfd59b61e74c22bad860d87499bf6ebb0904070"
+    sha256 cellar: :any,                 arm64_sonoma:  "033d1a3e05c9b78d84a620e5d706832b60ffd6084786772eb840214c57f17bf6"
+    sha256 cellar: :any,                 arm64_ventura: "e9582eb859ffeeb8fd94d52a61e746c182e991a877dc5c6dcd5d960340f46929"
+    sha256 cellar: :any,                 sonoma:        "e5f14bf360ceb2414d1e8bd8a7c2a08a3de2ea9f1e57bd5091a1acb975802892"
+    sha256 cellar: :any,                 ventura:       "aeb5198d0a539ca48dc4a83d86e9f5981bf9202522021fbdf5a5ed659eac82c4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4efcba5f465e43857fc1a94ff75fadc0aa10aaca831da360de3095399f5dfad6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a5f7b8de2024b0b9dedfc544a052fc282cac10679c3799f4d0cb0023e54fb55"
   end
 
   # Ref: https://github.com/Homebrew/homebrew-core/issues/112107
