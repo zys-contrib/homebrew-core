c5230a8c69c302882f16d07a3fefcd32f99e1a09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-03 07:48:38 +0000

    tgpt: update 2.9.3 bottle.

--- Formula/t/tgpt.rb
+++ Formula/t/tgpt.rb
@@ -7,12 +7,12 @@ class Tgpt < Formula
   head "https://github.com/aandrew-me/tgpt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f5a563fe2d9ba268541a0cef460f7cf2078e244dfd95f4604463f69369eda541"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f5a563fe2d9ba268541a0cef460f7cf2078e244dfd95f4604463f69369eda541"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f5a563fe2d9ba268541a0cef460f7cf2078e244dfd95f4604463f69369eda541"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b93a4502723ce9a4cc043e571af57789a787267fec4c4eacc02b3d350e6619a2"
-    sha256 cellar: :any_skip_relocation, ventura:       "b93a4502723ce9a4cc043e571af57789a787267fec4c4eacc02b3d350e6619a2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a16a73e2753d143f50145323db94d00b38fe05bc468c6f0d991c768567f5d9f6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9538fb2c28578522a79fb5a172207ba7a67e25635f44d7e6841f48f33179255"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9538fb2c28578522a79fb5a172207ba7a67e25635f44d7e6841f48f33179255"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d9538fb2c28578522a79fb5a172207ba7a67e25635f44d7e6841f48f33179255"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fa74d5e0e846b2fd1be359613cc38a87238fc7ba3fe430582ba3d7463c94290f"
+    sha256 cellar: :any_skip_relocation, ventura:       "fa74d5e0e846b2fd1be359613cc38a87238fc7ba3fe430582ba3d7463c94290f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "60facc507e4b868b067f56d7755182f412253f3579a263d883a34cdbf72c5ca3"
   end
 
   depends_on "go" => :build
