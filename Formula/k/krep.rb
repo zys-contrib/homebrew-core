df36c8171c9c2df66ea913f4e26d60452656f68a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-03 08:57:45 +0000

    krep: update 0.3.4 bottle.

--- Formula/k/krep.rb
+++ Formula/k/krep.rb
@@ -6,13 +6,13 @@ class Krep < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d24d956157b3980d291edd5e071626edd7bfa74876c77d7a001dc7e8f94d9731"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "17417a2b087e8ba6371535c958b5087c56b33c49a05212be3458623670129f83"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "33ce4160bbf2464f48993eaafbbecb11f994209a66fd31de32a16e8987695e33"
-    sha256 cellar: :any_skip_relocation, sonoma:        "db8dfa09c15cc0e9f7128452bd16f516a22eef5680927ba46efe1e6c1c38275b"
-    sha256 cellar: :any_skip_relocation, ventura:       "ee641f2271a4377d73ca5682c89726634b49a050381eb96c1a39ff1f56bdf9ba"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ba4644a0417b35d156bc03d8c0bdb4b2267fbc7b279afee968e7132ff142881"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a45ea62533ddd00653d0303178f5bebe7feb4f43bc85682723ca43512bf6beab"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "df92571a01b0cf4b817717b2db8cdbf1f61abcd753f7ba306032e2d60067ac94"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a69ae299744fdffda2d2ac212907988bb1ea7a1b9b08ab807bcca6e2976a0b9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2145a32c4d495f3a8b3e8437efaccf2bdb10281b25cd23774cf32cba030b6f1c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f9350d75e47c1b857c3d952c6981c09dc50c015374b80ad4944e000de295be9f"
+    sha256 cellar: :any_skip_relocation, ventura:       "791a0d318992a784ced2ec72a9fb0269707b572b5621a4e6977df4161bff40d8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cccb912f0012d0c2826558f519c6c3239c4f50dd99aee26b1bd26379dd0208d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf53dd4bde0597bda16147e54e8bfba51ea0e8c5f203b3161eac5f50037c0610"
   end
 
   def install
