85d3603c069fa73ed05ebb53d02c66c2bedaaf41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-03 23:09:09 +0000

    s2n 1.5.16

--- Formula/s/s2n.rb
+++ Formula/s/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/refs/tags/v1.5.15.tar.gz"
-  sha256 "103f9361c736fea7278038891b0566ff975c40ac59cef5ac5b9225a476c8abc6"
+  url "https://github.com/aws/s2n-tls/archive/refs/tags/v1.5.16.tar.gz"
+  sha256 "ab3e19f899e9b76cf7141f3b287e865db628f9e024360d7086c7f9c901909214"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
