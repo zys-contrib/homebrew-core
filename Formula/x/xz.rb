1bc804d7ed14a713b5bdf4c848f5709d9acbd01a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-03 17:20:16 +0000

    xz 5.8.1

--- Formula/x/xz.rb
+++ Formula/x/xz.rb
@@ -1,10 +1,10 @@
 class Xz < Formula
   desc "General-purpose data compression with high compression ratio"
   homepage "https://tukaani.org/xz/"
-  url "https://github.com/tukaani-project/xz/releases/download/v5.8.0/xz-5.8.0.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/lzmautils/xz-5.8.0.tar.gz"
-  mirror "http://downloads.sourceforge.net/project/lzmautils/xz-5.8.0.tar.gz"
-  sha256 "b523c5e47d1490338c5121bdf2a6ecca2bcf0dce05a83ad40a830029cbe6679b"
+  url "https://github.com/tukaani-project/xz/releases/download/v5.8.1/xz-5.8.1.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/lzmautils/xz-5.8.1.tar.gz"
+  mirror "http://downloads.sourceforge.net/project/lzmautils/xz-5.8.1.tar.gz"
+  sha256 "507825b599356c10dca1cd720c9d0d0c9d5400b9de300af00e4d1ea150795543"
   license all_of: [
     "0BSD",
     "GPL-2.0-or-later",
