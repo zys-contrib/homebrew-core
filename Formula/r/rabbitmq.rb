acfb5016b74bed39e777e797021429e9159b9113
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-03 08:50:51 +0000

    rabbitmq: update 4.0.8 bottle.

--- Formula/r/rabbitmq.rb
+++ Formula/r/rabbitmq.rb
@@ -11,7 +11,7 @@ class Rabbitmq < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "370d05d2d8688875c228774ad166508133cd31526285f65c90d6ae309fb6402f"
+    sha256 cellar: :any_skip_relocation, all: "b086d802bb907e813c0f2eaf933611c2e78efb33231f23e8d3a90f076c4a3553"
   end
 
   depends_on "erlang"
