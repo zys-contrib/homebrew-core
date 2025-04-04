1ae5b60b473e7d8ed7282c3fe9d31fd011413a91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-04 13:28:25 +0000

    testkube: update 2.1.135 bottle.

--- Formula/t/testkube.rb
+++ Formula/t/testkube.rb
@@ -12,12 +12,12 @@ class Testkube < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "344c3545bf301223047bac3abccedd5d1619c9e8aca4fe823dec771cf152c065"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "344c3545bf301223047bac3abccedd5d1619c9e8aca4fe823dec771cf152c065"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "344c3545bf301223047bac3abccedd5d1619c9e8aca4fe823dec771cf152c065"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3f8890ad42d3cade57e17597a0bf66ad571a784901e3def66cb9b5f46873e112"
-    sha256 cellar: :any_skip_relocation, ventura:       "3f8890ad42d3cade57e17597a0bf66ad571a784901e3def66cb9b5f46873e112"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2611ead21595551f124fc4cd2632618642eae26e337fcb61db0c70713d26e10c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "03e245cdaa799fe48ed6969f145b9f0d6f9a299e41bdc35d9cbfecd95321d844"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "03e245cdaa799fe48ed6969f145b9f0d6f9a299e41bdc35d9cbfecd95321d844"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "03e245cdaa799fe48ed6969f145b9f0d6f9a299e41bdc35d9cbfecd95321d844"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d744af6fa9aa0716d5fb961cb8257b65453fc036282b09b2a62ef52f08393dca"
+    sha256 cellar: :any_skip_relocation, ventura:       "d744af6fa9aa0716d5fb961cb8257b65453fc036282b09b2a62ef52f08393dca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c282b0b0fc372dfc0bc4b12db58c41b94c4e2c4696d20f3a52ad339719a21ef"
   end
 
   depends_on "go" => :build
