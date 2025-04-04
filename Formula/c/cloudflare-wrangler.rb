916467cd952b367c95ec2183f487a9912ba0a2ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-04 08:43:22 +0000

    cloudflare-wrangler: update 4.7.1 bottle.

--- Formula/c/cloudflare-wrangler.rb
+++ Formula/c/cloudflare-wrangler.rb
@@ -6,13 +6,13 @@ class CloudflareWrangler < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "a1ed0cdbfb64e9562cf13cbe1fa3e8001a3d77cb7e4bd82a96c6c40e79e71255"
-    sha256 cellar: :any,                 arm64_sonoma:  "a1ed0cdbfb64e9562cf13cbe1fa3e8001a3d77cb7e4bd82a96c6c40e79e71255"
-    sha256 cellar: :any,                 arm64_ventura: "a1ed0cdbfb64e9562cf13cbe1fa3e8001a3d77cb7e4bd82a96c6c40e79e71255"
-    sha256                               sonoma:        "33597ff6b916c92d78f6688b3bbc8d7d21562ec017c39bb21b532fbfd062345f"
-    sha256                               ventura:       "33597ff6b916c92d78f6688b3bbc8d7d21562ec017c39bb21b532fbfd062345f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3e8c5629cd3fe3ab91c90c0db8d614aee53b1277136f376bbffc1fd745657012"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9fe89fb8166f821a59d47a473c5b9b01f75f3a9a11a8d3dff1691db2236d5e0"
+    sha256 cellar: :any,                 arm64_sequoia: "c7d09e97febbf964c9d1f7bbeacba6ef2f003d616cceaa692ac7b4d344f54c47"
+    sha256 cellar: :any,                 arm64_sonoma:  "c7d09e97febbf964c9d1f7bbeacba6ef2f003d616cceaa692ac7b4d344f54c47"
+    sha256 cellar: :any,                 arm64_ventura: "c7d09e97febbf964c9d1f7bbeacba6ef2f003d616cceaa692ac7b4d344f54c47"
+    sha256                               sonoma:        "f242e1e6146ef7f1a6348dfd9c3f41004425d5e38f984f535e941deda69d827d"
+    sha256                               ventura:       "f242e1e6146ef7f1a6348dfd9c3f41004425d5e38f984f535e941deda69d827d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f80422b9af89ddca5dd5baa62d4e1f58d3b6736c11eeeef7d99a3af70556135c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "447299406bfb6d15f33e46b06b2444017796421a23e78a6e0007a8d2ba09dd11"
   end
 
   depends_on "node"
