3684c9729b704779f351fc08a31973143d88ee03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-04 08:14:06 +0000

    osc 1.15.0

--- Formula/o/osc.rb
+++ Formula/o/osc.rb
@@ -3,8 +3,8 @@ class Osc < Formula
 
   desc "Command-line interface to work with an Open Build Service"
   homepage "https://openbuildservice.org"
-  url "https://github.com/openSUSE/osc/archive/refs/tags/1.14.0.tar.gz"
-  sha256 "0f2e7dad5d4377530cd5839a6563889302048316ae403acb1a1e05c75aaa1486"
+  url "https://github.com/openSUSE/osc/archive/refs/tags/1.15.0.tar.gz"
+  sha256 "02768478c0d22222490875440e6a7badf3881909c5e8e2ae02bf2ac8873d3e90"
   license "GPL-2.0-or-later"
   head "https://github.com/openSUSE/osc.git", branch: "master"
 
