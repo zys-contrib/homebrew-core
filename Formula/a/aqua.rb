22019219e33c1366a177a8cc8efd3f67c6fc6acf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-04 10:49:59 +0000

    aqua 2.47.2

--- Formula/a/aqua.rb
+++ Formula/a/aqua.rb
@@ -1,8 +1,8 @@
 class Aqua < Formula
   desc "Declarative CLI Version manager"
   homepage "https://aquaproj.github.io/"
-  url "https://github.com/aquaproj/aqua/archive/refs/tags/v2.47.1.tar.gz"
-  sha256 "c2ace5300071b18ba78c99124d16485a4f3ad1b96a6b94c9a5a42aa9b9882971"
+  url "https://github.com/aquaproj/aqua/archive/refs/tags/v2.47.2.tar.gz"
+  sha256 "f3b84be00d65473973bac19d83e5f47c9d7fad0c57c46d226b9284f1ab4efb75"
   license "MIT"
   head "https://github.com/aquaproj/aqua.git", branch: "main"
 
