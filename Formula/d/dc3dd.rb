adef330262d0c5fc65933080fff87ddce27d6a8f
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-04-05 21:28:28 +0000

    dc3dd: update 7.3.1 bottle.

--- Formula/d/dc3dd.rb
+++ Formula/d/dc3dd.rb
@@ -15,6 +15,7 @@ class Dc3dd < Formula
     sha256 ventura:        "f65cccfafa99f62c687eb87c64cfdb3492f8050e86a268045d73f53f5c5dfe97"
     sha256 monterey:       "b8270f518be57090a150e2d78048b8f2e6a81e16092007e48bef6e5e567a4cf3"
     sha256 big_sur:        "f61a5f9196c0c30c9087d92b22522db1c344137406a3272381add30d49c9621f"
+    sha256 arm64_linux:    "f56498b96fad1eb7f15ad686bf19360f92844322299627e0e20c57c3a5d5c219"
     sha256 x86_64_linux:   "bdaae1ff1efcea3319dbf1e84f68c1d51a1b12e8a8eae4c13f2e2069084051c7"
   end
 
