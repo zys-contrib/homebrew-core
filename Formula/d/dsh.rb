d8010ba5aab23e355079b16648e1f3783a25a1e8
Michael Cho <michael@michaelcho.dev>
2025-04-05 16:07:59 -0400

    dsh: fix build on arm64 linux

--- Formula/d/dsh.rb
+++ Formula/d/dsh.rb
@@ -40,8 +40,11 @@ class Dsh < Formula
     # Reported to the upstream developer by email as a bug tracker does not exist.
     ENV.prepend_path "PATH", Formula["gnu-sed"].libexec/"gnubin" if OS.mac?
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    args = []
+    # Help old config scripts identify arm64 linux
+    args << "--build=aarch64-unknown-linux-gnu" if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+
+    system "./configure", *args, *std_configure_args
     system "make", "install"
   end
 end
