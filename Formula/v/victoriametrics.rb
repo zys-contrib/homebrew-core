8aa5c58c9ed7e1f7b1911059f0520146c28ece4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-07 11:21:38 +0000

    victoriametrics 1.115.0

--- Formula/v/victoriametrics.rb
+++ Formula/v/victoriametrics.rb
@@ -1,8 +1,8 @@
 class Victoriametrics < Formula
   desc "Cost-effective and scalable monitoring solution and time series database"
   homepage "https://victoriametrics.com/"
-  url "https://github.com/VictoriaMetrics/VictoriaMetrics/archive/refs/tags/v1.114.0.tar.gz"
-  sha256 "e4b1ec261e4059b310248033ff2c682ac5eb09572ae650f65a5dcf3f6492fdd0"
+  url "https://github.com/VictoriaMetrics/VictoriaMetrics/archive/refs/tags/v1.115.0.tar.gz"
+  sha256 "b7efc59b656bcfd09905aa220515adaad99bd3627e56d39bf20a849314f0c7ab"
   license "Apache-2.0"
 
   # There are tags like `pmm-6401-v1.89.1` in the upstream repo. They don't
