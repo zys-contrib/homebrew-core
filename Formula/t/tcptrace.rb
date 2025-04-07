11938527c959c0174b7c34ea9442443fa8e6bb5a
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-04-07 03:39:33 +0000

    tcptrace: update 6.6.7 bottle.

--- Formula/t/tcptrace.rb
+++ Formula/t/tcptrace.rb
@@ -23,6 +23,7 @@ class Tcptrace < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "39916506fcd6385aee6375813128a126a84f947623594011f6c2c9df1b6dc8b2"
     sha256 cellar: :any_skip_relocation, sierra:         "7ccc5e6859be970a5a8a064630704111d37b03a7e3cf3a9874e16a60e4abe02b"
     sha256 cellar: :any_skip_relocation, el_capitan:     "e46775d7cc808b5b52a0a36a33142b824a9b2d8bce5b0557bc1041c2e55c5ffb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:    "048969e3fdf2e0c22e2e65b3a6548a0e5da8dd032ee770b32f8444e5ce007835"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "7516135879ddee3a7a4271980b8485ac12c3b1826cb99ba23e9f6f849fda4ceb"
   end
 
