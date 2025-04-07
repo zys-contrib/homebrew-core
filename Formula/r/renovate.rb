26081dd842ff845d379db443fc209d5cb43fff1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-07 14:09:24 +0000

    renovate 39.235.0

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -1,8 +1,8 @@
 class Renovate < Formula
   desc "Automated dependency updates. Flexible so you don't need to be"
   homepage "https://github.com/renovatebot/renovate"
-  url "https://registry.npmjs.org/renovate/-/renovate-39.234.0.tgz"
-  sha256 "7af7504eed1e54f085ed5005c4714cd1bde49cca64f36d4913c117413413b786"
+  url "https://registry.npmjs.org/renovate/-/renovate-39.235.0.tgz"
+  sha256 "17249cb15b4883c3c3c9b6fbb82dd55a1d2409944588198b802eaf3b01c100e2"
   license "AGPL-3.0-only"
 
   # There are thousands of renovate releases on npm and the page the `Npm`
