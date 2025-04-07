53561d4499fbbf8670932b8b11eadb536d207d48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-07 14:17:41 +0000

    ftxui: add 6.0.2 bottle.

--- Formula/f/ftxui.rb
+++ Formula/f/ftxui.rb
@@ -6,6 +6,15 @@ class Ftxui < Formula
   license "MIT"
   head "https://github.com/ArthurSonzogni/FTXUI.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "af05df19efa91d46d6a93c906de707aecfa749634e786937c262ad3a77a39f68"
+    sha256 cellar: :any,                 arm64_sonoma:  "531be08751484686f71f359cbc621d3ecbeea8b137e2dfa8a53a7d7512f0aaae"
+    sha256 cellar: :any,                 arm64_ventura: "8dc0bb17ff94cad0c42c10b46512207303359149f8d7362edca99dad63a08499"
+    sha256 cellar: :any,                 sonoma:        "4085804f59cf1c50741e8f3a2747ab035553f5e207f9277d0db01116e0dd8bee"
+    sha256 cellar: :any,                 ventura:       "c869aaf1aa5a002275acfd70095f2e6afa4021adbbaa8668466f8c679d9c6866"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "31c5f0e57e1d18e2e1aede1f9708242b3542bde0035d918f73bfee714a45002f"
+  end
+
   depends_on "cmake" => :build
 
   def install
