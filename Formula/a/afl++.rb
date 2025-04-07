a7e0d4bdd2cd41f07a4a240bd74cd075b811758a
Michael Cho <michael@michaelcho.dev>
2025-04-07 16:02:18 -0400

    afl++ 4.31c

--- Formula/a/afl++.rb
+++ Formula/a/afl++.rb
@@ -2,13 +2,13 @@ class Aflxx < Formula
   desc "American Fuzzy Lop++"
   homepage "https://aflplus.plus/"
   url "https://github.com/AFLplusplus/AFLplusplus/archive/refs/tags/v4.31c.tar.gz"
+  version "4.31c"
   sha256 "8c6e9bef19b3d43020972701553734d1cb435c39a28b253f0dd6668e6ecb86bb"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
-    regex(/^v?(\d+(?:\.\d+)+)c$/i)
+    regex(/^v?(\d+(?:\.\d+)+c)$/i)
   end
 
   bottle do
