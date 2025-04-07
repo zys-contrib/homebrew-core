20a572923c051fb3b9441462bbe644b0e065512c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-07 08:00:57 +0000

    jackett 0.22.1740

--- Formula/j/jackett.rb
+++ Formula/j/jackett.rb
@@ -1,8 +1,8 @@
 class Jackett < Formula
   desc "API Support for your favorite torrent trackers"
   homepage "https://github.com/Jackett/Jackett"
-  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.22.1733.tar.gz"
-  sha256 "49ae4461436eaf50540221b76f7ef5f295516bb15388510e6f98b8050e78645d"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.22.1740.tar.gz"
+  sha256 "cdb65935ca84f3eed457d3f8769cfcd36d521e994bedf2db59e4701916bd8c2b"
   license "GPL-2.0-only"
   head "https://github.com/Jackett/Jackett.git", branch: "master"
 
