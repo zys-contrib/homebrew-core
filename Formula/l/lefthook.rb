0bb23a356915b96263f43bb1cadc710d031f58bf
Valentin Kiselev <mrexox@users.noreply.github.com>
2025-04-07 07:54:34 +0000

    lefthook 1.11.7

--- Formula/l/lefthook.rb
+++ Formula/l/lefthook.rb
@@ -1,8 +1,8 @@
 class Lefthook < Formula
   desc "Fast and powerful Git hooks manager for any type of projects"
   homepage "https://github.com/evilmartians/lefthook"
-  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.11.6.tar.gz"
-  sha256 "f573e9b8f08e8b2e26a820dc54f245d3ff9464d3edf3dd9e9b1276401be680aa"
+  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.11.7.tar.gz"
+  sha256 "2cca5df538ec285954cbbc518108eec4c13eae10b56540c5ab4d5c469459a0a0"
   license "MIT"
   head "https://github.com/evilmartians/lefthook.git", branch: "master"
 
