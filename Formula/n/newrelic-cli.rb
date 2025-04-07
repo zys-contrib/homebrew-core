2912c25ed86fa9b6f0ed15399b22f707cdcf279e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-07 11:09:27 +0000

    newrelic-cli 0.97.11

--- Formula/n/newrelic-cli.rb
+++ Formula/n/newrelic-cli.rb
@@ -1,8 +1,8 @@
 class NewrelicCli < Formula
   desc "Command-line interface for New Relic"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/refs/tags/v0.97.10.tar.gz"
-  sha256 "4a4b0986ff5eb9a21287e3535ebac69ac17866403bd46d4c22ea292f2c1b886f"
+  url "https://github.com/newrelic/newrelic-cli/archive/refs/tags/v0.97.11.tar.gz"
+  sha256 "9a1afdd6059e9e3fb4de78fb41f96b0bedfea702d43c5746bc9d9b3c8e7d205e"
   license "Apache-2.0"
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
