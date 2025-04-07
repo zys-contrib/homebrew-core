26bb8249ffb31b26411e18ff77975b571ecc3133
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-07 02:54:11 +0000

    marmite: add 0.2.5 bottle.

--- Formula/m/marmite.rb
+++ Formula/m/marmite.rb
@@ -6,6 +6,15 @@ class Marmite < Formula
   license "AGPL-3.0-or-later"
   head "https://github.com/rochacbruno/marmite.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9044fed2abbe05930f74bee4f4f1de20700f8d6f32a1e0d03fe6141f1036c8e8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f7636b8ca3ec1608843d67c90590ea95c71b4aefbdeba4d163fb40df66d8bc5b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "88830317a2357d363919fe06b1b4c0804860f257f0a859aa39f1ef622e0840de"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bff5c7b07e1e059b78fc491dc3898d8e2c1d2c3d6d76d27e9845c674edab3d15"
+    sha256 cellar: :any_skip_relocation, ventura:       "cc0448b1262edc1fe173095d3317c8945dd444d6627b07c81374cd901d279243"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "591b5b32a8a0e8ccf269226dfe0ece3947db93e80f58138a1db37a1836575e88"
+  end
+
   depends_on "rust" => :build
 
   def install
