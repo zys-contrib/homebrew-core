d0ef69287346b266190a572bc1ecab9ff6cb26b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-08 02:28:56 +0000

    goimports 0.32.0

--- Formula/g/goimports.rb
+++ Formula/g/goimports.rb
@@ -1,8 +1,8 @@
 class Goimports < Formula
   desc "Go formatter that additionally inserts import statements"
   homepage "https://pkg.go.dev/golang.org/x/tools/cmd/goimports"
-  url "https://github.com/golang/tools/archive/refs/tags/v0.31.0.tar.gz"
-  sha256 "e5d74f1e63a1ee669e75e76668cea1b110e2b9d19c67710f60939ee38070a5a7"
+  url "https://github.com/golang/tools/archive/refs/tags/v0.32.0.tar.gz"
+  sha256 "f2b22107aaea83bbcc4a34c86e4331dda035f71e72bdbc057c32ce7072a17c5b"
   license "BSD-3-Clause"
   head "https://github.com/golang/tools.git", branch: "master"
 
