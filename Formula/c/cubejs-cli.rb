82e3a0b7b726ab58e68a60fcf1018460b4d47fd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-08 19:05:02 +0000

    cubejs-cli: update 1.2.31 bottle.

--- Formula/c/cubejs-cli.rb
+++ Formula/c/cubejs-cli.rb
@@ -6,13 +6,13 @@ class CubejsCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "86724ab8c64f93cfeccc0ce671a2f0f083d493008e8105ac7ed0f6cb5348fed0"
-    sha256 cellar: :any,                 arm64_sonoma:  "86724ab8c64f93cfeccc0ce671a2f0f083d493008e8105ac7ed0f6cb5348fed0"
-    sha256 cellar: :any,                 arm64_ventura: "86724ab8c64f93cfeccc0ce671a2f0f083d493008e8105ac7ed0f6cb5348fed0"
-    sha256 cellar: :any,                 sonoma:        "aea83c3900d2bd39a0f9ec5d47caf688fc02f4e132dbba33e5299c2b85e33d10"
-    sha256 cellar: :any,                 ventura:       "aea83c3900d2bd39a0f9ec5d47caf688fc02f4e132dbba33e5299c2b85e33d10"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "076dcfac4a1b5dd1edb6de95cc593b92c6bd85acdb91cd5cee4f8b45133f6f6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a16dadcf58c6b2b1087bb969a86b100b506550567f0a8301c0e770cb564ac028"
+    sha256 cellar: :any,                 arm64_sequoia: "c7febef6b105f2da30eb5c40b38e3f31462253f8a1a20060095da9eaa72af317"
+    sha256 cellar: :any,                 arm64_sonoma:  "c7febef6b105f2da30eb5c40b38e3f31462253f8a1a20060095da9eaa72af317"
+    sha256 cellar: :any,                 arm64_ventura: "c7febef6b105f2da30eb5c40b38e3f31462253f8a1a20060095da9eaa72af317"
+    sha256 cellar: :any,                 sonoma:        "3af47a2f5390ea8bf845d131f47acee79f9a5e80854fa1b21da5c3f3d70edd78"
+    sha256 cellar: :any,                 ventura:       "3af47a2f5390ea8bf845d131f47acee79f9a5e80854fa1b21da5c3f3d70edd78"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8cdeb1ad6998b59b2344b6abf91ca6fa62c0f45259f7b820537c07cc0ac6e8e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eba2ae986dbc668003b82cbbdceb5d03511400949a78d53aed1da391f962c3b0"
   end
 
   depends_on "node"
