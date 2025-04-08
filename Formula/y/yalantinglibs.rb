b46851bddcde86102c6ca58e5e63ceac95fd4ccf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-08 10:00:47 +0000

    yalantinglibs: update 1.1.1 bottle.

--- Formula/y/yalantinglibs.rb
+++ Formula/y/yalantinglibs.rb
@@ -7,7 +7,7 @@ class Yalantinglibs < Formula
   head "https://github.com/alibaba/yalantinglibs.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e6e98c999fe9853935b3fc19703d5a5a16e335e5456deac04ae734828a09abd4"
+    sha256 cellar: :any_skip_relocation, all: "6be6e3d108faa90a6b5812b445fd0488c63d3d3f2ab83e18e258d7cfb3f9382b"
   end
 
   depends_on "cmake" => :build
