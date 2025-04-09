549aa65c6f7717eec3e743ab776014f251ea422b
bevanjkay <email@bevankay.me>
2025-04-09 23:51:06 +1000

    fizz 2025.04.07.00

--- Formula/f/fizz.rb
+++ Formula/f/fizz.rb
@@ -1,8 +1,8 @@
 class Fizz < Formula
   desc "C++14 implementation of the TLS-1.3 standard"
   homepage "https://github.com/facebookincubator/fizz"
-  url "https://github.com/facebookincubator/fizz/releases/download/v2025.03.10.00/fizz-v2025.03.10.00.tar.gz"
-  sha256 "f4ae5fcd9f35a44191c9d963ec2d51352a7cfbb5e8d5f59f1d7bbbae3a12fe5f"
+  url "https://github.com/facebookincubator/fizz/releases/download/v2025.04.07.00/fizz-v2025.04.07.00.tar.gz"
+  sha256 "119efbacce276bab0cb9f32cf873154e87bd040b03d70cb806e02e0c1789b6f2"
   license "BSD-3-Clause"
   head "https://github.com/facebookincubator/fizz.git", branch: "main"
 
@@ -66,7 +66,7 @@ class Fizz < Formula
     CPP
 
     (testpath/"CMakeLists.txt").write <<~CMAKE
-      cmake_minimum_required(VERSION 3.5)
+      cmake_minimum_required(VERSION 3.10)
       project(test LANGUAGES CXX)
       set(CMAKE_CXX_STANDARD 17)
 
@@ -80,7 +80,8 @@ class Fizz < Formula
 
     ENV.delete "CPATH"
 
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    args = OS.mac? ? [] : ["-DCMAKE_BUILD_RPATH=#{lib};#{HOMEBREW_PREFIX}/lib"]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     assert_match "TLS", shell_output("./build/test")
   end
