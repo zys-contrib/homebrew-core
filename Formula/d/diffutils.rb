aceb59dfafc1aa75e30c7990d812ba6f67e07625
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 04:56:04 +0000

    diffutils 3.12

--- Formula/d/diffutils.rb
+++ Formula/d/diffutils.rb
@@ -1,9 +1,9 @@
 class Diffutils < Formula
   desc "File comparison utilities"
   homepage "https://www.gnu.org/software/diffutils/"
-  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.11.tar.xz"
-  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.11.tar.xz"
-  sha256 "a73ef05fe37dd585f7d87068e4a0639760419f810138bd75c61ddaa1f9e2131e"
+  url "https://ftp.gnu.org/gnu/diffutils/diffutils-3.12.tar.xz"
+  mirror "https://ftpmirror.gnu.org/diffutils/diffutils-3.12.tar.xz"
+  sha256 "7c8b7f9fc8609141fdea9cece85249d308624391ff61dedaf528fcb337727dfd"
   license "GPL-3.0-or-later"
 
   bottle do
