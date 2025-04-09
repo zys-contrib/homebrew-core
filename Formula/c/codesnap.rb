ed0e34568f5c9feffef9ef548f5e326f895d29bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 09:28:32 +0000

    codesnap: update 0.10.7 bottle.

--- Formula/c/codesnap.rb
+++ Formula/c/codesnap.rb
@@ -7,13 +7,12 @@ class Codesnap < Formula
   head "https://github.com/codesnap-rs/codesnap.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9085159ad167f1a9373ce3ab3c0750fd4dec188a6415af70e68dc47cd3a1eed0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "849869809804e9e7b78b0bd08d0b57e3aa9d4f228deb402b84993e8d8d1e3a5e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "4573982698fd19774bef166fc7a2898ef3ca354e58c6aabc81821d187c3d06d2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5a7624a2c493e0011d72048cf5bebbe1204efa322171af57d45ac2f36e2ee9b8"
-    sha256 cellar: :any_skip_relocation, ventura:       "1cd2fe459aa0e56dc112b18f5c16e7c0978bc5b52a1711a63a5302ba8a59f48b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3092d52c4bf186ed27f637fc7c91119cc9a782863fa16ac59a21cac5a72a60f7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eb9fef5a58cd485a651539842623e408417908c32343237ee0e283b3c82fbb7c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "345398ce7f0f1671243f83c1996f69484f7cced7aae5c830bd2bcd067a6547c4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c15cf52b068e5881b799d6bc5c5584835b06f9b0841a837e22e896111a6fb455"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bf84042f95970a2698c636dc7ae9cabaf44d3ea048771048266878b96786043a"
+    sha256 cellar: :any_skip_relocation, ventura:       "38d6381731879ebb43109bbadaaa25e925a1f5b814e723df17f160b07d45e406"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "004539cf713f3c3c6a8f9b8f4b8f3347bf6abeabd169de5b7c72e37fab3126ba"
   end
 
   depends_on "rust" => :build
