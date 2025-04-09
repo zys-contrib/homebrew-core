2cec853ca5724b922573fb4f837a387328b1cca6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 20:04:12 +0000

    mockery 3.0.0

--- Formula/m/mockery.rb
+++ Formula/m/mockery.rb
@@ -1,10 +1,10 @@
 class Mockery < Formula
   desc "Mock code autogenerator for Golang"
   homepage "https://github.com/vektra/mockery"
-  url "https://github.com/vektra/mockery/archive/refs/tags/v2.53.3.tar.gz"
-  sha256 "c9495d19de1ca52a61a9ed299d9988a4f4a3cf1ad614954a15ca3920d8b852a2"
+  url "https://github.com/vektra/mockery/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "472380b8d1b0838274e3e3f8f11008504840aa08ffe61ce3184804d29f111b29"
   license "BSD-3-Clause"
-  head "https://github.com/vektra/mockery.git", branch: "master"
+  head "https://github.com/vektra/mockery.git", branch: "v3"
 
   # There can be a notable gap between when a version is tagged and a
   # corresponding release is created, so we check the "latest" release instead
@@ -26,17 +26,21 @@ class Mockery < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X github.com/vektra/mockery/v2/pkg/logging.SemVer=v#{version}"
+    ldflags = "-s -w -X github.com/vektra/mockery/v#{version.major}/internal/logging.SemVer=v#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
     generate_completions_from_executable(bin/"mockery", "completion")
   end
 
   test do
-    output = shell_output("#{bin}/mockery --keeptree 2>&1", 1)
-    assert_match "Starting mockery dry-run=false version=v#{version}", output
-
-    output = shell_output("#{bin}/mockery --all --dry-run 2>&1")
-    assert_match "INF Starting mockery dry-run=true version=v#{version}", output
+    (testpath/".mockery.yaml").write <<~YAML
+      packages:
+        github.com/vektra/mockery/v2/pkg:
+          interfaces:
+            TypesPackage:
+    YAML
+    output = shell_output("#{bin}/mockery 2>&1", 1)
+    assert_match "Starting mockery", output
+    assert_match "version=v#{version}", output
   end
 end
