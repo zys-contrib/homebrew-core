06ca61228b98f226e5d76810e99ff076fbd4cca4
Bevan Kay <email@bevankay.me>
2025-04-09 15:49:43 +1000

    dockerfmt 0.2.6 (new formula)

---
+++ Formula/d/dockerfmt.rb
@@ -0,0 +1,25 @@
+class Dockerfmt < Formula
+  desc "Dockerfile format and parser. a modern dockfmt"
+  homepage "https://github.com/reteps/dockerfmt"
+  url "https://github.com/reteps/dockerfmt/archive/refs/tags/0.2.6.tar.gz"
+  sha256 "6965c27d38203d56a7e68ef32213e8eefdaed1cf704196fcb09d9f2ea6db1675"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+    generate_completions_from_executable(bin/"dockerfmt", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dockerfmt version")
+
+    (testpath/"Dockerfile").write <<~DOCKERFILE
+      FROM alpine:latest
+    DOCKERFILE
+
+    output = shell_output("#{bin}/dockerfmt --check Dockerfile 2>&1", 1)
+    assert_match "File Dockerfile is not formatted", output
+  end
+end
