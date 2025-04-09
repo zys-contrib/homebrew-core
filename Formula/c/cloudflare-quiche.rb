bf071a8926459ddf569a4c04068d719aa4ce3954
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 10:55:05 +0000

    cloudflare-quiche 0.23.7

--- Formula/c/cloudflare-quiche.rb
+++ Formula/c/cloudflare-quiche.rb
@@ -2,8 +2,8 @@ class CloudflareQuiche < Formula
   desc "Savoury implementation of the QUIC transport protocol and HTTP/3"
   homepage "https://docs.quic.tech/quiche/"
   url "https://github.com/cloudflare/quiche.git",
-      tag:      "0.23.6",
-      revision: "42c8ea868b74df3cf4c6a533d15cb3bf3c3dbf79"
+      tag:      "0.23.7",
+      revision: "51cfd8a6f790064328273e9eaec83193c7b4171b"
   license "BSD-2-Clause"
   head "https://github.com/cloudflare/quiche.git", branch: "master"
 
