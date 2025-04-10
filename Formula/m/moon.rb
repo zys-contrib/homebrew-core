d073547545b2b02c083e2bc92bf26e9a58c343bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-10 23:06:58 +0000

    moon 1.34.3

--- Formula/m/moon.rb
+++ Formula/m/moon.rb
@@ -1,8 +1,8 @@
 class Moon < Formula
   desc "Task runner and repo management tool for the web ecosystem, written in Rust"
   homepage "https://moonrepo.dev/moon"
-  url "https://github.com/moonrepo/moon/archive/refs/tags/v1.34.2.tar.gz"
-  sha256 "2c94cb3a1a6ef8420ff54bf6a1ec9e24b581f2d8feaeffe19beb6e812e0c64b2"
+  url "https://github.com/moonrepo/moon/archive/refs/tags/v1.34.3.tar.gz"
+  sha256 "c2d53ec8f4874856f08d110f3567c18bd895090b8cd295f614432ef6d54e046f"
   license "MIT"
   head "https://github.com/moonrepo/moon.git", branch: "master"
 
