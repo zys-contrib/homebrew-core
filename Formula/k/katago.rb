014f3a7d739caf09706c6857f88a0e16afc71f79
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-04-10 14:16:18 +0000

    katago 1.16.0

--- Formula/k/katago.rb
+++ Formula/k/katago.rb
@@ -1,9 +1,14 @@
 class Katago < Formula
   desc "Neural Network Go engine with no human-provided knowledge"
   homepage "https://github.com/lightvector/KataGo"
-  url "https://github.com/lightvector/KataGo/archive/refs/tags/v1.15.3.tar.gz"
-  sha256 "96a1ef3d12eb30a950164f203b012b5f2257ef796b9bd15163d9ba3b79a32a1b"
-  license "MIT"
+  # Occasionally check upstream docs in case recommended model/network is changed.
+  # Ref: https://github.com/lightvector/KataGo?tab=readme-ov-file#other-questions
+  url "https://github.com/lightvector/KataGo/archive/refs/tags/v1.16.0.tar.gz"
+  sha256 "1786772c8490fb513522319554dfb41d93ecae4fb35e1b70249f3fe3c75c6cc1"
+  license all_of: [
+    "MIT",
+    "CC0-1.0", # g170 resources
+  ]
 
   livecheck do
     url :stable
@@ -29,32 +34,49 @@ class Katago < Formula
 
   uses_from_macos "zlib"
 
+  on_macos do
+    depends_on "ninja" => :build
+  end
+
   on_linux do
     depends_on "eigen" => :build
   end
 
+  # Using most recent b18c384nbt rather than strongest as it is easier to track
+  resource "b18c384nbt" do
+    url "https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b18c384nbt-s9996604416-d4316597426.bin.gz", using: :nounzip
+    version "s9996604416-d4316597426"
+    sha256 "9d7a6afed8ff5b74894727e156f04f0cd36060a24824892008fbb6e0cba51f1d"
+
+    livecheck do
+      url "https://katagotraining.org/networks/"
+      regex(/href=.*?kata1[._-]b18c384nbt[._-](s\d+[._-]d\d+)\.bin\.gz/i)
+    end
+  end
+
+  # Following resources are final g170 so shouldn't need to be updated
   resource "20b-network" do
     url "https://github.com/lightvector/KataGo/releases/download/v1.4.5/g170e-b20c256x2-s5303129600-d1228401921.bin.gz", using: :nounzip
     sha256 "7c8a84ed9ee737e9c7e741a08bf242d63db37b648e7f64942f3a8b1b5101e7c2"
-  end
 
-  resource "30b-network" do
-    url "https://github.com/lightvector/KataGo/releases/download/v1.4.5/g170-b30c320x2-s4824661760-d1229536699.bin.gz", using: :nounzip
-    sha256 "1e601446c870228932d44c8ad25fd527cb7dbf0cf13c3536f5c37cff1993fee6"
+    livecheck do
+      skip "Final g170 20-block network"
+    end
   end
 
   resource "40b-network" do
     url "https://github.com/lightvector/KataGo/releases/download/v1.4.5/g170-b40c256x2-s5095420928-d1229425124.bin.gz", using: :nounzip
     sha256 "2b3a78981d2b6b5fae1cf8972e01bf3e48d2b291bc5e52ef41c9b65c53d59a71"
+
+    livecheck do
+      skip "Final g170 40-block network"
+    end
   end
 
   def install
-    # Workaround for arm64 linux, issue ref: https://github.com/lightvector/KataGo/issues/1049
-    ENV.append_to_cflags "-fsigned-char" if OS.linux? && Hardware::CPU.arm?
-
     args = ["-DNO_GIT_REVISION=1"]
     args += if OS.mac?
-      ["-DUSE_BACKEND=OPENCL", "-DCMAKE_OSX_SYSROOT=#{MacOS.sdk_path}"]
+      ["-DUSE_BACKEND=METAL", "-GNinja"]
     else
       ["-DUSE_BACKEND=EIGEN"]
     end
@@ -64,9 +86,7 @@ class Katago < Formula
     bin.install "build/katago"
 
     pkgshare.install "cpp/configs"
-    pkgshare.install resource("20b-network")
-    pkgshare.install resource("30b-network")
-    pkgshare.install resource("40b-network")
+    resources.each { |r| pkgshare.install r }
   end
 
   test do
