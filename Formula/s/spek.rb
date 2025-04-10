7d868ed4f3cfd6da13cd811ff9024e6152c77463
Michael Cho <michael@michaelcho.dev>
2025-04-10 18:58:00 -0400

    spek: revision bump (wxwidgets 3.2.7)

--- Formula/s/spek.rb
+++ Formula/s/spek.rb
@@ -4,7 +4,7 @@ class Spek < Formula
   url "https://github.com/alexkay/spek/releases/download/v0.8.5/spek-0.8.5.tar.xz"
   sha256 "1bccf85a14a01af8f2f30476cbad004e8bf6031f500e562bbe5bbd1e5eb16c59"
   license "GPL-3.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia:  "58bc8505cebc07a5e85064476565f3201263f69562a8fff4d180b71e85980143"
