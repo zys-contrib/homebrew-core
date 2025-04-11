824075da8a553d72887820f2dfc9643ae8824bde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-11 16:51:54 +0000

    tfcmt: add 4.14.4 bottle.

--- Formula/t/tfcmt.rb
+++ Formula/t/tfcmt.rb
@@ -6,6 +6,15 @@ class Tfcmt < Formula
   license "MIT"
   head "https://github.com/suzuki-shunsuke/tfcmt.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "91bf37acdcbbdc231102894bba2468935b02800e0b97b75013b2bc69b23e6412"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "91bf37acdcbbdc231102894bba2468935b02800e0b97b75013b2bc69b23e6412"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "91bf37acdcbbdc231102894bba2468935b02800e0b97b75013b2bc69b23e6412"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3080ba5d4be4d2a50d62c2ec4ba05050e44717f95e79781f41d9de94f0b7d541"
+    sha256 cellar: :any_skip_relocation, ventura:       "3080ba5d4be4d2a50d62c2ec4ba05050e44717f95e79781f41d9de94f0b7d541"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13a6f9bb1b34d0f07b8682dffb8f5a794421a12470702721d4b69c6358deedc6"
+  end
+
   depends_on "go" => :build
 
   def install
