4a60ac6712c24ca02a2eef9eec7c8014d94178ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-12 22:57:24 +0000

    fern-api 0.57.24

--- Formula/f/fern-api.rb
+++ Formula/f/fern-api.rb
@@ -1,8 +1,8 @@
 class FernApi < Formula
   desc "Stripe-level SDKs and Docs for your API"
   homepage "https://buildwithfern.com/"
-  url "https://registry.npmjs.org/fern-api/-/fern-api-0.57.23.tgz"
-  sha256 "f86ab990ef51eac7dfdde5df665aee1609a0dd42416f8551ef6219f77492bc86"
+  url "https://registry.npmjs.org/fern-api/-/fern-api-0.57.24.tgz"
+  sha256 "46a9ea178d834ee887eb20fa90d7a80d0a4ef5aea22d9c04908519a67755ff70"
   license "MIT"
 
   bottle do
@@ -13,13 +13,13 @@ class FernApi < Formula
 
   def install
     system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink libexec.glob("bin/*")
   end
 
   test do
     system bin/"fern", "init", "--docs", "--org", "brewtest"
     assert_path_exists testpath/"fern/docs.yml"
-    assert_match "\"organization\": \"brewtest\"", (testpath/"fern/fern.config.json").read
+    assert_match '"organization": "brewtest"', (testpath/"fern/fern.config.json").read
 
     system bin/"fern", "--version"
   end
