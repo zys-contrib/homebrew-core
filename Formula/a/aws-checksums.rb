ae7ed178a9f29a32f00cab12c8d928ca405ebbf2
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-04-13 21:27:55 +0000

    aws-checksums: update 0.2.7 bottle.

--- Formula/a/aws-checksums.rb
+++ Formula/a/aws-checksums.rb
@@ -11,6 +11,7 @@ class AwsChecksums < Formula
     sha256 cellar: :any,                 arm64_ventura: "411782961b2ae15dbafeab24cc77f22ef3aafc58ae1d12eafd3655fee97e588f"
     sha256 cellar: :any,                 sonoma:        "5813da053800b6fff4cb6e7b091b0a24991226702ad8a2e1193d9e90a0d60285"
     sha256 cellar: :any,                 ventura:       "658f25960e74c00710c06ff1328678036b460f6e32f974ee3b1056f413628556"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "10aed55de1097e6ce6fdbfc40f93d8b148a7ede46eea8acb2b0c068d64136dd0"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "cef7f2358c7998d4da746e253e360b1665290a5893dc79344d44dbfe8d334abb"
   end
 
