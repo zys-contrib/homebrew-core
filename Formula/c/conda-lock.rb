a3ae32afef083a0a43bd2c0e6a9547fa3229da84
Daeho Ro <lamanus@outlook.kr>
2025-04-13 17:02:18 +0900

    conda-lock: add dependencies

--- Formula/c/conda-lock.rb
+++ Formula/c/conda-lock.rb
@@ -19,12 +19,10 @@ class CondaLock < Formula
 
   depends_on "rust" => :build # for pydantic
   depends_on "certifi"
+  depends_on "cffi"
+  depends_on "cryptography"
   depends_on "libyaml"
   depends_on "python@3.13"
-
-  on_linux do
-    depends_on "cryptography"
-  end
 
   resource "annotated-types" do
     url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
