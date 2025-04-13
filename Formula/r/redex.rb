37b876acda7ab46a61989fac2b98f6ea69d301d1
Michael Cho <michael@michaelcho.dev>
2025-04-13 11:15:31 -0400

    redex: revision bump (boost 1.88.0)

--- Formula/r/redex.rb
+++ Formula/r/redex.rb
@@ -5,7 +5,7 @@ class Redex < Formula
   desc "Bytecode optimizer for Android apps"
   homepage "https://fbredex.com/"
   license "MIT"
-  revision 18
+  revision 19
   head "https://github.com/facebook/redex.git", branch: "main"
 
   stable do
