891a46bc70a152fbfa33cc4ffc03c975310371db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-13 03:05:42 +0000

    serie 0.4.5

--- Formula/s/serie.rb
+++ Formula/s/serie.rb
@@ -1,8 +1,8 @@
 class Serie < Formula
   desc "Rich git commit graph in your terminal"
   homepage "https://github.com/lusingander/serie"
-  url "https://github.com/lusingander/serie/archive/refs/tags/v0.4.4.tar.gz"
-  sha256 "32ea162b9aece1e65e4d813063447004b77b27c85304a4122bb63b16f38389ea"
+  url "https://github.com/lusingander/serie/archive/refs/tags/v0.4.5.tar.gz"
+  sha256 "71cb537273c1dc11201406000e0375699d31a3733d849aee6921677ddeb4c425"
   license "MIT"
   head "https://github.com/lusingander/serie.git", branch: "master"
 
