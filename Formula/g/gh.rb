2d5693e6f2516b5ec79ebc1d508d24e6281e8c00
William Martin <williammartin@github.com>
2025-04-15 11:41:27 +0200

    gh 2.70.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/g/gh.rb
+++ Formula/g/gh.rb
@@ -1,8 +1,8 @@
 class Gh < Formula
   desc "GitHub command-line tool"
   homepage "https://cli.github.com/"
-  url "https://github.com/cli/cli/archive/refs/tags/v2.69.0.tar.gz"
-  sha256 "e2deb3759bbe4da8ad4f071ca604fda5c2fc803fef8b3b89896013e4b1c1fe65"
+  url "https://github.com/cli/cli/archive/refs/tags/v2.70.0.tar.gz"
+  sha256 "9e2247e5b31131fd4ac63916b9483a065fcfb861ebb93588cf2ff42952ae08c5"
   license "MIT"
   head "https://github.com/cli/cli.git", branch: "trunk"
 
