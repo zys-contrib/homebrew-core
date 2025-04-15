f52c40c68838aeaccb243c7f2cb246a8638f277f
Rui Chen <rui@chenrui.dev>
2025-04-14 21:42:04 -0400

    mvfst 2025.04.14.00
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mvfst.rb
+++ Formula/m/mvfst.rb
@@ -1,8 +1,8 @@
 class Mvfst < Formula
   desc "QUIC transport protocol implementation"
   homepage "https://github.com/facebook/mvfst"
-  url "https://github.com/facebook/mvfst/archive/refs/tags/v2025.04.07.00.tar.gz"
-  sha256 "2b324e104cdf8dd1c459535259b8c7c6670ebe83de827aa60aa0c37a6ea254ac"
+  url "https://github.com/facebook/mvfst/archive/refs/tags/v2025.04.14.00.tar.gz"
+  sha256 "4c0d4b461ab4ff016e9b29178d9b4c5e6cdbe46b13c76cdf9dfc1eabe198b202"
   license "MIT"
   head "https://github.com/facebook/mvfst.git", branch: "main"
 
