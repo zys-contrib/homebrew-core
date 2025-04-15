40a3579ba1c9df2f90d923e5539b350224e743b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-15 21:19:47 +0000

    toml-test: update 1.6.0 bottle.

--- Formula/t/toml-test.rb
+++ Formula/t/toml-test.rb
@@ -6,15 +6,13 @@ class TomlTest < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "8ded510f549bfdb671828c5a29fdd4ae9be18d24c89a1b759805a58548ce3316"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "e8f84fa60cb3575cbebdbb288bfd5b1b7cc1d07b3ca7e6028c5822ab42d53757"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e8f84fa60cb3575cbebdbb288bfd5b1b7cc1d07b3ca7e6028c5822ab42d53757"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e8f84fa60cb3575cbebdbb288bfd5b1b7cc1d07b3ca7e6028c5822ab42d53757"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a7471409f4ee3f531619b2bd63fbc770749e3b506e35ccb1d81fdcd92dc7bb87"
-    sha256 cellar: :any_skip_relocation, ventura:        "a7471409f4ee3f531619b2bd63fbc770749e3b506e35ccb1d81fdcd92dc7bb87"
-    sha256 cellar: :any_skip_relocation, monterey:       "a7471409f4ee3f531619b2bd63fbc770749e3b506e35ccb1d81fdcd92dc7bb87"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "04597899c9eec6324223895629a07adc2163dcee45fd4390329a469aed507991"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d052b80767db257261ffbc2c39125745f20e9d155cb35441788d6e8a3046da6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41dedc807934ac3a07fec76dfd5b281ce8545e7c53b2ce4820578840d78710b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "41dedc807934ac3a07fec76dfd5b281ce8545e7c53b2ce4820578840d78710b4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "41dedc807934ac3a07fec76dfd5b281ce8545e7c53b2ce4820578840d78710b4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f9b3b7fce7698c7e0f0302fefa8e243ab1592b879c4cbb910ab32716ae5dde20"
+    sha256 cellar: :any_skip_relocation, ventura:       "f9b3b7fce7698c7e0f0302fefa8e243ab1592b879c4cbb910ab32716ae5dde20"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "00ce205eb7bca140cb56d9fdddfcab657890587bf0f63bd48c78e433634df458"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa6601c9403bb864f502aa08e3df84a74688d6033a2c56f6dcbc6dc1a7c3d040"
   end
 
   depends_on "go" => :build
