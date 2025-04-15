c799be8a329a330b326656f134fc88a4fe435940
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-15 11:01:35 +0000

    k8sgpt 0.4.11

--- Formula/k/k8sgpt.rb
+++ Formula/k/k8sgpt.rb
@@ -1,8 +1,8 @@
 class K8sgpt < Formula
   desc "Scanning your k8s clusters, diagnosing, and triaging issues in simple English"
   homepage "https://k8sgpt.ai/"
-  url "https://github.com/k8sgpt-ai/k8sgpt/archive/refs/tags/v0.4.10.tar.gz"
-  sha256 "5ba544b01bb3eaa8fa6e04e8d71bdf5cda19bda40493915e8c53a687a396bac2"
+  url "https://github.com/k8sgpt-ai/k8sgpt/archive/refs/tags/v0.4.11.tar.gz"
+  sha256 "f18886490964ac3983316392c5b778ffd64e63942b62bf5c5e06ae0aa94c3ed0"
   license "Apache-2.0"
   head "https://github.com/k8sgpt-ai/k8sgpt.git", branch: "main"
 
