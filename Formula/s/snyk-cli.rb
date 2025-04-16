e9a68f12c8a331d997e81c4c2952ef1172930f46
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-16 18:51:13 +0000

    snyk-cli: update 1.1296.2 bottle.

--- Formula/s/snyk-cli.rb
+++ Formula/s/snyk-cli.rb
@@ -6,12 +6,12 @@ class SnykCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "efbc7928d000eff7e92acdd42d9614b527a7d54bf8fc425aec29e697620db318"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "efbc7928d000eff7e92acdd42d9614b527a7d54bf8fc425aec29e697620db318"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "efbc7928d000eff7e92acdd42d9614b527a7d54bf8fc425aec29e697620db318"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6128ebaf551bc458d4f20fb33f2e25c933b3f76b16680f32902a49b801db153b"
-    sha256 cellar: :any_skip_relocation, ventura:       "6128ebaf551bc458d4f20fb33f2e25c933b3f76b16680f32902a49b801db153b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d849e4b959713d3aa46db12e82fdc501738d7d6047b6cc7b07c958981578bec"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c62dbe503cf495199acfa3323ac47a4a68ab0b0d940d5dabdb19affa17da26ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c62dbe503cf495199acfa3323ac47a4a68ab0b0d940d5dabdb19affa17da26ae"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c62dbe503cf495199acfa3323ac47a4a68ab0b0d940d5dabdb19affa17da26ae"
+    sha256 cellar: :any_skip_relocation, sonoma:        "707e91ecbaca87942042e7e2dee4e267525c87066bde87333105048ae27acd6d"
+    sha256 cellar: :any_skip_relocation, ventura:       "707e91ecbaca87942042e7e2dee4e267525c87066bde87333105048ae27acd6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e567604edab9a1991df8ab0e75ee327ac240b86a5b2f122aeefa4d0df9a793c5"
   end
 
   depends_on "node"
