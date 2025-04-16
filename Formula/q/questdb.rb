0b8531f51755c54fdc8ca1a0c34c7a179cb5c1e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-16 20:10:59 +0000

    questdb 8.3.0

--- Formula/q/questdb.rb
+++ Formula/q/questdb.rb
@@ -1,8 +1,8 @@
 class Questdb < Formula
   desc "Time Series Database"
   homepage "https://questdb.io"
-  url "https://github.com/questdb/questdb/releases/download/8.2.3/questdb-8.2.3-no-jre-bin.tar.gz"
-  sha256 "863c7e483ab0978c1968b8ad20fdb6c142d4200cc0a1a2987a5e54593656bef1"
+  url "https://github.com/questdb/questdb/releases/download/8.3.0/questdb-8.3.0-no-jre-bin.tar.gz"
+  sha256 "732ba49457956f62f9f32a0e4fa961c26cc35a9b63f26cee9efb5d25f36e90bf"
   license "Apache-2.0"
 
   livecheck do
