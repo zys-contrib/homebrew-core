20b11f77d090066e229cc6ad21a6676392255f2b
Rui Chen <rui@chenrui.dev>
2025-04-17 19:31:22 +0000

    aqua: update 2.48.2 bottle.

--- Formula/a/aqua.rb
+++ Formula/a/aqua.rb
@@ -12,12 +12,12 @@ class Aqua < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "affaf7b3bbac3925807407d2cbb3fdd24d2e965ef1600e6eb731c26d96e0c7ee"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "affaf7b3bbac3925807407d2cbb3fdd24d2e965ef1600e6eb731c26d96e0c7ee"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "affaf7b3bbac3925807407d2cbb3fdd24d2e965ef1600e6eb731c26d96e0c7ee"
-    sha256 cellar: :any_skip_relocation, sonoma:        "872f6bb9c33b753bd81df4220c42f3e065ccbf960c33214d698449219386a09c"
-    sha256 cellar: :any_skip_relocation, ventura:       "872f6bb9c33b753bd81df4220c42f3e065ccbf960c33214d698449219386a09c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56a52e70b4be4f0586860a917b74da50101fc63a6138736475a8419792d46a9e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "357c448cc996c45bbceb5dce6a3dfd32ffbfaa132360238bca106d21bffae26e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "357c448cc996c45bbceb5dce6a3dfd32ffbfaa132360238bca106d21bffae26e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "357c448cc996c45bbceb5dce6a3dfd32ffbfaa132360238bca106d21bffae26e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c0fb80916035e799064bb67bc5716f1ba4f7d9f0937b553c41c2ec02d12a43df"
+    sha256 cellar: :any_skip_relocation, ventura:       "c0fb80916035e799064bb67bc5716f1ba4f7d9f0937b553c41c2ec02d12a43df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bcb9e49ca96696827644624b3cf7eb9ec0631be80563387b1455675ec6ea4cb6"
   end
 
   depends_on "go" => :build
