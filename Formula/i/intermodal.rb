947f74cf21c7d9784e0725d8504e5a73efb46a3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-17 12:52:26 +0000

    intermodal: add 0.1.14 bottle.

--- Formula/i/intermodal.rb
+++ Formula/i/intermodal.rb
@@ -5,6 +5,15 @@ class Intermodal < Formula
   sha256 "4b42fc39246a637e8011a520639019d33beffb337ed4e45110260eb67ecec9cb"
   license "CC0-1.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "852b6c8f3270eac4536874bcd3569c7b356f196095395587b827c19694bc8850"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e8a4fd8dbae699ee5489ee270a44217875c278e8dafd4b0d2ab59c0a53c233d7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f97365296218ba43ceae80cc225a3ede3974b0060dc6e22b0e9c7bb10fc10ce5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d67ee890e7374157bff784bc6a756b0a38156c90bf45dfbeed40860a060b82bf"
+    sha256 cellar: :any_skip_relocation, ventura:       "729f1a952834a171ae49923079e207ff23dca4df44975f22057cb3c7df555c7c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "302d5f8fcf0869b8dd99ad396a04ca70d4f4a07cdc500de72cd01dec07fccba1"
+  end
+
   depends_on "help2man" => :build
   depends_on "rust" => :build
 
