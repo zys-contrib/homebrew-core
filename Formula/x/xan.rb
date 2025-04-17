55502d6118e6c6f686048e7562a5c7cd4ec8a6b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-17 08:17:43 +0000

    xan 0.49.1

--- Formula/x/xan.rb
+++ Formula/x/xan.rb
@@ -1,8 +1,8 @@
 class Xan < Formula
   desc "CSV CLI magician written in Rust"
   homepage "https://github.com/medialab/xan"
-  url "https://github.com/medialab/xan/archive/refs/tags/0.49.0.tar.gz"
-  sha256 "46e73d305a9982eb219e45399590be02a24a03d97e91aa4adc080303dcaff87d"
+  url "https://github.com/medialab/xan/archive/refs/tags/0.49.1.tar.gz"
+  sha256 "a17cd8cc0463548541d2aadb0f2f559ec9d6cb3e58463ee32e4e50e09677a8a7"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/medialab/xan.git", branch: "master"
 
