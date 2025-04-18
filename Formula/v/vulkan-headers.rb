bacfd8b8a8261a4c433125d22882e767b7c7d4d4
Rui Chen <rui@chenrui.dev>
2025-04-18 19:37:29 -0400

    vulkan-headers 1.4.313
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vulkan-headers.rb
+++ Formula/v/vulkan-headers.rb
@@ -1,8 +1,8 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.4.312.tar.gz"
-  sha256 "0f1eb34aa1182baf636e3dbedc9e9ce0acad87794ea15e94378b5deeeb17463f"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.4.313.tar.gz"
+  sha256 "f3298b8dc620530493296759858a69b622f98ececa0e8c75488ad2000778148f"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-Headers.git", branch: "main"
 
