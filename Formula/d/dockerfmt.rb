12d1735b968599d6beaf5a2bd7398000b2c4d4a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-18 23:28:56 +0000

    dockerfmt: update 0.3.6 bottle.

--- Formula/d/dockerfmt.rb
+++ Formula/d/dockerfmt.rb
@@ -7,12 +7,12 @@ class Dockerfmt < Formula
   head "https://github.com/reteps/dockerfmt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "817dd9b6ee04239bed351847c818e3b03ad416de6f92c382b2912946adf6dad6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "817dd9b6ee04239bed351847c818e3b03ad416de6f92c382b2912946adf6dad6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "817dd9b6ee04239bed351847c818e3b03ad416de6f92c382b2912946adf6dad6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2fa53942ecbce05e5cadfbe91181a9a59821e289532aa4c16fc7586ad91815e4"
-    sha256 cellar: :any_skip_relocation, ventura:       "2fa53942ecbce05e5cadfbe91181a9a59821e289532aa4c16fc7586ad91815e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3eac7455c0c547cf141b7911cc85ee6a3b92b2a54bba7ab4429d72dfccf2aed"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "74ec6747b0589a7f1bb7185e1576d847ce7c182039233b12600905ab93b122df"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "74ec6747b0589a7f1bb7185e1576d847ce7c182039233b12600905ab93b122df"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "74ec6747b0589a7f1bb7185e1576d847ce7c182039233b12600905ab93b122df"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ac2b50117b3436870b6110a7342911a422f28edccf6278e0467a094c1524893e"
+    sha256 cellar: :any_skip_relocation, ventura:       "ac2b50117b3436870b6110a7342911a422f28edccf6278e0467a094c1524893e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1aeb3ba38382f7bc0070773ebf5417bd06bdeddb40635a3b27022df5d16f7a16"
   end
 
   depends_on "go" => :build
