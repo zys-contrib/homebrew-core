32049b115aaa028b97990a369bb74b67cd345a92
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-18 14:33:36 +0000

    pinact: update 3.0.5 bottle.

--- Formula/p/pinact.rb
+++ Formula/p/pinact.rb
@@ -14,12 +14,12 @@ class Pinact < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8bdf965f7a4bc4bfd89d7c1995af30af894e9b80a5910e962eba87717099d6db"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8bdf965f7a4bc4bfd89d7c1995af30af894e9b80a5910e962eba87717099d6db"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8bdf965f7a4bc4bfd89d7c1995af30af894e9b80a5910e962eba87717099d6db"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a4d53e1ca383291b6d79560e9248ede6250632939fc79fa3a73e4462abe70dc9"
-    sha256 cellar: :any_skip_relocation, ventura:       "a4d53e1ca383291b6d79560e9248ede6250632939fc79fa3a73e4462abe70dc9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eadb0836df3c77553f7f94ce2e07861e09b4c285c3ad79e8fae5fb3208ce6133"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0c54f1b6682fb962aa1b1c1defabb3b684802383bee51be92741434ac2e6a036"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0c54f1b6682fb962aa1b1c1defabb3b684802383bee51be92741434ac2e6a036"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0c54f1b6682fb962aa1b1c1defabb3b684802383bee51be92741434ac2e6a036"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b2e2d75d739894c5fdfdafbcaa645fbacbe3adac1acf64e29304cb0917770f77"
+    sha256 cellar: :any_skip_relocation, ventura:       "b2e2d75d739894c5fdfdafbcaa645fbacbe3adac1acf64e29304cb0917770f77"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fe3c25d9ee93cee16fd7b190498682542f21e4015e9deb9000cf1feb014fe3e"
   end
 
   depends_on "go" => :build
