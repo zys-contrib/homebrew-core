062a248f94f3463cf11177bc1b8f3acd7bb0704d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-19 12:41:56 +0000

    aqua: update 2.49.0 bottle.

--- Formula/a/aqua.rb
+++ Formula/a/aqua.rb
@@ -12,12 +12,12 @@ class Aqua < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1470bc6b5c68d0fc3d007a7a3c3d3917f57a039e084ecef1dac58135eca7bff3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1470bc6b5c68d0fc3d007a7a3c3d3917f57a039e084ecef1dac58135eca7bff3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "1470bc6b5c68d0fc3d007a7a3c3d3917f57a039e084ecef1dac58135eca7bff3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "991a8d86ada805585a017105516be05c775aab6788481620b239e7eef9437406"
-    sha256 cellar: :any_skip_relocation, ventura:       "991a8d86ada805585a017105516be05c775aab6788481620b239e7eef9437406"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46f81b95bf122b7c73f7585271040cff5e718455b04819b979a7eff42db52a84"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9524929bed99c17466cf3659c1b8affa1dcc95daa95501531294ff1980981241"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9524929bed99c17466cf3659c1b8affa1dcc95daa95501531294ff1980981241"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9524929bed99c17466cf3659c1b8affa1dcc95daa95501531294ff1980981241"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f81123f8963b9108936813e0d973bce6bc7396075d159d1e390e7009c6aeacff"
+    sha256 cellar: :any_skip_relocation, ventura:       "f81123f8963b9108936813e0d973bce6bc7396075d159d1e390e7009c6aeacff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d09e94d87c849742fb811c59c15794f40a170158847ccda2b95f613c182e30b"
   end
 
   depends_on "go" => :build
