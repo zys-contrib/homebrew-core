3917da012015dbd0151bd415232fad4f29ea1dbd
botantony <antonsm21@gmail.com>
2025-04-19 16:44:02 +0200

    lavat 2.2.0

--- Formula/l/lavat.rb
+++ Formula/l/lavat.rb
@@ -1,8 +1,8 @@
 class Lavat < Formula
   desc "Lava lamp simulation using metaballs in the terminal"
   homepage "https://github.com/AngelJumbo/lavat"
-  url "https://github.com/AngelJumbo/lavat/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "07b11ce4d15354d8fbb85b60955c74d52d72946d509ec7dda02498adc71e2df4"
+  url "https://github.com/AngelJumbo/lavat/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "e8ccda9798270838244bf60497062507cae75b6ab9fb198c54b5a005bea8b87f"
   license "MIT"
   head "https://github.com/AngelJumbo/lavat.git", branch: "main"
 
@@ -17,8 +17,7 @@ class Lavat < Formula
   end
 
   def install
-    system "make" # `make install` doesn't work on macOS
-    bin.install "lavat"
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
