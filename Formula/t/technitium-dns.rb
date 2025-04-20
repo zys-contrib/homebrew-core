afb5cfdba2df3ec4d63280ca0e2966929c33c4a1
Daeho Ro <lamanus@outlook.kr>
2025-04-20 17:31:06 +0900

    technitium-dns 13.5.0 (new formula)
    Co-authored-by: Ingmar Stein <490610+IngmarStein@users.noreply.github.com>

---
+++ Formula/t/technitium-dns.rb
@@ -0,0 +1,70 @@
+class TechnitiumDns < Formula
+  desc "Self host a DNS server for privacy & security"
+  homepage "https://technitium.com/dns/"
+  url "https://github.com/TechnitiumSoftware/DnsServer/archive/refs/tags/v13.5.0.tar.gz"
+  sha256 "7a5f47af4c938c5983e415547b777f853ede5ada8837012c8b5e8e36df1a380e"
+  license "GPL-3.0-or-later"
+
+  # TODO: update dotnet version
+  # Issue ref: https://github.com/TechnitiumSoftware/DnsServer/issues/1303
+  depends_on "dotnet@8"
+  depends_on "libmsquic"
+  depends_on "technitium-library"
+
+  on_linux do
+    depends_on "bind" => :test # for `dig`
+  end
+
+  def install
+    ENV["DOTNET_CLI_TELEMETRY_OPTOUT"] = "1"
+
+    dotnet = Formula["dotnet@8"]
+    args = %W[
+      --configuration Release
+      --framework net#{dotnet.version.major_minor}
+      --no-self-contained
+      --output #{libexec}
+      --use-current-runtime
+    ]
+
+    inreplace Dir.glob("**/*.csproj"),
+              "..\\..\\TechnitiumLibrary\\bin",
+              Formula["technitium-library"].libexec.to_s.tr("/", "\\"),
+              audit_result: false
+    system "dotnet", "publish", "DnsServerApp/DnsServerApp.csproj", *args
+
+    (bin/"technitium-dns").write <<~SHELL
+      #!/bin/bash
+      export DYLD_FALLBACK_LIBRARY_PATH=#{Formula["libmsquic"].opt_lib}
+      export DOTNET_ROOT=#{dotnet.opt_libexec}
+      exec #{dotnet.opt_libexec}/dotnet #{libexec}/DnsServerApp.dll #{etc}/technitium-dns
+    SHELL
+  end
+
+  service do
+    run opt_bin/"technitium-dns"
+    keep_alive true
+    error_log_path var/"log/technitium-dns.log"
+    log_path var/"log/technitium-dns.log"
+    working_dir var
+  end
+
+  test do
+    dotnet = Formula["dotnet@8"]
+    tmpdir = Pathname.new(Dir.mktmpdir)
+    # Start the DNS server
+    require "pty"
+    PTY.spawn("#{dotnet.opt_libexec}/dotnet #{libexec}/DnsServerApp.dll #{tmpdir}") do |r, _w, pid|
+      # Give the server time to start
+      sleep 2
+      # Use `dig` to resolve "localhost"
+      assert_match "Server was started successfully", r.gets
+      output = shell_output("dig @127.0.0.1 localhost 2>&1")
+      assert_match "ANSWER SECTION", output
+      assert_match "localhost.", output
+    ensure
+      Process.kill("KILL", pid)
+      Process.wait(pid)
+    end
+  end
+end
