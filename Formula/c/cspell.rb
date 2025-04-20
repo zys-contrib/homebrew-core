35e93e4a71b2afa740f392a3e8ce053f89b92866
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-20 08:48:49 +0000

    cspell: update 8.19.2 bottle.

--- Formula/c/cspell.rb
+++ Formula/c/cspell.rb
@@ -6,13 +6,13 @@ class Cspell < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "359416144ce7959ee1d97cc917ba676617bd0040e09f6c9c9212e2863489b059"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "359416144ce7959ee1d97cc917ba676617bd0040e09f6c9c9212e2863489b059"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "359416144ce7959ee1d97cc917ba676617bd0040e09f6c9c9212e2863489b059"
-    sha256 cellar: :any_skip_relocation, sonoma:        "305334c8a4a4ae2768b09633e467caaa8560b75173e769be48f32fdb8bfbfc39"
-    sha256 cellar: :any_skip_relocation, ventura:       "305334c8a4a4ae2768b09633e467caaa8560b75173e769be48f32fdb8bfbfc39"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "359416144ce7959ee1d97cc917ba676617bd0040e09f6c9c9212e2863489b059"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "359416144ce7959ee1d97cc917ba676617bd0040e09f6c9c9212e2863489b059"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e645a18da7a578b9f81fb60555628e5716a0cec5537222adfd1b09b3646c6ec1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e645a18da7a578b9f81fb60555628e5716a0cec5537222adfd1b09b3646c6ec1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e645a18da7a578b9f81fb60555628e5716a0cec5537222adfd1b09b3646c6ec1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "95d1b28e34021e0e01eedeb692d4cf1a62ef54e7f47675530d8bcd8f4a0e6ee0"
+    sha256 cellar: :any_skip_relocation, ventura:       "95d1b28e34021e0e01eedeb692d4cf1a62ef54e7f47675530d8bcd8f4a0e6ee0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e645a18da7a578b9f81fb60555628e5716a0cec5537222adfd1b09b3646c6ec1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e645a18da7a578b9f81fb60555628e5716a0cec5537222adfd1b09b3646c6ec1"
   end
 
   depends_on "node"
