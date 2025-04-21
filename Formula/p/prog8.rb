149e81e2faa380417bf42bd2d16ea100ec0c7f75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-21 15:13:53 +0000

    prog8: add 11.2 bottle.

--- Formula/p/prog8.rb
+++ Formula/p/prog8.rb
@@ -5,6 +5,15 @@ class Prog8 < Formula
   sha256 "2e06e38c5b83c277a1671cd5d20e02373dfb4b52f8ec0064dc21f8e272b9edb3"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "236ab9131d78c222312ae618875010180cbf02b157c79ee275ca9bf5960e96c8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "42f9f42656aae27aea98090928eb01f6c94ebc632be2e2b87b81962dae463a69"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7e36385c5968f414e976a547f344996dee0c8c5b3c9c437bd8ad75b4d7d75925"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2f8ab326a823ff286fc0d5dd717e2730b6d55b4926d8d55d5a7acc8e28cc3c8d"
+    sha256 cellar: :any_skip_relocation, ventura:       "8dd51e9bfe666751eff6ecde3b4d897df3de793ffc57681e8bd53a01401299cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "161cbfda304f5f4294c4eb7cffbd9beeda468295ed12c65cfc5d0b44e8cc4674"
+  end
+
   depends_on "gradle" => :build
   depends_on "kotlin" => :build
 
