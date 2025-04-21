56648eb0b88d9b8dc56d3cf9706e786adb0d9942
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-21 13:57:32 +0000

    cargo-shear: add 1.2.4 bottle.

--- Formula/c/cargo-shear.rb
+++ Formula/c/cargo-shear.rb
@@ -6,6 +6,15 @@ class CargoShear < Formula
   license "MIT"
   head "https://github.com/Boshen/cargo-shear.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9d1cef5fc862e8c26115a6cab56da98de553dd37ad01e10aee076d24b1ebabce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9794da55825255345537b9905e1c9196dd45e2b9f90e0bb55aa8205e19563028"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2ef55a63a6eb49f771f8ee9b48b7076e0c178d3776635482bea3add593a9c438"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5bd2f417d96e67a653772ab833a303f75bef2f92a0bd8fd2b93912ea5728604e"
+    sha256 cellar: :any_skip_relocation, ventura:       "d64bc681a19dfd38da4901612a456c9995d9eabecce5da192709c6dda77258ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a2453871d1d71575e462f91dfb9033db055055bf9ab3653ca5accf171da3d3a7"
+  end
+
   depends_on "rust" => :build
   depends_on "rustup" => :test
 
