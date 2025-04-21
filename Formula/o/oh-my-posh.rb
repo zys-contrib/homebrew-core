f8cdca75350b4cd6f80c8df16c74846fc90dd398
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-21 17:11:18 +0000

    oh-my-posh 25.17.0

--- Formula/o/oh-my-posh.rb
+++ Formula/o/oh-my-posh.rb
@@ -1,8 +1,8 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/refs/tags/v25.16.1.tar.gz"
-  sha256 "6bfe0aa257ea0db466c291f68d6596f6e0884c51970e94869629562459ba5bf2"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/refs/tags/v25.17.0.tar.gz"
+  sha256 "f3eca0e4e5d58ef45649602fd85a59be65d172936c0b6f1264ed7779a2c3aa90"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
