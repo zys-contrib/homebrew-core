bb5f440192e1c0efdda062d3788c286b15e7dd8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-21 17:20:36 +0000

    talisman 1.35.1
    talisman: add go.mod patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/talisman.rb
+++ Formula/t/talisman.rb
@@ -1,8 +1,8 @@
 class Talisman < Formula
   desc "Tool to detect and prevent secrets from getting checked in"
   homepage "https://thoughtworks.github.io/talisman/"
-  url "https://github.com/thoughtworks/talisman/archive/refs/tags/v1.34.0.tar.gz"
-  sha256 "273c200ce3950d3064c2077545b0d2d41d1c14708f61343b30ea8ee667b83474"
+  url "https://github.com/thoughtworks/talisman/archive/refs/tags/v1.35.1.tar.gz"
+  sha256 "bdf7be9943cea535f81e463a60464f7a5bbad2c08fe7096e9cd71a00bb8cfc48"
   license "MIT"
   version_scheme 1
   head "https://github.com/thoughtworks/talisman.git", branch: "master"
@@ -22,6 +22,12 @@ class Talisman < Formula
   end
 
   depends_on "go" => :build
+
+  # go.mod update, upstream pr ref, https://github.com/thoughtworks/talisman/pull/479
+  patch do
+    url "https://github.com/thoughtworks/talisman/commit/592b23a7a757e776a4203c23724181a1d13ab9ae.patch?full_index=1"
+    sha256 "f069f83c819a45496ca9edbca2c5fa244c18df5a1f45eaa1d99dfbbdc1180ec5"
+  end
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=#{version}"), "./cmd"
