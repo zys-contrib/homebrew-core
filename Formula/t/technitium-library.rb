e183adc3c4c2836ac7198c92e11316c4c784ae89
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-21 15:01:51 +0000

    technitium-library: add 13.5.0 bottle.

--- Formula/t/technitium-library.rb
+++ Formula/t/technitium-library.rb
@@ -5,6 +5,14 @@ class TechnitiumLibrary < Formula
   sha256 "8e0f3200ad6ee6a1ab346a7224a1bc538f6b3dc1b660cd8bb725d0dc09fa2bf9"
   license "GPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec851157536a44fd5d308a20ea095be18790f0532973072dc00bbcf34a76c192"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9e08722a6396e8a695202789ed5db5949594e8386b2b1ac5171a90696bd36a65"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "38d3dbdca7ad114282e27a3b3a7b08bd34ab9e5b1afdba0bfcb0e5d041aa8753"
+    sha256 cellar: :any_skip_relocation, ventura:       "c072c54bbd09d645d0c01864681ab22434bb3b9d27d2d039a2be3fde872a02e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aff4ab8e47c10b2674a6a1af0b698f5016cd5b4cc1aeaba975a1499310266080"
+  end
+
   depends_on "dotnet@8"
 
   def install
