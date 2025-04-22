fc6a0233a0006ea89711192b850ea1f662f14d12
Michael Cho <michael@michaelcho.dev>
2025-04-22 13:16:21 -0400

    sv2v: add `gmp` and `libffi` dependencies

--- Formula/s/sv2v.rb
+++ Formula/s/sv2v.rb
@@ -16,6 +16,9 @@ class Sv2v < Formula
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
+  depends_on "gmp"
+
+  uses_from_macos "libffi"
 
   def install
     system "cabal", "v2-update"
