fcf9800a1c2acd367b0c7c9c16e37f1e95cc9796
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-22 07:12:34 +0000

    wrkflw: add 0.3.0 bottle.

--- Formula/w/wrkflw.rb
+++ Formula/w/wrkflw.rb
@@ -6,6 +6,15 @@ class Wrkflw < Formula
   license "MIT"
   head "https://github.com/bahdotsh/wrkflw.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "06e62080c7571c442183998c02ef075be5d22d7001a7ae4fb9a6f8a253a79ad2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "57e3a4e16310b7ece6cf5b1e040999aa5dc3bd2e945f3e46a5b83d63aaa50e2c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2c84fe3c5dea486dded0ae9a833be74dabae399fa82d79d505203c47abd24c10"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2824fa1b316132df7d9499e2259298b32e1f2eb70096098158459affc931f30d"
+    sha256 cellar: :any_skip_relocation, ventura:       "197b230fe07bf13db3781c81961ce7c41a3dea6a3b0a9565a6a1872db000a917"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "124942cdeee15a286a94762048e3162285f56e59b67d535ad99cbd0340d8d68d"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
