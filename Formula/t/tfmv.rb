e7bad73f8bbc7e060b36ff5f9817bcc8d71c5e27
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-22 07:10:56 +0000

    tfmv: add 0.2.6 bottle.

--- Formula/t/tfmv.rb
+++ Formula/t/tfmv.rb
@@ -6,6 +6,15 @@ class Tfmv < Formula
   license "MIT"
   head "https://github.com/suzuki-shunsuke/tfmv.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "17ed9dfd157233dfd46b51ab27d61dfe27ac116f5a69c1c1c1cfdcf4188b5c79"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "17ed9dfd157233dfd46b51ab27d61dfe27ac116f5a69c1c1c1cfdcf4188b5c79"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "17ed9dfd157233dfd46b51ab27d61dfe27ac116f5a69c1c1c1cfdcf4188b5c79"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8b9ce03dd144442551b098971829eef99e9baaf5248f4b2c5aa6a9bfa32199ff"
+    sha256 cellar: :any_skip_relocation, ventura:       "8b9ce03dd144442551b098971829eef99e9baaf5248f4b2c5aa6a9bfa32199ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "37da63bcd510112e2ccfbdf666bb12b1f741ef1de37afda965510bfedac63b7f"
+  end
+
   depends_on "go" => :build
 
   def install
