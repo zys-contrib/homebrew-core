ef234788d6e0e33e0451a69ae4c0aa4b82bb4fb4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-22 17:20:00 +0000

    oxker 0.10.3

--- Formula/o/oxker.rb
+++ Formula/o/oxker.rb
@@ -1,8 +1,8 @@
 class Oxker < Formula
   desc "Terminal User Interface (TUI) to view & control docker containers"
   homepage "https://github.com/mrjackwills/oxker"
-  url "https://github.com/mrjackwills/oxker/archive/refs/tags/v0.10.2.tar.gz"
-  sha256 "b410039c1cbbada80cf010ccfd9450ea6761b0ed5ae9e7fc171b0958bef25089"
+  url "https://github.com/mrjackwills/oxker/archive/refs/tags/v0.10.3.tar.gz"
+  sha256 "6d27d6e9dfbe38270560751f932cd72659b73d4891117042aa21eba800f2ad9e"
   license "MIT"
   head "https://github.com/mrjackwills/oxker.git", branch: "main"
 
