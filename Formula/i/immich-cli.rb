395d31d6beef1c928b2e1253dc076bf1d378d037
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-23 23:18:05 +0000

    immich-cli: update 2.2.63 bottle.

--- Formula/i/immich-cli.rb
+++ Formula/i/immich-cli.rb
@@ -6,7 +6,7 @@ class ImmichCli < Formula
   license "AGPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2cae0aa479a4fb8724d5e83687f28402ba603d1ddff2c75461c2d23982562e67"
+    sha256 cellar: :any_skip_relocation, all: "3c8f4d5a61955936c953126989b244f4d5acc27f5c5062d3bf0a957c21f9b796"
   end
 
   depends_on "node"
