0e7b657639fcf2a60cb304b5f8cf8baa1973a0d3
Rui Chen <rui@chenrui.dev>
2025-04-23 17:55:54 -0400

    falcosecurity-libs: revision bump (grpc 1.72.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/falcosecurity-libs.rb
+++ Formula/f/falcosecurity-libs.rb
@@ -8,6 +8,7 @@ class FalcosecurityLibs < Formula
     { any_of: ["GPL-2.0-only", "MIT"] }, # driver/
     { "GPL-2.0-only" => { with: "Linux-syscall-note" } }, # userspace/libscap/compat/
   ]
+  revision 1
 
   livecheck do
     url :stable
