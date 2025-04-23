bed06f3f2bd70d1cfac3379a2beb81bf27894dc4
Rui Chen <rui@chenrui.dev>
2025-04-23 17:55:54 -0400

    etcd-cpp-apiv3: revision bump (grpc 1.72.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/e/etcd-cpp-apiv3.rb
+++ Formula/e/etcd-cpp-apiv3.rb
@@ -4,7 +4,7 @@ class EtcdCppApiv3 < Formula
   url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.15.4.tar.gz"
   sha256 "4516ecfa420826088c187efd42dad249367ca94ea6cdfc24e3030c3cf47af7b4"
   license "BSD-3-Clause"
-  revision 25
+  revision 26
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "41bea044776b9876a036125b174c010a180eda1dc5d2c8b8ef27202f107afb91"
