52cb4b2a3f18c7300c6efb5b393d4385e0746bca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-23 10:52:47 +0000

    benthos 4.48.0

--- Formula/b/benthos.rb
+++ Formula/b/benthos.rb
@@ -1,8 +1,8 @@
 class Benthos < Formula
   desc "Stream processor for mundane tasks written in Go"
   homepage "https://github.com/redpanda-data/benthos"
-  url "https://github.com/redpanda-data/benthos/archive/refs/tags/v4.47.0.tar.gz"
-  sha256 "3cecbfd79c81a23adde667b36f274a3c145e05b5ea09e8a69ff3e73358d80858"
+  url "https://github.com/redpanda-data/benthos/archive/refs/tags/v4.48.0.tar.gz"
+  sha256 "19bdb73c028524f2c52389a3a5144dbb65f807c7bac91197abaf20a421ea5783"
   license "MIT"
   head "https://github.com/redpanda-data/benthos.git", branch: "main"
 
