a60d646d58f48992f0508b8a64120c4506856192
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-23 21:07:39 +0000

    kubernetes-cli: update 1.33.0 bottle.

--- Formula/k/kubernetes-cli.rb
+++ Formula/k/kubernetes-cli.rb
@@ -13,13 +13,13 @@ class KubernetesCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ecec94a86bf2747a20a97e2bd8db4b20e54cac3aa4d762e33f60646d4d97300"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ed676e3f9e2664c536d3ff8d9694abd1316c8941e9dc19475977b7bc2b48f74e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "50b2f779cbf75369b220d2157348565ecac14fa683896d6e5431343a80ba2724"
-    sha256 cellar: :any_skip_relocation, sonoma:        "100eed4c7f30b03f3d52cb55515bd189fbee3ab528528a1425a0783bc890e0ba"
-    sha256 cellar: :any_skip_relocation, ventura:       "cf9dcf1ccc545847016e36430f47aa92b67eb21f65322bb06c2f250210da8d89"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9ae2dfce9560b7533c5220679182a23354fca9378eb56caa71c35f416b4335d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a922d49359951a683a6b84826b2052c5fca4e52627a8fd133213929c41616b2c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4cbf01387b2e22e797e23bdd20bbc739eeed94e7602d2960f4d14e9759b9e58d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2e24f2b786f03e73817040d1dc9ff09f05e9d3e6154fb25b0970ecc0e561527e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "587c59ca1979dc3599256e6cc4f4781df7d54488406e91a5b30e1215d7e92666"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1f7f73d0cc22396a7e73f2b462e09cde6d8e6cca7fc9c417963f2bec37bd27d1"
+    sha256 cellar: :any_skip_relocation, ventura:       "701242613095b28406bc2dcd5f653125ea8a9e179468c8940eb18d78dddf58b0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "34f7387f62bded778a26334cc6c1fd29f4be08ece33d63f4db3711023bcff556"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6852141de082462837b4b5c6990a9a059c8987b7a48ad68a561c9011f08cafde"
   end
 
   depends_on "bash" => :build
