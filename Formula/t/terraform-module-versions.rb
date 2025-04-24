f18b4e78d0abca11f2b3345e6d48659fee0d3fe4
Rui Chen <rui@chenrui.dev>
2025-04-23 23:49:50 -0400

    terraform-module-versions 3.3.10 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/terraform-module-versions.rb
@@ -0,0 +1,21 @@
+class TerraformModuleVersions < Formula
+  desc "CLI that checks Terraform code for module updates"
+  homepage "https://github.com/keilerkonzept/terraform-module-versions"
+  url "https://github.com/keilerkonzept/terraform-module-versions/archive/refs/tags/v3.3.10.tar.gz"
+  sha256 "c84e947c26741e4c95d9c0e0a5e7d01d41ebcdf7bbb85b0106f1013b08e20b05"
+  license "MIT"
+  head "https://github.com/keilerkonzept/terraform-module-versions.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/terraform-module-versions version")
+
+    assert_match "TYPE", shell_output("#{bin}/terraform-module-versions list")
+    assert_match "UPDATE?", shell_output("#{bin}/terraform-module-versions check")
+  end
+end
