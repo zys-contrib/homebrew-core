3324ab4a04379006ab5d1e742408f3ac09e9c266
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-24 11:48:17 +0000

    npm-check-updates: update 18.0.1 bottle.

--- Formula/n/npm-check-updates.rb
+++ Formula/n/npm-check-updates.rb
@@ -6,13 +6,13 @@ class NpmCheckUpdates < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dacdd8a71e1acab1a0143ffbf38273587f48e381ff42a0e44b06b8e6fc654bd8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dacdd8a71e1acab1a0143ffbf38273587f48e381ff42a0e44b06b8e6fc654bd8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "dacdd8a71e1acab1a0143ffbf38273587f48e381ff42a0e44b06b8e6fc654bd8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e661cbfcd1f5639f8d5b6fe456477e3312d296e958a86b185ce980049ac2e09c"
-    sha256 cellar: :any_skip_relocation, ventura:       "e661cbfcd1f5639f8d5b6fe456477e3312d296e958a86b185ce980049ac2e09c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dacdd8a71e1acab1a0143ffbf38273587f48e381ff42a0e44b06b8e6fc654bd8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dacdd8a71e1acab1a0143ffbf38273587f48e381ff42a0e44b06b8e6fc654bd8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "488928229409efa74b0cb47e1b91fe2b7e907471276f3a852e6f5982062a0abe"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "488928229409efa74b0cb47e1b91fe2b7e907471276f3a852e6f5982062a0abe"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "488928229409efa74b0cb47e1b91fe2b7e907471276f3a852e6f5982062a0abe"
+    sha256 cellar: :any_skip_relocation, sonoma:        "26edec4b36c15b232b4e30e205cd6aa938dd474e94b4c41f781e47ab46d1939d"
+    sha256 cellar: :any_skip_relocation, ventura:       "26edec4b36c15b232b4e30e205cd6aa938dd474e94b4c41f781e47ab46d1939d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "488928229409efa74b0cb47e1b91fe2b7e907471276f3a852e6f5982062a0abe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "488928229409efa74b0cb47e1b91fe2b7e907471276f3a852e6f5982062a0abe"
   end
 
   depends_on "node"
