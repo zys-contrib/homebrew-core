5892c43df4c77437c172e4dc60b3b3d05bc2f782
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-25 03:27:34 +0000

    go-md2man: update 2.0.7 bottle.

--- Formula/g/go-md2man.rb
+++ Formula/g/go-md2man.rb
@@ -7,13 +7,13 @@ class GoMd2man < Formula
   head "https://github.com/cpuguy83/go-md2man.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "386385f734e6980cd796ef8c730ce0a24cda6258d62583f63adbb4770b5a246c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "386385f734e6980cd796ef8c730ce0a24cda6258d62583f63adbb4770b5a246c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "386385f734e6980cd796ef8c730ce0a24cda6258d62583f63adbb4770b5a246c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3f403e810b84edebc51c4f8e23f6a9f944e7259df273976f8bce39c0f2bf704f"
-    sha256 cellar: :any_skip_relocation, ventura:       "3f403e810b84edebc51c4f8e23f6a9f944e7259df273976f8bce39c0f2bf704f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "899178bcab4903339128975a40bd8b5342f55036fab9db92bdcce8ea52765fa3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b560a2b606a1bb4065ec10a33dad917042b3cbd182e5bd900434d9555135b5cf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "88fbded5a27da6f0f25f33b129b3164a0c4509f49c635c56f5154c3503cef78b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "88fbded5a27da6f0f25f33b129b3164a0c4509f49c635c56f5154c3503cef78b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "88fbded5a27da6f0f25f33b129b3164a0c4509f49c635c56f5154c3503cef78b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "001d4ae9334dbaffa5b3d40beb39a21397ceb816f60a458c765b034bff064870"
+    sha256 cellar: :any_skip_relocation, ventura:       "001d4ae9334dbaffa5b3d40beb39a21397ceb816f60a458c765b034bff064870"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fa96d601898022e5d295bb25b910340e3a0be8353372480cc6415a38d677f471"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a1f544ec626911acd8c1572b41f2de2b97c1c2f49d0c6bcf85f31f12654ab3f"
   end
 
   depends_on "go" => :build
