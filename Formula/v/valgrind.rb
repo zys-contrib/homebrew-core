a322da64a7dca56f65de1ddb8e08cca9aeb27c4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-25 14:17:44 +0000

    valgrind 3.25.0

--- Formula/v/valgrind.rb
+++ Formula/v/valgrind.rb
@@ -1,8 +1,8 @@
 class Valgrind < Formula
   desc "Dynamic analysis tools (memory, debug, profiling)"
   homepage "https://www.valgrind.org/"
-  url "https://sourceware.org/pub/valgrind/valgrind-3.24.0.tar.bz2"
-  sha256 "71aee202bdef1ae73898ccf7e9c315134fa7db6c246063afc503aef702ec03bd"
+  url "https://sourceware.org/pub/valgrind/valgrind-3.25.0.tar.bz2"
+  sha256 "295f60291d6b64c0d90c1ce645634bdc5361d39b0c50ecf9de6385ee77586ecc"
   license "GPL-2.0-or-later"
 
   livecheck do
