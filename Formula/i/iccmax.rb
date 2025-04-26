998c3d5e3e3ccb327fc6b6efefeee381636167dc
David Hoyt <h02332@gmail.com>
2025-04-26 15:57:48 -0400

    iccmax 2.1.26 (new formula)
    Fix: Update desc field to align with Governance
    Fix: Align Cmake Config with PR Request
    Co-Authored-By: Sean Molenaar <SMillerDev@users.noreply.github.com>

---
+++ Formula/i/iccmax.rb
@@ -0,0 +1,40 @@
+class Iccmax < Formula
+  desc "Demonstration Implementation for iccMAX color profiles"
+  homepage "https://github.com/InternationalColorConsortium/DemoIccMAX"
+  url "https://github.com/InternationalColorConsortium/DemoIccMAX/archive/refs/tags/v2.1.26.tar.gz"
+  sha256 "e3bff2e0e7876faebe4a2097eefa2a190325bcc04c152ef470449f0c01b41fa7"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "nlohmann-json"
+  depends_on "wxwidgets"
+
+  uses_from_macos "libxml2"
+
+  def install
+    args = %W[
+      -DCMAKE_INSTALL_RPATH=#{opt_lib}
+      -DENABLE_TOOLS=ON
+      -DENABLE_SHARED_LIBS=ON
+      -DENABLE_INSTALL_RIM=ON
+      -DENABLE_ICCXML=ON
+    ]
+
+    system "cmake", "-S", "Build/Cmake", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "Testing/Calc/CameraModel.xml"
+  end
+
+  test do
+    system bin/"iccFromXml", pkgshare/"CameraModel.xml", "output.icc"
+    assert_path_exists testpath/"output.icc"
+
+    system bin/"iccToXml", "output.icc", "output.xml"
+    assert_path_exists testpath/"output.xml"
+  end
+end
