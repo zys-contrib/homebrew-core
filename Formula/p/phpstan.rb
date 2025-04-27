a38ed1ead96535c30ef26b220164b41780fd45ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-27 12:58:42 +0000

    phpstan: update 2.1.13 bottle.

--- Formula/p/phpstan.rb
+++ Formula/p/phpstan.rb
@@ -6,13 +6,13 @@ class Phpstan < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "095482e1aaf5af9bd8ebcb440b343b8efb8ac14883bb61f425df39174a499179"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "095482e1aaf5af9bd8ebcb440b343b8efb8ac14883bb61f425df39174a499179"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "095482e1aaf5af9bd8ebcb440b343b8efb8ac14883bb61f425df39174a499179"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2a55633b472a672badde28049302743578ab5a98f98b781682f2d7844850bb06"
-    sha256 cellar: :any_skip_relocation, ventura:       "2a55633b472a672badde28049302743578ab5a98f98b781682f2d7844850bb06"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "eeee5c8fd8f1e737aba47a984355c057770fbd0a4bb0ed2994bdf3b86c1b56ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eeee5c8fd8f1e737aba47a984355c057770fbd0a4bb0ed2994bdf3b86c1b56ce"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e156a6540c77b4ec49deaad69dbd5d652284dd563ac28d5a3749241309718a20"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e156a6540c77b4ec49deaad69dbd5d652284dd563ac28d5a3749241309718a20"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e156a6540c77b4ec49deaad69dbd5d652284dd563ac28d5a3749241309718a20"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a55e3bf0faa270e9f436f0fbf9da320657903ccd4653d16d63b554d067032291"
+    sha256 cellar: :any_skip_relocation, ventura:       "a55e3bf0faa270e9f436f0fbf9da320657903ccd4653d16d63b554d067032291"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9ad25c79c2ed7e12f73c183e87c8c337ef350b7b7eb948cddcb5dc7a93130fc4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9ad25c79c2ed7e12f73c183e87c8c337ef350b7b7eb948cddcb5dc7a93130fc4"
   end
 
   depends_on "php" => :test
