bc3adc9db0e4c2f8370f1029633ed9133d041e60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-27 09:07:08 +0000

    gradle-profiler: update 0.22.0 bottle.

--- Formula/g/gradle-profiler.rb
+++ Formula/g/gradle-profiler.rb
@@ -11,7 +11,8 @@ class GradleProfiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "768c3986e2042d8f35fe954ea0951d91a5aa7c53cca1013361f91f78321c0aec"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "4fdec547a78284921b7ccb437e045ec11fa0d0b16cf4959fbd2ddd8081209498"
   end
 
   depends_on "openjdk"
