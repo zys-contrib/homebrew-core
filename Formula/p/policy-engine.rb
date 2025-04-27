399c6195ef14b914192b98b326632c9ad30d997d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-27 22:35:33 +0000

    policy-engine: add 0.33.3 bottle.

--- Formula/p/policy-engine.rb
+++ Formula/p/policy-engine.rb
@@ -6,6 +6,15 @@ class PolicyEngine < Formula
   license "Apache-2.0"
   head "https://github.com/snyk/policy-engine.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "004a49eb32e9601864fa2abe9c9b24b583d33b6d312a48410a4700bf8e0f879f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "004a49eb32e9601864fa2abe9c9b24b583d33b6d312a48410a4700bf8e0f879f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "004a49eb32e9601864fa2abe9c9b24b583d33b6d312a48410a4700bf8e0f879f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d25b892684455ac1aac97f56b0eb41bf1086dc61aa9c840c3a0ce3bc6192fef6"
+    sha256 cellar: :any_skip_relocation, ventura:       "d25b892684455ac1aac97f56b0eb41bf1086dc61aa9c840c3a0ce3bc6192fef6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f117fbb9c6c7a90dd188f198efebbd9f03b2e0e00a11d01a9eab3ddea703d29a"
+  end
+
   depends_on "go" => :build
 
   def install
