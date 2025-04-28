e212f8b867aec7abda616cc603d1cdd7dee137cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-28 00:01:39 +0000

    unoserver: add 3.2 bottle.

--- Formula/u/unoserver.rb
+++ Formula/u/unoserver.rb
@@ -7,6 +7,10 @@ class Unoserver < Formula
   sha256 "331136b3f0eca52a36e723e2b99f2795f27c8b6d9d8c096ed4ef7ae33cbdf3d2"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7b161b4609c4c19868eb82bf7fd428deb260633e2a70c2929df1584a401f1a0c"
+  end
+
   depends_on "python@3.13"
 
   def install
