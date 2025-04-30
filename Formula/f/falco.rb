fe3d0e5bacaf41a633ec76ef41e9e368688d0897
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-30 02:34:06 +0000

    falco 1.15.3

--- Formula/f/falco.rb
+++ Formula/f/falco.rb
@@ -1,8 +1,8 @@
 class Falco < Formula
   desc "VCL parser and linter optimized for Fastly"
   homepage "https://github.com/ysugimoto/falco"
-  url "https://github.com/ysugimoto/falco/archive/refs/tags/v1.15.2.tar.gz"
-  sha256 "5b4e1a76ae3c5d1cfc2127d22703a661523f605dec511578477f13dd77592579"
+  url "https://github.com/ysugimoto/falco/archive/refs/tags/v1.15.3.tar.gz"
+  sha256 "9b96e4c891a6715df11078c05e9624bc8ab29b63d7bb9e0c23c197b8ff0ae62a"
   license "MIT"
   head "https://github.com/ysugimoto/falco.git", branch: "main"
 
