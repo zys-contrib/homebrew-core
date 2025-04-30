c1308ce0324e1bf245274b19f8f16ec5d14b1a4d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-30 12:22:31 +0000

    chroma: update 2.17.2 bottle.

--- Formula/c/chroma.rb
+++ Formula/c/chroma.rb
@@ -7,12 +7,12 @@ class Chroma < Formula
   head "https://github.com/alecthomas/chroma.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a26687f1e14bb55d720f4f41b184ad528dbd659c738d5234dc39d9764204a3f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1a26687f1e14bb55d720f4f41b184ad528dbd659c738d5234dc39d9764204a3f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "1a26687f1e14bb55d720f4f41b184ad528dbd659c738d5234dc39d9764204a3f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5e74cdfab7f5cd5a57a8f98eb4db383df4ff83bded68960ddc5514b8daa0423e"
-    sha256 cellar: :any_skip_relocation, ventura:       "5e74cdfab7f5cd5a57a8f98eb4db383df4ff83bded68960ddc5514b8daa0423e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f8a4e6eac5c771d9f8600c00dc23ac109455550331ad6aa7299c42bf1f432f0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe4077b698556a4189b14bedbdd6db1c32c89dc9cbe527ac85144d4e7d3d6b99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fe4077b698556a4189b14bedbdd6db1c32c89dc9cbe527ac85144d4e7d3d6b99"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fe4077b698556a4189b14bedbdd6db1c32c89dc9cbe527ac85144d4e7d3d6b99"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9dce961501e8f3b80dabdd38d6d00a36cfa300cb5f318e3bd889b4ef9e04a85e"
+    sha256 cellar: :any_skip_relocation, ventura:       "9dce961501e8f3b80dabdd38d6d00a36cfa300cb5f318e3bd889b4ef9e04a85e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac754dad4d1cd43333a5cb0e243e58980d502ff2819a7a5c10f415b456760dae"
   end
 
   depends_on "go" => :build
