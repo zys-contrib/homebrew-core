cab56c6a908292edda2ab47a82d16fc060ed8030
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-02 14:29:07 +0000

    gtree: update 1.11.6 bottle.

--- Formula/g/gtree.rb
+++ Formula/g/gtree.rb
@@ -7,13 +7,13 @@ class Gtree < Formula
   head "https://github.com/ddddddO/gtree.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f1eae0a9d240eba30b6d8e5eb44fc7c8c1189175239dc622203d208fcfe5fbde"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1eae0a9d240eba30b6d8e5eb44fc7c8c1189175239dc622203d208fcfe5fbde"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f1eae0a9d240eba30b6d8e5eb44fc7c8c1189175239dc622203d208fcfe5fbde"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9f719ef4cac83d1364ce970e46cc0f0bda1de888f1cce23e950f2a41eec2b9ca"
-    sha256 cellar: :any_skip_relocation, ventura:       "9f719ef4cac83d1364ce970e46cc0f0bda1de888f1cce23e950f2a41eec2b9ca"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2da025a6020a3676d11190f75c315a9924c8bd5fafbfdbb0aafa04aed6ed8814"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "389fa5d2152af9f330ab55f0830d022e9b7157445229a360fa8d31e87d80ddd3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b978a7cb8d1d14fab89a3799e6bcd1a8c38278e4424275fc3ae79ba3a138e235"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b978a7cb8d1d14fab89a3799e6bcd1a8c38278e4424275fc3ae79ba3a138e235"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b978a7cb8d1d14fab89a3799e6bcd1a8c38278e4424275fc3ae79ba3a138e235"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4627a727d891901de873e80210407631af21dd3ca3235905d59a1d27860ccc69"
+    sha256 cellar: :any_skip_relocation, ventura:       "4627a727d891901de873e80210407631af21dd3ca3235905d59a1d27860ccc69"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1b5a627e1f44d9403b2cd46f93dccd4d1de50fec4f841635b0ebbc80a5973fa7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "650440dba22014109f5e1d03ae3a787ad90e34bfb4bf6926d6d5c83322bfec40"
   end
 
   depends_on "go" => :build
