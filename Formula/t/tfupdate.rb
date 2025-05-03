5ecc6e09f0f95dfd0e51059a76be2494ef88fdc3
Rui Chen <rui@chenrui.dev>
2025-05-03 19:51:04 +0000

    tfupdate: update 0.9.0 bottle.

--- Formula/t/tfupdate.rb
+++ Formula/t/tfupdate.rb
@@ -7,14 +7,12 @@ class Tfupdate < Formula
   head "https://github.com/minamijoyo/tfupdate.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "ad947166ddc07f1a1199cb2b5e132e15b0fc20c802f23ebda399c01d7ba5129f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bcc8a2c59fef1a74eef87ea859df9f70c963e9b10a1781bc7c937579e7280155"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3acd595d6fcaac1619d7d2fd7ee07dcc18b1e53262af0e186484bb5eef8cb1fc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6ebf5cb8df8456456896bd581776c1543aa6b49697a0b6e07e9f9e3ef0f816e0"
-    sha256 cellar: :any_skip_relocation, sonoma:         "62588731be0bd37120b14168ed3e345c3d2fdbaa1a71d57489ad1c60f145cd97"
-    sha256 cellar: :any_skip_relocation, ventura:        "98a4c0e490801db1415de507a10140c85f0e4543d9d0651b827608b3fc25be0c"
-    sha256 cellar: :any_skip_relocation, monterey:       "4da185a6ee55f78e281677558eaab1ebe05d2e615c4c72023d4ad924ad113da5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "23d034a9be35da9cc453837166f3ffcc5b667e370555a05c9585b2f46869acfb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "758383e3b5dc2d5986f94bfd918454622c952950d1bf80bdba7d389dbf854859"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "758383e3b5dc2d5986f94bfd918454622c952950d1bf80bdba7d389dbf854859"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "758383e3b5dc2d5986f94bfd918454622c952950d1bf80bdba7d389dbf854859"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a97263cf53d5059a724cb3825ef2ef9a7b497aab9d90828786500911aa786f6f"
+    sha256 cellar: :any_skip_relocation, ventura:       "a97263cf53d5059a724cb3825ef2ef9a7b497aab9d90828786500911aa786f6f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a9a59de4820aed2f0c7b73a3674446d027c1f1e28ca6fea036ca7670f5023713"
   end
 
   depends_on "go" => :build
