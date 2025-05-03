71e2631cbb0c107cd93d83e1e1026a53efa61be4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-03 14:35:24 +0000

    miniflux: add 2.2.8 bottle.

--- Formula/m/miniflux.rb
+++ Formula/m/miniflux.rb
@@ -5,6 +5,15 @@ class Miniflux < Formula
   sha256 "1c360012c3dd0202601db26755c511969b6c527f96d6829861a74095633a35c2"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1e0d27dd1fef8dcdcd23598be51cc9819419419c84b62a62bf0b97d28db08b70"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45f578e2d4597071549c305ffeced5dc3e364ff171c28d365d253ec891e32bcd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "042c119283d08ce163e03ed113287b3da869ec3c2f9dd9b29d553b2b2c9155ab"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1697fb84642eeaed22e169595f8ae33fe18126d7d8121ad9278eaebff95b7137"
+    sha256 cellar: :any_skip_relocation, ventura:       "39e5e3f7701a0bb7dff79506a00c95958e79b0afd0c02f8c857cc3918e820305"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "be8b87cc40ab4b80182e3041936b2cf60de72623149b6cd216f3e5eeeff2c592"
+  end
+
   depends_on "go" => :build
   depends_on "postgresql@17" => :test
 
