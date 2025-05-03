4d35e0a1d9fb297652f0c8939a61a90fb2354d57
Daeho Ro <lamanus@outlook.kr>
2025-05-03 22:30:24 +0900

    fastnetmon: revision bump (hiredis 1.3.0)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.8.tar.gz"
   sha256 "d16901b00963f395241c818d02ad2751f14e33fd32ed3cb3011641ab680e0d01"
   license "GPL-2.0-only"
-  revision 3
+  revision 4
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "f1b311248b2ad6501f7bdebdc02d89c4547b48b16eaf71abbda723f09c06c1bb"
