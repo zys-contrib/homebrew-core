f249992ac196b1a1f23d4ef947e159c5c3bfbf1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 23:29:43 +0000

    git-credential-oauth: update 0.15.1 bottle.

--- Formula/g/git-credential-oauth.rb
+++ Formula/g/git-credential-oauth.rb
@@ -7,12 +7,12 @@ class GitCredentialOauth < Formula
   head "https://github.com/hickford/git-credential-oauth.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92b613c68a1fa516f514863a65d773234f3171dddb631a929b52436dd2a15ae6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92b613c68a1fa516f514863a65d773234f3171dddb631a929b52436dd2a15ae6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "92b613c68a1fa516f514863a65d773234f3171dddb631a929b52436dd2a15ae6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "458e6e4882554340e19783fe94977f9bcad2b72aedb7cf0a8247b11226d44cbe"
-    sha256 cellar: :any_skip_relocation, ventura:       "458e6e4882554340e19783fe94977f9bcad2b72aedb7cf0a8247b11226d44cbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ba0fc4b2072645c4b158dc583b8f1af9580d678c87678547ef7e6088a69be812"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80a29ec6e73aac63a5438c5f5aadfdb76f5255995b9f7074592033645bc286df"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "80a29ec6e73aac63a5438c5f5aadfdb76f5255995b9f7074592033645bc286df"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "80a29ec6e73aac63a5438c5f5aadfdb76f5255995b9f7074592033645bc286df"
+    sha256 cellar: :any_skip_relocation, sonoma:        "198d17566ed638fcc048dc8192d9d6207d870c113695f9f55b6a1bfba5e807b8"
+    sha256 cellar: :any_skip_relocation, ventura:       "198d17566ed638fcc048dc8192d9d6207d870c113695f9f55b6a1bfba5e807b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b64f14ad4f84b3c3d6c876a61b6e81197824c76d27c8764229b92032be20ea09"
   end
 
   depends_on "go" => :build
