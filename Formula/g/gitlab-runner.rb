50b99795d57e5e967f706994ac0a5659a7e4529f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 11:07:29 +0000

    gitlab-runner 17.11.1

--- Formula/g/gitlab-runner.rb
+++ Formula/g/gitlab-runner.rb
@@ -2,8 +2,8 @@ class GitlabRunner < Formula
   desc "Official GitLab CI runner"
   homepage "https://gitlab.com/gitlab-org/gitlab-runner"
   url "https://gitlab.com/gitlab-org/gitlab-runner.git",
-      tag:      "v17.11.0",
-      revision: "0f67ff190a5f069ef1e46f2a8950e1319e71d6e4"
+      tag:      "v17.11.1",
+      revision: "96856197176687bb5744a7562cc65130b32d4005"
   license "MIT"
   head "https://gitlab.com/gitlab-org/gitlab-runner.git", branch: "main"
 
