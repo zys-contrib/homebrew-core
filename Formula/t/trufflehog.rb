3a2ecbca1db2599cd44990d2bb32c21ffdb1b4c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 21:18:07 +0000

    trufflehog: update 3.88.28 bottle.

--- Formula/t/trufflehog.rb
+++ Formula/t/trufflehog.rb
@@ -8,13 +8,13 @@ class Trufflehog < Formula
   head "https://github.com/trufflesecurity/trufflehog.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45b4ec226a939d46644b184851829af78e11d5c2d8287c2d6d4becedbc315092"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb6febb332d11b1b1143a319a423045bd4648a7291235cd806d98088c9be28b1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "104b7c48ecd33001e9c63e3a9a1d6b9d4b67d1ed0b9dfd7f4b442c45c0a4a13b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2604a26501df7deb65d6360c041618f2573ca2190e41018d1e71b152478ca4f9"
-    sha256 cellar: :any_skip_relocation, ventura:       "d3ebc68f305c31fa1a7e4f49688a33b6c668cba9bb685d2448f8bceeb06506e0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "62f52262c87e8a6c453a9925cd1ca8080c314a8d1ba2dea66b722448e45e5198"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8dc4b0e0fe5a532379fa9f5484b9f2687224451cf61b74de1e3a1b3e351a760"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7216a3749eb0b7f89958981fc72f2892afe743e58d36c1193b441d24ceff13d1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89369f52f0b833147b9a06b49f2a7f372b0627d1f7ae72c29c5bb320e41d23f2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e81976b50f2945b90fae4418266449fceb6fc8f47ee3397f48db78d07558444c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "008855b7ef83a653e888e18ac785da6966ad518dc51ba0dc3ec562f2e723e905"
+    sha256 cellar: :any_skip_relocation, ventura:       "9b9d2852220ac51123adab0663e0601b1a3f348f2bc3e50416893ff034f65116"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2fd9c3c90443b7b5fdbecab633089bad640c5b1d16667981d1f2e11f20e40344"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "441bbfc73f7287253a6413c05ccea5fd65d445e89ee45f16982df2876cce7ab8"
   end
 
   depends_on "go" => :build
