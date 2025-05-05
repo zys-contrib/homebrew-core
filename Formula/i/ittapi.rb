2866be5f563ec27630c1a16cf8e7b0c2dbedfa1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 18:10:58 +0000

    ittapi: update 3.26.0 bottle.

--- Formula/i/ittapi.rb
+++ Formula/i/ittapi.rb
@@ -7,13 +7,13 @@ class Ittapi < Formula
   head "https://github.com/intel/ittapi.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "956b0c9fe7993a46da3a49133a88af419e547c3d821cf2bce98e6c9e4ed6c30a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f0e12409a1ba59ae6e673fc70a509c4a74cb6f76864eaef461ab919d81398f2e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "77730e201944e4ad0d74edc0ce6d074a9f8cb66398c4cbddedc27ecf087c0bda"
-    sha256 cellar: :any_skip_relocation, sonoma:        "929cc096155ade33af92e3e59d2a8fe142630db805a9b455413380fdcc236674"
-    sha256 cellar: :any_skip_relocation, ventura:       "6d289436aa1ddaf4e73c133ee11ee6ebdcaa8b041d58255f60d2276aa5b72406"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d4896546019f70102efca37b44eb966c5cbabaa77c72ed4b5694d650a21862fe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f362d43b190473d8fe34bb58be9c6204df811bb2963cc816e6966755b8bc3d4d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "69ac3f71c63082ee64291bcbece9ade2f5f9f718c00185b751e9c1ed901656c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb23a4b5b8e76362acb9bf650509fea5392d343c54c37e46ad65d4ae3bdce950"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7b51adc5f3f4c3458053a90e88c9fd073ea020744a14e332d8a4b24dec1cb318"
+    sha256 cellar: :any_skip_relocation, sonoma:        "922f6a5ccd3c7346eb4a8531a3eaa9d6ee161e9fb85da5b2862655938a2a2aef"
+    sha256 cellar: :any_skip_relocation, ventura:       "d1547116e8db91615040dc09341158ba029574ed842b8ebc7e086fa2e209a715"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bf5f2d6a7263bd302fca6f0b394165213586fa87228596f8ce7ab8bf529a30a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e406047ef68498e5318abdf057acfab785a0facee8064f2e1c4d736d1154c9ad"
   end
 
   depends_on "cmake" => :build
