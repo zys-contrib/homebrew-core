d045b0f001a95675426b45eb9c839dfe37ca491d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 17:12:58 +0000

    ittapi 3.26.0

--- Formula/i/ittapi.rb
+++ Formula/i/ittapi.rb
@@ -1,8 +1,8 @@
 class Ittapi < Formula
   desc "Intel Instrumentation and Tracing Technology (ITT) and Just-In-Time (JIT) API"
   homepage "https://github.com/intel/ittapi"
-  url "https://github.com/intel/ittapi/archive/refs/tags/v3.25.5.tar.gz"
-  sha256 "2d19243e7ac8a7de08bfd005429a308c1db52a18e5b7b66d29a6c19f066946e3"
+  url "https://github.com/intel/ittapi/archive/refs/tags/v3.26.0.tar.gz"
+  sha256 "3127dec3046dc1a90d03cc7d98d95beeec63696c4e57ef9e793df8330f54721b"
   license "GPL-2.0-only"
   head "https://github.com/intel/ittapi.git", branch: "master"
 
