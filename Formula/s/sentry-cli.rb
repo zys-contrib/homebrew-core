68546056a808be7bbaa63c65a58f617943b1e116
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-05 11:35:43 +0000

    sentry-cli 2.44.0

--- Formula/s/sentry-cli.rb
+++ Formula/s/sentry-cli.rb
@@ -1,8 +1,8 @@
 class SentryCli < Formula
   desc "Command-line utility to interact with Sentry"
   homepage "https://docs.sentry.io/cli/"
-  url "https://github.com/getsentry/sentry-cli/archive/refs/tags/2.43.1.tar.gz"
-  sha256 "9fcae5a4426660a8153932bac7b9760567a172dfa8b15898c5a5d89dfed1a7f5"
+  url "https://github.com/getsentry/sentry-cli/archive/refs/tags/2.44.0.tar.gz"
+  sha256 "c671816898cde99ddbb9d09a47681b98d26f8adbe5cec63695edadcef4cd6b65"
   license "BSD-3-Clause"
   head "https://github.com/getsentry/sentry-cli.git", branch: "master"
 
