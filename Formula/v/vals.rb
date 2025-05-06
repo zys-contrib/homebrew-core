2b6e304d597eddd727c33408c658299d64e2b915
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 08:34:11 +0000

    vals 0.41.0

--- Formula/v/vals.rb
+++ Formula/v/vals.rb
@@ -1,8 +1,8 @@
 class Vals < Formula
   desc "Helm-like configuration values loader with support for various sources"
   homepage "https://github.com/helmfile/vals"
-  url "https://github.com/helmfile/vals/archive/refs/tags/v0.40.1.tar.gz"
-  sha256 "3eb7a45d02f2dba39418ac44bec48069b23c5dc14b358c14a0e857abaf3b4117"
+  url "https://github.com/helmfile/vals/archive/refs/tags/v0.41.0.tar.gz"
+  sha256 "aa1133fc97a3ee75de0c7e3ace19324f7ab1655296e017f53aac9cdde9f5c759"
   license "Apache-2.0"
   head "https://github.com/helmfile/vals.git", branch: "main"
 
