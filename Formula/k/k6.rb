fa29da3f0ef3da03eaf4c1d3606d78dfd3d91f5e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 14:27:09 +0000

    k6: update 1.0.0 bottle.

--- Formula/k/k6.rb
+++ Formula/k/k6.rb
@@ -7,12 +7,12 @@ class K6 < Formula
   head "https://github.com/grafana/k6.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "995d62c03ac9a167a8bdc4a2e50bc785f8465223776d66e068c7071d8a7515be"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "995d62c03ac9a167a8bdc4a2e50bc785f8465223776d66e068c7071d8a7515be"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "995d62c03ac9a167a8bdc4a2e50bc785f8465223776d66e068c7071d8a7515be"
-    sha256 cellar: :any_skip_relocation, sonoma:        "44bb4d7152b3e5ce2d77515cde59ecc693979a9c9dac41a7479b8d07a4774bc0"
-    sha256 cellar: :any_skip_relocation, ventura:       "44bb4d7152b3e5ce2d77515cde59ecc693979a9c9dac41a7479b8d07a4774bc0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "353917b8a8fa1a2a38549cbbf37e1bab712139f5b3572f3d75ee82733f62addf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "38ad23f362dcc33ac2b99cc9c5c50a2f8bd46bea7bd79ca32818584f381776b0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "38ad23f362dcc33ac2b99cc9c5c50a2f8bd46bea7bd79ca32818584f381776b0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "38ad23f362dcc33ac2b99cc9c5c50a2f8bd46bea7bd79ca32818584f381776b0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cf258efa6b268478028b8e1cf2bdc902dafb3d32079fb47602897256a74b72cb"
+    sha256 cellar: :any_skip_relocation, ventura:       "cf258efa6b268478028b8e1cf2bdc902dafb3d32079fb47602897256a74b72cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a23290c9ad12400f796b7a0028190c78938b932610204c3d7d33ad1002850610"
   end
 
   depends_on "go" => :build
