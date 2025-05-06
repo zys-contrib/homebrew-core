0dd2121af6c05186e2d0f6fb708e23c04d594995
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 06:17:25 +0000

    dbml-cli: update 3.13.4 bottle.

--- Formula/d/dbml-cli.rb
+++ Formula/d/dbml-cli.rb
@@ -6,13 +6,13 @@ class DbmlCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b5e2f722159e5bb2f356e1f6ca9c8217d7a3696eace5c4a2db14e0c1dc51c795"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b5e2f722159e5bb2f356e1f6ca9c8217d7a3696eace5c4a2db14e0c1dc51c795"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b5e2f722159e5bb2f356e1f6ca9c8217d7a3696eace5c4a2db14e0c1dc51c795"
-    sha256 cellar: :any_skip_relocation, sonoma:        "237cf53dc5b68674f87055134bd9a2757e41cb25e362e9eed131f697102804f5"
-    sha256 cellar: :any_skip_relocation, ventura:       "237cf53dc5b68674f87055134bd9a2757e41cb25e362e9eed131f697102804f5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b5e2f722159e5bb2f356e1f6ca9c8217d7a3696eace5c4a2db14e0c1dc51c795"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b5e2f722159e5bb2f356e1f6ca9c8217d7a3696eace5c4a2db14e0c1dc51c795"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "634e925fef3d7224d5f50536307573363614739d6c123bd59b93b0982d486c7e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "634e925fef3d7224d5f50536307573363614739d6c123bd59b93b0982d486c7e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "634e925fef3d7224d5f50536307573363614739d6c123bd59b93b0982d486c7e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7bc879cb4c67a28f0f5b8e9c2e06300533f713d25925541c5f31a8d694542206"
+    sha256 cellar: :any_skip_relocation, ventura:       "7bc879cb4c67a28f0f5b8e9c2e06300533f713d25925541c5f31a8d694542206"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "634e925fef3d7224d5f50536307573363614739d6c123bd59b93b0982d486c7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "634e925fef3d7224d5f50536307573363614739d6c123bd59b93b0982d486c7e"
   end
 
   depends_on "node"
