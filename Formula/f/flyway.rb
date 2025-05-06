957c05f6eaeb21c79e4357b7779fc678a662c5ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 14:10:30 +0000

    flyway 11.8.1

--- Formula/f/flyway.rb
+++ Formula/f/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/11.8.0/flyway-commandline-11.8.0.tar.gz"
-  sha256 "738893be4aa295dfb56bf8d772cf7d626abe7a0b70c51c81f41461de21c1718f"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/11.8.1/flyway-commandline-11.8.1.tar.gz"
+  sha256 "df6f3e54db74eabfc22ad4a7b7735cca6039d036ceb438287204502861fb9602"
   license "Apache-2.0"
 
   livecheck do
