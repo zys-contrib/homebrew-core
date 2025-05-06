46c1519712053b20a18b1b0283840f4a409a68ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 20:25:12 +0000

    pkgx 2.7

--- Formula/p/pkgx.rb
+++ Formula/p/pkgx.rb
@@ -1,8 +1,8 @@
 class Pkgx < Formula
   desc "Standalone binary that can run anything"
   homepage "https://pkgx.sh"
-  url "https://github.com/pkgxdev/pkgx/archive/refs/tags/v2.6.tar.gz"
-  sha256 "770944fd946fef2e365d18568335483156ceefa4950ffb9d7b8f98dd2f3fe12e"
+  url "https://github.com/pkgxdev/pkgx/archive/refs/tags/v2.7.tar.gz"
+  sha256 "479897abc6d51df25bda10b98ab6efef8424cb14bfd3003148d55216e6ca353b"
   license "Apache-2.0"
   head "https://github.com/pkgxdev/pkgx.git", branch: "main"
 
