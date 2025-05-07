5a58b7b2cfc3b2aa92a53891b1fc4879aa27af36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-07 11:14:23 +0000

    kubetail 0.5.1

--- Formula/k/kubetail.rb
+++ Formula/k/kubetail.rb
@@ -1,8 +1,8 @@
 class Kubetail < Formula
   desc "Logging tool for Kubernetes with a real-time web dashboard"
   homepage "https://www.kubetail.com/"
-  url "https://github.com/kubetail-org/kubetail/archive/refs/tags/cli/v0.5.0.tar.gz"
-  sha256 "a045541fa94e3877df581c673227d8dd6c376d341f3148185ef5b6f40f9206a1"
+  url "https://github.com/kubetail-org/kubetail/archive/refs/tags/cli/v0.5.1.tar.gz"
+  sha256 "01cb656a34d22fc60284da29b11fd5ae5496e3f0933d5d51b68d0a8ab5cf7b75"
   license "Apache-2.0"
   head "https://github.com/kubetail-org/kubetail.git", branch: "main"
 
