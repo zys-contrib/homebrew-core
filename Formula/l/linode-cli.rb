1cd9752ca084c1d8a9619faebc5a57e7ecc3d11d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-08 17:20:59 +0000

    linode-cli 5.57.1

--- Formula/l/linode-cli.rb
+++ Formula/l/linode-cli.rb
@@ -3,8 +3,8 @@ class LinodeCli < Formula
 
   desc "CLI for the Linode API"
   homepage "https://github.com/linode/linode-cli"
-  url "https://files.pythonhosted.org/packages/b8/36/48895e350da98605a6364a4b15d8ab3796399f1d203dcc40c008d4cc6e25/linode_cli-5.57.0.tar.gz"
-  sha256 "f086867b54b3310ae90cd160912456d7f07e4800a3a57f58592148ced271ed33"
+  url "https://files.pythonhosted.org/packages/4c/b8/f65c0d0c578fc5c7e0fa85aa62c5da6fb60ecc661d54672b72c01602df26/linode_cli-5.57.1.tar.gz"
+  sha256 "ed5a338ca8c1a0db36710d04f169d6563f3359d6f728796c70852d71ce0fbe8e"
   license "BSD-3-Clause"
   head "https://github.com/linode/linode-cli.git", branch: "main"
 
@@ -28,8 +28,8 @@ class LinodeCli < Formula
   end
 
   resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/16/b0/572805e227f01586461c80e0fd25d65a2115599cc9dad142fee4b747c357/charset_normalizer-3.4.1.tar.gz"
-    sha256 "44251f18cd68a75b56585dd00dae26183e102cd5e0f9f1466e6df5da2ed64ea3"
+    url "https://files.pythonhosted.org/packages/e4/33/89c2ced2b67d1c2a61c19c6751aa8902d46ce3dacb23600a283619f5a12d/charset_normalizer-3.4.2.tar.gz"
+    sha256 "5baececa9ecba31eff645232d59845c07aa030f0c81ee70184a90d35099a0e63"
   end
 
   resource "h11" do
