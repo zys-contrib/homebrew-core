bf6170b4295a3e1649e7b94d4a2f395634f8f4cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-08 05:35:38 +0000

    talhelper 3.0.24

--- Formula/t/talhelper.rb
+++ Formula/t/talhelper.rb
@@ -1,8 +1,8 @@
 class Talhelper < Formula
   desc "Configuration helper for talos clusters"
   homepage "https://budimanjojo.github.io/talhelper/latest/"
-  url "https://github.com/budimanjojo/talhelper/archive/refs/tags/v3.0.23.tar.gz"
-  sha256 "4a73ef517470f8106c3ed2dbed09d33e1bb6d2151378d38da6f0088dbfb90bbe"
+  url "https://github.com/budimanjojo/talhelper/archive/refs/tags/v3.0.24.tar.gz"
+  sha256 "39f34dd4a5d94188bacdea85362307217e56eceba92801dd9a534c19fed91fe8"
   license "BSD-3-Clause"
   head "https://github.com/budimanjojo/talhelper.git", branch: "master"
 
