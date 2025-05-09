2edb6c93a674af59ce39eafd400ae4837fc33806
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-09 01:02:07 +0000

    silk: update 3.23.4 bottle.

--- Formula/s/silk.rb
+++ Formula/s/silk.rb
@@ -11,13 +11,13 @@ class Silk < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "4ec08564f5bc0da08669fcdd552ce86a8a1208f0fb901aed2d92eed840fd1acd"
-    sha256 arm64_sonoma:  "80f77b63679bcfd3999be42d31f6da5b76656961a565e7a2464123640fb4855e"
-    sha256 arm64_ventura: "5e5d808fc887a8e6dfa50d087046c89f40683403444133ad9275ba46f9904997"
-    sha256 sonoma:        "546bd6104614d6f05e53bac28a160a6083921d7357d0fc59e7ca1c804daca899"
-    sha256 ventura:       "de93ae1b70d76f666c5a03d70741a51c910f93a4822184031e3704adb2765c2d"
-    sha256 arm64_linux:   "20367aac96d0ebcd7f9fef99ce1bb9d63936d6d197cb64ee73d67bcb69f8178f"
-    sha256 x86_64_linux:  "09cf4e1b3f3492c1245f04da0eda5fd741fa96238fe8eff9c16bd7b8c7ce1251"
+    sha256 arm64_sequoia: "b875ee67577ab96da3d7c593218e407fd8249c46aebf215bac34c2ed2318aa08"
+    sha256 arm64_sonoma:  "189211ad4ba779d31573fd9a1332760e85588ac8e5e52453d89688fb716d4931"
+    sha256 arm64_ventura: "5252fe02085962df9e3fb10042f5e801a58b71dc4f5d53bef69c585407349a2a"
+    sha256 sonoma:        "0cee6423aa6b66ece3d65aa17a1d3d9f6d22c2411d6f6f4d90fea0cf9ed6ef23"
+    sha256 ventura:       "911268aac598d09cc6815cc086345c8356b6ad1c5933ef3e4ea03b0de9da0c53"
+    sha256 arm64_linux:   "4c483da4df5cc0bee310dad7edf51fcbc40101963642e0a639ea021bdcb51437"
+    sha256 x86_64_linux:  "9423c04a5e4863ad4f29d613581d0a9b769f75d8dee19f9e83db6f15d1d0b2f1"
   end
 
   depends_on "pkgconf" => :build
