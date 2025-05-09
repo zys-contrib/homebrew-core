4c0337c25f9dd7bb7d2503b5aa9e8772e2ccf4cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-09 04:30:00 +0000

    codex: add 0.1.2504301751 bottle.

--- Formula/c/codex.rb
+++ Formula/c/codex.rb
@@ -5,6 +5,15 @@ class Codex < Formula
   sha256 "534efae4ced56e98b136a13bee4523acebf261267fecc11ea3df9a2854875186"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "902627d20eec97ccfea442b6ad5aba8b3be30b39620e248e444af2f3557e83e9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "902627d20eec97ccfea442b6ad5aba8b3be30b39620e248e444af2f3557e83e9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "902627d20eec97ccfea442b6ad5aba8b3be30b39620e248e444af2f3557e83e9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4b0bf800246962f876e5c30c65e36c8cd1aef87c62e62880957fbf5aba88bcec"
+    sha256 cellar: :any_skip_relocation, ventura:       "4b0bf800246962f876e5c30c65e36c8cd1aef87c62e62880957fbf5aba88bcec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "902627d20eec97ccfea442b6ad5aba8b3be30b39620e248e444af2f3557e83e9"
+  end
+
   depends_on "node"
 
   def install
