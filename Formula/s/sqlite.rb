91246535c3cbe47b3f4e3526df53f9d0a149593f
botantony <antonsm21@gmail.com>
2025-05-09 19:15:37 +0200

    sqlite 3.49.2
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/s/sqlite.rb
+++ Formula/s/sqlite.rb
@@ -1,9 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/index.html"
-  url "https://www.sqlite.org/2025/sqlite-autoconf-3490100.tar.gz"
-  version "3.49.1"
-  sha256 "106642d8ccb36c5f7323b64e4152e9b719f7c0215acf5bfeac3d5e7f97b59254"
+  url "https://www.sqlite.org/2025/sqlite-autoconf-3490200.tar.gz"
+  version "3.49.2"
+  sha256 "5c6d8697e8a32a1512a9be5ad2b2e7a891241c334f56f8b0fb4fc6051e1652e8"
   license "blessing"
 
   livecheck do
@@ -13,6 +13,8 @@ class Sqlite < Formula
       page.scan(regex).map { |match| match&.first&.tr("_", ".") }
     end
   end
+
+  no_autobump! because: :incompatible_version_format
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "757c3d60f4d35fe1c3336207d63992fc804fa6ddd2c40ebb84eb7bfa07bc69e1"
