b1a0366215cd32a4faf4972d33fa6c524e4b9566
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-10 15:13:39 +0000

    cpdf: add 2.8.1 bottle.

--- Formula/c/cpdf.rb
+++ Formula/c/cpdf.rb
@@ -5,6 +5,15 @@ class Cpdf < Formula
   sha256 "bdd7caf1e5e55e65e4ece96eeeb3e5894c195ca5a9a274ddc27ac50a321d5c75"
   license "AGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e97a2d112914e6eac5e522a8663deb1771ef04e8296fdf34db39558a0dadca23"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8b36ddb34c391665d5337de6b63118487263285b5917528d4c52847510af548d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "91772514307f75224ff0558fcc18999cf76cb7603851a5f94258e272bfb3a5a0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5e63b9e7612e18c9c577aa502c7be73ee534e46650c51911372ab8ec032e0b8c"
+    sha256 cellar: :any_skip_relocation, ventura:       "ede4654c045aea5a2aff498c6bd77f3ff8d284df6c7dfc5c5ed0bd491c96d188"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4bcaa237cb5ad235f05cab8067bac39d4a57aa49195462872f409195a744b4aa"
+  end
+
   depends_on "ocaml-findlib" => :build
   depends_on "camlpdf"
   depends_on "ocaml"
