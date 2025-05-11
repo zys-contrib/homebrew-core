a9deac37faa83cc7339e52833e2dee1acc18a358
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-11 14:26:27 +0000

    bkmr: update 4.22.0 bottle.

--- Formula/b/bkmr.rb
+++ Formula/b/bkmr.rb
@@ -7,13 +7,13 @@ class Bkmr < Formula
   head "https://github.com/sysid/bkmr.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1eece03b8fdfbf998c5b6fa34404db71137baf585f1e3673c9b30029baacdad0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3418423b456b8be2af7bf63de8180adb41a76d2cc4aac97fb6e491bcadcad75b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "cc7b1b1b11a0a5fda8711e9107a24eb66fa918c59c4594cc7938d81fa10d649d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3860c732d974c3afc326d0e607b6d85976151ef954ab05179d33b3922405f283"
-    sha256 cellar: :any_skip_relocation, ventura:       "5496b10cca26c85c90671193d5238fe2234054d915f4db1a3ece1397e02e29b6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b6bac12c7ba573a72b9a7e3b769d77866291fdb369408e9a6c7dce7dfeb0270d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "36201b543cd529f86a830c62d56a188331b68e0a53179d93027f1b38d466697b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb5f5c94fd33deb2797271aaa6ee63c075f4cdab24eba4c567d51029d2be174a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "66c2653ba61a6598421a1f612c858527c5ff9a0fff1115837270e2f41431e762"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ccf15e03093c4675ee573d40915144f179c47bf2a764ef14899e1af8d4995f7d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1584fcdc3ff5e6be384cc985a946728aa5b6158af411d80006444c1f3e70109b"
+    sha256 cellar: :any_skip_relocation, ventura:       "910ecd555e20c26b8bfde7259ae8db9e23865d242d9fd1395f2ea44e8e6122be"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2d62da14e3ffd2fdfdea9378088db3404049b14d10b448306af1cd0a90c430ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c7627ec66de3b2c227aa9b6f752558f854c96da7f1708567ff1834b8dd4e7b29"
   end
 
   depends_on "rust" => :build
