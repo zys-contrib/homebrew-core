8b154557ad506f91792b4b06af01ef8c2631d7fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-11 12:27:37 +0000

    nerdlog: add 1.6.0 bottle.

--- Formula/n/nerdlog.rb
+++ Formula/n/nerdlog.rb
@@ -6,6 +6,15 @@ class Nerdlog < Formula
   license "BSD-2-Clause"
   head "https://github.com/dimonomid/nerdlog.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec219169ed96ac12fc190b65bf8daee3a155e560c8294614c513e7dc1f1a58d6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "10e835b240c0168ce54b9f5a80f1efc13c5db04f6c88a5b6ac8b0b90f8204ee2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5ea1b53bd7572f06e77db17c8fe095fba4b170d07594fd1e793b554d3447a69a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "78fb88a50c663341fd0d2f5f3ea960e3abc9de83922e373d30efd795aeb4b3bb"
+    sha256 cellar: :any_skip_relocation, ventura:       "da7186e9791489f57b740e10a7ecf5fc6f99536ac77ffc046590b0309541444d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66a4ada6105c759e7f43ebe5d221c82aab4e2c26257147490c495d989df91326"
+  end
+
   depends_on "go" => :build
 
   on_linux do
