92904fd19bb2dc23b5e64376bc88cf30489afb2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-11 20:18:22 +0000

    nerdlog: update 1.7.0 bottle.

--- Formula/n/nerdlog.rb
+++ Formula/n/nerdlog.rb
@@ -7,12 +7,12 @@ class Nerdlog < Formula
   head "https://github.com/dimonomid/nerdlog.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec219169ed96ac12fc190b65bf8daee3a155e560c8294614c513e7dc1f1a58d6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "10e835b240c0168ce54b9f5a80f1efc13c5db04f6c88a5b6ac8b0b90f8204ee2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5ea1b53bd7572f06e77db17c8fe095fba4b170d07594fd1e793b554d3447a69a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "78fb88a50c663341fd0d2f5f3ea960e3abc9de83922e373d30efd795aeb4b3bb"
-    sha256 cellar: :any_skip_relocation, ventura:       "da7186e9791489f57b740e10a7ecf5fc6f99536ac77ffc046590b0309541444d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66a4ada6105c759e7f43ebe5d221c82aab4e2c26257147490c495d989df91326"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fd80b7316656c42b47eea64444d67a23b8819186d1062c5a291a8c116dc87d6d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1e2f2e14bbb04c3db860902a7e5013d0e4af458249892a9a0dcdd7ebd718d3a9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b12fcc9c5892340359ea53f1e1b3967f20032b2359ff880fe015b06fb0e13205"
+    sha256 cellar: :any_skip_relocation, sonoma:        "354ef158702f5d972d23fe40cc7c605a9b6f95f58acb7721b11187a147ea312a"
+    sha256 cellar: :any_skip_relocation, ventura:       "763e45d4e5d7d878fb53c203382930e2b2b86da8c60c4ca5beddc1a67b98acf8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "41972a9060652b355390ce90d02370aefac6c9bb743c9d61d8800f2a31b5bfae"
   end
 
   depends_on "go" => :build
