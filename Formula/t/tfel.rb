c11eaa7a91f3871faca2f4dbb2520c6e5e5df0db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-12 14:20:41 +0000

    tfel 5.0.1
    tfel: remove build patches
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tfel.rb
+++ Formula/t/tfel.rb
@@ -1,30 +1,10 @@
 class Tfel < Formula
   desc "Code generation tool dedicated to material knowledge for numerical mechanics"
   homepage "https://thelfer.github.io/tfel/web/index.html"
+  url "https://github.com/thelfer/tfel/archive/refs/tags/TFEL-5.0.1.tar.gz"
+  sha256 "820b2f9d54e237b2c2d9d6f06aaf7d4a1d3f34fb373e6549bee4fd8b55ecfad1"
   license "GPL-1.0-or-later"
-  revision 1
   head "https://github.com/thelfer/tfel.git", branch: "master"
-
-  stable do
-    url "https://github.com/thelfer/tfel/archive/refs/tags/TFEL-5.0.0.tar.gz"
-    sha256 "fe1ec39eba7f23571c2b0c773dab1cc274fee4512c5b2f2fc54b231da4502e87"
-
-    # Backport fix for https://github.com/thelfer/tfel/issues/703
-    patch do
-      url "https://github.com/thelfer/tfel/commit/c4c564ab09a7c13c87ef3628ed89d2abe1c2aa0d.patch?full_index=1"
-      sha256 "34b217330ef72b12d19b820a7edd994f0107e295f96c779dfe40a990528e1c3a"
-    end
-
-    # Backport fix for https://github.com/thelfer/tfel/issues/740
-    patch do
-      url "https://github.com/thelfer/tfel/commit/331f889bec18329d2a8770cf72be33218c39b3f7.patch?full_index=1"
-      sha256 "901c94fe0a48890e4b17d6cefd87dde34dead3563544162b3196aacda04eebc0"
-    end
-    patch do
-      url "https://github.com/thelfer/tfel/commit/2ac23026e15c716c8b5364aa572fb651457ad786.patch?full_index=1"
-      sha256 "8becb7f82848cb36dd2fc200bed676c95692c9a451ca12c661ef1374ba87bbf1"
-    end
-  end
 
   bottle do
     sha256 arm64_sequoia: "50d3bd7962505ed2bc6fb2557947d7ec078167ac5d5e6525bb1ec644ab69514b"
@@ -63,6 +43,7 @@ class Tfel < Formula
       "-Denable-testing=OFF",
       "-Dpython-static-interpreter-workaround=ON",
     ]
+
     # Avoid linkage to boost container and graph modules
     # Issue ref: https://github.com/boostorg/boost/issues/985
     args << "-DCMAKE_MODULE_LINKER_FLAGS=-Wl,-dead_strip_dylibs" if OS.mac?
