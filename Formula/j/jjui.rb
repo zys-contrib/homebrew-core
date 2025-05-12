dd32b21c7841f7c3aa4f5d230831800d4e0c1d7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-12 23:32:28 +0000

    jjui: add 0.8.8 bottle.

--- Formula/j/jjui.rb
+++ Formula/j/jjui.rb
@@ -5,6 +5,15 @@ class Jjui < Formula
   sha256 "66963f2b091855d1e4116bdf45dbbde728ff6b2a65e4adb879e32249a3693242"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54e7a15721fb267acdaeb43aa73068361593666de8219496e1f19043547662b2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54e7a15721fb267acdaeb43aa73068361593666de8219496e1f19043547662b2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "54e7a15721fb267acdaeb43aa73068361593666de8219496e1f19043547662b2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "60186d9a662096c177aea03a42b2fc7b17045f8f04b6a250d39003d6cd0350b5"
+    sha256 cellar: :any_skip_relocation, ventura:       "60186d9a662096c177aea03a42b2fc7b17045f8f04b6a250d39003d6cd0350b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c619c378089dea967033ea1e753b09eed71dd8807e4dd10fb505547fdbeb3327"
+  end
+
   depends_on "go" => :build
   depends_on "jj"
 
