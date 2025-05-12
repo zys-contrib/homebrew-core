270f5ff696c7ba3332953971ce8ab91fdf13abfa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-12 12:06:28 +0000

    e2b: add 1.3.4 bottle.

--- Formula/e/e2b.rb
+++ Formula/e/e2b.rb
@@ -5,6 +5,15 @@ class E2b < Formula
   sha256 "ce969bfe26cb27a8c2bfdcc1391ab80d734a8706a339aa2a0f840080564472f6"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4d0b533b12488e56a9dfdeee3da7e4615f02dd4395d707bde7b59fb4e8d1299f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d0b533b12488e56a9dfdeee3da7e4615f02dd4395d707bde7b59fb4e8d1299f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4d0b533b12488e56a9dfdeee3da7e4615f02dd4395d707bde7b59fb4e8d1299f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fbf5e66ea9b8839c89dfaf5d50a8eb6d4e415661e0369c3d3eaaf9c7efeaf401"
+    sha256 cellar: :any_skip_relocation, ventura:       "fbf5e66ea9b8839c89dfaf5d50a8eb6d4e415661e0369c3d3eaaf9c7efeaf401"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d0b533b12488e56a9dfdeee3da7e4615f02dd4395d707bde7b59fb4e8d1299f"
+  end
+
   depends_on "node"
 
   def install
