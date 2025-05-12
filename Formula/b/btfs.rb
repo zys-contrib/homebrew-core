ff30356072a8f67b5ffa95e6dfbb7af42584bfaf
botantony <antonsm21@gmail.com>
2025-05-12 23:39:06 +0200

    btfs 3.1

--- Formula/b/btfs.rb
+++ Formula/b/btfs.rb
@@ -1,10 +1,9 @@
 class Btfs < Formula
   desc "BitTorrent filesystem based on FUSE"
   homepage "https://github.com/johang/btfs"
-  url "https://github.com/johang/btfs/archive/refs/tags/v2.24.tar.gz"
-  sha256 "d71ddefe3c572e05362542a0d9fd0240d8d4e1578ace55a8b3245176e7fd8935"
+  url "https://github.com/johang/btfs/archive/refs/tags/v3.1.tar.gz"
+  sha256 "c363f04149f97baf1c5e10ac90677b8309724f2042ab045a45041cfb7b44649b"
   license "GPL-3.0-only"
-  revision 1
   head "https://github.com/johang/btfs.git", branch: "master"
 
   bottle do
@@ -17,14 +16,13 @@ class Btfs < Formula
   depends_on "automake" => :build
   depends_on "pkgconf" => :build
   depends_on "curl"
-  depends_on "libfuse@2" # FUSE 3 issue: https://github.com/johang/btfs/issues/92
+  depends_on "libfuse"
   depends_on "libtorrent-rasterbar"
   depends_on :linux # on macOS, requires closed-source macFUSE
   depends_on "openssl@3"
 
   def install
     ENV.cxx11
-    inreplace "configure.ac", "fuse >= 2.8.0", "fuse >= 2.7.3"
     system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", "--disable-silent-rules", *std_configure_args
     system "make", "install"
