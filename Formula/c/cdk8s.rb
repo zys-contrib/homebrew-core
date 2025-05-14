f09ea2e343903eb9f6b24c9b0152ee7a13f76f54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-14 11:31:25 +0000

    cdk8s: update 2.200.71 bottle.

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -6,13 +6,13 @@ class Cdk8s < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a996b96173689bc3055484bd2448bf8ebd222aabb3c81cdf4683b5818673ab79"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a996b96173689bc3055484bd2448bf8ebd222aabb3c81cdf4683b5818673ab79"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a996b96173689bc3055484bd2448bf8ebd222aabb3c81cdf4683b5818673ab79"
-    sha256 cellar: :any_skip_relocation, sonoma:        "685efa3b9c42a71dc12ada9667c7704b874d607c7264db2370e5bd729b0c62c1"
-    sha256 cellar: :any_skip_relocation, ventura:       "685efa3b9c42a71dc12ada9667c7704b874d607c7264db2370e5bd729b0c62c1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a996b96173689bc3055484bd2448bf8ebd222aabb3c81cdf4683b5818673ab79"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a996b96173689bc3055484bd2448bf8ebd222aabb3c81cdf4683b5818673ab79"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d4ac3ae2f04fe226d9bef91cd949b422d7d259ec5a470bee20c7d28796d2963e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d4ac3ae2f04fe226d9bef91cd949b422d7d259ec5a470bee20c7d28796d2963e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d4ac3ae2f04fe226d9bef91cd949b422d7d259ec5a470bee20c7d28796d2963e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f4e37b24beb20ab43f1c6677a2d650a0877cc6ffaa6677a6a045a428f7d66a6d"
+    sha256 cellar: :any_skip_relocation, ventura:       "f4e37b24beb20ab43f1c6677a2d650a0877cc6ffaa6677a6a045a428f7d66a6d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d4ac3ae2f04fe226d9bef91cd949b422d7d259ec5a470bee20c7d28796d2963e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4ac3ae2f04fe226d9bef91cd949b422d7d259ec5a470bee20c7d28796d2963e"
   end
 
   depends_on "node"
