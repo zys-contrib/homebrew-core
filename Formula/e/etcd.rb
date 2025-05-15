33cb1fe28f2520f51f6b246dc85008c5a2254778
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-15 19:59:31 +0000

    etcd 3.6.0
    etcd: update test to use v3 api
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/e/etcd.rb
+++ Formula/e/etcd.rb
@@ -2,8 +2,8 @@ class Etcd < Formula
   desc "Key value store for shared configuration and service discovery"
   homepage "https://github.com/etcd-io/etcd"
   url "https://github.com/etcd-io/etcd.git",
-      tag:      "v3.5.21",
-      revision: "a17edfd59754d1aed29c2db33520ab9d401326a5"
+      tag:      "v3.6.0",
+      revision: "f5d605a93abe2fdfa13880aab0f1f3a4203628dd"
   license "Apache-2.0"
   head "https://github.com/etcd-io/etcd.git", branch: "main"
 
@@ -35,32 +35,35 @@ class Etcd < Formula
   test do
     test_string = "Hello from brew test!"
     etcd_pid = fork do
-      if OS.mac? && Hardware::CPU.arm?
-        # etcd isn't officially supported on arm64
-        # https://github.com/etcd-io/etcd/issues/10318
-        # https://github.com/etcd-io/etcd/issues/10677
-        ENV["ETCD_UNSUPPORTED_ARCH"]="arm64"
-      end
-
       exec bin/"etcd",
-        "--enable-v2", # enable etcd v2 client support
-        "--force-new-cluster",
-        "--logger=zap", # default logger (`capnslog`) to be deprecated in v3.5
-        "--data-dir=#{testpath}"
+           "--force-new-cluster",
+           "--logger=zap",
+           "--data-dir=#{testpath}"
     end
-    # sleep to let etcd get its wits about it
+
+    # Wait a bit for etcd to initialize
     sleep 10
 
-    etcd_uri = "http://127.0.0.1:2379/v2/keys/brew_test"
-    system "curl", "--silent", "-L", etcd_uri, "-XPUT", "-d", "value=#{test_string}"
-    curl_output = shell_output("curl --silent -L #{etcd_uri}")
+    key_base64 = Base64.strict_encode64("brew_test")
+    value_base64 = Base64.strict_encode64(test_string)
+
+    # PUT the key using the v3 API
+    put_payload = { key: key_base64, value: value_base64 }.to_json
+    system "curl", "-L", "http://127.0.0.1:2379/v3/kv/put", "-X", "POST", "-d", put_payload
+
+    # GET the key back
+    get_payload = { key: key_base64 }.to_json
+    curl_output = shell_output("curl -L http://127.0.0.1:2379/v3/kv/range -X POST -d '#{get_payload}'")
     response_hash = JSON.parse(curl_output)
-    assert_match(test_string, response_hash.fetch("node").fetch("value"))
+
+    retrieved_value_base64 = response_hash.dig("kvs", 0, "value")
+    retrieved_value = Base64.decode64(retrieved_value_base64)
+
+    assert_equal test_string, retrieved_value
 
     assert_equal "OK\n", shell_output("#{bin}/etcdctl put foo bar")
     assert_equal "foo\nbar\n", shell_output("#{bin}/etcdctl get foo 2>&1")
   ensure
-    # clean up the etcd process before we leave
     Process.kill("HUP", etcd_pid)
   end
 end
