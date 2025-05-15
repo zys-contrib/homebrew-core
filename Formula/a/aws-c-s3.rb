c46043470885d0fc112937f347f9d2804a7a7476
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-15 19:50:26 +0000

    aws-c-s3 0.7.18

--- Formula/a/aws-c-s3.rb
+++ Formula/a/aws-c-s3.rb
@@ -1,8 +1,8 @@
 class AwsCS3 < Formula
   desc "C99 library implementation for communicating with the S3 service"
   homepage "https://github.com/awslabs/aws-c-s3"
-  url "https://github.com/awslabs/aws-c-s3/archive/refs/tags/v0.7.17.tar.gz"
-  sha256 "cad16b82f628ae05a2bf072de19cad12f69dddcf7817b55eb00037628011fa92"
+  url "https://github.com/awslabs/aws-c-s3/archive/refs/tags/v0.7.18.tar.gz"
+  sha256 "89cf4a4906ebfd761c85acd161664c5cc640606a90daf8f06f681d4d939454fc"
   license "Apache-2.0"
 
   bottle do
