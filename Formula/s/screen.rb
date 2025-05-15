257c3c3e9e55331dc6f58c3ff247a03038fd8022
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-15 14:23:09 +0000

    screen 5.0.1

--- Formula/s/screen.rb
+++ Formula/s/screen.rb
@@ -1,9 +1,9 @@
 class Screen < Formula
   desc "Terminal multiplexer with VT100/ANSI terminal emulation"
   homepage "https://www.gnu.org/software/screen/"
-  url "https://ftp.gnu.org/gnu/screen/screen-5.0.0.tar.gz"
-  mirror "https://ftpmirror.gnu.org/screen/screen-5.0.0.tar.gz"
-  sha256 "f04a39d00a0e5c7c86a55338808903082ad5df4d73df1a2fd3425976aed94971"
+  url "https://ftp.gnu.org/gnu/screen/screen-5.0.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/screen/screen-5.0.1.tar.gz"
+  sha256 "2dae36f4db379ffcd14b691596ba6ec18ac3a9e22bc47ac239789ab58409869d"
   license "GPL-3.0-or-later"
   head "https://git.savannah.gnu.org/git/screen.git", branch: "master"
 
