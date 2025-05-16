86aefb9b26cf5b8910be3d9825ad5a6d4423b205
botantony <antonsm21@gmail.com>
2025-05-16 11:16:48 +0200

    e2b 1.4.1
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/e/e2b.rb
+++ Formula/e/e2b.rb
@@ -1,8 +1,8 @@
 class E2b < Formula
   desc "CLI to manage E2B sandboxes and templates"
   homepage "https://e2b.dev"
-  url "https://registry.npmjs.org/@e2b/cli/-/cli-1.3.4.tgz"
-  sha256 "ce969bfe26cb27a8c2bfdcc1391ab80d734a8706a339aa2a0f840080564472f6"
+  url "https://registry.npmjs.org/@e2b/cli/-/cli-1.4.1.tgz"
+  sha256 "ed6be9a7dcfb294e0eebe194d125292602f6539163e11f9f841e14f6ca87ceb9"
   license "Apache-2.0"
 
   bottle do
