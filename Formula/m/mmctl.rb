4f5a258b7bd5a5e46e13f649997f5f7134bf056a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-16 12:39:03 +0000

    mmctl: update 10.8.0 bottle.

--- Formula/m/mmctl.rb
+++ Formula/m/mmctl.rb
@@ -12,12 +12,12 @@ class Mmctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6ff49964cc573544dbd806fcd736ffca3dca9b1bac4290afd8c4781b46f06169"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ff49964cc573544dbd806fcd736ffca3dca9b1bac4290afd8c4781b46f06169"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6ff49964cc573544dbd806fcd736ffca3dca9b1bac4290afd8c4781b46f06169"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f19752a71fc5d4743e5bae14653ac5d5203d943fba1bdd3504cf36a66eff777f"
-    sha256 cellar: :any_skip_relocation, ventura:       "f19752a71fc5d4743e5bae14653ac5d5203d943fba1bdd3504cf36a66eff777f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "932146d69f61e08440d70c6f19c4dcdb749fb821a9b7c86a48844687ed5bed16"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe5b05b4b9f7c00b5adec2724e29c677d3377df12d7c86a518fe16a34a4f1fae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fe5b05b4b9f7c00b5adec2724e29c677d3377df12d7c86a518fe16a34a4f1fae"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fe5b05b4b9f7c00b5adec2724e29c677d3377df12d7c86a518fe16a34a4f1fae"
+    sha256 cellar: :any_skip_relocation, sonoma:        "060df52dedeed452531762969680834d008fb26e1e45186787fc0ee4fdd19cec"
+    sha256 cellar: :any_skip_relocation, ventura:       "060df52dedeed452531762969680834d008fb26e1e45186787fc0ee4fdd19cec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6da0df6c77b06ae5cdfdde2f9182f9621e827f3ad6de789eef05d90d7e93741a"
   end
 
   depends_on "go" => :build
