334868ea332ae6bddb03797c1ca89ead6ff0576b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-17 18:08:02 +0000

    chart-releaser: add 1.7.0 bottle.

--- Formula/c/chart-releaser.rb
+++ Formula/c/chart-releaser.rb
@@ -6,6 +6,15 @@ class ChartReleaser < Formula
   license "Apache-2.0"
   head "https://github.com/helm/chart-releaser.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90701fd2d173081f91119e9817a89a171d2e5b170a888f63105cd42a158109b2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4f5adc463c318777895b3370b7b876d3533b513e60fb364226ec4563bc29da3a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5d1c4cb8b0678c91261d8ef50caa05bbc99fc063dfbeeb26e8b25a7e0573719a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8589497b906501194855f60a2cbb4c576b666627e805525c77f94eb941cd0cfb"
+    sha256 cellar: :any_skip_relocation, ventura:       "18663efa332fd116acd16530d4ae73faa0eba3b21b2f8ce7697098203eee040d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9ebf5848614eb2953aa6f3a6d234a5f2d86957ffdb430aaac56cb919f1ceb95e"
+  end
+
   depends_on "go" => :build
   depends_on "helm" => :test
 
