d0e1e4b7b33f6691cc369ffd43409777e22f0135
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-17 09:09:24 +0000

    snakeviz: update 2.2.2_2 bottle.

--- Formula/s/snakeviz.rb
+++ Formula/s/snakeviz.rb
@@ -9,13 +9,13 @@ class Snakeviz < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "75ce7210b6e64830a317d2dce83ad07010f11970c4d6bccf63cba9c53796113f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6cacabdb1e48108acd4738b7b030d1b240e433783a0a1b6d0e8fc20735e58b8d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b4fc7432f7e622374f7aedc59312d820952cd8cd14d9f265b88ae206c58c83d2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cc5b7cd5bd74907bac04e6a0316954e197274e18215fd0b66cddc34093818b4c"
-    sha256 cellar: :any_skip_relocation, ventura:       "01cc2b2bae5b4b5f362626d37756efdccf87089f705a55a98841856f7845f485"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e4c653f9541943f6a11e86db50444b7cf42dc74577332c5a2ab17a9a8f903db9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2db01dca11b6887cf9428b9d29de8f1637bf380446ec19b1603bcb60773cbb06"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b4d4520c17ede830a4b318dc3e58676447f2cb0e5810ad8ebc4cf28cc01d090d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fac8bd6ed2a0deb2b93c66d3f7a1e7d45cd97ed3401644100ca5ed5bb6b99c45"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1e0e2b75a9317bdf6f00b62efd40979d04ef3cb7d9d09fb4fc5141425e113bda"
+    sha256 cellar: :any_skip_relocation, sonoma:        "93f50659ccb85e4fd236fbbf11c789b14d06e79ea0e01ff9b99bdbf8f503a9ab"
+    sha256 cellar: :any_skip_relocation, ventura:       "fe459ebc55f218ca933081e52990141c0ed1c83f8dd92d1b170fbd6e188b1a1b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "69f916e637ecc77844e6ca4de1bc87abf662e112f13418e2dc561d44e649ceed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "704638b72520fd6f9a35b271b1a428a0fe2afd82958e06297ccd7ed609af03ff"
   end
 
   depends_on "python@3.13"
