e33865f83facfc1864717bab3d26684195895c29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-17 19:35:19 +0000

    timoni: add 0.24.0 bottle.

--- Formula/t/timoni.rb
+++ Formula/t/timoni.rb
@@ -6,6 +6,15 @@ class Timoni < Formula
   license "Apache-2.0"
   head "https://github.com/stefanprodan/timoni.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9eb8a12c3189a6f1fc1d8f30d5dbbad8cdc33f7379f57550794b64b590a0c371"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a7908c8fb62bf682f84c1552ceddaedad9d22b8f08d1571c1d6ab56d80111b8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6d542d48ab9c827dd159ba362f874e99d9fc9752e0ab41241531e84249fa9da5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9e23b99bd61bfa5f21561e7ad60981252249cc7494e8403fe966f70f749b39dd"
+    sha256 cellar: :any_skip_relocation, ventura:       "8e9d22c3e0dd294d88ac00859370fffe95678d8153224439365e1d13b8c0b83d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b862eaf34380ef2d39125bb3c1f4598d64ff3920677f3d8e44f59a844e3f0502"
+  end
+
   depends_on "go" => :build
 
   def install
