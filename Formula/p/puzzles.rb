ff5a15d04970824bcca59e88f30b46335c1680e9
botantony <antonsm21@gmail.com>
2025-05-18 15:04:26 +0200

    puzzles 20250518
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/p/puzzles.rb
+++ Formula/p/puzzles.rb
@@ -2,9 +2,9 @@ class Puzzles < Formula
   desc "Collection of one-player puzzle games"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/"
   # Extract https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles.tar.gz to get the version number
-  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20250517.c9070b4.tar.gz"
-  version "20250517"
-  sha256 "6ca45aa54afcf3330ef9ea13748f3736f987cf840d6eedf419a8a0f123b94447"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/puzzles-20250518.e374a5a.tar.gz"
+  version "20250518"
+  sha256 "e2b7149863d016ff8dd67e3b57c58a14851a3f6bbe117c012ccfea42bba80906"
   license "MIT"
   head "https://git.tartarus.org/simon/puzzles.git", branch: "main"
 
