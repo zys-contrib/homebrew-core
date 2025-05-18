1b3b1ac6999b6307fed94bb8dc28ed3e73d4636a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-18 05:00:00 +0000

    vfox: update 0.6.7 bottle.

--- Formula/v/vfox.rb
+++ Formula/v/vfox.rb
@@ -12,12 +12,12 @@ class Vfox < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13645325445b061c993edbf6bbd288bd3bbef97bc574fbb64a604a47a5cf187a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "79565f604f7c981e922efd426816085e22446ddde1e7484916d00858a19ec0d4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "84fc5c5014a86f6a5e4eb506abc5b505330c8f2571f549e689290ad9bfde841f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "104c34110a53b74546ebefa64801afacd75a96dd4f790e31a5a59328362787fa"
-    sha256 cellar: :any_skip_relocation, ventura:       "b0b0b1af20779c411206d3c205799050fa6b7cf7352996d541044a605cec6c93"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b194da09f35b780dfea89a8e346a388e52e1e83c9f2a6d33fd4d4499bb09b18e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "70fe5d5fee78c146181974e46d84081c7243994800d6838db3d98e3a8c0acebf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54e70ef0db87295e56e4b07b42599cc34c2b690db5c3d041add31d296339041f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "565da4216d9d440763a310f3c6ed93b58a5df87c9d4b9a47693829796c6dd7a3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "382e8dda58609d08a91173f507dc55954e6cbc2437eb0e5a6c775a2ab80537c5"
+    sha256 cellar: :any_skip_relocation, ventura:       "cd7c8ba4694b9059a108d90096cb5f66eecca308975063fb0c084f1c8711fe0c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b2fbf1469782266018c291820769c069db43b7c9e0cbd4b31d223672aa370c3"
   end
 
   depends_on "go" => :build
