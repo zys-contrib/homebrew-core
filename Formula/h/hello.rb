c38c4048ff5492df0267c756deabd9ed594145ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-19 14:25:25 +0000

    hello 2.12.2

--- Formula/h/hello.rb
+++ Formula/h/hello.rb
@@ -1,8 +1,8 @@
 class Hello < Formula
   desc "Program providing model for GNU coding standards and practices"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz"
-  sha256 "8d99142afd92576f30b0cd7cb42a8dc6809998bc5d607d88761f512e26c7db20"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.12.2.tar.gz"
+  sha256 "5a9a996dc292cc24dcf411cee87e92f6aae5b8d13bd9c6819b4c7a9dce0818ab"
   license "GPL-3.0-or-later"
 
   bottle do
