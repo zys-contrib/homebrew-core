345215f3315ae4b20b1550dc3eb0e8eddff60745
Rui Chen <rui@chenrui.dev>
2025-05-19 16:33:12 -0400

    malcontent 1.11.3
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/m/malcontent.rb
+++ Formula/m/malcontent.rb
@@ -1,8 +1,8 @@
 class Malcontent < Formula
   desc "Supply Chain Attack Detection, via context differential analysis and YARA"
   homepage "https://github.com/chainguard-dev/malcontent"
-  url "https://github.com/chainguard-dev/malcontent/archive/refs/tags/v1.10.3.tar.gz"
-  sha256 "a57e3d7d6419e407a2dfe608f0629eab70817925df230b3f308256e1967c0e8b"
+  url "https://github.com/chainguard-dev/malcontent/archive/refs/tags/v1.11.3.tar.gz"
+  sha256 "57c2c875522306776fdf4a39bffd778c56a97d0d0f30c6d3299414f88fb9af77"
   license "Apache-2.0"
   head "https://github.com/chainguard-dev/malcontent.git", branch: "main"
 
