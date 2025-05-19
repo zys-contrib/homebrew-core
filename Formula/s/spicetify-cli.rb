9b9ad3f6902e978f529c202d2fa1353135f43ac1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-19 17:19:49 +0000

    spicetify-cli 2.40.9

--- Formula/s/spicetify-cli.rb
+++ Formula/s/spicetify-cli.rb
@@ -1,8 +1,8 @@
 class SpicetifyCli < Formula
   desc "Command-line tool to customize Spotify client"
   homepage "https://spicetify.app/"
-  url "https://github.com/spicetify/cli/archive/refs/tags/v2.40.8/v2.40.8.tar.gz"
-  sha256 "e4c0c38f8f6676d9410db9b958e5ea08d7a57e1e7b95f444432b102d1aeb57c0"
+  url "https://github.com/spicetify/cli/archive/refs/tags/v2.40.9/v2.40.9.tar.gz"
+  sha256 "33ffbdebf1296db67089722b375ee4dc3f7e8708815cc11c40e1a499370f08b5"
   license "LGPL-2.1-only"
   head "https://github.com/spicetify/cli.git", branch: "master"
 
