b2c702bf165b75908c5bedfc0158792931a45363
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-19 03:11:19 +0000

    spicetify-cli: update 2.40.8 bottle.

--- Formula/s/spicetify-cli.rb
+++ Formula/s/spicetify-cli.rb
@@ -7,12 +7,12 @@ class SpicetifyCli < Formula
   head "https://github.com/spicetify/cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "384628f34a946747e55d9de2dbf23153d090db9b368444d0b43bfbf83ba34eb0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "384628f34a946747e55d9de2dbf23153d090db9b368444d0b43bfbf83ba34eb0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "384628f34a946747e55d9de2dbf23153d090db9b368444d0b43bfbf83ba34eb0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "12fe28b9fa3d83a3a7d3b0a383d931085dc355b88345330e35b99f94f9f44d46"
-    sha256 cellar: :any_skip_relocation, ventura:       "12fe28b9fa3d83a3a7d3b0a383d931085dc355b88345330e35b99f94f9f44d46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1dd17c77a278506beaf1da3a0d167fd711d2f2f19cb3c3dcbe83182d39eda6f0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "727587a887279feb5d565cdff59bbb240d247d76ffbb2826aa9c48e0e8ce5075"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "727587a887279feb5d565cdff59bbb240d247d76ffbb2826aa9c48e0e8ce5075"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "727587a887279feb5d565cdff59bbb240d247d76ffbb2826aa9c48e0e8ce5075"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0d63962a8f8e86d1e70b5cc240ab1bac48304027cf4e68e52a15d5784a07ccd7"
+    sha256 cellar: :any_skip_relocation, ventura:       "0d63962a8f8e86d1e70b5cc240ab1bac48304027cf4e68e52a15d5784a07ccd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89f48359df04728d044b5b64f228932071a862fbc4fe470a2f3683c586a54cf4"
   end
 
   depends_on "go" => :build
