8037012370c97a25256eb2b79b5d51ac894fc945
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-20 04:17:14 +0000

    httpie: update 3.2.4_1 bottle.

--- Formula/h/httpie.rb
+++ Formula/h/httpie.rb
@@ -10,13 +10,13 @@ class Httpie < Formula
   head "https://github.com/httpie/cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83e92884af29f9d3f550603b35715ad22d04a353d754a0c9f237318166f24147"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5e6306cd7e9fb707552d406186b25a1774e436e218ec2ca1f8cc44319d73f591"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7e92db0acf947533e3ef430e7c0030819d2a616ad0290bbd2ba3038cdd53afcc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2c2b7f472c1baf574ac82ad47299ad3b838b1cb287d98165a395d5b05e371ad6"
-    sha256 cellar: :any_skip_relocation, ventura:       "49df2e4365298b54ebf24b0bd661e06f73dd3844d7575de76c74467c8cda8f3e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c574ce8746fe865719cd707022de24ec05a57733eaba7cf075b8bac19548233b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "72dc884db737283760aae001baadbf96fc3dd71119545eb09783f1523654408b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a451ae630be688ea83dfd2cd2030873bbdb7b13fc2d2b77048d091d58a27beac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7acdfde3fc34526a586f1df5077e9c722d85c46b88dd583d5d3feccb6aa94cbc"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a8d43ba63c0924a439c81387b6a7432703e00daae965173912618be026a203e1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e994062c42733f358a4752ec41c205526a4934e4d132d6f0ce4005677677dbdc"
+    sha256 cellar: :any_skip_relocation, ventura:       "eab041f8bebb9f83a3b81e25b6b1a7654659951a80563d39356be37402f35c77"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "996b86869f1524e55deb98f33bbc0484e87877ad3f22ceb7ba219b0fa40663ed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "95f70667f58908f43f116c1773d04821163a4638e79aad8a2f68358aebd0adbb"
   end
 
   depends_on "certifi"
