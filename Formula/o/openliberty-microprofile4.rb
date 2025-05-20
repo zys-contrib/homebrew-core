a479a2c0de7af5621d5eac76f9446967e78cf377
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-20 14:11:54 +0000

    openliberty-microprofile4 25.0.0.5

--- Formula/o/openliberty-microprofile4.rb
+++ Formula/o/openliberty-microprofile4.rb
@@ -1,8 +1,8 @@
 class OpenlibertyMicroprofile4 < Formula
   desc "Lightweight open framework for Java (Micro Profile 4)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/25.0.0.4/openliberty-microProfile4-25.0.0.4.zip"
-  sha256 "9b077c53256b8773e9365e530bd97adf238b46f39818138cbcbe2a699a60cfde"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/25.0.0.5/openliberty-microProfile4-25.0.0.5.zip"
+  sha256 "894fd3451a041db04c8b398b2dbefbcc7790581d424a7395993e2d05cfeefd0c"
   license "EPL-1.0"
 
   livecheck do
