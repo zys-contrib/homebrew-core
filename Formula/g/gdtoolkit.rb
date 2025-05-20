6afc0f5d2d7400ab453de65fc13900204d328d77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-20 03:52:07 +0000

    gdtoolkit: update 4.3.3_1 bottle.

--- Formula/g/gdtoolkit.rb
+++ Formula/g/gdtoolkit.rb
@@ -9,13 +9,13 @@ class Gdtoolkit < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "18c9d3e8faaf0038a293dc1ee6d5b8a57f0d46c50f43e1b164b8dea6a4a6e7e9"
-    sha256 cellar: :any,                 arm64_sonoma:  "34d4cf5f883f6b13dc87d513171f15b2d9bcb639e394bdefbcd92b272337b949"
-    sha256 cellar: :any,                 arm64_ventura: "ed386e1d59fe6471f1e192e7a82a1c4274ae628b4d35274ab1d8451d06d06099"
-    sha256 cellar: :any,                 sonoma:        "97cd6163cfe41d41fe0748c65bc7c838e83749f6d5635286019d6b4dd32dcfc6"
-    sha256 cellar: :any,                 ventura:       "951eeeeed7dbe1ef467685b65721c54f01323f2836225a737697bf822f854bd5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dd6ec3ac1a27aae5c0be8590c9b8ec7cbebdaddcd8b041c5e9a20730828324d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "377d555ae1057c4a0e61c54f0d0a44c44f6eaa19ef6bd0bfd41c854bcabd78f1"
+    sha256 cellar: :any,                 arm64_sequoia: "bf4f396f05200cb9b9acc892694f4bf4e5969420d21c1efa6d0509c5707854d2"
+    sha256 cellar: :any,                 arm64_sonoma:  "3d1ca6707157c01f628e1f335750a29ddb1638d15b7b1b846b9e32ae02a6916b"
+    sha256 cellar: :any,                 arm64_ventura: "795711b2b6e7820c18ae710f4b15a20cc34593eb9ef04b0c2570c266bd8a2bf1"
+    sha256 cellar: :any,                 sonoma:        "a484303d9f447dd76f825d29b8056b3b58aa2794b054766940f885aa89e31335"
+    sha256 cellar: :any,                 ventura:       "02d84ca798c5039e5df3fe1fbe727eedd818b03d43f071c24fbdf1a2d0180b45"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2e3417a05bc2349a9bbc260903e166c0be0c4c813a876808fa2e97bf31bec09a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1864a31849824628ae0898d5fba730754bab84b4de707d33b11a8bec6a72ab10"
   end
 
   depends_on "libyaml"
