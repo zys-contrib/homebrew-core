79f8c68d6df597da23b51217a56e77e3589987d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-20 19:50:40 +0000

    blocky 0.26

--- Formula/b/blocky.rb
+++ Formula/b/blocky.rb
@@ -1,8 +1,8 @@
 class Blocky < Formula
   desc "Fast and lightweight DNS proxy as ad-blocker for local network"
   homepage "https://0xerr0r.github.io/blocky/"
-  url "https://github.com/0xerr0r/blocky/archive/refs/tags/v0.25.tar.gz"
-  sha256 "856f09cd5df5dc20a44067113dd200a3e74465c3ded2b5dcd473188b3c764b11"
+  url "https://github.com/0xerr0r/blocky/archive/refs/tags/v0.26.tar.gz"
+  sha256 "3a5c4552a248399684ca5ec1ee40aae80b6b16a3c39c6123d9a8b1e9c7d5bce5"
   license "Apache-2.0"
   head "https://github.com/0xerr0r/blocky.git", branch: "main"
 
