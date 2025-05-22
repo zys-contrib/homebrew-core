a046bc06caafd5141480b8feb42e7f36c3afe3e6
Rui Chen <rui@chenrui.dev>
2025-05-22 18:50:18 -0400

    cereal: add clang 19+ build patch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cereal.rb
+++ Formula/c/cereal.rb
@@ -1,10 +1,20 @@
 class Cereal < Formula
   desc "C++11 library for serialization"
   homepage "https://uscilab.github.io/cereal/"
-  url "https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.2.tar.gz"
-  sha256 "16a7ad9b31ba5880dac55d62b5d6f243c3ebc8d46a3514149e56b5e7ea81f85f"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/USCiLab/cereal.git", branch: "develop"
+
+  stable do
+    url "https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.2.tar.gz"
+    sha256 "16a7ad9b31ba5880dac55d62b5d6f243c3ebc8d46a3514149e56b5e7ea81f85f"
+
+    # clang 19+ build patch, upstream pr ref, https://github.com/USCiLab/cereal/pull/835
+    patch do
+      url "https://github.com/USCiLab/cereal/commit/409db5e910279224bd7e78f8188450c7e7d34d87.patch?full_index=1"
+      sha256 "ad413ad34abb4ad515777013f1824f6f8eef879d3391f35cb0bf4b7e30937a7c"
+    end
+  end
 
   bottle do
     rebuild 1
