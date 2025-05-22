ec887a3c638362b6f6a6fe60d4d5ff8332fd96e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-22 20:09:29 +0000

    libunwind 1.8.2

--- Formula/lib/libunwind.rb
+++ Formula/lib/libunwind.rb
@@ -1,8 +1,8 @@
 class Libunwind < Formula
   desc "C API for determining the call-chain of a program"
   homepage "https://www.nongnu.org/libunwind/"
-  url "https://github.com/libunwind/libunwind/releases/download/v1.8.1/libunwind-1.8.1.tar.gz"
-  sha256 "ddf0e32dd5fafe5283198d37e4bf9decf7ba1770b6e7e006c33e6df79e6a6157"
+  url "https://github.com/libunwind/libunwind/releases/download/v1.8.2/libunwind-1.8.2.tar.gz"
+  sha256 "7f262f1a1224f437ede0f96a6932b582c8f5421ff207c04e3d9504dfa04c8b82"
   license "MIT"
 
   livecheck do
