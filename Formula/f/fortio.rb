0848e1f5a8cd776e8961b1440e4763fca4e8e8d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-23 20:00:16 +0000

    fortio 1.69.5

--- Formula/f/fortio.rb
+++ Formula/f/fortio.rb
@@ -2,8 +2,8 @@ class Fortio < Formula
   desc "HTTP and gRPC load testing and visualization tool and server"
   homepage "https://fortio.org/"
   url "https://github.com/fortio/fortio.git",
-      tag:      "v1.69.4",
-      revision: "54189cc772636e28108a43aab57f8bd528eef99b"
+      tag:      "v1.69.5",
+      revision: "929fca73f8f87c0b67a5b633b0d381738fc320fb"
   license "Apache-2.0"
   head "https://github.com/fortio/fortio.git", branch: "master"
 
