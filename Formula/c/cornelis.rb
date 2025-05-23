65491c6eebc5a176943e28b7a513554eb8993b19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-23 03:49:28 +0000

    cornelis: add 2.7.1 bottle.

--- Formula/c/cornelis.rb
+++ Formula/c/cornelis.rb
@@ -6,6 +6,15 @@ class Cornelis < Formula
   license "BSD-3-Clause"
   head "https://github.com/agda/cornelis.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "5469975994b43e45788ff4b27058dc3284fa90f63be85001603abc5228dd76d3"
+    sha256 cellar: :any,                 arm64_sonoma:  "845f96549a6d6c2937bedf9fb4031107f57b6b101275566702185b1625cfb733"
+    sha256 cellar: :any,                 arm64_ventura: "410c0032ecb66fb98642251a207bfad302f466c1334f317f02d641a3e353f9c8"
+    sha256 cellar: :any,                 sonoma:        "568be9e2acb19617e368400204f1664ed5b2839e6c96bf6adad497dd602b9511"
+    sha256 cellar: :any,                 ventura:       "218969d2f9bd9584c280135fd53704646386bcac869f77ffad1586c458095547"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9f0f3d6779acf8d5586343012554985bf180d316cdf9e58874e0a0b02913ec3"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
   depends_on "hpack" => :build
