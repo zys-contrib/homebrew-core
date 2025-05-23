918ed99089ef8829ec370b980944d0ae4f0422fb
Rui Chen <rui@chenrui.dev>
2025-05-23 23:48:02 +0000

    mesheryctl: update 0.8.87 bottle.

--- Formula/m/mesheryctl.rb
+++ Formula/m/mesheryctl.rb
@@ -10,13 +10,13 @@ class Mesheryctl < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0f0299b677cd45a095ece4a6ea1c10366387c3ed35e1b6adacc57ce5ee4e9fa6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f0299b677cd45a095ece4a6ea1c10366387c3ed35e1b6adacc57ce5ee4e9fa6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0f0299b677cd45a095ece4a6ea1c10366387c3ed35e1b6adacc57ce5ee4e9fa6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e576806cf46947703edc65fb848c1f7110702634cc2d5f3c1f9f902f840fd10c"
-    sha256 cellar: :any_skip_relocation, ventura:       "e576806cf46947703edc65fb848c1f7110702634cc2d5f3c1f9f902f840fd10c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "046925beaf14c82d5423d0328c6b404a02c6aea7842d91897a1a2be12b52e5b8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1b993a586579a0196b4bf7b0887788861f7b20012e24026a978c53be64d1046"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "75923b81cc0233cb2796b00a00b0c5442004f82cc9f46a2739db3562248a4f2a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75923b81cc0233cb2796b00a00b0c5442004f82cc9f46a2739db3562248a4f2a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "75923b81cc0233cb2796b00a00b0c5442004f82cc9f46a2739db3562248a4f2a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e97c60df520b586a046d753bc943c0caabe437d54c17760ccc3cf741a4ae8bc6"
+    sha256 cellar: :any_skip_relocation, ventura:       "e97c60df520b586a046d753bc943c0caabe437d54c17760ccc3cf741a4ae8bc6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a25d6b773e9a28f2d5d2d827623706827fef9cb4d1a1afded78509990d95771e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c92290c505896c19c695369ad19463856abcfafc347a25cdf7205b6be3be78ea"
   end
 
   depends_on "go" => :build
