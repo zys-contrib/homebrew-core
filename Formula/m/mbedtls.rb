29017cedd5834e3c473ce029cb290e70ff99e322
Rui Chen <rui@chenrui.dev>
2025-05-24 16:25:37 -0400

    mbedtls 3.6.3.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mbedtls.rb
+++ Formula/m/mbedtls.rb
@@ -1,8 +1,8 @@
 class Mbedtls < Formula
   desc "Cryptographic & SSL/TLS library"
   homepage "https://tls.mbed.org/"
-  url "https://github.com/Mbed-TLS/mbedtls/releases/download/mbedtls-3.6.3/mbedtls-3.6.3.tar.bz2"
-  sha256 "64cd73842cdc05e101172f7b437c65e7312e476206e1dbfd644433d11bc56327"
+  url "https://github.com/Mbed-TLS/mbedtls/releases/download/v3.6.3.1/mbedtls-3.6.3.1.tar.bz2"
+  sha256 "243ed496d5f88a5b3791021be2800aac821b9a4cc16e7134aa413c58b4c20e0c"
   license "Apache-2.0"
   head "https://github.com/Mbed-TLS/mbedtls.git", branch: "development"
 
