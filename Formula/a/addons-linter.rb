f932512f3fafb3634fc2237238aa0474f4106046
Rui Chen <rui@chenrui.dev>
2025-05-24 19:09:00 -0400

    addons-linter 7.13.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/a/addons-linter.rb
@@ -0,0 +1,35 @@
+class AddonsLinter < Formula
+  desc "Firefox Add-ons linter, written in JavaScript"
+  homepage "https://github.com/mozilla/addons-linter"
+  url "https://registry.npmjs.org/addons-linter/-/addons-linter-7.13.0.tgz"
+  sha256 "465faac15bc050894113b57dbcabf8abcc63240366f309c94f4bfab07eb1b414"
+  license "MPL-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/addons-linter --version")
+
+    (testpath/"manifest.json").write <<~JSON
+      {
+        "manifest_version": 2,
+        "name": "Test Addon",
+        "version": "1.0",
+        "description": "A test addon",
+        "applications": {
+          "gecko": {
+            "id": "test-addon@example.com"
+          }
+        }
+      }
+    JSON
+
+    output = shell_output("#{bin}/addons-linter #{testpath}/manifest.json 2>&1")
+    assert_match "BAD_ZIPFILE   Corrupt ZIP", output
+  end
+end
