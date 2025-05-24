a565ecc1f17043fffae7d523c29808dda9125352
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-24 11:20:27 +0000

    shepherd 1.0.5

--- Formula/s/shepherd.rb
+++ Formula/s/shepherd.rb
@@ -1,9 +1,9 @@
 class Shepherd < Formula
   desc "Service manager that looks after the herd of system services"
   homepage "https://www.gnu.org/software/shepherd/"
-  url "https://ftp.gnu.org/gnu/shepherd/shepherd-1.0.4.tar.gz"
-  mirror "https://ftpmirror.gnu.org/shepherd/shepherd-1.0.4.tar.gz"
-  sha256 "13306a6b56dfe252464e84a23c23a7234338cc752c565e1b865f7cbf8a03f0cf"
+  url "https://ftp.gnu.org/gnu/shepherd/shepherd-1.0.5.tar.gz"
+  mirror "https://ftpmirror.gnu.org/shepherd/shepherd-1.0.5.tar.gz"
+  sha256 "3c475069a02b49018491e5a5bbab5b7a424d76c7a06bdbf47afd005dc86805f8"
   license "GPL-3.0-or-later"
 
   bottle do
