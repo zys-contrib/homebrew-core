24cf920629f09b9765a1608856e943b5ee1e9ed2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-24 05:30:18 +0000

    boa: add 0.20 bottle.

--- Formula/b/boa.rb
+++ Formula/b/boa.rb
@@ -6,6 +6,15 @@ class Boa < Formula
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/boa-dev/boa.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c6511d59e732eb4093f55266cb82177c9561d461cdf6db8a300f6993c8f87e1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "065f0361e0ab41501e92db94e1cf043c5871ab29fbff039aed67255455a874d4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "08fb899017323c8854209da3d3b9190337803ef635a8b6a7d960f17141ccda6b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a2f8fba394237d297214d94a2f78a2da249cf19b481a7dcf0b41cde97b9ea2e6"
+    sha256 cellar: :any_skip_relocation, ventura:       "e55c1d32124a5dc83dad035ec5d3e6631b10b3d9cf41a6a92c949fc47e3d29c4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28ed6dc310185ff69da5a53d22b4c557b63dfa736557687ec11cc6591089b774"
+  end
+
   depends_on "rust" => :build
 
   def install
