b81b9d9def3a55e11be5884442f5c07c2feee35b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-24 05:12:31 +0000

    goshs: add 1.0.8 bottle.

--- Formula/g/goshs.rb
+++ Formula/g/goshs.rb
@@ -6,6 +6,15 @@ class Goshs < Formula
   license "MIT"
   head "https://github.com/patrickhener/goshs.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2aa7632d01f991f5155d1a8cace4020bffcef199adc9c768d59c4074d4359d9c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2aa7632d01f991f5155d1a8cace4020bffcef199adc9c768d59c4074d4359d9c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2aa7632d01f991f5155d1a8cace4020bffcef199adc9c768d59c4074d4359d9c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7c5a9a91e9529df0afd5df46aacf4ebd7dc408cef63f0b94d48cd1604a397108"
+    sha256 cellar: :any_skip_relocation, ventura:       "7c5a9a91e9529df0afd5df46aacf4ebd7dc408cef63f0b94d48cd1604a397108"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bb22983ed9ad12e9395b1683a49bead9de1ece58facc2fe281944eb9fdd0c610"
+  end
+
   depends_on "go" => :build
 
   def install
