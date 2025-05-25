cc86c7392467e751ddbdef248fb943d0cbfc3fb7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-25 01:21:03 +0000

    qnm: add 2.10.4 bottle.

--- Formula/q/qnm.rb
+++ Formula/q/qnm.rb
@@ -5,6 +5,15 @@ class Qnm < Formula
   sha256 "205044b4bbc4637917ac55f936c17b2763e622040cfa84acb1a0289b50b21098"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "360bcebfd1a8e6fba957b45863d7601122002474f16895e74e6fbc56100939f8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "360bcebfd1a8e6fba957b45863d7601122002474f16895e74e6fbc56100939f8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "360bcebfd1a8e6fba957b45863d7601122002474f16895e74e6fbc56100939f8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ba15766ca2e00720af7c0022eb8c05dbe2bcf2744fd61fa5ededd82da4a0d441"
+    sha256 cellar: :any_skip_relocation, ventura:       "ba15766ca2e00720af7c0022eb8c05dbe2bcf2744fd61fa5ededd82da4a0d441"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "360bcebfd1a8e6fba957b45863d7601122002474f16895e74e6fbc56100939f8"
+  end
+
   depends_on "node"
 
   def install
