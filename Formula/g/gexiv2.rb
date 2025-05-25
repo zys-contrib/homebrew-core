1059e1e6634387ddc150af1a4fa3bb653100a6e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-25 10:58:27 +0000

    gexiv2 0.14.5

--- Formula/g/gexiv2.rb
+++ Formula/g/gexiv2.rb
@@ -3,8 +3,8 @@ class Gexiv2 < Formula
   homepage "https://wiki.gnome.org/Projects/gexiv2"
   # release info on the website might lag behind, refer to gitlab tags for latest release info
   # see discussions in https://gitlab.gnome.org/GNOME/gexiv2/-/issues/77
-  url "https://download.gnome.org/sources/gexiv2/0.14/gexiv2-0.14.3.tar.xz"
-  sha256 "21e64d2c56e9b333d44fef3f2a4b25653d922c419acd972fa96fab695217e2c8"
+  url "https://download.gnome.org/sources/gexiv2/0.14/gexiv2-0.14.5.tar.xz"
+  sha256 "0913c53daabab1f1ab586afd55bb55370796f2b8abcc6e37640ab7704ad99ce1"
   license "GPL-2.0-or-later"
 
   bottle do
