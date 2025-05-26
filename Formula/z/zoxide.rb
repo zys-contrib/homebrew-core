ff51b102508108e568364daac0b9f3febafa7ba9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-26 23:31:30 +0000

    zoxide 0.9.8

--- Formula/z/zoxide.rb
+++ Formula/z/zoxide.rb
@@ -1,8 +1,8 @@
 class Zoxide < Formula
   desc "Shell extension to navigate your filesystem faster"
   homepage "https://github.com/ajeetdsouza/zoxide"
-  url "https://github.com/ajeetdsouza/zoxide/archive/refs/tags/v0.9.7.tar.gz"
-  sha256 "d93ab17a01de68529ef37b599524f882dc654835cc121a9097dd3f2952fd207e"
+  url "https://github.com/ajeetdsouza/zoxide/archive/refs/tags/v0.9.8.tar.gz"
+  sha256 "1b276edbf328aafc86afe1ebce41f45ccba3a3125412e89c8c5d8e825b0c7407"
   license "MIT"
 
   bottle do
