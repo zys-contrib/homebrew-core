c3273a9e047c78894d0c07050626a51f81adaab7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-26 20:06:15 +0000

    mdbook 0.4.51

--- Formula/m/mdbook.rb
+++ Formula/m/mdbook.rb
@@ -1,8 +1,8 @@
 class Mdbook < Formula
   desc "Create modern online books from Markdown files"
   homepage "https://rust-lang.github.io/mdBook/"
-  url "https://github.com/rust-lang/mdBook/archive/refs/tags/v0.4.50.tar.gz"
-  sha256 "2d86ec7769f4ff3068e86a9bbc46bec4f0b78cffb6021dd24c82a4d44b5d27f6"
+  url "https://github.com/rust-lang/mdBook/archive/refs/tags/v0.4.51.tar.gz"
+  sha256 "de5ee916157784e32451b81de01cc4c669b73e651e2db00c7b1809254dbb6259"
   license "MPL-2.0"
   head "https://github.com/rust-lang/mdBook.git", branch: "master"
 
