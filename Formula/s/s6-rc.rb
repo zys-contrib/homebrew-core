3d674f00391c0339f5da02a30549689386dbd381
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-26 18:42:25 +0000

    s6-rc: add 0.5.6.0 bottle.

--- Formula/s/s6-rc.rb
+++ Formula/s/s6-rc.rb
@@ -5,6 +5,16 @@ class S6Rc < Formula
   sha256 "81277f6805e8d999ad295bf9140a909943b687ffcfb5aa3c4efd84b1a574586e"
   license "ISC"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ffb8c25c6c698f13c255d694dfc6218116857cf633b6764a6ee4c026de314e20"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "120ab3b0a9ca7cf2db5f0b27ee6dffcebb3560e718956f5cab8693655d9aac14"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "06402eee4fafa9c0bcdbcce7dcd88054b9f142263a8ce6019ee1ec54b534beb3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "66888c1bab3552b773e18e694b52ce22bc525d343cdaf3622fded2921832c642"
+    sha256 cellar: :any_skip_relocation, ventura:       "486096d10e4ce41f00e27aa5d62d30c9acab44c539542db7ac34afc36ac25571"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e84a61c37b4988f98170ca737ea6b2fc95720619bb07253ea1e1a4d45233e36a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26be7908d0ae2d584614d6aefe7b4bc4ffc1b45dce5743b801f416fdf5310282"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "execline"
   depends_on "s6"
