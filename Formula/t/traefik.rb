acb3c728d9edd995bd340f399f504eba7d0a6d88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-27 14:30:20 +0000

    traefik 3.4.1

--- Formula/t/traefik.rb
+++ Formula/t/traefik.rb
@@ -1,8 +1,8 @@
 class Traefik < Formula
   desc "Modern reverse proxy"
   homepage "https://traefik.io/"
-  url "https://github.com/traefik/traefik/releases/download/v3.4.0/traefik-v3.4.0.src.tar.gz"
-  sha256 "da5dcdbf177c5008a157e4180b039a34a7ad10b710a9ce999545ac86d7c217e4"
+  url "https://github.com/traefik/traefik/releases/download/v3.4.1/traefik-v3.4.1.src.tar.gz"
+  sha256 "8d971571725057b2e8bdcc2441f9612f75fa3780cff62cfd52fbe3872786ca29"
   license "MIT"
   head "https://github.com/traefik/traefik.git", branch: "master"
 
