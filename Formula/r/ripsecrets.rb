0d39dfc346d613d124916178b070a321ca85b0a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-27 05:41:47 +0000

    ripsecrets: update 0.1.10 bottle.

--- Formula/r/ripsecrets.rb
+++ Formula/r/ripsecrets.rb
@@ -7,13 +7,13 @@ class Ripsecrets < Formula
   head "https://github.com/sirwart/ripsecrets.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8509e834f1ef8f9d72071ffbe9ea369f9e0806f3543848984f0ab6ccdb66c8b0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e149bd76eb597c10c6ce2ef09297ad024214b652a0f22c1e8c4e433da606e426"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "bab93d34726fe470b2b110f95f344702066dfea7b81711c6267dd2608c91bd7d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ca82959fc0f6457931e6dca1b364ff2016ee331739815452bcc75b9241028192"
-    sha256 cellar: :any_skip_relocation, ventura:       "b0686715c0fefea8ae7c6b1a329904f90f3d522cac0a2eb80b6e9f187e4fa4d3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ff4ae371281630bdaf268d9d2c52cb4ea2e5dae1ee13b4ef2e1ecb94572834e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c7a44e8f93b378dede7a94716d4d494927b8226954d824107e435bf6ce24630"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d24e8e746b554047afae26ffe5c263095e18162ca461c77805b8c7ea1b66f338"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e9cbb8da1f4eaf4c059803b870f39ef7d632a10dce9f152c0388513124f9a3e5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7e48364535dee58682e1eca9fcaa07bebb9be981436f544682df8ba332d91de1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8c84d3c02830f60ee6fe1f19633bac46dd586fbf07607072ece33b1f609f1a48"
+    sha256 cellar: :any_skip_relocation, ventura:       "b386766395ed06b864e9970e5aa91b7bcb0f7e4027625b5bc39361e443780fa7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e13826ae579b577c0dd07859560cb507a1417827395df93838902af32c451f82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab0ae451a6744962ab29372dbbfe142bd603cfc5e2bcac090c2dbd027a3bca74"
   end
 
   depends_on "rust" => :build
