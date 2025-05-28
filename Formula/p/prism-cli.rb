cec3711e9546a735f0e7c9d292a9e94722539a70
Branch Vincent <branchevincent@gmail.com>
2025-05-28 17:38:44 -0400

    prism-cli: revision bump (node 24.1.0)

--- Formula/p/prism-cli.rb
+++ Formula/p/prism-cli.rb
@@ -4,6 +4,7 @@ class PrismCli < Formula
   url "https://registry.npmjs.org/@stoplight/prism-cli/-/prism-cli-5.14.2.tgz"
   sha256 "61a3b45fbb0325b85fbe94baf836588de676c0db91c3b1f191738070c2f7410f"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8333b92f1cfcab951b0ebd6506a1c3449a31e7ea128bfdb68c09a43f8f925fe"
@@ -19,7 +20,13 @@ class PrismCli < Formula
 
   def install
     system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink libexec.glob("bin/*")
+
+    # Backport https://github.com/stoplightio/json-schema-ref-parser/commit/ab6ad5a312672c37fd5936d3cef78fbf0ad85234
+    node_modules = libexec/"lib/node_modules/@stoplight/prism-cli/node_modules"
+    inreplace node_modules/"@stoplight/json-schema-ref-parser/lib/resolvers/http.js",
+      'const { AbortController } = require("node-abort-controller");',
+      ""
   end
 
   test do
@@ -27,7 +34,7 @@ class PrismCli < Formula
     pid = spawn bin/"prism", "mock", "--port", port.to_s, "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/refs/tags/3.1.1/examples/v3.0/petstore.yaml"
 
     sleep 5
-    sleep 10 if OS.mac? && Hardware::CPU.intel?
+    sleep 15 if OS.mac? && Hardware::CPU.intel?
 
     system "curl", "http://127.0.0.1:#{port}/pets"
 
