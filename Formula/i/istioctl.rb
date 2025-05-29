87498a0179b642a9e96313ae3cc5f91825644e75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-29 17:06:43 +0000

    istioctl 1.26.1

--- Formula/i/istioctl.rb
+++ Formula/i/istioctl.rb
@@ -1,8 +1,8 @@
 class Istioctl < Formula
   desc "Istio configuration command-line utility"
   homepage "https://istio.io/"
-  url "https://github.com/istio/istio/archive/refs/tags/1.26.0.tar.gz"
-  sha256 "1498307624d9cb85d15f3e1892de6cf3d80fdfe888716b05ee4ff25f210c7663"
+  url "https://github.com/istio/istio/archive/refs/tags/1.26.1.tar.gz"
+  sha256 "98a5ddee12d5ca9bd57a5c5fb99b5b179a9cb1ebc2005e1fac680ce8255b90e4"
   license "Apache-2.0"
   head "https://github.com/istio/istio.git", branch: "master"
 
