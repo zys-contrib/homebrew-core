809fea98595d767af529ab91d3a4972c38094d7f
Rui Chen <rui@chenrui.dev>
2025-05-29 03:05:25 +0000

    kraken2: update 2.1.5 bottle.

--- Formula/k/kraken2.rb
+++ Formula/k/kraken2.rb
@@ -12,6 +12,7 @@ class Kraken2 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "a875a1a58dde84ca8651102d3e99fb1eb0ff2c1089fc5fc4587e56f0ac0ece33"
     sha256 cellar: :any_skip_relocation, sonoma:        "84e6e9db41f330e526dcdaa1061c530ecfc351f1f89004ce69695b480fffc195"
     sha256 cellar: :any_skip_relocation, ventura:       "43e3bb6870bb0d9cf4d5a9d09bc8850939922acca99ccfa56a84c9f77d9a3162"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba5490c61e588570bfbab8976c233dcf75d57ffe3113bfa800d6e8470f298b8f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ca42ce0aa2688d8ca4f9bb123ab1cf372a79e8944f511312a26b9da4ead3927"
   end
 
