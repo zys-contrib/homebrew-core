de720d7e518eca02aa0ff1397ba4a9e26beb67e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-29 02:34:45 +0000

    kraken2: add 2.1.5 bottle.

--- Formula/k/kraken2.rb
+++ Formula/k/kraken2.rb
@@ -6,6 +6,15 @@ class Kraken2 < Formula
   license "MIT"
   head "https://github.com/DerrickWood/kraken2.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ee275caf279a788ea6a7bce93190ab31165bbdc79fd1dc812ba120336a1918f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb22cec35e13ee86ec66e1f65f90a4b6454e418da5df13bf786e48a1dc846e8b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a875a1a58dde84ca8651102d3e99fb1eb0ff2c1089fc5fc4587e56f0ac0ece33"
+    sha256 cellar: :any_skip_relocation, sonoma:        "84e6e9db41f330e526dcdaa1061c530ecfc351f1f89004ce69695b480fffc195"
+    sha256 cellar: :any_skip_relocation, ventura:       "43e3bb6870bb0d9cf4d5a9d09bc8850939922acca99ccfa56a84c9f77d9a3162"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ca42ce0aa2688d8ca4f9bb123ab1cf372a79e8944f511312a26b9da4ead3927"
+  end
+
   depends_on "python@3.13"
   depends_on "wget"
 
