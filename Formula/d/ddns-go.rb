aa7071f4444e605bb22420698518ad49f48d9b11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-29 13:56:19 +0000

    ddns-go 6.9.4

--- Formula/d/ddns-go.rb
+++ Formula/d/ddns-go.rb
@@ -1,8 +1,8 @@
 class DdnsGo < Formula
   desc "Simple and easy-to-use DDNS"
   homepage "https://github.com/jeessy2/ddns-go"
-  url "https://github.com/jeessy2/ddns-go/archive/refs/tags/v6.9.3.tar.gz"
-  sha256 "898fbfdf774e1dd025bfe4c48b6b9cc6d4d16e8060009167ca3fae03e0e64d5b"
+  url "https://github.com/jeessy2/ddns-go/archive/refs/tags/v6.9.4.tar.gz"
+  sha256 "9430183bc24261060d1ff3353d6d9d1543a608f50d931953d64956629aaa67fe"
   license "MIT"
   head "https://github.com/jeessy2/ddns-go.git", branch: "master"
 
