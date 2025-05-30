15394bd1d24c4d9a77784134f75f229fb885ac3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-30 02:53:41 +0000

    qdmr: update 0.12.2 bottle.

--- Formula/q/qdmr.rb
+++ Formula/q/qdmr.rb
@@ -6,12 +6,12 @@ class Qdmr < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_sequoia: "466b1ea108063ed50d951b3e79694867255e395d516cbb43e294cbb9ffce4ca0"
-    sha256 arm64_sonoma:  "b16fb78360a1a05305e2f31dc1b6949e45271b57589b486e930bd77814d2b6cc"
-    sha256 arm64_ventura: "04817d917185b0e67cd0f9791108465ac2d01b54351931c2c902725f9c17bc91"
-    sha256 sonoma:        "86034b508621ad37972850dc34850dff0031e1ad7827b6ee69f60cc11e79b774"
-    sha256 ventura:       "43852bc488a99f1ea2eeeb638b59c24ddb30b8d559a45e0a978b26efca56b0e7"
-    sha256 x86_64_linux:  "7fbb488e9feba30be04cfa868546e46ea105bae7041549c7132a7e25eacb0910"
+    sha256 arm64_sequoia: "6ab16e6c0e074af48e283646dfe8fec3e4c43bd75a72c3d61e70c6aaaf7d52e1"
+    sha256 arm64_sonoma:  "9f328b9c8835430065a0b2fb6732b8af7bceb89f54fecc9e3069353327e00a04"
+    sha256 arm64_ventura: "1921e4934527288d3e837706cb5a53b74c46a89881f53ba09f8bb430a78577c2"
+    sha256 sonoma:        "9e07686e7cebb8ea130a9d1183541f3d8396e5fc0bd171e090317430e086e53f"
+    sha256 ventura:       "b47be9e66eed4887eadb16d6a0a1dd16a1f57943bf370342b06c1aef100a70fc"
+    sha256 x86_64_linux:  "7715c1833cdd017122b29d8966de514e21a908df7f77112feea00319a769ace2"
   end
 
   depends_on "cmake" => :build
