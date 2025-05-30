20b2d3fc6cad07c3fc0f8ae29a98071dc7db0284
MoonFruit <dkmoonfruit@gmail.com>
2025-05-30 13:58:56 +0800

    sqldiff 3.5.0

--- Formula/s/sqldiff.rb
+++ Formula/s/sqldiff.rb
@@ -1,9 +1,9 @@
 class Sqldiff < Formula
   desc "Displays the differences between SQLite databases"
   homepage "https://www.sqlite.org/sqldiff.html"
-  url "https://www.sqlite.org/2025/sqlite-src-3490200.zip"
-  version "3.49.2"
-  sha256 "c3101978244669a43bc09f44fa21e47a4e25cdf440f1829e9eff176b9a477862"
+  url "https://www.sqlite.org/2025/sqlite-src-3500000.zip"
+  version "3.50.0"
+  sha256 "af673f28f69b572b49bb1558c4f191fd66e31acb949468ad2b01b2b6ed8043a2"
   license "blessing"
 
   livecheck do
