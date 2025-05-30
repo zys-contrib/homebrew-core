6cdc275b752bcf526c1110fed1134ffdf6ed9b9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-30 16:20:32 +0000

    reckoner: add 6.1.0 bottle.

--- Formula/r/reckoner.rb
+++ Formula/r/reckoner.rb
@@ -6,6 +6,15 @@ class Reckoner < Formula
   license "Apache-2.0"
   head "https://github.com/FairwindsOps/reckoner.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "94837511a2176e73a13857a65e796c789b0eab21426754d8964fa5f23a70d830"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "94837511a2176e73a13857a65e796c789b0eab21426754d8964fa5f23a70d830"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "94837511a2176e73a13857a65e796c789b0eab21426754d8964fa5f23a70d830"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c306a999e8b9f99767918beaf886b698646bf0df84aaf0f84f8f2ba5be257dec"
+    sha256 cellar: :any_skip_relocation, ventura:       "c306a999e8b9f99767918beaf886b698646bf0df84aaf0f84f8f2ba5be257dec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "728e430d18ad959bd4722c12b93cc7f06462878e85de54d2ab069246e8326f63"
+  end
+
   depends_on "go" => :build
   depends_on "helm"
 
