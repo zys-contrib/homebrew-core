82a39a238ba32c2cc69b064b9a590e2624246447
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-31 05:43:37 +0000

    csvtk: update 0.34.0 bottle.

--- Formula/c/csvtk.rb
+++ Formula/c/csvtk.rb
@@ -7,12 +7,12 @@ class Csvtk < Formula
   head "https://github.com/shenwei356/csvtk.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "de4fd6a17277f4e9a178cab48b5ca6135c304b27fa1617a1b9ea49a3f96260d0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de4fd6a17277f4e9a178cab48b5ca6135c304b27fa1617a1b9ea49a3f96260d0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "de4fd6a17277f4e9a178cab48b5ca6135c304b27fa1617a1b9ea49a3f96260d0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "20deb9324aeacaf068a1edcf988890e0a1f15ff3b735599513fd028b871fa7ac"
-    sha256 cellar: :any_skip_relocation, ventura:       "20deb9324aeacaf068a1edcf988890e0a1f15ff3b735599513fd028b871fa7ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8ae2c10c57b6852b45ada75c178336f36b19e77f292ef25b65517baa8c52805"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fac1f71c7cd43fdf92ee9c4142af9df200117e097ba4e2c6b14610bd6f1f956e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fac1f71c7cd43fdf92ee9c4142af9df200117e097ba4e2c6b14610bd6f1f956e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fac1f71c7cd43fdf92ee9c4142af9df200117e097ba4e2c6b14610bd6f1f956e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f8aa4654478d26a028ff613490fa86e603ca73cedcab65d8680b9bef5ac4026d"
+    sha256 cellar: :any_skip_relocation, ventura:       "f8aa4654478d26a028ff613490fa86e603ca73cedcab65d8680b9bef5ac4026d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4cb6c6db444aad11c72af872e744a396562da0d2a27f08456b910354e2f18a2f"
   end
 
   depends_on "go" => :build
