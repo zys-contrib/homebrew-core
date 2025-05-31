0e81f0fcf4663066707b36a348fdccabfeed44b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-31 05:43:26 +0000

    croaring: update 4.3.3 bottle.

--- Formula/c/croaring.rb
+++ Formula/c/croaring.rb
@@ -7,13 +7,13 @@ class Croaring < Formula
   head "https://github.com/RoaringBitmap/CRoaring.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "36dc04f7eba347be669c6159e9b99bf7c03bd5c62bc9b66cd2bc38dccaea6daf"
-    sha256 cellar: :any,                 arm64_sonoma:  "04c90d9048f99501c87a184e64a779839b2ab337a8d99a78f0f56e78ebedd6d0"
-    sha256 cellar: :any,                 arm64_ventura: "957aa02e229aecbe48ad224ec86a409d5e5986f907daca6702520bd3e30bfeeb"
-    sha256 cellar: :any,                 sonoma:        "cd765f35ad55143577770139328c1f0660ed71cf1be81f13a89b2266b41f5c15"
-    sha256 cellar: :any,                 ventura:       "b515c494021189ff5023d5c88d28c79645a7cefc2a86beea254fb0a4dbe15d13"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e6002cb5e1ec57a7a2accdf8e091605e9f7a6fdbe01ef1209383d2ee0a4c31c5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cdfb6cf5645a3acdb5c5306da612877015bd5b24597cc8e75464ed6bd15e512a"
+    sha256 cellar: :any,                 arm64_sequoia: "da3280cac7c2a1682db618ade5c8e99e4fe5c7974415af6876e4fae7d42896b6"
+    sha256 cellar: :any,                 arm64_sonoma:  "e5e1f34175cdf5e7b288bbd266468295f5b0f4a47dbce3e9e0162b52d788708b"
+    sha256 cellar: :any,                 arm64_ventura: "72d5c0e583eef660240c7df8a088e397a367deaefa9a1fa4a46db1c982c64820"
+    sha256 cellar: :any,                 sonoma:        "fa987f6fa1b9d0cef284923f610fc9d5f7e21d509313d8072e881811f88396f0"
+    sha256 cellar: :any,                 ventura:       "d42071bbe6873d1697c36a6d8bbb95500e369dd8392177d9d312943928c60a8f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4846d6cc9976612c4455780e844b660481499d3bc91586543d21818ccc06426a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99b922f5a7549976baad51b5cec6dbd23d5c21313dce65046a43dd5e51da91ea"
   end
 
   depends_on "cmake" => :build
