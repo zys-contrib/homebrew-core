17d49469de040f9887c6f7f3f3a49fb5251e4eda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-31 07:57:23 +0000

    freebayes 1.3.10

--- Formula/f/freebayes.rb
+++ Formula/f/freebayes.rb
@@ -3,8 +3,8 @@ class Freebayes < Formula
   homepage "https://github.com/freebayes/freebayes"
   # pull from git tag to get submodules
   url "https://github.com/freebayes/freebayes.git",
-      tag:      "v1.3.9",
-      revision: "ab36d1f789c039ba872f5d911ce6ff09952dc329"
+      tag:      "v1.3.10",
+      revision: "b0d8efd9fa7f6612c883ec5ff79e4d17a0c29993"
   license "MIT"
   head "https://github.com/freebayes/freebayes.git", branch: "master"
 
@@ -28,73 +28,34 @@ class Freebayes < Formula
     sha256               x86_64_linux:  "8b76cc3e3db9575510c456b74286a1f5732e5495b3c1232311a2c0c220fc7e0f"
   end
 
-  depends_on "cmake" => :build # for vcflib
+  depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
-  depends_on "pybind11" => :build # for vcflib
   depends_on "simde" => :build
-  depends_on "wfa2-lib" => :build # for vcflib
-  depends_on "xz" => :build # for vcflib
+  depends_on "wfa2-lib" => :build
   depends_on "htslib"
+  depends_on "tabixpp"
 
-  uses_from_macos "bzip2" => :build # for vcflib
-  uses_from_macos "zlib" => :build # for vcflib
+  resource "intervaltree" do
+    url "https://github.com/ekg/intervaltree/archive/refs/tags/v0.1.tar.gz"
+    sha256 "7ba41f164a98bdcd570f1416fde1634b23d3b0d885b11ccebeec76f58810c307"
 
-  on_macos do
-    depends_on "libomp" => :build # for vcflib
-  end
-
-  resource "vcflib" do
-    url "https://github.com/vcflib/vcflib/releases/download/v1.0.12/vcflib-1.0.12-src.tar.gz"
-    sha256 "cb1e78d675f06ae3b22ffed7e1dea317e4c7f826e51e5b5dabd80240efbe1019"
-
-    # Backport fix for using external `wfa2-lib`
+    # Fix to error: ‘numeric_limits’ is not a member of ‘std’
     patch do
-      url "https://github.com/vcflib/vcflib/commit/5e4edec2fba5d5a51dae7a9fe48d0252ade53857.patch?full_index=1"
-      sha256 "e7d6d433d837dd823916ef91fe0165bf4ba4f05c52fd4c9517aef7f80653a2a8"
+      url "https://github.com/ekg/intervaltree/commit/aa5937755000f1cd007402d03b6f7ce4427c5d21.patch?full_index=1"
+      sha256 "7ae1070e3f776f10ed0b2ea1fdfada662fcba313bfc5649d7eb27e51bd2de07b"
     end
-
-    # Apply open PR to help find `wfa2-lib` include directory
-    # PR ref: https://github.com/vcflib/vcflib/pull/413
-    patch do
-      url "https://github.com/vcflib/vcflib/commit/9f9237ff0e6b4887f0edfc88587957aa736ced7b.patch?full_index=1"
-      sha256 "bede43d22b4b47141cd90edc4df90f65b9ac41e9598c2b05b2fe7fa84ea51aa8"
-    end
-  end
-
-  # Apply open PR to help Meson locate vcflib and wfa2 libraries
-  # PR ref: https://github.com/freebayes/freebayes/pull/822
-  patch do
-    url "https://github.com/freebayes/freebayes/commit/b458396e1acbad3983c70c202a6db2b3711a8eac.patch?full_index=1"
-    sha256 "b5c7d855d4d66c6c96dada307e82ccbf0b6904a25928c4f3e163f52e178b7907"
-  end
-
-  # Apply open PR to fix build when using git submodules
-  # PR ref: https://github.com/freebayes/freebayes/pull/823
-  patch do
-    url "https://github.com/freebayes/freebayes/commit/35eeacb6468fdce25233a33f7216f4e776d381f9.patch?full_index=1"
-    sha256 "1b6f0bb1e369a4b11e9a7754f3b789035b39cc5d9e3dbbac84fde21893f0d9be"
   end
 
   def install
-    resource("vcflib").stage do
-      rm_r(["contrib/WFA2-lib", "contrib/tabixpp/htslib"]) # avoid bundled libraries
+    # add contrib to include directories
+    inreplace "meson.build", "incdir = include_directories(", "incdir = include_directories('contrib',"
 
-      system "cmake", "-S", ".", "-B", "build",
-                      "-DBUILD_DOC=OFF",
-                      "-DBUILD_ONLY_LIB=ON",
-                      "-DZIG=OFF",
-                      *std_cmake_args(install_prefix: buildpath/"vendor")
-      system "cmake", "--build", "build"
-      system "cmake", "--install", "build"
-      system "make", "-C", "contrib/intervaltree", "install", "DESTDIR=", "PREFIX=#{buildpath}/vendor"
-      (buildpath/"vendor/include").install "contrib/tabixpp/tabix.hpp"
-
-      ENV.append_path "LIBRARY_PATH", buildpath/"vendor/bin"
-      ENV.append_to_cflags "-I#{buildpath}/vendor/include"
-      ENV.append_to_cflags "-I#{buildpath}/vendor/include/vcflib"
-    end
+    # install intervaltree
+    (buildpath/"contrib/intervaltree").install resource("intervaltree")
+    # add tabixpp to include directories
+    ENV.append_to_cflags "-I#{Formula["tabixpp"].opt_include} -L#{Formula["tabixpp"].opt_lib} -ltabix"
 
     # Set prefer_system_deps=false as we don't have formulae for these and some are not versioned/tagged
     system "meson", "setup", "build", "-Dcpp_std=c++14", "-Dprefer_system_deps=false", *std_meson_args
