ac6c2b7f082261e0a4924685f6a29197b829e114
Jack Hsu <jack.hsu@gmail.com>
2025-06-02 09:16:55 -0400

    nx 21.0.4 (new formula)

---
+++ Formula/n/nx.rb
@@ -0,0 +1,32 @@
+class Nx < Formula
+  desc "Smart, Fast and Extensible Build System"
+  homepage "https://nx.dev"
+  url "https://registry.npmjs.org/nx/-/nx-21.0.4.tgz"
+  sha256 "dd1bf64358661a2d14dd027f8b0c6d8eea051afff4ec8206407bdb9335991f9f"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"package.json").write <<~JSON
+      {
+        "name": "@acme/repo",
+        "version": "0.0.1",
+        "scripts": {
+          "test": "echo 'Tests passed'"
+        }
+      }
+    JSON
+
+    system bin/"nx", "init", "--no-interactive"
+    assert_path_exists testpath/"nx.json"
+
+    output = shell_output("#{bin}/nx 'test'")
+    assert_match "Successfully ran target test", output
+  end
+end
