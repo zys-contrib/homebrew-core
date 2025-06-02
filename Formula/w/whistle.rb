3d8ac21d0f0efb404b1a1e327bc62b52538752eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-02 17:31:52 +0000

    whistle 2.9.99
    whistle: split bottles
    
    whistle: drop bottle block due to issue with `:all` bottle
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/whistle.rb
+++ Formula/w/whistle.rb
@@ -1,13 +1,9 @@
 class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
-  url "https://registry.npmjs.org/whistle/-/whistle-2.9.98.tgz"
-  sha256 "eae840bd72e5a40c46f32669c1e67b30a959e614e015723a9343458ced6ce4fe"
+  url "https://registry.npmjs.org/whistle/-/whistle-2.9.99.tgz"
+  sha256 "ee6c285e2d6a36a018b6bdd30ac4027e864d457e6aa008b78c82f080d19b47a9"
   license "MIT"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "c0de5233a6fbbf508637af294b958271b8bb327e11afb966e767598bdf527fcc"
-  end
 
   depends_on "node"
 
