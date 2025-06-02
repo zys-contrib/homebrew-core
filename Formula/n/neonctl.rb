0c03b162e4ab981dff3596734321a9dcd862863a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-02 12:16:11 +0000

    neonctl: update 2.10.1 bottle.

--- Formula/n/neonctl.rb
+++ Formula/n/neonctl.rb
@@ -6,13 +6,13 @@ class Neonctl < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34690b17434aac6e9badb3a6ab988ffd767e1340d488551d85d4b0b78ec6b29c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "34690b17434aac6e9badb3a6ab988ffd767e1340d488551d85d4b0b78ec6b29c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "34690b17434aac6e9badb3a6ab988ffd767e1340d488551d85d4b0b78ec6b29c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "360d71f2e532a47fc45749eabb98e7b965f5b20f380725b6d9a7e605656ecb23"
-    sha256 cellar: :any_skip_relocation, ventura:       "360d71f2e532a47fc45749eabb98e7b965f5b20f380725b6d9a7e605656ecb23"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "34690b17434aac6e9badb3a6ab988ffd767e1340d488551d85d4b0b78ec6b29c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34690b17434aac6e9badb3a6ab988ffd767e1340d488551d85d4b0b78ec6b29c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4fe8d2d0403e5b91e0486c3092f836a7bbb945af1e70b5287652909ed8d6a54d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4fe8d2d0403e5b91e0486c3092f836a7bbb945af1e70b5287652909ed8d6a54d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4fe8d2d0403e5b91e0486c3092f836a7bbb945af1e70b5287652909ed8d6a54d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "52b18e57a55ec58d254189d256c57fc3c5f71242dfcd65a65f8406b1d95a6c94"
+    sha256 cellar: :any_skip_relocation, ventura:       "52b18e57a55ec58d254189d256c57fc3c5f71242dfcd65a65f8406b1d95a6c94"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4fe8d2d0403e5b91e0486c3092f836a7bbb945af1e70b5287652909ed8d6a54d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4fe8d2d0403e5b91e0486c3092f836a7bbb945af1e70b5287652909ed8d6a54d"
   end
 
   depends_on "node"
