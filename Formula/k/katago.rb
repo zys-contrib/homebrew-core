0255e7b28fa96ad07e9d00541135aa7cbe77782f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-03 02:41:04 +0000

    katago 1.16.1

--- Formula/k/katago.rb
+++ Formula/k/katago.rb
@@ -3,8 +3,8 @@ class Katago < Formula
   homepage "https://github.com/lightvector/KataGo"
   # Occasionally check upstream docs in case recommended model/network is changed.
   # Ref: https://github.com/lightvector/KataGo?tab=readme-ov-file#other-questions
-  url "https://github.com/lightvector/KataGo/archive/refs/tags/v1.16.0.tar.gz"
-  sha256 "1786772c8490fb513522319554dfb41d93ecae4fb35e1b70249f3fe3c75c6cc1"
+  url "https://github.com/lightvector/KataGo/archive/refs/tags/v1.16.1.tar.gz"
+  sha256 "5e004042dc1ee8185b8fb7a807e3d421d09349dccb858e41ffe2e9b96a4173d0"
   license all_of: [
     "MIT",
     "CC0-1.0", # g170 resources
