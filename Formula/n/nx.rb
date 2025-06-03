9d65b9092c852443e345cff33eb4bcefa10f9e54
Nanda H Krishna <me@nandahkrishna.com>
2025-06-03 02:04:00 +0000

    nx: add 21.0.4 bottle.

--- Formula/n/nx.rb
+++ Formula/n/nx.rb
@@ -5,6 +5,15 @@ class Nx < Formula
   sha256 "dd1bf64358661a2d14dd027f8b0c6d8eea051afff4ec8206407bdb9335991f9f"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "c1d5a9df1f46bf4e9216cd2d88153a9b32a6263fbbb6f700898792a28ad0cb28"
+    sha256 cellar: :any,                 arm64_sonoma:  "c1d5a9df1f46bf4e9216cd2d88153a9b32a6263fbbb6f700898792a28ad0cb28"
+    sha256 cellar: :any,                 arm64_ventura: "c1d5a9df1f46bf4e9216cd2d88153a9b32a6263fbbb6f700898792a28ad0cb28"
+    sha256 cellar: :any,                 sonoma:        "aaf2aab4c5272fbcdfbc4f09a8b5c85155319c34c4ea541a3bccbe372dd91743"
+    sha256 cellar: :any,                 ventura:       "aaf2aab4c5272fbcdfbc4f09a8b5c85155319c34c4ea541a3bccbe372dd91743"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a363a3395436db3f2721fcc40315d7c50a79ebeaaa480613df61ff7d77b04450"
+  end
+
   depends_on "node"
 
   def install
