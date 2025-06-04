a459403fcaa04f462f8233b9be318074cbaa8cd4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-04 23:15:13 +0000

    docker-machine: update 0.16.2-gitlab.37 bottle.

--- Formula/d/docker-machine.rb
+++ Formula/d/docker-machine.rb
@@ -8,12 +8,12 @@ class DockerMachine < Formula
   head "https://gitlab.com/gitlab-org/ci-cd/docker-machine.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7dca3494e255605f551cf8ebc6c3482a34a306bb70ce62792128bb81caff4a25"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7dca3494e255605f551cf8ebc6c3482a34a306bb70ce62792128bb81caff4a25"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7dca3494e255605f551cf8ebc6c3482a34a306bb70ce62792128bb81caff4a25"
-    sha256 cellar: :any_skip_relocation, sonoma:        "de1ef581df46f4cdc87934eacc457c0fea934a4ce200b9b4ec564fbd025b1f6f"
-    sha256 cellar: :any_skip_relocation, ventura:       "de1ef581df46f4cdc87934eacc457c0fea934a4ce200b9b4ec564fbd025b1f6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4bda465094ed8e540c294a3e785e59302390a0dd79bb9bb1d071c33024b7aaaa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d2fe267cd164e4a07b4dfe00cd2cc9f27f43a603d3dab2430e902a027ba3f778"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d2fe267cd164e4a07b4dfe00cd2cc9f27f43a603d3dab2430e902a027ba3f778"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d2fe267cd164e4a07b4dfe00cd2cc9f27f43a603d3dab2430e902a027ba3f778"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1b39735ecee37deed9255136719330ca95e3064fa18454bc3b7f4747f0ea41e6"
+    sha256 cellar: :any_skip_relocation, ventura:       "1b39735ecee37deed9255136719330ca95e3064fa18454bc3b7f4747f0ea41e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bfcc139368340357e8ee7a4262d033aca90567663458d725a3bee1f57aa2f117"
   end
 
   depends_on "go" => :build
