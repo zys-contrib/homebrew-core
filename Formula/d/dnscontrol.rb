d3a3c88f63cde099f61764565c9c50c46b0ff208
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-04 16:58:32 +0000

    dnscontrol 4.21.0

--- Formula/d/dnscontrol.rb
+++ Formula/d/dnscontrol.rb
@@ -1,8 +1,8 @@
 class Dnscontrol < Formula
   desc "Synchronize your DNS to multiple providers from a simple DSL"
   homepage "https://dnscontrol.org/"
-  url "https://github.com/StackExchange/dnscontrol/archive/refs/tags/v4.20.0.tar.gz"
-  sha256 "61fae2bf6fe20a8bb2f4b3313ea4c3add3068e280a2fb74b02e18c8fbe65c17b"
+  url "https://github.com/StackExchange/dnscontrol/archive/refs/tags/v4.21.0.tar.gz"
+  sha256 "fb28227d0b94e0fa645989ea811dde9ec1ba272fb8ed5b1011788a710ffde3a8"
   license "MIT"
   version_scheme 1
   head "https://github.com/StackExchange/dnscontrol.git", branch: "main"
