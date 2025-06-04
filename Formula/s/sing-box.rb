950e344f432ffc5290a9b35430941ee036001cf5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-04 15:34:45 +0000

    sing-box: update 1.11.12 bottle.

--- Formula/s/sing-box.rb
+++ Formula/s/sing-box.rb
@@ -7,12 +7,12 @@ class SingBox < Formula
   head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e614ef31a63a5ad8c66bd63a99c92fe521bb276110625591354b3a4d2b004943"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "456d9d83214a7814ca33f91b4dda4b8418bd68d7e1f5b7ca411e3a4b5dcf9ad1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0f0a00c0eb31ac9c224abf4872fb48c4fad1e9e39018c5c3a18077a4fc3d7c8c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "97b8ad1975e095e1e0d397b35610f8d626f3d760a98302c84b04b843996d3dfb"
-    sha256 cellar: :any_skip_relocation, ventura:       "d0174842a207ab6c789d838ba3a15504ec169940a7182d5a1e683a6da447d452"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7fb6c95e210fe750d8655672c0de2465f2d815c91ad0ac27fc8690e3b676eba0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1227b748b70d882fa87d7fe34090fc51a7d724f105736831aa910e566406ab72"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8ea24bba8a284b70e4a20d1a03badbd9b5bae05ba598e4e8bb9d744faf98c8a9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "02111c8645f8eb43a58e7186de7abd75ef3a9dd8ad6909eb2ada1da45f3fd867"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6e80af6c7fba224dfac60957b7d4d52d61618a710257be28d722dc4aee57af10"
+    sha256 cellar: :any_skip_relocation, ventura:       "80c206dbb14413b872bdab5d4ee60b81b968e54f88f41c0ac7a17ee2e608b33c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3020f190e0d157b4d56bcad3fd5ffe4f1c790ff9071155f890523b7122d8b2e"
   end
 
   depends_on "go" => :build
