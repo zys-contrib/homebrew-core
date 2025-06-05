77010304719f0c71e220add950bd39d52dbc3991
Kevin Albertson <kevin.albertson@mongodb.com>
2025-06-05 14:40:12 -0400

    mongo-c-driver@1: create v1 version
    Copy formula to create separate v1 version.
    
    Remove `bottle do` from v1 to silence `brew audit` warning:
    ```
    New formulae in homebrew/core should not have a `bottle do` block
    ```
    
    Remove head references from v1 to silence `brew audit` warning:
    
    ```
    Versioned formulae should not have a `head` spec
    ```
    
    Add `keg_only: versioned_formula` to silence `brew audit` warning:
    
    ```
    Versioned formulae in homebrew/core should use `keg_only :versioned_formula`
    ```
    
    This may not be strictly needed. v2 and v1 use different suffixes.
    
    Update v1 from 1.30.2 to 1.30.4
    
    Update `livecheck` to limit to 1.x regex.
    
    Add `deprecate!` call.

---
+++ Formula/m/mongo-c-driver@1.rb
@@ -0,0 +1,47 @@
+class MongoCDriverAT1 < Formula
+  desc "C driver for MongoDB"
+  homepage "https://github.com/mongodb/mongo-c-driver"
+  url "https://github.com/mongodb/mongo-c-driver/archive/refs/tags/1.30.4.tar.gz"
+  sha256 "03e484ff8b382ad0ddf03fbf70e88a82292d753ac2fbf37bac67c2860117b0a9"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(1(?:\.\d+)+)$/i)
+  end
+
+  keg_only :versioned_formula
+
+  deprecate! date: "2026-04-01", because: :unmaintained
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "sphinx-doc" => :build
+  depends_on "openssl@3"
+
+  uses_from_macos "zlib"
+
+  def install
+    File.write "VERSION_CURRENT", version.to_s
+    inreplace "src/libmongoc/src/mongoc/mongoc-config.h.in", "@MONGOC_CC@", ENV.cc
+
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    (pkgshare/"libbson").install "src/libbson/examples"
+    (pkgshare/"libmongoc").install "src/libmongoc/examples"
+  end
+
+  test do
+    system ENV.cc, "-o", "test", pkgshare/"libbson/examples/json-to-bson.c",
+      "-I#{include}/libbson-1.0", "-L#{lib}", "-lbson-1.0"
+    (testpath/"test.json").write('{"name": "test"}')
+    assert_match "\u0000test\u0000", shell_output("./test test.json")
+
+    system ENV.cc, "-o", "test", pkgshare/"libmongoc/examples/mongoc-ping.c",
+      "-I#{include}/libmongoc-1.0", "-I#{include}/libbson-1.0",
+      "-L#{lib}", "-lmongoc-1.0", "-lbson-1.0"
+    assert_match "No suitable servers", shell_output("./test mongodb://0.0.0.0 2>&1", 3)
+  end
+end
