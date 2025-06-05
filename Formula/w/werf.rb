20f09330aeae771a75a31119641f2af085eeec8a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-05 14:54:30 +0000

    werf: update 2.37.1 bottle.

--- Formula/w/werf.rb
+++ Formula/w/werf.rb
@@ -15,13 +15,13 @@ class Werf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "23d8755d4f84a1206266b5ce2a4d58b742a029fab341dbe961abd5b79792a00f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb11fbdc62e92f9f98e4016f2b2133e0cf664f55f67f2cfb04c0555f75da195b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6b15a86cf23876da0cc2563e675443a855ebbfbd8e5c961d2be6edec00892bb1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6604f92ec6bfa90ff64b387162a4e6212ae96d48ad0e35896ba0482db26cb01e"
-    sha256 cellar: :any_skip_relocation, ventura:       "71609c5a6566ec49eb27b1600a4ed2959b9c803716a4550b758a05af6a10406e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9cfd0dcd98a09f62760ee08f53bd4ba490c407f5d41003d19d80487022494d87"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2a4db5ae90e0efb61a11fd6c8ad3192d6d6857a0b3e8a2aa93c4a504a47fbd1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cac72e78a8ce2d07de7b612e3b5b6b31aa96296e47fb3fb975f49b5f5b1eac1e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bbca9ff7aef1d422abc2a07dea8b02316b3a7723799625c2f094081afcdcb7d4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "39c2a12b22e9e6f61807c6977b55236131863dab41d90244cf30b4c9ad66fc55"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e83f37c2ff00ec044cb94e9bf7987e37ffa1f99e8b7360f2dd94f6c5bfad6f56"
+    sha256 cellar: :any_skip_relocation, ventura:       "9ebdc1b06ad4038380f25f96cf3ec3613dc9485b44c7e3f667fbbd2d3c22751a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fe7fe2e865ad88f6cc94d6659d149124e59f418502f8ef8dc2de24790d4fd6c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "220ff0484f1900edb67984738189befedb9dd9bf93437bd1ea73e97f242f75b6"
   end
 
   depends_on "go" => :build
