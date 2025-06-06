17bef2144e9b5e1d189673929707eea66c6e6adf
Rui Chen <rui@chenrui.dev>
2025-06-06 14:40:18 +0000

    toml-bombadil: update 4.2.0 bottle.

--- Formula/t/toml-bombadil.rb
+++ Formula/t/toml-bombadil.rb
@@ -12,6 +12,7 @@ class TomlBombadil < Formula
     sha256 cellar: :any,                 arm64_ventura: "b4e073f82e21d3a73f1ca641524f1cd5b10891dba22e4db989aa1909cafa2477"
     sha256 cellar: :any,                 sonoma:        "a26d4a4ac5a06a3ebaf3ccf6e6c54f1c8d37faed1fb33f43a2ee89c448aa4df5"
     sha256 cellar: :any,                 ventura:       "e1b81c25d689463b534a94f6d6d3a0ccf9987eed52b874e8c4a8f80891b5fa71"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "afbfb3763e73ccf60daddd2d636827817ef09d072f4de66a5bae6bb54362cd36"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "63847b627610a6981f8afe87c9de344b1cad2ac95a6fdfaff3af7b8071235d7a"
   end
 
