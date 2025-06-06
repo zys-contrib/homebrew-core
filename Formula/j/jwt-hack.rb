b80455bb663569381bf16f83f1c58a69ca341a0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-06 15:24:46 +0000

    jwt-hack: add 2.0.0 bottle.

--- Formula/j/jwt-hack.rb
+++ Formula/j/jwt-hack.rb
@@ -6,6 +6,15 @@ class JwtHack < Formula
   license "MIT"
   head "https://github.com/hahwul/jwt-hack.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a873fbf22a7fdda5a65150c817257cefc89ccd562286ede59d01e4a970e74847"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ccc825f3b87b0bfe0e309e217cac7f8a10c2d9a5e6bfb103c47818ace888eeef"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0e8a8e363356cbe92fb51fe20f0a2421ad1bd68109152e6d249adfe274b42008"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5dbe60466f3be0d379c5a049637831e97dcdeebe0b88c4ef7dbbd5be57ef8492"
+    sha256 cellar: :any_skip_relocation, ventura:       "ee3e1ba9b675a289aa00161021d983c400a48a2295844a23be993044697afe59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a76d31e93d7bd5557503096ba4e09c2add33e4ce688b98fad157956d88c0264"
+  end
+
   depends_on "rust" => :build
 
   def install
