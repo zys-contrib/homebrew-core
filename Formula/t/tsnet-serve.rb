1879730eadc21ba3dd9b152ab05f2ce5d283173c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-07 14:39:59 +0000

    tsnet-serve: add 1.2.2 bottle.

--- Formula/t/tsnet-serve.rb
+++ Formula/t/tsnet-serve.rb
@@ -6,6 +6,15 @@ class TsnetServe < Formula
   license "MIT"
   head "https://github.com/shayne/tsnet-serve.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "331db5b5052dbd753feece4c2b53f26bc60bbd5043366272f373847a3bff3c90"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c1174c09fd0bff9359d086b494d1080ab68b3262e4ab3ec06d3cd3cfab68961b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b83bec3f46ea89ec189f4cbb236eff876e77ef2e3c38766790b3a188c02731a0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4cf5f5587606b7eccf13cc6ebf99f760968fb95c44e205921d576bffa6fea477"
+    sha256 cellar: :any_skip_relocation, ventura:       "39b8503b95a78e17f6c1a9e2add4519380ad11a556f6cc8fc92998ea876b25df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61082ea4b3cf7a31efc98274928fd075e04aa6bf8b3373ce007edbb6b964f86c"
+  end
+
   depends_on "go" => :build
 
   def install
