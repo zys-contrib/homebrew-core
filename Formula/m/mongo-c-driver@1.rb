527bba935ea23c4f792653f0be4aa8959cbbd6ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-07 14:39:13 +0000

    mongo-c-driver@1: add 1.30.4 bottle.

--- Formula/m/mongo-c-driver@1.rb
+++ Formula/m/mongo-c-driver@1.rb
@@ -10,6 +10,16 @@ class MongoCDriverAT1 < Formula
     regex(/^v?(1(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "11ec4903de5e8816f1e3670644995240062bf6fb7f55a17c76a571b9b0576f51"
+    sha256 cellar: :any,                 arm64_sonoma:  "679d591289a2da855fb9091c41518207af700dd1ea888be468707960c228f705"
+    sha256 cellar: :any,                 arm64_ventura: "de9c0bd3f75edf2fea59d2cccf791980ea7c0fa0cf6dfa1aa6254584e9740595"
+    sha256 cellar: :any,                 sonoma:        "aec28e36191558d3509704146f72ea9d3f25b61f1c555cda35ed8a670cb0ea6c"
+    sha256 cellar: :any,                 ventura:       "2c05d4458319c3e7dede70c30043219e10a28219f6f0c544c98930cb81594c8d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "50c902b7643871073f781dd801c0f99bac8f9d966ead902cd3f594131f6b562c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9009dc4549e27ad5c5db699f435ed379354c5da6aa3ca69a0b1ea144002b97c"
+  end
+
   keg_only :versioned_formula
 
   deprecate! date: "2026-04-01", because: :unmaintained
