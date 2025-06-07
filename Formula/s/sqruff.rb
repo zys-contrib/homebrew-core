f8319c22ae3a148f9e0cc77ac24016643ae05aae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-07 18:02:20 +0000

    sqruff: update 0.26.4 bottle.

--- Formula/s/sqruff.rb
+++ Formula/s/sqruff.rb
@@ -7,12 +7,13 @@ class Sqruff < Formula
   head "https://github.com/quarylabs/sqruff.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0010a615c779365c01f381b4d3e3589b60fa3305a83cfe54379a32f1f0c76cf8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eece37504ea2e0c4fcf57c277934494991ddeb710bd425a9035f4719ad814228"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "537879681a0399d1fb8ff9c261e87e2e0f62b193e741799a76c64c339fd221a2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9726d2c6b1bab7bf0beb919d514183bd5f6e0f5ae780ed382b16515f80874075"
-    sha256 cellar: :any_skip_relocation, ventura:       "a2ae1ed5ad789f3b47c43c540769d369110a8d702b0bad423dde2fbf911b1025"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e7102fc621958ae4a0270b7acc5ad118b234cea2ee8c21ef2b5130cd4dd8339c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "58a6c9ed797bfbbd3ffa6133bb0019ade5605663be5d9079df0998297a1148ad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "294002a6947b6ba034a3b6cd71a3e6fc439dec853f2273f2b84edbacf0fe2501"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a7f78076b0d9c80c96a2ccc93880c786271cc3f3db313d2a20d250662a6aa5cf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "31d318233e21e7fc67764361f0264369a6d417cdb99a4816158f949cbb2aa725"
+    sha256 cellar: :any_skip_relocation, ventura:       "5174488c786c2f3cb5b55a193cd35b03a5e2e8e32ae11c8514aca24192142f2f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ca2f76f8b4ac13aacc7d75c68d714ec099f4e84b84577c21d3eb120fbfa3e7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7d72f5a591fd3c608cc29b6a8f8ce6bd7f24b67360c59b41a76ddced4532f9d"
   end
 
   depends_on "rust" => :build
