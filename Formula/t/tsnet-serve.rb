198ddcaac7351c4a1ad9cf87a57bfb6f3c723196
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-08 14:14:48 +0000

    tsnet-serve 1.2.3
    tsnet-serve: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tsnet-serve.rb
+++ Formula/t/tsnet-serve.rb
@@ -1,8 +1,8 @@
 class TsnetServe < Formula
   desc "Expose HTTP applications to a Tailscale Tailnet network"
   homepage "https://github.com/shayne/tsnet-serve"
-  url "https://github.com/shayne/tsnet-serve/archive/refs/tags/v1.2.2.tar.gz"
-  sha256 "8919abe9e4d7a54539f06369c4155df57bbf7427a6007c9d4e13a908847c7308"
+  url "https://github.com/shayne/tsnet-serve/archive/refs/tags/v1.2.3.tar.gz"
+  sha256 "74eb6bc77187dc96fa0ca5a615347314ad3016887942fc4b66445250a0cf50ab"
   license "MIT"
   head "https://github.com/shayne/tsnet-serve.git", branch: "main"
 
@@ -34,7 +34,6 @@ class TsnetServe < Formula
     sleep 1
 
     output = logfile.read
-    assert_match "starting tsnet-server (#{version})", output
     assert_match "proxying traffic to #{backend}", output
     assert_match "tsnet starting with hostname \"#{hostname}\"", output
     assert_match "LocalBackend state is NeedsLogin", output
