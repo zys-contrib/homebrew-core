6e73a5809a679b6e2028d203753756f0e4fb1b35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-08 03:56:39 +0000

    xkeyboard-config: update 2.45 bottle.

--- Formula/x/xkeyboard-config.rb
+++ Formula/x/xkeyboard-config.rb
@@ -7,7 +7,7 @@ class XkeyboardConfig < Formula
   head "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "fe749c953e59bccd2a20d2d5576a20bf1b2a29ce3830ed81f9b9a9658838682e"
+    sha256 cellar: :any_skip_relocation, all: "8c5aca9bc9aa0b519cb62bbf072cc862ee15d80ed62835fc99df85d2cbee2d23"
   end
 
   depends_on "gettext" => :build
