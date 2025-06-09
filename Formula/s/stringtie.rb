71331b6df973f130ef1c637d40000f23ad246081
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-09 01:25:00 +0000

    stringtie: add 3.0.1 bottle.

--- Formula/s/stringtie.rb
+++ Formula/s/stringtie.rb
@@ -6,6 +6,15 @@ class Stringtie < Formula
   license "MIT"
   head "https://github.com/gpertea/stringtie.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "b8635ef5a2c5bb7d9badbab1fc0cebd6251eb37b2e86b7e3a49398946c77db10"
+    sha256 cellar: :any,                 arm64_sonoma:  "e0cfafab884859f6f119ad427f41383c5db78602e17d0125e83855f8574388ad"
+    sha256 cellar: :any,                 arm64_ventura: "fda12598aa1f3d83e317ae9c6b60a1a957cb72ea11ade9587863f255b19a8a3e"
+    sha256 cellar: :any,                 sonoma:        "ca08236c2b9faa6036eecd48f829b5fd9d4939c1ba7f3975afef37d6d27e15f8"
+    sha256 cellar: :any,                 ventura:       "9f34f83c30482fdf8ad0516a706b32b6bd570808f72c887ba5a915bcbe5f1340"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6dbebfd9c277e5e08b6e4e39842d672bf760fc0bea23fd9919668f794ef7e120"
+  end
+
   depends_on "htslib"
 
   def install
