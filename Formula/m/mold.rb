945d8036134c7af02f3b451c526bb8c88c6f3e3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-09 05:13:54 +0000

    mold 2.40.1

--- Formula/m/mold.rb
+++ Formula/m/mold.rb
@@ -1,8 +1,8 @@
 class Mold < Formula
   desc "Modern Linker"
   homepage "https://github.com/rui314/mold"
-  url "https://github.com/rui314/mold/archive/refs/tags/v2.40.0.tar.gz"
-  sha256 "ee93ef0868cd54d1f30dfba1070dbacf04c1a35428bbea307a4b9242a883ad20"
+  url "https://github.com/rui314/mold/archive/refs/tags/v2.40.1.tar.gz"
+  sha256 "d1ce09a69941f8158604c3edcc96c7178231e7dba2da66b20f5ef6e112c443b7"
   license "MIT"
   head "https://github.com/rui314/mold.git", branch: "main"
 
