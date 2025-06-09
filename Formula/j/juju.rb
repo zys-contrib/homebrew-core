9d1edcf801dda4ca989b708b0c3a999b182ab4cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-09 17:10:36 +0000

    juju 3.6.7

--- Formula/j/juju.rb
+++ Formula/j/juju.rb
@@ -1,8 +1,8 @@
 class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
-  url "https://launchpad.net/juju/3.6/3.6.6/+download/juju-core_3.6.6.tar.gz"
-  sha256 "f84dc452c7344e50a6880abeb63001bd767c1230e853dcf736915282997a7405"
+  url "https://launchpad.net/juju/3.6/3.6.7/+download/juju-core_3.6.7.tar.gz"
+  sha256 "456395d2974c88a69731f3f1af20a8a989b9b512e664bd0064dea1b9c02d879b"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git", branch: "develop"
