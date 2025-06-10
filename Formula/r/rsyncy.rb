56220cea7457a329951309ee549003d786b152ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 23:30:33 +0000

    rsyncy 2.0.0
    rsyncy: update to go build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/rsyncy.rb
+++ Formula/r/rsyncy.rb
@@ -1,24 +1,16 @@
 class Rsyncy < Formula
-  include Language::Python::Virtualenv
-
   desc "Status/progress bar for rsync"
   homepage "https://github.com/laktak/rsyncy"
-  url "https://github.com/laktak/rsyncy/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "3832f71fbdfb3fbc3d135da91864abab44f16a9e9918d46389604a463bbf840f"
+  url "https://github.com/laktak/rsyncy/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "a19136713aee2242bf702dbd9be7a2deb6a94a001910b9a297e236bb00ccac5e"
   license "MIT"
   head "https://github.com/laktak/rsyncy.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "a67e111ca3ef8b9e2b4fd568b818e79fbb3ca6a8f1310d8707c2d79beb69e681"
-  end
-
-  depends_on "python@3.13"
+  depends_on "go" => :build
   depends_on "rsync"
 
-  uses_from_macos "zlib"
-
   def install
-    virtualenv_install_with_resources
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
