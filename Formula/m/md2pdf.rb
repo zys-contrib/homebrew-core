cdcd2c70ca189b36520ba340e80d7c1cb5cb1abe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 21:55:24 +0000

    md2pdf: update 2.2.18 bottle.

--- Formula/m/md2pdf.rb
+++ Formula/m/md2pdf.rb
@@ -7,12 +7,12 @@ class Md2pdf < Formula
   head "https://github.com/solworktech/mdtopdf.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff52784e2e724ac33d5ffa5b850a2afc618eda444b17dd08e488ea0df2cd515d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ff52784e2e724ac33d5ffa5b850a2afc618eda444b17dd08e488ea0df2cd515d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ff52784e2e724ac33d5ffa5b850a2afc618eda444b17dd08e488ea0df2cd515d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "274fe07a1fa7b5d769609d585614dc9b74d45ebe3b721738c97751c06475861f"
-    sha256 cellar: :any_skip_relocation, ventura:       "274fe07a1fa7b5d769609d585614dc9b74d45ebe3b721738c97751c06475861f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d45567c571274052dc83e71fc228686f9493ceaf28892593b77105dca3d0b932"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a3f31a7fd7fc59f484e62023b815331c624d0caac12e8573e3dce5a72a53d9f7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a3f31a7fd7fc59f484e62023b815331c624d0caac12e8573e3dce5a72a53d9f7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a3f31a7fd7fc59f484e62023b815331c624d0caac12e8573e3dce5a72a53d9f7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1067ffc7b46773d8b98334e7ce11bc5e654f06ec4d891e545856191d5d0ca597"
+    sha256 cellar: :any_skip_relocation, ventura:       "1067ffc7b46773d8b98334e7ce11bc5e654f06ec4d891e545856191d5d0ca597"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "210dde51799b9cd2fa6037e4f1be39042346be5ea96a1a4cceef8bfb8df00180"
   end
 
   depends_on "go" => :build
