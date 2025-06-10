89d70d6dc419e24c3ef97f7683fe91321a3fa478
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 11:17:11 +0000

    boring: update 0.11.5 bottle.

--- Formula/b/boring.rb
+++ Formula/b/boring.rb
@@ -9,13 +9,13 @@ class Boring < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ebf05f99bd43f16fdd89a8884b3c6be5b3cd86097ea9a73f746221cd277043a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ebf05f99bd43f16fdd89a8884b3c6be5b3cd86097ea9a73f746221cd277043a2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ebf05f99bd43f16fdd89a8884b3c6be5b3cd86097ea9a73f746221cd277043a2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fed2db3cc5792d30011ed4e887869b275b0ee357dd88733601c4b407d8563ecd"
-    sha256 cellar: :any_skip_relocation, ventura:       "fed2db3cc5792d30011ed4e887869b275b0ee357dd88733601c4b407d8563ecd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fd80ecd0cc095245eadbfc52ecd8a2e8d30b71d970c4f4854feeb0405481a2c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ebb99a37b8bded7f8667d915865cae568800494d8a6b15306ed3172f5872f16f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa648fed1371ebf70a7bf6e074b42fef429c894c83716c83cb68e4d85115b7bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa648fed1371ebf70a7bf6e074b42fef429c894c83716c83cb68e4d85115b7bd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fa648fed1371ebf70a7bf6e074b42fef429c894c83716c83cb68e4d85115b7bd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "13af457719b2caf176eaad23a801f58d5451bceb831376724c5a02b632711a25"
+    sha256 cellar: :any_skip_relocation, ventura:       "13af457719b2caf176eaad23a801f58d5451bceb831376724c5a02b632711a25"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "71f8c0338971cfb88d2a3449af922f3baaaf083f0a1fdd42708ab1b3ac496ec8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b74a7276ffb6e139fccc3f491521db8b986a99efe6f5607f05aa22aebdb79f73"
   end
 
   depends_on "go" => :build
