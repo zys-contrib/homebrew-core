fc8a0b9170bb0a236e8e2d4db94c3f49053af2c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 10:53:07 +0000

    apprise: update 1.9.3_1 bottle.

--- Formula/a/apprise.rb
+++ Formula/a/apprise.rb
@@ -9,13 +9,13 @@ class Apprise < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "354b28f0fb23363894667645c28ca9901a14ef20542ed7e1f217008b9060e30e"
-    sha256 cellar: :any,                 arm64_sonoma:  "ce7f87a5e4b166890f249446033ffccd2f75240a9a7756c5d09e9b6105241812"
-    sha256 cellar: :any,                 arm64_ventura: "05c8c0fd8290b6ad5d60a606270857a0c1c6a5812248e7cd7a7fad426e996fe9"
-    sha256 cellar: :any,                 sonoma:        "ada7b48bcb889febea70f31b506ab1357ee00109815c2f72e2262d784c4409db"
-    sha256 cellar: :any,                 ventura:       "0ff02200c042ed717c0202f4088055e0d611a21dba4620727177cc654384fd51"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f554695504727b87700262ef6cc6fe87040bffb8c1d6d059d69fbdd6984188d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c761465aba70b06dd7757c62c301daac2dc273c3d53feec956b4d3502633d030"
+    sha256 cellar: :any,                 arm64_sequoia: "efca83e33402cd9c97a47bc12239af471f75226c7e607d9cf9f545dc142469e8"
+    sha256 cellar: :any,                 arm64_sonoma:  "7f839ea7e876f169d2681fd8b8929c3dea64f28670d4f6ac1efd1b9d5fde9f39"
+    sha256 cellar: :any,                 arm64_ventura: "3578cdef5e6ee0d226c1ee22999a28d22c9e83e2ca57684ed4b025e0c7fa39f2"
+    sha256 cellar: :any,                 sonoma:        "479fdb08b2da41f4815691c88ad54d09d42337e2d095d66bcdd37786e28cd36a"
+    sha256 cellar: :any,                 ventura:       "1a5047e13d554eba586031bc1d278bc5f641e77d5f3b5f074937f908b05c1940"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6439b294a7c9e49ceddfdca012bdcf9f6bd532c5de6b962818221666ebcd7ef8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38c97c36594a2066ca1debd1ab23ab3ef0417adf9250a46b6380383b5ccd2c98"
   end
 
   depends_on "certifi"
