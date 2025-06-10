adf91a7c9dc051801915f676cd30715668c41797
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 13:28:35 +0000

    sprocket: add 0.13.0 bottle.

--- Formula/s/sprocket.rb
+++ Formula/s/sprocket.rb
@@ -6,6 +6,15 @@ class Sprocket < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/stjude-rust-labs/sprocket.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "4a8019db8b73c7e9c54438cfaa0ca7f33d6c2b982ad68f602fc6e4c951bbfb84"
+    sha256 cellar: :any,                 arm64_sonoma:  "8f3c2d00dd33710838da51ebb4705b0bff1ee5807757a6cc837fa14f56ba2c75"
+    sha256 cellar: :any,                 arm64_ventura: "90974677931267583f965dd5a7b2f772c8bbef398c619f45cb816152434f91eb"
+    sha256 cellar: :any,                 sonoma:        "3caf0f88b2abae6ab506527a7d9b520dd853a128e0810db7676854decaeb4878"
+    sha256 cellar: :any,                 ventura:       "aaa8fbb13dc34176c80acb78ec4b0e106ce211b1349236fb9b2d91774405bb29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bf77fdc5153e3d1505fc267da7a686687323d77f384647fff55708f2c4152d9"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
