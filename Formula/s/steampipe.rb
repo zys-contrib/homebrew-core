55d8f38cf7cb7f0815fbbf349ded76bfb445f3f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-11 23:15:57 +0000

    steampipe: update 2.0.1 bottle.

--- Formula/s/steampipe.rb
+++ Formula/s/steampipe.rb
@@ -12,12 +12,12 @@ class Steampipe < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d4b57231af1b7d8b8660c7e91ea97b2a327fc480a679c0a47a48459168a7ee11"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2641c831c3019a4a66ec89549665462951844c3764eb152b30d8636bb6229a5f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d34cfe8e39e3f23a31ee54afadbdf91a31624a443ff2590c74372a0ddebe49f5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c6d5eb17e7c6897f3cce3a315af24a42701cbceb9d599f6cfccdaf94b6738d54"
-    sha256 cellar: :any_skip_relocation, ventura:       "c98e6d7a849d81e26c3ed222827319c865c3a8a7872b29d0ce39b20f7eee7ead"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45d537ab1f891cec172b266aaf26a9e1432d58f6df26431e7691a42ee277db7b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0fe10140fd0ff7ddec5f7eee3c0055ff1b6ea65c25493ffaa98d302d8aa2fc55"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20218e3ecbaed55408e9e861ab8899cd1beffd66892a0f518867d9260e41f8db"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "4875653d0d2ca1a724500d57500bc26fd96d12c7b1eef802ead765e76c51f5c2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e98fff514b11e556bb4927b8b539d1302afe3a2a127c10ab0d364a91199667c0"
+    sha256 cellar: :any_skip_relocation, ventura:       "6962b5065285ed1da4e40a595827d79fe6cdff363cbee9d98f76b0afa1758ff8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d90a0be1f744d0b006ba363c1ac902d56b9fc989b0377fa00e405e372c27bd7"
   end
 
   depends_on "go" => :build
