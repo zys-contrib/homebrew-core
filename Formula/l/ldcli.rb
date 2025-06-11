77deb7444d10b24fcfa69a2be7f92d59cbbed8d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-11 17:14:59 +0000

    ldcli: add 1.15.3 bottle.

--- Formula/l/ldcli.rb
+++ Formula/l/ldcli.rb
@@ -6,6 +6,15 @@ class Ldcli < Formula
   license "Apache-2.0"
   head "https://github.com/launchdarkly/ldcli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "641649c78d79f7ef2d6f7a69eb82eaec1e575002f3c315775101cd0c5ca5714c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a80d80df78551298064a22c647541f5f7dbedfaa31a7a5a7c2b1e329a707af26"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "964ac434235946fbc8845ca6149f36155428793747ea115ba840ebb675cf36df"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a38772c1dd6f6af380e43897e396908472881420571b2990ce1d5104501a10bd"
+    sha256 cellar: :any_skip_relocation, ventura:       "5856accf4f9bd7ec17a34d7a5184f8b253e4fc0cd078432046210c80a4326b81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f13544ef2d9b4c3bba419aadaacf4df17bb527428c49d15684d8abc3b74aba3"
+  end
+
   depends_on "go" => :build
 
   def install
