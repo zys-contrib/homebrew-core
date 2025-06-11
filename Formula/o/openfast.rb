a822084ab4904e06ed9f900258ab28fbaf063386
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-11 20:11:34 +0000

    openfast 4.0.5

--- Formula/o/openfast.rb
+++ Formula/o/openfast.rb
@@ -2,8 +2,8 @@ class Openfast < Formula
   desc "NREL-supported OpenFAST whole-turbine simulation code"
   homepage "https://openfast.readthedocs.io"
   url "https://github.com/openfast/openfast.git",
-      tag:      "v4.0.4",
-      revision: "d28a823169e75029d73362b07a2942d0a454f03b"
+      tag:      "v4.0.5",
+      revision: "f4c0b48139479fcfdca10b4dfed6ef86fa1e0d7d"
   license "Apache-2.0"
 
   bottle do
