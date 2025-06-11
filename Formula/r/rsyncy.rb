945ea8719c82d0191c517d2c5b3a164ffb659b5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-11 06:13:14 +0000

    rsyncy: add 2.0.0 bottle.

--- Formula/r/rsyncy.rb
+++ Formula/r/rsyncy.rb
@@ -6,6 +6,15 @@ class Rsyncy < Formula
   license "MIT"
   head "https://github.com/laktak/rsyncy.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce8bfe4b5cf09f2cce0c7a2fe54caf1ebe24918cff5576e512767e100b54f647"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ce8bfe4b5cf09f2cce0c7a2fe54caf1ebe24918cff5576e512767e100b54f647"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ce8bfe4b5cf09f2cce0c7a2fe54caf1ebe24918cff5576e512767e100b54f647"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2a58b48e86714bfa7e0bd72616aa49f3ef084090a1f1f61da9eba3c65dc466cb"
+    sha256 cellar: :any_skip_relocation, ventura:       "2a58b48e86714bfa7e0bd72616aa49f3ef084090a1f1f61da9eba3c65dc466cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "280965bc23031ff27e7a031a6234ac15a0e9634de66420c3404d149b3ca45fc9"
+  end
+
   depends_on "go" => :build
   depends_on "rsync"
 
