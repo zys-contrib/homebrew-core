fffac4d0706945a1282d23b80739f3484a5fe6ff
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-06-12 09:08:35 +0900

    symengine: revision bump (flint 3.3.1)

--- Formula/s/symengine.rb
+++ Formula/s/symengine.rb
@@ -4,7 +4,7 @@ class Symengine < Formula
   url "https://github.com/symengine/symengine/archive/refs/tags/v0.14.0.tar.gz"
   sha256 "11c5f64e9eec998152437f288b8429ec001168277d55f3f5f1df78e3cf129707"
   license "MIT"
-  revision 2
+  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "df79e834fa3f34d24c03f89e336595fca313ac465697889dfaf8ed5d91d16dbf"
