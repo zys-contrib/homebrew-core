a223ba98319331730c4ab16ec1792cb30ba112f6
Branch Vincent <branchevincent@gmail.com>
2025-06-11 20:00:13 -0700

    python-gdbm@3.13 3.13.5

--- Formula/p/python-gdbm@3.13.rb
+++ Formula/p/python-gdbm@3.13.rb
@@ -1,8 +1,8 @@
 class PythonGdbmAT313 < Formula
   desc "Python interface to gdbm"
   homepage "https://www.python.org/"
-  url "https://www.python.org/ftp/python/3.13.4/Python-3.13.4.tgz"
-  sha256 "2666038f1521b7a8ec34bf2997b363778118d6f3979282c93723e872bcd464e0"
+  url "https://www.python.org/ftp/python/3.13.5/Python-3.13.5.tgz"
+  sha256 "e6190f52699b534ee203d9f417bdbca05a92f23e35c19c691a50ed2942835385"
   license "Python-2.0"
 
   livecheck do
