5ba6b3b1a74b0843239927ca76273decc636f1a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-14 14:25:01 +0000

    px: update 3.6.12 bottle.

--- Formula/p/px.rb
+++ Formula/p/px.rb
@@ -11,13 +11,13 @@ class Px < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8f26807f87e7d74cab75efbe877bb0f1709d33711ae0c5d33a6507c7719946ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8f26807f87e7d74cab75efbe877bb0f1709d33711ae0c5d33a6507c7719946ff"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8f26807f87e7d74cab75efbe877bb0f1709d33711ae0c5d33a6507c7719946ff"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7c1df093286c5d56073b2dcce0c06bd75fdfc8efae0f29f875241e032fea30bf"
-    sha256 cellar: :any_skip_relocation, ventura:       "7c1df093286c5d56073b2dcce0c06bd75fdfc8efae0f29f875241e032fea30bf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8f26807f87e7d74cab75efbe877bb0f1709d33711ae0c5d33a6507c7719946ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f26807f87e7d74cab75efbe877bb0f1709d33711ae0c5d33a6507c7719946ff"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2fee9f20e486a9ecf3f39b5984280ac6f9f21321af5bd70178c1b0b0a560ddb8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2fee9f20e486a9ecf3f39b5984280ac6f9f21321af5bd70178c1b0b0a560ddb8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2fee9f20e486a9ecf3f39b5984280ac6f9f21321af5bd70178c1b0b0a560ddb8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e13cd275ed42a692779378656b6c107929c5481c69d132ae1c372c1701020660"
+    sha256 cellar: :any_skip_relocation, ventura:       "e13cd275ed42a692779378656b6c107929c5481c69d132ae1c372c1701020660"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2fee9f20e486a9ecf3f39b5984280ac6f9f21321af5bd70178c1b0b0a560ddb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2fee9f20e486a9ecf3f39b5984280ac6f9f21321af5bd70178c1b0b0a560ddb8"
   end
 
   depends_on "python@3.13"
