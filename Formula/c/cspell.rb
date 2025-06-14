05d9543613b33020346efdca50008ecd5befcc0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-14 14:23:03 +0000

    cspell: update 9.1.1 bottle.

--- Formula/c/cspell.rb
+++ Formula/c/cspell.rb
@@ -6,13 +6,13 @@ class Cspell < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7569e790a0992b229a479822217fa8b7d410caefd0dd00357508b1d77ba85032"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7569e790a0992b229a479822217fa8b7d410caefd0dd00357508b1d77ba85032"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7569e790a0992b229a479822217fa8b7d410caefd0dd00357508b1d77ba85032"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8af48d108e33d112185817f57034dbb2f69e769b6cfaf935cbf90c3e8c70132c"
-    sha256 cellar: :any_skip_relocation, ventura:       "8af48d108e33d112185817f57034dbb2f69e769b6cfaf935cbf90c3e8c70132c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7569e790a0992b229a479822217fa8b7d410caefd0dd00357508b1d77ba85032"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7569e790a0992b229a479822217fa8b7d410caefd0dd00357508b1d77ba85032"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e9a8b193beaf864701005cf44a94ea4bbbd7874ff08e5a89b2f760bb997c85de"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e9a8b193beaf864701005cf44a94ea4bbbd7874ff08e5a89b2f760bb997c85de"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e9a8b193beaf864701005cf44a94ea4bbbd7874ff08e5a89b2f760bb997c85de"
+    sha256 cellar: :any_skip_relocation, sonoma:        "77989511b517e120e9158e04de87d6a99303a8afac48e39096280d82f7f068c2"
+    sha256 cellar: :any_skip_relocation, ventura:       "77989511b517e120e9158e04de87d6a99303a8afac48e39096280d82f7f068c2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e9a8b193beaf864701005cf44a94ea4bbbd7874ff08e5a89b2f760bb997c85de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9a8b193beaf864701005cf44a94ea4bbbd7874ff08e5a89b2f760bb997c85de"
   end
 
   depends_on "node"
