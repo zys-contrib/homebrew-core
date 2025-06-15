7150f408bb3babc8e35d161a6051b166c464a8e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-15 03:55:28 +0000

    diagram: add 1.0.6 bottle.

--- Formula/d/diagram.rb
+++ Formula/d/diagram.rb
@@ -6,6 +6,15 @@ class Diagram < Formula
   license "MIT"
   head "https://github.com/esimov/diagram.git", branch: "master"
 
+  bottle do
+    sha256 arm64_sequoia: "049ae370aab2ab7876282b307a01bda38e2e254807f422cece826e4ec556d09e"
+    sha256 arm64_sonoma:  "38ac3fbc992a5611bf30e1848123efda5274a7d994049a24ec55102871886776"
+    sha256 arm64_ventura: "a234cd90fd88eaa7025cd28843b37d9feb6f07a012ee57f5e1a5a7f288677562"
+    sha256 sonoma:        "3eb79fb18800c3f1c9eebc261c15f06db339d0b2df017184757b6e39126df8f8"
+    sha256 ventura:       "6c8079e485d9bb3c95cb22221a6a2fdc4fe4801d6f1d965c0ac7941aecf04c79"
+    sha256 x86_64_linux:  "6ba263cef3accdf1a171e19d5aebf1493c304f601dd490753ae06a0b5932c459"
+  end
+
   depends_on "go" => :build
   depends_on "pkgconf" => :build
 
