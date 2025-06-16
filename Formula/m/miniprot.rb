f6ae47a9ea16bc0d9310188703de70124ac44a9e
Rui Chen <rui@chenrui.dev>
2025-06-16 03:15:07 +0000

    miniprot 0.17
    autobump: add miniprot
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/miniprot.rb
+++ Formula/m/miniprot.rb
@@ -1,8 +1,8 @@
 class Miniprot < Formula
   desc "Align proteins to genomes with splicing and frameshift"
   homepage "https://lh3.github.io/miniprot/"
-  url "https://github.com/lh3/miniprot/archive/refs/tags/v0.16.tar.gz"
-  sha256 "1ec0290552a6c80ad71657a44c767c3a2a2bbcfe3c7cc150083de7f9dc4b3ed0"
+  url "https://github.com/lh3/miniprot/archive/refs/tags/v0.17.tar.gz"
+  sha256 "afdad05d18290756a7056ca7f67a91bd55d56006100653fd3dd956652206a415"
   license "MIT"
 
   no_autobump! because: :requires_manual_review
