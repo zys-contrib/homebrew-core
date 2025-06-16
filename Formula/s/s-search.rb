d376adca326b2e619cf0bb029f2b6ed1e51bf892
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-16 02:58:49 +0000

    s-search 0.7.3

--- Formula/s/s-search.rb
+++ Formula/s/s-search.rb
@@ -1,8 +1,8 @@
 class SSearch < Formula
   desc "Web search from the terminal"
   homepage "https://github.com/zquestz/s"
-  url "https://github.com/zquestz/s/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "d36474448d9594743ff8841585e596c24ef0be9110f15e6b39baa96c8d982e6e"
+  url "https://github.com/zquestz/s/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "260df5364b947f19a12af9018cdeffecd5b572d1662ba2bbcbfba906c39330c3"
   license "MIT"
   head "https://github.com/zquestz/s.git", branch: "master"
 
