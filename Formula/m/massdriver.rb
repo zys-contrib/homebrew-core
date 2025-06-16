2f7957955154f2c0e69400cca5b7cac559b77ec8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-16 23:31:53 +0000

    massdriver: update 1.11.2 bottle.

--- Formula/m/massdriver.rb
+++ Formula/m/massdriver.rb
@@ -15,12 +15,12 @@ class Massdriver < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b806c5272ff004cdb8a846106591ab2e47c499cd2ddf8512f1be0c895dfc3a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b806c5272ff004cdb8a846106591ab2e47c499cd2ddf8512f1be0c895dfc3a2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "2b806c5272ff004cdb8a846106591ab2e47c499cd2ddf8512f1be0c895dfc3a2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "de82a6c01b310a3c7ffee6688047108cacb1a3b15b7abb4326adf796c6325f13"
-    sha256 cellar: :any_skip_relocation, ventura:       "de82a6c01b310a3c7ffee6688047108cacb1a3b15b7abb4326adf796c6325f13"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ae067d338cc3aa745923a883733bde0df24c89e333cd6efb408899f4cd4fa7f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3f6cddd55b5f57c582fa5ca59cae6e1711489f202393728bba86d5fd20886f91"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3f6cddd55b5f57c582fa5ca59cae6e1711489f202393728bba86d5fd20886f91"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3f6cddd55b5f57c582fa5ca59cae6e1711489f202393728bba86d5fd20886f91"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b07c4bdddec6358a033d6055595ae71f0384afb7423cb640749b8df532152aee"
+    sha256 cellar: :any_skip_relocation, ventura:       "b07c4bdddec6358a033d6055595ae71f0384afb7423cb640749b8df532152aee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "59dd5b4d01f8f6e880dac588873672ba85c2a213021f53ab07301c0b025a8a16"
   end
 
   depends_on "go" => :build
