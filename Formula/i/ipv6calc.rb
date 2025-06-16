6cdba2acebf42b734e292aead358b6542a83fdaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-16 17:11:57 +0000

    ipv6calc 4.3.2

--- Formula/i/ipv6calc.rb
+++ Formula/i/ipv6calc.rb
@@ -1,8 +1,8 @@
 class Ipv6calc < Formula
   desc "Small utility for manipulating IPv6 addresses"
   homepage "https://www.deepspace6.net/projects/ipv6calc.html"
-  url "https://github.com/pbiering/ipv6calc/archive/refs/tags/4.3.1.tar.gz"
-  sha256 "b1c5006edebaad3e2e286d12f70d136bf05658e9e8bda8d67ef7c477322a1a47"
+  url "https://github.com/pbiering/ipv6calc/archive/refs/tags/4.3.2.tar.gz"
+  sha256 "4335c4edf7d51dbd0cafdd42ecda5598101f7a8ab257a74be2d9ec81f5bf895b"
   license "GPL-2.0-only"
 
   # Upstream creates stable version tags (e.g., `v1.2.3`) before a release but
