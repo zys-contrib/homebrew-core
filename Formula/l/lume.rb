b0254b7c286978359f6511e3f7fefc5c8790b085
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-17 03:14:27 +0000

    lume: update 0.2.22 bottle.

--- Formula/l/lume.rb
+++ Formula/l/lume.rb
@@ -12,8 +12,8 @@ class Lume < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa9208b508b9d09146ff8af9dd774fd44c4cf501268db10681281ee3ddc9fa9a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "65d6226f54c29189e8acfc413431c2cef38ac22085f3e69c414209138dec960c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c30490fd0d2ae219b00fc2428dee485f4d241a0db491e0c369828ccd1d5ffae8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5940b327e623da53d2c5109a1432481eb10cd3053fb8a1baf2bce9a8571b41d0"
   end
 
   depends_on xcode: ["16.0", :build]
