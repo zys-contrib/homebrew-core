636ef3a38311f101fc39c225478ad87e69b583cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-17 11:10:32 +0000

    linux-pam 1.7.1

--- Formula/l/linux-pam.rb
+++ Formula/l/linux-pam.rb
@@ -1,8 +1,8 @@
 class LinuxPam < Formula
   desc "Pluggable Authentication Modules for Linux"
   homepage "https://github.com/linux-pam/linux-pam"
-  url "https://github.com/linux-pam/linux-pam/releases/download/v1.7.0/Linux-PAM-1.7.0.tar.xz"
-  sha256 "57dcd7a6b966ecd5bbd95e1d11173734691e16b68692fa59661cdae9b13b1697"
+  url "https://github.com/linux-pam/linux-pam/releases/download/v1.7.1/Linux-PAM-1.7.1.tar.xz"
+  sha256 "21dbcec6e01dd578f14789eac9024a18941e6f2702a05cf91b28c232eeb26ab0"
   license any_of: ["BSD-3-Clause", "GPL-1.0-only"]
   head "https://github.com/linux-pam/linux-pam.git", branch: "master"
 
