66ac86336a973b9d0250e315dca45c48e27c5e57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-17 03:14:47 +0000

    bandit: update 1.8.5 bottle.

--- Formula/b/bandit.rb
+++ Formula/b/bandit.rb
@@ -9,13 +9,13 @@ class Bandit < Formula
   head "https://github.com/PyCQA/bandit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "964e40d6436049ce517cebd56a98939ea0eeb4405858f971c5a92e6c7d35771c"
-    sha256 cellar: :any,                 arm64_sonoma:  "86b80a53756ed989f5387e99ffc2cdf66471de8ebb1e13613cb6d8687803c0b1"
-    sha256 cellar: :any,                 arm64_ventura: "2b0d40a68e816b4c22a454e1ad537d2159dd324db64f6c252a1aade66b23a576"
-    sha256 cellar: :any,                 sonoma:        "2e8d5dc4e55e16ca0182caf25594fad20de87751965c2209cc35d7f8cd8aad7d"
-    sha256 cellar: :any,                 ventura:       "eba00adfe42e1862711d1e0da30a88a081dcf9d73b463ca44fa2090e22c33db1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "756d1d881affc851dec0c7c3f8670cbd1444f47054aa23e343102e5f2341a8ad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dda916adc0015f3f5169e61bbd390979dc962f59a346624d0cad5fa824f37034"
+    sha256 cellar: :any,                 arm64_sequoia: "433446f0a00382f4d134e458bfae5482dbb0a139fcc144ae65578656ebdf8cd6"
+    sha256 cellar: :any,                 arm64_sonoma:  "95148b9d41699d7f135b86d18e0fc4a0c717e7b6f1579f858cde9a5f6141d21e"
+    sha256 cellar: :any,                 arm64_ventura: "c0500467fe18f4e97a1935cb60fb92c3c7b8c1b588806cc5d841efddb62e99b5"
+    sha256 cellar: :any,                 sonoma:        "092ba89ceb814c02a2c5b5c91a0377c4503b4c7ddd426ed15e580ee03adbbaab"
+    sha256 cellar: :any,                 ventura:       "4f0ed8c5331aff4b98bcf686c2da3208cf2f750dc86bde1b7f89c234dc703c41"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9f6117e4e209b5711d64ba19c31e8a837bfdfc70fc0d80e52d40d0aa8868f7fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a112b745e37ab7aca2000f18dd0f5b2166cac53c2ef810af9617493423b91317"
   end
 
   depends_on "libyaml"
