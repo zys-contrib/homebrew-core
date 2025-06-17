5db32b7ddf5ec0b56e3dbca72e5be67107bff339
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-17 22:56:16 +0000

    cosign 2.5.2

--- Formula/c/cosign.rb
+++ Formula/c/cosign.rb
@@ -2,8 +2,8 @@ class Cosign < Formula
   desc "Container Signing"
   homepage "https://github.com/sigstore/cosign"
   url "https://github.com/sigstore/cosign.git",
-      tag:      "v2.5.1",
-      revision: "a7345fb2ce17b52b5bc687970fa31ff85bc2f7ca"
+      tag:      "v2.5.2",
+      revision: "af5a988bb15a03919ccaac7a2ddcad7a9d006f38"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
