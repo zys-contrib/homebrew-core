32aa22358c1b769ab4dfc2fcfa7c363ea729b6a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-18 18:22:17 +0000

    bruno-cli: update 2.6.1 bottle.

--- Formula/b/bruno-cli.rb
+++ Formula/b/bruno-cli.rb
@@ -6,13 +6,13 @@ class BrunoCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "887868034d819e89df562a3e52f5355ec65aacdacd33dfc6531e5a35e6c3fce4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "887868034d819e89df562a3e52f5355ec65aacdacd33dfc6531e5a35e6c3fce4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "887868034d819e89df562a3e52f5355ec65aacdacd33dfc6531e5a35e6c3fce4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "27f78613b8d60e5d7826da1d83968e818961bc7458cbe9799f915b8dad40041a"
-    sha256 cellar: :any_skip_relocation, ventura:       "27f78613b8d60e5d7826da1d83968e818961bc7458cbe9799f915b8dad40041a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "887868034d819e89df562a3e52f5355ec65aacdacd33dfc6531e5a35e6c3fce4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "887868034d819e89df562a3e52f5355ec65aacdacd33dfc6531e5a35e6c3fce4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "61c5f10e1776ac03c81dc6009b90ec9c8e32603298c3a5175239a3bd5fa6f9f2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "61c5f10e1776ac03c81dc6009b90ec9c8e32603298c3a5175239a3bd5fa6f9f2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "61c5f10e1776ac03c81dc6009b90ec9c8e32603298c3a5175239a3bd5fa6f9f2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "695ef1c48cbccd18bb52a47fc43525120d60c7724c050667b837ad2ce2226e8d"
+    sha256 cellar: :any_skip_relocation, ventura:       "695ef1c48cbccd18bb52a47fc43525120d60c7724c050667b837ad2ce2226e8d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "61c5f10e1776ac03c81dc6009b90ec9c8e32603298c3a5175239a3bd5fa6f9f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61c5f10e1776ac03c81dc6009b90ec9c8e32603298c3a5175239a3bd5fa6f9f2"
   end
 
   depends_on "node"
