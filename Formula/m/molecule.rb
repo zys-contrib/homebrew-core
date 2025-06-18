b514d8b62834abf96a3f3410cd642ecc1f0c1fff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-18 15:02:06 +0000

    molecule: update 25.6.0 bottle.

--- Formula/m/molecule.rb
+++ Formula/m/molecule.rb
@@ -8,13 +8,13 @@ class Molecule < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "e5b06a38c2bc38fb543e89a9a299ae19cd4bc340830dcd87e984fb7db229abbb"
-    sha256 cellar: :any,                 arm64_sonoma:  "333be3c67c93221f1b70fec38a3f0d55d06f72d659ed97b2e8e2db5fb75dc26a"
-    sha256 cellar: :any,                 arm64_ventura: "85954f4d7ff1683c9a218618cd97b2ca9cc2543bf66634162e4c48a10166df3f"
-    sha256 cellar: :any,                 sonoma:        "f4cc0b08694cad35fae4f77665959f752b1d1fdbe926990f95d1e47207a3e8fa"
-    sha256 cellar: :any,                 ventura:       "010d8d2f778fdc42ef3f3cf941915814484468fb2bf0f74b61ec62c031803213"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "16f7d2e8ca52da9d01aca5ea05281931d907ff937a50b3fa4dd5985535f3fdb3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fb5e5489b0bc31d77bd8b32f1b2037e05ee90a0ab23c01eaa324d599727cb87"
+    sha256 cellar: :any,                 arm64_sequoia: "dee9cc0b0372366e9b1aa05c3b06d3ac9bd9862f0f20f373cf699c03c23750bc"
+    sha256 cellar: :any,                 arm64_sonoma:  "8ccd642a426fbea119b7d5683a4fc5dcc4c6b09350a0c78b517c1daa73e21868"
+    sha256 cellar: :any,                 arm64_ventura: "5634c22d1157f0646c4fb929aeaf6e3464c7907caab4edb081d95ab7a7b43c50"
+    sha256 cellar: :any,                 sonoma:        "974e386a00ae15a0471e9f77b32216b99bf45c8dab6ac6b77c3491ccb6cca6b8"
+    sha256 cellar: :any,                 ventura:       "159421177661a054eaa4692041f5ab890286649def3c79f15bfb3addcdaf68a6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "da476d38679d1a10cdd1e02dfa26cfca859053a211af193dc91deb396b67be4d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2fdb00206d7ab54bf3bb9d417233964f2ad58413dcbce06bc110d1df5e1cd65f"
   end
 
   depends_on "rust" => :build
