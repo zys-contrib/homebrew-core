ea86745f5092a56fd8e9785aa579cf2f32e41057
botantony <antonsm21@gmail.com>
2025-06-18 22:59:37 +0200

    eask-cli 0.11.6 (new formula)
    Signed-off-by: botantony <antonsm21@gmail.com>

---
+++ Formula/e/eask-cli.rb
@@ -0,0 +1,22 @@
+class EaskCli < Formula
+  desc "CLI for building, running, testing, and managing your Emacs Lisp dependencies"
+  homepage "https://emacs-eask.github.io/"
+  url "https://github.com/emacs-eask/cli/archive/refs/tags/0.11.6.tar.gz"
+  sha256 "66faf98d76e6c6ca9b38f7721ef4aeba251181bc0aef4c692cf6d09a8988c896"
+  license "GPL-3.0-or-later"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/eask --version")
+
+    system bin/"eask", "create", "package", "test-project"
+    assert_path_exists testpath/"test-project"
+    assert_match "Emacs is not installed", shell_output("#{bin}/eask compile 2>&1")
+  end
+end
