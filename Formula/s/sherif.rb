cc6d70b4b7ff02f388144bb830605037b07012b5
Rui Chen <rui@chenrui.dev>
2025-06-19 14:38:12 -0400

    sherif 1.5.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/sherif.rb
@@ -0,0 +1,30 @@
+class Sherif < Formula
+  desc "Opinionated, zero-config linter for JavaScript monorepos"
+  homepage "https://github.com/QuiiBz/sherif"
+  url "https://registry.npmjs.org/sherif/-/sherif-1.5.0.tgz"
+  sha256 "9d34335e549940b1aa0ed4c2d96f6794863904ab30e9461c7bf8ff4dc879ca70"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"package.json").write <<~JSON
+      {
+        "name": "test",
+        "version": "1.0.0",
+        "private": true,
+        "packageManager": "npm",
+        "workspaces": [ "." ]
+      }
+    JSON
+    (testpath/"test.js").write <<~JS
+      console.log('Hello, world!');
+    JS
+    assert_match "No issues found", shell_output("#{bin}/sherif --no-install .")
+  end
+end
