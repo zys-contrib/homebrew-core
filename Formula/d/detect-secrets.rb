8c2e6efc24494539b67460cc1557110ccab035e4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-19 11:17:48 +0000

    detect-secrets: bump python resources

--- Formula/d/detect-secrets.rb
+++ Formula/d/detect-secrets.rb
@@ -6,7 +6,7 @@ class DetectSecrets < Formula
   url "https://files.pythonhosted.org/packages/69/67/382a863fff94eae5a0cf05542179169a1c49a4c8784a9480621e2066ca7d/detect_secrets-1.5.0.tar.gz"
   sha256 "6bb46dcc553c10df51475641bb30fd69d25645cc12339e46c824c1e0c388898a"
   license "Apache-2.0"
-  revision 3
+  revision 4
   head "https://github.com/Yelp/detect-secrets.git", branch: "master"
 
   bottle do
@@ -44,8 +44,8 @@ class DetectSecrets < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8a/78/16493d9c386d8e60e442a35feac5e00f0913c0f4b7c217c11e8ec2ff53e0/urllib3-2.4.0.tar.gz"
-    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+    url "https://files.pythonhosted.org/packages/15/22/9ee70a2574a4f4599c47dd506532914ce044817c7752a79b6a51286319bc/urllib3-2.5.0.tar.gz"
+    sha256 "3fc47733c7e419d4bc3f6b3dc2b4f890bb743906a30d56ba4a5bfa4bbff92760"
   end
 
   def install
