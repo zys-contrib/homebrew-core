b0d12827918c6b34051e42b5a7ddc94051bcdf28
Rui Chen <rui@chenrui.dev>
2025-06-19 19:05:33 -0400

    tiledb 2.28.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/tiledb.rb
@@ -0,0 +1,49 @@
+class Tiledb < Formula
+  desc "Universal storage engine"
+  homepage "https://tiledb.com/"
+  url "https://github.com/TileDB-Inc/TileDB/archive/refs/tags/2.28.0.tar.gz"
+  sha256 "de731cd0c8e82fe8cfca084b937dc0df41e451c8eb93071e4cc5aba7bbef854e"
+  license "MIT"
+
+  depends_on "cmake" => :build
+
+  depends_on "fmt"
+  depends_on "libmagic"
+  depends_on "lz4"
+  depends_on "openssl@3"
+  depends_on "spdlog"
+  depends_on "webp"
+  depends_on "zstd"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  def install
+    args = %w[
+      -DTILEDB_EXPERIMENTAL_FEATURES=OFF
+      -DTILEDB_TESTS=OFF
+      -DTILEDB_VERBOSE=ON
+      -DTILEDB_WERROR=OFF
+      -DTILEDB_DISABLE_AUTO_VCPKG=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <tiledb/tiledb>
+      #include <iostream>
+
+      int main() {
+        auto [major, minor, patch] = tiledb::version();
+        std::cout << major << "." << minor << "." << patch << std::endl;
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++17", "-I#{include}", "-L#{lib}", "-ltiledb", "-o", "test"
+    assert_match version.to_s, shell_output("./test")
+  end
+end
