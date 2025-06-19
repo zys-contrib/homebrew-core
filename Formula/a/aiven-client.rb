42d68f42824964d999a527487eda53dcbb85b542
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-19 08:09:29 +0000

    aiven-client: bump python resources

--- Formula/a/aiven-client.rb
+++ Formula/a/aiven-client.rb
@@ -6,6 +6,7 @@ class AivenClient < Formula
   url "https://files.pythonhosted.org/packages/a0/62/55a75ab81c5d030ad45cf66ad35545abf907bab5608be234e6bb1c3fa1d6/aiven_client-4.7.3.tar.gz"
   sha256 "ec823cb3b5065417c6b4875c9c42f1810cad265f941974d618d602698647d29f"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/aiven/aiven-client.git", branch: "main"
 
   bottle do
@@ -36,8 +37,8 @@ class AivenClient < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8a/78/16493d9c386d8e60e442a35feac5e00f0913c0f4b7c217c11e8ec2ff53e0/urllib3-2.4.0.tar.gz"
-    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+    url "https://files.pythonhosted.org/packages/15/22/9ee70a2574a4f4599c47dd506532914ce044817c7752a79b6a51286319bc/urllib3-2.5.0.tar.gz"
+    sha256 "3fc47733c7e419d4bc3f6b3dc2b4f890bb743906a30d56ba4a5bfa4bbff92760"
   end
 
   def install
