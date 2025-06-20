31fc1069deaa78c993050196a1b5fbf5fae2a87d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 05:04:03 +0000

    git-extras 7.4.0

--- Formula/g/git-extras.rb
+++ Formula/g/git-extras.rb
@@ -1,8 +1,8 @@
 class GitExtras < Formula
   desc "Small git utilities"
   homepage "https://github.com/tj/git-extras"
-  url "https://github.com/tj/git-extras/archive/refs/tags/7.3.0.tar.gz"
-  sha256 "89bae1a05731f4aaafb04066ea0186e181117b74fcfbf89d686cf205459220b7"
+  url "https://github.com/tj/git-extras/archive/refs/tags/7.4.0.tar.gz"
+  sha256 "aaab3bab18709ec6825a875961e18a00e0c7d8214c39d6e3a63aeb99fa11c56e"
   license "MIT"
   head "https://github.com/tj/git-extras.git", branch: "main"
 
