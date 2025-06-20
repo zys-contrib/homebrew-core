53640e9d34349e56a992ddb601c6de89fd2a0112
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 00:00:44 +0000

    libcec: update 7.1.0 bottle.

--- Formula/lib/libcec.rb
+++ Formula/lib/libcec.rb
@@ -6,13 +6,13 @@ class Libcec < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "6c3e6496b7aa4fc0b2ccb840c23b2881ce4f0d1a4419854c1e21e6e316a98b7f"
-    sha256 cellar: :any,                 arm64_sonoma:  "de4f26ed875eb592e3fc420963d62ffe939f38745951eacdb431517d3c297eed"
-    sha256 cellar: :any,                 arm64_ventura: "a3f634f61d5a27f31057a3e5deec39973d7bbdda09a8cefeeb903bf155411587"
-    sha256 cellar: :any,                 sonoma:        "559cbd188d933dad82b923434da5cbdcbc8f59972f157d04b518e968a05a62dc"
-    sha256 cellar: :any,                 ventura:       "a942eaf1c28a2ac335203421541da6347dc4f5bd5da1ce5015241d79dc845d73"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8dc79b71ae7d6917a919038ee0aa6a75adf4f97a6ea49dd389ee54efc53d5e9d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "916e33306e1006b5a8c5337c8ed5341c445598bb5eb90de65137ff1bbc0a1225"
+    sha256 cellar: :any,                 arm64_sequoia: "5d628750a848f9fe50e393d2c188fda70aa698c19a20bbbe7dc457d5c8ddce4e"
+    sha256 cellar: :any,                 arm64_sonoma:  "d43c94ebd2342cf25142ac4a489836d7f48547ea3c4da91f11cde2f9e319d401"
+    sha256 cellar: :any,                 arm64_ventura: "3c2c98844f1c667bf427d61f2c8d0fa56c1dea1f9ae4aa70fa67883358826976"
+    sha256 cellar: :any,                 sonoma:        "eadbe1896553e66368673fafafa958de45215947dc5991e33238e607a2f9ce57"
+    sha256 cellar: :any,                 ventura:       "ef7a56b4a3c551e42823704361cc09c05c5ebd372eb3c70d9f69700a7a1cdf2b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3624133784229fd025d4b4d067ec8efaaac2724b968fe09b0ab8498cb4f1ebda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05c12de1b683cf73a0a6a9034566c9be66e62bc51bb84e399fe913e6f6a84dff"
   end
 
   depends_on "cmake" => :build
