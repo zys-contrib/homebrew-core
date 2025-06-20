090e8ba28f1debcfcb7560dec6d087f664d348f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 08:16:19 +0000

    yewtube: bump python resources

--- Formula/y/yewtube.rb
+++ Formula/y/yewtube.rb
@@ -6,7 +6,7 @@ class Yewtube < Formula
   url "https://github.com/mps-youtube/yewtube/archive/refs/tags/v2.12.1.tar.gz"
   sha256 "012c1a8a185dd4ef81074631bca91e327ac4e634b36301a50ffbcd67838b847f"
   license "GPL-3.0-or-later"
-  revision 2
+  revision 3
   head "https://github.com/mps-youtube/yewtube.git", branch: "master"
 
   bottle do
@@ -75,8 +75,8 @@ class Yewtube < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8a/78/16493d9c386d8e60e442a35feac5e00f0913c0f4b7c217c11e8ec2ff53e0/urllib3-2.4.0.tar.gz"
-    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+    url "https://files.pythonhosted.org/packages/15/22/9ee70a2574a4f4599c47dd506532914ce044817c7752a79b6a51286319bc/urllib3-2.5.0.tar.gz"
+    sha256 "3fc47733c7e419d4bc3f6b3dc2b4f890bb743906a30d56ba4a5bfa4bbff92760"
   end
 
   resource "youtube-search-python" do
