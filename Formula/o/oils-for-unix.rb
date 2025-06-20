f8312f0ae2b947e8a4b920975640cfdfe28fa0cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 02:44:18 +0000

    oils-for-unix 0.30.0

--- Formula/o/oils-for-unix.rb
+++ Formula/o/oils-for-unix.rb
@@ -1,8 +1,8 @@
 class OilsForUnix < Formula
   desc "Bash-compatible Unix shell with more consistent syntax and semantics"
   homepage "https://oils.pub/"
-  url "https://oils.pub/download/oils-for-unix-0.29.0.tar.gz"
-  sha256 "34fdb53f1dbd2b8742ef74cd3a46d87abfcda18d0d645f29762c4fddb9a2b3ac"
+  url "https://oils.pub/download/oils-for-unix-0.30.0.tar.gz"
+  sha256 "d1d79dd41d0775b376184e6cd4cf8a1ea369aa9d9383abcc7d96725e2ffb82f2"
   license "Apache-2.0"
 
   livecheck do
