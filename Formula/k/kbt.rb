bc55fada45c6c015ccafa44859eb19763127a60b
Rui Chen <rui@chenrui.dev>
2025-06-20 17:35:58 +0000

    kbt: update 2.1.0 bottle.

--- Formula/k/kbt.rb
+++ Formula/k/kbt.rb
@@ -12,6 +12,7 @@ class Kbt < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "c0b91f3101be3a1c6a433602c7f629091bf5a689ecd8ce18eef413baafc267c3"
     sha256 cellar: :any_skip_relocation, sonoma:        "ac9a8d2c6fbb46d83056267b83050a1384b66ceb48d7cfd784e80215e84baabc"
     sha256 cellar: :any_skip_relocation, ventura:       "24a8ec918a6dfea51eb963c9538e7523476ba09680eee1252dc17fd37bad9f71"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d293593b2dc1f1b6eb0b9121dd9b465b8ed9f662c5018abf830089c5a9ea2301"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "811dc47da36e5d3914fc3c14fd0e2c4a3a797e1fadfc12191947c889d3fbd805"
   end
 
