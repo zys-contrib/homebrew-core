406110f89248e38df2982557a4b5beba618b6139
Rui Chen <rui@chenrui.dev>
2025-06-21 05:00:52 +0000

    arp-scan-rs: update 0.14.0 bottle.

--- Formula/a/arp-scan-rs.rb
+++ Formula/a/arp-scan-rs.rb
@@ -12,6 +12,7 @@ class ArpScanRs < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "4121091ba5ad198c3498f015af7ca9b6ede928b8e43b525529b9e0554d3fa07f"
     sha256 cellar: :any_skip_relocation, sonoma:        "95a48cfa4db0dd5ae427484d1785ec5cc6bc1d741c3930ded3423dcaf8b7afbc"
     sha256 cellar: :any_skip_relocation, ventura:       "435b583d3a01fa0504ac92950115c41a72d23c96001f170fd9e3316465758b5b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "22825e69ba7b9bef557bfdf6145a5c5b5a53a655c294028c5e359f039d253253"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "de71563f7071cdd6b37930ca4f20f2e9c3746f3befa53c9353822dc0dc838c70"
   end
 
