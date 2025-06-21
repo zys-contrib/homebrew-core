b838f8ef55b6472163d71f6b183bdcc4c85ce52a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-21 14:55:35 +0000

    tiledb: add 2.28.0 bottle.

--- Formula/t/tiledb.rb
+++ Formula/t/tiledb.rb
@@ -5,6 +5,16 @@ class Tiledb < Formula
   sha256 "de731cd0c8e82fe8cfca084b937dc0df41e451c8eb93071e4cc5aba7bbef854e"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "9468ff50f991980cd4a1f271157acda6c86180ade8ba9bb6d112c77a9376e791"
+    sha256 cellar: :any,                 arm64_sonoma:  "40161d0f697370e27e84f0c0a070936a9e1cf0f648d1b3797f85c36dc16d2525"
+    sha256 cellar: :any,                 arm64_ventura: "a845576b2650eaa736ecc376f3870210930a90fc5a539cac056abeaa320ee2ef"
+    sha256 cellar: :any,                 sonoma:        "1b49eb9525ee4fd3c1fe79c02ab9b63d8ea2fbf4ea235de722850b82fc9b71a2"
+    sha256 cellar: :any,                 ventura:       "38d698425d8a9c8010c5fd6ea2db466979954f6c87ac1d07f2010cd7faa8cf1d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eeb80c36f1f88dafe9bcd4982ca9089cc55338df63d13cfb2aaa6692a6db9648"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "367e2dddca418e1fcca9065fc02e3064cf3f6ade0586dde6d022016423895dec"
+  end
+
   depends_on "cmake" => :build
 
   depends_on "fmt"
