490cc357403fd05cb79ed3b0f0896366870fec19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-21 10:21:29 +0000

    rnp: add 0.18.0 bottle.

--- Formula/r/rnp.rb
+++ Formula/r/rnp.rb
@@ -10,6 +10,15 @@ class Rnp < Formula
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "3f32421017f5d7b8cbe686b773d77f7c489fc745aa187feb587b603a65a971eb"
+    sha256 cellar: :any,                 arm64_sonoma:  "63ad3905d1286594f3f98de8343474a660929974cd25caa0ebe4e9af505b5e41"
+    sha256 cellar: :any,                 arm64_ventura: "1793864c5ac801908df2c185aaa75fdf1ec78b589f4d686a50fbc94f6ac2e357"
+    sha256 cellar: :any,                 sonoma:        "6bbb2952df99c6b04c3cbc4c4465e6ce0eeebdebde37fe971d55f2f782b7b58b"
+    sha256 cellar: :any,                 ventura:       "b1045aedf6befd06a9fc6c2399282298b74d034eb4d01b09eef31bcb94be6dda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "426c2a6c95992b27f8b08631049fac856888caf66ef7bfd6d5c2afd50c9f98a8"
+  end
   depends_on "cmake" => :build
   depends_on "botan"
   depends_on "json-c"
