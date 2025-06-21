dcfd1863c495846c2c14702b0991158bb0c1a352
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-21 17:16:41 +0000

    svgo 4.0.0

--- Formula/s/svgo.rb
+++ Formula/s/svgo.rb
@@ -1,8 +1,8 @@
 class Svgo < Formula
   desc "Nodejs-based tool for optimizing SVG vector graphics files"
   homepage "https://svgo.dev/"
-  url "https://github.com/svg/svgo/archive/refs/tags/v3.3.2.tar.gz"
-  sha256 "bf79f18acd85764bd12ed7335f2d8fdc7d11760e7c4ed8bd0dc39f1272825671"
+  url "https://github.com/svg/svgo/archive/refs/tags/v4.0.0.tar.gz"
+  sha256 "dabd71f420d29747e8a10b15c4fafff7477ff2de2846c20f8e84de3bc093adaf"
   license "MIT"
 
   bottle do
