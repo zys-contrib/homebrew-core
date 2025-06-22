66eadbb5d754687dd9662b643ece47159395f70c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-22 11:17:16 +0000

    rospo: update 0.15.0 bottle.

--- Formula/r/rospo.rb
+++ Formula/r/rospo.rb
@@ -6,12 +6,12 @@ class Rospo < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a38299addf3e2fb0de87abb726fb40dc629af9d9b370ba656749efb7490e87ed"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a38299addf3e2fb0de87abb726fb40dc629af9d9b370ba656749efb7490e87ed"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a38299addf3e2fb0de87abb726fb40dc629af9d9b370ba656749efb7490e87ed"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c8f39e941e155718d2e6033d946952ac1095321a206491ca3908af8574e97187"
-    sha256 cellar: :any_skip_relocation, ventura:       "c8f39e941e155718d2e6033d946952ac1095321a206491ca3908af8574e97187"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa3e69f483e95ffa4457f8a0f9e68cbbd313c864cdb24c316d729f8ed5693c33"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78ba435c179d319f9a69ab7ccb723016b6ae62c530233cb33c657439d0eded78"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78ba435c179d319f9a69ab7ccb723016b6ae62c530233cb33c657439d0eded78"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "78ba435c179d319f9a69ab7ccb723016b6ae62c530233cb33c657439d0eded78"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0421d64f7b8d2a15bf72950f9f0d1ba9716c55a78f76a6d6395a19097ad116cf"
+    sha256 cellar: :any_skip_relocation, ventura:       "0421d64f7b8d2a15bf72950f9f0d1ba9716c55a78f76a6d6395a19097ad116cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "530ff6c3733b5f2ef745c4aced487c64a5c86baecdd28bf70d520ef2e9d090bd"
   end
 
   depends_on "go" => :build
