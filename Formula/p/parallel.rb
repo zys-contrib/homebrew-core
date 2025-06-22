7761b2237dab0174e4ac5b24d03bea3ee6fd7e38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-22 23:09:24 +0000

    parallel 20250622

--- Formula/p/parallel.rb
+++ Formula/p/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20250522.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20250522.tar.bz2"
-  sha256 "b4b28f475f8cff8bb6ed4b03cc5a67041f18fc73fa256923b23181b56afdb2cb"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20250622.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20250622.tar.bz2"
+  sha256 "69f578cf11f1b124ba3c2b673a16641debe63aeb3d2ac4cec5ad65f8a53d489b"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
