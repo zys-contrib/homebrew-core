97cb674f172b23b11b4671e629dbc3558c3c5f5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-22 10:49:28 +0000

    activemq 6.1.7

--- Formula/a/activemq.rb
+++ Formula/a/activemq.rb
@@ -1,9 +1,9 @@
 class Activemq < Formula
   desc "Apache ActiveMQ: powerful open source messaging server"
   homepage "https://activemq.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=activemq/6.1.6/apache-activemq-6.1.6-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/activemq/6.1.6/apache-activemq-6.1.6-bin.tar.gz"
-  sha256 "32ac692da486d7e82819586dd3c5661bbbb5d6d42599832a494797058608f0a4"
+  url "https://www.apache.org/dyn/closer.lua?path=activemq/6.1.7/apache-activemq-6.1.7-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/activemq/6.1.7/apache-activemq-6.1.7-bin.tar.gz"
+  sha256 "75cc41109a897745d44aca27358568f3cbe0cd58fc6bbff035a83c4ddf48d316"
   license "Apache-2.0"
 
   bottle do
