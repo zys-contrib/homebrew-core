712fd7b82d3383313e86e9d423bc1556ce435d5c
Mike McQuaid <mike@mikemcquaid.com>
2025-06-23 08:42:25 +0100

    nova-fairwinds: rename from nova.
    Needed for https://github.com/Homebrew/brew/pull/20134

---
+++ Formula/n/nova-fairwinds.rb
@@ -0,0 +1,27 @@
+class NovaFairwinds < Formula
+  desc "Find outdated or deprecated Helm charts running in your cluster"
+  homepage "https://github.com/FairwindsOps/nova"
+  url "https://github.com/FairwindsOps/nova/archive/refs/tags/v3.11.4.tar.gz"
+  sha256 "e4364254b385a84701ff649bb47114b5e7b5a59d3d1e61a12ea9663715164fd8"
+  license "Apache-2.0"
+  head "https://github.com/FairwindsOps/nova.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user}"
+    system "go", "build", *std_go_args(output: bin/"nova", ldflags:)
+
+    generate_completions_from_executable(bin/"nova", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nova version")
+
+    system bin/"nova", "generate-config", "--config=nova.yaml"
+    assert_match "chart-ignore-list: []", (testpath/"nova.yaml").read
+
+    output = shell_output("#{bin}/nova find --helm 2>&1", 255)
+    assert_match "try setting KUBERNETES_MASTER environment variable", output
+  end
+end
