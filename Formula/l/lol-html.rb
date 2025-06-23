1b80e5bb0fd0618ab8846d767e77f1f2033f2e10
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-23 15:11:50 +0000

    lol-html: update 2.5.0 bottle.

--- Formula/l/lol-html.rb
+++ Formula/l/lol-html.rb
@@ -7,13 +7,13 @@ class LolHtml < Formula
   head "https://github.com/cloudflare/lol-html.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "5147b4f58bf5dd2f379559e569cf021a572b853568d1bf8d59416fe869754662"
-    sha256 cellar: :any,                 arm64_sonoma:  "dc66ba33c71ca9ebf648f7382f44709f06620bedde3a55a614fca5296400bc8c"
-    sha256 cellar: :any,                 arm64_ventura: "a7be91540787540353ef9fe9ce687d777141e1d6fb6bb2603872ee933b726481"
-    sha256 cellar: :any,                 sonoma:        "71bc45af79edf3ef82ee1f98e781b9a4c578c35bdeaffdaa8c390a2f78a1fa96"
-    sha256 cellar: :any,                 ventura:       "47ecf2b443bbe11730c2600ba0a5d3f3827b6d060887b3d30a859b3f48830578"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "093b5c6fd7b095a387d8469ac58b90773e8fea08fa568c610b85c9ed6f3f87ee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "915a8368d4c5a083aafa5d8745d2515fd331aa540b9683ba1afff952946f5bdb"
+    sha256 cellar: :any,                 arm64_sequoia: "9a5bf511b0e79fafebf68e51d8d70ce2849b7cac56441234176b45b0d4283984"
+    sha256 cellar: :any,                 arm64_sonoma:  "b1071b1948acd68a50e8cb9f119533de8417bbc5ec0faa1c53fdd47e83ffe93e"
+    sha256 cellar: :any,                 arm64_ventura: "adca8690479cc493a5d8faccec5325382dceddef1700cfe0d1c2063d093ac280"
+    sha256 cellar: :any,                 sonoma:        "441869e3e8e1c9807844c36e7cd0e21d25d2f4c753374554c6835bbc9d6448fd"
+    sha256 cellar: :any,                 ventura:       "5b06c440efeee13f08a8560fc990c1c2ee960f75e77537e4ea471f478615f70f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9c2095241f8b34ccc99cb2db8fa903a96348508638aadb468b4f89a79e9408d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "17393e6b70e0c070a3b363e44466bb4fa6f767936604c601c5aaae4a58381f87"
   end
 
   depends_on "cargo-c" => :build
