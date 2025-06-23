8c24e98f4eee8b7b0517e7ba4d278fbf1ad95dc3
Mike McQuaid <mike@mikemcquaid.com>
2025-06-23 12:53:00 +0000

    rio-terminal: add 0.2.20 bottle.

--- Formula/r/rio-terminal.rb
+++ Formula/r/rio-terminal.rb
@@ -11,6 +11,14 @@ class RioTerminal < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "63e09e743db4330037115be47c3a07574f783f3879e7f0ceda64b228ea26334a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aa14fdda5addcc287151a57650214e38be7ab91a2d0ba81f3c4d16bfb1ae2981"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "072d52ac1161c68b075aff880a50733aa9e9c05d30c9ba20984912e1efdf9e2d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "86b53b046508dcd7a6e4ada2c3399e413f7a716053cb26d4bd1b49996368c7d5"
+    sha256 cellar: :any_skip_relocation, ventura:       "0407e0ab3bc337af5587784cbec9f49c5da133377a9e15323aeb9e7d28edd2e6"
+  end
+
   depends_on "rust" => :build
   # Rio does work for Linux although it requires a specification of which
   # window manager will be used (x11 or wayland) otherwise will not work.
