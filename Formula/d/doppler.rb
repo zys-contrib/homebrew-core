98c096479be75b8445cbd9953f269fae8cbee3f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-23 20:22:01 +0000

    doppler: update 3.75.1 bottle.

--- Formula/d/doppler.rb
+++ Formula/d/doppler.rb
@@ -7,12 +7,12 @@ class Doppler < Formula
   head "https://github.com/DopplerHQ/cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "16c1776f8ba531da5df3be67735536c3f1cc45d111872a91e0ef77f973008ed5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "16c1776f8ba531da5df3be67735536c3f1cc45d111872a91e0ef77f973008ed5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "16c1776f8ba531da5df3be67735536c3f1cc45d111872a91e0ef77f973008ed5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6dc099de992e5cb5999553896be256a99fd5c57479bbec640a6a67ab337e394b"
-    sha256 cellar: :any_skip_relocation, ventura:       "6dc099de992e5cb5999553896be256a99fd5c57479bbec640a6a67ab337e394b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23c23f1560ef0f92b319916ff4e3e6cd9472c574355da71baa48409e41623688"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c6c9327df7c0c4b1e8c497fdd3acc7d8fb13c34fa8d38d6733e1203428b31b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c6c9327df7c0c4b1e8c497fdd3acc7d8fb13c34fa8d38d6733e1203428b31b4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9c6c9327df7c0c4b1e8c497fdd3acc7d8fb13c34fa8d38d6733e1203428b31b4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bff18de552956fa3893a9f8eb2da726de251d7c20429798224eb3cec82e8dfe3"
+    sha256 cellar: :any_skip_relocation, ventura:       "bff18de552956fa3893a9f8eb2da726de251d7c20429798224eb3cec82e8dfe3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "08ce028ea8d6d035e69508e1a66294e84cc2fc6a14f9e6f29ea1e4c836ebb6bc"
   end
 
   depends_on "go" => :build
