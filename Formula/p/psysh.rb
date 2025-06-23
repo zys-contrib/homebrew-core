e5891350a551f50ae3f688b13345270a2786e0c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-23 03:09:36 +0000

    psysh: update 0.12.9 bottle.

--- Formula/p/psysh.rb
+++ Formula/p/psysh.rb
@@ -6,13 +6,13 @@ class Psysh < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f7af2ac0c32da2877143af8acdd102ca56dbf11939c69447ca6c667a4919a09e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f7af2ac0c32da2877143af8acdd102ca56dbf11939c69447ca6c667a4919a09e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f7af2ac0c32da2877143af8acdd102ca56dbf11939c69447ca6c667a4919a09e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4ac72659f7e05d491e23124f7acc8c8379ada20040bfee36e056db2a136ddfb4"
-    sha256 cellar: :any_skip_relocation, ventura:       "4ac72659f7e05d491e23124f7acc8c8379ada20040bfee36e056db2a136ddfb4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f7af2ac0c32da2877143af8acdd102ca56dbf11939c69447ca6c667a4919a09e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7af2ac0c32da2877143af8acdd102ca56dbf11939c69447ca6c667a4919a09e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2fcbe60c7c8061b73002734ee99c45340a6bb8021ef13fc3a332f5bb253c4c7e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2fcbe60c7c8061b73002734ee99c45340a6bb8021ef13fc3a332f5bb253c4c7e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2fcbe60c7c8061b73002734ee99c45340a6bb8021ef13fc3a332f5bb253c4c7e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "41572ae18ef058a3b6d20b955d50170737bbebcf882e44bf2f06e603032922c3"
+    sha256 cellar: :any_skip_relocation, ventura:       "41572ae18ef058a3b6d20b955d50170737bbebcf882e44bf2f06e603032922c3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2fcbe60c7c8061b73002734ee99c45340a6bb8021ef13fc3a332f5bb253c4c7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2fcbe60c7c8061b73002734ee99c45340a6bb8021ef13fc3a332f5bb253c4c7e"
   end
 
   depends_on "php"
