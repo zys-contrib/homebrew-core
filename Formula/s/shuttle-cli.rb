996fe3d9cd1405770cb9606949cb70d0f4499250
Mike McQuaid <mike@mikemcquaid.com>
2025-06-23 12:53:00 +0000

    shuttle-cli: add 0.24.3 bottle.

--- Formula/s/shuttle-cli.rb
+++ Formula/s/shuttle-cli.rb
@@ -6,6 +6,15 @@ class ShuttleCli < Formula
   license "Apache-2.0"
   head "https://github.com/lunarway/shuttle.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6239f4fa616ca21a781595b6da437d6debcb1ad013242c33c3a54aba9764da64"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6239f4fa616ca21a781595b6da437d6debcb1ad013242c33c3a54aba9764da64"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6239f4fa616ca21a781595b6da437d6debcb1ad013242c33c3a54aba9764da64"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5aed20ea25a270dd87249277003e75da31e91988fb461b95596b727bb35df25c"
+    sha256 cellar: :any_skip_relocation, ventura:       "5aed20ea25a270dd87249277003e75da31e91988fb461b95596b727bb35df25c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "be0bb28ca0505b75b4c6606f528eac6a2b6fdbb1e40e1ed3fb788793114daaff"
+  end
+
   depends_on "go" => :build
 
   conflicts_with "cargo-shuttle", because: "both install `shuttle` binaries"
