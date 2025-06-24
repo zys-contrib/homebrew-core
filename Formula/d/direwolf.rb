b30a79e59f03e22cf2751f811a0f4d444f825b93
Rui Chen <rui@chenrui.dev>
2025-06-24 14:58:15 +0000

    direwolf: update 1.7_1 bottle.

--- Formula/d/direwolf.rb
+++ Formula/d/direwolf.rb
@@ -23,15 +23,13 @@ class Direwolf < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256                               arm64_sequoia:  "fb3b2a272641595f3ea481b05ac39409f75466cdf50fc9592b3cdddeca1b3d20"
-    sha256                               arm64_sonoma:   "f15cf78ea350bac7d0daf8663e54242eed663f577400de7630f8225c4e08e340"
-    sha256                               arm64_ventura:  "494d3a0854c7d919fa8446915bec34806b387c138fd23210c05ab3677c701faf"
-    sha256                               arm64_monterey: "1c437d0c62b29032faf6eb115caef44b62bc7ef7525d5a137c09eec5015a0f49"
-    sha256                               sonoma:         "5ad4a23ede0053b7c587b99514c48982378ad7f5e4ae4871e8b8eea6dccb6249"
-    sha256                               ventura:        "e3064f51ee693453adc1e25a4dc7e7f1617f51d070dc209c847a7459968de0dc"
-    sha256                               monterey:       "16947ff3e289c953ddb75acb44215ab7f74a5128a20464cfb7f6332b4438f25d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "2dc92264176a4397db556c4add6b01609e6a0364a39359f4b9d04adee3b1ec97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b20722ff017ed04a511f79061ad26959cdb92e5c7dc1080dbc22cc05c07e374"
+    sha256                               arm64_sequoia: "f99bcf9fef1cc83130d68c43ae4da0bb86cf3020c53898272c9f3dc3fe1a69b6"
+    sha256                               arm64_sonoma:  "d99b828167a2e694d76319a97c1a5806be165f0005849fad1f3c2ed31b8b3486"
+    sha256                               arm64_ventura: "32e606d1d65f94fb27a348abf01abadd78c74601c89c47d4ad84bc957fe0c071"
+    sha256                               sonoma:        "f34cd15417a27425220ab4f6624f6c90844eb7ac5c15785cccb9122a49537f9e"
+    sha256                               ventura:       "185a41a84b513d3bbc9427a55675abdcbdde62b0dca9b736f9016f135f6a5268"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "29394877b827bdec83e7082dd20c31ff3481bccbbdb98f77c243b90788c60ff4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89c355af2b949f2b14372ef9b9935a0de9aca700660987313d52f30887e51a8d"
   end
 
   depends_on "cmake" => :build
