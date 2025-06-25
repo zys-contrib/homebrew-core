3ced8c20e9b310758938151c081617427451d974
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 08:06:35 +0000

    kumactl 2.11.1

--- Formula/k/kumactl.rb
+++ Formula/k/kumactl.rb
@@ -1,8 +1,8 @@
 class Kumactl < Formula
   desc "Kuma control plane command-line utility"
   homepage "https://kuma.io/"
-  url "https://github.com/kumahq/kuma/archive/refs/tags/2.11.0.tar.gz"
-  sha256 "1887e12a27b779d41111347bc5181573165cf6063bbcd622b50977d26c4e2a59"
+  url "https://github.com/kumahq/kuma/archive/refs/tags/2.11.1.tar.gz"
+  sha256 "0a18d919efdbaae3ca725aa9c33fe7441e99d7ba230b84145f90018f1c00b267"
   license "Apache-2.0"
   head "https://github.com/kumahq/kuma.git", branch: "master"
 
