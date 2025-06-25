37ab8d60925349fa2675055b8a2b358b7102d9d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 21:06:02 +0000

    mockery: update 3.5.0 bottle.

--- Formula/m/mockery.rb
+++ Formula/m/mockery.rb
@@ -15,12 +15,12 @@ class Mockery < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "31e13ab8f668e28dcacb564c4ed837f78f8cf1c5ffa7b593ddb51016148df304"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "31e13ab8f668e28dcacb564c4ed837f78f8cf1c5ffa7b593ddb51016148df304"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "31e13ab8f668e28dcacb564c4ed837f78f8cf1c5ffa7b593ddb51016148df304"
-    sha256 cellar: :any_skip_relocation, sonoma:        "14bc419a50659dbb608307dbe86aff9fe6b529d0189bdae04bb861e4baca1d89"
-    sha256 cellar: :any_skip_relocation, ventura:       "14bc419a50659dbb608307dbe86aff9fe6b529d0189bdae04bb861e4baca1d89"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6478eb1a5220ec758350ffddb1f914118a77f7b043dc2e278337a3a2e7bdadf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a291387bf9f3ec3fb1947f08e1cd686ae36502d226220ed513248aea6766fafd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a291387bf9f3ec3fb1947f08e1cd686ae36502d226220ed513248aea6766fafd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a291387bf9f3ec3fb1947f08e1cd686ae36502d226220ed513248aea6766fafd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "94bb183ece74ea3040bca9fe64e166901cd0bc10356fd787e47f664406ac8755"
+    sha256 cellar: :any_skip_relocation, ventura:       "94bb183ece74ea3040bca9fe64e166901cd0bc10356fd787e47f664406ac8755"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "914def2eb5ad6dda1ff9d93ba5134f3791e59e9cd4ae63ab6a37fc72af640e5a"
   end
 
   depends_on "go" => :build
