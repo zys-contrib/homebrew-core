fa50398b0a43b7ec8e70f9cdeb8d0d0f066a8b8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 03:45:20 +0000

    renovate: update 41.8.0 bottle.

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -17,13 +17,13 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "afb05e6aebf6da45f277a67587669fbd8e56e7a4e42702a08dc99421c88d653f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "098a2ee1e97ce2d878cd7a5b6f19fd9eecefa3d343e75f82a3c289c4226e09ec"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5d68735f844a1ea3360029f95fb6111f8b0e50a50d157949d37f17e615af8ebc"
-    sha256 cellar: :any_skip_relocation, sonoma:        "37005553fd47e95363e9cd4526513335651c2ffb3957d4396fe89170c31d5f10"
-    sha256 cellar: :any_skip_relocation, ventura:       "13a41232d0db3beeb5ec5133cacbbdf20c6b4f0de58af506ed5f728f267f093c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7dae3c0ec3c5f5300f73a14b0a200f5597af79c3507acfcb498e0854dc813f03"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fef6b1e7f6f783e09bc839c1cb3679a6126ae60eef933c33071e6b49338ad91e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5cfd1f022aa4201a92e5ab13567c2c5220809500c9b228b48e41b2278d2bee0d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f3e7d3c69f0bb63a4a10c45537772999e618f481b18c375caa405586038d5b31"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "290adae2d1fd84ed8356a2de300680d1d1acfdd40433bd34d9249778877c5a1c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1ddc2ae0094d0c18c446e45d79f37c7de9c5bec7758fe64f6d900de64a572953"
+    sha256 cellar: :any_skip_relocation, ventura:       "3999a4276f5851954f33bf55e0dccb67413d31971c80dacba08521adcbb9960c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d7e7e2bd37905f3aeeaddd0401ad55113bbe528b8ac2c765583fce176ff9d312"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6537e286f979a8d700589002088af67aca2c4d9df27ce151c2247acf093461ca"
   end
 
   depends_on "node@22"
