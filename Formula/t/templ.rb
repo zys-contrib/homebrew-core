0a30d24c6f6dccaa0804728e55fc075e15e45078
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 09:10:26 +0000

    templ: update 0.3.906 bottle.

--- Formula/t/templ.rb
+++ Formula/t/templ.rb
@@ -6,12 +6,12 @@ class Templ < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c996089deed3bed467992b73b22e24e93f3f0318ab01a0693e217499a59652e5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c996089deed3bed467992b73b22e24e93f3f0318ab01a0693e217499a59652e5"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "c996089deed3bed467992b73b22e24e93f3f0318ab01a0693e217499a59652e5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c77ceac08bd26e98c4d1857ee1161e8d52687d92a3bbd4d9a16f793b78778729"
-    sha256 cellar: :any_skip_relocation, ventura:       "c77ceac08bd26e98c4d1857ee1161e8d52687d92a3bbd4d9a16f793b78778729"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a3ae81640e92aa900cea2ed0b25e4c38cd519496a8b6c6ac905e402ec7a76ea1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c2369906ae3f750d3094635fe33c8ae3ec0b5d5e646c44602b8665258eeb2dab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c2369906ae3f750d3094635fe33c8ae3ec0b5d5e646c44602b8665258eeb2dab"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c2369906ae3f750d3094635fe33c8ae3ec0b5d5e646c44602b8665258eeb2dab"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a317b26c379fdf6a72ed54d818794498d1475a14e598c4c04273414393f84051"
+    sha256 cellar: :any_skip_relocation, ventura:       "a317b26c379fdf6a72ed54d818794498d1475a14e598c4c04273414393f84051"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc29e24659c290f61effc35212affd5b4540ba4dbcd5402e25290c1374d8f9cd"
   end
 
   depends_on "go" => :build
