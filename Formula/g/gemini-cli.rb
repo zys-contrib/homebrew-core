969abb9be8858abd46a947f7ff0bd8587dc91595
Patrick Linnane <patrick@linnane.io>
2025-06-25 12:56:53 -0700

    gemini-cli 0.1.3 (new formula)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/g/gemini-cli.rb
@@ -0,0 +1,20 @@
+class GeminiCli < Formula
+  desc "Interact with Google Gemini AI models from the command-line"
+  homepage "https://github.com/google-gemini/gemini-cli"
+  url "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.1.3.tgz"
+  sha256 "c12db51e176e9f1b0f5c329df7596d1099e9193c44c2ef6f3c5b39c0f568a184"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gemini --version")
+
+    assert_match "Please set an Auth method", shell_output("#{bin}/gemini --prompt Homebrew 2>&1", 1)
+  end
+end
