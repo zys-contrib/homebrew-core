bfc56c230593139bd7695cbf523edcf180850358
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-26 18:28:21 +0000

    pnpm: update 10.12.4 bottle.

--- Formula/p/pnpm.rb
+++ Formula/p/pnpm.rb
@@ -13,13 +13,13 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "32872b400613460d29c430d7ed3d5ad1e94815b278da34a167fa4b08253ea648"
-    sha256 cellar: :any,                 arm64_sonoma:  "32872b400613460d29c430d7ed3d5ad1e94815b278da34a167fa4b08253ea648"
-    sha256 cellar: :any,                 arm64_ventura: "32872b400613460d29c430d7ed3d5ad1e94815b278da34a167fa4b08253ea648"
-    sha256 cellar: :any,                 sonoma:        "504ae9c03a3a5417fdbdff2abe58bb609473f5041504ea8c62665cb79efd9613"
-    sha256 cellar: :any,                 ventura:       "504ae9c03a3a5417fdbdff2abe58bb609473f5041504ea8c62665cb79efd9613"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "338c9c7f8ef1380e2460a459d37c55d8b3635ad1401b03156d091d629c000ab4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "338c9c7f8ef1380e2460a459d37c55d8b3635ad1401b03156d091d629c000ab4"
+    sha256 cellar: :any,                 arm64_sequoia: "f6482b264d7f9f15383f6aedc856c9b745f48e1d819dc63f932de1d4b0a7e7aa"
+    sha256 cellar: :any,                 arm64_sonoma:  "f6482b264d7f9f15383f6aedc856c9b745f48e1d819dc63f932de1d4b0a7e7aa"
+    sha256 cellar: :any,                 arm64_ventura: "f6482b264d7f9f15383f6aedc856c9b745f48e1d819dc63f932de1d4b0a7e7aa"
+    sha256 cellar: :any,                 sonoma:        "1ec9276b0f0cbaadf9a3d866f36ef084d5c00dc65283fb08b56fc7e7d651b359"
+    sha256 cellar: :any,                 ventura:       "1ec9276b0f0cbaadf9a3d866f36ef084d5c00dc65283fb08b56fc7e7d651b359"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6de72928cd67c7fceb4770226c314e73907c373d192a473b6f5033ce584c22fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6de72928cd67c7fceb4770226c314e73907c373d192a473b6f5033ce584c22fb"
   end
 
   depends_on "node" => [:build, :test]
