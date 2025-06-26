fb24afb2287c0151d52c23246e03fd8c35184c6f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-26 17:14:37 +0000

    claude-squad: update 1.0.7 bottle.

--- Formula/c/claude-squad.rb
+++ Formula/c/claude-squad.rb
@@ -7,12 +7,12 @@ class ClaudeSquad < Formula
   head "https://github.com/smtg-ai/claude-squad.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ffecddd650604eae9d3310add80b7dd038afbaf8a380c38e4e5228e67ed8b00c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ffecddd650604eae9d3310add80b7dd038afbaf8a380c38e4e5228e67ed8b00c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ffecddd650604eae9d3310add80b7dd038afbaf8a380c38e4e5228e67ed8b00c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "06685cb47f88f02a82e76a416a491077ce161e7db9d7cf3680c1888214de9c51"
-    sha256 cellar: :any_skip_relocation, ventura:       "06685cb47f88f02a82e76a416a491077ce161e7db9d7cf3680c1888214de9c51"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e37f2489bf1eb830a9ee08b4a7988f2d44488b174bf110e7fd1de538174d71e5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe712cc3be008b8d012e5955d9d2427620f2838808a58945f08a401974e1e1b1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fe712cc3be008b8d012e5955d9d2427620f2838808a58945f08a401974e1e1b1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fe712cc3be008b8d012e5955d9d2427620f2838808a58945f08a401974e1e1b1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d575b578990779da2668e755be1207550ec3aa47a34df21fb64c2c054334bc8a"
+    sha256 cellar: :any_skip_relocation, ventura:       "d575b578990779da2668e755be1207550ec3aa47a34df21fb64c2c054334bc8a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9104ee1bfdf0205766ee85dc264d1b4b5b12a683e08d17449f75b3732228c584"
   end
 
   depends_on "go" => :build
