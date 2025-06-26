af5525856ee4c6c981c8a5568c326e51aa40785a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-26 02:51:29 +0000

    swc 1.12.7

--- Formula/s/swc.rb
+++ Formula/s/swc.rb
@@ -1,8 +1,8 @@
 class Swc < Formula
   desc "Super-fast Rust-based JavaScript/TypeScript compiler"
   homepage "https://swc.rs"
-  url "https://github.com/swc-project/swc/archive/refs/tags/v1.12.6.tar.gz"
-  sha256 "292817448a5fe1c8c82d721ce4154806efde082aaf513808528e1a9983e3273d"
+  url "https://github.com/swc-project/swc/archive/refs/tags/v1.12.7.tar.gz"
+  sha256 "4b8a7988943ded37449c5be65d878ba9b810e656e41118ac848a30c79137e4cd"
   license "Apache-2.0"
   head "https://github.com/swc-project/swc.git", branch: "main"
 
