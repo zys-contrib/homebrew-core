3d6ae7627746d8afb76a9e056141a4819a64826a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-26 14:57:26 +0000

    yek: add 0.21.0 bottle.

--- Formula/y/yek.rb
+++ Formula/y/yek.rb
@@ -11,6 +11,15 @@ class Yek < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "2d5276241deded5acf624a31b29ac860986c82fa12d46e3bc0dd697b86316730"
+    sha256 cellar: :any,                 arm64_sonoma:  "e002487f80db2f64322b9110b33705159d36da98dba3916e832d88caf8143346"
+    sha256 cellar: :any,                 arm64_ventura: "a44f0bd71899bbb2bd95d3c1cf07e72b4a511c861e7bef2a1883e572df2c0f32"
+    sha256 cellar: :any,                 sonoma:        "1627a4d75dd780a1ecf0c2f756327102ef247c30b612aa931e4067684c4a97af"
+    sha256 cellar: :any,                 ventura:       "1117b2d2b0d74e8c3dd4214e5dda6ced03090ccea62d54d214d376aca945fa96"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a63bf23710982ff18db2e129a2001c7447007d24ca9b30be4a26de2c0b83f16"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
