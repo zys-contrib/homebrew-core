f8bcad855e8604ab48e05231fbfdca87d1b9009d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-27 10:51:13 +0000

    appwrite 8.1.0

--- Formula/a/appwrite.rb
+++ Formula/a/appwrite.rb
@@ -1,8 +1,8 @@
 class Appwrite < Formula
   desc "Command-line tool for Appwrite"
   homepage "https://appwrite.io"
-  url "https://registry.npmjs.org/appwrite-cli/-/appwrite-cli-8.0.2.tgz"
-  sha256 "a87f5ddb4b7119b1a40e272983be6f061f3d595ff0407f9cdfced0e2e8d9872d"
+  url "https://registry.npmjs.org/appwrite-cli/-/appwrite-cli-8.1.0.tgz"
+  sha256 "4bb97995b58e7a0cd7756fbe4dbdfe82527518d832e4163d0d2e5b3eb5bb044d"
   license "BSD-3-Clause"
 
   bottle do
