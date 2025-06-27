d496d9df1998a494b4c6c3e882be170f2145873e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-27 14:40:56 +0000

    jq-lsp: add 0.1.13 bottle.

--- Formula/j/jq-lsp.rb
+++ Formula/j/jq-lsp.rb
@@ -6,6 +6,15 @@ class JqLsp < Formula
   license "MIT"
   head "https://github.com/wader/jq-lsp.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a9cec3d399e0d6c5d50e89d4dcf8f953b77fd362b592f6e63b0b0be286e9ed42"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a9cec3d399e0d6c5d50e89d4dcf8f953b77fd362b592f6e63b0b0be286e9ed42"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a9cec3d399e0d6c5d50e89d4dcf8f953b77fd362b592f6e63b0b0be286e9ed42"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ecc89cf44dcdd6924883a47fca57336d075678c5c7862d4849c7ebe9aa23fc04"
+    sha256 cellar: :any_skip_relocation, ventura:       "ecc89cf44dcdd6924883a47fca57336d075678c5c7862d4849c7ebe9aa23fc04"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0feaf2b9afcdabee223722922c26b851e2eb3536f5a873b6529f5a3c073b2ca"
+  end
+
   depends_on "go" => :build
 
   def install
