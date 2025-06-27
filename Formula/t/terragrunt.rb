282cc9e5f09c9e9f2440e988cd608c2a4c4290dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-27 17:19:54 +0000

    terragrunt 0.82.3

--- Formula/t/terragrunt.rb
+++ Formula/t/terragrunt.rb
@@ -1,8 +1,8 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v0.82.2.tar.gz"
-  sha256 "d953552ecff91be5f9fec86672a8eaf380820e884f11c6c97116727c8ee3fa5b"
+  url "https://github.com/gruntwork-io/terragrunt/archive/refs/tags/v0.82.3.tar.gz"
+  sha256 "7b5689b06fc14ca391452645d88e4d8a22f2fccad1a2a7ac406a366eb4319075"
   license "MIT"
 
   # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
