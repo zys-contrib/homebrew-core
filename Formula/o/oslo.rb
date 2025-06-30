9afcdb0dd8aa74d14823ff7be106f3ba96f0e85a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-30 04:02:50 +0000

    oslo: update 0.13.0 bottle.

--- Formula/o/oslo.rb
+++ Formula/o/oslo.rb
@@ -7,14 +7,12 @@ class Oslo < Formula
   head "https://github.com/openslo/oslo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "a5977795384029ca047af4c7226a66c98907587e183469462a0f6c8ecb46e659"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "8aa369ef73a62437f2aa4bd37d40e508c15a77faf74be64b254052c09f7136d1"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "588243b56e54179dbabc31b5441cd280fd48e8c0d4c6e810728e432f21f1cdfc"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "081177cd01addb83aa55c63a939fc19a14a957ee288271553fd0f044c551ee66"
-    sha256 cellar: :any_skip_relocation, sonoma:         "a73ea7107111dc167676f2bc7e9ab87d6fde983a81240c158d77cac3bb74a5e7"
-    sha256 cellar: :any_skip_relocation, ventura:        "6d05071e13f3980b317c5ff34678006735d85b0dd6e1254b8685586f9d112eb5"
-    sha256 cellar: :any_skip_relocation, monterey:       "60bc14460a1534923cd36f62ec3a4b48e2f65decddb0cacb2c8ee8789bba433d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eb22c17240703d9b7348e2e5711b6949751a9a6b629e22b39101d19bc14e369c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1dc252e4e8d683047409636266e7c11e62725cc9ac3a21c394c1a0f04b644654"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1dc252e4e8d683047409636266e7c11e62725cc9ac3a21c394c1a0f04b644654"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1dc252e4e8d683047409636266e7c11e62725cc9ac3a21c394c1a0f04b644654"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bd87cd615862dce34c7e3372d0b2496cb34a4813a352fa0e9c677eaaaa2cbf95"
+    sha256 cellar: :any_skip_relocation, ventura:       "bd87cd615862dce34c7e3372d0b2496cb34a4813a352fa0e9c677eaaaa2cbf95"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ab3bfa70149e279e8a2902afe6ff19044c128da80df54b70fd711ed3ac445be"
   end
 
   depends_on "go" => :build
