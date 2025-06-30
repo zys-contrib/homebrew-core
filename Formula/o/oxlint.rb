928bb32c3126ec79e19444c3156bb64788c5003e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-30 08:13:56 +0000

    oxlint 1.4.0
    oxlint: add exit code patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/oxlint.rb
+++ Formula/o/oxlint.rb
@@ -1,10 +1,19 @@
 class Oxlint < Formula
   desc "Suite of high-performance tools for JavaScript and TypeScript written in Rust"
   homepage "https://oxc.rs/"
-  url "https://github.com/oxc-project/oxc/archive/refs/tags/oxlint_v1.3.0.tar.gz"
-  sha256 "660fffae9e46724e5dab8dbb13fbe50becef537057790dcd659f85727dbaa823"
   license "MIT"
   head "https://github.com/oxc-project/oxc.git", branch: "main"
+
+  stable do
+    url "https://github.com/oxc-project/oxc/archive/refs/tags/oxlint_v1.4.0.tar.gz"
+    sha256 "cfb910f8100da7abbc31fbac3a25acd73039d98787b27bc463db137c3e2e2317"
+
+    # fix exit code for `--version`
+    patch do
+      url "https://github.com/oxc-project/oxc/commit/4b2c6587c0a4ef41c187da915e591eda64deb81a.patch?full_index=1"
+      sha256 "14cf100271b9ccdc883a5859e53b33637aec797cd8f7e8cbd5fc03715e4663b5"
+    end
+  end
 
   livecheck do
     url :stable
