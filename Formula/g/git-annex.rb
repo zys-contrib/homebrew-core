031e0153532808c1db3291f16992a4ef1c016b3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-30 14:07:50 +0000

    git-annex 10.20250630

--- Formula/g/git-annex.rb
+++ Formula/g/git-annex.rb
@@ -1,8 +1,8 @@
 class GitAnnex < Formula
   desc "Manage files with git without checking in file contents"
   homepage "https://git-annex.branchable.com/"
-  url "https://hackage.haskell.org/package/git-annex-10.20250605/git-annex-10.20250605.tar.gz"
-  sha256 "72782fde9a2ddadc6f09f2d90cde2eb7a13248f19f95d7a41ba95ba4bd347e2b"
+  url "https://hackage.haskell.org/package/git-annex-10.20250630/git-annex-10.20250630.tar.gz"
+  sha256 "03df602f2f72110d5a782a760399b64a57d37661f84aed6612d9d62d727459ed"
   license all_of: ["AGPL-3.0-or-later", "BSD-2-Clause", "BSD-3-Clause",
                    "GPL-2.0-only", "GPL-3.0-or-later", "MIT"]
   head "git://git-annex.branchable.com/", branch: "master"
