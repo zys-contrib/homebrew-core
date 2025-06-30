e6336faa110195ad544c2f530cc560ab38d9daf1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-30 13:18:11 +0000

    rsql: add 0.19.0 bottle.

--- Formula/r/rsql.rb
+++ Formula/r/rsql.rb
@@ -6,6 +6,15 @@ class Rsql < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/theseus-rs/rsql.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea669ada67646a584f8e2710050903185e7a81f977df63ffc17b01e9c8c13843"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aed527eb81dad75fae3883db1b60760f2793622aea6ba9fd55d205232a373f76"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3e3abb78b28b21eebba40d8c5f06bb4d0446080cc36fb2d77c55eb75aa70a9ec"
+    sha256 cellar: :any_skip_relocation, sonoma:        "302b943217f5ddf884acca6d92ae2eca70e182d903355cdd905613fb3ecfdc94"
+    sha256 cellar: :any_skip_relocation, ventura:       "04eab41ab71c7ce385c5c40e289570d4358ec3f44d1dd5b432deef6a6ebe8834"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad1cfd407f2020aa2afa33c07c4b8e397eff3a84e68fbc4214fe1c01518acf66"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
