3f5dc24a60bde891c62ce31e750d9d037836d493
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-01 07:58:11 +0000

    stencil: update 2.5.3 bottle.

--- Formula/s/stencil.rb
+++ Formula/s/stencil.rb
@@ -7,13 +7,13 @@ class Stencil < Formula
   head "https://github.com/rgst-io/stencil.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ef9a76d7b1187558e2e2e548a86f21d116c7188a19318debf90dfc1eb7da65b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "11c9cfb6a7e9eaeefe89431d1e15ab27e3ae6ee758b6c2371479ba6ba90f6ef9"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "458c416958051e90ebc648a5d6e5300bb345eef0d2af682718adc21195e49bc0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7262b6eb393593ff73b153e83afe9154a8b8742b907f7e219e5590b622efb2b2"
-    sha256 cellar: :any_skip_relocation, ventura:       "3f70a6dcb1a99d8b3f4be6f1e78fb7d5e416eb167df45aa19c742583dfbb7e32"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0239e6634ac8ae5766c8e50eff0c206bf222fd31e60e650cbfbc7949d3471226"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8abbee13c3bfd532037ea18bd5d1aa8006a39b2d5a5a8e5c391d3610ccd8d84c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa101b222ff5d267a90cda63048c51c8b6855591c4fc601e7f91b2abf689797e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "39845f95598ea113ce69027b818aa7a20eba07281e060bc42b3faf448eaf3971"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1ab9910f5670fa3898eacfce42fdc63b4cc667324a4107ff5bf8313fb370330c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b697461b2ebaeb6ef3990d2f1dbf85e53517f9be0c622dafdb3cd6f62fff3ebb"
+    sha256 cellar: :any_skip_relocation, ventura:       "a29f210529d0af53a9038a8b6f7726a0a2fc0a2baa7ea23f4ec71e7b316aadc3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7b6edcc24e73a13ca8bd468fc0536a59ea21b30d64f78e6793a70ed6c2dd461d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f1b665cb214d9fd772f3496cf7e4549745937abfaa07583f8047759aa727985"
   end
 
   depends_on "go" => :build
