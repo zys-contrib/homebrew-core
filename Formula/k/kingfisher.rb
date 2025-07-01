47309a393b0ade86377de91548cf2e80d06e384b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-01 20:58:07 +0000

    kingfisher: update 1.18.1 bottle.

--- Formula/k/kingfisher.rb
+++ Formula/k/kingfisher.rb
@@ -6,13 +6,13 @@ class Kingfisher < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35c66e54243a8ecc47e5e1bf028541461fe136b31c2234fc72e3623d5bcbd8da"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "41fb293bf17f1b0c96efc53fe08d68df45efaedec0dd99d54563188f26520ad6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3a7f5c95f81f8f872ec42d8b446b214fc867ea3500a8a02f7b54d66da883d996"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f77813a283bb541873ab8c4ee00b370da04e3455607f9792ef7099dd5793f108"
-    sha256 cellar: :any_skip_relocation, ventura:       "4057278b7acc29ad2b1a02a7191b52c1bbe7c110796fd02ff2511fba42005a17"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5632f7b110a70e93702f7012cc2bd2444d8fb473079311f05b1784922e4fd102"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "03f59c93e4f552c9cb124fa45f4cece10cd771c5ea4172a8bc25b812e36396e2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "057c527f4fd697a3e5f1e3b053d0bd5abb219c98ae0fb1816b3f6ed8e8b89776"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a42075334de10a8a3eb902dadcd82f96f60088462f7155fef80d4e2735beb831"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ccc6418ef64d2adf745aaf5b27f3115cd8d99265fa43a1fc13af0fa07b725565"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3d21ca43f4364f919713db0da4525c0c6dc2a102d5ad5a9b9b545a2cf4739a15"
+    sha256 cellar: :any_skip_relocation, ventura:       "6f8cc42e405f923ba093795b431ea5272d45f1c57c8ce21f7beb91aca21de7c6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "989a88e82b34d09330fa9a28674245a6826b2e8057f7f67928d60f070643ff56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "560719e64b3fa18e58c31caf64ec91f8ca148f7383a8785951feced9db14c3ad"
   end
 
   depends_on "boost" => :build
