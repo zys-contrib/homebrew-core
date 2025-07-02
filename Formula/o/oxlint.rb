3b225514be548febdef5acaf0989bcc9560533a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-02 02:31:53 +0000

    oxlint: update 1.4.0 bottle.

--- Formula/o/oxlint.rb
+++ Formula/o/oxlint.rb
@@ -21,13 +21,13 @@ class Oxlint < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7674697d7bebe28b8be2204eb32587d97f6da8be0cf15d9197c78d4e3b47c520"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a7eb52ba7ad2a798ed8a0f594c937c08f22f00d1036e19b793a552237239c52c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a794c0b06beb7de35df729f78e7b8f41fa067bfd3888968090568e6e0082da6c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a5d15f9e9b28510d034ed8b6ac6777051b3940ab6fb1dcdd8e043ac0e82454ee"
-    sha256 cellar: :any_skip_relocation, ventura:       "d9d5cfa20ec071509044592a6a683d26af9a962fd69dbe556cc59518f7d143c3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8286e58843e98a8576650b1b0ef541c2eac6d7d67ce637c2a0af1efb0c693297"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "859234fcbced88031760d367bd44e3d0bd16c2730ee86382ae6aa96ddc621495"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "200f1a0d54821ccbeed3aed5c8e3a9a7e830ce854f461332ca5ddb9ca5c50a9f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "55f11e1304fdacfff6c629688e06d95b481a0f6a9ded04985bbd1daa166513df"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f743d4acc94792d76426d817ab49abbe9e328c45c40578c4b265a8b9a9f2fa0a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7a0a5b5cdf31edb2da10576a5f770f4efa0939e08c5e0ea181d187e27567bec8"
+    sha256 cellar: :any_skip_relocation, ventura:       "c63688a1fcb4cbb3d8135fbaf567e09c564c7eebb65b6736325a5fa73be84f17"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "53754260dc96a55bfee776f9b7371df34c4240d3a81a35c0f41a9070e80a85aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e43659c3e8bc8c6a4bd82640b153154ce83e8dae06d0c43cdec82ce0ef10b55"
   end
 
   depends_on "rust" => :build
