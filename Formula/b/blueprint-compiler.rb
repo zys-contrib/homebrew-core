08b7189ec52515c051abe57d715403b1b1c31ea3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-02 10:24:27 +0000

    blueprint-compiler: update 0.18.0 bottle.

--- Formula/b/blueprint-compiler.rb
+++ Formula/b/blueprint-compiler.rb
@@ -8,13 +8,13 @@ class BlueprintCompiler < Formula
   head "https://gitlab.gnome.org/GNOME/blueprint-compiler.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "934e915f724789737e2aff49d9607966366342257941e1f44ee5d03c358d331a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "934e915f724789737e2aff49d9607966366342257941e1f44ee5d03c358d331a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "934e915f724789737e2aff49d9607966366342257941e1f44ee5d03c358d331a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6bea1eb3d4da51a7e9e8ee222b7c8dec211a3cbffc00932d1e26ffc6645d499e"
-    sha256 cellar: :any_skip_relocation, ventura:       "6bea1eb3d4da51a7e9e8ee222b7c8dec211a3cbffc00932d1e26ffc6645d499e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "43d7f2741a5fa0dd074c78f6eac27decea1c8105ed1f9c53892ca7f3d2e7c10e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dd8299938011b24bc84df5f61ec14fea43ccd470ada9f70e92a14e195a138a8b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "046469ad95ca3a3f99e314ec0d0bfc926b4ecc3f1901a05c586b9166db3b564b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "046469ad95ca3a3f99e314ec0d0bfc926b4ecc3f1901a05c586b9166db3b564b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "046469ad95ca3a3f99e314ec0d0bfc926b4ecc3f1901a05c586b9166db3b564b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cef2f0f835f83500bc1668e917d095315b9fb82a669705848d5e9de3eba5b64f"
+    sha256 cellar: :any_skip_relocation, ventura:       "cef2f0f835f83500bc1668e917d095315b9fb82a669705848d5e9de3eba5b64f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "02cf8b504b6e344146768692bbd18be7a5bc26df926f7dabb09459b147d1d3e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02cf8b504b6e344146768692bbd18be7a5bc26df926f7dabb09459b147d1d3e1"
   end
 
   depends_on "meson" => :build
