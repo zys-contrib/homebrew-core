4aec95c1f1f503ddacf94067962337798acb4148
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-02 20:56:49 +0000

    vercel-cli: update 44.2.9 bottle.

--- Formula/v/vercel-cli.rb
+++ Formula/v/vercel-cli.rb
@@ -6,13 +6,13 @@ class VercelCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "759ed6cef3f496b985bdd048cf4e33b7969a8e3070d452e136400854ce02627b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "759ed6cef3f496b985bdd048cf4e33b7969a8e3070d452e136400854ce02627b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "759ed6cef3f496b985bdd048cf4e33b7969a8e3070d452e136400854ce02627b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ba233ace03590b1b6d386591c00a3b637f089071fbf0a2169ea1c71302970ce8"
-    sha256 cellar: :any_skip_relocation, ventura:       "ba233ace03590b1b6d386591c00a3b637f089071fbf0a2169ea1c71302970ce8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c9fb1fd54b001172afdbba9be4e12736054a9aa20c354e6de2446f1e5f2dc66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ba8be8b041b6c54b4e2ead79ceedbe710b22339af6ec9ce542f9da58bc2440fa"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d1b53adc381eeea0ae5530c60f9b12955e8f630cd526308fd229d95d96189700"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d1b53adc381eeea0ae5530c60f9b12955e8f630cd526308fd229d95d96189700"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d1b53adc381eeea0ae5530c60f9b12955e8f630cd526308fd229d95d96189700"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0ee335c0c3bf141cfe1de2ca6275a597bb9d08705b9280d2041c611b4ad81c6e"
+    sha256 cellar: :any_skip_relocation, ventura:       "0ee335c0c3bf141cfe1de2ca6275a597bb9d08705b9280d2041c611b4ad81c6e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c3042b98fa418bad080634cfe6a58a2355f99f9f0e599a58599e2e8b83d0d586"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c8909cb7d04489a69e6ac999eadc2f5846d8f6168fe65c357487e440380691d"
   end
 
   depends_on "node"
