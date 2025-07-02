563a3a1dba11ef795c45dd7980af08fa38409c7f
Rui Chen <rui@chenrui.dev>
2025-07-02 19:39:35 -0400

    turso 0.1.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/turso.rb
@@ -0,0 +1,44 @@
+class Turso < Formula
+  desc "Interactive SQL shell for Turso"
+  homepage "https://github.com/tursodatabase/turso"
+  url "https://github.com/tursodatabase/turso/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "04ea555053f6d2a85cbf07e9f8eccd4dcbdeeb0e0c914941783db59513ec1ded"
+  license "MIT"
+  head "https://github.com/tursodatabase/turso.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "rust" => :build
+  uses_from_macos "sqlite" => :test
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "cli")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tursodb --version")
+
+    # Fails in Linux CI with "Error: I/O error: Operation not permitted (os error 1)"
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    data = %w[Bob 14 Sue 12 Tim 13]
+    create = "create table students (name text, age integer);\n"
+    data.each_slice(2) do |n, a|
+      create << "insert into students (name, age) values ('#{n}', '#{a}');\n"
+    end
+    pipe_output("sqlite3 school.sqlite", create, 0)
+
+    begin
+      output_log = testpath/"output.log"
+      pid = spawn bin/"tursodb", "school.sqlite", [:out, :err] => output_log.to_s
+      sleep 2
+      assert_match "\".help\" for usage hints.", output_log.read
+    ensure
+      Process.kill("TERM", pid)
+      Process.wait(pid)
+    end
+  end
+end
