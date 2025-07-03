07635b3f0f915598bd4d6613836164e3889c99b5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 17:16:31 +0000

    mods 1.8.0
    mods: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    mods: add build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mods.rb
+++ Formula/m/mods.rb
@@ -1,8 +1,8 @@
 class Mods < Formula
   desc "AI on the command-line"
   homepage "https://github.com/charmbracelet/mods"
-  url "https://github.com/charmbracelet/mods/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "44e15c13d70d74369467df4d18b0a5ad9d977b344e76a39898002a04712271ca"
+  url "https://github.com/charmbracelet/mods/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "d8877258877408e90889385f1d3106278e71d56223e08e35dc60b120c95c903d"
   license "MIT"
   head "https://github.com/charmbracelet/mods.git", branch: "main"
 
@@ -17,19 +17,27 @@ class Mods < Formula
 
   depends_on "go" => :build
 
+  # skip db init for `--version` and `--help` commands
+  # upstream pr ref, https://github.com/charmbracelet/mods/pull/543
+  patch do
+    url "https://github.com/charmbracelet/mods/commit/d18b03d0306116108d5bc50f58a4e81b6480cb74.patch?full_index=1"
+    sha256 "b1ae4388376787219ebdc5b1be97b2222beb83043f467588ec48ead2154be342"
+  end
+
   def install
     ldflags = "-s -w -X main.Version=#{version} -X main.CommitSHA=#{tap.user} -X main.CommitDate=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"mods", "completion")
+    generate_completions_from_executable(bin/"mods", "completion", shells: [:bash, :zsh, :fish, :pwsh])
   end
 
   test do
     ENV["OPENAI_API_KEY"] = "faketest"
 
     output = pipe_output(bin/"mods 2>&1", "Hello, Homebrew!", 1)
-    assert_match "ERROR  Invalid openai API key", output
+    assert_match "ERROR  Could not open database", output
 
-    assert_match version.to_s, shell_output(bin/"mods --version")
+    assert_match version.to_s, shell_output("#{bin}/mods --version")
+    assert_match "GPT on the command line", shell_output("#{bin}/mods --help")
   end
 end
