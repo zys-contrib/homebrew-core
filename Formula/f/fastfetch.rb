1a0203f875cad79191fa3fadace577f4b840cf21
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 07:56:36 +0000

    fastfetch 2.47.0

--- Formula/f/fastfetch.rb
+++ Formula/f/fastfetch.rb
@@ -1,8 +1,8 @@
 class Fastfetch < Formula
   desc "Like neofetch, but much faster because written mostly in C"
   homepage "https://github.com/fastfetch-cli/fastfetch"
-  url "https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/2.46.0.tar.gz"
-  sha256 "7e4e4ab8d3e0e2ad896f97effc58df4c8d23bb88273bbdf1221bd8f0a4beb736"
+  url "https://github.com/fastfetch-cli/fastfetch/archive/refs/tags/2.47.0.tar.gz"
+  sha256 "c1482b8075718a2b2db95f823419b04b81384237fdba8af56092f184e89797d3"
   license "MIT"
   head "https://github.com/fastfetch-cli/fastfetch.git", branch: "dev"
 
