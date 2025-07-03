f8a7eec4a0c3c835f57c7feb24a135e7312c26c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 17:52:17 +0000

    soft-serve: update 0.9.0 bottle.

--- Formula/s/soft-serve.rb
+++ Formula/s/soft-serve.rb
@@ -7,12 +7,12 @@ class SoftServe < Formula
   head "https://github.com/charmbracelet/soft-serve.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "559974917d8f906fd451f58a24b16f5d50bb3806785b7bc9f003e628e2e34d59"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "299e9067d96c78b4cea98c8b29c5429cee8ed376937523057cc348571e536439"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ae92b211b6025dcba653a8752cf2615ba3d92110f8b9363b3d8cb39246cd07aa"
-    sha256 cellar: :any_skip_relocation, sonoma:        "21e1fca746da038795826c4b64a0ee3d9696a7e941127665a0b90cf2889c1b23"
-    sha256 cellar: :any_skip_relocation, ventura:       "7d7bed45e73c6c2bff333d9b684e75c95460322099314c8319f1916cb22f33fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38dfd97a35a041d749cf6adef5060c6f35b4b9bf876e0c7ea4aefa972c92c966"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d89338a0132682d796a9a2da0048e3bd1fe99a3ef2ab4be94252b8bc7df71e33"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "109d924ca9f0c99d61809875c38adc91eef9a4eb32009b8d7fbace9ed323966f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7a0f8246eb5333a79806dd80ea192c25b6887f160d5a0c13611fec759d917b5a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "310fd7f17c0168b861a6b5cf4d5f6061c70bb476fb0f41b6c64c6ded090a4442"
+    sha256 cellar: :any_skip_relocation, ventura:       "291ab0287c0258feebe938a12f2d999a45418a68bdb9c9e282e1e0cae92d1a70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1fa85c0043c5cded505120bc5fd65d883b0e55019d7d4306b5c1752a3c0452d8"
   end
 
   depends_on "go" => :build
