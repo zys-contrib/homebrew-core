98fd3e8161c664df8a23ab4350b238e2c42b59ad
Branch Vincent <branchevincent@gmail.com>
2025-07-02 20:21:11 -0700

    gitmoji: fix prefix replacement

--- Formula/g/gitmoji.rb
+++ Formula/g/gitmoji.rb
@@ -13,13 +13,15 @@ class Gitmoji < Formula
 
   def install
     system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+
+    # Ensure we have uniform bottles.
     files = ["global-directory/index.d.ts", "npm-run-path/node_modules/path-key/index.d.ts",
              "path-key/index.d.ts", "xdg-basedir/index.d.ts", "xdg-basedir/index.js",
              "npm-run-path/index.d.ts", "global-directory/index.js", "@pnpm/npm-conf/lib/defaults.js"]
     files.each do |file|
-      inreplace libexec/"lib/node_modules/gitmoji-cli/node_modules/#{file}", "/usr/local", "@@HOMEBREW_PREFIX@@"
+      inreplace libexec/"lib/node_modules/gitmoji-cli/node_modules/#{file}", "/usr/local", HOMEBREW_PREFIX
     end
-    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
