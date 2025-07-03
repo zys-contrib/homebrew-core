9ca1424dc8b345e2874191da49add359ea416c61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 20:09:20 +0000

    openfga 1.9.0

--- Formula/o/openfga.rb
+++ Formula/o/openfga.rb
@@ -1,8 +1,8 @@
 class Openfga < Formula
   desc "High performance and flexible authorization/permission engine"
   homepage "https://openfga.dev/"
-  url "https://github.com/openfga/openfga/archive/refs/tags/v1.8.16.tar.gz"
-  sha256 "be00254a940c5effac7a6b6fa521be1c40d1df9dd65554d050654c45453f116f"
+  url "https://github.com/openfga/openfga/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "b7c37da1f8b59f40e833b29179b2bf1ccd34cc04f5ab499f00e62cdff0c4042f"
   license "Apache-2.0"
   head "https://github.com/openfga/openfga.git", branch: "main"
 
