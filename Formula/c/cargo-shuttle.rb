7715332b02f8057b6e600324359bb1589e1ec1fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-04 13:53:40 +0000

    cargo-shuttle 0.56.1

--- Formula/c/cargo-shuttle.rb
+++ Formula/c/cargo-shuttle.rb
@@ -1,8 +1,8 @@
 class CargoShuttle < Formula
   desc "Build & ship backends without writing any infrastructure files"
   homepage "https://shuttle.dev"
-  url "https://github.com/shuttle-hq/shuttle/archive/refs/tags/v0.56.0.tar.gz"
-  sha256 "3194debf1fd1b559ef1f17a8c116813140078acc00fde5fd04310e9af33c8e67"
+  url "https://github.com/shuttle-hq/shuttle/archive/refs/tags/v0.56.1.tar.gz"
+  sha256 "3676bd29d727f2be55207deaba130cf3323cbf4c9715be8e4639808265bfaa76"
   license "Apache-2.0"
   head "https://github.com/shuttle-hq/shuttle.git", branch: "main"
 
