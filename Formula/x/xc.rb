95bda73f0b42fd38024eced70430bf0b069f6808
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-04 11:23:05 +0000

    xc 0.8.6

--- Formula/x/xc.rb
+++ Formula/x/xc.rb
@@ -1,8 +1,8 @@
 class Xc < Formula
   desc "Markdown defined task runner"
   homepage "https://xcfile.dev/"
-  url "https://github.com/joerdav/xc/archive/refs/tags/v0.8.5.tar.gz"
-  sha256 "374b3d4fe19355a1bc5ba63fd8bc346f027e6a1dbb04af631683ca45a24d806a"
+  url "https://github.com/joerdav/xc/archive/refs/tags/v0.8.6.tar.gz"
+  sha256 "142c28aafa454b461b2950d980f0a8a5d89b59fb9032bbe23ca1015472bff691"
   license "MIT"
   head "https://github.com/joerdav/xc.git", branch: "main"
 
