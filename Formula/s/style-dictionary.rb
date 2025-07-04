081a0dd66805e34c87b71f03ac6fb5074d5e3bdb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-04 16:09:49 +0000

    style-dictionary: update 5.0.1 bottle.

--- Formula/s/style-dictionary.rb
+++ Formula/s/style-dictionary.rb
@@ -6,13 +6,13 @@ class StyleDictionary < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8c57727da1d5247d0df445e3a713f4f4b05c079de39097f7305e7294a0ed889f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8c57727da1d5247d0df445e3a713f4f4b05c079de39097f7305e7294a0ed889f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8c57727da1d5247d0df445e3a713f4f4b05c079de39097f7305e7294a0ed889f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0ea502e55352ddc783a2fe366c25fca345b98097f825c71434d0071c102cdfc0"
-    sha256 cellar: :any_skip_relocation, ventura:       "0ea502e55352ddc783a2fe366c25fca345b98097f825c71434d0071c102cdfc0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8c57727da1d5247d0df445e3a713f4f4b05c079de39097f7305e7294a0ed889f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c57727da1d5247d0df445e3a713f4f4b05c079de39097f7305e7294a0ed889f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "46fd27ccde57b8903f0c0dd1fc9cc84383e1033c8168a029bcf73e4643a6cc2d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "46fd27ccde57b8903f0c0dd1fc9cc84383e1033c8168a029bcf73e4643a6cc2d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "46fd27ccde57b8903f0c0dd1fc9cc84383e1033c8168a029bcf73e4643a6cc2d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5c7f4c7e7e61c50cc76645599b626beae8db5cc3ac11557af42475d43eac11e4"
+    sha256 cellar: :any_skip_relocation, ventura:       "5c7f4c7e7e61c50cc76645599b626beae8db5cc3ac11557af42475d43eac11e4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "46fd27ccde57b8903f0c0dd1fc9cc84383e1033c8168a029bcf73e4643a6cc2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46fd27ccde57b8903f0c0dd1fc9cc84383e1033c8168a029bcf73e4643a6cc2d"
   end
 
   depends_on "node"
