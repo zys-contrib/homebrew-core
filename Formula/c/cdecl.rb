95918103a3661d5f93495346f1ba9eb433d11833
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-04 16:56:16 +0000

    cdecl 18.5

--- Formula/c/cdecl.rb
+++ Formula/c/cdecl.rb
@@ -1,8 +1,8 @@
 class Cdecl < Formula
   desc "Turn English phrases to C or C++ declarations"
   homepage "https://github.com/paul-j-lucas/cdecl"
-  url "https://github.com/paul-j-lucas/cdecl/releases/download/cdecl-18.4.2/cdecl-18.4.2.tar.gz"
-  sha256 "7ab5a9241734153d7d81b29787914b3120d23fdfa5c3a2e2fdde26208a35435c"
+  url "https://github.com/paul-j-lucas/cdecl/releases/download/cdecl-18.5/cdecl-18.5.tar.gz"
+  sha256 "e4e212db3f997a9afe629fa4868062e0a92d6dd79ff374eca07b331220936362"
   license all_of: [
     "GPL-3.0-or-later",
     "LGPL-2.1-or-later", # gnulib
