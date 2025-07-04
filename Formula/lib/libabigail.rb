83c3d422cc8bc2b0f9f17ee3699be6278143454e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-04 16:15:09 +0000

    libabigail: update 2.8 bottle.

--- Formula/lib/libabigail.rb
+++ Formula/lib/libabigail.rb
@@ -11,8 +11,8 @@ class Libabigail < Formula
   end
 
   bottle do
-    sha256 arm64_linux:  "7de8d14d00c8437fa48029c8b8ba3a4cdd7e9d7730d30f130b57d98f1ba77f76"
-    sha256 x86_64_linux: "c285f7fac9ec608cbde9846f968103f6c8df6a94c786399ee2c66e270d1fb631"
+    sha256 arm64_linux:  "ea4bb90e1a4f2243dbb3da8b41c639b8f86cf26b31a81a0ed0f78fbd5b2dbfb9"
+    sha256 x86_64_linux: "107b3688c8a17ab46cc159f6e28401af834ad1175590d6a57f601e659fe9752d"
   end
 
   head do
